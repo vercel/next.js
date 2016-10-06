7a39dac8c7addadbcb480ebdbccf84812dc901d4
Dan Zajdband <dan.zajdband@gmail.com>
2016-10-05 22:42:55 -0400

    Added css using aphrodite

--- server/render.js
+++ server/render.js
@@ -4,6 +4,7 @@ import { renderToString, renderToStaticMarkup } from 'react-dom/server'
 import fs from 'mz/fs'
 import Document from '../lib/document'
 import App from '../lib/app'
+import { StyleSheetServer } from '../lib/css'
 
 export async function render (path, req, res, { root = process.cwd() } = {}) {
   const mod = require(resolve(root, '.next', 'pages', path)) || {}
@@ -17,15 +18,20 @@ export async function render (path, req, res, { root = process.cwd() } = {}) {
   const bundlePath = resolve(root, '.next', '.next', 'pages', path || 'index.js')
   const component = await fs.readFile(bundlePath, 'utf8')
 
-  const app = createElement(App, {
-    Component,
-    props,
-    router: {}
+  const { html, css } = StyleSheetServer.renderStatic(() => {
+    const app = createElement(App, {
+      Component,
+      props,
+      router: {}
+    })
+
+    return renderToString(app)
   })
 
   const doc = createElement(Document, {
     head: [],
-    html: renderToString(app),
+    html: html,
+    css: css,
     data: { component },
     hotReload: false
   })
