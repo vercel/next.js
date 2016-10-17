3c44137b9b3fb922c328fb0514b7c061284ab19c
nkzawa <naoyuki.kanezawa@gmail.com>
2016-10-17 23:35:31 +0900

    improve hot-reloading

--- server/build/loaders/hot-self-accept-loader.js
+++ server/build/loaders/hot-self-accept-loader.js
@@ -1,14 +1,23 @@
+import { resolve, relative } from 'path'
 
 module.exports = function (content) {
   this.cacheable()
+
+  const route = getRoute(this)
 
   return content + `
     if (module.hot) {
       module.hot.accept()
       if ('idle' !== module.hot.status()) {
         const Component = module.exports.default || module.exports
-        next.router.notify({ Component })
+        next.router.update('${route}', Component)
       }
     }
   `
+}
+
+function getRoute (loaderContext) {
+  const pagesDir = resolve(loaderContext.options.context, 'pages')
+  const path = loaderContext.resourcePath
+  return '/' + relative(pagesDir, path).replace(/((^|\/)index)?\.js$/, '')
 }
