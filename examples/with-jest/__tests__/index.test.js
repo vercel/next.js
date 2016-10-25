00cc7155cfdca712c563da99478d8362bf5676fe
Pavithra K <pksjce@gmail.com>
2016-10-26 00:10:27 +0530

    Adds jest configuration npm run test shows failed results

---
+++ examples/with-jest/__tests__/index.test.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from '../pages/index.js';
+
+it('App shows "Hello world!"', () => {
+  const app = shallow(
+    <App/>
+  );
+
+  expect(app.find('p').text()).toEqual('Hello world!');
+});
