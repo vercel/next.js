9a78dd8d1c32ca4dbfef166e91d172ebe7708c4b
nkzawa <naoyuki.kanezawa@gmail.com>
2016-10-26 19:28:41 +0900

    fix jest example styles

--- examples/with-jest/__tests__/index.test.js
+++ examples/with-jest/__tests__/index.test.js
@@ -1,12 +1,12 @@
-
-import React from 'react';
-import {shallow} from 'enzyme';
-import App from '../pages/index.js';
+/* global it, expect */
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from '../pages/index.js'
 
 it('App shows "Hello world!"', () => {
   const app = shallow(
-    <App/>
-  );
+    <App />
+  )
 
-  expect(app.find('p').text()).toEqual('Hello world!');
-});
+  expect(app.find('p').text()).toEqual('Hello world!')
+})
