a1337320ea99b5e941d2b6bb2416fd973a3ae445
nkzawa <naoyuki.kanezawa@gmail.com>
2016-11-14 13:56:29 +0900

    remove unused code

--- client/next.js
+++ client/next.js
@@ -3,14 +3,13 @@ import { render } from 'react-dom'
 import HeadManager from './head-manager'
 import { rehydrate } from '../lib/css'
 import Router from '../lib/router'
-import DefaultApp from '../lib/app'
+import App from '../lib/app'
 import evalScript from '../lib/eval-script'
 
 const {
-  __NEXT_DATA__: { app, component, props, ids, err }
+  __NEXT_DATA__: { component, props, ids, err }
 } = window
 
-const App = app ? evalScript(app).default : DefaultApp
 const Component = evalScript(component).default
 
 export const router = new Router(window.location.href, { Component, ctx: { err } })
