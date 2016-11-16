59a8a4a813db68d0d504eebb16cc8111d5ce4f98
nkzawa <naoyuki.kanezawa@gmail.com>
2016-11-17 05:00:26 +0900

    render Error component on client error

--- client/next.js
+++ client/next.js
@@ -7,12 +7,17 @@ import App from '../lib/app'
 import evalScript from '../lib/eval-script'
 
 const {
-  __NEXT_DATA__: { component, props, ids, err }
+  __NEXT_DATA__: { component, errorComponent, props, ids, err }
 } = window
 
 const Component = evalScript(component).default
+const ErrorComponent = evalScript(errorComponent).default
 
-export const router = new Router(window.location.href, { Component, ctx: { err } })
+export const router = new Router(window.location.href, {
+  Component,
+  ErrorComponent,
+  ctx: { err }
+})
 
 const headManager = new HeadManager()
 const container = document.getElementById('__next')
