83187281ac53b3dbfdd5193da68c2f02bd4fd52e
Jared Palmer <jaredloganpalmer@gmail.com>
2016-12-12 22:53:04 -0500

    Add custom-server-express example (#352)
    * Add custom-server-express example
    
    * Remove extraneous nexts in express routes defs
    
    * Update next config in server.js

---
+++ examples/custom-server-express/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const next = require('next')
+const accepts = require('accepts')
+
+const app = next({ dir: '.', dev: true })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.use((req, res, next) => {
+    const accept = accepts(req)
+    const type = accept.type(['json', 'html'])
+    if (type === 'json') {
+      return handle(req, res)
+    }
+    next()
+  })
+
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/b', req.query)
+  })
+
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/a', req.query)
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
