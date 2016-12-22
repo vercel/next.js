b8291abeaca54f4b2640b7bcc5ec98bedb250423
nkzawa <naoyuki.kanezawa@gmail.com>
2016-12-22 17:10:17 +0900

    improve the styled-component example

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -2,14 +2,11 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import styleSheet from 'styled-components/lib/models/StyleSheet'
 
 export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
-    const renderPage = () => {
-      return ctx.renderPage()
-    }
-
-    const props = await Document.getInitialProps({ ...ctx, renderPage })
+  static async getInitialProps ({ renderPage }) {
+    styleSheet.flush()
+    const page = renderPage()
     const style = styleSheet.rules().map(rule => rule.cssText).join('\n')
-    return { ...props, style }
+    return { ...page, style }
   }
 
   render () {
