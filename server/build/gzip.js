c834551db56424509a5d375252aadf25bfe59b57
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-12-29 23:24:15 +0530

    Add AOT gzip content-encoding support. Currently we only do this for main.js and commons.js only.

---
+++ server/build/gzip.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+import zlib from 'zlib'
+
+export default async function gzipAssets (dir) {
+  const nextDir = path.resolve(dir, '.next')
+
+  await gzip(path.resolve(nextDir, 'commons.js'))
+  await gzip(path.resolve(nextDir, 'main.js'))
+}
+
+export function gzip (filePath) {
+  const input = fs.createReadStream(filePath)
+  const output = fs.createWriteStream(`${filePath}.gz`)
+
+  return new Promise((resolve, reject) => {
+    const stream = input.pipe(zlib.createGzip()).pipe(output)
+    stream.on('error', reject)
+    stream.on('finish', resolve)
+  })
+}
