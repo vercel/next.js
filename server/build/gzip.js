3af72f19ba8e3b317bbd5fd9f9a4fe8b153faed0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-12-30 17:33:35 +0530

    Use glob-promise instead of recursive-readdir

--- server/build/gzip.js
+++ server/build/gzip.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import zlib from 'zlib'
-import rreaddir from 'recursive-readdir'
+import glob from 'glob-promise'
 
 export default async function gzipAssets (dir) {
   const nextDir = path.resolve(dir, '.next')
@@ -10,8 +10,12 @@ export default async function gzipAssets (dir) {
     path.join(nextDir, 'commons.js'),
     path.join(nextDir, 'main.js')
   ]
-  const pages = await getAllPages(nextDir)
-  const allAssets = [...coreAssets, ...pages]
+  const pages = await glob('bundles/pages/**/*.js', { cwd: nextDir })
+
+  const allAssets = [
+    ...coreAssets,
+    ...pages.map(page => path.join(nextDir, page))
+  ]
 
   while (true) {
     // gzip only 10 assets in parallel at a time.
@@ -31,16 +35,5 @@ export function gzip (filePath) {
     const stream = input.pipe(zlib.createGzip()).pipe(output)
     stream.on('error', reject)
     stream.on('finish', resolve)
-  })
-}
-
-async function getAllPages (nextDir) {
-  const pagesDir = path.join(nextDir, 'bundles', 'pages')
-
-  return new Promise((resolve, reject) => {
-    rreaddir(pagesDir, (err, pages) => {
-      if (err) return reject(err)
-      resolve(pages)
-    })
   })
 }
