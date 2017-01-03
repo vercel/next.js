30093ca415f8b71bfccb9e827101fc979642a1ba
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-01-03 14:08:16 +0530

    Add isolated tests for server/resolve

--- server/resolve.js
+++ server/resolve.js
@@ -30,8 +30,8 @@ function getPaths (id) {
 
   if (i[i.length - 1] === sep) {
     return [
-      i + 'index.json',
-      i + 'index.js'
+      i + 'index.js',
+      i + 'index.json'
     ]
   }
 
