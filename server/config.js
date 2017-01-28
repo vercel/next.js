fa07f9d49e8ce25c8fdacde1ee03d644b4becd8b
George Pantazis <gpantazis@thumbtack.com>
2017-01-28 10:37:56 -0800

    Make file-system based routes optional for custom servers

--- server/config.js
+++ server/config.js
@@ -5,7 +5,8 @@ const cache = new Map()
 
 const defaultConfig = {
   webpack: null,
-  poweredByHeader: true
+  poweredByHeader: true,
+  useFileSystemPublicRoutes: true
 }
 
 export default function getConfig (dir) {
--- server/index.js
+++ server/index.js
@@ -100,9 +100,11 @@ export default class Server {
       '/static/:path+': async (req, res, params) => {
         const p = join(this.dir, 'static', ...(params.path || []))
         await this.serveStatic(req, res, p)
-      },
+      }
+    }
 
-      '/:path*': async (req, res) => {
+    if (this.config.useFileSystemPublicRoutes) {
+      routes['/:path*'] = async (req, res) => {
         const { pathname, query } = parse(req.url, true)
         await this.render(req, res, pathname, query)
       }
