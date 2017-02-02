3df8808e450222fbea109b6deda1f5907d384f97
Luke Edwards <luke.edwards05@gmail.com>
2017-02-01 16:18:40 -0800

    start flyfile conversion

---
+++ flyfile.js
@@ -0,0 +1,161 @@
+const {resolve} = require('path')
+const webpack = require('webpack')
+const childProcess = require('child_process')
+const isWindows = /^win/.test(process.platform)
+
+exports.compile = function * (fly) {
+  // yield fly.parallel(['compile-bin', 'compile-server', 'compile-lib', 'compile-client'])
+  yield fly.serial(['compile-bin', 'compile-server', 'compile-lib', 'compile-client'])
+  yield fly.start('remove-strict-mode')
+}
+
+exports['compile-bin'] = function * (fly, opts) {
+  yield fly.source(opts.src || 'bin/*').babel().target('dist/bin')
+  // .pipe(notify('Compiled binaries'))
+}
+
+exports['compile-lib'] = function * (fly, opts) {
+  yield fly.source(opts.src || 'lib/**/*.js').babel().target('dist/lib')
+  // .pipe(notify('Compiled lib files'))
+}
+
+exports['compile-server'] = function * (fly, opts) {
+  yield fly.source(opts.src || 'server/**/*.js').babel().target('dist/server')
+  // .pipe(notify('Compiled server files'))
+}
+
+exports['compile-client'] = function * (fly, opts) {
+  yield fly.source(opts.src || 'client/**/*.js').babel().target('dist/client')
+  // .pipe(notify('Compiled client files'))
+}
+
+exports['compile-bench'] = function * (fly, opts) {
+  yield fly.source(opts.src || 'bench/*.js').babel().target('dist/bench')
+  // .pipe(notify('Compiled bench files'))
+}
+
+exports['remove-strict-mode'] = function * (fly) {
+  yield fly.source('dist/lib/eval-script.js').babel({
+    babelrc: false,
+    plugins: ['babel-plugin-transform-remove-strict-mode']
+  }).target('dist/lib')
+  // .pipe(notify('Completed removing strict mode for eval script'))
+}
+
+exports.copy = function * (fly) {
+  yield fly.source('pages/**/*.js').target('dist/pages')
+}
+
+exports['copy-bench-fixtures'] = function * (fly) {
+  yield fly.source('bench/fixtures/**/*').target('dist/bench/fixtures')
+}
+
+exports.build = function * (fly) {
+  // yield fly.parallel(['copy', 'compile'])
+  // yield fly.start('build-prefetcher')
+  yield fly.serial(['copy', 'compile', 'build-prefetcher'])
+}
+
+exports['build-prefetcher'] = function * (fly) {
+  yield Promise.resolve()
+  // webpack({
+  //   context: resolve(__dirname, 'client'),
+  //   entry: './next-prefetcher.js',
+  //   output: {
+  //     filename: '[name]-bundle.js',
+  //     path: resolve(__dirname, 'dist/client')
+  //   },
+  //   plugins: [
+  //     new webpack.DefinePlugin({
+  //       'process.env': {
+  //         NODE_ENV: JSON.stringify('production')
+  //       }
+  //     })
+  //   ],
+  //   module: {
+  //     rules: [{
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       loader: 'babel-loader',
+  //       options: {
+  //         babelrc: false,
+  //         presets: [
+  //           ['env', {
+  //             targets: {
+  //               // All browsers which supports service workers
+  //               browsers: ['chrome 49', 'firefox 49', 'opera 41']
+  //             }
+  //           }]
+  //         ]
+  //       }
+  //     }]
+  //   }
+  // })
+  // .pipe(notify('Built release prefetcher'))
+}
+
+exports.bench = function * (fly) {
+  console.log('inside bench');
+  yield Promise.resolve()
+// ['compile', 'copy', 'compile-bench', 'copy-bench-fixtures'], () => {
+  // yield fly.source('dist/bench/*.js').benchmark({
+    // benchmark.reporters.etalon('RegExp#test')
+  // })
+}
+
+// exports.watch = function * (fly) {
+// }
+
+exports.default = function * (fly) {
+  yield fly.watch('bin/*', 'compile-bin')
+  yield fly.watch('pages/**/*.js', 'copy')
+  yield fly.watch('server/**/*.js', 'compile-server')
+  yield fly.watch('{client,lib}/**/*.js', ['compile-client', 'compile-lib', 'build-prefetcher'])
+  // yield fly.serial(['build', 'watch'])
+  // yield fly.start('watch')
+}
+
+exports.release = function * (fly) {
+  yield fly.clear('dist').start('build')
+}
+
+// We run following task inside a NPM script chain and it runs chromedriver
+// inside a child process tree.
+// Even though we kill this task's process, chromedriver exists throughout
+// the lifetime of the original npm script.
+
+// gulp.task('start-chromedriver', ['stop-chromedriver'], (cb) => {
+//   const processName =  isWindows? 'chromedriver.cmd' : 'chromedriver'
+//   const chromedriver = childProcess.spawn(processName, { stdio: 'inherit' })
+
+//   const timeoutHandler = setTimeout(() => {
+//     // We need to do this, otherwise this task's process will keep waiting.
+//     process.exit(0)
+//   }, 2000)
+// })
+
+// gulp.task('stop-chromedriver', () => {
+//   try {
+//     if (isWindows) {
+//       childProcess.execSync('taskkill /im chromedriver* /t /f', { stdio: 'ignore' })
+//     } else {
+//       childProcess.execSync('pkill chromedriver', { stdio: 'ignore' })
+//     }
+//   } catch(ex) {
+//     // Do nothing
+//   }
+// })
+
+// // avoid logging to the console
+// // that we created a notification
+// notify_.logLevel(0)
+
+// // notification helper
+// function notify (msg) {
+//   return notify_({
+//     title: '▲ Next',
+//     message: msg,
+//     icon: false,
+//     onLast: true
+//   })
+// }
