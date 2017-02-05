f6510c05b26934e544fad2c48c93caed7ac2de58
Emanuel Kluge <kraemerseele@gmx.de>
2017-02-05 16:38:20 +0100

    Pass parsed request-URL into the run-method (#1000)

--- examples/custom-server-hapi/next-wrapper.js
+++ examples/custom-server-hapi/next-wrapper.js
@@ -2,7 +2,7 @@ const pathWrapper = (app, pathName, opts) => ({ raw, query }, hapiReply) =>
 app.renderToHTML(raw.req, raw.res, pathName, query, opts)
 .then(hapiReply)
 
-const defaultHandlerWrapper = app => ({ raw }, hapiReply) =>
-app.run(raw.req, raw.res)
+const defaultHandlerWrapper = app => ({ raw, url }, hapiReply) =>
+app.run(raw.req, raw.res, url)
 
 module.exports = { pathWrapper, defaultHandlerWrapper }
