85ee482e2811a0fcc000bd3f8e3d182d452cc73b
David Brown <david@devanco.com>
2017-02-05 15:34:57 +0100

    use single quotes even in React components

--- examples/with-pretty-url-routing/pages/index.js
+++ examples/with-pretty-url-routing/pages/index.js
@@ -4,9 +4,9 @@ export default function IndexPage () {
   return (
     <div>
       <h1>Homepage</h1>
-      <form method="GET" action="/greeting">
-        Name: <input name="name" />
-        <input type="submit" />
+      <form method='GET' action='/greeting'>
+        Name: <input name='name' />
+        <input type='submit' />
       </form>
     </div>
   )
