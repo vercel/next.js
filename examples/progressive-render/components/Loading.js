3ced357da36a9274671048fb90c4e2f8fa3c8008
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-02-05 06:44:12 -0500

    [add] example of a progressive rendered app

---
+++ examples/progressive-render/components/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default () => (
+  <div>
+    <h3>Loading...</h3>
+    <style jsx>{`
+      div {
+        align-items: center;
+        display: flex;
+        height: 50vh;
+        justify-content: center;
+      }
+    `}</style>
+  </div>
+)
