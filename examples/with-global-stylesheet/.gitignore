516167f637618a35450ddc8648e35f8fddf02d64
Davide Bertola <dade@dadeb.it>
2017-02-06 19:38:39 +0100

    add global stylesheet example

---
+++ examples/with-global-stylesheet/.gitignore
@@ -0,0 +1,2 @@
+.next
+node_modules
---
+++ examples/with-global-stylesheet/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import style from './style.scss'
+// or, if you work with plain css
+// import style from './style.css'
+
+export default () =>
+  <div>
+    <style>{ style }</style>
+    <p>ciao</p>
+  </div>
