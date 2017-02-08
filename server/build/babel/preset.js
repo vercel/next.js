5e19c7fa870b11b9505331ccd0f35baaff6e8b18
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-02-08 09:00:16 +0530

    Remove babel-plugin-transform-async-to-generator in favor of babel-plugin-latest It has es2017 preset and it comes with the above plugin.

--- server/build/babel/preset.js
+++ server/build/babel/preset.js
@@ -10,7 +10,6 @@ module.exports = {
   ],
   plugins: [
     require.resolve('babel-plugin-react-require'),
-    require.resolve('babel-plugin-transform-async-to-generator'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
     require.resolve('babel-plugin-transform-class-properties'),
     require.resolve('babel-plugin-transform-runtime'),
