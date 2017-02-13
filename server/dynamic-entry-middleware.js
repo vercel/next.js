93b0967e8b49fe2eedc64a9ec8eb264f3b9f8959
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-02-14 00:54:40 +0530

    Add the first version of middleware but not tested.

--- server/dynamic-entry-middleware.js
+++ server/dynamic-entry-middleware.js
@@ -1,52 +1,89 @@
-import { join } from 'path'
 import DynamicEntryPlugin from 'webpack/lib/DynamicEntryPlugin'
+import { EventEmitter } from 'events'
 
 export default function dynamicEntryMiddleware (devMiddleware, compiler, { dir, dev }) {
-  // LRU cache
-  const entries = []
-  let makeCallbacks = []
-  let doneCallbacks = []
+  // TODO: Make this a LRU cache
+  const entries = {}
+  let doingEntries = {}
+  let completedEntries = {}
 
-  // Based on the http request to add an entry. Add it and invalidate.
-  setInterval(function () {
-    devMiddleware.invalidate()
-  }, 5000)
-
-  const defaultEntries = dev === 'development'
-    ? [join(__dirname, '..', 'client/webpack-hot-middleware-client.js')] : []
+  const doneCallbacks = new EventEmitter()
 
   compiler.plugin('make', function (compilation, done) {
-    entries.forEach(() => null)
+    const allEntries = Object.keys(entries).map((name) => {
+      doingEntries[name] = true
+      const entry = entries[name]
+      return addEntry(compilation, name, entry)
+    })
 
-    const entry = [
-      ...defaultEntries,
-      join(dir, 'pages', 'index.js?entry')
-    ]
+    Promise.all(allEntries)
+      .then(() => done())
+      .catch(done)
 
-    const name = 'bundles/pages/index.js'
-
-    compilation.addEntry(this.context, DynamicEntryPlugin.createDependency(entry, name), name, done)
-    console.log('Adding entries...')
-    compilation
-    // Add all entries and move makeCallbacks to doneCallbacks
-    doneCallbacks = [
-      ...doneCallbacks,
-      ...makeCallbacks
-    ]
-    makeCallbacks = []
-    // done()
+    console.log('MAKE')
   })
 
   compiler.plugin('done', function (stats) {
     // Call all the doneCallbacks
-    doneCallbacks.forEach((fn) => fn())
-    console.log('Done!')
+    Object.keys(doingEntries).forEach((name) => {
+      doneCallbacks.emit(name)
+    })
+
+    completedEntries = doingEntries
+    doingEntries = {}
+
+    console.log('DONE')
   })
 
   // Add the entries to the map and invalidate
   // Set the next to nextCallbacks
   return function (req, res, next) {
-    console.log('req', req.url)
-    next()
+    if (req.method !== 'POST') return next()
+    if (req.url !== '/webpack-config/add_entry') return next()
+
+    reqToData(req, (err, data) => {
+      if (err) {
+        console.error(err.stack)
+        res.statusCode = 500
+        res.end('Internal Error')
+        return
+      }
+
+      if (completedEntries[data.name]) {
+        next()
+        return
+      }
+
+      if (entries[data.name]) {
+        doneCallbacks.on(data.name, next)
+        return
+      }
+
+      entries[data.name] = data.entry
+      doneCallbacks.on(data.name, next)
+
+      devMiddleware.invalidate()
+    })
   }
+}
+
+function addEntry (compilation, name, entry) {
+  return new Promise((resolve, reject) => {
+    const dep = DynamicEntryPlugin.createDependency(entry, name)
+    compilation.addEntry(this.context, dep, name, (err) => {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
+}
+
+function reqToData (req, cb) {
+  if (req.data) return cb(null, req.data)
+
+  let jsonString = ''
+  req.on('data', (data) => {
+    jsonString += data.toString()
+  })
+  req.on('end', () => cb(null, JSON.parse(jsonString)))
+  req.on('error', (err) => cb(err))
 }
