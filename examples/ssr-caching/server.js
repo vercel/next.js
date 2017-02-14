de77426962d68b31c720aa3a3581684ab43be04b
Sebastien Dubois <sdubois@protonmail.com>
2017-02-14 18:27:30 +0200

    simplify comment

--- examples/ssr-caching/server.js
+++ examples/ssr-caching/server.js
@@ -37,7 +37,7 @@ app.prepare()
 })
 
 /*
- * NB: make sure to modify this to return an object taking into account anything that should trigger
+ * NB: make sure to modify this to take into account anything that should trigger
  * an immediate page change (e.g a locale stored in req.session)
  */
 function getCacheKey (req) {
