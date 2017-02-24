f345503ff703078e52f389edf08c9485fa0f78e4
Steve Fan <stevefan@Steves-MacBook-Air.local>
2017-02-24 14:26:43 +0800

    fix standard style

--- examples/with-react-helmet/pages/_document.js
+++ examples/with-react-helmet/pages/_document.js
@@ -3,41 +3,41 @@ import Helmet from 'react-helmet'
 
 export default class extends Document {
   static async getInitialProps ({ renderPage }) {
-      const page = renderPage()
+    const page = renderPage()
 
       // see https://github.com/nfl/react-helmet#server-usage for more information
       // 'head' was occupied by 'page.head', we cannot use it
-      return { ...page, helmetHead: Helmet.rewind() }
+    return { ...page, helmetHead: Helmet.rewind() }
   }
 
   // should render on <html>
-  get helmetHtmlAttrComponents() {
+  get helmetHtmlAttrComponents () {
     return this.props.helmetHead.htmlAttributes.toComponent()
   }
 
   // should render on <head>
-  get helmetHeadComponents() {
+  get helmetHeadComponents () {
     return Object.keys(this.props.helmetHead)
-      .filter(el => el != "htmlAttributes") // remove htmlAttributes which is not for <head> but for <html>
+      .filter(el => el !== 'htmlAttributes') // remove htmlAttributes which is not for <head> but for <html>
       .map(el => this.props.helmetHead[el].toComponent())
   }
 
-  get helmetJsx() {
+  get helmetJsx () {
     return (<Helmet
-      title="Hello next.js!"
+      title='Hello next.js!'
     />)
   }
 
   render () {
     return (<html {...this.helmetHtmlAttrComponents}>
-       <Head>
-         { this.helmetJsx }
-         { this.helmetHeadComponents }
-       </Head>
-       <body>
-           <Main />
-           <NextScript />
-       </body>
+      <Head>
+        { this.helmetJsx }
+        { this.helmetHeadComponents }
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
     </html>)
   }
 }
--- examples/with-react-helmet/pages/index.js
+++ examples/with-react-helmet/pages/index.js
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 export default () => (<div>
   Hello World!
 </div>)
