e24db68f8b7e755fa61594492199bb8f95f8d1ed
Eric Ferraiuolo <edf@ericf.me>
2017-02-24 16:45:18 -0500

    Add example app with React Intl (#1055)
    * Add example app with React Intl
    
    Fixes #1022
    
    * Update examples/with-react-intl/package.json to be consistent

---
+++ examples/with-react-intl/pages/about.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import {FormattedRelative} from 'react-intl'
+import pageWithIntl from '../components/PageWithIntl'
+import Layout from '../components/Layout'
+
+class About extends Component {
+  static async getInitialProps ({req}) {
+    return {someDate: Date.now()}
+  }
+
+  render () {
+    return (
+      <Layout>
+        <p>
+          <FormattedRelative
+            value={this.props.someDate}
+            updateInterval={1000}
+          />
+        </p>
+      </Layout>
+    )
+  }
+}
+
+export default pageWithIntl(About)
