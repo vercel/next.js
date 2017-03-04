93425f49d5720470df73638dda85be4c072aa0c6
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-03-04 07:44:57 +0530

    Change the polyfill location.

--- client/next.js
+++ client/next.js
@@ -1,12 +1,3 @@
 import next from './'
 
-// Polyfill Promise globally
-// This is needed because Webpack2's dynamic loading(common chunks) code
-// depends on Promise.
-// So, we need to polyfill it.
-// See: https://github.com/webpack/webpack/issues/4254
-if (!window.Promise) {
-  window.Promise = Promise
-}
-
 next()
