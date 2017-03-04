be183c024a59f384dd4ae70412cfa217aceced90
tim neutkens <tim@weprovide.com>
2017-03-04 12:00:37 +0100

    Use module-alias to alias preact server side

--- examples/using-preact/next.config.js
+++ examples/using-preact/next.config.js
@@ -4,6 +4,16 @@ module.exports = {
       'react': 'preact-compat/dist/preact-compat',
       'react-dom': 'preact-compat/dist/preact-compat'
     }
+
+    // Disable uglify. This has been fixed in https://github.com/developit/preact-compat/issues/155.
+    // Can be removed once there is a new preact-compat release.
+    config.plugins = config.plugins.filter((plugin) => {
+      if (plugin.constructor.name === 'UglifyJsPlugin') {
+        return false
+      } else {
+        return true
+      }
+    })
     return config
   }
 }
---
+++ examples/using-preact/server.js
@@ -0,0 +1,32 @@
+const moduleAlias = require('module-alias')
+
+moduleAlias.addAlias('react', 'preact-compat')
+moduleAlias.addAlias('react-dom', 'preact-compat')
+
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/b', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/a', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  })
+  .listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
