5947716ff7828278d4febb095c7f98db40a17454
Tim Neutkens <tim@timneutkens.nl>
2017-03-05 02:47:56 +0100

    Implement preact/inferno SSR (#1346)
    * Use module-alias to alias preact server side
    
    * Use module-alias to alias inferno server side
    
    * Remove unneeded routes example

--- examples/using-inferno/next.config.js
+++ examples/using-inferno/next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  webpack: function (config) {
+  webpack: function (config, { dev }) {
+    // For the development version, we'll use React.
+    // Because, it support react hot loading and so on.
+    if (dev) {
+      return config
+    }
+
     config.resolve.alias = {
       'react': 'inferno-compat',
       'react-dom': 'inferno-compat'
--- examples/using-preact/next.config.js
+++ examples/using-preact/next.config.js
@@ -1,9 +1,26 @@
 module.exports = {
-  webpack: function (config) {
+  webpack: function (config, { dev }) {
+    // For the development version, we'll use React.
+    // Because, it support react hot loading and so on.
+    if (dev) {
+      return config
+    }
+
     config.resolve.alias = {
       'react': 'preact-compat/dist/preact-compat',
       'react-dom': 'preact-compat/dist/preact-compat'
     }
+
+    // Disable uglify. This has been fixed in https://github.com/developit/preact-compat/issues/155.
+    // Can be removed once there is a new preact-compat release.
+    config.plugins = config.plugins.filter((plugin) => {
+      if (plugin.constructor.name === 'UglifyJsPlugin') {
+        return false
+      } else {
+        return true
+      }
+    })
+
     return config
   }
 }
