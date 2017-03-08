a6f172c6d4a1e371e9fb629c669ceb9211698fe0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-03-08 11:57:12 +0530

    Add some debug logs.

--- test/lib/next-webdriver.js
+++ test/lib/next-webdriver.js
@@ -1,10 +1,12 @@
 import wd from 'wd'
 
 export default async function (appPort, pathname) {
+  console.warn(`> Start loading browser with path: ${pathname}`)
   const url = `http://localhost:${appPort}${pathname}`
 
   const browser = wd.promiseChainRemote('http://localhost:9515/')
   await browser.init({browserName: 'chrome'}).get(url)
 
+  console.warn(`> Complete loading browser with path: ${pathname}`)
   return browser
 }
