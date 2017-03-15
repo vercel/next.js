db7c2867403921fab451c5051f8fa1608e8eb199
BDav24 <BDav24@users.noreply.github.com>
2017-03-15 15:24:54 +0100

    [POC] Pretty url routing (#1001)
    * [example] with pretty url routing
    
    * use single quotes even in React components
    
    * improve Link import

---
+++ examples/with-pretty-url-routing/pages/greeting.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'next-url-prettifier'
+import {Router} from '../routes'
+
+export default class GreetingPage extends React.Component {
+  static getInitialProps ({query: {lang, name}}) {
+    return {lang, name}
+  }
+
+  renderSwitchLangageLink () {
+    const {lang, name} = this.props
+    const switchLang = lang === 'fr' ? 'en' : 'fr'
+    return (
+      <Link route={Router.linkPage('greeting', {name, lang: switchLang})}>
+        <a>{switchLang === 'fr' ? 'Français' : 'English'}</a>
+      </Link>
+    )
+  }
+
+  render () {
+    const {lang, name} = this.props
+    return (
+      <div>
+        <h1>{lang === 'fr' ? 'Bonjour' : 'Hello'} {name}</h1>
+        <div>{this.renderSwitchLangageLink()}</div>
+      </div>
+    )
+  }
+}
+
+GreetingPage.propTypes = {
+  lang: React.PropTypes.string,
+  name: React.PropTypes.string
+}
