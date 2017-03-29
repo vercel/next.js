deeaf6ab919fbc03da3033a1808dd96e98136a65
Tomas Roos <ptomasroos@gmail.com>
2017-03-29 09:28:48 +0200

    auto format with standard instead of prettier :( fixes broken test

--- examples/with-mobx/server.js
+++ examples/with-mobx/server.js
@@ -1,20 +1,20 @@
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production'
 
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const mobxReact = require('mobx-react');
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+const mobxReact = require('mobx-react')
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-mobxReact.useStaticRendering(true);
+mobxReact.useStaticRendering(true)
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
   }).listen(3000, err => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
-});
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
