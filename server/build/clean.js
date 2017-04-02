f5817d7005e97a3cd7298ed7475169fc58639880
Alex Newman <Alex.newman@bedegaming.com>
2017-04-02 19:04:14 +0100

    Remove lint errors

--- server/build/clean.js
+++ server/build/clean.js
@@ -4,5 +4,5 @@ import getConfig from '../config'
 
 export default function clean (dir) {
   const dist = getConfig(dir).options.dist
-  return del(resolve(dir, dist)
+  return del(resolve(dir, dist))
 }
