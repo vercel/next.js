6f65a053a85b84f602beae95409199ae042b05ea
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-04-05 08:21:20 +0530

    Fix ondemand test cases.

--- test/integration/ondemand/test/index.test.js
+++ test/integration/ondemand/test/index.test.js
@@ -31,13 +31,13 @@ describe('On Demand Entries', () => {
   })
 
   it('should compile pages for JSON page requests', async () => {
-    const pageContent = await renderViaHTTP(context.appPort, '/_next/-/pages/about')
+    const pageContent = await renderViaHTTP(context.appPort, '/_next/-/page/about')
     expect(pageContent.includes('About Page')).toBeTruthy()
   })
 
   it('should dispose inactive pages', async () => {
     await renderViaHTTP(context.appPort, '/_next/-/pages/about')
-    const aboutPagePath = resolve(__dirname, '../.next/bundles/pages/about.json')
+    const aboutPagePath = resolve(__dirname, '../.next/client-bundles/pages/about.js')
     expect(existsSync(aboutPagePath)).toBeTruthy()
 
     // Wait maximum of jasmine.DEFAULT_TIMEOUT_INTERVAL checking
