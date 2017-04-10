8e6615dcf925a2bfb67e79c0b68e55b86670643d
C. T. Lin <chentsulin@gmail.com>
2017-04-11 02:35:26 +0800

    upgrade react to v15.5 and use prop-types instead of React.PropTypes (#1684)
    * upgrade react to v15.5 and use prop-types instead of React.PropTypes
    
    * Update package.json

--- server/document.js
+++ server/document.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import htmlescape from 'htmlescape'
 import flush from 'styled-jsx/server'
 
