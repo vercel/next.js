0259a1adce9aa0f81f53be624490713b7222f32e
paul <paul.christophe6@gmail.com>
2017-04-12 18:25:20 -0400

    setting messages on the client side

--- examples/with-firebase/pages/index.js
+++ examples/with-firebase/pages/index.js
@@ -14,18 +14,30 @@ export default class Index extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      user: this.props.user
+      user: this.props.user,
+      value: '',
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     firebase.initializeApp({
       // firebase client config goes here
+      apiKey: "AIzaSyDB4PTmby-vRWeU3XUayR6oSpZ5q56cBp8",
+      authDomain: "test-56060.firebaseapp.com",
+      databaseURL: "https://test-56060.firebaseio.com",
+      projectId: "test-56060",
+      storageBucket: "test-56060.appspot.com",
+      messagingSenderId: "622813644360"
+    })
+
+    firebase.database().ref('messages').on('value', snap => {
+      this.setState({ messages: snap.val() })
     })
 
     firebase.auth().onAuthStateChanged(user => {
-      console.log('user', user);
-
       if (user) {
         this.setState({ user: user })
         return user.getToken()
@@ -38,10 +50,7 @@ export default class Index extends Component {
               credentials: 'same-origin',
               body: JSON.stringify({token})
             })
-          })
-          .then((res) => {
-            console.log(res);
-          })
+          }).then((res) => console.log(res))
       } else {
         this.setState({ user: null })
         fetch('/api/logout', {
@@ -50,6 +59,21 @@ export default class Index extends Component {
         })
       }
     })
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const date = new Date().getTime()
+    firebase.database().ref(`messages/${date}`).set({
+      id: date,
+      text: this.state.value,
+    });
+    this.setState({ value: '' });
   }
 
   handleLogin() {
@@ -61,12 +85,32 @@ export default class Index extends Component {
   }
 
   render() {
-    const { user } = this.state
+    const { user, value, messages } = this.state
+
     return <div>
       {
         user
         ? <button onClick={this.handleLogout}>Logout</button>
         : <button onClick={this.handleLogin}>Login</button>
+      }
+      {
+        user &&
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              onChange={this.handleChange}
+              placeholder="add message"
+              value={value}
+            />
+          </form>
+          <ul>
+            {
+              messages &&
+              Object.keys(messages).map(key => <li key={key}>{messages[key].text}</li>)
+            }
+          </ul>
+        </div>
       }
     </div>
   }
