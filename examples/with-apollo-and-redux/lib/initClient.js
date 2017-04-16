f03e1964add8e233e7f1419fa7e76386cf87c31d
Ademola Adegbuyi <ooade96@gmail.com>
2017-04-16 22:14:17 +0100

    Update Apollo examples

--- examples/with-apollo-and-redux/lib/initClient.js
+++ examples/with-apollo-and-redux/lib/initClient.js
@@ -1,8 +1,6 @@
 import { ApolloClient, createNetworkInterface } from 'react-apollo'
 
-let apolloClient = null
-
-function _initClient (headers, initialState) {
+export const initClient = (headers, initialState = {}) => {
   return new ApolloClient({
     initialState,
     ssrMode: !process.browser,
@@ -15,14 +13,4 @@ function _initClient (headers, initialState) {
       }
     })
   })
-}
-
-export const initClient = (headers, initialState = {}) => {
-  if (!process.browser) {
-    return _initClient(headers, initialState)
-  }
-  if (!apolloClient) {
-    apolloClient = _initClient(headers, initialState)
-  }
-  return apolloClient
 }
