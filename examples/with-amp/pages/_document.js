6106155037c78ee3954a8ade3ad9f2bd5380d99e
Dan Zajdband <dan.zajdband@gmail.com>
2017-04-18 22:44:16 -0500

    Fix lint

--- examples/with-amp/pages/_document.js
+++ examples/with-amp/pages/_document.js
@@ -5,7 +5,7 @@ import { properties as DOMProperties } from 'react-dom/lib/DOMProperty'
 // By default React limit the set of valid DOM elements and attributes
 // (https://github.com/facebook/react/issues/140) this config whitelist
 // Amp elements/attributes
-if (typeof DOMProperties.amp == 'undefined') {
+if (typeof DOMProperties.amp === 'undefined') {
   DOMProperty.injectDOMPropertyConfig({
     Properties: { amp: DOMProperty.MUST_USE_ATTRIBUTE },
     isCustomAttribute: attributeName => attributeName.startsWith('amp-')
