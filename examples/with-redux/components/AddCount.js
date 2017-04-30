2e7bc1074d517abea8d9bb5a4db7f800b2293a38
spencer <spencer.bigum@gmail.com>
2017-04-30 16:44:24 -0400

    Update to examples: with-redux (updated) (#1813)
    * With-Redux-example-update-request
    
    Hello Next.js,
    
    I’ve added an additional example to “With-Redux” and updated some of
    the original code to help illustrate to less inexperienced developers
    how to implement Redux with Next.js.
    
    The example is a simple counter to help reinforce how the client and
    server renderings work together. In addition I also updated some of the
    redux boilerplate code to help fully demonstrate how redux can be
    implemented when using is with Next.js
    
    Please contact me at spencer.bigum@gmail.com for further questions or
    anything else you might need.
    
    Thanks,
    Spencer
    
    * fixed listing issues: examples/with-redux
    
    * Updated code based on @impronunciable Feedback

---
+++ examples/with-redux/components/AddCount.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { addCount } from '../store'
+
+class AddCount extends Component {
+  add = () => {
+    this.props.addCount()
+  }
+
+  render () {
+    const { count } = this.props
+    return (
+      <div>
+        <style jsx>{`
+          div {
+            padding: 0 0 20px 0;
+          }
+      `}</style>
+        <h1>AddCount: <span>{count}</span></h1>
+        <button onClick={this.add}>Add To Count</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ count }) => ({ count })
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCount: bindActionCreators(addCount, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddCount)
