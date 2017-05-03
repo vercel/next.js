e97a7c2f1f52fb6655d2bef741b3b9ae823ece5d
Simeon Theobald <simeon@theobald.nz>
2017-05-03 23:56:30 +1200

    linting

--- examples/with-aphrodite/pages/_document.js
+++ examples/with-aphrodite/pages/_document.js
@@ -10,8 +10,8 @@ export default class MyDocument extends Document {
 
   constructor (props) {
     super(props)
-    /* Take the renderedClassNames from aphrodite (as generated 
-    in getInitialProps) and assign them to __NEXT_DATA__ so that they 
+    /* Take the renderedClassNames from aphrodite (as generated
+    in getInitialProps) and assign them to __NEXT_DATA__ so that they
     are accessible to the client for rehydration. */
     const { __NEXT_DATA__, ids } = props
     if (ids) {
@@ -20,9 +20,9 @@ export default class MyDocument extends Document {
   }
 
   render () {
-    /* Make sure to use data-aphrodite attribute in the style tag here 
+    /* Make sure to use data-aphrodite attribute in the style tag here
     so that aphrodite knows which style tag it's in control of when
-    the client goes to render styles. If you don't you'll get a second 
+    the client goes to render styles. If you don't you'll get a second
     <style> tag */
     return (
       <html>
