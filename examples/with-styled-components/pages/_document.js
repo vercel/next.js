1b893518bbdfda487c0c7fa2042c206c432016bc
Grokling <Grokling@users.noreply.github.com>
2017-05-08 00:25:33 +1200

    Add SSR to examples/with-styled-components + bump styled_components  v2 (#1908)

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -1,23 +1,21 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-import styleSheet from 'styled-components/lib/models/StyleSheet'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps ({ renderPage }) {
-    const page = renderPage()
-    const styles = (
-      <style dangerouslySetInnerHTML={{ __html: styleSheet.rules().map(rule => rule.cssText).join('\n') }} />
-    )
-    return { ...page, styles }
-  }
-
   render () {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
     return (
       <html>
         <Head>
           <title>My page</title>
+          {styleTags}
         </Head>
         <body>
-          <Main />
+          <div className='root'>
+            {main}
+          </div>
           <NextScript />
         </body>
       </html>
