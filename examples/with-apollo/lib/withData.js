f4d6cbfc197e1b8d1674e10a9821ba5387cc2032
Roland Warmerdam <rowno@webspirited.com>
2017-05-10 15:23:11 -0700

    Many improvements to the Apollo examples (#1905)
    * Many improvements to the Apollo examples
    
    * Use static properties

--- examples/with-apollo/lib/withData.js
+++ examples/with-apollo/lib/withData.js
@@ -1,50 +1,66 @@
-import 'isomorphic-fetch'
 import React from 'react'
+import PropTypes from 'prop-types'
 import { ApolloProvider, getDataFromTree } from 'react-apollo'
-import { initClient } from './initClient'
+import initApollo from './initApollo'
 
-export default (Component) => (
-  class extends React.Component {
+export default ComposedComponent => {
+  return class WithData extends React.Component {
+    static displayName = `WithData(${ComposedComponent.displayName})`
+    static propTypes = {
+      serverState: PropTypes.object.isRequired
+    }
+
     static async getInitialProps (ctx) {
-      const headers = ctx.req ? ctx.req.headers : {}
-      const client = initClient(headers)
+      let serverState = {}
 
-      const props = {
-        url: { query: ctx.query, pathname: ctx.pathname },
-        ...await (Component.getInitialProps ? Component.getInitialProps(ctx) : {})
+      // Evaluate the composed component's getInitialProps()
+      let composedInitialProps = {}
+      if (ComposedComponent.getInitialProps) {
+        composedInitialProps = await ComposedComponent.getInitialProps(ctx)
       }
 
+      // Run all graphql queries in the component tree
+      // and extract the resulting data
       if (!process.browser) {
+        const apollo = initApollo()
+        // Provide the `url` prop data in case a graphql query uses it
+        const url = {query: ctx.query, pathname: ctx.pathname}
+
+        // Run all graphql queries
         const app = (
-          <ApolloProvider client={client}>
-            <Component {...props} />
+          <ApolloProvider client={apollo}>
+            <ComposedComponent url={url} {...composedInitialProps} />
           </ApolloProvider>
         )
         await getDataFromTree(app)
+
+        // Extract query data from the Apollo's store
+        const state = apollo.getInitialState()
+
+        serverState = {
+          apollo: { // Make sure to only include Apollo's data state
+            data: state.data
+          }
+        }
       }
 
       return {
-        initialState: {
-          apollo: {
-            data: client.getInitialState().data
-          }
-        },
-        headers,
-        ...props
+        serverState,
+        ...composedInitialProps
       }
     }
 
     constructor (props) {
       super(props)
-      this.client = initClient(this.props.headers, this.props.initialState)
+      this.apollo = initApollo(this.props.serverState)
     }
 
     render () {
       return (
-        <ApolloProvider client={this.client}>
-          <Component {...this.props} />
+        <ApolloProvider client={this.apollo}>
+          <ComposedComponent {...this.props} />
         </ApolloProvider>
       )
     }
   }
-)
+}
