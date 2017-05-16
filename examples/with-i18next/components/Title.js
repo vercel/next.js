c912af8cf53e3808f5463d5c21a6f798c1ee4b1e
Ardani <master.ardani@gmail.com>
2017-05-16 22:48:33 +0700

    improve example fallback translation and add new sample locale for lang id

--- examples/with-i18next/components/Title.js
+++ examples/with-i18next/components/Title.js
@@ -1,2 +1,2 @@
 import { translate } from 'react-i18next'
-export default translate(['common'])((props) => (<h1>{props.t('hello')}</h1>))
+export default translate(['common'])((props) => (<h1>{props.t('hello')}, {props.t('morning')}</h1>))
