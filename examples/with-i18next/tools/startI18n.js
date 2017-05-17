443dceaeec307070ec1fa3974b8d201abff330fd
Ardani Rohman <ardani@users.noreply.github.com>
2017-05-18 00:41:54 +0700

    improve example fallback translation and add new sample locale for lang id (#1983)

--- examples/with-i18next/tools/startI18n.js
+++ examples/with-i18next/tools/startI18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
 
-const startI18n = file => i18n.init({
+const startI18n = (file, lang) => i18n.init({
+  lng: lang, // active language http://i18next.com/translate/
   fallbackLng: 'pt',
   resources: file,
   ns: ['common'],
