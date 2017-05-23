0a280a1b4b858ed92fd2aae92b19f61075546f9b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-05-23 09:42:55 +0200

    JavaScript standard

--- examples/with-react-toolbox/pages/index.js
+++ examples/with-react-toolbox/pages/index.js
@@ -1,13 +1,13 @@
-import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
 import theme from '../static/theme'
 import Head from 'next/head'
 
-import Button from 'react-toolbox/lib/button/Button';
+import Button from 'react-toolbox/lib/button/Button'
 
 export default () => (
   <div>
     <Head>
-      <link href="/static/theme.css" rel="stylesheet" />
+      <link href='/static/theme.css' rel='stylesheet' />
     </Head>
     <ThemeProvider theme={theme}>
       <Button raised primary>Hello</Button>
