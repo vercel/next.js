15e9573ded8448855591449a733a3e851def1a5b
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-05-25 18:36:21 +0200

    feat: add an example with the next branch of Material-UI (#2036)

---
+++ examples/with-material-ui-next/pages/_document.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
+import { getDefaultContext, setDefaultContext } from '../styles/createDefaultContext'
+
+export default class MyDocument extends Document {
+  static async getInitialProps (ctx) {
+    setDefaultContext()
+    const page = ctx.renderPage()
+    const styleContext = getDefaultContext()
+    return {
+      ...page,
+      styles: (
+        <style id='jss-server-side' type='text/css'>
+          {styleContext.styleManager.sheetsToString()}
+        </style>
+      )
+    }
+  }
+
+  render () {
+    const styleContext = getDefaultContext()
+    return (
+      <html lang='en'>
+        <Head>
+          <title>My page</title>
+          <meta charSet='utf-8' />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name='viewport'
+            content={
+              'user-scalable=0, initial-scale=1, maximum-scale=1, ' +
+                'minimum-scale=1, width=device-width, height=device-height'
+            }
+          />
+          {/* PWA primary color */}
+          <meta name='theme-color' content={styleContext.theme.palette.primary[500]} />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ examples/with-material-ui-next/styles/createDefaultContext.js
@@ -0,0 +1,38 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import createPalette from 'material-ui/styles/palette'
+import createMuiTheme from 'material-ui/styles/theme'
+import { purple, green } from 'material-ui/styles/colors'
+
+const createDefaultContext = () =>
+  MuiThemeProvider.createDefaultContext({
+    theme: createMuiTheme({
+      palette: createPalette({
+        primary: purple,
+        accent: green
+      })
+    })
+  })
+
+// Singleton hack as there is no way to pass variables from _document.js to pages yet.
+let context = null
+
+export function setDefaultContext () {
+  context = createDefaultContext()
+}
+
+export function getDefaultContext () {
+  // Make sure to create a new store for every server-side request so that data
+  // isn't shared between connections (which would be bad)
+  if (!process.browser) {
+    return context
+  }
+
+  // Reuse store on the client-side
+  if (!context) {
+    context = createDefaultContext()
+  }
+
+  return context
+}
+
+export default createDefaultContext
