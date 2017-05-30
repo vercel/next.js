539379bff2c6b15406bbc77aa233ae04f943bf6b
Watcharakrit Phantu <watcharakrit@gmail.com>
2017-05-31 01:11:12 +0700

    update: disable uglify for next build.
    add: require('fs') for next build.
    add: disable uglify on next.config.js for next build.

--- examples/with-scoped-stylesheets-and-postcss/next.config.js
+++ examples/with-scoped-stylesheets-and-postcss/next.config.js
@@ -1,7 +1,12 @@
+const fs = require('fs')
 const trash = require('trash')
 
 module.exports = {
   webpack: (config) => {
+    config.plugins = config.plugins.filter(
+      (plugin) => (plugin.constructor.name !== 'UglifyJsPlugin')
+    )
+
     config.module.rules.push(
       {
         test: /\.css$/,
