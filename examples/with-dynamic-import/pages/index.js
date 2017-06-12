3d87dad8e40b537c61644865eddeded4b43055f2
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-06-12 09:38:25 +0530

    Implement props aware dynamic bundle API.

--- examples/with-dynamic-import/pages/index.js
+++ examples/with-dynamic-import/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Router from 'next/router'
 import Header from '../components/Header'
 import Counter from '../components/Counter'
 import dynamic from 'next/dynamic'
@@ -23,35 +24,63 @@ const DynamicComponentWithAsyncReactor = asyncReactor(async () => {
 const DynamicComponent5 = dynamic(import('../components/hello5'))
 
 const DynamicBundle = dynamic({
-  modules: ({
-    Hello6: import('../components/hello6'),
-    Hello7: import('../components/hello7')
-  }),
+  modules: (props) => {
+    const components = {
+      Hello6: import('../components/hello6')
+    }
+
+    if (props.showMore) {
+      components.Hello7 = import('../components/hello7')
+    }
+
+    return components
+  },
   render: (props, { Hello6, Hello7 }) => (
     <div style={{padding: 10, border: '1px solid #888'}}>
       <Hello6 />
-      <Hello7 />
+      {Hello7 ? <Hello7 /> : null}
     </div>
   )
 })
 
-export default () => (
-  <div>
-    <Header />
-    <DynamicComponent />
-    <DynamicComponentWithCustomLoading />
-    <DynamicComponentWithNoSSR />
-    <DynamicComponentWithAsyncReactor />
-    <DynamicBundle />
-    {
-      /*
-        Since DynamicComponent5 does not render in the client,
-        it won't get downloaded.
-      */
-    }
-    { React.noSuchField === true ? <DynamicComponent5 /> : null }
+export default class Index extends React.Component {
+  static getInitialProps ({ query }) {
+    return { showMore: Boolean(query.showMore) }
+  }
 
-    <p>HOME PAGE is here!</p>
-    <Counter />
-  </div>
-)
+  toggleShowMore () {
+    const { showMore } = this.props
+    if (showMore) {
+      Router.push('/')
+      return
+    }
+
+    Router.push('/?showMore=1')
+  }
+
+  render () {
+    const { showMore } = this.props
+
+    return (
+      <div>
+        <Header />
+        <DynamicComponent />
+        <DynamicComponentWithCustomLoading />
+        <DynamicComponentWithNoSSR />
+        <DynamicComponentWithAsyncReactor />
+        <DynamicBundle showMore={showMore} />
+        <button onClick={() => this.toggleShowMore()}>Toggle Show More</button>
+        {
+          /*
+            Since DynamicComponent5 does not render in the client,
+            it won't get downloaded.
+          */
+        }
+        { React.noSuchField === true ? <DynamicComponent5 /> : null }
+
+        <p>HOME PAGE is here!</p>
+        <Counter />
+      </div>
+    )
+  }
+}
