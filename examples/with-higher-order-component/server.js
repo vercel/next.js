6470b373e95a17d984818a6009851fc7598f9eb3
ngocdungnguyen <ngocdungnguyen@chotot.vn>
2017-06-21 22:16:08 +0700

    completed example higher order component

---
+++ examples/with-higher-order-component/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.get('/', (req, res) => {
+    return app.render(req, res, '/index', req.query)
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
