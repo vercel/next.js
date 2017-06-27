b6ae342442b3f89955acd4efee5a678827c6d9cc
myxvisual <myxvisual@live.com>
2017-06-27 17:41:54 +0800

    feat: Add react-uwp examples

---
+++ examples/with-react-uwp/components/ThemeWrapper.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import Theme from 'react-uwp/Theme'
+
+export class ThemeWrapper extends Component {
+  render () {
+    const { children, style, ...props } = this.props;
+    console.log(props.theme)
+    return (
+      <Theme {...props} style={(props && props.theme) ? props.theme.prepareStyles(style) : void 0}>
+        {children}
+      </Theme>
+    )
+  }
+}
+
+export default ThemeWrapper
