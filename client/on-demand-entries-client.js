fc259cf985327f4b54f27503c824f253c506639f
Paul Reichelt <reicheltp@users.noreply.github.com>
2017-07-08 21:45:34 +0200

    Send credentials with on-demand-entries-ping (#2509)

--- client/on-demand-entries-client.js
+++ client/on-demand-entries-client.js
@@ -11,12 +11,16 @@ export default () => {
   async function ping () {
     try {
       const url = `/_next/on-demand-entries-ping?page=${Router.pathname}`
-      const res = await fetch(url)
+      const res = await fetch(url, {
+        credentials: 'same-origin'
+      })
       const payload = await res.json()
       if (payload.invalid) {
         // Payload can be invalid even if the page is not exists.
         // So, we need to make sure it's exists before reloading.
-        const pageRes = await fetch(location.href)
+        const pageRes = await fetch(location.href, {
+          credentials: 'same-origin'
+        })
         if (pageRes.status === 200) {
           location.reload()
         }
