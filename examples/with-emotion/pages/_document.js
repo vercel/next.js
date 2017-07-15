4df0bf7ff30b27d05b440c0f8e1da14f48642732
Matt Fysh <hi@mattfysh.com.au>
2017-07-15 17:11:01 +1000

    fix env determination

--- examples/with-emotion/pages/_document.js
+++ examples/with-emotion/pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { extractCritical } from 'emotion/server'
 import { flush } from 'emotion'
 
-const dev = process.env.NODE_ENV === 'production'
+const dev = process.env.NODE_ENV !== 'production'
 
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
