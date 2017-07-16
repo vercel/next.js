8a5678932790b48350656bf4ec02901d8a180cbb
Jan Muehlemann <jan.muehlemann@gmail.com>
2017-07-16 19:15:10 +0200

    lint example code

--- examples/with-react-i18next/components/ExtendedComponent.js
+++ examples/with-react-i18next/components/ExtendedComponent.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { translate } from 'react-i18next';
-import i18n from '../i18n';
+import React from 'react'
+import { translate } from 'react-i18next'
 
-function MyComponennt({ t }) {
+function MyComponennt ({ t }) {
   return (
     <div>
       {t('extendedComponent')}
     </div>
-  );
+  )
 }
 
-const Extended = translate('common')(MyComponennt);
+const Extended = translate('common')(MyComponennt)
 
-export default Extended;
+export default Extended
--- examples/with-react-i18next/i18n.js
+++ examples/with-react-i18next/i18n.js
@@ -1,6 +1,6 @@
-const i18n = require('i18next');
-const XHR = require('i18next-xhr-backend');
-const LanguageDetector = require('i18next-browser-languagedetector');
+const i18n = require('i18next')
+const XHR = require('i18next-xhr-backend')
+const LanguageDetector = require('i18next-browser-languagedetector')
 
 const options = {
   fallbackLng: 'en',
@@ -17,43 +17,43 @@ const options = {
     escapeValue: false, // not needed for react!!
     formatSeparator: ',',
     format: (value, format, lng) => {
-      if (format === 'uppercase') return value.toUpperCase();
-      return value;
+      if (format === 'uppercase') return value.toUpperCase()
+      return value
     }
   }
-};
+}
 
 // for browser use xhr backend to load translations and browser lng detector
 if (process.browser) {
   i18n
     .use(XHR)
     // .use(Cache)
-    .use(LanguageDetector);
+    .use(LanguageDetector)
 }
 
 // initialize if not already initialized
-if (!i18n.isInitialized) i18n.init(options);
+if (!i18n.isInitialized) i18n.init(options)
 
 // a simple helper to getInitialProps passed on loaded i18n data
 i18n.getInitialProps = (req, namespaces) => {
-  if (!namespaces) namespaces = i18n.options.defautlNS;
-  if (typeof namespaces === 'string') namespaces = [namespaces];
+  if (!namespaces) namespaces = i18n.options.defautlNS
+  if (typeof namespaces === 'string') namespaces = [namespaces]
 
-  req.i18n.toJSON = () => null; // do not serialize i18next instance and send to client
+  req.i18n.toJSON = () => null // do not serialize i18next instance and send to client
 
-  const initialI18nStore = {};
+  const initialI18nStore = {}
   req.i18n.languages.forEach((l) => {
-    initialI18nStore[l] = {};
+    initialI18nStore[l] = {}
     namespaces.forEach((ns) => {
-      initialI18nStore[l][ns] = req.i18n.services.resourceStore.data[l][ns] || {};
-    });
-  });
+      initialI18nStore[l][ns] = req.i18n.services.resourceStore.data[l][ns] || {}
+    })
+  })
 
   return {
     i18n: req.i18n, // use the instance on req - fixed language on request (avoid issues in race conditions with lngs of different users)
     initialI18nStore,
     initialLanguage: req.i18n.language
-  };
-};
+  }
+}
 
-module.exports = i18n;
+module.exports = i18n
