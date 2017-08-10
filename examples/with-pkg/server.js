cc5289a89f03f2db5097a1a3517bbbe37241f189
Sergio Xalambrí <sergiodxa@gmail.com>
2017-08-10 01:06:14 -0500

    Add with pkg example (#2751)

---
+++ examples/with-pkg/server.js
@@ -0,0 +1,16 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  createServer((req, res) => handle(req, res, parse(req.url, true).pathname))
+  .listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
