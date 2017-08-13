62ed18d67eedeab4d741d9099e4c71a9b0507c7e
Petr Vlček <petr.vlcek@gmail.com>
2017-08-13 02:34:11 +0200

    #1757 Relay Modern Example (#2696)

---
+++ examples/with-relay-modern/lib/createRelayEnvironment.js
@@ -0,0 +1,50 @@
+import { Environment, Network, RecordSource, Store } from 'relay-runtime'
+import fetch from 'isomorphic-unfetch'
+
+let relayEnvironment = null
+
+// Define a function that fetches the results of an operation (query/mutation/etc)
+// and returns its results as a Promise:
+function fetchQuery (
+    operation,
+    variables,
+    cacheConfig,
+    uploadables,
+) {
+  return fetch(process.env.RELAY_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }, // Add authentication and other headers here
+    body: JSON.stringify({
+      query: operation.text, // GraphQL text from input
+      variables
+    })
+  }).then(response => response.json())
+}
+
+export default function initEnvironment ({ records = {} } = {}) {
+    // Create a network layer from the fetch function
+  const network = Network.create(fetchQuery)
+  const store = new Store(new RecordSource(records))
+
+    // Make sure to create a new Relay environment for every server-side request so that data
+    // isn't shared between connections (which would be bad)
+  if (!process.browser) {
+    return new Environment({
+      network,
+      store
+    })
+  }
+
+    // reuse Relay environment on client-side
+  if (!relayEnvironment) {
+    relayEnvironment = new Environment({
+      network,
+      store
+    })
+  }
+
+  return relayEnvironment
+}
