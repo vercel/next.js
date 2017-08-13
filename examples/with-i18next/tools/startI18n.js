8acdae66d756f8782bda90b2e6999bf05030e9aa
whphhg <whphhg@users.noreply.github.com>
2017-08-13 02:28:20 +0200

    Add support for fetching multiple translation files (#2743)
    * Add support for fetching multiple translation files
    
    * Cleanup

--- examples/with-i18next/tools/startI18n.js
+++ examples/with-i18next/tools/startI18n.js
@@ -1,9 +1,15 @@
 import i18n from 'i18next'
 
-const startI18n = (file, lang) => i18n.init({
+/**
+ * Initialize a i18next instance.
+ * @function startI18n
+ * @param {object} files - Translation files.
+ * @param {string} lang - Active language.
+ */
+const startI18n = (files, lang) => i18n.init({
   lng: lang, // active language http://i18next.com/translate/
   fallbackLng: 'pt',
-  resources: file,
+  resources: files,
   ns: ['common'],
   defaultNS: 'common',
   debug: false
