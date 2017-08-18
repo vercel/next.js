727e3229e54ade1512238820e3b2b31ca34f543a
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-08-17 22:13:20 -0700

    Removed createStyleSheet() as a result of API change in material-ui-next

--- examples/with-material-ui-next/components/withRoot.js
+++ examples/with-material-ui-next/components/withRoot.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { JssProvider } from 'react-jss'
-import { withStyles, createStyleSheet, MuiThemeProvider } from 'material-ui/styles'
+import { withStyles, MuiThemeProvider } from 'material-ui/styles'
 import { getContext } from '../styles/context'
 
 // Apply some reset
-const styleSheet = createStyleSheet(theme => ({
+const styles = theme => ({
   '@global': {
     html: {
       background: theme.palette.background.default,
@@ -15,11 +15,11 @@ const styleSheet = createStyleSheet(theme => ({
       margin: 0
     }
   }
-}))
+})
 
 let AppWrapper = props => props.children
 
-AppWrapper = withStyles(styleSheet)(AppWrapper)
+AppWrapper = withStyles(styles)(AppWrapper)
 
 function withRoot (BaseComponent) {
   class WithRoot extends Component {
