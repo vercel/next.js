80ddc3afce565104b5f1f84e5aa7c78e27f23b2d
Shaleen Jain <shaleen.jain95@gmail.com>
2017-09-13 20:30:46 +0530

    fix lint errors

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -3,13 +3,12 @@ import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
-
     const sheet = new ServerStyleSheet()
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
     const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags}
+    return { ...page, styleTags }
   }
-  
+
   render () {
     return (
       <html>
