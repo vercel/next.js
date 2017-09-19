10bad9bfbf83bd6e0ea1008497d854893f733774
alejandro estrada <estrada9166@gmail.com>
2017-09-19 08:20:29 -0500

    inputChange refactoring

---
+++ examples/form-handler/handlers/Input.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { inputChange } from '../actions'
+
+class Input extends Component {
+  inputChange = (e) => this.props.inputChange(this.props.title, this.props.name, e.target.value);
+
+  render () {
+    return (
+      <div>
+        <FormGroup controlId='formBasicText'>
+          <ControlLabel>{this.props.controlLabel}</ControlLabel>
+          <FormControl
+            disabled={this.props.disabled}
+            type={this.props.type || 'Text'}
+            placeholder={this.props.controlLabel}
+            onChange={this.inputChange}
+            value={this.props.val}
+          />
+        </FormGroup>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    inputChange: bindActionCreators(inputChange, dispatch)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Input)
