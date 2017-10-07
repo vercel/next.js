72e04bfde0d3de58a90346ad8b18db2dc76898da
Tim Neutkens <timneutkens@me.com>
2017-10-07 19:44:10 +0200

    Make styled-jsx configurable

--- server/build/babel/preset.js
+++ server/build/babel/preset.js
@@ -24,9 +24,8 @@ module.exports = (context, opts = {}) => ({
     require.resolve('./plugins/handle-import'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
     require.resolve('babel-plugin-transform-class-properties'),
-    [require.resolve('babel-plugin-transform-runtime'),
-      opts['transform-runtime'] || {}],
-    require.resolve('styled-jsx/babel'),
+    [require.resolve('babel-plugin-transform-runtime'), opts['transform-runtime'] || {}],
+    [require.resolve('styled-jsx/babel'), opts['styled-jsx'] || {}],
     ...plugins,
     [
       require.resolve('babel-plugin-module-resolver'),
