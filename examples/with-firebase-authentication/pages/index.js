6c9da429efa625bb4515f6ffad011dd75d4b743d
Sitian Liu <goldensunliu@gmail.com>
2017-10-13 16:48:19 -0400

    fix lint error

--- examples/with-firebase-authentication/pages/index.js
+++ examples/with-firebase-authentication/pages/index.js
@@ -8,7 +8,7 @@ export default class Index extends Component {
     const user = req && req.session ? req.session.decodedToken : null
     // don't fetch anything from firebase if the user is not found
     const snap = user && await req.firebaseServer.database().ref('messages').once('value')
-    const messages = snap && snap.val();
+    const messages = snap && snap.val()
     return { user, messages }
   }
 
