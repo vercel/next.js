e317a2ba7d3b299dad988a30566b69ed9adef935
MFCo <marianococirio@gmail.com>
2017-10-13 18:14:21 -0300

    linting issues solved

--- examples/with-styled-jsx-postcss/babel-preset.js
+++ examples/with-styled-jsx-postcss/babel-preset.js
@@ -1,17 +1,17 @@
-const nextBabelPreset = require('next/babel');
+const nextBabelPreset = require('next/babel')
 
 /*
-  This code will load the 'styled-jsx-postcss/babel' package instead 
+  This code will load the 'styled-jsx-postcss/babel' package instead
   of built-in 'styled-jsx/babel'
 */
 
-let presetArray = nextBabelPreset();
+let presetArray = nextBabelPreset()
 
 presetArray.plugins = presetArray.plugins.map(plugin => {
   if (!Array.isArray(plugin) && plugin.indexOf('styled-jsx/babel') !== -1) {
-    return require.resolve('styled-jsx-postcss/babel');
+    return require.resolve('styled-jsx-postcss/babel')
   }
-  return plugin;
-});
+  return plugin
+})
 
-module.exports = presetArray;
+module.exports = presetArray
