7db649f70fb1b58179b5da38ff392e8f44fcb8f7
Roman Vanesyan <romain.vanesyan@gmail.com>
2017-10-15 16:34:09 +0500

    Fix linter

--- server/document.js
+++ server/document.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import htmlescape from 'htmlescape'
 import flush from 'styled-jsx/server'
 
-function Fragment({ children }) {
-  return children;
+function Fragment ({ children }) {
+  return children
 }
 
 export default class Document extends Component {
