8e55adf4ecf35d1edb105b2817a42d144645d46c
Tim Neutkens <tim@timneutkens.nl>
2017-10-22 01:47:55 +0200

    Render error as errorHtml (#3142)

--- server/render.js
+++ server/render.js
@@ -78,16 +78,19 @@ async function doRender (req, res, pathname, query, {
     let html
     let head
     let errorHtml = ''
+
     try {
-      html = render(app)
+      if (err && dev) {
+        errorHtml = render(createElement(ErrorDebug, { error: err }))
+      } else if (err) {
+        errorHtml = render(app)
+      } else {
+        html = render(app)
+      }
     } finally {
       head = Head.rewind() || defaultHead()
     }
     const chunks = loadChunks({ dev, dir, dist, availableChunks })
-
-    if (err && dev) {
-      errorHtml = render(createElement(ErrorDebug, { error: err }))
-    }
 
     return { html, head, errorHtml, chunks }
   }
