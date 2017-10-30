4c18678d54947811f1f557c1396e3a3e7ae120cb
Remy Sharp <remy@remysharp.com>
2017-10-30 14:24:31 +0000

    Create example of activeClassLink using withRouter (#3188)

---
+++ examples/active-class-name/components/Nav.js
@@ -0,0 +1,31 @@
+import Link from './Link';
+import Head from 'next/head';
+
+export default () => (
+  <nav>
+    <style jsx>{`
+      .active:after {
+        content: ' (current page)';
+      }
+
+      .nav-link {
+        text-decoration: none;
+        padding: 10px;
+        display: block;
+      }
+    `}</style>
+
+    <ul>
+      <li>
+        <Link activeClassName="active" href="/">
+          <a className="nav-link home-link">Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link activeClassName="active" href="/about">
+          <a className="nav-link">About</a>
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
