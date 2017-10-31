18755192b6ce0b290b705e38e56036e65826272c
Leandro Ardissone <lardissone@gmail.com>
2017-10-30 22:30:21 -0300

    Added Sentry example

---
+++ examples/with-sentry/components/withSentry.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Raven from 'raven-js'
+
+function withSentry(Child) {
+  return class WrappedComponent extends React.Component {
+    static getInitialProps(context) {
+      return Child.getInitialProps(context)
+    }
+    constructor(props) {
+      super(props)
+      this.state = {
+        error: null
+      }
+      Raven.config(
+        // change this for your Sentry DSN
+        'https://cb0826e4617043e1bdafcb519df0992b@sentry.io/237733'
+      ).install()
+    }
+
+    componentDidCatch(error, errorInfo) {
+      this.setState({ error })
+      Raven.captureException(error, { extra: errorInfo })
+    }
+
+    render() {
+      return <Child {...this.props} />;
+    }
+  }
+}
+
+export default withSentry
