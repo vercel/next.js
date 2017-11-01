4fc4eff18ece524f82a3ac77cc5b45f768abea76
Brian <bdombro@gmail.com>
2017-11-01 10:52:55 -0400

    More complete with-apollo-and-redux example with dynamic post route

--- examples/with-apollo-and-redux/pages/index.js
+++ examples/with-apollo-and-redux/pages/index.js
@@ -7,7 +7,6 @@ import withData from '../lib/withData'
 export default withData((props) => (
   <App>
     <Header pathname={props.url.pathname} />
-    <Submit />
-    <PostList />
+    <h1>Welcome Home.</h1>
   </App>
 ))
