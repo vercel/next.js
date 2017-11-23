a8c344fa19ae242f90277391340463c5050b107e
Dan Argue <dargue3@users.noreply.github.com>
2017-11-23 04:41:59 -0800

    Add with-mobx-state-tree example (#3179)
    * Adapt with-mobx example for with-mobx-state-tree
    
    * Remove unnecessary lastUpdate parameter to show off snapshot
    
    * update readme
    
    * make other.js more closely mimic index.js

---
+++ examples/with-mobx-state-tree/pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Provider } from 'mobx-react'
+import { getSnapshot } from 'mobx-state-tree'
+import { initStore } from '../store'
+import Page from '../components/Page'
+
+export default class Counter extends React.Component {
+  static getInitialProps({ req }) {
+    const isServer = !!req
+    const store = initStore(isServer)
+    return { initialState: getSnapshot(store), isServer }
+  }
+
+  constructor(props) {
+    super(props)
+    this.store = initStore(props.isServer, props.initialState)
+  }
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <Page title='Index Page' linkTo='/other' />
+      </Provider>
+    )
+  }
+}
