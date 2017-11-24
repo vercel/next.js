957b2b381ba8915fe2fdc02a3c3e0f10746d26f1
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-11-24 12:24:06 +0530

    Add an example with dynamic imports.

--- examples/with-zones/home/pages/index.js
+++ examples/with-zones/home/pages/index.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import asset from 'next/asset'
+import dynamic from 'next/dynamic'
+
+const Header = dynamic(import('../components/Header'))
 
 export default () => (
   <div>
-    <h2>Our Homepage </h2>
+    <Header />
+    <p>This is our homepage</p>
     <div><Link href='/blog' as='/blog'><a>Blog</a></Link></div>
     <div><Link href='/about' as='/about'><a>About us</a></Link></div>
     <img width={200} src={asset('/nextjs.png')} />
