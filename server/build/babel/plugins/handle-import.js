b3d06fd9f1d7ceb558cbeb86c7172cfd7e4fdd39
Prudvi Gali <prudvi.gali@gmail.com>
2017-12-05 16:52:23 -0600

    Added typeof function check for require.ensure

--- server/build/babel/plugins/handle-import.js
+++ server/build/babel/plugins/handle-import.js
@@ -20,21 +20,20 @@ const buildImport = (args) => (template(`
       })
       :
       new (require('next/dynamic').SameLoopPromise)((resolve, reject) => {
-        // !require.ensure && eval('require.ensure = function (deps, callback) { callback(require) }')
-        const weakId = require.resolveWeak ? require.resolveWeak(SOURCE) : /*require.resolveWeak*/(SOURCE)
-        try {
-          const weakModule = __webpack_require__(weakId)
-          return resolve(weakModule)
-        } catch (err) {}
-
-        require.ensure([], (require) => {
+          const weakId = require.resolveWeak ? require.resolveWeak(SOURCE) : (SOURCE)
           try {
-            let m = require(SOURCE)
-            resolve(m)
-          } catch(error) {
-            reject(error)
-          }
-        }, 'chunks/${args.name}.js');
+            const weakModule = __webpack_require__(weakId)
+            return resolve(weakModule)
+          } catch (err) {}
+          (typeof require.ensure !== "function") && eval('(require.ensure = function (deps, callback) { callback(require) })')
+          require.ensure([], (require) => {
+            try {
+              let m = require(SOURCE)
+              resolve(m)
+            } catch(error) {
+              reject(error)
+            }
+          }, 'chunks/${args.name}.js');
       })
   )
 `))
