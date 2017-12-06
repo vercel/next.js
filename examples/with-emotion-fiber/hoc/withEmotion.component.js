278f58b54bf649de0d3408e150ff3d3b2736e041
Thomas Greco <tgreco4@fordham.edu>
2017-12-05 19:46:06 -0500

    pushing updated emotion example (#3344)
    * pushing updated emotion example
    
    * Fix linting

---
+++ examples/with-emotion-fiber/hoc/withEmotion.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { hydrate } from 'react-emotion'
+import { injectGlobalStyles } from '../shared/styles'
+
+const withEmotion = ComposedComponent => {
+  class HOC extends Component {
+    componentWillMount () {
+      if (typeof window !== 'undefined') {
+        hydrate(window.__NEXT_DATA__.ids)
+      }
+      injectGlobalStyles()
+    }
+
+    render () {
+      return <ComposedComponent />
+    }
+  };
+
+  return HOC
+}
+
+export default withEmotion
