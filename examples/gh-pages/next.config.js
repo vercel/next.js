ff79ac59bcd7dd9a24d57d161dd182f1a21eae19
Thierry Charbonnel <thierryc@users.noreply.github.com>
2017-12-06 08:08:03 -0500

    lint fix

--- examples/gh-pages/next.config.js
+++ examples/gh-pages/next.config.js
@@ -2,29 +2,14 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-const debug = process.env.NODE_ENV !== "production";
+const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   exportPathMap: function () {
     return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
+      '/': { page: '/' },
+      '/about': { page: '/about' }
     }
   },
-  //assetPrefix: '',
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
-  /* webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    // Important: return the modified config
-    return config
-  },
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */
+  assetPrefix: !debug ? '/Next-gh-page-example/' : ''
 }
