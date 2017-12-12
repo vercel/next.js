263a38d469c62619c927854f27193a916f2e2685
Brice BERNARD <brikou@gmail.com>
2017-12-12 10:44:51 +0100

    Move raf/polyfill to jest.config.js

--- examples/with-jest/jest.config.js
+++ examples/with-jest/jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFiles: ['raf/polyfill', '<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
 }
