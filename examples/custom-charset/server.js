52a8d50beddbe927e417d62cbfbdaf0f92b1207b
Ricardo <ricardoplouis@gmail.com>
2017-12-19 13:45:48 -0600

    Removed unused routes

--- examples/custom-charset/server.js
+++ examples/custom-charset/server.js
@@ -17,14 +17,6 @@ app.prepare()
 
   server.use(setCharset)
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
-
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
-
   server.get('*', (req, res) => {
     return handle(req, res)
   })
