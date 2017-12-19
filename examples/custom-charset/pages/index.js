2fece075c1fffd06da65e8054b61a701b90ecf67
Ricardo <ricardoplouis@gmail.com>
2017-12-19 13:41:51 -0600

    added default contentType if none specified in header

--- examples/custom-charset/pages/index.js
+++ examples/custom-charset/pages/index.js
@@ -3,17 +3,17 @@ import Head from 'next/head'
 export default class Index extends React.Component {
   static async getInitialProps ({res}) {
     let contentType = res._headers['content-type']
-    let charSet = contentType.slice(contentType.indexOf('=') + 1, contentType.length)
+    let charSet = contentType ? contentType.slice(contentType.indexOf('=') + 1, contentType.length) : 'utf-8';
     return { charSet }
   }
   render () {
     return (
-      <ul>
+      <div>
         <Head>
-          <meta charSet={this.props.charSet} class='next-head' />
+          <meta charSet={this.props.charSet} className='next-head' />
           <div>Current charSet {this.props.charSet}</div>
         </Head>
-      </ul>
+      </div>
     )
   }
 }
