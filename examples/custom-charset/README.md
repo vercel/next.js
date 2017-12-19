5d4b61e8defd3c88df2a3225ac293dd64d8e29a8
Ricardo <ricardoplouis@gmail.com>
2017-12-19 12:51:39 -0600

    Custom-charset example using custom express server

---
+++ examples/custom-charset/README.md
@@ -0,0 +1,25 @@
+# Custom Charset example
+
+## How to use
+
+### Download manually
+
+Download the example [or clone the repo](https://github.com/zeit/next.js):
+
+```bash
+curl https://codeload.github.com/zeit/next.js/tar.gz/canary | tar -xz --strip=2 next.js-canary/examples/custom-charset
+cd custom-charset
+```
+
+Install it and run:
+
+```bash
+npm install
+npm run dev
+```
+
+Deploy it to the cloud with [now](https://zeit.co/now) ([download](https://zeit.co/download))
+
+```bash
+now
+```
---
+++ examples/custom-charset/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+var setCharset = (req, res, next) => {
+  res.set({'Content-Type': 'text/html; charset=iso-8859-2'})
+  next();
+}
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.use(setCharset)
+
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/b', req.query)
+  })
+
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/a', req.query)
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
