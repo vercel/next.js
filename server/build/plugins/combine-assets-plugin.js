443ece9b47de59dd9e685951e86a26c32203d066
Tim Neutkens <timneutkens@me.com>
2017-12-21 08:34:46 +0100

    Remove comment

--- server/build/plugins/combine-assets-plugin.js
+++ server/build/plugins/combine-assets-plugin.js
@@ -24,27 +24,6 @@ export default class CombineAssetsPlugin {
 
         compilation.additionalChunkAssets.push(this.output)
         compilation.assets[this.output] = concat
-
-        // Register the combined file as an output of the associated chunks
-        // chunks.forEach((chunk) => {
-          
-
-        //   chunk.files.forEach((file) => {
-        //     if(!this.input.includes(file)) {
-        //       return
-        //     }
-
-        //     const index = chunk.files.indexOf(file)
-        //     chunk.files.splice(index, 1)
-        //   })
-
-        //   if(chunk.files.length === 0) {
-        //     const index = compilation.chunks.indexOf(chunk)
-        //     compilation.chunks.splice(index, 1)
-        //   }
-          
-        //   chunk.files.reduce((prev, file) => prev || this.input.includes(file), false)
-        // })
       })
     })
   }
