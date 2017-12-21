a4cf418d963a8628c8cd6eb2f8fc5700ce2921aa
Jerome Fitzgerald <jeromes@gmail.com>
2017-12-21 11:00:35 -0500

    [refactor] remove old migration code

--- examples/with-apollo-and-redux-saga/lib/clock/reducers.js
+++ examples/with-apollo-and-redux-saga/lib/clock/reducers.js
@@ -7,18 +7,6 @@ export const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case actionTypes.FAILURE:
-      return {
-        ...state,
-        ...{ error: action.error }
-      }
-
-    case actionTypes.LOAD_DATA_SUCCESS:
-      return {
-        ...state,
-        ...{ placeholderData: action.data }
-      }
-
     case actionTypes.TICK_CLOCK:
       return {
         ...state,
--- examples/with-apollo-and-redux-saga/lib/count/reducers.js
+++ examples/with-apollo-and-redux-saga/lib/count/reducers.js
@@ -6,8 +6,10 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.COUNT_INCREASE:
       return state + 1
+
     case actionTypes.COUNT_DECREASE:
       return state - 1
+
     default:
       return state
   }
