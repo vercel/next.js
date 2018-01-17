495e0a4c65f78d7b4bda4f5b7d58b09481e56146
Tim Neutkens <timneutkens@me.com>
2018-01-17 14:06:17 +0100

    Add comment about preact support

--- client/index.js
+++ client/index.js
@@ -159,6 +159,7 @@ async function doRender ({ Component, props, hash, err, emitter: emitterProp = e
 
 let isInitialRender = true
 function renderReactElement (reactEl, domEl) {
+  // The check for `.hydrate` is there to support React alternatives like preact
   if (isInitialRender && typeof ReactDOM.hydrate === 'function') {
     ReactDOM.hydrate(reactEl, domEl)
     isInitialRender = false
