de8d0fb1de618afeab2d2d7091c98fea446205f5
Tim Neutkens <timneutkens@me.com>
2018-01-18 11:16:11 +0100

    Apply optimization to uglifyjs

--- server/build/webpack.js
+++ server/build/webpack.js
@@ -249,7 +249,34 @@ export default async function getBaseWebpackConfig (dir, {dev = false, isServer 
         sourceMap: false,
         uglifyOptions: {
           compress: {
-            comparisons: false
+            arrows: false,
+            booleans: false,
+            cascade: false,
+            collapse_vars: false,
+            comparisons: false,
+            computed_props: false,
+            hoist_funs: false,
+            hoist_props: false,
+            hoist_vars: false,
+            if_return: false,
+            inline: false,
+            join_vars: false,
+            keep_infinity: true,
+            loops: false,
+            negate_iife: false,
+            properties: false,
+            reduce_funcs: false,
+            reduce_vars: false,
+            sequences: false,
+            side_effects: false,
+            switches: false,
+            top_retain: false,
+            toplevel: false,
+            typeofs: false,
+            unused: false,
+            conditionals: false,
+            dead_code: true,
+            evaluate: false
           }
         }
       }),
