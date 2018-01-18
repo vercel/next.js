30af0bf68cd5ca5a7ee743d5137273bbbe303db9
Tim Neutkens <timneutkens@me.com>
2018-01-18 01:32:42 +0100

    Resolve page entries like pages/day/index.js to pages/day.js

--- server/build/webpack/utils.js
+++ server/build/webpack/utils.js
@@ -4,12 +4,12 @@ import glob from 'glob-promise'
 const nextPagesDir = path.join(__dirname, '..', '..', '..', 'pages')
 
 export async function getPages (dir, {dev, isServer}) {
-  const pageFiles = await getPageFiles(dir, {dev, isServer})
+  const pageFiles = await getPagePaths(dir, {dev, isServer})
 
-  return getPageEntries(pageFiles, {isServer})
+  return getPageEntries(pageFiles, {dir, isServer})
 }
 
-async function getPageFiles (dir, {dev, isServer}) {
+async function getPagePaths (dir, {dev, isServer}) {
   let pages
 
   if (dev) {
@@ -21,24 +21,55 @@ async function getPageFiles (dir, {dev, isServer}) {
   return pages
 }
 
-export function getPageEntries (pageFiles, {isServer}) {
-  const entries = {}
-  const bundleLocation = 'bundles'
-  for (const p of pageFiles) {
-    entries[path.join(bundleLocation, p.replace(/\.+(jsx|tsx|ts)/, '.js'))] = [`./${p}`]
-  }
+// Convert page path into single entry
+export function entry ({isServer}) {
+  return function createEntry (filePath, name) {
+    const parsedPath = path.parse(filePath)
+    let entryName = name || filePath
 
-  // The default pages (_document.js and _error.js) are only added when they're not provided by the user
-  const defaultPages = [
-    '_error.js',
-    isServer && '_document.js'
-  ].filter(Boolean)
-  for (const p of defaultPages) {
-    const entryName = path.join(bundleLocation, 'pages', p)
-    if (!entries[entryName]) {
-      entries[entryName] = path.join(nextPagesDir, p)
+    // This makes sure we compile `pages/blog/index.js` to `pages/blog.js`.
+    // Excludes `pages/index.js` from this rule since we do want `/` to route to `pages/index.js`
+    if (parsedPath.dir !== 'pages' && parsedPath.name === 'index') {
+      entryName = `${parsedPath.dir}.js`
+    }
+
+    // Makes sure supported extensions are stripped off. The outputted file should always be `.js`
+    entryName = entryName.replace(/\.+(jsx|tsx|ts)/, '.js')
+
+    const bundlesDir = isServer ? '' : 'bundles'
+    return {
+      name: path.join(bundlesDir, entryName),
+      file: parsedPath.root ? filePath : `./${filePath}`
     }
   }
+}
+
+// Convert page paths into entries
+export function getPageEntries (pagePaths, {dir, isServer}) {
+  const entries = {}
+
+  const createEntry = entry({isServer})
+
+  for (const filePath of pagePaths) {
+    const entry = createEntry(filePath)
+    entries[entry.name] = entry.file
+  }
+
+  const errorPagePath = path.join(nextPagesDir, '_error.js')
+  const errorPageEntry = createEntry(errorPagePath, 'pages/_error.js') // default error.js
+  if (!entries[errorPageEntry.name]) {
+    entries[errorPageEntry.name] = errorPageEntry.file
+  }
+
+  if (isServer) {
+    const documentPagePath = path.join(nextPagesDir, '_document.js')
+    const documentPageEntry = createEntry(documentPagePath, 'pages/_document.js')
+    if (!entries[documentPageEntry.name]) {
+      entries[documentPageEntry.name] = documentPageEntry.file
+    }
+  }
+
+  console.log(entries)
 
   return entries
 }
