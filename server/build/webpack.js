827c8134ea882f23c3ff5d3e644e1aef33c0607a
Tim Neutkens <timneutkens@me.com>
2018-01-22 11:11:48 +0100

    Check node_modules reliably

--- server/build/webpack.js
+++ server/build/webpack.js
@@ -1,7 +1,7 @@
 import path, {sep} from 'path'
 import fs from 'fs'
 import webpack from 'webpack'
-
+import resolve from 'resolve'
 import nodeExternals from 'webpack-node-externals'
 import UglifyJSPlugin from 'uglifyjs-webpack-plugin'
 import CaseSensitivePathPlugin from 'case-sensitive-paths-webpack-plugin'
@@ -70,14 +70,23 @@ function externalsConfig (dir, isServer) {
     }))
   }
 
-  const dirNodeModules = path.join(dir, 'node_modules')
-  if (fs.existsSync(dirNodeModules)) {
-    nodeExternals({
-      modulesDir: dirNodeModules,
-      includeAbsolutePaths: true,
-      whitelist: [/\.(?!(?:js|json)$).{1,5}$/i]
+  // This will externalize all the 'next/xxx' modules to load from
+  // node_modules always.
+  // This is very useful in Next.js development where we use symlinked version
+  // of Next.js or using next/xxx inside test apps.
+  externals.push((context, request, callback) => {
+    resolve(request, { basedir: dir }, (err, res) => {
+      if (err) {
+        return callback()
+      }
+
+      if (res.match(/node_modules/)) {
+        return callback(null, `commonjs ${request}`)
+      }
+
+      callback()
     })
-  }
+  })
 
   return externals
 }
