4db7e8fe9e2252121fc55aba57c32c7de16b8777
Tim Neutkens <tim@timneutkens.nl>
2018-01-31 18:20:27 +0100

    Fix __route error when importing a page inside a page (#3644)
    * Check if Component is defined
    
    * Add comment
    
    * Fix linting

--- server/build/loaders/hot-self-accept-loader.js
+++ server/build/loaders/hot-self-accept-loader.js
@@ -5,8 +5,11 @@ module.exports = function (content, sourceMap) {
 
   const route = getRoute(this)
 
+  // Webpack has a built in system to prevent default from colliding, giving it a random letter per export.
+  // We can safely check if Component is undefined since all other pages imported into the entrypoint don't have __webpack_exports__.default
   this.callback(null, `${content}
     (function (Component, route) {
+      if(!Component) return
       if (!module.hot) return
       module.hot.accept()
       Component.__route = route
