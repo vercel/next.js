53b03ddf57db792c52179c27f34b8a538e76946e
Tim Neutkens <timneutkens@me.com>
2018-02-05 14:52:16 +0100

    Remove obsolete files

--- test/integration/basic/components/hello-webpack-css.js
+++
@@ -1,3 +0,0 @@
-import css from './hello-webpack-css.css'
-import framework from 'css-framework/framework.css'
-export default () => <div className={`${css.helloWorld} ${framework.frameworkClass}`}>Hello World</div>
