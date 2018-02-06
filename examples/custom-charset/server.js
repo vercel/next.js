847774423fb4dca537ee573503d54c481903bab9
Sitian Liu <sliu@tripadvisor.com>
2018-02-06 00:15:56 -0500

    Create example using custom charset https://github.com/zeit/next.js/issues/3401

---
+++ examples/custom-charset/server.js
@@ -0,0 +1,21 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    res.setHeader('Content-Type', 'text/html; charset=iso-8859-2')
+    handle(req, res, parsedUrl)
+  })
+  .listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
