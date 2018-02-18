ac87b313b99b27a0e1d3185d354cdb091853e433
Kevin Decker <kpdecker@gmail.com>
2018-02-17 20:08:31 -0600

    Pull process.exit call out of client code

--- server/utils.js
+++ server/utils.js
@@ -27,3 +27,13 @@ export function getAvailableChunks (dir, dist) {
 
   return chunksMap
 }
+
+export function printAndExit (message, code = 1) {
+  if (code === 0) {
+    console.log(message)
+  } else {
+    console.error(message)
+  }
+
+  process.exit(code)
+}
