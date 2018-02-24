5ebb943c84195c553d6ba1b4937fe76318726ca2
Jerome Fitzgerald <jeromes@gmail.com>
2018-02-24 18:17:04 -0500

    [example] with-apollo-and-redux-saga (#3488)
    * [example] with-apollo-and-redux-saga
    
    - Using Apollo to get GraphQL Data? Dope.
    - Using Redux Saga to do other stuff outside of that? Cool.
    - Nary the two shall meet? Most likely. 😀️
    
    This is a breakout of #3463 where we were combining Apollo and Redux.
    
    This may not be an example that gets a PR.
    
    Why? Well, the examples are meant to pick and choose and combine
    yourself. At least I believe, and this is basically a combination of two
    examples (`with-apollo` and `with-redux-saga`) with some reworking.
    
    **pages/**:
    `index`: withReduxSaga()
    `about`: ()
    `blog/index`: withReduxSaga(withApollo())
    `blog/entry`: withApollo()
    
    * [refactor] fix lint (again), remove superfluous calls
    
    * [fix] package.json: with-apollo-and-redux-saga
    
    Updated the `name` and made sure `es6-promise` was in dependencies
    
    * [refactor] remove semi-colons in clock/sagas
    
    * [refactor] remove old migration code

---
+++ examples/with-apollo-and-redux-saga/lib/clock/reducers.js
@@ -0,0 +1,21 @@
+import { actionTypes } from './actions'
+
+export const initialState = {
+  lastUpdate: 0,
+  light: false
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.TICK_CLOCK:
+      return {
+        ...state,
+        ...{ lastUpdate: action.lastUpdate, light: !!action.light }
+      }
+
+    default:
+      return state
+  }
+}
+
+export default reducer
