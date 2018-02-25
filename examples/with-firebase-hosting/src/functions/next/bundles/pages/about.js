2483aa8a66fbffd68efc45c34e3b9f86d6fdfad0
James Hegedus <jthegedus@jttech.io>
2018-02-26 00:48:06 +1100

    remove old build artefact

--- examples/with-firebase-hosting/src/functions/next/bundles/pages/about.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/about",function(){var e=webpackJsonp([4],{186:function(e,t,n){e.exports=n(187)},187:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:true});var u=n(4);var a=n.n(u);var r=n(92);t["default"]=function(){return a.a.createElement(r["a"],null,a.a.createElement("p",null,"About Page"))}}},[186]);return{page:e.default}});
