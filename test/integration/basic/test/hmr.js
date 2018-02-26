0117e2b3fa246eed2ce39b8a35d97d234fbcde66
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2018-02-26 14:45:39 +0530

    Add a test case on error recovery in the module scope. (#3892)
    This is a test case related for #3888

--- test/integration/basic/test/hmr.js
+++ test/integration/basic/test/hmr.js
@@ -16,8 +16,8 @@ async function check (contentFn, regex) {
 
 export default (context, render) => {
   describe('Hot Module Reloading', () => {
-    describe('syntax error', () => {
-      it('should detect the error and recover', async () => {
+    describe('errors', () => {
+      it('should detect syntax errors and recover', async () => {
         const browser = await webdriver(context.appPort, '/hmr/about')
         const text = await browser
           .elementByCss('p').text()
@@ -69,6 +69,36 @@ export default (context, render) => {
         await check(
           () => browser.elementByCss('body').text(),
           /This is the contact page/
+        )
+
+        browser.close()
+      })
+
+      it('should detect runtime errors on the module scope', async () => {
+        const browser = await webdriver(context.appPort, '/hmr/about')
+        const text = await browser
+          .elementByCss('p').text()
+        expect(text).toBe('This is the about page.')
+
+        const aboutPagePath = join(__dirname, '../', 'pages', 'hmr', 'about.js')
+
+        const originalContent = readFileSync(aboutPagePath, 'utf8')
+        const erroredContent = originalContent.replace('export', 'aa=20;\nexport')
+
+        // change the content
+        writeFileSync(aboutPagePath, erroredContent, 'utf8')
+
+        await check(
+          () => browser.elementByCss('body').text(),
+          /aa is not defined/
+        )
+
+        // add the original content
+        writeFileSync(aboutPagePath, originalContent, 'utf8')
+
+        await check(
+          () => browser.elementByCss('body').text(),
+          /This is the about page/
         )
 
         browser.close()
