dbb1b732a06e92f8730d26fa01dac4d53c744732
Alexander Kachkaev <alexander@kachkaev.ru>
2018-03-05 11:46:13 +0000

    Update with-react-i18next example (#3932)
    - simplify pages by introducing withI18next hoc
    - add Trans component example
    - replace <div> with <p> in demo components to make text on pages easier to read
    
    German translation by @marinaroot

---
+++ examples/with-react-i18next/lib/withI18next.js
@@ -0,0 +1,24 @@
+import { translate } from 'react-i18next'
+import i18n from '../i18n'
+
+export const withI18next = (namespaces = ['common']) => ComposedComponent => {
+  const Extended = translate(namespaces, { i18n, wait: process.browser })(
+    ComposedComponent
+  )
+
+  Extended.getInitialProps = async (ctx) => {
+    const composedInitialProps = ComposedComponent.getInitialProps
+      ? await ComposedComponent.getInitialProps(ctx)
+      : {}
+
+    const i18nInitialProps =
+      ctx.req && !process.browser ? i18n.getInitialProps(ctx.req, namespaces) : {}
+
+    return {
+      ...composedInitialProps,
+      ...i18nInitialProps
+    }
+  }
+
+  return Extended
+}
