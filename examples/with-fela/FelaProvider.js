658470dc45f887723ed0dff9e52e69bddc4fdffd
Robin Frischmann <robin.frischmann@dm.de>
2018-03-06 14:18:03 +0100

    #3757 update fela example for Next 5
    closes #3757

---
+++ examples/with-fela/FelaProvider.js
@@ -0,0 +1,26 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { Provider } from 'react-fela'
+import getFelaRenderer from './getFelaRenderer'
+
+const clientRenderer = getFelaRenderer()
+
+export default class FelaProvider extends Component {
+  static contextTypes = {
+    renderer: PropTypes.object
+  };
+
+  render () {
+    if (this.context.renderer) {
+      return this.props.children
+    }
+
+    const renderer = this.props.renderer || clientRenderer
+    return (
+      <Provider renderer={renderer}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
