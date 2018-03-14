0eb5bab4522808916dfe9414aaf14b7af4e09a8c
Wagner Camarao <wagner@camarao.io>
2018-03-14 05:06:53 -0300

    Fix a/b typo in custom-server-typescript routing (#4003)

--- examples/custom-server-typescript/pages/index.tsx
+++ examples/custom-server-typescript/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default () => (
   <ul>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
+    <li><Link href='/a' as='/a'><a>a</a></Link></li>
+    <li><Link href='/b' as='/b'><a>b</a></Link></li>
   </ul>
 )
