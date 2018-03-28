b20668fac5bcc67248f40bdb431c65c63030ef90
Tage A. L. K <DevSpeak@users.noreply.github.com>
2018-03-28 17:45:54 +0200

    Remove deprected use of apollo-client-preset, and refactor
    Changes
    * Remove deprected use of apollo-client-preset in favor of apollo-boost
    * Refactor for usage of react-apollo@2.1

--- examples/with-apollo-auth/lib/initApollo.js
+++ examples/with-apollo-auth/lib/initApollo.js
@@ -1,6 +1,5 @@
-import { ApolloClient } from 'apollo-client'
+import { ApolloClient, InMemoryCache } from 'apollo-boost'
 import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 import fetch from 'isomorphic-unfetch'
 
