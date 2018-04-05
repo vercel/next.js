dac2f39a257a690124abf372c25d2c2b0e769d23
Tim Neutkens <tim@timneutkens.nl>
2018-04-05 03:48:11 +0200

    Add support for exportPathMap in development (#4094)
    * Add support for exportPathMap in development
    
    * Add comment about what it does

--- test/integration/static/test/index.test.js
+++ test/integration/static/test/index.test.js
@@ -5,16 +5,22 @@ import {
   nextBuild,
   nextExport,
   startStaticServer,
-  stopApp
+  launchApp,
+  stopApp,
+  killApp,
+  findPort,
+  renderViaHTTP
 } from 'next-test-utils'
 
 import ssr from './ssr'
 import browser from './browser'
+import dev from './dev'
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 
 const appDir = join(__dirname, '../')
 const context = {}
+const devContext = {}
 
 describe('Static Export', () => {
   beforeAll(async () => {
@@ -24,9 +30,22 @@ describe('Static Export', () => {
 
     context.server = await startStaticServer(join(appDir, 'out'))
     context.port = context.server.address().port
+
+    devContext.appPort = await findPort()
+    devContext.server = await launchApp(join(__dirname, '../'), devContext.appPort, true)
+
+    // pre-build all pages at the start
+    await Promise.all([
+      renderViaHTTP(devContext.appPort, '/'),
+      renderViaHTTP(devContext.appPort, '/dynamic/one')
+    ])
   })
-  afterAll(() => stopApp(context.server))
+  afterAll(() => {
+    stopApp(context.server)
+    killApp(devContext.server)
+  })
 
   ssr(context)
   browser(context)
+  dev(context)
 })
