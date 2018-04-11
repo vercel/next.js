7958096263affaccf708d5a1c7384e4a0071c096
Prateek Rastogi <prateek.rastogi@timesinternet.in>
2018-04-11 19:32:23 +0530

    added videojs example

--- examples/with-videojs/components/Player.js
+++ examples/with-videojs/components/Player.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import videojs from 'video.js'
 import 'videojs-youtube'
-import '../node_modules/video.js/dist/video-js.css'
+import 'video.js/dist/video-js.css'
 
 class Player extends Component {
   componentDidMount () {
