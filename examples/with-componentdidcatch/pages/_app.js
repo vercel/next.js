65c0014ad174e5dcf3b991cfacd9c91edb3972d4
Tim Neutkens <timneutkens@me.com>
2018-04-23 14:22:36 -0700

    Add componentDidCatch example

---
+++ examples/with-componentdidcatch/pages/_app.js
@@ -0,0 +1,9 @@
+import App from 'next/app'
+
+export default class MyApp extends App {
+  componentDidCatch (error, errorInfo) {
+    console.log('CUSTOM ERROR HANDLING', error)
+    // This is needed to render errors correctly in development / production
+    super.componentDidCatch(error, errorInfo)
+  }
+}
