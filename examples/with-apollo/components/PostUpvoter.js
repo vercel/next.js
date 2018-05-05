b0b1ea5bcb03f81ceacd22f6cdda6dcc9e667ba6
Brice BERNARD <brikou@gmail.com>
2018-05-05 23:21:12 +0200

    Use Mutation cmpt in PostUpvoter cmpt

--- examples/with-apollo/components/PostUpvoter.js
+++ examples/with-apollo/components/PostUpvoter.js
@@ -1,58 +1,54 @@
-import React from 'react'
-import { graphql } from 'react-apollo'
 import { gql } from 'apollo-boost'
+import { Mutation } from 'react-apollo'
 
-function PostUpvoter ({ upvote, votes, id }) {
-  return (
-    <button onClick={() => upvote(id, votes + 1)}>
-      {votes}
-      <style jsx>{`
-        button {
-          background-color: transparent;
-          border: 1px solid #e4e4e4;
-          color: #000;
+export default ({ id, votes }) => (
+  <Mutation
+    mutation={gql`
+      mutation updatePost($id: ID!, $votes: Int) {
+        updatePost(id: $id, votes: $votes) {
+          __typename
+          id
+          votes
         }
-        button:active {
-          background-color: transparent;
-        }
-        button:before {
-          align-self: center;
-          border-color: transparent transparent #000000 transparent;
-          border-style: solid;
-          border-width: 0 4px 6px 4px;
-          content: '';
-          height: 0;
-          margin-right: 5px;
-          width: 0;
-        }
-      `}</style>
-    </button>
-  )
-}
-
-const upvotePost = gql`
-  mutation updatePost($id: ID!, $votes: Int) {
-    updatePost(id: $id, votes: $votes) {
-      id
-      __typename
-      votes
-    }
-  }
-`
-
-export default graphql(upvotePost, {
-  props: ({ ownProps, mutate }) => ({
-    upvote: (id, votes) =>
-      mutate({
-        variables: { id, votes },
-        optimisticResponse: {
-          __typename: 'Mutation',
-          updatePost: {
-            __typename: 'Post',
-            id: ownProps.id,
-            votes: ownProps.votes + 1
+      }
+    `}
+    variables={{ id, votes: votes + 1 }}
+    optimisticResponse={{
+      updatePost: {
+        __typename: 'Post',
+        id,
+        votes: votes + 1
+      }
+    }}
+  >
+    {upvote => (
+      <button
+        onClick={() => {
+          upvote()
+        }}
+      >
+        {votes}
+        <style jsx>{`
+          button {
+            background-color: transparent;
+            border: 1px solid #e4e4e4;
+            color: #000;
           }
-        }
-      })
-  })
-})(PostUpvoter)
+          button:active {
+            background-color: transparent;
+          }
+          button:before {
+            align-self: center;
+            border-color: transparent transparent #000000 transparent;
+            border-style: solid;
+            border-width: 0 4px 6px 4px;
+            content: '';
+            height: 0;
+            margin-right: 5px;
+            width: 0;
+          }
+        `}</style>
+      </button>
+    )}
+  </Mutation>
+)
