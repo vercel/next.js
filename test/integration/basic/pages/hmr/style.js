ed924b09cdb3b034d6555ed002655358994cf1c6
Tim Neutkens <timneutkens@me.com>
2018-05-05 13:47:53 +0200

    Upgrade react-hot-loader
    Fixes zeit/styled-jsx#425
    Fixes #4246
    Fixes #4273

---
+++ test/integration/basic/pages/hmr/style.js
@@ -0,0 +1,14 @@
+export default () => {
+  return (
+    <>'     '<div className='hmr-style-page'>
+      <p>
+          This is the style page.
+        <style jsx>{`
+            p {
+              font-size: 100px;
+            }
+          `}</style>
+      </p>
+    </div>'   '</>
+  )
+}
