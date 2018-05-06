bf7b147f62d590924e4247a19299c183e36ad705
Tim Neutkens <timneutkens@me.com>
2018-05-07 01:29:36 +0200

    Move withData to _app

--- examples/with-apollo/pages/_app.js
+++ examples/with-apollo/pages/_app.js
@@ -1,8 +1,23 @@
 import App, {Container} from 'next/app'
 import React from 'react'
-import withData from '../lib/withData'
+import PropTypes from 'prop-types'
+import { ApolloProvider, getDataFromTree } from 'react-apollo'
+import Head from 'next/head'
+import initApollo from '../lib/initApollo'
 
-class MyApp extends App {
+export default class MyApp extends App {
+  static childContextTypes = {
+    ...App.childContextTypes,
+    apollo: PropTypes.object
+  }
+
+  getChildContext () {
+    return {
+      ...super.getChildContext(),
+      apollo: this.apollo
+    }
+  }
+
   static async getInitialProps ({ Component, router, ctx }) {
     let pageProps = {}
 
@@ -10,15 +25,51 @@ class MyApp extends App {
       pageProps = await Component.getInitialProps(ctx)
     }
 
-    return {pageProps}
+    const serverState = {
+      apollo: {}
+    }
+
+    // Run all GraphQL queries in the component tree
+    // and extract the resulting data
+    const apollo = initApollo()
+    try {
+      // Run all GraphQL queries
+      await getDataFromTree(
+        <MyApp Component={Component} router={router} pageProps={pageProps} serverState={serverState} />
+      )
+    } catch (error) {
+      // Prevent Apollo Client GraphQL errors from crashing SSR.
+      // Handle them in components via the data.error prop:
+      // http://dev.apollodata.com/react/api-queries.html#graphql-query-data-error
+      console.error('Error while running `getDataFromTree`', error)
+    }
+
+    if (!process.browser) {
+      // getDataFromTree does not call componentWillUnmount
+      // head side effect therefore need to be cleared manually
+      Head.rewind()
+    }
+
+    // Extract query data from the Apollo store
+    serverState.apollo.data = apollo.cache.extract()
+
+    return {
+      pageProps,
+      serverState
+    }
+  }
+
+  constructor (props) {
+    super(props)
+    this.apollo = initApollo(props.serverState.apollo.data)
   }
 
   render () {
     const {Component, pageProps} = this.props
     return <Container>
-      <Component {...pageProps} />
+      <ApolloProvider client={this.apollo}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Container>
   }
 }
-
-export default withData(MyApp)
