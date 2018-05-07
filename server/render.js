7f3de57548ffde5ef40c9a7338f732988a1bab2d
Tapani Moilanen <moilanen.tapani@gmail.com>
2018-05-07 07:10:38 +0300

    Wrap app component with enhancer when rendering
    App needs the same shared state from renderPage enhancer function as other components for styling to work

--- server/render.js
+++ server/render.js
@@ -82,7 +82,7 @@ async function doRender (req, res, pathname, query, {
   if (isResSent(res)) return
 
   const renderPage = (enhancer = Page => Page) => {
-    const app = createElement(App, {
+    const app = createElement(enhancer(App), {
       Component: enhancer(Component),
       router,
       ...props
