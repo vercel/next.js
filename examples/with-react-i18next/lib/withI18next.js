0d4d7d840a1769ad0c65a9c62efa5958b429f894
Andrew Razumovsky <Grimones00@gmail.com>
2018-05-09 16:06:37 +0300

    [with-react-i18next] Fix flickering on page change via <Link /> (#4318)
    * Fix flickering on page change via <Link />
    
    * Remove process.browser check

--- examples/with-react-i18next/lib/withI18next.js
+++ examples/with-react-i18next/lib/withI18next.js
@@ -1,4 +1,4 @@
-import { translate } from 'react-i18next'
+import { translate, loadNamespaces } from 'react-i18next'
 import { getInitialProps, I18n } from '../i18n'
 
 export const withI18next = (namespaces = ['common']) => ComposedComponent => {
@@ -11,8 +11,12 @@ export const withI18next = (namespaces = ['common']) => ComposedComponent => {
       ? await ComposedComponent.getInitialProps(ctx)
       : {}
 
-    const i18nInitialProps =
-      ctx.req && !process.browser ? getInitialProps(ctx.req, namespaces) : {}
+    const i18nInitialProps = ctx.req 
+      ? getInitialProps(ctx.req, namespaces)
+      : await loadNamespaces({
+          components: [{ props: { namespaces } }],
+          i18n: I18n,
+        });
 
     return {
       ...composedInitialProps,
