160f503774b5f2437da23e02ab4a43fa24f140c2
Henrik Wenz <henrik.wenz@gmail.com>
2018-05-15 20:43:33 +0200

    Update example/with-electron
    Electron recommends to disable nodeIntegration in BrowserViews.
    This PR follows this recommendation and updates the example accordingly.
    
     Changes:
    
    - disable nodeIntegration
    - update dependencies

--- examples/with-electron/renderer/pages/start.js
+++ examples/with-electron/renderer/pages/start.js
@@ -1,5 +1,4 @@
 import { Component } from 'react'
-import { ipcRenderer } from 'electron'
 
 export default class extends Component {
   state = {
@@ -9,12 +8,12 @@ export default class extends Component {
 
   componentDidMount () {
     // start listening the channel message
-    ipcRenderer.on('message', this.handleMessage)
+    global.ipcRenderer.on('message', this.handleMessage)
   }
 
   componentWillUnmount () {
     // stop listening the channel message
-    ipcRenderer.removeListener('message', this.handleMessage)
+    global.ipcRenderer.removeListener('message', this.handleMessage)
   }
 
   handleMessage = (event, message) => {
@@ -28,7 +27,7 @@ export default class extends Component {
 
   handleSubmit = event => {
     event.preventDefault()
-    ipcRenderer.send('message', this.state.input)
+    global.ipcRenderer.send('message', this.state.input)
     this.setState({ message: null })
   }
 
