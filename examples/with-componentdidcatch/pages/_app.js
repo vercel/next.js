21b175391ae065857115a1ae8729d60cc560b88b
luc <luc.leray@gmail.com>
2018-05-17 00:43:38 +0200

    Revert _app.js

--- examples/with-componentdidcatch/pages/_app.js
+++ examples/with-componentdidcatch/pages/_app.js
@@ -1,48 +1,9 @@
-import App, { Container } from 'next/app'
-import React from 'react'
+import App from 'next/app'
 
 export default class MyApp extends App {
-  static async getInitialProps ({ Component, router, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
-  state = { error: null, errorInfo: null }
-
   componentDidCatch (error, errorInfo) {
-    this.setState({ error, errorInfo })
-
-    // If you wish to render error with the usual error page, you can use :
-    // super.componentDidCatch(error, errorInfo)
-  }
-
-  render () {
-    const { Component, pageProps } = this.props
-
-    if (this.state.errorInfo) {
-      return (
-        <Container>
-          <div>
-            <h2>Something went wrong.</h2>
-            <details style={{ whiteSpace: 'pre-wrap' }}>
-              {this.state.error && this.state.error.toString()}
-              <br />
-              {this.state.errorInfo.componentStack}
-            </details>
-          </div>
-        </Container>
-      )
-    }
-
-    return (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    )
+    console.log('CUSTOM ERROR HANDLING', error)
+    // This is needed to render errors correctly in development / production
+    super.componentDidCatch(error, errorInfo)
   }
 }
