b6d2a727b9e9bd60295a9322eeb61cf22e09b051
luc <luc.leray@gmail.com>
2018-05-16 22:52:37 +0200

    Fix example with-componentdidcatch

--- examples/with-componentdidcatch/pages/index.js
+++ examples/with-componentdidcatch/pages/index.js
@@ -1,4 +1,7 @@
 export default () => {
-  // Render time error
-  throw new Error('Test')
+  // componentDidCatch only catches client-side errors
+  if (typeof window !== 'undefined') {
+    throw new Error('Test')
+  }
+  return <div>server rendered</div>
 }
