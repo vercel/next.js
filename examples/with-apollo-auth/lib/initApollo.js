59aa5a4dde548e1b06423301977f148a70f3b2d9
Yuri Yakovlev <contact@yakovlevyuri.com>
2018-05-16 12:09:23 +0200

    Return empty string instead of null in the Authorization (#4389)
    I was unable to make right check of `authorization` in the header on GraphQL side because now it returns `null` as string instead of empty string

--- examples/with-apollo-auth/lib/initApollo.js
+++ examples/with-apollo-auth/lib/initApollo.js
@@ -21,7 +21,7 @@ function create (initialState, { getToken }) {
     return {
       headers: {
         ...headers,
-        authorization: token ? `Bearer ${token}` : null
+        authorization: token ? `Bearer ${token}` : ''
       }
     }
   })
