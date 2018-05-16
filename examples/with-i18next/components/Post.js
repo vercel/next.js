41c5b2068bd0c6bd766e689f5c9a7529988388ac
Jesús Lobos <jlobos@outlook.cl>
2018-05-16 05:16:18 -0400

    Trailing space removed

--- examples/with-i18next/components/Post.js
+++ examples/with-i18next/components/Post.js
@@ -4,7 +4,7 @@ import { translate } from 'react-i18next'
 class Post extends React.Component {
   render () {
     const { t } = this.props
-    
+
     return (
       <div>
         {t('namespace1:greatMorning')}
