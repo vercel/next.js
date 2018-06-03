c8cfe18f6025a1614bc2c41dc11d2599e4f968f6
JJ Kasper <jj@jjsweb.site>
2018-06-03 16:36:44 -0500

    Update with-jest-typescript example

--- examples/with-jest-typescript/jest.config.js
+++ examples/with-jest-typescript/jest.config.js
@@ -2,11 +2,6 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
 
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  globals: {
-    'ts-jest': {
-      'useBabelrc': true
-    }
-  },
   testRegex: TEST_REGEX,
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
