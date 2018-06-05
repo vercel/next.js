fdf466acde58541c06876d32f1b83d2148c5f867
Nick Gauthier <ngauthier@gmail.com>
2018-06-05 13:50:34 -0400

    Upgrade with-jest-typescript example to next 6.0.0

--- examples/with-jest-typescript/jest.config.js
+++ examples/with-jest-typescript/jest.config.js
@@ -2,14 +2,9 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
 
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  globals: {
-    'ts-jest': {
-      'useBabelrc': true
-    }
-  },
   testRegex: TEST_REGEX,
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'babel-jest'
   },
   testPathIgnorePatterns: [
     '<rootDir>/.next/', '<rootDir>/node_modules/'
