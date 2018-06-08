9731221289f3299282f2a6c76b39059645a657f5
Brice BERNARD <brikou@gmail.com>
2018-06-08 10:05:12 +0200

    Remove useless check from constructor
    We don't pass apolloClient as props, so I removed the check as confusing and always false. Again please explain me if I am wrong :)

--- examples/with-apollo-auth/lib/withApollo.js
+++ examples/with-apollo-auth/lib/withApollo.js
@@ -79,9 +79,7 @@ export default App => {
       super(props)
       // `getDataFromTree` renders the component first, the client is passed off as a property.
       // After that rendering is done using Next's normal rendering pipeline
-      this.apolloClient =
-        props.apolloClient ||
-        initApollo(props.apolloState.data, {
+      this.apolloClient = initApollo(props.apolloState.data, {
           getToken: () => parseCookies().token
         })
     }
