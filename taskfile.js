54dd1251d9d92ee05fd2b7fdc504d1cd569e2629
Kevin Decker <kpdecker@gmail.com>
2018-06-16 19:46:39 -0500

    Update lib compile to target babel env

--- taskfile.js
+++ taskfile.js
@@ -2,42 +2,56 @@ const notifier = require('node-notifier')
 const childProcess = require('child_process')
 const isWindows = /^win/.test(process.platform)
 
+const babelNodeConfig = {
+  babelrc: false,
+  presets: [
+    ['env', {
+      targets: {
+        node: 'current'
+      }
+    }],
+    'react'
+  ],
+  plugins: ['transform-object-rest-spread', 'transform-class-properties']
+}
+
 export async function compile (task) {
-  await task.parallel(['bin', 'server', 'lib', 'client'])
+  await task.parallel(['bin', 'server', 'pages', 'lib', 'client'])
 }
 
 export async function bin (task, opts) {
-  await task.source(opts.src || 'bin/*').babel().target('dist/bin', {mode: '0755'})
+  await task.source(opts.src || 'bin/*').babel(babelNodeConfig).target('dist/bin', {mode: '0755'})
   notify('Compiled binaries')
 }
 
 export async function lib (task, opts) {
-  await task.source(opts.src || 'lib/**/*.js').babel().target('dist/lib')
+  await task.source(opts.src || 'lib/**/*.js').babel(babelNodeConfig).target('dist/lib')
   notify('Compiled lib files')
 }
 
+export async function pages (task, opts) {
+  await task.source(opts.src || 'pages/**/*.js').babel(babelNodeConfig).target('dist/pages')
+  notify('Compiled pages files')
+}
+
 export async function server (task, opts) {
-  await task.source(opts.src || 'server/**/*.js').babel().target('dist/server')
+  await task.source(opts.src || 'server/**/*.js').babel(babelNodeConfig).target('dist/server')
   notify('Compiled server files')
 }
 
 export async function client (task, opts) {
-  await task.source(opts.src || 'client/**/*.js').babel().target('dist/client')
+  await task.source(opts.src || 'client/**/*.js').babel(babelNodeConfig).target('dist/client')
   notify('Compiled client files')
 }
 
-export async function copy (task) {
-  await task.source('pages/**/*.js').target('dist/pages')
-}
-
 export async function build (task) {
-  await task.serial(['copy', 'compile'])
+  await task.serial(['compile'])
 }
 
 export default async function (task) {
   await task.start('build')
   await task.watch('bin/*', 'bin')
-  await task.watch('pages/**/*.js', 'copy')
+  await task.watch('pages/**/*.js', 'pages')
   await task.watch('server/**/*.js', 'server')
   await task.watch('client/**/*.js', 'client')
   await task.watch('lib/**/*.js', 'lib')
