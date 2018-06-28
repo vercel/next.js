89d88fa358bacd946b2ebe150601d8980bfa947c
Tim Neutkens <timneutkens@me.com>
2018-06-28 19:19:20 +0200

    Remove obsolete parameter

--- test/integration/page-extensions/test/index.test.js
+++ test/integration/page-extensions/test/index.test.js
@@ -17,7 +17,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 describe('Page Extensions', () => {
   beforeAll(async () => {
     context.appPort = await findPort()
-    context.server = await launchApp(join(__dirname, '../'), context.appPort, true)
+    context.server = await launchApp(join(__dirname, '../'), context.appPort)
 
     // pre-build all pages at the start
     await Promise.all([
