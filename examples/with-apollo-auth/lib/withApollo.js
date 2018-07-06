20b4bd8d79a32d6ba884779d8f9bc9bebaac20ce
Marcelo Zapaia <zapaiamarce@gmail.com>
2018-07-06 10:06:47 -0300

    with-apollo-auth fix get token
     Fix get token in the constructor

--- examples/with-apollo-auth/lib/withApollo.js
+++ examples/with-apollo-auth/lib/withApollo.js
@@ -78,7 +78,7 @@ export default App => {
       // `getDataFromTree` renders the component first, the client is passed off as a property.
       // After that rendering is done using Next's normal rendering pipeline
       this.apolloClient = initApollo(props.apolloState, {
-        getToken: () => parseCookies().token
+        getToken: () => props.token
       })
     }
 
