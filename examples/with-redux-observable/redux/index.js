40727c5d0d1886f5885f3c6e8f880210016072a0
tomekmularczyk <tomasz.mularczyk@risendot.com>
2018-07-09 19:28:14 +0200

    update with-redux-observable example

--- examples/with-redux-observable/redux/index.js
+++ examples/with-redux-observable/redux/index.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { createEpicMiddleware } from 'redux-observable'
 import reducer from './reducer'
 import { rootEpic } from './epics'
 
 export default function initStore (initialState) {
-  const epicMiddleware = createEpicMiddleware(rootEpic)
+  const epicMiddleware = createEpicMiddleware()
   const logger = createLogger({ collapsed: true }) // log every action to see what's happening behind the scenes.
-  const reduxMiddleware = applyMiddleware(thunkMiddleware, epicMiddleware, logger)
+  const reduxMiddleware = applyMiddleware(epicMiddleware, logger)
 
-  return createStore(reducer, initialState, reduxMiddleware)
+  const store = createStore(reducer, initialState, reduxMiddleware)
+  epicMiddleware.run(rootEpic)
+
+  return store
 };
