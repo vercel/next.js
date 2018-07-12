5ff6c0d28831c97adf10cd7acf81e16e4a894fa6
Tim Neutkens <timneutkens@me.com>
2018-07-12 16:44:52 +0200

    Reload _app on self-accept

--- test/integration/app-document/test/client.js
+++ test/integration/app-document/test/client.js
@@ -8,33 +8,37 @@ import { check } from 'next-test-utils'
 export default (context, render) => {
   describe('Client side', () => {
     it('should detect the changes to pages/_app.js and display it', async () => {
-      const browser = await webdriver(context.appPort, '/')
-      const text = await browser
-        .elementByCss('#hello-hmr').text()
-      expect(text).toBe('Hello HMR')
+      let browser
+      try {
+        browser = await webdriver(context.appPort, '/')
+        const text = await browser.elementByCss('#hello-hmr').text()
+        expect(text).toBe('Hello HMR')
 
-      const appPath = join(__dirname, '../', 'pages', '_app.js')
+        const appPath = join(__dirname, '../', 'pages', '_app.js')
 
-      const originalContent = readFileSync(appPath, 'utf8')
-      const editedContent = originalContent.replace('Hello HMR', 'Hi HMR')
+        const originalContent = readFileSync(appPath, 'utf8')
+        const editedContent = originalContent.replace('Hello HMR', 'Hi HMR')
 
-      // change the content
-      writeFileSync(appPath, editedContent, 'utf8')
+        // change the content
+        writeFileSync(appPath, editedContent, 'utf8')
 
-      await check(
-        () => browser.elementByCss('body').text(),
-        /Hi HMR/
-      )
+        await check(
+          () => browser.elementByCss('body').text(),
+          /Hi HMR/
+        )
 
-      // add the original content
-      writeFileSync(appPath, originalContent, 'utf8')
+        // add the original content
+        writeFileSync(appPath, originalContent, 'utf8')
 
-      await check(
-        () => browser.elementByCss('body').text(),
-        /Hello HMR/
-      )
-
-      browser.close()
+        await check(
+          () => browser.elementByCss('body').text(),
+          /Hello HMR/
+        )
+      } finally {
+        if (browser) {
+          browser.close()
+        }
+      }
     })
 
     it('should detect the changes to pages/_document.js and display it', async () => {
