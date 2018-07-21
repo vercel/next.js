24b01f2f68c03d0c97dc7fb661c36ae138d9f9db
Adrian Li <li.adrianmc@gmail.com>
2018-07-21 02:54:28 -0400

    use new semantic-ui API and add _document.js

--- examples/with-semantic-ui/pages/index.js
+++ examples/with-semantic-ui/pages/index.js
@@ -1,19 +1,8 @@
-import 'semantic-ui-css/components/modal.css'
-import 'semantic-ui-css/components/header.css'
-import 'semantic-ui-css/components/button.css'
-import 'semantic-ui-css/components/list.css'
-import 'semantic-ui-css/components/icon.css'
-import 'semantic-ui-css/themes/default/assets/fonts/icons.eot'
-import 'semantic-ui-css/themes/default/assets/fonts/icons.woff'
-import 'semantic-ui-css/themes/default/assets/fonts/icons.woff2'
+import 'semantic-ui-css/semantic.min.css'
 import { Modal, Header, Button, List, Icon } from 'semantic-ui-react'
-import Head from 'next/head'
 
 export default () => (
   <div>
-    <Head>
-      <link rel='stylesheet' href='/_next/static/style.css' />
-    </Head>
     <Modal trigger={<Button>Show Modal</Button>}>
       <Modal.Header>Select a Photo</Modal.Header>
       <Modal.Content image>
@@ -25,7 +14,7 @@ export default () => (
       </Modal.Content>
     </Modal>
 
-    <List vertical relaxed>
+    <List relaxed>
       <List.Item>
         <List.Content>
           <List.Header as='a'>Next.js</List.Header>
