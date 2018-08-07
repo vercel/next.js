83970c908d808a48175382f2df62bac579b0df26
Thomas Hermann <novascreen@gmail.com>
2018-08-06 23:19:16 -0400

    Add with-strict-csp example (#4858)

---
+++ examples/with-strict-csp/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const next = require('next')
+const csp = require('./csp')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    csp(server)
+
+    server.use(handle)
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
