7a6a694a8ec68de1c2f49e712c0de3c38ddbed46
Gary Meehan <garymeehan@outlook.com>
2018-08-11 14:54:03 +0100

    add tests

--- test/integration/basic/pages/head.js
+++ test/integration/basic/pages/head.js
@@ -15,6 +15,20 @@ export default () => <div>
     <meta property='article:tag' content='tag2' key='tag2key' />
     <meta property='dedupe:tag' content='tag3' key='tag3key' />
     <meta property='dedupe:tag' content='tag4' key='tag4key' />
+    <meta property='og:image' content='ogImageTag1' key='ogImageTag1Key' />
+    <meta property='og:image' content='ogImageTag2' key='ogImageTag2Key' />
+    <meta property='og:image:alt' content='ogImageAltTag1' key='ogImageAltTag1Key' />
+    <meta property='og:image:alt' content='ogImageAltTag2' key='ogImageAltTag2Key' />
+    <meta property='og:image:width' content='ogImageWidthTag1' key='ogImageWidthTag1Key' />
+    <meta property='og:image:width' content='ogImageWidthTag2' key='ogImageWidthTag2Key' />
+    <meta property='og:image:height' content='ogImageHeightTag1' key='ogImageHeightTag1Key' />
+    <meta property='og:image:height' content='ogImageHeightTag2' key='ogImageHeightTag2Key' />
+    <meta property='og:image:type' content='ogImageTypeTag1' key='ogImageTypeTag1Key' />
+    <meta property='og:image:type' content='ogImageTypeTag2' key='ogImageTypeTag2Key' />
+    <meta property='og:image:secure_url' content='ogImageSecureUrlTag1' key='ogImageSecureUrlTag1Key' />
+    <meta property='og:image:secure_url' content='ogImageSecureUrlTag2' key='ogImageSecureUrlTag2Key' />
+    <meta property='og:image:url' content='ogImageUrlTag1' key='ogImageUrlTag1Key' />
+    <meta property='og:image:url' content='ogImageUrlTag2' key='ogImageUrlTag2Key' />
 
     <React.Fragment>
       <title>Fragment title</title>
