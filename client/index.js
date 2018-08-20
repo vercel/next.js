fb2c1b69e15ab5b30c589ed368015ccfdea6a11f
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-08-20 09:43:38 +0200

    'Promise' is undefined
    I get the following error on IE11. Do we still need this fix?

--- client/index.js
+++ client/index.js
@@ -9,15 +9,6 @@ import * as asset from '../lib/asset'
 import * as envConfig from '../lib/runtime-config'
 import ErrorBoundary from './error-boundary'
 
-// Polyfill Promise globally
-// This is needed because Webpack2's dynamic loading(common chunks) code
-// depends on Promise.
-// So, we need to polyfill it.
-// See: https://github.com/webpack/webpack/issues/4254
-if (!window.Promise) {
-  window.Promise = Promise
-}
-
 const {
   __NEXT_DATA__: {
     props,
