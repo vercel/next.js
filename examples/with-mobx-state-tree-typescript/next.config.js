5ba25c3933bf5bce2f58847979e324c8b8339d02
Don Alvarez <alvarezdon@outlook.com>
2018-08-28 19:34:55 -0700

    Fix eslint errors on next.config.js

--- examples/with-mobx-state-tree-typescript/next.config.js
+++ examples/with-mobx-state-tree-typescript/next.config.js
@@ -1,2 +1,2 @@
-const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+const withTypescript = require('@zeit/next-typescript')
+module.exports = withTypescript()
