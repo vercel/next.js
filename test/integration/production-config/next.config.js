3582496913b33092f2c7e869529c14c49b37b8e3
Tim Neutkens <tim@timneutkens.nl>
2018-09-11 20:03:20 +0200

    Trigger page register when module is executed (#5115)
    Solves inconsistent loading issues when one bundle is loaded faster than the other
    
    Fixes zeit/next-plugins#244
    Fixes #4997
    Fixes #4620

---
+++ test/integration/production-config/next.config.js
@@ -0,0 +1,23 @@
+const withCSS = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+const path = require('path')
+module.exports = withCSS(withSass({
+  onDemandEntries: {
+    // Make sure entries are not getting disposed.
+    maxInactiveAge: 1000 * 60 * 60
+  },
+  webpack (config, {buildId}) {
+    // When next-css is `npm link`ed we have to solve loaders from the project root
+    const nextLocation = path.join(require.resolve('next/package.json'), '../')
+    const nextCssNodeModulesLocation = path.join(
+      require.resolve('@zeit/next-css'),
+      '../../../node_modules'
+    )
+
+    if (nextCssNodeModulesLocation.indexOf(nextLocation) === -1) {
+      config.resolveLoader.modules.push(nextCssNodeModulesLocation)
+    }
+
+    return config
+  }
+}))
