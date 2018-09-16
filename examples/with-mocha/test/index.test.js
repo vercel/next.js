ff151d26573a46b4af9380b0a3a65e5c657bb1f2
Stephen Sauceda <contact@stephensauceda.com>
2018-09-16 17:23:38 -0400

    add mocha example

---
+++ examples/with-mocha/test/index.test.js
@@ -0,0 +1,13 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import expect from 'expect.js'
+
+import App from '../pages/index.js'
+
+describe('With Enzyme', () => {
+  it('App shows "Hello world!"', () => {
+    const app = shallow(<App />)
+
+    expect(app.find('p').text()).to.equal('Hello World!')
+  })
+})
