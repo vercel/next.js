777c8844bebda6f8bade893ac56ce2a96710d1b1
Tim Neutkens <timneutkens@me.com>
2018-09-25 13:37:58 +0200

    Fix test

--- test/integration/basic/pages/dynamic/multiple-modules.js
+++ test/integration/basic/pages/dynamic/multiple-modules.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 import dynamic from 'next/dynamic'
 
-const Hello = dynamic(import(/* webpackChunkName: 'hello1' */ '../../components/hello1'))
-const Hello2 = dynamic(import(/* webpackChunkName: 'hello2' */ '../../components/hello2'))
+const Hello = dynamic(import(/* webpackChunkName: 'hello1' */ '../../components/hello3'))
+const Hello2 = dynamic(import(/* webpackChunkName: 'hello2' */ '../../components/hello4'))
 
 export default () => {
  return <div>
