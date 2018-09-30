95ad035b231beb4107c881207742807121aa8909
Tim Neutkens <timneutkens@me.com>
2018-10-01 00:12:40 +0200

    Remove root babel.config.js

--- packages/next/babel.config.js
+++ packages/next/babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  'presets': [require('../../babel.config.js')],
+  'presets': [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-flow'
+  ],
   'plugins': [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-transform-runtime', {
+      'corejs': 2
+    }],
     ['babel-plugin-transform-define', {
       'process.env.NEXT_VERSION': require('./package.json').version
     }]
