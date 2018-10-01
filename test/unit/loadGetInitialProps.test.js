c97187b97c77470459127fd4fe4a508966285c39
Tim Neutkens <timneutkens@me.com>
2018-10-02 00:33:45 +0200

    Fix paths

--- test/unit/loadGetInitialProps.test.js
+++ test/unit/loadGetInitialProps.test.js
@@ -1,5 +1,5 @@
 /* global describe, it, expect */
-import { loadGetInitialProps } from 'next/dist/lib/utils'
+import { loadGetInitialProps } from 'next-server/dist/lib/utils'
 
 describe('loadGetInitialProps', () => {
   it('should throw if getInitialProps is defined as an instance method', () => {
