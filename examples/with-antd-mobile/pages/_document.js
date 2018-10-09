2f1b30035e39c1c8a51b8570a879e2176b095e47
Adam Lane <enalmada@gmail.com>
2018-10-09 15:10:32 -0700

    update antd-mobile to v2

--- examples/with-antd-mobile/pages/_document.js
+++ examples/with-antd-mobile/pages/_document.js
@@ -1,7 +1,8 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
-// Note that fastclick is here because it is in the antd-mobile docs
+// Note that antd-mobile docs initialize fast click here
 // but it may not be required: https://github.com/ant-design/ant-design-mobile/issues/576
+// https://developers.google.com/web/updates/2013/12/300ms-tap-delay-gone-away
 
 export default class extends Document {
   render () {
@@ -10,20 +11,6 @@ export default class extends Document {
         <Head>
 
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
-          <script src="https://as.alipayobjects.com/g/component/fastclick/1.0.6/fastclick.js"></script>
-
-          <style dangerouslySetInnerHTML={{
-            __html: `
-              if ('addEventListener' in document) {
-                document.addEventListener('DOMContentLoaded', function() {
-                  FastClick.attach(document.body);
-                }, false);
-              }
-              if(!window.Promise) {
-                document.writeln('<script src="https://as.alipayobjects.com/g/component/es6-promise/3.2.2/es6-promise.min.js"'+'>'+'<'+'/'+'script>');
-              }
-          `,
-          }}/>
 
           <link rel='stylesheet' type='text/css' href='//unpkg.com/antd-mobile/dist/antd-mobile.min.css' />
         </Head>
