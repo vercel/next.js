2104febf6a4db2d8b9d1c5a2ff3b73235c9ca0a3
Truong Hoang Dung <checkraiser11@gmail.com>
2018-10-12 06:49:48 +0700

    Check i18n.getInitialProps for null

--- examples/with-react-i18next/i18n.js
+++ examples/with-react-i18next/i18n.js
@@ -39,23 +39,25 @@ if (!i18n.isInitialized) i18n.init(options)
 
 // a simple helper to getInitialProps passed on loaded i18n data
 i18n.getInitialProps = (req, namespaces) => {
-  if (!namespaces) namespaces = i18n.options.defaultNS
-  if (typeof namespaces === 'string') namespaces = [namespaces]
+  if (req && req.i18n) {
+    if (!namespaces) namespaces = i18n.options.defaultNS
+    if (typeof namespaces === 'string') namespaces = [namespaces]
 
-  req.i18n.toJSON = () => {} // do not serialize i18next instance to prevent circular references on the client
+    req.i18n.toJSON = () => {} // do not serialize i18next instance to prevent circular references on the client
 
-  const initialI18nStore = {}
-  req.i18n.languages.forEach((l) => {
-    initialI18nStore[l] = {}
-    namespaces.forEach((ns) => {
-      initialI18nStore[l][ns] = (req.i18n.services.resourceStore.data[l] || {})[ns] || {}
+    const initialI18nStore = {}
+    req.i18n.languages.forEach((l) => {
+      initialI18nStore[l] = {}
+      namespaces.forEach((ns) => {
+        initialI18nStore[l][ns] = (req.i18n.services.resourceStore.data[l] || {})[ns] || {}
+      })
     })
-  })
 
-  return {
-    i18n: req.i18n, // use the instance on req - fixed language on request (avoid issues in race conditions with lngs of different users)
-    initialI18nStore,
-    initialLanguage: req.i18n.language
+    return {
+      i18n: req.i18n, // use the instance on req - fixed language on request (avoid issues in race conditions with lngs of different users)
+      initialI18nStore,
+      initialLanguage: req.i18n.language
+    }
   }
 }
 
