3ddd73eaf4c81faa8ec65a55a1fd6e40d7e5d231
Pawel Sas <p.sas@gingco.net>
2018-10-17 08:15:43 +0200

    Update to default layout

--- examples/with-dynamic-app-layout/pages/_app.js
+++ examples/with-dynamic-app-layout/pages/_app.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import App, { Container } from 'next/app'
+import App, {Container} from 'next/app'
+
+import DefaultLayout from '../layouts/BlueLayout'
 
 export default class MyApp extends App {
   render () {
-    const { Component, pageProps } = this.props
-    return <Container>
-      <Component.Layout>
-        <Component {...pageProps} />
-      </Component.Layout>
-    </Container>
+    const {Component, pageProps} = this.props
+    const Layout = Component.Layout || DefaultLayout
+
+    return (
+      <Container>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Container>
+    )
   }
 }
