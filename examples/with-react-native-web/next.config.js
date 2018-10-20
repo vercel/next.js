1671fb14a2e28be93048a35fe14570a3c899439c
David Calhoun <dpcalhoun@gmail.com>
2018-10-20 13:38:16 -0500

    [change] Update react-native-web example
    Utilize ES modules, which are now the default export for
    `react-native-web`.
    
    _NOTE:_ [This example requires `next@^7.0.0`](https://bit.ly/2PaEhao).

---
+++ examples/with-react-native-web/next.config.js
@@ -0,0 +1,32 @@
+const esNodeModules = /react-native-web(?!.*node_modules)/;
+const cjsNodeModules = /node_modules\/(?!react-native-web(?!.*node_modules))/;
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Alias all `react-native` imports to `react-native-web`
+    config.resolve.alias = {
+      'react-native$': 'react-native-web',
+    };
+
+    // Compile ES modules
+    config.resolve.symlinks = false;
+    config.externals = config.externals.map(
+      external =>
+        typeof external === 'function'
+          ? (ctx, req, cb) => (esNodeModules.test(req) ? cb(null) : external(ctx, req, cb))
+          : external
+    );
+    config.module.rules.push({
+      test: /\.js$/,
+      loader: defaultLoaders.babel,
+      include: [esNodeModules],
+    });
+
+    return config;
+  },
+  webpackDevMiddleware: config => {
+    // Ignore CJS modules
+    config.watchOptions.ignored = [config.watchOptions.ignored[0], cjsNodeModules];
+    return config;
+  },
+};
