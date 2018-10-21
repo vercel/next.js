f33f7fb440c96ab9441d15a2f877173976e3d127
David Calhoun <dpcalhoun@gmail.com>
2018-10-21 11:46:48 -0500

    [change] Use next-plugin-transpile-modules
    Replace custom webpack config with Next.js plugin.

--- examples/with-react-native-web/next.config.js
+++ examples/with-react-native-web/next.config.js
@@ -1,32 +1,13 @@
-const esNodeModules = /react-native-web(?!.*node_modules)/
-const cjsNodeModules = /node_modules\/(?!react-native-web(?!.*node_modules))/
+const withTM = require('next-plugin-transpile-modules')
 
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+module.exports = withTM({
+  transpileModules: ['react-native-web'],
+  webpack: (config) => {
     // Alias all `react-native` imports to `react-native-web`
     config.resolve.alias = {
       'react-native$': 'react-native-web'
     }
 
-    // Compile ES modules
-    config.resolve.symlinks = false
-    config.externals = config.externals.map(
-      external =>
-        typeof external === 'function'
-          ? (ctx, req, cb) => (esNodeModules.test(req) ? cb(null) : external(ctx, req, cb))
-          : external
-    )
-    config.module.rules.push({
-      test: /\.js$/,
-      loader: defaultLoaders.babel,
-      include: [esNodeModules]
-    })
-
-    return config
-  },
-  webpackDevMiddleware: config => {
-    // Ignore CJS modules
-    config.watchOptions.ignored = [config.watchOptions.ignored[0], cjsNodeModules]
     return config
   }
-}
+})
