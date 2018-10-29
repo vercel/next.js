1336e8f80ddd0ebe954711932387b6bb95d11894
Anton Moiseev <a@antonmoiseev.com>
2018-10-29 19:53:15 +0400

    Replace _document.tsx example with _app.tsx

---
+++ examples/with-typescript/pages/_app.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+import Head from 'next/head'
+
+// App component is strongly typed with `@types/next`
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+    return <Container>
+      <Head>
+        <title>My new cool app</title>
+      </Head>
+      <Component {...pageProps} />
+    </Container>
+  }
+}
