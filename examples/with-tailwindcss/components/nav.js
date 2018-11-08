998b595523975f2a9297c28c9431db3d65d372d3
Tim Neutkens <timneutkens@me.com>
2018-11-08 14:41:36 +0100

    Clean up example

--- examples/with-tailwindcss/components/nav.js
+++ examples/with-tailwindcss/components/nav.js
@@ -1,14 +1,12 @@
 import Link from 'next/link'
+
 const links = [
   { href: 'https://github.com/zeit/next.js', label: 'Github' },
   { href: 'https://nextjs.org/docs', label: 'Docs' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+]
 
-const Nav = () => (
-  <nav>
+export default function Nav () {
+  return <nav>
     <ul className='flex justify-between items-center p-8'>
       <li className='list-reset'>
         <Link prefetch href='/'>
@@ -17,8 +15,8 @@ const Nav = () => (
       </li>
       <ul className='flex justify-between items-center'>
         {links.map(
-          ({ key, href, label }) => (
-            <li key={key} className='list-reset ml-4'>
+          ({ href, label }) => (
+            <li key={`${href}${label}`} className='list-reset ml-4'>
               <Link href={href}>
                 <a className='btn-blue no-underline'>{label}</a>
               </Link>
@@ -28,6 +26,4 @@ const Nav = () => (
       </ul>
     </ul>
   </nav>
-)
-
-export default Nav
+}
