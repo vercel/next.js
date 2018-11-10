363016e202cea13f9b35403c7a906674ef78fe83
Olivier Tassinari <olivier.tassinari@gmail.com>
2018-11-10 13:24:35 +0100

    Update _error.js

--- packages/next/pages/_error.js
+++ packages/next/pages/_error.js
@@ -18,7 +18,7 @@ export default class Error extends React.Component {
     return <div style={styles.error}>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        {statusCode === 404 ? <meta name="robots" content="noindex" /> : null}
+        {statusCode === 404 ? <meta name='robots' content='noindex' /> : null}
         <title>{statusCode}: {title}</title>
       </Head>
       <div>
