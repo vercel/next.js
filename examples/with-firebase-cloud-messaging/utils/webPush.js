b54c2366bd095855e629cdbcbbb3f7c3bd34ca1d
Ming Hann <eldy8888@gmail.com>
2018-11-16 23:59:01 +0800

    added firebase cloud messaging example

---
+++ examples/with-firebase-cloud-messaging/utils/webPush.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app'
+import 'firebase/messaging'
+import localforage from 'localforage'
+
+const firebaseCloudMessaging = {
+	tokenInlocalforage: async () => {
+		return await localforage.getItem('fcm_token', (err, value) => value)
+	},
+
+	init: async function() {
+		firebase.initializeApp({
+			messagingSenderId: 'your sender id'
+		})
+
+		try {
+			if ((await this.tokenInlocalforage()) !== null) {
+				return false
+			}
+
+			const messaging = firebase.messaging()
+			await messaging.requestPermission()
+			const token = await messaging.getToken()
+
+			localforage.setItem('fcm_token', token)
+			console.log('fcm_token', token)
+		} catch (error) {
+			console.error(error)
+		}
+	}
+}
+
+export { firebaseCloudMessaging }
