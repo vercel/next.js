0e99c01e364c11cf193fac6e28b67fb5cc889668
Adam Stankiewicz <sheerun@sher.pl>
2018-11-21 20:39:08 +0100

    Improve with-sentry example

--- examples/with-sentry/pages/index.js
+++ examples/with-sentry/pages/index.js
@@ -1,23 +1,48 @@
 import React from 'react'
+import Link from 'next/link'
 
 class Index extends React.Component {
+  static getInitialProps ({ query, req }) {
+    if (query.raiseError) {
+      throw new Error('Error in getInitialProps')
+    }
+  }
+
   state = {
     raiseError: false
   }
 
   componentDidUpdate () {
-    if (this.state.raiseError) {
-      throw new Error('Houston, we have a problem')
+    if (this.state.raiseErrorInUpdate) {
+      throw new Error('Error in componentDidUpdate')
     }
   }
 
-  raiseError = () => this.setState({ raiseError: true })
+  raiseErrorInUpdate = () => this.setState({ raiseErrorInUpdate: '1' })
+  raiseErrorInRender = () => this.setState({ raiseErrorInRender: '1' })
 
   render () {
+    if (this.state.raiseErrorInRender) {
+      throw new Error('Error in render')
+    }
+
     return (
       <div>
         <h2>Index page</h2>
-        <button onClick={this.raiseError}>Click to raise the error</button>
+        <ul>
+          <li><a href='#' onClick={this.raiseErrorInRender}>Raise the error in render</a></li>
+          <li><a href='#' onClick={this.raiseErrorInUpdate}>Raise the error in componentDidUpdate</a></li>
+          <li>
+            <Link href={{ pathname: '/', query: { raiseError: '1' } }}>
+              <a>Raise error in getInitialProps of client-loaded page</a>
+            </Link>
+          </li>
+          <li>
+            <a href='/?raiseError=1'>
+              Raise error in getInitialProps of server-loaded page
+            </a>
+          </li>
+        </ul>
       </div>
     )
   }
---
+++ examples/with-sentry/utils/sentry.js
@@ -0,0 +1,62 @@
+const Sentry = require('@sentry/node')
+const Cookie = require('js-cookie')
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    release: process.env.SENTRY_RELEASE,
+    maxBreadcrumbs: 50,
+    attachStacktrace: true
+  })
+}
+
+function captureException (err, { req, res, extra }) {
+  Sentry.configureScope(scope => {
+    if (err.message) {
+      // De-duplication currently doesn't work correctly for SSR / browser errors
+      // so we force deduplication by error message if it is present
+      scope.setFingerprint([err.message])
+    }
+
+    if (err.statusCode) {
+      scope.setExtra('statusCode', err.statusCode)
+    }
+
+    if (res && res.statusCode) {
+      scope.setExtra('statusCode', res.statusCode)
+    }
+
+    if (process.browser) {
+      scope.setTag('ssr', false)
+
+      // On client-side we use js-cookie package to fetch it
+      const sessionId = Cookie.get('sid')
+      if (sessionId) {
+        scope.setUser({ id: sessionId })
+      }
+    } else {
+      scope.setTag('ssr', true)
+      scope.setExtra('url', req.url)
+      scope.setExtra('params', req.params)
+      scope.setExtra('query', req.query)
+      scope.setExtra('headers', req.headers)
+
+      // On server-side we take session cookie directly from request
+      if (req.cookies.sid) {
+        scope.setUser({ id: req.cookies.sid })
+      }
+    }
+
+    if (extra) {
+      Object.keys(extra).forEach(key => {
+        scope.setExtra(key, extra[key])
+      })
+    }
+  })
+
+  Sentry.captureException(err)
+}
+
+module.exports = {
+  captureException
+}
