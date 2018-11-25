b370077fa05eb5889e58532f0cfc0ece4876332d
Kevin Decker <kpdecker@gmail.com>
2018-11-25 16:13:03 -0600

    Fix duplicate load issue for page script
    This caused issues with not having a singleton for the side effect HOC, which caused the header to become corrupted at runtime.

--- server/document.js
+++ server/document.js
@@ -111,19 +111,6 @@ export class NextScript extends Component {
     _documentProps: PropTypes.any
   }
 
-  getChunkScript (filename, additionalProps = {}) {
-    let { publicPath } = this.context._documentProps.__NEXT_DATA__
-
-    return (
-      <script
-        key={filename}
-        type='text/javascript'
-        src={`${publicPath}${filename}`}
-        {...additionalProps}
-      />
-    )
-  }
-
   getScripts () {
     const { __NEXT_DATA__, entrypoints } = this.context._documentProps
     const { pathname } = __NEXT_DATA__
@@ -132,25 +119,28 @@ export class NextScript extends Component {
 
     // In the production mode, we have a single asset with all the JS content.
     // So, we can load the script with async
-    return scripts.map(name => this.getChunkScript(name, { async: true }))
+    return scripts.map((filename) => {
+      let { publicPath } = this.context._documentProps.__NEXT_DATA__
+
+      return (
+        <script
+          key={filename}
+          type='text/javascript'
+          src={`${publicPath}${filename}`}
+          async
+        />
+      )
+    })
   }
 
   render () {
     const { __NEXT_DATA__ } = this.context._documentProps
-    const { pathname, publicPath } = __NEXT_DATA__
-    const pagePathname = getPagePathname(pathname)
 
     return <div>
       <script nonce={this.props.nonce} dangerouslySetInnerHTML={{
         __html: `module={};__NEXT_DATA__ = ${htmlescape(__NEXT_DATA__)}`
       }} />
-      <script async id={`__NEXT_PAGE__${pathname}`} type='text/javascript' src={`${publicPath}pages${pagePathname}.js`} />
       {this.getScripts()}
     </div>
   }
-}
-
-function getPagePathname (pathname) {
-  if (pathname === '/') return '/index'
-  return pathname.replace(/(\/index)?\.js$/, '')
 }
