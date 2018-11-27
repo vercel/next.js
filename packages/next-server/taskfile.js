b1502af79cfc90d25ab64103995ad9105f290652
Tim Neutkens <timneutkens@me.com>
2018-11-27 16:54:05 +0100

    Run files through typescript instead of babel

--- packages/next-server/taskfile.js
+++ packages/next-server/taskfile.js
@@ -1,12 +1,7 @@
 const notifier = require('node-notifier')
 
-export async function bin (task, opts) {
-  await task.source(opts.src || 'bin/*').babel().target('dist/bin', {mode: '0755'})
-  notify('Compiled binaries')
-}
-
 export async function lib (task, opts) {
-  await task.source(opts.src || 'lib/**/*.js').babel().target('dist/lib')
+  await task.source(opts.src || 'lib/**/*.js').typescript({module: 'commonjs'}).target('dist/lib')
   notify('Compiled lib files')
 }
 
@@ -16,12 +11,11 @@ export async function server (task, opts) {
 }
 
 export async function build (task) {
-  await task.parallel(['bin', 'server', 'lib'])
+  await task.parallel(['server', 'lib'])
 }
 
 export default async function (task) {
   await task.start('build')
-  await task.watch('bin/*', 'bin')
   await task.watch('server/**/*.js', 'server')
   await task.watch('lib/**/*.js', 'lib')
 }
