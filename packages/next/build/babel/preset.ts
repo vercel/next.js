2d5b56a181a3635cffd5b75aacc2ce87c317053e
Tim Neutkens <timneutkens@me.com>
2018-12-04 20:10:05 +0100

    Disable usage of esmodules when commonjs is enabled on the preset

--- packages/next/build/babel/preset.ts
+++ packages/next/build/babel/preset.ts
@@ -48,14 +48,15 @@ type BabelPreset = {
 }
 
 module.exports = (context: any, options: NextBabelPresetOptions = {}): BabelPreset => {
+  const presetEnvConfig = {
+    // In the test environment `modules` is often needed to be set to true, babel figures that out by itself using the `'auto'` option
+    // In production/development this option is set to `false` so that webpack can handle import/export with tree-shaking
+    modules: isDevelopment || isProduction ? false : 'auto',
+    ...options['preset-env']
+  }
   return {
     presets: [
-      [require('@babel/preset-env').default, {
-        // In the test environment `modules` is often needed to be set to true, babel figures that out by itself using the `'auto'` option
-        // In production/development this option is set to `false` so that webpack can handle import/export with tree-shaking
-        modules: isDevelopment || isProduction ? false : 'auto',
-        ...options['preset-env']
-      }],
+      [require('@babel/preset-env').default, presetEnvConfig],
       [require('@babel/preset-react'), {
         // This adds @babel/plugin-transform-react-jsx-source and
         // @babel/plugin-transform-react-jsx-self automatically in development
@@ -74,7 +75,7 @@ module.exports = (context: any, options: NextBabelPresetOptions = {}): BabelPres
         corejs: 2,
         helpers: true,
         regenerator: true,
-        useESModules: !isTest,
+        useESModules: !isTest && presetEnvConfig.modules !== 'commonjs',
         ...options['transform-runtime']
       }],
       [require('styled-jsx/babel'), styledJsxOptions(options['styled-jsx'])],
