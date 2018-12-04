83a708f9458949d7f44d4de533b2b1444d5ef35c
Oscar Busk <oscar.busk@gmail.com>
2018-12-04 23:31:21 +0100

    Revert accidental spacing changes

--- examples/with-firebase-hosting-and-typescript/src/functions/index.ts
+++ examples/with-firebase-hosting-and-typescript/src/functions/index.ts
@@ -6,6 +6,6 @@ const app = next({ dev, conf: { distDir: 'next' } });
 const handle = app.getRequestHandler();
 
 export const nextApp = functions.https.onRequest((req, res) => {
-    console.log('File: ' + req.originalUrl);
-    return app.prepare().then(() => handle(req, res));
+  console.log('File: ' + req.originalUrl);
+  return app.prepare().then(() => handle(req, res));
 });
