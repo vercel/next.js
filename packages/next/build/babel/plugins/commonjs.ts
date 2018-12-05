6542750e1295aea4f9c86c0f8f035bbe855f7b3c
Tim Neutkens <tim@timneutkens.nl>
2018-12-05 14:37:26 +0100

    Fix edge case where file had `module.export` in the content (#5823)
    We ran into this eg on hyper-site, which has `module.exports` in the content.

---
+++ packages/next/build/babel/plugins/commonjs.ts
@@ -0,0 +1,34 @@
+import {PluginObj} from '@babel/core'
+import {NodePath} from '@babel/traverse'
+import {Program} from '@babel/types'
+import commonjsPlugin from '@babel/plugin-transform-modules-commonjs'
+// Rewrite imports using next/<something> to next-server/<something>
+export default function NextToNextServer (...args: any): PluginObj {
+  const commonjs = commonjsPlugin(...args)
+  return {
+    visitor: {
+      Program: {
+        exit (path: NodePath<Program>, state) {
+          let foundModuleExports = false
+          path.traverse({
+            MemberExpression (
+              path: any
+            ) {
+              if (path.node.object.name !== 'module') return
+              if (path.node.property.name !== 'exports') return
+              foundModuleExports = true
+              console.log('FOUND', state.file.opts.filename)
+            }
+          })
+
+          if (!foundModuleExports) {
+            console.log('NOT FOUND', state.file.opts.filename)
+            return
+          }
+
+          commonjs.visitor.Program.exit.call(this, path, state)
+        }
+      }
+    }
+  }
+}
