1a5fc941ce53a3e192a388b0caa65922078e96e4
Tim Neutkens <timneutkens@me.com>
2018-12-05 15:05:14 +0100

    Remove console.log after verifying the correct files are ignored

--- packages/next/build/babel/plugins/commonjs.ts
+++ packages/next/build/babel/plugins/commonjs.ts
@@ -17,12 +17,10 @@ export default function NextToNextServer (...args: any): PluginObj {
               if (path.node.object.name !== 'module') return
               if (path.node.property.name !== 'exports') return
               foundModuleExports = true
-              console.log('FOUND', state.file.opts.filename)
             }
           })
 
           if (!foundModuleExports) {
-            console.log('NOT FOUND', state.file.opts.filename)
             return
           }
 
