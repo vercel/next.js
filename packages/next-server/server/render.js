0e6d190706678c95bcf7f6556bad5e50ecf4d8e5
Tim Neutkens <tim@timneutkens.nl>
2018-12-07 15:52:29 +0100

    Move sendHTML and rewrite in ts (#5839)
    * Move send-html function and rewrite in typescript
    
    * Move getPageFiles and convert to ts
    
    * Move getPageFiles and convert to ts (#5841)
    
    * Move getPageFiles and convert to ts
    
    # Conflicts:
    #	packages/next-server/server/render.js
    
    * Fix unit tests

--- packages/next-server/server/render.js
+++ packages/next-server/server/render.js
@@ -1,8 +1,6 @@
 import { join } from 'path'
 import React from 'react'
 import { renderToString, renderToStaticMarkup } from 'react-dom/server'
-import generateETag from 'etag'
-import fresh from 'fresh'
 import {requirePage} from './require'
 import Router from '../lib/router/router'
 import { loadGetInitialProps, isResSent } from '../lib/utils'
@@ -140,32 +138,6 @@ async function doRender (req, res, pathname, query, {
   }} />
 
   return '<!DOCTYPE html>' + renderToStaticMarkup(doc)
-}
-
-export function sendHTML (req, res, html, method, { dev, generateEtags }) {
-  if (isResSent(res)) return
-  const etag = generateEtags && generateETag(html)
-
-  if (fresh(req.headers, { etag })) {
-    res.statusCode = 304
-    res.end()
-    return
-  }
-
-  if (dev) {
-    // In dev, we should not cache pages for any reason.
-    res.setHeader('Cache-Control', 'no-store, must-revalidate')
-  }
-
-  if (etag) {
-    res.setHeader('ETag', etag)
-  }
-
-  if (!res.getHeader('Content-Type')) {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  }
-  res.setHeader('Content-Length', Buffer.byteLength(html))
-  res.end(method === 'HEAD' ? null : html)
 }
 
 function errorToJSON (err) {
