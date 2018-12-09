2c4cfa420b13016ebeea6efb306f1b634b84ae00
Kevin Decker <kpdecker@gmail.com>
2018-12-09 10:23:56 -0600

    Remove client-side async transpile

--- client/index.js
+++ client/index.js
@@ -30,80 +30,82 @@ const errorContainer = document.getElementById('__next-error')
 let lastAppProps
 export let router
 export let ErrorComponent
-let Component
 
 export const emitter = new EventEmitter()
 
-export default async () => {
-  ErrorComponent = await loadPage('/_error')
+export default () => {
+  return Promise.all([
+    loadPage('/_error'),
+    loadPage(pathname).catch(console.error)
+  ])
+    .then(([_ErrorComponent, Component]) => {
+      ErrorComponent = _ErrorComponent
+      Component = Component || ErrorComponent
 
-  try {
-    Component = await loadPage(pathname)
-  } catch (err) {
-    console.error(err)
-    Component = ErrorComponent
-  }
+      router = createRouter(pathname, query, asPath, {
+        Component,
+        ErrorComponent,
+        err
+      })
 
-  router = createRouter(pathname, query, asPath, {
-    Component,
-    ErrorComponent,
-    err
-  })
+      router.subscribe(({ Component, props, hash, err }) => {
+        render({ Component, props, err, hash, emitter })
+      })
 
-  router.subscribe(({ Component, props, hash, err }) => {
-    render({ Component, props, err, hash, emitter })
-  })
+      const hash = location.hash.substring(1)
+      render({ Component, props, hash, err, emitter })
 
-  const hash = location.hash.substring(1)
-  render({ Component, props, hash, err, emitter })
-
-  return emitter
+      return emitter
+    })
 }
 
-export async function render ({ Component, props, hash, err, emitter: emitterProp = emitter }) {
+export function render ({ Component, props, hash, err, emitter: emitterProp = emitter }) {
   // There are some errors we should ignore.
   // Next.js rendering logic knows how to handle them.
   // These are specially 404 errors
   if (err && !err.ignore) {
-    await renderError(err)
-    return
+    return renderError(err)
   }
 
-  try {
-    if (!props && Component &&
-    Component !== ErrorComponent &&
-    lastAppProps.Component === ErrorComponent) {
-    // fetch props if ErrorComponent was replaced with a page component by HMR
-      const { pathname, query, asPath } = router
-      props = await Component.getInitialProps({ err, pathname, query, asPath })
-    }
-
-    Component = Component || lastAppProps.Component
-    props = props || lastAppProps.props
-
-    const appProps = { Component, props, hash, err, router }
-    // lastAppProps has to be set before ReactDom.render to account for ReactDom throwing an error.
-    lastAppProps = appProps
-
-    emitterProp.emit('before-reactdom-render', { Component, ErrorComponent, appProps })
-
-    // We need to clear any existing runtime error messages
-    ReactDOM.unmountComponentAtNode(errorContainer)
-    errorContainer.innerHTML = ''
-
-    renderReactElement(createElement(App, appProps), appContainer)
-
-    emitterProp.emit('after-reactdom-render', { Component, ErrorComponent, appProps })
-  } catch (err) {
-    if (err.abort) return
-    await renderError(err)
+  let loadProps
+  if (!props && Component &&
+  Component !== ErrorComponent &&
+  lastAppProps.Component === ErrorComponent) {
+  // fetch props if ErrorComponent was replaced with a page component by HMR
+    const { pathname, query, asPath } = router
+    loadProps = Component.getInitialProps({ err, pathname, query, asPath })
+  } else {
+    loadProps = Promise.resolve(props)
   }
+
+  return loadProps
+    .then((props) => {
+      Component = Component || lastAppProps.Component
+      props = props || lastAppProps.props
+
+      const appProps = { Component, props, hash, err, router }
+      // lastAppProps has to be set before ReactDom.render to account for ReactDom throwing an error.
+      lastAppProps = appProps
+
+      emitterProp.emit('before-reactdom-render', { Component, ErrorComponent, appProps })
+
+      // We need to clear any existing runtime error messages
+      ReactDOM.unmountComponentAtNode(errorContainer)
+      errorContainer.innerHTML = ''
+
+      renderReactElement(createElement(App, appProps), appContainer)
+
+      emitterProp.emit('after-reactdom-render', { Component, ErrorComponent, appProps })
+    }).catch((err) => {
+      if (err.abort) return
+      return renderError(err)
+    })
 }
 
 // This method handles all runtime and debug errors.
 // 404 and 500 errors are special kind of errors
 // and they are still handle via the main render method.
-export async function renderError (error) {
+export function renderError (error) {
   // We need to unmount the current app component because it's
   // in the inconsistant state.
   // Otherwise, we need to face issues when the issue is fixed and
@@ -112,11 +114,13 @@ export async function renderError (error) {
 
   console.error(error)
 
-  const props = await ErrorComponent.getInitialProps({ err: error, pathname, query, asPath })
-  const appProps = { Component: ErrorComponent, props, err, router }
-  renderReactElement(createElement(App, appProps), errorContainer)
+  return ErrorComponent.getInitialProps({ err: error, pathname, query, asPath })
+    .then((props) => {
+      const appProps = { Component: ErrorComponent, props, err, router }
+      renderReactElement(createElement(App, appProps), errorContainer)
 
-  appContainer.innerHTML = ''
+      appContainer.innerHTML = ''
+    })
 }
 
 let isInitialRender = true
