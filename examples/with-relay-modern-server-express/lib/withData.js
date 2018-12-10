a996fba09c68f513dc0081a5105cede95dffa4a5
Diogo Dutra <diogodutradamata@gmail.com>
2018-12-10 13:50:35 -0200

    Added a new example with relay modern and a graphql server with express. (#4670)
    * Added a new example with relay modern and a graphql server with express.
    
    * removed .graphqlconfig file from with-relay-modern-server-express example

---
+++ examples/with-relay-modern-server-express/lib/withData.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import initEnvironment from './createRelayEnvironment'
+import { fetchQuery } from 'react-relay'
+import RelayProvider from './RelayProvider'
+
+export default (ComposedComponent, options = {}) => {
+  return class WithData extends React.Component {
+    static displayName = `WithData(${ComposedComponent.displayName})`
+
+    static async getInitialProps (ctx) {
+      // Evaluate the composed component's getInitialProps()
+      let composedInitialProps = {}
+      if (ComposedComponent.getInitialProps) {
+        composedInitialProps = await ComposedComponent.getInitialProps(ctx)
+      }
+
+      let queryProps = {}
+      let queryRecords = {}
+      const environment = initEnvironment()
+
+      if (options.query) {
+        const variables = composedInitialProps.relayVariables || {}
+        // TODO: Consider RelayQueryResponseCache
+        // https://github.com/facebook/relay/issues/1687#issuecomment-302931855
+        queryProps = await fetchQuery(environment, options.query, variables)
+        queryRecords = environment.getStore().getSource().toJSON()
+      }
+
+      return {
+        ...composedInitialProps,
+        ...queryProps,
+        queryRecords
+      }
+    }
+
+    constructor (props) {
+      super(props)
+      this.environment = initEnvironment({
+        records: props.queryRecords
+      })
+    }
+
+    render () {
+      return (
+        <RelayProvider environment={this.environment} variables={{}}>
+          <ComposedComponent {...this.props} />
+        </RelayProvider>
+      )
+    }
+  }
+}
