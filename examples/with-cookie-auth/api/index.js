5e370cf4f80e5b1903e0c929e549e8431cf22175
thinkxl <jolvera@posteo.de>
2018-12-11 19:50:41 -0600

    Replace Express with Micro on the index.js file

--- examples/with-cookie-auth/api/index.js
+++ examples/with-cookie-auth/api/index.js
@@ -1,7 +1,7 @@
 const { send } = require('micro')
 
 const index = async (req, res) => {
-  send(res, 200, '<h1>Hello from Express on Now 2.0!</h2>')
+  send(res, 200, '<h1>Hello from Micro on Now 2.0!</h2>')
 }
 
 module.exports = index
