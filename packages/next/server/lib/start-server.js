2ed3e7c4498cbefcfde6121cfcf6100023fb169f
Brian Beck <exogen@gmail.com>
2018-12-16 20:42:59 -0800

    Improve dev experience by listening faster

--- packages/next/server/lib/start-server.js
+++ packages/next/server/lib/start-server.js
@@ -3,7 +3,6 @@ import next from '../next'
 
 export default async function start (serverOptions, port, hostname) {
   const app = next(serverOptions)
-  await app.prepare()
   const srv = http.createServer(app.getRequestHandler())
   await new Promise((resolve, reject) => {
     // This code catches EADDRINUSE error if the port is already in use
@@ -11,4 +10,7 @@ export default async function start (serverOptions, port, hostname) {
     srv.on('listening', () => resolve())
     srv.listen(port, hostname)
   })
+  // It's up to caller to run `app.prepare()`, so it can notify that the server
+  // is listening before starting any intensive operations.
+  return app
 }
