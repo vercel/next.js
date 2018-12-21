f4c18824867d4ecd50129a5a88f5d7227ba6805b
Tim Neutkens <timneutkens@me.com>
2018-12-21 14:13:38 +0100

    Implement file generation in loader

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -1,23 +1,16 @@
-import { join, dirname } from 'path'
+import { join } from 'path'
 import nanoid from 'nanoid'
 import loadConfig from 'next-server/next-config'
-import { PHASE_PRODUCTION_BUILD, BUILD_MANIFEST, REACT_LOADABLE_MANIFEST } from 'next-server/constants'
-
+import { PHASE_PRODUCTION_BUILD } from 'next-server/constants'
 import getBaseWebpackConfig from './webpack-config'
 import {generateBuildId} from './generate-build-id'
 import {writeBuildId} from './write-build-id'
 import {isWriteable} from './is-writeable'
 import {runCompiler, CompilerResult} from './compiler'
 import globModule from 'glob'
-import mkdirpModule from 'mkdirp'
-import rimrafModule from 'rimraf'
 import {promisify} from 'util'
-import fsModule from 'fs'
 
 const glob = promisify(globModule)
-const mkdirp = promisify(mkdirpModule)
-const writeFile = promisify(fsModule.writeFile)
-const rimraf = promisify(rimrafModule)
 
 function collectPages (directory: string, pageExtensions: string[]): Promise<string[]> {
   return glob(`**/*.+(${pageExtensions.join('|')})`, {cwd: directory})
@@ -43,79 +36,13 @@ export default async function build (dir: string, conf = null, target: string = 
   }, {})
 
   let entrypoints
-  let tmpServerless: any
   if (target === 'serverless') {
-    tmpServerless = join(distDir, 'tmp')
-    const buildManifest = join(distDir, BUILD_MANIFEST)
-    const reactLoadableManifest = join(distDir, REACT_LOADABLE_MANIFEST)
-    await mkdirp(tmpServerless)
-
     const serverlessEntrypoints: any = {}
-    await Promise.all(Object.keys(pages).map(async (page) => {
+    Object.keys(pages).forEach(async (page) => {
       const relativePagePath = pages[page]
-      const absolutePagePath = join(pagesDir, relativePagePath)
-      const source = `
-        import {parse} from 'url'
-        import {renderToHTML} from 'next-server/dist/server/render';
-        import buildManifest from '${buildManifest}';
-        import reactLoadableManifest from '${reactLoadableManifest}';
-        import Document from 'next/dist/pages/_document';
-        import Error from 'next/dist/pages/_error';
-        import App from 'next/dist/pages/_app';
-        import Component from '${absolutePagePath}';
-        module.exports = async (req, res) => {
-          try {
-            const parsedUrl = parse(req.url, true)
-            try {
-              const result = await renderToHTML(req, res, "${page}", parsedUrl.query, {
-                App,
-                Document,
-                Component,
-                buildManifest,
-                reactLoadableManifest,
-                buildId: "${buildId}"
-              })
-              return result
-            } catch (err) {
-              if (err.code === 'ENOENT') {
-                res.statusCode = 404
-                const result = await renderToHTML(req, res, "/_error", parsedUrl.query, {
-                  App,
-                  Document,
-                  Component: Error,
-                  buildManifest,
-                  reactLoadableManifest,
-                  buildId: "${buildId}"
-                })
-                return result
-              } else {
-                console.error(err)
-                res.statusCode = 500
-                const result = await renderToHTML(req, res, "/_error", parsedUrl.query, {
-                  App,
-                  Document,
-                  Component: Error,
-                  buildManifest,
-                  reactLoadableManifest,
-                  buildId: "${buildId}"
-                })
-                return result
-              }
-            }
-          } catch(err) {
-            console.error(err)
-            res.statusCode = 500
-            res.end('Internal Server Error')
-          }
-        }
-      `
-
       const bundleFile = page === '/' ? '/index.js' : `${page}.js`
-      const tmpFile = join(tmpServerless, bundleFile)
-      await mkdirp(dirname(tmpFile))
-      await writeFile(tmpFile, source, 'utf8')
-      serverlessEntrypoints[join('serverless', bundleFile)] = tmpFile
-    }))
+      serverlessEntrypoints[join('serverless', bundleFile)] = `next-serverless-loader?page=${page}&absolutePagePath=${join(pagesDir, relativePagePath)}&distDir=${distDir}&buildId=${buildId}!`
+    })
 
     entrypoints = serverlessEntrypoints
   }
@@ -132,11 +59,6 @@ export default async function build (dir: string, conf = null, target: string = 
     result = {warnings: [...clientResult.warnings, ...serverResult.warnings], errors: [...clientResult.errors, ...serverResult.errors]}
   } else {
     result = await runCompiler(configs)
-  }
-
-  // Clean up temporary files
-  if (tmpServerless) {
-    await rimraf(tmpServerless)
   }
 
   if (result.warnings.length > 0) {
---
+++ packages/next/build/webpack/loaders/next-serverless-loader.ts
@@ -0,0 +1,74 @@
+import {loader} from 'webpack'
+import {join} from 'path'
+import {parse} from 'querystring'
+import { BUILD_MANIFEST, REACT_LOADABLE_MANIFEST } from 'next-server/constants'
+
+type Query = {
+  page: string,
+  distDir: string,
+  absolutePagePath: string,
+  buildId: string
+}
+
+const nextServerlessLoader: loader.Loader = function () {
+  const {distDir, absolutePagePath, page, buildId}: Query = typeof this.query === 'string' ? parse(this.query) : this.query
+  const buildManifest = join(distDir, BUILD_MANIFEST)
+  const reactLoadableManifest = join(distDir, REACT_LOADABLE_MANIFEST)
+  return `
+    import {parse} from 'url'
+    import {renderToHTML} from 'next-server/dist/server/render';
+    import buildManifest from '${buildManifest}';
+    import reactLoadableManifest from '${reactLoadableManifest}';
+    import Document from 'next/dist/pages/_document';
+    import Error from 'next/dist/pages/_error';
+    import App from 'next/dist/pages/_app';
+    import Component from '${absolutePagePath}';
+    module.exports = async (req, res) => {
+      try {
+        const parsedUrl = parse(req.url, true)
+        try {
+          const result = await renderToHTML(req, res, "${page}", parsedUrl.query, {
+            App,
+            Document,
+            Component,
+            buildManifest,
+            reactLoadableManifest,
+            buildId: "${buildId}"
+          })
+          return result
+        } catch (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404
+            const result = await renderToHTML(req, res, "/_error", parsedUrl.query, {
+              App,
+              Document,
+              Component: Error,
+              buildManifest,
+              reactLoadableManifest,
+              buildId: "${buildId}"
+            })
+            return result
+          } else {
+            console.error(err)
+            res.statusCode = 500
+            const result = await renderToHTML(req, res, "/_error", parsedUrl.query, {
+              App,
+              Document,
+              Component: Error,
+              buildManifest,
+              reactLoadableManifest,
+              buildId: "${buildId}"
+            })
+            return result
+          }
+        }
+      } catch(err) {
+        console.error(err)
+        res.statusCode = 500
+        res.end('Internal Server Error')
+      }
+    }
+  `
+}
+
+export default nextServerlessLoader
