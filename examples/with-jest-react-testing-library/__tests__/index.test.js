4ec8866a875478cc4c3848fce95a7193d476ef5c
jeanbauerc@thoughtworks.com <jbauer@thoughtworks.com>
2018-12-23 18:05:40 -0200

    Add react-testing-library example

---
+++ examples/with-jest-react-testing-library/__tests__/index.test.js
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+
+import React from 'react'
+import { render } from 'react-testing-library'
+
+import App from '../pages/index.js'
+
+describe('With React Testing Library', () => {
+  it('Shows "Hello world!"', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('Hello World!')).not.toBeNull()
+  })
+})
