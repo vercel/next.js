d7922b8eb0832c5d7c03ea1128fdd6d908e59a1f
luc <luc.leray@gmail.com>
2019-01-03 23:13:07 +0800

    replace EventEmitter with mitt source code

--- packages/next-server/lib/event-emitter.js
+++ packages/next-server/lib/event-emitter.js
@@ -1,32 +1,60 @@
-export default class EventEmitter {
-  listeners = {}
+// @flow
+// An event handler can take an optional event argument
+// and should not return a value
+// type EventHandler = (event?: any) => void;
+// type WildCardEventHandler = (type: string, event?: any) => void
 
-  on (event, cb) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = new Set()
+// An array of all currently registered event handlers for a type
+// type EventHandlerList = Array<EventHandler>;
+// type WildCardEventHandlerList = Array<WildCardEventHandler>;
+// A map of event types and their corresponding event handlers.
+// type EventHandlerMap = {
+//   '*'?: WildCardEventHandlerList,
+//   [type: string]: EventHandlerList,
+// };
+
+/** Mitt: Tiny (~200b) functional event emitter / pubsub.
+ *  @name mitt
+ *  @returns {Mitt}
+ */
+export default function mitt (all) {
+  all = all || Object.create(null)
+
+  return {
+    /**
+     * Register an event handler for the given type.
+     *
+     * @param  {String} type  Type of event to listen for, or `"*"` for all events
+     * @param  {Function} handler Function to call in response to given event
+     * @memberOf mitt
+     */
+    on (type, handler) {
+      (all[type] || (all[type] = [])).push(handler)
+    },
+
+    /**
+     * Remove an event handler for the given type.
+     *
+     * @param  {String} type  Type of event to unregister `handler` from, or `"*"`
+     * @param  {Function} handler Handler function to remove
+     * @memberOf mitt
+     */
+    off (type, handler) {
+      if (all[type]) {
+        all[type].splice(all[type].indexOf(handler) >>> 0, 1)
+      }
+    },
+
+    /**
+     * Invoke all handlers for the given type.
+     * If present, `"*"` handlers are invoked after type-matched handlers.
+     *
+     * @param {String} type  The event type to invoke
+     * @param {Any} [evt]  Any value (object is recommended and powerful), passed to each handler
+     * @memberOf mitt
+     */
+    emit (type, ...evts) {
+      (all[type] || []).slice().map((handler) => { handler(...evts) })
     }
-
-    if (this.listeners[event].has(cb)) {
-      throw new Error(`Listener already exists for router event: \`${event}\``)
-    }
-
-    this.listeners[event].add(cb)
-    return this
-  }
-
-  emit (event, ...data) {
-    const listeners = this.listeners[event]
-    const hasListeners = listeners && listeners.size
-    if (!hasListeners) {
-      return false
-    }
-
-    listeners.forEach(cb => cb(...data)) // eslint-disable-line standard/no-callback-literal
-    return true
-  }
-
-  off (event, cb) {
-    this.listeners[event].delete(cb)
-    return this
   }
 }
