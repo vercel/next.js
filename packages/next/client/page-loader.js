fc19b233ebcb6074a55ef4a00771ef7b0a3bcc33
Luc <luc.leray@gmail.com>
2019-01-05 04:49:21 +0800

    Replace event-emitter.js by mitt (#5987)
    This PR aims at replacing next-server/lib/event-emitter.js by mitt.
    
    Fix https://github.com/zeit/next.js/issues/4908
    
    event-emitter.js is ~400 bytes gzipped vs mitt is 200 bytes

--- packages/next/client/page-loader.js
+++ packages/next/client/page-loader.js
@@ -1,5 +1,5 @@
 /* global document */
-import EventEmitter from 'next-server/dist/lib/event-emitter'
+import mitt from 'next-server/dist/lib/mitt'
 
 // smaller version of https://gist.github.com/igrigorik/a02f2359f3bc50ca7a9c
 function supportsPreload (list) {
@@ -22,7 +22,7 @@ export default class PageLoader {
 
     this.pageCache = {}
     this.prefetchCache = new Set()
-    this.pageRegisterEvents = new EventEmitter()
+    this.pageRegisterEvents = mitt()
     this.loadingRoutes = {}
   }
 
