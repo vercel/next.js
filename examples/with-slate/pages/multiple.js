cc470462f2dcfada8b637ec4024858fcf7b06693
astenmies <s.dolbecke@gmail.com>
2019-01-11 09:44:00 +0100

    Add multiple Editors example

---
+++ examples/with-slate/pages/multiple.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import CustomKeygenEditor from './CustomKeygenEditor'
+
+const content = {
+  'first-editor': 'This example shows how to have multiple instances of the editor.',
+  'second-editor': 'Without a custom key generator, you could not focus here.'
+}
+
+class MultipleEditors extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Link href='/'>
+          <a>Go to Home</a>
+        </Link>
+        {Object.keys(content).map((key, idx) => (
+          <CustomKeygenEditor key={idx} uniqueId={key} content={content[key]} style={{ margin: 20 }} />
+        ))}
+      </React.Fragment>
+    )
+  }
+}
+
+export default MultipleEditors
