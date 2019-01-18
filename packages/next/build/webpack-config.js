2161dcbdf126f5f02e47946605c77e38ff3bfe75
Connor Davis <mail@connordav.is>
2019-01-17 23:37:05 -0600

    Use webpack Ignore Plugin to exclude 'react-is'

--- packages/next/build/webpack-config.js
+++ packages/next/build/webpack-config.js
@@ -291,6 +291,7 @@ export default async function getBaseWebpackConfig (dir, {dev = false, isServer 
       dev && new UnlinkFilePlugin(),
       dev && new CaseSensitivePathPlugin(), // Since on macOS the filesystem is case-insensitive this will make sure your path are case-sensitive
       !dev && new webpack.HashedModuleIdsPlugin(),
+      process.env.NODE_ENV === 'production' && new webpack.IgnorePlugin(/^react-is$/),
       // Removes server/client code by minifier
       new webpack.DefinePlugin({
         'process.crossOrigin': JSON.stringify(config.crossOrigin),
