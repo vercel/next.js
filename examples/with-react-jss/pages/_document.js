93e9258ee824c76d568ba31ab440448809a49fe9
Henri Beck <henribeck.dev@gmail.com>
2019-01-20 14:01:46 +0100

    Fix react-jss example for styling component inside custom App component

--- examples/with-react-jss/pages/_document.js
+++ examples/with-react-jss/pages/_document.js
@@ -3,18 +3,24 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { SheetsRegistry, JssProvider } from 'react-jss'
 
 export default class JssDocument extends Document {
-  static getInitialProps (ctx) {
+  static async getInitialProps (ctx) {
     const registry = new SheetsRegistry()
-    const page = ctx.renderPage(App => props => (
-      <JssProvider registry={registry}>
-        <App {...props} />
-      </JssProvider>
-    ))
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => (
+          <JssProvider registry={registry}>
+            <App {...props} />
+          </JssProvider>
+        ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
 
     return {
-      ...page,
-      registry
-    }
+      ...initialProps,
+      registry,
+    };
   }
 
   render () {
