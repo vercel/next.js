340a18b5a4c11edf3930440c81785146ff55e9fe
Lucas Feliciano <mail@lucasfeliciano.com>
2019-01-23 16:49:41 +0100

    Manually call sheet.seal() to avoid memory leaks

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -4,17 +4,21 @@ import { ServerStyleSheet } from 'styled-components'
 export default class MyDocument extends Document {
   static async getInitialProps (ctx) {
     const sheet = new ServerStyleSheet()
-
     const originalRenderPage = ctx.renderPage
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-      })
 
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: [...initialProps.styles, ...sheet.getStyleElement()]
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [...initialProps.styles, ...sheet.getStyleElement()]
+      }
+    } finally {
+      sheet.seal()
     }
   }
 }
