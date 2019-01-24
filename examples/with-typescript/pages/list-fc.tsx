6d41ed7e6f2f5a16c419393b52ef80636c5ef3ae
Resi Respati <resir014@gmail.com>
2019-01-24 16:18:43 +0700

    [with-typescript] Updated typescript and removed unused deps (#6116)
    I've updated the TypeScript dependency to the latest version. Also
    removed some dependencies that may not be needed.
    
    I've also fixed tslint errors which may have appeared because of
    previous updates to this starter kit, as well as added comments
    to explain some parts of the code.

--- examples/with-typescript/pages/list-fc.tsx
+++ examples/with-typescript/pages/list-fc.tsx
@@ -7,17 +7,24 @@ type Props = {
   items: IDataObject[],
 }
 
-const list: NextFunctionComponent<Props> = ({ items }) => (
-  <Layout title="About | Next.js + TypeScript Example">
+const ListFunction: NextFunctionComponent<Props> = ({ items }) => (
+  <Layout title="List Example (with Function Components) | Next.js + TypeScript Example">
     <List items={items} />
   </Layout>
 )
 
-list.getInitialProps = async ({ pathname }: NextContext) => {
-  const dataArray: IDataObject[] =
-    [{ id: 101, name: 'larry' }, { id: 102, name: 'sam' }, { id: 103, name: 'jill' }, { id: 104, name: pathname }]
+ListFunction.getInitialProps = async ({ pathname }: NextContext) => {
+  // Example for including initial props in a Next.js function compnent page.
+  // Don't forget to include the respective types for any props passed into
+  // the component.
+  const dataArray: IDataObject[] = [
+    { id: 101, name: 'larry' },
+    { id: 102, name: 'sam' },
+    { id: 103, name: 'jill' },
+    { id: 104, name: pathname },
+  ]
 
   return { items: dataArray }
 }
 
-export default list
+export default ListFunction
