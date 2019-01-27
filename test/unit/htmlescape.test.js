5a5ad31735ad16efb99d2887a8b1eb6002e35f2a
Tim Neutkens <timneutkens@me.com>
2019-01-27 14:33:13 +0100

    Fix tests

--- test/unit/htmlescape.test.js
+++ test/unit/htmlescape.test.js
@@ -1,41 +1,41 @@
 /* eslint-env jest */
-import {htmlescape} from 'next/dist/server/htmlescape'
+import {htmlEscapeJsonString} from 'next/dist/server/htmlescape'
 import vm from 'vm'
 
 describe('htmlescape', () => {
   test('with angle brackets should escape', () => {
     const evilObj = {evil: '<script></script>'}
-    expect(htmlescape(evilObj)).toBe('{"evil":"\\u003cscript\\u003e\\u003c/script\\u003e"}')
+    expect(htmlEscapeJsonString(JSON.stringify(evilObj))).toBe('{"evil":"\\u003cscript\\u003e\\u003c/script\\u003e"}')
   })
 
   test('with angle brackets should parse back', () => {
     const evilObj = {evil: '<script></script>'}
-    expect(JSON.parse(htmlescape(evilObj))).toMatchObject(evilObj)
+    expect(JSON.parse(htmlEscapeJsonString(JSON.stringify(evilObj)))).toMatchObject(evilObj)
   })
 
   test('with ampersands should escape', () => {
     const evilObj = {evil: '&'}
-    expect(htmlescape(evilObj)).toBe('{"evil":"\\u0026"}')
+    expect(htmlEscapeJsonString(JSON.stringify(evilObj))).toBe('{"evil":"\\u0026"}')
   })
 
   test('with ampersands should parse back', () => {
     const evilObj = {evil: '&'}
-    expect(JSON.parse(htmlescape(evilObj))).toMatchObject(evilObj)
+    expect(JSON.parse(htmlEscapeJsonString(JSON.stringify(evilObj)))).toMatchObject(evilObj)
   })
 
   test('with "LINE SEPARATOR" and "PARAGRAPH SEPARATOR" should escape', () => {
     const evilObj = {evil: '\u2028\u2029'}
-    expect(htmlescape(evilObj)).toBe('{"evil":"\\u2028\\u2029"}')
+    expect(htmlEscapeJsonString(JSON.stringify(evilObj))).toBe('{"evil":"\\u2028\\u2029"}')
   })
 
   test('with "LINE SEPARATOR" and "PARAGRAPH SEPARATOR" should parse back', () => {
     const evilObj = {evil: '\u2028\u2029'}
-    expect(JSON.parse(htmlescape(evilObj))).toMatchObject(evilObj)
+    expect(JSON.parse(htmlEscapeJsonString(JSON.stringify(evilObj)))).toMatchObject(evilObj)
   })
 
   test('escaped line terminators should work', () => {
     expect(() => {
-      vm.runInNewContext('(' + htmlescape({evil: '\u2028\u2029'}) + ')')
+      vm.runInNewContext('(' + htmlEscapeJsonString(JSON.stringify({evil: '\u2028\u2029'})) + ')')
     }).not.toThrow()
   })
 })
