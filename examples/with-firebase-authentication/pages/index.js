2aab791383720e01b3922c083b65782290481f3d
lukas strassel <lukasstrassel@googlemail.com>
2019-01-31 20:39:30 +0100

    remove duplicate user

--- examples/with-firebase-authentication/pages/index.js
+++ examples/with-firebase-authentication/pages/index.js
@@ -7,12 +7,11 @@ import {AuthContext} from '../env/auth-context'
 
 export default class Index extends Component {
   static async getInitialProps ({ req, query }) {
-    const user = req && req.session ? req.session.decodedToken : null
     // don't fetch anything from firebase if the user is not found
     // const snap = user && await req.firebaseServer.database().ref('messages').once('value')
     // const messages = snap && snap.val()
     const messages = null
-    return { user, messages }
+    return { messages }
   }
 
   constructor (props) {
