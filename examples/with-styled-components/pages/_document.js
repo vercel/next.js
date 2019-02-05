87a63c760bd866ed99c1951157bf63449d0a2d56
rouzbeh84 <rouzbeh.sarrafieh@gmail.com>
2019-02-04 17:46:57 -0800

    fix(example): fixes styled-components example

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
         styles: [...initialProps.styles, ...sheet.getStyleElement()]
       }
     } finally {
-      sheet.seal()
+      Object.seal(sheet)
     }
   }
 }
