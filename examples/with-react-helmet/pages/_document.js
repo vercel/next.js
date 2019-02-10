b05df7087212ba4384d8aec93c769089491a1b27
Fredrik Höglund <fredrik.hoglund@gmail.com>
2019-02-10 20:48:13 +0100

    Fix first render of with-react-helmet example (#6235)

--- examples/with-react-helmet/pages/_document.js
+++ examples/with-react-helmet/pages/_document.js
@@ -26,24 +26,10 @@ export default class extends Document {
       .map(el => this.props.helmet[el].toComponent())
   }
 
-  get helmetJsx () {
-    return (
-      <Helmet
-        htmlAttributes={{ lang: 'en' }}
-        title='Hello next.js!'
-        meta={[
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-          { property: 'og:title', content: 'Hello next.js!' }
-        ]}
-      />
-    )
-  }
-
   render () {
     return (
       <html {...this.helmetHtmlAttrComponents}>
         <Head>
-          {this.helmetJsx}
           {this.helmetHeadComponents}
         </Head>
         <body {...this.helmetBodyAttrComponents}>
