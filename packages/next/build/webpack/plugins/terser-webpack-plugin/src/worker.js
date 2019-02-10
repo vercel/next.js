45f566355896ff166be7d69a4835fc6ce32a7658
Tim Neutkens <tim@timneutkens.nl>
2019-02-10 04:55:09 +0100

    Bring in terser-webpack-plugin (#6231)
    * Bring in terser-webpack-plugin
    
    * Ignore terser from linting

---
+++ packages/next/build/webpack/plugins/terser-webpack-plugin/src/worker.js
@@ -0,0 +1,21 @@
+import minify from './minify';
+
+module.exports = (options, callback) => {
+  try {
+    // 'use strict' => this === undefined (Clean Scope)
+    // Safer for possible security issues, albeit not critical at all here
+    // eslint-disable-next-line no-new-func, no-param-reassign
+    options = new Function(
+      'exports',
+      'require',
+      'module',
+      '__filename',
+      '__dirname',
+      `'use strict'\nreturn ${options}`
+    )(exports, require, module, __filename, __dirname);
+
+    callback(null, minify(options));
+  } catch (errors) {
+    callback(errors);
+  }
+};
