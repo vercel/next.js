0afe8dd780b20df4b0a6e711640772ebec4c51de
Kenneth Luján Rosas <kenneth.lujan@kw.com>
2019-02-14 15:25:59 -0500

    refactor: handle when fileName is already absolute

--- packages/next/server/lib/error-overlay-middleware.js
+++ packages/next/server/lib/error-overlay-middleware.js
@@ -1,17 +1,19 @@
 import url from 'url'
 import launchEditor from 'launch-editor'
+import fs from 'fs'
 import path from 'path'
 
-export default function errorOverlayMiddleware(req, res, next) {
-
-  const srcRoot = process.cwd()
-
+export default function errorOverlayMiddleware (req, res, next) {
   if (req.url.startsWith('/_next/development/open-stack-frame-in-editor')) {
     const query = url.parse(req.url, true).query
     const lineNumber = parseInt(query.lineNumber, 10) || 1
     const colNumber = parseInt(query.colNumber, 10) || 1
 
-    const resolvedFileName = path.join(srcRoot, query.fileName)
+    let resolvedFileName = query.fileName
+
+    if (!fs.existsSync(resolvedFileName)) {
+      resolvedFileName = path.join(process.cwd(), resolvedFileName)
+    }
 
     launchEditor(`${resolvedFileName}:${lineNumber}:${colNumber}`)
     res.end()
