616139315b357d84244a73b609979a49c3e03787
Kenneth Luján Rosas <kenneth.lujan@kw.com>
2019-02-14 14:07:33 -0500

    fix(launch-editor): resolve filename including current working directory

--- packages/next/server/lib/error-overlay-middleware.js
+++ packages/next/server/lib/error-overlay-middleware.js
@@ -1,12 +1,19 @@
 import url from 'url'
 import launchEditor from 'launch-editor'
+import path from 'path'
 
-export default function errorOverlayMiddleware (req, res, next) {
+export default function errorOverlayMiddleware(req, res, next) {
+
+  const srcRoot = process.cwd()
+
   if (req.url.startsWith('/_next/development/open-stack-frame-in-editor')) {
     const query = url.parse(req.url, true).query
     const lineNumber = parseInt(query.lineNumber, 10) || 1
     const colNumber = parseInt(query.colNumber, 10) || 1
-    launchEditor(`${query.fileName}:${lineNumber}:${colNumber}`)
+
+    const resolvedFileName = path.join(srcRoot, query.fileName)
+
+    launchEditor(`${resolvedFileName}:${lineNumber}:${colNumber}`)
     res.end()
   } else {
     next()
