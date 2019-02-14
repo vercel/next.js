3bd33802ae8facea16dec2d438c4c4163edb7297
Joe Haddad <joe.haddad@zeit.co>
2019-02-14 14:31:08 -0500

    Improve clear console sequence

--- packages/next/prompt/clearConsole.ts
+++ packages/next/prompt/clearConsole.ts
@@ -1,5 +1,20 @@
+const isInteractive = process.stdout.isTTY
+
 export function clearConsole() {
+  if (!isInteractive) {
+    return
+  }
+
+  // Push the user's current terminal output off the screen prior to erase
+  for (let row = 0; row < process.stdout.rows!; ++row) {
+    process.stdout.write(process.platform === 'win32' ? '\r\n' : '\n')
+  }
+
+  // \x1B: control terminal
+  //    [2J: erase entire display
+  //    [0f: move to line 0 (win32)
+  //    [H: go home (nix)
   process.stdout.write(
-    process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H',
+    process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[H',
   )
 }
