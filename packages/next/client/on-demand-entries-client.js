5d779a028942b4ada0e25d45ea7952447361a322
JJ Kasper <22380829+ijjk@users.noreply.github.com>
2019-02-15 15:22:21 -0600

    Add falling back to fetch based pinging for onDemandEntries (#6310)
    After discussion, I added falling back to fetch based pinging when the WebSocket fails to connect. I also added an example of how to proxy the onDemandEntries WebSocket when using a custom server. Fixes: #6296

--- packages/next/client/on-demand-entries-client.js
+++ packages/next/client/on-demand-entries-client.js
@@ -8,25 +8,37 @@ const wsProtocol = protocol.includes('https') ? 'wss' : 'ws'
 const retryTime = 5000
 let ws = null
 let lastHref = null
+let wsConnectTries = 0
+let showedWarning = false
 
 export default async ({ assetPrefix }) => {
   Router.ready(() => {
     Router.events.on('routeChangeComplete', ping)
   })
 
-  const setup = async (reconnect) => {
+  const setup = async () => {
     if (ws && ws.readyState === ws.OPEN) {
       return Promise.resolve()
+    } else if (wsConnectTries > 1) {
+      return
     }
+    wsConnectTries++
 
     return new Promise(resolve => {
       ws = new WebSocket(`${wsProtocol}://${hostname}:${process.env.__NEXT_WS_PORT}${process.env.__NEXT_WS_PROXY_PATH}`)
-      ws.onopen = () => resolve()
+      ws.onopen = () => {
+        wsConnectTries = 0
+        resolve()
+      }
       ws.onclose = () => {
         setTimeout(async () => {
-          // check if next restarted and we have to reload to get new port
           await fetch(`${assetPrefix}/_next/on-demand-entries-ping`)
-            .then(res => res.status === 200 && location.reload())
+            .then(res => {
+              // Only reload if next was restarted and we have a new WebSocket port
+              if (res.status === 200 && res.headers.get('port') !== process.env.__NEXT_WS_PORT + '') {
+                location.reload()
+              }
+            })
             .catch(() => {})
           await setup(true)
           resolve()
@@ -49,11 +61,36 @@ export default async ({ assetPrefix }) => {
       }
     })
   }
-  await setup()
+  setup()
 
   async function ping () {
-    if (ws.readyState === ws.OPEN) {
-      ws.send(Router.pathname)
+    // Use WebSocket if available
+    if (ws && ws.readyState === ws.OPEN) {
+      return ws.send(Router.pathname)
+    }
+    if (!showedWarning) {
+      console.warn('onDemandEntries WebSocket failed to connect, falling back to fetch based pinging. https://err.sh/zeit/next.js/on-demand-entries-websocket-unavailable')
+      showedWarning = true
+    }
+    // If not, fallback to fetch based pinging
+    try {
+      const url = `${assetPrefix || ''}/_next/on-demand-entries-ping?page=${Router.pathname}`
+      const res = await fetch(url, {
+        credentials: 'same-origin'
+      })
+      const payload = await res.json()
+      if (payload.invalid) {
+        // Payload can be invalid even if the page does not exist.
+        // So, we need to make sure it exists before reloading.
+        const pageRes = await fetch(location.href, {
+          credentials: 'same-origin'
+        })
+        if (pageRes.status === 200) {
+          location.reload()
+        }
+      }
+    } catch (err) {
+      console.error(`Error with on-demand-entries-ping: ${err.message}`)
     }
   }
 
