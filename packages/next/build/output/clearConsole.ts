ce4616cdbf6f755fba961443b96c61ff85503d5d
Joe Haddad <joe.haddad@zeit.co>
2019-02-15 14:36:22 -0500

    Add new clearConsole helper

---
+++ packages/next/build/output/clearConsole.ts
@@ -0,0 +1,13 @@
+// This file is derived from Jest:
+// https://github.com/facebook/jest/blob/d9d501ac342212b8a58ddb23a31518beb7b56f47/packages/jest-util/src/specialChars.ts#L18
+
+const isWindows = process.platform === 'win32'
+const isInteractive = process.stdout.isTTY
+
+export function clearConsole() {
+  if (!isInteractive) {
+    return
+  }
+
+  process.stdout.write(isWindows ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
+}
