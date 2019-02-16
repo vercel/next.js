c2b27471be63822d8880157c74c921d93fbda229
pkellner <peter@peterkellner.net>
2019-02-16 14:23:32 -0800

    added .eslintrc so that ` eslint . --fix` would work (not sure if that was really necessary). I assume that is same as `yarn lint-fix`.

---
+++ examples/with-env-from-next-config-js/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+
+        "node": true,
+        "browser": false,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
