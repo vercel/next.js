fb672478c130faaea5aa1e119d6c40077126ab1f
Kenneth Luján Rosas <kenneth.lujan@kw.com>
2019-02-18 09:46:54 -0500

    fix: pass full path to `launch-editor` (#6302)
    * fix(launch-editor): resolve filename including current working directory
    
    * refactor: handle when fileName is already absolute
    
    * feat: use Next.js dir instead of process.cwd()

--- packages/next/server/hot-reloader.js
+++ packages/next/server/hot-reloader.js
@@ -254,7 +254,7 @@ export default class HotReloader {
     this.middlewares = [
       webpackDevMiddleware,
       webpackHotMiddleware,
-      errorOverlayMiddleware,
+      errorOverlayMiddleware({ dir: this.dir }),
       onDemandEntries.middleware()
     ]
   }
