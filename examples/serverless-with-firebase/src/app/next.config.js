7687acf9cc01ed8063f52f99ec492f03060f260f
Wei Wang <weiwio@outlook.com>
2019-02-18 16:13:14 -0800

    Fix single quote in serverless-with-firebase example

--- examples/serverless-with-firebase/src/app/next.config.js
+++ examples/serverless-with-firebase/src/app/next.config.js
@@ -1,6 +1,6 @@
 const withTypescript = require('@zeit/next-typescript')
 
 module.exports = withTypescript({
-  target: "serverless",
+  target: 'serverless',
   distDir: '../../dist/functions/next'
 })
