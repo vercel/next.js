1ffd3ad480016620d82c7f8151336cf609555e17
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2019-02-19 08:51:42 -0700

    Make `getBaseWebpackConfig` synchronous.

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -58,7 +58,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
   const pagePaths = await collectPages(pagesDir, config.pageExtensions)
   const pages = createPagesMapping(pagePaths, config.pageExtensions)
   const entrypoints = createEntrypoints(pages, config.target, buildId, config)
-  const configs: any = await Promise.all([
+  const configs: any = [
     getBaseWebpackConfig(dir, {
       buildId,
       isServer: false,
@@ -73,7 +73,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
       target: config.target,
       entrypoints: entrypoints.server,
     }),
-  ])
+  ]
 
   let result: CompilerResult = { warnings: [], errors: [] }
   if (config.target === 'serverless') {
