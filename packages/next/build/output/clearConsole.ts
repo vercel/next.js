90f9c64b74f0dae11c33627d2ce062744c2508e3
Joe Haddad <timer150@gmail.com>
2019-02-20 07:20:09 -0500

    Use alternate screen buffer if terminal supports it (#6364)

--- packages/next/build/output/clearConsole.ts
+++ packages/next/build/output/clearConsole.ts
@@ -2,12 +2,12 @@
 // https://github.com/facebook/jest/blob/d9d501ac342212b8a58ddb23a31518beb7b56f47/packages/jest-util/src/specialChars.ts#L18
 
 const isWindows = process.platform === 'win32'
-const isInteractive = process.stdout.isTTY
+const isInteractive = process.stderr.isTTY
 
 export function clearConsole() {
   if (!isInteractive) {
     return
   }
 
-  process.stdout.write(isWindows ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
+  process.stderr.write(isWindows ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
 }
