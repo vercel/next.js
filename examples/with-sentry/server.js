cfae3502b3b63f665a612755cf0d03852bba4ffa
Jess Telford <jess+github@jes.st>
2019-02-20 14:10:36 +1100

    Fixes for with-sentry example

--- examples/with-sentry/server.js
+++ examples/with-sentry/server.js
@@ -1,66 +1,62 @@
 const next = require('next')
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const Sentry = require('@sentry/node')
 const uuidv4 = require('uuid/v4')
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const handler = app.getRequestHandler()
 
-require('./utils/sentry')
+function sessionCookie (req, res, next) {
+  const htmlPage =
+    !req.path.match(/^\/(_next|static)/) &&
+    !req.path.match(/\.(js|map)$/) &&
+    req.accepts('text/html', 'text/css', 'image/png') === 'text/html'
 
-app.prepare()
-  .then(() => {
-    const server = express()
+  if (!htmlPage) {
+    next()
+    return
+  }
 
+  if (!req.cookies.sid || req.cookies.sid.length === 0) {
+    req.cookies.sid = uuidv4()
+    res.cookie('sid', req.cookies.sid)
+  }
+
+  next()
+}
+
+const sourcemapsForSentryOnly = token => (req, res, next) => {
+  // In production we only want to serve source maps for sentry
+  if (!dev && !!token && req.headers['x-sentry-token'] !== token) {
+    res
+      .status(401)
+      .send('Authentication access token is required to access the source map.')
+    return
+  }
+  next()
+}
+
+app.prepare().then(() => {
+  // The app.buildId is only available after app.prepare(), hence why we setup
+  // here.
+  const { Sentry } = require('./utils/sentry')({ release: app.buildId })
+
+  express()
     // This attaches request information to sentry errors
-    server.use(Sentry.Handlers.requestHandler())
-
-    server.use(cookieParser())
-
-    server.use((req, res, next) => {
-      const htmlPage =
-        !req.path.match(/^\/(_next|static)/) &&
-        !req.path.match(/\.(js|map)$/) &&
-        req.accepts('text/html', 'text/css', 'image/png') === 'text/html'
-
-      if (!htmlPage) {
-        next()
-        return
-      }
-
-      if (!req.cookies.sid || req.cookies.sid.length === 0) {
-        req.cookies.sid = uuidv4()
-        res.cookie('sid', req.cookies.sid)
-      }
-
-      next()
-    })
-
-    // In production we don't want to serve sourcemaps for anyone
-    if (!dev) {
-      const hasSentryToken = !!process.env.SENTRY_TOKEN
-      server.get(/\.map$/, (req, res, next) => {
-        if (hasSentryToken && req.headers['x-sentry-token'] !== process.env.SENTRY_TOKEN) {
-          res
-            .status(401)
-            .send(
-              'Authentication access token is required to access the source map.'
-            )
-          return
-        }
-        next()
-      })
-    }
-
-    server.get('*', (req, res) => handle(req, res))
-
+    .use(Sentry.Handlers.requestHandler())
+    .use(cookieParser())
+    .use(sessionCookie)
+    .get(/\.map$/, sourcemapsForSentryOnly(process.env.SENTRY_TOKEN))
+    // Regular next.js request handler
+    .use(handler)
     // This handles errors if they are thrown before raching the app
-    server.use(Sentry.Handlers.errorHandler())
-
-    server.listen(port, err => {
-      if (err) throw err
+    .use(Sentry.Handlers.errorHandler())
+    .listen(port, err => {
+      if (err) {
+        throw err
+      }
+      // eslint-disable-next-line no-console
       console.log(`> Ready on http://localhost:${port}`)
     })
-  })
+})
--- examples/with-sentry/utils/sentry.js
+++ examples/with-sentry/utils/sentry.js
@@ -1,63 +1,82 @@
+// NOTE: This require will be replaced with `@sentry/browser` when
+// process.browser === true thanks to the webpack config in next.config.js
 const Sentry = require('@sentry/node')
 const Cookie = require('js-cookie')
 
-if (process.env.SENTRY_DSN) {
-  Sentry.init({
+module.exports = ({ release }) => {
+  const sentryOptions = {
     dsn: process.env.SENTRY_DSN,
-    release: process.env.SENTRY_RELEASE,
+    release,
     maxBreadcrumbs: 50,
     attachStacktrace: true
-  })
-}
+  }
 
-function captureException (err, { req, res, errorInfo, query, pathname }) {
-  Sentry.configureScope(scope => {
-    if (err.message) {
-      // De-duplication currently doesn't work correctly for SSR / browser errors
-      // so we force deduplication by error message if it is present
-      scope.setFingerprint([err.message])
+  // When we're developing locally
+  if (process.env.NODE_ENV !== 'production') {
+    /* eslint-disable-next-line global-require */
+    const sentryTestkit = require('sentry-testkit')
+    const { sentryTransport } = sentryTestkit()
+
+    // Don't actually send the errors to Sentry
+    sentryOptions.transport = sentryTransport
+
+    // Instead, dump the errors to the console
+    sentryOptions.integrations = [new Sentry.Integrations.Debug({
+      // Trigger DevTools debugger instead of using console.log
+      debugger: false
+    })]
+  }
+
+  Sentry.init(sentryOptions)
+
+  return {
+    Sentry,
+    captureException: (err, { req, res, errorInfo, query, pathname }) => {
+      Sentry.configureScope((scope) => {
+        if (err.message) {
+          // De-duplication currently doesn't work correctly for SSR / browser errors
+          // so we force deduplication by error message if it is present
+          scope.setFingerprint([err.message])
+        }
+
+        if (err.statusCode) {
+          scope.setExtra('statusCode', err.statusCode)
+        }
+
+        if (res && res.statusCode) {
+          scope.setExtra('statusCode', res.statusCode)
+        }
+
+        if (process.browser) {
+          scope.setTag('ssr', false)
+          scope.setExtra('query', query)
+          scope.setExtra('pathname', pathname)
+
+          // On client-side we use js-cookie package to fetch it
+          const sessionId = Cookie.get('sid')
+          if (sessionId) {
+            scope.setUser({ id: sessionId })
+          }
+        } else {
+          scope.setTag('ssr', true)
+          scope.setExtra('url', req.url)
+          scope.setExtra('method', req.method)
+          scope.setExtra('headers', req.headers)
+          scope.setExtra('params', req.params)
+          scope.setExtra('query', req.query)
+
+          // On server-side we take session cookie directly from request
+          if (req.cookies.sid) {
+            scope.setUser({ id: req.cookies.sid })
+          }
+        }
+
+        if (errorInfo) {
+          Object.keys(errorInfo).forEach(key => scope.setExtra(key, errorInfo[key]))
+        }
+      })
+
+      return Sentry.captureException(err)
     }
-
-    if (err.statusCode) {
-      scope.setExtra('statusCode', err.statusCode)
-    }
-
-    if (res && res.statusCode) {
-      scope.setExtra('statusCode', res.statusCode)
-    }
-
-    if (process.browser) {
-      scope.setTag('ssr', false)
-      scope.setExtra('query', query)
-      scope.setExtra('pathname', pathname)
-
-      // On client-side we use js-cookie package to fetch it
-      const sessionId = Cookie.get('sid')
-      if (sessionId) {
-        scope.setUser({ id: sessionId })
-      }
-    } else {
-      scope.setTag('ssr', true)
-      scope.setExtra('url', req.url)
-      scope.setExtra('method', req.method)
-      scope.setExtra('headers', req.headers)
-      scope.setExtra('params', req.params)
-      scope.setExtra('query', req.query)
-
-      // On server-side we take session cookie directly from request
-      if (req.cookies.sid) {
-        scope.setUser({ id: req.cookies.sid })
-      }
-    }
-
-    if (errorInfo) {
-      scope.setExtra('componentStack', errorInfo.componentStack)
-    }
-  })
-
-  Sentry.captureException(err)
-}
-
-module.exports = {
-  captureException
+  }
 }
