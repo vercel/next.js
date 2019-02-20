8a35ac944ba2a96b4e1b51616c6ed822007c84ef
Jess Telford <jess+github@jes.st>
2019-02-21 10:20:49 +1100

    [with-sentry] Use env config instead of webpack where possible

--- examples/with-sentry/next.config.js
+++ examples/with-sentry/next.config.js
@@ -2,10 +2,12 @@ const webpack = require('webpack')
 const nextSourceMaps = require('@zeit/next-source-maps')()
 
 module.exports = nextSourceMaps({
+  env: {
+    SENTRY_DSN: process.env.SENTRY_DSN
+  },
   webpack: (config, { isServer, buildId }) => {
     config.plugins.push(
       new webpack.DefinePlugin({
-        'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
         'process.env.SENTRY_RELEASE': JSON.stringify(buildId)
       })
     )
