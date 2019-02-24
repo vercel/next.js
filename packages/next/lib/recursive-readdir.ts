9de831ba1ce3d0daa2d39561bee6670b2f9d9e82
Tim Neutkens <timneutkens@me.com>
2019-02-24 22:03:05 +0100

    Update comment

--- packages/next/lib/recursive-readdir.ts
+++ packages/next/lib/recursive-readdir.ts
@@ -10,7 +10,7 @@ const stat = promisify(fs.stat)
  * @param  {string} dir Directory to read
  * @param  {RegExp} filter Filter for the file name, only the name part is considered, not the full path
  * @param  {string[]=[]} arr This doesn't have to be provided, it's used for the recursion
- * @param  {RegExp=newRegExp(`${dir}[\\/]`} rootDirRegex Used to replace the initial path, only the relative path is left, it's faster than path.relative.
+ * @param  {string=dir`} rootDir Used to replace the initial path, only the relative path is left, it's faster than path.relative.
  * @returns Promise array holding all relative paths
  */
 export async function recursiveReadDir(dir: string, filter: RegExp, arr: string[] = [], rootDir: string = dir): Promise<string[]> {
