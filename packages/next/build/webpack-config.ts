e24aea81b6190a64e45f23f0887b67f3fa798580
Joe Haddad <joe.haddad@zeit.co>
2019-02-25 11:13:22 -0500

    Disable AMD support for the server
    We probably want to disable AMD everywhere in the future

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -88,35 +88,35 @@ export default function getBaseWebpackConfig (dir: string, {dev = false, isServe
         if (notExternalModules.indexOf(request) !== -1) {
           return callback()
         }
-    
+
         resolve(request, { basedir: context, preserveSymlinks: true }, (err, res) => {
           if (err) {
             return callback()
           }
-    
+
           if (!res) {
             return callback()
           }
-    
+
           // Default pages have to be transpiled
           if (res.match(/next[/\\]dist[/\\]/) || res.match(/node_modules[/\\]@babel[/\\]runtime[/\\]/) || res.match(/node_modules[/\\]@babel[/\\]runtime-corejs2[/\\]/)) {
             return callback()
           }
-    
+
           // Webpack itself has to be compiled because it doesn't always use module relative paths
           if (res.match(/node_modules[/\\]webpack/) || res.match(/node_modules[/\\]css-loader/)) {
             return callback()
           }
-    
+
           // styled-jsx has to be transpiled
           if (res.match(/node_modules[/\\]styled-jsx/)) {
             return callback()
           }
-    
+
           if (res.match(/node_modules[/\\].*\.js$/)) {
             return callback(undefined, `commonjs ${request}`)
           }
-    
+
           callback()
         })
       }
@@ -213,15 +213,13 @@ export default function getBaseWebpackConfig (dir: string, {dev = false, isServe
       rules: [
         ...isServer ? [{
           test: /\.(m?js|node)$/,
-          // this may be a breaking change
-          // parser: { amd: false },
+          parser: { amd: false },
           use: {
             loader: '@zeit/webpack-asset-relocator-loader',
             options: {
               outputAssetBase: 'assets',
               existingAssetNames: [],
-              // this may be a breaking change
-              wrapperCompatibility: false,
+              wrapperCompatibility: true,
               escapeNonAnalyzableRequires: true
             }
           }
