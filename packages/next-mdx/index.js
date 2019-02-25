3b790b22923e940776705b33630d1bcae212dd37
JJ Kasper <jj@jjsweb.site>
2019-02-25 15:08:46 -0600

    Remove unsupported version error from next-mdx

--- packages/next-mdx/index.js
+++ packages/next-mdx/index.js
@@ -3,12 +3,6 @@ module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
 
   return Object.assign({}, nextConfig, {
     webpack (config, options) {
-      if (!options.defaultLoaders) {
-        throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
-      }
-
       config.module.rules.push({
         test: extension,
         use: [
