135aeb98726074c834bc3307c0386e0dc2a32c88
JJ Kasper <jj@jjsweb.site>
2019-02-27 14:24:51 -0600

    Re-add Layout with just body styles in global styles

--- examples/experimental-amp/pages/index.js
+++ examples/experimental-amp/pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import { useAmp } from 'next/amp'
+import Layout from '../components/Layout'
 import Byline from '../components/Byline'
 
 export default () => {
   const isAmp = useAmp()
 
   return (
-    <>
+    <Layout>
       <Head>
         <title>The Cat</title>
       </Head>
@@ -201,11 +202,10 @@ export default () => {
         for lounge in doorway for chew iPad power cord.
       </p>
       <style jsx>{`
-        :global(body) {font-family: Roboto, sans-serif; padding: 30px; color: #444;}
         h1 {margin-bottom: 5px;}
         p {font-size: 18px; line-height: 30px; margin-top: 30px;}
         .caption {color: #ccc; margin-top: 0; font-size: 14px; text-align: center;}
       `}</style>
-    </>
+    </Layout>
   )
 }
