319b1ea58a561b2d5033d8683f9174b900b0f02e
JJ Kasper <jj@jjsweb.site>
2019-02-27 10:58:41 -0600

    Updated to default enabled to true

--- packages/next-bundle-analyzer/index.js
+++ packages/next-bundle-analyzer/index.js
@@ -1,4 +1,4 @@
-module.exports = pluginOptions => (nextConfig = {}) => {
+module.exports = (pluginOptions = { enabled: true }) => (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack (config, options) {
       if (pluginOptions.enabled) {
