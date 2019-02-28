8924306460e3f1b9268cf453b8b8652b8f3ac5ea
Connor Davis <mail@connordav.is>
2019-02-27 23:40:50 -0600

    Try and fix docs

--- packages/next/lib/recursive-helpers.ts
+++ packages/next/lib/recursive-helpers.ts
@@ -32,7 +32,7 @@ const deleteDir: pathCallback = async (_, absolute) => await rmdir(absolute).the
 /**
  * Recursively delete files
  * @param  {string} dir Directory to read
- * @param  {{force: boolean}} options Filter for the file name, only the name part is considered, not the full path
+ * @param  {{}} options None yet
  * @param  {string[]=[]} arr This doesn't have to be provided, it's used for the recursion
  * @param  {string=dir`} rootDir Used to replace the initial path, only the relative path is left, it's faster than path.relative.
  * @returns Promise array holding all relative paths
