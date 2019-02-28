ed5a16d693ad2694d24d721c0543b4507ca86e06
Joe Haddad <joe.haddad@zeit.co>
2019-02-28 11:59:31 -0500

    Missed a call

--- packages/next/client/on-demand-entries-client.js
+++ packages/next/client/on-demand-entries-client.js
@@ -5,7 +5,10 @@ import { setupPing, currentPage } from './on-demand-entries-utils'
 
 export default async ({ assetPrefix }) => {
   Router.ready(() => {
-    Router.events.on('routeChangeComplete', setupPing)
+    Router.events.on(
+      'routeChangeComplete',
+      setupPing.bind(this, assetPrefix, () => Router.pathname)
+    )
   })
 
   setupPing(assetPrefix, () => Router.pathname, currentPage)
