912e45b50692244b08c526592212c9fd2620b179
JJ Kasper <22380829+ijjk@users.noreply.github.com>
2019-03-01 11:08:27 -0600

    Show error when `router` or `Component` are returned in _app.js (#6487)
    * Show error when `router` or `Component` are returned in _app.js
    getInitialProps
    
    * Update to only show error in dev mode
    
    * Update packages/next-server/server/render.tsx
    
    Co-Authored-By: ijjk <22380829+ijjk@users.noreply.github.com>

--- packages/next-server/server/render.tsx
+++ packages/next-server/server/render.tsx
@@ -212,6 +212,10 @@ export async function renderToHTML(
       return render(renderElementToString, <ErrorDebug error={err} />)
     }
 
+    if (dev && (props.router || props.Component)) {
+      throw new Error(`'router' and 'Component' can not be returned in getInitialProps from _app.js https://err.sh/zeit/next.js/cant-override-next-props.md`)
+    }
+
     const {
       App: EnhancedApp,
       Component: EnhancedComponent,
---
+++ test/integration/no-override-next-props/pages/index.js
@@ -0,0 +1,3 @@
+export default () => (
+  <p>Hello there 👋</p>
+)
