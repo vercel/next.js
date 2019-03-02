89ad01cab848329f3a7752b2499bfe6dd3b9c89f
JJ Kasper <22380829+ijjk@users.noreply.github.com>
2019-03-01 18:11:37 -0600

    Update with-zones example to support Now v2 / serverless (#6485)
    Fixes: #6468

--- examples/with-zones/home/next.config.js
+++ examples/with-zones/home/next.config.js
@@ -1,6 +1,7 @@
-const { NOW_URL } = process.env
-const { alias } = require('./now.json')
+const { DEPLOY } = process.env
+const alias = 'with-zones.nextjs.org'
 
 module.exports = {
-  assetPrefix: NOW_URL ? `https://${alias}` : 'http://localhost:4000'
+  target: 'serverless',
+  assetPrefix: DEPLOY ? `https://${alias}` : 'http://localhost:4000'
 }
