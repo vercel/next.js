24c8ca792ffb304f123ffd7c5c28d0ddd6f305aa
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-03-06 00:53:12 -0300

    Remove React usage

--- packages/next/client/dev-build-watcher.js
+++ packages/next/client/dev-build-watcher.js
@@ -1,158 +1,185 @@
-import React, { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-
 export default function initializeBuildWatcher (webpackHMR) {
   const shadowHost = document.getElementById('__next-build-watcher')
   if (!shadowHost) return
   const shadowRoot = shadowHost.attachShadow({ mode: 'open' })
 
-  const reactRoot = document.createElement('div')
-  reactRoot.setAttribute('id', 'react-root')
+  // Get the current style
+  const style = shadowHost.getAttribute('buildwatcherstyle')
 
-  shadowRoot.appendChild(reactRoot)
-  ReactDOM.render(<BuildWatcher webpackHMR={webpackHMR} />, reactRoot)
-}
+  // Container
+  const container = createContainer()
+  shadowRoot.appendChild(container)
 
-function BuildWatcher ({ webpackHMR }) {
-  const [isBuilding, setIsBuilding] = useState(false)
-  const [isVisible, setIsVisible] = useState(false)
+  // CSS
+  const css = createCss(style)
+  shadowRoot.appendChild(css)
+
+  // State
+  let isVisible = false
+  let isBuilding = false
   let timeoutId = null
 
-  useEffect(() => {
-    webpackHMR.addMessageListenerToEventSourceWrapper((event) => {
-      // This is the heartbeat event
-      if (event.data === '\uD83D\uDC93') {
-        return
-      }
+  // Handle events
+  webpackHMR.addMessageListenerToEventSourceWrapper((event) => {
+    // This is the heartbeat event
+    if (event.data === '\uD83D\uDC93') {
+      return
+    }
 
-      try {
-        handleMessage(event)
-      } catch { }
-    })
-  }, [])
+    try {
+      handleMessage(event)
+    } catch { }
+  })
 
-  const handleMessage = (event) => {
+  function handleMessage (event) {
     const obj = JSON.parse(event.data)
 
     switch (obj.action) {
       case 'building':
         timeoutId && clearTimeout(timeoutId)
-        setIsBuilding(true)
-        setIsVisible(true)
+        isVisible = true
+        isBuilding = true
+        updateContainer()
         break
       case 'built':
-        setIsBuilding(false)
-        // Remove from DOM after the fade out animation is complete
-        timeoutId = setTimeout(() => setIsVisible(false), 100)
+        isBuilding = false
+        // Wait for the fade out transtion to complete
+        timeoutId = setTimeout(() => {
+          isVisible = false
+          updateContainer()
+        }, 100)
+
+        updateContainer()
         break
     }
   }
 
-  const style = document.getElementById('__next-build-watcher').getAttribute('buildwatcherstyle')
+  function updateContainer () {
+    if (isBuilding) {
+      container.classList.add('building')
+    } else {
+      container.classList.remove('building')
+    }
 
-  return (
-    <>
-      {isVisible && (
-        <div id='container' className={isBuilding ? 'building' : null}>
-          <div id='icon-wrapper'>
-            <svg
-              width='114px'
-              height='100px'
-              viewBox='0 0 226 200'
-              version='1.1'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <defs>
-                <linearGradient
-                  x1='114.720775%'
-                  y1='181.283245%'
-                  x2='39.5399306%'
-                  y2='100%'
-                  id='linearGradient-1'
-                >
-                  <stop stopColor='#FFFFFF' offset='0%' />
-                  <stop stopColor='#000000' offset='100%' />
-                </linearGradient>
-              </defs>
-              <g id='icon-group' fill='none' stroke='url(#linearGradient-1)' strokeWidth='18'>
-                <path d='M113,5.08219117 L4.28393801,197.5 L221.716062,197.5 L113,5.08219117 Z' />
-              </g>
-            </svg>
-          </div>
+    if (isVisible) {
+      container.classList.add('visible')
+    } else {
+      container.classList.remove('visible')
+    }
+  }
+}
 
-          <span id='building-text' />
-        </div>
-      )}
+function createContainer () {
+  const container = document.createElement('div')
+  container.setAttribute('id', 'container')
+  container.innerHTML = `
+    <div id="icon-wrapper">
+      <svg
+        width="114px"
+        height="100px"
+        viewBox="0 0 226 200"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <linearGradient
+            x1="114.720775%"
+            y1="181.283245%"
+            x2="39.5399306%"
+            y2="100%"
+            id="linearGradient-1"
+          >
+            <stop stop-color="#FFFFFF" offset="0%" />
+            <stop stop-color="#000000" offset="100%" />
+          </linearGradient>
+        </defs>
+        <g id="icon-group" fill="none" stroke="url(#linearGradient-1)" stroke-width="18">
+          <path d="M113,5.08219117 L4.28393801,197.5 L221.716062,197.5 L113,5.08219117 Z" />
+        </g>
+      </svg>
+    </div>
 
-      <style>{`
-        #container {
-          position: absolute;
-          bottom: 10px;
-          right: 30px;
+    <span id="building-text"></span>
+  `
 
-          padding: 8px 10px;
-          background: white;
-          display: flex;
-          align-items: center;
-          box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.38), 0 2px 10px 0 rgba(0, 0, 0, 0.48);
+  return container
+}
 
-          opacity: 0;
-          transition: opacity 0.1s ease, bottom 0.1s ease;
-          animation: fade-in 0.1s ease-in-out;
-        }
+function createCss (style) {
+  const css = document.createElement('style')
+  css.innerHTML = `
+    #container {
+      position: absolute;
+      bottom: 10px;
+      right: 30px;
 
-        #container.building {
-          bottom: 20px;
-          opacity: 1;
-        }
+      padding: 8px 10px;
+      background: white;
+      align-items: center;
+      box-shadow: 0 11px 40px 0 rgba(0, 0, 0, 0.38), 0 2px 10px 0 rgba(0, 0, 0, 0.48);
 
-        #icon-wrapper {
-          width: 16px;
-          height: 16px;
-        }
+      display: none;
+      opacity: 0;
+      transition: opacity 0.1s ease, bottom 0.1s ease;
+      animation: fade-in 0.1s ease-in-out;
+    }
 
-        #icon-wrapper > svg {
-          width: 100%;
-          height: 100%;
-        }
+    #container.visible {
+      display: flex;
+    }
 
-        #building-text {
-          font-family: monospace;
-          margin-top: 2px;
-          margin-left: 8px;
-          display: ${style === 'minimalist' ? 'none' : 'unset'};
-        }
+    #container.building {
+      bottom: 20px;
+      opacity: 1;
+    }
 
-        #building-text::after {
-          content: 'Building...';
-        }
+    #icon-wrapper {
+      width: 16px;
+      height: 16px;
+    }
 
-        #icon-group {
-          stroke-dasharray: 0 226;
-          animation: strokedash 1s ease-in-out both infinite;
-        }
+    #icon-wrapper > svg {
+      width: 100%;
+      height: 100%;
+    }
 
-        @keyframes fade-in {
-          from {
-            bottom: 10px;
-            opacity: 0;
-          }
-          to {
-            bottom: 20px;
-            opacity: 1;
-          }
-        }
+    #building-text {
+      font-family: monospace;
+      margin-top: 2px;
+      margin-left: 8px;
+      display: ${style === 'minimalist' ? 'none' : 'unset'};
+    }
 
-        @keyframes strokedash {
-          0% {
-            stroke-dasharray: 0 226;
-          }
-          80%,
-          100% {
-            stroke-dasharray: 659 226;
-          }
-        }
-      `}</style>
-    </>
-  )
+    #building-text::after {
+      content: 'Building...';
+    }
+
+    #icon-group {
+      stroke-dasharray: 0 226;
+      animation: strokedash 1s ease-in-out both infinite;
+    }
+
+    @keyframes fade-in {
+      from {
+        bottom: 10px;
+        opacity: 0;
+      }
+      to {
+        bottom: 20px;
+        opacity: 1;
+      }
+    }
+
+    @keyframes strokedash {
+      0% {
+        stroke-dasharray: 0 226;
+      }
+      80%,
+      100% {
+        stroke-dasharray: 659 226;
+      }
+    }
+  `
+
+  return css
 }
