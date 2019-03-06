3eab273519ca5c16c7c3ad338ff940d627c7ac38
JJ Kasper <jj@jjsweb.site>
2019-03-06 14:40:06 -0600

    Update to use the correct router instance in withRouter so error is thrown when router method is used during SSR

--- packages/next/client/with-router.js
+++ packages/next/client/with-router.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import router from './router'
 import hoistStatics from 'hoist-non-react-statics'
 import { getDisplayName } from 'next-server/dist/lib/utils'
 
@@ -7,15 +7,11 @@ export default function withRouter (ComposedComponent) {
   const displayName = getDisplayName(ComposedComponent)
 
   class WithRouteWrapper extends Component {
-    static contextTypes = {
-      router: PropTypes.object
-    }
-
     static displayName = `withRouter(${displayName})`
 
     render () {
       return <ComposedComponent
-        router={this.context.router}
+        router={router}
         {...this.props}
       />
     }
