6b987ac6c7248f710060b0a73c8457bcffee6171
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2019-03-07 12:28:33 +0530

    prettified

--- examples/with-i18next-typescript/next.config.js
+++ examples/with-i18next-typescript/next.config.js
@@ -1,13 +1,15 @@
-const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
+const withTypescript = require('@zeit/next-typescript')
+const withSass = require('@zeit/next-sass')
 
-module.exports = withTypescript(withSass({
-    webpack(config) {
-        // Fixes npm packages that depend on `fs` module
-        config.node = {
-            fs: 'empty'
-        }
+module.exports = withTypescript(
+  withSass({
+    webpack (config) {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty'
+      }
 
-        return config
+      return config
     }
-}));
+  })
+)
