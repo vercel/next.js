681c4dc8b09fe5501e8d20b9324bb23edeabad1a
Tim Neutkens <timneutkens@me.com>
2019-03-08 15:40:59 +0100

    Use CRA terser options

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -69,9 +69,38 @@ export default function getBaseWebpackConfig (dir: string, {dev = false, isServe
     sourceMap: false,
     cache: true,
     terserOptions: {
-      compress: false,
-      // mangle: false,
-      safari10: true
+      parse: {
+        // we want terser to parse ecma 8 code. However, we don't want it
+        // to apply any minfication steps that turns valid ecma 5 code
+        // into invalid ecma 5 code. This is why the 'compress' and 'output'
+        // sections only apply transformations that are ecma 5 safe
+        // https://github.com/facebook/create-react-app/pull/4234
+        ecma: 8,
+      },
+      compress: {
+        ecma: 5,
+        warnings: false,
+        // Disabled because of an issue with Uglify breaking seemingly valid code:
+        // https://github.com/facebook/create-react-app/issues/2376
+        // Pending further investigation:
+        // https://github.com/mishoo/UglifyJS2/issues/2011
+        comparisons: false,
+        // Disabled because of an issue with Terser breaking valid code:
+        // https://github.com/facebook/create-react-app/issues/5250
+        // Pending futher investigation:
+        // https://github.com/terser-js/terser/issues/120
+        inline: 2,
+      },
+      mangle: {
+        safari10: true,
+      },
+      output: {
+        ecma: 5,
+        comments: false,
+        // Turned on because emoji and regex is not minified properly using default
+        // https://github.com/facebook/create-react-app/issues/2488
+        ascii_only: true,
+      },
     }
   }
 
