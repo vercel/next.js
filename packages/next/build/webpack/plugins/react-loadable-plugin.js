f3e6c410e56f8a830cb4dce4de080444d110a813
Joe Haddad <joe.haddad@zeit.co>
2019-03-12 03:12:42 -0400

    Alternative way to compute chunks for files

--- packages/next/build/webpack/plugins/react-loadable-plugin.js
+++ packages/next/build/webpack/plugins/react-loadable-plugin.js
@@ -27,29 +27,52 @@ function buildManifest (compiler, compilation) {
   let context = compiler.options.context
   let manifest = {}
 
-  compilation.chunks.forEach(chunk => {
-    chunk.files.forEach(file => {
-      for (const module of chunk.modulesIterable) {
-        let id = module.id
-        let name = typeof module.libIdent === 'function' ? module.libIdent({ context }) : null
-        // If it doesn't end in `.js` Next.js can't handle it right now.
-        if (!file.match(/\.js$/) || !file.match(/^static\/chunks\//)) {
-          return
-        }
-        let publicPath = url.resolve(compilation.outputOptions.publicPath || '', file)
+  compilation.chunkGroups.forEach(chunkGroup => {
+    if (chunkGroup.isInitial()) {
+      return
+    }
 
-        let currentModule = module
-        if (module.constructor.name === 'ConcatenatedModule') {
-          currentModule = module.rootModule
-        }
-        if (!manifest[currentModule.rawRequest]) {
-          manifest[currentModule.rawRequest] = []
-        }
+    chunkGroup.origins.forEach(chunkGroupOrigin => {
+      const { request } = chunkGroupOrigin
 
-        manifest[currentModule.rawRequest].push({ id, name, file, publicPath })
-      }
+      const manifestOption = []
+      chunkGroup.chunks.forEach(chunk => {
+        chunk.files.forEach(file => {
+          for (const chunkModule of chunk.modulesIterable) {
+            let id = chunkModule.id
+            let name =
+              typeof chunkModule.libIdent === 'function'
+                ? chunkModule.libIdent({ context })
+                : null
+
+            // If it doesn't end in `.js` Next.js can't handle it right now.
+            if (!file.match(/\.js$/) || !file.match(/^static\/chunks\//)) {
+              return
+            }
+            let publicPath = url.resolve(
+              compilation.outputOptions.publicPath || '',
+              file
+            )
+
+            manifestOption.push({ id, name, file, publicPath })
+          }
+        })
+
+        if (
+          manifest[request] == null ||
+          manifest[request].length > manifestOption.length
+        ) {
+          // TODO: maybe compute this via the smallest file size instead?
+          manifest[request] = manifestOption
+        }
+      })
     })
   })
+
+  manifest = Object.keys(manifest)
+    .sort()
+    // eslint-disable-next-line
+    .reduce((a, c) => ((a[c] = manifest[c]), a), {})
 
   return manifest
 }
@@ -60,18 +83,21 @@ export class ReactLoadablePlugin {
   }
 
   apply (compiler) {
-    compiler.hooks.emit.tapAsync('ReactLoadableManifest', (compilation, callback) => {
-      const manifest = buildManifest(compiler, compilation)
-      var json = JSON.stringify(manifest, null, 2)
-      compilation.assets[this.filename] = {
-        source () {
-          return json
-        },
-        size () {
-          return json.length
+    compiler.hooks.emit.tapAsync(
+      'ReactLoadableManifest',
+      (compilation, callback) => {
+        const manifest = buildManifest(compiler, compilation)
+        var json = JSON.stringify(manifest, null, 2)
+        compilation.assets[this.filename] = {
+          source () {
+            return json
+          },
+          size () {
+            return json.length
+          }
         }
+        callback()
       }
-      callback()
-    })
+    )
   }
 }
