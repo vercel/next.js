2623a9965c71a8b788dbcdda0276373b6b6898dc
shashkovdanil <shashkovdanil@gmail.com>
2019-03-16 22:40:37 +0500

    Remove dirty hack

--- examples/with-linaria/next.config.js
+++ examples/with-linaria/next.config.js
@@ -2,17 +2,17 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
   webpack (config, options) {
-    const jsRule = config.module.rules.findIndex(loader => loader.test.test('test.jsx'))
-    // Dirty dirty dirty hack to make the zero runtime stuff work
-    config.module.rules[jsRule].use = [
-      config.module.rules[jsRule].use,
-      {
-        loader: 'linaria/loader',
-        options: {
-          sourceMap: process.env.NODE_ENV !== 'production'
+    config.module.rules.push({
+      test: /\.js$/,
+      use: [
+        {
+          loader: 'linaria/loader',
+          options: {
+            sourceMap: process.env.NODE_ENV !== 'production',
+          },
         }
-      }
-    ]
+      ],
+    })
 
     return config
   }
