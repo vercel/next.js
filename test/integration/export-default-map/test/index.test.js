e1cf32ca60c4e118d38b8d2697d6f9083eb4b662
JJ Kasper <22380829+ijjk@users.noreply.github.com>
2019-03-20 10:42:02 -0500

    Update export tests and fixed import (#6738)
    * Fix import in export worker
    
    * Update export tests
    
    * Turn test parallelism up
    
    * Fix config in export-default-map
    
    * Switch parallelism back to 3

---
+++ test/integration/export-default-map/test/index.test.js
@@ -0,0 +1,37 @@
+/* eslint-env jest */
+/* global jasmine */
+import fs from 'fs'
+import { join } from 'path'
+import { promisify } from 'util'
+import {
+  nextBuild,
+  nextExport
+} from 'next-test-utils'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+const access = promisify(fs.access)
+const appDir = join(__dirname, '../')
+const outdir = join(appDir, 'out')
+
+describe('Export with default map', () => {
+  beforeAll(async () => {
+    await nextBuild(appDir)
+    await nextExport(appDir, { outdir })
+  })
+
+  it('should export with folder that has dot in name', async () => {
+    expect.assertions(1)
+    await expect(access(join(outdir, 'v1.12/index.html'))).resolves.toBe(undefined)
+  })
+
+  it('should export an amp only page to clean path', async () => {
+    expect.assertions(1)
+    await expect(access(join(outdir, 'docs/index.html'))).resolves.toBe(undefined)
+  })
+
+  it('should export hybrid amp page correctly', async () => {
+    expect.assertions(2)
+    await expect(access(join(outdir, 'some/index.html'))).resolves.toBe(undefined)
+    await expect(access(join(outdir, 'some.amp/index.html'))).resolves.toBe(undefined)
+  })
+})
