b674b3bca27cfeca487218c584372ebc50b1e612
James Hardwick <jamesdh@gmail.com>
2019-03-21 14:40:18 -0500

    Make page/components match the default create-next-app setup
    But obviously using styled-components in favor of styled-jsx

---
+++ examples/with-styled-components/components/nav.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Link from 'next/link'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const StyledNav = styled.nav`
+  text-align: center;
+  ul {
+    display: flex;
+    justify-content: space-between;
+    padding: 4px 16px;
+    li {
+      display: flex;
+      padding: 6px 8px;
+    }
+  }
+  a {
+    color: #067df7;
+    text-decoration: none;
+    font-size: 13px;
+  }
+` 
+
+const links = [
+  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
+].map(link => {
+  link.key = `nav-link-${link.href}-${link.label}`
+  return link
+})
+
+const Nav = () => (
+  <StyledNav>
+    <ul>
+      <li>
+        <Link prefetch href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <ul>
+        {links.map(({ key, href, label }) => (
+          <li key={key}>
+            <Link href={href}>
+              <a>{label}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </ul>
+  </StyledNav>
+)
+
+export default Nav
--- examples/with-styled-components/pages/index.js
+++ examples/with-styled-components/pages/index.js
@@ -1,9 +1,93 @@
 import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
 import styled from 'styled-components'
 
+const Hero = styled.div`
+  width: 100%;
+  color: #333;
+`
 const Title = styled.h1`
-  color: red;
-  font-size: 50px;
+  margin: 0;
+  width: 100%;
+  padding-top: 80px;
+  line-height: 1.15;
+  font-size: 48px;
+  text-align: center;
+`
+const Description = styled.p`
+  text-align: center;
+`
+const Row = styled.div`
+  max-width: 880px;
+  margin: 80px auto 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+const Card = styled.a`
+  padding: 18px 18px 24px;
+  width: 220px;
+  text-align: left;
+  text-decoration: none;
+  color: #434343;
+  border: 1px solid #9b9b9b;
+
+  &:hover {
+    border-color: #067df7;
+  }
+
+  h3 {
+    margin: 0;
+    color: #067df7;
+    font-size: 18px;
+  }
+
+  p {
+    margin: 0;
+    padding: 12px 0 0;
+    font-size: 13px;
+    color: #333;
+  }
 `
 
-export default () => <Title>My page</Title>
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+
+    <Hero>
+      <Title>Welcome to Next!</Title>
+      <Description>
+        To get started, edit <code>pages/index.js</code> and save to reload.
+      </Description>
+
+      <Row>
+        <Link href="https://github.com/zeit/next.js#getting-started">
+          <Card>
+            <h3>Getting Started &rarr;</h3>
+            <p>Learn more about Next on Github and in their examples</p>
+          </Card>
+        </Link>
+        <Link href="https://open.segment.com/create-next-app">
+          <Card>
+            <h3>Examples &rarr;</h3>
+            <p>
+              Find other example boilerplates on the{' '}
+              <code>create-next-app</code> site
+            </p>
+          </Card>
+        </Link>
+        <Link href="https://github.com/segmentio/create-next-app">
+          <Card>
+            <h3>Create Next App &rarr;</h3>
+            <p>Was this tool helpful? Let us know how we can improve it</p>
+          </Card>
+        </Link>
+      </Row>
+    </Hero>
+  </div>
+)
+
+export default Home
