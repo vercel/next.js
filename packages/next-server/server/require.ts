397674c40e9f9af0c115708d2464dd4e7253075c
JJ Kasper <22380829+ijjk@users.noreply.github.com>
2019-03-21 20:40:42 -0500

    Fix export not checking amp right for /index.amp (#6748)
    * Fix export not checking amp right for /index.amp
    
    * Add test checking hybrid amp index behavior

--- packages/next-server/server/require.ts
+++ packages/next-server/server/require.ts
@@ -14,6 +14,7 @@ export function pageNotFoundError(page: string): Error {
 }
 
 export const tryAmp = (manifest: any, page: string) => {
+  page = page === '/' ? '/index' : page
   const hasAmp = manifest[page + '.amp']
   if (hasAmp) {
     page += '.amp'
