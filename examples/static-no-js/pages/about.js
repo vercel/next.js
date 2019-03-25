879da368886eb4e29eb0b85c9f95f6d310fc1839
Giuseppe <giuseppeg@users.noreply.github.com>
2019-03-25 09:04:47 +0100

    Add static-no-js site example (#6770)

---
+++ examples/static-no-js/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+
+const About = () => (
+  <div>
+    <Head>
+      <title>About page</title>
+    </Head>
+
+    <h1>Static, no-js Next site.</h1>
+
+    <p>How about that!</p>
+    <p>
+      This example aims to show that Next.js can be used to create static sites
+      that don't use any JavaScript.
+    </p>
+  </div>
+)
+
+export default About
