5be2d7917ec86fb1d3e8086e5fff11ae932b1beb
JJ Kasper <jj@jjsweb.site>
2019-03-28 13:30:27 -0500

    Don't throw error on teardown

--- test/jest-global-teardown.js
+++ test/jest-global-teardown.js
@@ -15,11 +15,13 @@ module.exports = async () => {
 
   if (browser) {
     // Close all remaining browser windows
-    const windows = await browser.windowHandles()
-    for (const window of windows) {
-      if (!window) continue
-      await browser.window(window)
-      await browser.origClose()
-    }
+    try {
+      const windows = await browser.windowHandles()
+      for (const window of windows) {
+        if (!window) continue
+        await browser.window(window)
+        await browser.origClose()
+      }
+    } catch (_) {}
   }
 }
