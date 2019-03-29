45a16d17aad52d5da1a8dd64492022056c22a26f
JJ Kasper <jj@jjsweb.site>
2019-03-28 23:20:10 -0500

    Add dropping of custom scripts in AMP mode

--- packages/next/pages/_document.js
+++ packages/next/pages/_document.js
@@ -154,6 +154,7 @@ export class Head extends Component {
       hasAmp,
       ampPath,
       assetPrefix,
+      dev,
       __NEXT_DATA__,
     } = this.context._documentProps
     const { _devOnlyInvalidateCacheQueryString } = this.context
@@ -186,6 +187,14 @@ export class Head extends Component {
         badProp = 'name="viewport"'
       } else if (type === 'link' && props.rel === 'canonical') {
         badProp = 'rel="canonical"'
+      } else if (!dev && type === 'script') {
+        // In production drop custom scripts since they're not allowed in AMP
+        // in dev we leave them so the validation warning is shown
+        badProp = '<script'
+        Object.keys(props).forEach(prop => {
+          badProp += ` ${prop}="${props[prop]}"`
+        })
+        badProp += '/>'
       }
 
       if (badProp) {
