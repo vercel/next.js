a3d7fab3f181d310673af31b7ac14522856e219f
Joe Haddad <timer150@gmail.com>
2019-04-01 13:30:38 -0500

    Update the `ad` comment
    Co-Authored-By: lfades <luis@zeit.co>

--- packages/next/build/generate-build-id.ts
+++ packages/next/build/generate-build-id.ts
@@ -2,7 +2,8 @@ export async function generateBuildId (generate: () => string|null, fallback: ()
   let buildId = await generate()
   // If there's no buildId defined we'll fall back
   if (buildId === null) {
-    // We also create a new buildId if it starts with ad_ to avoid ad blockers
+    // We also create a new buildId if it starts with `ad` to avoid false 
+    // positives with ad blockers
     while (!buildId || buildId.startsWith('ad')) {
       buildId = fallback()
     }
