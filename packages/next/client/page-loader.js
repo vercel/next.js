34c9f4e18d75d6c4480d1d991faa740cfb71cdcb
Connor Davis <mail@connordav.is>
2019-04-04 15:04:06 -0500

    Check in router instead

--- packages/next/client/page-loader.js
+++ packages/next/client/page-loader.js
@@ -129,7 +129,7 @@ export default class PageLoader {
   async prefetch (route) {
     route = this.normalizeRoute(route)
     const scriptRoute = route === '/' ? '/index.js' : `${route}.js`
-    if (this.prefetchCache.has(scriptRoute) || process.env.__NEXT_EXPERIMENTAL_DEBUG) {
+    if (this.prefetchCache.has(scriptRoute)) {
       return
     }
     this.prefetchCache.add(scriptRoute)
