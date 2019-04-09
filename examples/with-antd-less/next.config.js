3aba501d0ef86701741c86a0a61dbec4e463973d
Leo <lisd199001@gmail.com>
2019-04-09 18:14:46 +0800

    add new antd-less intergration method

---
+++ examples/with-antd-less/next.config.js
@@ -0,0 +1,19 @@
+const antdLessLoader = require("next-antd-aza-less")
+// const modifyVars = require("./your/custom/vars")
+
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = (file) => {}
+}
+
+/* Without CSS Modules, with PostCSS */
+module.exports = antdLessLoader({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]",
+    },
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+    //   modifyVars: modifyVars
+    }
+});
---
+++ examples/with-antd-less/pages/index.js
@@ -0,0 +1,4 @@
+import styles from "./index.less";
+import { Button} from "antd";
+
+export default ()=> <p className={styles.index}>index Page<Button>按钮</Button></p>
