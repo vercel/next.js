0325eda31894a2444bd05a1121865768b7cbed0f
Joe Haddad <joe.haddad@zeit.co>
2019-04-10 11:45:24 -0400

    Complicate dynamic testing

--- test/integration/flying-shuttle/components/something.js
+++ test/integration/flying-shuttle/components/something.js
@@ -1,1 +1,10 @@
-export default () => <div>lol</div>
+import dynamic from 'next/dynamic'
+
+const DynamicComponent = dynamic(() => import('./something2'))
+
+export default () => (
+  <>
+    <div>lol</div>
+    <DynamicComponent />
+  </>
+)
