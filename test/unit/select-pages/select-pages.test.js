b5430714405cae028a0c36b2dd09636fbc2243eb
Joe Haddad <timer150@gmail.com>
2019-04-09 22:57:46 -0400

    Remove CLI and negative page building (#6986)
    * Remove CLI and negative page building
    
    * Remove select all tests
    
    * Update test of getSpecifiedPages
    
    * Prune old tests from snapshot
    
    * Remove old flying shuttle tests

--- test/unit/select-pages/select-pages.test.js
+++ test/unit/select-pages/select-pages.test.js
@@ -6,61 +6,26 @@ const getSpecifiedPages = async (...args) =>
 
 describe('getSpecifiedPages', () => {
   it('should only choose selected', async () => {
-    expect(await getSpecifiedPages(__dirname, ['a'], ['js'])).toMatchSnapshot()
+    expect(await getSpecifiedPages(__dirname, 'a', ['js'])).toMatchSnapshot()
 
     let err
     try {
-      await getSpecifiedPages(__dirname, ['a', 'b'], ['js'])
+      await getSpecifiedPages(__dirname, 'a,b', ['js'])
     } catch (e) {
       err = e
     }
     expect(err).toBeTruthy()
 
     expect(
-      await getSpecifiedPages(__dirname, ['a', 'b'], ['js', 'jsx'])
+      await getSpecifiedPages(__dirname, 'a,b', ['js', 'jsx'])
     ).toMatchSnapshot()
 
     expect(
-      await getSpecifiedPages(__dirname, ['a', 'b'], ['js', 'jsx', 'mdx'])
+      await getSpecifiedPages(__dirname, 'a,b', ['js', 'jsx', 'mdx'])
     ).toMatchSnapshot()
 
     expect(
-      await getSpecifiedPages(__dirname, ['a', 'b', 'c'], ['js', 'jsx', 'mdx'])
-    ).toMatchSnapshot()
-
-    expect(
-      await getSpecifiedPages(__dirname, ['a', 'b', 'c'], ['js', 'jsx', 'mdx'])
-    ).toEqual(
-      await getSpecifiedPages(__dirname, ['a,b,c'], ['js', 'jsx', 'mdx'])
-    )
-    expect(
-      await getSpecifiedPages(__dirname, ['a', 'b', 'c'], ['js', 'jsx', 'mdx'])
-    ).toEqual(
-      await getSpecifiedPages(__dirname, ['a,c', 'b'], ['js', 'jsx', 'mdx'])
-    )
-    expect(
-      await getSpecifiedPages(__dirname, ['a', 'b', 'c'], ['js', 'jsx', 'mdx'])
-    ).toEqual(
-      await getSpecifiedPages(__dirname, ['a', 'c,b'], ['js', 'jsx', 'mdx'])
-    )
-  })
-
-  it('should select all', async () => {
-    expect(await getSpecifiedPages(__dirname, ['**'], ['js'])).toMatchSnapshot()
-    expect(
-      await getSpecifiedPages(__dirname, ['**'], ['js', 'mdx', 'jsx'])
-    ).toMatchSnapshot()
-  })
-
-  it('should filter pages', async () => {
-    expect(
-      await getSpecifiedPages(__dirname, ['**', '-a'], ['js'])
-    ).toMatchSnapshot()
-    expect(
-      await getSpecifiedPages(__dirname, ['**', '-a'], ['js', 'mdx', 'jsx'])
-    ).toMatchSnapshot()
-    expect(
-      await getSpecifiedPages(__dirname, ['**,-a', '-c'], ['js', 'mdx', 'jsx'])
+      await getSpecifiedPages(__dirname, 'a,b,c', ['js', 'jsx', 'mdx'])
     ).toMatchSnapshot()
   })
 })
