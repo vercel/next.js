7b2c701658cafa897677b5288c82d795f5d4bc6a
Leo <lisd199001@gmail.com>
2019-04-12 09:53:28 +0800

    check extensions

--- examples/with-antd-less/pages/index.js
+++ examples/with-antd-less/pages/index.js
@@ -1,72 +1,72 @@
-import styles from "./index.less";
-
-import { Form, Icon, Input, Button, Checkbox } from "antd";
+import React from 'react'
+import styles from './index.less'
+import { Form, Icon, Input, Button, Checkbox } from 'antd'
 
 class NormalLoginForm extends React.Component {
   handleSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log("Received values of form: ", values);
+        console.log('Received values of form: ', values)
       }
-    });
-  };
+    })
+  }
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
+  render () {
+    const { getFieldDecorator } = this.props.form
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
+      <Form onSubmit={this.handleSubmit} className='login-form'>
         <Form.Item>
-          {getFieldDecorator("userName", {
-            rules: [{ required: true, message: "Please input your username!" }]
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }]
           })(
             <Input
-              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="Username"
+              prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder='Username'
             />
           )}
         </Form.Item>
         <Form.Item>
-          {getFieldDecorator("password", {
-            rules: [{ required: true, message: "Please input your Password!" }]
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }]
           })(
             <Input
-              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-              type="password"
-              placeholder="Password"
+              prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type='password'
+              placeholder='Password'
             />
           )}
         </Form.Item>
         <Form.Item>
-          {getFieldDecorator("remember", {
-            valuePropName: "checked",
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
             initialValue: true
           })(<Checkbox>Remember me</Checkbox>)}
-          <a className="login-form-forgot" href="">
+          <a className='login-form-forgot' href=''>
             Forgot password
           </a>
           <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+            type='primary'
+            htmlType='submit'
+            className='login-form-button'
           >
             Log in
           </Button>
-          Or <a href="">register now!</a>
+          Or <a href=''>register now!</a>
         </Form.Item>
       </Form>
-    );
+    )
   }
 }
 
-const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(
   NormalLoginForm
-);
+)
 export default () => (
   <div>
     <p className={styles.index}>index Page with custom less styles</p>
-    <div style={{ width: 300, margin: "0 auto" }}>
+    <div style={{ width: 300, margin: '0 auto' }}>
       <WrappedNormalLoginForm />
     </div>
   </div>
-);
+)
