825e2e22e4fca5a52931530628df3d6420883e8c
Luis Alvarez <luisito453@gmail.com>
2019-04-12 13:26:35 -0500

    Added types to next/router

--- packages/next/client/router.ts
+++ packages/next/client/router.ts
@@ -1,19 +1,37 @@
 /* global window */
 import React from 'react'
-import _Router from 'next-server/dist/lib/router/router'
-import { RouterContext } from 'next-server/dist/lib/router-context'
-import { RequestContext } from 'next-server/dist/lib/request-context'
+import _Router, { IRouterInterface } from 'next-server/lib/router/router'
+import { RouterContext } from 'next-server/lib/router-context'
+import { RequestContext } from 'next-server/lib/request-context'
 
-const SingletonRouter = {
+type ClassArguments<T> = T extends new(...args: infer U) => any ? U : any
+
+type RouterArgs = ClassArguments<typeof _Router>
+
+interface ISingletonRouterBase {
+  router: _Router | null
+  readyCallbacks: Array<() => any>
+  ready(cb: () => any): void
+}
+
+export interface IPublicRouterInstance extends IRouterInterface, Pick<_Router, | 'components' | 'push' | 'replace' | 'reload' | 'back' | 'prefetch' | 'beforePopState'> {
+  events: typeof _Router['events']
+}
+
+export interface ISingletonRouter extends ISingletonRouterBase, IPublicRouterInstance {}
+
+const SingletonRouter: ISingletonRouterBase = {
   router: null, // holds the actual router instance
   readyCallbacks: [],
-  ready (cb) {
+  ready(cb: () => void) {
     if (this.router) return cb()
     if (typeof window !== 'undefined') {
       this.readyCallbacks.push(cb)
     }
-  }
+  },
 }
+
+// const x = SingletonRouter as IRealRouter
 
 // Create public properties and methods of the router in the SingletonRouter
 const urlPropertyFields = ['pathname', 'route', 'query', 'asPath']
@@ -23,9 +41,9 @@ const coreMethodFields = ['push', 'replace', 'reload', 'back', 'prefetch', 'befo
 
 // Events is a static property on the router, the router doesn't have to be initialized to use it
 Object.defineProperty(SingletonRouter, 'events', {
-  get () {
+  get() {
     return _Router.events
-  }
+  },
 })
 
 propertyFields.concat(urlPropertyFields).forEach((field) => {
@@ -34,17 +52,18 @@ propertyFields.concat(urlPropertyFields).forEach((field) => {
   // The value might get changed as we change routes and this is the
   // proper way to access it
   Object.defineProperty(SingletonRouter, field, {
-    get () {
-      throwIfNoRouter()
-      return SingletonRouter.router[field]
-    }
+    get() {
+      const router = getRouter() as any
+      return router[field] as string
+    },
   })
 })
 
 coreMethodFields.forEach((field) => {
-  SingletonRouter[field] = (...args) => {
-    throwIfNoRouter()
-    return SingletonRouter.router[field](...args)
+  // We don't really know the types here, so we add them later instead
+  (SingletonRouter as any)[field] = (...args: any[]) => {
+    const router = getRouter() as any
+    return router[field](...args)
   }
 })
 
@@ -52,11 +71,14 @@ routerEvents.forEach((event) => {
   SingletonRouter.ready(() => {
     _Router.events.on(event, (...args) => {
       const eventField = `on${event.charAt(0).toUpperCase()}${event.substring(1)}`
-      if (SingletonRouter[eventField]) {
+      const singletonRouter = SingletonRouter as any
+      if (singletonRouter[eventField]) {
         try {
-          SingletonRouter[eventField](...args)
+          singletonRouter[eventField](...args)
         } catch (err) {
+          // tslint:disable-next-line:no-console
           console.error(`Error when running the Router event: ${eventField}`)
+          // tslint:disable-next-line:no-console
           console.error(`${err.message}\n${err.stack}`)
         }
       }
@@ -64,25 +86,26 @@ routerEvents.forEach((event) => {
   })
 })
 
-function throwIfNoRouter () {
+function getRouter() {
   if (!SingletonRouter.router) {
     const message = 'No router instance found.\n' +
       'You should only use "next/router" inside the client side of your app.\n'
     throw new Error(message)
   }
+  return SingletonRouter.router
 }
 
 // Export the SingletonRouter and this is the public API.
-export default SingletonRouter
+export default SingletonRouter as ISingletonRouter
 
 // Reexport the withRoute HOC
 export { default as withRouter } from './with-router'
 
-export function useRouter () {
+export function useRouter() {
   return React.useContext(RouterContext)
 }
 
-export function useRequest () {
+export function useRequest() {
   return React.useContext(RequestContext)
 }
 
@@ -93,9 +116,9 @@ export function useRequest () {
 // Create a router and assign it as the singleton instance.
 // This is used in client side when we are initilizing the app.
 // This should **not** use inside the server.
-export const createRouter = function (...args) {
+export const createRouter = (...args: RouterArgs) => {
   SingletonRouter.router = new _Router(...args)
-  SingletonRouter.readyCallbacks.forEach(cb => cb())
+  SingletonRouter.readyCallbacks.forEach((cb) => cb())
   SingletonRouter.readyCallbacks = []
 
   return SingletonRouter.router
@@ -105,16 +128,17 @@ export const createRouter = function (...args) {
 export const Router = _Router
 
 // This function is used to create the `withRouter` router instance
-export function makePublicRouterInstance (router) {
-  const instance = {}
+export function makePublicRouterInstance(router: _Router): IPublicRouterInstance {
+  const _router = router as any
+  const instance = {} as any
 
   for (const property of urlPropertyFields) {
-    if (typeof router[property] === 'object') {
-      instance[property] = { ...router[property] } // makes sure query is not stateful
+    if (typeof _router[property] === 'object') {
+      instance[property] = { ..._router[property] } // makes sure query is not stateful
       continue
     }
 
-    instance[property] = router[property]
+    instance[property] = _router[property]
   }
 
   // Events is a static property on the router, the router doesn't have to be initialized to use it
@@ -126,15 +150,15 @@ export function makePublicRouterInstance (router) {
     // The value might get changed as we change routes and this is the
     // proper way to access it
     Object.defineProperty(instance, field, {
-      get () {
-        return router[field]
-      }
+      get() {
+        return _router[field]
+      },
     })
   })
 
   coreMethodFields.forEach((field) => {
-    instance[field] = (...args) => {
-      return router[field](...args)
+    instance[field] = (...args: any[]) => {
+      return _router[field](...args)
     }
   })
 
