916a6768192f446ef592be55de4fb02862f91329
Luis Alvarez <luisito453@gmail.com>
2019-04-23 14:22:08 -0500

    moved server/next.js to js again

--- packages/next/server/next.js
+++ packages/next/server/next.js
@@ -1,5 +1,5 @@
 // This file is used for when users run `require('next')`
-export default (options: any) => {
+module.exports = options => {
   if (options.dev) {
     const Server = require('./next-dev-server').default
     return new Server(options)
