aa01901adfa024a18af63298c96fb38c3bb80790
JJ Kasper <jj@jjsweb.site>
2019-04-30 10:24:28 -0500

    Make sure not to require react before NODE_ENV has been set

--- test/lib/next-test-utils.js
+++ test/lib/next-test-utils.js
@@ -75,7 +75,7 @@ export function runNextCommand (argv, options = {}) {
   const nextBin = path.join(nextDir, 'dist/bin/next')
   const cwd = options.cwd || nextDir
   // Let Next.js decide the environment
-  const env = { ...process.env, ...options.env, NODE_ENV: undefined }
+  const env = { ...process.env, ...options.env, NODE_ENV: '' }
 
   return new Promise((resolve, reject) => {
     console.log(`Running command "next ${argv.join(' ')}"`)
