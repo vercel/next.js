c0b59e174907ad13162ca993db155f5d36e8614f
JJ Kasper <jj@jjsweb.site>
2019-05-01 14:05:04 -0500

    Add test for title updating when navigating with next/link

--- test/integration/client-navigation/pages/nav/head-2.js
+++ test/integration/client-navigation/pages/nav/head-2.js
@@ -8,6 +8,7 @@ export default (props) => <div id='head-2'>
       name='description'
       content='Head Two'
     />
+    <title>this is head-2</title>
   </Head>
   <Link href='/nav/head-1'>
     <a id='to-head-1'>to head 1</a>
