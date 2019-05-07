4a4d394bf67f3a19d6128d5c4c7b91653e121a4c
JJ Kasper <jj@jjsweb.site>
2019-05-07 07:29:32 -0500

    Add starting slash to test for clarity

--- test/integration/dynamic-routing/pages/index.js
+++ test/integration/dynamic-routing/pages/index.js
@@ -3,15 +3,15 @@ import Link from 'next/link'
 export default () => (
   <div>
     <h3>My blog</h3>
-    <Link href='$post' as='/post-1'>
+    <Link href='/$post' as='/post-1'>
       <a id='view-post-1'>View post 1</a>
     </Link>
     <br />
-    <Link href='$post/comments' as='/post-1/comments'>
+    <Link href='/$post/comments' as='/post-1/comments'>
       <a id='view-post-1-comments'>View post 1 comments</a>
     </Link>
     <br />
-    <Link href='$post/$comment' as='/post-1/comment-1'>
+    <Link href='/$post/$comment' as='/post-1/comment-1'>
       <a id='view-post-1-comment-1'>View comment 1 on post 1</a>
     </Link>
   </div>
