34f5bbfcd377079099057913c4fc80fef14e02fb
Resi Respati <resir014@gmail.com>
2019-05-16 01:08:45 +0700

    undo FC transition in detail page

--- examples/with-typescript/pages/detail.tsx
+++ examples/with-typescript/pages/detail.tsx
@@ -1,38 +1,46 @@
 import * as React from 'react'
-import { NextPageContext, NextPage } from 'next'
+import { NextPageContext } from 'next'
 import Layout from '../components/Layout'
 import { User } from '../interfaces'
 import { findData } from '../utils/sample-api'
-import ListDetail from '../components/ListDetail';
+import ListDetail from '../components/ListDetail'
 
 type Props = {
   item?: User,
   errors?: string,
 }
 
-const InitialPropsDetail: NextPage<Props> = ({ item, errors }) => {
-  if (errors) {
-    return (
-      <Layout title={`Error | Next.js + TypeScript Example`}>
-        <p><span style={{ color: 'red' }}>Error:</span> {errors}</p>
-      </Layout>
-    )
+class InitialPropsDetail extends React.Component<Props> {
+  static getInitialProps = async ({ query }: NextPageContext) => {
+    try {
+      const { id } = query
+      const item = await findData(Array.isArray(id) ? id[0] : id)
+      return { item }
+    } catch (err) {
+      return { errors: err.message }
+    }
   }
 
-  return (
-    <Layout title={`${item ? item.name : 'Detail'} | Next.js + TypeScript Example`}>
-      {item && <ListDetail item={item} />}
-    </Layout>
-  )
-}
+  render() {
+    const { item, errors } = this.props
 
-InitialPropsDetail.getInitialProps = async ({ query }: NextPageContext) => {
-  try {
-    const { id } = query
-    const item = await findData(Array.isArray(id) ? id[0] : id);
-    return { item }
-  } catch (err) {
-    return { errors: err.message }
+    if (errors) {
+      return (
+        <Layout title={`Error | Next.js + TypeScript Example`}>
+          <p>
+            <span style={{ color: 'red' }}>Error:</span> {errors}
+          </p>
+        </Layout>
+      )
+    }
+
+    return (
+      <Layout
+        title={`${item ? item.name : 'Detail'} | Next.js + TypeScript Example`}
+      >
+        {item && <ListDetail item={item} />}
+      </Layout>
+    )
   }
 }
 
