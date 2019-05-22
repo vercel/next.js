47f472e266e27309045bd09c424aaed7b5cd523d
Lukáš Huvar <lukas@huvar.cz>
2019-05-23 01:22:56 +0200

    Update packages/next/pages/_app.tsx
    Co-Authored-By: Luis Fernando Alvarez D. <luis@zeit.co>

--- packages/next/pages/_app.tsx
+++ packages/next/pages/_app.tsx
@@ -10,7 +10,7 @@ export type AppContext = AppContextType<Router>
 export type AppProps<P = {}> = AppPropsType<Router, P>
 
 /**
- * `App` component is used for initialize of pages. It allows for overriding and full control of the `page` initialization.
+ * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
  * This allows for keeping state between navigation, custom error handling, injecting additional data.
  */
 async function appGetInitialProps({ Component, ctx }: AppContext): Promise<AppInitialProps> {
