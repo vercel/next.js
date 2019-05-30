9851aacb54c4b60dc59686ee2fac41f10bc38bd5
Joe Haddad <joe.haddad@zeit.co>
2019-05-29 18:08:32 -0700

    Fix type error

--- packages/next/pages/_document.tsx
+++ packages/next/pages/_document.tsx
@@ -271,6 +271,7 @@ export class Head extends Component<OriginProps> {
       styles &&
       // @ts-ignore Property 'props' does not exist on type ReactElement
       styles.props &&
+      // @ts-ignore Property 'props' does not exist on type ReactElement
       Array.isArray(styles.props.children)
     ) {
       const hasStyles = (el: React.ReactElement) =>
