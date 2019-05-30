16984728dc61186020ce69742485b20b324eb397
JJ Kasper <jj@jjsweb.site>
2019-05-29 17:59:08 -0700

    Upgrade to latest AMP toolbox optimizer

--- packages/next-server/server/optimize-amp.ts
+++ packages/next-server/server/optimize-amp.ts
@@ -1,44 +1,10 @@
-import { ParsedUrlQuery } from 'querystring'
-
-interface IOptimizerConfig {
-  transforms?: string[]
-  validAmp?: boolean
-  verbose?: boolean
-}
-
-interface IOptimizer {
-  transformHtml: (html: string, params?: object) => Promise<string>
-  setConfig: (config: IOptimizerConfig) => void
-}
-
-interface IOptimizeOptions {
-  amphtml?: boolean
-  query?: ParsedUrlQuery
-}
-
-export default async function optimize(
-  html: string,
-  { amphtml, query }: IOptimizeOptions
-): Promise<string> {
-  let ampOptimizer: IOptimizer
+export default async function optimize(html: string): Promise<string> {
+  let AmpOptimizer
   try {
-    ampOptimizer = require('amp-toolbox-optimizer')
+    AmpOptimizer = require('amp-toolbox-optimizer')
   } catch (_) {
     return html
   }
-  const validAmp = Boolean(amphtml && query && query.amp)
-  //  Examples below
-  //
-  //  pages/foo.js
-  //  pages/foo.amp.js
-  //  pages/bar.amp.js
-  //
-  //  /foo => traditional
-  //  /foo?amp=1 => clean AMP
-  //  /bar => dirty amp
-  //  /bar?amp=1 => clean AMP
-
-  ampOptimizer.setConfig({ validAmp })
-  html = await ampOptimizer.transformHtml(html)
-  return html
+  const optimizer = AmpOptimizer.create()
+  return optimizer.transformHtml(html)
 }
