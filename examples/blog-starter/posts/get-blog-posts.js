3f40bc2fdc26a11ca0d04f429f0b95bda162e36b
j0lv3r4 <jolvera@posteo.de>
2019-06-05 12:24:28 -0500

    add eslint rule for no-eval

--- examples/blog-starter/posts/get-blog-posts.js
+++ examples/blog-starter/posts/get-blog-posts.js
@@ -1,30 +1,30 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const DIR = path.join(process.cwd(), "/pages/posts/");
-const META = /export\s+const\s+meta\s+=\s+(\{(\n|.)*?\n\})/;
+const DIR = path.join(process.cwd(), '/pages/posts/')
+const META = /export\s+const\s+meta\s+=\s+(\{(\n|.)*?\n\})/
 const files = fs
   .readdirSync(DIR)
-  .filter(file => file.endsWith(".md") || file.endsWith(".mdx"));
+  .filter(file => file.endsWith('.md') || file.endsWith('.mdx'))
 
 module.exports = files
   .map((file, index) => {
-    const name = path.join(DIR, file);
-    const contents = fs.readFileSync(name, "utf-8");
-    const match = META.exec(contents);
+    const name = path.join(DIR, file)
+    const contents = fs.readFileSync(name, 'utf-8')
+    const match = META.exec(contents)
 
-    if (!match || typeof match[1] !== "string") {
-      throw new Error(`${name} needs to export const meta = {}`);
+    if (!match || typeof match[1] !== 'string') {
+      throw new Error(`${name} needs to export const meta = {}`)
     }
 
     // eslint-disable no-eval
-    const meta = eval("(" + match[1] + ")");
+    const meta = eval('(' + match[1] + ')')
 
     return {
       ...meta,
-      path: "/posts/" + file.replace(/\.mdx?$/, ""),
+      path: '/posts/' + file.replace(/\.mdx?$/, ''),
       index
-    };
+    }
   })
   .filter(meta => meta.published)
-  .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+  .sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt))
