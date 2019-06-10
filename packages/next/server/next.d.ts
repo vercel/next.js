8fb740927515b0847234e9863bc5b6b18a2dd6a6
Lukáš Huvar <lukas@huvar.cz>
2019-06-10 14:49:47 +0200

    Adding Typescript description to next fixes #7529

--- packages/next/server/next.d.ts
+++ packages/next/server/next.d.ts
@@ -1,1 +1,13 @@
-export default function(options: any): any
+import Server from 'next-server/dist/server/next-server'
+
+interface DevServer extends Server {
+  hotLoader: any
+  renderOpts: Server['renderOpts'] & { dev: true }
+  addExportPathMapRoutes(): Promise<any>
+  getCompilationError(page: any): Promise<any>
+}
+
+declare function next(options: any): Server
+declare function next(options: any & { dev: true }): DevServer
+
+export = next
