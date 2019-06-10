f4bd3afe9e71ad3d523aa34749b69f33a91820cf
Lukáš Huvar <lukas@huvar.cz>
2019-06-10 15:15:05 +0200

    Improve typing for next

--- packages/next/server/next.d.ts
+++ packages/next/server/next.d.ts
@@ -1,10 +1,7 @@
 import Server from 'next-server/dist/server/next-server'
 
 interface DevServer extends Server {
-  hotLoader: any
   renderOpts: Server['renderOpts'] & { dev: true }
-  addExportPathMapRoutes(): Promise<any>
-  getCompilationError(page: any): Promise<any>
 }
 
 declare function next(options: any): Server
