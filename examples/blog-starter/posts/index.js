7f6473ebcccd0010d53fa87801859a22ade20182
j0lv3r4 <jolvera@posteo.de>
2019-06-12 16:53:39 -0500

    remove trailing spaces

--- examples/blog-starter/posts/index.js
+++ examples/blog-starter/posts/index.js
@@ -2,10 +2,10 @@ import preval from 'babel-plugin-preval/macro'
 
 /**
  * The preval plugin pre-evaluates code at build time. We use this to get the
- * meta from the MDX files (blog posts) and use it for displaying the list of 
+ * meta from the MDX files (blog posts) and use it for displaying the list of
  * posts in the `index.js` page
- * 
- * This code is not used in the browser or the app at all, but only in build 
+ *
+ * This code is not used in the browser or the app at all, but only in build
  * time when Node is available.
  */
 
