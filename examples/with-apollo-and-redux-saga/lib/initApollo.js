bbb1fdd030b643c880ae6f7aa7db71b52f775771
谢祯晖 <xiezh0831@yahoo.co.jp>
2019-06-16 09:45:02 +0800

    Update initApollo.js

--- examples/with-apollo-and-redux-saga/lib/initApollo.js
+++ examples/with-apollo-and-redux-saga/lib/initApollo.js
@@ -4,6 +4,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'isomorphic-unfetch'
 
 let apolloClient = null
+const uri = process.browser
+  ? 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn'  // Client URL
+  : 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn'  // Server URL (must be absolute)
 
 // Polyfill fetch() on the server (used by apollo-client)
 if (!process.browser) {
@@ -16,7 +19,7 @@ function create (initialState) {
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
+      uri,
       credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache().restore(initialState || {})
