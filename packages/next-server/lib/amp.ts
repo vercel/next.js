b9d4762ff09202c4f089c4b00b3b9dad849ecaf0
Joe Haddad <joe.haddad@zeit.co>
2019-06-27 15:12:30 -0400

    Remove `withAmp` and do not expose `isInAmpMode` This pull request removes the `withAmp` higher-order component in favor of the exported configuration object. Retaining this backwards compatibility isn't worth the extra maintenance burden, given it hasn't been released for very long and the migration is ultra-simple.
    I'm :-1: on retaining backwards compatibility just for the sake of it.
    
    Migration example follows. I'm willing to write a codemod if reviewers feel it's necessary.
    
    *Before*
    ```js
    import { withAmp } from 'next/amp'
    
    function Home() {
      return <h1>My AMP Page</h1>
    }
    
    export default withAmp(Home)
    // or
    export default withAmp(Home, { hybrid: true })
    ```
    
    *After*
    ```js
    export default function Home() {
      return <h1>My AMP Page</h1>
    }
    
    export const config = {
      amp: true,
      // or
      amp: 'hybrid',
    }
    ```

--- packages/next-server/lib/amp.ts
+++ packages/next-server/lib/amp.ts
@@ -14,20 +14,3 @@ export function useAmp() {
   // un-comment below to not be considered AMP in dirty mode
   return isInAmpMode(ampState) // && ampMode.hasQuery
 }
-
-/**
- * @deprecated This is no longer required, use export const config = { amp: true }
- */
-export function withAmp(Component: any, { hybrid = false } = {}): any {
-  function WithAmpWrapper(props = {}) {
-    const ampState = React.useContext(AmpStateContext)
-    ampState.ampFirst = !hybrid
-    ampState.hybrid = hybrid
-
-    return React.createElement(Component, props)
-  }
-
-  WithAmpWrapper.__nextAmpOnly = !hybrid
-  WithAmpWrapper.getInitialProps = Component.getInitialProps
-  return WithAmpWrapper
-}
