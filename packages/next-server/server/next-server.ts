9959e1776474a422c2d020e0a11788696369c5a7
Joe Haddad <joe.haddad@zeit.co>
2019-07-03 15:40:29 -0400

    Revert "Handle sent responses"
    This reverts commit 5e9012ae29b611c25ed1e090756a25dbe3437182.

--- packages/next-server/server/next-server.ts
+++ packages/next-server/server/next-server.ts
@@ -22,7 +22,7 @@ import {
   isDynamicRoute,
 } from '../lib/router/utils'
 import * as envConfig from '../lib/runtime-config'
-import { NextApiRequest, NextApiResponse, isResSent } from '../lib/utils'
+import { NextApiRequest, NextApiResponse } from '../lib/utils'
 import {
   getQueryParser,
   sendJson,
@@ -533,8 +533,6 @@ export default class Server {
       .then(
         result => {
           if (!(req.method === 'GET' || req.method === 'HEAD')) {
-            if (isResSent(res)) return null
-
             res.statusCode = 405
             res.setHeader('Allow', ['GET', 'HEAD'])
             return this.renderError(null, req, res, pathname, query)
@@ -563,8 +561,6 @@ export default class Server {
             return this.findPageComponents(dynamicRoute.page, query).then(
               result => {
                 if (!(req.method === 'GET' || req.method === 'HEAD')) {
-                  if (isResSent(res)) return null
-
                   res.statusCode = 405
                   res.setHeader('Allow', ['GET', 'HEAD'])
                   return this.renderError(null, req, res, pathname, query)
