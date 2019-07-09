c512cb15444edba1939d55e2f8820ca630853955
Luis Alvarez <luis@zeit.co>
2019-07-09 12:12:38 -0500

    Added tests for types of useRouter and withRouter

--- test/integration/typescript/pages/hello.tsx
+++ test/integration/typescript/pages/hello.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { hello } from '../components/hello'
 import { World } from '../components/world'
 
 export default function HelloPage(): JSX.Element {
+  const router = useRouter()
   console.log(process.browser)
+  console.log(router.pathname)
   return (
     <div>
       {hello()} <World />
