0288e8b047059c3175c97dbce0686843fa61ea2d
Marais Rossouw <me@maraisr.com>
2019-07-09 12:54:28 +1000

    feat: Props and InitialProps should be a union

--- packages/next/types/index.d.ts
+++ packages/next/types/index.d.ts
@@ -38,7 +38,7 @@ declare module 'react' {
  * `Page` type, use it as a guide to create `pages`.
  */
 export type NextPage<P = {}, IP = P> = {
-  (props: P): JSX.Element
+  (props: P & IP): JSX.Element
   /**
    * Used for initial page load data population. Data returned from `getInitialProps` is serialized when server rendered.
    * Make sure to return plain `Object` without using `Date`, `Map`, `Set`.
