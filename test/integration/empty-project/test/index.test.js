b7751b40e6899b6314c8c4c532b08c2ce971a241
Joe Haddad <joe.haddad@zeit.co>
2019-07-10 11:19:00 -0400

    Fix test re-runs

--- test/integration/empty-project/test/index.test.js
+++ test/integration/empty-project/test/index.test.js
@@ -21,5 +21,8 @@ describe('Empty Project', () => {
     expect(res.status).toBe(404)
   })
 
-  afterAll(() => killApp(context.server))
+  afterAll(() => {
+    killApp(context.server)
+    fs.closeSync(fs.openSync(join(__dirname, '..', 'pages', '.gitkeep'), 'w'))
+  })
 })
