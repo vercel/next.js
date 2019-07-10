8122d821306497fa6c8598270eceaa56fa175e02
Tim Neutkens <tim@timneutkens.nl>
2019-07-10 17:45:53 +0200

    Handle empty directory (#7876)
    * Add test for empty directory
    
    * Fix test re-runs
    
    * Handle empty `pages/` directory

---
+++ test/integration/empty-project/test/index.test.js
@@ -0,0 +1,28 @@
+/* eslint-env jest */
+/* global jasmine */
+import { join } from 'path'
+import fs from 'fs'
+import { fetchViaHTTP, findPort, launchApp, killApp } from 'next-test-utils'
+
+const context = {}
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+
+describe('Empty Project', () => {
+  beforeAll(async () => {
+    fs.unlinkSync(join(__dirname, '..', 'pages', '.gitkeep'))
+    context.appPort = await findPort()
+    context.server = await launchApp(join(__dirname, '../'), context.appPort)
+  })
+
+  const fetch = (p, q) => fetchViaHTTP(context.appPort, p, q, { timeout: 5000 })
+
+  it('Should not time out and return 404', async () => {
+    const res = await fetch('/')
+    expect(res.status).toBe(404)
+  })
+
+  afterAll(() => {
+    killApp(context.server)
+    fs.closeSync(fs.openSync(join(__dirname, '..', 'pages', '.gitkeep'), 'w'))
+  })
+})
