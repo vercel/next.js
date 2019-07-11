5ec94dc2b77386cceab27fcc47a5867c1312eba0
Janicklas Ralph James <janicklas@google.com>
2019-07-11 15:02:48 -0700

    Adding testcase for modern build

--- test/integration/production-build-dir/test/index.test.js
+++ test/integration/production-build-dir/test/index.test.js
@@ -1,6 +1,9 @@
 /* eslint-env jest */
 /* global jasmine */
 import { join } from 'path'
+import { readFileSync, readdirSync } from 'fs'
+import rimraf from 'rimraf'
+import { promisify } from 'util'
 import {
   nextServer,
   runNextCommand,
@@ -11,29 +14,69 @@ import {
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 
+const rimrafPromise = promisify(rimraf)
+let appDir = join(__dirname, '..')
+let buildDir = join(__dirname, '../build')
+let appPort
+let server
+let result
+
 describe('Production Custom Build Directory', () => {
+  beforeAll(async () => {
+    result = await runNextCommand(['build', 'build'], {
+      cwd: appDir,
+      stdout: true,
+      stderr: true
+    })
+
+    const app = nextServer({
+      dir: buildDir,
+      dev: false,
+      quiet: true
+    })
+
+    server = await startApp(app)
+    appPort = server.address().port
+  })
+  afterAll(async () => {
+    stopApp(server)
+    rimrafPromise(join(buildDir, '.next'))
+  })
+
   describe('With basic usage', () => {
     it('should render the page', async () => {
-      const result = await runNextCommand(['build', 'build'], {
-        cwd: join(__dirname, '..'),
-        stdout: true,
-        stderr: true
-      })
       expect(result.stderr).toBe('')
-
-      const app = nextServer({
-        dir: join(__dirname, '../build'),
-        dev: false,
-        quiet: true
-      })
-
-      const server = await startApp(app)
-      const appPort = server.address().port
-
       const html = await renderViaHTTP(appPort, '/')
       expect(html).toMatch(/Hello World/)
+    })
 
-      await stopApp(server)
+    it('should generate client side modern and legacy build files', async () => {
+      const buildId = readFileSync(join(buildDir, '.next/BUILD_ID'), 'utf8')
+
+      const expectedFiles = [
+        'index',
+        '_app',
+        '_error',
+        'main',
+        'webpack',
+        'commons'
+      ]
+      const buildFiles = [
+        ...readdirSync(join(buildDir, '.next/static', buildId, 'pages')),
+        ...readdirSync(join(buildDir, '.next/static/runtime')).map(
+          file => file.replace(/-\w+\./, '.') // remove hash
+        ),
+        ...readdirSync(join(buildDir, '.next/static/chunks')).map(
+          file => file.replace(/\.\w+\./, '.') // remove hash
+        )
+      ]
+
+      console.log(`Client files: ${buildFiles.join(', ')}`)
+
+      expectedFiles.forEach(file => {
+        expect(buildFiles).toContain(`${file}.js`)
+        expect(buildFiles).toContain(`${file}.es6.js`)
+      })
     })
   })
 })
