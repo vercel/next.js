b94123ccc9d801848a949586ed654a69a5bb970a
Luis Fernando Alvarez D <luis@zeit.co>
2019-07-11 12:35:39 -0500

    Improve exported router types (#7853)
    * Added the RouteUrl type and improved router types
    
    * Added more tests for router types
    
    * Add build test for typescript types
    
    * Add next-env.d.ts to the typescript test
    
    * Removed next-env.d.ts
    
    * Added next-env.d.ts to gitignore
    
    * Remove route url re-exports
    
    * renamed PublicRouterInstance to be NextRouter
    
    * export the Url type
    
    * Replaced BaseRouter with NextRouter in server/utils
    
    * Don't export the Url type
    
    * Update tsconfig.json

--- test/integration/typescript/pages/hello.tsx
+++ test/integration/typescript/pages/hello.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { hello } from '../components/hello'
 import { World } from '../components/world'
+import Router from '../components/router'
 
 export default function HelloPage(): JSX.Element {
-  // TEST: verify process.browser extension works
+  const router = useRouter()
   console.log(process.browser)
+  console.log(router.pathname)
   return (
     <div>
       {hello()} <World />
+      <Router />
     </div>
   )
 }
