e2f43daa6b5dc3c333224c765c1d0c9209d0df56
ak <ak@tangro.de>
2019-07-12 09:00:49 +0200

    added HTTP Post example

--- examples/api-routes/pages/api/people/index.js
+++ examples/api-routes/pages/api/people/index.js
@@ -1,5 +1,14 @@
 import { people } from '../../../data'
 
 export default (req, res) => {
-  res.status(200).json(people)
+  if (req.method === 'POST') {
+    if (req.headers['content-type'].startsWith('application/json')) {
+      // req.body will be automatically parsed to JSON when the request's Content-Type is `application/json`
+      res.json(req.body)
+    } else {
+      res.send(req.body)
+    }
+  } else {
+    res.status(200).json(people)
+  }
 }
