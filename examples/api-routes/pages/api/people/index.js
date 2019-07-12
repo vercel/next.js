032e8d4dff7259890cf6cb701a47d6e917b8309f
ak <ak@tangro.de>
2019-07-12 14:45:31 +0200

    no longer returns the posted req.body

--- examples/api-routes/pages/api/people/index.js
+++ examples/api-routes/pages/api/people/index.js
@@ -4,9 +4,13 @@ export default (req, res) => {
   if (req.method === 'POST') {
     if (req.headers['content-type'].startsWith('application/json')) {
       // req.body will be automatically parsed to JSON when the request's Content-Type is `application/json`
-      res.json(req.body)
+      res.send('The data you sent has been interpreted as application/json.')
     } else {
-      res.send(req.body)
+      res
+        .status(400)
+        .send(
+          'Please POST JSON and use Content-Type="application/json" in your HTTP headers.'
+        )
     }
   } else {
     res.status(200).json(people)
