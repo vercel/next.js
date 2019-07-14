87dce27c9ad64770831d037788160d827637d0a9
Natalie Marleny <nataliemarleny@gmail.com>
2019-07-14 21:25:10 +0100

    Update test from autoExport to automatic static optimization
    - Also reflect new wording of error message for readability

--- test/integration/invalid-page-automatic-static-optimization/test/index.test.js
+++ test/integration/invalid-page-automatic-static-optimization/test/index.test.js
@@ -6,12 +6,12 @@ import { nextBuild } from 'next-test-utils'
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 const appDir = path.join(__dirname, '..')
 
-describe('Invalid Page autoExport', () => {
+describe('Invalid Page automatic static optimization', () => {
   it('Fails softly with descriptive error', async () => {
     const { stderr } = await nextBuild(appDir, [], { stderr: true })
 
     expect(stderr).toMatch(
-      /autoExport failed: found pages without React Component as default export/
+      /automatic static optimization failed: found pages without a React Component as default export in/
     )
     expect(stderr).toMatch(/pages\/invalid/)
     expect(stderr).toMatch(/pages\/also-invalid/)
