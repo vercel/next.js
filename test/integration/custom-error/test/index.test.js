8b8ad7556cba9bd02e313e18af3ae18263cbd80c
JJ Kasper <jj@jjsweb.site>
2019-07-15 16:39:59 -0700

    Add serverless mode for custom-error test

--- test/integration/custom-error/test/index.test.js
+++ test/integration/custom-error/test/index.test.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 /* global jasmine */
+import fs from 'fs-extra'
 import { join } from 'path'
 import {
   renderViaHTTP,
@@ -11,19 +12,44 @@ import {
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 const appDir = join(__dirname, '..')
+const nextConfig = join(appDir, 'next.config.js')
 let appPort
 let app
 
-describe('Custom _error', () => {
-  beforeAll(async () => {
-    await nextBuild(appDir)
-    appPort = await findPort()
-    app = await nextStart(appDir, appPort)
-  })
-  afterAll(() => killApp(app))
-
+const runTests = () => {
   it('renders custom _error successfully', async () => {
     const html = await renderViaHTTP(appPort, '/')
     expect(html).toMatch(/Custom error/)
+  })
+}
+
+describe('Custom _error', () => {
+  describe('production mode', () => {
+    beforeAll(async () => {
+      await nextBuild(appDir)
+      appPort = await findPort()
+      app = await nextStart(appDir, appPort)
+    })
+    afterAll(() => killApp(app))
+
+    runTests()
+  })
+
+  describe('serverless mode', () => {
+    beforeAll(async () => {
+      await fs.writeFile(
+        nextConfig,
+        `module.exports = { target: 'serverless' }`
+      )
+      await nextBuild(appDir)
+      appPort = await findPort()
+      app = await nextStart(appDir, appPort)
+    })
+    afterAll(async () => {
+      await killApp(app)
+      await fs.remove(nextConfig)
+    })
+
+    runTests()
   })
 })
