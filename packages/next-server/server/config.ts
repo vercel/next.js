3bc16ce92dbf7a0ba9aca5e4ae73b5f88d752b4f
Janicklas Ralph James <janicklas@google.com>
2019-07-15 11:54:31 -0700

    Setting default crossOrigin value

--- packages/next-server/server/config.ts
+++ packages/next-server/server/config.ts
@@ -17,6 +17,7 @@ const defaultConfig: { [key: string]: any } = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   target: process.env.__NEXT_BUILDER_EXPERIMENTAL_TARGET || 'server',
   poweredByHeader: true,
+  crossOrigin: 'anonymous',
   modern: true,
   safari10NomoduleFix: false,
   onDemandEntries: {
