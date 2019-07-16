d84f2919e6fc65d2ca6a3daa3378e79ea7aeade1
Lukáš Huvar <lukas@huvar.cz>
2019-07-16 18:43:23 +0200

    Make tests for both async and sync GIP (#7999)

--- test/integration/typescript/pages/_error.tsx
+++ test/integration/typescript/pages/_error.tsx
@@ -2,7 +2,7 @@ import { NextPageContext } from 'next'
 import ErrorComponent from 'next/error'
 
 class CustomError extends ErrorComponent {
-  static async getInitialProps({ res }: NextPageContext) {
+  static getInitialProps({ res }: NextPageContext) {
     const statusCode = 500
     return { statusCode, title: 'CustomError' }
   }
