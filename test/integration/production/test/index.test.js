f43ae7363ebc72e0406862728a36a6e55a1f57f6
Joe Haddad <joe.haddad@zeit.co>
2019-07-16 07:40:02 -0700

    Adjust test names for `send` (#7996)
    * Adjust test names for `serve`
    
    * Clarify names more

--- test/integration/production/test/index.test.js
+++ test/integration/production/test/index.test.js
@@ -126,19 +126,30 @@ describe('Production Usage', () => {
       expect(res.status).toBe(405)
     })
 
-    it('should return 412 method on static file if If-Unmodified-Since is invalid', async () => {
+    it('should return 412 on static file when If-Unmodified-Since is provided and file is modified', async () => {
       const buildId = readFileSync(join(__dirname, '../.next/BUILD_ID'), 'utf8')
 
       const res = await fetch(
         `http://localhost:${appPort}/_next/static/${buildId}/pages/index.js`,
         {
           method: 'GET',
-          headers: {
-            'if-unmodified-since': 'Fri, 12 Jul 2019 20:00:13 GMT'
-          }
+          headers: { 'if-unmodified-since': 'Fri, 12 Jul 2019 20:00:13 GMT' }
         }
       )
       expect(res.status).toBe(412)
+    })
+
+    it('should return 200 on static file if If-Unmodified-Since is invalid date', async () => {
+      const buildId = readFileSync(join(__dirname, '../.next/BUILD_ID'), 'utf8')
+
+      const res = await fetch(
+        `http://localhost:${appPort}/_next/static/${buildId}/pages/index.js`,
+        {
+          method: 'GET',
+          headers: { 'if-unmodified-since': 'nextjs' }
+        }
+      )
+      expect(res.status).toBe(200)
     })
 
     it('should set Content-Length header', async () => {
