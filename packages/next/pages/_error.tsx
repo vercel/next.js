a379e1cc997d8d5cb2c664f4daa054349450e06a
Adam Watters <adamwatters11@gmail.com>
2019-07-24 11:20:44 -0400

    Add default message for 410
    Added default message "Page Deleted" for when Error component receives prop `statusCode` but nothing for prop `title`

--- packages/next/pages/_error.tsx
+++ packages/next/pages/_error.tsx
@@ -6,6 +6,7 @@ const statusCodes: { [code: number]: string } = {
   400: 'Bad Request',
   404: 'This page could not be found',
   405: 'Method Not Allowed',
+  410: 'Page Deleted',
   500: 'Internal Server Error',
 }
 
