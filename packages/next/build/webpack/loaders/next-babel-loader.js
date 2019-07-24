51ee54909cd8aff99656c47f49916f1db614a84a
Joe Haddad <joe.haddad@zeit.co>
2019-07-24 14:33:04 -0400

    Tweak names

--- packages/next/build/webpack/loaders/next-babel-loader.js
+++ packages/next/build/webpack/loaders/next-babel-loader.js
@@ -20,8 +20,8 @@ const getModernOptions = (babelOptions = {}) => {
   presetEnvOptions.exclude = [
     ...(presetEnvOptions.exclude || []),
     // Blacklist accidental inclusions
-    'transform-regenerator',
-    'transform-async-to-generator'
+    'plugin-transform-regenerator',
+    'plugin-transform-async-to-generator'
   ]
 
   return {
