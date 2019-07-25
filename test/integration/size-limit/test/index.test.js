46fed08cc755eff76f825cf1e407cac5ba78f680
Joe Haddad <joe.haddad@zeit.co>
2019-07-24 20:23:53 -0400

    Correctly clamp bundle sizes

--- test/integration/size-limit/test/index.test.js
+++ test/integration/size-limit/test/index.test.js
@@ -81,7 +81,7 @@ describe('Production response size', () => {
     )
 
     // These numbers are without gzip compression!
-    expect(responseSizeKilobytes).toBeLessThanOrEqual(216) // Kilobytes
+    expect(responseSizeKilobytes).toBeLessThanOrEqual(213) // Kilobytes
   })
 
   it('should not increase the overall response size of modern build', async () => {
@@ -99,6 +99,6 @@ describe('Production response size', () => {
     )
 
     // These numbers are without gzip compression!
-    expect(responseSizeKilobytes).toBeLessThanOrEqual(216) // Kilobytes
+    expect(responseSizeKilobytes).toBeLessThanOrEqual(186) // Kilobytes
   })
 })
