58082f626aa9b984e5e4dace9d2ae96b2a66d77f
Joe Haddad <joe.haddad@zeit.co>
2019-07-24 20:33:14 -0400

    Revert test

--- test/integration/production-build-dir/test/index.test.js
+++ test/integration/production-build-dir/test/index.test.js
@@ -1,9 +1,6 @@
 /* eslint-env jest */
 /* global jasmine */
 import { join } from 'path'
-import { readFileSync, readdirSync, writeFileSync, unlinkSync } from 'fs'
-import rimraf from 'rimraf'
-import { promisify } from 'util'
 import {
   nextServer,
   runNextCommand,
@@ -14,105 +11,29 @@ import {
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 
-const rimrafPromise = promisify(rimraf)
-let appDir = join(__dirname, '..')
-let buildDir = join(__dirname, '../build')
-let nextConfigFile = join(appDir, 'next.config.js')
-let appPort
-let server
-let result
-
 describe('Production Custom Build Directory', () => {
   describe('With basic usage', () => {
-    beforeAll(async () => {
-      result = await runNextCommand(['build', 'build'], {
-        cwd: appDir,
+    it('should render the page', async () => {
+      const result = await runNextCommand(['build', 'build'], {
+        cwd: join(__dirname, '..'),
         stdout: true,
         stderr: true
       })
+      expect(result.stderr).toBe('')
 
       const app = nextServer({
-        dir: buildDir,
+        dir: join(__dirname, '../build'),
         dev: false,
         quiet: true
       })
 
-      server = await startApp(app)
-      appPort = server.address().port
-    })
-    afterAll(async () => {
-      stopApp(server)
-      rimrafPromise(join(buildDir, '.next'))
-    })
-    it('should render the page', async () => {
-      expect(result.stderr).toBe('')
+      const server = await startApp(app)
+      const appPort = server.address().port
+
       const html = await renderViaHTTP(appPort, '/')
       expect(html).toMatch(/Hello World/)
-    })
-  })
 
-  describe('With experimental modern flag enabled', () => {
-    beforeAll(async () => {
-      writeFileSync(
-        nextConfigFile,
-        `{
-        module.exports = {
-          experimental: {
-            modern: true
-          }
-        }
-      }`
-      )
-      result = await runNextCommand(['build', 'build'], {
-        cwd: appDir,
-        stdout: true,
-        stderr: true
-      })
-
-      const app = nextServer({
-        dir: buildDir,
-        dev: false,
-        quiet: true,
-        experimental: {
-          modern: true
-        }
-      })
-
-      server = await startApp(app)
-      appPort = server.address().port
-    })
-    afterAll(async () => {
-      unlinkSync(nextConfigFile)
-      stopApp(server)
-      rimrafPromise(join(buildDir, '.next'))
-    })
-    it('should generate client side modern and legacy build files', async () => {
-      const buildId = readFileSync(join(buildDir, '.next/BUILD_ID'), 'utf8')
-
-      const expectedFiles = [
-        'index',
-        '_app',
-        '_error',
-        'main',
-        'webpack',
-        'commons'
-      ]
-      const buildFiles = [
-        ...readdirSync(join(buildDir, '.next/static', buildId, 'pages')),
-        ...readdirSync(join(buildDir, '.next/static/runtime')).map(
-          file => file.replace(/-\w+\./, '.') // remove hash
-        ),
-        ...readdirSync(join(buildDir, '.next/static/chunks')).map(
-          file => file.replace(/\.\w+\./, '.') // remove hash
-        )
-      ]
-
-      console.log(`Client files: ${buildFiles.join(', ')}`)
-
-      expectedFiles.forEach(file => {
-        expect(buildFiles).toContain(`${file}.js`)
-        expect(buildFiles).toContain(`${file}.module.js`)
-      })
+      await stopApp(server)
     })
   })
 })
