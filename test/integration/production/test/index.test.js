88f15646c457b23eb002b03cbd852d1b903b8227
Alex Castle <atcastle@gmail.com>
2019-08-07 15:29:43 -0700

    Update test/integration/production/test/index.test.js
    Co-Authored-By: Joe Haddad <timer150@gmail.com>

--- test/integration/production/test/index.test.js
+++ test/integration/production/test/index.test.js
@@ -417,7 +417,7 @@ describe('Production Usage', () => {
     if (browserName === 'chrome') {
       it('should add preload tags when Link prefetch prop is used', async () => {
         const browser = await webdriver(appPort, '/prefetch')
-        await waitFor(50)
+        await waitFor(2000)
         const elements = await browser.elementsByCss('link[rel=preload]')
 
         expect(elements.length).toBe(9)
