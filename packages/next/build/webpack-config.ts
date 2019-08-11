debc6bef7d33ec884791b58fd965dbe58e54b440
Steven Sinatra <5153378+diagramatics@users.noreply.github.com>
2019-08-11 20:24:21 +1000

    Add info about removing @zeit/next-typescript on babelrc

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -652,7 +652,7 @@ export default async function getBaseWebpackConfig(
 
     if (foundTsRule) {
       console.warn(
-        '\n@zeit/next-typescript is no longer needed since Next.js has built-in support for TypeScript now. Please remove it from your next.config.js\n'
+        '\n@zeit/next-typescript is no longer needed since Next.js has built-in support for TypeScript now. Please remove it from your next.config.js and your .babelrc\n'
       )
     }
   }
