5770cee1432e61464e86aa504001adb8aba010cf
Jason Miller <developit@google.com>
2019-08-13 10:42:08 -0400

    fix build (oh dear)

--- test/integration/optimize-libraries/pages/index.js
+++ test/integration/optimize-libraries/pages/index.js
@@ -1,3 +1,5 @@
 import moment from 'moment'
-window.moment = moment
+if (typeof window !== 'undefined') {
+  window.moment = moment
+}
 export default () => <h1>Current time: {moment().format('LLL')}</h1>
