1765007a10c63cbeebe74da6044b9158ff32fb85
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-08-14 17:00:34 -0300

    Update with-apollo example with hooks (#8363)
    * Add @apollo/react-hooks and @apollo/react-ssr
    
    * Migrate react-apollo to @apollo/react-hooks and Query component
    
    * Migrate direct client mutations to useMutation hook
    
    * Disable submit button when submiting a new post
    
    * Fix Show More button not updating when loading more entries
    
    * All queries with uppercase name
    
    * Remove react-apollo
    
    * Update getDataFromTree links
    
    * Fixed lint issues

--- examples/with-apollo/components/PostList.js
+++ examples/with-apollo/components/PostList.js
@@ -1,9 +1,10 @@
-import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
+import { NetworkStatus } from 'apollo-boost'
 import gql from 'graphql-tag'
 import ErrorMessage from './ErrorMessage'
 import PostUpvoter from './PostUpvoter'
 
-export const allPostsQuery = gql`
+export const ALL_POSTS_QUERY = gql`
   query allPosts($first: Int!, $skip: Int!) {
     allPosts(orderBy: createdAt_DESC, first: $first, skip: $skip) {
       id
@@ -23,92 +24,98 @@ export const allPostsQueryVars = {
 }
 
 export default function PostList () {
-  return (
-    <Query query={allPostsQuery} variables={allPostsQueryVars}>
-      {({ loading, error, data: { allPosts, _allPostsMeta }, fetchMore }) => {
-        if (error) return <ErrorMessage message='Error loading posts.' />
-        if (loading) return <div>Loading</div>
-
-        const areMorePosts = allPosts.length < _allPostsMeta.count
-        return (
-          <section>
-            <ul>
-              {allPosts.map((post, index) => (
-                <li key={post.id}>
-                  <div>
-                    <span>{index + 1}. </span>
-                    <a href={post.url}>{post.title}</a>
-                    <PostUpvoter id={post.id} votes={post.votes} />
-                  </div>
-                </li>
-              ))}
-            </ul>
-            {areMorePosts ? (
-              <button onClick={() => loadMorePosts(allPosts, fetchMore)}>
-                {' '}
-                {loading ? 'Loading...' : 'Show More'}{' '}
-              </button>
-            ) : (
-              ''
-            )}
-            <style jsx>{`
-              section {
-                padding-bottom: 20px;
-              }
-              li {
-                display: block;
-                margin-bottom: 10px;
-              }
-              div {
-                align-items: center;
-                display: flex;
-              }
-              a {
-                font-size: 14px;
-                margin-right: 10px;
-                text-decoration: none;
-                padding-bottom: 0;
-                border: 0;
-              }
-              span {
-                font-size: 14px;
-                margin-right: 5px;
-              }
-              ul {
-                margin: 0;
-                padding: 0;
-              }
-              button:before {
-                align-self: center;
-                border-style: solid;
-                border-width: 6px 4px 0 4px;
-                border-color: #ffffff transparent transparent transparent;
-                content: '';
-                height: 0;
-                margin-right: 5px;
-                width: 0;
-              }
-            `}</style>
-          </section>
-        )
-      }}
-    </Query>
-  )
-}
-
-function loadMorePosts (allPosts, fetchMore) {
-  fetchMore({
-    variables: {
-      skip: allPosts.length
-    },
-    updateQuery: (previousResult, { fetchMoreResult }) => {
-      if (!fetchMoreResult) {
-        return previousResult
-      }
-      return Object.assign({}, previousResult, {
-        // Append the new posts results to the old one
-        allPosts: [...previousResult.allPosts, ...fetchMoreResult.allPosts]
-      })
+  const { loading, error, data, fetchMore, networkStatus } = useQuery(
+    ALL_POSTS_QUERY,
+    {
+      variables: allPostsQueryVars,
+      // Setting this value to true will make the component rerender when
+      // the "networkStatus" changes, so we are able to know if it is fetching
+      // more data
+      notifyOnNetworkStatusChange: true
     }
-  })
+  )
+
+  const loadingMorePosts = networkStatus === NetworkStatus.fetchMore
+
+  const loadMorePosts = () => {
+    fetchMore({
+      variables: {
+        skip: allPosts.length
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          return previousResult
+        }
+        return Object.assign({}, previousResult, {
+          // Append the new posts results to the old one
+          allPosts: [...previousResult.allPosts, ...fetchMoreResult.allPosts]
+        })
+      }
+    })
+  }
+
+  if (error) return <ErrorMessage message='Error loading posts.' />
+  if (loading && !loadingMorePosts) return <div>Loading</div>
+
+  const { allPosts, _allPostsMeta } = data
+  const areMorePosts = allPosts.length < _allPostsMeta.count
+
+  return (
+    <section>
+      <ul>
+        {allPosts.map((post, index) => (
+          <li key={post.id}>
+            <div>
+              <span>{index + 1}. </span>
+              <a href={post.url}>{post.title}</a>
+              <PostUpvoter id={post.id} votes={post.votes} />
+            </div>
+          </li>
+        ))}
+      </ul>
+      {areMorePosts && (
+        <button onClick={() => loadMorePosts()} disabled={loadingMorePosts}>
+          {loadingMorePosts ? 'Loading...' : 'Show More'}
+        </button>
+      )}
+      <style jsx>{`
+        section {
+          padding-bottom: 20px;
+        }
+        li {
+          display: block;
+          margin-bottom: 10px;
+        }
+        div {
+          align-items: center;
+          display: flex;
+        }
+        a {
+          font-size: 14px;
+          margin-right: 10px;
+          text-decoration: none;
+          padding-bottom: 0;
+          border: 0;
+        }
+        span {
+          font-size: 14px;
+          margin-right: 5px;
+        }
+        ul {
+          margin: 0;
+          padding: 0;
+        }
+        button:before {
+          align-self: center;
+          border-style: solid;
+          border-width: 6px 4px 0 4px;
+          border-color: #ffffff transparent transparent transparent;
+          content: '';
+          height: 0;
+          margin-right: 5px;
+          width: 0;
+        }
+      `}</style>
+    </section>
+  )
 }
