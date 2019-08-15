247e31f09ed44e0c6ef83c8af4cc8bf91641f3d3
Evan Bacon <baconbrix@gmail.com>
2019-08-14 19:00:07 -0700

    Created a new example for React Native web
    - use the latest `react-native-web`
    - remove unused `react-art`
    - remove redundant `babel-plugin-module-resolver`
    - remove unused `.babelrc`
    - add missing `app.json` (standard in React Native)
    - added example for rendering an `<a />`
    - added header examples

--- examples/with-react-native-web/next.config.js
+++ examples/with-react-native-web/next.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   webpack: config => {
-    // Alias all `react-native` imports to `react-native-web`
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      'react-native$': 'react-native-web'
+      // Transform all direct `react-native` imports to `react-native-web`
+      'react-native$': 'react-native-web',
     }
-
     return config
-  }
+  },
 }
