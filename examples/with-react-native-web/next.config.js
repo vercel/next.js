f0171120c18bb6b3882add39a8c39f5ed51f5d30
Evan Bacon <baconbrix@gmail.com>
2019-08-14 19:07:21 -0700

    `yarn lint-fix`

--- examples/with-react-native-web/next.config.js
+++ examples/with-react-native-web/next.config.js
@@ -3,8 +3,8 @@ module.exports = {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
-      'react-native$': 'react-native-web',
+      'react-native$': 'react-native-web'
     }
     return config
-  },
+  }
 }
