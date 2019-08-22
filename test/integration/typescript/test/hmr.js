58480196f336d221279830fd66d62f5359442c1d
JJ Kasper <jj@jjsweb.site>
2019-08-22 13:06:30 -0500

    Revert "Tweak typescript test suite"
    This reverts commit 9e0108da7c70e7ae45b412b60c07162383de65f3.

--- test/integration/typescript/test/hmr.js
+++ test/integration/typescript/test/hmr.js
@@ -46,19 +46,17 @@ export default (context, renderViaHTTP) => {
       try {
         browser = await webdriver(context.appPort, '/type-error-recover')
         const errContent = origContent.replace('() =>', '(): boolean =>')
-        const errRegex = /Type 'Element' is not assignable to type 'boolean'/
+
         writeFileSync(pagePath, errContent)
+        await check(
+          () => getReactErrorOverlayContent(browser),
+          /Type 'Element' is not assignable to type 'boolean'/
+        )
 
-        await check(() => getReactErrorOverlayContent(browser), errRegex)
         writeFileSync(pagePath, origContent)
-
         await check(async () => {
-          const hasIframe = await browser.hasElementByCssSelector('iframe')
-          if (!hasIframe) return 'success'
-          const html = await browser.eval(
-            `document.querySelector('iframe').contentWindow.document.body.innerHTML`
-          )
-          return html.match(errRegex) ? 'fail' : 'success'
+          const html = await browser.eval('document.documentElement.innerHTML')
+          return html.match(/iframe/) ? 'fail' : 'success'
         }, /success/)
       } finally {
         if (browser) browser.close()
--- test/integration/typescript/test/typescript.js
+++ test/integration/typescript/test/typescript.js
@@ -13,7 +13,7 @@ export default (context, render) => {
   describe('TypeScript', () => {
     describe('default behavior', () => {
       it('should render the page', async () => {
-        const $ = await get$('/another')
+        const $ = await get$('/hello')
         expect($('body').text()).toMatch(/Hello World/)
       })
 
