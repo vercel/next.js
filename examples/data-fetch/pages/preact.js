483cd1052f92d6d1b58144bd678bd990cc12759e
Henrik Wenz <HaNdTriX@users.noreply.github.com>
2019-08-23 17:24:47 +0200

    Refactor data-fetch example (#8490)
    * Remove eslint exclusion
    
    https://www.npmjs.com/package/isomorphic-unfetch
    
    * Fix emoji char
    
    * Update preact.js

--- examples/data-fetch/pages/preact.js
+++ examples/data-fetch/pages/preact.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
-import 'isomorphic-unfetch'
+import fetch from 'isomorphic-unfetch'
 
 export default class Preact extends React.Component {
   static async getInitialProps () {
-    // eslint-disable-next-line no-undef
     const res = await fetch('https://api.github.com/repos/developit/preact')
     const json = await res.json()
     return { stars: json.stargazers_count }
@@ -13,9 +12,9 @@ export default class Preact extends React.Component {
   render () {
     return (
       <div>
-        <p>Preact has {this.props.stars} ⭝︝</p>
+        <p>Preact has {this.props.stars} ⭐</p>
         <Link href='/'>
-          <a>I bet next has more stars (?)</a>
+          <a>I bet Next.js has more stars (?)</a>
         </Link>
       </div>
     )
