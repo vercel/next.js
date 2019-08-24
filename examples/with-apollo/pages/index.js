4321c469466160d13bcd52afa099385e84a112a0
handtrix <handtrix@gmail.com>
2019-08-24 16:36:32 +0200

    Remove _app from with-apollo example
    This allows automatic static optimization for pages, that don’t need apollo.

--- examples/with-apollo/pages/index.js
+++ examples/with-apollo/pages/index.js
@@ -2,11 +2,14 @@ import App from '../components/App'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
+import withApolloClient from '../lib/with-apollo-client'
 
-export default () => (
+const IndexPage = props => (
   <App>
     <Header />
     <Submit />
     <PostList />
   </App>
 )
+
+export default withApolloClient(IndexPage)
