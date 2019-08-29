3b601e1702dbc3c42b761a1beefa76ba4871203b
handtrix <handtrix@gmail.com>
2019-08-29 17:47:03 +0200

    Remove getInitialProps from app
    Allow automatic static optimization

--- examples/with-loading/pages/_app.js
+++ examples/with-loading/pages/_app.js
@@ -12,16 +12,6 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 export default class MyApp extends App {
-  static async getInitialProps ({ Component, router, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render () {
     const { Component, pageProps } = this.props
     return (
