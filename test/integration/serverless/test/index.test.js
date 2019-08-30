e5fd72ced1439fd86d33cd94b7b07d731bf1d377
Joe Haddad <joe.haddad@zeit.co>
2019-08-30 00:10:52 -0400

    Fix Serverless Chunking

--- test/integration/serverless/test/index.test.js
+++ test/integration/serverless/test/index.test.js
@@ -2,7 +2,7 @@
 /* global jasmine */
 import webdriver from 'next-webdriver'
 import { join } from 'path'
-import { existsSync } from 'fs'
+import { existsSync, readdirSync, readFileSync } from 'fs'
 import {
   killApp,
   findPort,
@@ -15,6 +15,8 @@ import fetch from 'node-fetch'
 
 const appDir = join(__dirname, '../')
 const serverlessDir = join(appDir, '.next/serverless/pages')
+const chunksDir = join(appDir, '.next/static/chunks')
+const buildIdFile = join(appDir, '.next/BUILD_ID')
 let appPort
 let app
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
@@ -96,6 +98,19 @@ describe('Serverless', () => {
     } finally {
       await browser.close()
     }
+  })
+
+  it('should not have combined client-side chunks', () => {
+    expect(readdirSync(chunksDir).length).toBeGreaterThanOrEqual(2)
+    const buildId = readFileSync(buildIdFile, 'utf8').trim()
+
+    const pageContent = join(
+      appDir,
+      '.next/static',
+      buildId,
+      'pages/dynamic.js'
+    )
+    expect(readFileSync(pageContent, 'utf8')).not.toContain('Hello!')
   })
 
   it('should not output _app.js and _document.js to serverless build', () => {
