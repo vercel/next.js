b022e8a3242999163a2a13f3b508c9a06cf681c7
Elliott Wagener <u6053577@anu.edu.au>
2019-08-31 21:01:14 +1000

    Resolve absolute import paths in the 'bar' package

--- examples/with-yarn-workspaces/packages/web-app/next.config.js
+++ examples/with-yarn-workspaces/packages/web-app/next.config.js
@@ -1,7 +1,18 @@
 const withTM = require('next-transpile-modules')
+const path = require('path')
 
 // Tell webpack to compile the "bar" package
 // https://www.npmjs.com/package/next-transpile-modules
 module.exports = withTM({
-  transpileModules: ['bar']
+  transpileModules: ['bar'],
+
+  // Add this if you want webpack to resolve the absolute import
+  // paths that are internal to the transpiled module
+  // (e.g. if you had the "paths" option set in bar/tsconfig.json)
+  webpack: function(config) {
+    const rootDirOfEntireProject = path.resolve('../../')
+    const bar = path.join(rootDirOfEntireProject, 'node_modules/bar')
+    config.resolve.modules.push(bar)
+    return config
+  }
 })
