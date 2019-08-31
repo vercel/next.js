a19389a455b3fb8facd95d79315cf912989eac52
Eltyo <elliott.wagener@hotmail.com>
2019-08-31 21:42:36 +1000

    Fixed lint error

--- examples/with-yarn-workspaces/packages/web-app/next.config.js
+++ examples/with-yarn-workspaces/packages/web-app/next.config.js
@@ -9,7 +9,7 @@ module.exports = withTM({
   // Add this if you want webpack to resolve the absolute import
   // paths that are internal to the transpiled module
   // (e.g. if you had the "paths" option set in bar/tsconfig.json)
-  webpack: function(config) {
+  webpack: function (config) {
     const rootDirOfEntireProject = path.resolve('../../')
     const bar = path.join(rootDirOfEntireProject, 'node_modules/bar')
     config.resolve.modules.push(bar)
