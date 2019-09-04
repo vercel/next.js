cba5c05b36b05da77760b38d93303a1f18d5b2b5
Henrik Wenz <HaNdTriX@users.noreply.github.com>
2019-09-04 18:46:39 +0200

    Fix getInitialProps issue in with-apollo example (#8620)
    * Fix getInitialProps in with-apollo example
    
    The code before had two design flaws:
    
    * When we skip WithApollo.getInitialProps we must hoist PageComponent.getInitialProps if it is present.
    * We should expose the apolloClient to underlying PageComponent.getInitialProps contexts.
    
    * Add abort check
    
    * Add some comments
    
    * Add client only example page
    
    https://github.com/zeit/next.js/pull/8620#issuecomment-527870886
    
    * Remove connectToDevTools setting in favor default config
    
    https://github.com/apollographql/apollo-client/blob/7eaf4132cd2cd6244260777799406aaa03fcf377/packages/apollo-client/src/ApolloClient.ts#L170-L173
    
    * Remove fetch check
    
    This is done by https://www.npmjs.com/package/isomorphic-unfetch
    
    * Remove apollo-boost
    
    I am removing this package, because we never actually used it.
    This is because we use the named export of apollo boost wich resolves to apollo-client.
    
    This way we removed apollo-link-state, apollo-link-error
    
    * Remove redirect code from the client

--- examples/with-apollo/lib/apollo.js
+++ examples/with-apollo/lib/apollo.js
@@ -1,7 +1,9 @@
 import React, { useMemo } from 'react'
 import Head from 'next/head'
 import { ApolloProvider } from '@apollo/react-hooks'
-import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
 import fetch from 'isomorphic-unfetch'
 
 let apolloClient = null
@@ -39,50 +41,60 @@ export function withApollo (PageComponent, { ssr = true } = {}) {
     WithApollo.displayName = `withApollo(${displayName})`
   }
 
-  // Allow Next.js to remove getInitialProps from the browser build
-  if (typeof window === 'undefined') {
-    if (ssr) {
-      WithApollo.getInitialProps = async ctx => {
-        const { AppTree } = ctx
+  if (ssr || PageComponent.getInitialProps) {
+    WithApollo.getInitialProps = async ctx => {
+      const { AppTree } = ctx
 
-        let pageProps = {}
-        if (PageComponent.getInitialProps) {
-          pageProps = await PageComponent.getInitialProps(ctx)
+      // Initialize ApolloClient, add it to the ctx object so
+      // we can use it in `PageComponent.getInitialProp`.
+      const apolloClient = (ctx.apolloClient = initApolloClient())
+
+      // Run wrapped getInitialProps methods
+      let pageProps = {}
+      if (PageComponent.getInitialProps) {
+        pageProps = await PageComponent.getInitialProps(ctx)
+      }
+
+      // Only on the server:
+      if (typeof window === 'undefined') {
+        // When redirecting, the response is finished.
+        // No point in continuing to render
+        if (ctx.res && ctx.res.finished) {
+          return pageProps
         }
 
-        // Run all GraphQL queries in the component tree
-        // and extract the resulting data
-        const apolloClient = initApolloClient()
+        // Only if ssr is enabled
+        if (ssr) {
+          try {
+            // Run all GraphQL queries
+            const { getDataFromTree } = await import('@apollo/react-ssr')
+            await getDataFromTree(
+              <AppTree
+                pageProps={{
+                  ...pageProps,
+                  apolloClient
+                }}
+              />
+            )
+          } catch (error) {
+            // Prevent Apollo Client GraphQL errors from crashing SSR.
+            // Handle them in components via the data.error prop:
+            // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
+            console.error('Error while running `getDataFromTree`', error)
+          }
 
-        try {
-          // Run all GraphQL queries
-          const { getDataFromTree } = await import('@apollo/react-ssr')
-          await getDataFromTree(
-            <AppTree
-              pageProps={{
-                ...pageProps,
-                apolloClient
-              }}
-            />
-          )
-        } catch (error) {
-          // Prevent Apollo Client GraphQL errors from crashing SSR.
-          // Handle them in components via the data.error prop:
-          // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
-          console.error('Error while running `getDataFromTree`', error)
+          // getDataFromTree does not call componentWillUnmount
+          // head side effect therefore need to be cleared manually
+          Head.rewind()
         }
+      }
 
-        // getDataFromTree does not call componentWillUnmount
-        // head side effect therefore need to be cleared manually
-        Head.rewind()
+      // Extract query data from the Apollo store
+      const apolloState = apolloClient.cache.extract()
 
-        // Extract query data from the Apollo store
-        const apolloState = apolloClient.cache.extract()
-
-        return {
-          ...pageProps,
-          apolloState
-        }
+      return {
+        ...pageProps,
+        apolloState
       }
     }
   }
@@ -116,15 +128,12 @@ function initApolloClient (initialState) {
  */
 function createApolloClient (initialState = {}) {
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
-  const isBrowser = typeof window !== 'undefined'
   return new ApolloClient({
-    connectToDevTools: isBrowser,
-    ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
+    ssrMode: typeof window === 'undefined', // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
       uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
       credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
-      // Use fetch() polyfill on the server
-      fetch: !isBrowser && fetch
+      fetch
     }),
     cache: new InMemoryCache().restore(initialState)
   })
