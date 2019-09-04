64b74fdd64280b65190888fa404706be355cef62
handtrix <handtrix@gmail.com>
2019-09-04 14:44:24 +0200

    Add client only example page
    https://github.com/zeit/next.js/pull/8620#issuecomment-527870886

--- examples/with-apollo/pages/client-only.js
+++ examples/with-apollo/pages/client-only.js
@@ -5,24 +5,27 @@ import Submit from '../components/Submit'
 import PostList from '../components/PostList'
 import { withApollo } from '../lib/apollo'
 
-const IndexPage = props => (
+const ClientOnlyPage = props => (
   <App>
     <Header />
     <InfoBox>
-      ℹ️ This example shows how to fetch all initial apollo queries on the
-      server. If you <a href='/'>reload</a> this page you won't see a loader
-      since Apollo fetched all needed data on the server. This prevents{' '}
+      ℹ️ This example shows how to disable apollos query fetching on the server.
+      If you <a href='/client-only'>reload</a> this page, you will see a loader
+      since Apollo didn't fetch any data on the server. This allows{' '}
       <a
         href='https://nextjs.org/blog/next-9#automatic-static-optimization'
         target='_blank'
       >
         automatic static optimization
-      </a>{' '}
-      in favour of full Server-Side-Rendering.
+      </a>
+      .
     </InfoBox>
     <Submit />
     <PostList />
   </App>
 )
 
-export default withApollo(IndexPage)
+export default withApollo(ClientOnlyPage, {
+  // Disable apollo ssr fetching in favour of automatic static optimization
+  ssr: false
+})
