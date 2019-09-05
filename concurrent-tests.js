e9701181c30be3e1e6a8abe53b201fcca560fb72
JJ Kasper <jj@jjsweb.site>
2019-09-05 17:24:47 -0500

    Add cleaning test files and windows handling

--- concurrent-tests.js
+++ concurrent-tests.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const _glob = require('glob')
+const fs = require('fs-extra')
 const { promisify } = require('util')
 const { Sema } = require('async-sema')
-const { spawn } = require('child_process')
+const { spawn, exec: execOrig } = require('child_process')
+
 const glob = promisify(_glob)
+const exec = promisify(execOrig)
 
 const NUM_RETRIES = 2
 const DEFAULT_CONCURRENCY = 2
@@ -38,17 +41,24 @@ const DEFAULT_CONCURRENCY = 2
 
   console.log('Found tests:', testNames)
   const sema = new Sema(concurrency, { capacity: testNames.length })
+  const jestPath = path.join(
+    path.dirname(require.resolve('jest-cli/package')),
+    'bin/jest.js'
+  )
+  const children = new Set()
 
   const runTest = (test = '') =>
     new Promise((resolve, reject) => {
       const child = spawn(
-        'yarn',
-        ['jest', '--runInBand', '--forceExit', '--verbose', test],
+        'node',
+        [jestPath, '--runInBand', '--forceExit', '--verbose', test],
         {
           stdio: 'inherit'
         }
       )
+      children.add(child)
       child.on('exit', code => {
+        children.delete(child)
         if (code) reject(new Error(`failed with code: ${code}`))
         resolve()
       })
@@ -58,15 +68,45 @@ const DEFAULT_CONCURRENCY = 2
     testNames.map(async test => {
       await sema.acquire()
       let passed = false
+      const getTestFiles = () =>
+        glob('**/*', {
+          nodir: true,
+          cwd: path.join(__dirname, test),
+          dot: true
+        })
+      const testFiles = new Set(await getTestFiles())
+
       for (let i = 0; i < NUM_RETRIES; i++) {
         try {
           await runTest(test)
           passed = true
           break
-        } catch (err) {}
+        } catch (err) {
+          if (i < NUM_RETRIES - 1) {
+            try {
+              console.log('Cleaning test files for', test)
+              const curFiles = await getTestFiles()
+
+              for (const file of curFiles) {
+                if (!testFiles.has(file)) {
+                  await fs.remove(path.join(__dirname, test, file))
+                }
+              }
+              for (const file of testFiles) {
+                try {
+                  await exec(
+                    `git checkout "${path.join(__dirname, test, file)}"`
+                  )
+                } catch (err) {}
+              }
+            } catch (err) {}
+          }
+        }
       }
       if (!passed) {
-        throw new Error(`${test} failed to pass within ${NUM_RETRIES} retries`)
+        console.error(`${test} failed to pass within ${NUM_RETRIES} retries`)
+        children.forEach(child => child.kill())
+        process.exit(1)
       }
       sema.release()
     })
