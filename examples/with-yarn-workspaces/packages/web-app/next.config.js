72e317d3c5778aaddac4329ada1cbcf6e6dbfd8d
Lukáš Huvar <lukas@huvar.cz>
2019-09-05 09:58:29 +0200

    Fix next.config.js

--- examples/with-yarn-workspaces/packages/web-app/next.config.js
+++ examples/with-yarn-workspaces/packages/web-app/next.config.js
@@ -3,5 +3,6 @@ const withTM = require('next-transpile-modules')
 // Tell webpack to compile the "bar" package
 // https://www.npmjs.com/package/next-transpile-modules
 module.exports = withTM({
+  target: 'serverless',
   transpileModules: ['bar']
 })
