140c163de3f1bc32acedad9a738d9e7842ffa39a
JJ Kasper <jj@jjsweb.site>
2019-09-05 23:34:04 -0500

    Add test for warning in development

--- test/integration/duplicate-pages/test/index.test.js
+++ test/integration/duplicate-pages/test/index.test.js
@@ -2,14 +2,43 @@
 /* global jasmine */
 import path from 'path'
 
-import { nextBuild } from 'next-test-utils'
+import {
+  nextBuild,
+  findPort,
+  launchApp,
+  renderViaHTTP,
+  killApp,
+  waitFor
+} from 'next-test-utils'
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 1
 const appDir = path.join(__dirname, '..')
 
 describe('Handles Duplicate Pages', () => {
-  it('Throws an error during build', async () => {
-    const { stderr } = await nextBuild(appDir, [], { stderr: true })
-    expect(stderr).toContain('Duplicate page detected')
+  describe('production', () => {
+    it('Throws an error during build', async () => {
+      const { stderr } = await nextBuild(appDir, [], { stderr: true })
+      expect(stderr).toContain('Duplicate page detected')
+    })
+  })
+
+  describe('dev mode', () => {
+    it('Shows warning in development', async () => {
+      let output
+      const handleOutput = msg => {
+        output += msg
+      }
+      const appPort = await findPort()
+      const app = await launchApp(appDir, appPort, {
+        onStdout: handleOutput,
+        onStderr: handleOutput
+      })
+      await renderViaHTTP(appPort, '/hello')
+      await waitFor(1000)
+      await killApp(app)
+      expect(output).toMatch(
+        /Duplicate page detected\. The following files both resolve to/
+      )
+    })
   })
 })
