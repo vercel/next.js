dadf4598a47fae4d4ab9df991da2b8ec73de78dd
Weston Thayer <weston@cryclops.com>
2019-09-09 14:56:34 -0700

    Remove accidental mount throw on test 8

--- examples/with-sentry-simple/pages/client/test8.js
+++ examples/with-sentry-simple/pages/client/test8.js
@@ -1,24 +1,16 @@
 import React from 'react'
 
-const Test8 = () => {
-  React.useEffect(async () => {
-    const doAsyncWork = () => Promise.reject(new Error('Client Test 8'))
-    const result = await doAsyncWork()
-    console.log(result)
-  }, [])
-
-  return (
-    <React.Fragment>
-      <h1>Client Test 8</h1>
-      <button
-        onClick={() => {
-          throw new Error('Client Test 8')
-        }}
-      >
-        Click me to throw an Error
-      </button>
-    </React.Fragment>
-  )
-}
+const Test8 = () => (
+  <React.Fragment>
+    <h1>Client Test 8</h1>
+    <button
+      onClick={() => {
+        throw new Error('Client Test 8')
+      }}
+    >
+      Click me to throw an Error
+    </button>
+  </React.Fragment>
+)
 
 export default Test8
