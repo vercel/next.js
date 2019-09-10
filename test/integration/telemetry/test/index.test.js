0b1bfca7f2db264adf8e6f2a771623c7d9f40389
Joe Haddad <timer150@gmail.com>
2019-09-10 12:21:29 -0400

    Apply suggestions from code review
    Co-Authored-By: JJ Kasper <jj@jjsweb.site>

--- test/integration/telemetry/test/index.test.js
+++ test/integration/telemetry/test/index.test.js
@@ -2,13 +2,12 @@
 /* global jasmine */
 import { runNextCommand } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 
 describe('Telemetry CLI', () => {
   it('can print telemetry status', async () => {
     const { stdout } = await runNextCommand(['telemetry'], {
       stdout: true,
-      stderr: true
     })
     expect(stdout).toMatch(/Status: .*/)
   })
@@ -16,7 +15,6 @@ describe('Telemetry CLI', () => {
   it('can enable telemetry', async () => {
     const { stdout } = await runNextCommand(['telemetry', '--enable'], {
       stdout: true,
-      stderr: true
     })
     expect(stdout).toMatch(/Success/)
     expect(stdout).toMatch(/Status: Enabled/)
@@ -25,7 +23,6 @@ describe('Telemetry CLI', () => {
   it('can disable telemetry', async () => {
     const { stdout } = await runNextCommand(['telemetry', '--disable'], {
       stdout: true,
-      stderr: true
     })
     expect(stdout).toMatch(/Your preference has been saved/)
     expect(stdout).toMatch(/Status: Disabled/)
