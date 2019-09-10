f30e7ddff9e4a7552fafda14d13336cf99e8b6ea
Joe Haddad <joe.haddad@zeit.co>
2019-09-10 12:17:08 -0400

    Add telemetry CLI tests

---
+++ test/integration/telemetry/test/index.test.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+/* global jasmine */
+import { runNextCommand } from 'next-test-utils'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+
+describe('Telemetry CLI', () => {
+  it('can print telemetry status', async () => {
+    const { stdout } = await runNextCommand(['telemetry'], {
+      stdout: true,
+      stderr: true
+    })
+    expect(stdout).toMatch(/Status: .*/)
+  })
+
+  it('can enable telemetry', async () => {
+    const { stdout } = await runNextCommand(['telemetry', '--enable'], {
+      stdout: true,
+      stderr: true
+    })
+    expect(stdout).toMatch(/Success/)
+    expect(stdout).toMatch(/Status: Enabled/)
+  })
+
+  it('can disable telemetry', async () => {
+    const { stdout } = await runNextCommand(['telemetry', '--disable'], {
+      stdout: true,
+      stderr: true
+    })
+    expect(stdout).toMatch(/Your preference has been saved/)
+    expect(stdout).toMatch(/Status: Disabled/)
+  })
+})
