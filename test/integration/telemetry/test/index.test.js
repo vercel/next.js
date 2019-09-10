22915edb3b09b55b0bcfc40e9ff8296b0ecf1e8c
Joe Haddad <joe.haddad@zeit.co>
2019-09-10 12:24:35 -0400

    Update index.test.js

--- test/integration/telemetry/test/index.test.js
+++ test/integration/telemetry/test/index.test.js
@@ -7,14 +7,14 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 describe('Telemetry CLI', () => {
   it('can print telemetry status', async () => {
     const { stdout } = await runNextCommand(['telemetry'], {
-      stdout: true,
+      stdout: true
     })
     expect(stdout).toMatch(/Status: .*/)
   })
 
   it('can enable telemetry', async () => {
     const { stdout } = await runNextCommand(['telemetry', '--enable'], {
-      stdout: true,
+      stdout: true
     })
     expect(stdout).toMatch(/Success/)
     expect(stdout).toMatch(/Status: Enabled/)
@@ -22,7 +22,7 @@ describe('Telemetry CLI', () => {
 
   it('can disable telemetry', async () => {
     const { stdout } = await runNextCommand(['telemetry', '--disable'], {
-      stdout: true,
+      stdout: true
     })
     expect(stdout).toMatch(/Your preference has been saved/)
     expect(stdout).toMatch(/Status: Disabled/)
