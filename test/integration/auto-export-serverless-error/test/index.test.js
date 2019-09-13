cda161b550e5a8b4c533f2ae36a96ae0a0271748
Joe Haddad <joe.haddad@zeit.co>
2019-09-13 18:52:11 -0400

    Update index.test.js

--- test/integration/auto-export-serverless-error/test/index.test.js
+++ test/integration/auto-export-serverless-error/test/index.test.js
@@ -8,7 +8,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 1
 const appDir = path.join(__dirname, '..')
 
 describe('Auto Export Error Serverless', () => {
-  it('Refreshes query on mount', async () => {
+  it('fails to emit the page', async () => {
     await nextBuild(appDir)
 
     expect(
