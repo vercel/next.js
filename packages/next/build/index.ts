91a865371c843cc644c7517237e9a4ceb5e56d9f
Joe Haddad <joe.haddad@zeit.co>
2019-09-20 13:26:44 -0400

    Rename variable for clarity

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -273,7 +273,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
   const staticPages = new Set<string>()
   const invalidPages = new Set<string>()
   const hybridAmpPages = new Set<string>()
-  const prerenderRoutes = new Map<string, Array<string>>()
+  const additionalSprPaths = new Map<string, Array<string>>()
   const pageInfos = new Map<string, PageInfo>()
   const pagesManifest = JSON.parse(await fsReadFile(manifestPath, 'utf8'))
   const buildManifest = JSON.parse(await fsReadFile(buildManifestPath, 'utf8'))
@@ -361,7 +361,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
             sprPages.add(page)
 
             if (result.prerenderRoutes) {
-              prerenderRoutes.set(page, result.prerenderRoutes)
+              additionalSprPaths.set(page, result.prerenderRoutes)
             }
           }
 
@@ -424,7 +424,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
       ...config,
       revalidations: {},
       exportPathMap: (defaultMap: any) => {
-        prerenderRoutes.forEach((routes, page) => {
+        additionalSprPaths.forEach((routes, page) => {
           // remove /blog/[post] from being exported itself
           if (isDynamicRoute(page)) {
             delete defaultMap[page]
@@ -491,7 +491,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
             revalidate: exportConfig.revalidations[page],
           }
         }
-        const extraRoutes = prerenderRoutes.get(page)
+        const extraRoutes = additionalSprPaths.get(page)
         if (extraRoutes) {
           for (const route of extraRoutes) {
             await moveExportedPage(route, route, true)
