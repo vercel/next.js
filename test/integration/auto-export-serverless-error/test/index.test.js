d28e9211c498260a1c1829a6d48b38c415ee636a
Joe Haddad <joe.haddad@zeit.co>
2019-09-26 11:07:24 -0400

    Better test case for serverless export This checks that the program output actually claims there was a build error.

--- test/integration/auto-export-serverless-error/test/index.test.js
+++ test/integration/auto-export-serverless-error/test/index.test.js
@@ -9,10 +9,14 @@ const appDir = path.join(__dirname, '..')
 
 describe('Auto Export Error Serverless', () => {
   it('fails to emit the page', async () => {
-    await nextBuild(appDir)
+    const { stderr } = await nextBuild(appDir, [], {
+      stderr: true
+    })
 
     expect(
       fs.existsSync(path.join(appDir, '.next/serverless/pages/index.html'))
     ).toBe(false)
+    expect(stderr).toContain('ReferenceError')
+    expect(stderr).toContain('Build error occurred')
   })
 })
