8217eecfd868aa564e6777ee1209209418f6a4b0
arya <aryasadeghy@gmail.com>
2019-09-29 18:54:52 +0330

    fix  code  lints

--- examples/with-quill-js/pages/index.js
+++ examples/with-quill-js/pages/index.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'
+import 'react-quill/dist/quill.snow.css'
+
 const QuillNoSSRWrapper = dynamic(
-    import('react-quill'), {ssr: false, loading: () => <p>Loading ...</p>}
-);
-import 'react-quill/dist/quill.snow.css';
+    import('react-quill'), { ssr: false, loading: () => <p>Loading ...</p> }
+)
 
 const modules = {
     toolbar: [
-        [{'header': '1'}, {'header': '2'}, {'font': []}],
-        [{size: []}],
+        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+        [{ size: [] }],
         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{'list': 'ordered'}, {'list': 'bullet'},
-            {'indent': '-1'}, {'indent': '+1'}],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' },
+        { 'indent': '-1' }, { 'indent': '+1' }],
         ['link', 'image', 'video'],
         ['clean']
     ],
     clipboard: {
         // toggle to add extra line breaks when pasting HTML:
         matchVisual: false,
-    },
-
-};
+     }
+ };
 /*
  * Quill editor formats
  * See https://quilljs.com/docs/formats/
