0547855c1d6ee6e11c164c447b7aaca3a8768466
arya <aryasadeghy@gmail.com>
2019-09-29 18:58:46 +0330

    imporve lin

--- examples/with-quill-js/pages/index.js
+++ examples/with-quill-js/pages/index.js
@@ -7,38 +7,38 @@ const QuillNoSSRWrapper = dynamic(
 )
 
 const modules = {
-    toolbar: [
-        [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-        [{ size: [] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' },
-        { 'indent': '-1' }, { 'indent': '+1' }],
-        ['link', 'image', 'video'],
-        ['clean']
-    ],
-    clipboard: {
-        // toggle to add extra line breaks when pasting HTML:
-        matchVisual: false,
-     }
+  toolbar: [
+    [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' },
+    { 'indent': '-1' }, { 'indent': '+1' }],
+    ['link', 'image', 'video'],
+    ['clean']
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  }
  };
 /*
  * Quill editor formats
  * See https://quilljs.com/docs/formats/
  */
 const formats = [
-    'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image', 'video'
+  'header', 'font', 'size',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image', 'video'
 ];
 export default class App extends React.Component {
-    render() {
-        return (
-            <QuillNoSSRWrapper
-                modules={modules}
-                formats={formats}
-                theme="snow"
-            />
+  render() {
+    return (
+        <QuillNoSSRWrapper
+            modules={modules}
+            formats={formats}
+            theme="snow"
+        />
         )
     }
 }
