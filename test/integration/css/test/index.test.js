a187ec397516386e77a6a4afc9e335e0e9d7cf9e
Joe Haddad <joe.haddad@zeit.co>
2019-10-02 14:57:03 -0400

    Add test for when unused

--- test/integration/css/test/index.test.js
+++ test/integration/css/test/index.test.js
@@ -325,6 +325,39 @@ describe('CSS Support', () => {
     })
   })
 
+  describe('Body is not hidden when unused in Development', () => {
+    const appDir = join(fixturesDir, 'unused')
+
+    beforeAll(async () => {
+      await remove(join(appDir, '.next'))
+    })
+
+    let appPort
+    let app
+    beforeAll(async () => {
+      appPort = await findPort()
+      app = await launchApp(appDir, appPort)
+    })
+    afterAll(async () => {
+      await killApp(app)
+    })
+
+    it('should have body visible', async () => {
+      let browser
+      try {
+        browser = await webdriver(appPort, '/')
+        const currentDisplay = await browser.eval(
+          `window.getComputedStyle(document.querySelector('body')).display`
+        )
+        expect(currentDisplay).toBe('block')
+      } finally {
+        if (browser) {
+          await browser.close()
+        }
+      }
+    })
+  })
+
   describe('Has CSS in computed styles in Production', () => {
     const appDir = join(fixturesDir, 'multi-page')
 
