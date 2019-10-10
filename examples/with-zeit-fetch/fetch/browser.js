9ff7339e0d55fafc68bd39d01ad1e57b99c38366
Tim Neutkens <tim@timneutkens.nl>
2019-10-10 20:30:04 +0200

    Add @zeit/fetch example (#9029)

---
+++ examples/with-zeit-fetch/fetch/browser.js
@@ -0,0 +1,3 @@
+import fetch from 'isomorphic-fetch'
+
+export default fetch
---
+++ examples/with-zeit-fetch/pages/preact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+import fetch from '../fetch'
+
+function Preact (props) {
+  return (
+    <div>
+      <p>Preact has {props.stars} ⭐</p>
+      <Link href='/'>
+        <a>I bet Next.js has more stars (?)</a>
+      </Link>
+    </div>
+  )
+}
+
+Preact.getInitialProps = async () => {
+  const res = await fetch('https://api.github.com/repos/developit/preact')
+  const json = await res.json() // better use it inside try .. catch
+  return { stars: json.stargazers_count }
+}
+
+export default Preact
