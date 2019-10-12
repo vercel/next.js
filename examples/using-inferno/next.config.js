ece656f740bf7334b527ef4ddedb2c66287d712c
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-10-12 15:51:17 -0300

    Fix linter errors

--- examples/using-inferno/next.config.js
+++ examples/using-inferno/next.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  webpack: function(config, { dev }) {
+  webpack: function (config, { dev }) {
     // For the development version, we'll use React.
     // Because, it support react hot loading and so on.
     if (dev) {
@@ -12,9 +12,9 @@ module.exports = {
       ...config.resolve.alias,
       react: path.resolve('./lib/inferno-compat.js'),
       'react-dom': path.resolve('./lib/inferno-compat.js'),
-      'react-dom/server': 'inferno-server',
+      'react-dom/server': 'inferno-server'
     }
 
     return config
-  },
+  }
 }
