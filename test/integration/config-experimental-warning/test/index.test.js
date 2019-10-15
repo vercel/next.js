2b82ffde74de694eb1fec22ea31700243685d698
JJ Kasper <jj@jjsweb.site>
2019-10-15 16:35:04 -0500

    Migrate config-experimental-warning tests

--- test/integration/config-experimental-warning/test/index.test.js
+++ test/integration/config-experimental-warning/test/index.test.js
@@ -1,62 +1,60 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { join } from 'path'
 import { nextBuild, File } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 30
 const appDir = join(__dirname, '..')
 const configFile = new File(join(appDir, '/next.config.js'))
 
-describe('Promise in next config', () => {
-  afterAll(() => configFile.delete())
+fixture('Promise in next config').after(() => configFile.delete())
 
-  it('should not show warning with default config from function', async () => {
-    configFile.write(`
-      module.exports = (phase, { defaultConfig }) => {
-        return {
-          target: 'server',
-          ...defaultConfig,
-        }
-      }
-    `)
-
-    const { stderr } = await nextBuild(appDir, [], { stderr: true })
-    expect(stderr).not.toMatch(/experimental feature/)
-  })
-
-  it('should not show warning with config from object', async () => {
-    configFile.write(`
-      module.exports = {
-        target: 'server'
-      }
-    `)
-    const { stderr } = await nextBuild(appDir, [], { stderr: true })
-    expect(stderr).not.toMatch(/experimental feature/)
-  })
-
-  it('should show warning with config from object with experimental', async () => {
-    configFile.write(`
-      module.exports = {
+test('should not show warning with default config from function', async t => {
+  configFile.write(`
+    module.exports = (phase, { defaultConfig }) => {
+      return {
         target: 'server',
-        experimental: {
-          something: true
-        }
+        ...defaultConfig,
       }
-    `)
-    const { stderr } = await nextBuild(appDir, [], { stderr: true })
-    expect(stderr).toMatch(/experimental feature/)
-  })
+    }
+  `)
 
-  it('should show warning with config from function with experimental', async () => {
-    configFile.write(`
-      module.exports = (phase) => ({
-        target: 'server',
-        experimental: {
-          something: true
-        }
-      })
-    `)
-    const { stderr } = await nextBuild(appDir, [], { stderr: true })
-    expect(stderr).toMatch(/experimental feature/)
-  })
+  const { stderr } = await nextBuild(appDir, [], { stderr: true })
+  await t.expect(stderr).notMatch(/experimental feature/)
+})
+
+test('should not show warning with config from object', async t => {
+  configFile.write(`
+    module.exports = {
+      target: 'server'
+    }
+  `)
+  const { stderr } = await nextBuild(appDir, [], { stderr: true })
+  await t.expect(stderr).notMatch(/experimental feature/)
+})
+
+test('should show warning with config from object with experimental', async t => {
+  configFile.write(`
+    module.exports = {
+      target: 'server',
+      experimental: {
+        something: true
+      }
+    }
+  `)
+  const { stderr } = await nextBuild(appDir, [], { stderr: true })
+  await t.expect(stderr).match(/experimental feature/)
+})
+
+test('should show warning with config from function with experimental', async t => {
+  configFile.write(`
+    module.exports = (phase) => ({
+      target: 'server',
+      experimental: {
+        something: true
+      }
+    })
+  `)
+  const { stderr } = await nextBuild(appDir, [], { stderr: true })
+  await t.expect(stderr).match(/experimental feature/)
 })
