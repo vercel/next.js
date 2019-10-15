5524bedd8b2664ed6de84c7fe64d5cf385b9bf5a
JJ Kasper <jj@jjsweb.site>
2019-10-15 14:40:44 -0500

    Migrate cli tests

--- test/integration/cli/test/index.test.js
+++ test/integration/cli/test/index.test.js
@@ -1,147 +1,151 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { runNextCommand, runNextCommandDev, findPort } from 'next-test-utils'
 import { join } from 'path'
 import pkg from 'next/package'
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 
 const dir = join(__dirname, '..')
 
-describe('CLI Usage', () => {
-  describe('no command', () => {
-    test('--help', async () => {
-      const help = await runNextCommand(['--help'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Usage/)
-    })
+fixture('CLI Usage')
 
-    test('-h', async () => {
-      const help = await runNextCommand(['-h'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Usage/)
-    })
+fixture('no command')
 
-    test('--version', async () => {
-      const output = await runNextCommand(['--version'], {
-        stdout: true
-      })
-      expect(output.stdout).toMatch(
-        new RegExp(`Next\\.js v${pkg.version.replace(/\./g, '\\.')}`)
-      )
-    })
-
-    test('-v', async () => {
-      const output = await runNextCommand(['--version'], {
-        stdout: true
-      })
-      expect(output.stdout).toMatch(
-        new RegExp(`Next\\.js v${pkg.version.replace(/\./g, '\\.')}`)
-      )
-    })
+test('--help', async t => {
+  const help = await runNextCommand(['--help'], {
+    stdout: true
   })
-  describe('build', () => {
-    test('--help', async () => {
-      const help = await runNextCommand(['build', '--help'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(
-        /Compiles the application for production deployment/
-      )
-    })
+  await t.expect(help.stdout).match(/Usage/)
+})
 
-    test('-h', async () => {
-      const help = await runNextCommand(['build', '-h'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(
-        /Compiles the application for production deployment/
-      )
-    })
+test('-h', async t => {
+  const help = await runNextCommand(['-h'], {
+    stdout: true
   })
+  await t.expect(help.stdout).match(/Usage/)
+})
 
-  describe('dev', () => {
-    test('--help', async () => {
-      const help = await runNextCommand(['dev', '--help'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Starts the application in development mode/)
-    })
-
-    test('-h', async () => {
-      const help = await runNextCommand(['dev', '-h'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Starts the application in development mode/)
-    })
-
-    test('custom directory', async () => {
-      const port = await findPort()
-      const output = await runNextCommandDev([dir, '--port', port], true)
-      expect(output).toMatch(/ready on/i)
-    })
-
-    test('--port', async () => {
-      const port = await findPort()
-      const output = await runNextCommandDev([dir, '--port', port], true)
-      expect(output).toMatch(new RegExp(`http://localhost:${port}`))
-    })
-
-    test('-p', async () => {
-      const port = await findPort()
-      const output = await runNextCommandDev([dir, '-p', port], true)
-      expect(output).toMatch(new RegExp(`http://localhost:${port}`))
-    })
-
-    test('--hostname', async () => {
-      const port = await findPort()
-      const output = await runNextCommandDev(
-        [dir, '--hostname', '0.0.0.0', '--port', port],
-        true
-      )
-      expect(output).toMatch(new RegExp(`http://0.0.0.0:${port}`))
-    })
-
-    test('-H', async () => {
-      const port = await findPort()
-      const output = await runNextCommandDev(
-        [dir, '-H', '0.0.0.0', '--port', port],
-        true
-      )
-      expect(output).toMatch(new RegExp(`http://0.0.0.0:${port}`))
-    })
+test('--version', async t => {
+  const output = await runNextCommand(['--version'], {
+    stdout: true
   })
+  await t
+    .expect(output.stdout)
+    .match(new RegExp(`Next\\.js v${pkg.version.replace(/\./g, '\\.')}`))
+})
 
-  describe('start', () => {
-    test('--help', async () => {
-      const help = await runNextCommand(['start', '--help'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Starts the application in production mode/)
-    })
-
-    test('-h', async () => {
-      const help = await runNextCommand(['start', '-h'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Starts the application in production mode/)
-    })
+test('-v', async t => {
+  const output = await runNextCommand(['--version'], {
+    stdout: true
   })
+  await t
+    .expect(output.stdout)
+    .match(new RegExp(`Next\\.js v${pkg.version.replace(/\./g, '\\.')}`))
+})
 
-  describe('export', () => {
-    test('--help', async () => {
-      const help = await runNextCommand(['export', '--help'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Exports the application/)
-    })
+fixture('build')
 
-    test('-h', async () => {
-      const help = await runNextCommand(['export', '-h'], {
-        stdout: true
-      })
-      expect(help.stdout).toMatch(/Exports the application/)
-    })
+test('--help', async t => {
+  const help = await runNextCommand(['build', '--help'], {
+    stdout: true
   })
+  await t
+    .expect(help.stdout)
+    .match(/Compiles the application for production deployment/)
+})
+
+test('-h', async t => {
+  const help = await runNextCommand(['build', '-h'], {
+    stdout: true
+  })
+  await t
+    .expect(help.stdout)
+    .match(/Compiles the application for production deployment/)
+})
+
+fixture('dev')
+test('--help', async t => {
+  const help = await runNextCommand(['dev', '--help'], {
+    stdout: true
+  })
+  await t
+    .expect(help.stdout)
+    .match(/Starts the application in development mode/)
+})
+
+test('-h', async t => {
+  const help = await runNextCommand(['dev', '-h'], {
+    stdout: true
+  })
+  await t
+    .expect(help.stdout)
+    .match(/Starts the application in development mode/)
+})
+
+test('custom directory', async t => {
+  const port = await findPort()
+  const output = await runNextCommandDev([dir, '--port', port], true)
+  await t.expect(output).match(/ready on/i)
+})
+
+test('--port', async t => {
+  const port = await findPort()
+  const output = await runNextCommandDev([dir, '--port', port], true)
+  await t.expect(output).match(new RegExp(`http://localhost:${port}`))
+})
+
+test('-p', async t => {
+  const port = await findPort()
+  const output = await runNextCommandDev([dir, '-p', port], true)
+  await t.expect(output).match(new RegExp(`http://localhost:${port}`))
+})
+
+test('--hostname', async t => {
+  const port = await findPort()
+  const output = await runNextCommandDev(
+    [dir, '--hostname', '0.0.0.0', '--port', port],
+    true
+  )
+  await t.expect(output).match(new RegExp(`http://0.0.0.0:${port}`))
+})
+
+test('-H', async t => {
+  const port = await findPort()
+  const output = await runNextCommandDev(
+    [dir, '-H', '0.0.0.0', '--port', port],
+    true
+  )
+  await t.expect(output).match(new RegExp(`http://0.0.0.0:${port}`))
+})
+
+fixture('start')
+
+test('--help', async t => {
+  const help = await runNextCommand(['start', '--help'], {
+    stdout: true
+  })
+  await t.expect(help.stdout).match(/Starts the application in production mode/)
+})
+
+test('-h', async t => {
+  const help = await runNextCommand(['start', '-h'], {
+    stdout: true
+  })
+  await t.expect(help.stdout).match(/Starts the application in production mode/)
+})
+
+fixture('export')
+
+test('--help', async t => {
+  const help = await runNextCommand(['export', '--help'], {
+    stdout: true
+  })
+  await t.expect(help.stdout).match(/Exports the application/)
+})
+
+test('-h', async t => {
+  const help = await runNextCommand(['export', '-h'], {
+    stdout: true
+  })
+  await t.expect(help.stdout).match(/Exports the application/)
 })
