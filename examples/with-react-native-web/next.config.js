5f961382564a5616616a74be3a3a66089518d66f
Brandon Bayer <b@bayer.ws>
2019-10-14 20:36:43 -0700

    fix react-native-web example by adding babel plugin

--- examples/with-react-native-web/next.config.js
+++ examples/with-react-native-web/next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  webpack: config => {
+  webpack: (config, { defaultLoaders }) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web'
     }
+    defaultLoaders.babel.options.plugins = [
+      ['react-native-web', { commonjs: true }]
+    ]
     return config
   }
 }
