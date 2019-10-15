94fc07abf69e81aa1464ebb84b227d890fc81280
JJ Kasper <jj@jjsweb.site>
2019-10-15 14:26:16 -0500

    Migrate build-indicator tests

--- test/integration/build-indicator/test/index.test.js
+++ test/integration/build-indicator/test/index.test.js
@@ -1,15 +1,13 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import fs from 'fs-extra'
 import { join } from 'path'
 import webdriver from 'next-webdriver'
 import { findPort, launchApp, killApp, waitFor } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 const appDir = join(__dirname, '..')
 const nextConfig = join(appDir, 'next.config.js')
-let appPort
-let app
 
 const installCheckVisible = browser => {
   return browser.eval(`(function() {
@@ -24,69 +22,67 @@ const installCheckVisible = browser => {
   })()`)
 }
 
-describe('Build Activity Indicator', () => {
-  describe('Enabled', () => {
-    beforeAll(async () => {
-      await fs.remove(nextConfig)
-      appPort = await findPort()
-      app = await launchApp(appDir, appPort)
-    })
-    afterAll(() => killApp(app))
+fixture('Build Activity Indicator')
 
-    it('Adds the build indicator container', async () => {
-      const browser = await webdriver(appPort, '/')
-      const html = await browser.eval('document.body.innerHTML')
-      expect(html).toMatch(/__next-build-watcher/)
-      await browser.close()
-    })
+fixture('Enabled')
+  .before(async ctx => {
+    await fs.remove(nextConfig)
+    ctx.appPort = await findPort()
+    ctx.app = await launchApp(appDir, ctx.appPort)
+  })
+  .after(ctx => killApp(ctx.app))
 
-    it('Shows the build indicator when a page is built during navigation', async () => {
-      const browser = await webdriver(appPort, '/')
-      await installCheckVisible(browser)
-      await browser.elementByCss('#to-a').click()
-      await waitFor(500)
-      const wasVisible = await browser.eval('window.showedBuilder')
-      expect(wasVisible).toBe(true)
-      await browser.close()
-    })
+test('Adds the build indicator container', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  const html = await browser.eval('document.body.innerHTML')
+  await t.expect(html).match(/__next-build-watcher/)
+  await browser.close()
+})
 
-    it('Shows build indicator when page is built from modifying', async () => {
-      const browser = await webdriver(appPort, '/b')
-      await installCheckVisible(browser)
-      const pagePath = join(appDir, 'pages/b.js')
-      const origContent = await fs.readFile(pagePath, 'utf8')
-      const newContent = origContent.replace('b', 'c')
+test('Shows the build indicator when a page is built during navigation', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  await installCheckVisible(browser)
+  await browser.elementByCss('#to-a').click()
+  await waitFor(500)
+  const wasVisible = await browser.eval('window.showedBuilder')
+  await t.expect(wasVisible).eql(true)
+  await browser.close()
+})
 
-      await fs.writeFile(pagePath, newContent, 'utf8')
-      await waitFor(500)
-      const wasVisible = await browser.eval('window.showedBuilder')
+test('Shows build indicator when page is built from modifying', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/b')
+  await installCheckVisible(browser)
+  const pagePath = join(appDir, 'pages/b.js')
+  const origContent = await fs.readFile(pagePath, 'utf8')
+  const newContent = origContent.replace('b', 'c')
 
-      expect(wasVisible).toBe(true)
-      await fs.writeFile(pagePath, origContent, 'utf8')
-      await browser.close()
-    })
+  await fs.writeFile(pagePath, newContent, 'utf8')
+  await waitFor(500)
+  const wasVisible = await browser.eval('window.showedBuilder')
+
+  await t.expect(wasVisible).eql(true)
+  await fs.writeFile(pagePath, origContent, 'utf8')
+  await browser.close()
+})
+
+fixture('Disabled with next.config.js')
+  .before(async ctx => {
+    await fs.writeFile(
+      nextConfig,
+      'module.exports = { devIndicators: { buildActivity: false } }',
+      'utf8'
+    )
+    ctx.appPort = await findPort()
+    ctx.app = await launchApp(appDir, ctx.appPort)
+  })
+  .after(async ctx => {
+    await killApp(ctx.app)
+    await fs.remove(nextConfig)
   })
 
-  describe('Disabled with next.config.js', () => {
-    beforeAll(async () => {
-      await fs.writeFile(
-        nextConfig,
-        'module.exports = { devIndicators: { buildActivity: false } }',
-        'utf8'
-      )
-      appPort = await findPort()
-      app = await launchApp(appDir, appPort)
-    })
-    afterAll(async () => {
-      await killApp(app)
-      await fs.remove(nextConfig)
-    })
-
-    it('Does not add the build indicator container', async () => {
-      const browser = await webdriver(appPort, '/')
-      const html = await browser.eval('document.body.innerHTML')
-      expect(html).not.toMatch(/__next-build-watcher/)
-      await browser.close()
-    })
-  })
+test('Does not add the build indicator container', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  const html = await browser.eval('document.body.innerHTML')
+  await t.expect(html).notMatch(/__next-build-watcher/)
+  await browser.close()
 })
