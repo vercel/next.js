a73712bf99dafbb8abffd057b17e49b822ba8444
JJ Kasper <jj@jjsweb.site>
2019-10-15 16:35:56 -0500

    Migrate config-promise-error tests

--- test/integration/config-promise-error/test/index.test.js
+++ test/integration/config-promise-error/test/index.test.js
@@ -1,52 +1,54 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { join } from 'path'
 import { runNextCommand, findPort, File } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 30
 const configFile = new File(join(__dirname, '../next.config.js'))
 
-describe('Promise in next config', () => {
-  afterAll(() => configFile.restore())
+fixture('Promise in next config').after(() => configFile.restore())
 
-  it('should throw error when a promise is return on config', async () => {
-    configFile.write(`
-      module.exports = (phase, { isServer }) => {
-        return new Promise((resolve) => {
-          resolve({ target: 'serverless' })
-        })
-      }
-    `)
+test('should throw error when a promise is return on config', async t => {
+  configFile.write(`
+    module.exports = (phase, { isServer }) => {
+      return new Promise((resolve) => {
+        resolve({ target: 'serverless' })
+      })
+    }
+  `)
 
-    const { stderr } = await runNextCommand(
-      ['dev', join(__dirname, '..'), '-p', await findPort()],
-      { stderr: true }
-    )
+  const { stderr } = await runNextCommand(
+    ['dev', join(__dirname, '..'), '-p', await findPort()],
+    { stderr: true }
+  )
 
-    expect(stderr).toMatch(
+  await t
+    .expect(stderr)
+    .match(
       /Error: > Promise returned in next config\. https:\/\/err\.sh\/zeit\/next\.js\/promise-in-next-config/
     )
-  })
+})
 
-  it('should warn when a promise is returned on webpack', async () => {
-    configFile.write(`
-      setTimeout(() => process.exit(0), 2 * 1000)
-      module.exports = (phase, { isServer }) => {
-        return {
-          webpack: async (config) => {
-            return config
-          }
+test('should warn when a promise is returned on webpack', async t => {
+  configFile.write(`
+    setTimeout(() => process.extest(0), 2 * 1000)
+    module.exports = (phase, { isServer }) => {
+      return {
+        webpack: async (config) => {
+          return config
         }
       }
-    `)
+    }
+  `)
 
-    const { stderr } = await runNextCommand(
-      ['dev', join(__dirname, '..'), '-p', await findPort()],
-      { stderr: true }
-    )
+  const { stderr } = await runNextCommand(
+    ['dev', join(__dirname, '..'), '-p', await findPort()],
+    { stderr: true }
+  )
 
-    expect(stderr).toMatch(
+  await t
+    .expect(stderr)
+    .match(
       /> Promise returned in next config\. https:\/\/err\.sh\/zeit\/next\.js\/promise-in-next-config/
     )
-  })
 })
