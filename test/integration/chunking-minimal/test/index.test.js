4b472d1f139d67c7ea0baba4fb18165e694a9b67
JJ Kasper <jj@jjsweb.site>
2019-10-15 14:34:06 -0500

    Migrate chunking-minimal tests

--- test/integration/chunking-minimal/test/index.test.js
+++ test/integration/chunking-minimal/test/index.test.js
@@ -1,33 +1,34 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { join } from 'path'
 import { readFile } from 'fs-extra'
 import { nextBuild } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 1
-
 const appDir = join(__dirname, '../')
-let buildId
 
-describe('Chunking (minimal)', () => {
-  beforeAll(async () => {
-    await nextBuild(appDir)
-    buildId = await readFile(join(appDir, '.next/BUILD_ID'), 'utf8')
-  })
+fixture('Chunking (minimal)').before(async ctx => {
+  await nextBuild(appDir)
+  ctx.buildId = await readFile(join(appDir, '.next/BUILD_ID'), 'utf8')
+})
 
-  it('should have an empty client-manifest', async () => {
-    const manifest = await readFile(
-      join(appDir, '.next/static', buildId, '_buildManifest.js'),
-      'utf8'
-    )
-    expect(manifest).not.toMatch(/\.js/)
-  })
+test('should have an empty client-manifest', async t => {
+  const manifest = await readFile(
+    join(appDir, '.next/static', t.fixtureCtx.buildId, '_buildManifest.js'),
+    'utf8'
+  )
+  await t.expect(manifest).notMatch(/\.js/)
+})
 
-  it('should have an empty modern client-manifest', async () => {
-    const manifest = await readFile(
-      join(appDir, '.next/static', buildId, '_buildManifest.module.js'),
-      'utf8'
-    )
-    expect(manifest).not.toMatch(/\.js/)
-  })
+test('should have an empty modern client-manifest', async t => {
+  const manifest = await readFile(
+    join(
+      appDir,
+      '.next/static',
+      t.fixtureCtx.buildId,
+      '_buildManifest.module.js'
+    ),
+    'utf8'
+  )
+  await t.expect(manifest).notMatch(/\.js/)
 })
