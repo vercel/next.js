d4d706fe4c4b0dcc6a41a9930c7c6de0f682d980
JJ Kasper <jj@jjsweb.site>
2019-10-15 16:41:04 -0500

    Migrate create-next-app tests

--- test/integration/create-next-app/index.test.js
+++ test/integration/create-next-app/index.test.js
@@ -1,4 +1,5 @@
-/* eslint-env jest */
+/* global fixture, test */
+import 'testcafe'
 import mkdirpModule from 'mkdirp'
 import path from 'path'
 import fs from 'fs'
@@ -18,63 +19,62 @@ const cwd = path.join(
 
 const run = (...args) => execa('node', [cli, ...args], { cwd })
 
-describe('create next app', () => {
-  beforeAll(async () => {
-    jest.setTimeout(1000 * 30)
-    await mkdirp(cwd)
-  })
+fixture('create next app').before(async () => {
+  await mkdirp(cwd)
+})
 
-  it('non-empty directory', async () => {
-    const projectName = 'non-empty-directory'
+test('non-empty directory', async t => {
+  const projectName = 'non-empty-directory'
 
-    await mkdirp(path.join(cwd, projectName))
-    const pkg = path.join(cwd, projectName, 'package.json')
-    fs.writeFileSync(pkg, '{ "foo": "bar" }')
+  await mkdirp(path.join(cwd, projectName))
+  const pkg = path.join(cwd, projectName, 'package.json')
+  fs.writeFileSync(pkg, '{ "foo": "bar" }')
+  let errorMsg
 
-    expect.assertions(1)
-    try {
-      await run(projectName)
-    } catch (e) {
-      expect(e.stdout).toMatch(/contains files that could conflict/)
-    }
-  })
+  try {
+    await run(projectName)
+  } catch (e) {
+    errorMsg = e.stdout
+  }
+  await t.expect(errorMsg).match(/contains files that could conflict/)
+})
 
-  it('empty directory', async () => {
-    const projectName = 'empty-directory'
-    const res = await run(projectName)
+test('empty directory', async t => {
+  const projectName = 'empty-directory'
+  const res = await run(projectName)
 
-    expect(res.exitCode).toBe(0)
-    expect(
-      fs.existsSync(path.join(cwd, projectName, 'package.json'))
-    ).toBeTruthy()
-    expect(
-      fs.existsSync(path.join(cwd, projectName, 'pages/index.js'))
-    ).toBeTruthy()
-  })
+  await t.expect(res.exitCode).eql(0)
+  await t
+    .expect(fs.existsSync(path.join(cwd, projectName, 'package.json')))
+    .ok()
+  await t
+    .expect(fs.existsSync(path.join(cwd, projectName, 'pages/index.js')))
+    .ok()
+})
 
-  it('invalid example name', async () => {
-    const projectName = 'invalid-example-name'
-    expect.assertions(2)
-    try {
-      await run(projectName, '--example', 'not a real example')
-    } catch (e) {
-      expect(e.stderr).toMatch(/Could not locate an example named/i)
-    }
-    expect(
-      fs.existsSync(path.join(cwd, projectName, 'package.json'))
-    ).toBeFalsy()
-  })
+test('invalid example name', async t => {
+  const projectName = 'invalid-example-name'
+  let errorMsg
+  try {
+    await run(projectName, '--example', 'not a real example')
+  } catch (e) {
+    errorMsg = e.stderr
+  }
+  await t.expect(errorMsg).match(/Could not locate an example named/i)
+  await t
+    .expect(fs.existsSync(path.join(cwd, projectName, 'package.json')))
+    .notOk()
+})
 
-  it('valid example', async () => {
-    const projectName = 'valid-example'
-    const res = await run(projectName, '--example', 'basic-css')
-    expect(res.exitCode).toBe(0)
+test('valid example', async t => {
+  const projectName = 'valid-example'
+  const res = await run(projectName, '--example', 'basic-css')
+  await t.expect(res.exitCode).eql(0)
 
-    expect(
-      fs.existsSync(path.join(cwd, projectName, 'package.json'))
-    ).toBeTruthy()
-    expect(
-      fs.existsSync(path.join(cwd, projectName, 'pages/index.js'))
-    ).toBeTruthy()
-  })
+  await t
+    .expect(fs.existsSync(path.join(cwd, projectName, 'package.json')))
+    .ok()
+  await t
+    .expect(fs.existsSync(path.join(cwd, projectName, 'pages/index.js')))
+    .ok()
 })
