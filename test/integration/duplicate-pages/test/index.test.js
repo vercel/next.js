2ef93c98baddce59140744e1a75d3b5c71f540c6
JJ Kasper <jj@jjsweb.site>
2019-10-15 20:59:22 -0500

    Migrate duplicate-pages tests

--- test/integration/duplicate-pages/test/index.test.js
+++ test/integration/duplicate-pages/test/index.test.js
@@ -1,5 +1,6 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import path from 'path'
 
 import {
@@ -11,32 +12,31 @@ import {
   waitFor
 } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 1
 const appDir = path.join(__dirname, '..')
 
-describe('Handles Duplicate Pages', () => {
-  describe('production', () => {
-    it('Throws an error during build', async () => {
-      const { stdout } = await nextBuild(appDir, [], { stdout: true })
-      expect(stdout).toContain('Duplicate page detected')
-    })
-  })
+fixture('Handles Duplicate Pages')
 
-  describe('dev mode', () => {
-    it('Shows warning in development', async () => {
-      let output
-      const handleOutput = msg => {
-        output += msg
-      }
-      const appPort = await findPort()
-      const app = await launchApp(appDir, appPort, {
-        onStdout: handleOutput,
-        onStderr: handleOutput
-      })
-      await renderViaHTTP(appPort, '/hello')
-      await waitFor(3000)
-      await killApp(app)
-      expect(output).toMatch(/Duplicate page detected/)
-    })
+fixture('production')
+
+test('Throws an error during build', async t => {
+  const { stdout } = await nextBuild(appDir, [], { stdout: true })
+  await t.expect(stdout).contains('Duplicate page detected')
+})
+
+fixture('dev mode')
+
+test('Shows warning in development', async t => {
+  let output
+  const handleOutput = msg => {
+    output += msg
+  }
+  const appPort = await findPort()
+  const app = await launchApp(appDir, appPort, {
+    onStdout: handleOutput,
+    onStderr: handleOutput
   })
+  await renderViaHTTP(appPort, '/hello')
+  await waitFor(3000)
+  await killApp(app)
+  await t.expect(output).match(/Duplicate page detected/)
 })
