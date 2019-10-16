e806a895bb868b1befdd3d92409590a687c62643
JJ Kasper <jj@jjsweb.site>
2019-10-16 10:35:52 -0500

    Migrate relay-analytics test

--- test/integration/relay-analytics/test/index.test.js
+++ test/integration/relay-analytics/test/index.test.js
@@ -1,41 +1,38 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { join } from 'path'
 import webdriver from 'next-webdriver'
 import { killApp, findPort, launchApp } from 'next-test-utils'
 
 const appDir = join(__dirname, '../')
-let appPort
-let server
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 
-describe('Analytics relayer', () => {
-  beforeAll(async () => {
-    appPort = await findPort()
-    server = await launchApp(appDir, appPort)
+fixture('Analytics relayer')
+  .before(async ctx => {
+    ctx.appPort = await findPort()
+    ctx.server = await launchApp(appDir, ctx.appPort)
   })
-  afterAll(() => killApp(server))
+  .after(ctx => killApp(ctx.server))
 
-  it('Relays the data to user code', async () => {
-    const browser = await webdriver(appPort, '/')
-    await browser.waitForElementByCss('h1')
-    const h1Text = await browser.elementByCss('h1').text()
-    const data = parseFloat(
-      await browser.eval('localStorage.getItem("Next.js-hydration")')
-    )
-    const firstPaint = parseFloat(
-      await browser.eval('localStorage.getItem("first-paint")')
-    )
-    const firstContentfulPaint = parseFloat(
-      await browser.eval('localStorage.getItem("first-contentful-paint")')
-    )
-    expect(h1Text).toMatch(/Hello!/)
-    expect(data).not.toBeNaN()
-    expect(data).toBeGreaterThan(0)
-    expect(firstPaint).not.toBeNaN()
-    expect(firstPaint).toBeGreaterThan(0)
-    expect(firstContentfulPaint).not.toBeNaN()
-    expect(firstContentfulPaint).toBeGreaterThan(0)
-    await browser.close()
-  })
+test('Relays the data to user code', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  await browser.waitForElementByCss('h1')
+  const h1Text = await browser.elementByCss('h1').text()
+  const data = parseFloat(
+    await browser.eval('localStorage.getItem("Next.js-hydration")')
+  )
+  const firstPaint = parseFloat(
+    await browser.eval('localStorage.getItem("first-paint")')
+  )
+  const firstContentfulPaint = parseFloat(
+    await browser.eval('localStorage.getItem("first-contentful-paint")')
+  )
+  await t.expect(h1Text).match(/Hello!/)
+  await t.expect(isNaN(data)).notOk()
+  await t.expect(data > 0).ok()
+  await t.expect(isNaN(firstPaint)).notOk()
+  await t.expect(firstPaint > 0).ok()
+  await t.expect(isNaN(firstContentfulPaint)).notOk()
+  await t.expect(firstContentfulPaint > 0).ok()
+  await browser.close()
 })
