3371e9fd9797fe584df3b907d6bf487fc81d68e0
JJ Kasper <jj@jjsweb.site>
2019-10-15 21:36:50 -0500

    Migrate export-subfolders test

--- test/integration/export-subfolders/test/index.test.js
+++ test/integration/export-subfolders/test/index.test.js
@@ -1,39 +1,35 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import fs from 'fs'
 import { join } from 'path'
 import cheerio from 'cheerio'
 import { promisify } from 'util'
 import { nextBuild, nextExport } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 const readFile = promisify(fs.readFile)
 const access = promisify(fs.access)
 const appDir = join(__dirname, '../')
 const outdir = join(appDir, 'out')
 
-describe('Export config#exportTrailingSlash set to false', () => {
-  beforeAll(async () => {
-    await nextBuild(appDir)
-    await nextExport(appDir, { outdir })
-  })
+fixture('Export config#exportTrailingSlash set to false').before(async () => {
+  await nextBuild(appDir)
+  await nextExport(appDir, { outdir })
+})
 
-  it('should export pages as [filename].html instead of [filename]/index.html', async () => {
-    expect.assertions(6)
+test('should export pages as [filename].html instead of [filename]/index.html', async t => {
+  await t.expect(await access(join(outdir, 'index.html'))).eql(undefined)
+  await t.expect(await access(join(outdir, 'about.html'))).eql(undefined)
+  await t.expect(await access(join(outdir, 'posts.html'))).eql(undefined)
+  await t
+    .expect(await access(join(outdir, 'posts', 'single.html')))
+    .eql(undefined)
 
-    await expect(access(join(outdir, 'index.html'))).resolves.toBe(undefined)
-    await expect(access(join(outdir, 'about.html'))).resolves.toBe(undefined)
-    await expect(access(join(outdir, 'posts.html'))).resolves.toBe(undefined)
-    await expect(access(join(outdir, 'posts', 'single.html'))).resolves.toBe(
-      undefined
-    )
+  const html = await readFile(join(outdir, 'index.html'))
+  const $ = cheerio.load(html)
+  await t.expect($('p').text()).eql('I am a home page')
 
-    const html = await readFile(join(outdir, 'index.html'))
-    const $ = cheerio.load(html)
-    expect($('p').text()).toBe('I am a home page')
-
-    const htmlSingle = await readFile(join(outdir, 'posts', 'single.html'))
-    const $single = cheerio.load(htmlSingle)
-    expect($single('p').text()).toBe('I am a single post')
-  })
+  const htmlSingle = await readFile(join(outdir, 'posts', 'single.html'))
+  const $single = cheerio.load(htmlSingle)
+  await t.expect($single('p').text()).eql('I am a single post')
 })
