5a078e0a2eae4eb23b45f7d5aa2b4723fd0f1088
JJ Kasper <jj@jjsweb.site>
2019-10-15 21:03:53 -0500

    Migrate dynamic-routing tests

--- test/integration/dynamic-routing/test/index.test.js
+++ test/integration/dynamic-routing/test/index.test.js
@@ -1,5 +1,6 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import webdriver from 'next-webdriver'
 import { join } from 'path'
 import fs from 'fs-extra'
@@ -13,190 +14,194 @@ import {
   nextStart
 } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
-
-let app
-let appPort
-let buildId
 const appDir = join(__dirname, '../')
 const buildIdPath = join(appDir, '.next/BUILD_ID')
 
 function runTests (dev) {
-  it('should render normal route', async () => {
-    const html = await renderViaHTTP(appPort, '/')
-    expect(html).toMatch(/my blog/i)
+  test('should render normal route', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/')
+    await t.expect(html).match(/my blog/i)
   })
 
-  it('should render another normal route', async () => {
-    const html = await renderViaHTTP(appPort, '/another')
-    expect(html).toMatch(/hello from another/)
+  test('should render another normal route', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/another')
+    await t.expect(html).match(/hello from another/)
   })
 
-  it('should render dynamic page', async () => {
-    const html = await renderViaHTTP(appPort, '/post-1')
-    expect(html).toMatch(/this is.*?post-1/i)
+  test('should render dynamic page', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/post-1')
+    await t.expect(html).match(/this is.*?post-1/i)
   })
 
-  it('should prioritize a non-dynamic page', async () => {
-    const html = await renderViaHTTP(appPort, '/post-1/comments')
-    expect(html).toMatch(/show comments for.*post-1.*here/i)
+  test('should prioritize a non-dynamic page', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/post-1/comments')
+    await t.expect(html).match(/show comments for.*post-1.*here/i)
   })
 
-  it('should render nested dynamic page', async () => {
-    const html = await renderViaHTTP(appPort, '/post-1/comment-1')
-    expect(html).toMatch(/i am.*comment-1.*on.*post-1/i)
+  test('should render nested dynamic page', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/post-1/comment-1')
+    await t.expect(html).match(/i am.*comment-1.*on.*post-1/i)
   })
 
-  it('should render optional dynamic page', async () => {
-    const html = await renderViaHTTP(appPort, '/blog/543/comment')
-    // expect(html).toMatch(/blog post.*543.*comment.*all/i)
-    expect(html).toMatch(/404/i)
+  test('should render optional dynamic page', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/blog/543/comment')
+    // await t.expect(html).match(/blog post.*543.*comment.*all/i)
+    await t.expect(html).match(/404/i)
   })
 
-  it('should render nested optional dynamic page', async () => {
-    const html = await renderViaHTTP(appPort, '/blog/321/comment/123')
-    expect(html).toMatch(/blog post.*321.*comment.*123/i)
+  test('should render nested optional dynamic page', async t => {
+    const html = await renderViaHTTP(
+      t.fixtureCtx.appPort,
+      '/blog/321/comment/123'
+    )
+    await t.expect(html).match(/blog post.*321.*comment.*123/i)
   })
 
-  it('should render dynamic route with query', async () => {
-    const browser = await webdriver(appPort, '/')
+  test('should render dynamic route with query', async t => {
+    const browser = await webdriver(t.fixtureCtx.appPort, '/')
     await browser.elementByCss('#view-post-1-with-query').click()
     await waitFor(1000)
     const url = await browser.eval('window.location.search')
-    expect(url).toBe('?fromHome=true')
+    await t.expect(url).eql('?fromHome=true')
   })
 
-  it('should navigate to a dynamic page successfully', async () => {
+  test('should navigate to a dynamic page successfully', async t => {
     let browser
     try {
-      browser = await webdriver(appPort, '/')
+      browser = await webdriver(t.fixtureCtx.appPort, '/')
       await browser.elementByCss('#view-post-1').click()
       await browser.waitForElementByCss('p')
 
       const text = await browser.elementByCss('p').text()
-      expect(text).toMatch(/this is.*?post-1/i)
+      await t.expect(text).match(/this is.*?post-1/i)
     } finally {
       if (browser) await browser.close()
     }
   })
 
-  // it('should navigate optional dynamic page', async () => {
+  // test('should navigate optional dynamic page', async t => {
   //   let browser
   //   try {
-  //     browser = await webdriver(appPort, '/')
+  //     browser = await webdriver(t.fixtureCtx.appPort, '/')
   //     await browser.elementByCss('#view-blog-post-1-comments').click()
   //     await browser.waitForElementByCss('p')
 
   //     const text = await browser.elementByCss('p').text()
-  //     expect(text).toMatch(/blog post.*543.*comment.*\(all\)/i)
+  //     await t.expect(text).match(/blog post.*543.*comment.*\(all\)/i)
   //   } finally {
   //     if (browser) await browser.close()
   //   }
   // })
 
-  it('should navigate optional dynamic page with value', async () => {
+  test('should navigate optional dynamic page with value', async t => {
     let browser
     try {
-      browser = await webdriver(appPort, '/')
+      browser = await webdriver(t.fixtureCtx.appPort, '/')
       await browser.elementByCss('#view-nested-dynamic-cmnt').click()
       await browser.waitForElementByCss('p')
 
       const text = await browser.elementByCss('p').text()
-      expect(text).toMatch(/blog post.*321.*comment.*123/i)
+      await t.expect(text).match(/blog post.*321.*comment.*123/i)
     } finally {
       if (browser) await browser.close()
     }
   })
 
-  it('should navigate to a nested dynamic page successfully', async () => {
+  test('should navigate to a nested dynamic page successfully', async t => {
     let browser
     try {
-      browser = await webdriver(appPort, '/')
+      browser = await webdriver(t.fixtureCtx.appPort, '/')
       await browser.elementByCss('#view-post-1-comment-1').click()
       await browser.waitForElementByCss('p')
 
       const text = await browser.elementByCss('p').text()
-      expect(text).toMatch(/i am.*comment-1.*on.*post-1/i)
+      await t.expect(text).match(/i am.*comment-1.*on.*post-1/i)
     } finally {
       if (browser) await browser.close()
     }
   })
 
-  it('should pass params in getInitialProps during SSR', async () => {
-    const html = await renderViaHTTP(appPort, '/post-1/cmnt-1')
-    expect(html).toMatch(/gip.*post-1/i)
+  test('should pass params in getInitialProps during SSR', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/post-1/cmnt-1')
+    await t.expect(html).match(/gip.*post-1/i)
   })
 
-  it('should pass params in getInitialProps during client navigation', async () => {
+  test('should pass params in getInitialProps during client navigation', async t => {
     let browser
     try {
-      browser = await webdriver(appPort, '/')
+      browser = await webdriver(t.fixtureCtx.appPort, '/')
       await browser.elementByCss('#view-post-1-comment-1').click()
       await browser.waitForElementByCss('span')
 
       const text = await browser.elementByCss('span').text()
-      expect(text).toMatch(/gip.*post-1/i)
+      await t.expect(text).match(/gip.*post-1/i)
     } finally {
       if (browser) await browser.close()
     }
   })
 
-  it('should update dynamic values on mount', async () => {
-    const html = await renderViaHTTP(appPort, '/on-mount/post-1')
-    expect(html).toMatch(/onmpost:.*pending/)
+  test('should update dynamic values on mount', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/on-mount/post-1')
+    await t.expect(html).match(/onmpost:.*pending/)
 
-    const browser = await webdriver(appPort, '/on-mount/post-1')
+    const browser = await webdriver(t.fixtureCtx.appPort, '/on-mount/post-1')
     await waitFor(1000)
     const text = await browser.eval(`document.body.innerHTML`)
-    expect(text).toMatch(/onmpost:.*post-1/)
+    await t.expect(text).match(/onmpost:.*post-1/)
   })
 
-  it('should not have placeholder query values for SSS', async () => {
-    const html = await renderViaHTTP(appPort, '/on-mount/post-1')
-    expect(html).not.toMatch(/post:.*?\[post\].*?<\/p>/)
+  test('should not have placeholder query values for SSS', async t => {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, '/on-mount/post-1')
+    await t.expect(html).notMatch(/post:.*?\[post\].*?<\/p>/)
   })
 
-  it('should update with a hash in the URL', async () => {
-    const browser = await webdriver(appPort, '/on-mount/post-1#abc')
+  test('should update with a hash in the URL', async t => {
+    const browser = await webdriver(
+      t.fixtureCtx.appPort,
+      '/on-mount/post-1#abc'
+    )
     await waitFor(1000)
     const text = await browser.eval(`document.body.innerHTML`)
-    expect(text).toMatch(/onmpost:.*post-1/)
+    await t.expect(text).match(/onmpost:.*post-1/)
   })
 
-  it('should scroll to a hash on mount', async () => {
-    const browser = await webdriver(appPort, '/on-mount/post-1#item-400')
+  test('should scroll to a hash on mount', async t => {
+    const browser = await webdriver(
+      t.fixtureCtx.appPort,
+      '/on-mount/post-1#item-400'
+    )
     await waitFor(1000)
 
     const text = await browser.eval(`document.body.innerHTML`)
-    expect(text).toMatch(/onmpost:.*post-1/)
+    await t.expect(text).match(/onmpost:.*post-1/)
 
     const scrollPosition = await browser.eval('window.pageYOffset')
-    expect(scrollPosition).toBe(7232)
+    await t.expect(scrollPosition).eql(7232)
   })
 
-  it('should scroll to a hash on client-side navigation', async () => {
-    const browser = await webdriver(appPort, '/')
+  test('should scroll to a hash on client-side navigation', async t => {
+    const browser = await webdriver(t.fixtureCtx.appPort, '/')
     await waitFor(1000)
     await browser.elementByCss('#view-dynamic-with-hash').click()
     await browser.waitForElementByCss('p')
 
     const text = await browser.elementByCss('p').text()
-    expect(text).toMatch(/onmpost:.*test-w-hash/)
+    await t.expect(text).match(/onmpost:.*test-w-hash/)
 
     const scrollPosition = await browser.eval('window.pageYOffset')
-    expect(scrollPosition).toBe(7232)
+    await t.expect(scrollPosition).eql(7232)
   })
 
-  it('should prioritize public files over dynamic route', async () => {
-    const data = await renderViaHTTP(appPort, '/hello.txt')
-    expect(data).toMatch(/hello world/)
+  test('should prioritize public files over dynamic route', async t => {
+    const data = await renderViaHTTP(t.fixtureCtx.appPort, '/hello.txt')
+    await t.expect(data).match(/hello world/)
   })
 
   if (dev) {
-    it('should work with HMR correctly', async () => {
-      const browser = await webdriver(appPort, '/post-1/comments')
+    test('should work with HMR correctly', async t => {
+      const browser = await webdriver(t.fixtureCtx.appPort, '/post-1/comments')
       let text = await browser.eval(`document.documentElement.innerHTML`)
-      expect(text).toMatch(/comments for.*post-1/)
+      await t.expect(text).match(/comments for.*post-1/)
 
       const page = join(appDir, 'pages/[name]/comments.js')
       const origContent = await fs.readFile(page, 'utf8')
@@ -207,18 +212,18 @@ function runTests (dev) {
         await waitFor(3 * 1000)
 
         let text = await browser.eval(`document.documentElement.innerHTML`)
-        expect(text).toMatch(/commentss for.*post-1/)
+        await t.expect(text).match(/commentss for.*post-1/)
       } finally {
         await fs.writeFile(page, origContent, 'utf8')
         if (browser) await browser.close()
       }
     })
   } else {
-    it('should output modern bundles with dynamic route correctly', async () => {
+    test('should output modern bundles with dynamic route correctly', async t => {
       const bundlePath = join(
         appDir,
         '.next/static/',
-        buildId,
+        t.fixtureCtx.buildId,
         'pages/blog/[name]/comment/[id]'
       )
 
@@ -230,52 +235,26 @@ function runTests (dev) {
 
 const nextConfig = join(appDir, 'next.config.js')
 
-describe('Dynamic Routing', () => {
-  describe('dev mode', () => {
-    beforeAll(async () => {
-      appPort = await findPort()
-      app = await launchApp(appDir, appPort)
-    })
-    afterAll(() => killApp(app))
+fixture('Dynamic Routing')
 
-    runTests(true)
+fixture('dev mode')
+  .before(async ctx => {
+    ctx.appPort = await findPort()
+    ctx.app = await launchApp(appDir, ctx.appPort)
   })
+  .after(ctx => killApp(ctx.app))
 
-  describe('production mode', () => {
-    beforeAll(async () => {
-      const curConfig = await fs.readFile(nextConfig, 'utf8')
+runTests(true)
 
-      if (curConfig.includes('target')) {
-        await fs.writeFile(
-          nextConfig,
-          `
-          module.exports = {
-            experimental: {
-              modern: true,
-              publicDirectory: true
-            }
-          }
-        `
-        )
-      }
-      await nextBuild(appDir)
-      buildId = await fs.readFile(buildIdPath, 'utf8')
+fixture('production mode')
+  .before(async ctx => {
+    const curConfig = await fs.readFile(nextConfig, 'utf8')
 
-      appPort = await findPort()
-      app = await nextStart(appDir, appPort)
-    })
-    afterAll(() => killApp(app))
-
-    runTests()
-  })
-
-  describe('SSR production mode', () => {
-    beforeAll(async () => {
+    if (curConfig.includes('target')) {
       await fs.writeFile(
         nextConfig,
         `
         module.exports = {
-          target: 'serverless',
           experimental: {
             modern: true,
             publicDirectory: true
@@ -283,14 +262,38 @@ describe('Dynamic Routing', () => {
         }
       `
       )
+    }
+    await nextBuild(appDir)
+    ctx.buildId = await fs.readFile(buildIdPath, 'utf8')
 
-      await nextBuild(appDir)
-      buildId = await fs.readFile(buildIdPath, 'utf8')
-
-      appPort = await findPort()
-      app = await nextStart(appDir, appPort)
-    })
-    afterAll(() => killApp(app))
-    runTests()
+    ctx.appPort = await findPort()
+    ctx.app = await nextStart(appDir, ctx.appPort)
   })
-})
+  .after(ctx => killApp(ctx.app))
+
+runTests()
+
+fixture('serverless production mode')
+  .before(async ctx => {
+    await fs.writeFile(
+      nextConfig,
+      `
+      module.exports = {
+        target: 'serverless',
+        experimental: {
+          modern: true,
+          publicDirectory: true
+        }
+      }
+    `
+    )
+
+    await nextBuild(appDir)
+    ctx.buildId = await fs.readFile(buildIdPath, 'utf8')
+
+    ctx.appPort = await findPort()
+    ctx.app = await nextStart(appDir, ctx.appPort)
+  })
+  .after(ctx => killApp(ctx.app))
+
+runTests()
