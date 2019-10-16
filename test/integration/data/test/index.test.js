288169bd7c9ac28552dda62b61bd557d712ba2f3
JJ Kasper <jj@jjsweb.site>
2019-10-15 20:54:18 -0500

    Migrate data tests

--- test/integration/data/test/index.test.js
+++ test/integration/data/test/index.test.js
@@ -1,5 +1,6 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { join } from 'path'
 import webdriver from 'next-webdriver'
 import {
@@ -11,48 +12,44 @@ import {
 } from 'next-test-utils'
 
 const appDir = join(__dirname, '../')
-let appPort
-let server
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 
-describe('AMP Bind Initial Data', () => {
-  beforeAll(async () => {
-    appPort = await findPort()
-    server = await launchApp(appDir, appPort)
+fixture('AMP Bind Initial Data')
+  .before(async ctx => {
+    ctx.appPort = await findPort()
+    ctx.server = await launchApp(appDir, ctx.appPort)
   })
-  afterAll(() => killApp(server))
+  .after(ctx => killApp(ctx.server))
 
-  it('responds with json with accept header on page', async () => {
-    const data = await fetchViaHTTP(appPort, '/', null, {
-      headers: {
-        accept: 'application/amp.bind+json'
-      }
-    }).then(res => res.ok && res.text())
+test('responds with json with accept header on page', async t => {
+  const data = await fetchViaHTTP(t.fixtureCtx.appPort, '/', null, {
+    headers: {
+      accept: 'application/amp.bind+json'
+    }
+  }).then(res => res.ok && res.text())
 
-    let isJSON = false
-    try {
-      JSON.parse(data)
-      isJSON = true
-    } catch (_) {}
-    expect(isJSON).toBe(true)
-  })
+  let isJSON = false
+  try {
+    JSON.parse(data)
+    isJSON = true
+  } catch (_) {}
+  await t.expect(isJSON).eql(true)
+})
 
-  it('renders the data during SSR', async () => {
-    const html = await renderViaHTTP(appPort, '/')
-    expect(html).toMatch(/The uptime of the server is.*?\d.*?\d/)
-  })
+test('renders the data during SSR', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/')
+  await t.expect(html).match(/The uptime of the server is.*?\d.*?\d/)
+})
 
-  it('renders a page without data', async () => {
-    const html = await renderViaHTTP(appPort, '/about')
-    expect(html).toMatch(/<a.*?home/)
-  })
+test('renders a page without data', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/about')
+  await t.expect(html).match(/<a.*?home/)
+})
 
-  it('navigates to a page with data correctly', async () => {
-    const browser = await webdriver(appPort, '/about')
-    await browser.elementByCss('a').click()
-    await browser.waitForElementByCss('h1')
-    const h1Text = await browser.elementByCss('h1').text()
-    expect(h1Text).toMatch(/The uptime of the server is.*?\d.*?\d/)
-    await browser.close()
-  })
+test('navigates to a page with data correctly', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/about')
+  await browser.elementByCss('a').click()
+  await browser.waitForElementByCss('h1')
+  const h1Text = await browser.elementByCss('h1').text()
+  await t.expect(h1Text).match(/The uptime of the server is.*?\d.*?\d/)
+  await browser.close()
 })
