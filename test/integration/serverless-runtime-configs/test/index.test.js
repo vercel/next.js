990aea6cb1d60d56084eefae13d8c55bb1f422f6
JJ Kasper <jj@jjsweb.site>
2019-10-16 10:45:16 -0500

    Migrate serverless-runtime-configs tests

--- test/integration/serverless-runtime-configs/test/index.test.js
+++ test/integration/serverless-runtime-configs/test/index.test.js
@@ -1,50 +1,49 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import fs from 'fs-extra'
 import { join } from 'path'
 import { nextBuild } from 'next-test-utils'
 
 const appDir = join(__dirname, '../')
 const nextConfigPath = join(appDir, 'next.config.js')
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 
 const cleanUp = () => fs.remove(nextConfigPath)
 
-describe('Serverless runtime configs', () => {
-  beforeAll(() => cleanUp())
-  afterAll(() => cleanUp())
+fixture('Serverless runtime configs')
+  .before(() => cleanUp())
+  .after(() => cleanUp())
 
-  it('should error on usage of publicRuntimeConfig', async () => {
-    await fs.writeFile(
-      nextConfigPath,
-      `module.exports = {
-      target: 'serverless',
-      publicRuntimeConfig: {
-        hello: 'world'
-      }
-    }`
-    )
+test('should error on usage of publicRuntimeConfig', async t => {
+  await fs.writeFile(
+    nextConfigPath,
+    `module.exports = {
+    target: 'serverless',
+    publicRuntimeConfig: {
+      hello: 'world'
+    }
+  }`
+  )
 
-    const { stderr } = await nextBuild(appDir, undefined, { stderr: true })
-    expect(stderr).toMatch(
-      /Cannot use publicRuntimeConfig or serverRuntimeConfig/
-    )
-  })
+  const { stderr } = await nextBuild(appDir, undefined, { stderr: true })
+  await t
+    .expect(stderr)
+    .match(/Cannot use publicRuntimeConfig or serverRuntimeConfig/)
+})
 
-  it('should error on usage of serverRuntimeConfig', async () => {
-    await fs.writeFile(
-      nextConfigPath,
-      `module.exports = {
-      target: 'serverless',
-      serverRuntimeConfig: {
-        hello: 'world'
-      }
-    }`
-    )
+test('should error on usage of serverRuntimeConfig', async t => {
+  await fs.writeFile(
+    nextConfigPath,
+    `module.exports = {
+    target: 'serverless',
+    serverRuntimeConfig: {
+      hello: 'world'
+    }
+  }`
+  )
 
-    const { stderr } = await nextBuild(appDir, undefined, { stderr: true })
-    expect(stderr).toMatch(
-      /Cannot use publicRuntimeConfig or serverRuntimeConfig/
-    )
-  })
+  const { stderr } = await nextBuild(appDir, undefined, { stderr: true })
+  await t
+    .expect(stderr)
+    .match(/Cannot use publicRuntimeConfig or serverRuntimeConfig/)
 })
