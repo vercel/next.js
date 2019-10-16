7576e0afeb37d2fe9118e40451dd98c82f3c0366
JJ Kasper <jj@jjsweb.site>
2019-10-16 13:49:36 -0500

    Remove mock-require for incorrect react test

--- test/unit/handles-incorrect-react.test.js
+++ test/unit/handles-incorrect-react.test.js
@@ -1,19 +1,20 @@
 /* global fixture, test */
 import 'testcafe'
 import path from 'path'
-import mock from 'mock-require'
 import { didThrow } from 'next-test-utils'
-
-mock('react', {
-  Suspense: undefined
-})
 
 const nextDir = path.dirname(require.resolve('next/package'))
 const nextBin = path.join(nextDir, 'dist/bin/next')
+const reactKey = require.resolve('react')
+require('react')
 
-fixture('Handles Incorrect React Version')
+fixture('Handles Incorrect React Version').after(() => {
+  delete require.cache[reactKey]
+})
 
 test('should throw an error when building with next', async t => {
+  require.cache[reactKey].exports.Suspense = undefined
+
   await didThrow(
     () => require(nextBin),
     true,
