f9a0c05493dce22d97a4eb931aa18fbe48c58b53
JJ Kasper <jj@jjsweb.site>
2019-10-16 09:56:46 -0500

    Migrate modern-mode test

--- test/integration/modern-mode/test/index.test.js
+++ test/integration/modern-mode/test/index.test.js
@@ -1,27 +1,24 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import { join } from 'path'
 import { readFileSync, readdirSync } from 'fs'
 import rimraf from 'rimraf'
 import { promisify } from 'util'
 import { nextServer, runNextCommand, startApp, stopApp } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
-
 const rimrafPromise = promisify(rimraf)
 let appDir = join(__dirname, '..')
-let server
-// let appPort
 
-describe('Modern Mode', () => {
-  beforeAll(async () => {
+fixture('Modern Mode')
+  .before(async ctx => {
     await runNextCommand(['build'], {
       cwd: appDir,
       stdout: true,
       stderr: true
     })
 
-    const app = nextServer({
+    ctx.app = nextServer({
       dir: appDir,
       dev: false,
       quiet: true,
@@ -30,39 +27,41 @@ describe('Modern Mode', () => {
       }
     })
 
-    server = await startApp(app)
+    ctx.server = await startApp(ctx.app)
     // appPort = server.address().port
   })
-  afterAll(async () => {
-    stopApp(server)
+  .after(async ctx => {
+    stopApp(ctx.server)
     rimrafPromise(join(appDir, '.next'))
   })
-  it('should generate client side modern and legacy build files', async () => {
-    const buildId = readFileSync(join(appDir, '.next/BUILD_ID'), 'utf8')
 
-    const expectedFiles = [
-      'index',
-      '_app',
-      '_error',
-      'main',
-      'webpack',
-      'commons'
-    ]
-    const buildFiles = [
-      ...readdirSync(join(appDir, '.next/static', buildId, 'pages')),
-      ...readdirSync(join(appDir, '.next/static/runtime')).map(
-        file => file.replace(/-\w+\./, '.') // remove hash
-      ),
-      ...readdirSync(join(appDir, '.next/static/chunks')).map(
-        file => file.replace(/\.\w+\./, '.') // remove hash
-      )
-    ]
+test('should generate client side modern and legacy build files', async t => {
+  const buildId = readFileSync(join(appDir, '.next/BUILD_ID'), 'utf8')
 
-    console.log(`Client files: ${buildFiles.join(', ')}`)
+  const expectedFiles = [
+    'index',
+    '_app',
+    '_error',
+    'main',
+    'webpack',
+    'commons'
+  ]
+  const buildFiles = [
+    ...readdirSync(join(appDir, '.next/static', buildId, 'pages')),
+    ...readdirSync(join(appDir, '.next/static/runtime')).map(
+      file => file.replace(/-\w+\./, '.') // remove hash
+    ),
+    ...readdirSync(join(appDir, '.next/static/chunks')).map(
+      file => file.replace(/\.\w+\./, '.') // remove hash
+    )
+  ]
 
-    expectedFiles.forEach(file => {
-      expect(buildFiles).toContain(`${file}.js`)
-      expect(buildFiles).toContain(`${file}.module.js`)
+  console.log(`Client files: ${buildFiles.join(', ')}`)
+
+  await Promise.all(
+    expectedFiles.map(async file => {
+      await t.expect(buildFiles).contains(`${file}.js`)
+      await t.expect(buildFiles).contains(`${file}.module.js`)
     })
-  })
+  )
 })
