a19a0e2c305517dab70aaecbc10ff4bca73c8097
JJ Kasper <jj@jjsweb.site>
2019-10-15 21:40:45 -0500

    Migrate filesystempublicroutes tests

--- test/integration/filesystempublicroutes/test/index.test.js
+++ test/integration/filesystempublicroutes/test/index.test.js
@@ -1,57 +1,52 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import { t } from 'testcafe'
+
 import { join } from 'path'
 import getPort from 'get-port'
 import { fetchViaHTTP, initNextServerScript, killApp } from 'next-test-utils'
 import clone from 'clone'
 
 const appDir = join(__dirname, '../')
-let appPort
-let server
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
 
-const context = {}
-
-const startServer = async (optEnv = {}) => {
+const startServer = async (ctx, optEnv = {}) => {
   const scriptPath = join(appDir, 'server.js')
-  context.appPort = appPort = await getPort()
+  ctx.appPort = await getPort()
   const env = Object.assign(
     {},
     clone(process.env),
-    { PORT: `${appPort}` },
+    { PORT: `${ctx.appPort}` },
     optEnv
   )
 
-  server = await initNextServerScript(scriptPath, /ready on/i, env)
+  ctx.server = await initNextServerScript(scriptPath, /ready on/i, env)
 }
 
-describe('FileSystemPublicRoutes', () => {
-  beforeAll(() => startServer())
-  afterAll(() => killApp(server))
+fixture('FileSystemPublicRoutes')
+  .before(ctx => startServer(ctx))
+  .after(ctx => killApp(ctx.server))
 
-  const fetch = (p, q) => fetchViaHTTP(context.appPort, p, q)
+const fetch = (p, q) => fetchViaHTTP(t.fixtureCtx.appPort, p, q)
 
-  it('should not route to the index page', async () => {
-    const res = await fetch('/')
-    expect(res.status).toBe(404)
-    const body = await res.text()
-    expect(body).toMatch(/404/)
-  })
+test('should not route to the index page', async t => {
+  const res = await fetch('/')
+  await t.expect(res.status).eql(404)
+  const body = await res.text()
+  await t.expect(body).match(/404/)
+})
 
-  it('should route to exportPathMap defined routes in development', async () => {
-    const res = await fetch('/exportpathmap-route')
-    expect(res.status).toBe(200)
-    const body = await res.text()
-    expect(body).toMatch(/exportpathmap was here/)
-  })
+test('should route to exportPathMap defined routes in development', async t => {
+  const res = await fetch('/exportpathmap-route')
+  await t.expect(res.status).eql(200)
+  const body = await res.text()
+  await t.expect(body).match(/exportpathmap was here/)
+})
 
-  it('should still handle /_next routes', async () => {
-    await fetch('/exportpathmap-route') // make sure it's built
-    const res = await fetch(
-      '/_next/static/development/pages/exportpathmap-route.js'
-    )
-    expect(res.status).toBe(200)
-    const body = await res.text()
-    expect(body).toMatch(/exportpathmap was here/)
-  })
+test('should still handle /_next routes', async t => {
+  await fetch('/exportpathmap-route') // make sure it's built
+  const res = await fetch(
+    '/_next/static/development/pages/exportpathmap-route.js'
+  )
+  await t.expect(res.status).eql(200)
+  const body = await res.text()
+  await t.expect(body).match(/exportpathmap was here/)
 })
