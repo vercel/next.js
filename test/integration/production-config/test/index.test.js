4e93193aea1d246e8b2e7d7c30fbcca04fa7e6b8
JJ Kasper <jj@jjsweb.site>
2019-10-16 10:32:49 -0500

    Migrate production-config tests

--- test/integration/production-config/test/index.test.js
+++ test/integration/production-config/test/index.test.js
@@ -1,8 +1,10 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import { t } from 'testcafe'
+
 import webdriver from 'next-webdriver'
 import { join } from 'path'
 import {
+  waitFor,
   nextServer,
   nextBuild,
   startApp,
@@ -10,86 +12,76 @@ import {
   runNextCommand
 } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
-
 const appDir = join(__dirname, '../')
 
-let appPort
-let server
+async function testBrowser () {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  await waitFor(500)
+  const element = await browser.elementByCss('#mounted')
+  const text = await element.text()
+  await t.expect(text).match(/ComponentDidMount executed on client\./)
+  await t.expect(await element.getComputedCss('font-size')).eql('40px')
+  await t.expect(await element.getComputedCss('color')).eql('rgb(255, 0, 0)')
+  await browser.close()
+}
 
-describe('Production Config Usage', () => {
-  beforeAll(async () => {
+fixture('Production Config Usage')
+  .before(async ctx => {
     await nextBuild(appDir)
     const app = nextServer({
       dir: join(__dirname, '../'),
       dev: false,
       quiet: true
     })
-    server = await startApp(app)
-    appPort = server.address().port
+    ctx.server = await startApp(app)
+    ctx.appPort = ctx.server.address().port
   })
-  afterAll(() => stopApp(server))
+  .after(ctx => stopApp(ctx.server))
 
-  describe('with next-css', () => {
-    it('should load styles', async () => {
-      // Try 3 times as the breaking happens intermittently
-      await testBrowser()
-      await testBrowser()
-      await testBrowser()
-    })
-  })
-
-  describe('env', () => {
-    it('should fail with leading __ in env key', async () => {
-      const result = await runNextCommand(['build', appDir], {
-        env: { ENABLE_ENV_FAIL_UNDERSCORE: true },
-        stdout: true,
-        stderr: true
-      })
-
-      expect(result.stderr).toMatch(/The key "__NEXT_MY_VAR" under/)
-    })
-
-    it('should fail with NODE_ in env key', async () => {
-      const result = await runNextCommand(['build', appDir], {
-        env: { ENABLE_ENV_FAIL_NODE: true },
-        stdout: true,
-        stderr: true
-      })
-
-      expect(result.stderr).toMatch(/The key "NODE_ENV" under/)
-    })
-
-    it('should allow __ within env key', async () => {
-      const result = await runNextCommand(['build', appDir], {
-        env: { ENABLE_ENV_WITH_UNDERSCORES: true },
-        stdout: true,
-        stderr: true
-      })
-
-      expect(result.stderr).not.toMatch(/The key "SOME__ENV__VAR" under/)
-    })
-  })
-
-  describe('with generateBuildId', () => {
-    it('should add the custom buildid', async () => {
-      const browser = await webdriver(appPort, '/')
-      const text = await browser.elementByCss('#mounted').text()
-      expect(text).toMatch(/ComponentDidMount executed on client\./)
-
-      const html = await browser.elementByCss('html').getAttribute('innerHTML')
-      expect(html).toMatch('custom-buildid')
-      await browser.close()
-    })
-  })
+test('should load styles', async t => {
+  // Try 3 times as the breaking happens intermittently
+  await testBrowser()
+  await testBrowser()
+  await testBrowser()
 })
 
-async function testBrowser () {
-  const browser = await webdriver(appPort, '/')
-  const element = await browser.elementByCss('#mounted')
-  const text = await element.text()
-  expect(text).toMatch(/ComponentDidMount executed on client\./)
-  expect(await element.getComputedCss('font-size')).toBe('40px')
-  expect(await element.getComputedCss('color')).toBe('rgba(255, 0, 0, 1)')
+test('should fail with leading __ in env key', async t => {
+  const result = await runNextCommand(['build', appDir], {
+    env: { ENABLE_ENV_FAIL_UNDERSCORE: true },
+    stdout: true,
+    stderr: true
+  })
+
+  await t.expect(result.stderr).match(/The key "__NEXT_MY_VAR" under/)
+})
+
+test('should fail with NODE_ in env key', async t => {
+  const result = await runNextCommand(['build', appDir], {
+    env: { ENABLE_ENV_FAIL_NODE: true },
+    stdout: true,
+    stderr: true
+  })
+
+  await t.expect(result.stderr).match(/The key "NODE_ENV" under/)
+})
+
+test('should allow __ within env key', async t => {
+  const result = await runNextCommand(['build', appDir], {
+    env: { ENABLE_ENV_WITH_UNDERSCORES: true },
+    stdout: true,
+    stderr: true
+  })
+
+  await t.expect(result.stderr).notMatch(/The key "SOME__ENV__VAR" under/)
+})
+
+test('should add the custom buildid', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  await waitFor(500)
+  const text = await browser.elementByCss('#mounted').text()
+  await t.expect(text).match(/ComponentDidMount executed on client\./)
+
+  const html = await browser.elementByCss('html').getAttribute('innerHTML')
+  await t.expect(html).contains('custom-buildid')
   await browser.close()
-}
+})
