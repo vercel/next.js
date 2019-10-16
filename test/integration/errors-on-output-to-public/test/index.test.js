ca0c0757ba51747975e3d74e02b8c26ae0596434
JJ Kasper <jj@jjsweb.site>
2019-10-15 21:10:59 -0500

    Migrate errors-on-output-to-public tests

--- test/integration/errors-on-output-to-public/test/index.test.js
+++ test/integration/errors-on-output-to-public/test/index.test.js
@@ -1,37 +1,41 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import path from 'path'
 import fs from 'fs-extra'
 import { nextBuild, nextExport } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 1
 const appDir = path.join(__dirname, '..')
 const nextConfig = path.join(appDir, 'next.config.js')
 
-describe('Errors on output to public', () => {
-  it('Throws error when `distDir` is set to public', async () => {
-    await fs.writeFile(nextConfig, `module.exports = { distDir: 'public' }`)
-    const results = await nextBuild(appDir, [], { stdout: true, stderr: true })
-    expect(results.stdout + results.stderr).toMatch(
+fixture('Errors on output to public')
+
+test('Throws error when `distDir` is set to public', async t => {
+  await fs.writeFile(nextConfig, `module.exports = { distDir: 'public' }`)
+  const results = await nextBuild(appDir, [], { stdout: true, stderr: true })
+  await t
+    .expect(results.stdout + results.stderr)
+    .match(
       /The 'public' directory is reserved in Next.js and can not be set as/
     )
-    await fs.remove(nextConfig)
-  })
+  await fs.remove(nextConfig)
+})
 
-  it('Throws error when export out dir is public', async () => {
-    await fs.remove(nextConfig)
-    await nextBuild(appDir)
-    const outdir = path.join(appDir, 'public')
-    const results = await nextExport(
-      appDir,
-      { outdir },
-      {
-        stdout: true,
-        stderr: true
-      }
-    )
-    expect(results.stdout + results.stderr).toMatch(
+test('Throws error when export out dir is public', async t => {
+  await fs.remove(nextConfig)
+  await nextBuild(appDir)
+  const outdir = path.join(appDir, 'public')
+  const results = await nextExport(
+    appDir,
+    { outdir },
+    {
+      stdout: true,
+      stderr: true
+    }
+  )
+  await t
+    .expect(results.stdout + results.stderr)
+    .match(
       /The 'public' directory is reserved in Next.js and can not be used as/
     )
-  })
 })
