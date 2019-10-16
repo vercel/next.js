f375b0e1d1de8087431b471156bb0a46e95e0611
JJ Kasper <jj@jjsweb.site>
2019-10-16 10:44:22 -0500

    Migrate serverless tests

--- test/integration/serverless/test/index.test.js
+++ test/integration/serverless/test/index.test.js
@@ -1,5 +1,6 @@
-/* eslint-env jest */
-/* global jasmine */
+/* global fixture, test */
+import 'testcafe'
+
 import webdriver from 'next-webdriver'
 import { join } from 'path'
 import cheerio from 'cheerio'
@@ -19,216 +20,212 @@ const appDir = join(__dirname, '../')
 const serverlessDir = join(appDir, '.next/serverless/pages')
 const chunksDir = join(appDir, '.next/static/chunks')
 const buildIdFile = join(appDir, '.next/BUILD_ID')
-let appPort
-let app
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 
-describe('Serverless', () => {
-  beforeAll(async () => {
+fixture('Serverless')
+  .before(async ctx => {
     await nextBuild(appDir)
-    appPort = await findPort()
-    app = await nextStart(appDir, appPort)
+    ctx.appPort = await findPort()
+    ctx.app = await nextStart(appDir, ctx.appPort)
   })
-  afterAll(() => killApp(app))
+  .after(ctx => killApp(ctx.app))
 
-  it('should render the page', async () => {
-    const html = await renderViaHTTP(appPort, '/')
-    expect(html).toMatch(/Hello World/)
-  })
+test('should render the page', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/')
+  await t.expect(html).match(/Hello World/)
+})
 
-  it('should add autoExport for auto pre-rendered pages', async () => {
-    for (const page of ['/', '/abc']) {
-      const html = await renderViaHTTP(appPort, page)
-      const $ = cheerio.load(html)
-      const data = JSON.parse($('#__NEXT_DATA__').html())
-      expect(data.autoExport).toBe(true)
-    }
-  })
-
-  it('should not add autoExport for non pre-rendered pages', async () => {
-    for (const page of ['/fetch']) {
-      const html = await renderViaHTTP(appPort, page)
-      const $ = cheerio.load(html)
-      const data = JSON.parse($('#__NEXT_DATA__').html())
-      expect(!!data.autoExport).toBe(false)
-    }
-  })
-
-  it('should serve file from public folder', async () => {
-    const content = await renderViaHTTP(appPort, '/hello.txt')
-    expect(content.trim()).toBe('hello world')
-
-    const legacy = await renderViaHTTP(appPort, '/static/legacy.txt')
-    expect(legacy).toMatch(`new static folder`)
-  })
-
-  it('should render the page with dynamic import', async () => {
-    const html = await renderViaHTTP(appPort, '/dynamic')
-    expect(html).toMatch(/Hello!/)
-  })
-
-  it('should render the page with same dynamic import', async () => {
-    const html = await renderViaHTTP(appPort, '/dynamic-two')
-    expect(html).toMatch(/Hello!/)
-  })
-
-  it('should render 404', async () => {
-    const html = await renderViaHTTP(appPort, '/404')
-    expect(html).toMatch(/This page could not be found/)
-  })
-
-  it('should render 404 for /_next/static', async () => {
-    const html = await renderViaHTTP(appPort, '/_next/static')
-    expect(html).toMatch(/This page could not be found/)
-  })
-
-  it('should render an AMP page', async () => {
-    const html = await renderViaHTTP(appPort, '/some-amp?amp=1')
-    expect(html).toMatch(/Hi Im an AMP page/)
-    expect(html).toMatch(/ampproject\.org/)
-  })
-
-  it('should have correct amphtml rel link', async () => {
-    const html = await renderViaHTTP(appPort, '/some-amp')
-    expect(html).toMatch(/Hi Im an AMP page/)
-    expect(html).toMatch(/rel="amphtml" href="\/some-amp\?amp=1"/)
-  })
-
-  it('should have correct canonical link', async () => {
-    const html = await renderViaHTTP(appPort, '/some-amp?amp=1')
-    expect(html).toMatch(/rel="canonical" href="\/some-amp"/)
-  })
-
-  it('should render correctly when importing isomorphic-unfetch', async () => {
-    const url = `http://localhost:${appPort}/fetch`
-    const res = await fetch(url)
-    expect(res.status).toBe(200)
-    const text = await res.text()
-    expect(text.includes('failed')).toBe(false)
-  })
-
-  it('should render correctly when importing isomorphic-unfetch on the client side', async () => {
-    const browser = await webdriver(appPort, '/')
-    try {
-      const text = await browser
-        .elementByCss('a')
-        .click()
-        .waitForElementByCss('.fetch-page')
-        .elementByCss('#text')
-        .text()
-
-      expect(text).toMatch(/fetch page/)
-    } finally {
-      await browser.close()
-    }
-  })
-
-  it('should not have combined client-side chunks', () => {
-    expect(readdirSync(chunksDir).length).toBeGreaterThanOrEqual(2)
-    const buildId = readFileSync(buildIdFile, 'utf8').trim()
-
-    const pageContent = join(
-      appDir,
-      '.next/static',
-      buildId,
-      'pages/dynamic.js'
-    )
-    expect(readFileSync(pageContent, 'utf8')).not.toContain('Hello!')
-  })
-
-  it('should not output _app.js and _document.js to serverless build', () => {
-    expect(existsSync(join(serverlessDir, '_app.js'))).toBeFalsy()
-    expect(existsSync(join(serverlessDir, '_document.js'))).toBeFalsy()
-  })
-
-  it('should replace static pages with HTML files', async () => {
-    const staticFiles = ['abc', 'dynamic', 'dynamic-two', 'some-amp']
-    for (const file of staticFiles) {
-      expect(existsSync(join(serverlessDir, file + '.html'))).toBe(true)
-      expect(existsSync(join(serverlessDir, file + '.js'))).toBe(false)
-    }
-  })
-
-  it('should not replace non-static pages with HTML files', async () => {
-    const nonStaticFiles = ['fetch', '_error']
-    for (const file of nonStaticFiles) {
-      expect(existsSync(join(serverlessDir, file + '.js'))).toBe(true)
-      expect(existsSync(join(serverlessDir, file + '.html'))).toBe(false)
-    }
-  })
-
-  it('should reply on API request successfully', async () => {
-    const content = await renderViaHTTP(appPort, '/api/hello')
-    expect(content).toMatch(/hello world/)
-  })
-
-  it('should reply on dynamic API request successfully', async () => {
-    const result = await renderViaHTTP(appPort, '/api/posts/post-1')
-    const { id } = JSON.parse(result)
-    expect(id).toBe('post-1')
-  })
-
-  it('should reply on dynamic API request successfully with query parameters', async () => {
-    const result = await renderViaHTTP(appPort, '/api/posts/post-1?param=val')
-    const { id, param } = JSON.parse(result)
-    expect(id).toBe('post-1')
-    expect(param).toBe('val')
-  })
-
-  it('should reply on dynamic API index request successfully', async () => {
-    const result = await renderViaHTTP(appPort, '/api/dynamic/post-1')
-    const { path } = JSON.parse(result)
-    expect(path).toBe('post-1')
-  })
-
-  it('should reply on dynamic API index request successfully with query parameters', async () => {
-    const result = await renderViaHTTP(appPort, '/api/dynamic/post-1?param=val')
-    const { path, param } = JSON.parse(result)
-    expect(path).toBe('post-1')
-    expect(param).toBe('val')
-  })
-
-  it('should 404 on API request with trailing slash', async () => {
-    const res = await fetchViaHTTP(appPort, '/api/hello/')
-    expect(res.status).toBe(404)
-  })
-
-  it('should have the correct query string for a dynamic route', async () => {
-    const paramRaw = 'test % 123'
-    const param = encodeURIComponent(paramRaw)
-
-    const html = await renderViaHTTP(appPort, `/dr/${param}`)
+test('should add autoExport for auto pre-rendered pages', async t => {
+  for (const page of ['/', '/abc']) {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, page)
     const $ = cheerio.load(html)
     const data = JSON.parse($('#__NEXT_DATA__').html())
+    await t.expect(data.autoExport).eql(true)
+  }
+})
 
-    expect(data.query).toEqual({ slug: paramRaw })
-  })
-
-  it('should have the correct query string for a spr route', async () => {
-    const paramRaw = 'test % 123'
-    const html = await fetchViaHTTP(appPort, `/dr/[slug]`, '', {
-      headers: { 'x-now-route-params': qs.stringify({ 1: paramRaw }) }
-    }).then(res => res.text())
+test('should not add autoExport for non pre-rendered pages', async t => {
+  for (const page of ['/fetch']) {
+    const html = await renderViaHTTP(t.fixtureCtx.appPort, page)
     const $ = cheerio.load(html)
     const data = JSON.parse($('#__NEXT_DATA__').html())
+    await t.expect(!!data.autoExport).eql(false)
+  }
+})
 
-    expect(data.query).toEqual({ slug: paramRaw })
-  })
+test('should serve file from public folder', async t => {
+  const content = await renderViaHTTP(t.fixtureCtx.appPort, '/hello.txt')
+  await t.expect(content.trim()).eql('hello world')
 
-  describe('With basic usage', () => {
-    it('should allow etag header support', async () => {
-      const url = `http://localhost:${appPort}/`
-      const etag = (await fetch(url)).headers.get('ETag')
+  const legacy = await renderViaHTTP(t.fixtureCtx.appPort, '/static/legacy.txt')
+  await t.expect(legacy).contains(`new static folder`)
+})
 
-      const headers = { 'If-None-Match': etag }
-      const res2 = await fetch(url, { headers })
-      expect(res2.status).toBe(304)
-    })
+test('should render the page with dynamic import', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/dynamic')
+  await t.expect(html).match(/Hello!/)
+})
 
-    it('should set Content-Length header', async () => {
-      const url = `http://localhost:${appPort}`
-      const res = await fetch(url)
-      expect(res.headers.get('Content-Length')).toBeDefined()
-    })
-  })
+test('should render the page with same dynamic import', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/dynamic-two')
+  await t.expect(html).match(/Hello!/)
+})
+
+test('should render 404', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/404')
+  await t.expect(html).match(/This page could not be found/)
+})
+
+test('should render 404 for /_next/static', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/_next/static')
+  await t.expect(html).match(/This page could not be found/)
+})
+
+test('should render an AMP page', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/some-amp?amp=1')
+  await t.expect(html).match(/Hi Im an AMP page/)
+  await t.expect(html).match(/ampproject\.org/)
+})
+
+test('should have correct amphtml rel link', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/some-amp')
+  await t.expect(html).match(/Hi Im an AMP page/)
+  await t.expect(html).match(/rel="amphtml" href="\/some-amp\?amp=1"/)
+})
+
+test('should have correct canonical link', async t => {
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, '/some-amp?amp=1')
+  await t.expect(html).match(/rel="canonical" href="\/some-amp"/)
+})
+
+test('should render correctly when importing isomorphic-unfetch', async t => {
+  const url = `http://localhost:${t.fixtureCtx.appPort}/fetch`
+  const res = await fetch(url)
+  await t.expect(res.status).eql(200)
+  const text = await res.text()
+  await t.expect(text.includes('failed')).eql(false)
+})
+
+test('should render correctly when importing isomorphic-unfetch on the client side', async t => {
+  const browser = await webdriver(t.fixtureCtx.appPort, '/')
+  try {
+    await browser.elementByCss('a').click()
+
+    await browser.waitForElementByCss('.fetch-page')
+
+    const text = await browser.elementByCss('#text').text()
+    await t.expect(text).match(/fetch page/)
+  } finally {
+    await browser.close()
+  }
+})
+
+test('should not have combined client-side chunks', async t => {
+  await t.expect(readdirSync(chunksDir).length >= 2).ok()
+  const buildId = readFileSync(buildIdFile, 'utf8').trim()
+
+  const pageContent = join(appDir, '.next/static', buildId, 'pages/dynamic.js')
+  await t.expect(readFileSync(pageContent, 'utf8')).notContains('Hello!')
+})
+
+test('should not output _app.js and _document.js to serverless build', async t => {
+  await t.expect(existsSync(join(serverlessDir, '_app.js'))).notOk()
+  await t.expect(existsSync(join(serverlessDir, '_document.js'))).notOk()
+})
+
+test('should replace static pages with HTML files', async t => {
+  const staticFiles = ['abc', 'dynamic', 'dynamic-two', 'some-amp']
+  for (const file of staticFiles) {
+    await t.expect(existsSync(join(serverlessDir, file + '.html'))).eql(true)
+    await t.expect(existsSync(join(serverlessDir, file + '.js'))).eql(false)
+  }
+})
+
+test('should not replace non-static pages with HTML files', async t => {
+  const nonStaticFiles = ['fetch', '_error']
+  for (const file of nonStaticFiles) {
+    await t.expect(existsSync(join(serverlessDir, file + '.js'))).eql(true)
+    await t.expect(existsSync(join(serverlessDir, file + '.html'))).eql(false)
+  }
+})
+
+test('should reply on API request successfully', async t => {
+  const content = await renderViaHTTP(t.fixtureCtx.appPort, '/api/hello')
+  await t.expect(content).match(/hello world/)
+})
+
+test('should reply on dynamic API request successfully', async t => {
+  const result = await renderViaHTTP(t.fixtureCtx.appPort, '/api/posts/post-1')
+  const { id } = JSON.parse(result)
+  await t.expect(id).eql('post-1')
+})
+
+test('should reply on dynamic API request successfully with query parameters', async t => {
+  const result = await renderViaHTTP(
+    t.fixtureCtx.appPort,
+    '/api/posts/post-1?param=val'
+  )
+  const { id, param } = JSON.parse(result)
+  await t.expect(id).eql('post-1')
+  await t.expect(param).eql('val')
+})
+
+test('should reply on dynamic API index request successfully', async t => {
+  const result = await renderViaHTTP(
+    t.fixtureCtx.appPort,
+    '/api/dynamic/post-1'
+  )
+  const { path } = JSON.parse(result)
+  await t.expect(path).eql('post-1')
+})
+
+test('should reply on dynamic API index request successfully with query parameters', async t => {
+  const result = await renderViaHTTP(
+    t.fixtureCtx.appPort,
+    '/api/dynamic/post-1?param=val'
+  )
+  const { path, param } = JSON.parse(result)
+  await t.expect(path).eql('post-1')
+  await t.expect(param).eql('val')
+})
+
+test('should 404 on API request with trailing slash', async t => {
+  const res = await fetchViaHTTP(t.fixtureCtx.appPort, '/api/hello/')
+  await t.expect(res.status).eql(404)
+})
+
+test('should have the correct query string for a dynamic route', async t => {
+  const paramRaw = 'test % 123'
+  const param = encodeURIComponent(paramRaw)
+
+  const html = await renderViaHTTP(t.fixtureCtx.appPort, `/dr/${param}`)
+  const $ = cheerio.load(html)
+  const data = JSON.parse($('#__NEXT_DATA__').html())
+
+  await t.expect(data.query).eql({ slug: paramRaw })
+})
+
+test('should have the correct query string for a spr route', async t => {
+  const paramRaw = 'test % 123'
+  const html = await fetchViaHTTP(t.fixtureCtx.appPort, `/dr/[slug]`, '', {
+    headers: { 'x-now-route-params': qs.stringify({ 1: paramRaw }) }
+  }).then(res => res.text())
+  const $ = cheerio.load(html)
+  const data = JSON.parse($('#__NEXT_DATA__').html())
+
+  await t.expect(data.query).eql({ slug: paramRaw })
+})
+
+test('should allow etag header support', async t => {
+  const url = `http://localhost:${t.fixtureCtx.appPort}/`
+  const etag = (await fetch(url)).headers.get('ETag')
+
+  const headers = { 'If-None-Match': etag }
+  const res2 = await fetch(url, { headers })
+  await t.expect(res2.status).eql(304)
+})
+
+test('should set Content-Length header', async t => {
+  const url = `http://localhost:${t.fixtureCtx.appPort}`
+  const res = await fetch(url)
+  await t.expect(typeof res.headers.get('Content-Length')).notEql('undefined')
 })
