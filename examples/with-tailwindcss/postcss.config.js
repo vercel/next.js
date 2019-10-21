5839ffeb0c813fce287401df1659575b01a1a7bc
Taylor Bryant <taylor@bryant.io>
2019-10-20 22:48:17 -0500

    Fix linting errors

--- examples/with-tailwindcss/postcss.config.js
+++ examples/with-tailwindcss/postcss.config.js
@@ -4,8 +4,8 @@ module.exports = {
     require('tailwindcss'),
     require('@fullhuman/postcss-purgecss')({
       content: ['./pages/**/*.js', './components/**/*.js'],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     }),
     require('autoprefixer')
-  ],
+  ]
 }
