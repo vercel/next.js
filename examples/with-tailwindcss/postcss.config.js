e1814c2a00306e08fb2959c68f402d23485150c4
Taylor Bryant <taylor@bryant.io>
2019-10-20 22:38:40 -0500

    Remove trailing comma

--- examples/with-tailwindcss/postcss.config.js
+++ examples/with-tailwindcss/postcss.config.js
@@ -6,6 +6,6 @@ module.exports = {
       content: ['./pages/**/*.js', './components/**/*.js'],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
-    require('autoprefixer'),
+    require('autoprefixer')
   ],
 }
