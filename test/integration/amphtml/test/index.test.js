7f908c78a0c9b59793ba965b1a9761e8ab0c6760
Joe Haddad <joe.haddad@zeit.co>
2019-10-22 11:44:01 -0400

    Reenable AMP Tests

--- test/integration/amphtml/test/index.test.js
+++ test/integration/amphtml/test/index.test.js
@@ -27,7 +27,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
 
 const context = {}
 
-xdescribe('AMP Usage', () => {
+describe('AMP Usage', () => {
   beforeAll(async () => {
     await nextBuild(appDir)
     app = nextServer({
