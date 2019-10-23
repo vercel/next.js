55149f4b94cc9d67b98b4b663492b3d011a1b786
Taylor Bryant <taylor@bryant.io>
2019-10-22 21:42:49 -0500

    Use single quotes

--- examples/with-tailwindcss/postcss.config.js
+++ examples/with-tailwindcss/postcss.config.js
@@ -7,6 +7,6 @@ module.exports = {
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     }),
     require('autoprefixer'),
-    require("cssnano")
+    require('cssnano')
   ]
 }
