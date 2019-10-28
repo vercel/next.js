5bbf5855f241fb41fb5724d92ce21a40de071eca
Janicklas Ralph James <janicklas@google.com>
2019-10-28 10:36:47 -0700

    Adding try catch to the error lines

--- test/integration/chunking/test/index.test.js
+++ test/integration/chunking/test/index.test.js
@@ -27,19 +27,19 @@ describe('Chunking', () => {
           return unlink(join(appDir, '.next', 'static', 'chunks', chunk))
         })
       )
-    } catch (e) {
-      // Error here means old chunks don't exist, so we don't need to do anything
-    }
-    await nextBuild(appDir)
-    stats = (await readFile(join(appDir, '.next', 'stats.json'), 'utf8'))
-      // fixes backslashes in keyNames not being escaped on windows
-      .replace(/"static\\(.*?":)/g, '"static\\\\$1')
-      .replace(/("static\\.*?)\\pages\\(.*?":)/g, '$1\\\\pages\\\\$2')
 
-    console.error(stats)
-    stats = JSON.parse(stats)
-    buildId = await readFile(join(appDir, '.next/BUILD_ID'), 'utf8')
-    chunks = await readdir(join(appDir, '.next', 'static', 'chunks'))
+      await nextBuild(appDir)
+      stats = (await readFile(join(appDir, '.next', 'stats.json'), 'utf8'))
+        // fixes backslashes in keyNames not being escaped on windows
+        .replace(/"static\\(.*?":)/g, '"static\\\\$1')
+        .replace(/("static\\.*?)\\pages\\(.*?":)/g, '$1\\\\pages\\\\$2')
+
+      stats = JSON.parse(stats)
+      buildId = await readFile(join(appDir, '.next/BUILD_ID'), 'utf8')
+      chunks = await readdir(join(appDir, '.next', 'static', 'chunks'))
+    } catch (e) {
+      console.error(stats)
+    }
   })
 
   it('should use all url friendly names', () => {
