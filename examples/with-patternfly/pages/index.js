9798490aa556166fecc132df8477fc3ee0523e26
Daniel Reinoso <danielreinoso1807@gmail.com>
2019-10-30 10:54:59 -0300

    Remove trailing comma

--- examples/with-patternfly/pages/index.js
+++ examples/with-patternfly/pages/index.js
@@ -6,7 +6,7 @@ const steps = [
   { name: 'Step 2', component: <p>Step 2</p> },
   { name: 'Step 3', component: <p>Step 3</p> },
   { name: 'Step 4', component: <p>Step 4</p> },
-  { name: 'Review', component: <p>Review Step</p>, nextButtonText: 'Finish' },
+  { name: 'Review', component: <p>Review Step</p>, nextButtonText: 'Finish' }
 ]
 
 export default () => {
