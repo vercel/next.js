75d15681ffb32464a503c6024000ae0b428e8975
Joe Haddad <timer@zeit.co>
2019-10-30 10:00:11 -0400

    Revert page1.js

--- test/integration/chunking/pages/page1.js
+++ test/integration/chunking/pages/page1.js
@@ -1,14 +1,8 @@
 import ReactDOM from 'react-dom'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
 const Page = () => {
   console.log(ReactDOM)
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/page2')
-  })
-  return <div id='page-1'>page1</div>
+  return <div>page1</div>
 }
 
 export default Page
