0499316650478d4340addc2c64ebe53c5e7e146f
Tim Neutkens <tim@timneutkens.nl>
2019-11-08 09:03:50 -0800

    Add check for now.json and custom server (#9346)
    * Check if the user has a now.json
    
    * Check for now.json
    
    * Add check for custom server

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -6,7 +6,7 @@ import mkdirpOrig from 'mkdirp'
 import nanoid from 'next/dist/compiled/nanoid/index.js'
 import path from 'path'
 import { promisify } from 'util'
-
+import findUp from 'find-up'
 import formatWebpackMessages from '../client/dev/error-overlay/format-webpack-messages'
 import { PUBLIC_DIR_MIDDLEWARE_CONFLICT } from '../lib/constants'
 import { findPagesDir } from '../lib/find-pages-dir'
@@ -125,6 +125,8 @@ export default async function build(dir: string, conf = null): Promise<void> {
       eventVersion({
         cliCommand: 'build',
         isSrcDir: path.relative(dir, pagesDir!).startsWith('src'),
+        hasNowJson: !!(await findUp('now.json', { cwd: dir })),
+        isCustomServer: null,
       })
     ),
     eventNextPlugins(path.resolve(dir)).then(events => telemetry.record(events))
