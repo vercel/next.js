c08756d2dd5f137e53a315a63cd830fc4fd5d154
Joe Haddad <joe.haddad@zeit.co>
2019-11-09 21:09:11 -0800

    Test CJS Import of `isomorphic-unfetch`

--- test/integration/serverless/pages/index.js
+++ test/integration/serverless/pages/index.js
@@ -6,6 +6,9 @@ export default () => {
       <Link href='/fetch'>
         <a id='fetchlink'>fetch page</a>
       </Link>
+      <Link href='/fetch-cjs'>
+        <a id='fetchcjslink'>fetch cjs page</a>
+      </Link>
     </div>
   )
 }
