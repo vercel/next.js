68fc6714d9d543d285b9544f2908a956e70329b8
Joe Haddad <joe.haddad@zeit.co>
2019-11-10 20:36:56 -0800

    fix formatting

--- packages/next/build/polyfills/object.assign/index.js
+++ packages/next/build/polyfills/object.assign/index.js
@@ -1,10 +1,10 @@
 var assign = Object.assign.bind(Object)
-function g () {
+function g() {
   return assign
 }
 Object.defineProperties(g(), {
   implementation: { get: g },
   shim: { value: g },
-  getPolyfill: { value: g }
+  getPolyfill: { value: g },
 })
 module.exports = g()
