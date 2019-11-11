6f8073fcf086af81ae98db482596308fd75268a5
JJ Kasper <jj@jjsweb.site>
2019-11-10 19:34:13 -0800

    Update env var name

--- examples/with-dotenv/next.config.js
+++ examples/with-dotenv/next.config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
-    SECRET_TEST_KEY: process.env.SECRET_TEST_KEY,
+    TEST_VAR: process.env.TEST_VAR,
   },
 }
