feb6b623c0558cd4265a3e11acf56ca80a50fd71
Brecht <databrecht@gmail.com>
2019-11-12 20:12:28 +0100

    with-graphql-faunadb

---
+++ examples/with-graphql-faunadb/components/Header.js
@@ -0,0 +1,17 @@
+import { headerBanner, headerTitle } from '../styles/header'
+
+export default props => (
+  <>
+    <a href="https://fauna.com" target="_blank">
+      <img
+        className={headerBanner.className}
+        src="/static/fauna-logo-blue.png"
+        height="35px"
+        width="auto"
+      />
+    </a>
+    <h1 className={headerTitle.className}>Guestbook</h1>
+    {headerBanner.styles}
+    {headerTitle.styles}
+  </>
+)
