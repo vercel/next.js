75a41e93a932170a214785fd040f8dc431304fe3
sinanyalcinkaya <sinanyalcinkaya@gmail.com>
2019-11-13 11:13:55 +0300

    1. Yükleme
    Denemek için

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default () => (
   <div>
-    Hello World.{' '}
-    <Link href='/about'>
+    Merhaba Dünyaaa.{" "}
+    <Link href="/about">
       <a>About</a>
     </Link>
   </div>
-)
+);
