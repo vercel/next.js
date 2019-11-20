625190d06df36f5c40d2a3d682238c5b32ac53f5
Glen Ihrig <glen@arkadias.com>
2019-11-20 11:13:45 -0800

    Update purgecss search path (#9432)
    * Update purgecss search string to include typescript files - closes #9415
    
    * fix minor formatting issue

--- examples/with-tailwindcss/postcss.config.js
+++ examples/with-tailwindcss/postcss.config.js
@@ -3,7 +3,10 @@ module.exports = {
     require('postcss-easy-import'),
     require('tailwindcss'),
     require('@fullhuman/postcss-purgecss')({
-      content: ['./pages/**/*.js', './components/**/*.js'],
+      content: [
+        './pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+      ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
     require('autoprefixer'),
