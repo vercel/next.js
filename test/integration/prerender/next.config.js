046c9274c3ed3029899227f383fc9f00bfa797bb
Joe Haddad <joe.haddad@zeit.co>
2019-11-21 16:36:22 -0500

    Move config to correct place

--- test/integration/prerender/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  exportPathMap: function(defaultPathMap) {
-    if (defaultPathMap['/blog/[post]']) {
-      throw new Error('Found SPR page in the default export path map')
-    }
-    return defaultPathMap
-  },
-}
