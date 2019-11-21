fcf38d37e8d61d5d9e4f1b6b3a7d1a699b9380e2
Alejandro Garcia Anglada <aganglada@gmail.com>
2019-11-21 21:33:19 +0000

    Fix linting issue

--- examples/with-jest/__tests__/index.test.js
+++ examples/with-jest/__tests__/index.test.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 import App from '../pages/index'
-
-configure({ adapter: new Adapter() })
 
 describe('With Enzyme', () => {
   it('App shows "Hello world!"', () => {
