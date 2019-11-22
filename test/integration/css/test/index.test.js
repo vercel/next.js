af3a22b7a98ac4091129322838fc8fecff285f06
Joe Haddad <joe.haddad@zeit.co>
2019-11-22 16:28:37 -0500

    Add css customization test

--- test/integration/css/test/index.test.js
+++ test/integration/css/test/index.test.js
@@ -202,13 +202,77 @@ describe('CSS Support', () => {
 
       const { version, mappings, sourcesContent } = JSON.parse(cssMapContent)
       expect({ version, mappings, sourcesContent }).toMatchInlineSnapshot(`
+                Object {
+                  "mappings": "AAAA,+CACE,4BACE,WACF,CAFA,mBACE,WACF,CAFA,uBACE,WACF,CAFA,wBACE,WACF,CAFA,cACE,WACF,CACF",
+                  "sourcesContent": Array [
+                    "@media (480px <= width < 768px) {
+                  ::placeholder {
+                    color: green;
+                  }
+                }
+                ",
+                  ],
+                  "version": 3,
+                }
+            `)
+    })
+  })
+
+  describe('CSS Customization', () => {
+    const appDir = join(fixturesDir, 'custom-configuration')
+
+    beforeAll(async () => {
+      await remove(join(appDir, '.next'))
+    })
+
+    it('should build successfully', async () => {
+      await nextBuild(appDir)
+    })
+
+    it(`should've compiled and prefixed`, async () => {
+      const cssFolder = join(appDir, '.next/static/css')
+
+      const files = await readdir(cssFolder)
+      const cssFiles = files.filter(f => /\.css$/.test(f))
+
+      expect(cssFiles.length).toBe(1)
+      const cssContent = await readFile(join(cssFolder, cssFiles[0]), 'utf8')
+      expect(
+        cssContent.replace(/\/\*.*?\*\//g, '').trim()
+      ).toMatchInlineSnapshot(
+        `"@media (480px <= width < 768px){::placeholder{color:green}}.video{max-width:400px;max-height:300px}"`
+      )
+
+      // Contains a source map
+      expect(cssContent).toMatch(/\/\*#\s*sourceMappingURL=(.+\.map)\s*\*\//)
+    })
+
+    it(`should've emitted a source map`, async () => {
+      const cssFolder = join(appDir, '.next/static/css')
+
+      const files = await readdir(cssFolder)
+      const cssMapFiles = files.filter(f => /\.css\.map$/.test(f))
+
+      expect(cssMapFiles.length).toBe(1)
+      const cssMapContent = (
+        await readFile(join(cssFolder, cssMapFiles[0]), 'utf8')
+      ).trim()
+
+      const { version, mappings, sourcesContent } = JSON.parse(cssMapContent)
+      expect({ version, mappings, sourcesContent }).toMatchInlineSnapshot(`
         Object {
-          "mappings": "AAAA,+CACE,4BACE,WACF,CAFA,mBACE,WACF,CAFA,uBACE,WACF,CAFA,wBACE,WACF,CAFA,cACE,WACF,CACF",
+          "mappings": "AACA,gCACE,cACE,WACF,CACF,CAGA,OACE,eAA0B,CAA1B,gBACF",
           "sourcesContent": Array [
-            "@media (480px <= width < 768px) {
+            "/* this should pass through untransformed */
+        @media (480px <= width < 768px) {
           ::placeholder {
             color: green;
           }
+        }
+
+        /* this should be transformed to width/height */
+        .video {
+          -xyz-max-size: 400px 300px;
         }
         ",
           ],
@@ -560,12 +624,12 @@ describe('CSS Support', () => {
           )
           .sort()
       ).toMatchInlineSnapshot(`
-        Array [
-          "dark.svg",
-          "dark2.svg",
-          "light.svg",
-        ]
-      `)
+                Array [
+                  "dark.svg",
+                  "dark2.svg",
+                  "light.svg",
+                ]
+            `)
     })
   })
 
