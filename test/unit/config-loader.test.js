9b3f2697affee07f22541c093ae81ff691470ba4
Joe Haddad <joe.haddad@zeit.co>
2019-11-22 16:55:05 -0500

    Add load config test

---
+++ test/unit/config-loader.test.js
@@ -0,0 +1,43 @@
+/* eslint-env jest */
+import { getConfig } from 'next/dist/lib/load-config'
+import { join } from 'path'
+
+const fixtureDir = join(__dirname, 'fixtures')
+
+describe('load config', () => {
+  it('should resolve rc.js', async () => {
+    const config = await getConfig(join(fixtureDir, 'config-js'), 'test')
+    expect(config).toEqual({ foo: 'bar' })
+  })
+
+  it('should resolve rc.json', async () => {
+    const config = await getConfig(join(fixtureDir, 'config-json'), 'test')
+    expect(config).toEqual({ foo: 'bar' })
+  })
+
+  it('should resolve rc (no ext)', async () => {
+    const config = await getConfig(join(fixtureDir, 'config-no-ext'), 'test')
+    expect(config).toEqual({ foo: 'bar' })
+  })
+
+  it('should resolve package.json', async () => {
+    const config = await getConfig(
+      join(fixtureDir, 'config-package-json'),
+      'test'
+    )
+    expect(config).toEqual({ foo: 'bar' })
+  })
+
+  it('should resolve config.js', async () => {
+    const config = await getConfig(join(fixtureDir, 'dot-config-js'), 'test')
+    expect(config).toEqual({ foo: 'bar' })
+  })
+
+  it('should resolve down', async () => {
+    const config = await getConfig(
+      join(fixtureDir, 'config-down/one/two/three/'),
+      'test'
+    )
+    expect(config).toEqual({ foo: 'bar' })
+  })
+})
