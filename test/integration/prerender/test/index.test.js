a1bca84bc83a5127d0ff0c06a749ecaab7159fb6
JJ Kasper <jj@jjsweb.site>
2019-11-25 20:18:32 -0600

    Make sure to show missing getStaticProps error in serverless mode

--- test/integration/prerender/test/index.test.js
+++ test/integration/prerender/test/index.test.js
@@ -248,29 +248,6 @@ const runTests = (dev = false) => {
         await fs.writeFile(indexPage, origContent)
       }
     })
-
-    it('should show error when getStaticParams is used without getStaticProps', async () => {
-      const pagePath = join(appDir, 'pages/no-getStaticProps.js')
-      await fs.writeFile(
-        pagePath,
-        `
-        export async function unstable_getStaticParams() {
-          return []
-        }
-
-        export default () => 'hi'
-      `,
-        'utf8'
-      )
-
-      const html = await renderViaHTTP(appPort, '/no-getStaticProps')
-      await fs.remove(pagePath)
-      await waitFor(500)
-
-      expect(html).toMatch(
-        /unstable_getStaticParams was added without a unstable_getStaticProps in/
-      )
-    })
   } else {
     it('should should use correct caching headers for a no-revalidate page', async () => {
       const initialRes = await fetchViaHTTP(appPort, '/something')
