0ffa39b2e80ea2c699e0f6022805a683fa99118b
aganglada <aganglada@gmail.com>
2019-11-26 17:08:08 +0000

    replaced custom server with rewrites

--- examples/with-next-offline/next.config.js
+++ examples/with-next-offline/next.config.js
@@ -1,3 +1,17 @@
 const withOffline = require('next-offline')
 
-module.exports = withOffline()
+module.exports = withOffline({
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+  },
+  experimental: {
+    async rewrites() {
+      return [
+        {
+          source: '/service-worker.js',
+          destination: '/_next/static/service-worker.js',
+        },
+      ]
+    },
+  },
+})
