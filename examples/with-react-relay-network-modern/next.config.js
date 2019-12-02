f1b92aec9d85134f10c45aa57194c1dc30725800
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-12-02 17:52:46 -0300

    fix(build): fix relay build

--- examples/with-react-relay-network-modern/next.config.js
+++ examples/with-react-relay-network-modern/next.config.js
@@ -5,6 +5,20 @@ const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   webpack: (config, { isServer }) => {
+
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      const keys = Object.keys(entries);
+      keys.forEach(key => {
+        if (key.includes('__generated__')) {
+          delete entries[key];
+        }
+      });
+
+      return entries;
+    };
+
     config.plugins.push(
       new Dotenv({
         path: path.join(__dirname, '.env'),
--- examples/with-relay-modern-server-express/next.config.js
+++ examples/with-relay-modern-server-express/next.config.js
@@ -5,6 +5,20 @@ const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   webpack: config => {
+    
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      const keys = Object.keys(entries);
+      keys.forEach(key => {
+        if (key.includes('__generated__')) {
+          delete entries[key];
+        }
+      });
+
+      return entries;
+    };
+
     config.plugins = config.plugins || []
 
     config.plugins = [
