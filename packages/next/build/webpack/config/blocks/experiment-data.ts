4df7bd35d90bb5781a7f932f8901501a586d20fe
Joe Haddad <joe.haddad@zeit.co>
2019-12-06 17:33:24 -0500

    Correct next data behavior

--- packages/next/build/webpack/config/blocks/experiment-data.ts
+++ packages/next/build/webpack/config/blocks/experiment-data.ts
@@ -1,7 +1,7 @@
 import curry from 'lodash.curry'
 import path from 'path'
 import { Configuration } from 'webpack'
-import { loader } from '../helpers'
+import { unshiftLoader } from '../helpers'
 import { ConfigurationContext, pipe } from '../utils'
 
 export const experimentData = curry(function experimentData(
@@ -18,7 +18,7 @@ export const experimentData = curry(function experimentData(
   }
 
   const fn = pipe(
-    loader({
+    unshiftLoader({
       test: /\.(tsx|ts|js|mjs|jsx)$/,
       include: [path.join(ctx.rootDirectory, 'data')],
       use: 'next-data-loader',
--- packages/next/build/webpack/config/helpers.ts
+++ packages/next/build/webpack/config/helpers.ts
@@ -20,3 +20,23 @@ export const loader = curry(function loader(
   config.module.rules.push(rule)
   return config
 })
+
+export const unshiftLoader = curry(function loader(
+  rule: RuleSetRule,
+  config: Configuration
+) {
+  if (!config.module) {
+    config.module = { rules: [] }
+  }
+
+  if (rule.oneOf) {
+    const existing = config.module.rules.find(rule => rule.oneOf)
+    if (existing) {
+      existing.oneOf!.unshift(...rule.oneOf)
+      return config
+    }
+  }
+
+  config.module.rules.unshift(rule)
+  return config
+})
