465f87689ee605b21c19794f95bce6bc5702bcdd
qrg <qualle.rhythmus+d@gmail.com>
2019-12-07 04:07:46 +0900

    Remove empty webpack config from analyze bundle example
    * https://github.com/zeit/next.js/pull/9645/files/4943d844cc4bd73edfc58ae10d781def3de687af#r354844766

--- examples/analyze-bundles/next.config.js
+++ examples/analyze-bundles/next.config.js
@@ -4,11 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const nextConfig = {
   // any configs you need
-  webpack(config) {
-    // any customized webpack config
-    // https://nextjs.org/docs#customizing-webpack-config
-    return config
-  },
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
