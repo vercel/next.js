db9943045be8c6bb637d2a58489e1dc089c9519b
Joe Haddad <joe.haddad@zeit.co>
2019-12-07 00:48:33 -0500

    Disable SSG Transform on Non-SSG Pages

--- packages/next/build/babel/plugins/next-ssg-transform.ts
+++ packages/next/build/babel/plugins/next-ssg-transform.ts
@@ -133,6 +133,10 @@ export default function nextTransformSsg({
           state.done = false
         },
         exit(path, state) {
+          if (!state.isPrerender) {
+            return
+          }
+
           const refs = state.refs
           let count: number
 
@@ -212,9 +216,7 @@ export default function nextTransformSsg({
             })
           } while (count)
 
-          if (state.isPrerender) {
-            decorateSsgExport(t, path, state)
-          }
+          decorateSsgExport(t, path, state)
         },
       },
       VariableDeclarator(path, state) {
