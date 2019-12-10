2ea08d86c939be77cfba9f5446fb1c5ad64faf4d
JJ Kasper <jj@jjsweb.site>
2019-12-10 12:35:06 -0600

    Fix hasPage binding

--- packages/next/next-server/server/next-server.ts
+++ packages/next/next-server/server/next-server.ts
@@ -504,7 +504,7 @@ export default class Server {
       fsRoutes,
       catchAllRoute,
       dynamicRoutes: this.dynamicRoutes,
-      pageChecker: this.hasPage,
+      pageChecker: this.hasPage.bind(this),
     }
   }
 
