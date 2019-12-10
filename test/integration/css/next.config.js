29d178942bd3da1d1d1b32d0993b6d34269e8dd2
Joe Haddad <joe.haddad@zeit.co>
2019-12-10 14:53:44 -0500

    Enable New Chunking for CSS

--- test/integration/css/next.config.js
+++ test/integration/css/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
-  experimental: { css: true, granularChunks: true },
+  experimental: { css: true },
   webpack(cfg) {
     cfg.devtool = 'source-map'
     return cfg
