338e08b49f381508ab2e61750e7e29e67cecd8d4
Joe Haddad <joe.haddad@zeit.co>
2019-12-10 21:29:39 -0500

    Adjust Commit Linting Hook (#9694)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,8 +7,10 @@ module.exports = {
       .map(filename => `"${isWin ? filename : escape([filename])}"`)
       .join(' ')
     return [
-      `prettier --write ${escapedFileNames}`,
-      `eslint --max-warnings=0 --fix ${filenames.map(f => `"${f}"`).join(' ')}`,
+      `prettier --with-node-modules --ignore-path='./.prettierignore_staged' --write ${escapedFileNames}`,
+      `eslint --no-ignore --max-warnings=0 --fix ${filenames
+        .map(f => `"${f}"`)
+        .join(' ')}`,
       `git add ${escapedFileNames}`,
     ]
   },
@@ -17,7 +19,7 @@ module.exports = {
       .map(filename => `"${isWin ? filename : escape([filename])}"`)
       .join(' ')
     return [
-      `prettier --write ${escapedFileNames}`,
+      `prettier --with-node-modules --ignore-path='./.prettierignore_staged' --write ${escapedFileNames}`,
       `git add ${escapedFileNames}`,
     ]
   },
