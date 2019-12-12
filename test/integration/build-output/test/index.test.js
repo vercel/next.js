42fb9022546e27dc08ffabfe631e0b40bca7ab74
Joe Haddad <joe.haddad@zeit.co>
2019-12-12 14:02:46 -0500

    fix test

--- test/integration/build-output/test/index.test.js
+++ test/integration/build-output/test/index.test.js
@@ -23,8 +23,8 @@ describe('Build Output', () => {
       })
 
       expect(stdout).toMatch(/\/ [ ]* \d{1,} B/)
-      expect(stdout).toMatch(/\+ shared by all [ 0-9]* kB/)
-      expect(stdout).toMatch(/ runtime\/main\.js [ 0-9]* kB/)
+      expect(stdout).toMatch(/\+ shared by all [ 0-9.]* kB/)
+      expect(stdout).toMatch(/ runtime\/main\.js [ 0-9.]* kB/)
 
       expect(stdout).not.toContain('/_document')
       expect(stdout).not.toContain('/_app')
@@ -48,8 +48,8 @@ describe('Build Output', () => {
 
       expect(stdout).toMatch(/\/ [ ]* \d{1,} B/)
       expect(stdout).toMatch(/\/_app [ ]* \d{1,} B/)
-      expect(stdout).toMatch(/\+ shared by all [ 0-9]* kB/)
-      expect(stdout).toMatch(/ runtime\/main\.js [ 0-9]* kB/)
+      expect(stdout).toMatch(/\+ shared by all [ 0-9.]* kB/)
+      expect(stdout).toMatch(/ runtime\/main\.js [ 0-9.]* kB/)
 
       expect(stdout).not.toContain('/_document')
       expect(stdout).not.toContain('/_error')
@@ -73,8 +73,8 @@ describe('Build Output', () => {
 
       expect(stdout).toMatch(/\/ [ ]* \d{1,} B/)
       expect(stdout).toMatch(/\/_error [ ]* \d{1,} B/)
-      expect(stdout).toMatch(/\+ shared by all [ 0-9]* kB/)
-      expect(stdout).toMatch(/ runtime\/main\.js [ 0-9]* kB/)
+      expect(stdout).toMatch(/\+ shared by all [ 0-9.]* kB/)
+      expect(stdout).toMatch(/ runtime\/main\.js [ 0-9.]* kB/)
 
       expect(stdout).not.toContain('/_document')
       expect(stdout).not.toContain('/_app')
