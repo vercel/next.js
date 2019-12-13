677a9509f3ae049146707fb1c6004cc67d5e7c0b
JJ Kasper <jj@jjsweb.site>
2019-12-13 12:14:09 -0600

    Fix query values being passed in dev mode for SSG (#9734)
    * Fix query values being passed in dev mode for SSG
    
    * Update test names

--- test/integration/prerender/pages/something.js
+++ test/integration/prerender/pages/something.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 // eslint-disable-next-line camelcase
-export async function unstable_getStaticProps() {
+export async function unstable_getStaticProps({ params }) {
   return {
     props: {
       world: 'world',
+      params: params || {},
       time: new Date().getTime(),
     },
     revalidate: false,
   }
 }
 
-export default ({ world, time }) => {
+export default ({ world, time, params }) => {
   return (
     <>
       <p>hello: {world}</p>
       <span>time: {time}</span>
+      <div id="params">{JSON.stringify(params)}</div>
+      <div id="query">{JSON.stringify(useRouter().query)}</div>
       <Link href="/">
         <a id="home">to home</a>
       </Link>
