1bf98b80c25c922e9bb671cb796ee6aa183369ec
JJ Kasper <jj@jjsweb.site>
2019-12-24 12:20:43 -0600

    Make sure to add chromedriver to path

--- test/lib/next-webdriver.js
+++ test/lib/next-webdriver.js
@@ -22,6 +22,10 @@ const isSafari = browserName === 'safari'
 const isFirefox = browserName === 'firefox'
 const isIE = browserName === 'internet explorer'
 
+if (process.env.ChromeWebDriver) {
+  process.env.PATH = `${process.env.ChromeWebDriver}${path.delimiter}${process.env.PATH}`
+}
+
 const isBrowserStack =
   BROWSERSTACK && BROWSERSTACK_USERNAME && BROWSERSTACK_ACCESS_KEY
 
