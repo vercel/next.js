af82f322e54917afee3b93578830defe66aca59f
Joe Haddad <timer150@gmail.com>
2020-01-09 15:31:28 -0500

    Enable New CSS Support by Default (#9927)
    * Enable New CSS Support by Default
    
    * Adjust configs
    
    * Fix invisible AMP body
    
    * Fix AMP validation warning
    
    * test fix
    
    * Use expression that won't be eliminated by babel

--- packages/next/next-server/server/config.ts
+++ packages/next/next-server/server/config.ts
@@ -41,7 +41,7 @@ const defaultConfig: { [key: string]: any } = {
       (Number(process.env.CIRCLE_NODE_TOTAL) ||
         (os.cpus() || { length: 1 }).length) - 1
     ),
-    css: false,
+    css: true,
     documentMiddleware: false,
     granularChunks: true,
     modern: false,
@@ -107,7 +107,11 @@ function assignDefaults(userConfig: { [key: string]: any }) {
   if (result.experimental) {
     if (result.experimental.css) {
       // The new CSS support requires granular chunks be enabled.
-      result.experimental.granularChunks = true
+      if (result.experimental.granularChunks !== true) {
+        throw new Error(
+          `The new CSS support requires granular chunks be enabled.`
+        )
+      }
     }
 
     if (typeof result.experimental.basePath !== 'string') {
