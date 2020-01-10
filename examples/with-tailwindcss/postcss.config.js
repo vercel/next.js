810830c385fe281e7b3c93ec7e4d20d30b2df4f2
Tim Neutkens <timneutkens@me.com>
2020-01-10 13:09:10 +0100

    Update tailwind example

--- examples/with-tailwindcss/postcss.config.js
+++ examples/with-tailwindcss/postcss.config.js
@@ -1,16 +1,19 @@
 module.exports = {
   plugins: [
-    require('postcss-easy-import'),
-    require('tailwindcss'),
-    process.env.NODE_ENV === 'production' &&
-      require('@fullhuman/postcss-purgecss')({
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
-    require('autoprefixer'),
-    require('cssnano'),
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: content =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          },
+        ]
+      : undefined,
+    'autoprefixer',
   ],
 }
