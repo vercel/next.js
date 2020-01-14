61c7274602e64eafde26fdec0236778288817201
Joe Haddad <joe.haddad@zeit.co>
2020-01-14 14:45:05 -0500

    Add Test for PR #10037

---
+++ test/integration/render-error-on-module-error/test/index.test.js
@@ -0,0 +1,45 @@
+/* eslint-env jest */
+/* global jasmine */
+import {
+  nextBuild,
+  nextServer,
+  startApp,
+  stopApp,
+  waitFor,
+} from 'next-test-utils'
+import webdriver from 'next-webdriver'
+import { join } from 'path'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
+
+const appDir = join(__dirname, '..')
+
+let appPort
+let app
+let server
+
+describe('Module Init Error', () => {
+  beforeAll(async () => {
+    await nextBuild(appDir)
+    app = nextServer({
+      dir: join(__dirname, '../'),
+      dev: false,
+      quiet: true,
+    })
+
+    server = await startApp(app)
+    appPort = server.address().port
+  })
+  afterAll(() => stopApp(server))
+
+  it('should render error page', async () => {
+    const browser = await webdriver(appPort, '/')
+    try {
+      await waitFor(2000)
+      const text = await browser.elementByCss('#error-p').text()
+      expect(text).toBe('Error Rendered')
+    } finally {
+      await browser.close()
+    }
+  })
+})
