5ecd6f712fa1b1a27361f14b33f1fc608beaaf44
Victor Mota <vimota@gmail.com>
2020-01-13 23:39:25 -0800

    Lint fix

--- examples/with-cookie-auth-fauna/utils/fauna-auth.js
+++ examples/with-cookie-auth-fauna/utils/fauna-auth.js
@@ -1,7 +1,6 @@
 import faunadb from 'faunadb'
 import cookie from 'cookie'
 
-
 export const FAUNA_SECRET_COOKIE = 'faunaSecret'
 
 export const serverClient = new faunadb.Client({
@@ -9,17 +8,18 @@ export const serverClient = new faunadb.Client({
 })
 
 // Used for any authed requests.
-export const faunaClient = (secret) => new faunadb.Client({
-  secret,
-});
+export const faunaClient = secret =>
+  new faunadb.Client({
+    secret,
+  })
 
-export const serializeFaunaCookie = (userSecret) => {
+export const serializeFaunaCookie = userSecret => {
   const cookieSerialized = cookie.serialize(FAUNA_SECRET_COOKIE, userSecret, {
-      sameSite: 'lax',
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 72576000,
-      httpOnly: true,
-      path: '/',
-  });
+    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 72576000,
+    httpOnly: true,
+    path: '/',
+  })
   return cookieSerialized
 }
