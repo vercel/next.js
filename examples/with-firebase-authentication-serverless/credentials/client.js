6d969d3d1f22f7508e03327353f86f292df86280
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-13 16:07:10 -0800

    Remove unused files

--- examples/with-firebase-authentication-serverless/credentials/client.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-}
