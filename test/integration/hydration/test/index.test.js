af32fc3d00ce3c675db702267c552bf7e30c14d6
JJ Kasper <jj@jjsweb.site>
2020-01-17 13:06:37 -0600

    Add failing hydration test

---
+++ test/integration/hydration/test/index.test.js
@@ -0,0 +1,31 @@
+/* eslint-env jest */
+/* global jasmine */
+import path from 'path'
+import webdriver from 'next-webdriver'
+import {
+  nextBuild,
+  nextStart,
+  findPort,
+  waitFor,
+  killApp,
+} from 'next-test-utils'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 1
+const appDir = path.join(__dirname, '..')
+let app
+let appPort
+
+describe('Hydration', () => {
+  beforeAll(async () => {
+    await nextBuild(appDir)
+    appPort = await findPort()
+    app = await nextStart(appDir, appPort)
+  })
+  afterAll(() => killApp(app))
+
+  it('Hydrates correctly', async () => {
+    const browser = await webdriver(appPort, '/')
+    await waitFor(2000)
+    expect(await browser.eval('window.didHydrate')).toBe(true)
+  })
+})
