c6de50a7795a258ae76b95171f3f3ab757358d91
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-17 13:34:54 -0800

    Remove the bundle analyzer

--- examples/with-firebase-authentication-serverless/next.config.js
+++ examples/with-firebase-authentication-serverless/next.config.js
@@ -1,10 +1,6 @@
 require('./env.js')
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE_BUNDLE === 'true',
-})
-
-const nextConfig = {
+module.exports = {
   // Public, build-time env vars.
   // https://nextjs.org/docs#build-time-configuration
   env: {
@@ -14,5 +10,3 @@ const nextConfig = {
     FIREBASE_PUBLIC_API_KEY: process.env.FIREBASE_PUBLIC_API_KEY,
   },
 }
-
-module.exports = withBundleAnalyzer(nextConfig)
