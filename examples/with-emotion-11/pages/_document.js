ecf2b9806f484fc75d950b845bfb72fa47006554
Aziz Khambati <aziz.h.khambati@gmail.com>
2020-01-21 02:56:22 +0530

    [with-emotion-10,11] Update emotion examples to v10 & v11 (#9646)
    * Update emotion to v10, use @emotion/core, @emotion/styled
    
    * Add with-emotion-11 and make review changes
    
    * Fix package names and run prettier
    
    * fix readmes
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- examples/with-emotion-11/pages/_document.js
+++ examples/with-emotion-11/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-import { extractCritical } from 'emotion-server'
+import { extractCritical } from '@emotion/server'
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -8,19 +8,14 @@ export default class MyDocument extends Document {
     return { ...page, ...styles }
   }
 
-  constructor(props) {
-    super(props)
-    const { __NEXT_DATA__, ids } = props
-    if (ids) {
-      __NEXT_DATA__.ids = ids
-    }
-  }
-
   render() {
     return (
       <html>
         <Head>
-          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+          <style
+            data-emotion-css={this.props.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
         </Head>
         <body>
           <Main />
