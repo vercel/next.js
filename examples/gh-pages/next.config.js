bcff94658b2068163ef0f5f74832ce191a483e69
Shu Uesugi <shu@chibicode.com>
2020-01-22 13:06:07 -0800

    Remove unnecessary exportPathMap from examples (#10218)
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- examples/gh-pages/next.config.js
+++ examples/gh-pages/next.config.js
@@ -5,11 +5,5 @@
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-    }
-  },
   assetPrefix: !debug ? '/Next-gh-page-example/' : '',
 }
