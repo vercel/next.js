30006435b2610ee0706eb35f27c3670a3678fc58
Joe Haddad <joe.haddad@zeit.co>
2020-01-22 14:57:19 -0500

    Turn on Scss support

--- test/integration/scss-fixtures/next.config.js
+++ test/integration/scss-fixtures/next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
+  experimental: { scss: true },
   webpack(cfg) {
     cfg.devtool = 'source-map'
     return cfg
