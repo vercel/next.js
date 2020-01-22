27694fd377a7800c2ea23d63d8f8b9822f30cf38
JJ Kasper <jj@jjsweb.site>
2020-01-22 12:34:36 -0600

    Add splitting by timings for Azure

--- run-tests.js
+++ run-tests.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const _glob = require('glob')
 const fs = require('fs-extra')
+const fetch = require('node-fetch')
 const { promisify } = require('util')
 const { Sema } = require('async-sema')
 const { spawn, exec: execOrig } = require('child_process')
@@ -11,6 +12,7 @@ const exec = promisify(execOrig)
 const NUM_RETRIES = 2
 const DEFAULT_CONCURRENCY = 2
 const timings = []
+const TIMINGS_API = 'http://next-timings.jjsweb.site/api/timings'
 
 ;(async () => {
   let concurrencyIdx = process.argv.indexOf('-c')
@@ -23,12 +25,25 @@ const timings = []
 
   console.log('Running tests with concurrency:', concurrency)
   let tests = process.argv.filter(arg => arg.endsWith('.test.js'))
+  let prevTimings
 
   if (tests.length === 0) {
     tests = await glob('**/*.test.js', {
       nodir: true,
       cwd: path.join(__dirname, 'test'),
     })
+
+    if (outputTimings) {
+      console.log('Fetching previous timings data')
+      const res = await fetch(TIMINGS_API)
+
+      if (res.ok) {
+        prevTimings = await res.json()
+        console.log('Fetched previous timings data')
+      } else {
+        console.log('Failed to fetch previous timings status:', res.status)
+      }
+    }
   }
 
   let testNames = [
@@ -49,9 +64,43 @@ const timings = []
     let offset = groupPos === 1 ? 0 : (groupPos - 1) * numPerGroup - 1
     // if there's an odd number of suites give the first group the extra
     if (testNames.length % 2 !== 0 && groupPos !== 1) offset++
-    testNames = testNames.splice(offset, numPerGroup)
-  }
 
+    if (prevTimings) {
+      const groups = [[]]
+      const groupTimes = [0]
+
+      for (const testName of testNames) {
+        let smallestGroup = groupTimes[0]
+        let smallestGroupIdx = 0
+
+        // get the samllest group time to add current one to
+        for (let i = 1; i < groupTotal; i++) {
+          if (!groups[i]) {
+            groups[i] = []
+            groupTimes[i] = 0
+          }
+
+          const time = groupTimes[i]
+          if (time < smallestGroup) {
+            smallestGroup = time
+            smallestGroupIdx = i
+          }
+        }
+        groups[smallestGroupIdx].push(testName)
+        groupTimes[smallestGroupIdx] += prevTimings[testName] || 1
+      }
+
+      const curGroupIdx = groupPos - 1
+      testNames = groups[curGroupIdx]
+
+      console.log(
+        'Current group previous accumulated times:',
+        Math.round(groupTimes[curGroupIdx]) + 's'
+      )
+    } else {
+      testNames = testNames.splice(offset, numPerGroup)
+    }
+  }
   console.log('Running tests:', '\n', ...testNames.map(name => `${name}\n`))
 
   const sema = new Sema(concurrency, { capacity: testNames.length })
@@ -61,7 +110,7 @@ const timings = []
   )
   const children = new Set()
 
-  const runTest = (test = '') =>
+  const runTest = (test = '', usePolling) =>
     new Promise((resolve, reject) => {
       const start = new Date().getTime()
       const child = spawn(
@@ -69,6 +118,17 @@ const timings = []
         [jestPath, '--runInBand', '--forceExit', '--verbose', test],
         {
           stdio: 'inherit',
+          env: {
+            ...process.env,
+            ...(usePolling
+              ? {
+                  // Events can be finicky in CI. This switches to a more reliable
+                  // polling method.
+                  CHOKIDAR_USEPOLLING: 'true',
+                  CHOKIDAR_INTERVAL: 500,
+                }
+              : {}),
+          },
         }
       )
       children.add(child)
@@ -86,7 +146,7 @@ const timings = []
 
       for (let i = 0; i < NUM_RETRIES + 1; i++) {
         try {
-          const time = await runTest(test)
+          const time = await runTest(test, i > 0)
           timings.push({
             file: test,
             time,
@@ -113,6 +173,8 @@ const timings = []
   )
 
   if (outputTimings) {
+    const timings = {}
+
     let junitData = `<testsuites name="jest tests">`
     /*
       <testsuite name="/__tests__/bar.test.js" tests="1" errors="0" failures="0" skipped="0" timestamp="2017-10-10T21:56:49" time="0.323">
@@ -123,6 +185,8 @@ const timings = []
 
     for (const timing of timings) {
       const timeInSeconds = timing.time / 1000
+      timings[timing.file] = timeInSeconds
+
       junitData += `
         <testsuite name="${timing.file}" file="${
         timing.file
@@ -137,5 +201,21 @@ const timings = []
     junitData += `</testsuites>`
     await fs.writeFile('test/junit.xml', junitData, 'utf8')
     console.log('output timing data to junit.xml')
+
+    if (prevTimings) {
+      const res = await fetch(TIMINGS_API, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ timings }),
+      })
+
+      if (res.ok) {
+        console.log('Posted current timings successfully')
+      } else {
+        console.log('Failed to post current timings status:', res.status)
+      }
+    }
   }
 })()
