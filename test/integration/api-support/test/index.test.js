31f0a4f2d897ca6339f6acad62de9fbcc0f2604c
JJ Kasper <jj@jjsweb.site>
2020-01-27 12:05:18 -0600

    Revert "Revert "Update test to handle EPIPE in test""
    This reverts commit efd2abe7c35c12bf78530064796d853c171efcf1.

--- test/integration/api-support/test/index.test.js
+++ test/integration/api-support/test/index.test.js
@@ -118,16 +118,30 @@ function runTests(dev = false) {
   })
 
   it('should return error exceeded body limit', async () => {
-    const data = await fetchViaHTTP(appPort, '/api/parse', null, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify(json),
-    })
+    let res
+    let error
 
-    expect(data.status).toEqual(413)
-    expect(data.statusText).toEqual('Body exceeded 1mb limit')
+    try {
+      res = await fetchViaHTTP(appPort, '/api/parse', null, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(json),
+      })
+    } catch (err) {
+      error = err
+    }
+
+    if (error) {
+      // This is a temporary workaround for testing since node doesn't handle
+      // closed connections when POSTing data to an endpoint correctly
+      // https://github.com/nodejs/node/issues/12339
+      expect(error.code).toBe('EPIPE')
+    } else {
+      expect(res.status).toEqual(413)
+      expect(res.statusText).toEqual('Body exceeded 1mb limit')
+    }
   })
 
   it('should parse bigger body then 1mb', async () => {
