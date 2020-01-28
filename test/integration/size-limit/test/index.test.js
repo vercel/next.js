63fbf1dfac7c8fd5ab3f73d89fd9688cac79d444
Tim Neutkens <timneutkens@me.com>
2020-01-28 12:08:47 +0100

    Update polyfills size

--- test/integration/size-limit/test/index.test.js
+++ test/integration/size-limit/test/index.test.js
@@ -81,7 +81,7 @@ describe('Production response size', () => {
     )
 
     // These numbers are without gzip compression!
-    const delta = responseSizeKilobytes - 235
+    const delta = responseSizeKilobytes - 275
     expect(delta).toBeLessThanOrEqual(0) // don't increase size
     expect(delta).toBeGreaterThanOrEqual(-1) // don't decrease size without updating target
   })
