127690e3b32126550308c208a40039f70e76d9ce
JJ Kasper <jj@jjsweb.site>
2020-01-27 22:48:52 -0600

    Update actions config for canary jobs (#10299)
    * Update actions config for canary jobs
    
    * Update exiting for mising value and size limit test
    
    * Update config
    
    * Update check
    
    * Update check
    
    * Remove extra check

--- publish-release.sh
+++ publish-release.sh
@@ -1,21 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-if
-  ls ~/.npmrc >/dev/null 2>&1 &&
-  [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
+git describe --exact-match
+
+if [[ ! $? -eq 0 ]];then
+  echo "Nothing to publish, exiting.."
+  exit 0;
+fi
+
+if [[ -z "$NPM_TOKEN" ]];then
+  echo "No NPM_TOKEN, exiting.."
+  exit 0;
+fi
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
+if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
-  # yarn run lerna publish from-git --npm-tag canary --yes
-  echo "publishing canary"
+  echo "Publishing canary"
+  yarn run lerna publish from-git --npm-tag canary --yes
 else
   echo "Did not publish canary"
 fi
 
-if
-  ls ~/.npmrc >/dev/null 2>&1 &&
-  [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
-then
-  # yarn run lerna publish from-git --yes
-  echo "publishing stable"
+if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];then
+  echo "Publishing stable"
+  yarn run lerna publish from-git --yes
 else
   echo "Did not publish stable"
 fi
