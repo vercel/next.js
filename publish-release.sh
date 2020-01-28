725dda1b68a06bde1b360bd3415368d5c69a9e60
JJ Kasper <jj@jjsweb.site>
2020-01-28 13:03:32 -0600

    Make sure to exit publish script with correct code (#10310)

--- publish-release.sh
+++ publish-release.sh
@@ -18,6 +18,11 @@ if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
   echo "Publishing canary"
   yarn run lerna publish from-git --npm-tag canary --yes
+
+  # Make sure to exit script with code 1 if publish failed
+  if [[ ! $? -eq 0 ]];then
+    exit 1;
+  fi
 else
   echo "Did not publish canary"
 fi
@@ -25,6 +30,11 @@ fi
 if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];then
   echo "Publishing stable"
   yarn run lerna publish from-git --yes
+
+  # Make sure to exit script with code 1 if publish failed
+  if [[ ! $? -eq 0 ]];then
+    exit 1;
+  fi
 else
   echo "Did not publish stable"
 fi
