b40c64d0a7ed556c1636b3112cd52e1846d8ada4
JJ Kasper <jj@jjsweb.site>
2020-01-27 21:40:40 -0600

    Update actions config for canary jobs

--- publish-release.sh
+++ publish-release.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-if
-  ls ~/.npmrc >/dev/null 2>&1 &&
-  [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
+git describe --exact-match
+
+if [[ ! $? -eq 0 ]];then
+  echo "Nothing to publish, exiting.."
+  exit 0;
+fi
+
+if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
-  # yarn run lerna publish from-git --npm-tag canary --yes
-  echo "publishing canary"
+  echo "Publishing canary"
+  yarn run lerna publish from-git --npm-tag canary --yes
 else
   echo "Did not publish canary"
 fi
 
-if
-  ls ~/.npmrc >/dev/null 2>&1 &&
-  [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
-then
-  # yarn run lerna publish from-git --yes
-  echo "publishing stable"
+if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];then
+  echo "Publishing stable"
+  yarn run lerna publish from-git --yes
 else
   echo "Did not publish stable"
 fi
