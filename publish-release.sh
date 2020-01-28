3e35fa801277c8e3eb621950b5f3d4df6c808105
JJ Kasper <jj@jjsweb.site>
2020-01-27 22:23:26 -0600

    Update check

--- publish-release.sh
+++ publish-release.sh
@@ -7,6 +7,13 @@ if [[ ! $? -eq 0 ]];then
   exit 0;
 fi
 
+if [[ -z "$NPM_TOKEN" ]];then
+  echo "No NPM_TOKEN, exiting.."
+  exit 0;
+fi
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
 if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
   echo "Publishing canary"
