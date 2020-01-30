157a7a7b8ccc10419a534656a0db775a37575e03
Tim Neutkens <tim@timneutkens.nl>
2020-01-30 13:26:26 +0100

    Upgrade with-carbon-components to built-in SCSS (#10321)
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- examples/with-carbon-components/pages/index.js
+++ examples/with-carbon-components/pages/index.js
@@ -1,8 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { Button } from 'carbon-components-react'
 
-import '../static/myCustomTheme.scss'
-
 export default class DemoApp extends Component {
   render() {
     return (
