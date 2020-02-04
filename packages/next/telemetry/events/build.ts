bbafde3d95856b5e4e8677e3bd884c34ee002612
Joe Haddad <joe.haddad@zeit.co>
2020-02-04 13:55:43 -0500

    Detect Invalid Pages Before Optimize (#10418)

--- packages/next/telemetry/events/build.ts
+++ packages/next/telemetry/events/build.ts
@@ -1,15 +1,34 @@
+const REGEXP_DIRECTORY_DUNDER = /[\\/]__[^\\/]+(?<![\\/]__(?:tests|mocks))__[\\/]/i
+const REGEXP_DIRECTORY_TESTS = /[\\/]__(tests|mocks)__[\\/]/i
+const REGEXP_FILE_TEST = /\.(?:spec|test)\.[^.]+$/i
+
 const EVENT_BUILD_DURATION = 'NEXT_BUILD_COMPLETED'
 type EventBuildCompleted = {
   durationInSeconds: number
   totalPageCount: number
+  hasDunderPages: boolean
+  hasTestPages: boolean
 }
 
-export function eventBuildDuration(
-  event: EventBuildCompleted
+export function eventBuildCompleted(
+  pagePaths: string[],
+  event: Omit<
+    EventBuildCompleted,
+    'totalPageCount' | 'hasDunderPages' | 'hasTestPages'
+  >
 ): { eventName: string; payload: EventBuildCompleted } {
   return {
     eventName: EVENT_BUILD_DURATION,
-    payload: event,
+    payload: {
+      ...event,
+      totalPageCount: pagePaths.length,
+      hasDunderPages: pagePaths.some(path =>
+        REGEXP_DIRECTORY_DUNDER.test(path)
+      ),
+      hasTestPages: pagePaths.some(
+        path => REGEXP_DIRECTORY_TESTS.test(path) || REGEXP_FILE_TEST.test(path)
+      ),
+    },
   }
 }
 
@@ -22,10 +41,6 @@ type EventBuildOptimized = {
   hasDunderPages: boolean
   hasTestPages: boolean
 }
-
-const REGEXP_DIRECTORY_DUNDER = /[\\/]__[^\\/]+(?<![\\/]__(?:tests|mocks))__[\\/]/i
-const REGEXP_DIRECTORY_TESTS = /[\\/]__(tests|mocks)__[\\/]/i
-const REGEXP_FILE_TEST = /\.(?:spec|test)\.[^.]+$/i
 
 export function eventBuildOptimize(
   pagePaths: string[],
