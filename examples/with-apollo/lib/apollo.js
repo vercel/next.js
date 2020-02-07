6f820be19ddd4f449d82093a4cf1523b0065a68d
handtrix <handtrix@gmail.com>
2020-02-07 16:26:50 +0100

    Remove Head.rewind() We can get rid of .rewind by now as the latest next/head no longer uses legacy context.

--- examples/with-apollo/lib/apollo.js
+++ examples/with-apollo/lib/apollo.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import App from 'next/app'
-import Head from 'next/head'
 import { ApolloProvider } from '@apollo/react-hooks'
 import createApolloClient from '../apolloClient'
 
@@ -153,10 +152,6 @@ export const withApollo = ({ ssr = false } = {}) => PageComponent => {
             // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
             console.error('Error while running `getDataFromTree`', error)
           }
-
-          // getDataFromTree does not call componentWillUnmount
-          // head side effect therefore need to be cleared manually
-          Head.rewind()
         }
       }
 
