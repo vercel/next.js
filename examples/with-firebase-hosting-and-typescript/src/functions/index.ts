92ca46644ae916598e9bdbc5f34769c69aac0dfd
fnwiya <fnwiya@gmail.com>
2020-02-08 15:58:01 +0900

    Fix Error: Could not find a valid build in the 'next' directory

--- examples/with-firebase-hosting-and-typescript/src/functions/index.ts
+++ examples/with-firebase-hosting-and-typescript/src/functions/index.ts
@@ -2,7 +2,8 @@ import * as functions from 'firebase-functions'
 import next from 'next'
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, conf: { distDir: 'next' } })
+const dir = __dirname
+const app = next({ dev, dir, conf: { distDir: 'next' } })
 const handle = app.getRequestHandler()
 
 export const nextApp = functions.https.onRequest((req, res) => {
