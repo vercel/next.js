a56e9f91206a2e6bad068c50e71383319174bdb7
Vinci Rufus <vinci.rufus@gmail.com>
2020-02-09 11:26:05 +0000

    link to the issue

--- examples/with-http2/next.config.js
+++ examples/with-http2/next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  // this needs to be set false unitl to a bug in the compression npm module gets fixed.
+  /*  this needs to be set false unitl to a bug in the compression npm module gets fixed. 
+reference: https://github.com/expressjs/compression/issues/122
+  */
   compress: false,
 }
