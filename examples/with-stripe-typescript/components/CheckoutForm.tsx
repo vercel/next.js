aa26963b22c8c7b89e0d616b3d6905e4d52b5947
Thor <thorsten-stripe@users.noreply.github.com>
2020-02-11 11:24:38 +0800

    Add Stripe TypeScript Example (#10482)
    Co-authored-by: Luis Alvarez D. <luis@zeit.co>

---
+++ examples/with-stripe-typescript/components/CheckoutForm.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+import CustomDonationInput from '../components/CustomDonationInput'
+
+import { fetchPostJSON } from '../utils/api-helpers'
+import { formatAmountForDisplay } from '../utils/stripe-helpers'
+import * as config from '../config'
+
+import { useStripe } from '@stripe/react-stripe-js'
+
+const CheckoutForm: React.FunctionComponent = () => {
+  const [input, setInput] = useState({ customDonation: config.MIN_AMOUNT })
+  const stripe = useStripe()
+
+  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = e =>
+    setInput({
+      ...input,
+      [e.currentTarget.name]: e.currentTarget.value,
+    })
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async e => {
+    e.preventDefault()
+    // Create a Checkout Session.
+    const response = await fetchPostJSON('/api/checkout_sessions', {
+      amount: input.customDonation,
+    })
+
+    if (response.statusCode === 500) {
+      console.error(response.message)
+      return
+    }
+
+    // Redirect to Checkout.
+    const { error } = await stripe!.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: response.id,
+    })
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CustomDonationInput
+        name={'customDonation'}
+        value={input.customDonation}
+        min={config.MIN_AMOUNT}
+        max={config.MAX_AMOUNT}
+        step={config.AMOUNT_STEP}
+        currency={config.CURRENCY}
+        onChange={handleInputChange}
+      />
+      <button type="submit" disabled={!stripe}>
+        Donate {formatAmountForDisplay(input.customDonation, config.CURRENCY)}
+      </button>
+    </form>
+  )
+}
+
+export default CheckoutForm
---
+++ examples/with-stripe-typescript/pages/api/payment_intents/index.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { CURRENCY, MIN_AMOUNT, MAX_AMOUNT } from '../../../config'
+import { formatAmountForStripe } from '../../../utils/stripe-helpers'
+
+import Stripe from 'stripe'
+const stripeSecretKey: string = process.env.STRIPE_SECRET_KEY!
+const stripe = new Stripe(stripeSecretKey, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: '2019-12-03',
+  typescript: true,
+  telemetry: true,
+})
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { amount }: { amount: number } = req.body
+    try {
+      // Validate the amount that was passed from the client.
+      if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
+        throw new Error('Invalid amount.')
+      }
+      // Create PaymentIntent from body params.
+      const params: Stripe.PaymentIntentCreateParams = {
+        payment_method_types: ['card'],
+        amount: formatAmountForStripe(amount, CURRENCY),
+        currency: CURRENCY,
+      }
+      const payment_intent: Stripe.PaymentIntent = await stripe.paymentIntents.create(
+        params
+      )
+
+      res.status(200).json(payment_intent)
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message })
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+}
