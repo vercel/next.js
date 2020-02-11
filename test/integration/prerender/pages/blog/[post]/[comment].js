f7cc57dc03c892478a9bf4fe326bb247317be449
JJ Kasper <jj@jjsweb.site>
2020-02-10 20:21:06 -0600

    Add failing SSG syntax

--- test/integration/prerender/pages/blog/[post]/[comment].js
+++ test/integration/prerender/pages/blog/[post]/[comment].js
@@ -23,20 +23,27 @@ export async function unstable_getStaticProps({ params }) {
   }
 }
 
-export default ({ post, comment, time }) => {
-  // we're in a loading state
-  if (!post) {
-    return <p>loading...</p>
-  }
+// Do not change this, it is making sure our SSG transform handles
+// export default in this way with a class component
+class Page extends React.Component {
+  render() {
+    const { post, comment, time } = this.props
+    // we're in a loading state
+    if (!post) {
+      return <p>loading...</p>
+    }
 
-  return (
-    <>
-      <p>Post: {post}</p>
-      <p>Comment: {comment}</p>
-      <span>time: {time}</span>
-      <Link href="/">
-        <a id="home">to home</a>
-      </Link>
-    </>
-  )
+    return (
+      <>
+        <p>Post: {post}</p>
+        <p>Comment: {comment}</p>
+        <span>time: {time}</span>
+        <Link href="/">
+          <a id="home">to home</a>
+        </Link>
+      </>
+    )
+  }
 }
+
+export default Page
