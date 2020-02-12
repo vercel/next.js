3013af41fed09f22a845f654f3d434c155a7ea79
Joe Haddad <joe.haddad@zeit.co>
2020-02-11 20:16:42 -0500

    SSG Preview Mode (#10459)
    * checkpoint: api impl
    
    * Add support for tryGetPreviewData
    
    * snapshot: server(less) support
    
    * Add X-Prerender-Bypass-Mode header support
    
    * Pass preview data to getStaticProps call
    
    * add TODO
    
    * setPreviewData
    
    * 100k iterations
    
    * Handle jwt error
    
    * Write out preview values
    
    * forgot file
    
    * set preview props
    
    * Send preview props
    
    * add preview props
    
    * Pass around more data
    
    * update yarn lock
    
    * Fail on Invalid Prerender Manifest
    
    * Make Missing Prerender Manifest Fatal
    
    * fix ts errors
    
    * fix test
    
    * Fix setting cookies + maxage
    
    * Secure is not needed as we encrypt necessary data
    
    * Set on domain root
    
    * Set cookie max ages
    
    * Render a fallback on-demand for non-dynamic pages
    
    * Test preview mode
    
    * remove old build
    
    * remove snapshots
    
    * Add serverless tests
    
    * use afterAll
    
    * Remove object assigns
    
    * fix cookie spread
    
    * add comment

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk'
 import ciEnvironment from 'ci-info'
+import crypto from 'crypto'
 import escapeStringRegexp from 'escape-string-regexp'
 import findUp from 'find-up'
 import fs from 'fs'
@@ -41,9 +42,11 @@ import {
   getSortedRoutes,
   isDynamicRoute,
 } from '../next-server/lib/router/utils'
+import { __ApiPreviewProps } from '../next-server/server/api-utils'
 import loadConfig, {
   isTargetLikeServerless,
 } from '../next-server/server/config'
+import { normalizePagePath } from '../next-server/server/normalize-page-path'
 import {
   eventBuildCompleted,
   eventBuildOptimize,
@@ -67,7 +70,6 @@ import {
 } from './utils'
 import getBaseWebpackConfig from './webpack-config'
 import { writeBuildId } from './write-build-id'
-import { normalizePagePath } from '../next-server/server/normalize-page-path'
 
 const fsAccess = promisify(fs.access)
 const fsUnlink = promisify(fs.unlink)
@@ -97,6 +99,7 @@ export type PrerenderManifest = {
   version: number
   routes: { [route: string]: SsgRoute }
   dynamicRoutes: { [route: string]: DynamicSsgRoute }
+  preview: __ApiPreviewProps
 }
 
 export default async function build(dir: string, conf = null): Promise<void> {
@@ -198,8 +201,20 @@ export default async function build(dir: string, conf = null): Promise<void> {
   const allStaticPages = new Set<string>()
   let allPageInfos = new Map<string, PageInfo>()
 
+  const previewProps: __ApiPreviewProps = {
+    previewModeId: crypto.randomBytes(16).toString('hex'),
+    previewModeSigningKey: crypto.randomBytes(32).toString('hex'),
+    previewModeEncryptionKey: crypto.randomBytes(32).toString('hex'),
+  }
+
   const mappedPages = createPagesMapping(pagePaths, config.pageExtensions)
-  const entrypoints = createEntrypoints(mappedPages, target, buildId, config)
+  const entrypoints = createEntrypoints(
+    mappedPages,
+    target,
+    buildId,
+    previewProps,
+    config
+  )
   const pageKeys = Object.keys(mappedPages)
   const dynamicRoutes = pageKeys.filter(page => isDynamicRoute(page))
   const conflictingPublicFiles: string[] = []
@@ -802,6 +817,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
       version: 1,
       routes: finalPrerenderRoutes,
       dynamicRoutes: finalDynamicRoutes,
+      preview: previewProps,
     }
 
     await fsWriteFile(
@@ -814,6 +830,7 @@ export default async function build(dir: string, conf = null): Promise<void> {
       version: 1,
       routes: {},
       dynamicRoutes: {},
+      preview: previewProps,
     }
     await fsWriteFile(
       path.join(distDir, PRERENDER_MANIFEST),
