3ac46ab03d5db21fdb065ef703bb7874f6a0d3ef
Luis Alvarez <luisito453@gmail.com>
2020-02-12 18:31:40 -0500

    Added signup page

---
+++ examples/with-passport/pages/signup.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import Layout from '../components/layout'
+import Form from '../components/form'
+
+const Signup = () => {
+  const [errorMsg, setErrorMsg] = useState('')
+
+  async function handleSubmit(e) {
+    event.preventDefault()
+
+    if (errorMsg) setErrorMsg('')
+
+    const body = {
+      username: e.currentTarget.username.value,
+      password: e.currentTarget.password.value,
+    }
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      if (response.status === 200) {
+        // const { token } = await response.json()
+        // await login({ token })
+      } else {
+        throw new Error(response.statusText)
+      }
+    } catch (error) {
+      console.error('An unexpected error happened occurred:', error)
+      setErrorMsg(error.message)
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="login">
+        <Form isLogin={false} errorMessage={errorMsg} onSubmit={handleSubmit} />
+      </div>
+      <style jsx>{`
+        .login {
+          max-width: 21rem;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Signup
