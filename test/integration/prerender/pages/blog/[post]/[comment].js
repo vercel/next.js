38758865fa03d52a362edfc1fbe6eeb91fac7f2a
Joe Haddad <joe.haddad@zeit.co>
2020-02-13 00:03:26 -0500

    Revert [comment].js

--- test/integration/prerender/pages/blog/[post]/[comment].js
+++ test/integration/prerender/pages/blog/[post]/[comment].js
@@ -23,27 +23,20 @@ export async function unstable_getStaticProps({ params }) {
   }
 }
 
-// Do not change this, it is making sure our SSG transform handles
-// export default in this way with a class component
-class Page extends React.Component {
-  render() {
-    const { post, comment, time } = this.props
-    // we're in a loading state
-    if (!post) {
-      return <p>loading...</p>
-    }
-
-    return (
-      <>
-        <p>Post: {post}</p>
-        <p>Comment: {comment}</p>
-        <span>time: {time}</span>
-        <Link href="/">
-          <a id="home">to home</a>
-        </Link>
-      </>
-    )
+export default ({ post, comment, time }) => {
+  // we're in a loading state
+  if (!post) {
+    return <p>loading...</p>
   }
-}
 
-export default Page
+  return (
+    <>
+      <p>Post: {post}</p>
+      <p>Comment: {comment}</p>
+      <span>time: {time}</span>
+      <Link href="/">
+        <a id="home">to home</a>
+      </Link>
+    </>
+  )
+}
