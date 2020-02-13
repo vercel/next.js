87cd1c0dde211bc8537c6a9072a93c9d9cd360ee
Luis Alvarez <luisito453@gmail.com>
2020-02-13 16:08:05 -0500

    Set the cookie

--- examples/with-passport/pages/api/login.js
+++ examples/with-passport/pages/api/login.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import passport from 'passport'
+import { serialize } from 'cookie'
 import { localStrategy } from '../../lib/password-login'
 
 const app = express()
@@ -24,7 +25,17 @@ passport.use(localStrategy)
 app.post('/api/login', async (req, res) => {
   try {
     const token = await authenticate('local', req, res)
-    console.log(token)
+    const maxAge = 60 * 60 * 8 // 8 hours
+    const cookie = serialize('token', token, {
+      maxAge,
+      expires: new Date(Date.now() + maxAge * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      sameSite: 'lax',
+    })
+
+    res.setHeader('Set-Cookie', cookie)
     res.status(200).send({ done: true })
   } catch (error) {
     console.error(error)
