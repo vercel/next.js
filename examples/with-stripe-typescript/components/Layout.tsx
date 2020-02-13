e22fedef8424541f94766347db3652bf7175946e
Thorsten Schaeff <thorsten@stripe.com>
2020-02-13 23:08:25 +0800

    Add twitter meta.

--- examples/with-stripe-typescript/components/Layout.tsx
+++ examples/with-stripe-typescript/components/Layout.tsx
@@ -19,6 +19,17 @@ const Layout: React.FunctionComponent<Props> = ({
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@thorwebdev" />
+      <meta name="twitter:title" content="TypeScript Next.js Stripe Example" />
+      <meta
+        name="twitter:description"
+        content="Full-stack TypeScript example using Next.js, react-stripe-js, and stripe-node."
+      />
+      <meta
+        name="twitter:image"
+        content="https://nextjs-typescript-react-stripe-js.now.sh/social_card.png"
+      />
     </Head>
     <div className="container">
       <header>
