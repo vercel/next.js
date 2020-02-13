71019e70d303380b275e8027c317452a1b076fa2
Joe Haddad <joe.haddad@zeit.co>
2020-02-13 17:54:25 -0500

    Remove `native-url` Again (#10526)
    * Remove `native-url` Again
    
    * update comment
    
    * update sizes

--- test/integration/size-limit/test/index.test.js
+++ test/integration/size-limit/test/index.test.js
@@ -80,7 +80,7 @@ describe('Production response size', () => {
     )
 
     // These numbers are without gzip compression!
-    const delta = responseSizesBytes - 226 * 1024
+    const delta = responseSizesBytes - 233 * 1024
     expect(delta).toBeLessThanOrEqual(1024) // don't increase size more than 1kb
     expect(delta).toBeGreaterThanOrEqual(-1024) // don't decrease size more than 1kb without updating target
   })
@@ -100,7 +100,7 @@ describe('Production response size', () => {
     )
 
     // These numbers are without gzip compression!
-    const delta = responseSizesBytes - 195 * 1024
+    const delta = responseSizesBytes - 201 * 1024
     expect(delta).toBeLessThanOrEqual(1024) // don't increase size more than 1kb
     expect(delta).toBeGreaterThanOrEqual(-1024) // don't decrease size more than 1kb without updating target
   })
