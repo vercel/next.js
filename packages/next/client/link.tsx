c5508c4f997ac33074b71c97a14c9427f2a79546
JJ Kasper <jj@jjsweb.site>
2020-02-18 14:52:42 -0600

    Update comment

--- packages/next/client/link.tsx
+++ packages/next/client/link.tsx
@@ -209,9 +209,10 @@ class Link extends Component<LinkProps> {
     if (!this.p || typeof window === 'undefined') return
     // Prefetch the JSON page if asked (only in the client)
     const [href, asPath] = this.getPaths()
-    // make sure to catch here since we should have an unhandledRejection
+    // make sure to catch here since we should handle an unhandledRejection
     // since we're doing this automatically and we don't want to reload the
-    // page while automatically prefetching for the user
+    // page while automatically prefetching for the user and this only occurs
+    // when doing an actual navigation
     Router.prefetch(href, asPath, options).catch(() => {})
     prefetched[href] = true
   }
