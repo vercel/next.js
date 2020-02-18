1beb836e6d572a1c3e51f29b9260faffefb20a99
Tim Neutkens <tim@timneutkens.nl>
2020-02-18 10:30:34 +0100

    Add extra profiling app (#10568)
    Just another app that I’ve been using for analysis

---
+++ test/integration/bundle-size-profiling/next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  experimental: {
+    polyfillsOptimization: true,
+  },
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.profile = true
+      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          generateStatsFile: true,
+          analyzerMode: 'static',
+          reportFilename: options.isServer
+            ? '../analyze/server.html'
+            : './analyze/client.html',
+        })
+      )
+    }
+
+    return config
+  },
+}
