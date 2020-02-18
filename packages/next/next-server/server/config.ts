ee4270815440b6dc9b7e60abfe62e25137716946
Tim Neutkens <tim@timneutkens.nl>
2020-02-18 14:35:12 +0100

    Enable scss/sass support (#10571)
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- packages/next/next-server/server/config.ts
+++ packages/next/next-server/server/config.ts
@@ -42,7 +42,7 @@ const defaultConfig: { [key: string]: any } = {
         (os.cpus() || { length: 1 }).length) - 1
     ),
     css: true,
-    scss: false,
+    scss: true,
     documentMiddleware: false,
     granularChunks: true,
     modern: false,
