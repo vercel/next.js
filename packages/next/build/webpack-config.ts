6dcf9baaa15d28637fdef37097fb37a10b6bdac8
Tim Neutkens <timneutkens@me.com>
2020-02-18 16:03:13 +0100

    Enable polyfillsOptimization

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -54,10 +54,7 @@ const escapePathVariables = (value: any) => {
     : value
 }
 
-function getOptimizedAliases(
-  isServer: boolean,
-  polyfillsOptimization: boolean
-): { [pkg: string]: string } {
+function getOptimizedAliases(isServer: boolean): { [pkg: string]: string } {
   if (isServer) {
     return {}
   }
@@ -68,12 +65,6 @@ function getOptimizedAliases(
   const shimAssign = path.join(__dirname, 'polyfills', 'object.assign')
   return Object.assign(
     {},
-    // Polyfill: Window#fetch
-    polyfillsOptimization
-      ? undefined
-      : {
-          __next_polyfill__fetch: require.resolve('whatwg-fetch'),
-        },
     {
       unfetch$: stubWindowFetch,
       'isomorphic-unfetch$': stubWindowFetch,
@@ -84,13 +75,6 @@ function getOptimizedAliases(
         'whatwg-fetch.js'
       ),
     },
-    polyfillsOptimization
-      ? undefined
-      : {
-          // Polyfill: Object.assign
-          __next_polyfill__object_assign: require.resolve('object-assign'),
-          '@babel/runtime-corejs2/core-js/object/assign': stubObjectAssign,
-        },
     {
       'object-assign$': stubObjectAssign,
 
@@ -168,7 +152,6 @@ export default async function getBaseWebpackConfig(
         babelPresetPlugins,
         hasModern: !!config.experimental.modern,
         development: dev,
-        polyfillsOptimization: !!config.experimental.polyfillsOptimization,
       },
     },
     // Backwards compat
@@ -215,9 +198,7 @@ export default async function getBaseWebpackConfig(
           ),
         [CLIENT_STATIC_FILES_RUNTIME_POLYFILLS]: path.join(
           NEXT_PROJECT_ROOT_DIST_CLIENT,
-          config.experimental.polyfillsOptimization
-            ? 'polyfills-nomodule.js'
-            : 'polyfills.js'
+          'polyfills.js'
         ),
       } as ClientEntries)
     : undefined
@@ -267,17 +248,7 @@ export default async function getBaseWebpackConfig(
       next: NEXT_PROJECT_ROOT,
       [PAGES_DIR_ALIAS]: pagesDir,
       [DOT_NEXT_ALIAS]: distDir,
-      ...getOptimizedAliases(
-        isServer,
-        !!config.experimental.polyfillsOptimization
-      ),
-
-      // Temporary to allow runtime-corejs2 to be stubbed in experimental polyfillsOptimization
-      ...(config.experimental.polyfillsOptimization
-        ? {
-            '@babel/runtime-corejs2': '@babel/runtime',
-          }
-        : undefined),
+      ...getOptimizedAliases(isServer),
     },
     mainFields: isServer ? ['main', 'module'] : ['browser', 'module', 'main'],
     plugins: [PnpWebpackPlugin],
@@ -528,11 +499,7 @@ export default async function getBaseWebpackConfig(
               !res.match(/next[/\\]dist[/\\]next-server[/\\]/) &&
               (res.match(/[/\\]next[/\\]dist[/\\]/) ||
                 // This is the @babel/plugin-transform-runtime "helpers: true" option
-                res.match(/node_modules[/\\]@babel[/\\]runtime[/\\]/) ||
-                (!config.experimental.polyfillsOptimization &&
-                  res.match(
-                    /node_modules[/\\]@babel[/\\]runtime-corejs2[/\\]/
-                  )))
+                res.match(/node_modules[/\\]@babel[/\\]runtime[/\\]/))
             ) {
               return callback()
             }
@@ -727,9 +694,6 @@ export default async function getBaseWebpackConfig(
         ),
         'process.env.__NEXT_MODERN_BUILD': JSON.stringify(
           config.experimental.modern && !dev
-        ),
-        'process.env.__NEXT_POLYFILLS_OPTIMIZATION': JSON.stringify(
-          !!config.experimental.polyfillsOptimization
         ),
         'process.env.__NEXT_GRANULAR_CHUNKS': JSON.stringify(
           config.experimental.granularChunks && !dev
