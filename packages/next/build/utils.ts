c62ed11911e29dedd008ec7f3091df442de980d6
JJ Kasper <jj@jjsweb.site>
2020-02-20 23:57:10 -0600

    Add error messages for dynamic SSG page without getStaticPaths (#10620)
    * Add error messages for dynamic SSG page without getStaticPaths
    
    * Update error check
    
    * Update test file
    
    * Update another test file
    
    * Adjust
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- packages/next/build/utils.ts
+++ packages/next/build/utils.ts
@@ -543,10 +543,19 @@ export async function isPageStatic(
       throw new Error(SERVER_PROPS_SSG_CONFLICT)
     }
 
+    const pageIsDynamic = isDynamicRoute(page)
     // A page cannot have static parameters if it is not a dynamic page.
-    if (hasStaticProps && hasStaticPaths && !isDynamicRoute(page)) {
+    if (hasStaticProps && hasStaticPaths && !pageIsDynamic) {
       throw new Error(
-        `unstable_getStaticPaths can only be used with dynamic pages. https://nextjs.org/docs#dynamic-routing`
+        `unstable_getStaticPaths can only be used with dynamic pages, not '${page}'.` +
+          `\nLearn more: https://nextjs.org/docs#dynamic-routing`
+      )
+    }
+
+    if (hasStaticProps && pageIsDynamic && !hasStaticPaths) {
+      throw new Error(
+        `unstable_getStaticPaths is required for dynamic SSG pages and is missing for '${page}'.` +
+          `\nRead more: https://err.sh/next.js/invalid-getstaticpaths-value`
       )
     }
 
