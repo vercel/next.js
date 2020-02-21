b916401ab1e7c67b42f9ddd1383345a6deab61a2
Joe Haddad <joe.haddad@zeit.co>
2020-02-21 01:16:07 -0500

    Revert "Re-Enable Native `url` Module (#10530)" (#10623)
    This reverts commit b329f6a2d6642f4618b6c9d3e97e5217d0faf98e.

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -88,8 +88,9 @@ function getOptimizedAliases(isServer: boolean): { [pkg: string]: string } {
       'object.assign/polyfill': path.join(shimAssign, 'polyfill.js'),
       'object.assign/shim': path.join(shimAssign, 'shim.js'),
 
-      // Replace: full URL polyfill with platform-based polyfill
-      url: require.resolve('native-url'),
+      // TODO: re-enable when `native-url` supports Safari 10
+      // // Replace: full URL polyfill with platform-based polyfill
+      // url: require.resolve('native-url'),
     }
   )
 }
