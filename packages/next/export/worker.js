301242b696db5b53d19cce4204d36cfc8875c35c
Joe Haddad <joe.haddad@zeit.co>
2020-02-26 16:59:04 -0500

    Adjust error message

--- packages/next/export/worker.js
+++ packages/next/export/worker.js
@@ -264,7 +264,7 @@ export default async function({
     return results
   } catch (error) {
     console.error(
-      `\nError occurred prerendering page "${path}" https://err.sh/next.js/prerender-error:\n` +
+      `\nError occurred prerendering page "${path}". Read more: https://err.sh/next.js/prerender-error:\n` +
         error
     )
     return { ...results, error: true }
