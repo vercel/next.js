ed561d71eb79fc515ea621ee84e3cc1a741065db
Luis Alvarez <luis@zeit.co>
2020-02-29 16:51:16 -0500

    Updated _app components and static files

--- examples/with-takeshape-graphql/src/components/footer/footer.jsx
+++ examples/with-takeshape-graphql/src/components/footer/footer.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
 import baseTheme from '../../base.module.css'
 import theme from './footer.module.css'
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className={theme.footer}>
-        <div className={baseTheme.container}>
-          <small>
-            &copy;{new Date().getFullYear()}
-            <a href="https://www.takeshape.io" rel="nofollow">
-              {' '}
-              TakeShape Inc.
-            </a>
-          </small>
-        </div>
-      </footer>
-    )
-  }
+export default function Footer() {
+  return (
+    <footer className={theme.footer}>
+      <div className={baseTheme.container}>
+        <small>
+          &copy;{new Date().getFullYear()}
+          <a
+            target="_blank"
+            href="https://www.takeshape.io"
+            rel="noopener noreferrer"
+          >
+            {' '}
+            TakeShape Inc.
+          </a>
+        </small>
+      </div>
+    </footer>
+  )
 }
