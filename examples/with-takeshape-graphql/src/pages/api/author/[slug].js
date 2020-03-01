97c644f3680c97f8dd227a2c66ae29bc808657b6
Luis Alvarez <luis@zeit.co>
2020-03-01 13:02:37 -0500

    Removed the takeshape provider and api pages

--- examples/with-takeshape-graphql/src/pages/api/author/[slug].js
+++
@@ -1,9 +0,0 @@
-import { authorQuery } from '../../author/[slug].js'
-import { getTakeShapeData } from '../index'
-
-export default async (req, res) => {
-  const {
-    query: { slug },
-  } = req
-  return getTakeShapeData(req, res, authorQuery(slug))
-}
--- examples/with-takeshape-graphql/src/pages/api/posts/index.js
+++
@@ -1,6 +0,0 @@
-import { postListQuery } from '../../posts'
-import { getTakeShapeData } from '../index'
-
-export default async (req, res) => {
-  return getTakeShapeData(req, res, postListQuery)
-}
