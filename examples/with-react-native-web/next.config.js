1b7e78821c5c4919a20329604b7adc79c7657450
Joe Haddad <joe.haddad@zeit.co>
2020-03-01 12:31:06 -0500

    lint-fix

--- examples/with-react-native-web/next.config.js
+++ examples/with-react-native-web/next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  webpack: (config) => {
+  webpack: config => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web',
     }
     config.resolve.extensions = [
-      ".web.js",
-      ".web.ts",
-      ".web.tsx",
+      '.web.js',
+      '.web.ts',
+      '.web.tsx',
       ...config.resolve.extensions,
-    ];
+    ]
     return config
   },
 }
