589717b16a034bf919f4b1b3e23f42d96d8307f0
Shelton <sheltonli@tencent.com>
2020-03-01 17:02:27 +0800

    lint fix

--- examples/with-antd-mobile-less/next.config.js
+++ examples/with-antd-mobile-less/next.config.js
@@ -1,95 +1,105 @@
 /* eslint-disable */
-const withLess = require('./less-config');
-const lessToJS = require('less-vars-to-js');
-const path = require('path');
-const fs = require('fs');
-const resolve = require('resolve');
+const withLess = require('./less-config')
+const lessToJS = require('less-vars-to-js')
+const path = require('path')
+const fs = require('fs')
+const resolve = require('resolve')
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-    fs.readFileSync(path.resolve(__dirname, './less-config/default.less'), 'utf8')
+  fs.readFileSync(path.resolve(__dirname, './less-config/default.less'), 'utf8')
 )
 
 if (typeof require !== 'undefined') {
-    require.extensions['.less'] = () => { }
-    require.extensions['.css'] = () => { }
+  require.extensions['.less'] = () => {}
+  require.extensions['.css'] = () => {}
 }
 
 module.exports = withLess({
-    cssModules: true,
-    cssLoaderOptions: {
-        url: false,
-        modules: {
-            localIdentName: '[local]-[hash:base64:4]'
-        },
-        import: true
+  cssModules: true,
+  cssLoaderOptions: {
+    url: false,
+    modules: {
+      localIdentName: '[local]-[hash:base64:4]'
     },
-    lessLoaderOptions: {
-        javascriptEnabled: true,
-        modifyVars: themeVariables
-    },
-    webpack: (config, options) => {
+    import: true
+  },
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables
+  },
+  webpack: (config, options) => {
+    const { isServer, dir } = options
 
-        const { isServer, dir } = options;
+    if (isServer) {
+      const antStyles = /antd-mobile\/.*?\/style.*?/
 
-        if (isServer) {
-            const antStyles = /antd-mobile\/.*?\/style.*?/;
+      config.externals = [
+        (_, request, callback) => {
+          const notExternalModules = [
+            'next/app',
+            'next/document',
+            'next/link',
+            'next/error',
+            'string-hash',
+            'next/constants'
+          ]
 
-            config.externals = [
-                (_, request, callback) => {
-                    const notExternalModules = [
-                        'next/app', 'next/document', 'next/link', 'next/error',
-                        'string-hash',
-                        'next/constants'
-                    ]
+          if (notExternalModules.indexOf(request) !== -1) {
+            return callback()
+          }
 
-                    if (notExternalModules.indexOf(request) !== -1) {
-                        return callback()
-                    }
+          resolve(
+            request,
+            { baseDir: dir, preserveSymlinks: true },
+            (err, res) => {
+              if (err) {
+                return callback()
+              }
 
-                    resolve(request, { baseDir: dir, preserveSymlinks: true }, (err, res) => {
+              if (!res) {
+                return callback()
+              }
 
-                        if (err) {
-                            return callback()
-                        }
+              if (
+                res.match(/next[/\\]dist[/\\]/) ||
+                res.match(/node_modules[/\\]@babel[/\\]runtime[/\\]/) ||
+                res.match(/node_modules[/\\]@babel[/\\]runtime-corejs2[/\\]/)
+              ) {
+                return callback()
+              }
 
-                        if (!res) {
-                            return callback();
-                        }
+              if (
+                res.match(/node_modules[/\\]webpack/) ||
+                res.match(/node_modules[/\\]css-loader/)
+              ) {
+                return callback()
+              }
 
-                        if (res.match(/next[/\\]dist[/\\]/) || res.match(/node_modules[/\\]@babel[/\\]runtime[/\\]/) || res.match(/node_modules[/\\]@babel[/\\]runtime-corejs2[/\\]/)) {
-                            return callback()
-                        }
+              if (res.match(/node_modules[/\\]styled-jsx/)) {
+                return callback()
+              }
 
-                        if (res.match(/node_modules[/\\]webpack/) || res.match(/node_modules[/\\]css-loader/)) {
-                            return callback()
-                        }
+              if (
+                res.match(/node_modules[/\\].*\.js/) &&
+                !res.match(/node_modules[/\\]webpack/) &&
+                !res.match(antStyles)
+              ) {
+                return callback(null, `commonjs ${request}`)
+              }
 
-                        if (res.match(/node_modules[/\\]styled-jsx/)) {
-                            return callback()
-                        }
-
-                        if (
-                            res.match(/node_modules[/\\].*\.js/)
-                            && !res.match(/node_modules[/\\]webpack/)
-                            && !res.match(antStyles)
-                        ) {
-                            return callback(null, `commonjs ${request}`);
-                        }
-
-                        callback();
-
-                    })
-
-                },
-            ];
-
-            config.module.rules.unshift({
-                test: antStyles,
-                use: 'null-loader',
-            });
+              callback()
+            }
+          )
         }
+      ]
 
-        return config;
-    },
-});
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader'
+      })
+    }
+
+    return config
+  }
+})
