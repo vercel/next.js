16672a435391928eb3dcffa74a3a93a93bff9447
Prateek Bhatnagar <prateek89born@gmail.com>
2020-03-02 13:53:18 -0800

    Adding conformance checks (#10314)
    * adding tests  for rect sync conformance check
    
    * adding test for react sync script conformance check
    
    * reverting yarn lock changes
    
    * adding duplicate polyfill conformance
    
    * bug fixes in dulpicate polyfill conformance check
    
    * adding settings capability to conformance plugin
    
    * removing minification check from server build
    
    * bug fix
    
    * settings for react sync script check

---
+++ packages/next/build/webpack/plugins/webpack-conformance-plugin/utils/ast-utils.ts
@@ -0,0 +1,25 @@
+import { types } from 'recast'
+import { namedTypes } from 'ast-types'
+
+export function isNodeCreatingScriptElement(node: namedTypes.CallExpression) {
+  const callee = node.callee as namedTypes.Identifier
+  if (callee.type !== 'Identifier') {
+    return false
+  }
+  const componentNode = node.arguments[0] as namedTypes.Literal
+  if (componentNode.type !== 'Literal') {
+    return false
+  }
+  // Next has pragma: __jsx.
+  return callee.name === '__jsx' && componentNode.value === 'script'
+}
+
+export function reducePropsToObject(
+  propsNode: types.namedTypes.ObjectExpression
+) {
+  return propsNode.properties.reduce((originalProps, prop: any) => {
+    // @ts-ignore
+    originalProps[prop.key.name] = prop.value.value
+    return originalProps
+  }, {})
+}
