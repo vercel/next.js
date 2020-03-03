4ef1cd4ebe574700baafbd603d940fc2467818cb
JJ Kasper <jj@jjsweb.site>
2020-03-03 12:39:08 -0600

    Add identifier to NEXT_DATA for gs(s)p (#10812)
    * Add identifier to NEXT_DATA for gs(s)p
    
    * Apply suggestions from code review
    
    Co-Authored-By: Joe Haddad <joe.haddad@zeit.co>
    
    * fix lint
    
    * apply lint fix
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- test/integration/getserversideprops/test/index.test.js
+++ test/integration/getserversideprops/test/index.test.js
@@ -191,6 +191,18 @@ const runTests = (dev = false) => {
     expect(html).toMatch(/Post:.*?post-1/)
   })
 
+  it('should have gssp in __NEXT_DATA__', async () => {
+    const html = await renderViaHTTP(appPort, '/')
+    const $ = cheerio.load(html)
+    expect(JSON.parse($('#__NEXT_DATA__').text()).gssp).toBe(true)
+  })
+
+  it('should not have gssp in __NEXT_DATA__ for non-GSSP page', async () => {
+    const html = await renderViaHTTP(appPort, '/normal')
+    const $ = cheerio.load(html)
+    expect('gssp' in JSON.parse($('#__NEXT_DATA__').text())).toBe(false)
+  })
+
   it('should supply query values SSR', async () => {
     const html = await renderViaHTTP(appPort, '/blog/post-1?hello=world')
     const $ = cheerio.load(html)
