d964dd71e1af0a93939d064facf1c8d064b41a3e
Rifai <abdelrahman.rifai@gmail.com>
2020-03-04 14:37:44 +0100

    fix: remove minimum-scale from default viewport

--- packages/next/next-server/lib/head.tsx
+++ packages/next/next-server/lib/head.tsx
@@ -12,10 +12,7 @@ export function defaultHead(inAmpMode = false) {
   const head = [<meta charSet="utf-8" />]
   if (!inAmpMode) {
     head.push(
-      <meta
-        name="viewport"
-        content="width=device-width,minimum-scale=1,initial-scale=1"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     )
   }
   return head
