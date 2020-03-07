8ee5bf80461fe66cd26865a1125f330243d4f627
Shu Uesugi <shu@chibicode.com>
2020-03-06 23:36:33 -0800

    Fix height issue

--- examples/cms-datocms/pages/article.js
+++ examples/cms-datocms/pages/article.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default () => {
   return (
-    <>
+    <div className="min-h-screen bg-accent-1">
       <div className="bg-accent-1 border-b border-accent-2">
         <div className="container mx-auto px-5 py-2 text-center text-sm">
           The source code for this blog is{' '}
@@ -15,134 +15,123 @@ export default () => {
           .
         </div>
       </div>
-      <div className="container mx-auto px-5 mt-8">
-        <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20">
-          <Link href="/">
-            <a className="hover:underline">Blog</a>
-          </Link>
-          .
-        </h2>
-        <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
-          Learn How to Pre-render Pages Using Static Generation with Next.js
-        </h1>
-        <div className="hidden md:flex items-center md:mb-12">
-          <img
-            src="/images/author.jpg"
-            className="w-12 h-12 rounded-full mr-4 grayscale"
-          />
-          <div className="text-xl font-bold">Shu Uesugi</div>
-        </div>
-        <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
-          <img src="/images/image.jpg" className="shadow-magical" />
-        </div>
-        <div className="max-w-2xl mx-auto mb-28">
-          <div className="flex md:hidden items-center mb-6">
+      <div className="bg-white py-px">
+        <div className="container mx-auto px-5 mt-8">
+          <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20">
+            <Link href="/">
+              <a className="hover:underline">Blog</a>
+            </Link>
+            .
+          </h2>
+          <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+            Deploying Next.js Apps
+          </h1>
+          <div className="hidden md:flex items-center md:mb-12">
             <img
               src="/images/author.jpg"
               className="w-12 h-12 rounded-full mr-4 grayscale"
             />
             <div className="text-xl font-bold">Shu Uesugi</div>
           </div>
-          <div className="text-lg mb-6">March 10, 2020</div>
-          <p className="my-6 text-lg leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent
-            elementum facilisis leo vel fringilla est ullamcorper eget. At
-            imperdiet dui accumsan sit amet nulla facilisi morbi tempus.
-            Praesent elementum facilisis leo vel fringilla. Congue mauris
-            rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra
-            massa massa ultricies.
-          </p>
-          <p className="my-6 text-lg leading-relaxed">
-            Venenatis cras sed felis eget velit. Consectetur libero id faucibus
-            nisl tincidunt. Gravida in fermentum et sollicitudin ac orci
-            phasellus egestas tellus. Volutpat consequat mauris nunc congue nisi
-            vitae. Id aliquet risus feugiat in ante metus dictum at tempor. Sed
-            blandit libero volutpat sed cras. Sed odio morbi quis commodo odio
-            aenean sed adipiscing. Velit euismod in pellentesque massa placerat.
-            Mi bibendum neque egestas congue quisque egestas diam in arcu. Nisi
-            lacus sed viverra tellus in. Nibh cras pulvinar mattis nunc sed.
-            Luctus accumsan tortor posuere ac ut consequat semper viverra.
-            Fringilla ut morbi tincidunt augue interdum velit euismod.
-          </p>
-          <h2 className="text-3xl mt-12 mb-4 leading-snug">Lorem Ipsum</h2>
-          <p className="my-6 text-lg leading-relaxed">
-            Tristique senectus et netus et malesuada fames ac turpis. Ridiculus
-            mus mauris vitae ultricies leo integer malesuada nunc vel. In mollis
-            nunc sed id semper. Egestas tellus rutrum tellus pellentesque.
-            Phasellus vestibulum lorem sed risus ultricies tristique nulla. Quis
-            blandit turpis cursus in hac habitasse platea dictumst quisque. Eros
-            donec ac odio tempor orci dapibus ultrices. Aliquam sem et tortor
-            consequat id porta nibh. Adipiscing elit duis tristique sollicitudin
-            nibh sit amet commodo nulla. Diam vulputate ut pharetra sit amet. Ut
-            tellus elementum sagittis vitae et leo. Arcu non odio euismod
-            lacinia at quis risus sed vulputate.
-          </p>
-          <p className="my-6 text-lg leading-relaxed">
-            Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Eu turpis
-            egestas pretium aenean. Diam phasellus vestibulum lorem sed risus.
-            Amet venenatis urna cursus eget nunc scelerisque viverra.
-            Pellentesque habitant morbi tristique senectus et netus et. Luctus
-            venenatis lectus magna fringilla urna. Interdum posuere lorem ipsum
-            dolor. Nullam vehicula ipsum a arcu cursus vitae congue mauris.
-            Tempor nec feugiat nisl pretium fusce id velit. Semper viverra nam
-            libero justo laoreet sit amet cursus sit. A lacus vestibulum sed
-            arcu non odio euismod lacinia. Fermentum dui faucibus in ornare.
-            Imperdiet sed euismod nisi porta. Pulvinar sapien et ligula
-            ullamcorper malesuada proin. Mauris rhoncus aenean vel elit
-            scelerisque mauris pellentesque. Eget egestas purus viverra
-            accumsan. Pellentesque id nibh tortor id aliquet.
-          </p>
-        </div>
-        <hr className="border-accent-2 mb-24" />
-        <div className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-          More Stories
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
-          <div>
-            <div className="-mx-5 sm:mx-0">
-              <img src="/images/image.jpg" className="mb-5 shadow-magical" />
-            </div>
-            <h3 className="text-3xl mb-3 leading-snug">
-              Preview Mode for Static Generation
-            </h3>
-            <div className="text-lg mb-4">March 10, 2020</div>
-            <p className="text-lg mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
-              <img
-                src="/images/author.jpg"
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-              />
-              <div className="text-xl font-bold">Shu Uesugi</div>
-            </div>
+          <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
+            <img src="/images/image.jpg" className="shadow-magical" />
           </div>
-          <div>
-            <div className="-mx-5 sm:mx-0">
-              <img src="/images/image.jpg" className="mb-5 shadow-magical" />
-            </div>
-            <h3 className="text-3xl mb-3 leading-snug">
-              Dynamic Routing and Static Generation
-            </h3>
-            <div className="text-lg mb-4">March 10, 2020</div>
-            <p className="text-lg mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
+          <div className="max-w-2xl mx-auto mb-28">
+            <div className="flex md:hidden items-center mb-6">
               <img
                 src="/images/author.jpg"
                 className="w-12 h-12 rounded-full mr-4 grayscale"
               />
               <div className="text-xl font-bold">Shu Uesugi</div>
+            </div>
+            <div className="text-lg mb-6">March 10, 2020</div>
+            <p className="my-6 text-lg leading-relaxed">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Praesent elementum facilisis leo vel fringilla est ullamcorper
+              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+              tempus. Praesent elementum facilisis leo vel fringilla. Congue
+              mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra
+              pharetra massa massa ultricies.
+            </p>
+            <p className="my-6 text-lg leading-relaxed">
+              Venenatis cras sed felis eget velit. Consectetur libero id
+              faucibus nisl tincidunt. Gravida in fermentum et sollicitudin ac
+              orci phasellus egestas tellus. Volutpat consequat mauris nunc
+              congue nisi vitae. Id aliquet risus feugiat in ante metus dictum
+              at tempor. Sed blandit libero volutpat sed cras. Sed odio morbi
+              quis commodo odio aenean sed adipiscing. Velit euismod in
+              pellentesque massa placerat. Mi bibendum neque egestas congue
+              quisque egestas diam in arcu. Nisi lacus sed viverra tellus in.
+              Nibh cras pulvinar mattis nunc sed. Luctus accumsan tortor posuere
+              ac ut consequat semper viverra. Fringilla ut morbi tincidunt augue
+              interdum velit euismod.
+            </p>
+            <h2 className="text-3xl mt-12 mb-4 leading-snug">Lorem Ipsum</h2>
+            <p className="my-6 text-lg leading-relaxed">
+              Tristique senectus et netus et malesuada fames ac turpis.
+              Ridiculus mus mauris vitae ultricies leo integer malesuada nunc
+              vel. In mollis nunc sed id semper. Egestas tellus rutrum tellus
+              pellentesque. Phasellus vestibulum lorem sed risus ultricies
+              tristique nulla. Quis blandit turpis cursus in hac habitasse
+              platea dictumst quisque. Eros donec ac odio tempor orci dapibus
+              ultrices. Aliquam sem et tortor consequat id porta nibh.
+              Adipiscing elit duis tristique sollicitudin nibh sit amet commodo
+              nulla. Diam vulputate ut pharetra sit amet. Ut tellus elementum
+              sagittis vitae et leo. Arcu non odio euismod lacinia at quis risus
+              sed vulputate.
+            </p>
+          </div>
+          <hr className="border-accent-2 mb-24" />
+          <div className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+            More Stories
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+            <div>
+              <div className="-mx-5 sm:mx-0">
+                <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+              </div>
+              <h3 className="text-3xl mb-3 leading-snug">
+                Preview Mode for Static Generation
+              </h3>
+              <div className="text-lg mb-4">March 10, 2020</div>
+              <p className="text-lg mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
+            </div>
+            <div>
+              <div className="-mx-5 sm:mx-0">
+                <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+              </div>
+              <h3 className="text-3xl mb-3 leading-snug">
+                Dynamic Routing and Static Generation
+              </h3>
+              <div className="text-lg mb-4">March 10, 2020</div>
+              <p className="text-lg mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
             </div>
           </div>
         </div>
@@ -170,6 +159,6 @@ export default () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
--- examples/cms-datocms/pages/index.js
+++ examples/cms-datocms/pages/index.js
@@ -1,6 +1,6 @@
 export default () => {
   return (
-    <>
+    <div className="min-h-screen bg-accent-1">
       <div className="bg-accent-1 border-b border-accent-2">
         <div className="container mx-auto px-5 py-2 text-center text-sm">
           The source code for this blog is{' '}
@@ -13,150 +13,153 @@ export default () => {
           .
         </div>
       </div>
-      <div className="container mx-auto px-5 mt-16">
-        <div className="flex-col md:flex-row flex items-center md:justify-between mb-16 md:mb-12">
-          <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-            Blog.
-          </h1>
-          <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-            A statically generated blog example using{' '}
-            <a
-              href="https://nextjs.org/"
-              className="underline hover:text-success duration-200 transition-colors"
-            >
-              Next.js
-            </a>{' '}
-            and{' '}
-            <a
-              href="https://www.datocms.com/"
-              className="underline hover:text-success duration-200 transition-colors"
-            >
-              DatoCMS
-            </a>
-            .
-          </h4>
-        </div>
-        <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
-          <img src="/images/image.jpg" className="shadow-magical" />
-        </div>
-        <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
-          <div>
-            <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-              Learn How to Pre-render Pages Using Static Generation with Next.js
-            </h3>
-            <div className="text-lg mb-4 md:mb-0">March 10, 2020</div>
+      <div className="bg-white py-px">
+        <div className="container mx-auto px-5 mt-16">
+          <div className="flex-col md:flex-row flex items-center md:justify-between mb-16 md:mb-12">
+            <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+              Blog.
+            </h1>
+            <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+              A statically generated blog example using{' '}
+              <a
+                href="https://nextjs.org/"
+                className="underline hover:text-success duration-200 transition-colors"
+              >
+                Next.js
+              </a>{' '}
+              and{' '}
+              <a
+                href="https://www.datocms.com/"
+                className="underline hover:text-success duration-200 transition-colors"
+              >
+                DatoCMS
+              </a>
+              .
+            </h4>
           </div>
-          <div>
-            <p className="text-lg leading-relaxed mb-5">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
-              <img
-                src="/images/author.jpg"
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-              />
-              <div className="text-xl font-bold">Shu Uesugi</div>
-            </div>
+          <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
+            <img src="/images/image.jpg" className="shadow-magical" />
           </div>
-        </div>
-        <div className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-          More Stories
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
-          <div>
-            <div className="-mx-5 sm:mx-0">
-              <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+          <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+            <div>
+              <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+                Learn How to Pre-render Pages Using Static Generation with
+                Next.js
+              </h3>
+              <div className="text-lg mb-4 md:mb-0">March 10, 2020</div>
             </div>
-            <h3 className="text-3xl mb-3 leading-snug">
-              Preview Mode for Static Generation
-            </h3>
-            <div className="text-lg mb-4">March 10, 2020</div>
-            <p className="text-lg mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
-              <img
-                src="/images/author.jpg"
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-              />
-              <div className="text-xl font-bold">Shu Uesugi</div>
+            <div>
+              <p className="text-lg leading-relaxed mb-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
             </div>
           </div>
-          <div>
-            <div className="-mx-5 sm:mx-0">
-              <img src="/images/image.jpg" className="mb-5 shadow-magical" />
-            </div>
-            <h3 className="text-3xl mb-3 leading-snug">
-              Dynamic Routing and Static Generation
-            </h3>
-            <div className="text-lg mb-4">March 10, 2020</div>
-            <p className="text-lg mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
-              <img
-                src="/images/author.jpg"
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-              />
-              <div className="text-xl font-bold">Shu Uesugi</div>
-            </div>
+          <div className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+            More Stories
           </div>
-          <div>
-            <div className="-mx-5 sm:mx-0">
-              <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+          <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+            <div>
+              <div className="-mx-5 sm:mx-0">
+                <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+              </div>
+              <h3 className="text-3xl mb-3 leading-snug">
+                Preview Mode for Static Generation
+              </h3>
+              <div className="text-lg mb-4">March 10, 2020</div>
+              <p className="text-lg mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
             </div>
-            <h3 className="text-3xl mb-3 leading-snug">
-              Deploying Next.js apps
-            </h3>
-            <div className="text-lg mb-4">March 10, 2020</div>
-            <p className="text-lg mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
-              <img
-                src="/images/author.jpg"
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-              />
-              <div className="text-xl font-bold">Shu Uesugi</div>
+            <div>
+              <div className="-mx-5 sm:mx-0">
+                <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+              </div>
+              <h3 className="text-3xl mb-3 leading-snug">
+                Dynamic Routing and Static Generation
+              </h3>
+              <div className="text-lg mb-4">March 10, 2020</div>
+              <p className="text-lg mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="-mx-5 sm:mx-0">
-              <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+            <div>
+              <div className="-mx-5 sm:mx-0">
+                <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+              </div>
+              <h3 className="text-3xl mb-3 leading-snug">
+                Deploying Next.js Apps
+              </h3>
+              <div className="text-lg mb-4">March 10, 2020</div>
+              <p className="text-lg mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
             </div>
-            <h3 className="text-3xl mb-3 leading-snug">
-              From Server-side Rendering to Static Generation
-            </h3>
-            <div className="text-lg mb-4">March 10, 2020</div>
-            <p className="text-lg mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Praesent elementum facilisis leo vel fringilla est ullamcorper
-              eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
-              tempus.
-            </p>
-            <div className="flex items-center">
-              <img
-                src="/images/author.jpg"
-                className="w-12 h-12 rounded-full mr-4 grayscale"
-              />
-              <div className="text-xl font-bold">Shu Uesugi</div>
+            <div>
+              <div className="-mx-5 sm:mx-0">
+                <img src="/images/image.jpg" className="mb-5 shadow-magical" />
+              </div>
+              <h3 className="text-3xl mb-3 leading-snug">
+                From Server-side Rendering to Static Generation
+              </h3>
+              <div className="text-lg mb-4">March 10, 2020</div>
+              <p className="text-lg mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Praesent elementum facilisis leo vel fringilla est ullamcorper
+                eget. At imperdiet dui accumsan sit amet nulla facilisi morbi
+                tempus.
+              </p>
+              <div className="flex items-center">
+                <img
+                  src="/images/author.jpg"
+                  className="w-12 h-12 rounded-full mr-4 grayscale"
+                />
+                <div className="text-xl font-bold">Shu Uesugi</div>
+              </div>
             </div>
           </div>
         </div>
@@ -184,6 +187,6 @@ export default () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
