ca993f9aeeb55d43d5adc99a7a4c904669d0cb1d
Shu Uesugi <shu@chibicode.com>
2020-03-08 14:48:33 -0700

    Add favicon

--- examples/cms-datocms/components/layout.js
+++ examples/cms-datocms/components/layout.js
@@ -1,9 +1,11 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
+import Meta from '../components/meta'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Meta />
       <div className="min-h-screen">
         <Alert />
         <main>{children}</main>
