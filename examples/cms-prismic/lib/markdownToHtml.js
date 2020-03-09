f1b16478b0575e9dffef65f5c26d24b2e8dd144a
Tim Neutkens <timneutkens@me.com>
2020-03-09 15:30:28 +0100

    Update package.json

--- examples/cms-prismic/lib/markdownToHtml.js
+++
@@ -1,9 +0,0 @@
-import remark from 'remark'
-import html from 'remark-html'
-
-export default async function markdownToHtml(markdown) {
-  const result = await remark()
-    .use(html)
-    .processSync(markdown)
-  return result.toString()
-}
