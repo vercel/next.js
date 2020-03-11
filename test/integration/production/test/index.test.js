bb98f00de1f5817ed537af49fb7f8919b3a1e1e4
JJ Kasper <jj@jjsweb.site>
2020-03-11 14:08:43 -0500

    Add ie11 test for bad Promise

--- test/integration/production/test/index.test.js
+++ test/integration/production/test/index.test.js
@@ -55,6 +55,13 @@ describe('Production Usage', () => {
       expect(html).toMatch(/Hello World/)
     })
 
+    if (browserName === 'internet explorer') {
+      it('should handle bad Promise polyfill', async () => {
+        const browser = await webdriver(appPort, '/bad-promise')
+        expect(await browser.eval('window.didRender')).toBe(true)
+      })
+    }
+
     it('should allow etag header support', async () => {
       const url = `http://localhost:${appPort}/`
       const etag = (await fetch(url)).headers.get('ETag')
