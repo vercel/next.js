9943334d44e20707b597985df3ffc2c63d94d058
JJ Kasper <jj@jjsweb.site>
2020-03-12 13:24:44 -0500

    Update RegExp test and remove extra script (#11006)
    Co-authored-by: Joe Haddad <joe.haddad@zeit.co>

--- test/integration/production/pages/regexp-polyfill.js
+++ test/integration/production/pages/regexp-polyfill.js
@@ -1,12 +1,23 @@
-export default () => {
+import * as React from 'react'
+
+const Home = () => {
   if (typeof window !== 'undefined') {
     window.didRender = true
   }
 
+  React.useEffect(() => {
+    // If this script is loaded before the polyfills it will
+    // still fail
+    const el = document.createElement('script')
+    el.src = '/regexp-test.js'
+    document.querySelector('body').appendChild(el)
+  }, [])
+
   return (
     <>
       <p>hi</p>
-      <script src="/regexp-test.js" />
     </>
   )
 }
+
+export default Home
