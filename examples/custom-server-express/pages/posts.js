f4b27d2a0159e9a2d8e85a005a40381e587ba82d
“kosuke_matano” <kosuke_matano@cyberagent.co.jp>
2020-03-13 12:06:06 +0900

    use getServerSideProps

--- examples/custom-server-express/pages/posts.js
+++ examples/custom-server-express/pages/posts.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class extends Component {
-  static getInitialProps({ query: { id } }) {
-    return { postId: id }
-  }
+export default function (props) {
+  return (
+    <div>
+      <h1>My {props.id} blog post</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  )
+}
 
-  render() {
-    return (
-      <div>
-        <h1>My blog post #{this.props.postId}</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
-    )
-  }
+export async function getServerSideProps({ query: { id } }) {
+  return { props: { id } }
 }
