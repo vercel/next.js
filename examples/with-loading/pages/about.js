1851cbc8473faf1edeabf3d012db475655a74608
Luis Alvarez <luis@zeit.co>
2020-03-13 16:27:55 -0500

    Updated example

--- examples/with-loading/pages/about.js
+++ examples/with-loading/pages/about.js
@@ -1,8 +1,6 @@
-import React from 'react'
-
 const AboutPage = () => <p>This is about Next.js!</p>
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   await new Promise(resolve => {
     setTimeout(resolve, 500)
   })
--- examples/with-loading/pages/forever.js
+++ examples/with-loading/pages/forever.js
@@ -1,8 +1,6 @@
-import React from 'react'
-
 const ForeverPage = () => <p>This page was rendered for a while!</p>
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   await new Promise(resolve => {
     setTimeout(resolve, 3000)
   })
