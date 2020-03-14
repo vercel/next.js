861d5e33ac69ea34ad94e4c76a026475965f72ca
Andres Cabana <andrescabana86@gmail.com>
2020-03-13 22:36:58 -0300

    feat: adding bundle analyzer options
    Feature Request:
    The previous version of bundle analyzer had the configuration at the nextConfig object.
    Adding this to the initializer to accept custom configuration for webpack-bundle-analyzer

--- packages/next-bundle-analyzer/index.js
+++ packages/next-bundle-analyzer/index.js
@@ -1,16 +1,31 @@
-module.exports = ({ enabled = true } = {}) => (nextConfig = {}) => {
+module.exports = ({ enabled = true, ...bundleAnalyzerOptions } = {}) => (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       if (enabled) {
-        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-        config.plugins.push(
-          new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            reportFilename: options.isServer
-              ? '../analyze/server.html'
-              : './analyze/client.html',
-          })
-        )
+        // compatible with 'next-bundle-analyzer options
+        const { analyzeServer, analyzeBrowser } = bundleAnalyzerOptions
+        const {
+          bundleAnalyzerConfig: { browser = {}, server = {} } = {}
+        } = bundleAnalyzerOptions
+        
+        const { isServer } = options
+
+        if ((isServer && analyzeServer) || (!isServer && analyzeBrowser)) {
+          const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+          config.plugins.push(
+            new BundleAnalyzerPlugin(
+              Object.assign(
+                {},
+                {
+                  analyzerMode: 'server',
+                  analyzerPort: isServer ? 8888 : 8889,
+                  openAnalyzer: isServer ? server.openAnalyzer : browser.openAnalyzer
+                },
+                isServer ? server : browser
+              )
+            )
+          )
+        }
       }
 
       if (typeof nextConfig.webpack === 'function') {
