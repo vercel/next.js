68eacafa67202511cc5ca5a91f3a6b923a7855cf
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-03-15 13:07:38 +0530

    Update with-typescript example to SSG

--- examples/with-typescript/pages/users/[id].tsx
+++ examples/with-typescript/pages/users/[id].tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { NextPageContext } from 'next'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
 import { User } from '../../interfaces'
 import Layout from '../../components/Layout'
@@ -11,19 +11,7 @@ type Props = {
   errors?: string
 }
 
-class InitialPropsDetail extends React.Component<Props> {
-  static getInitialProps = async ({ query }: NextPageContext) => {
-    try {
-      const { id } = query
-      const item = await sampleFetchWrapper(
-        `http://localhost:3000/api/users/${Array.isArray(id) ? id[0] : id}`
-      )
-      return { item }
-    } catch (err) {
-      return { errors: err.message }
-    }
-  }
-
+class StaticPropsDetail extends React.Component<Props> {
   render() {
     const { item, errors } = this.props
 
@@ -49,4 +37,36 @@ class InitialPropsDetail extends React.Component<Props> {
   }
 }
 
-export default InitialPropsDetail
+export const getStaticPaths: GetStaticPaths = async () => {
+  const users: User[] = await sampleFetchWrapper(
+    'http://localhost:3000/api/users'
+  )
+
+  // Get the paths we want to pre-render based on users
+  const paths = users.map(user => ({
+    params: { id: user.id.toString() },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    const id = params?.id
+    const item = await sampleFetchWrapper(
+      `http://localhost:3000/api/users/${Array.isArray(id) ? id[0] : id}`
+    )
+    // By returning { props: item }, the StaticPropsDetail component
+    // will receive `item` as a prop at build time
+    return { props: { item } }
+  } catch (err) {
+    return { props: { errors: err.message } }
+  }
+}
+
+export default StaticPropsDetail
--- examples/with-typescript/pages/users/index.tsx
+++ examples/with-typescript/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 import Link from 'next/link'
 
 import Layout from '../../components/Layout'
@@ -8,16 +8,15 @@ import { sampleFetchWrapper } from '../../utils/sample-api'
 
 type Props = {
   items: User[]
-  pathname: string
 }
 
-const WithInitialProps: NextPage<Props> = ({ items, pathname }) => (
+const WithStaticProps: NextPage<Props> = ({ items }) => (
   <Layout title="Users List | Next.js + TypeScript Example">
     <h1>Users List</h1>
     <p>
-      Example fetching data from inside <code>getInitialProps()</code>.
+      Example fetching data from inside <code>getStaticProps()</code>.
     </p>
-    <p>You are currently on: {pathname}</p>
+    <p>You are currently on: /users</p>
     <List items={items} />
     <p>
       <Link href="/">
@@ -27,15 +26,16 @@ const WithInitialProps: NextPage<Props> = ({ items, pathname }) => (
   </Layout>
 )
 
-WithInitialProps.getInitialProps = async ({ pathname }) => {
-  // Example for including initial props in a Next.js function component page.
+export const getStaticProps: GetStaticProps = async () => {
+  // Example for including static props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
+
   const items: User[] = await sampleFetchWrapper(
     'http://localhost:3000/api/users'
   )
 
-  return { items, pathname }
+  return { props: { items } }
 }
 
-export default WithInitialProps
+export default WithStaticProps
