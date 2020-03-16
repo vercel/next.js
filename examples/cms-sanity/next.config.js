570ab8c43b55662959fc2c9046dac9183aac1c25
Shu Uesugi <shu@chibicode.com>
2020-03-15 21:27:58 -0700

    Fix environment variable names

--- examples/cms-sanity/next.config.js
+++ examples/cms-sanity/next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    SANITY_PROJECT_ID: process.env.NEXT_EXAMPLE_CMS_SANITY_PROJECT_ID,
+    NEXT_EXAMPLE_CMS_SANITY_PROJECT_ID:
+      process.env.NEXT_EXAMPLE_CMS_SANITY_PROJECT_ID,
   },
 }
