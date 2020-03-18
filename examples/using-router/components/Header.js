98f86bd4efddbf60cc695d8b0245012da457e8ff
Tim Neutkens <timneutkens@me.com>
2020-03-18 11:25:07 +0100

    Update using-router to use useRouter

--- examples/using-router/components/Header.js
+++ examples/using-router/components/Header.js
@@ -1,4 +1,4 @@
-import Router from 'next/router'
+import { useRouter } from 'next/router'
 
 export default () => (
   <div>
@@ -7,24 +7,25 @@ export default () => (
   </div>
 )
 
-// typically you want to use `next/link` for this usecase
-// but this example shows how you can also access the router
-// and use it manually
-
-function onClickHandler(href) {
-  return e => {
-    e.preventDefault()
-    Router.push(href)
-  }
+const Link = ({ children, href }) => {
+  const router = useRouter()
+  return (
+    <a
+      href="#"
+      onClick={e => {
+        e.preventDefault()
+        // typically you want to use `next/link` for this usecase
+        // but this example shows how you can also access the router
+        // and use it manually
+        router.push(href)
+      }}
+    >
+      {children}
+      <style jsx>{`
+        a {
+          margin-right: 10px;
+        }
+      `}</style>
+    </a>
+  )
 }
-
-const Link = ({ children, href }) => (
-  <a href="#" onClick={onClickHandler(href)}>
-    {children}
-    <style jsx>{`
-      a {
-        margin-right: 10px;
-      }
-    `}</style>
-  </a>
-)
