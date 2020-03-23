f19dcf9b6e623415eb192694d51b3fdbcb2c8c90
Joe Haddad <joe.haddad@zeit.co>
2020-03-23 18:41:35 -0400

    Add Numeric Separator Support for TypeScript (#11308)
    * Add Numeric Separator Support for TypeScript
    
    * Check for number in render

---
+++ test/integration/typescript-numeric-sep-exclusive/test/index.test.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+/* global jasmine */
+import { nextBuild } from 'next-test-utils'
+import { join } from 'path'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
+
+const appDir = join(__dirname, '../')
+
+describe('TypeScript Exclusivity of Numeric Separator', () => {
+  it('should fail to build for a JavaScript file', async () => {
+    const { code, stderr } = await nextBuild(appDir, [], {
+      stderr: true,
+    })
+
+    expect(code).toBe(1)
+
+    expect(stderr).toContain('Failed to compile.')
+    expect(stderr).toContain('SyntaxError:')
+    expect(stderr).toContain('Identifier directly after number')
+  })
+})
