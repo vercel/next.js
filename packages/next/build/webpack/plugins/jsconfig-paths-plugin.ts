7fce52b90539203a8f9e9f5f1423397660d5a8f5
Tim Neutkens <tim@timneutkens.nl>
2020-03-23 15:45:51 +0100

    Add support for paths in tsconfig.json and jsconfig.json (#11293)
    * Add support for tsconfig/json `paths` option
    
    * Add tests for paths in tsconfig.json
    
    * Don't apply aliases when paths is empty
    
    * Clean up unused methods and link to TypeScript license
    
    * Add tests for jsconfig
    
    * Put feature under an experimental flag
    
    * Enable to see if tests pass
    
    * Update types
    
    * Add feature under an experimental flag

---
+++ packages/next/build/webpack/plugins/jsconfig-paths-plugin.ts
@@ -0,0 +1,209 @@
+/**
+ * This webpack resolver is largely based on TypeScript's "paths" handling
+ * The TypeScript license can be found here:
+ * https://github.com/microsoft/TypeScript/blob/214df64e287804577afa1fea0184c18c40f7d1ca/LICENSE.txt
+ */
+import { ResolvePlugin } from 'webpack'
+import { join } from 'path'
+
+export interface Pattern {
+  prefix: string
+  suffix: string
+}
+
+const asterisk = 0x2a
+
+export function hasZeroOrOneAsteriskCharacter(str: string): boolean {
+  let seenAsterisk = false
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) === asterisk) {
+      if (!seenAsterisk) {
+        seenAsterisk = true
+      } else {
+        // have already seen asterisk
+        return false
+      }
+    }
+  }
+  return true
+}
+
+export function tryParsePattern(pattern: string): Pattern | undefined {
+  // This should be verified outside of here and a proper error thrown.
+  const indexOfStar = pattern.indexOf('*')
+  return indexOfStar === -1
+    ? undefined
+    : {
+        prefix: pattern.substr(0, indexOfStar),
+        suffix: pattern.substr(indexOfStar + 1),
+      }
+}
+
+function isPatternMatch({ prefix, suffix }: Pattern, candidate: string) {
+  return (
+    candidate.length >= prefix.length + suffix.length &&
+    candidate.startsWith(prefix) &&
+    candidate.endsWith(suffix)
+  )
+}
+
+/** Return the object corresponding to the best pattern to match `candidate`. */
+export function findBestPatternMatch<T>(
+  values: readonly T[],
+  getPattern: (value: T) => Pattern,
+  candidate: string
+): T | undefined {
+  let matchedValue: T | undefined
+  // use length of prefix as betterness criteria
+  let longestMatchPrefixLength = -1
+
+  for (const v of values) {
+    const pattern = getPattern(v)
+    if (
+      isPatternMatch(pattern, candidate) &&
+      pattern.prefix.length > longestMatchPrefixLength
+    ) {
+      longestMatchPrefixLength = pattern.prefix.length
+      matchedValue = v
+    }
+  }
+
+  return matchedValue
+}
+
+/**
+ * patternStrings contains both pattern strings (containing "*") and regular strings.
+ * Return an exact match if possible, or a pattern match, or undefined.
+ * (These are verified by verifyCompilerOptions to have 0 or 1 "*" characters.)
+ */
+export function matchPatternOrExact(
+  patternStrings: readonly string[],
+  candidate: string
+): string | Pattern | undefined {
+  const patterns: Pattern[] = []
+  for (const patternString of patternStrings) {
+    if (!hasZeroOrOneAsteriskCharacter(patternString)) continue
+    const pattern = tryParsePattern(patternString)
+    if (pattern) {
+      patterns.push(pattern)
+    } else if (patternString === candidate) {
+      // pattern was matched as is - no need to search further
+      return patternString
+    }
+  }
+
+  return findBestPatternMatch(patterns, _ => _, candidate)
+}
+
+/**
+ * Tests whether a value is string
+ */
+export function isString(text: unknown): text is string {
+  return typeof text === 'string'
+}
+
+/**
+ * Given that candidate matches pattern, returns the text matching the '*'.
+ * E.g.: matchedText(tryParsePattern("foo*baz"), "foobarbaz") === "bar"
+ */
+export function matchedText(pattern: Pattern, candidate: string): string {
+  return candidate.substring(
+    pattern.prefix.length,
+    candidate.length - pattern.suffix.length
+  )
+}
+
+export function patternText({ prefix, suffix }: Pattern): string {
+  return `${prefix}*${suffix}`
+}
+
+const NODE_MODULES_REGEX = /node_modules/
+
+type Paths = { [match: string]: string[] }
+
+/**
+ * Handles tsconfig.json or jsconfig.js "paths" option for webpack
+ * Largely based on how the TypeScript compiler handles it:
+ * https://github.com/microsoft/TypeScript/blob/1a9c8197fffe3dace5f8dca6633d450a88cba66d/src/compiler/moduleNameResolver.ts#L1362
+ */
+export class JsConfigPathsPlugin implements ResolvePlugin {
+  paths: Paths
+  resolvedBaseUrl: string
+  constructor(paths: Paths, resolvedBaseUrl: string) {
+    this.paths = paths
+
+    this.resolvedBaseUrl = resolvedBaseUrl
+  }
+  apply(resolver: any) {
+    const paths = this.paths
+    const pathsKeys = Object.keys(paths)
+
+    // If no aliases are added bail out
+    if (pathsKeys.length === 0) {
+      return
+    }
+
+    const baseDirectory = this.resolvedBaseUrl
+    const target = resolver.ensureHook('resolve')
+    resolver
+      .getHook('described-resolve')
+      .tapPromise(
+        'JsConfigPathsPlugin',
+        async (request: any, resolveContext: any) => {
+          // Exclude node_modules from paths support (speeds up resolving)
+          if (request.path.match(NODE_MODULES_REGEX)) {
+            return
+          }
+
+          const moduleName = request.request
+
+          // If the module name does not match any of the patterns in `paths` we hand off resolving to webpack
+          const matchedPattern = matchPatternOrExact(pathsKeys, moduleName)
+          if (!matchedPattern) {
+            return
+          }
+
+          const matchedStar = isString(matchedPattern)
+            ? undefined
+            : matchedText(matchedPattern, moduleName)
+          const matchedPatternText = isString(matchedPattern)
+            ? matchedPattern
+            : patternText(matchedPattern)
+
+          let triedPaths = []
+
+          for (const subst of paths[matchedPatternText]) {
+            const path = matchedStar ? subst.replace('*', matchedStar) : subst
+            const candidate = join(baseDirectory, path)
+            const [err, result] = await new Promise((resolve, reject) => {
+              const obj = Object.assign({}, request, {
+                request: candidate,
+              })
+              resolver.doResolve(
+                target,
+                obj,
+                `Aliased with tsconfig.json or jsconfig.json ${matchedPatternText} to ${candidate}`,
+                resolveContext,
+                (err: any, result: any | undefined) => {
+                  resolve([err, result])
+                }
+              )
+            })
+
+            // There's multiple paths values possible, so we first have to iterate them all first before throwing an error
+            if (err || result === undefined) {
+              triedPaths.push(candidate)
+              continue
+            }
+
+            return result
+          }
+
+          throw new Error(`
+      Request "${moduleName}" matched tsconfig.json or jsconfig.json "paths" pattern ${matchedPatternText} but could not be resolved.
+      Tried paths: ${triedPaths.join(' ')}
+      `)
+        }
+      )
+  }
+}
