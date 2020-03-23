0ed59ea88d0afcfa98e7725ec92f184d0cd15998
Tim Neutkens <timneutkens@me.com>
2020-03-23 14:55:27 +0100

    Add feature under an experimental flag

--- packages/next/next-server/server/config.ts
+++ packages/next/next-server/server/config.ts
@@ -41,7 +41,7 @@ const defaultConfig: { [key: string]: any } = {
       (Number(process.env.CIRCLE_NODE_TOTAL) ||
         (os.cpus() || { length: 1 }).length) - 1
     ),
-    jsconfigPaths: true,
+    jsconfigPaths: false,
     css: true,
     scss: true,
     documentMiddleware: false,
--- test/integration/jsconfig-paths/next.config.js
+++ test/integration/jsconfig-paths/next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  experimental: {
+    jsconfigPaths: true,
+  },
   onDemandEntries: {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
