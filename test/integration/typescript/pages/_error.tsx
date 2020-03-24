6d4816bf677ad7eeaaa906f249913bfa9876aa2d
Antoni Kepinski <xxczaki@pm.me>
2020-03-25 00:25:15 +0100

    lint

--- test/integration/typescript/pages/_error.tsx
+++ test/integration/typescript/pages/_error.tsx
@@ -2,7 +2,7 @@ import { NextPageContext } from 'next'
 import ErrorComponent from 'next/error'
 
 class CustomError extends ErrorComponent {
-  static getInitialProps({ res }: NextPageContext) {
+  static async getInitialProps({ res }: NextPageContext) {
     const statusCode = 500
     return { statusCode, title: 'CustomError' }
   }
