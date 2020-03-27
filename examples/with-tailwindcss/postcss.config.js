6035f321f99903cbb26d71832c86578d42a826e5
Paul van Dyk <39598117+PaulPCIO@users.noreply.github.com>
2020-03-27 16:50:21 +0100

    Update postcss.config.js
    remove extra operator

--- examples/with-tailwindcss/postcss.config.js
+++ examples/with-tailwindcss/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     'tailwindcss',
-      ...(process.env.NODE_ENV === 'production' ?
+      ...(process.env.NODE_ENV === 'production'
       ? [
           [
             '@fullhuman/postcss-purgecss',
