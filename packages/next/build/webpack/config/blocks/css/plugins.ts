af40b5b2f86d7dc1a6d693fc82d29076f99fd7df
Guy Bedford <guybedford@gmail.com>
2020-03-28 17:43:52 -0600

    chalk, ci-info

--- packages/next/build/webpack/config/blocks/css/plugins.ts
+++ packages/next/build/webpack/config/blocks/css/plugins.ts
@@ -1,4 +1,4 @@
-import chalk from 'chalk'
+import chalk from 'next/dist/compiled/chalk'
 import { findConfig } from '../../../../../lib/find-config'
 import { resolveRequest } from '../../../../../lib/resolve-request'
 import browserslist from 'next/dist/compiled/browserslist'
