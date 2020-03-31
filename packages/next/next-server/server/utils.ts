b8d075ef0e68b8407644b17813c510b4e30fd9ff
Tim Neutkens <tim@timneutkens.nl>
2020-03-31 17:53:50 +0200

    Update environment support (#11524)
    * Remove req.env
    
    * Rename NEXT_APP_ to NEXT_PUBLIC_
    
    * Remove key exposing env config
    
    * Update tests
    
    * Update NEXT_APP_ test to NEXT_PUBLIC_

--- packages/next/next-server/server/utils.ts
+++ packages/next/next-server/server/utils.ts
@@ -1,5 +1,4 @@
 import { BLOCKED_PAGES } from '../lib/constants'
-import { Env } from '../../lib/load-env-config'
 
 export function isBlockedPage(pathname: string): boolean {
   return BLOCKED_PAGES.indexOf(pathname) !== -1
@@ -14,29 +13,4 @@ export function cleanAmpPath(pathname: string): string {
   }
   pathname = pathname.replace(/\?$/, '')
   return pathname
-}
-
-export function collectEnv(page: string, env: Env, pageEnv?: string[]): Env {
-  const missingEnvKeys = new Set()
-  const collected = pageEnv
-    ? pageEnv.reduce((prev: Env, key): Env => {
-        if (typeof env[key] !== 'undefined') {
-          prev[key] = env[key]!
-        } else {
-          missingEnvKeys.add(key)
-        }
-        return prev
-      }, {})
-    : {}
-
-  if (missingEnvKeys.size > 0) {
-    console.warn(
-      `Missing env value${missingEnvKeys.size === 1 ? '' : 's'}: ${[
-        ...missingEnvKeys,
-      ].join(', ')} for ${page}.\n` +
-        `Make sure to supply this value in either your .env file or in your environment.\n` +
-        `See here for more info: https://err.sh/next.js/missing-env-value`
-    )
-  }
-  return collected
 }
