2e17a9712b1622edab160673b4e39897cc99f6b7
Vincent Voyer <vincent@codeagain.com>
2020-04-01 10:12:38 +0200

    Remove passing of NODE_OPTIONS='--inspect' to subprocesses (#11041)
    * fix(debugging): do not pass NODE_OPTIONS='--inspect' to subprocesses
    
    fixes #11030
    
    * fix(debugger): use a regex to remove bad NODE_OPTIONS flags
    
    Co-authored-by: Alec Larson <alec.stanford.larson@gmail.com>

--- packages/next/server/lib/utils.ts
+++ packages/next/server/lib/utils.ts
@@ -8,3 +8,8 @@ export function printAndExit(message: string, code = 1) {
 
   process.exit(code)
 }
+
+export function getNodeOptionsWithoutInspect() {
+  const NODE_INSPECT_RE = /--inspect(-brk)?(=\S+)? ?/
+  return (process.env.NODE_OPTIONS || '').replace(NODE_INSPECT_RE, '')
+}
