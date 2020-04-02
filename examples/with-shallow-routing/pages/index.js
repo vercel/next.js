a1f4a57f7cf1ac1804f5d0c2705989c760d8dee5
Luis Alvarez <luis@zeit.co>
2020-04-02 17:18:59 -0500

    Updated page and readme

--- examples/with-shallow-routing/pages/index.js
+++ examples/with-shallow-routing/pages/index.js
@@ -1,60 +1,37 @@
-import React from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
-import Router, { withRouter } from 'next/router'
 import { format } from 'url'
 
-let counter = 1
-
-class Index extends React.Component {
-  reload() {
-    const { pathname, query } = Router
-    Router.push(format({ pathname, query }))
-  }
-
-  incrementStateCounter() {
-    const { router } = this.props
-    const currentCounter = router.query.counter
-      ? parseInt(router.query.counter)
-      : 0
-    const href = `/?counter=${currentCounter + 1}`
-    Router.push(href, href, { shallow: true })
-  }
-
-  render() {
-    const { initialPropsCounter, router } = this.props
-
-    return (
-      <div>
-        <h2>This is the Home Page</h2>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <button onClick={() => this.reload()}>Reload</button>
-        <button onClick={() => this.incrementStateCounter()}>
-          Change State Counter
-        </button>
-        <p>"getServerSideProps" ran for "{initialPropsCounter}" times.</p>
-        <p>Counter: "{router.query.counter || 0}".</p>
-      </div>
-    )
-  }
-}
+let counter = 0
 
 export async function getServerSideProps({ res }) {
-  if (res) {
-    return {
-      props: {
-        initialPropsCounter: 1,
-      },
-    }
-  } else {
-    counter++
-    return {
-      props: {
-        initialPropsCounter: counter,
-      },
-    }
-  }
+  counter++
+  return { props: { initialPropsCounter: counter } }
 }
 
-export default withRouter(Index)
+export default function Index({ initialPropsCounter }) {
+  const router = useRouter()
+  const { pathname, query } = router
+  const reload = () => {
+    router.push(format({ pathname, query }))
+  }
+  const incrementCounter = () => {
+    const currentCounter = query.counter ? parseInt(query.counter) : 0
+    const href = `/?counter=${currentCounter + 1}`
+
+    router.push(href, href, { shallow: true })
+  }
+
+  return (
+    <div>
+      <h2>This is the Home Page</h2>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <button onClick={reload}>Reload</button>
+      <button onClick={incrementCounter}>Change State Counter</button>
+      <p>"getServerSideProps" ran for "{initialPropsCounter}" times.</p>
+      <p>Counter: "{query.counter || 0}".</p>
+    </div>
+  )
+}
