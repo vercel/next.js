f70e8aec4d0f18795cae234d24a9117693323d1c
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2020-04-04 21:12:49 -0700

    feat(examples/with-next-sass): demo global '@use' statements

--- examples/with-next-sass/pages/index.js
+++ examples/with-next-sass/pages/index.js
@@ -2,6 +2,9 @@ import HelloWorld from '../components/hello-world'
 
 export default () => (
   <div className="app">
+    <div className="mdc-typography mdc-typography--headline1">
+      This is a headline1.
+    </div>
     <HelloWorld />
   </div>
 )
