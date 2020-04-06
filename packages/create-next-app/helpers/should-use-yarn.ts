8e86bbebf723418a548a6e2d933a4103eda89603
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-04-06 13:02:20 +0530

    [create-next-app] update logic for shouldUseYarn function (#11683)
    Co-authored-by: Joe Haddad <joe.haddad@zeit.co>

--- packages/create-next-app/helpers/should-use-yarn.ts
+++ packages/create-next-app/helpers/should-use-yarn.ts
@@ -2,6 +2,10 @@ import { execSync } from 'child_process'
 
 export function shouldUseYarn() {
   try {
+    const userAgent = process.env.npm_config_user_agent
+    if (userAgent) {
+      return Boolean(userAgent && userAgent.startsWith('yarn'))
+    }
     execSync('yarnpkg --version', { stdio: 'ignore' })
     return true
   } catch (e) {
