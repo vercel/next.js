7694567fd3c5cbfa772f75f39e955c5f9be5f148
JJ Kasper <jj@jjsweb.site>
2020-04-07 12:17:00 -0500

    Update test file

--- test/integration/production/pages/next-import.js
+++ test/integration/production/pages/next-import.js
@@ -1,4 +1,8 @@
 import Link from 'next/link'
+// Leave below import, testing that importing "next"
+// doesn't stall the build
+// eslint-disable-next-line no-unused-vars
+import next from 'next'
 
 export default () => (
   <div>
