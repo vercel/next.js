007ccfb8e03212de9c44414a777636a3b5391523
David Janda <djanda97@gmail.com>
2020-04-06 17:50:10 -0700

    Removed unnecessary curly braces

--- examples/with-mobx-state-tree/pages/index.js
+++ examples/with-mobx-state-tree/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SampleComponent from '../components/SampleComponent'
 
 const Index = () => {
-  return <SampleComponent title={'Index Page'} linkTo={'/other'} />
+  return <SampleComponent title="Index Page" linkTo="/other" />
 }
 
 export default Index
--- examples/with-mobx-state-tree/pages/other.js
+++ examples/with-mobx-state-tree/pages/other.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SampleComponent from '../components/SampleComponent'
 
 const Other = () => {
-  return <SampleComponent title={'Other Page'} linkTo={'/'} />
+  return <SampleComponent title="Other Page" linkTo="/" />
 }
 
 export default Other
