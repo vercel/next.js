5a786bdfd80a3f5edf779867692418163251ec1f
Luis Alvarez D <luis@zeit.co>
2020-04-08 02:44:52 -0500

    Bump timeout for create-next-app tests (#11743)

--- test/integration/create-next-app/index.test.js
+++ test/integration/create-next-app/index.test.js
@@ -29,7 +29,7 @@ const runStarter = (...args) => {
 
 describe('create next app', () => {
   beforeAll(async () => {
-    jest.setTimeout(1000 * 60 * 2)
+    jest.setTimeout(1000 * 60 * 3)
     await fs.mkdirp(cwd)
   })
 
