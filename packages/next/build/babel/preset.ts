ec5c8d1bf1906bd5830a31ba713e191c0fb3b277
Simon Knott <info@simonknott.de>
2020-04-12 10:40:00 +0200

    Allow `@babel/preset-typescript` to be configured
    Some usages, e.g. [getting parameter decorators to work properly](https://github.com/WarnerHooh/babel-plugin-parameter-decorator/pull/19), require configuring `@babel/preset-typescript`.
    
    This commit adds that functionality to `next/babel`.

--- packages/next/build/babel/preset.ts
+++ packages/next/build/babel/preset.ts
@@ -43,6 +43,7 @@ type NextBabelPresetOptions = {
   'transform-runtime'?: any
   'experimental-modern-preset'?: PluginItem
   'styled-jsx'?: StyledJsxBabelOptions
+  'preset-typescript'?: any
 }
 
 type BabelPreset = {
@@ -116,7 +117,10 @@ module.exports = (
           ...options['preset-react'],
         },
       ],
-      [require('@babel/preset-typescript'), { allowNamespaces: true }],
+      [
+        require('@babel/preset-typescript'),
+        { allowNamespaces: true, ...options['preset-typescript'] },
+      ],
     ],
     plugins: [
       [
