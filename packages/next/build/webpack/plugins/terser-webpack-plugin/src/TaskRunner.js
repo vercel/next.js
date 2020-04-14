3eb2e7b4308691c48e9e7c5f29f82227434bda30
Joe Haddad <joe.haddad@zeit.co>
2020-04-14 09:59:20 -0400

    Fix Situation Where Build Can Hang Indefinitely

--- packages/next/build/webpack/plugins/terser-webpack-plugin/src/TaskRunner.js
+++ packages/next/build/webpack/plugins/terser-webpack-plugin/src/TaskRunner.js
@@ -57,8 +57,16 @@ export default class TaskRunner {
           const done = () => step(index, result)
           if (cachePath) {
             writeFileP(cachePath, JSON.stringify(result), 'utf8')
-              .then(done)
-              .catch(done)
+              // This is important to stay as `.then(fn1, fn2)` and not
+              // `.then(fn1).catch(fn2)` so that we don't double-invoke `step`
+              // when `done` emits an error.
+              .then(done, done)
+              .catch(err => {
+                // Abort task on internal error (`done` failed). This can
+                // happen when users have a bad `package-lock.json` with an
+                // invalid webpack version, etc:
+                callback(err)
+              })
           }
         } catch (error) {
           step(index, { error })
@@ -66,9 +74,22 @@ export default class TaskRunner {
       }
 
       if (this.cacheDir) {
-        readFileP(cachePath, 'utf8')
-          .then(data => step(index, JSON.parse(data)))
-          .catch(() => enqueue())
+        // This is important to stay as `.then(fn1, fn2)` and not
+        // `.then(fn1).catch(fn2)` so that we don't double-invoke `step` when
+        // `step` emits an error.
+        readFileP(cachePath, 'utf8').then(
+          data => {
+            try {
+              step(index, JSON.parse(data))
+            } catch (err) {
+              // Abort task on internal error (`done` failed). This can happen
+              // when users have a bad `package-lock.json` with an invalid
+              // webpack version, etc:
+              callback(err)
+            }
+          },
+          () => enqueue()
+        )
       } else {
         enqueue()
       }
