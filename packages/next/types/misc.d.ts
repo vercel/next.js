386a207d1e0055d0085c93a5888ab31135a90e02
Joe Haddad <joe.haddad@zeit.co>
2020-04-15 12:59:09 -0400

    Upgrade `terser-webpack-plugin` for Webpack 5

--- packages/next/types/misc.d.ts
+++ packages/next/types/misc.d.ts
@@ -212,6 +212,14 @@ declare module 'next/dist/compiled/webpack-hot-middleware' {
   import m from 'webpack-hot-middleware'
   export = m
 }
+declare module 'next/dist/compiled/cacache' {
+  import m from 'cacache'
+  export = m
+}
+declare module 'next/dist/compiled/terser-webpack-plugin' {
+  import m from 'terser-webpack-plugin'
+  export = m
+}
 
 declare module 'autodll-webpack-plugin' {
   import webpack from 'webpack'
