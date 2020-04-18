7841c2cfc7d4640a75b9e52be1140889008c80a1
Joe Haddad <joe.haddad@zeit.co>
2020-04-18 01:22:19 -0400

    Create React Refresh Utils

---
+++ packages/react-refresh-utils/internal/helpers.ts
@@ -0,0 +1,151 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// This file is copied from the Metro JavaScript bundler, with minor tweaks for
+// webpack 4 compatibility.
+//
+// https://github.com/facebook/metro/blob/d6b9685c730d0d63577db40f41369157f28dfa3a/packages/metro/src/lib/polyfills/require.js
+
+import RefreshRuntime from 'react-refresh/runtime'
+
+declare const module: {
+  hot: {
+    status: () =>
+      | 'idle'
+      | 'check'
+      | 'prepare'
+      | 'ready'
+      | 'dispose'
+      | 'apply'
+      | 'abort'
+      | 'fail'
+  }
+}
+
+function registerExportsForReactRefresh(
+  moduleExports: unknown,
+  moduleID: string
+) {
+  RefreshRuntime.register(moduleExports, moduleID + ' %exports%')
+  if (moduleExports == null || typeof moduleExports !== 'object') {
+    // Exit if we can't iterate over exports.
+    // (This is important for legacy environments.)
+    return
+  }
+  for (const key in moduleExports) {
+    const exportValue = moduleExports[key]
+    const typeID = moduleID + ' %exports% ' + key
+    RefreshRuntime.register(exportValue, typeID)
+  }
+}
+
+function isReactRefreshBoundary(moduleExports: unknown): boolean {
+  if (RefreshRuntime.isLikelyComponentType(moduleExports)) {
+    return true
+  }
+  if (moduleExports == null || typeof moduleExports !== 'object') {
+    // Exit if we can't iterate over exports.
+    return false
+  }
+  let hasExports = false
+  let areAllExportsComponents = true
+  for (const key in moduleExports) {
+    hasExports = true
+    if (key === '__esModule') {
+      continue
+    }
+    const exportValue = moduleExports[key]
+    if (!RefreshRuntime.isLikelyComponentType(exportValue)) {
+      areAllExportsComponents = false
+    }
+  }
+  return hasExports && areAllExportsComponents
+}
+
+function shouldInvalidateReactRefreshBoundary(
+  prevExports: unknown,
+  nextExports: unknown
+): boolean {
+  const prevSignature = getRefreshBoundarySignature(prevExports)
+  const nextSignature = getRefreshBoundarySignature(nextExports)
+  if (prevSignature.length !== nextSignature.length) {
+    return true
+  }
+  for (let i = 0; i < nextSignature.length; i++) {
+    if (prevSignature[i] !== nextSignature[i]) {
+      return true
+    }
+  }
+  return false
+}
+
+function getRefreshBoundarySignature(moduleExports: unknown): Array<unknown> {
+  const signature = []
+  signature.push(RefreshRuntime.getFamilyByType(moduleExports))
+  if (moduleExports == null || typeof moduleExports !== 'object') {
+    // Exit if we can't iterate over exports.
+    // (This is important for legacy environments.)
+    return signature
+  }
+  for (const key in moduleExports) {
+    if (key === '__esModule') {
+      continue
+    }
+    const exportValue = moduleExports[key]
+    signature.push(key)
+    signature.push(RefreshRuntime.getFamilyByType(exportValue))
+  }
+  return signature
+}
+
+let isUpdateScheduled: boolean = false
+function scheduleUpdate() {
+  if (isUpdateScheduled) {
+    return
+  }
+
+  function canApplyUpdate() {
+    return module.hot.status() === 'idle'
+  }
+
+  isUpdateScheduled = true
+  setTimeout(() => {
+    isUpdateScheduled = false
+
+    // Only trigger refresh if the webpack HMR state is idle
+    if (canApplyUpdate()) {
+      return RefreshRuntime.performReactRefresh()
+    }
+
+    return scheduleUpdate()
+  }, 30)
+}
+
+export default {
+  registerExportsForReactRefresh,
+  isReactRefreshBoundary,
+  shouldInvalidateReactRefreshBoundary,
+  getRefreshBoundarySignature,
+  scheduleUpdate,
+}
