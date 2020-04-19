f523ffe17e7fd2d69cb3cc1d726ea5e5acb8f81d
Joe Haddad <joe.haddad@zeit.co>
2020-04-19 15:59:28 -0400

    Fast Refresh: Test Syntax Error Recovery

--- test/acceptance/ReactRefresh.test.js
+++ test/acceptance/ReactRefresh.test.js
@@ -58,3 +58,57 @@ test('basic: can edit a component without losing state', async () => {
 
   await cleanup()
 })
+
+test('can recover from a syntax error without losing state', async () => {
+  const [session, cleanup] = await sandbox()
+
+  await session.patch(
+    'index.js',
+    `
+      import { useCallback, useState } from 'react'
+
+      export default function Index() {
+        const [count, setCount] = useState(0)
+        const increment = useCallback(() => setCount(c => c + 1), [setCount])
+        return (
+          <main>
+            <p>{count}</p>
+            <button onClick={increment}>Increment</button>
+          </main>
+        )
+      }
+    `
+  )
+
+  await session.evaluate(() => document.querySelector('button').click())
+  expect(
+    await session.evaluate(() => document.querySelector('p').textContent)
+  ).toBe('1')
+
+  await session.patch('index.js', `export default () => <div/`)
+  expect(await session.getOverlayContent()).toMatch('Failed to compile')
+
+  await session.patch(
+    'index.js',
+    `
+      import { useCallback, useState } from 'react'
+
+      export default function Index() {
+        const [count, setCount] = useState(0)
+        const increment = useCallback(() => setCount(c => c + 1), [setCount])
+        return (
+          <main>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+          </main>
+        )
+      }
+    `
+  )
+
+  expect(
+    await session.evaluate(() => document.querySelector('p').textContent)
+  ).toBe('Count: 1')
+
+  await cleanup()
+})
