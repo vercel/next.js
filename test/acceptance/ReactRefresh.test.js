15b9b24c6e74ade937008453a65b23f28551a966
Joe Haddad <joe.haddad@zeit.co>
2020-04-20 14:30:04 -0400

    propagates a hot update to closest accepted module

--- test/acceptance/ReactRefresh.test.js
+++ test/acceptance/ReactRefresh.test.js
@@ -114,7 +114,7 @@ test('can recover from a syntax error without losing state', async () => {
 })
 
 // https://github.com/facebook/metro/blob/b651e535cd0fc5df6c0803b9aa647d664cb9a6c3/packages/metro/src/lib/polyfills/__tests__/require-test.js#L989-L1048
-test.only('re-runs accepted modules', async () => {
+test('re-runs accepted modules', async () => {
   const [session, cleanup] = await sandbox()
 
   await session.patch(
@@ -122,7 +122,6 @@ test.only('re-runs accepted modules', async () => {
     `export default function Noop() { return null; };`
   )
 
-  await session.evaluate(() => (window.log = []))
   await session.write(
     './foo.js',
     `window.log.push('init FooV1'); require('./bar');`
@@ -131,11 +130,12 @@ test.only('re-runs accepted modules', async () => {
     './bar.js',
     `window.log.push('init BarV1'); export default function Bar() { return null; };`
   )
+
+  await session.evaluate(() => (window.log = []))
   await session.patch(
     'index.js',
     `require('./foo'); export default function Noop() { return null; };`
   )
-
   expect(await session.evaluate(() => window.log)).toEqual([
     'init FooV1',
     'init BarV1',
@@ -158,6 +158,109 @@ test.only('re-runs accepted modules', async () => {
     `window.log.push('init BarV3'); export default function Bar() { return null; };`
   )
   expect(await session.evaluate(() => window.log)).toEqual(['init BarV3'])
+
+  // TODO:
+  // expect(Refresh.performReactRefresh).toHaveBeenCalled();
+  // expect(Refresh.performFullRefresh).not.toHaveBeenCalled();
+
+  await cleanup()
+})
+
+// https://github.com/facebook/metro/blob/b651e535cd0fc5df6c0803b9aa647d664cb9a6c3/packages/metro/src/lib/polyfills/__tests__/require-test.js#L1050-L1137
+test('propagates a hot update to closest accepted module', async () => {
+  const [session, cleanup] = await sandbox()
+
+  await session.patch(
+    'index.js',
+    `export default function Noop() { return null; };`
+  )
+
+  await session.write(
+    './foo.js',
+    `
+    window.log.push('init FooV1');
+    require('./bar');
+
+    // Exporting a component marks it as auto-accepting.
+    export default function Foo() {};
+    `
+  )
+
+  await session.write('./bar.js', `window.log.push('init BarV1');`)
+
+  await session.evaluate(() => (window.log = []))
+  await session.patch(
+    'index.js',
+    `require('./foo'); export default function Noop() { return null; };`
+  )
+
+  expect(await session.evaluate(() => window.log)).toEqual([
+    'init FooV1',
+    'init BarV1',
+  ])
+
+  // We edited Bar, but it doesn't accept.
+  // So we expect it to re-run together with Foo which does.
+  await session.evaluate(() => (window.log = []))
+  await session.patch('./bar.js', `window.log.push('init BarV2');`)
+  expect(await session.evaluate(() => window.log)).toEqual([
+    // // FIXME: Metro order:
+    // 'init BarV2',
+    // 'init FooV1',
+    'init FooV1',
+    'init BarV2',
+    // Webpack runs in this order because it evaluates modules parent down, not
+    // child up. Parents will re-run child modules in the order that they're
+    // imported from the parent.
+  ])
+
+  // We edited Bar, but it doesn't accept.
+  // So we expect it to re-run together with Foo which does.
+  await session.evaluate(() => (window.log = []))
+  await session.patch('./bar.js', `window.log.push('init BarV3');`)
+  expect(await session.evaluate(() => window.log)).toEqual([
+    // // FIXME: Metro order:
+    // 'init BarV3',
+    // 'init FooV1',
+    'init FooV1',
+    'init BarV3',
+    // Webpack runs in this order because it evaluates modules parent down, not
+    // child up. Parents will re-run child modules in the order that they're
+    // imported from the parent.
+  ])
+
+  // We edited Bar so that it accepts itself.
+  // We still re-run Foo because the exports of Bar changed.
+  await session.evaluate(() => (window.log = []))
+  await session.patch(
+    './bar.js',
+    `
+    window.log.push('init BarV3');
+    // Exporting a component marks it as auto-accepting.
+    export default function Bar() {};
+    `
+  )
+  expect(await session.evaluate(() => window.log)).toEqual([
+    // // FIXME: Metro order:
+    // 'init BarV3',
+    // 'init FooV1',
+    'init FooV1',
+    'init BarV3',
+    // Webpack runs in this order because it evaluates modules parent down, not
+    // child up. Parents will re-run child modules in the order that they're
+    // imported from the parent.
+  ])
+
+  // Further edits to Bar don't re-run Foo.
+  await session.evaluate(() => (window.log = []))
+  await session.patch(
+    './bar.js',
+    `
+    window.log.push('init BarV4');
+    export default function Bar() {};
+    `
+  )
+  expect(await session.evaluate(() => window.log)).toEqual(['init BarV4'])
 
   // TODO:
   // expect(Refresh.performReactRefresh).toHaveBeenCalled();
