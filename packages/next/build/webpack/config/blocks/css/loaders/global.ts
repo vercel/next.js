c27762b96e520d067a060d53e7267118a982f1de
Joe Haddad <timer150@gmail.com>
2020-04-21 23:56:13 -0400

    Revert global.ts

--- packages/next/build/webpack/config/blocks/css/loaders/global.ts
+++ packages/next/build/webpack/config/blocks/css/loaders/global.ts
@@ -1,60 +1,7 @@
 import postcss from 'postcss'
 import webpack from 'webpack'
 import { ConfigurationContext } from '../../../utils'
-import MiniCssExtractPlugin from '../../../../plugins/mini-css-extract-plugin'
-
-function getStyleLoader({
-  isDevelopment,
-  assetPrefix,
-}: {
-  isDevelopment: boolean
-  assetPrefix: string
-}): webpack.RuleSetUseItem {
-  return isDevelopment
-    ? {
-        loader: require.resolve('style-loader'),
-        options: {
-          // By default, style-loader injects CSS into the bottom
-          // of <head>. This causes ordering problems between dev
-          // and prod. To fix this, we render a <noscript> tag as
-          // an anchor for the styles to be placed before. These
-          // styles will be applied _before_ <style jsx global>.
-          insert: function(element: Element) {
-            // These elements should always exist. If they do not,
-            // this code should fail.
-            var anchorElement = document.querySelector(
-              '#__next_css__DO_NOT_USE__'
-            )!
-
-            // Append each global script immediately BEFORE the placeholder
-            anchorElement.insertAdjacentElement('beforebegin', element)
-
-            // Remember: this is development only code.
-            //
-            // After styles are injected, we need to remove the
-            // <style> tags that set `body { display: none; }`.
-            //
-            // We use `requestAnimationFrame` as a way to defer
-            // this operation since there may be multiple style
-            // tags.
-            ;(self.requestAnimationFrame || setTimeout)(function() {
-              for (
-                var x = document.querySelectorAll('[data-next-hide-fouc]'),
-                  i = x.length;
-                i--;
-
-              ) {
-                x[i].parentNode!.removeChild(x[i])
-              }
-            })
-          },
-        },
-      }
-    : {
-        loader: MiniCssExtractPlugin.loader,
-        options: { publicPath: `${assetPrefix}/_next/` },
-      }
-}
+import { getClientStyleLoader } from './client'
 
 export function getGlobalCssLoader(
   ctx: ConfigurationContext,
@@ -67,7 +14,7 @@ export function getGlobalCssLoader(
     // Add appropriate development more or production mode style
     // loader
     loaders.push(
-      getStyleLoader({
+      getClientStyleLoader({
         isDevelopment: ctx.isDevelopment,
         assetPrefix: ctx.assetPrefix,
       })
