0f3530948712c5480222c6fc9e5f28400b11af5a
matamatanot <matamatanot@gmail.com>
2020-04-27 01:48:38 +0900

    remove git add

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -11,16 +11,12 @@ module.exports = {
       `eslint --no-ignore --max-warnings=0 --fix ${filenames
         .map(f => `"${f}"`)
         .join(' ')}`,
-      `git add ${escapedFileNames}`,
     ]
   },
   '**/*.{json,md,mdx,css,html,yml,yaml,scss}': filenames => {
     const escapedFileNames = filenames
       .map(filename => `"${isWin ? filename : escape([filename])}"`)
       .join(' ')
-    return [
-      `prettier --with-node-modules --ignore-path .prettierignore_staged --write ${escapedFileNames}`,
-      `git add ${escapedFileNames}`,
-    ]
+    return `prettier --with-node-modules --ignore-path .prettierignore_staged --write ${escapedFileNames}`
   },
 }
