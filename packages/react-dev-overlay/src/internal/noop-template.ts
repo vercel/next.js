2ad65f5580a44771d799d9b40d11b5a3b06766b8
Joe Haddad <joe.haddad@zeit.co>
2020-04-27 15:02:06 -0400

    Type noop template

--- packages/react-dev-overlay/src/internal/noop-template.ts
+++ packages/react-dev-overlay/src/internal/noop-template.ts
@@ -1,4 +1,7 @@
-export function noop(strings, ...keys) {
+export function noop(
+  strings: TemplateStringsArray,
+  ...keys: readonly string[]
+) {
   const lastIndex = strings.length - 1
   return (
     strings.slice(0, lastIndex).reduce((p, s, i) => p + s + keys[i], '') +
