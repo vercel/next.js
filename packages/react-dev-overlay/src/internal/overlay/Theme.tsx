92f0a777904961c2740265d92d2b3af2facfeb58
Jeff Escalante <jescalan@users.noreply.github.com>
2020-04-27 19:39:50 -0400

    css tweaks

--- packages/react-dev-overlay/src/internal/overlay/Theme.tsx
+++ packages/react-dev-overlay/src/internal/overlay/Theme.tsx
@@ -133,13 +133,17 @@ export function Theme() {
             align-items: center;
           }
           [data-nextjs-dialog-header] nav > button {
-            border: none;
-            border-radius: 4px;
+            border: 1px solid #f7c8c8;
+            border-radius: 50%;
             background-color: rgba(230, 0, 0, 0.1);
             color: rgba(230, 0, 0, 1);
-            padding: 3px 9px;
+            padding: 3px 8.1px;
             cursor: pointer;
             font-weight: 500;
+            transition: background-color 0.25s ease;
+          }
+          [data-nextjs-dialog-header] nav > button:hover {
+            background-color: rgba(230, 0, 0, 0.2);
           }
           [data-nextjs-dialog-header] nav > button[disabled] {
             background-color: unset;
@@ -148,37 +152,40 @@ export function Theme() {
             cursor: not-allowed;
           }
           [data-nextjs-dialog-header] nav > button:first-of-type {
-            border-top-right-radius: 0px;
-            border-bottom-right-radius: 0px;
-            margin-right: 1px;
-          }
-          [data-nextjs-dialog-header] nav > button:last-of-type {
-            border-top-left-radius: 0px;
-            border-bottom-left-radius: 0px;
+            margin-right: 8px;
           }
           [data-nextjs-dialog-header] nav > span {
             font-size: 0.875rem;
+            color: #757575;
+            font-family: monospace;
+            margin-left: 9px;
           }
 
           [data-nextjs-dialog-header] h4 {
             margin-bottom: 0;
             line-height: 1.5;
+            margin-top: 1.2rem;
           }
           [data-nextjs-dialog-header] p {
             margin-bottom: 0;
+            color: #6a6a6a;
           }
           [data-nextjs-dialog-header] button.close {
-            font-size: 1.5rem;
-            font-weight: 700;
+            font-size: 1.8rem;
+            font-weight: 500;
             line-height: 1;
             color: #000;
             text-shadow: 0 1px 0 #fff;
-            opacity: 0.5;
+            opacity: 0.4;
             background-color: transparent;
             border: 0;
             appearance: none;
             padding: 1rem 1rem;
             margin: -1rem -1rem -1rem auto;
+            transition: opacity 0.25s ease;
+          }
+          [data-nextjs-dialog-header] button.close:hover {
+            opacity: 0.7;
           }
           [data-nextjs-dialog-body] {
             position: relative;
@@ -194,7 +201,7 @@ export function Theme() {
           [data-nextjs-call-stack-frame] > p {
             padding-left: 1rem;
             font-size: 0.875rem;
-            color: rgba(25, 25, 25, 0.8);
+            color: rgba(25, 25, 25, 0.5);
           }
         `,
       }}
