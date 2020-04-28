833ae9d5ebd174634314bf41f9cc830ab2693a32
Joe Haddad <joe.haddad@zeit.co>
2020-04-28 18:40:07 -0400

    Add `getOriginalStackFrame` Helper

--- packages/react-dev-overlay/src/internal/helpers/stack-frame.ts
+++ packages/react-dev-overlay/src/internal/helpers/stack-frame.ts
@@ -1,4 +1,62 @@
 import { StackFrame } from 'stacktrace-parser'
+import { OriginalStackFrameResponse } from '../../middleware'
+
+export type OriginalStackFrame =
+  | {
+      error: true
+      reason: string
+      sourceStackFrame: StackFrame
+      originalStackFrame: null
+      originalCodeFrame: null
+    }
+  | {
+      error: false
+      reason: null
+      sourceStackFrame: StackFrame
+      originalStackFrame: StackFrame
+      originalCodeFrame: string | null
+    }
+
+export function getOriginalStackFrame(
+  source: StackFrame
+): Promise<OriginalStackFrame> {
+  async function _getOriginalStackFrame(): Promise<OriginalStackFrame> {
+    const params = new URLSearchParams()
+    for (const key in source) {
+      params.append(key, (source[key] ?? '').toString())
+    }
+
+    const controller = new AbortController()
+    const tm = setTimeout(() => controller.abort(), 3000)
+    const res = await self
+      .fetch(`/__nextjs_original-stack-frame?${params.toString()}`, {
+        signal: controller.signal,
+      })
+      .finally(() => {
+        clearTimeout(tm)
+      })
+    if (!res.ok) {
+      return Promise.reject(new Error(await res.text()))
+    }
+
+    const body: OriginalStackFrameResponse = await res.json()
+    return {
+      error: false,
+      reason: null,
+      sourceStackFrame: source,
+      originalStackFrame: body.originalStackFrame,
+      originalCodeFrame: body.originalCodeFrame || null,
+    }
+  }
+
+  return _getOriginalStackFrame().catch((err: Error) => ({
+    error: true,
+    reason: err?.message ?? err?.toString() ?? 'Unknown Error',
+    sourceStackFrame: source,
+    originalStackFrame: null,
+    originalCodeFrame: null,
+  }))
+}
 
 export function createOriginalStackFrame(
   source: StackFrame,
