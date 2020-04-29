d648854e4ba441a5899cdb1b9201e17585605e3a
Joe Haddad <joe.haddad@zeit.co>
2020-04-29 12:04:08 -0400

    Still register old overlay for build errors

--- packages/next/client/dev/error-overlay/hot-dev-client.js
+++ packages/next/client/dev/error-overlay/hot-dev-client.js
@@ -64,25 +64,29 @@ export default function connect(options) {
 
   if (process.env.__NEXT_FAST_REFRESH) {
     DevOverlay.register()
-  } else {
-    // We need to keep track of if there has been a runtime error.
-    // Essentially, we cannot guarantee application state was not corrupted by the
-    // runtime error. To prevent confusing behavior, we forcibly reload the entire
-    // application. This is handled below when we are notified of a compile (code
-    // change).
-    // See https://github.com/facebook/create-react-app/issues/3096
-    ErrorOverlay.startReportingRuntimeErrors({
-      onError: function() {
-        hadRuntimeError = true
-      },
-    })
+  }
 
-    if (module.hot && typeof module.hot.dispose === 'function') {
-      module.hot.dispose(function() {
-        // TODO: why do we need this?
-        ErrorOverlay.stopReportingRuntimeErrors()
-      })
-    }
+  // We need to keep track of if there has been a runtime error.
+  // Essentially, we cannot guarantee application state was not corrupted by the
+  // runtime error. To prevent confusing behavior, we forcibly reload the entire
+  // application. This is handled below when we are notified of a compile (code
+  // change).
+  // See https://github.com/facebook/create-react-app/issues/3096
+  ErrorOverlay.startReportingRuntimeErrors({
+    onError: function() {
+      if (process.env.__NEXT_FAST_REFRESH) {
+        return
+      }
+
+      hadRuntimeError = true
+    },
+  })
+
+  if (module.hot && typeof module.hot.dispose === 'function') {
+    module.hot.dispose(function() {
+      // TODO: why do we need this?
+      ErrorOverlay.stopReportingRuntimeErrors()
+    })
   }
 
   getEventSourceWrapper(options).addMessageListener(event => {
