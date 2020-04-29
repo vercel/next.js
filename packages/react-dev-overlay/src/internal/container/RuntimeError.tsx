712e3c9ed3151280f29149de1a30b58fe8f2bfe9
Joe Haddad <joe.haddad@zeit.co>
2020-04-29 01:54:25 -0400

    Render the first CodeFrame

--- packages/react-dev-overlay/src/internal/container/RuntimeError.tsx
+++ packages/react-dev-overlay/src/internal/container/RuntimeError.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+import { CodeFrame } from '../components/CodeFrame'
 import { noop as css } from '../helpers/noop-template'
+import { OriginalStackFrame } from '../helpers/stack-frame'
 import { ReadyRuntimeError } from './Errors'
 
 export type RuntimeErrorProps = { className?: string; error: ReadyRuntimeError }
@@ -8,7 +10,39 @@ const RuntimeError: React.FC<RuntimeErrorProps> = function RuntimeError({
   className,
   error,
 }) {
-  return <div className={className}></div>
+  const firstFirstPartyFrameIndex = React.useMemo<number>(() => {
+    const idx = error.frames.findIndex(
+      entry =>
+        entry.expanded &&
+        Boolean(entry.originalCodeFrame) &&
+        Boolean(entry.originalStackFrame)
+    )
+    if (idx === -1) {
+      return error.frames.length
+    }
+    return idx
+  }, [error.frames])
+  const firstFrame = React.useMemo<OriginalStackFrame | null>(() => {
+    return error.frames[firstFirstPartyFrameIndex] ?? null
+  }, [error.frames, firstFirstPartyFrameIndex])
+  // const leadingFrames = React.useMemo<OriginalStackFrame[]>(
+  //   () => error.frames.slice(0, firstFirstPartyFrameIndex),
+  //   [error.frames, firstFirstPartyFrameIndex]
+  // )
+
+  return (
+    <div className={className}>
+      {firstFrame && (
+        <React.Fragment>
+          <h5>Source</h5>
+          <CodeFrame
+            stackFrame={firstFrame.originalStackFrame}
+            codeFrame={firstFrame.originalCodeFrame}
+          />
+        </React.Fragment>
+      )}
+    </div>
+  )
 }
 
 export const styles = css``
