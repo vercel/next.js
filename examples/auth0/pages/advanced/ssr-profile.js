ba6d2fe4d2b4a1a5d4d9592a9148810e9e567950
Marko Djordjevic <hello@markodjordjevic.dev>
2020-05-02 15:25:34 +1000

    Fix 'user undefined' error with ssr profile in auth0 example (#11934)

--- examples/auth0/pages/advanced/ssr-profile.js
+++ examples/auth0/pages/advanced/ssr-profile.js
@@ -21,9 +21,9 @@ export async function getServerSideProps({ req, res }) {
   // Here you can check authentication status directly before rendering the page,
   // however the page would be a serverless function, which is more expensive and
   // slower than a static page with client side authentication
-  const { user } = await auth0.getSession(req)
+  const session = await auth0.getSession(req)
 
-  if (!user) {
+  if (!session || !session.user) {
     res.writeHead(302, {
       Location: '/api/login',
     })
@@ -31,7 +31,7 @@ export async function getServerSideProps({ req, res }) {
     return
   }
 
-  return { props: { user } }
+  return { props: { user: session.user } }
 }
 
 export default Profile
