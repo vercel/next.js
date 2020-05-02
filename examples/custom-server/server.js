a1eb61b347289d9149c2cb4836c80d19928c95e1
Joe McCann <joseph.isaac@gmail.com>
2020-05-01 22:24:58 -0700

    upgrade from deprecated url.parse() since Node 11 (#11825)
    Co-Authored-By: Tim Neutkens <tim@timneutkens.nl>

--- examples/custom-server/server.js
+++ examples/custom-server/server.js
@@ -1,5 +1,4 @@
 const { createServer } = require('http')
-const { parse } = require('url')
 const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -9,7 +8,7 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
+    const parsedUrl = new URL(req.url, 'http://w.w')
     const { pathname, query } = parsedUrl
 
     if (pathname === '/a') {
