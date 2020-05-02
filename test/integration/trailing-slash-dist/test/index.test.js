b3bf7940a96e25f66254c192edafaae6e8a137d0
Fabian Mastenbroek <mail.fabianm@gmail.com>
2020-05-01 21:29:13 -0700

    Support distDir with trailing slash for static files (#10592)

---
+++ test/integration/trailing-slash-dist/test/index.test.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+/* global jasmine */
+import { join } from 'path'
+import { killApp, findPort, launchApp, fetchViaHTTP } from 'next-test-utils'
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
+
+const appDir = join(__dirname, '../')
+
+let appPort
+let app
+
+const runTest = (mode = 'server') => {
+  it('supports trailing slash', async () => {
+    const res = await fetchViaHTTP(
+      appPort,
+      '/_next/static/development/pages/index.js'
+    )
+
+    expect(res.status).toBe(200)
+  })
+}
+
+describe('Trailing slash in distDir', () => {
+  describe('dev mode', () => {
+    beforeAll(async () => {
+      appPort = await findPort()
+      app = await launchApp(appDir, appPort)
+    })
+    afterAll(() => killApp(app))
+    runTest('dev')
+  })
+})
