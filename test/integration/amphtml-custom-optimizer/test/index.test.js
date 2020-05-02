948e8466fa3f1646e072802ce5dfad1ae125ee13
Shotaro Watanabe <shotaro.watanabe0715@gmail.com>
2020-05-02 13:38:04 +0900

    Update @ampproject/toolbox-optimizer to v2.3.0 (#12039)

--- test/integration/amphtml-custom-optimizer/test/index.test.js
+++ test/integration/amphtml-custom-optimizer/test/index.test.js
@@ -30,7 +30,9 @@ describe('AMP Custom Optimizer', () => {
       'amp-twitter width="500" height="500" layout="responsive" data-tweetid="1159145442896166912"'
     )
     expect(html).toContain('i-amphtml-version="001515617716922"')
-    expect(html).toContain('script async src="/amp/rtv/001515617716922/v0.js"')
+    expect(html).toContain(
+      'script async src="https://cdn.ampproject.org/rtv/001515617716922/v0.js"'
+    )
   })
 
   it('should build and start for dynamic page', async () => {
@@ -47,6 +49,8 @@ describe('AMP Custom Optimizer', () => {
       'amp-img width="500" height="500" layout="responsive" src="https://amp.dev/static/samples/img/story_dog2_portrait.jpg"'
     )
     expect(html).toContain('i-amphtml-version="001515617716922"')
-    expect(html).toContain('script async src="/amp/rtv/001515617716922/v0.js"')
+    expect(html).toContain(
+      'script async src="https://cdn.ampproject.org/rtv/001515617716922/v0.js"'
+    )
   })
 })
