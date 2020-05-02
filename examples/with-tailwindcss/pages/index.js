16fcce4439d2bf1c16e2bd5286957b1a8b6597ca
Taylor Bryant <taylor@bryant.io>
2020-05-02 14:08:23 -0500

    Add name to component in with-tailwindcss example (#12417)

--- examples/with-tailwindcss/pages/index.js
+++ examples/with-tailwindcss/pages/index.js
@@ -1,10 +1,12 @@
 import Nav from '../components/nav'
 
-export default () => (
-  <div>
-    <Nav />
-    <div className="hero">
-      <h1 className="title">Next.js + Tailwind CSS</h1>
+export default function IndexPage() {
+  return (
+    <div>
+      <Nav />
+      <div className="hero">
+        <h1 className="title">Next.js + Tailwind CSS</h1>
+      </div>
     </div>
-  </div>
-)
+  )
+}
