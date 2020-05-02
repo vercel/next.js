b453b6cf7bf7d7acefb718327906337ddc4278f7
mit123suki <61997457+mit123suki@users.noreply.github.com>
2020-05-02 10:29:56 +0530

    example: antd-4 with built-in css support (#11837)

--- examples/with-ant-design/pages/_app.js
+++ examples/with-ant-design/pages/_app.js
@@ -1,4 +1,7 @@
+import React from 'react'
 import 'antd/dist/antd.css'
+import '../styles/vars.css'
+import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
