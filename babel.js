85bcf4b2ceb29e47841ef01e010503db92d4c17d
Kevin Decker <kpdecker@gmail.com>
2020-05-04 18:43:59 -0500

    Rename package to @healthline/next

--- babel.js
+++ babel.js
@@ -19,12 +19,12 @@ module.exports = (context, opts = {}) => {
       require.resolve('babel-plugin-module-resolver'),
       {
         alias: {
-          'next/client': isServer ? undefined : '@kpdecker/next/browser/client',
-          'next/link': isServer ? '@kpdecker/next/node/lib/link' : '@kpdecker/next/browser/lib/link',
-          'next/head': isServer ? '@kpdecker/next/node/lib/head' : '@kpdecker/next/browser/lib/head',
-          'next/page-loader': isServer ? '@kpdecker/next/node/lib/page-loader' : '@kpdecker/next/browser/lib/page-loader',
-          'next/document': isServer ? '@kpdecker/next/node/server/document' : undefined,
-          'next/router': isServer ? '@kpdecker/next/node/server/router' : '@kpdecker/next/browser/client/router'
+          'next/client': isServer ? undefined : '@healthline/next/browser/client',
+          'next/link': isServer ? '@healthline/next/node/lib/link' : '@healthline/next/browser/lib/link',
+          'next/head': isServer ? '@healthline/next/node/lib/head' : '@healthline/next/browser/lib/head',
+          'next/page-loader': isServer ? '@healthline/next/node/lib/page-loader' : '@healthline/next/browser/lib/page-loader',
+          'next/document': isServer ? '@healthline/next/node/server/document' : undefined,
+          'next/router': isServer ? '@healthline/next/node/server/router' : '@healthline/next/browser/client/router'
         }
       }
     ])
