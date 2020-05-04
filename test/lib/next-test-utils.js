00d930aae82e59dd2d4facd25af249c31c9933c5
JJ Kasper <jj@jjsweb.site>
2020-05-04 11:58:19 -0500

    Add additional file serving tests (#12479)
    * Test `static/` file name encoding
    
    * Fix `static/` file name encoding
    
    * Add additional file-serving tests
    
    * bump
    
    Co-authored-by: Joe Haddad <joe.haddad@zeit.co>

--- test/lib/next-test-utils.js
+++ test/lib/next-test-utils.js
@@ -172,7 +172,10 @@ export function runNextCommandDev(argv, stdOut, opts = {}) {
       if (typeof opts.onStdout === 'function') {
         opts.onStdout(message)
       }
-      process.stdout.write(message)
+
+      if (opts.stdout !== false) {
+        process.stdout.write(message)
+      }
     }
 
     function handleStderr(data) {
@@ -180,7 +183,10 @@ export function runNextCommandDev(argv, stdOut, opts = {}) {
       if (typeof opts.onStderr === 'function') {
         opts.onStderr(message)
       }
-      process.stderr.write(message)
+
+      if (opts.stderr !== false) {
+        process.stderr.write(message)
+      }
     }
 
     instance.stdout.on('data', handleStdout)
