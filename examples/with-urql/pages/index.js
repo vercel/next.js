b468d98bc220f303b22b4366345ca236a16ab31d
Alejandro Nanez <alejonanez@gmail.com>
2020-05-04 13:33:06 -0500

    Basic UrQL example
    - Connects to Pokemon GraphQL API

--- examples/with-urql/pages/index.js
+++ examples/with-urql/pages/index.js
@@ -1,5 +1,26 @@
-export default () => (
-  <div>
-    <h1>UrQL + Next.js</h1>
-  </div>
-)
+import { useQuery } from 'urql'
+const PikachuQuery = `
+  {
+    pokemon(name: "pikachu") {
+      name
+      image
+    }
+  }
+`
+
+export default () => {
+  const [{ data, fetching, error }] = useQuery({ query: PikachuQuery })
+  if (fetching) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>{error.message}</div>
+  }
+
+  return (
+    <div>
+      <h2>{data.pokemon.name}</h2>
+      <img src={data.pokemon.image} alt={`${data.pokemon.name} picture`} />
+    </div>
+  )
+}
