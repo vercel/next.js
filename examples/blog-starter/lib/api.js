70793867fc559bea82952fb307afcd675ebd5584
Tomas Dabašinskas <tomas.it@gmail.com>
2020-05-13 03:40:51 +1000

    sort posts by date in descending order (#12303)
    * sort posts by date in descending order
    
    Hi, I'm extending getAllPosts function to sort posts array by date in descending order. 
    Any feedback is appreciated
    
    * sort posts by date timestamp in descending order
    
    * Lint fix
    
    Co-authored-by: Luis Alvarez <luis@vercel.com>

--- examples/blog-starter/lib/api.js
+++ examples/blog-starter/lib/api.js
@@ -35,5 +35,9 @@ export function getPostBySlug(slug, fields = []) {
 
 export function getAllPosts(fields = []) {
   const slugs = getPostSlugs()
-  return slugs.map(slug => getPostBySlug(slug, fields))
+  const posts = slugs
+    .map(slug => getPostBySlug(slug, fields))
+    // sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? '-1' : '1'))
+  return posts
 }
