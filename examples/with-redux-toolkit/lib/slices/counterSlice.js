582b2787060be81d4c45ae4b8a104f139d9427fd
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-05-14 08:14:36 -0600

    docs(with-redux-toolkit): Add comment
    Explain that reducers use Immer under the hood to update the state.

--- examples/with-redux-toolkit/lib/slices/counterSlice.js
+++ examples/with-redux-toolkit/lib/slices/counterSlice.js
@@ -6,6 +6,10 @@ const counterSlice = createSlice({
     value: 0,
   },
   reducers: {
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
     increment: state => {
       state.value += 1;
     },
