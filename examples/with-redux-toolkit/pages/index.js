3c7cd8de23dfdb12c0813c4abe4f24ba9f1dfdb7
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-05-14 08:25:18 -0600

    perf(with-redux-toolkit): Prefer hooks over HOC

--- examples/with-redux-toolkit/pages/index.js
+++ examples/with-redux-toolkit/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Clock from '../components/clock';
 import Counter from '../components/counter';
 import { tick } from '../lib/slices/clockSlice';
 import useInterval from '../lib/useInterval';
 
-const IndexPage = ({ dispatch }) => {
+const IndexPage = () => {
   // Use state or dispatch here
-
+  const dispatch = useDispatch();
   // Tick the time every second
   useInterval(() => {
     dispatch(tick(true));
@@ -22,4 +22,4 @@ const IndexPage = ({ dispatch }) => {
   );
 };
 
-export default connect(state => state)(IndexPage);
+export default IndexPage;
