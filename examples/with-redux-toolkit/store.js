1d424f36ebce3b5233a0fe16a6d3624384e14189
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-05-13 22:02:13 -0600

    feat(with-redux-toolkit): Import reducers from slices
    Remove the reducer definition and only configure the store.
    Enable Redux dev-tools.

--- examples/with-redux-toolkit/store.js
+++ examples/with-redux-toolkit/store.js
@@ -1,26 +1,12 @@
-import { configureStore, createReducer } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
-  light: false,
-  lastUpdate: 0,
-  count: 0,
-}
+import clock from './lib/slices/clockSlice';
+import count from './lib/slices/counterSlice';
 
-const reducer = createReducer(initialState, {
-  TICK: (state, action) => ({
-    ...state,
-    lastUpdate: action.payload.lastUpdate,
-    light: !!action.light,
-  }),
-  INCREMENT: (state, action) => ({ ...state, count: state.count + 1 }),
-  DECREMENT: (state, action) => ({ ...state, count: state.count - 1 }),
-  RESET: (state, action) => ({ ...state, count: initialState.count }),
-})
-
-const initializeStore = (preloadedState = initialState) => {
-  return configureStore({
-    reducer,
-    preloadedState,
-  })
-}
-export const store = initializeStore()
+export default configureStore({
+  reducer: {
+    count,
+    clock,
+  },
+  devTools: true,
+});
