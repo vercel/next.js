ee11bf2d8b58974801e4c1ff28a2c1addd06f552
Joe Haddad <joe.haddad@zeit.co>
2020-05-15 19:55:58 -0400

    Test TypeScript Enum (#12954)

--- test/integration/typescript/pages/hello.tsx
+++ test/integration/typescript/pages/hello.tsx
@@ -6,6 +6,14 @@ import Router from '../components/router'
 import { World } from '../components/world'
 import { value as resolveOrderValue } from '../extension-order/js-first'
 
+export enum SearchEntity {
+  SEARCH_ENTITY_NONE = 0,
+  SEARCH_ENTITY_POSITION = 1,
+  SEARCH_ENTITY_USER = 2,
+  SEARCH_ENTITY_QUESTION = 3,
+  SEARCH_ENTITY_TOPIC = 4,
+}
+
 export default function HelloPage(): JSX.Element {
   const router = useRouter()
   console.log(process.browser)
