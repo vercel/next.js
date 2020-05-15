ed847b62a82ef9bbb81475eaf993531cc7d83c89
JJ Kasper <jj@jjsweb.site>
2020-05-15 10:26:31 -0500

    Rename preset and remove files from testing

--- examples/with-storybook/.storybook/presets.js
+++
@@ -1,1 +0,0 @@
-module.exports = ['next/storybook']
--- examples/with-storybook/components/index.js
+++ examples/with-storybook/components/index.js
@@ -1,3 +1,2 @@
 import React from 'react'
-import styles from './index.module.css'
-export default () => <div className={styles.hello}>Hello World</div>
+export default () => <div>Hello World</div>
