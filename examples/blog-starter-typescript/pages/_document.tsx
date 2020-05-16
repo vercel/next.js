649822810c9729b092f1faebd54fe856aa8bd80b
UndyingSugimoto <kouji36000@yahoo.co.jp>
2020-05-16 16:58:23 +0900

    feature: add blog-starter-typescript

---
+++ examples/blog-starter-typescript/pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
