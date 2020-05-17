42d87dd342d56e542802e87382c63912675b5150
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-05-17 09:39:01 +0200

    Only retry tests on CI

--- test/jest-setup-after-env.js
+++ test/jest-setup-after-env.js
@@ -1,3 +1,7 @@
 /* eslint-env jest */
 
-jest.retryTimes(3)
+if (process.env.JEST_RETRY_TIMES) {
+  const retries = Number(process.env.JEST_RETRY_TIMES)
+  console.log(`Configuring jest retries: ${retries}`)
+  jest.retryTimes(retries)
+}
