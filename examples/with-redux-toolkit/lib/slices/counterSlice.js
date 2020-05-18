d8f209d51fbbe1a65fa50dcf9d3aa558dbb42938
Joe Haddad <joe.haddad@zeit.co>
2020-05-18 14:29:57 -0400

    Lint

--- examples/with-redux-toolkit/lib/slices/counterSlice.js
+++ examples/with-redux-toolkit/lib/slices/counterSlice.js
@@ -10,13 +10,13 @@ const counterSlice = createSlice({
     // doesn't actually mutate the state because it uses the Immer library,
     // which detects changes to a "draft state" and produces a brand new
     // immutable state based off those changes
-    increment: (state) => {
+    increment: state => {
       state.value += 1
     },
-    decrement: (state) => {
+    decrement: state => {
       state.value -= 1
     },
-    reset: (state) => {
+    reset: state => {
       state.value = 0
     },
     incrementByAmount: (state, action) => {
@@ -31,7 +31,7 @@ const counterSlice = createSlice({
  * @param   {Object} state The root state
  * @returns {number} The current count
  */
-export const selectCount = (state) => state.counter.value
+export const selectCount = state => state.counter.value
 
 export const {
   increment,
@@ -44,7 +44,7 @@ export const {
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount) => (dispatch) => {
+export const incrementAsync = amount => dispatch => {
   setTimeout(() => {
     dispatch(incrementByAmount(amount))
   }, 1000)
