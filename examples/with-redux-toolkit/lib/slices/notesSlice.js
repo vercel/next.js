caed0a1e1fed7330fcce157b27ad9f12e4006400
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-05-17 20:17:52 -0600

    feat(with-redux-toolkit): Add async action to load notes

--- examples/with-redux-toolkit/lib/slices/notesSlice.js
+++ examples/with-redux-toolkit/lib/slices/notesSlice.js
@@ -25,10 +25,24 @@ export const addNote = createAsyncThunk(
   }
 )
 
+export const loadNotes = createAsyncThunk(
+  'notes/loadNotes',
+  async (_, thunkAPI) => {
+    try {
+      const response = await fetch('/api/notes')
+
+      return response.json()
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: error.message })
+    }
+  }
+)
+
 const notesSlice = createSlice({
   name: 'notes',
   initialState: {
     notes: [],
+    loading: 'idle',
   },
   reducers: {},
   extraReducers: {
@@ -39,6 +53,18 @@ const notesSlice = createSlice({
       state.notes.push(action.payload)
     },
     [addNote.rejected]: (state, action) => {
+      state.error = action.payload.error
+    },
+    [loadNotes.pending]: state => {
+      state.notes = []
+      state.loading = 'loading'
+    },
+    [loadNotes.fulfilled]: (state, action) => {
+      state.notes = action.payload
+      state.loading = 'loaded'
+    },
+    [loadNotes.rejected]: (state, action) => {
+      state.loading = 'error'
       state.error = action.payload.error
     },
   },
