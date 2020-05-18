019c112d7f0dbf2d68c61df24a44ed9fa4ecc8e4
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-05-18 18:07:08 +0200

    Improve jsconfig.json error message (#13053)

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -51,6 +51,7 @@ import WebpackConformancePlugin, {
   ReactSyncScriptsConformanceCheck,
 } from './webpack/plugins/webpack-conformance-plugin'
 import { WellKnownErrorsPlugin } from './webpack/plugins/wellknown-errors-plugin'
+import { codeFrameColumns } from '@babel/code-frame'
 
 type ExcludesFalse = <T>(x: T | false) => x is T
 
@@ -65,7 +66,16 @@ const escapePathVariables = (value: any) => {
 function parseJsonFile(path: string) {
   const JSON5 = require('next/dist/compiled/json5')
   const contents = readFileSync(path)
-  return JSON5.parse(contents)
+  try {
+    return JSON5.parse(contents)
+  } catch (err) {
+    const codeFrame = codeFrameColumns(
+      String(contents),
+      { start: { line: err.lineNumber, column: err.columnNumber } },
+      { message: err.message, highlightCode: true }
+    )
+    throw new Error(`Failed to parse "${path}":\n${codeFrame}`)
+  }
 }
 
 function getOptimizedAliases(isServer: boolean): { [pkg: string]: string } {
