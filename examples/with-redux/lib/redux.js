ad4704a374f985c91353d58433e196bc8925ebef
chandan-reddy-k <chandan@commutatus.com>
2020-05-18 10:06:46 +0530

    remove: react import statements in with-redux example

--- examples/with-redux/lib/redux.js
+++ examples/with-redux/lib/redux.js
@@ -29,44 +29,14 @@ export const withRedux = (PageComponent, { ssr = true } = {}) => {
 
     WithRedux.displayName = `withRedux(${displayName})`
   }
-
-  if (ssr || PageComponent.getInitialProps) {
-    WithRedux.getInitialProps = async context => {
-      // Get or Create the store with `undefined` as initialState
-      // This allows you to set a custom default initialState
-      const reduxStore = getOrInitializeStore()
-
-      // Provide the store to getInitialProps of pages
-      context.reduxStore = reduxStore
-
-      // Run getInitialProps from HOCed PageComponent
-      const pageProps =
-        typeof PageComponent.getInitialProps === 'function'
-          ? await PageComponent.getInitialProps(context)
-          : {}
-
-      // Pass props to PageComponent
-      return {
-        ...pageProps,
-        initialReduxState: reduxStore.getState(),
-      }
-    }
-  }
-
   return WithRedux
 }
 
 let reduxStore
 const getOrInitializeStore = initialState => {
-  // Always make a new store if server, otherwise state is shared between requests
-  if (typeof window === 'undefined') {
-    return initializeStore(initialState)
-  }
-
   // Create store if unavailable on the client and set it on the window object
   if (!reduxStore) {
     reduxStore = initializeStore(initialState)
   }
-
   return reduxStore
 }
