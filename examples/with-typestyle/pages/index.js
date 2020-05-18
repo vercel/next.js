d0d044fc9df2800bf20940d80dea2cd301dc79c5
Juan Mateo Garcia Ramirez <juanmateo.garcia@globant.com>
2020-05-18 15:44:12 -0500

    feat: Remove some unnecesary imports

--- examples/with-typestyle/pages/index.js
+++ examples/with-typestyle/pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { style } from 'typestyle'
 
 const className = style({ color: 'red' })
--- examples/with-universal-configuration-runtime/pages/index.js
+++ examples/with-universal-configuration-runtime/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import { Component } from 'react'
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
 const { API_URL } = publicRuntimeConfig
 
-export default class extends React.Component {
+export default class extends Component {
   render() {
     return <div>The API_URL is {API_URL}</div>
   }
