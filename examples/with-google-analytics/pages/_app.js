342475e108c4ab39537506c40022c0d81394c015
cjimmy <jimmychion@gmail.com>
2020-05-19 12:08:38 -0400

    forgot to run prettier

--- examples/with-google-analytics/pages/_app.js
+++ examples/with-google-analytics/pages/_app.js
@@ -1,18 +1,18 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import Router from 'next/router'
 import * as gtag from '../lib/gtag'
 
 const App = ({ Component, pageProps }) => {
   useEffect(() => {
-    const handleRouteChange = url => {
-      gtag.pageview(url);
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
     }
     Router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       Router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [])
-  
+
   return <Component {...pageProps} />
 }
 
