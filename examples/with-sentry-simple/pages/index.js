477e5fab1a6e4540e9e471c74b08c5ece1943173
Luis Alvarez <luis@vercel.com>
2020-05-18 22:03:35 -0500

    Updated client tests

--- examples/with-sentry-simple/pages/index.js
+++ examples/with-sentry-simple/pages/index.js
@@ -55,34 +55,44 @@ const Index = () => (
       <li>Client exceptions</li>
       <ul>
         <li>
-          getInitialProps throws an Error. This should cause _error.js to render
-          and record Error('Client Test 1') in Sentry. Note Sentry will double
-          count this exception. Once from an unhandledrejection and again in
-          _error.js. Could be a bug in Next.js or Sentry, requires more
-          debugging.{' '}
+          There is a top-of-module Promise that rejects, but its result is not
+          awaited. Sentry should record Error('Client Test 1').{' '}
           <Link href="/client/test1">
             <a>Perform client side navigation</a>
-          </Link>
+          </Link>{' '}
+          or{' '}
+          <a href="/client/test1" target="_blank">
+            Open in a new tab
+          </a>
         </li>
         <li>
-          getInitialProps returns a Promise that rejects. This should cause
-          _error.js to render and record Error('Client Test 2') in Sentry. As
-          above, Sentry will double count this exception.{' '}
+          There is a top-of-module exception. _error.js should render and record
+          ReferenceError('process is not defined') in Sentry.{' '}
           <Link href="/client/test2">
             <a>Perform client side navigation</a>
-          </Link>
+          </Link>{' '}
+          or{' '}
+          <a href="/client/test2" target="_blank">
+            Open in a new tab
+          </a>
         </li>
         <li>
-          getInitialProps calls a Promise that rejects, but does not handle the
-          rejection or await its result (returning synchronously). Sentry should
-          record Error('Client Test 3').{' '}
+          There is an exception during React lifecycle that is caught by
+          Next.js's React Error Boundary. In this case, when the component
+          mounts. This should cause _error.js to render and record Error('Client
+          Test 3') in Sentry.{' '}
           <Link href="/client/test3">
             <a>Perform client side navigation</a>
-          </Link>
+          </Link>{' '}
+          or{' '}
+          <a href="/client/test3" target="_blank">
+            Open in a new tab
+          </a>
         </li>
         <li>
-          There is a top-of-module Promise that rejects, but its result is not
-          awaited. Sentry should record Error('Client Test 4').{' '}
+          There is an unhandled Promise rejection during React lifecycle. In
+          this case, when the component mounts. Sentry should record
+          Error('Client Test 4').{' '}
           <Link href="/client/test4">
             <a>Perform client side navigation</a>
           </Link>{' '}
@@ -92,49 +102,13 @@ const Index = () => (
           </a>
         </li>
         <li>
-          There is a top-of-module exception. _error.js should render and record
-          ReferenceError('process is not defined') in Sentry.{' '}
+          An Error is thrown from an event handler. Sentry should record
+          Error('Client Test 5').{' '}
           <Link href="/client/test5">
             <a>Perform client side navigation</a>
           </Link>{' '}
           or{' '}
           <a href="/client/test5" target="_blank">
-            Open in a new tab
-          </a>
-        </li>
-        <li>
-          There is an exception during React lifecycle that is caught by
-          Next.js's React Error Boundary. In this case, when the component
-          mounts. This should cause _error.js to render and record Error('Client
-          Test 6') in Sentry.{' '}
-          <Link href="/client/test6">
-            <a>Perform client side navigation</a>
-          </Link>{' '}
-          or{' '}
-          <a href="/client/test6" target="_blank">
-            Open in a new tab
-          </a>
-        </li>
-        <li>
-          There is an unhandled Promise rejection during React lifecycle. In
-          this case, when the component mounts. Sentry should record
-          Error('Client Test 7').{' '}
-          <Link href="/client/test7">
-            <a>Perform client side navigation</a>
-          </Link>{' '}
-          or{' '}
-          <a href="/client/test7" target="_blank">
-            Open in a new tab
-          </a>
-        </li>
-        <li>
-          An Error is thrown from an event handler. Sentry should record
-          Error('Client Test 8').{' '}
-          <Link href="/client/test8">
-            <a>Perform client side navigation</a>
-          </Link>{' '}
-          or{' '}
-          <a href="/client/test8" target="_blank">
             Open in a new tab
           </a>
         </li>
