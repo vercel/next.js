dc25ee33bd3a3ad4f4e499e22108c6df637dcd8a
Jimmy Chion <jimmychion@gmail.com>
2020-05-19 12:37:54 -0400

    [Example] Unsubscribe Router.on.events (#13102)

--- examples/with-google-analytics/pages/_app.js
+++ examples/with-google-analytics/pages/_app.js
@@ -1,8 +1,19 @@
-import App from 'next/app'
+import { useEffect } from 'react'
 import Router from 'next/router'
-
 import * as gtag from '../lib/gtag'
 
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return <Component {...pageProps} />
+}
 
 export default App
