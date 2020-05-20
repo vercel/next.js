196231f23f417d88cd297a15f836ec2d4e86074d
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-05-20 13:43:40 -0700

    remove unused styles

--- examples/with-mux-video/components/layout.js
+++ examples/with-mux-video/components/layout.js
@@ -114,36 +114,6 @@ export default function Layout ({ title, description, children }) {
           margin-top: 1rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
         .logo {
           height: 1em;
         }
