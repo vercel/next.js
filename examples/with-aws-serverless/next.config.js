cf8bad64f853d4f462bb1d16a34546522e0dd4f0
kimcoder <kimcoder@me.com>
2020-05-21 14:45:08 +0900

    [with-aws-serverless] run prettier

--- examples/with-aws-serverless/next.config.js
+++ examples/with-aws-serverless/next.config.js
@@ -1,10 +1,10 @@
-const withImages = require('next-images');
-const serverEnv = process.env.SERVER_ENV || 'local';
+const withImages = require('next-images')
+const serverEnv = process.env.SERVER_ENV || 'local'
 
 module.exports = withImages({
-  target: "serverless",
+  target: 'serverless',
   inlineImageLimit: 8 * 1024,
-  env : {
-    SERVER_ENV: serverEnv,
+  env: {
+    SERVER_ENV: serverEnv
   }
-});
+})
