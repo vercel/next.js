d35c4cc39958c24f78e6d2365436e0d68d7b92d0
Chad Johnson <chad.d.johnson@gmail.com>
2020-05-21 19:42:22 -0400

    Fix style edit loss on browser refresh when using Emotion.

--- examples/with-emotion/pages/_document.js
+++ examples/with-emotion/pages/_document.js
@@ -1,28 +1,24 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { renderToString } from 'react-dom/server'
 import { extractCritical } from 'emotion-server'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-    const styles = extractCritical(initialProps.html)
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style
-            data-emotion-css={styles.ids.join(' ')}
-            dangerouslySetInnerHTML={{ __html: styles.css }}
-          />
-        </>
-      ),
-    }
+    const styles = extractCritical(renderToString(initialProps.html))
+
+    return { ...initialProps, ...styles }
   }
 
   render() {
     return (
       <html>
-        <Head />
+        <Head>
+          <style
+            data-emotion-css={this.props.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
