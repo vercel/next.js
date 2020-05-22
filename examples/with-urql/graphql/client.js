d1947894e4c808909bdb4c4ac050616ccaf2d6f1
Tim Neutkens <tim@timneutkens.nl>
2020-05-22 18:21:18 +0200

    Update examples/with-urql/graphql/client.js
    Co-authored-by: Joe Haddad <timer150@gmail.com>

--- examples/with-urql/graphql/client.js
+++ examples/with-urql/graphql/client.js
@@ -1,11 +1,5 @@
 import { createClient } from 'urql'
 
-/**
- * Needed because `window.fetch` will not be available in `getStaticProps`,
- * `window.fetch` is what `urql` uses under the hood
- */
-import 'isomorphic-unfetch'
-
 export const client = createClient({
   url: 'https://graphql-pokemon.now.sh/',
 })
