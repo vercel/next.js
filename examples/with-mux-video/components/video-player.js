b58d78ce4a3cc06b2fc0bc74f78f875ce0ea9dbd
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-05-22 10:04:51 -0700

    call hls.destroy() when the component is unmounted

--- examples/with-mux-video/components/video-player.js
+++ examples/with-mux-video/components/video-player.js
@@ -9,18 +9,26 @@ export default function VideoPlayer({ src, poster }) {
     if (!video) return
 
     video.controls = true
+    let hls
+
     if (video.canPlayType('application/vnd.apple.mpegurl')) {
       // This will run in safari, where HLS is supported natively
       video.src = src
     } else if (Hls.isSupported()) {
       // This will run in all other modern browsers
-      const hls = new Hls()
+      hls = new Hls()
       hls.loadSource(src)
       hls.attachMedia(video)
     } else {
       console.error(
         'This is an old browser that does not support MSE https://developer.mozilla.org/en-US/docs/Web/API/Media_Source_Extensions_API'
       )
+    }
+
+    return () => {
+      if (hls) {
+        hls.destroy()
+      }
     }
   }, [src, videoRef])
 
