96e25fd9d347012839586d7b5f41e3b4c3d54603
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-22 21:11:01 +0300

    with-monaco-editor-example-fix (#13186)
    * Modified nextjs config so that it works with all types of fonts and images.
    
    * Update next.config.js
    
    * Fix code
    
    Co-authored-by: Tim Neutkens <timneutkens@me.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- examples/with-monaco-editor/next.config.js
+++ examples/with-monaco-editor/next.config.js
@@ -1,19 +1,26 @@
 const withCSS = require('@zeit/next-css')
-const withFonts = require('next-fonts')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
-module.exports = withCSS(
-  withFonts({
-    webpack: (config) => {
-      config.plugins.push(
-        new MonacoWebpackPlugin({
-          // Add languages as needed...
-          languages: ['javascript', 'typescript'],
-          filename: 'static/[name].worker.js',
-        })
-      )
+module.exports = withCSS({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    })
 
-      return config
-    },
-  })
-)
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+        // Add languages as needed...
+        languages: ['javascript', 'typescript'],
+        filename: 'static/[name].worker.js',
+      })
+    )
+
+    return config
+  },
+})
