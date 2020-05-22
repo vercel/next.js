ed8a3cf246e5b7bc534ab9c2ad21447f5737d926
Joe Haddad <timer150@gmail.com>
2020-05-22 12:15:52 -0400

    Apply suggestions from code review

--- examples/with-react-with-styles/pages/_app.js
+++ examples/with-react-with-styles/pages/_app.js
@@ -1,4 +1,3 @@
-import App from 'next/app'
 import React from 'react'
 import WithStylesContext from 'react-with-styles/lib/WithStylesContext'
 import AphroditeInterface from 'react-with-styles-interface-aphrodite'
@@ -6,22 +5,21 @@ import { DIRECTIONS } from 'react-with-direction'
 
 import defaultTheme from '../defaultTheme'
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
+function MyApp(props) {
+  const { Component, pageProps } = props;
 
-    return (
-      <WithStylesContext.Provider
-        value={{
-          stylesInterface: AphroditeInterface,
-          stylesTheme: defaultTheme,
-          direction: DIRECTIONS.LTR,
-        }}
-      >
-        <Component {...pageProps} />
-      </WithStylesContext.Provider>
-    )
-  }
+  return (
+    <WithStylesContext.Provider
+      value={{
+        stylesInterface: AphroditeInterface,
+        stylesTheme: defaultTheme,
+        direction: DIRECTIONS.LTR,
+      }}
+    >
+      <Component {...pageProps} />
+    </WithStylesContext.Provider>
+  );
 }
+
 
 export default MyApp
