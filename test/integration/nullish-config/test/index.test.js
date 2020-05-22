5415021679d5bdaf9dd11c2a7e80382df1bd4d10
Joe Haddad <joe.haddad@zeit.co>
2020-05-22 11:51:13 -0400

    Apply suggestions from code review

--- test/integration/nullish-config/test/index.test.js
+++ test/integration/nullish-config/test/index.test.js
@@ -1,10 +1,9 @@
 /* eslint-env jest */
-/* global jasmine */
 import fs from 'fs-extra'
 import { join } from 'path'
 import { launchApp, findPort, nextBuild } from 'next-test-utils'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 2
+jest.setTimeout(1000 * 60 * 2)
 
 const appDir = join(__dirname, '..')
 const nextConfig = join(appDir, 'next.config.js')
