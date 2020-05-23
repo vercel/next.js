07967fce5ffbcfa361b208b4f3013dc3afda49df
jazibsawar <jazibsawar@gmail.com>
2020-05-23 04:06:09 +0300

    Used imgix image with lazyloading

--- examples/cms-cosmic/components/cover-image.js
+++ examples/cms-cosmic/components/cover-image.js
@@ -1,14 +1,24 @@
 import cn from 'classnames'
 import Link from 'next/link'
+import Imgix from 'react-imgix'
 
 export default function CoverImage({ title, url, slug }) {
   const image = (
-    <img
+    <Imgix
       src={url}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
+      className={cn('lazyload shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
+      sizes="100vw"
+      attributeConfig={{
+        src: 'data-src',
+        srcSet: 'data-srcset',
+        sizes: 'data-sizes',
+      }}
+      htmlAttributes={{
+        src: `${url}?auto=format,compress&q=1&blur=500&w=auto`,
+      }}
     />
   )
   return (
