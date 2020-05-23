b7d977dda2b279a3922ab289f82802820431d5d7
Koji Sugimoto <40754318+UndyingSugimoto@users.noreply.github.com>
2020-05-24 03:34:35 +0900

    [Examples] Added blog-starter-typescript (#12981)
    * feature: add blog-starter-typescript
    
    * chore: edit tailwind.config
    
    * Fix linting
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: Tim Neutkens <timneutkens@me.com>

---
+++ examples/blog-starter-typescript/components/container.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Container: React.FC = ({ children }) => {
+  return <div className="container mx-auto px-5">{children}</div>
+}
+
+export default Container
---
+++ examples/blog-starter-typescript/postcss.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  plugins: ['tailwindcss', 'postcss-preset-env'],
+}
