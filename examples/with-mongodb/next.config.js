fcc5d7258966b067b0dbad4394bd4f5328b91e45
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-23 18:49:24 -0400

    deleted next.config.js file

--- examples/with-mongodb/next.config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config() /* For local environment var configuration */
-
-module.exports = {
-  env: {
-    MONGO_DBURI: process.env.MONGO_DBURI,
-    VERCEL_URL: process.env.VERCEL_URL,
-  },
-}
