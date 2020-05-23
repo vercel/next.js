65b2c7203836c29ae4b816454f008a649911aab2
jazibsawar <jazibsawar@gmail.com>
2020-05-23 04:05:33 +0300

    Load lazysizes in layout

--- examples/cms-cosmic/components/layout.js
+++ examples/cms-cosmic/components/layout.js
@@ -1,6 +1,8 @@
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
+import 'lazysizes'
+import 'lazysizes/plugins/parent-fit/ls.parent-fit'
 
 export default function Layout({ preview, children }) {
   return (
