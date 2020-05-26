f724b63281c7bde0e98e825437cbbd911abbb593
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-26 15:11:10 +0300

    upgraded packages and converted all the components from class to functional

--- examples/with-videojs/components/Player.js
+++ examples/with-videojs/components/Player.js
@@ -1,34 +1,23 @@
-import { Component } from 'react'
+import { useRef, useEffect } from 'react'
 import videojs from 'video.js'
 import 'videojs-youtube'
 
-class Player extends Component {
-  componentDidMount() {
-    // instantiate Video.js
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log('onPlayerReady', this)
-    })
-  }
+const Player = (props) => {
+  const playerRef = useRef()
 
-  // destroy player on unmount
-  componentWillUnmount() {
-    if (this.player) {
-      this.player.dispose()
+  useEffect(() => {
+    const player = videojs(playerRef.current, props)
+
+    return () => {
+      player.dispose()
     }
-  }
+  }, [])
 
-  // wrap the player in a div with a `data-vjs-player` attribute
-  // so videojs won't create additional wrapper in the DOM
-  // see https://github.com/videojs/video.js/pull/3856
-  render() {
-    return (
-      <div>
-        <div data-vjs-player>
-          <video ref={(node) => (this.videoNode = node)} className="video-js" />
-        </div>
-      </div>
-    )
-  }
+  return (
+    <div data-vjs-player>
+      <video ref={playerRef} className="video-js" playsInline />
+    </div>
+  )
 }
 
 export default Player
