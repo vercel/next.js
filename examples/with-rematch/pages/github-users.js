1274093531a4c2b44c1b7b48818b3fc6955b5715
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-26 14:22:20 +0300

    added ssg

--- examples/with-rematch/pages/github-users.js
+++ examples/with-rematch/pages/github-users.js
@@ -5,12 +5,14 @@ import { initializeStore } from '../shared/store'
 import CounterDisplay from '../shared/components/counter-display'
 import Header from '../shared/components/header'
 
-const Github = () => {
+const Github = (props) => {
   const github = useSelector((state) => state.github)
   const { users, isLoading } = github
   const { fetchUsers } = useRematchDispatch((dispatch) => ({
     fetchUsers: dispatch.github.fetchUsers,
   }))
+
+  const { usersList } = props
   return (
     <div>
       <Header />
@@ -19,10 +21,21 @@ const Github = () => {
         Server rendered github user list. You can also reload the users from the
         api by clicking the <b>Get users</b> button below.
       </p>
+      <h1> Users passed as property from getStaticProps</h1>
+      {usersList.map((user) => (
+        <div key={user.login}>
+          <a href={user.html_url}>
+            <img height="45" width="45" src={user.avatar_url} />
+            <span> Username - {user.login}</span>
+          </a>
+          <br />
+        </div>
+      ))}
       {isLoading ? <p>Loading ...</p> : null}
       <p>
         <button onClick={fetchUsers}>Get users</button>
       </p>
+      {users.length === 0 ? null : <h1> Users fetched from async function</h1>}
       {users.map((user) => (
         <div key={user.login}>
           <a href={user.html_url}>
@@ -33,10 +46,21 @@ const Github = () => {
         </div>
       ))}
       <br />
-
       <CounterDisplay />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const store = initializeStore()
+  const usersList = await store.dispatch.github.fetchUsers()
+  console.log(usersList)
+
+  return {
+    props: {
+      usersList,
+    },
+  }
 }
 
 export default Github
