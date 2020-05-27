640c9b911c6a3393e4c32d1b9b78c8c3554d843e
Luis Alvarez <luis@vercel.com>
2020-05-27 15:43:32 -0500

    Updated description

--- examples/with-route-as-modal/components/Grid.js
+++ examples/with-route-as-modal/components/Grid.js
@@ -6,7 +6,7 @@ export const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 export default function PostCardGrid() {
   return (
     <div className={styles.postCardGridWrapper}>
-      <h2>With QueryString Routing</h2>
+      <h2>With QueryString Routing, and a reload won't use the modal</h2>
       <div className={styles.postCardGrid}>
         {data.map((id, index) => (
           <Link key={index} href={`/?postId=${id}`} as={`/post/${id}`}>
@@ -15,7 +15,7 @@ export default function PostCardGrid() {
         ))}
       </div>
 
-      <h2>With Dynamic Routing</h2>
+      <h2>With Dynamic Routing, and reloads will keep the modal</h2>
       <div className={styles.postCardGrid}>
         {data.map((id, index) => (
           <Link key={index} href="/article/[articleId]" as={`/article/${id}`}>
