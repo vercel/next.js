c893717dbb5e81e59e54da4c1e877d37675b3188
Michael Albertz <m.albertz@sb-konzept.de>
2020-05-27 16:08:32 +0200

    Remove some unused things

--- examples/with-web-worker/pages/index.js
+++ examples/with-web-worker/pages/index.js
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useCallback } from 'react'
 
-export default function Index({ stars }) {
+export default function Index() {
   const workerRef = useRef()
   useEffect(() => {
     workerRef.current = new Worker('../worker.js', { type: 'module' })
-    workerRef.current.onmessage = evt =>
+    workerRef.current.onmessage = (evt) =>
       alert(`WebWorker Response => ${evt.data}`)
     return () => {
       workerRef.current.terminate()
@@ -18,9 +18,7 @@ export default function Index({ stars }) {
   return (
     <div>
       <p>Do work in a WebWorker!</p>
-      <button onClick={handleWork} className="card">
-        Calculate PI
-      </button>
+      <button onClick={handleWork}>Calculate PI</button>
     </div>
   )
 }
