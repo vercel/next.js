c461d278f6e113e7643cc6208edabe058da19f04
Chandu <43699825+chandan-reddy-k@users.noreply.github.com>
2020-05-28 10:42:27 +0530

    refactor(with-redux): update example to use getStaticProps (#13378)
    @lfades hope this works. I have updated the example to show differences between client/ssr/ssg initial state in redux. Let me know if something needs to be changed.

--- examples/with-redux/pages/index.js
+++ examples/with-redux/pages/index.js
@@ -1,39 +1,5 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { withRedux } from '../lib/redux'
-import useInterval from '../lib/useInterval'
-import Clock from '../components/clock'
-import Counter from '../components/counter'
+import Page from '../components/page'
 
-const IndexPage = () => {
-  // Tick the time every second
-  const dispatch = useDispatch()
-  useInterval(() => {
-    dispatch({
-      type: 'TICK',
-      light: true,
-      lastUpdate: Date.now(),
-    })
-  }, 1000)
-  return (
-    <>
-      <Clock />
-      <Counter />
-    </>
-  )
+export default function Index() {
+  return <Page />
 }
-
-IndexPage.getInitialProps = ({ reduxStore }) => {
-  // Tick the time once, so we'll have a
-  // valid time before first render
-  const { dispatch } = reduxStore
-  dispatch({
-    type: 'TICK',
-    light: typeof window === 'object',
-    lastUpdate: Date.now(),
-  })
-
-  return {}
-}
-
-export default withRedux(IndexPage)
