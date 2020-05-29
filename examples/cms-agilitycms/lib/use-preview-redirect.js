12b6e2f0e71e7e10256ff226c32bac69374a98c1
Luis Alvarez <luis@vercel.com>
2020-05-28 20:26:02 -0500

    Fix

--- examples/cms-agilitycms/lib/use-preview-redirect.js
+++ examples/cms-agilitycms/lib/use-preview-redirect.js
@@ -4,18 +4,18 @@ import { useRouter } from 'next/router'
 export default function usePreviewRedirect() {
   const router = useRouter()
   const { query, asPath } = router
-  const { agilityPreviewKey, contentid } = query
+  const { agilitypreviewkey, contentid } = query
 
   useEffect(() => {
     // kickout if we don't have an agilityPreviewKey
-    if (!agilityPreviewKey) return
+    if (!agilitypreviewkey) return
 
     // redirect to our preview API route
-    let redirectLink = `/api/preview?slug=${asPath}&agilitypreviewkey=${agilityPreviewKey}`
+    let redirectLink = `/api/preview?slug=${asPath}&agilitypreviewkey=${agilitypreviewkey}`
 
     // Check if we have a `contentid` in the query, if so this is a preview request for a Dynamic Page Item
     if (contentid) redirectLink = `${redirectLink}&contentid=${contentid}`
 
     window.location.href = redirectLink
-  }, [asPath, agilityPreviewKey, contentid])
+  }, [asPath, agilitypreviewkey, contentid])
 }
