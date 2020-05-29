87a6413dec090c132b6ca030e22a89b092f8ebce
JJ Kasper <jj@jjsweb.site>
2020-05-29 09:47:24 -0500

    run lint

--- test/integration/prerender/pages/lang/[lang]/about.js
+++ test/integration/prerender/pages/lang/[lang]/about.js
@@ -7,6 +7,6 @@ export const getStaticProps = ({ params: { lang } }) => ({
 })
 
 export const getStaticPaths = () => ({
-  paths: ['en', 'es', 'fr', 'de'].map(p => `/lang/${p}/about`),
+  paths: ['en', 'es', 'fr', 'de'].map((p) => `/lang/${p}/about`),
   fallback: false,
 })
