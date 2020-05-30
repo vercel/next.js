4e4cb1401a44dc10a058d2448e8ca140b6817cd0
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-05-30 18:49:37 +0200

    Fix, rename var name inside .env.example Prefix variables name with NEXT_PUBLIC_ to align with Nextjs 9.4

--- examples/with-firebase-authentication/utils/auth/firebaseAdmin.js
+++ examples/with-firebase-authentication/utils/auth/firebaseAdmin.js
@@ -1,17 +1,17 @@
 import * as admin from 'firebase-admin'
 
 export const verifyIdToken = (token) => {
-  const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY
+  const firebasePrivateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
 
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.NEXT_PUBLIC_IREBASE_CLIENT_EMAIL,
         // https://stackoverflow.com/a/41044630/1332513
         privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
       }),
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     })
   }
 
