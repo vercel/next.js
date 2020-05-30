9044f654fe6bae130bdc0e5361a3167a9800d307
Dave Brudner <dbrudne1@gmail.com>
2020-05-30 08:56:15 -0500

    Adds type for NextApiRequest.body

--- packages/next/next-server/lib/utils.ts
+++ packages/next/next-server/lib/utils.ts
@@ -174,7 +174,7 @@ export type DocumentProps = DocumentInitialProps & {
 /**
  * Next `API` route request
  */
-export interface NextApiRequest extends IncomingMessage {
+export interface NextApiRequest<T = any> extends IncomingMessage {
   /**
    * Object of `query` values from url
    */
@@ -188,7 +188,7 @@ export interface NextApiRequest extends IncomingMessage {
     [key: string]: string
   }
 
-  body: any
+  body: T
 
   env: Env
 }
