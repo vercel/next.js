0fc29c9efa6135cd65e2d9d9664b767811a1754f
MichelleLucero <31500626+MichelleLucero@users.noreply.github.com>
2020-05-30 13:15:00 -0400

    [ EXAMPLES ] Added with-mongodb example (#13028)
    * created with-mongodb, Next.js + MongoDB
    
    Co-authored-by: Shania Dhani <31454777+sdhani@users.noreply.github.com>
    Co-authored-by: Michelle Lucero <31500626+MichelleLucero@users.noreply.github.com>
    Co-authored-by: Chi Shing Lee <60354073+chislee0708@users.noreply.github.com>
    
    * Update examples/with-mongodb/README.md
    
    Co-authored-by: Tim Neutkens <tim@timneutkens.nl>
    
    * modified for dotenv and environment var setup
    
    * removed extra instructions on readme, linked it to the MongoDB guides
    
    * changes to environment var
    
    * replaced document with react logic
    
    * removed Pet header
    
    * removed isomorphic-unfetch
    
    * replaced getInitialProps with getStaticProps (SSG)
    
    * removed console log
    
    * deleted next.config.js file
    
    * fixed prettier styling
    
    * fixed link 404 server rendering issue
    
    * modified server path for delete/edit
    
    * fixed prettier styling
    
    * fixed a typo
    
    * change .env to .env.local
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>
    
    * fixed typo, change .env to .env.local
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>
    
    * change .env to .env.local
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>
    
    * Update examples/with-mongodb/package.json
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>
    
    * removed dotenv dev dependency
    
    Co-authored-by: Joe Haddad <timer150@gmail.com>
    
    * cleaned up code and instructions
    
    * Update examples/with-mongodb/.gitignore
    
    Co-authored-by: Luis Alvarez D. <luis@vercel.com>
    
    * minor changes to code style and logic
    
    * Updated index page and .env.local.example
    
    * Updated new page
    
    * Don't use SSG for the edit page
    
    * Fixed revalidation issue
    
    * Use es6 for the model
    
    * Updated edit page
    
    * Removed VERCEL_URL
    
    * Updated readme and renamed env
    
    * Added temporal vercel.json
    
    * Removed seed until added with a script
    
    * Updated package.json
    
    * Renamed example in readme
    
    * Renamed example
    
    * Lint fix
    
    Co-authored-by: liulanz <zhengliulan@gmail.com>
    Co-authored-by: Shania Dhani <31454777+sdhani@users.noreply.github.com>
    Co-authored-by: Chi Shing Lee <60354073+chislee0708@users.noreply.github.com>
    Co-authored-by: Tim Neutkens <tim@timneutkens.nl>
    Co-authored-by: sdhani <shania.dhani46@myhunter.cuny.edu>
    Co-authored-by: liulanz <37808313+liulanz@users.noreply.github.com>
    Co-authored-by: Joe Haddad <timer150@gmail.com>
    Co-authored-by: Luis Alvarez D. <luis@vercel.com>

---
+++ examples/with-mongodb-mongoose/utils/dbConnect.js
@@ -0,0 +1,22 @@
+/* This is a database connection function*/
+import mongoose from 'mongoose'
+
+const connection = {} /* creating connection object*/
+
+async function dbConnect() {
+  /* check if we have connection to our databse*/
+  if (connection.isConnected) {
+    return
+  }
+
+  /* connecting to our database */
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+
+  connection.isConnected = db.connections[0].readyState
+}
+
+export default dbConnect
