07bd8e3e67e8342e7a82d50e795e855f294be2a7
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-31 17:05:19 +0300

    Adjusted the homepage to the new functionality

--- examples/with-apollo/pages/index.js
+++ examples/with-apollo/pages/index.js
@@ -3,27 +3,22 @@ import InfoBox from '../components/InfoBox'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
-import { withApollo } from '../lib/apollo'
+import renderWithApollo from '../lib/renderWithApollo'
 
 const IndexPage = () => (
   <App>
-    <Header />
+    <Header pathname="/" />
     <InfoBox>
-      ℹ️ This example shows how to fetch all initial apollo queries on the
-      server. If you <a href="/">reload</a> this page you won't see a loader
-      since Apollo fetched all needed data on the server. This prevents{' '}
-      <a
-        href="https://nextjs.org/blog/next-9#automatic-static-optimization"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        automatic static optimization
-      </a>{' '}
-      in favour of full Server-Side-Rendering.
+      ℹ️ This example shows how to use SSG with Apollo provider.
     </InfoBox>
     <Submit />
     <PostList />
   </App>
 )
 
-export default withApollo({ ssr: true })(IndexPage)
+export async function getStaticProps() {
+  await renderWithApollo(IndexPage)
+  return { props: {} }
+}
+
+export default IndexPage
