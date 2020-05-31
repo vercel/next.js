3c3d4628d67c49b3686b77752e3b529a5bee9d2c
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-31 18:05:54 +0300

    forgot to commit that one

--- examples/with-apollo-and-redux/pages/apollo.js
+++ examples/with-apollo-and-redux/pages/apollo.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout'
 import Submit from '../components/Submit'
 import PostList from '../components/PostList'
+import { renderWithApollo } from '../lib/apollo'
 
 const ApolloPage = () => (
   <Layout>
@@ -8,5 +9,12 @@ const ApolloPage = () => (
     <PostList />
   </Layout>
 )
+
+export function getStaticProps() {
+  renderWithApollo(ApolloPage)
+  return {
+    props: {},
+  }
+}
 
 export default ApolloPage
