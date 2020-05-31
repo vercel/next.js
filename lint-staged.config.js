5efb52520c7127edfb6728e4cb82b9d96a066fa9
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-05-31 16:03:16 +0200

    Actually, this is what we want

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,19 +7,18 @@ const isWin = process.platform === 'win32'
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': (filenames) => {
     const escapedFileNames = filenames
-      .filter((file) => !cli.isPathIgnored(file))
       .map((filename) => `"${isWin ? filename : escape([filename])}"`)
       .join(' ')
     return [
       `prettier --with-node-modules --ignore-path .prettierignore_staged --write ${escapedFileNames}`,
       `eslint --no-ignore --max-warnings=0 --fix ${filenames
+        .filter((file) => !cli.isPathIgnored(file))
         .map((f) => `"${f}"`)
         .join(' ')}`,
     ]
   },
   '**/*.{json,md,mdx,css,html,yml,yaml,scss}': (filenames) => {
     const escapedFileNames = filenames
-      .filter((file) => !cli.isPathIgnored(file))
       .map((filename) => `"${isWin ? filename : escape([filename])}"`)
       .join(' ')
     return [
