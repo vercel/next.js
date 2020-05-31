c190a162f52e5a2cc6e8c36a071e70b638c1ce1b
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-31 17:37:04 +0300

    Refactored the store

--- examples/with-apollo-and-redux/store.js
+++ examples/with-apollo-and-redux/store.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux'
+import { useMemo } from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+let store
 
 const initialState = {
   lastUpdate: 0,
@@ -34,6 +38,37 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export const initializeStore = (preloadedState = initialState) => {
-  return createStore(reducer, preloadedState)
+function initStore(preloadedState = initialState) {
+  return createStore(
+    reducer,
+    preloadedState,
+    composeWithDevTools(applyMiddleware())
+  )
+}
+
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState)
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+}
+
+export function useStore(initialState) {
+  const store = useMemo(() => initializeStore(initialState), [initialState])
+  return store
 }
