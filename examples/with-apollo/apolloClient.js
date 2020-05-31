50e8db4bf564187451a55906f97db9511c85abb4
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-31 17:04:02 +0300

    Turned ssr mode to false in favor of SSG

--- examples/with-apollo/apolloClient.js
+++ examples/with-apollo/apolloClient.js
@@ -2,11 +2,9 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 
-export default function createApolloClient(initialState, ctx) {
-  // The `ctx` (NextPageContext) will only be present on the server.
-  // use it to extract auth headers (ctx.req) or similar.
+export default function createApolloClient(initialState) {
   return new ApolloClient({
-    ssrMode: Boolean(ctx),
+    ssrMode: false,
     link: new HttpLink({
       uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
       credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
