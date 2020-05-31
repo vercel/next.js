fe5fe8cb38a1b186e3f5a4140d2235867362e51c
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-05-31 19:31:23 +0300

    Refactored the characterinfo component

--- examples/with-redux-observable/components/CharacterInfo.js
+++ examples/with-redux-observable/components/CharacterInfo.js
@@ -1,44 +1,43 @@
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-const CharacterInfo = ({
-  character,
-  error,
-  fetchCharacter,
-  isFetchedOnServer = false,
-}) => (
-  <div className="CharacterInfo">
-    {error ? (
-      <p>We encountered and error.</p>
-    ) : (
-      <article>
-        <h3>Character: {character.name}</h3>
-        <p>birth year: {character.birth_year}</p>
-        <p>gender: {character.gender}</p>
-        <p>skin color: {character.skin_color}</p>
-        <p>eye color: {character.eye_color}</p>
-      </article>
-    )}
-    <p>
-      (was character fetched on server? - <b>{isFetchedOnServer.toString()})</b>
-    </p>
-    <style jsx>{`
-      article {
-        background-color: #528ce0;
-        border-radius: 15px;
-        padding: 15px;
-        width: 250px;
-        margin: 15px 0;
-        color: white;
-      }
-      button {
-        margin-right: 10px;
-      }
-    `}</style>
-  </div>
-)
+const UserInfo = ({ error, fetchCharacter, isFetchedOnServer = false }) => {
+  const { name, id, username, email, phone, website } = useSelector(
+    (state) => state.character
+  )
 
-export default connect((state) => ({
-  character: state.character,
-  error: state.error,
-  isFetchedOnServer: state.isFetchedOnServer,
-}))(CharacterInfo)
+  return (
+    <div className="UserInfo">
+      {error ? (
+        <p>We encountered and error.</p>
+      ) : (
+        <article>
+          <h3>Name: {name}</h3>
+          <p>Id: {id}</p>
+          <p>Username: {username}</p>
+          <p>Email: {email}</p>
+          <p>Phone: {phone}</p>
+          <p>Website: {website}</p>
+        </article>
+      )}
+      <p>
+        (was character fetched on server? -{' '}
+        <b>{isFetchedOnServer.toString()})</b>
+      </p>
+      <style jsx>{`
+        article {
+          background-color: #528ce0;
+          border-radius: 15px;
+          padding: 15px;
+          width: 250px;
+          margin: 15px 0;
+          color: white;
+        }
+        button {
+          margin-right: 10px;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default UserInfo
