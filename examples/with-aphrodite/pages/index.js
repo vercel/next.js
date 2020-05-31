98547e7a8ead2bab51c8b3076795cf54427c26f9
Martin Bavio <mbavio@gmail.com>
2020-05-31 17:20:01 -0300

    Fix examples using __NEXT_DATA__ (#13567)
    This PR fixes https://github.com/vercel/next.js/issues/4883.
    
    It introduces a new global variable (`window.__REHYDRATE_IDS`) only when necessary.
    
    In the case of the `with-react-with-styles` example, rehydration isn't happening, so I removed the creation of the variable. 
    
    In the case of the `with-react-intl` example, the information doesn't seem to be set anymore on `__NEXT_DATA__`, so I just removed the default reference to it.

--- examples/with-aphrodite/pages/index.js
+++ examples/with-aphrodite/pages/index.js
@@ -1,10 +1,10 @@
 import { StyleSheet, css } from 'aphrodite'
 
+// Rehydrate to ensure that the client doesn't duplicate styles
+// It has to execute before any code that defines styles
+// '__REHYDRATE_IDS' is set in '_document.js'
 if (typeof window !== 'undefined') {
-  /* StyleSheet.rehydrate takes an array of rendered classnames,
-  and ensures that the client side render doesn't generate
-  duplicate style definitions in the <style data-aphrodite> tag */
-  StyleSheet.rehydrate(window.__NEXT_DATA__.ids)
+  StyleSheet.rehydrate(window.__REHYDRATE_IDS)
 }
 
 export default function Home() {
