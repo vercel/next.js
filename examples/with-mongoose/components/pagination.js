e690ab2fac8a4c2ff0cbfefec332b8248178d8e9
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-05-31 20:09:26 -0600

    feat(with-mongoose): Improve pagination component

--- examples/with-mongoose/components/pagination.js
+++ examples/with-mongoose/components/pagination.js
@@ -1,36 +1,46 @@
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 
-const Pagination = (props) => (
-  <nav className="blog-pagination">
-    <Link href={`?page=${props.prevPage}`}>
-      <a
-        className={`btn ${
-          props.hasPrevPage
-            ? 'btn-outline-primary'
-            : 'btn-outline-secondary disabled'
-        }`}
-        {...(props.hasPrevPage
-          ? {}
-          : { tabIndex: '-1', 'aria-disabled': 'true' })}
-      >
-        Newer
-      </a>
-    </Link>
-    <Link href={`?page=${props.nextPage}`}>
-      <a
-        className={`btn ${
-          props.hasNextPage
-            ? 'btn-outline-primary'
-            : 'btn-outline-secondary disabled'
-        }`}
-        {...(props.hasNextPage
-          ? {}
-          : { tabIndex: '-1', 'aria-disabled': 'true' })}
-      >
-        Older
-      </a>
-    </Link>
+const Pagination = ({
+  hasNextPage,
+  hasPrevPage,
+  nextPage,
+  prevPage,
+  page,
+  totalPages,
+}) => (
+  <nav className="blog-pagination" aria-label="Blog pagination">
+    <ul className="pagination justify-content-center">
+      <li className={`page-item${hasPrevPage ? '' : ' disabled'}`}>
+        <Link href={`?page=${prevPage}`}>
+          <a
+            className="page-link"
+            {...(hasPrevPage
+              ? {}
+              : { tabIndex: '-1', 'aria-disabled': 'true' })}
+          >
+            Newer
+          </a>
+        </Link>
+      </li>
+      <li className="page-item active">
+        <span className="page-link">
+          Page {page} of {totalPages}
+        </span>
+      </li>
+      <li className={`page-item${hasNextPage ? '' : ' disabled'}`}>
+        <Link href={`?page=${nextPage}`}>
+          <a
+            className="page-link"
+            {...(hasNextPage
+              ? {}
+              : { tabIndex: '-1', 'aria-disabled': 'true' })}
+          >
+            Older
+          </a>
+        </Link>
+      </li>
+    </ul>
   </nav>
 )
 Pagination.propTypes = {
@@ -39,9 +49,6 @@ Pagination.propTypes = {
   nextPage: PropTypes.number,
   prevPage: PropTypes.number,
   page: PropTypes.number,
-  limit: PropTypes.number,
-  pagingCounter: PropTypes.number,
-  totalDocs: PropTypes.number,
   totalPages: PropTypes.number,
 }
 
