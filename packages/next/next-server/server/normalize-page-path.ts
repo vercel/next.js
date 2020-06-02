b9dbea0e69971aacfd7a1f73678a80a9a40787c7
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-06-02 14:58:34 +0200

    build without errors

--- packages/next/next-server/server/normalize-page-path.ts
+++ packages/next/next-server/server/normalize-page-path.ts
@@ -3,6 +3,8 @@ export function normalizePagePath(page: string): string {
   // If the page is `/` we need to append `/index`, otherwise the returned directory root will be bundles instead of pages
   if (page === '/') {
     page = '/index'
+  } else if (page === '/index' || page.startsWith('/index/')) {
+    page = `/index${page}`
   }
   // Resolve on anything that doesn't start with `/`
   if (!page.startsWith('/')) {
@@ -14,6 +16,15 @@ export function normalizePagePath(page: string): string {
     throw new Error(
       `Requested and resolved page mismatch: ${page} ${resolvedPage}`
     )
+  }
+  return page
+}
+
+export function denormalizePagePath(page: string) {
+  if (page.startsWith('/index/')) {
+    page = page.slice(6)
+  } else if (page === '/index') {
+    page = '/'
   }
   return page
 }
