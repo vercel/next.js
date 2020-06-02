6c7e66f034af00358009fea46f820f2a3e6d5dbc
James Vidler <james@agilitycms.com>
2020-06-01 21:49:04 -0400

    Added Agility CMS example 🏆 (#12788)

---
+++ examples/cms-agilitycms/postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    'postcss-preset-env',
+  ],
+}
