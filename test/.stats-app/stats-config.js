a92e976231a56b9883132cafc17d2640507c5c29
Joe Haddad <joe.haddad@zeit.co>
2020-06-02 10:45:07 -0400

    Remove remaining granular chunks references (#13672)
    This removes remaining references to `granularChunks` in configs, error messages, and comments.
    
    Also removed the `process.env.__NEXT_GRANULAR_CHUNKS` value.
    
    ---
    
    Follow up to: https://github.com/vercel/next.js/pull/13663

--- test/.stats-app/stats-config.js
+++ test/.stats-app/stats-config.js
@@ -112,8 +112,7 @@ module.exports = {
                 return config
               },
               experimental: {
-                modern: true,
-                granularChunks: true
+                modern: true
               }
             }
           `,
@@ -128,8 +127,7 @@ module.exports = {
             module.exports = {
               generateBuildId: () => 'BUILD_ID',
               experimental: {
-                modern: true,
-                granularChunks: true
+                modern: true
               }
             }
           `,
@@ -164,8 +162,7 @@ module.exports = {
               generateBuildId: () => 'BUILD_ID',
               target: 'serverless',
               experimental: {
-                modern: true,
-                granularChunks: true
+                modern: true
               }
             }
           `,
