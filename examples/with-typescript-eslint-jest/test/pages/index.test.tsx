8a09ea38f507114acb96f1c54b1c655fa8bbb384
Luis Alvarez <luis@vercel.com>
2020-06-03 16:26:14 -0500

    prettier fix

--- examples/with-typescript-eslint-jest/test/pages/index.test.tsx
+++ examples/with-typescript-eslint-jest/test/pages/index.test.tsx
@@ -7,11 +7,11 @@ describe('Home page', () => {
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
-  
+
   it('clicking button triggers alert', () => {
     const { getByText } = render(<Home />, {})
     window.alert = jest.fn()
     fireEvent.click(getByText('Test Button'))
     expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
-  })  
+  })
 })
