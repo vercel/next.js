5aeedea8601f468ccd2a6c92be776c293c156a35
Filipe Marins <filipe.marins@olist.com>
2020-06-03 18:43:53 -0300

    Update test description of with typescript eslint jest example (#13732)
    The tests was with description changed

--- examples/with-typescript-eslint-jest/test/pages/index.test.tsx
+++ examples/with-typescript-eslint-jest/test/pages/index.test.tsx
@@ -4,14 +4,14 @@ import { Home } from '../../pages/index'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
+    const { asFragment } = render(<Home />, {})
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('clicking button triggers alert', () => {
     const { getByText } = render(<Home />, {})
     window.alert = jest.fn()
     fireEvent.click(getByText('Test Button'))
     expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
-  })
-
-  it('clicking button triggers alert', () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
   })
 })
