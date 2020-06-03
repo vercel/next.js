55b316f19d67e62e75c1cb355b996ffc91b244d0
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2020-06-03 14:46:50 +0200

    chore: abstract next koa middlewares

--- examples/custom-server-koa/server.js
+++ examples/custom-server-koa/server.js
@@ -1,38 +1,21 @@
 const Koa = require('koa')
-const next = require('next')
 const Router = require('@koa/router')
+const { handleRoutes, defaultReturn, render } = require('nextjs-koa-middleware')
 
 const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = new Koa()
-  const router = new Router()
+const app = new Koa()
 
-  router.get('/a', async (ctx) => {
-    await app.render(ctx.req, ctx.res, '/a', ctx.query)
-    ctx.respond = false
-  })
+const router = new Router()
 
-  router.get('/b', async (ctx) => {
-    await app.render(ctx.req, ctx.res, '/b', ctx.query)
-    ctx.respond = false
-  })
+app.use(defaultReturn())
 
-  router.all('*', async (ctx) => {
-    await handle(ctx.req, ctx.res)
-    ctx.respond = false
-  })
+router.get('/a', render('/a'))
+router.get('/b', render('/b'))
 
-  server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200
-    await next()
-  })
+router.all('*', handleRoutes())
 
-  server.use(router.routes())
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+app.use(router.routes())
+app.listen(port, () => {
+  console.log(`> Ready on http://localhost:${port}`)
 })
