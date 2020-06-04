1327ef781ab3a0d8b9ff8cf625aaee80c34deef7
Timothy <1695613+timothyis@users.noreply.github.com>
2020-06-04 15:57:17 +0100

    Update instances of Now to Vercel where applicable (#13760)
    This pull request renames various instances of `now` to `vercel` etc. in order to match content that's being updated on the Vercel platform.

--- examples/with-iron-session/lib/session.js
+++ examples/with-iron-session/lib/session.js
@@ -3,13 +3,13 @@ import { withIronSession } from 'next-iron-session'
 
 export default function withSession(handler) {
   return withIronSession(handler, {
-    // The password in this example is in plain text (inside `now.json`) for ease of deployment and understanding.
+    // The password in this example is in plain text (inside `vercel.json`) for ease of deployment and understanding.
     // ⚠️ Do not reuse the same password, create a different password for you and store it in a secret management system
     // Example for Vercel: https://vercel.com/docs/v2/serverless-functions/env-and-secrets
     password: process.env.SECRET_COOKIE_PASSWORD,
     cookieName: 'next.js/examples/with-iron-session',
     cookieOptions: {
-      // the next line allows to use the session in non-https environements like
+      // the next line allows to use the session in non-https environments like
       // Next.js dev mode (http://localhost:3000)
       secure: process.env.NODE_ENV === 'production' ? true : false,
     },
