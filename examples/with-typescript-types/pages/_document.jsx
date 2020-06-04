c2a6ea61c5d2b1a78fee44767eb5643bdbd869d0
Todor Totev <51530311+TodorTotev@users.noreply.github.com>
2020-06-04 19:25:28 +0300

    [Example] with-typescript-types (#13746)
    Closes [7882](https://github.com/vercel/next.js/issues/7882).
    Created as requested by @timneutkens 
    
    I'm unsure if that's exactly what you wanted, so let me know what you want me to change and I'll do it asap.

---
+++ examples/with-typescript-types/pages/_document.jsx
@@ -0,0 +1,17 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
