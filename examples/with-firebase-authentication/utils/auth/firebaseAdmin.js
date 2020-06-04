f4b0e7459776b507620e63335893bd8c4f6a8908
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-06-04 09:01:22 +0200

    Update Firebase Auth Example for New Env Support (#13587)
    Prefixed variables name with NEXT_PUBLIC_.
    
    Solving #13585 issue related to get undefined from process.env.VARNAME inside .env* files.
    
    I'm not secure if SESSION_SECRET_* need the prefix.

--- examples/with-firebase-authentication/utils/auth/firebaseAdmin.js
+++ examples/with-firebase-authentication/utils/auth/firebaseAdmin.js
@@ -6,12 +6,12 @@ export const verifyIdToken = (token) => {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // https://stackoverflow.com/a/41044630/1332513
         privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
       }),
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     })
   }
 
--- examples/with-firebase-authentication/utils/auth/initFirebase.js
+++ examples/with-firebase-authentication/utils/auth/initFirebase.js
@@ -2,10 +2,10 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const config = {
-  apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 }
 
 export default function initFirebase() {
