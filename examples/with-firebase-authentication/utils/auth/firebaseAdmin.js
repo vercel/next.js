eb6c907047a7bb78a4924c026955e8ccf680f3f8
Luis Alvarez <luis@vercel.com>
2020-06-04 01:42:49 -0500

    Fixes and updated readme

--- examples/with-firebase-authentication/utils/auth/firebaseAdmin.js
+++ examples/with-firebase-authentication/utils/auth/firebaseAdmin.js
@@ -1,13 +1,13 @@
 import * as admin from 'firebase-admin'
 
 export const verifyIdToken = (token) => {
-  const firebasePrivateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
+  const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY
 
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_PUBLIC_IREBASE_CLIENT_EMAIL,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // https://stackoverflow.com/a/41044630/1332513
         privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
       }),
