792a113d482b770f412403b4a0d33b3451bac23d
Luis Alvarez D <luis@vercel.com>
2020-06-06 08:04:52 -0500

    [Examples] Move api-routes-apollo-server-and-client to SSG (#13782)
    Ref: https://github.com/vercel/next.js/pull/13742

---
+++ examples/api-routes-apollo-server-and-client/pages/_app.js
@@ -0,0 +1,12 @@
+import { ApolloProvider } from '@apollo/react-hooks'
+import { useApollo } from '../apollo/client'
+
+export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
