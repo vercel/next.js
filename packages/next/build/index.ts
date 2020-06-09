1f3566d9eb730f0cc7475ef49f63f14e4ae742d0
Tim Neutkens <timneutkens@me.com>
2020-06-09 14:01:20 +0200

    Get rid of commented line

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -1018,8 +1018,6 @@ export default async function build(dir: string, conf = null): Promise<void> {
       JSON.stringify(prerenderManifest),
       'utf8'
     )
-    // No need to call this fn as we already emitted a default SSG manifest:
-    // await generateClientSsgManifest(prerenderManifest, { distDir, buildId })
   }
 
   await promises.writeFile(
