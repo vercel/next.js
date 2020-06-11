21bb7f68f310ab027800793c66fd74627ab52caf
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-06-11 10:44:45 +0300

    added logout to the useUser hook

--- examples/with-firebase-authentication/utils/auth/useUser.js
+++ examples/with-firebase-authentication/utils/auth/useUser.js
@@ -1,9 +1,29 @@
 import { useEffect, useState } from 'react'
 import Router from 'next/router'
 import cookies from 'js-cookie'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import initFirebase from '../auth/initFirebase'
+
+initFirebase()
 
 const useUser = () => {
   const [user, setUser] = useState()
+
+  const logout = async () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        cookies.remove('auth')
+        Router.push('/auth')
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  }
+
   useEffect(() => {
     const cookie = cookies.get('auth')
     if (!cookie) {
@@ -13,7 +33,7 @@ const useUser = () => {
     setUser(JSON.parse(cookie))
   }, [])
 
-  return { user }
+  return { user, logout }
 }
 
 export { useUser }
