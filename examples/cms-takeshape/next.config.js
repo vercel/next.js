0595ab0ab70ac19256fc2fdc08ef9b309f382de0
Luis Alvarez <luis@vercel.com>
2020-06-11 00:53:42 -0500

    Removed dotenv from cms-takeshape

--- examples/cms-takeshape/next.config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-  env: {
-    TAKESHAPE_API_KEY: process.env.TAKESHAPE_API_KEY,
-    TAKESHAPE_PROJECT_ID: process.env.TAKESHAPE_PROJECT_ID,
-  },
-}
