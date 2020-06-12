6018100e33329e29f35e56ec060d97bfa62a092b
Luis Alvarez D <luis@vercel.com>
2020-06-11 21:29:26 -0500

    Use the expected import (#14111)

--- examples/api-routes-apollo-server-and-client-auth/lib/user.js
+++ examples/api-routes-apollo-server-and-client-auth/lib/user.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
-import uuidv4 from 'uuid/v4'
+import { v4 as uuidv4 } from 'uuid'
 
 /**
  * User methods. The example doesn't contain a DB, but for real applications you must use a
