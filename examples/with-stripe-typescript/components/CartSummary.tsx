64ce389ed776e935584d35f9859a50f0f6b2c745
Thorsten Schaeff <thorsten@stripe.com>
2020-06-13 21:24:05 +0800

    suppressHydrationWarning

--- examples/with-stripe-typescript/components/CartSummary.tsx
+++ examples/with-stripe-typescript/components/CartSummary.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { useShoppingCart } from 'use-shopping-cart'
 import { fetchPostJSON } from '../utils/api-helpers'
 
 const CartSummary = () => {
   const [loading, setLoading] = useState(false)
+  const [cartEmpty, setCartEmpty] = useState(true)
   const {
     formattedTotalPrice,
     cartCount,
@@ -13,28 +14,35 @@ const CartSummary = () => {
     redirectToCheckout,
   } = useShoppingCart()
 
+  useEffect(() => setCartEmpty(!cartCount), [cartCount])
+
   const handleCheckout: React.FormEventHandler<HTMLFormElement> = async (
     event
   ) => {
     event.preventDefault()
     setLoading(true)
 
-    const { id: sessionId } = await fetchPostJSON(
+    const response = await fetchPostJSON(
       '/api/checkout_sessions/cart',
       cartDetails
     )
 
-    redirectToCheckout({ sessionId })
+    if (response.statusCode === 500) {
+      console.error(response.message)
+      return
+    }
+
+    redirectToCheckout({ sessionId: response.id })
   }
 
   return (
     <form onSubmit={handleCheckout}>
       <h2>Cart summary</h2>
       {/* This is where we'll render our cart */}
-      <p>
+      <p suppressHydrationWarning>
         <strong>Number of Items:</strong> {cartCount}
       </p>
-      <p>
+      <p suppressHydrationWarning>
         <strong>Total:</strong> {formattedTotalPrice}
       </p>
 
@@ -42,7 +50,7 @@ const CartSummary = () => {
       <button
         className="cart-style-background"
         type="submit"
-        disabled={!cartCount || loading}
+        disabled={cartEmpty || loading}
       >
         Checkout
       </button>
