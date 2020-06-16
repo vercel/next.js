262d2d2be47828a65a7d88da1c66919a88258ece
Tylerian <jairo.eog@outlook.com>
2020-06-16 09:18:41 +0200

    Modify generator to use base62 instead of base36

--- packages/next/build/webpack/config/blocks/css/loaders/utils/SequentialIDGenerator.ts
+++ packages/next/build/webpack/config/blocks/css/loaders/utils/SequentialIDGenerator.ts
@@ -1,6 +1,12 @@
 // Utility class used to help us minify CSS classnames
 // down to two characters each for production builds.
-// https://github.com/styletron/styletron/blob/master/packages/styletron-engine-atomic/src/sequential-id-generator.js
+//
+// Original: https://github.com/styletron/styletron/blob/master/packages/styletron-engine-atomic/src/sequential-id-generator.js
+// Modifications:
+//   - single-character id generation
+//   - use base62 instead of base36
+import base62 from './base62'
+
 export default class SequentialIDGenerator {
   count: number
   msb: number
@@ -10,27 +16,22 @@ export default class SequentialIDGenerator {
 
   constructor(prefix: string = '') {
     this.count = 0
-    this.msb = 35
+    this.msb = 61
     this.offset = 10
     this.power = 1
     this.prefix = prefix
   }
 
   next(): string {
-    const id = this.increment().toString(36)
+    const id = base62(this.increment())
     return this.prefix ? `${this.prefix}${id}` : id
   }
 
   increment(): number {
     const id = this.count + this.offset
-    // ensure "ad" is never produced just in case it conflicts with adblockers
-    if (id === 373) {
-      this.count++
-      return this.increment()
-    }
     if (id === this.msb) {
       this.offset += (this.msb + 1) * 9
-      this.msb = Math.pow(36, ++this.power) - 1
+      this.msb = Math.pow(62, ++this.power) - 1
     }
     this.count++
     return id
