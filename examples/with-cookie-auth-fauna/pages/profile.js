e3b65f16629669ef24616366f17d4e8c609a10eb
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-06-16 10:07:53 +0300

    added swr

--- examples/with-cookie-auth-fauna/pages/profile.js
+++ examples/with-cookie-auth-fauna/pages/profile.js
@@ -1,16 +1,19 @@
-import cookie from 'cookie'
+import useSWR from 'swr'
 import { withAuthSync } from '../utils/auth'
-import { FAUNA_SECRET_COOKIE } from '../utils/fauna-auth'
-import { profileApi } from './api/profile'
 import Layout from '../components/layout'
 
-const Profile = (props) => {
-  const { userId } = props
+const fetcher = async () => {
+  let response = await fetch('/api/profile')
+  let data = await response.json()
+  return data
+}
 
+const Profile = () => {
+  const { data, error } = useSWR('profile', fetcher)
+  if (error) return <p>{error.message}</p>
   return (
     <Layout>
-      <h1>Your user id is {userId}</h1>
-
+      <h1>Your user id is {data.userId} </h1>
       <style jsx>{`
         h1 {
           margin-bottom: 0;
@@ -18,26 +21,6 @@ const Profile = (props) => {
       `}</style>
     </Layout>
   )
-}
-
-export const getServerSideProps = async (context) => {
-  const { req, res } = context
-  const cookies = cookie.parse(req.headers.cookie ?? '')
-  const faunaSecret = cookies[FAUNA_SECRET_COOKIE]
-
-  if (!faunaSecret) {
-    res.writeHead(302, { Location: '/login' })
-    res.end()
-    return
-  }
-
-  const userId = await profileApi(faunaSecret)
-
-  return {
-    props: {
-      userId,
-    },
-  }
 }
 
 export default withAuthSync(Profile)
