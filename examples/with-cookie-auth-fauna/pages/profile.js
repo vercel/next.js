703995a698ecdc8d0764b2a0735dc47f3c0abdc0
Vorathep Sumetphong <vorathep055@gmail.com>
2020-06-17 12:41:44 +0700

    Update profile.js

--- examples/with-cookie-auth-fauna/pages/profile.js
+++ examples/with-cookie-auth-fauna/pages/profile.js
@@ -35,7 +35,7 @@ export async function getServerSideProps(ctx) {
 
     const profileInfo = await profileApi(faunaSecret)
 
-    return { props: { userId: data.userId } }
+    return { props: { userId: profileInfo } }
   }
 
   const response = await fetch('/api/profile')
