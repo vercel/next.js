38043dc5a6e02cd9418fe2e11f1e7e7d3fb860c0
Tim Neutkens <tim@timneutkens.nl>
2020-06-17 17:00:29 +0200

    Prepare for webpack 5 upgrade (#14268)
    Tweaked the config that can already be updated a bit.

--- packages/next/build/compiler.ts
+++ packages/next/build/compiler.ts
@@ -18,34 +18,49 @@ function generateStats(result: CompilerResult, stat: Stats): CompilerResult {
   return result
 }
 
+// Webpack 5 requires the compiler to be closed (to save caches)
+// Webpack 4 does not have this close method so in order to be backwards compatible we check if it exists
+function closeCompiler(compiler: webpack.Compiler | webpack.MultiCompiler) {
+  return new Promise((resolve, reject) => {
+    if ('close' in compiler) {
+      // @ts-ignore Close only exists on the compiler in webpack 5
+      return compiler.close((err: any) => (err ? reject(err) : resolve()))
+    }
+
+    resolve()
+  })
+}
+
 export function runCompiler(
   config: Configuration | Configuration[]
 ): Promise<CompilerResult> {
-  return new Promise(async (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const compiler = webpack(config)
     compiler.run(
       (err: Error, statsOrMultiStats: { stats: Stats[] } | Stats) => {
-        if (err) {
-          const reason = err?.toString()
-          if (reason) {
-            return resolve({ errors: [reason], warnings: [] })
+        closeCompiler(compiler).then(() => {
+          if (err) {
+            const reason = err?.toString()
+            if (reason) {
+              return resolve({ errors: [reason], warnings: [] })
+            }
+            return reject(err)
           }
-          return reject(err)
-        }
 
-        if ('stats' in statsOrMultiStats) {
-          const result: CompilerResult = statsOrMultiStats.stats.reduce(
-            generateStats,
-            { errors: [], warnings: [] }
+          if ('stats' in statsOrMultiStats) {
+            const result: CompilerResult = statsOrMultiStats.stats.reduce(
+              generateStats,
+              { errors: [], warnings: [] }
+            )
+            return resolve(result)
+          }
+
+          const result = generateStats(
+            { errors: [], warnings: [] },
+            statsOrMultiStats
           )
           return resolve(result)
-        }
-
-        const result = generateStats(
-          { errors: [], warnings: [] },
-          statsOrMultiStats
-        )
-        return resolve(result)
+        })
       }
     )
   })
