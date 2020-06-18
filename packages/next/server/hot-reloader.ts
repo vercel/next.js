9c2306988c408d41bbf0c9a0bc9270777116d80c
Tim Neutkens <timneutkens@me.com>
2020-06-18 18:32:27 +0200

    Update page-loader path for development

--- packages/next/server/hot-reloader.ts
+++ packages/next/server/hot-reloader.ts
@@ -80,7 +80,7 @@ function addCorsSupport(req: IncomingMessage, res: ServerResponse) {
 }
 
 const matchNextPageBundleRequest = route(
-  '/_next/static/:buildId/pages/:path*.js(\\.map|)'
+  '/_next/static/pages/:path*.js(\\.map|)'
 )
 
 // Recursively look up the issuer till it ends up at the root
