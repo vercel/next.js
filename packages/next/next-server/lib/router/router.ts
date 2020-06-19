20133c65b9d337ae50f32e50ad099afaa3432b98
JJ Kasper <jj@jjsweb.site>
2020-06-19 16:06:19 -0500

    Correct /_next/data link for GS(S)P with basePath

--- packages/next/next-server/lib/router/router.ts
+++ packages/next/next-server/lib/router/router.ts
@@ -31,7 +31,7 @@ function toRoute(path: string): string {
 }
 
 const prepareRoute = (path: string) =>
-  toRoute(!path || path === '/' ? '/index' : path)
+  toRoute(!path || delBasePath(path) === '/' ? '/index' : path)
 
 type Url = UrlObject | string
 
@@ -106,7 +106,7 @@ function fetchNextData(
       formatWithValidation({
         pathname: addBasePath(
           // @ts-ignore __NEXT_DATA__
-          `/_next/data/${__NEXT_DATA__.buildId}${delBasePath(pathname)}.json`
+          `/_next/data/${__NEXT_DATA__.buildId}${pathname}.json`
         ),
         query,
       }),
@@ -446,13 +446,12 @@ export default class Router implements BaseRouter {
 
       const route = toRoute(pathname)
       const { shallow = false } = options
+      const cleanedAs = delBasePath(as)
 
       if (isDynamicRoute(route)) {
-        const { pathname: asPathname } = parse(as)
+        const { pathname: asPathname } = parse(cleanedAs)
         const routeRegex = getRouteRegex(route)
-        const routeMatch = getRouteMatcher(routeRegex)(
-          delBasePath(asPathname || '')
-        )
+        const routeMatch = getRouteMatcher(routeRegex)(asPathname)
         if (!routeMatch) {
           const missingParams = Object.keys(routeRegex.groups).filter(
             (param) => !query[param]
@@ -502,17 +501,15 @@ export default class Router implements BaseRouter {
               !(routeInfo.Component as any).getInitialProps
           }
 
-          this.set(route, pathname!, query, delBasePath(as), routeInfo).then(
-            () => {
-              if (error) {
-                Router.events.emit('routeChangeError', error, as)
-                throw error
-              }
-
-              Router.events.emit('routeChangeComplete', as)
-              return resolve(true)
+          this.set(route, pathname!, query, cleanedAs, routeInfo).then(() => {
+            if (error) {
+              Router.events.emit('routeChangeError', error, as)
+              throw error
             }
-          )
+
+            Router.events.emit('routeChangeComplete', as)
+            return resolve(true)
+          })
         },
         reject
       )
