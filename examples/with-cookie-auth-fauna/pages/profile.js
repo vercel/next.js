7829fd3bdabacdd5a93437fb42c04f75f09d2a4a
Todor Totev <51530311+todortotev@users.noreply.github.com>
2020-06-19 10:16:26 +0300

    Update examples/with-cookie-auth-fauna/pages/profile.js
    Co-authored-by: Luis Alvarez D. <luis@vercel.com>

--- examples/with-cookie-auth-fauna/pages/profile.js
+++ examples/with-cookie-auth-fauna/pages/profile.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 const Profile = () => {
-  const { data, error } = useSWR('profile', fetcher)
+  const { data, error } = useSWR('/api/profile', fetcher)
   if (error) return <p>{error.message}</p>
   return (
     <Layout>
