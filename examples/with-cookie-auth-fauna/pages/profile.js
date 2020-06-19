0c57539572d543d19ff689ccf7d6efab7a5c4c16
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-06-19 10:45:59 +0300

    redirect if not found

--- examples/with-cookie-auth-fauna/pages/profile.js
+++ examples/with-cookie-auth-fauna/pages/profile.js
@@ -1,21 +1,33 @@
-import useSWR from 'swr'
 import { withAuthSync } from '../utils/auth'
 import Layout from '../components/layout'
+import useSWR from 'swr'
+import Router from 'next/router'
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) =>
+  fetch(url).then((res) => {
+    if (res.status >= 400 && res.status <= 499) {
+      throw new Error('API Client error')
+    }
+
+    return res.json()
+  })
 
 const Profile = () => {
-  const { data, error } = useSWR('/api/profile', fetcher)
-  if (error) return <p>{error.message}</p>
+  const { data: user, error } = useSWR('/api/profile', fetcher)
+  if (error) Router.push('/')
   return (
-    <Layout>
-      <h1>Your user id is {data.userId} </h1>
-      <style jsx>{`
-        h1 {
-          margin-bottom: 0;
-        }
-      `}</style>
-    </Layout>
+    <>
+      {user && (
+        <Layout>
+          <h1>Your user id is {user.userId} </h1>
+          <style jsx>{`
+            h1 {
+              margin-bottom: 0;
+            }
+          `}</style>
+        </Layout>
+      )}
+    </>
   )
 }
 
