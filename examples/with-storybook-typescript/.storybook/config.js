77b034136648834e540b30f6e3145be2bd517622
Hyeungshik Jung <hyungshike@gmail.com>
2020-06-21 17:03:44 +0900

    Add typescript config for storybook

--- examples/with-storybook-typescript/.storybook/config.js
+++ examples/with-storybook-typescript/.storybook/config.js
@@ -17,8 +17,8 @@ addParameters({
   },
 })
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/)
+// automatically import all files ending in *.stories.(ts|tsx)
+const req = require.context('../stories', true, /.stories.tsx?$/)
 
 // the first argument can be an array too, so if you want to load from different locations or
 // different extensions, you can do it like this: configure([req1, req2], module)
