2136c46b89972610e750517546b66eb2c0a76d91
JJ Kasper <jj@jjsweb.site>
2020-06-23 10:11:12 -0500

    Ensure dev overlay uses basePath for requests (#14475)
    This fixes the `basePath` not being used when making requests for the `react-dev-overlay`. We might want to pass down the `basePath` to the overlay instead of relying on the `process.env` injection although may be fine this way, will defer to @Timer for preferred way to pass this value to the dev-overlay
    
    Closes: https://github.com/vercel/next.js/issues/14470

--- packages/react-dev-overlay/src/internal/components/CodeFrame/CodeFrame.tsx
+++ packages/react-dev-overlay/src/internal/components/CodeFrame/CodeFrame.tsx
@@ -46,12 +46,18 @@ export const CodeFrame: React.FC<CodeFrameProps> = function CodeFrame({
       params.append(key, (stackFrame[key] ?? '').toString())
     }
 
-    self.fetch(`/__nextjs_launch-editor?${params.toString()}`).then(
-      () => {},
-      () => {
-        // TODO: report error
-      }
-    )
+    self
+      .fetch(
+        `${
+          process.env.__NEXT_ROUTER_BASEPATH || ''
+        }/__nextjs_launch-editor?${params.toString()}`
+      )
+      .then(
+        () => {},
+        () => {
+          // TODO: report error
+        }
+      )
   }, [stackFrame])
 
   // TODO: make the caret absolute
