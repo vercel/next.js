c39ead1bd8281c6fce452d5882450806ed7f3dd3
Joe Haddad <joe.haddad@zeit.co>
2020-06-24 00:18:56 -0400

    Increase timeout

--- test/integration/no-anon-default-export/test/index.test.js
+++ test/integration/no-anon-default-export/test/index.test.js
@@ -5,7 +5,7 @@ import { check, findPort, killApp, launchApp } from 'next-test-utils'
 import webdriver from 'next-webdriver'
 import { join } from 'path'
 
-jest.setTimeout(1000 * 30)
+jest.setTimeout(1000 * 60 * 3)
 
 const appDir = join(__dirname, '../')
 
