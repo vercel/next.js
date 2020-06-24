5a40e5b9750f5de922422e40fd26974be2674301
Joe Haddad <joe.haddad@zeit.co>
2020-06-23 23:54:28 -0400

    Update test

--- test/integration/config-devtool-dev/test/index.test.js
+++ test/integration/config-devtool-dev/test/index.test.js
@@ -1,7 +1,14 @@
 /* eslint-env jest */
 
-import { check, findPort, killApp, launchApp } from 'next-test-utils'
-import { recursiveReadDir } from 'next/dist/lib/recursive-readdir'
+import {
+  check,
+  findPort,
+  getRedboxSource,
+  hasRedbox,
+  killApp,
+  launchApp,
+} from 'next-test-utils'
+import webdriver from 'next-webdriver'
 import { join } from 'path'
 
 jest.setTimeout(1000 * 30)
@@ -9,11 +16,11 @@ jest.setTimeout(1000 * 30)
 const appDir = join(__dirname, '../')
 
 describe('devtool set in developmemt mode in next config', () => {
-  it('should warn when a devtool is set in development mode', async () => {
+  it('should warn and revert when a devtool is set in development mode', async () => {
     let stderr = ''
 
     const appPort = await findPort()
-    const app = await launchApp(join(__dirname, '..'), appPort, {
+    const app = await launchApp(appDir, appPort, {
       env: { __NEXT_TEST_WITH_DEVTOOL: true },
       onStderr(msg) {
         stderr += msg || ''
@@ -26,29 +33,26 @@ describe('devtool set in developmemt mode in next config', () => {
       false
     )
 
+    const browser = await webdriver(appPort, '/')
+    expect(await hasRedbox(browser)).toBe(true)
+    if (process.platform === 'win32') {
+      // TODO: add win32 snapshot
+    } else {
+      expect(await getRedboxSource(browser)).toMatchInlineSnapshot(`
+        "pages/index.js (5:10) @ eval
+
+          3 | export default function Index(props) {
+          4 |   useEffect(() => {
+        > 5 |     throw new Error('this should render')
+            |          ^
+          6 |   }, [])
+          7 |   return <div>Index Page</div>
+          8 | }"
+      `)
+    }
+    await browser.close()
+
     await killApp(app)
     expect(found).toBeTruthy()
-  })
-
-  // TODO: update test to check the error overlay correctly resolves source
-  // frames, which is a more robust test
-  it('should revert to the original devtool when user set in development mode', async () => {
-    const appPort = await findPort()
-    const app = await launchApp(join(__dirname, '..'), appPort, {
-      env: { __NEXT_TEST_WITH_DEVTOOL: true },
-    })
-    const browserFiles = await recursiveReadDir(
-      join(appDir, '.next', 'static'),
-      /.*/
-    )
-    const jsFiles = browserFiles.filter(
-      (file) => file.endsWith('.js') && file.includes('/pages/')
-    )
-
-    jsFiles.forEach((file) => {
-      expect(browserFiles.includes(`${file}.map`)).toBe(false)
-    })
-
-    await killApp(app)
   })
 })
