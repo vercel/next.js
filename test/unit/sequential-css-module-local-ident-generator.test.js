b08729831634b25e06e3f46d4b642f76b8c09abe
Tylerian <jairo.eog@outlook.com>
2020-06-24 23:17:34 +0200

    Fix typo again

---
+++ test/unit/sequential-css-module-local-ident-generator.test.js
@@ -0,0 +1,27 @@
+import SequentialCSSModuleLocalIdentGenerator from 'next/dist/build/webpack/config/blocks/css/loaders/utils/SequentialCSSModuleLocalIdentGenerator'
+
+describe('', () => {
+  let generator
+
+  beforeEach(() => {
+    generator = new SequentialCSSModuleLocalIdentGenerator()
+  })
+
+  it('avoids classnames starting with digit characters', () => {
+    let result = generator.next()
+
+    expect(generator.count).toBe(0)
+    expect(generator.offset).toBe(10)
+    expect(generator.msb).toBe(61)
+    expect(generator.power).toBe(1)
+    expect(result).not.toMatch(/^[0-9]$/)
+
+    generator.count = 51
+    result = generator.next()
+
+    expect(generator.count).toBe(52)
+    expect(generator.msb).toBe(3843)
+    expect(generator.offset).toBe(568)
+    expect(result).not.toMatch(/^[0-9].*/)
+  })
+})
