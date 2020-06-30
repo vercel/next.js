520b5abd531b29eaef5e43acf5a9a4d9e7548430
JJ Kasper <jj@jjsweb.site>
2020-06-30 17:13:41 -0500

    Update warning for rewriting to auto-export dynamic pages

--- packages/next/next-server/server/render.tsx
+++ packages/next/next-server/server/render.tsx
@@ -304,7 +304,7 @@ export async function renderToHTML(
 
   const headTags = (...args: any) => callMiddleware('headTags', args)
 
-  const didRewrite = (req as any)._nextDidRewrite
+  const didRewrite = (req as any)._nextRewroteUrl !== req.url
   const isFallback = !!query.__nextFallback
   delete query.__nextFallback
 
