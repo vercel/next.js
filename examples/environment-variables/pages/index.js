ebe9376d65036caacd8b5a1596cf106c1f67e2d5
Luis Alvarez D <luis@vercel.com>
2020-06-30 09:31:30 -0500

    Apply suggestions from code review

--- examples/environment-variables/pages/index.js
+++ examples/environment-variables/pages/index.js
@@ -86,11 +86,11 @@ const IndexPage = () => (
 )
 
 // `getStaticProps`, and similar Next.js methods like `getStaticPaths` and `getServerSideProps`
-// only run in the server. Check the terminal to see the environment variables
+// only run in Node.js. Check the terminal to see the environment variables
 export async function getStaticProps() {
   // Using the variables below in the browser will return `undefined`. Next.js doesn't
   // expose environment variables unless they start with `NEXT_PUBLIC_`
-  console.log('[Server only] ENV_VARIABLE:', process.env.ENV_VARIABLE)
+  console.log('[Node.js only] ENV_VARIABLE:', process.env.ENV_VARIABLE)
   console.log(
     '[Server only] ENV_LOCAL_VARIABLE:',
     process.env.ENV_LOCAL_VARIABLE
