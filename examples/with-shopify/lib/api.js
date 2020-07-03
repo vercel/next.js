8183f8334ca93dadd73a84be32bd9dcb726b8e13
Luis Alvarez <luis@vercel.com>
2020-07-02 22:55:44 -0500

    Removed dead code

--- examples/with-shopify/lib/api.js
+++ examples/with-shopify/lib/api.js
@@ -1,15 +1,4 @@
-import Cosmic from 'cosmicjs'
 import graphqlFetch from './graphql-fetch'
-
-const BUCKET_SLUG = process.env.NEXT_EXAMPLE_CMS_COSMIC_BUCKET_SLUG
-const READ_KEY = process.env.NEXT_EXAMPLE_CMS_COSMIC_READ_KEY
-
-const bucket = Cosmic().bucket({
-  slug: BUCKET_SLUG,
-  read_key: READ_KEY,
-})
-
-const is404 = (error) => /not found/i.test(error.message)
 
 export async function getShopDataForHome() {
   const data = await graphqlFetch(`
@@ -71,23 +60,6 @@ export async function getShopDataForHome() {
   return data
 }
 
-export async function getPreviewPostBySlug(slug) {
-  const params = {
-    slug,
-    props: 'slug',
-    status: 'all',
-  }
-
-  try {
-    const data = await bucket.getObject(params)
-    return data.object
-  } catch (error) {
-    // Don't throw if an slug doesn't exist
-    if (is404(error)) return
-    throw error
-  }
-}
-
 export async function getAllProductsWithSlug() {
   const data = await graphqlFetch(`
     {
@@ -102,25 +74,6 @@ export async function getAllProductsWithSlug() {
   `)
 
   return data.products
-}
-
-export async function getAllPostsWithSlug() {
-  const params = {
-    type: 'posts',
-    props: 'slug',
-  }
-  const data = await bucket.getObjects(params)
-  return data.objects
-}
-
-export async function getAllPostsForHome(preview) {
-  const params = {
-    type: 'posts',
-    props: 'title,slug,metadata,created_at',
-    ...(preview && { status: 'all' }),
-  }
-  const data = await bucket.getObjects(params)
-  return data.objects
 }
 
 export async function getProductAndMoreProducts(handle) {
@@ -183,33 +136,5 @@ export async function getProductAndMoreProducts(handle) {
   return {
     shop: data.shop,
     product: data.productByHandle,
-  }
-}
-
-export async function getPostAndMorePosts(slug, preview) {
-  const singleObjectParams = {
-    slug,
-    props: 'slug,title,metadata,created_at',
-    ...(preview && { status: 'all' }),
-  }
-  const moreObjectParams = {
-    type: 'posts',
-    limit: 3,
-    props: 'title,slug,metadata,created_at',
-    ...(preview && { status: 'all' }),
-  }
-  const object = await bucket.getObject(singleObjectParams).catch((error) => {
-    // Don't throw if an slug doesn't exist
-    if (is404(error)) return
-    throw error
-  })
-  const moreObjects = await bucket.getObjects(moreObjectParams)
-  const morePosts = moreObjects.objects
-    ?.filter(({ slug: object_slug }) => object_slug !== slug)
-    .slice(0, 2)
-
-  return {
-    post: object?.object,
-    morePosts,
   }
 }
--- examples/with-shopify/pages/api/exit-preview.js
+++
@@ -1,8 +0,0 @@
-export default async function exit(_, res) {
-  // Exit the current user from "Preview Mode". This function accepts no args.
-  res.clearPreviewData()
-
-  // Redirect the user back to the index page.
-  res.writeHead(307, { Location: '/' })
-  res.end()
-}
