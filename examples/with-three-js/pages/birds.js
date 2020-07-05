cbf77d4be326ef805c7638c7d1b82749ce7a46e4
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-07-05 13:53:01 +0200

    updates thre three-js example with drei and modules transpilation

--- examples/with-three-js/pages/birds.js
+++ examples/with-three-js/pages/birds.js
@@ -1,10 +1,13 @@
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 import { Canvas } from 'react-three-fiber'
+import { OrbitControls, StandardEffects } from 'drei'
+
 const Bird = dynamic(() => import('../components/Bird'), { ssr: false })
 
 const Birds = () => {
   return new Array(5).fill().map((_, i) => {
+
     const x = (15 + Math.random() * 30) * (Math.round(Math.random()) ? -1 : 1)
     const y = -10 + Math.random() * 20
     const z = -5 + Math.random() * 10
@@ -14,8 +17,9 @@ const Birds = () => {
       bird === 'stork'
         ? 0.5 + Math.random()
         : bird === 'flamingo'
-        ? 0.25 + Math.random()
-        : 1 + Math.random() - 0.5
+          ? 0.25 + Math.random()
+          : 1 + Math.random() - 0.5
+
     return (
       <Bird
         key={i}
@@ -35,8 +39,10 @@ const BirdsPage = (props) => {
       <Canvas camera={{ position: [0, 0, 35] }}>
         <ambientLight intensity={2} />
         <pointLight position={[40, 40, 40]} />
+        <OrbitControls />
         <Suspense fallback={null}>
           <Birds />
+          <StandardEffects smaa />
         </Suspense>
       </Canvas>
     </>
