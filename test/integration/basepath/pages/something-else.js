f5c9cea3acaa7f61eb53c065aae668f8355601b6
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-07-05 12:51:51 +0200

    simplify

--- test/integration/basepath/pages/something-else.js
+++ test/integration/basepath/pages/something-else.js
@@ -1,13 +1,3 @@
-import { useRouter } from 'next/router'
-
 export default function Page() {
-  const router = useRouter()
-  return (
-    <>
-      <h1 id="something-else-page">something else</h1>
-      <button id="go-back" onClick={() => router.back()}>
-        back
-      </button>
-    </>
-  )
+  return <h1 id="something-else-page">something else</h1>
 }
