a45efc44561fda9a382d481948bcf2b9b0fd1df1
Luis Alvarez <luis@vercel.com>
2020-07-06 19:16:11 -0500

    Updated main grid

--- examples/with-shopify/components/products.js
+++ examples/with-shopify/components/products.js
@@ -2,7 +2,7 @@ import Product from './product'
 
 export default function Products({ products }) {
   return (
-    <div className="grid grid-cols-p-1 md:grid-cols-2 lg:grid-cols-3 justify-center md:col-gap-6 lg:col-gap-12 row-gap-20 md:row-gap-32">
+    <div className="grid grid-cols-p-1 sm:grid-cols-2 lg:grid-cols-3 justify-center sm:col-gap-6 lg:col-gap-12 row-gap-20 lg:row-gap-32">
       {products.map(({ node }) => (
         <Product key={node.id} product={node} />
       ))}
