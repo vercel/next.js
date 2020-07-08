f162b434610ece5ae0ccd712ade759d188ee75a7
Darsh Patel <darshkpatel@gmail.com>
2020-07-08 17:10:19 +0530

    Fix: Destructuring error in the cms-contenful example (#14970)

--- examples/cms-contentful/components/date.js
+++ examples/cms-contentful/components/date.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 
-export default function Date({ dateString }) {
+export default function DateComponent({ dateString }) {
   return (
     <time dateTime={dateString}>
       {format(new Date(dateString), 'LLLL	d, yyyy')}
