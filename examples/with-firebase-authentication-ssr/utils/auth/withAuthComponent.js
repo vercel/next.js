d43ee2839aaf23b06d572eec0d945b6e0acdbb37
Kevin Jennison <kevin.jennison1@gmail.com>
2020-07-09 15:54:55 -0700

    Remove outdated HOC

--- examples/with-firebase-authentication-ssr/utils/auth/withAuthComponent.js
+++ examples/with-firebase-authentication-ssr/utils/auth/withAuthComponent.js
@@ -11,6 +11,22 @@ const withAuthComponent = (ChildComponent) => {
     //   JS SDK when it's loaded.
     // TODO: set the AuthUser context
     return <ChildComponent {...otherProps} />
+
+    // TODO
+    // We'll use the authed user from client-side auth (Firebase JS SDK)
+    // when available. On the server side, we'll use the authed user from
+    // the session. This allows us to server-render while also using Firebase's
+    // client-side auth functionality.
+    //     const { user: firebaseUser } = useFirebaseAuth()
+    //     const AuthUserFromClient = createAuthUser(firebaseUser)
+    //     const { AuthUser: AuthUserFromSession, token } = AuthUserInfo
+    //     const AuthUser = AuthUserFromClient || AuthUserFromSession || null
+    //
+    //     return (
+    //       <AuthUserInfoContext.Provider value={{ AuthUser, token }}>
+    //         <ComposedComponent {...otherProps} />
+    //       </AuthUserInfoContext.Provider>
+    //     )
   }
 }
 
