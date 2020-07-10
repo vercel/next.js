eef37e0e5a152031130bbc85494c1cd896079546
Luis Alvarez <luis@vercel.com>
2020-07-10 00:09:49 -0500

    Updated product shadow

--- examples/with-shopify/tailwind.config.js
+++ examples/with-shopify/tailwind.config.js
@@ -29,8 +29,8 @@ module.exports = {
         '8xl': '6.25rem',
       },
       boxShadow: {
-        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        small: ' 0 5px 10px rgba(0, 0, 0, 0.15)',
+        medium: '0 8px 30px rgba(0, 0, 0, 0.25)',
       },
       gridTemplateColumns: {
         header: 'repeat(auto-fit, minmax(3rem, max-content))',
