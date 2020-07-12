179b6f4b886468c4ab8a71b204ec7080f8fafc76
Tim Neutkens <tim@timneutkens.nl>
2020-07-12 14:03:17 +0200

    Update ignored patterns for webpack 4 (#15096)
    Found while working on figuring out this bug: https://twitter.com/timneutkens/status/1282129714627448832
    
     I noticed that the node_modules got passed by the ignore still because when chokidar identifies a ignore pattern is a glob it treats the glob as-is instead of appending `/**` to the glob

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -711,7 +711,7 @@ export default async function getBaseWebpackConfig(
       }
     },
     watchOptions: {
-      ignored: ['**/.git', '**/node_modules'],
+      ignored: ['**/.git/**', '**/node_modules/**', '**/.next/**'],
     },
     output: {
       path: outputPath,
