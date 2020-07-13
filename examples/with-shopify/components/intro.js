b3c37afd8c5e026f0740061561b93414e8d5fd61
Luis Alvarez <luis@vercel.com>
2020-07-13 16:16:29 -0500

    Make sure the intro is always centered

--- examples/with-shopify/components/intro.js
+++ examples/with-shopify/components/intro.js
@@ -3,7 +3,7 @@ import { CMS_NAME, CMS_URL } from '@/lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-6xl md:text-7xl font-bold text-center sm:text-left tracking-tighter leading-tight md:pr-8">
         Next.js Example
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
