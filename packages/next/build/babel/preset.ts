6374a2603ebce198d7b1dcc442868f90edb6b063
Tanmay <tanmaylaud@gmail.com>
2020-07-13 23:27:47 +0530

    Fixing the bug that cause React to be imported as a global import

--- packages/next/build/babel/preset.ts
+++ packages/next/build/babel/preset.ts
@@ -114,6 +114,7 @@ module.exports = (
           // @babel/plugin-transform-react-jsx-self automatically in development
           development: isDevelopment || isTest,
           pragma: '__jsx',
+          runtime: 'automatic',
           ...options['preset-react'],
         },
       ],
