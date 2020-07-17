b4f00d42a438b54877100c48d1143c4c6ca20eca
Ojo Temiloluwa Philip <themmyloluwaa@gmail.com>
2020-07-17 06:52:13 +0100

    added pages

---
+++ examples/with-apollo-subscription/pages/index.js
@@ -0,0 +1,29 @@
+import App from '../components/App'
+import InfoBox from '../components/InfoBox'
+import Header from '../components/Header'
+import Submit from '../components/Submit'
+import PostList from '../components/PostList'
+import { withApollo } from '../lib/apollo'
+
+const IndexPage = () => (
+  <App>
+    <Header />
+    <InfoBox>
+      ℹ️ This example shows how to fetch all initial apollo queries on the
+      server. If you <a href="/">reload</a> this page you won't see a loader
+      since Apollo fetched all needed data on the server. This prevents{' '}
+      <a
+        href="https://nextjs.org/blog/next-9#automatic-static-optimization"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        automatic static optimization
+      </a>{' '}
+      in favour of full Server-Side-Rendering.
+    </InfoBox>
+    <Submit />
+    <PostList />
+  </App>
+)
+
+export default withApollo({ ssr: true })(IndexPage)
---
+++ examples/with-apollo-subscription/pages/subscription.js
@@ -0,0 +1,83 @@
+import App from "../components/App";
+import InfoBox from "../components/InfoBox";
+import Header from "../components/Header";
+import SubscriptionSubmit from "../components/SubscriptionSubmit";
+import SubscriptionList from "../components/SubscriptionList";
+import { withApollo } from "../lib/apollo";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+export const ALL_POSTS_QUERY = gql`
+  query allPosts($first: Int!, $skip: Int!) {
+    allPosts(orderBy: createdAt_DESC, first: $first, skip: $skip) {
+      id
+      title
+      votes
+      url
+      createdAt
+    }
+  }
+`;
+const POST_SUBSCRIPTION = gql`
+  subscription {
+    Post {
+      node {
+        id
+        title
+        votes
+        url
+        createdAt
+      }
+    }
+  }
+`;
+
+export const allPostsQueryVars = {
+  skip: 0,
+  first: 10
+};
+
+const handleSubscribe = subscribeToMore => {
+  subscribeToMore({
+    document: POST_SUBSCRIPTION,
+
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newPost = subscriptionData.data.Post.node;
+      return {
+        allPosts: [newPost, ...prev.allPosts]
+      };
+    }
+  });
+};
+
+const Subscription = () => {
+  const { subscribeToMore, ...result } = useQuery(ALL_POSTS_QUERY, {
+    variables: allPostsQueryVars,
+    // Setting this value to true will make the component rerender when
+    // the "networkStatus" changes, so we are able to know if it is fetching
+    // more data
+    notifyOnNetworkStatusChange: true
+  });
+
+  return (
+    <App>
+      <Header />
+      <InfoBox>
+        ℹ️ This example shows you a working example of apollo subscriptions with
+        nextjs. If you Subscriptionsubmit a new url, all clients currently on
+        this page will get the update not just you. To see it in action, open up
+        two browsers and create a link in one, you'd notice the other one gets
+        the update instantly. This sends all our request in the client because
+        subscriptions don't work on the server.
+      </InfoBox>
+      <SubscriptionSubmit />
+      <SubscriptionList
+        {...result}
+        handleSubscribe={() => handleSubscribe(subscribeToMore)}
+      />
+    </App>
+  );
+};
+
+export default withApollo({ ssr: true })(Subscription);
