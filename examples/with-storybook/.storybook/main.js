f0f2d536e3b844d5339a4ef40b181c9b3a02debe
Darsh <darshkpatel@gmail.com>
2020-07-20 16:25:28 +0530

    config for storybook

---
+++ examples/with-storybook/.storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      },
+    },
+  ],
+}
