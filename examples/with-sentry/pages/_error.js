c0507029aed63c466f9600446545e3b9b79a7976
Colleen O'Rourke <colleen@sentry.io>
2020-07-21 16:32:05 -0700

    Update readme, add comment

--- examples/with-sentry/pages/_error.js
+++ examples/with-sentry/pages/_error.js
@@ -7,6 +7,7 @@ const MyError = async ({ statusCode, hasGetInitialPropsRun, err }) => {
     // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
     // err via _app.js so it can be captured
     Sentry.captureException(err)
+    // flush is needed after calling captureException to send server side errors to Sentry, otherwise the serverless function will exit before it's sent
     await Sentry.flush(2000)
   }
 
