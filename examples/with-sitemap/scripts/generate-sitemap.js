34626f2928344aa5c7b4eaffaad2e2da09735cdc
Arsalan Khattak <37709578+eKhattak@users.noreply.github.com>
2020-07-23 11:50:08 +0500

    Update examples/with-sitemap/scripts/generate-sitemap.js
    Co-authored-by: Luis Alvarez D. <luis@vercel.com>

--- examples/with-sitemap/scripts/generate-sitemap.js
+++ examples/with-sitemap/scripts/generate-sitemap.js
@@ -1,35 +1,28 @@
 const fs = require('fs')
-
 const globby = require('globby')
 
-const WEBSITE_URL = 'http://localhost:3000'
+function addPage(page) {
+  const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '')
+  const route = path === '/index' ? '' : path
 
-;(async () => {
+  return `  <url>
+    <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
+    <changefreq>hourly</changefreq>
+  </url>`
+}
+
+async function generateSitemap() {
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
     'pages/**/*{.js,.mdx}',
     '!pages/_*.js',
     '!pages/api',
   ])
-  const sitemap = `
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace('pages', '')
-                  .replace('.js', '')
-                  .replace('.mdx', '')
-                const route = path === '/index' ? '' : path
-
-                return `
-                        <url>
-                            <loc>${`${WEBSITE_URL}${route}`}</loc>
-                        </url>
-                    `
-              })
-              .join('')}
-              </urlset>
-    `
+  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(addPage).join('\n')}
+</urlset>`
 
   fs.writeFileSync('public/sitemap.xml', sitemap)
-})()
+}
+
+generateSitemap()
