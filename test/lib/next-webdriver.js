3c5dc9cfeeb3ddd082d72bbab53482fe6d833852
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-07-27 17:00:52 +0200

    more logging

--- test/lib/next-webdriver.js
+++ test/lib/next-webdriver.js
@@ -130,8 +130,10 @@ const getDeviceIP = async () => {
 // eslint-disable-next-line no-unused-vars
 const freshWindow = async () => {
   // First we close all extra windows left over
+  console.log('getAllWindowHandles')
   let allWindows = await browser.getAllWindowHandles()
-
+  console.log('closing windows')
+  console.log(allWindows)
   for (const win of allWindows) {
     if (win === initialWindow) continue
     try {
@@ -139,16 +141,22 @@ const freshWindow = async () => {
       await browser.close()
     } catch (_) {}
   }
+  console.log('windows closed')
   await browser.switchTo().window(initialWindow)
 
+  console.log('open fresh window')
   // now we open a fresh window
   await browser.get(`http://${deviceIP}:${global._newTabPort}`)
 
   const newTabLink = await browser.findElement(By.css('#new'))
   await newTabLink.click()
+  console.log('new window open')
 
   allWindows = await browser.getAllWindowHandles()
+  console.log('all windows')
+  console.log(allWindows)
   const newWindow = allWindows.find((win) => win !== initialWindow)
+  console.log('switching new window')
   await browser.switchTo().window(newWindow)
 }
 
