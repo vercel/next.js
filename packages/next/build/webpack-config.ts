2a092cd72c999a308bc1939e75ba104d3c17ec36
Tim Neutkens <tim@timneutkens.nl>
2020-07-28 11:33:21 +0200

    Add polyfill for process and Buffer in webpack 5 (#15499)

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -802,6 +802,14 @@ export default async function getBaseWebpackConfig(
     },
     plugins: [
       hasReactRefresh && new ReactRefreshWebpackPlugin(),
+      // Makes sure `Buffer` is polyfilled in client-side bundles (same behavior as webpack 4)
+      isWebpack5 &&
+        !isServer &&
+        new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] }),
+      // Makes sure `process` is polyfilled in client-side bundles (same behavior as webpack 4)
+      isWebpack5 &&
+        !isServer &&
+        new webpack.ProvidePlugin({ process: ['process'] }),
       // This plugin makes sure `output.filename` is used for entry chunks
       !isWebpack5 && new ChunkNamesPlugin(),
       new webpack.DefinePlugin({
