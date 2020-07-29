06b93f21e7857a59f648f36038d389ea8c5e16cb
Darsh <darshkpatel@gmail.com>
2020-07-29 11:22:27 +0530

    add test to check multiple link handling

--- test/integration/error-is-clickable/pages/first.js
+++ test/integration/error-is-clickable/pages/first.js
@@ -1,5 +1,5 @@
 export default function Index() {
   throw new Error(
-    'This error should be clickable. https://nextjs.org is the best'
+    'This error should be clickable. https://nextjs.org is the best. Visit https://nextjs.org/docs for documentation'
   )
 }
