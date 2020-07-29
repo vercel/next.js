4b2f148f925cf5438e0c8c64c58eaeab0f0e645b
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-07-29 12:30:06 +0200

    Fix webdriver error handling (#15491)
    errors in script execution will result in timeout instead of failure. This PR makes sure to pass all arguments to `.then` so error handling is correctly set up. (`.then` needs to be able to take a second argument). I also removed `.finally`.
    
    I don't know why, but it looks like the following test does something that prevents the tests after it from opening a new browser window on azure. I'm giving up, I shuffled the tests to put this one last.
    ```js
        it('shows warning when dynamic route mismatch is used on Link', async () => {
          await showsError(
            '/dynamic-route-mismatch',
            /Mismatching `as` and `href` failed to manually provide the params: post in the `href`'s `query`/,
            true,
            true
          )
        })
    ```

--- test/lib/wd-chain.js
+++ test/lib/wd-chain.js
@@ -10,9 +10,8 @@ export default class Chain {
       this.promise = Promise.resolve()
     }
     this.promise = this.promise.then(nextCall)
-    this.then = (cb) => this.promise.then(cb)
-    this.catch = (cb) => this.promise.catch(cb)
-    this.finally = (cb) => this.promise.finally(cb)
+    this.then = (...args) => this.promise.then(...args)
+    this.catch = (...args) => this.promise.catch(...args)
     return this
   }
 
