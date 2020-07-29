7c28a2043c06f3b163f46090e2d49f90db882b55
Joe Knowles <joejknowles@gmail.com>
2020-07-29 12:56:45 +0100

    fix linting

--- examples/with-firebase-authentication/utils/auth/mapUserData.js
+++ examples/with-firebase-authentication/utils/auth/mapUserData.js
@@ -1,8 +1,8 @@
-export const mapUserData = user => {
-    const { uid, email, xa } = user
-    return {
-        id: uid,
-        email,
-        token: xa,
-    }
+export const mapUserData = (user) => {
+  const { uid, email, xa } = user
+  return {
+    id: uid,
+    email,
+    token: xa,
+  }
 }
--- examples/with-firebase-authentication/utils/auth/userCookies.js
+++ examples/with-firebase-authentication/utils/auth/userCookies.js
@@ -2,17 +2,17 @@ import cookies from 'js-cookie'
 
 export const getUserFromCookie = () => {
   const cookie = cookies.get('auth')
-    if (!cookie) {
-      return
-    }
-    return JSON.parse(cookie)
+  if (!cookie) {
+    return
+  }
+  return JSON.parse(cookie)
 }
 
-export const setUserCookie = user => {
+export const setUserCookie = (user) => {
   cookies.set('auth', user, {
     // firebase id tokens expire in one hour
     // set cookie expiry to match
-    expires: 1/24,
+    expires: 1 / 24,
   })
 }
 
