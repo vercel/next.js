e658792220a5d17ba52bd622d8a62afb23e6db71
Prateek Bhatnagar <prateek89born@gmail.com>
2020-07-30 02:22:23 -0700

    reverting changes from serverless loader

--- packages/next/build/webpack/loaders/next-serverless-loader.ts
+++ packages/next/build/webpack/loaders/next-serverless-loader.ts
@@ -59,6 +59,10 @@ const nextServerlessLoader: loader.Loader = function () {
     '/'
   )
   const routesManifest = join(distDir, ROUTES_MANIFEST).replace(/\\/g, '/')
+  const fontManifest = join(distDir, 'serverless', FONT_MANIFEST).replace(
+    /\\/g,
+    '/'
+  )
 
   const escapedBuildId = escapeRegexp(buildId)
   const pageIsDynamicRoute = isDynamicRoute(page)
@@ -437,9 +441,7 @@ const nextServerlessLoader: loader.Loader = function () {
 
         if (process.env.__NEXT_OPTIMIZE_FONTS) {
           renderOpts.optimizeFonts = true
-          renderOpts.fontManifest = __non_webpack_require__('${
-            this._compilation.options.output.path
-          }/${FONT_MANIFEST}');
+          renderOpts.fontManifest = require('${fontManifest}')
           process.env['__NEXT_OPTIMIZE_FONT'+'S'] = true
         }
         let result = await renderToHTML(req, res, "${page}", Object.assign({}, getStaticProps ? { ...(parsedUrl.query.amp ? { amp: '1' } : {}) } : parsedUrl.query, nowParams ? nowParams : params, _params, isFallback ? { __nextFallback: 'true' } : {}), renderOpts)
