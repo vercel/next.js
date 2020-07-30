2e4419783b9aef78822cbdf6f3d68d2e39948044
Laura <laurabeatriserafim@gmail.com>
2020-07-30 11:12:55 -0300

    Fix type of spreaded events to emit event handler

--- packages/next/next-server/lib/mitt.ts
+++ packages/next/next-server/lib/mitt.ts
@@ -1,12 +1,8 @@
 /*
 MIT License
-
 Copyright (c) Jason Miller (https://jasonformat.com/)
-
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
@@ -28,7 +24,7 @@ export type EventMap = Record<string, any>
 export type EventHandlersMap = Record<string, any>
 export type EventKey<T extends EventMap> = string & keyof T
 
-export interface Emitter<T extends EventMap, H extends Record<string, any>> {
+export interface Emitter<T extends EventMap, H extends EventHandlersMap> {
   on<K extends EventKey<T>>(eventName: K, fn: H[K]): void
   off<K extends EventKey<T>>(eventName: K, fn: H[K]): void
   emit<K extends EventKey<T>>(
@@ -54,7 +50,7 @@ export default function mitt<
     emit(type, ...evts) {
       ;(all[type] || []).slice().map(
         // eslint-disable-next-line array-callback-return
-        (handler) => {
+        (handler: (...evts: any[]) => void) => {
           handler(...evts)
         }
       )
