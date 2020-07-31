37bfc65638f7041c14e8135904f0939c280848f7
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-07-31 08:38:39 +0200

    Fix wrong asPath on 404 (#15728)
    Caught this while reviewing router code for https://github.com/vercel/next.js/pull/15710

--- packages/next/client/index.js
+++ packages/next/client/index.js
@@ -58,7 +58,7 @@ if (
     page === '/_error' &&
     hydrateProps &&
     hydrateProps.pageProps &&
-    hydrateProps.pageProps.statusCode === '404'
+    hydrateProps.pageProps.statusCode === 404
   )
 ) {
   asPath = delBasePath(asPath)
