7b61256bacef26e97a4a70409af670ae2f7475be
Kristóf Poduszló <kripod@protonmail.com>
2020-08-01 15:11:04 +0200

    test: fix tsconfig snapshots

--- test/integration/tsconfig-verifier/test/index.test.js
+++ test/integration/tsconfig-verifier/test/index.test.js
@@ -30,7 +30,7 @@ describe('tsconfig.json verifier', () => {
           \\"lib\\": [
             \\"dom\\",
             \\"dom.iterable\\",
-            \\"esnext\\"
+            \\"es2018\\"
           ],
           \\"allowJs\\": true,
           \\"skipLibCheck\\": true,
@@ -74,7 +74,7 @@ describe('tsconfig.json verifier', () => {
           \\"lib\\": [
             \\"dom\\",
             \\"dom.iterable\\",
-            \\"esnext\\"
+            \\"es2018\\"
           ],
           \\"allowJs\\": true,
           \\"skipLibCheck\\": true,
@@ -141,7 +141,7 @@ describe('tsconfig.json verifier', () => {
           \\"lib\\": [
             \\"dom\\",
             \\"dom.iterable\\",
-            \\"esnext\\"
+            \\"es2018\\"
           ],
           \\"allowJs\\": true,
           \\"skipLibCheck\\": true,
@@ -189,7 +189,7 @@ describe('tsconfig.json verifier', () => {
           \\"lib\\": [
             \\"dom\\",
             \\"dom.iterable\\",
-            \\"esnext\\"
+            \\"es2018\\"
           ],
           \\"allowJs\\": true,
           \\"skipLibCheck\\": true,
@@ -234,7 +234,7 @@ describe('tsconfig.json verifier', () => {
           \\"lib\\": [
             \\"dom\\",
             \\"dom.iterable\\",
-            \\"esnext\\"
+            \\"es2018\\"
           ],
           \\"allowJs\\": true,
           \\"skipLibCheck\\": true,
