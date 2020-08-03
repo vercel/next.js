788c0a022d19ed32e9191e48624db94bec11378f
Kristóf Poduszló <kripod@protonmail.com>
2020-08-03 23:50:18 +0200

    test: try fixing Babel integration test

--- test/integration/babel/pages/index.js
+++ test/integration/babel/pages/index.js
@@ -1,9 +1,10 @@
 // This page is written in flowtype to test Babel's functionality
-import * as React from '../test/namespace-exported-react'
+import * as React from 'react'
+import { Component } from '../test/namespace-exported-react'
 
 type Props = {}
 
-export default class MyComponent extends React.Component<Props> {
+export default class MyComponent extends Component<Props> {
   render() {
     return <div id="text">Test Babel</div>
   }
