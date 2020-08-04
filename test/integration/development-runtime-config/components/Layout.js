b6e3bd64698f7381da23047e4d83a39935aeb5f3
JJ Kasper <jj@jjsweb.site>
2020-08-04 10:56:54 -0500

    Update test a bit

--- test/integration/development-runtime-config/components/Layout.js
+++ test/integration/development-runtime-config/components/Layout.js
@@ -1,13 +1,12 @@
 import getConfig from 'next/config'
 
-const {
-  serverRuntimeConfig: { mySecret },
-} = getConfig()
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <h2>mySecret: {mySecret}</h2>
+      <p id="server-runtime-config">{JSON.stringify(serverRuntimeConfig)}</p>
+      <p id="public-runtime-config">{JSON.stringify(publicRuntimeConfig)}</p>
       <div>{children}</div>
     </div>
   )
--- test/integration/development-runtime-config/test/index.test.js
+++ test/integration/development-runtime-config/test/index.test.js
@@ -2,6 +2,7 @@
 
 import fs from 'fs-extra'
 import { join } from 'path'
+import cheerio from 'cheerio'
 import {
   findPort,
   renderViaHTTP,
@@ -10,30 +11,36 @@ import {
   killApp,
 } from 'next-test-utils'
 
-jest.setTimeout(1000 * 60 * 5)
+jest.setTimeout(1000 * 60 * 2)
 
 const appDir = join(__dirname, '../')
 const nextConfig = join(appDir, 'next.config.js')
 
-const runApp = async (dir) => {
+const runApp = async (config) => {
   const port = await findPort()
 
   let stderr = ''
-  const app = await launchApp(dir, port, {
+  const app = await launchApp(appDir, port, {
     onStderr(err) {
       stderr += err
     },
   })
 
-  await renderViaHTTP(port, '/post/1')
+  const html = await renderViaHTTP(port, '/post/1')
+  const $ = cheerio.load(html)
   await waitFor(1000)
 
   await killApp(app)
-
   await fs.remove(nextConfig)
 
   expect(stderr).not.toMatch(
     /Cannot read property 'serverRuntimeConfig' of undefined/i
+  )
+  expect(JSON.parse($('#server-runtime-config').text())).toEqual(
+    config.serverRuntimeConfig || {}
+  )
+  expect(JSON.parse($('#public-runtime-config').text())).toEqual(
+    config.publicRuntimeConfig || {}
   )
 }
 
@@ -47,24 +54,26 @@ describe('should work with runtime-config in next.config.js', () => {
     `
     )
 
-    await runApp(appDir)
+    await runApp({})
   })
 
   test('with runtime-config', async () => {
+    const config = {
+      serverRuntimeConfig: {
+        mySecret: '**********',
+      },
+      publicRuntimeConfig: {
+        staticFolder: '/static',
+      },
+    }
+
     await fs.writeFile(
       nextConfig,
       `
-      module.exports = {
-        serverRuntimeConfig: {
-          mySecret: '**********'
-        },
-        publicRuntimeConfig: {
-          staticFolder: '/static'
-        }
-      }
+      module.exports = ${JSON.stringify(config)}
     `
     )
 
-    await runApp(appDir)
+    await runApp(config)
   })
 })
