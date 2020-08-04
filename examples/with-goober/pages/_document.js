a9c0018c9a58c137a5d3f0a6876bf663d7a27f4d
agstover <agstover@gmail.com>
2020-08-04 13:46:40 -0500

    await ctx.renderPage() in examples for TypeScript (#15528)
    Co-authored-by: Joe Haddad <joe.haddad@zeit.co>

--- examples/with-goober/pages/_document.js
+++ examples/with-goober/pages/_document.js
@@ -2,9 +2,8 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { extractCss } from 'goober'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const page = renderPage()
-
+  static async getInitialProps({ renderPage }) {
+    const page = await renderPage()
     // Extrach the css for each page render
     const css = extractCss()
     return { ...page, css }
