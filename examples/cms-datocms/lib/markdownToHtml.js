6d60f5301aba377f9d8948ece4b2d860c0bc0069
Aleksander <ankezy@gmail.com>
2020-08-05 17:30:04 +0200

    Fix awaiting for sync method of remark in cms examples (#15555)

--- examples/cms-datocms/lib/markdownToHtml.js
+++ examples/cms-datocms/lib/markdownToHtml.js
@@ -2,6 +2,6 @@ import remark from 'remark'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).processSync(markdown)
+  const result = await remark().use(html).process(markdown)
   return result.toString()
 }
