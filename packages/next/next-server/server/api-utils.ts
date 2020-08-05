0a741e530c77f8bc1850445a7d154a82d9353a01
markoz <markdrew53@gmail.com>
2020-08-05 16:38:10 -0500

    Improvement in logic to cover the case that is passed null

--- packages/next/next-server/server/api-utils.ts
+++ packages/next/next-server/server/api-utils.ts
@@ -235,8 +235,8 @@ export function redirect(
     url = statusOrUrl
     statusOrUrl = 307
   }
-  if (typeof url !== 'string') {
-    throw new Error(`Invalid redirect arguments. Make sure the second argument is a URL when setting the status code e.g. res.redirect(307, '/redirect-destination').`)
+  if (typeof statusOrUrl !== 'number'||typeof url !== 'string') {
+    throw new Error(`Invalid redirect arguments. Please use a single argument URL, e.g. res.redirect('/destination') or use a status code and URL, e.g. res.redirect(307, '/destination').`)
   }
   res.writeHead(statusOrUrl, { Location: url }).end()
   return res
