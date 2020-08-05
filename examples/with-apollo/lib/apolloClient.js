dc0e759cd4d4862448dee5e1a1af54079c858fc0
Jack Clackett <jack@noquarter.co>
2020-08-05 17:30:29 +0200

    Update with-apollo examples to merge existing cache (#15804)

--- examples/with-apollo/lib/apolloClient.js
+++ examples/with-apollo/lib/apolloClient.js
@@ -29,7 +29,11 @@ export function initializeApollo(initialState = null) {
   // If your page has Next.js data fetching methods that use Apollo Client, the initial state
   // gets hydrated here
   if (initialState) {
-    _apolloClient.cache.restore(initialState)
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract()
+    // Restore the cache using the data passed from getStaticProps/getServerSideProps
+    // combined with the existing cached data
+    _apolloClient.cache.restore({ ...existingCache, ...initialState })
   }
   // For SSG and SSR always create a new Apollo Client
   if (typeof window === 'undefined') return _apolloClient
