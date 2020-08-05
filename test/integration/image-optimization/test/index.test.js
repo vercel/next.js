b6060fa4a5babbb71848c6fa2f0ff656e46c5f2c
Alex Castle <atcastle@gmail.com>
2020-08-05 10:49:44 -0700

    Add experimental image post-processing (#15875)
    This PR adds a second experimental post-processing step for the framework introduced by @prateekbh in #14746. The image post-processing step scans the rendered document for the first few images and uses a simple heuristic to determine if the images should be automatically preloaded.
    
    Analysis of quite a few production Next apps has shown that a lot of sites are taking a substantial hit to their [LCP](https://web.dev/lcp/) score because an image that's part of the "hero" element on the page is not preloaded and is getting downloaded with lower priority than the JavaScript bundles. This post-processor should automatically fix that for a lot of sites, without causing any real performance effects in cases where it fails to identify the hero image.
    
    This feature is behind an experimental flag, and will be subject to quite a bit of experimentation and tweaking before it's ready to be made a default setting.

---
+++ test/integration/image-optimization/test/index.test.js
@@ -0,0 +1,89 @@
+/* eslint-env jest */
+
+import { join } from 'path'
+import {
+  killApp,
+  findPort,
+  nextStart,
+  nextBuild,
+  renderViaHTTP,
+} from 'next-test-utils'
+import fs from 'fs-extra'
+
+jest.setTimeout(1000 * 30)
+
+const appDir = join(__dirname, '../')
+const nextConfig = join(appDir, 'next.config.js')
+let appPort
+let app
+
+function runTests() {
+  describe('On a static page', () => {
+    checkImagesOnPage('/')
+  })
+
+  describe('On an SSR page', () => {
+    checkImagesOnPage('/stars')
+  })
+}
+
+function checkImagesOnPage(path) {
+  it('should not preload tiny images', async () => {
+    const html = await renderViaHTTP(appPort, path)
+    expect(html).not.toContain('<link rel="preload" href="tiny-image.jpg"/>')
+  })
+  it('should not add a preload if one already exists', async () => {
+    let html = await renderViaHTTP(appPort, path)
+    html = html.replace(
+      '<link rel="preload" href="already-preloaded.jpg"/>',
+      ''
+    )
+    expect(html).not.toContain(
+      '<link rel="preload" href="already-preloaded.jpg"/>'
+    )
+  })
+  it('should not preload hidden images', async () => {
+    const html = await renderViaHTTP(appPort, path)
+    expect(html).not.toContain(
+      '<link rel="preload" href="hidden-image-1.jpg"/>'
+    )
+    expect(html).not.toContain(
+      '<link rel="preload" href="hidden-image-2.jpg"/>'
+    )
+  })
+  it('should preload exactly two eligible images', async () => {
+    const html = await renderViaHTTP(appPort, path)
+    expect(html).toContain('<link rel="preload" href="main-image-1.jpg"/>')
+    expect(html).not.toContain('<link rel="preload" href="main-image-2.jpg"/>')
+  })
+}
+
+describe('Image optimization for SSR apps', () => {
+  beforeAll(async () => {
+    await fs.writeFile(
+      nextConfig,
+      `module.exports = { experimental: {optimizeImages: true} }`,
+      'utf8'
+    )
+    await nextBuild(appDir)
+    appPort = await findPort()
+    app = await nextStart(appDir, appPort)
+  })
+  afterAll(() => killApp(app))
+  runTests()
+})
+
+describe('Image optimization for serverless apps', () => {
+  beforeAll(async () => {
+    await fs.writeFile(
+      nextConfig,
+      `module.exports = { target: 'serverless', experimental: {optimizeImages: true} }`,
+      'utf8'
+    )
+    await nextBuild(appDir)
+    appPort = await findPort()
+    app = await nextStart(appDir, appPort)
+  })
+  afterAll(() => killApp(app))
+  runTests()
+})
