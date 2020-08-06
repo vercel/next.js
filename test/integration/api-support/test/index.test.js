f24ec80dbafcca63ea4f6a89c460b0fd88469488
JJ Kasper <jj@jjsweb.site>
2020-08-06 15:51:12 -0600

    Run lint-fix

--- test/integration/api-support/test/index.test.js
+++ test/integration/api-support/test/index.test.js
@@ -238,12 +238,16 @@ function runTests(dev = false) {
 
   it('should show friendly error for invalid redirect', async () => {
     await fetchViaHTTP(appPort, '/api/redirect-error', null, {})
-    expect(stderr).toContain(`Invalid redirect arguments. Please use a single argument URL, e.g. res.redirect('/destination') or use a status code and URL, e.g. res.redirect(307, '/destination').`)
+    expect(stderr).toContain(
+      `Invalid redirect arguments. Please use a single argument URL, e.g. res.redirect('/destination') or use a status code and URL, e.g. res.redirect(307, '/destination').`
+    )
   })
 
   it('should show friendly error in case of passing null as first argument redirect', async () => {
     await fetchViaHTTP(appPort, '/api/redirect-null', null, {})
-    expect(stderr).toContain(`Invalid redirect arguments. Please use a single argument URL, e.g. res.redirect('/destination') or use a status code and URL, e.g. res.redirect(307, '/destination').`)
+    expect(stderr).toContain(
+      `Invalid redirect arguments. Please use a single argument URL, e.g. res.redirect('/destination') or use a status code and URL, e.g. res.redirect(307, '/destination').`
+    )
   })
 
   it('should redirect with status code 307', async () => {
