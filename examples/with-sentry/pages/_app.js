4aa6d0335f0763e581480b9d875dc9ae32c21bfb
Kazuhito Hokamura <k.hokamura@gmail.com>
2020-08-07 16:22:08 +0900

    Update Sentry example to improve display of server errors (#15495)
    In the Sentry example, sourcemaps is not enabled in server error.
    
    I solved this problem by using `RewriteFrames`. On the client side, the sourcemaps path is `~/_next`, but on the server side it needs to be `~/.next` (distDir), so I rewrite it using the `iteratee`.
    
    ref. https://docs.sentry.io/platforms/node/sourcemaps/#updating-sentry-sdk-configuration-to-support-source-maps
    
    -----
    
    ## before
    <img width="983" alt="ss2" src="https://user-images.githubusercontent.com/39471/88479997-29188580-cf8e-11ea-8d16-10ccfa6fc1a4.png">
    
    ## after
    <img width="989" alt="ss1" src="https://user-images.githubusercontent.com/39471/88479995-26b62b80-cf8e-11ea-8b1f-7784b32b9e6d.png">

--- examples/with-sentry/pages/_app.js
+++ examples/with-sentry/pages/_app.js
@@ -1,8 +1,20 @@
 import * as Sentry from '@sentry/node'
+import { RewriteFrames } from '@sentry/integrations'
+import getConfig from 'next/config'
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  const config = getConfig()
+  const distDir = `${config.serverRuntimeConfig.rootDir}/.next`
   Sentry.init({
     enabled: process.env.NODE_ENV === 'production',
+    integrations: [
+      new RewriteFrames({
+        iteratee: (frame) => {
+          frame.filename = frame.filename.replace(distDir, 'app:///_next')
+          return frame
+        },
+      }),
+    ],
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   })
 }
