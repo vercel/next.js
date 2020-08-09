52339dc3943af68f77c7f73a33bda5c179dd9759
mackarelfish <mackarelfish93@gmail.com>
2020-08-09 15:31:20 +0700

    Change the order of state overriding, merge initial state with preloaded state

--- examples/with-redux/store.js
+++ examples/with-redux/store.js
@@ -41,7 +41,7 @@ const reducer = (state = initialState, action) => {
 function initStore(preloadedState = initialState) {
   return createStore(
     reducer,
-    preloadedState,
+    { ...initialState, ...preloadedState },
     composeWithDevTools(applyMiddleware())
   )
 }
@@ -53,8 +53,8 @@ export const initializeStore = (preloadedState) => {
   // with the current state in the store, and create a new store
   if (preloadedState && store) {
     _store = initStore({
-      ...store.getState(),
       ...preloadedState,
+      ...store.getState(),
     })
     // Reset the current store
     store = undefined
