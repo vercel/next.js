2a805fa8a00933d4b3dac85e0b052a558de25f3d
ondrejch <ondrejch@kentico.com>
2020-08-10 11:11:54 +0200

    add kontent example

---
+++ examples/cms-kontent/lib/api.js
@@ -0,0 +1,62 @@
+import { DeliveryClient } from '@kentico/kontent-delivery'
+
+const client = new DeliveryClient({
+  projectId: process.env.KONTENT_PROJECT_ID,
+  previewApiKey: process.env.KONTENT_PREVIEW_API_KEY,
+})
+
+function parseAuthor(author) {
+  return {
+    name: author.name.value,
+    picture: author.picture.value[0].url,
+  }
+}
+
+function parsePost(post) {
+  return {
+    title: post.title.value,
+    slug: post.slug.value,
+    date: post.date.value.toISOString(),
+    content: post.content.value,
+    excerpt: post.excerpt.value,
+    coverImage: post.cover_image.value[0].url,
+    author: parseAuthor(post.author.value[0]),
+  }
+}
+
+export async function getAllPostSlugs() {
+  const postsResponse = await client
+    .items()
+    .type('post')
+    .elementsParameter(['slug'])
+    .toPromise()
+
+  return postsResponse.items.map((post) => post.slug.value)
+}
+
+export async function getPostBySlug(slug, preview) {
+  const post = await client
+    .items()
+    .queryConfig({
+      usePreviewMode: !!preview,
+    })
+    .type('post')
+    .equalsFilter('elements.slug', slug)
+    .toPromise()
+    .then((result) => result.getFirstItem())
+    .then((post) => parsePost(post))
+  return post
+}
+
+export async function getAllPosts(preview) {
+  const postsResponse = await client
+    .items()
+    .queryConfig({
+      usePreviewMode: !!preview,
+    })
+    .type('post')
+    .orderByDescending('elements.date')
+    .toPromise()
+
+  return postsResponse.items.map((post) => parsePost(post))
+}
---
+++ examples/cms-kontent/pages/posts/[slug].js
@@ -0,0 +1,72 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import Layout from '../../components/layout'
+import { getPostBySlug, getAllPostSlugs } from '../../lib/api'
+import PostTitle from '../../components/post-title'
+import Head from 'next/head'
+import { CMS_NAME } from '../../lib/constants'
+
+export default function Post({ post, morePosts, preview }) {
+  const router = useRouter()
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>
+                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                </title>
+                <meta property="og:image" content={post.coverImage.url} />
+              </Head>
+              <PostHeader
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                author={post.author}
+              />
+              <PostBody content={post.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params, preview = null }) {
+  const post = await getPostBySlug(params.slug, preview)
+
+  return {
+    props: {
+      post,
+      preview,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const slugs = await getAllPostSlugs(['slug'])
+  return {
+    paths: slugs.map(
+      (slug) =>
+        ({
+          params: {
+            slug,
+          },
+        } || [])
+    ),
+    fallback: false,
+  }
+}
