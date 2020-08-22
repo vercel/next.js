629a9ed5048629dc12670ef457fe6efaa2e8d3b6
Weston Thayer <weston@cryclops.com>
2020-08-22 16:51:11 -0700

    Fix server source maps in Vercel
    serverRuntimeConfig does not pull in the correct __dirname when deployed to Vercel, it gets set to __dirname in the build step (similar to /vercel/5857612a/). This is different than the __dirname when a serverless function is executing (it's set to /var/task/).
    
    This caused Sentry's RewriteFrames integration not to work as intended, Error.stack retains absolute file paths, so source maps are not applied in Sentry.
    
    Until https://github.com/vercel/next.js/issues/8251 is fixed, hardcoding the directory in an environment variable might be the best fix.

--- examples/with-sentry/pages/_app.js
+++ examples/with-sentry/pages/_app.js
@@ -1,21 +1,34 @@
 import * as Sentry from '@sentry/node'
 import { RewriteFrames } from '@sentry/integrations'
-import getConfig from 'next/config'
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  const config = getConfig()
-  const distDir = `${config.serverRuntimeConfig.rootDir}/.next`
-  Sentry.init({
-    enabled: process.env.NODE_ENV === 'production',
-    integrations: [
+  const integrations = []
+  if (
+    process.env.NEXT_IS_SERVER === 'true' &&
+    process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR
+  ) {
+    // For Node.js, rewrite Error.stack to use relative paths, so that source
+    // maps starting with ~/_next map to files in Error.stack with path
+    // app:///_next
+    integrations.push(
       new RewriteFrames({
         iteratee: (frame) => {
-          frame.filename = frame.filename.replace(distDir, 'app:///_next')
+          frame.filename = frame.filename.replace(
+            process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
+            'app:///'
+          )
+          frame.filename = frame.filename.replace('.next', '_next')
           return frame
         },
-      }),
-    ],
+      })
+    )
+  }
+
+  Sentry.init({
+    enabled: process.env.NODE_ENV === 'production',
+    integrations,
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    release: process.env.NEXT_PUBLIC_COMMIT_SHA,
   })
 }
 
