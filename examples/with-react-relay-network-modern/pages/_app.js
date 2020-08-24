c24eac4599e51510576c2cb576bbcfa88724cd57
HsuTing <hsuting0106@gmail.com>
2020-08-24 13:44:45 +0800

    feat(with-react-relay-network-modern): use new RelayEnvironmentProvider

--- examples/with-react-relay-network-modern/pages/_app.js
+++ examples/with-react-relay-network-modern/pages/_app.js
@@ -1,6 +1,15 @@
+import { RelayEnvironmentProvider } from 'relay-hooks'
+
 import { createEnvironment } from '../lib/createEnvironment'
 
 export default function App({ Component, pageProps }) {
-  const environment = createEnvironment(pageProps.records)
-  return <Component {...pageProps} environment={environment} />
+  return (
+    <RelayEnvironmentProvider
+      environment={
+        pageProps.environment || createEnvironment(pageProps.relayData)
+      }
+    >
+      <Component {...pageProps} />
+    </RelayEnvironmentProvider>
+  )
 }
