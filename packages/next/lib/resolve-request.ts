87e0974628d93061cc8edc1a52dbf7ca9ad42844
OBrien, Kevin <kobrien@cvent.com>
2020-08-26 16:57:07 -0400

    fix: all pnpm to be used with next.js 9.5+

--- packages/next/lib/resolve-request.ts
+++ packages/next/lib/resolve-request.ts
@@ -8,6 +8,17 @@ export function resolveRequest(req: string, issuer: string): string {
   if (process.versions.pnp) {
     const { resolveRequest: pnpResolveRequest } = require(`pnpapi`)
     return pnpResolveRequest(req, issuer, { considerBuiltins: false })
+  } else if (process?.env?.npm_config_user_agent?.startsWith('pnpm/')) {
+    // fixes webpack builds when using pnpm by switching to the default node resolution algorithm
+    // see: preserveSymlinks
+    // from : https://www.npmjs.com/package/resolve
+    //  - Note: this property is currently true by default but it will be changed to false in the next major
+    //           version because Node's resolution algorithm does not preserve symlinks by default.
+    const basedir =
+      issuer.endsWith(path.posix.sep) || issuer.endsWith(path.win32.sep)
+        ? issuer
+        : path.dirname(issuer)
+    return resolve.sync(req, { basedir, preserveSymlinks: false })
   } else {
     const basedir =
       issuer.endsWith(path.posix.sep) || issuer.endsWith(path.win32.sep)
