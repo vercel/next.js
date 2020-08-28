919e469799a6eef2a4667d7d210fd60ad39d6c72
Daniel Eden <dan.eden@me.com>
2020-08-28 10:48:03 +0100

    Remove next.config.js

--- examples/with-mdx-remote/next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: {
-    mdxPath: 'posts',
-  },
-}
--- examples/with-mdx-remote/utils/mdxUtils.js
+++ examples/with-mdx-remote/utils/mdxUtils.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), process.env.mdxPath)
+export const POSTS_PATH = path.join(process.cwd(), 'posts')
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
