1dd2e7eb3103f03aff92f614fde4a2ad9f444ca1
Patrick Krawczykowski <patrick@dreadful.design>
2020-08-29 11:46:50 -0500

    Use default export

--- examples/with-styled-components/pages/_app.js
+++ examples/with-styled-components/pages/_app.js
@@ -6,7 +6,7 @@ const theme = {
   },
 }
 
-export function App({Component, pageProps}) {
+export default function App({Component, pageProps}) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
