4fec6b3de909c44e2466cabdfb165b4bca50f3d8
Joe Haddad <joe.haddad@zeit.co>
2020-08-29 22:20:27 -0400

    lint

--- examples/with-styled-components/pages/_app.js
+++ examples/with-styled-components/pages/_app.js
@@ -6,7 +6,7 @@ const theme = {
   },
 }
 
-export default function App({Component, pageProps}) {
+export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
