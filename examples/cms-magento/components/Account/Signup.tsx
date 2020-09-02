b8ce15e3bcd332528bae0fa14a65dd4ae822a314
Sharzil Shaikh <iamsharzil@gmail.com>
2020-09-02 17:02:13 +0530

    solved regex error

--- examples/cms-magento/components/Account/Signup.tsx
+++ examples/cms-magento/components/Account/Signup.tsx
@@ -85,6 +85,7 @@ const Signup: FC = () => {
             placeholder="Enter email"
             ref={register({
               required: 'Please enter your email address',
+              pattern: /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             })}
           />
           <FormErrorMessage name="email" errors={errors} />
