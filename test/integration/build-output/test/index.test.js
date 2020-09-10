5aed1d2e353a1e49db49a69405ab6cd17328743a
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-09-10 17:12:37 +0200

    Correct query behavior for falsey values to pre 9.5.1 behavior (#16608)
    Fixes https://github.com/vercel/next.js/issues/16147

--- test/integration/build-output/test/index.test.js
+++ test/integration/build-output/test/index.test.js
@@ -104,7 +104,7 @@ describe('Build Output', () => {
       expect(parseFloat(err404FirstLoad) - 63.6).toBeLessThanOrEqual(0)
       expect(err404FirstLoad.endsWith('kB')).toBe(true)
 
-      expect(parseFloat(sharedByAll) - 60.1).toBeLessThanOrEqual(0)
+      expect(parseFloat(sharedByAll) - 60.2).toBeLessThanOrEqual(0)
       expect(sharedByAll.endsWith('kB')).toBe(true)
 
       if (_appSize.endsWith('kB')) {
