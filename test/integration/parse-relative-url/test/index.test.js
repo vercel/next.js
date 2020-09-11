eca966e869d4da299ffd8ebd1b62da3d617809d5
cowboy <dreamran43@gmail.com>
2020-09-11 19:04:16 +0800

    Try to fix Chrome failed on CICD 4

--- test/integration/parse-relative-url/test/index.test.js
+++ test/integration/parse-relative-url/test/index.test.js
@@ -25,6 +25,8 @@ describe('Parse Relative Url', () => {
           '--headless',
           '--disable-dev-shm-usage',
           '--disable-gpu',
+          '--window-size=800,600',
+          '--remote-debugging-port=9515',
         ],
       })
       await app.start()
