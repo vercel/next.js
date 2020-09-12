a6c8e9bbc972631f405c486484921706f6f5ee94
Ed-Ed <edwardandrewb@gmail.com>
2020-09-12 15:05:02 +0100

    change /favicon to /images/favicon

--- packages/create-next-app/templates/default/pages/index.js
+++ packages/create-next-app/templates/default/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
