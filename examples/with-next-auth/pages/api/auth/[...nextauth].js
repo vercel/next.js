832c42fcc03dc65810eadec6a381406e64a2128a
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-09-21 19:38:43 +0200

    fix: cs style

--- examples/with-next-auth/pages/api/auth/[...nextauth].js
+++ examples/with-next-auth/pages/api/auth/[...nextauth].js
@@ -38,9 +38,9 @@ const options = {
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
     // Note: `jwt` is automatically set to `true` if no database is specified.
-    //jwt: true,
+    // jwt: true,
     // Seconds - How long until an idle session expires and is no longer valid.
-    //maxAge: 30 * 24 * 60 * 60, // 30 days
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
     // Note: This option is ignored if using JSON Web Tokens
