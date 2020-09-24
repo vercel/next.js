7f86d64fe8f0dad5f182c06af51e359b23329049
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2020-09-24 14:53:01 +0200

    revert to this

--- packages/next/next-server/lib/router/utils/parse-relative-url.ts
+++ packages/next/next-server/lib/router/utils/parse-relative-url.ts
@@ -5,51 +5,70 @@ const DUMMY_BASE = new URL(
   typeof window === 'undefined' ? 'http://n' : getLocationOrigin()
 )
 
+const URL_PROPERTY = Symbol()
+
 /**
  * Parses path-relative urls (e.g. `/hello/world?foo=bar`). If url isn't path-relative
  * (e.g. `./hello`) then at least base must be.
  * Absolute urls are rejected with one exception, in the browser, absolute urls that are on
  * the current origin will be parsed as relative
  */
-export class RelativeURL extends URL {
+export class RelativeURL {
+  private readonly [URL_PROPERTY]: URL
+
+  readonly host = ''
+  readonly hostname = ''
+  readonly protocol = ''
+  readonly port = ''
+  readonly origin = ''
+
   constructor(url: string, base?: string) {
     const resolvedBase = base ? new URL(base, DUMMY_BASE) : DUMMY_BASE
-    super(
+    this[URL_PROPERTY] = new URL(
       // avoid URL parsing errors with //. WHATWG URL will try to parse this as
       // a protocol-relative url, which we don't want
       url.startsWith('/') ? resolvedBase.origin + url : url,
       resolvedBase
     )
     if (
-      super.origin !== DUMMY_BASE.origin ||
-      (super.protocol !== 'http:' && super.protocol !== 'https:')
+      this[URL_PROPERTY].origin !== DUMMY_BASE.origin ||
+      (this[URL_PROPERTY].protocol !== 'http:' &&
+        this[URL_PROPERTY].protocol !== 'https:')
     ) {
       throw new Error('invariant: invalid relative URL')
     }
   }
 
-  get host() {
-    return ''
+  get pathname() {
+    return this[URL_PROPERTY].pathname
   }
 
-  get hostname() {
-    return ''
+  set pathname(value: string) {
+    this[URL_PROPERTY].pathname = value
   }
 
-  get protocol() {
-    return ''
+  get search() {
+    return this[URL_PROPERTY].search
   }
 
-  get port() {
-    return ''
+  set search(value: string) {
+    this[URL_PROPERTY].search = value
   }
 
-  get origin() {
-    return ''
+  get searchParams() {
+    return this[URL_PROPERTY].searchParams
+  }
+
+  get hash() {
+    return this[URL_PROPERTY].hash
+  }
+
+  set hash(value: string) {
+    this[URL_PROPERTY].hash = value
   }
 
   get href() {
-    return super.href.slice(super.origin.length)
+    return this[URL_PROPERTY].href.slice(this[URL_PROPERTY].origin.length)
   }
 }
 
