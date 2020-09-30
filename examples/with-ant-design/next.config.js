3092611c39ccd56dd54fa43a64eb8b5354805038
Dennis Schneider <ds@thepeaklab.com>
2020-09-30 10:37:18 +0200

    add static export to example, fixes #17385

--- examples/with-ant-design/next.config.js
+++ examples/with-ant-design/next.config.js
@@ -2,4 +2,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer()
+module.exports = withBundleAnalyzer({
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
+})
