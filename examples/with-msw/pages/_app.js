51927052b24a991dde2356d7e9abbacf8cfabe04
Jens Meindertsma <jens@jensmeindertsma.com>
2020-10-07 18:15:20 +0200

    Update conditional require statement

--- examples/with-msw/pages/_app.js
+++ examples/with-msw/pages/_app.js
@@ -1,6 +1,12 @@
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+// Enable API mocking in all environments, this is only for the sake of the example.
+// In a real app you should remove this code and uncomment the code below.
+if (true) {
   require('../mocks')
 }
+
+// if (process.env.NODE_ENV !== 'production') {
+//   require('../mocks')
+// }
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
