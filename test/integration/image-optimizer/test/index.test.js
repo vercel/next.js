689f7c1287da2221a407b76eb4ee8e94d7b58b73
Steven <steven@ceriously.com>
2020-10-09 17:37:36 -0400

    Add tests

--- test/integration/image-optimizer/test/index.test.js
+++ test/integration/image-optimizer/test/index.test.js
@@ -14,6 +14,7 @@ jest.setTimeout(1000 * 60 * 2)
 
 const appDir = join(__dirname, '../')
 const nextConfig = join(appDir, 'next.config.js')
+const nextConfigContent = fs.readFileSync(nextConfig, 'utf8')
 let appPort
 let app
 
@@ -27,54 +28,94 @@ function runTests() {
     const query = { w: 64, q: 100 }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('url is missing')
+    expect(await res.text()).toBe(`"url" parameter is required`)
   })
 
   it('should fail when w is missing', async () => {
     const query = { url: '/vercel-logo.png', q: 100 }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('w is missing')
+    expect(await res.text()).toBe(`"w" parameter (width) is required`)
   })
 
   it('should fail when q is missing', async () => {
     const query = { url: '/vercel-logo.png', w: 64 }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('q is missing')
+    expect(await res.text()).toBe(`"q" parameter (quality) is required`)
   })
 
   it('should fail when q is greater than 100', async () => {
     const query = { url: '/vercel-logo.png', w: 64, q: 101 }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('q must be between 1 and 100')
+    expect(await res.text()).toBe(
+      `"q" parameter (quality) must be a number between 1 and 100`
+    )
   })
 
   it('should fail when q is less than 1', async () => {
     const query = { url: '/vercel-logo.png', w: 64, q: 0 }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('q must be between 1 and 100')
+    expect(await res.text()).toBe(
+      `"q" parameter (quality) must be a number between 1 and 100`
+    )
+  })
+
+  it('should fail when w is 0 or less', async () => {
+    const query = { url: '/vercel-logo.png', w: 0, q: 100 }
+    const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
+    expect(res.status).toBe(400)
+    expect(await res.text()).toBe(
+      `"w" parameter (width) must be a number greater than 0`
+    )
   })
 
   it('should fail when w is not a number', async () => {
     const query = { url: '/vercel-logo.png', w: 'foo', q: 100 }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('w must be a number')
+    expect(await res.text()).toBe(
+      `"w" parameter (width) must be a number greater than 0`
+    )
   })
 
   it('should fail when q is not a number', async () => {
     const query = { url: '/vercel-logo.png', w: 64, q: 'foo' }
     const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
     expect(res.status).toBe(400)
-    expect(await res.text()).toBe('q must be a number')
+    expect(await res.text()).toBe(
+      `"q" parameter (quality) must be a number between 1 and 100`
+    )
+  })
+
+  it('should fail when domain is not in next.config.js', async () => {
+    const query = {
+      url: `http://vercel.com/button`,
+      w: 64,
+      q: 100,
+    }
+    const opts = { headers: { accept: 'image/webp' } }
+    const res = await fetchViaHTTP(appPort, '/_next/image', query, opts)
+    expect(res.status).toBe(400)
+    expect(await res.text()).toBe(`"url" parameter is not allowed`)
+  })
+
+  it('should fail when width is not in next.config.js', async () => {
+    const query = { url: '/vercel-logo.png', w: 1000, q: 100 }
+    const opts = { headers: { accept: 'image/webp' } }
+    const res = await fetchViaHTTP(appPort, '/_next/image', query, opts)
+    expect(res.status).toBe(400)
+    expect(await res.text()).toBe(
+      `"w" parameter (width) of 1000 is not allowed`
+    )
   })
 
   it('should resize relative url', async () => {
     const query = { url: '/vercel-logo.png', w: 64, q: 100 }
-    const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
+    const opts = { headers: { accept: 'image/webp' } }
+    const res = await fetchViaHTTP(appPort, '/_next/image', query, opts)
     expect(res.status).toBe(200)
     expect(res.headers.get('content-type')).toBe('image/webp')
   })
@@ -85,7 +126,8 @@ function runTests() {
       w: 64,
       q: 100,
     }
-    const res = await fetchViaHTTP(appPort, '/_next/image', query, {})
+    const opts = { headers: { accept: 'image/webp' } }
+    const res = await fetchViaHTTP(appPort, '/_next/image', query, opts)
     expect(res.status).toBe(200)
     expect(res.headers.get('content-type')).toBe('image/webp')
   })
@@ -115,17 +157,17 @@ describe('API routes', () => {
 
   describe('Serverless support', () => {
     beforeAll(async () => {
-      await fs.writeFile(
-        nextConfig,
-        `module.exports = { target: 'serverless' }`
-      )
+      const target = 'target: "serverless",'
+      const [first, ...rest] = nextConfigContent.split('\n')
+      const content = [first, target, ...rest].join('\n')
+      await fs.writeFile(nextConfig, content)
       await nextBuild(appDir)
       appPort = await findPort()
       app = await nextStart(appDir, appPort)
     })
     afterAll(async () => {
       await killApp(app)
-      await fs.remove(nextConfig)
+      await fs.writeFile(nextConfig, nextConfigContent)
     })
 
     runTests()
