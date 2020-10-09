5da980b13c7622a3115ad724d528f5e50caa0cfc
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2020-10-09 07:21:09 +0200

    add suggested changes

--- examples/with-emotion/pages/_app.js
+++ examples/with-emotion/pages/_app.js
@@ -1,14 +1,10 @@
-import createCache from '@emotion/cache'
-import { CacheProvider } from '@emotion/core'
 import { globalStyles } from '../shared/styles'
 
-const cache = createCache()
-
 const App = ({ Component, pageProps }) => (
-  <CacheProvider value={cache}>
+  <>
     {globalStyles}
     <Component {...pageProps} />
-  </CacheProvider>
+  </>
 )
 
 export default App
