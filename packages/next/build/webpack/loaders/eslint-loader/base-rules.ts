f278c21705578b3f0e80fd23d5c1febc0f194b59
Prateek Bhatnagar <prateek89born@gmail.com>
2020-10-14 13:40:46 -0700

    fixes typescript rules problems

--- packages/next/build/webpack/loaders/eslint-loader/base-rules.ts
+++ packages/next/build/webpack/loaders/eslint-loader/base-rules.ts
@@ -1,7 +1,4 @@
-const generalRules = {
-  'react-hooks/exhaustive-deps': 'off',
-}
-const jsOnlyRules = {
+const baseRules = {
   'array-callback-return': 'warn',
   'default-case': ['warn', { commentPattern: '^no default$' }],
   'dot-location': ['warn', 'property'],
@@ -134,12 +131,41 @@ const jsOnlyRules = {
     },
   ],
   'getter-return': 'warn',
+  'react-hooks/exhaustive-deps': 'off',
 }
 
-export const getBaseRules = (isTypescript: Boolean) =>
-  isTypescript
-    ? generalRules
-    : {
-        ...generalRules,
-        ...jsOnlyRules,
-      }
+// Copied from https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/eslint-recommended.ts
+const tsRules = {
+  'constructor-super': 'off', // ts(2335) & ts(2377)
+  'getter-return': 'off', // ts(2378)
+  'no-const-assign': 'off', // ts(2588)
+  'no-dupe-args': 'off', // ts(2300)
+  'no-dupe-class-members': 'off', // ts(2393) & ts(2300)
+  'no-dupe-keys': 'off', // ts(1117)
+  'no-func-assign': 'off', // ts(2539)
+  'no-import-assign': 'off', // ts(2539) & ts(2540)
+  'no-new-symbol': 'off', // ts(2588)
+  'no-obj-calls': 'off', // ts(2349)
+  'no-redeclare': 'off', // ts(2451)
+  'no-setter-return': 'off', // ts(2408)
+  'no-this-before-super': 'off', // ts(2376)
+  'no-undef': 'off', // ts(2304)
+  'no-unreachable': 'off', // ts(7027)
+  'no-unsafe-negation': 'off', // ts(2365) & ts(2360) & ts(2358)
+  'no-var': 'error', // ts transpiles let/const to var, so no need for vars any more
+  'prefer-const': 'error', // ts provides better types with const
+  'prefer-rest-params': 'error', // ts provides better types with rest args over arguments
+  'prefer-spread': 'error', // ts transpiles spread to apply, so no need for manual apply
+  'valid-typeof': 'off', // ts(2367),
+  'no-unused-vars': 'off',
+}
+
+export const getBaseRules = (isTypescript: Boolean) => {
+  if (!isTypescript) {
+    return baseRules
+  }
+  return {
+    ...baseRules,
+    ...tsRules,
+  }
+}
