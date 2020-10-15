e57794b02c4a791d86908730d18235f601d942a4
JJ Kasper <jj@jjsweb.site>
2020-10-14 19:07:15 -0500

    Add locale prop for transitioning locales client side

--- packages/next/next-server/lib/router/router.ts
+++ packages/next/next-server/lib/router/router.ts
@@ -29,6 +29,7 @@ import escapePathDelimiters from './utils/escape-path-delimiters'
 
 interface TransitionOptions {
   shallow?: boolean
+  locale?: string
 }
 
 interface NextHistoryState {
@@ -592,6 +593,7 @@ export default class Router implements BaseRouter {
       window.location.href = url
       return false
     }
+    this.locale = options.locale || this.locale
 
     if (!(options as any)._h) {
       this.isSsr = false
