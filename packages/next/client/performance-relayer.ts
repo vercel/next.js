880113ab0fa49cc5e4eeb3d1a507daf390b9d493
Joe Haddad <joe.haddad@zeit.co>
2020-10-16 04:27:50 -0400

    Call Web Vitals reporting at correct time (#17933)
    This PR adjust the Web Vitals reporting to be called *after* rendering occurs. It used to work like this, but React's render method is no longer synchronous—so we have to do it in an effect.
    
    Existing tests should cover this code path, and IMO it's unfeasible to test that it's invoked _after_ hydration.
    
    Also removed the `&& ST` condition which is not relevant to Web Vitals.

--- packages/next/client/performance-relayer.ts
+++ packages/next/client/performance-relayer.ts
@@ -4,13 +4,32 @@ import {
   getFID,
   getLCP,
   getTTFB,
+  Metric,
   ReportHandler,
 } from 'web-vitals'
 
-export default (onPerfEntry: ReportHandler) => {
-  getCLS(onPerfEntry)
-  getFID(onPerfEntry)
-  getFCP(onPerfEntry)
-  getLCP(onPerfEntry)
-  getTTFB(onPerfEntry)
+let isRegistered = false
+let userReportHandler: ReportHandler | undefined
+
+function onReport(metric: Metric) {
+  if (userReportHandler) {
+    userReportHandler(metric)
+  }
+}
+
+export default (onPerfEntry?: ReportHandler) => {
+  // Update function if it changes:
+  userReportHandler = onPerfEntry
+
+  // Only register listeners once:
+  if (isRegistered) {
+    return
+  }
+  isRegistered = true
+
+  getCLS(onReport)
+  getFID(onReport)
+  getFCP(onReport)
+  getLCP(onReport)
+  getTTFB(onReport)
 }
