01e6bd16843411d2d3bae1f0387284b1fbb6c442
Alex Castle <atcastle@gmail.com>
2020-10-20 07:28:01 -0700

    Remove multi-host support for image component and support quality pass-through (#18038)
    Co-authored-by: Tim Neutkens <timneutkens@me.com>

--- test/integration/image-component/basic/next.config.js
+++ test/integration/image-component/basic/next.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   images: {
     sizes: [480, 1024, 1600],
-    hosts: {
-      default: {
-        path: 'https://example.com/myaccount/',
-        loader: 'imgix',
-      },
-      secondary: {
-        path: 'https://examplesecondary.com/images/',
-        loader: 'cloudinary',
-      },
-    },
+    autoOptimize: false,
+    path: 'https://example.com/myaccount/',
+    loader: 'imgix',
   },
 }
