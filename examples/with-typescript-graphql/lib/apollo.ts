e10481a6d1a4804a25ec1a69895a8851c8c4f397
SmileJayden <wkdwodud07@gmail.com>
2020-10-20 14:25:46 +0900

    fix wrong url in HttpLink

--- examples/with-typescript-graphql/lib/apollo.ts
+++ examples/with-typescript-graphql/lib/apollo.ts
@@ -21,7 +21,7 @@ function createIsomorphLink(context: ResolverContext = {}) {
   } else {
     const { HttpLink } = require('@apollo/client')
     return new HttpLink({
-      uri: '/api/graphql',
+      uri: 'http://localhost:3000/api/graphql',
       credentials: 'same-origin',
     })
   }
