7cddb6103a7b00eddfd5520cf83d8787fb7efab2
Tasuku Uno <tsk.uno@gmail.com>
2020-10-21 19:11:18 +0900

    Fix css dependency in /_error (#17301)

---
+++ test/integration/document-file-dependencies/pages/_error.js
@@ -0,0 +1,9 @@
+import style from '../css/error.module.css'
+
+export default function Error() {
+  return (
+    <div id="error" className={`global ${style.error}`}>
+      error
+    </div>
+  )
+}
