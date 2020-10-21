432767215c402ba75129a7e27c611ebe0b5acbf6
Tuan Nguyen <69383359+gr-qft@users.noreply.github.com>
2020-10-20 21:17:39 -0400

    Use ya in case xa is false (#18074)
    This closes [#18023](https://github.com/vercel/next.js/issues/18023). 
    
    I was following the example mentioned in the issue (https://github.com/vercel/next.js/tree/canary/examples/with-firebase-authentication) and for some reason `xa` was always false, failing fetching food. Through `console.log` I saw that `ya` contained a token value and using `ya` worked.

--- examples/with-firebase-authentication/utils/auth/mapUserData.js
+++ examples/with-firebase-authentication/utils/auth/mapUserData.js
@@ -1,8 +1,8 @@
 export const mapUserData = (user) => {
-  const { uid, email, xa } = user
+  const { uid, email, xa, ya } = user
   return {
     id: uid,
     email,
-    token: xa,
+    token: xa || ya,
   }
 }
