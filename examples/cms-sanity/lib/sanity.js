5bc47db1f3e219fcd968a36ad2877d030aee2542
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-10-22 18:32:34 +0200

    [cms-sanity] Allow environment configurable dataset

--- examples/cms-sanity/lib/sanity.js
+++ examples/cms-sanity/lib/sanity.js
@@ -6,7 +6,7 @@ import {
 
 const options = {
   // Find your project ID and dataset in `sanity.json` in your studio project
-  dataset: 'production',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: process.env.NODE_ENV === 'production',
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
