06465898c11046ef1a2c791c417fc9b05d4a6be5
rokobekavac0 <roko.beakavac2003@gmail.com>
2020-10-26 10:00:47 +0100

    Fix getStaticPaths return

--- examples/cms-contentful/pages/posts/[slug].js
+++ examples/cms-contentful/pages/posts/[slug].js
@@ -68,7 +68,7 @@ export async function getStaticProps({ params, preview = false }) {
 export async function getStaticPaths() {
   const allPosts = await getAllPostsWithSlug()
   return {
-    paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
+    paths: allPosts?.map(({ slug }) => ({ params: { slug } })) ?? [],
     fallback: true,
   }
 }
