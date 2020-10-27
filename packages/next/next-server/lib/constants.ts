c06c96d6cbf1e8fc8d7a34fbf7505b55853918aa
Conor Dockry <conordockry@gmail.com>
2020-10-27 19:28:19 -0400

    change TEMPORARY_REDIRECT_STATUS to 303
    fixes #18350

--- packages/next/next-server/lib/constants.ts
+++ packages/next/next-server/lib/constants.ts
@@ -32,7 +32,7 @@ export const CLIENT_STATIC_FILES_RUNTIME_AMP = `amp`
 export const CLIENT_STATIC_FILES_RUNTIME_WEBPACK = `webpack`
 // static/runtime/polyfills.js
 export const CLIENT_STATIC_FILES_RUNTIME_POLYFILLS = `polyfills`
-export const TEMPORARY_REDIRECT_STATUS = 307
+export const TEMPORARY_REDIRECT_STATUS = 303
 export const PERMANENT_REDIRECT_STATUS = 308
 export const STATIC_PROPS_ID = '__N_SSG'
 export const SERVER_PROPS_ID = '__N_SSP'
