8ae5e80eb6c1a8f96e39736e8abb127efcecf656
JJ Kasper <jj@jjsweb.site>
2020-10-28 16:54:58 -0500

    Add err.sh for i18n config errors

--- packages/next/next-server/server/config.ts
+++ packages/next/next-server/server/config.ts
@@ -315,24 +315,28 @@ function assignDefaults(userConfig: { [key: string]: any }) {
     const i18nType = typeof i18n
 
     if (i18nType !== 'object') {
-      throw new Error(`Specified i18n should be an object received ${i18nType}`)
+      throw new Error(
+        `Specified i18n should be an object received ${i18nType}.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
+      )
     }
 
     if (!Array.isArray(i18n.locales)) {
       throw new Error(
-        `Specified i18n.locales should be an Array received ${typeof i18n.locales}`
+        `Specified i18n.locales should be an Array received ${typeof i18n.locales}.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
       )
     }
 
     const defaultLocaleType = typeof i18n.defaultLocale
 
     if (!i18n.defaultLocale || defaultLocaleType !== 'string') {
-      throw new Error(`Specified i18n.defaultLocale should be a string`)
+      throw new Error(
+        `Specified i18n.defaultLocale should be a string.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
+      )
     }
 
     if (typeof i18n.domains !== 'undefined' && !Array.isArray(i18n.domains)) {
       throw new Error(
-        `Specified i18n.domains must be an array of domain objects e.g. [ { domain: 'example.fr', defaultLocale: 'fr', locales: ['fr'] } ] received ${typeof i18n.domains}`
+        `Specified i18n.domains must be an array of domain objects e.g. [ { domain: 'example.fr', defaultLocale: 'fr', locales: ['fr'] } ] received ${typeof i18n.domains}.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
       )
     }
 
@@ -370,14 +374,14 @@ function assignDefaults(userConfig: { [key: string]: any }) {
             .map((item: any) => JSON.stringify(item))
             .join(
               '\n'
-            )}\n\ndomains value must follow format { domain: 'example.fr', defaultLocale: 'fr', locales: ['fr'] }`
+            )}\n\ndomains value must follow format { domain: 'example.fr', defaultLocale: 'fr', locales: ['fr'] }.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
         )
       }
     }
 
     if (!Array.isArray(i18n.locales)) {
       throw new Error(
-        `Specified i18n.locales must be an array of locale strings e.g. ["en-US", "nl-NL"] received ${typeof i18n.locales}`
+        `Specified i18n.locales must be an array of locale strings e.g. ["en-US", "nl-NL"] received ${typeof i18n.locales}.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
       )
     }
 
@@ -387,14 +391,18 @@ function assignDefaults(userConfig: { [key: string]: any }) {
 
     if (invalidLocales.length > 0) {
       throw new Error(
-        `Specified i18n.locales contains invalid values, locales must be valid locale tags provided as strings e.g. "en-US".\n` +
+        `Specified i18n.locales contains invalid values (${invalidLocales
+          .map(String)
+          .join(
+            ', '
+          )}), locales must be valid locale tags provided as strings e.g. "en-US".\n` +
           `See here for list of valid language sub-tags: http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry`
       )
     }
 
     if (!i18n.locales.includes(i18n.defaultLocale)) {
       throw new Error(
-        `Specified i18n.defaultLocale should be included in i18n.locales`
+        `Specified i18n.defaultLocale should be included in i18n.locales.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
       )
     }
 
@@ -411,7 +419,7 @@ function assignDefaults(userConfig: { [key: string]: any }) {
       localeDetectionType !== 'undefined'
     ) {
       throw new Error(
-        `Specified i18n.localeDetection should be undefined or a boolean received ${localeDetectionType}`
+        `Specified i18n.localeDetection should be undefined or a boolean received ${localeDetectionType}.\nSee more info here: https://err.sh/nextjs/invalid-i18n-config`
       )
     }
   }
