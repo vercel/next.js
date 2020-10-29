b7d08a7776fe5c108f0ab18d2879a6ab470850b1
Luis Alvarez <luis@vercel.com>
2020-10-29 17:59:55 -0500

    Lint fix

--- examples/cms-wordpress/components/hero-post.js
+++ examples/cms-wordpress/components/hero-post.js
@@ -15,9 +15,8 @@ export default function HeroPost({
     <section>
       <div className="mb-8 md:mb-16">
         {coverImage && (
-        <CoverImage title={title} coverImage={coverImage} slug={slug} />
-        )
-      }
+          <CoverImage title={title} coverImage={coverImage} slug={slug} />
+        )}
       </div>
       <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
         <div>
