8994de7ed2e61d54d811ad8940bed2d8094dcdf1
Luis Alvarez <luis@vercel.com>
2020-10-29 17:16:11 -0500

    Use a list for the links

--- examples/using-preact/pages/index.js
+++ examples/using-preact/pages/index.js
@@ -4,15 +4,23 @@ export default function Home() {
   return (
     <div>
       Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/ssr">
-        <a>SSR</a>
-      </Link>
-      <Link href="/ssg">
-        <a>SSG</a>
-      </Link>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/ssr">
+            <a>SSR</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/ssg">
+            <a>SSG</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
