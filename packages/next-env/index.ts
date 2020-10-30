7d605d9d80ba74a25ad447775118a24b53d90c75
Matt <carlotta.matt@gmail.com>
2020-10-30 16:38:00 -0700

    Update to snackables 1.0.0 release

--- packages/next-env/index.ts
+++ packages/next-env/index.ts
@@ -2,7 +2,7 @@
 import * as snackables from 'snackables'
 
 export type Env = snackables.ProcessEnv
-export type LoadedEnvFiles = snackables.CachedEnvFiles
+export type LoadedEnvFiles = Array<snackables.CachedEnvFiles>
 
 // NOTE: don't reload envs if we already loaded them since this breaks escaped
 // environment values e.g. \$ENV_FILE_KEY
@@ -25,11 +25,9 @@ export function loadEnvConfig(
   combinedEnv: Env
   loadedEnvFiles: LoadedEnvFiles
 } {
-  // flag to let snackables know not to reload the same .env file
-  process.env.ENV_CACHE = 'true'
+  const { ENV_DEBUG, NODE_ENV } = process.env
 
-  const isTest = process.env.NODE_ENV === 'test'
-  const mode = isTest ? 'test' : dev ? 'development' : 'production'
+  const mode = NODE_ENV === 'test' ? 'test' : dev ? 'development' : 'production'
 
   // parses and extracts .env files where the lower the .env is positioned
   // within the 'path' argument, the more important it is
@@ -49,7 +47,9 @@ export function loadEnvConfig(
       `.env.${mode}.local`,
     ].filter(Boolean) as string[],
     // displays messages about loaded ENVs or warnings
-    debug: Boolean(process.env.ENV_DEBUG) || true,
+    debug: ENV_DEBUG || true,
+    // caches ENV files so that they won't be reloaded
+    cache: true,
   })
 
   return { combinedEnv: parsed, loadedEnvFiles: cachedEnvFiles }
