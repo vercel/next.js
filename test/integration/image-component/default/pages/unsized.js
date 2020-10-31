0fd424a70eb1f7db80b049ffc4895f2ec29d71f5
Joe Haddad <joe.haddad@zeit.co>
2020-10-31 16:05:48 -0400

    fix text

--- test/integration/image-component/default/pages/unsized.js
+++ test/integration/image-component/default/pages/unsized.js
@@ -4,15 +4,15 @@ import Image from 'next/image'
 const Page = () => {
   return (
     <div>
-      <p>Layout Fill</p>
+      <p>Layout Unsized</p>
       <div style={{ position: 'relative', width: '600px', height: '350px' }}>
         <Image id="unsized1" src="/wide.png" unsized />
       </div>
-      <p>Layout Fill</p>
+      <p>Layout Unsized</p>
       <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
         <Image id="unsized2" src="/wide.png" unsized />
       </div>
-      <p>Layout Fill</p>
+      <p>Layout Unsized</p>
     </div>
   )
 }
