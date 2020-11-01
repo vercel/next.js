54ded746632bde3e637bb67459dfa25eeab9c9cc
Steven <steven@ceriously.com>
2020-10-31 20:26:57 -0400

    Add all deviceSizes to srcset for responsive and fill (#18594)

--- test/integration/image-component/default/pages/layout-responsive.js
+++ test/integration/image-component/default/pages/layout-responsive.js
@@ -11,28 +11,28 @@ const Page = () => {
         width="1200"
         height="700"
         layout="responsive"
-      ></Image>
+      />
       <Image
         id="responsive2"
         src="/wide.png"
         width="1200"
         height="700"
         layout="responsive"
-      ></Image>
+      />
       <Image
         id="responsive3"
         src="/wide.png"
         width="1200"
         height="700"
         layout="responsive"
-      ></Image>
+      />
       <Image
         id="responsive4"
         src="/wide.png"
         width="1200"
         height="700"
         layout="responsive"
-      ></Image>
+      />
       <p>Layout Responsive</p>
     </div>
   )
