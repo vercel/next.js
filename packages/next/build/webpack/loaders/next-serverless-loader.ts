f31bf939ebe732b90ed39846a1c951c422f4eb43
JJ Kasper <jj@jjsweb.site>
2020-11-02 15:11:55 -0600

    Update serverless loader

--- packages/next/build/webpack/loaders/next-serverless-loader.ts
+++ packages/next/build/webpack/loaders/next-serverless-loader.ts
@@ -368,7 +368,10 @@ const nextServerlessLoader: loader.Loader = function () {
         return
       }
 
-      detectedLocale = detectedLocale || defaultLocale
+      detectedLocale =
+        localePathResult.detectedLocale ||
+        (detectedDomain && detectedDomain.defaultLocale) ||
+        defaultLocale
     `
     : `
       const i18n = {}
