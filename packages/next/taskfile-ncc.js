d0a42210aa795919364a39ef49aaff3c75eedb76
Guy Bedford <guybedford@gmail.com>
2020-11-05 13:23:09 -0800

    reenable minification

--- packages/next/taskfile-ncc.js
+++ packages/next/taskfile-ncc.js
@@ -12,7 +12,7 @@ module.exports = function (task) {
     }
     return ncc(join(__dirname, file.dir, file.base), {
       filename: file.base,
-      minify: false,
+      minify: true,
       ...options,
     }).then(({ code, assets }) => {
       Object.keys(assets).forEach((key) => {
