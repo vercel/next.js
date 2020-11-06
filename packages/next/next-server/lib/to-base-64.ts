3c534c9e8ec2d4a6118af6eaa831d26ac4e94e06
Steven <steven@ceriously.com>
2020-11-06 18:34:23 -0500

    Fix typo

--- packages/next/next-server/lib/to-base-64.ts
+++ packages/next/next-server/lib/to-base-64.ts
@@ -1,5 +1,5 @@
 /**
- * Isomorphic base64 that works on the sever and client
+ * Isomorphic base64 that works on the server and client
  */
 export function toBase64(str: string) {
   if (typeof window === 'undefined') {
