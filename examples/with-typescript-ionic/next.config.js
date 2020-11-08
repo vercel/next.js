bc52f828d95b6fb6f92523636ad9400a9134f561
Hidetaka Okamoto <info@wp-kyoto.net>
2020-11-08 18:02:02 +0900

    docs: example remove invalid config

--- examples/with-typescript-ionic/next.config.js
+++ examples/with-typescript-ionic/next.config.js
@@ -1,13 +1,6 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    iconSizes: [],
-    domains: [],
-    path: '/_next/image',
-    loader: 'default',
-  },
   webpack: (config) => {
     config.plugins.push(
       new CopyPlugin({
