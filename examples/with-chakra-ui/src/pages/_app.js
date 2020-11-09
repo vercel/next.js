296f20e510485ed11325889ee613718560f46aad
Jesse Jafa <jesse.jafa@gmail.com>
2020-11-09 05:03:26 +0200

    Fixes with-chakra-ui example error (#18954)
    Resolves https://github.com/vercel/next.js/issues/18941

--- examples/with-chakra-ui/src/pages/_app.js
+++ examples/with-chakra-ui/src/pages/_app.js
@@ -5,7 +5,11 @@ import theme from '../theme'
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <ColorModeProvider>
+      <ColorModeProvider
+        options={{
+          useSystsemColorMode: true,
+        }}
+      >
         <CSSReset />
         <Component {...pageProps} />
       </ColorModeProvider>
