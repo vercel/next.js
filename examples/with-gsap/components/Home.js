04de69631cf5c4fa2c06477c5a6271fe2ffb76b8
Luis Alvarez <luis@vercel.com>
2020-11-09 15:03:41 -0500

    Removed react imports

--- examples/with-gsap/components/Home.js
+++ examples/with-gsap/components/Home.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import Title from './Title'
 import Content from './Content'
 
--- examples/with-gsap/components/Title.js
+++ examples/with-gsap/components/Title.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 
 const Title = ({ lineContent, lineContent2 }) => {
