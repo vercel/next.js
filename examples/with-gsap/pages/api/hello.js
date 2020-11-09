c2c49c871a4de05d3e4c821601e2daf39333ac78
Galanggg <tugasgalang@gmail.com>
2020-11-10 04:38:07 +0800

    Add `with-gsap` example (#17195)
    This PR adds an example Next.js project with GSAP
    ![example-next-gsap](https://user-images.githubusercontent.com/21099312/93552465-170dfe80-f9a3-11ea-978c-4e1dc01731e8.gif)

---
+++ examples/with-gsap/pages/api/hello.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.statusCode = 200
+  res.json({ name: 'John Doe' })
+}
