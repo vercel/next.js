1690f7144ef23944e352a31598b297792e30d75d
WeichienHung <weichien.hung@gmail.com>
2020-11-11 05:10:54 +0800

    Add mutation example to with-typescript-graphql (#16742)
    Fixes  https://github.com/vercel/next.js/issues/16550
    
    Add a graphql mutation operation in this example.
    The changes are:
    1. Define mutation in schema `type-defs.graphqls`
    2. Add mutation to `viewer.graphql ` that compiled to typescript code.
    3. Add mutation resolver in `resolvers.ts`
    4. [UI] add a input box and button to submit mutation request to change name. 
    
    screenshot of change
    ![mutation_example](https://user-images.githubusercontent.com/1462027/91797123-5ffc4e00-ec54-11ea-910b-3ddb399b42f0.gif)

--- examples/with-typescript-graphql/pages/index.tsx
+++ examples/with-typescript-graphql/pages/index.tsx
@@ -1,17 +1,58 @@
 import Link from 'next/link'
-import { useViewerQuery, ViewerDocument } from '../lib/viewer.graphql'
+import { useState } from 'react'
+import {
+  useViewerQuery,
+  useUpdateNameMutation,
+  ViewerDocument,
+} from '../lib/viewer.graphql'
 import { initializeApollo } from '../lib/apollo'
 
 const Index = () => {
   const { viewer } = useViewerQuery().data!
+  const [newName, setNewName] = useState('')
+  const [updateNameMutation] = useUpdateNameMutation()
+
+  const onChangeName = () => {
+    updateNameMutation({
+      variables: {
+        name: newName,
+      },
+      //Follow apollo suggestion to update cache
+      //https://www.apollographql.com/docs/angular/features/cache-updates/#update
+      update: (
+        store,
+        {
+          data: {
+            updateName: { name },
+          },
+        }
+      ) => {
+        // Read the data from our cache for this query.
+        const { viewer } = store.readQuery({ query: ViewerDocument })
+        const newViewer = { ...viewer }
+        // Add our comment from the mutation to the end.
+        newViewer.name = name
+        // Write our data back to the cache.
+        store.writeQuery({ query: ViewerDocument, data: { viewer: newViewer } })
+      },
+    })
+  }
 
   return (
     <div>
-      You're signed in as {viewer.name} and you're {viewer.status} go to the{' '}
+      You're signed in as {viewer.name} and you're {viewer.status}. Go to the{' '}
       <Link href="/about">
         <a>about</a>
       </Link>{' '}
       page.
+      <div>
+        <input
+          type="text"
+          placeholder="your new name..."
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <input type="button" value="change" onClick={onChangeName} />
+      </div>
     </div>
   )
 }
