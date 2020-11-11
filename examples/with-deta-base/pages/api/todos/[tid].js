633115fc60ab2eee3e25301280208d6b12b6b236
Max <maxeust@gmail.com>
2020-11-11 16:23:12 +0100

    export endpoints as named funcs

--- examples/with-deta-base/pages/api/todos/[tid].js
+++ examples/with-deta-base/pages/api/todos/[tid].js
@@ -1,26 +1,27 @@
-import { Deta } from 'deta';
+import { Deta } from 'deta'
 
-const deta = Deta(process.env.DETA_PROJECT_KEY);
+const deta = Deta(process.env.DETA_PROJECT_KEY)
 
-const base = deta.Base('todos');
+const base = deta.Base('todos')
 
-export default async (req, res) => {
-
-  let { body, method, query: { tid } } = req;
-  let respBody = {};
+const handler = async (req, res) => {
+  let {
+    body,
+    method,
+    query: { tid },
+  } = req
+  let respBody = {}
 
   if (method === 'PUT') {
-
-    body = JSON.parse(body);
-    respBody = await base.put(body);
-    res.statusCode = 200;
-
+    body = JSON.parse(body)
+    respBody = await base.put(body)
+    res.statusCode = 200
   } else if (method === 'DELETE') {
-
-    respBody = await base.delete(tid);
-    res.statusCode = 200;
-
+    respBody = await base.delete(tid)
+    res.statusCode = 200
   }
 
-  res.json(respBody);
+  res.json(respBody)
 }
+
+export default handler
--- examples/with-deta-base/pages/index.js
+++ examples/with-deta-base/pages/index.js
@@ -1,100 +1,115 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
-const ToDo = ({content, isCompleted, onChange, onDelete}) => {
-  const cards = ['card', 'card2', 'card3', 'card4', 'card5'];
+const ToDo = ({ content, isCompleted, onChange, onDelete }) => {
+  const cards = ['card', 'card2', 'card3', 'card4', 'card5']
   return (
-  <div className={styles[cards[Math.floor(Math.random() * cards.length)]]}>
-    <div className={styles.text} style={{ textDecoration: isCompleted ? "line-through" : "" }}>{content}</div>
-    <div className={styles.reverseWrapper}>
-      <input type="checkbox" className={styles.check} checked={isCompleted} onChange={onChange} />
-      <button className={styles.delBtn} onClick={onDelete}>&#10005;</button>
+    <div className={styles[cards[Math.floor(Math.random() * cards.length)]]}>
+      <div
+        className={styles.text}
+        style={{ textDecoration: isCompleted ? 'line-through' : '' }}
+      >
+        {content}
+      </div>
+      <div className={styles.reverseWrapper}>
+        <input
+          type="checkbox"
+          className={styles.check}
+          checked={isCompleted}
+          onChange={onChange}
+        />
+        <button className={styles.delBtn} onClick={onDelete}>
+          &#10005;
+        </button>
+      </div>
     </div>
-  </div>
   )
 }
 
 export default function Home() {
+  const [newContent, setNewContent] = useState('')
 
-  const [newContent, setNewContent] = useState('');
-
-  const [toDos, setToDos] = useState([]);
+  const [toDos, setToDos] = useState([])
 
   const getToDos = async () => {
-    const resp = await fetch('api/todos');
-    const toDos = await resp.json();
-    setToDos(toDos);
+    const resp = await fetch('api/todos')
+    const toDos = await resp.json()
+    setToDos(toDos)
   }
-  
-  const createToDo = async () => {
-    const resp = await fetch('api/todos', 
-      {
-        method: 'post', 
-        body: JSON.stringify({content: newContent})
-      }
-    );
-    await getToDos();
-  }
-  
-  const updateToDo = async (todo) => {
-    let newBody = { 
-      ...todo,
-      isCompleted: !todo.isCompleted
-    };
-    const resp = await fetch(`api/todos/${todo.key}`, 
-      {
-        method: 'put', 
-        body: JSON.stringify(newBody)
-      }
-    );
 
-    await getToDos();
+  const createToDo = async () => {
+    await fetch('api/todos', {
+      method: 'post',
+      body: JSON.stringify({ content: newContent }),
+    })
+    await getToDos()
   }
-  
+
+  const updateToDo = async (todo) => {
+    let newBody = {
+      ...todo,
+      isCompleted: !todo.isCompleted,
+    }
+    await fetch(`api/todos/${todo.key}`, {
+      method: 'put',
+      body: JSON.stringify(newBody),
+    })
+
+    await getToDos()
+  }
+
   const deleteToDo = async (tid) => {
-    const resp = fetch(`api/todos/${tid}`, {method: 'delete'});
-    setTimeout(getToDos, 300);
+    await fetch(`api/todos/${tid}`, { method: 'delete' })
+    setTimeout(getToDos, 300)
   }
 
   useEffect(() => {
-    getToDos();
+    getToDos()
   }, [])
 
-  const completed = toDos.filter(todo => todo.isCompleted);
-  const notCompleted = toDos.filter(todo => !todo.isCompleted);
+  const completed = toDos.filter((todo) => todo.isCompleted)
+  const notCompleted = toDos.filter((todo) => !todo.isCompleted)
   return (
     <div className={styles.container}>
       <Head>
         <title>deta + next.js</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" /> 
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <header className={styles.header}>
         <h2>
-        <a href="https://www.deta.sh">deta base</a> + <a href="https://nextjs.org">next.js</a> to dos
-          </h2>
+          <a href="https://www.deta.sh">deta base</a> +{' '}
+          <a href="https://nextjs.org">next.js</a> to dos
+        </h2>
       </header>
       <main className={styles.main}>
-
         <div className={styles.incomplete}>
           <div className={styles.firstRow}>
-              <div className={styles.title}>to dos</div>
-              <div className={styles.reverseWrapper}>
-                <input className={styles.inpt} onChange={e => setNewContent(e.target.value)}></input>
-                <button className={styles.addBtn} onClick={createToDo}>&#10011;</button>
-              </div>
+            <div className={styles.title}>to dos</div>
+            <div className={styles.reverseWrapper}>
+              <input
+                className={styles.inpt}
+                onChange={(e) => setNewContent(e.target.value)}
+              ></input>
+              <button className={styles.addBtn} onClick={createToDo}>
+                &#10011;
+              </button>
+            </div>
           </div>
           <div className={styles.scrolly}>
-            {notCompleted.map((todo, index) => 
-              <ToDo 
-                key={todo.key} 
-                content={`${index + 1}. ${todo.content}`} 
-                isCompleted={todo.isCompleted} 
+            {notCompleted.map((todo, index) => (
+              <ToDo
+                key={todo.key}
+                content={`${index + 1}. ${todo.content}`}
+                isCompleted={todo.isCompleted}
                 onChange={() => updateToDo(todo)}
                 onDelete={() => deleteToDo(todo.key)}
               />
-            )}
+            ))}
           </div>
         </div>
 
@@ -103,15 +118,15 @@ export default function Home() {
             <div className={styles.title}>done</div>
           </div>
           <div className={styles.scrolly}>
-            {completed.map((todo, index) => 
-              <ToDo 
-                key={todo.key} 
-                content={`${index + 1}. ${todo.content}`} 
+            {completed.map((todo, index) => (
+              <ToDo
+                key={todo.key}
+                content={`${index + 1}. ${todo.content}`}
                 isCompleted={todo.isCompleted}
                 onChange={() => updateToDo(todo)}
                 onDelete={() => deleteToDo(todo.key)}
               />
-            )}
+            ))}
           </div>
         </div>
       </main>
@@ -123,7 +138,8 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           powered by
-          <img src="/deta.svg" alt="Deta Logo" className={styles.logo} />&nbsp;deta & next.js
+          <img src="/deta.svg" alt="Deta Logo" className={styles.logo} />
+          &nbsp;deta & next.js
         </a>
       </footer>
     </div>
