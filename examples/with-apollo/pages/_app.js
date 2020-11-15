1c4216002394d9eb1439f5ee54768ee733b3e6f4
Yordis Prieto <yordis.prieto@gmail.com>
2020-11-15 18:04:22 -0500

    Add apollo state func (#19137)
    The intention is to show people the correlation between things, in this case, understand the usage of the same key from the props and the rehydration.
    
    Hopefully, this change will bring value as it has done with some Juniors already.

--- examples/with-apollo/pages/_app.js
+++ examples/with-apollo/pages/_app.js
@@ -2,7 +2,7 @@ import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apolloClient'
 
 export default function App({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
+  const apolloClient = useApollo(pageProps)
 
   return (
     <ApolloProvider client={apolloClient}>
