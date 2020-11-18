85f4eb4f765cf7813f9e479086a790a41b3db4be
Tobias Lundgren <tobias@lundgren.design>
2020-11-18 22:17:24 +0100

    Upgrade to Chakra UI v1

---
+++ examples/with-chakra-ui-typescript/src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
+
+export default class Document extends NextDocument {
+  static getInitialProps(ctx: DocumentContext) {
+    return NextDocument.getInitialProps(ctx)
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          {/* Make Color mode to persists when you refresh the page. */}
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
