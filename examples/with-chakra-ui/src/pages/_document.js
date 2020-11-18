ae43e96f53579748e96e7b515d4cfbbf6f9bbace
Tobias Lundgren <tobias@lundgren.design>
2020-11-18 22:41:21 +0100

    Update theming in Chakra UI example to match v1

---
+++ examples/with-chakra-ui/src/pages/_document.js
@@ -0,0 +1,22 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
+
+export default class Document extends NextDocument {
+  static getInitialProps(ctx) {
+    return NextDocument.getInitialProps(ctx)
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          {/* Make Color mode to persists when you refresh the page. */}
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- examples/with-chakra-ui/src/theme.js
+++ examples/with-chakra-ui/src/theme.js
@@ -1,19 +1,22 @@
-import { theme as chakraTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { ...chakraTheme.fonts, mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` }
 
-const breakpoints = ['40em', '52em', '64em']
+const breakpoints = createBreakpoints({
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  xl: '80em',
+})
 
 const theme = {
-  ...chakraTheme,
   colors: {
-    ...chakraTheme.colors,
     black: '#16161D',
   },
   fonts,
   breakpoints,
   icons: {
-    ...chakraTheme.icons,
     logo: {
       path: (
         <svg
@@ -35,4 +38,4 @@ const theme = {
   },
 }
 
-export default theme
+export default extendTheme(theme)
