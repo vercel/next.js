45125797a0395648ea21ae952a9297d6dcdadcfd
Tobias Lundgren <tobias@lundgren.design>
2020-11-18 22:41:46 +0100

    Fix icons

--- examples/with-chakra-ui/src/pages/index.js
+++ examples/with-chakra-ui/src/pages/index.js
@@ -2,12 +2,11 @@ import {
   Link as ChakraLink,
   Text,
   Code,
-  Icon,
   List,
   ListIcon,
   ListItem,
 } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
@@ -25,20 +24,20 @@ const Index = () => (
 
       <List spacing={3} my={0}>
         <ListItem>
-          <ListIcon icon="check-circle" color="green.500" />
+          <ListIcon as={CheckCircleIcon} color="green.500" />
           <ChakraLink
             isExternal
             href="https://chakra-ui.com"
             flexGrow={1}
             mr={2}
           >
-            Chakra UI <Icon as={ExternalLinkIcon} mx="2px" />
+            Chakra UI <LinkIcon />
           </ChakraLink>
         </ListItem>
         <ListItem>
-          <ListIcon icon="check-circle" color="green.500" />
+          <ListIcon as={CheckCircleIcon} color="green.500" />
           <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <Icon as={ExternalLinkIcon} mx="2px" />
+            Next.js <LinkIcon />
           </ChakraLink>
         </ListItem>
       </List>
