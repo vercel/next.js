5772d3c0956afc0cd0c08a809d348c793f377909
Luis Alvarez <luis@vercel.com>
2020-11-20 12:55:32 -0500

    lint fix

--- examples/with-tailwindcss/components/nav.js
+++ examples/with-tailwindcss/components/nav.js
@@ -11,7 +11,9 @@ export default function Nav() {
       <ul className="flex items-center justify-between p-8">
         <li>
           <Link href="/">
-            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">Home</a>
+            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+              Home
+            </a>
           </Link>
         </li>
         <ul className="flex items-center justify-between space-x-4">
