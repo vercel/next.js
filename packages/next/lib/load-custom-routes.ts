f94e4b8df08fc62f24d0c2571dfa470820603530
Connor Davis <mail@connordav.is>
2020-11-20 16:17:58 -0500

    Exclude well known files when trailing slash: true (#19364)
    * Exclude well known files when trailing slash: true
    
    * Update manifest test
    
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- packages/next/lib/load-custom-routes.ts
+++ packages/next/lib/load-custom-routes.ts
@@ -388,13 +388,13 @@ export default async function loadCustomRoutes(
   if (config.trailingSlash) {
     redirects.unshift(
       {
-        source: '/:file((?:[^/]+/)*[^/]+\\.\\w+)/',
+        source: '/:file((?!\\.well-known(?:/.*)?)(?:[^/]+/)*[^/]+\\.\\w+)/',
         destination: '/:file',
         permanent: true,
         locale: config.i18n ? false : undefined,
       },
       {
-        source: '/:notfile((?:[^/]+/)*[^/\\.]+)',
+        source: '/:notfile((?!\\.well-known(?:/.*)?)(?:[^/]+/)*[^/\\.]+)',
         destination: '/:notfile/',
         permanent: true,
         locale: config.i18n ? false : undefined,
