c9366427e811dfd0e615c8988245ccdc284e1d80
Pier-Luc Gendreau <Zertz@users.noreply.github.com>
2020-11-20 23:00:39 -0500

    Update with-mongodb to be TypeScript-friendly
    I slightly modified the approach so TypeScript can correctly infer types without actually having to type anything but the global:
    
    **index.d.ts**
    ```ts
    import { Db, MongoClient } from "mongodb";
    
    declare global {
      namespace NodeJS {
        interface Global {
          mongoCache: {
            conn: {
              client: MongoClient | null;
              db: Db | null;
            }
            promise: Promise<MongoClient> | null;
          };
        }
      }
    }
    ```

--- examples/with-mongodb/util/mongodb.js
+++ examples/with-mongodb/util/mongodb.js
@@ -20,26 +20,23 @@ if (!MONGODB_DB) {
  * during API Route usage.
  */
 let cached = global.mongo
-if (!cached) cached = global.mongo = {}
+if (!cached) cached = global.mongo = { conn: null, promise: null }
 
 export async function connectToDatabase() {
   if (cached.conn) return cached.conn
   if (!cached.promise) {
-    const conn = {}
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
     cached.promise = MongoClient.connect(MONGODB_URI, opts)
       .then((client) => {
-        conn.client = client
-        return client.db(MONGODB_DB)
-      })
-      .then((db) => {
-        conn.db = db
-        cached.conn = conn
+        return {
+          client,
+          db: client.db(MONGODB_DB),
+        }
       })
   }
-  await cached.promise
+  cached.conn = await cached.promise
   return cached.conn
 }
