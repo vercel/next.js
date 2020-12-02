ef845d33f1986f80220d2ed15f0a82e1567984d7
Nick Carchedi <nick.carchedi@gmail.com>
2020-12-02 10:27:45 -0700

    tidy up class orders

--- examples/with-tailwindcss/pages/index.js
+++ examples/with-tailwindcss/pages/index.js
@@ -2,36 +2,36 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className='min-h-screen py-2 flex flex-col justify-center items-center'>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='px-20 flex-1 flex flex-col justify-center items-center text-center'>
+      <main className='flex flex-col items-center justify-center flex-1 px-20 text-center'>
         <h1 className='text-6xl font-bold'>
           Welcome to <a className='text-blue-600' href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className='text-2xl mt-3'>
+        <p className='mt-3 text-2xl'>
           Get started by editing{' '}
-          <code className='bg-gray-100 rounded-md p-3 text-lg font-mono'>pages/index.js</code>
+          <code className='p-3 font-mono text-lg bg-gray-100 rounded-md'>pages/index.js</code>
         </p>
 
-        <div className='flex items-center justify-around flex-wrap mt-6 max-w-4xl sm:w-full'>
-          <a href="https://nextjs.org/docs" className='w-96 mt-6 p-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600'>
+        <div className='flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full'>
+          <a href="https://nextjs.org/docs" className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'>
             <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
             <p className='mt-4 text-xl'>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className='w-96 mt-6 p-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600'>
+          <a href="https://nextjs.org/learn" className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'>
             <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
             <p className='mt-4 text-xl'>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className='w-96 mt-6 p-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600'
+            className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
           >
             <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
             <p className='mt-4 text-xl'>Discover and deploy boilerplate example Next.js projects.</p>
@@ -39,7 +39,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className='w-96 mt-6 p-6 text-left border rounded-xl hover:text-blue-600 focus:text-blue-600'
+            className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
           >
             <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
             <p className='mt-4 text-xl'>
@@ -49,9 +49,9 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className='w-full h-24 border-t flex justify-center items-center'>
+      <footer className='flex items-center justify-center w-full h-24 border-t'>
         <a
-          className='flex justify-center items-center'
+          className='flex items-center justify-center'
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
