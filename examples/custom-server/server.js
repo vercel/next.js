5188d33376a80e583162bcfa5fa574a8e2bbedc2
Giovanni Giordano <giordano.giovanni1993@gmail.com>
2020-12-06 10:31:39 +0100

    Update with-custom-server example
    Fix url parse issue

--- examples/custom-server/server.js
+++ examples/custom-server/server.js
@@ -8,7 +8,9 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = new URL(req.url, 'http://w.w')
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
 
     if (pathname === '/a') {
