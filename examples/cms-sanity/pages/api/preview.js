41ed49250f84d01deeaec92b55f08297135c5d99
Espen Hovlandsdal <espen@hovlandsdal.com>
2020-12-17 13:24:04 +0100

    [cms-sanity] Use slug from post instead of query params

--- examples/cms-sanity/pages/api/preview.js
+++ examples/cms-sanity/pages/api/preview.js
@@ -26,6 +26,6 @@ export default async function preview(req, res) {
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/posts/${req.query.slug}` })
+  res.writeHead(307, { Location: `/posts/${post.slug}` })
   res.end()
 }
