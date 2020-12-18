3dd7037b2150431713af917e7edc8c0fa848de5b
Long Ho <holevietlong@gmail.com>
2020-12-18 00:19:49 -0500

    chore: upgrade react-intl example to next.js 10 part of #20290

---
+++ examples/with-react-intl/next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+module.exports = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'en-GB', 'fr', 'zh-Hans-CN', 'zh-Hant-HK'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+  },
+};
