81b1c8b19940433bea959ebae89bbbe0a7d39eea
MMT-LD <liam.davers.work@gmail.com>
2020-12-20 19:25:01 +0000

    feature/vanilla-emotion - fixes to pr based on comments

--- examples/with-emotion-vanilla/pages/_app.js
+++ examples/with-emotion-vanilla/pages/_app.js
@@ -1,18 +1,3 @@
-import * as React from 'react'
-import { hydrate } from '@emotion/css'
-
-// Adds server generated styles to emotion cache.
-// this needs to come before the app mounts
-// We use query selector here as setting on window results in emotion error `Cannot read property 'forEach' of undefined`
-if (typeof window !== 'undefined') {
-  const ids = JSON.parse(
-    document
-      .querySelector('[data-emotion-ssr]')
-      .getAttribute('data-emotion-ssr')
-  )
-  hydrate(ids)
-}
-
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
--- examples/with-emotion-vanilla/pages/_document.js
+++ examples/with-emotion-vanilla/pages/_document.js
@@ -1,10 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import * as React from 'react'
 import { renderStatic } from '../shared/renderer'
-
-// Please note: - the reason for data-emotion-ssr={JSON.stringify(ids)} is because
-// hydrate can't find the ids if we set them using window.__ids
-// adding this and looking for it in _app.js allows it to always be ready so we just parse that attribute to get the ids array
 export default class AppDocument extends Document {
   static async getInitialProps(ctx) {
     const page = await ctx.renderPage()
