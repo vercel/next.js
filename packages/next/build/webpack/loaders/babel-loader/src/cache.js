8ec99a3419d7f1fe26a73d584037d9b56a7d2a11
Tim Neutkens <timneutkens@me.com>
2020-12-23 18:20:04 +0100

    Use cacache (same as terser-plugin)

--- packages/next/build/webpack/loaders/babel-loader/src/cache.js
+++ packages/next/build/webpack/loaders/babel-loader/src/cache.js
@@ -1,98 +1,45 @@
-/**
- * Filesystem Cache
- *
- * Given a file and a transform function, cache the result into files
- * or retrieve the previously cached files if the given file is already known.
- *
- * @see https://github.com/babel/babel-loader/issues/34
- * @see https://github.com/babel/babel-loader/pull/41
- */
-import { promises as fs } from 'fs'
-import { join } from 'path'
 import { createHash } from 'crypto'
-import { tracer, traceAsyncFn, traceFn } from '../../../../tracer'
+import { tracer, traceAsyncFn } from '../../../../tracer'
 import transform from './transform'
+import cacache from 'next/dist/compiled/cacache'
 
-/**
- * Read the contents from the compressed file.
- *
- * @async
- * @params {String} filename
- * @params {Boolean} compress
- */
-const read = async function (filename) {
-  const data = await traceAsyncFn(
+async function read(cacheDirectory, etag) {
+  const cachedResult = await traceAsyncFn(
     tracer.startSpan('read-cache-file'),
-    async () => await fs.readFile(filename)
+    async () => await cacache.get(cacheDirectory, etag)
   )
 
-  return traceFn(tracer.startSpan('parse-data'), () =>
-    JSON.parse(data.toString())
-  )
+  return JSON.parse(cachedResult.data)
 }
 
-/**
- * Write contents into a compressed file.
- *
- * @async
- * @params {String} filename
- * @params {Boolean} compress
- * @params {String} result
- */
-const write = async function (filename, result) {
-  const content = JSON.stringify(result)
-
-  return await fs.writeFile(filename, content)
+function write(cacheDirectory, etag, data) {
+  return cacache.put(cacheDirectory, etag, JSON.stringify(data))
 }
 
-/**
- * Build the filename for the cached file
- *
- * @params {String} source  File source code
- * @params {Object} options Options used
- *
- * @return {String}
- */
-const filename = function (source, identifier, options) {
+const etag = function (source, identifier, options) {
   const hash = createHash('md4')
 
   const contents = JSON.stringify({ source, options, identifier })
 
   hash.update(contents)
 
-  return hash.digest('hex') + '.json'
+  return hash.digest('hex')
 }
 
-/**
- * Handle the cache
- *
- * @params {String} directory
- * @params {Object} params
- */
 export default async function handleCache(params) {
   const span = tracer.startSpan('handle-cache')
   return traceAsyncFn(span, async () => {
     const { source, options = {}, cacheIdentifier, cacheDirectory } = params
 
-    const file = join(
-      cacheDirectory,
-      filename(source, cacheIdentifier, options)
-    )
+    const file = etag(source, cacheIdentifier)
 
     try {
       // No errors mean that the file was previously cached
       // we just need to return it
-      const res = await read(file)
+      const res = await read(cacheDirectory, file)
       span.setAttribute('cache', res ? 'HIT' : 'MISS')
       return res
     } catch (err) {}
-
-    // Make sure the directory exists.
-    try {
-      await fs.mkdir(cacheDirectory, { recursive: true })
-    } catch (err) {
-      throw err
-    }
 
     // Otherwise just transform the file
     // return it to the user asap and write it in cache
@@ -103,11 +50,7 @@ export default async function handleCache(params) {
       }
     )
 
-    try {
-      await write(file, result)
-    } catch (err) {
-      throw err
-    }
+    await write(cacheDirectory, file, result)
 
     return result
   })
