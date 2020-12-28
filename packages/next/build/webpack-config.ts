0d2e97cd6912f6322ecc469d0dd490a79ef449a4
JJ Kasper <jj@jjsweb.site>
2020-12-28 16:04:51 -0600

    Remove import type syntax from core files (#20379)
    This removes `import type` usage from our core files since `import type` requires a higher TypeScript version than currently expected.
    
    Fixes: https://github.com/vercel/next.js/issues/19300

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -8,7 +8,7 @@ import semver from 'next/dist/compiled/semver'
 import TerserPlugin from './webpack/plugins/terser-webpack-plugin/src/index.js'
 import path from 'path'
 import webpack from 'webpack'
-import type { Configuration } from 'webpack'
+import { Configuration } from 'webpack'
 import {
   DOT_NEXT_ALIAS,
   NEXT_PROJECT_ROOT,
--- packages/next/server/hot-middleware.ts
+++ packages/next/server/hot-middleware.ts
@@ -21,8 +21,8 @@
 // CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-import type webpack from 'webpack'
-import type http from 'http'
+import webpack from 'webpack'
+import http from 'http'
 
 export class WebpackHotMiddleware {
   eventStream: EventStream
