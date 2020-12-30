f81795f1e279fb4b33fb44ac063a50a9cd7f3087
Luis Alvarez <luis@vercel.com>
2020-12-30 10:49:01 -0500

    Lint fix and minor changes

--- examples/with-mongodb-mongoose/utils/dbConnect.js
+++ examples/with-mongodb-mongoose/utils/dbConnect.js
@@ -1,14 +1,13 @@
-/* This is a database connection function*/
 import mongoose from 'mongoose'
 
 async function dbConnect() {
-  /* check if we have connection to our database or database is currently connecting or disconnecting (readyState 1, 2 and 3) */
+  // check if we have a connection to the database or if it's currently
+  // connecting or disconnecting (readyState 1, 2 and 3)
   if (mongoose.connection.readyState >= 1) {
-    return;
+    return
   }
 
-  /* connecting to our database */
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
+  return mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
