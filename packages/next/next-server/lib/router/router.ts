1c388387a6cd629b073e0ae600707c47701e9b76
JJ Kasper <jj@jjsweb.site>
2020-12-29 21:37:16 -0600

    Ensure index basePath is detected correctly

--- packages/next/next-server/lib/router/router.ts
+++ packages/next/next-server/lib/router/router.ts
@@ -92,7 +92,13 @@ export function delLocale(path: string, locale?: string) {
 }
 
 export function hasBasePath(path: string): boolean {
-  return path === basePath || path.startsWith(basePath + '/')
+  const queryIndex = path.indexOf('?')
+  const hashIndex = path.indexOf('#')
+
+  if (queryIndex > -1 || hashIndex > -1) {
+    path = path.substring(0, queryIndex > -1 ? queryIndex : hashIndex)
+  }
+  return path === basePath || path.startsWith(basePath)
 }
 
 export function addBasePath(path: string): string {
