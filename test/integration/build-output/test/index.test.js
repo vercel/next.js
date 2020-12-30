2868ab5594254bc5f581c845fd4aad337ce35e7a
Joe Haddad <joe.haddad@zeit.co>
2020-12-30 10:24:20 -0500

    update size test

--- test/integration/build-output/test/index.test.js
+++ test/integration/build-output/test/index.test.js
@@ -101,7 +101,7 @@ describe('Build Output', () => {
       expect(parseFloat(err404Size) - 3.7).toBeLessThanOrEqual(0)
       expect(err404Size.endsWith('kB')).toBe(true)
 
-      expect(parseFloat(err404FirstLoad) - 65.2).toBeLessThanOrEqual(0)
+      expect(parseFloat(err404FirstLoad)).toBeCloseTo(65.3, 1)
       expect(err404FirstLoad.endsWith('kB')).toBe(true)
 
       expect(parseFloat(sharedByAll) - 61.8).toBeLessThanOrEqual(0)
