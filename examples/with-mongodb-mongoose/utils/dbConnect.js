fd32f3c262f2f70fb73f3d416bc3a83f8bf9a40d
mustafaKamal-fe <54776055+mustafaKamal-fe@users.noreply.github.com>
2020-12-30 23:18:32 +0300

    Update dbConnect.js (#19644)
    * Update dbConnect.js
    
    The previous logic was in my opinion prone to errors in situations when different calls to this function from different locations may happen at the same time ( like a race condition  ). If the readyState is anything but not 0, then we must not try to perform a new connection as this may cause consequent connections!
    
    I hope this makes sense
    
    * Update examples/with-mongodb-mongoose/utils/dbConnect.js
    
    * Lint fix and minor changes
    
    Co-authored-by: Luis Alvarez D <luis@vercel.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- examples/with-mongodb-mongoose/utils/dbConnect.js
+++ examples/with-mongodb-mongoose/utils/dbConnect.js
@@ -1,22 +1,17 @@
-/* This is a database connection function*/
 import mongoose from 'mongoose'
 
-const connection = {} /* creating connection object*/
-
 async function dbConnect() {
-  /* check if we have connection to our databse*/
-  if (connection.isConnected) {
+  // check if we have a connection to the database or if it's currently
+  // connecting or disconnecting (readyState 1, 2 and 3)
+  if (mongoose.connection.readyState >= 1) {
     return
   }
 
-  /* connecting to our database */
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
+  return mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-
-  connection.isConnected = db.connections[0].readyState
 }
 
 export default dbConnect
