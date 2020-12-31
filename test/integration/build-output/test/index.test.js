7fb0f680930cfaccaff9ab49c5bb80ee93d6f9d8
JJ Kasper <jj@jjsweb.site>
2020-12-30 22:10:39 -0600

    Update test

--- test/integration/build-output/test/index.test.js
+++ test/integration/build-output/test/index.test.js
@@ -104,7 +104,7 @@ describe('Build Output', () => {
       expect(parseFloat(err404FirstLoad)).toBeCloseTo(65.3, 1)
       expect(err404FirstLoad.endsWith('kB')).toBe(true)
 
-      expect(parseFloat(sharedByAll) - 61.8).toBeLessThanOrEqual(0)
+      expect(parseFloat(sharedByAll) - 61.9).toBeLessThanOrEqual(0)
       expect(sharedByAll.endsWith('kB')).toBe(true)
 
       if (_appSize.endsWith('kB')) {
