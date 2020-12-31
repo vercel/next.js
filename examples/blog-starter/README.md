e0f601426f75fcb482b80e0af3eccaa1d7a01135
Greg Rickaby <gregrickaby@gmail.com>
2020-12-30 18:04:23 -0600

    Update blog starter example (#19698)
    * bump dependencies to latest versions
    
    * use tailwindcss class names
    
    * use next/image component
    
    * pass in height & width. update tailwindcss classes
    
    * update tailwindcss classes
    
    * mention tailwindcss 2.0

--- examples/blog-starter/README.md
+++ examples/blog-starter/README.md
@@ -48,4 +48,4 @@ Deploy it to the cloud with [Vercel](https://vercel.com/import?filter=next.js&ut
 
 # Notes
 
-This blog-starter uses [Tailwind CSS](https://tailwindcss.com). To control the generated stylesheet's filesize, this example uses Tailwind CSS' v1.4 [`purge` option](https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css) to remove unused CSS.
+This blog-starter uses [Tailwind CSS](https://tailwindcss.com). To control the generated stylesheet's filesize, this example uses Tailwind CSS' v2.0 [`purge` option](https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css) to remove unused CSS.
--- examples/blog-starter/components/hero-post.js
+++ examples/blog-starter/components/hero-post.js
@@ -14,9 +14,15 @@ export default function HeroPost({
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
+        <CoverImage
+          title={title}
+          src={coverImage}
+          slug={slug}
+          height={620}
+          width={1240}
+        />
       </div>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
