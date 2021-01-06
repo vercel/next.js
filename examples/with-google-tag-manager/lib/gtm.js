9250b92b0a88820ad343e8fb95962285f103f464
Bar <bar.isyo@gmail.com>
2021-01-06 13:15:26 -0500

    Update gtm.js (#20807)

--- examples/with-google-tag-manager/lib/gtm.js
+++ examples/with-google-tag-manager/lib/gtm.js
@@ -1,7 +1,7 @@
 export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
 export const pageview = (url) => {
-  window.dataLayer({
+  window.dataLayer.push({
     event: 'pageview',
     page: url,
   })
