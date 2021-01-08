d2c45567dba4ee9383dcdb4374677dc7c83880aa
Tim Neutkens <timneutkens@me.com>
2021-01-08 14:40:01 +0100

    Update instrumentation to not batch out of band

--- bench/instrument.js
+++ bench/instrument.js
@@ -1,11 +1,11 @@
 const { NodeTracerProvider } = require('@opentelemetry/node')
-const { BatchSpanProcessor } = require('@opentelemetry/tracing')
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing')
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
 
 const tracerProvider = new NodeTracerProvider()
 
 tracerProvider.addSpanProcessor(
-  new BatchSpanProcessor(
+  new SimpleSpanProcessor(
     new ZipkinExporter({
       serviceName: 'next-js',
     })
