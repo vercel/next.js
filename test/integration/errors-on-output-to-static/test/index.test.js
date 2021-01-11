f860c3b5c24989b772a9b91510e9a5effedf7f08
Tim Neutkens <timneutkens@me.com>
2021-01-11 13:26:35 +0100

    Remove extra test

--- test/integration/errors-on-output-to-static/test/index.test.js
+++ test/integration/errors-on-output-to-static/test/index.test.js
@@ -9,15 +9,6 @@ const appDir = path.join(__dirname, '..')
 const nextConfig = path.join(appDir, 'next.config.js')
 
 describe('Errors on output to static', () => {
-  it('Throws error when `distDir` is set to static', async () => {
-    await fs.writeFile(nextConfig, `module.exports = { distDir: 'static' }`)
-    const results = await nextBuild(appDir, [], { stdout: true, stderr: true })
-    expect(results.stdout + results.stderr).toMatch(
-      /The 'static' directory is reserved in Next\.js and can not be set as/
-    )
-    await fs.remove(nextConfig)
-  })
-
   it('Throws error when export out dir is static', async () => {
     await fs.remove(nextConfig)
     await nextBuild(appDir)
