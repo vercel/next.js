a587456ece6e463234815f8c2123ea231f2bceb7
Matt Sacks <github@mattsacks.email>
2021-01-11 12:26:07 -0800

    Allow webpack-bundle-analyzer options to be passed through
    Configured as:
    
    ```js
    const withBundleAnalyzer = require('@next/bundle-analyzer')({
      enabled: process.env.ANALYZE === 'true',
      defaultSizes: 'stat',
      openAnalyzer: false,
    });
    ```

--- packages/next-bundle-analyzer/index.js
+++ packages/next-bundle-analyzer/index.js
@@ -1,10 +1,11 @@
-module.exports = ({ enabled = true } = {}) => (nextConfig = {}) => {
+module.exports = ({ enabled = true, ...bundleAnalyzerOptions } = {}) => (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       if (enabled) {
         const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
         config.plugins.push(
           new BundleAnalyzerPlugin({
+            ...bundleAnalyzerOptions,
             analyzerMode: 'static',
             reportFilename: options.isServer
               ? '../analyze/server.html'
