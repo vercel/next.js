7226ee4c1fb21c223abd94176a23f2812111fd2f
Guy Bedford <guybedford@gmail.com>
2021-01-10 22:45:56 -0500

    adjust build output

--- test/integration/build-output/test/index.test.js
+++ test/integration/build-output/test/index.test.js
@@ -168,7 +168,7 @@ describe('Build Output', () => {
       expect(parseFloat(indexSize)).toBeGreaterThanOrEqual(2)
       expect(indexSize.endsWith('kB')).toBe(true)
 
-      expect(parseFloat(indexFirstLoad)).toBeLessThanOrEqual(65)
+      expect(parseFloat(indexFirstLoad)).toBeLessThanOrEqual(65.2)
       expect(parseFloat(indexFirstLoad)).toBeGreaterThanOrEqual(60)
       expect(indexFirstLoad.endsWith('kB')).toBe(true)
     })
