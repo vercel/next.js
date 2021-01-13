82fcb811bcb8591f511f44dd4cedce590c447618
Ari Freyr Asgeirsson <haugurjr@gmail.com>
2021-01-13 16:15:11 +0000

    Wrong index path revalidation timer (#20818)
    Hello friends
    
    Ran into this bug on our production site, prerenderManifest stores revalidation info for the index as `"/": { .. }`, but the code tries to access this information as `"/index"`.
    
    This leads to our index page always having s-max-age: 1

--- test/integration/prerender/test/index.test.js
+++ test/integration/prerender/test/index.test.js
@@ -58,7 +58,7 @@ const startServer = async (optEnv = {}) => {
 const expectedManifestRoutes = () => ({
   '/': {
     dataRoute: `/_next/data/${buildId}/index.json`,
-    initialRevalidateSeconds: 1,
+    initialRevalidateSeconds: 2,
     srcRoute: null,
   },
   '/blog/[post3]': {
@@ -520,6 +520,15 @@ const runTests = (dev = false, isEmulatedServerless = false) => {
     )
     expect(data.pageProps.post).toBe('post-3')
   })
+
+  if (!dev) {
+    it('should use correct caching headers for a revalidate page', async () => {
+      const initialRes = await fetchViaHTTP(appPort, '/')
+      expect(initialRes.headers.get('cache-control')).toBe(
+        's-maxage=2, stale-while-revalidate'
+      )
+    })
+  }
 
   it('should navigate to a normal page and back', async () => {
     const browser = await webdriver(appPort, '/')
