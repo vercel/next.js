47ce7907c1816e2f8b7cad9eb4a93130f9f21d67
Ludovico Fischer <livrerie@gmail.com>
2021-01-14 18:24:04 +0100

    fix(test): import writeFile from 'fs-extra' in TypeScript config test
    writeFile from 'fs' requires a callback parameter since node 10
    (see https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback)
    other tests import writeFile from 'fs-extra'
    I do not understand why this test ran before

--- test/integration/tsconfig-verifier/test/index.test.js
+++ test/integration/tsconfig-verifier/test/index.test.js
@@ -1,7 +1,6 @@
 /* eslint-env jest */
 
-import { writeFile } from 'fs'
-import { createFile, exists, readFile, remove } from 'fs-extra'
+import { createFile, exists, readFile, remove, writeFile } from 'fs-extra'
 import { nextBuild } from 'next-test-utils'
 import path from 'path'
 
