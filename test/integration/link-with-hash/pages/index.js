cdab7bf83191c055c364af7ce964afa820da9f15
kaykdm <34934746+kaykdm@users.noreply.github.com>
2021-01-16 01:53:10 +0900

    fix hydration mismatch on href for url with anchor refs (#21065)
    Co-authored-by: Tim Neutkens <tim@timneutkens.nl>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ test/integration/link-with-hash/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Home = () => {
+  return (
+    <>
+      <Link href="#hash-link">
+        <a>Hash Link</a>
+      </Link>
+    </>
+  )
+}
+
+export default Home
