ed31e67e7c55795ec834ac3376a77036640659b8
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-01-19 10:01:25 +0100

    Add failing e2e test for styled-jsx

--- test-pnp.sh
+++ test-pnp.sh
@@ -5,6 +5,7 @@ declare -a testCases=(
   "with-mdx"
   # Tests babel config
   "with-styled-components"
+  "with-styled-jsx"
 )
 
 set -e
@@ -12,7 +13,7 @@ set -e
 # Speeds up testing locally
 export CI=1
 
-rm -rf ./e2e-tests
+rm -rf /tmp/e2e-tests
 
 initialDir=$(pwd)
 
@@ -21,15 +22,15 @@ do
   cd $initialDir
 
   echo "--- Testing $testCase ---"
-  mkdir -p "./e2e-tests/$testCase"
-  cp -r "./examples/$testCase/." "./e2e-tests/$testCase"
-  cd "./e2e-tests/$testCase"
+  mkdir -p "/tmp/e2e-tests/$testCase"
+  cp -r "./examples/$testCase/." "/tmp/e2e-tests/$testCase"
+  cd "/tmp/e2e-tests/$testCase"
 
   touch yarn.lock
   yarn set version berry
   yarn config set pnpFallbackMode none
   yarn config set enableGlobalCache true
-  yarn link --all --private -r ../..
+  yarn link --all --private -r "$initialDir"
 
   yarn build
 done
