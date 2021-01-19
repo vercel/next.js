9323e536683181f32edb18888f1a9985037d0574
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2021-01-19 21:10:56 +0800

    Update Supabase example. (#21200)

--- examples/with-supabase-auth-realtime-db/pages/index.js
+++ examples/with-supabase-auth-realtime-db/pages/index.js
@@ -1,87 +1,121 @@
-import { useState, useContext } from 'react'
-import UserContext from 'lib/UserContext'
-import { createClient } from '@supabase/supabase-js'
+import Link from 'next/link'
+import useSWR from 'swr'
+import { Auth, Card, Typography, Space, Button, Icon } from '@supabase/ui'
+import { supabase } from '../utils/initSupabase'
+import { useEffect, useState } from 'react'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-)
+const fetcher = (url, token) =>
+  fetch(url, {
+    method: 'GET',
+    headers: new Headers({ 'Content-Type': 'application/json', token }),
+    credentials: 'same-origin',
+  }).then((res) => res.json())
 
-const Home = () => {
-  const { signIn } = useContext(UserContext)
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+const Index = () => {
+  const { user, session } = Auth.useUser()
+  const { data, error } = useSWR(
+    session ? ['/api/getUser', session.access_token] : null,
+    fetcher
+  )
+  const [authView, setAuthView] = useState('sign_in')
 
-  const handleLogin = async (type, username, password) => {
-    try {
-      const {
-        body: { user },
-      } =
-        type === 'LOGIN'
-          ? await supabase.auth.login(username, password)
-          : await supabase.auth.signup(username, password)
-      if (!!user) signIn(user.id, user.email)
-    } catch (error) {
-      console.log('error', error)
-      alert(error.error_description || error)
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (event === 'PASSWORD_RECOVERY') setAuthView('update_password')
+        if (event === 'USER_UPDATED')
+          setTimeout(() => setAuthView('sign_in'), 1000)
+        // Send session to /api/auth route to set the auth cookie.
+        // NOTE: this is only needed if you're doing SSR (getServerSideProps)!
+        fetch('/api/auth', {
+          method: 'POST',
+          headers: new Headers({ 'Content-Type': 'application/json' }),
+          credentials: 'same-origin',
+          body: JSON.stringify({ event, session }),
+        }).then((res) => res.json())
+      }
+    )
+
+    return () => {
+      authListener.unsubscribe()
     }
+  }, [])
+
+  const View = () => {
+    if (!user)
+      return (
+        <Space direction="vertical" size={8}>
+          <div>
+            <img
+              src="https://app.supabase.io/img/supabase-dark.svg"
+              width="96"
+            />
+            <Typography.Title level={3}>
+              Welcome to Supabase Auth
+            </Typography.Title>
+          </div>
+          <Auth
+            supabaseClient={supabase}
+            providers={['google', 'github']}
+            view={authView}
+            socialLayout="horizontal"
+            socialButtonSize="xlarge"
+          />
+        </Space>
+      )
+
+    return (
+      <Space direction="vertical" size={6}>
+        {authView === 'update_password' && (
+          <Auth.UpdatePassword supabaseClient={supabase} />
+        )}
+        {user && (
+          <>
+            <Typography.Text>You're signed in</Typography.Text>
+            <Typography.Text strong>Email: {user.email}</Typography.Text>
+
+            <Button
+              icon={<Icon type="LogOut" />}
+              type="outline"
+              onClick={() => supabase.auth.signOut()}
+            >
+              Log out
+            </Button>
+            {error && (
+              <Typography.Text danger>Failed to fetch user!</Typography.Text>
+            )}
+            {data && !error ? (
+              <>
+                <Typography.Text type="success">
+                  User data retrieved server-side (in API route):
+                </Typography.Text>
+
+                <Typography.Text>
+                  <pre>{JSON.stringify(data, null, 2)}</pre>
+                </Typography.Text>
+              </>
+            ) : (
+              <div>Loading...</div>
+            )}
+
+            <Typography.Text>
+              <Link href="/profile">
+                <a>SSR example with getServerSideProps</a>
+              </Link>
+            </Typography.Text>
+          </>
+        )}
+      </Space>
+    )
   }
 
   return (
-    <div className="w-full h-full flex justify-center items-center p-4 bg-gray-300">
-      <div className="w-full sm:w-1/2 xl:w-1/3">
-        <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg bg-white">
-          <div className="mb-4">
-            <label className="font-bold text-grey-darker block mb-2">
-              Email
-            </label>
-            <input
-              type="text"
-              className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-              placeholder="Your Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label className="font-bold text-grey-darker block mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-              placeholder="Your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <a
-              onClick={(e) => {
-                e.preventDefault()
-                handleLogin('SIGNUP', username, password)
-              }}
-              href={'/channels'}
-              className="bg-indigo-700 hover:bg-teal text-white py-2 px-4 rounded text-center transition duration-150 hover:bg-indigo-600 hover:text-white"
-            >
-              Sign up
-            </a>
-            <a
-              onClick={(e) => {
-                e.preventDefault()
-                handleLogin('LOGIN', username, password)
-              }}
-              href={'/channels'}
-              className="border border-indigo-700 text-indigo-700 py-2 px-4 rounded w-full text-center transition duration-150 hover:bg-indigo-700 hover:text-white"
-            >
-              Login
-            </a>
-          </div>
-        </div>
-      </div>
+    <div style={{ maxWidth: '420px', margin: '96px auto' }}>
+      <Card>
+        <View />
+      </Card>
     </div>
   )
 }
 
-export default Home
+export default Index
