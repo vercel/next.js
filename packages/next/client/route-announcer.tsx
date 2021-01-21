8adf039134681c2dfa89cd45ac466e5bbf3812fc
Joe Haddad <joe.haddad@zeit.co>
2021-01-21 12:14:01 -0500

    Fix import

--- packages/next/client/route-announcer.tsx
+++ packages/next/client/route-announcer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from './router'
 
 export function RouteAnnouncer() {
   const router = useRouter()
