7718be13eada8c1346c4f27a13ed60188ee4f8f1
Hanakla / ragg <ragg@pixiv.com>
2021-01-24 00:07:46 +0900

    Add getInitialProp example case

--- examples/with-fleur/pages/index.tsx
+++ examples/with-fleur/pages/index.tsx
@@ -1,5 +1,21 @@
+import { FleurishNextPageContext, serializeContext } from '@fleur/next'
+import { NextPage } from 'next'
 import { Page } from '../components/page'
+import { TimerOps } from '../domains/timer'
+import { FleurSSProps } from '../lib/fleur'
 
-export default function Index() {
+const Index: NextPage<{}, FleurSSProps> = () => {
   return <Page />
 }
+
+Index.getInitialProps = async (context: FleurishNextPageContext) => {
+  await context.executeOperation(TimerOps.increment)
+  await context.executeOperation(TimerOps.tick, {
+    light: false,
+    lastUpdate: Date.now(),
+  })
+
+  return { __FLEUR_STATE__: serializeContext(context.fleurContext) }
+}
+
+export default Index
