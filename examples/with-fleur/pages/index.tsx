1d7bf7f100f63b0060e4aed523ad8466cafb9adc
Hanakla / ragg <ragg@pixiv.com>
2021-01-24 00:42:43 +0900

    Fix getInitialProps style
    To prevent App.getInitialProps from interfering with staticization

--- examples/with-fleur/pages/index.tsx
+++ examples/with-fleur/pages/index.tsx
@@ -1,21 +1,23 @@
-import { FleurishNextPageContext, serializeContext } from '@fleur/next'
+import { serializeContext } from '@fleur/next'
 import { NextPage } from 'next'
 import { Page } from '../components/page'
 import { TimerOps } from '../domains/timer'
-import { FleurSSProps } from '../lib/fleur'
+import { FleurSSProps, getOrCreateFleurContext } from '../lib/fleur'
 
 const Index: NextPage<{}, FleurSSProps> = () => {
   return <Page />
 }
 
-Index.getInitialProps = async (context: FleurishNextPageContext) => {
-  await context.executeOperation(TimerOps.increment)
-  await context.executeOperation(TimerOps.tick, {
+Index.getInitialProps = async () => {
+  const fleurContext = getOrCreateFleurContext()
+
+  await fleurContext.executeOperation(TimerOps.increment)
+  await fleurContext.executeOperation(TimerOps.tick, {
     light: false,
     lastUpdate: Date.now(),
   })
 
-  return { __FLEUR_STATE__: serializeContext(context.fleurContext) }
+  return { __FLEUR_STATE__: serializeContext(fleurContext) }
 }
 
 export default Index
