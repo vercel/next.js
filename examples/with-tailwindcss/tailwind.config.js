eacaf946537b078b59f543dd78837e2ff6b31ded
Tim Neutkens <tim@timneutkens.nl>
2021-01-25 12:17:48 +0100

    Update examples/with-tailwindcss/tailwind.config.js

--- examples/with-tailwindcss/tailwind.config.js
+++ examples/with-tailwindcss/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
