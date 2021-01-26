c67c796e471d2c32ef58860d68432b7227f997f2
= <lucas.f.dib@gmail.com>
2021-01-26 15:27:54 -0300

    added _app.tsx on ~/pages for global stylization

---
+++ examples/with-typescript-styled-components/pages/_app.tsx
@@ -0,0 +1,30 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  * {
+    font-family: 'Open Sans', sans-serif;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
--- examples/with-typescript-styled-components/pages/index.tsx
+++ examples/with-typescript-styled-components/pages/index.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 const Title = styled.h1`
-  color: red;
   font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
 `
 
 export default function Home() {
