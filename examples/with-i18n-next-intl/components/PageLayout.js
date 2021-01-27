543843dea04734e2720e6437c58b111afeaa81e3
Jan Amann <jan@amann.me>
2021-01-27 12:37:14 +0100

    Improve styling

--- examples/with-i18n-next-intl/components/PageLayout.js
+++ examples/with-i18n-next-intl/components/PageLayout.js
@@ -2,7 +2,13 @@ import Navigation from './Navigation'
 
 export default function PageLayout({ children, title }) {
   return (
-    <div style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
+    <div
+      style={{
+        padding: 24,
+        fontFamily: 'system-ui, sans-serif',
+        lineHeight: 1.5,
+      }}
+    >
       <Navigation />
       <div style={{ maxWidth: 510 }}>
         <h1>{title}</h1>
