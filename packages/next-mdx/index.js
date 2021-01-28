b2b9cd4eaf2eed03a36b099acab5b6ef27fecf93
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-28 21:59:44 +0700

    Allow using custom loader and options

--- packages/next-mdx/index.js
+++ packages/next-mdx/index.js
@@ -1,17 +1,21 @@
 module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
   const extension = pluginOptions.extension || /\.mdx$/
+  const loader = pluginOptions.loader || require.resolve('@mdx-js/loader')
 
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       config.module.rules.push({
         test: extension,
-        use: [
-          options.defaultLoaders.babel,
-          {
-            loader: require.resolve('@mdx-js/loader'),
-            options: pluginOptions.options,
-          },
-        ],
+        use:
+          typeof pluginOptions.options === 'function'
+            ? (info) => [
+                options.defaultLoaders.babel,
+                { loader, options: pluginOptions.options(info) },
+              ]
+            : [
+                options.defaultLoaders.babel,
+                { loader, options: pluginOptions.options },
+              ],
       })
 
       if (typeof nextConfig.webpack === 'function') {
