431918c933a76ece597ddd4957e6b7172f102305
Rasmus John Pedersen <mail@rjp.dk>
2021-01-29 10:47:26 +0100

    Uncomment og:image meta tag

--- examples/cms-umbraco-heartcore/pages/[...slug].js
+++ examples/cms-umbraco-heartcore/pages/[...slug].js
@@ -32,7 +32,7 @@ export default function Post({ post, morePosts, preview }) {
                 <title>
                   {post.title} | Next.js Blog Example with {CMS_NAME}
                 </title>
-                {/* <meta property="og:image" content={post.ogImage.url} /> */}
+                {<meta property="og:image" content={post.ogImage.url} />}
               </Head>
               <PostHeader
                 title={post.title}
