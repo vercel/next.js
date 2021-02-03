b5e11c86df94abbe854401cae724afde3d478d31
Balázs Orbán <info@balazsorban.com>
2021-02-03 20:47:07 +0100

    minor formatting changes

--- examples/with-google-tag-manager/lib/gtm.js
+++ examples/with-google-tag-manager/lib/gtm.js
@@ -1,6 +1,6 @@
 export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
-export const pageview = (url) => {
+export function pageview(url) {
   window.dataLayer.push({
     event: 'pageview',
     page: url,
