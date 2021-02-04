efd6a0de9df6b3e485b8890704487ded7bc0b622
Tim Neutkens <timneutkens@me.com>
2021-02-04 17:11:37 +0100

    Reduce size of stats

--- packages/next/build/webpack/plugins/build-stats-plugin.ts
+++ packages/next/build/webpack/plugins/build-stats-plugin.ts
@@ -8,6 +8,58 @@ import { tracer, traceAsyncFn } from '../../tracer'
 
 const STATS_VERSION = 0
 
+function reduceSize(stats: any) {
+  const modules = new Map()
+  stats.chunks = stats.chunks.map((chunk: any) => {
+    const reducedChunk: any = {
+      id: chunk.id,
+      files: chunk.files,
+      size: chunk.size,
+    }
+
+    for (const module of chunk.modules) {
+      if (!module.identifier) {
+        continue
+      }
+
+      const reducedModule: any = {
+        type: module.type,
+        moduleType: module.moduleType,
+        size: module.size,
+      }
+
+      if (module.reasons) {
+        for (const reason of module.reasons) {
+          if (!reason.moduleIdentifier) {
+            continue
+          }
+
+          if (!reducedModule.reasons) {
+            reducedModule.reasons = []
+          }
+
+          reducedModule.reasons.push({
+            moduleIdentifier: reason.moduleIdentifier,
+          })
+        }
+      }
+      // Identifier is part of the Map
+      modules.set(module.identifier, reducedModule)
+
+      if (!reducedChunk.modules) {
+        reducedChunk.modules = []
+      }
+
+      reducedChunk.modules.push(module.identifier)
+    }
+
+    return reducedChunk
+  })
+
+  stats.modules = [...modules.entries()]
+  return stats
+}
+
 // This plugin creates a stats.json for a build when enabled
 export default class BuildStatsPlugin {
   private distDir: string
@@ -25,13 +77,23 @@ export default class BuildStatsPlugin {
             const writeStatsSpan = tracer.startSpan('NextJsBuildStats')
             await traceAsyncFn(writeStatsSpan, () => {
               return new Promise((resolve, reject) => {
-                const statsJson = stats.toJson({
-                  all: false,
-                  modules: true,
-                  reasons: true,
-                  entrypoints: true,
-                  chunks: true,
-                })
+                const statsJson = reduceSize(
+                  stats.toJson({
+                    all: false,
+                    cached: true,
+                    reasons: true,
+                    entrypoints: true,
+                    chunks: true,
+                    errors: false,
+                    warnings: false,
+                    maxModules: Infinity,
+                    // @ts-ignore this option exists
+                    chunkGroups: true,
+                    chunkOrigins: true,
+                    chunkModules: true,
+                    ids: true,
+                  })
+                )
                 const fileStream = fs.createWriteStream(
                   path.join(this.distDir, 'next-stats.json')
                 )
