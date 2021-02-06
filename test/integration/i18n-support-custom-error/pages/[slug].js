805057e61390f319b89c2df13e7ad12e04a377d6
Luigi Colombi <lucolombi89@gmail.com>
2021-02-06 17:16:33 +0100

    init: setup new test folder

---
+++ test/integration/i18n-support-custom-error/pages/[slug].js
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router'
+
+const SlugPage = (props) => {
+  const router = useRouter()
+
+  return router.isFallback ? null : (
+    <>
+      <div>{props.title}</div>
+    </>
+  )
+}
+
+export const getStaticProps = async (ctx) => {
+  if (ctx.params.slug === 'my-custom-path-3') {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: {
+      title: ctx.params.slug,
+    },
+  }
+}
+
+export const getStaticPaths = async ({ locales }) => {
+  const mySlugs = ['my-custom-path-1', 'my-custom-path-1']
+
+  return {
+    paths: locales.reduce(
+      (paths, locale) => [
+        ...paths,
+        ...mySlugs.map((slug) => ({ locale, params: { slug } })),
+      ],
+      []
+    ),
+    fallback: 'blocking',
+  }
+}
+
+export default SlugPage
