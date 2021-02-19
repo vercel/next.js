d14c74791831afea74a647b4fea37d5b6604c884
Joe Haddad <joe.haddad@zeit.co>
2021-02-19 01:49:28 -0500

    Fix import statement

--- packages/next/telemetry/events/version.ts
+++ packages/next/telemetry/events/version.ts
@@ -6,7 +6,7 @@ import {
   PHASE_EXPORT,
   PHASE_PRODUCTION_BUILD,
 } from '../../next-server/lib/constants'
-import { normalizeConfig } from '../../next-server/server/config'
+import { normalizeConfig } from '../../next-server/server/config-shared'
 
 const EVENT_VERSION = 'NEXT_CLI_SESSION_STARTED'
 
