974ada5645521884d0cbb18e2d01a56f9af98881
Joe Haddad <joe.haddad@zeit.co>
2021-02-19 01:54:24 -0500

    fix more

--- packages/next/build/webpack/config/utils.ts
+++ packages/next/build/webpack/config/utils.ts
@@ -1,5 +1,5 @@
 import { webpack } from 'next/dist/compiled/webpack/webpack'
-import { NextConfig } from '../../../next-server/server/config'
+import { NextConfig } from '../../../next-server/server/config-shared'
 
 export type ConfigurationContext = {
   rootDirectory: string
