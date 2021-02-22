7a99a3ca084b8059bcfc03ef8de67f497f25fbcb
Tim Neutkens <timneutkens@me.com>
2021-02-22 19:20:18 +0100

    Add publish through Bazel

--- publish-release.sh
+++ publish-release.sh
@@ -14,10 +14,21 @@ fi
 
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 
+function publish_bazel() {
+  yarn run bazel run //packages/eslint-plugin-next:publish -- --tag=$1
+  yarn run bazel run //packages/next-bundle-analyzer:publish -- --tag=$1
+  yarn run bazel run //packages/next-codemod:publish -- --tag=$1
+  yarn run bazel run //packages/next-mdx:publish -- --tag=$1
+  yarn run bazel run //packages/next-plugin-google-analytics:publish -- --tag=$1
+  yarn run bazel run //packages/next-plugin-sentry:publish -- --tag=$1
+  yarn run bazel run //packages/next-plugin-storybook:publish -- --tag=$1
+}
+
 if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
   echo "Publishing canary"
-  yarn run lerna publish from-git --npm-tag canary --yes
+  publish_bazel canary
+  yarn run lerna publish from-package --npm-tag canary --yes
 
   # Make sure to exit script with code 1 if publish failed
   if [[ ! $? -eq 0 ]];then
@@ -29,7 +40,8 @@ fi
 
 if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];then
   echo "Publishing stable"
-  yarn run lerna publish from-git --yes
+  publish_bazel latest
+  yarn run lerna publish from-package --yes
 
   # Make sure to exit script with code 1 if publish failed
   if [[ ! $? -eq 0 ]];then
