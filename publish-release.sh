729e73ed54796a1fe9c3af229f63798cf5d367cb
Tim Neutkens <timneutkens@me.com>
2021-03-01 17:10:06 +0100

    Add moved packages to publish command

--- publish-release.sh
+++ publish-release.sh
@@ -15,20 +15,25 @@ fi
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 
 function publish_bazel() {
+  yarn run bazel run //packages/create-next-app:publish -- --tag=$1
   yarn run bazel run //packages/eslint-plugin-next:publish -- --tag=$1
+  yarn run bazel run //packages/next:publish -- --tag=$1
   yarn run bazel run //packages/next-bundle-analyzer:publish -- --tag=$1
   yarn run bazel run //packages/next-codemod:publish -- --tag=$1
   yarn run bazel run //packages/next-mdx:publish -- --tag=$1
   yarn run bazel run //packages/next-plugin-google-analytics:publish -- --tag=$1
   yarn run bazel run //packages/next-plugin-sentry:publish -- --tag=$1
   yarn run bazel run //packages/next-plugin-storybook:publish -- --tag=$1
+  yarn run bazel run //packages/next-polyfill-module:publish -- --tag=$1
+  yarn run bazel run //packages/next-polyfill-nomodule:publish -- --tag=$1
+  yarn run bazel run //packages/react-dev-overlay:publish -- --tag=$1
+  yarn run bazel run //packages/react-refresh-utils:publish -- --tag=$1
 }
 
 if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
   echo "Publishing canary"
   publish_bazel canary
-  yarn run lerna publish from-package --npm-tag canary --yes
 
   # Make sure to exit script with code 1 if publish failed
   if [[ ! $? -eq 0 ]];then
@@ -41,7 +46,6 @@ fi
 if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];then
   echo "Publishing stable"
   publish_bazel latest
-  yarn run lerna publish from-package --yes
 
   # Make sure to exit script with code 1 if publish failed
   if [[ ! $? -eq 0 ]];then
