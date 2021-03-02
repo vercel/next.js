a107dcb73268500e926856a224767788bcfa12fd
Janicklas Ralph <janicklas@google.com>
2021-03-02 11:17:33 -0800

    Experimental script loader changes (#22038)
     Making experimental script work in _document.js - Fixes for server to client transition
    Adding additional test for _document.js

--- test/integration/script-loader/test/index.test.js
+++ test/integration/script-loader/test/index.test.js
@@ -40,26 +40,28 @@ describe('Script Loader', () => {
       browser = await webdriver(appPort, '/')
       await waitFor(1000)
 
-      const script = await browser.elementById('script')
-      const src = await script.getAttribute('src')
-      const scriptPreload = await browser.elementsByCss(
-        `link[rel=preload][href="${src}"]`
-      )
-      const endScripts = await browser.elementsByCss(
-        '#script ~ script[src^="/_next/static/"]'
-      )
-      const endPreloads = await browser.elementsByCss(
-        `link[rel=preload][href="${src}"] ~ link[rel=preload][href^="/_next/static/"]`
-      )
+      async function test(id) {
+        const script = await browser.elementById(id)
+        const src = await script.getAttribute('src')
+        const endScripts = await browser.elementsByCss(
+          `#${id} ~ script[src^="/_next/static/"]`
+        )
+        const endPreloads = await browser.elementsByCss(
+          `link[rel=preload][href="${src}"] ~ link[rel=preload][href^="/_next/static/"]`
+        )
 
-      // Renders script tag
-      expect(script).toBeDefined()
-      // Renders preload
-      expect(scriptPreload.length).toBeGreaterThan(0)
-      // Script is inserted at the end
-      expect(endScripts.length).toBe(0)
-      //Preload is defined at the end
-      expect(endPreloads.length).toBe(0)
+        // Renders script tag
+        expect(script).toBeDefined()
+        // Script is inserted at the end
+        expect(endScripts.length).toBe(0)
+        //Preload is defined at the end
+        expect(endPreloads.length).toBe(0)
+      }
+
+      // Defer script in page
+      await test('scriptDefer')
+      // Defer script in _document
+      await test('documentDefer')
     } finally {
       if (browser) await browser.close()
     }
@@ -73,15 +75,22 @@ describe('Script Loader', () => {
       await browser.waitForElementByCss('#onload-div')
       await waitFor(1000)
 
-      const script = await browser.elementById('script')
-      const endScripts = await browser.elementsByCss(
-        '#script ~ script[src^="/_next/static/"]'
-      )
+      async function test(id) {
+        const script = await browser.elementById(id)
+        const endScripts = await browser.elementsByCss(
+          `#${id} ~ script[src^="/_next/static/"]`
+        )
 
-      // Renders script tag
-      expect(script).toBeDefined()
-      // Script is inserted at the end
-      expect(endScripts.length).toBe(0)
+        // Renders script tag
+        expect(script).toBeDefined()
+        // Script is inserted at the end
+        expect(endScripts.length).toBe(0)
+      }
+
+      // Lazy script in page
+      await test('scriptLazy')
+      // Lazy script in _document
+      await test('documentLazy')
     } finally {
       if (browser) await browser.close()
     }
@@ -91,33 +100,38 @@ describe('Script Loader', () => {
     const html = await renderViaHTTP(appPort, '/page1')
     const $ = cheerio.load(html)
 
-    const script = $('#script')
-    const src = script.attr('src')
+    function test(id) {
+      const script = $(`#${id}`)
+      const src = script.attr('src')
 
-    // Renders script tag
-    expect(script).toBeDefined()
-    // Preload is inserted at the beginning
-    expect(
-      $(
-        `link[rel=preload][href="${src}"] ~ link[rel=preload][href^="/_next/static/"]`
-      ).length &&
-        !$(
-          `link[rel=preload][href^="/_next/static/chunks/main"] ~ link[rel=preload][href="${src}"]`
+      // Renders script tag
+      expect(script).toBeDefined()
+      // Preload is inserted at the beginning
+      expect(
+        $(
+          `link[rel=preload][href="${src}"] ~ link[rel=preload][href^="/_next/static/"]`
+        ).length &&
+          !$(
+            `link[rel=preload][href^="/_next/static/chunks/main"] ~ link[rel=preload][href="${src}"]`
+          ).length
+      ).toBeTruthy()
+
+      // Preload is inserted after fonts and CSS
+      expect(
+        $(
+          `link[rel=stylesheet][href^="/_next/static/css"] ~ link[rel=preload][href="${src}"]`
         ).length
-    ).toBeTruthy()
+      ).toBeGreaterThan(0)
 
-    // Preload is inserted after fonts and CSS
-    expect(
-      $(
-        `link[rel=stylesheet][href^="/_next/static/css"] ~ link[rel=preload][href="${src}"]`
-      ).length
-    ).toBeGreaterThan(0)
+      // Script is inserted before NextScripts
+      expect(
+        $(`#__NEXT_DATA__ ~ #${id} ~ script[src^="/_next/static/chunks/main"]`)
+          .length
+      ).toBeGreaterThan(0)
+    }
 
-    // Script is inserted before NextScripts
-    expect(
-      $('#__NEXT_DATA__ ~ #script ~ script[src^="/_next/static/chunks/main"]')
-        .length
-    ).toBeGreaterThan(0)
+    test('scriptEager')
+    test('documentEager')
   })
 
   it('priority dangerouslyBlockRendering', async () => {
@@ -125,10 +139,11 @@ describe('Script Loader', () => {
     const $ = cheerio.load(html)
 
     // Script is inserted in place
-    expect($('.container #script').length).toBeGreaterThan(0)
+    expect($('.container #scriptBlock').length).toBeGreaterThan(0)
+    expect($('head #documentBlock').length).toBeGreaterThan(0)
   })
 
-  it('onloads fire correctly', async () => {
+  it('onload fires correctly', async () => {
     let browser
     try {
       browser = await webdriver(appPort, '/page4')
