7878aac19b225160e169ae115e34fa690dd5eb4a
Dale Bustad <dale@divmain.com>
2021-03-03 00:37:30 -0800

    Remove unused trace features.

--- packages/next/telemetry/trace/index.ts
+++ packages/next/telemetry/trace/index.ts
@@ -1,25 +1,5 @@
 import { trace, Span, SpanStatus } from './trace'
-import { noop } from './report'
 import { SpanId, setGlobal } from './shared'
 import { stackPush, stackPop } from './autoparent'
 
-const traceLevel = process.env.TRACE_LEVEL
-  ? Number.parseInt(process.env.TRACE_LEVEL)
-  : 1
-const primary = traceLevel >= 1 ? trace : noop
-const secondary = traceLevel >= 2 ? trace : noop
-const sensitive = traceLevel >= 3 ? trace : noop
-
-export {
-  trace,
-  traceLevel,
-  primary,
-  secondary,
-  sensitive,
-  SpanId,
-  Span,
-  SpanStatus,
-  stackPush,
-  stackPop,
-  setGlobal,
-}
+export { trace, SpanId, Span, SpanStatus, stackPush, stackPop, setGlobal }
