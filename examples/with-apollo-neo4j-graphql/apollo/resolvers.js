7c0a188ab9a3ff0aa21b59d3be981baabeabd2e4
bion howard <bion@bitpharma.com>
2021-03-07 06:15:51 -0500

    remove unnecessary arguments
    there's no need to explicitly pass these arguments here, and it would reduce performance

--- examples/with-apollo-neo4j-graphql/apollo/resolvers.js
+++ examples/with-apollo-neo4j-graphql/apollo/resolvers.js
@@ -2,14 +2,7 @@ import { neo4jgraphql } from 'neo4j-graphql-js'
 
 export default {
   Query: {
-    getMovies: (parent, args, context, resolveInfo) => {
-      return neo4jgraphql(parent, args, context, resolveInfo)
-    },
-    getMovie: (parent, args, context, resolveInfo) => {
-      return neo4jgraphql(parent, args, context, resolveInfo)
-    },
-    getActor: (parent, args, context, resolveInfo) => {
-      return neo4jgraphql(parent, args, context, resolveInfo)
-    },
-  },
+    getMovies: neo4jgraphql,
+    getMovie: neo4jgraphql,
+    getActor: neo4jgraphql,
 }
