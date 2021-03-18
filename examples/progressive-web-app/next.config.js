a22033c87b4ebe32d113bc18ed2f143f1f1e2967
Houssein Djirdeh <houssein@google.com>
2021-03-18 13:57:33 -0400

    fix formatting

--- examples/progressive-web-app/next.config.js
+++ examples/progressive-web-app/next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  eslint: {
+    build: false,
+  },
   pwa: {
     dest: 'public',
     runtimeCaching,
