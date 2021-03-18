2b305f408f8cfd3f4bfce4146eb6cbeed7b6e5f0
Houssein Djirdeh <houssein@google.com>
2021-03-18 14:43:30 -0400

    updates bash script

--- examples/progressive-web-app/next.config.js
+++ examples/progressive-web-app/next.config.js
@@ -2,9 +2,6 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
-  eslint: {
-    build: false,
-  },
   pwa: {
     dest: 'public',
     runtimeCaching,
