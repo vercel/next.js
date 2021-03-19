236515a223fb08bf7154d18f6020d1cf12a5b381
chequerNoel <noel@chequer.io>
2021-03-19 18:58:33 +0900

    Chore: Rename argument

--- packages/next/lib/typescript/hasBabelConfiguration.ts
+++ packages/next/lib/typescript/hasBabelConfiguration.ts
@@ -1,17 +1,17 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-export async function hasBabelConfiguration(dir: string) {
+export async function hasBabelConfiguration(baseDir: string) {
   const packageJson = JSON.parse(
-    fs.readFileSync(path.join(dir, 'package.json'), 'utf8')
+    fs.readFileSync(path.join(baseDir, 'package.json'), 'utf8')
   )
 
   return (
-    fs.existsSync(path.join(dir, '.babelrc')) ||
-    fs.existsSync(path.join(dir, '.babelrc.js')) ||
-    fs.existsSync(path.join(dir, '.babelrc.json')) ||
-    fs.existsSync(path.join(dir, 'babel.config.js')) ||
-    fs.existsSync(path.join(dir, 'babel.config.json')) ||
+    fs.existsSync(path.join(baseDir, '.babelrc')) ||
+    fs.existsSync(path.join(baseDir, '.babelrc.js')) ||
+    fs.existsSync(path.join(baseDir, '.babelrc.json')) ||
+    fs.existsSync(path.join(baseDir, 'babel.config.js')) ||
+    fs.existsSync(path.join(baseDir, 'babel.config.json')) ||
     packageJson.babel != null
   )
 }
