f731a5916e5e4786c1ffaef68aa12115982d0825
Tim Neutkens <timneutkens@me.com>
2021-03-19 16:14:10 +0100

    Change tree shaking of resolve-rewrites

--- packages/next/next-server/lib/router/utils/resolve-rewrites.ts
+++ packages/next/next-server/lib/router/utils/resolve-rewrites.ts
@@ -16,65 +16,69 @@ export default function resolveRewrites(
   query: ParsedUrlQuery,
   resolveHref: (path: string) => string,
   locales?: string[]
-): {
-  matchedPage: boolean
-  parsedAs: ReturnType<typeof parseRelativeUrl>
-  asPath: string
-  resolvedHref?: string
-} {
-  let matchedPage = false
-  let parsedAs = parseRelativeUrl(asPath)
-  let fsPathname = removePathTrailingSlash(
-    normalizeLocalePath(delBasePath(parsedAs.pathname), locales).pathname
-  )
-  let resolvedHref
+):
+  | {
+      matchedPage: boolean
+      parsedAs: ReturnType<typeof parseRelativeUrl>
+      asPath: string
+      resolvedHref?: string
+    }
+  | undefined {
+  if (process.env.__NEXT_HAS_REWRITES) {
+    let matchedPage = false
+    let parsedAs = parseRelativeUrl(asPath)
+    let fsPathname = removePathTrailingSlash(
+      normalizeLocalePath(delBasePath(parsedAs.pathname), locales).pathname
+    )
+    let resolvedHref
 
-  if (!pages.includes(fsPathname)) {
-    for (const rewrite of rewrites) {
-      const matcher = customRouteMatcher(rewrite.source)
-      const params = matcher(parsedAs.pathname)
+    if (!pages.includes(fsPathname)) {
+      for (const rewrite of rewrites) {
+        const matcher = customRouteMatcher(rewrite.source)
+        const params = matcher(parsedAs.pathname)
 
-      if (params) {
-        if (!rewrite.destination) {
-          // this is a proxied rewrite which isn't handled on the client
-          break
-        }
-        const destRes = prepareDestination(
-          rewrite.destination,
-          params,
-          query,
-          true
-        )
-        parsedAs = destRes.parsedDestination
-        asPath = destRes.newUrl
-        Object.assign(query, destRes.parsedDestination.query)
+        if (params) {
+          if (!rewrite.destination) {
+            // this is a proxied rewrite which isn't handled on the client
+            break
+          }
+          const destRes = prepareDestination(
+            rewrite.destination,
+            params,
+            query,
+            true
+          )
+          parsedAs = destRes.parsedDestination
+          asPath = destRes.newUrl
+          Object.assign(query, destRes.parsedDestination.query)
 
-        fsPathname = removePathTrailingSlash(
-          normalizeLocalePath(delBasePath(asPath), locales).pathname
-        )
+          fsPathname = removePathTrailingSlash(
+            normalizeLocalePath(delBasePath(asPath), locales).pathname
+          )
 
-        if (pages.includes(fsPathname)) {
-          // check if we now match a page as this means we are done
-          // resolving the rewrites
-          matchedPage = true
-          resolvedHref = fsPathname
-          break
-        }
+          if (pages.includes(fsPathname)) {
+            // check if we now match a page as this means we are done
+            // resolving the rewrites
+            matchedPage = true
+            resolvedHref = fsPathname
+            break
+          }
 
-        // check if we match a dynamic-route, if so we break the rewrites chain
-        resolvedHref = resolveHref(fsPathname)
+          // check if we match a dynamic-route, if so we break the rewrites chain
+          resolvedHref = resolveHref(fsPathname)
 
-        if (resolvedHref !== asPath && pages.includes(resolvedHref)) {
-          matchedPage = true
-          break
+          if (resolvedHref !== asPath && pages.includes(resolvedHref)) {
+            matchedPage = true
+            break
+          }
         }
       }
     }
-  }
-  return {
-    asPath,
-    parsedAs,
-    matchedPage,
-    resolvedHref,
+    return {
+      asPath,
+      parsedAs,
+      matchedPage,
+      resolvedHref,
+    }
   }
 }
