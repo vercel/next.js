6b4d2cae8c0e5afab0cc1c59071f3e405d35275b
Mohamed Sadat <mohamed.sadat@frichti.co>
2021-03-22 21:05:34 +0100

    update with-xstate example and add inspect mode for nextjs

--- examples/with-xstate/pages/index.js
+++ examples/with-xstate/pages/index.js
@@ -1,11 +1,25 @@
 import { useMachine } from '@xstate/react'
+import { inspect } from '@xstate/inspect'
 import { Counter, Toggle } from '../components'
 import { toggleMachine } from '../machines/toggleMachine'
 import { counterMachine } from '../machines/counterMachine'
 
+/**
+ * In order to use the xstate inspect module,
+ * we should check that we are on client, rather than the server
+ */
+if (typeof window !== 'undefined') {
+  inspect({
+    iframe: false,
+  })
+}
+
 const IndexPage = () => {
-  const [toggleCurrent, toggleSend] = useMachine(toggleMachine)
+  const [toggleCurrent, toggleSend] = useMachine(toggleMachine, {
+    devTools: true,
+  })
   const [counterCurrent, counterSend] = useMachine(counterMachine, {
+    devTools: true,
     context: { count: 999 },
   })
 
