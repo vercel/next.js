c219c1d9ceba30111501dd6203890d61b34dc6ae
Tim Neutkens <tim@timneutkens.nl>
2021-03-23 21:59:42 +0100

    Add polyfills for node_modules that are covered in webpack 4 to webpack 5 (#23316)
    Brings back the remaining Node.js module polyfills to not break existing apps upgrading from webpack 4 to webpack 5.
    
    Fixes #23169
    
    
    
    ## Bug
    
    - [x] Related issues linked using `fixes #number`
    - [x] Integration tests added
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the feature request has been accepted for implementation before opening a PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -393,11 +393,29 @@ export default async function getBaseWebpackConfig(
           // Full list of old polyfills is accessible here:
           // https://github.com/webpack/webpack/blob/2a0536cf510768111a3a6dceeb14cb79b9f59273/lib/ModuleNotFoundError.js#L13-L42
           fallback: {
-            buffer: require.resolve('buffer'),
+            assert: require.resolve('assert/'),
+            buffer: require.resolve('buffer/'),
+            constants: require.resolve('constants-browserify'),
             crypto: require.resolve('crypto-browserify'),
+            domain: require.resolve('domain-browser'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify/browser'),
             path: require.resolve('path-browserify'),
+            punycode: require.resolve('punycode'),
+            process: require.resolve('process/browser'),
+            // Handled in separate alias
+            querystring: require.resolve('querystring-es3'),
             stream: require.resolve('stream-browserify'),
+            string_decoder: require.resolve('string_decoder'),
+            sys: require.resolve('util/'),
+            timers: require.resolve('timers-browserify'),
+            tty: require.resolve('tty-browserify'),
+            // Handled in separate alias
+            // url: require.resolve('url/'),
+            util: require.resolve('util/'),
             vm: require.resolve('vm-browserify'),
+            zlib: require.resolve('browserify-zlib'),
           },
         }
       : undefined),
