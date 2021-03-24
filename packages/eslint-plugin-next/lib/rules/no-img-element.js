7333371733de2d2fe4d96f4b6fc6ca0441872db2
Houssein Djirdeh <houssein@google.com>
2021-03-24 17:38:36 -0400

    modify img eslint rules

---
+++ packages/eslint-plugin-next/lib/rules/no-img-element.js
@@ -0,0 +1,29 @@
+module.exports = {
+  meta: {
+    docs: {
+      description: 'Prohibit usage of HTML <img> element',
+      category: 'HTML',
+      recommended: true,
+    },
+    fixable: 'code',
+  },
+
+  create: function (context) {
+    return {
+      JSXOpeningElement(node) {
+        if (node.name.name !== 'img') {
+          return
+        }
+
+        if (node.attributes.length === 0) {
+          return
+        }
+
+        context.report({
+          node,
+          message: `Use Image from 'next/image' instead of the <img> element for better performance.`,
+        })
+      },
+    }
+  },
+}
