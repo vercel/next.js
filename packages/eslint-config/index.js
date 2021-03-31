da2b9b559b017efa91cc24f79190094aeba2cb11
Houssein Djirdeh <houssein@google.com>
2021-03-31 19:09:17 -0400

    creates next shareable eslint config

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  plugins: ['import', 'react'],
+  rules: {
+    'import/no-anonymous-default-export': 'warn',
+    'react/react-in-jsx-scope': 'off', // can be removed when Next.js only supports React versions with the new JSX transform
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    babelOptions: {
+      presets: ['next/babel'],
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
