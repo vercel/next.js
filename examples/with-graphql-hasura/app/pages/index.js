ed958fef6ed3527b0c3c2552cf8da9e941c72187
Gaurav Paruthi <gparuthi@umich.edu>
2021-04-06 20:42:23 -0700

    replace apollo with graphql-request

--- examples/with-graphql-hasura/app/pages/index.js
+++ examples/with-graphql-hasura/app/pages/index.js
@@ -1,71 +1,85 @@
-import { gql, useMutation, useQuery } from '@apollo/client';
-import { useRouter } from "next/router";
-import styles from '../styles/Home.module.css';
+import { GraphQLClient, gql } from 'graphql-request'
+import { useEffect, useRef, useState } from 'react'
+import styles from '../styles/Home.module.css'
 
-const Query = gql`
-query Todos {
-  todos {
-    id, 
-    value
-  }
-}
-`
-const AddTodoQuery = gql`
-mutation AddTodo($value: String = "") {
-  insert_todos_one(object: {value: $value}) {
-    id
-    value
-  }
-}
-`
+export const client = new GraphQLClient('http://localhost:8080/v1/graphql')
+client.setHeader('x-hasura-admin-secret', 'myadminsecretkey')
 
-const DelTodoQuery = gql`
-mutation DelTodo($id: uuid!) {
-  delete_todos_by_pk(id: $id) {
-    id
-    value
+const FetchTodosQuery = gql`
+  query Todos {
+    todos {
+      id
+      value
+    }
   }
-}
+`
+const AddTodoMutation = gql`
+  mutation AddTodo($value: String = "") {
+    insert_todos_one(object: { value: $value }) {
+      id
+      value
+    }
+  }
 `
 
+const DelTodoMutation = gql`
+  mutation DelTodo($id: uuid!) {
+    delete_todos_by_pk(id: $id) {
+      id
+      value
+    }
+  }
+`
 
 export default function Home() {
-  const router = useRouter()
-  const { loading, error, data, refetch } = useQuery(Query);
-  const [updateTodo] = useMutation(AddTodoQuery, { onCompleted: () => refetch() });
-  const [delTodo] = useMutation(DelTodoQuery, { onCompleted: () => refetch() });
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [todos, setTodos] = useState([])
+  const inputRef = useRef(null)
 
+  const fetch = async () => {
+    setLoading(true)
+    try {
+    } catch (error) {
+      setError(true)
+    }
+    const data = await client.request(FetchTodosQuery)
+    setTodos(data.todos)
+    setLoading(false)
+  }
+  useEffect(() => {
+    fetch()
+  }, [])
 
-  let todoInput
-
-  const onAdd = (e) => {
+  const onAdd = async (e) => {
     e.preventDefault()
-    console.log(todoInput.value)
-    updateTodo({ variables: { value: todoInput.value } })
-    todoInput.value = ""
+    console.log(inputRef.current.value)
+    await client.request(AddTodoMutation, { value: inputRef.current.value })
+    inputRef.current.value = ''
+    fetch()
   }
-  const onDel = (id) => {
+  const onDel = async (id) => {
     console.log(id)
-    delTodo({ variables: { id: id } })
+    await client.request(DelTodoMutation, { id })
+    fetch()
   }
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
 
   return (
     <div className={styles.container}>
       <div className={styles.box}></div>
       <h1>TODOS</h1>
       <hr />
-      {data.todos.map(t =>
+      {todos.map((t) => (
         <div key={t.id} className={styles.todo}>
           <div>{t.value}</div>
           <button onClick={() => onDel(t.id)}>x</button>
-        </div>)
-      }
+        </div>
+      ))}
       <hr />
-      <input type='text' placeholder='Enter TODO'
-        ref={el => todoInput = el} /> 
+      <input type="text" placeholder="Enter TODO" ref={inputRef} />
       <button onClick={onAdd}>+</button>
     </div>
   )
