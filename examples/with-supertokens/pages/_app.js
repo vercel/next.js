13ad8f2b2d78e3ad776da23a19acf9f521b417da
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-04-08 15:02:06 +0530

    removes need for backend init in app.jsx

--- examples/with-supertokens/pages/_app.js
+++ examples/with-supertokens/pages/_app.js
@@ -1,13 +1,10 @@
 import '../styles/globals.css'
 import React from 'react'
 import SuperTokensReact from 'supertokens-auth-react'
-import SuperTokensNode from 'supertokens-node'
 import * as SuperTokensConfig from './supertokensConfig'
 
 if (typeof window !== 'undefined') {
   SuperTokensReact.init(SuperTokensConfig.frontendConfig())
-} else {
-  SuperTokensNode.init(SuperTokensConfig.backendConfig())
 }
 
 function MyApp({ Component, pageProps }) {
