0e92463a25d772d30ee1f1a2fe8b50c397bf32b8
Tim Neutkens <tim@timneutkens.nl>
2021-04-08 12:02:54 +0200

    Update packages/next/next-server/lib/i18n/detect-locale-cookie.ts

--- packages/next/next-server/lib/i18n/detect-locale-cookie.ts
+++ packages/next/next-server/lib/i18n/detect-locale-cookie.ts
@@ -1,7 +1,7 @@
 import { IncomingMessage } from 'http'
 
 export function detectLocaleCookie(req: IncomingMessage, locales: string[]) {
-  if (req.headers.cookies && req.headers.cookies.hasOwnProperty('NEXT_LOCALE')) {
+  if (req.headers.cookie && req.headers.cookie.hasOwnProperty('NEXT_LOCALE=')) {
     const { NEXT_LOCALE } = (req as any).cookies
     return locales.find(
       (locale: string) => NEXT_LOCALE.toLowerCase() === locale.toLowerCase()
