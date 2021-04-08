ca4424990e9c6c8875c38debe693fc68c2da3436
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-04-08 14:39:32 +0530

    extracts config into its own file and calls it in all serverless functions

--- examples/with-supertokens/pages/api/auth/[[...path]].js
+++ examples/with-supertokens/pages/api/auth/[[...path]].js
@@ -1,11 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { superTokensNextWrapper } from 'supertokens-node/nextjs'
-import { middleware } from 'supertokens-node'
+import supertokens from 'supertokens-node'
+import * as SuperTokensConfig from '../../supertokensConfig'
+
+supertokens.init(SuperTokensConfig.backendConfig())
 
 export default async function superTokens(req, res) {
   return await superTokensNextWrapper(
     async (next) => {
-      await middleware()(req, res, next)
+      await supertokens.middleware()(req, res, next)
     },
     req,
     res
--- examples/with-supertokens/pages/api/user.js
+++ examples/with-supertokens/pages/api/user.js
@@ -1,6 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { superTokensNextWrapper } from 'supertokens-node/nextjs'
 import { verifySession } from 'supertokens-node/recipe/session'
+import supertokens from 'supertokens-node'
+import * as SuperTokensConfig from '../supertokensConfig'
+
+supertokens.init(SuperTokensConfig.backendConfig())
 
 export default async function user(req, res) {
   if (req.method !== 'GET') {
@@ -18,8 +21,8 @@ export default async function user(req, res) {
   return res.json({
     note:
       'Fetch any data from your application for authenticated user after using verifySession middleware',
-    userId: req.session.userId,
-    sessionHandle: req.session.sessionHandle,
-    userDataInJWT: req.session.userDataInJWT,
+    userId: req.session.getUserId(),
+    sessionHandle: req.session.getHandle(),
+    userDataInJWT: req.session.getJWTPayload(),
   })
 }
