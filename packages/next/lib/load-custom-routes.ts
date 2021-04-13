4d291bd810bc4245c241bed9ae79228febb704fc
JJ Kasper <jj@jjsweb.site>
2021-04-13 07:34:51 -0500

    Use regex lexer for gathering named groups from has (#23626)
    This is a follow-up to https://github.com/vercel/next.js/pull/23588 to update to use a regex lexer to gather the named regex groups instead of attempting to gather them through executing the regex since it can fail to gather the regex groups when they are using specific matching. This also ensures we don't pass the value as a segment when value is defined and it doesn't use a capture group. Additional tests are added to cover these cases and documentation updated to reflect this. 
    
    Closes: https://github.com/vercel/next.js/issues/23415
    
    ## Bug
    
    - [x] Related issues linked using `fixes #number`
    - [x] Integration tests added
    
    ## Documentation / Examples
    
    - [x] Make sure the linting passes

--- packages/next/lib/load-custom-routes.ts
+++ packages/next/lib/load-custom-routes.ts
@@ -8,7 +8,10 @@ import {
 } from '../next-server/lib/constants'
 import { execOnce } from '../next-server/lib/utils'
 import * as Log from '../build/output/log'
-import { getSafeParamName } from '../next-server/lib/router/utils/prepare-destination'
+// @ts-ignore
+import Lexer from 'next/dist/compiled/regexr-lexer/lexer'
+// @ts-ignore
+import lexerProfiles from 'next/dist/compiled/regexr-lexer/profiles'
 
 export type RouteHas =
   | {
@@ -336,20 +339,16 @@ function checkCustomRoutes(
 
         if (hasItem.value) {
           const matcher = new RegExp(`^${hasItem.value}$`)
-          const matches = matcher.exec('')
+          const lexer = new Lexer()
+          lexer.profile = lexerProfiles.js
+          lexer.parse(`/${matcher.source}/`)
 
-          if (matches) {
-            if (matches.groups) {
-              Object.keys(matches.groups).forEach((groupKey) => {
-                const safeKey = getSafeParamName(groupKey)
+          Object.keys(lexer.namedGroups).forEach((groupKey) => {
+            hasSegments.add(groupKey)
+          })
 
-                if (safeKey && matches.groups![groupKey]) {
-                  hasSegments.add(safeKey)
-                }
-              })
-            } else {
-              hasSegments.add(hasItem.key || 'host')
-            }
+          if (hasItem.type === 'host') {
+            hasSegments.add('host')
           }
         }
       }
