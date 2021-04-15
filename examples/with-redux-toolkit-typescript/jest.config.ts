2599d09254bb67c6c07eacbad0ff04041271bedc
Jaime Leonardo Suncin Cruz <leosuncin@users.noreply.github.com>
2021-04-15 00:13:15 +0000

    build(with-redux-toolkit-typescript): Jest transform CSS Modules

--- examples/with-redux-toolkit-typescript/jest.config.ts
+++ examples/with-redux-toolkit-typescript/jest.config.ts
@@ -3,6 +3,9 @@ import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
 const config: InitialOptionsTsJest = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+  },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
