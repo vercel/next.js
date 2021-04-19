06e86eee0ca9ad274e7f52d4d768231d28256db0
Tim Neutkens <timneutkens@me.com>
2021-04-19 20:09:22 +0200

    Add webpackVersion to dev server

--- packages/next/server/next-dev-server.ts
+++ packages/next/server/next-dev-server.ts
@@ -308,7 +308,7 @@ export default class DevServer extends Server {
     const telemetry = new Telemetry({ distDir: this.distDir })
     telemetry.record(
       eventCliSession(PHASE_DEVELOPMENT_SERVER, this.distDir, {
-        webpackVersion: null,
+        webpackVersion: this.hotReloader.isWebpack5 ? 5 : 4,
         cliCommand: 'dev',
         isSrcDir: relative(this.dir, this.pagesDir!).startsWith('src'),
         hasNowJson: !!(await findUp('now.json', { cwd: this.dir })),
