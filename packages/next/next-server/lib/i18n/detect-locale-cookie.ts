e532c58c23c530c46990c86ea33d9371918becf7
JJ Kasper <jj@jjsweb.site>
2021-04-20 08:41:14 -0500

    Ensure locale cookie does not error with similarly named cookie (#24182)
    This ensures we don't error when a cookie with a similar name to the `NEXT_LOCALE` cookie is present. 
    
    ## Bug
    
    - [x] Related issues linked using `fixes #number`
    - [x] Integration tests added
    
    Fixes: https://github.com/vercel/next.js/issues/23787
    Closes: https://github.com/vercel/next.js/pull/23788

--- packages/next/next-server/lib/i18n/detect-locale-cookie.ts
+++ packages/next/next-server/lib/i18n/detect-locale-cookie.ts
@@ -1,10 +1,10 @@
 import { IncomingMessage } from 'http'
 
 export function detectLocaleCookie(req: IncomingMessage, locales: string[]) {
-  if (req.headers.cookie && req.headers.cookie.includes('NEXT_LOCALE')) {
-    const { NEXT_LOCALE } = (req as any).cookies
-    return locales.find(
-      (locale: string) => NEXT_LOCALE.toLowerCase() === locale.toLowerCase()
-    )
-  }
+  const { NEXT_LOCALE } = (req as any).cookies || {}
+  return NEXT_LOCALE
+    ? locales.find(
+        (locale: string) => NEXT_LOCALE.toLowerCase() === locale.toLowerCase()
+      )
+    : undefined
 }
