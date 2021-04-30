b75cc8e119aa8f934b1e65823995c052a4604346
Houssein Djirdeh <houssein@google.com>
2021-04-30 14:40:33 -0400

    adds lint rule for missing passHref

--- packages/eslint-plugin-next/lib/index.js
+++ packages/eslint-plugin-next/lib/index.js
@@ -4,6 +4,7 @@ module.exports = {
     'no-sync-scripts': require('./rules/no-sync-scripts'),
     'no-html-link-for-pages': require('./rules/no-html-link-for-pages'),
     'no-unwanted-polyfillio': require('./rules/no-unwanted-polyfillio'),
+    'link-passhref': require('./rules/link-passhref'),
   },
   configs: {
     recommended: {
@@ -13,6 +14,7 @@ module.exports = {
         '@next/next/no-sync-scripts': 1,
         '@next/next/no-html-link-for-pages': 1,
         '@next/next/no-unwanted-polyfillio': 1,
+        '@next/next/link-passhref': 1,
       },
     },
   },
---
+++ packages/eslint-plugin-next/lib/rules/link-passhref.js
@@ -0,0 +1,62 @@
+const NodeAttributes = require('../utils/nodeAttributes.js')
+
+module.exports = {
+  meta: {
+    docs: {
+      description:
+        'Ensure passHref is assigned if child of Link component is a custom component',
+      category: 'HTML',
+      recommended: true,
+    },
+    fixable: null,
+  },
+
+  create: function (context) {
+    return {
+      JSXOpeningElement(node) {
+        const attributes = new NodeAttributes(node)
+        const children = node.parent.children
+
+        if (node.name.name !== 'Link') {
+          return
+        }
+
+        if (
+          !attributes.hasAny() ||
+          !children.some((attr) => attr.type === 'JSXElement')
+        ) {
+          return
+        }
+
+        const hasHref = attributes.has('href')
+
+        if (!hasHref) {
+          return
+        }
+
+        const hasPassHref =
+          attributes.has('passHref') &&
+          (typeof attributes.value('passHref') === 'undefined' ||
+            attributes.value('passHref') === true)
+
+        const hasAnchorChild = children.some(
+          (attr) =>
+            attr.type === 'JSXElement' &&
+            attr.openingElement.name.name === 'a' &&
+            attr.closingElement.name.name === 'a'
+        )
+
+        if (!hasAnchorChild && !hasPassHref) {
+          context.report({
+            node,
+            message: `passHref ${
+              attributes.value('passHref') !== true
+                ? 'must be set to true'
+                : 'is missing'
+            }. See: https://nextjs.org/docs/messages/link-passhref.`,
+          })
+        }
+      },
+    }
+  },
+}
