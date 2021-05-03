7989b7917ddbe2025906a35022b25a48242db77f
Houssein Djirdeh <houssein@google.com>
2021-05-03 14:51:30 -0400

    adds rule for google font display

--- packages/eslint-plugin-next/lib/index.js
+++ packages/eslint-plugin-next/lib/index.js
@@ -4,6 +4,7 @@ module.exports = {
     'no-sync-scripts': require('./rules/no-sync-scripts'),
     'no-html-link-for-pages': require('./rules/no-html-link-for-pages'),
     'no-unwanted-polyfillio': require('./rules/no-unwanted-polyfillio'),
+    'google-font-display': require('./rules/google-font-display'),
   },
   configs: {
     recommended: {
@@ -13,6 +14,7 @@ module.exports = {
         '@next/next/no-sync-scripts': 1,
         '@next/next/no-html-link-for-pages': 1,
         '@next/next/no-unwanted-polyfillio': 1,
+        '@next/next/google-font-display': 1,
       },
     },
   },
