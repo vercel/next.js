7fd159e9cf4f7dc4c8b70cbb5c4100b48be08e04
Tim Neutkens <timneutkens@me.com>
2021-05-06 12:29:09 +0200

    Add types (same as docs)

--- packages/create-next-app/templates/typescript/pages/api/hello.ts
+++ packages/create-next-app/templates/typescript/pages/api/hello.ts
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
+type Data = {
+  name: string
+}
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.status(200).json({ name: 'John Doe' })
 }
