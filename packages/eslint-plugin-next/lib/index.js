599f844e14f316d4d01424071740c043e4275cca
Houssein Djirdeh <houssein@google.com>
2021-05-07 13:59:53 -0400

    adds eslint rule to preconnect to Google fonts domain

--- packages/eslint-plugin-next/lib/index.js
+++ packages/eslint-plugin-next/lib/index.js
@@ -6,6 +6,7 @@ module.exports = {
     'no-unwanted-polyfillio': require('./rules/no-unwanted-polyfillio'),
     'no-title-in-document-head': require('./rules/no-title-in-document-head'),
     'google-font-display': require('./rules/google-font-display'),
+    'google-font-preconnect': require('./rules/google-font-preconnect'),
   },
   configs: {
     recommended: {
@@ -17,6 +18,7 @@ module.exports = {
         '@next/next/no-unwanted-polyfillio': 1,
         '@next/next/no-title-in-document-head': 1,
         '@next/next/google-font-display': 1,
+        '@next/next/google-font-preconnect': 1,
       },
     },
   },
