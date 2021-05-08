5741fa3a4c6b87de675a07bf5f057a3d89531ab3
Rohithgilla12 <gillarohith1@gmail.com>
2021-05-08 13:14:03 +0530

    Formatting

--- examples/with-redux-toolkit-chakra-ui-typescript/src/features/counter/Counter.tsx
+++ examples/with-redux-toolkit-chakra-ui-typescript/src/features/counter/Counter.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
 import {
-	decrement,
-	increment,
-	incrementByAmount,
-	incrementAsync,
-	incrementIfOdd,
-	selectCount,
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
 } from './counterSlice';
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { Box, Button, Input, Text } from '@chakra-ui/react';
@@ -16,66 +16,66 @@ interface CounterProps {
 }
 
 export const Counter: React.FC<CounterProps> = ({ }) => {
-	const count = useAppSelector(selectCount);
-	const dispatch = useAppDispatch();
-	const [incrementAmount, setIncrementAmount] = useState('2');
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
 
-	const incrementValue = Number(incrementAmount) || 0;
+  const incrementValue = Number(incrementAmount) || 0;
 
-	return (
-		<Box p={4} m={4}>
-			<Box d="flex" justifyContent="center" alignItems="center">
-				<Button
-					px={4}
-					mx={4}
-					variant="outline" colorScheme="green"
-					onClick={() => dispatch(decrement())}
-				>
-					-
+  return (
+    <Box p={4} m={4}>
+      <Box d="flex" justifyContent="center" alignItems="center">
+        <Button
+          px={4}
+          mx={4}
+          variant="outline" colorScheme="green"
+          onClick={() => dispatch(decrement())}
+        >
+          -
         </Button>
-				<Text fontSize="sm">{count}</Text>
-				<Button
-					px={4}
-					mx={4}
-					variant="outline" colorScheme="green"
-					onClick={() => dispatch(increment())}
-				>
-					+
+        <Text fontSize="sm">{count}</Text>
+        <Button
+          px={4}
+          mx={4}
+          variant="outline" colorScheme="green"
+          onClick={() => dispatch(increment())}
+        >
+          +
         </Button>
-			</Box>
-			<div >
-				<Input
-					value={incrementAmount}
-					onChange={(e) => setIncrementAmount(e.target.value)}
-					p={4}
-					m={4}
+      </Box>
+      <div >
+        <Input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+          p={4}
+          m={4}
 
-				/>
-				<Button
-					px={4}
-					mx={4}
-					variant="solid" colorScheme="green"
-					onClick={() => dispatch(incrementByAmount(incrementValue))}
-				>
-					Add Amount
+        />
+        <Button
+          px={4}
+          mx={4}
+          variant="solid" colorScheme="green"
+          onClick={() => dispatch(incrementByAmount(incrementValue))}
+        >
+          Add Amount
         </Button>
-				<Button
-					px={4}
-					mx={4}
-					variant="solid" colorScheme="green"
-					onClick={() => dispatch(incrementAsync(incrementValue))}
-				>
-					Add Async
+        <Button
+          px={4}
+          mx={4}
+          variant="solid" colorScheme="green"
+          onClick={() => dispatch(incrementAsync(incrementValue))}
+        >
+          Add Async
         </Button>
-				<Button
-					px={4}
-					mx={4}
-					variant="solid" colorScheme="green"
-					onClick={() => dispatch(incrementIfOdd(incrementValue))}
-				>
-					Add If Odd
+        <Button
+          px={4}
+          mx={4}
+          variant="solid" colorScheme="green"
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+        >
+          Add If Odd
         </Button>
-			</div>
-		</Box>
-	);
+      </div>
+    </Box>
+  );
 }
