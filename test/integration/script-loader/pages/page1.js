86827058e474cf4a6db5aee009d46ca42e56e55f
Janicklas Ralph <janicklas@google.com>
2021-05-10 01:39:08 -0700

    Refactor experimental-script component (#24940)

--- test/integration/script-loader/pages/page1.js
+++ test/integration/script-loader/pages/page1.js
@@ -4,9 +4,9 @@ const Page = () => {
   return (
     <div class="container">
       <Script
-        id="scriptEager"
-        src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js?a=scriptEager"
-        strategy="eager"
+        id="scriptBeforeInteraction"
+        src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js?a=scriptBeforeInteraction"
+        strategy="beforeInteraction"
       ></Script>
       <div>page1</div>
     </div>
