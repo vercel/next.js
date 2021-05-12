114bd567aaba08a8f184bdbb86e6ef3c4a1d1654
Fabien Le Frapper <contact@fabienlefrapper.me>
2021-05-12 17:21:28 +0200

    Lint 😇

--- examples/with-apollo/lib/apolloClient.js
+++ examples/with-apollo/lib/apolloClient.js
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client'
-import { onError } from "@apollo/client/link/error"
+import { onError } from '@apollo/client/link/error'
 import { concatPagination } from '@apollo/client/utilities'
 import merge from 'deepmerge'
 import isEqual from 'lodash/isEqual'
