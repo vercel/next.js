21c478dfba4dd742d3af536562104427c81f075e
Adem ilter <ademilter@gmail.com>
2021-05-12 23:14:07 +0300

    fix: blog title

--- examples/blog-with-comment/pages/_app.js
+++ examples/blog-with-comment/pages/_app.js
@@ -12,6 +12,7 @@ export default function MyApp({ Component, pageProps }) {
     >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>My awesome blog</title>
       </Head>
 
       <Header />
