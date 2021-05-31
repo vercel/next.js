989056598329e23b6d019e5cbe244c623c1b4afa
Tim Neutkens <tim@timneutkens.nl>
2021-05-31 21:41:57 +0200

    Add helpful error for link with multiple children (#25657)
    Makes sure a helpful error is shown for `<Link>` with multiple children
    
    
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the feature request has been accepted for implementation before opening a PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes

--- packages/next/client/link.tsx
+++ packages/next/client/link.tsx
@@ -229,7 +229,21 @@ function Link(props: React.PropsWithChildren<LinkProps>) {
   }
 
   // This will return the first child, if multiple are provided it will throw an error
-  const child: any = Children.only(children)
+  let child: any
+  if (process.env.NODE_ENV === 'development') {
+    try {
+      child = Children.only(children)
+    } catch (err) {
+      throw new Error(
+        `Multiple children were passed to <Link> with \`href\` of \`${props.href}\` but only one child is supported https://nextjs.org/docs/messages/link-multiple-children` +
+          (typeof window !== 'undefined'
+            ? "\nOpen your browser's console to view the Component stack trace."
+            : '')
+      )
+    }
+  } else {
+    child = Children.only(children)
+  }
   const childRef: any = child && typeof child === 'object' && child.ref
 
   const [setIntersectionRef, isVisible] = useIntersection({
