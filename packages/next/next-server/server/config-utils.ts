f071327a7629bc56767ca3b1ed2f81815ed22ae8
Tim Neutkens <timneutkens@me.com>
2021-05-31 21:09:04 +0200

    Revert "Don't show webpack version message in production server"
    This reverts commit c12839e0e6bec83ee7f0a743dcc57526a40a8e1a.

--- packages/next/next-server/server/config-utils.ts
+++ packages/next/next-server/server/config-utils.ts
@@ -3,7 +3,6 @@ import { Worker } from 'jest-worker'
 import * as Log from '../../build/output/log'
 import { CheckReasons, CheckResult } from './config-utils-worker'
 import { install, shouldLoadWithWebpack5 } from './config-utils-worker'
-import { PHASE_PRODUCTION_SERVER } from '../lib/constants'
 
 export { install, shouldLoadWithWebpack5 }
 
@@ -37,15 +36,11 @@ export async function loadWebpackHook(phase: string, dir: string) {
   )
   try {
     const result: CheckResult = await worker.shouldLoadWithWebpack5(phase, dir)
-    // Don't log which webpack version is being used when booting production server as it's not used after build
-    if (phase !== PHASE_PRODUCTION_SERVER) {
-      Log.info(
-        `Using webpack ${result.enabled ? '5' : '4'}. Reason: ${reasonMessage(
-          result.reason
-        )} https://nextjs.org/docs/messages/webpack5`
-      )
-    }
-
+    Log.info(
+      `Using webpack ${result.enabled ? '5' : '4'}. Reason: ${reasonMessage(
+        result.reason
+      )} https://nextjs.org/docs/messages/webpack5`
+    )
     useWebpack5 = Boolean(result.enabled)
   } catch {
     // If this errors, it likely will do so again upon boot, so we just swallow
