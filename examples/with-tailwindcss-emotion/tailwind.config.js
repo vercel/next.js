ea7cb33717e77f37a012432cd75979e35ed66398
Pavel Mineev <pavel@mineev.me>
2021-06-11 19:21:34 +0300

    (examples/with-tailwind-emotion): disable jit mode

--- examples/with-tailwindcss-emotion/tailwind.config.js
+++ examples/with-tailwindcss-emotion/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
