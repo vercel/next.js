85102704983fe8299b191a0beaf51c40fc87778b
Steven <steven@ceriously.com>
2021-06-15 18:58:04 -0400

    Add width/height back

--- examples/image-component/pages/placeholder.js
+++ examples/image-component/pages/placeholder.js
@@ -6,7 +6,13 @@ const Responsive = () => (
   <div>
     <ViewSource pathname="pages/placeholder.js" />
     <h1>Image Component With Placeholder</h1>
-    <Image alt="Mountains" src={mountains} placeholder="blur" />
+    <Image
+      alt="Mountains"
+      src={mountains}
+      placeholder="blur"
+      width={700}
+      height={475}
+    />
   </div>
 )
 
