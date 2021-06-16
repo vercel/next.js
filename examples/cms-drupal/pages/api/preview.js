515a0b773b890ef5ca085368aeceb0b4ee028672
arshad <arshad@chummun.com>
2021-06-16 11:57:38 +0400

    feat(cms-drupal): update preview

--- examples/cms-drupal/pages/api/preview.js
+++ examples/cms-drupal/pages/api/preview.js
@@ -1,28 +1,3 @@
-import { getEntityByPath } from 'next-drupal'
+import { DrupalPreview } from 'next-drupal'
 
-export default async function preview(req, res) {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (
-    req.query.secret !== process.env.DRUPAL_PREVIEW_SECRET ||
-    !req.query.slug
-  ) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
-
-  // Fetch the headless CMS to check if the provided `slug` exists
-  const node = await getEntityByPath(req.query.slug)
-
-  // If the slug doesn't exist prevent preview mode from being enabled
-  if (!node) {
-    return res.status(401).json({ message: 'Invalid slug' })
-  }
-
-  // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
-
-  // Redirect to the path from the fetched node
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: node.path.alias })
-  res.end()
-}
+export default DrupalPreview()
