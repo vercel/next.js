f38c76a798be76cf56195c82f734749ecaff0595
arshad <arshad@chummun.com>
2021-06-16 21:25:19 +0400

    fix(cms-drupal):  use the Image component

--- examples/cms-drupal/components/cover-image.js
+++ examples/cms-drupal/components/cover-image.js
@@ -1,9 +1,13 @@
 import cn from 'classnames'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function CoverImage({ title, coverImage, slug }) {
   const image = (
-    <img
+    <Image
+      width={2000}
+      height={1000}
+      alt={`Cover Image for ${title}`}
       src={coverImage?.sourceUrl}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
