ff12a0e1a40f30f02c8fd31b231f7428a9772e2e
Gerald Monaco <gbmonaco@google.com>
2021-06-17 19:27:55 +0000

    DRY

--- test/integration/react-18/test/index.test.js
+++ test/integration/react-18/test/index.test.js
@@ -12,27 +12,43 @@ const UNSUPPORTED_PRERELEASE =
   "You are using an unsupported prerelease of 'react-dom'"
 const USING_CREATE_ROOT = 'Using the createRoot API for React'
 
+async function getBuildOutput(dir) {
+  const { stdout, stderr } = await runNextCommand(['build', dir], {
+    stdout: true,
+    stderr: true,
+  })
+  return stdout + stderr
+}
+
+async function getDevOutput(dir) {
+  const port = await findPort()
+
+  let stdout = ''
+  let stderr = ''
+  let instance = await launchApp(dir, port, {
+    stdout: true,
+    stderr: true,
+    onStdout(msg) {
+      stdout += msg
+    },
+    onStderr(msg) {
+      stderr += msg
+    },
+  })
+  await killApp(instance)
+  return stdout + stderr
+}
+
 describe('React 18 Support', () => {
   describe('build', () => {
     test('supported version of React', async () => {
-      const { stdout, stderr } = await runNextCommand(['build', dirSupported], {
-        stdout: true,
-        stderr: true,
-      })
-      const output = stdout + stderr
+      const output = await getBuildOutput(dirSupported)
       expect(output).not.toMatch(USING_CREATE_ROOT)
       expect(output).not.toMatch(UNSUPPORTED_PRERELEASE)
     })
 
     test('prerelease version of React', async () => {
-      const { stdout, stderr } = await runNextCommand(
-        ['build', dirPrerelease],
-        {
-          stdout: true,
-          stderr: true,
-        }
-      )
-      const output = stdout + stderr
+      const output = await getBuildOutput(dirPrerelease)
       expect(output).toMatch(USING_CREATE_ROOT)
       expect(output).toMatch(UNSUPPORTED_PRERELEASE)
     })
@@ -40,45 +56,13 @@ describe('React 18 Support', () => {
 
   describe('dev', () => {
     test('supported version of React', async () => {
-      const port = await findPort()
-
-      let stdout = ''
-      let stderr = ''
-      let instance = await launchApp(dirSupported, port, {
-        stdout: true,
-        stderr: true,
-        onStdout(msg) {
-          stdout += msg
-        },
-        onStderr(msg) {
-          stderr += msg
-        },
-      })
-      await killApp(instance)
-
-      let output = stdout + stderr
+      let output = await getDevOutput(dirSupported)
       expect(output).not.toMatch(USING_CREATE_ROOT)
       expect(output).not.toMatch(UNSUPPORTED_PRERELEASE)
     })
 
     test('prerelease version of React', async () => {
-      const port = await findPort()
-
-      let stdout = ''
-      let stderr = ''
-      let instance = await launchApp(dirPrerelease, port, {
-        stdout: true,
-        stderr: true,
-        onStdout(msg) {
-          stdout += msg
-        },
-        onStderr(msg) {
-          stderr += msg
-        },
-      })
-      await killApp(instance)
-
-      let output = stdout + stderr
+      let output = await getDevOutput(dirPrerelease)
       expect(output).toMatch(USING_CREATE_ROOT)
       expect(output).toMatch(UNSUPPORTED_PRERELEASE)
     })
