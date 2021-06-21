6c7d323cdf85534caf165a182f32e94e37e03709
Jesse Jafa <jesse.jafa@gmail.com>
2021-06-21 18:40:49 +0300

    fix(with-webassembly): change config for webpack5

--- examples/with-webassembly/next.config.js
+++ examples/with-webassembly/next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   webpack(config) {
-    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true
+    }
+
     return config
   },
 }
