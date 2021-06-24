2250f834430c21c037310d84f0ebb3d67bb8c024
Pavel Mineev <pavel@mineev.me>
2021-06-24 23:36:55 +0300

    Fix with-zustang example

--- examples/with-zustand/pages/_app.js
+++ examples/with-zustand/pages/_app.js
@@ -2,8 +2,9 @@ import { useHydrate, Provider } from '../lib/store'
 
 export default function App({ Component, pageProps }) {
   const store = useHydrate(pageProps.initialZustandState)
+
   return (
-    <Provider initialStore={store}>
+    <Provider createStore={store}>
       <Component {...pageProps} />
     </Provider>
   )
