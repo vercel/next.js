9fbc666ab2387426dbdf6cedddc23eb76d14b7a9
lsndr <alexander@lsndr.me>
2021-06-26 02:38:04 +0300

    add rewrites-destination-query-array integration test

---
+++ test/integration/rewrites-destination-query-array/pages/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export function getServerSideProps(context) {
+  return {
+    props: {
+      query: context.query,
+    },
+  }
+}
+
+export default function Page(props) {
+  return (
+    <>
+      <p id="items">{props.query.items.join(',')}</p>
+    </>
+  )
+}
