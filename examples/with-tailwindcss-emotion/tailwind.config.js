5e8a7175f7ee21dac1936ed3083469db7da8f4f6
Asai Masami <shakeforme@gmail.com>
2021-06-27 10:33:37 +0900

    disable jit and tailwindcss fixed version 2.1.4

--- examples/with-tailwindcss-emotion/tailwind.config.js
+++ examples/with-tailwindcss-emotion/tailwind.config.js
@@ -1,7 +1,6 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
