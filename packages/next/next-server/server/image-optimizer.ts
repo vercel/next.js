582ad80924f6719d5fa891cca1c703f39984059c
Steven <steven@ceriously.com>
2021-06-28 19:11:49 -0400

    Fix 400 when upstream `content-type: octet-stream`

--- packages/next/next-server/server/image-optimizer.ts
+++ packages/next/next-server/server/image-optimizer.ts
@@ -434,27 +434,27 @@ function parseCacheControl(str: string | null): Map<string, string> {
  * https://en.wikipedia.org/wiki/List_of_file_signatures
  */
 function detectContentType(buffer: Buffer) {
-  if ([0xff, 0xd8, 0xff].every((i, b) => buffer[i] === b)) {
+  if ([0xff, 0xd8, 0xff].every((b, i) => buffer[i] === b)) {
     return JPEG
   }
   if (
     [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a].every(
-      (i, b) => buffer[i] === b
+      (b, i) => buffer[i] === b
     )
   ) {
     return PNG
   }
-  if ([0x47, 0x49, 0x46, 0x38].every((i, b) => buffer[i] === b)) {
+  if ([0x47, 0x49, 0x46, 0x38].every((b, i) => buffer[i] === b)) {
     return GIF
   }
   if (
     [0x52, 0x49, 0x46, 0x46, 0, 0, 0, 0, 0x57, 0x45, 0x42, 0x50].every(
-      (i, b) => buffer[i] === b
+      (b, i) => !b || buffer[i] === b
     )
   ) {
     return WEBP
   }
-  if ([0x3c, 0x3f, 0x78, 0x6d, 0x6c].every((i, b) => buffer[i] === b)) {
+  if ([0x3c, 0x3f, 0x78, 0x6d, 0x6c].every((b, i) => buffer[i] === b)) {
     return SVG
   }
   return null
