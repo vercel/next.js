1dd6eeb59892944eeb06e0fdffe3e868b77e0a71
Lee Robinson <me@leerob.io>
2021-06-28 22:19:15 -0500

    Update layouts example to persist state across client-side transitions. (#26706)
    * Update layouts example
    
    * Update examples/layout-component/components/layout.js
    
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

---
+++ examples/layout-component/components/sidebar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import styles from './sidebar.module.css'
+
+export default function Sidebar() {
+  return (
+    <nav className={styles.nav}>
+      <input className={styles.input} placeholder="Search..." />
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact</a>
+      </Link>
+    </nav>
+  )
+}
