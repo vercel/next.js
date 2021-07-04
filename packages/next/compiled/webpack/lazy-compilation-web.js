e7d284b5cbc3f284dc46cbfb5f6565c393f9b7c8
Tim Neutkens <timneutkens@me.com>
2021-07-04 12:53:39 +0200

    Update compiled

--- packages/next/compiled/webpack/lazy-compilation-web.js
+++ packages/next/compiled/webpack/lazy-compilation-web.js
@@ -3,7 +3,7 @@ module.exports =
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 949:
+/***/ 454:
 /***/ (function(__unused_webpack_module, exports) {
 
 var __resourceQuery = "";
@@ -123,6 +123,6 @@ exports.keepAlive = function (options) {
 /******/ 	// module exports must be returned from runtime so entry inlining is disabled
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	return __nccwpck_require__(949);
+/******/ 	return __nccwpck_require__(454);
 /******/ })()
 ;
