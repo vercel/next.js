4906b4a5b535cc4ef08f44c9c30a83f288d99cf0
Tim Neutkens <timneutkens@me.com>
2021-07-05 13:08:48 +0200

    Update precompiled

--- packages/next/compiled/webpack/webpack.js
+++ packages/next/compiled/webpack/webpack.js
@@ -1,20 +1,7 @@
 exports.__esModule = true
 
-exports.isWebpack5 = false
-
 exports.default = undefined
 
-let initializedWebpack5 = false
-let initializedWebpack4 = false
-let initFns = []
 exports.init = function () {
   Object.assign(exports, require('./bundle5')())
-  exports.isWebpack5 = true
-  if (!initializedWebpack5) for (const cb of initFns) cb()
-  initializedWebpack5 = true
-}
-
-exports.onWebpackInit = function (cb) {
-  if (initializedWebpack5 || initializedWebpack4) cb()
-  else initFns.push(cb)
 }
