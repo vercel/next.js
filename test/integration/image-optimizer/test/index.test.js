f76883dd9f309e172bdc0528875399ba7143a732
Steven <steven@ceriously.com>
2021-07-07 19:28:38 -0400

    Add test for `loader=custom` without implementation

--- test/integration/image-optimizer/test/index.test.js
+++ test/integration/image-optimizer/test/index.test.js
@@ -9,6 +9,7 @@ import {
   launchApp,
   nextBuild,
   nextStart,
+  renderViaHTTP,
   waitFor,
 } from 'next-test-utils'
 import isAnimated from 'next/dist/compiled/is-animated'
@@ -731,6 +732,33 @@ describe('Image Optimizer', () => {
 
       expect(stderr).toContain(
         'Specified images.loader should be one of (default, imgix, cloudinary, akamai, custom, dangerously-unoptimized), received invalid value (notreal)'
+      )
+    })
+
+    it('should error when loader=custom but loader prop is undefined', async () => {
+      await nextConfig.replace(
+        '{ /* replaceme */ }',
+        JSON.stringify({
+          images: {
+            loader: 'custom',
+          },
+        })
+      )
+      let output = ''
+      const appPort = await findPort()
+      app = await launchApp(appDir, appPort, {
+        onStderr(msg) {
+          output += msg || ''
+        },
+        onStdout(msg) {
+          output += msg || ''
+        },
+      })
+      await renderViaHTTP(appPort, '/', {})
+      await killApp(app).catch(() => {})
+      await nextConfig.restore()
+      expect(output).toContain(
+        'Error: Expected `loader` prop on `next/image` component'
       )
     })
   })
