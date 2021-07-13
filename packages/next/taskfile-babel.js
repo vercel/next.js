2ef8ba323dd7e46b17388fe249541f646b784357
Tim Neutkens <timneutkens@me.com>
2021-07-13 23:02:40 +0200

    Use SWC to compile Next.js core server files
    Just an initial comparison

--- packages/next/taskfile-babel.js
+++ packages/next/taskfile-babel.js
@@ -33,6 +33,15 @@ const babelClientOpts = {
     // eslint-disable-next-line no-useless-concat
     '@babel/plugin-syntax-dynamic-impor' + 't',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: false,
+        useESModules: false,
+      },
+    ],
   ],
   overrides: [
     {
@@ -90,21 +99,6 @@ module.exports = function (task) {
 
     const options = {
       ...babelOpts,
-      plugins: [
-        ...babelOpts.plugins,
-        // pages dir doesn't need core-js
-        serverOrClient === 'client'
-          ? [
-              '@babel/plugin-transform-runtime',
-              {
-                corejs: false,
-                helpers: true,
-                regenerator: false,
-                useESModules: false,
-              },
-            ]
-          : false,
-      ].filter(Boolean),
       compact: !dev,
       babelrc: false,
       configFile: false,
