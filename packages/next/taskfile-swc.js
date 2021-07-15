6e9a73320ae5c6e40f26984db0dcaa7ceba9adfa
Tim Neutkens <timneutkens@me.com>
2021-07-15 17:26:21 +0200

    Ensure Babel transform does not break SWC output

--- packages/next/taskfile-swc.js
+++ packages/next/taskfile-swc.js
@@ -22,22 +22,10 @@ module.exports = function (task) {
       module: {
         type: 'commonjs',
       },
-      env: {
-        targets: {
-          android: '61.0.0',
-          chrome: '61.0.0',
-          edge: '16.0.0',
-          firefox: '60.0.0',
-          ios: '10.3.0',
-          node: '13.2.0',
-          opera: '48.0.0',
-          safari: '10.1.0',
-          samsung: '8.2.0',
-        },
-      },
       jsc: {
         loose: true,
 
+        target: 'es2016',
         parser: {
           syntax: 'typescript',
           dynamicImport: true,
