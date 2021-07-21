0ea98b325529f6b3b952ed0badcdca72f5e1e9ad
Javi Velasco <javier.velasco86@gmail.com>
2021-07-21 10:36:25 +0200

    Document normalizeLocalePath

--- packages/next/shared/lib/i18n/normalize-locale-path.ts
+++ packages/next/shared/lib/i18n/normalize-locale-path.ts
@@ -3,6 +3,15 @@ export interface PathLocale {
   pathname: string
 }
 
+/**
+ * For a pathname that may include a locale from a list of locales, it
+ * removes the locale from the pathname returning it alongside with the
+ * detected locale.
+ *
+ * @param pathname A pathname that may include a locale.
+ * @param locales A list of locales.
+ * @returns The detected locale and pathname without locale
+ */
 export function normalizeLocalePath(
   pathname: string,
   locales?: string[]
