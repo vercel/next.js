34c5f5e050d597fba96d80f73524dd311a311a44
GP <ganesh.prasannah@oslash.com>
2021-07-23 16:18:06 +0530

    chore(tests): include tests for AVIF image optimization

--- test/integration/image-optimizer/test/index.test.js
+++ test/integration/image-optimizer/test/index.test.js
@@ -335,6 +335,23 @@ function runTests({ w, isDev, domains = [], ttl, isSharp }) {
     await expectWidth(res, w)
   })
 
+  if (isSharp) {
+    it('should resize relative url and Chrome accept header as avif', async () => {
+      const query = { url: '/test.avif', w, q: 80 }
+      const opts = {
+        headers: { accept: 'image/avif,image/webp,image/apng,image/*,*/*;q=0.8' },
+      }
+      const res = await fetchViaHTTP(appPort, '/_next/image', query, opts)
+      expect(res.status).toBe(200)
+      expect(res.headers.get('Content-Type')).toBe('image/avif')
+      expect(res.headers.get('cache-control')).toBe(
+        'public, max-age=0, must-revalidate'
+      )
+      expect(res.headers.get('etag')).toBeTruthy()
+      await expectWidth(res, w)
+    })
+  }
+
   if (domains.includes('localhost')) {
     it('should resize absolute url from localhost', async () => {
       const url = `http://localhost:${appPort}/test.png`
