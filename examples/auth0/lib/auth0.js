f6b28095f23998835cc8f135bd0ba2b93c193ad7
JJ Kasper <jj@jjsweb.site>
2021-07-25 00:03:17 -0500

    Update to favor note in readme

--- examples/auth0/lib/auth0.js
+++ examples/auth0/lib/auth0.js
@@ -2,7 +2,7 @@ import { initAuth0 } from '@auth0/nextjs-auth0'
 
 export default initAuth0({
   secret: process.env.SESSION_COOKIE_SECRET,
-  issuerBaseURL: `https://${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}`,
+  issuerBaseURL: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   clientID: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
