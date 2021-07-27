5eaca0ffe5d210c42ed40e6770680694ccea41c8
JJ Kasper <jj@jjsweb.site>
2021-07-27 15:09:36 -0500

    Document i18n locale limit (#27542)
    Add check for i18n locale count and ensure the limit is documented
    
    ## Documentation / Examples
    
    - [x] Make sure the linting passes

--- packages/next/server/config.ts
+++ packages/next/server/config.ts
@@ -320,6 +320,12 @@ function assignDefaults(userConfig: { [key: string]: any }) {
       )
     }
 
+    if (i18n.locales.length > 100) {
+      throw new Error(
+        `Received ${i18n.locales.length} i18n.locales items which exceeds the max of 100, please reduce the number of items to continue.\nSee more info here: https://nextjs.org/docs/messages/invalid-i18n-config`
+      )
+    }
+
     const defaultLocaleType = typeof i18n.defaultLocale
 
     if (!i18n.defaultLocale || defaultLocaleType !== 'string') {
