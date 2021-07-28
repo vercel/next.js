ba3cfc72ecc7db71e3930e1e13912726293ee39d
강동윤 <kdy1997.dev@gmail.com>
2021-07-28 14:25:57 +0900

    Add some methods to Sweeper

--- packages/next/build/swc/src/next_ssg.rs
+++ packages/next/build/swc/src/next_ssg.rs
@@ -1,4 +1,6 @@
 use fxhash::FxHashSet;
+use retain_mut::RetainMut;
+use swc_common::pass::{Repeat, Repeated};
 use swc_common::DUMMY_SP;
 use swc_ecmascript::ast::*;
 use swc_ecmascript::utils::find_ids;
@@ -82,7 +84,37 @@ struct State {
 }
 
 impl State {
-    fn is_data_identifier(&self, i: &Ident) -> bool {}
+    fn is_data_identifier(&mut self, i: &Ident) -> bool {
+        let ssg_exports = &[
+            "getStaticProps",
+            "getStaticPaths",
+            "getServerSideProps",
+            "unstable_getStaticProps",
+            "unstable_getStaticPaths",
+            "unstable_getServerProps",
+            "unstable_getServerSideProps",
+        ];
+
+        if ssg_exports.contains(&&*i.sym) {
+            if &*i.sym == "" {
+                if self.is_prerenderer {
+                    panic!("You can not use getStaticProps or getStaticPaths with getServerSideProps. To use SSG, please remove getServerSideProps")
+                }
+
+                self.is_server_props = true;
+            } else {
+                if self.is_server_props {
+                    panic!("You can not use getStaticProps or getStaticPaths with getServerSideProps. To use SSG, please remove getServerSideProps")
+                }
+
+                self.is_prerenderer = true;
+            }
+
+            true
+        } else {
+            false
+        }
+    }
 }
 
 struct Analyzer<'a> {
@@ -153,8 +185,8 @@ impl Fold for Analyzer<'_> {
                 return ModuleItem::ModuleDecl(ModuleDecl::ExportNamed(e));
             }
 
-            ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(e)) => {
-                match &e.decl {
+            ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(mut e)) => {
+                match &mut e.decl {
                     Decl::Fn(d) => {
                         if self.state.is_data_identifier(&d.ident) {
                             return ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP }));
@@ -170,6 +202,7 @@ impl Fold for Analyzer<'_> {
                             return ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP }));
                         }
                     }
+                    _ => {}
                 }
 
                 return ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(e.fold_with(self)));
@@ -208,6 +241,124 @@ impl Fold for Analyzer<'_> {
     }
 }
 
+/// Last pass.
+struct Sweeper<'a> {
+    state: &'a State,
+    should_run_again: bool,
+}
+
+impl Sweeper<'_> {
+    fn should_remove(&self, id: Id) -> bool {
+        self.state.refs.contains(&id) && !self.state.referenced_ids.contains(&id)
+    }
+}
+
+impl Fold for Sweeper<'_> {
+    // This is important for reducing binary sizes.
+    noop_fold_type!();
+
+    fn fold_import_decl(&mut self, mut i: ImportDecl) -> ImportDecl {
+        // Imports for side effects.
+        if i.specifiers.is_empty() {
+            return i;
+        }
+
+        i.specifiers.retain(|s| match s {
+            ImportSpecifier::Named(ImportNamedSpecifier { local, .. })
+            | ImportSpecifier::Default(ImportDefaultSpecifier { local, .. })
+            | ImportSpecifier::Namespace(ImportStarAsSpecifier { local, .. }) => {
+                if self.should_remove(local.to_id()) {
+                    self.should_run_again = true;
+                    false
+                } else {
+                    true
+                }
+            }
+        });
+
+        i
+    }
+
+    fn fold_module_item(&mut self, i: ModuleItem) -> ModuleItem {
+        match i {
+            ModuleItem::ModuleDecl(ModuleDecl::Import(i)) => {
+                let is_for_side_effect = i.specifiers.is_empty();
+                let i = i.fold_children_with(self);
+
+                if !is_for_side_effect && i.specifiers.is_empty() {
+                    return ModuleItem::Stmt(Stmt::Empty(EmptyStmt { span: DUMMY_SP }));
+                }
+
+                return ModuleItem::ModuleDecl(ModuleDecl::Import(i));
+            }
+            _ => {}
+        }
+
+        i.fold_children_with(self)
+    }
+
+    fn fold_stmt(&mut self, s: Stmt) -> Stmt {
+        match &s {
+            Stmt::Decl(Decl::Fn(f)) => {
+                if self.should_remove(f.ident.to_id()) {
+                    return Stmt::Empty(EmptyStmt { span: DUMMY_SP });
+                }
+            }
+            _ => {}
+        }
+
+        let s = s.fold_children_with(self);
+        match s {
+            Stmt::Decl(Decl::Var(v)) if v.decls.is_empty() => {
+                return Stmt::Empty(EmptyStmt { span: DUMMY_SP });
+            }
+            _ => {}
+        }
+
+        s
+    }
+
+    /// This method make `name` of [VarDeclarator] to [Pat::Invalid] if it should be removed.
+    fn fold_var_declarator(&mut self, mut d: VarDeclarator) -> VarDeclarator {
+        d = d.fold_children_with(self);
+
+        match &d.name {
+            Pat::Ident(name) => {
+                if self.should_remove(name.id.to_id()) {
+                    d.name = Pat::Invalid(Invalid { span: DUMMY_SP });
+                    return d;
+                }
+            }
+            Pat::Array(name) => {
+                // TODO
+            }
+            Pat::Object(name) => {
+                // TODO
+            }
+            _ => {}
+        }
+
+        d
+    }
+
+    fn fold_var_declarators(&mut self, mut decls: Vec<VarDeclarator>) -> Vec<VarDeclarator> {
+        decls = decls.fold_children_with(self);
+        decls.retain(|d| !d.name.is_invalid());
+
+        decls
+    }
+}
+
+impl Repeated for Sweeper<'_> {
+    fn changed(&self) -> bool {
+        self.should_run_again
+    }
+
+    fn reset(&mut self) {
+        self.should_run_again = false;
+    }
+}
+
 /// Actual implementation of the transform.
 struct NextSsg {
     state: State,
@@ -239,7 +390,10 @@ impl Fold for NextSsg {
             return m;
         }
 
-        let p = m.fold_children_with(self);
+        m.fold_children_with(&mut Repeat::new(Sweeper {
+            state: &self.state,
+            should_run_again: false,
+        }))
     }
 
     // Note: We don't implement fold_script because next.js doesn't use it.
