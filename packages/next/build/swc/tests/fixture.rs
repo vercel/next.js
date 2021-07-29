f87abad214a522046917041a001a6f31d426b5fd
강동윤 <kdy1997.dev@gmail.com>
2021-07-29 19:23:05 +0900

    Reorder items

--- packages/next/build/swc/tests/fixture.rs
+++ packages/next/build/swc/tests/fixture.rs
@@ -1,10 +1,11 @@
-#[path = "../src/next_ssg.rs"]
-mod next_ssg;
-
 use self::next_ssg::next_ssg;
 use std::path::PathBuf;
 use swc_ecma_transforms_testing::{test, test_fixture};
 use swc_ecmascript::parser::{EsConfig, Syntax};
+use testing::fixture;
+
+#[path = "../src/next_ssg.rs"]
+mod next_ssg;
 
 fn syntax() -> Syntax {
     Syntax::Es(EsConfig {
@@ -13,7 +14,7 @@ fn syntax() -> Syntax {
     })
 }
 
-#[testing::fixture("tests/fixture/ssg/**/input.js")]
+#[fixture("tests/fixture/ssg/**/input.js")]
 fn next_ssg_fixture(input: PathBuf) {
     let output = input.parent().unwrap().join("output.js");
     test_fixture(syntax(), &|_| next_ssg(), &input, &output);
