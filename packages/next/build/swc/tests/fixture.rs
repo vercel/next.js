33ed3f7364b7dc24933ba2b6ce8ec6e75b4336f5
강동윤 <kdy1997.dev@gmail.com>
2021-07-29 19:31:34 +0900

    Apply jsx pass

--- packages/next/build/swc/tests/fixture.rs
+++ packages/next/build/swc/tests/fixture.rs
@@ -1,7 +1,11 @@
 use self::next_ssg::next_ssg;
 use std::path::PathBuf;
+use swc_common::{chain, comments::SingleThreadedComments};
 use swc_ecma_transforms_testing::{test, test_fixture};
-use swc_ecmascript::parser::{EsConfig, Syntax};
+use swc_ecmascript::{
+    parser::{EsConfig, Syntax},
+    transforms::react::jsx,
+};
 use testing::fixture;
 
 #[path = "../src/next_ssg.rs"]
@@ -17,5 +21,28 @@ fn syntax() -> Syntax {
 #[fixture("tests/fixture/ssg/**/input.js")]
 fn next_ssg_fixture(input: PathBuf) {
     let output = input.parent().unwrap().join("output.js");
-    test_fixture(syntax(), &|_| next_ssg(), &input, &output);
+    test_fixture(
+        syntax(),
+        &|tr| {
+            let jsx = jsx::<SingleThreadedComments>(
+                tr.cm.clone(),
+                None,
+                swc_ecmascript::transforms::react::Options {
+                    next: false,
+                    runtime: None,
+                    import_source: "".into(),
+                    pragma: "__jsx".into(),
+                    pragma_frag: "__jsxFrag".into(),
+                    throw_if_namespace: false,
+                    development: false,
+                    use_builtins: true,
+                    use_spread: true,
+                    refresh: Default::default(),
+                },
+            );
+            chain!(next_ssg(), jsx)
+        },
+        &input,
+        &output,
+    );
 }
