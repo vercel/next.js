2639a9afadb9c5a1d6eb0afdd60a8addc186aabb
강동윤 <kdy1997.dev@gmail.com>
2021-07-29 19:15:57 +0900

    Add a fixture-based testing system

---
+++ packages/next/build/swc/tests/fixture.rs
@@ -0,0 +1,20 @@
+#[path = "../src/next_ssg.rs"]
+mod next_ssg;
+
+use self::next_ssg::next_ssg;
+use std::path::PathBuf;
+use swc_ecma_transforms_testing::{test, test_fixture};
+use swc_ecmascript::parser::{EsConfig, Syntax};
+
+fn syntax() -> Syntax {
+    Syntax::Es(EsConfig {
+        jsx: true,
+        ..Default::default()
+    })
+}
+
+#[testing::fixture("tests/fixture/ssg/**/input.js")]
+fn next_ssg_fixture(input: PathBuf) {
+    let output = input.parent().unwrap().join("output.js");
+    test_fixture(syntax(), &|_| next_ssg(), &input, &output);
+}
