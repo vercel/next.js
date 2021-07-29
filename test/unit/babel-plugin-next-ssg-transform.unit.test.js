9d3efb5f8d15152d2c4674aafcf2337d6121f0c6
강동윤 <kdy1997.dev@gmail.com>
2021-07-29 19:28:01 +0900

    Move one more test

--- test/unit/babel-plugin-next-ssg-transform.unit.test.js
+++ test/unit/babel-plugin-next-ssg-transform.unit.test.js
@@ -24,24 +24,6 @@ const swc = async (code) => {
 
 describe('babel plugin (next-ssg-transform)', () => {
   describe('getStaticProps support', () => {
-    it('should not remove extra named export function declarations', async () => {
-      const output = await swc(trim`
-        export function getStaticProps() {
-          return { props: {} }
-        }
-
-        export function Noop() {}
-
-        export default function Test() {
-          return <div />
-        }
-      `)
-
-      expect(output).toMatchInlineSnapshot(
-        `"export var __N_SSG=true;export function Noop(){}export default function Test(){return __jsx(\\"div\\",null);}"`
-      )
-    })
-
     it('should remove named export variable declarations', async () => {
       const output = await swc(trim`
         export const getStaticPaths = () => {
