e6b2272cd8c80f905d42289e279aa5dd575d5a7d
smakosh <ismai23l@hotmail.com>
2021-08-02 03:32:44 -0700

    chore(examples): switch to Script component on the google analytics example

--- examples/with-google-analytics/pages/_app.js
+++ examples/with-google-analytics/pages/_app.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 
@@ -14,7 +15,30 @@ const App = ({ Component, pageProps }) => {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
