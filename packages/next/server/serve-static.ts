43839a0ed3753b5fb7c3ad005865faff35b5934e
Alula <6276139+alula@users.noreply.github.com>
2021-08-02 15:38:22 +0200

    add missing catch in serve-static.ts

--- packages/next/server/serve-static.ts
+++ packages/next/server/serve-static.ts
@@ -7,16 +7,20 @@ export function serveStatic(
   path: string
 ): Promise<void> {
   return new Promise((resolve, reject) => {
-    send(req, path)
-      .on('directory', () => {
-        // We don't allow directories to be read.
-        const err: any = new Error('No directory access')
-        err.code = 'ENOENT'
-        reject(err)
-      })
-      .on('error', reject)
-      .pipe(res)
-      .on('finish', resolve)
+    try {
+      send(req, path)
+        .on('directory', () => {
+          // We don't allow directories to be read.
+          const err: any = new Error('No directory access')
+          err.code = 'ENOENT'
+          reject(err)
+        })
+        .on('error', reject)
+        .pipe(res)
+        .on('finish', resolve)
+    } catch(e) {
+      reject(e)
+    }
   })
 }
 
