8d721978c8fd604a3f6a6a5722b4be181881758e
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-08-09 21:44:05 +0100

    Short term fix for Cypress tests to pass Next.js repo linting
    Alternative is to install ESLint Cypress Plugin in the root directory (https://github.com/cypress-io/eslint-plugin-cypress).

--- examples/with-cypress/cypress/integration/app.spec.js
+++ examples/with-cypress/cypress/integration/app.spec.js
@@ -1,3 +1,8 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
