032cbb924eb5153c71a4f9b04d544b48e6dd2a38
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-08-09 16:50:54 +0100

    Add module alias configuration example

--- examples/with-jest/jest.config.js
+++ examples/with-jest/jest.config.js
@@ -15,6 +15,9 @@ module.exports = {
     // Handle image imports
     // https://jestjs.io/docs/webpack#handling-static-assets
     '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle module aliases
+    '^@/components/(.*)$': '<rootDir>/components/$1', 
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
