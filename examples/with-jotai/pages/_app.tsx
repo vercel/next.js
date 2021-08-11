84712d71073feface25880b6b6916aab38bb6578
Mathis Møller <thisen-dk@hotmail.com>
2021-08-11 19:36:28 +0200

    Apply suggestions from code review
    Co-authored-by: Lee Robinson <me@leerob.io>

--- examples/with-jotai/pages/_app.tsx
+++ examples/with-jotai/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-export default MyApp
