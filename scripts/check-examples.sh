cc0ce7a41f2c0dfdac1cab7b64d53b533fcf8def
Oscar Busk <oscar.busk@gmail.com>
2021-08-12 11:39:10 +0200

    Only copy .gitignores to examples that are actual packages.
    `custom-server` is just a `.md` redirecting users to the documentation.

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for folder in examples/* ; do
-  cp -n packages/create-next-app/templates/default/gitignore $folder/.gitignore;
   if [ -f "$folder/package.json" ]; then
+    cp -n packages/create-next-app/templates/default/gitignore $folder/.gitignore;
     cat $folder/package.json | jq '.license = "MIT"' | sponge $folder/package.json
   fi
 done;
