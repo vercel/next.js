2e71a4fb77167360473fbf941bd85e9707a570cc
즈눅 <xnuk@users.noreply.github.com>
2021-08-13 02:07:11 +0900

    Fix `generateBuildId` type that can be async

--- packages/next/server/config-shared.ts
+++ packages/next/server/config-shared.ts
@@ -70,7 +70,7 @@ export type NextConfig = { [key: string]: any } & {
   cleanDistDir?: boolean
   assetPrefix?: string
   useFileSystemPublicRoutes?: boolean
-  generateBuildId?: () => string | null
+  generateBuildId?: () => string | null | Promise<string | null>
   generateEtags?: boolean
   pageExtensions?: string[]
   compress?: boolean
