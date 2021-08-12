80666429dd54e418103a85a083e1ad4aa80549e9
Maia Teegarden <dev@padmaia.rocks>
2021-08-11 17:29:33 -0700

    Add try catch in publish script

--- scripts/publish-native.js
+++ scripts/publish-native.js
@@ -9,65 +9,71 @@ const exec = util.promisify(require('child_process').exec)
 const cwd = process.cwd()
 
 ;(async function () {
-  let version = JSON.parse(await readFile(path.join(cwd, 'lerna.json'))).version
-  let gitref = process.argv.slice(2)[0]
+  try {
+    let version = JSON.parse(await readFile(path.join(cwd, 'lerna.json')))
+      .version
+    let gitref = process.argv.slice(2)[0]
 
-  // Copy binaries to package folders, update version, and publish
-  let nativePackagesDir = path.join(cwd, 'packages/next/build/swc/npm')
-  let nativePackages = await readdir(nativePackagesDir)
-  let publishPromises = []
-  for (let nativePackage of nativePackages) {
-    if (nativePackage === '.gitignore') {
-      continue
+    // Copy binaries to package folders, update version, and publish
+    let nativePackagesDir = path.join(cwd, 'packages/next/build/swc/npm')
+    let nativePackages = await readdir(nativePackagesDir)
+    let publishPromises = []
+    for (let nativePackage of nativePackages) {
+      if (nativePackage === '.gitignore') {
+        continue
+      }
+      let binaryName = `next-swc.${nativePackage.substr(9)}.node`
+      await copy(
+        path.join(cwd, 'packages/next/build/swc/dist', binaryName),
+        path.join(nativePackagesDir, nativePackage, binaryName)
+      )
+      let pkg = JSON.parse(
+        await readFile(
+          path.join(nativePackagesDir, nativePackage, 'package.json')
+        )
+      )
+      pkg.version = version
+      await writeFile(
+        path.join(nativePackagesDir, nativePackage, 'package.json'),
+        JSON.stringify(pkg, null, 2)
+      )
+      publishPromises.push(
+        exec(
+          `npm publish ${path.join(nativePackagesDir, nativePackage)}${
+            gitref.contains('canary') ? ' --tag canary' : ''
+          }`
+        )
+      )
+      // lerna publish in next step will fail if git status is not clean
+      publishPromises.push(
+        exec(
+          `git update-index --skip-worktree ${path.join(
+            nativePackagesDir,
+            nativePackage,
+            'package.json'
+          )}`
+        )
+      )
     }
-    let binaryName = `next-swc.${nativePackage.substr(9)}.node`
-    await copy(
-      path.join(cwd, 'packages/next/build/swc/dist', binaryName),
-      path.join(nativePackagesDir, nativePackage, binaryName)
+    await Promise.all(publishPromises)
+
+    // Update optional dependencies versions
+    let nextPkg = JSON.parse(
+      await readFile(path.join(cwd, 'packages/next/package.json'))
     )
-    let pkg = JSON.parse(
-      await readFile(
-        path.join(nativePackagesDir, nativePackage, 'package.json')
-      )
-    )
-    pkg.version = version
+    for (let name of nativePackages) {
+      let optionalDependencies = nextPkg.optionalDependencies || {}
+      optionalDependencies[name] = version
+      nextPkg.optionalDependencies = optionalDependencies
+    }
     await writeFile(
-      path.join(nativePackagesDir, nativePackage, 'package.json'),
-      JSON.stringify(pkg, null, 2)
-    )
-    publishPromises.push(
-      exec(
-        `npm publish ${path.join(nativePackagesDir, nativePackage)}${
-          gitref.contains('canary') ? ' --tag canary' : ''
-        }`
-      )
+      path.join(path.join(cwd, 'packages/next/package.json')),
+      JSON.stringify(nextPkg, null, 2)
     )
     // lerna publish in next step will fail if git status is not clean
-    publishPromises.push(
-      exec(
-        `git update-index --skip-worktree ${path.join(
-          nativePackagesDir,
-          nativePackage,
-          'package.json'
-        )}`
-      )
-    )
+    await exec('git update-index --skip-worktree packages/next/package.json')
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
   }
-  await Promise.all(publishPromises)
-
-  // Update optional dependencies versions
-  let nextPkg = JSON.parse(
-    await readFile(path.join(cwd, 'packages/next/package.json'))
-  )
-  for (let name of nativePackages) {
-    let optionalDependencies = nextPkg.optionalDependencies || {}
-    optionalDependencies[name] = version
-    nextPkg.optionalDependencies = optionalDependencies
-  }
-  await writeFile(
-    path.join(path.join(cwd, 'packages/next/package.json')),
-    JSON.stringify(nextPkg, null, 2)
-  )
-  // lerna publish in next step will fail if git status is not clean
-  await exec('git update-index --skip-worktree packages/next/package.json')
 })()
