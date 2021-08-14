56366795cf59cc3fe93bbfd5e4b555f384c01acd
Luan Devecchi <luan@engineer.com>
2021-08-14 00:49:47 -0300

    Preserve `next-env.d.ts` line ending
    Prevent next from changing already existing line ending on
    `next-env.d.ts` for no good reason

--- packages/next/lib/typescript/writeAppTypeDeclarations.ts
+++ packages/next/lib/typescript/writeAppTypeDeclarations.ts
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs'
+import { constants as FS, promises as fs } from 'fs'
 import os from 'os'
 import path from 'path'
 
@@ -9,19 +9,39 @@ export async function writeAppTypeDeclarations(
   // Reference `next` types
   const appTypeDeclarations = path.join(baseDir, 'next-env.d.ts')
 
+  // Defaults EOL to system default
+  let eol = os.EOL
+
+  try {
+    await fs.access(appTypeDeclarations, FS.F_OK)
+    const buffer = await fs.readFile(appTypeDeclarations)
+    const fileContent = buffer.toString()
+
+    // If file already exists then preserve its line ending
+    const lf = fileContent.indexOf('\n', /* skip first so we can lf - 1 */ 1)
+
+    if (lf != -1) {
+      if (fileContent[lf - 1] === '\r') {
+        eol = '\r\n'
+      } else {
+        eol = '\n'
+      }
+    }
+  } catch (err) {}
+
   await fs.writeFile(
     appTypeDeclarations,
     '/// <reference types="next" />' +
-      os.EOL +
+      eol +
       '/// <reference types="next/types/global" />' +
-      os.EOL +
+      eol +
       (imageImportsEnabled
-        ? '/// <reference types="next/image-types/global" />' + os.EOL
+        ? '/// <reference types="next/image-types/global" />' + eol
         : '') +
-      os.EOL +
+      eol +
       '// NOTE: This file should not be edited' +
-      os.EOL +
+      eol +
       '// see https://nextjs.org/docs/basic-features/typescript for more information.' +
-      os.EOL
+      eol
   )
 }
