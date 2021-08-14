57253d5e4e58be5944ebb7e1d143cc65f32ddde3
Oscar Busk <oscar.busk@gmail.com>
2021-08-14 14:26:00 +0200

    Start breaking the jq script into multiple lines

--- scripts/check-examples.sh
+++ scripts/check-examples.sh
@@ -3,7 +3,10 @@
 for folder in examples/* ; do
   if [ -f "$folder/package.json" ]; then
     cp -n packages/create-next-app/templates/default/gitignore $folder/.gitignore;
-    cat $folder/package.json | jq '.license = "MIT" | .private = true' | sponge $folder/package.json
+    cat $folder/package.json | jq '
+      .license = "MIT" |
+      .private = true
+    ' | sponge $folder/package.json
   fi
   if [ -f "$folder/tsconfig.json" ]; then
     cp packages/create-next-app/templates/typescript/next-env.d.ts $folder/next-env.d.ts
