44e098a6424d2cea2f6f15a8e8242b9f0764e96b
Houssein Djirdeh <houssein@google.com>
2021-08-18 13:07:14 -0400

    handle edge case for no-import-document rule

--- test/eslint-plugin-next/no-document-import-in-page.unit.test.js
+++ test/eslint-plugin-next/no-document-import-in-page.unit.test.js
@@ -89,18 +89,37 @@ ruleTester.run('no-document-import-in-page', rule, {
     {
       code: `import Document from "next/document"
 
-      export const Test = () => <p>Test</p>
-      `,
-      filename: 'components/test.js',
+    export default class MyDocument extends Document {
+      render() {
+        return (
+          <Html>
+          </Html>
+        );
+      }
+    }
+    `,
+      filename: 'pagesapp/src/pages/_document.js',
     },
   ],
   invalid: [
     {
       code: `import Document from "next/document"
 
-      export const Test = () => (
-          <p>Test</p>
-      )
+      export const Test = () => <p>Test</p>
+      `,
+      filename: 'components/test.js',
+      errors: [
+        {
+          message:
+            'next/document should not be imported outside of pages/_document.js. See https://nextjs.org/docs/messages/no-document-import-in-page.',
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: `import Document from "next/document"
+
+      export const Test = () => <p>Test</p>
       `,
       filename: 'pages/test.js',
       errors: [
