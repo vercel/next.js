06624b54d57bf164efe15e5f08df2548a1ddfbd3
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2021-08-20 14:26:20 -0400

    Update examples/with-temporal/components/ListItem.tsx
    Co-authored-by: Lee Robinson <me@leerob.io>

--- examples/with-temporal/components/ListItem.tsx
+++ examples/with-temporal/components/ListItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const ListItem = ({ data }: Props) => (
-  <Link href="/users/[id]" as={`/users/${data.id}`}>
+  <Link href={`/users/${data.id}`}>
     <a>
       {data.id}: {data.name}
     </a>
