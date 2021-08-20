c51f232512363d651d7f5a69138abe9fd6058e1c
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2021-08-20 14:24:23 -0400

    Remove unnecessary meta tags from Layout
    Co-authored-by: Lee Robinson <me@leerob.io>

--- examples/with-temporal/components/Layout.tsx
+++ examples/with-temporal/components/Layout.tsx
@@ -11,8 +11,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
       <nav>
