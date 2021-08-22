b1b5cbdeba823eea9f0d81e40b88a080e14d16a8
jj@jjsweb.site <jj@jjsweb.site>
2021-08-22 16:35:28 -0500

    Update test config to leverage swc

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,39 @@ module.exports = {
   globalTeardown: '<rootDir>/jest-global-teardown.js',
   setupFilesAfterEnv: ['<rootDir>/jest-setup-after-env.js'],
   testEnvironment: '<rootDir>/jest-environment.js',
+  transformIgnorePatterns: ['/node_modules/', '/next[/\\\\]dist/'],
+  transform: {
+    '.+\\.(t|j)sx?$': [
+      // this matches our SWC options used in https://github.com/vercel/next.js/blob/canary/packages/next/taskfile-swc.js
+      '@swc/jest',
+      {
+        module: {
+          type: 'commonjs',
+        },
+        env: {
+          targets: {
+            node: '12.0.0',
+          },
+        },
+        jsc: {
+          loose: true,
+
+          parser: {
+            syntax: 'typescript',
+            dynamicImport: true,
+            tsx: true,
+          },
+          transform: {
+            react: {
+              pragma: 'React.createElement',
+              pragmaFrag: 'React.Fragment',
+              throwIfNamespace: true,
+              development: false,
+              useBuiltins: true,
+            },
+          },
+        },
+      },
+    ],
+  },
 }
