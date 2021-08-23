9aefc344d26bf7b143b500301d54c9f96e8d3baa
Hunter Tunnicliff <htunnicliff@interos.ai>
2021-08-23 09:33:17 -0700

    Use recommended pattern in test

--- examples/with-jest/__tests__/testing-library.js
+++ examples/with-jest/__tests__/testing-library.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Index from '../pages/index'
 
 describe('App', () => {
   it('renders a heading', () => {
-    const { getByRole } = render(<Index />)
+    render(<Index />)
 
-    const heading = getByRole('heading', {
+    const heading = screen.getByRole('heading', {
       name: /welcome to next\.js!/i,
     })
 
