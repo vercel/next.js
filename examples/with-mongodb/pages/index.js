0e4263d32f5e04790506251d98e56f98120bc4ef
jj@jjsweb.site <jj@jjsweb.site>
2021-08-25 12:45:09 -0500

    lint-fix

--- examples/with-mongodb/pages/index.js
+++ examples/with-mongodb/pages/index.js
@@ -226,7 +226,7 @@ export async function getServerSideProps(context) {
   const client = await clientPromise
 
   // client.db() will be the default database passed in the MONGODB_URI
-  // You can change the database by calling the client.db() function and specifying a database like: 
+  // You can change the database by calling the client.db() function and specifying a database like:
   // const db = client.db("myDatabase");
   // Then you can execute queries against your database like so:
   // db.find({}) or any of the MongoDB Node Driver commands
