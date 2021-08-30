72a645d857c980bcfb2be602d6992e9487b564ac
jj@jjsweb.site <jj@jjsweb.site>
2021-08-30 16:20:58 -0500

    Ensure custom app regex is correct for Windows

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -1561,7 +1561,7 @@ export default async function getBaseWebpackConfig(
 
   webpackConfig = await buildConfiguration(webpackConfig, {
     rootDirectory: dir,
-    customAppFile: new RegExp(path.join(pagesDir, `_app`)),
+    customAppFile: new RegExp(path.join(pagesDir, `_app`).replace(/\\/g, '(/|\\\\)')),
     isDevelopment: dev,
     isServer,
     assetPrefix: config.assetPrefix || '',
