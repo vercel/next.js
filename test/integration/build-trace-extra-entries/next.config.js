9a6542ba683b7ee4bd566af0dca06bb6b72835a7
JJ Kasper <jj@jjsweb.site>
2021-09-01 10:56:04 -0500

    Ensure build trace is correct with extra entries (#28667)
    * Ensure build trace is correct with extra entries
    
    * lint-fix
    
    * fix check on windows

---
+++ test/integration/build-trace-extra-entries/next.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  experimental: {
+    nftTracing: true,
+  },
+  webpack(cfg, { isServer }) {
+    console.log(cfg.entry)
+    const origEntry = cfg.entry
+    cfg.entry = async () => {
+      const origEntries = await origEntry()
+
+      if (isServer) {
+        const curEntry = origEntries['pages/_app']
+        origEntries['pages/_app'] = [
+          path.resolve('lib/get-data.js'),
+          ...curEntry,
+        ]
+        console.log(origEntries)
+      }
+      return origEntries
+    }
+    return cfg
+  },
+}
