a43370a016eceed059e827ce93573c2c3e5a6516
jj@jjsweb.site <jj@jjsweb.site>
2021-09-04 10:55:17 -0500

    update timeout in CI

--- test/jest-setup-after-env.ts
+++ test/jest-setup-after-env.ts
@@ -1,3 +1,9 @@
 // A default max-timeout of 1 minute is allowed
 // we should aim to bring this down though
-jest.setTimeout(60 * 1000)
+
+if (process.env.CI) {
+  // give an extra 30 seconds in CI for slower resources
+  jest.setTimeout(90 * 1000)
+} else {
+  jest.setTimeout(60 * 1000)
+}
