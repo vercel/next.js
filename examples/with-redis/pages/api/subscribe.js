fea3d8fd612184430a04cad5cbcf0f8f672a34e0
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2021-09-10 16:15:03 +0700

    Support UTF-8 Email

--- examples/with-redis/pages/api/subscribe.js
+++ examples/with-redis/pages/api/subscribe.js
@@ -17,6 +17,6 @@ export default async function upvote(req, res) {
 
 function validateEmail(email) {
   const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-zA-Z\-0-9]+\.)+[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFa-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
 }
