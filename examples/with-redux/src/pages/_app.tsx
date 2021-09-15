c5456c1037b6f521d05d943bac84f34d426b4592
Lee Robinson <lrobinson2011@gmail.com>
2021-09-15 08:54:06 -0500

    Update Redux example to use Toolkit + update dependencies.

--- examples/with-redux/src/pages/_app.tsx
+++ examples/with-redux/src/pages/_app.tsx
@@ -5,12 +5,10 @@ import type { AppProps } from 'next/app'
 
 import store from '../app/store'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   )
 }
-
-export default MyApp
