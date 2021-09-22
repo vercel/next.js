60f198584d808c18a73ecee7a01403d7bf9488d1
Tim Neutkens <tim@timneutkens.nl>
2021-09-22 07:20:18 +0200

    Add internal mode for debugging Next.js core webpack (#29254)
    Co-authored-by: jj@jjsweb.site <jj@jjsweb.site>

--- packages/next/compiled/webpack/webpack.js
+++ packages/next/compiled/webpack/webpack.js
@@ -8,7 +8,25 @@ let initializedWebpack5 = false
 let initializedWebpack4 = false
 let initFns = []
 exports.init = function (useWebpack5) {
-  if (useWebpack5) {
+  if (process.env.NEXT_PRIVATE_LOCAL_WEBPACK5 && useWebpack5) {
+    Object.assign(exports, {
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      BasicEvaluatedExpression: require('webpack/lib/javascript/BasicEvaluatedExpression'),
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      ModuleFilenameHelpers: require('webpack/lib/ModuleFilenameHelpers'),
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      NodeTargetPlugin: require('webpack/lib/node/NodeTargetPlugin'),
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      StringXor: require('webpack/lib/util/StringXor'),
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      NormalModule: require('webpack/lib/NormalModule'),
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      sources: require('webpack').sources,
+      // eslint-disable-next-line import/no-extraneous-dependencies
+      webpack: require('webpack'),
+    })
+    exports.isWebpack5 = true
+  } else if (useWebpack5) {
     Object.assign(exports, require('./bundle5')())
     exports.isWebpack5 = true
     if (!initializedWebpack5) for (const cb of initFns) cb()
