d5b2eac2f3b071275393976320a6e4583cb0188e
Janicklas Ralph James <janicklas@google.com>
2021-09-21 18:48:07 -0700

    Update with-segment-analytics example to use next/script component

--- examples/with-segment-analytics/pages/_app.js
+++ examples/with-segment-analytics/pages/_app.js
@@ -1,8 +1,29 @@
 import Page from '../components/Page'
+import Script from 'next/script'
+import * as snippet from '@segment/snippet'
+
+function renderSnippet() {
+  const opts = {
+    apiKey:
+      process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY ||
+      'NPsk1GimHq09s7egCUlv7D0tqtUAU5wa',
+    // note: the page option only covers SSR tracking.
+    // Page.js is used to track other events using `window.analytics.page()`
+    page: true,
+  }
+
+  if (process.env.NEXT_PUBLIC_NODE_ENV === 'development') {
+    return snippet.max(opts)
+  }
+
+  return snippet.min(opts)
+}
 
 function MyApp({ Component, pageProps }) {
   return (
     <Page>
+      {/* Inject the Segment snippet into the <head> of the document  */}
+      <Script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
       <Component {...pageProps} />
     </Page>
   )
