6f5bea1b7226d212ed6f3c57506171c24915dfe6
Tim Neutkens <timneutkens@me.com>
2021-09-22 07:38:51 +0200

    Update webpack5 import when using local webpack version

--- packages/next/compiled/webpack/webpack.js
+++ packages/next/compiled/webpack/webpack.js
@@ -11,19 +11,19 @@ exports.init = function (useWebpack5) {
   if (process.env.NEXT_PRIVATE_LOCAL_WEBPACK5 && useWebpack5) {
     Object.assign(exports, {
       // eslint-disable-next-line import/no-extraneous-dependencies
-      BasicEvaluatedExpression: require('webpack/lib/javascript/BasicEvaluatedExpression'),
+      BasicEvaluatedExpression: require('webpack5/lib/javascript/BasicEvaluatedExpression'),
       // eslint-disable-next-line import/no-extraneous-dependencies
-      ModuleFilenameHelpers: require('webpack/lib/ModuleFilenameHelpers'),
+      ModuleFilenameHelpers: require('webpack5/lib/ModuleFilenameHelpers'),
       // eslint-disable-next-line import/no-extraneous-dependencies
-      NodeTargetPlugin: require('webpack/lib/node/NodeTargetPlugin'),
+      NodeTargetPlugin: require('webpack5/lib/node/NodeTargetPlugin'),
       // eslint-disable-next-line import/no-extraneous-dependencies
-      StringXor: require('webpack/lib/util/StringXor'),
+      StringXor: require('webpack5/lib/util/StringXor'),
       // eslint-disable-next-line import/no-extraneous-dependencies
-      NormalModule: require('webpack/lib/NormalModule'),
+      NormalModule: require('webpack5/lib/NormalModule'),
       // eslint-disable-next-line import/no-extraneous-dependencies
-      sources: require('webpack').sources,
+      sources: require('webpack5').sources,
       // eslint-disable-next-line import/no-extraneous-dependencies
-      webpack: require('webpack'),
+      webpack: require('webpack5'),
     })
     exports.isWebpack5 = true
   } else if (useWebpack5) {
