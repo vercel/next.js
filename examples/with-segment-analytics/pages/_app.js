f001df4b0dcca7b94d2ca81c01eb3ff2bf29feb0
Janicklas Ralph James <janicklas@google.com>
2021-09-21 20:47:41 -0700

    Fix review comment

--- examples/with-segment-analytics/pages/_app.js
+++ examples/with-segment-analytics/pages/_app.js
@@ -2,11 +2,12 @@ import Page from '../components/Page'
 import Script from 'next/script'
 import * as snippet from '@segment/snippet'
 
+// This write key is associated with https://segment.com/nextjs-example/sources/nextjs.
+const DEFAULT_WRITE_KEY = 'NPsk1GimHq09s7egCUlv7D0tqtUAU5wa'
+
 function renderSnippet() {
   const opts = {
-    apiKey:
-      process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY ||
-      'NPsk1GimHq09s7egCUlv7D0tqtUAU5wa',
+    apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY || DEFAULT_WRITE_KEY,
     // note: the page option only covers SSR tracking.
     // Page.js is used to track other events using `window.analytics.page()`
     page: true,
