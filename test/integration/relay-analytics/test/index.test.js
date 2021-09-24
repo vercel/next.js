741ef9032cb937a96c01ee148ec28f3167551f35
Jiachi Liu <inbox@huozhi.im>
2021-09-25 02:35:33 +0800

    Experimental: hook version of reportWebVitals (#28769)
    ### Experimental
    
    Introduce `next/vitals` and `useExperimentalWebVitalsReport` API which is not limited by `_app`.
    
    `pages/index.js`
    ```jsx
    import { useExperimentalWebVitalsReport } from 'next/vitals'
    
    export default function Index() {
       useExperimentalWebVitalsReport((metric) => {
        // handle metric...
      })
      return 'sup'
    }
    ```

--- test/integration/relay-analytics/test/index.test.js
+++ test/integration/relay-analytics/test/index.test.js
@@ -2,27 +2,70 @@
 
 import { join } from 'path'
 import webdriver from 'next-webdriver'
-import { killApp, findPort, nextBuild, nextStart, check } from 'next-test-utils'
+import {
+  File,
+  killApp,
+  findPort,
+  nextBuild,
+  nextStart,
+  check,
+} from 'next-test-utils'
 
 const appDir = join(__dirname, '../')
+const customApp = new File(join(appDir, 'pages/_app.js'))
+const indexPage = new File(join(appDir, 'pages/index.js'))
+
 let appPort
 let server
+let stdout
+jest.setTimeout(1000 * 60 * 2)
 
-describe('Analytics relayer', () => {
-  let stdout
+async function buildApp() {
+  appPort = await findPort()
+  ;({ stdout } = await nextBuild(appDir, [], {
+    env: { VERCEL_ANALYTICS_ID: 'test' },
+    stdout: true,
+  }))
+  server = await nextStart(appDir, appPort)
+}
+async function killServer() {
+  await killApp(server)
+}
+
+describe('Analytics relayer with exported method', () => {
   beforeAll(async () => {
-    appPort = await findPort()
-    ;({ stdout } = await nextBuild(appDir, [], {
-      env: { VERCEL_ANALYTICS_ID: 'test' },
-      stdout: true,
-    }))
-    server = await nextStart(appDir, appPort)
+    // Keep app exported reporting and comment the hook one
+    indexPage.replace('///* useExperimentalWebVitalsReport', '/*')
+    indexPage.replace('// useExperimentalWebVitalsReport */', '*/')
+    await buildApp()
   })
-  afterAll(() => killApp(server))
+  afterAll(async () => {
+    indexPage.restore()
+    await killServer()
+  })
+  runTest()
+})
 
+describe('Analytics relayer with hook', () => {
+  beforeAll(async () => {
+    // Keep hook reporting and comment the app exported one
+    customApp.replace('///* reportWebVitals', '/*')
+    customApp.replace('// reportWebVitals */', '*/')
+    await buildApp()
+  })
+
+  afterAll(async () => {
+    customApp.restore()
+    await killServer()
+  })
+  runTest()
+})
+
+function runTest() {
   it('Relays the data to user code', async () => {
     const browser = await webdriver(appPort, '/')
     await browser.waitForElementByCss('h1')
+
     const h1Text = await browser.elementByCss('h1').text()
     const data = parseFloat(
       await browser.eval('localStorage.getItem("Next.js-hydration")')
@@ -46,6 +89,12 @@ describe('Analytics relayer', () => {
     expect(firstContentfulPaint).toBeGreaterThan(0)
     expect(largestContentfulPaint).toBeNull()
     expect(cls).toBeNull()
+
+    const beaconsCountBeforeCLS = await browser.eval('window.__BEACONS_COUNT')
+    expect(
+      Object.values(beaconsCountBeforeCLS).every((value) => value === 1)
+    ).toBe(true)
+
     // Create an artificial layout shift
     await browser.eval('document.querySelector("h1").style.display = "none"')
     await browser.refresh()
@@ -69,6 +118,11 @@ describe('Analytics relayer', () => {
       Object.fromEntries(new URLSearchParams(value))
     )
 
+    const beaconsCountAfterCLS = await browser.eval('window.__BEACONS_COUNT')
+    expect(
+      Object.values(beaconsCountAfterCLS).every((value) => value === 2)
+    ).toBe(true)
+
     expect(beacons.length).toBe(2)
 
     for (const beacon of beacons) {
@@ -86,4 +140,4 @@ describe('Analytics relayer', () => {
     expect(stdout).toMatch('Next.js Analytics')
     await browser.close()
   })
-})
+}
