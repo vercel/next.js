22273ac79abf197eeace75d57219846e000b3a1b
Tim Neutkens <timneutkens@me.com>
2021-09-27 13:24:54 +0200

    Remove unsafeCache from benchmark + use cross-env for benchmark
    Fixes from @sokra's review of #29325

--- bench/nested-deps/next.config.js
+++ bench/nested-deps/next.config.js
@@ -2,10 +2,6 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack(config) {
-    config.module.unsafeCache = true
-    return config
-  },
   experimental: {
     swcLoader: true,
     swcMinify: true,
