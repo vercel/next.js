5fab08f643c891712eba84e0f5b67028db6f428f
Donny <kdy1997.dev@gmail.com>
2021-09-28 22:10:06 +0900

    Update test refs

---
+++ packages/next/build/swc/tests/fixture/styled-jsx/tpl-placeholder-1/output.js
@@ -0,0 +1,28 @@
+import _JSXStyle from "styled-jsx/style";
+export default class {
+    render() {
+        return <div className={_JSXStyle.dynamic([
+            [
+                "8b268cf2c0a3f89b",
+                [
+                    inputSize ? "height: calc(2 * var(--a)) !important;" : ""
+                ]
+            ]
+        ])}>
+
+          <p className={_JSXStyle.dynamic([
+            [
+                "8b268cf2c0a3f89b",
+                [
+                    inputSize ? "height: calc(2 * var(--a)) !important;" : ""
+                ]
+            ]
+        ])}>test</p>
+
+          <_JSXStyle id={"8b268cf2c0a3f89b"} dynamic={[
+            inputSize ? "height: calc(2 * var(--a)) !important;" : ""
+        ]}>{`@media only screen {a.__jsx-style-dynamic-selector {${inputSize ? "height: calc(2 * var(--a)) !important;" : ""} }}`}</_JSXStyle>
+
+        </div>;
+    }
+};
