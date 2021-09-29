aaaac9de30cf3a6b95cddb2d2042a2101445225d
jj@jjsweb.site <jj@jjsweb.site>
2021-09-29 12:18:50 -0500

    Increase windows test timeout

--- test/jest-setup-after-env.ts
+++ test/jest-setup-after-env.ts
@@ -1,3 +1,5 @@
+import os from 'os'
+
 // A default max-timeout of 90 seconds is allowed
 // per test we should aim to bring this down though
-jest.setTimeout(90 * 1000)
+jest.setTimeout(os.platform() === 'win32' ? 120 * 1000 : 90 * 1000)
