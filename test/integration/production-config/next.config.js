36d7ccbfe87f556831b8ad18296a5654af079d6d
Tim Neutkens <timneutkens@me.com>
2021-10-06 11:52:53 +0200

    Update tests using webpack 4

--- test/integration/production-config/next.config.js
+++ test/integration/production-config/next.config.js
@@ -1,50 +1,41 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
 const path = require('path')
-module.exports = withCSS(
-  withSass({
-    // @zeit/next-sass is not supported with webpack 5
-    webpack5: false,
-    env: {
-      ...(process.env.ENABLE_ENV_FAIL_UNDERSCORE
-        ? {
-            __NEXT_MY_VAR: 'test',
-          }
-        : {}),
-      ...(process.env.ENABLE_ENV_FAIL_NODE
-        ? {
-            NODE_ENV: 'abc',
-          }
-        : {}),
-      ...(process.env.ENABLE_ENV_WITH_UNDERSCORES
-        ? {
-            SOME__ENV__VAR: '123',
-          }
-        : {}),
-    },
-    onDemandEntries: {
-      // Make sure entries are not getting disposed.
-      maxInactiveAge: 1000 * 60 * 60,
-    },
-    webpack(config) {
-      // When next-css is `npm link`ed we have to solve loaders from the project root
-      const nextLocation = path.join(
-        require.resolve('next/package.json'),
-        '../'
-      )
-      const nextCssNodeModulesLocation = path.join(
-        require.resolve('@zeit/next-css'),
-        '../../../node_modules'
-      )
+module.exports = {
+  env: {
+    ...(process.env.ENABLE_ENV_FAIL_UNDERSCORE
+      ? {
+          __NEXT_MY_VAR: 'test',
+        }
+      : {}),
+    ...(process.env.ENABLE_ENV_FAIL_NODE
+      ? {
+          NODE_ENV: 'abc',
+        }
+      : {}),
+    ...(process.env.ENABLE_ENV_WITH_UNDERSCORES
+      ? {
+          SOME__ENV__VAR: '123',
+        }
+      : {}),
+  },
+  onDemandEntries: {
+    // Make sure entries are not getting disposed.
+    maxInactiveAge: 1000 * 60 * 60,
+  },
+  webpack(config) {
+    // When next-css is `npm link`ed we have to solve loaders from the project root
+    const nextLocation = path.join(require.resolve('next/package.json'), '../')
+    const nextCssNodeModulesLocation = path.join(
+      require.resolve('@zeit/next-css'),
+      '../../../node_modules'
+    )
 
-      if (nextCssNodeModulesLocation.indexOf(nextLocation) === -1) {
-        config.resolveLoader.modules.push(nextCssNodeModulesLocation)
-      }
+    if (nextCssNodeModulesLocation.indexOf(nextLocation) === -1) {
+      config.resolveLoader.modules.push(nextCssNodeModulesLocation)
+    }
 
-      return config
-    },
-    async generateBuildId() {
-      return 'custom-buildid'
-    },
-  })
-)
+    return config
+  },
+  async generateBuildId() {
+    return 'custom-buildid'
+  },
+}
