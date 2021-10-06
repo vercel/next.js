d91dd51ffe61ecaf883506068bec18be99c90c1b
Tim Neutkens <timneutkens@me.com>
2021-10-06 11:54:35 +0200

    Remove leftover next-css/next-sass linking

--- test/integration/production-config/next.config.js
+++ test/integration/production-config/next.config.js
@@ -21,20 +21,6 @@ module.exports = {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
-  webpack(config) {
-    // When next-css is `npm link`ed we have to solve loaders from the project root
-    const nextLocation = path.join(require.resolve('next/package.json'), '../')
-    const nextCssNodeModulesLocation = path.join(
-      require.resolve('@zeit/next-css'),
-      '../../../node_modules'
-    )
-
-    if (nextCssNodeModulesLocation.indexOf(nextLocation) === -1) {
-      config.resolveLoader.modules.push(nextCssNodeModulesLocation)
-    }
-
-    return config
-  },
   async generateBuildId() {
     return 'custom-buildid'
   },
