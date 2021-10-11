085e389e245787b0bb3b0331b31dcb693567fbaf
Tim Neutkens <timneutkens@me.com>
2021-10-11 15:46:00 +0200

    Disable swcLoader for amp tests

--- test/integration/amphtml/next.config.js
+++ test/integration/amphtml/next.config.js
@@ -3,6 +3,9 @@ module.exports = {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
+  experimental: {
+    swcLoader: false,
+  },
   amp: {
     canonicalBase: 'http://localhost:1234',
   },
