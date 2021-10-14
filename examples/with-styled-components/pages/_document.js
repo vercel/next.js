bfd2c7ac134015076b091353d1940a336ffe63cc
Leticijak <65724560+Leticijak@users.noreply.github.com>
2021-10-15 00:54:04 +0200

    Update _document.js
    adding {Head, Html, Main, NextScript} from 'next/document'
    setting lang="en"
    
    I find it very useful for loading external scripts, fonts or whatever you may need for a starter package

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -1,7 +1,18 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Html, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
