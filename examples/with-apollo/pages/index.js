a00e814ab95bc7560a14bcca1e8282e3a928d0f0
ScottAgirs <scott@ijs.to>
2021-10-16 12:49:59 +0300

    feat(Errors): `try {} catch {}` to `getStaticProps()`
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- examples/with-apollo/pages/index.js
+++ examples/with-apollo/pages/index.js
@@ -1,4 +1,5 @@
 import App from '../components/App'
+import ErrorMessage from '../components/ErrorMessage'
 import InfoBox from '../components/InfoBox'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
@@ -8,27 +9,42 @@ import PostList, {
 } from '../components/PostList'
 import { initializeApollo, addApolloState } from '../lib/apolloClient'
 
-const IndexPage = () => (
+const IndexPage = ({ errorMessage, errors }) => (
   <App>
     <Header />
     <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
     <Submit />
-    <PostList />
+    
+    {!errorMessage && !errors && <PostList />}
+    {errors?.map(err=> <ErrorMessage message={err.message} />)}
   </App>
 )
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  })
-
-  return addApolloState(apolloClient, {
-    props: {},
-    revalidate: 1,
-  })
+  try {
+    await apolloClient.query({
+      query: ALL_POSTS_QUERY,
+      variables: allPostsQueryVars,
+    })
+  
+    return addApolloState(apolloClient, {
+      props: {},
+      revalidate: 1,
+    })
+  } catch (error) {
+    const errorMessage = error.message;
+    const errors = error.networkError?.result?.errors
+    
+    return addApolloState(apolloClient, {
+      props: {
+        errorMessage,
+        errors
+      },
+      revalidate: 1,
+    }) 
+  }
 }
 
 export default IndexPage
