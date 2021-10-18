17e70d257d8a00245d3f07cf6235e161c7dbc91d
Daniel Esteves <estevesd8@gmail.com>
2021-10-18 11:35:03 -0400

    feat: spacing for more posts and avatar validation

--- examples/cms-dotcms/components/avatar.js
+++ examples/cms-dotcms/components/avatar.js
@@ -4,12 +4,14 @@ export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
       <div className="relative w-12 h-12 mr-4">
-        <DotCmsImage
-          src={picture.idPath}
-          layout="fill"
-          className="rounded-full"
-          alt={name}
-        />
+        {picture?.idPath ? (
+          <DotCmsImage
+            src={picture?.idPath}
+            layout="fill"
+            className="rounded-full"
+            alt={name}
+          />
+        ) : null}
       </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
--- examples/cms-dotcms/components/more-stories.js
+++ examples/cms-dotcms/components/more-stories.js
@@ -6,7 +6,7 @@ export default function MoreStories({ posts }) {
       <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 row-gap-20 mb-32 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 md:row-gap-32">
+      <div className="grid grid-cols-1 gap-20 mb-32 md:grid-cols-2 md:gap-y-16 lg:gap-y-32 md:gap-x-32">
         {posts.map((post) => (
           <PostPreview
             key={post.urlTitle}
