cf7d4dd551615c4e7621767274bb69da0556a271
jj@jjsweb.site <jj@jjsweb.site>
2021-10-19 18:52:24 -0500

    remove more private class fields for version compat

--- packages/next/server/web/next-url.ts
+++ packages/next/server/web/next-url.ts
@@ -20,8 +20,8 @@ interface Options {
 }
 
 export class NextURL extends URL {
-  #basePath: string
-  #locale?: {
+  private _basePath: string
+  private _locale?: {
     defaultLocale: string
     domain?: DomainLocale
     locale: string
@@ -29,33 +29,33 @@ export class NextURL extends URL {
     redirect?: string
     trailingSlash?: boolean
   }
-  #options: Options
-  #url: URL
+  private options: Options
+  private url: URL
 
   constructor(url: string, options: Options = {}) {
     super(formatRelative(url))
-    this.#options = options
-    this.#basePath = ''
-    this.#url = formatRelative(url)
-    this.#analyzeUrl()
+    this.options = options
+    this._basePath = ''
+    this.url = formatRelative(url)
+    this.analyzeUrl()
   }
 
-  get #absolute() {
-    return this.#url.hostname !== 'localhost'
+  get absolute() {
+    return this.url.hostname !== 'localhost'
   }
 
-  #analyzeUrl() {
-    const { headers = {}, basePath, i18n } = this.#options
+  analyzeUrl() {
+    const { headers = {}, basePath, i18n } = this.options
 
-    if (basePath && this.#url.pathname.startsWith(basePath)) {
-      this.#url.pathname = this.#url.pathname.replace(basePath, '') || '/'
-      this.#basePath = basePath
+    if (basePath && this.url.pathname.startsWith(basePath)) {
+      this.url.pathname = this.url.pathname.replace(basePath, '') || '/'
+      this._basePath = basePath
     } else {
-      this.#basePath = ''
+      this._basePath = ''
     }
 
     if (i18n) {
-      this.#locale = getLocaleMetadata({
+      this._locale = getLocaleMetadata({
         cookies: () => {
           const value = headers['cookie']
           return value
@@ -68,154 +68,154 @@ export class NextURL extends URL {
           i18n: i18n,
         },
         url: {
-          hostname: this.#url.hostname || null,
-          pathname: this.#url.pathname,
+          hostname: this.url.hostname || null,
+          pathname: this.url.pathname,
         },
       })
 
-      if (this.#locale?.path.detectedLocale) {
-        this.#url.pathname = this.#locale.path.pathname
+      if (this._locale?.path.detectedLocale) {
+        this.url.pathname = this._locale.path.pathname
       }
     }
   }
 
-  #formatPathname() {
-    const { i18n } = this.#options
-    let pathname = this.#url.pathname
+  formatPathname() {
+    const { i18n } = this.options
+    let pathname = this.url.pathname
 
-    if (this.#locale?.locale && i18n?.defaultLocale !== this.#locale?.locale) {
-      pathname = `/${this.#locale?.locale}${pathname}`
+    if (this._locale?.locale && i18n?.defaultLocale !== this._locale?.locale) {
+      pathname = `/${this._locale?.locale}${pathname}`
     }
 
-    if (this.#basePath) {
-      pathname = `${this.#basePath}${pathname}`
+    if (this._basePath) {
+      pathname = `${this._basePath}${pathname}`
     }
 
     return pathname
   }
 
   get locale() {
-    if (!this.#locale) {
+    if (!this._locale) {
       throw new TypeError(`The URL is not configured with i18n`)
     }
 
-    return this.#locale.locale
+    return this._locale.locale
   }
 
   set locale(locale: string) {
-    if (!this.#locale) {
+    if (!this._locale) {
       throw new TypeError(`The URL is not configured with i18n`)
     }
 
-    this.#locale.locale = locale
+    this._locale.locale = locale
   }
 
   get defaultLocale() {
-    return this.#locale?.defaultLocale
+    return this._locale?.defaultLocale
   }
 
   get domainLocale() {
-    return this.#locale?.domain
+    return this._locale?.domain
   }
 
   get searchParams() {
-    return this.#url.searchParams
+    return this.url.searchParams
   }
 
   get host() {
-    return this.#absolute ? this.#url.host : ''
+    return this.absolute ? this.url.host : ''
   }
 
   set host(value: string) {
-    this.#url.host = value
+    this.url.host = value
   }
 
   get hostname() {
-    return this.#absolute ? this.#url.hostname : ''
+    return this.absolute ? this.url.hostname : ''
   }
 
   set hostname(value: string) {
-    this.#url.hostname = value || 'localhost'
+    this.url.hostname = value || 'localhost'
   }
 
   get port() {
-    return this.#absolute ? this.#url.port : ''
+    return this.absolute ? this.url.port : ''
   }
 
   set port(value: string) {
-    this.#url.port = value
+    this.url.port = value
   }
 
   get protocol() {
-    return this.#absolute ? this.#url.protocol : ''
+    return this.absolute ? this.url.protocol : ''
   }
 
   set protocol(value: string) {
-    this.#url.protocol = value
+    this.url.protocol = value
   }
 
   get href() {
-    const pathname = this.#formatPathname()
-    return this.#absolute
-      ? `${this.protocol}//${this.host}${pathname}${this.#url.search}`
-      : `${pathname}${this.#url.search}`
+    const pathname = this.formatPathname()
+    return this.absolute
+      ? `${this.protocol}//${this.host}${pathname}${this.url.search}`
+      : `${pathname}${this.url.search}`
   }
 
   set href(url: string) {
-    this.#url = formatRelative(url)
-    this.#analyzeUrl()
+    this.url = formatRelative(url)
+    this.analyzeUrl()
   }
 
   get origin() {
-    return this.#absolute ? this.#url.origin : ''
+    return this.absolute ? this.url.origin : ''
   }
 
   get pathname() {
-    return this.#url.pathname
+    return this.url.pathname
   }
 
   set pathname(value: string) {
-    this.#url.pathname = value
+    this.url.pathname = value
   }
 
   get hash() {
-    return this.#url.hash
+    return this.url.hash
   }
 
   set hash(value: string) {
-    this.#url.hash = value
+    this.url.hash = value
   }
 
   get search() {
-    return this.#url.search
+    return this.url.search
   }
 
   set search(value: string) {
-    this.#url.search = value
+    this.url.search = value
   }
 
   get password() {
-    return this.#url.password
+    return this.url.password
   }
 
   set password(value: string) {
-    this.#url.password = value
+    this.url.password = value
   }
 
   get username() {
-    return this.#url.username
+    return this.url.username
   }
 
   set username(value: string) {
-    this.#url.username = value
+    this.url.username = value
   }
 
   get basePath() {
-    return this.#basePath
+    return this._basePath
   }
 
   set basePath(value: string) {
-    this.#basePath = value.startsWith('/') ? value : `/${value}`
+    this._basePath = value.startsWith('/') ? value : `/${value}`
   }
 
   toString() {
