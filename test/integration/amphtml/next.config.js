f43858d18931059c8b4b94b10428c539185b5260
Maia Teegarden <dev@padmaia.rocks>
2021-10-22 16:13:15 -0700

    Re-enable amp tests with swc

--- test/integration/amphtml/next.config.js
+++ test/integration/amphtml/next.config.js
@@ -3,9 +3,6 @@ module.exports = {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
-  experimental: {
-    swcLoader: false,
-  },
   amp: {
     canonicalBase: 'http://localhost:1234',
   },
