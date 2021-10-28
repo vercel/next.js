05732a63b69a30840a5a86b694dd06b33703454b
Rodrigo Delduca <rodrigodelduca@gmail.com>
2021-10-28 11:06:49 -0300

    Some Google fonts have a little different path, like the Material Icons
    https://fonts.googleapis.com/icon?family=Material+Icons
    
    This patch include the font above to be optimized

--- packages/next/shared/lib/constants.ts
+++ packages/next/shared/lib/constants.ts
@@ -39,7 +39,7 @@ export const TEMPORARY_REDIRECT_STATUS = 307
 export const PERMANENT_REDIRECT_STATUS = 308
 export const STATIC_PROPS_ID = '__N_SSG'
 export const SERVER_PROPS_ID = '__N_SSP'
-export const GOOGLE_FONT_PROVIDER = 'https://fonts.googleapis.com/css'
+export const GOOGLE_FONT_PROVIDER = 'https://fonts.googleapis.com/'
 export const OPTIMIZED_FONT_PROVIDERS = [
   { url: GOOGLE_FONT_PROVIDER, preconnect: 'https://fonts.gstatic.com' },
   { url: 'https://use.typekit.net', preconnect: 'https://use.typekit.net' },
