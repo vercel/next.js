fccde240d61e2047cfce98be9f2688c40979ede0
jj@jjsweb.site <jj@jjsweb.site>
2021-10-28 20:56:14 -0500

    Use fs.copy

--- .github/actions/next-stats-action/src/index.js
+++ .github/actions/next-stats-action/src/index.js
@@ -118,11 +118,9 @@ if (!allowedActions.has(actionInfo.actionName) && !actionInfo.isRelease) {
         // in case of noisy environment slowing down initial repo build
         await exec(buildCommand, false, { timeout: 5 * 60 * 1000 })
       }
-      await exec(
-        `cp -r ${path.join(__dirname, '../native')} ${path.join(
-          dir,
-          'packages/next/native'
-        )}`
+      await fs.copy(
+        path.join(__dirname, '../native'),
+        path.join(dir, 'packages/next/native')
       )
 
       logger(`Linking packages in ${dir}`)
