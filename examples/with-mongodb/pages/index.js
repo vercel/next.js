fa94d1f41b052e543e9d5fe1bbebfe4888bdd5da
harimkims <harimkims@gmail.com>
2021-10-31 03:26:10 +0900

    Remove deprecated function

--- examples/with-mongodb/pages/index.js
+++ examples/with-mongodb/pages/index.js
@@ -223,17 +223,20 @@ export default function Home({ isConnected }) {
 }
 
 export async function getServerSideProps(context) {
-  const client = await clientPromise
-
-  // client.db() will be the default database passed in the MONGODB_URI
-  // You can change the database by calling the client.db() function and specifying a database like:
-  // const db = client.db("myDatabase");
-  // Then you can execute queries against your database like so:
-  // db.find({}) or any of the MongoDB Node Driver commands
-
-  const isConnected = await client.isConnected()
-
-  return {
-    props: { isConnected },
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    const client = await clientPromise
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
   }
 }
