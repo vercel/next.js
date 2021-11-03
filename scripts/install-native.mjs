507ac981ef50b99ebed321398a84bc613dbbe1c5
JJ Kasper <jj@jjsweb.site>
2021-11-02 19:02:16 -0500

    Ensure dev react bundles are not ignored in traces (#30849)
    * Ensure dev react bundles are not ignored in traces
    
    * update test
    
    * update lock

--- scripts/install-native.mjs
+++ scripts/install-native.mjs
@@ -1,6 +1,5 @@
 import { execFile } from 'child_process'
-import fs from 'fs/promises'
-import fse from 'fs-extra'
+import fs from 'fs-extra'
 import os from 'os'
 import path from 'path'
 import util from 'util'
@@ -10,7 +9,7 @@ const exec = util.promisify(execFile)
 ;(async function () {
   try {
     let tmpdir = os.tmpdir() + `next-swc-${Date.now()}`
-    await fse.ensureDir(tmpdir)
+    await fs.ensureDir(tmpdir)
     let cwd = process.cwd()
     let pkgJson = {
       name: 'dummy-package',
@@ -36,18 +35,18 @@ const exec = util.promisify(execFile)
     let { stdout } = await exec('yarn', ['--force'], { cwd: tmpdir })
     console.log(stdout)
     let pkgs = await fs.readdir(path.join(tmpdir, 'node_modules/@next'))
-    await fse.ensureDir(path.join(cwd, 'node_modules/@next'))
+    await fs.ensureDir(path.join(cwd, 'node_modules/@next'))
 
     await Promise.all(
       pkgs.map((pkg) =>
-        fse.move(
+        fs.move(
           path.join(tmpdir, 'node_modules/@next', pkg),
           path.join(cwd, 'node_modules/@next', pkg),
           { overwrite: true }
         )
       )
     )
-    await fse.remove(tmpdir)
+    await fs.remove(tmpdir)
     console.log('Installed the following binary packages:', pkgs)
   } catch (e) {
     console.error(e)
