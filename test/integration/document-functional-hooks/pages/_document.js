b07d1b897e8475cf875b64fb694d777a8074e458
Gerald Monaco <gbmonaco@google.com>
2021-11-06 20:23:49 +0000

    Fix lint

--- test/integration/document-functional-hooks/pages/_document.js
+++ test/integration/document-functional-hooks/pages/_document.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  const state = React.useState(null)
+  React.useState(null)
   return (
     <Html>
       <Head />
