4365131bcbfc91241c2fee1e08f1e9b0256cb9fe
Matt Kane <m@mk.gg>
2021-11-08 21:43:23 +0000

    Don't proxy middleware if host is the same

--- packages/next/server/next-server.ts
+++ packages/next/server/next-server.ts
@@ -248,8 +248,8 @@ export default class Server {
           : null,
       optimizeImages: !!this.nextConfig.experimental.optimizeImages,
       optimizeCss: this.nextConfig.experimental.optimizeCss,
-      disableOptimizedLoading:
-        this.nextConfig.experimental.disableOptimizedLoading,
+      disableOptimizedLoading: this.nextConfig.experimental
+        .disableOptimizedLoading,
       domainLocales: this.nextConfig.i18n?.domains,
       distDir: this.distDir,
       concurrentFeatures: this.nextConfig.experimental.concurrentFeatures,
@@ -379,8 +379,9 @@ export default class Server {
       typeof req.headers['x-matched-path'] === 'string'
     ) {
       const reqUrlIsDataUrl = req.url?.includes('/_next/data')
-      const matchedPathIsDataUrl =
-        req.headers['x-matched-path']?.includes('/_next/data')
+      const matchedPathIsDataUrl = req.headers['x-matched-path']?.includes(
+        '/_next/data'
+      )
       const isDataUrl = reqUrlIsDataUrl || matchedPathIsDataUrl
 
       let parsedPath = parseUrl(
@@ -882,8 +883,9 @@ export default class Server {
               finished: true,
             }
           }
-          const { imageOptimizer } =
-            require('./image-optimizer') as typeof import('./image-optimizer')
+          const {
+            imageOptimizer,
+          } = require('./image-optimizer') as typeof import('./image-optimizer')
 
           return imageOptimizer(
             server,
@@ -1052,8 +1054,9 @@ export default class Server {
               let updatedDestination = formatUrl(parsedDestination)
 
               if (updatedDestination.startsWith('/')) {
-                updatedDestination =
-                  normalizeRepeatedSlashes(updatedDestination)
+                updatedDestination = normalizeRepeatedSlashes(
+                  updatedDestination
+                )
               }
 
               res.setHeader('Location', updatedDestination)
@@ -1224,7 +1227,12 @@ export default class Server {
           if (result.response.headers.has('x-middleware-rewrite')) {
             const rewrite = result.response.headers.get('x-middleware-rewrite')!
             const rewriteParsed = simpleParseUrl(rewrite)
-            if (rewriteParsed.protocol) {
+            if (
+              rewriteParsed.protocol &&
+              (rewriteParsed.port
+                ? `${rewriteParsed.hostname}:${rewriteParsed.port}`
+                : rewriteParsed.hostname) !== req.headers.host
+            ) {
               return proxyRequest(req, res, rewriteParsed)
             }
 
@@ -1746,7 +1754,9 @@ export default class Server {
     return null
   }
 
-  protected async getStaticPaths(pathname: string): Promise<{
+  protected async getStaticPaths(
+    pathname: string
+  ): Promise<{
     staticPaths: string[] | undefined
     fallbackMode: 'static' | 'blocking' | false
   }> {
@@ -1755,8 +1765,8 @@ export default class Server {
     const staticPaths = undefined
 
     // Read whether or not fallback should exist from the manifest.
-    const fallbackField =
-      this.getPrerenderManifest().dynamicRoutes[pathname].fallback
+    const fallbackField = this.getPrerenderManifest().dynamicRoutes[pathname]
+      .fallback
 
     return {
       staticPaths,
@@ -1952,17 +1962,20 @@ export default class Server {
 
       // handle serverless
       if (isLikeServerless) {
-        const renderResult = await (
-          components.ComponentMod as any
-        ).renderReqToHTML(req, res, 'passthrough', {
-          locale,
-          locales,
-          defaultLocale,
-          optimizeCss: this.renderOpts.optimizeCss,
-          distDir: this.distDir,
-          fontManifest: this.renderOpts.fontManifest,
-          domainLocales: this.renderOpts.domainLocales,
-        })
+        const renderResult = await (components.ComponentMod as any).renderReqToHTML(
+          req,
+          res,
+          'passthrough',
+          {
+            locale,
+            locales,
+            defaultLocale,
+            optimizeCss: this.renderOpts.optimizeCss,
+            distDir: this.distDir,
+            fontManifest: this.renderOpts.fontManifest,
+            domainLocales: this.renderOpts.domainLocales,
+          }
+        )
 
         body = renderResult.html
         pageData = renderResult.renderOpts.pageData
