afdb89fa01f4d949c10dee5449d696da90cb2801
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-11-08 14:08:58 +0100

    Give priority to document.title over h1 when announcing page change

--- test/integration/client-navigation-a11y/pages/page-with-h1-and-title.js
+++ test/integration/client-navigation-a11y/pages/page-with-h1-and-title.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 
 export default () => (
-  <div id="page-with-h1">
+  <div id="page-with-h1-and-title">
+    <Head>
+      <title>Another Page's Title</title>
+    </Head>
     <h1>My heading</h1>
     <div>Extraneous stuff</div>
   </div>
