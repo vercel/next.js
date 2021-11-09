c4e0ce0c1294147037239cedf8e02cf18595fded
Matt Kane <m@mk.gg>
2021-11-09 10:20:44 +0000

    lint

--- packages/next/server/next-server.ts
+++ packages/next/server/next-server.ts
@@ -248,8 +248,8 @@ export default class Server {
           : null,
       optimizeImages: !!this.nextConfig.experimental.optimizeImages,
       optimizeCss: this.nextConfig.experimental.optimizeCss,
-      disableOptimizedLoading: this.nextConfig.experimental
-        .disableOptimizedLoading,
+      disableOptimizedLoading:
+        this.nextConfig.experimental.disableOptimizedLoading,
       domainLocales: this.nextConfig.i18n?.domains,
       distDir: this.distDir,
       concurrentFeatures: this.nextConfig.experimental.concurrentFeatures,
@@ -379,9 +379,8 @@ export default class Server {
       typeof req.headers['x-matched-path'] === 'string'
     ) {
       const reqUrlIsDataUrl = req.url?.includes('/_next/data')
-      const matchedPathIsDataUrl = req.headers['x-matched-path']?.includes(
-        '/_next/data'
-      )
+      const matchedPathIsDataUrl =
+        req.headers['x-matched-path']?.includes('/_next/data')
       const isDataUrl = reqUrlIsDataUrl || matchedPathIsDataUrl
 
       let parsedPath = parseUrl(
@@ -883,9 +882,8 @@ export default class Server {
               finished: true,
             }
           }
-          const {
-            imageOptimizer,
-          } = require('./image-optimizer') as typeof import('./image-optimizer')
+          const { imageOptimizer } =
+            require('./image-optimizer') as typeof import('./image-optimizer')
 
           return imageOptimizer(
             server,
@@ -1054,9 +1052,8 @@ export default class Server {
               let updatedDestination = formatUrl(parsedDestination)
 
               if (updatedDestination.startsWith('/')) {
-                updatedDestination = normalizeRepeatedSlashes(
-                  updatedDestination
-                )
+                updatedDestination =
+                  normalizeRepeatedSlashes(updatedDestination)
               }
 
               res.setHeader('Location', updatedDestination)
@@ -1754,9 +1751,7 @@ export default class Server {
     return null
   }
 
-  protected async getStaticPaths(
-    pathname: string
-  ): Promise<{
+  protected async getStaticPaths(pathname: string): Promise<{
     staticPaths: string[] | undefined
     fallbackMode: 'static' | 'blocking' | false
   }> {
@@ -1765,8 +1760,8 @@ export default class Server {
     const staticPaths = undefined
 
     // Read whether or not fallback should exist from the manifest.
-    const fallbackField = this.getPrerenderManifest().dynamicRoutes[pathname]
-      .fallback
+    const fallbackField =
+      this.getPrerenderManifest().dynamicRoutes[pathname].fallback
 
     return {
       staticPaths,
@@ -1962,20 +1957,17 @@ export default class Server {
 
       // handle serverless
       if (isLikeServerless) {
-        const renderResult = await (components.ComponentMod as any).renderReqToHTML(
-          req,
-          res,
-          'passthrough',
-          {
-            locale,
-            locales,
-            defaultLocale,
-            optimizeCss: this.renderOpts.optimizeCss,
-            distDir: this.distDir,
-            fontManifest: this.renderOpts.fontManifest,
-            domainLocales: this.renderOpts.domainLocales,
-          }
-        )
+        const renderResult = await (
+          components.ComponentMod as any
+        ).renderReqToHTML(req, res, 'passthrough', {
+          locale,
+          locales,
+          defaultLocale,
+          optimizeCss: this.renderOpts.optimizeCss,
+          distDir: this.distDir,
+          fontManifest: this.renderOpts.fontManifest,
+          domainLocales: this.renderOpts.domainLocales,
+        })
 
         body = renderResult.html
         pageData = renderResult.renderOpts.pageData
