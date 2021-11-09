cf5939b07d2662f54d06dae3696044368f706d2b
Rishabh <rishabh@supertokens.io>
2021-11-09 21:25:29 +0530

    extracted oauth keys to .env file

--- examples/with-supertokens/config/backendConfig.js
+++ examples/with-supertokens/config/backendConfig.js
@@ -15,27 +15,21 @@ export let backendConfig = () => {
           // We have provided you with development keys which you can use for testing.
           // IMPORTANT: Please replace them with your own OAuth keys for production use.
           ThirdPartyEmailPasswordNode.Google({
-            clientId:
-              '1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com',
-            clientSecret: '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',
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
           }),
           ThirdPartyEmailPasswordNode.Github({
-            clientId: '467101b197249757c71f',
-            clientSecret: 'e97051221f4b6426e8fe8d51486396703012f5bd',
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
           }),
           ThirdPartyEmailPasswordNode.Apple({
-            clientId: '4398792-io.supertokens.example.service',
+            clientId: process.env.APPLE_CLIENT_ID,
             clientSecret: {
-              keyId: '7M48Y4RYDL',
-              privateKey:
-                '-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----',
-              teamId: 'YWQCXGJRJL',
+              keyId: process.env.APPLE_KEY_ID,
+              privateKey: process.env.APPLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+              teamId: process.env.APPLE_TEAM_ID,
             },
           }),
-          // ThirdPartyEmailPasswordNode.Facebook({
-          //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET || 'PLACEHOLDER',
-          //   clientId: process.env.FACEBOOK_CLIENT_ID || 'PLACEHOLDER',
-          // }),
         ],
       }),
       SessionNode.init(),
