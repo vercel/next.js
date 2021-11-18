d8720d9c90eb452831e8b8dbdd708002f85e2214
Gerrit Alex <admin@gerritalex.de>
2021-11-18 01:15:09 +0000

    fix: support function components in _document in no-page-custom-font
    - fixes #29970

--- packages/eslint-plugin-next/lib/rules/no-page-custom-font.js
+++ packages/eslint-plugin-next/lib/rules/no-page-custom-font.js
@@ -1,4 +1,5 @@
 const NodeAttributes = require('../utils/node-attributes.js')
+const { sep, posix } = require('path')
 
 module.exports = {
   meta: {
@@ -10,7 +11,22 @@ module.exports = {
     },
   },
   create: function (context) {
+    const paths = context.getFilename().split('pages')
+    const page = paths[paths.length - 1]
+
+    // outside of a file within `pages`, bail
+    if (!page) {
+      return {}
+    }
+
+    const is_Document =
+      page.startsWith(`${sep}_document`) ||
+      page.startsWith(`${posix.sep}_document`)
+
     let documentImportName
+    let localDefaultExportName
+    let exportDeclarationType
+
     return {
       ImportDeclaration(node) {
         if (node.source.value === 'next/document') {
@@ -22,17 +38,91 @@ module.exports = {
           }
         }
       },
+
+      ExportDefaultDeclaration(node) {
+        if (node.declaration.type === 'FunctionDeclaration') {
+          localDefaultExportName = node.declaration.id.name
+          exportDeclarationType = 'FunctionDeclaration'
+          return
+        }
+
+        if (
+          node.declaration.type === 'ClassDeclaration' &&
+          node.declaration.superClass &&
+          node.declaration.superClass.name === documentImportName
+        ) {
+          localDefaultExportName = node.declaration.id.name
+          exportDeclarationType = 'ClassDeclaration'
+        }
+      },
+
       JSXOpeningElement(node) {
-        const documentClass = context
-          .getAncestors()
-          .find(
-            (ancestorNode) =>
-              ancestorNode.type === 'ClassDeclaration' &&
-              ancestorNode.superClass &&
-              ancestorNode.superClass.name === documentImportName
+        if (node.name.name !== 'link') {
+          return
+        }
+
+        const ancestors = context.getAncestors()
+
+        // if `export default <name>` is further down within the file after the
+        // currently traversed component, then `localDefaultExportName` will
+        // still be undefined
+        if (!localDefaultExportName) {
+          // find the top level of the module
+          const program = ancestors.find(
+            (ancestor) => ancestor.type === 'Program'
           )
 
-        if (documentClass || node.name.name !== 'link') {
+          // go over each token to find the combination of `export default <name>`
+          for (let i = 0; i <= program.tokens.length - 1; i++) {
+            if (localDefaultExportName) {
+              break
+            }
+
+            const token = program.tokens[i]
+
+            if (token.type === 'Keyword' && token.value === 'export') {
+              const nextToken = program.tokens[i + 1]
+
+              if (
+                nextToken &&
+                nextToken.type === 'Keyword' &&
+                nextToken.value === 'default'
+              ) {
+                const maybeIdentifier = program.tokens[i + 2]
+
+                if (maybeIdentifier && maybeIdentifier.type === 'Identifier') {
+                  localDefaultExportName = maybeIdentifier.value
+                }
+              }
+            }
+          }
+        }
+
+        const parentComponent = ancestors.find((ancestor) => {
+          // export default class ... extends ...
+          if (exportDeclarationType === 'ClassDeclaration') {
+            return (
+              ancestor.type === exportDeclarationType &&
+              ancestor.superClass &&
+              ancestor.superClass.name === documentImportName
+            )
+          }
+
+          // export default function ...
+          if (exportDeclarationType === 'FunctionDeclaration') {
+            return (
+              ancestor.type === exportDeclarationType &&
+              ancestor.id.name === localDefaultExportName
+            )
+          }
+
+          // function Name() {} export default XY
+          // class Name extends Document; export default Name
+          return ancestor.id && ancestor.id.name === localDefaultExportName
+        })
+
+        // file starts with _document and this <link /> is within the default export
+        if (is_Document && parentComponent) {
           return
         }
 
