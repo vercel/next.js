146653ea0ebaa1ce541fa368d7e158cab790ad10
dptoinformatica3ser <93710690+dptoinformatica3ser@users.noreply.github.com>
2021-11-19 16:52:49 +0100

    Update index.jsx

--- examples/with-react-md/pages/index.jsx
+++ examples/with-react-md/pages/index.jsx
@@ -6,5 +6,17 @@ export default function Home() {
     <TextContainer>
       <Text type="headline-4">Hello, world!</Text>
     </TextContainer>
+     <div className="container">
+      <h1>Hello Next.js</h1>
+      <p>Let's explore different ways to style Next.js apps</p>
+      <style jsx>{`
+        .container {
+          margin: 50px;
+        }
+        p {
+          color: blue;
+        }
+      `}</style>
+    </div>
   )
 }
