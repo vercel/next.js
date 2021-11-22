5358520341bd8259b14f1572a05deb9b9522523f
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2021-11-22 14:56:39 +0000

    Add footer to with-jest-babel example

--- examples/with-jest-babel/pages/index.js
+++ examples/with-jest-babel/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from './index.module.css'
+import Image from 'next/image'
 
 const Home = () => (
   <div className={styles.container}>
@@ -42,6 +43,19 @@ const Home = () => (
         </a>
       </div>
     </main>
+
+    <footer className={styles.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <span className={styles.logo}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
+      </a>
+    </footer>
   </div>
 )
 
