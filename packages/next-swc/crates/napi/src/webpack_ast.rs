3403917791356ec61cd7f55aa5811eb620867ffd
Donny <kdy1997.dev@gmail.com>
2021-11-23 13:24:45 +0900

    Parallel

--- packages/next-swc/crates/napi/src/webpack_ast.rs
+++ packages/next-swc/crates/napi/src/webpack_ast.rs
@@ -2,6 +2,7 @@
 //!
 //! This code lives at `napi` crate because it's not used by wasm.
 
+use rayon::prelude::*;
 use swc_ecmascript::{
     ast::*,
     utils::StmtOrModuleItem,
@@ -17,10 +18,17 @@ struct Minimalizer;
 impl Minimalizer {
     fn visit_mut_stmt_likes<T>(&mut self, stmts: &mut Vec<T>)
     where
-        T: StmtOrModuleItem,
+        T: StmtOrModuleItem + VisitMutWith<Self>,
         Vec<T>: VisitMutWith<Self>,
     {
-        stmts.visit_mut_children_with(self);
+        // Process in parallel, if required
+        if stmts.len() >= 8 {
+            stmts
+                .par_iter_mut()
+                .for_each(|stmt| stmt.visit_mut_with(&mut Minimalizer));
+        } else {
+            stmts.visit_mut_children_with(self);
+        }
     }
 }
 
