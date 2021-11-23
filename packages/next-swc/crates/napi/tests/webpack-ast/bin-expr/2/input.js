70708a430e123eb7065b9320562eede1cc10c915
Donny <kdy1997.dev@gmail.com>
2021-11-23 16:43:42 +0900

    Add tests

---
+++ packages/next-swc/crates/napi/tests/webpack-ast/bin-expr/2/input.js
@@ -0,0 +1,9 @@
+import x from 'x;/'
+import y from 'y;'
+
+const obj = {
+    [1 + y]: x,
+    y,
+}
+
+console.log(obj)
---
+++ packages/next-swc/crates/napi/tests/webpack-ast/bin-expr/3/input.js
@@ -0,0 +1,9 @@
+import x from 'x;/'
+import y from 'y;'
+
+const obj = {
+    [1 + y]: 1,
+    x,
+}
+
+console.log(obj)
