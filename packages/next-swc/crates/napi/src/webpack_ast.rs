b4b108706c69d0b46daa5fc674b6803869931d4f
Donny <kdy1997.dev@gmail.com>
2021-11-23 12:45:50 +0900

    minimalizer

--- packages/next-swc/crates/napi/src/webpack_ast.rs
+++ packages/next-swc/crates/napi/src/webpack_ast.rs
@@ -1,3 +1,15 @@
 //! Minimalize AST for webpack.
 //!
 //! This code lives at `napi` crate because it's not used by wasm.
+
+use swc_ecmascript::visit::{noop_visit_mut_type, VisitMut};
+
+pub fn ast_minimalizer() -> impl VisitMut {
+    Minimalizer
+}
+
+struct Minimalizer;
+
+impl VisitMut for Minimalizer {
+    noop_visit_mut_type!();
+}
