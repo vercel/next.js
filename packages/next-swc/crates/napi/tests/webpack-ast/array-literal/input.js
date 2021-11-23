1eb7b89f47045ce2478b1157a85b3b51793a6007
Donny <kdy1997.dev@gmail.com>
2021-11-23 15:58:06 +0900

    More test

---
+++ packages/next-swc/crates/napi/tests/webpack-ast/array-literal/input.js
@@ -0,0 +1,5 @@
+import { a } from "x";
+import b from "y";
+
+const arr = [a, 'foo', b(), 'bar'];
+console.log(arr);
---
+++ packages/next-swc/crates/napi/tests/webpack-ast/object-literal/input.js
@@ -0,0 +1,9 @@
+import { a } from "x";
+import b from "y";
+
+const obj = {
+    a,
+    [b]: 'foo',
+    c: 'bar'
+};
+console.log(obj);
