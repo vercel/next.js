dc721e5294380d0e0149bf5f077e3cbfb18c29b4
Yousef <majhool.yf@gmail.com>
2021-11-25 06:47:32 -0800

    login API extended

--- examples/with-iron-session-and-JWT-authentication/pages/api/login.js
+++ examples/with-iron-session-and-JWT-authentication/pages/api/login.js
@@ -1,13 +1,27 @@
 import fetchJson from '../../lib/fetchJson'
 import withSession from '../../lib/session'
+import sampleData from "../../lib/sampleData.json"
 
 export default withSession(async (req, res) => {
   const { username } = await req.body
-  const url = `https://api.github.com/users/${username}`
-
+  const { password } = await req.body
+  // const url = `https://api.github.com/users/${username}`
   try {
-    // we check that the user exists on GitHub and store some data in session
-    const { login, avatar_url: avatarUrl } = await fetchJson(url)
+    // we check that the user exists on server and store tokens and login data in session
+    // const { login, avatar_url: avatarUrl } = await fetchJson(URL_TO_AUTHENTICATE_AND_GET_LOGIN_DATA, {
+    //   method: 'POST',
+    //   headers: {
+    //      'Content-Type': 'application/json'
+    //   }
+    //   body: {
+    //      username,
+    //      password
+    //   }
+    // });
+
+    // Comment below line after your implementation
+    const { login, avatar_url: avatarUrl } = sampleData
+
     const user = { isLoggedIn: true, login, avatarUrl }
     req.session.set('user', user)
     await req.session.save()
