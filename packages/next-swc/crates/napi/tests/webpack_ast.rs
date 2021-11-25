3d19241a176ee60b952d9c2d89df4fc6d45d55f3
Donny <kdy1997.dev@gmail.com>
2021-11-25 23:54:52 +0900

    jsx

--- packages/next-swc/crates/napi/tests/webpack_ast.rs
+++ packages/next-swc/crates/napi/tests/webpack_ast.rs
@@ -1,15 +1,23 @@
-use next_swc_napi::webpack_ast;
+use next_swc_napi::webpack_ast::{self, parse_file_as_webpack_ast};
 use std::path::PathBuf;
 use swc_common::{chain, Mark};
 use swc_ecma_transforms_testing::test_fixture;
-use swc_ecmascript::{parser::Syntax, transforms::resolver_with_mark, visit::as_folder};
+use swc_ecmascript::{
+    parser::{EsConfig, Syntax},
+    transforms::resolver_with_mark,
+    visit::as_folder,
+};
+use testing::NormalizedOutput;
 
 #[testing::fixture("tests/webpack-ast/**/input.js")]
 fn fixture(input: PathBuf) {
     let output = input.parent().unwrap().join("output.js");
 
     test_fixture(
-        Syntax::default(),
+        Syntax::Es(EsConfig {
+            jsx: true,
+            ..Default::default()
+        }),
         &|_| {
             let top_level_mark = Mark::fresh(Mark::root());
 
