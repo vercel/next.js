5f7ea0d08c202f6c30047ef3e2df5bb25cb20236
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-11-29 15:55:57 +0100

    Don't use yarn if a package-lock.json file is found

--- packages/next/lib/helpers/should-use-yarn.ts
+++ packages/next/lib/helpers/should-use-yarn.ts
@@ -1,13 +1,20 @@
 import { execSync } from 'child_process'
+import fs from 'fs'
 
 export function shouldUseYarn(): boolean {
   try {
     const userAgent = process.env.npm_config_user_agent
     if (userAgent) {
       return Boolean(userAgent && userAgent.startsWith('yarn'))
+    } else {
+      if (fs.existsSync('yarn.lock')) {
+        return true
+      } else if (fs.existsSync('package-lock.json')) {
+        return false
+      }
+      execSync('yarnpkg --version', { stdio: 'ignore' })
+      return true
     }
-    execSync('yarnpkg --version', { stdio: 'ignore' })
-    return true
   } catch (e) {
     return false
   }
