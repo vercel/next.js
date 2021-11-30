d838e458f48d1312eea98f083f73854ea8599d62
Maia Teegarden <dev@padmaia.rocks>
2021-11-30 14:11:45 -0800

    Attempt to load native binaries before wasm

--- packages/next/build/swc/index.js
+++ packages/next/build/swc/index.js
@@ -1,21 +1,46 @@
 import { platform, arch } from 'os'
 import { platformArchTriples } from '@napi-rs/triples'
-import Log from '../output/log'
+import * as Log from '../output/log'
 
 const ArchName = arch()
 const PlatformName = platform()
 const triples = platformArchTriples[PlatformName][ArchName] || []
 
 async function loadBindings() {
-  return (await loadWasm()) || loadNative()
+  let bindings = loadNative()
+  if (bindings) {
+    return bindings
+  }
+
+  bindings = await loadWasm()
+  if (bindings) {
+    return bindings
+  }
+
+  logLoadFailure()
+}
+
+function loadBindingsSync() {
+  let bindings = loadNative()
+  if (bindings) {
+    return bindings
+  }
+
+  logLoadFailure()
+}
+
+function logLoadFailure() {
+  Log.error(
+    `Failed to load SWC binary, see more info here: https://nextjs.org/docs/messages/failed-loading-swc`
+  )
+  process.exit(1)
 }
 
 async function loadWasm() {
-  // Try to load wasm bindings
-  for (let specifier of ['@next/swc-wasm-web', '@next/swc-wasm-nodejs']) {
+  for (let pkg of ['@next/swc-wasm-web', '@next/swc-wasm-nodejs']) {
     try {
-      let bindings = await import(specifier)
-      if (specifier === '@next/swc-wasm-web') {
+      let bindings = await import(pkg)
+      if (pkg === '@next/swc-wasm-web') {
         bindings = await bindings.default()
       }
       return {
@@ -34,30 +59,22 @@ async function loadWasm() {
 
 function loadNative() {
   let bindings
-  let loadError
 
   for (const triple of triples) {
     try {
       bindings = require(`@next/swc/native/next-swc.${triple.platformArchABI}.node`)
       Log.info('Using locally built binary of @next/swc')
       break
-    } catch (e) {
-      if (e?.code !== 'MODULE_NOT_FOUND') {
-        loadError = e
-      }
-    }
+    } catch (e) {}
   }
 
   if (!bindings) {
     for (const triple of triples) {
+      let pkg = `@next/swc-${triple.platformArchABI}`
       try {
-        bindings = require(`@next/swc-${triple.platformArchABI}`)
+        bindings = require(pkg)
         break
-      } catch (e) {
-        if (e?.code !== 'MODULE_NOT_FOUND') {
-          loadError = e
-        }
-      }
+      } catch (e) {}
     }
   }
 
@@ -118,15 +135,6 @@ function loadNative() {
       },
     }
   }
-
-  if (loadError) {
-    console.error(loadError)
-  }
-
-  Log.error(
-    `Failed to load SWC binary, see more info here: https://nextjs.org/docs/messages/failed-loading-swc`
-  )
-  process.exit(1)
 }
 
 function toBuffer(t) {
@@ -139,7 +147,7 @@ export async function transform(src, options) {
 }
 
 export function transformSync(src, options) {
-  let bindings = loadNative()
+  let bindings = loadBindingsSync()
   return bindings.transformSync(src, options)
 }
 
@@ -149,11 +157,11 @@ export async function minify(src, options) {
 }
 
 export function minifySync(src, options) {
-  let bindings = loadNative()
+  let bindings = loadBindingsSync()
   return bindings.minifySync(src, options)
 }
 
 export async function bundle(options) {
-  let bindings = loadNative()
+  let bindings = loadBindingsSync()
   return bindings.bundle(toBuffer(options))
 }
