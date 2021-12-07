99c6afc94d87e15d8732f9da2bfa8b25f21fadda
Donny <kdy1997.dev@gmail.com>
2021-12-07 21:11:09 +0900

    Update test refs

--- packages/next-swc/crates/core/tests/fixture/styled-jsx/comments/output.js
+++ packages/next-swc/crates/core/tests/fixture/styled-jsx/comments/output.js
@@ -5,7 +5,7 @@ export default class {
 
         <p className={"jsx-1952086b0a5ae64c"}>test</p>
 
-        <_JSXStyle id={"1952086b0a5ae64c"}>{"p.jsx-1952086b0a5ae64c {color:red}"}</_JSXStyle>
+        <_JSXStyle id={"1952086b0a5ae64c"}>{"p.jsx-1952086b0a5ae64c{color:red}"}</_JSXStyle>
 
       </div>;
     }
