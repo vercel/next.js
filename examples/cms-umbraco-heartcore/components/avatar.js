99ac63bcea390adc5ae6a19905489c7d92900722
Tim Neutkens <tim@timneutkens.nl>
2021-12-07 16:37:56 +0100

    Fix lint

--- examples/cms-umbraco-heartcore/components/avatar.js
+++ examples/cms-umbraco-heartcore/components/avatar.js
@@ -5,12 +5,12 @@ export default function Avatar({ name, picture }) {
     <div className="flex items-center">
       <div className="w-12 h-12 relative mr-4">
         <Image
-            src={picture}
-            layout="fill"
-            className="rounded-full"
-            alt={name}
-          />
-        </div>
+          src={picture}
+          layout="fill"
+          className="rounded-full"
+          alt={name}
+        />
+      </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
