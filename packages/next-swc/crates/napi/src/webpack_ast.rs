ae995b3c7ad88929c2e1407dfce2ecc1c04df884
Donny <kdy1997.dev@gmail.com>
2021-12-08 21:04:26 +0900

    Include src

--- packages/next-swc/crates/napi/src/webpack_ast.rs
+++ packages/next-swc/crates/napi/src/webpack_ast.rs
@@ -5,50 +5,60 @@
 
 use crate::util::MapErr;
 use anyhow::{anyhow, Context, Error};
-use napi::{CallContext, JsObject, JsString, Task};
+use napi::{CallContext, JsBoolean, JsObject, JsString, Task};
+use serde::Serialize;
 use std::{
     path::{Path, PathBuf},
     sync::Arc,
 };
 use swc::try_with_handler;
+use swc_common::sync::Lrc;
 use swc_common::{FilePathMapping, Globals, SourceMap, GLOBALS};
 use swc_ecmascript::{
     ast::*,
     parser::{lexer::Lexer, EsConfig, Parser, StringInput, Syntax, TsConfig},
 };
 
-#[js_function(1)]
+#[derive(Serialize)]
+pub struct AstOutput {
+    ast: String,
+    src: Option<Lrc<String>>,
+}
+
+#[js_function(2)]
 pub(crate) fn process_webpack_ast(cx: CallContext) -> napi::Result<JsObject> {
     let path = cx
         .get::<JsString>(0)?
         .into_utf8()?
         .as_str()
         .map(PathBuf::from)?;
+    let include_src = cx.get::<JsBoolean>(1)?.get_value()?;
 
-    let task = WebpackAstTask { path };
+    let task = WebpackAstTask { path, include_src };
     cx.env.spawn(task).map(|t| t.promise_object())
 }
 
 pub(crate) struct WebpackAstTask {
     pub path: PathBuf,
+    pub include_src: bool,
 }
 
 impl Task for WebpackAstTask {
     /// JSON string.
-    type Output = String;
+    type Output = AstOutput;
 
-    type JsValue = JsString;
+    type JsValue = JsObject;
 
     fn compute(&mut self) -> napi::Result<Self::Output> {
-        parse_file_as_webpack_ast(&self.path).convert_err()
+        parse_file_as_webpack_ast(&self.path, self.include_src).convert_err()
     }
 
     fn resolve(self, env: napi::Env, output: Self::Output) -> napi::Result<Self::JsValue> {
-        env.create_string(&output)
+        env.to_js_value(&output)?.coerce_to_object()
     }
 }
 
-pub fn parse_file_as_webpack_ast(path: &Path) -> Result<String, Error> {
+pub fn parse_file_as_webpack_ast(path: &Path, include_src: bool) -> Result<AstOutput, Error> {
     let globals = Globals::new();
     let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
 
@@ -90,8 +100,17 @@ pub fn parse_file_as_webpack_ast(path: &Path) -> Result<String, Error> {
             })?
         };
 
-        GLOBALS.set(&globals, || {
-            swc_webpack_ast::webpack_ast(cm.clone(), fm, module)
+        let ast = GLOBALS.set(&globals, || {
+            swc_webpack_ast::webpack_ast(cm.clone(), fm.clone(), module)
+        })?;
+
+        Ok(AstOutput {
+            ast,
+            src: if include_src {
+                Some(fm.src.clone())
+            } else {
+                None
+            },
         })
     })
 }
