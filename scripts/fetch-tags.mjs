1e9454741b71de567bb36b4bc4a80700016e3f9f
JJ Kasper <jj@jjsweb.site>
2021-12-09 14:45:15 -0600

    lint-fix

--- scripts/fetch-tags.mjs
+++ scripts/fetch-tags.mjs
@@ -1,6 +1,5 @@
-import {execSync} from 'child_process'
+import { execSync } from 'child_process'
 import execa from 'execa'
-
 ;(async () => {
   // <hash> (<tag>) <message>
   // parse only the last string which should be version if
@@ -8,25 +7,29 @@ import execa from 'execa'
   const commitMsg = execSync('git log --oneline -1').toString().trim()
   const versionString = commitMsg.split(' ').pop().trim()
   const publishMsgRegex = /^v\d{1,}\.\d{1,}\.\d{1,}(-\w{1,}\.\d{1,})?$/
-  
-  console.log({commitMsg, versionString});
-  
+
+  console.log({ commitMsg, versionString })
+
   if (publishMsgRegex.test(versionString)) {
-    console.log('publish commit, fetching tags'); 
-    
-    const result = await execa('git', ['fetch', '--depth=1', 'origin', '+refs/tags/*:refs/tags/*'], {
-      stdio: ['ignore', 'inherit', 'inherit'],
-      env: {
-        ...process.env,
-      },
-    })
-    
+    console.log('publish commit, fetching tags')
+
+    const result = await execa(
+      'git',
+      ['fetch', '--depth=1', 'origin', '+refs/tags/*:refs/tags/*'],
+      {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        env: {
+          ...process.env,
+        },
+      }
+    )
+
     if (result.exitCode !== 0) {
       throw new Error(
         `Failed to fetch tags, exited with code ${result.exitCode}`
       )
     }
   } else {
-    console.log('not publish commit');
+    console.log('not publish commit')
   }
 })()
