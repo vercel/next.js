9875cc48a66f4ed8951938960032069134883124
JJ Kasper <jj@jjsweb.site>
2021-12-09 14:32:17 -0600

    Update only fetch all tags for publish commits

---
+++ scripts/fetch-tags.mjs
@@ -0,0 +1,32 @@
+import {execSync} from 'child_process'
+import execa from 'execa'
+
+;(async () => {
+  // <hash> (<tag>) <message>
+  // parse only the last string which should be version if
+  // it's a publish commit
+  const commitMsg = execSync('git log --oneline -1').toString().trim()
+  const versionString = commitMsg.split(' ').pop().trim()
+  const publishMsgRegex = /^v\d{1,}\.\d{1,}\.\d{1,}(-\w{1,}\.\d{1,})?$/
+  
+  console.log({commitMsg, versionString});
+  
+  if (publishMsgRegex.test(versionString)) {
+    console.log('publish commit, fetching tags'); 
+    
+    const result = await execa('git', ['fetch', '--depth=1', 'origin', '+refs/tags/*:refs/tags/*'], {
+      stdio: ['ignore', 'inherit', 'inherit'],
+      env: {
+        ...process.env,
+      },
+    })
+    
+    if (result.exitCode !== 0) {
+      throw new Error(
+        `Failed to fetch tags, exited with code ${result.exitCode}`
+      )
+    }
+  } else {
+    console.log('not publish commit');
+  }
+})()
