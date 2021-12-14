ee220730dbc30ae710cf835257dc2f95a9c07124
Tobias Koppers <tobias.koppers@googlemail.com>
2021-12-14 11:33:04 +0100

    fixes to allow lazy compilation for import() (#32441)

--- packages/next-swc/crates/core/tests/fixture/next-dynamic/with-options/output-dev.js
+++ packages/next-swc/crates/core/tests/fixture/next-dynamic/with-options/output-dev.js
@@ -2,10 +2,6 @@ import dynamic from "next/dynamic";
 const DynamicComponentWithCustomLoading = dynamic(()=>import("../components/hello")
 , {
     loadableGenerated: {
-        webpack: ()=>[
-                require.resolveWeak("../components/hello")
-            ]
-        ,
         modules: [
             "some-file.js -> " + "../components/hello"
         ]
