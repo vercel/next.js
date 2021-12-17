0227d69392a8fdf6c064d28774248f2c8ed6a71d
Agustín Díaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2021-12-17 19:29:38 -0300

    Remove Image to be SSG compilant
    Change Image from "next/image" to img common tag

--- examples/blog-starter/components/cover-image.js
+++ examples/blog-starter/components/cover-image.js
@@ -1,10 +1,9 @@
 import cn from 'classnames'
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, height, width }) {
   const image = (
-    <Image
+    <img
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-sm', {
