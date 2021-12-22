48dba6457d842dab446ac48a37dc84a70644a5bc
Balázs Orbán <info@balazsorban.com>
2021-12-22 01:12:07 +0100

    chore: convert Jest examples to TypeScript (#32705)
    * chore: convert Jest examples to TypeScript
    
    * test: use absolute imports in test files
    
    * chore: address review comments
    
    * fix: add pages to tsconfig.json paths
    
    * fix: add styles to tsconfig.json paths
    
    * chore: upgrade dependencies/snapshots
    
    * docs: match Testing docs with example code
    
    * docs: link to docs from mock test files
    
    Co-authored-by: Lee Robinson <me@leerob.io>

--- examples/with-jest/__tests__/index.test.tsx
+++ examples/with-jest/__tests__/index.test.tsx
@@ -1,10 +1,5 @@
-/**
- * @jest-environment jsdom
- */
-
-import React from 'react'
 import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
+import Home from '@/pages/index'
 
 describe('Home', () => {
   it('renders a heading', () => {
