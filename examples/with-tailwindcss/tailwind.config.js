7f3cbc589f3c25b3bc4731b0bac7c17e91618ab5
kumard3 <kumardeepanshu157200@gmail.com>
2021-12-25 09:03:40 +0530

    converted to typescript

--- examples/with-tailwindcss/tailwind.config.js
+++ examples/with-tailwindcss/tailwind.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+module.exports = { 
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
