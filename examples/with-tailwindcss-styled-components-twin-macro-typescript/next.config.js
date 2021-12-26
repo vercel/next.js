b2ffd287740fd8f66cbc6f1156b093b75e7c33ea
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-12-26 22:57:38 +0530

    tailwindcss-styled-components and twin.macro integrated.

---
+++ examples/with-tailwindcss-styled-components-twin-macro-typescript/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    reactStrictMode: true,
+    // webpack5: true,
+    // webpack: (config) => {
+    //   config.resolve.fallback = { fs: false,
+    //     path: false, os:false};
+  
+    //   return config;
+    // },
+  }
