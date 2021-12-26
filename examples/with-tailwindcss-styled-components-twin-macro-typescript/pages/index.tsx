4f22cfc8d8fa8b899ffa71a3899eca17ceece2df
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-12-26 23:26:52 +0530

    ran prettier for linting

--- examples/with-tailwindcss-styled-components-twin-macro-typescript/pages/index.tsx
+++ examples/with-tailwindcss-styled-components-twin-macro-typescript/pages/index.tsx
@@ -1,8 +1,21 @@
 import Head from 'next/head'
-import {Container, Main, Title, TitleLink, Description, Code, Grid, Card, CardTitle, CardDetail, Footer, FooterLink, FooterImage } from '../styled-components'
+import {
+  Container,
+  Main,
+  Title,
+  TitleLink,
+  Description,
+  Code,
+  Grid,
+  Card,
+  CardTitle,
+  CardDetail,
+  Footer,
+  FooterLink,
+  FooterImage,
+} from '../styled-components'
 
 export default function Home() {
- 
   return (
     <Container>
       <Head>
@@ -12,49 +25,36 @@ export default function Home() {
 
       <Main>
         <Title>
-          Welcome to{' '}
-          <TitleLink href="https://nextjs.org">
-            Next.js!
-          </TitleLink>
+          Welcome to <TitleLink href="https://nextjs.org">Next.js!</TitleLink>
         </Title>
 
         <Description>
-          Get started by editing{' '}
-          <Code>
-            pages/index.tsx
-          </Code>
+          Get started by editing <Code>pages/index.tsx</Code>
         </Description>
 
         <Grid>
-          <Card
-            href="https://nextjs.org/docs">
-            <CardTitle >Documentation &rarr;</CardTitle>
+          <Card href="https://nextjs.org/docs">
+            <CardTitle>Documentation &rarr;</CardTitle>
             <CardDetail>
               Find in-depth information about Next.js features and API.
             </CardDetail>
           </Card>
 
-          <Card
-            href="https://nextjs.org/learn"
-          >
+          <Card href="https://nextjs.org/learn">
             <CardTitle>Learn &rarr;</CardTitle>
             <CardDetail className="mt-4 text-xl">
               Learn about Next.js in an interactive course with quizzes!
             </CardDetail>
           </Card>
 
-          <Card
-            href="https://github.com/vercel/next.js/tree/master/examples"
-         >
+          <Card href="https://github.com/vercel/next.js/tree/master/examples">
             <CardTitle>Examples &rarr;</CardTitle>
             <CardDetail>
               Discover and deploy boilerplate example Next.js projects.
             </CardDetail>
           </Card>
 
-          <Card
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
+          <Card href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
             <CardTitle>Deploy &rarr;</CardTitle>
             <CardDetail>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -69,8 +69,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <FooterImage src="/vercel.svg" alt="Vercel Logo"/>
+          Powered by <FooterImage src="/vercel.svg" alt="Vercel Logo" />
         </FooterLink>
       </Footer>
     </Container>
--- examples/with-tailwindcss-styled-components-twin-macro-typescript/postcss.config.js
+++ examples/with-tailwindcss-styled-components-twin-macro-typescript/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
