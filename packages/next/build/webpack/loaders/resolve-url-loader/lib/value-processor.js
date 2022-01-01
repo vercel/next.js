3b6b459e163ab75dccd5c63b581f08a56c2cf3c1
Tim Neutkens <tim@timneutkens.nl>
2022-01-01 19:23:53 +0100

    Use loader-utils 2

--- packages/next/build/webpack/loaders/resolve-url-loader/lib/value-processor.js
+++ packages/next/build/webpack/loaders/resolve-url-loader/lib/value-processor.js
@@ -22,16 +22,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-import loaderUtils from 'next/dist/compiled/loader-utils3'
+import loaderUtils from 'next/dist/compiled/loader-utils2'
 import path from 'path'
 
-/**
- * Create a value processing function for a given file path.
- *
- * @param {string} filename The current file being processed
- * @param {{absolute:string, keepQuery:boolean, join:function, root:string}} options Options hash
- * @return {function} value processing function
- */
 function valueProcessor(filename, options) {
   const URL_STATEMENT_REGEX =
     /(url\s*\()\s*(?:(['"])((?:(?!\2).)*)(\2)|([^'"](?:(?!\)).)*[^'"]))\s*(\))/g
@@ -49,57 +42,49 @@ function valueProcessor(filename, options) {
     //  split by url statements and process the content
     //  additional capture groups are needed to match quotations correctly
     //  escaped quotations are not considered
-    return value.split(URL_STATEMENT_REGEX).map(eachSplitOrGroup).join('')
+    return value
+      .split(URL_STATEMENT_REGEX)
+      .map((token, i, arr) => {
+        // we can get groups as undefined under certain match circumstances
+        const initialised = token || ''
 
-    /**
-     * Encode the content portion of <code>url()</code> statements.
-     * There are 4 capture groups in the split making every 5th unmatched.
-     *
-     * @param {string} token A single split item
-     * @param {number} i The index of the item in the split
-     * @param {Array} arr The array of split values
-     * @returns {string} Every 3 or 5 items is an encoded url everything else is as is
-     */
-    function eachSplitOrGroup(token, i, arr) {
-      // we can get groups as undefined under certain match circumstances
-      const initialised = token || ''
+        // the content of the url() statement is either in group 3 or group 5
+        const mod = i % 7
+        if (mod === 3 || mod === 5) {
+          // detect quoted url and unescape backslashes
+          const before = arr[i - 1],
+            after = arr[i + 1],
+            isQuoted = before === after && (before === "'" || before === '"'),
+            unescaped = isQuoted
+              ? initialised.replace(/\\{2}/g, '\\')
+              : initialised
 
-      // the content of the url() statement is either in group 3 or group 5
-      const mod = i % 7
-      if (mod === 3 || mod === 5) {
-        // detect quoted url and unescape backslashes
-        const before = arr[i - 1],
-          after = arr[i + 1],
-          isQuoted = before === after && (before === "'" || before === '"'),
-          unescaped = isQuoted
-            ? initialised.replace(/\\{2}/g, '\\')
-            : initialised
+          // split into uri and query/hash and then find the absolute path to the uri
+          const split = unescaped.split(/([?#])/g),
+            uri = split[0],
+            absolute =
+              (testIsRelative(uri) && join(uri, candidate)) ||
+              (testIsAbsolute(uri) && join(uri)),
+            query = options.keepQuery ? split.slice(1).join('') : ''
 
-        // split into uri and query/hash and then find the absolute path to the uri
-        const split = unescaped.split(/([?#])/g),
-          uri = split[0],
-          absolute =
-            (testIsRelative(uri) && join(uri, candidate)) ||
-            (testIsAbsolute(uri) && join(uri)),
-          query = options.keepQuery ? split.slice(1).join('') : ''
-
-        // use the absolute path in absolute mode or else relative path (or default to initialised)
-        // #6 - backslashes are not legal in URI
-        if (!absolute) {
-          return initialised
-        } else if (options.absolute) {
-          return absolute.replace(/\\/g, '/') + query
-        } else {
-          return loaderUtils.urlToRequest(
-            path.relative(directory, absolute).replace(/\\/g, '/') + query
-          )
+          // use the absolute path in absolute mode or else relative path (or default to initialised)
+          // #6 - backslashes are not legal in URI
+          if (!absolute) {
+            return initialised
+          } else if (options.absolute) {
+            return absolute.replace(/\\/g, '/') + query
+          } else {
+            return loaderUtils.urlToRequest(
+              path.relative(directory, absolute).replace(/\\/g, '/') + query
+            )
+          }
         }
-      }
-      // everything else, including parentheses and quotation (where present) and media statements
-      else {
-        return initialised
-      }
-    }
+        // everything else, including parentheses and quotation (where present) and media statements
+        else {
+          return initialised
+        }
+      })
+      .join('')
   }
 
   /**
