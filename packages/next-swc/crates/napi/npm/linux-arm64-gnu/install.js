d195de9c3c23199ccaa09ebaca324d0f648c07ee
Steven <steven@ceriously.com>
2022-01-03 15:26:27 -0500

    Simplify with ternary

--- packages/next-swc/crates/napi/npm/linux-arm64-gnu/install.js
+++ packages/next-swc/crates/napi/npm/linux-arm64-gnu/install.js
@@ -1,8 +1,2 @@
 const { glibcVersionRuntime } = process.report.getReport().header;
-const { name } = require('./package.json');
-
-if (glibcVersionRuntime && name.endsWith('-gnu') || !glibcVersionRuntime && !name.endsWith('-gnu')) {
-  process.exit(0);
-} else {
-  process.exit(1);
-}
+process.exit(glibcVersionRuntime ? 0 : 1);
