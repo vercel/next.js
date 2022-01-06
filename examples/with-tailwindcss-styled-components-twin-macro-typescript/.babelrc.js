f407a01f8e597cc6f73ac56b0afe1ea0df4cc420
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-06 15:41:05 +0530

    removed previous examples

--- examples/with-tailwindcss-styled-components-twin-macro-typescript/.babelrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [['next/babel', { 'preset-react': { runtime: 'automatic' } }]],
-  plugins: [
-    'babel-plugin-macros',
-    ['babel-plugin-styled-components', { ssr: true }],
-  ],
-}
