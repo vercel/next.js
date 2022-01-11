9836429beed54a7dffeaa88c199a77046b26608f
Tobias Koppers <tobias.koppers@googlemail.com>
2022-01-11 14:46:28 +0100

    drop dynamic import with `ssr: false` on server-side (#32606)

--- packages/next-swc/crates/core/tests/fixture/next-dynamic/with-options/input.js
+++ packages/next-swc/crates/core/tests/fixture/next-dynamic/with-options/input.js
@@ -4,3 +4,8 @@ const DynamicComponentWithCustomLoading = dynamic(
   () => import('../components/hello'),
   { loading: () => <p>...</p> }
 )
+
+const DynamicClientOnlyComponent = dynamic(
+  () => import('../components/hello'),
+  { ssr: false }
+)
