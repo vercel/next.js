c2c9bb06ce9e91f8b84ca30d507c457905c199f5
Donny <kdy1997.dev@gmail.com>
2022-01-12 09:23:33 +0900

    Update hash

--- packages/next-swc/crates/core/tests/fixture/styled-jsx/issue-30480/output.js
+++ packages/next-swc/crates/core/tests/fixture/styled-jsx/issue-30480/output.js
@@ -1,14 +1,14 @@
 import _JSXStyle from "styled-jsx/style";
 export default (({ breakPoint  })=><div className={_JSXStyle.dynamic([
         [
-            "6b843a9852a4be26",
+            "42a6f8ca08e1a043",
             [
                 breakPoint
             ]
         ]
     ])}>
 
-    <_JSXStyle id={"6b843a9852a4be26"} dynamic={[
+    <_JSXStyle id={"42a6f8ca08e1a043"} dynamic={[
         breakPoint
     ]}>{`@media (${breakPoint}) {}`}</_JSXStyle>
 
