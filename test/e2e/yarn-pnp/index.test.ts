0695815c2df6442fe781eae2a3a3b4ada4a164ee
JJ Kasper <jj@jjsweb.site>
2022-01-12 18:20:44 -0600

    update destory

--- test/e2e/yarn-pnp/index.test.ts
+++ test/e2e/yarn-pnp/index.test.ts
@@ -1,10 +1,18 @@
+import fs from 'fs-extra'
+import { join } from 'path'
+import { fetchViaHTTP } from 'next-test-utils'
 import { createNext, FileRef } from 'e2e-utils'
 import { NextInstance } from 'test/lib/next-modes/base'
-import { fetchViaHTTP } from 'next-test-utils'
-import { join } from 'path'
-import fs from 'fs-extra'
 
 describe('yarn PnP', () => {
+  let next: NextInstance
+
+  afterEach(async () => {
+    if (next) {
+      await next.destroy().catch(() => {})
+    }
+  })
+
   for (const example of [
     'progressive-web-app',
     'with-eslint',
@@ -13,36 +21,32 @@ describe('yarn PnP', () => {
     'with-mdx',
     'with-styled-components',
   ]) {
+    // eslint-disable-next-line no-loop-func
     it(`should compile and serve the index page correctly ${example}`, async () => {
       const srcDir = join(__dirname, '../../../examples', example)
       const srcFiles = await fs.readdir(srcDir)
-      let next: NextInstance
 
       const packageJson = await fs.readJson(join(srcDir, 'package.json'))
 
-      try {
-        next = await createNext({
-          files: srcFiles.reduce((prev, file) => {
-            prev[file] = new FileRef(join(srcDir, file))
-            return prev
-          }, {} as { [key: string]: FileRef }),
-          dependencies: {
-            ...packageJson.dependencies,
-            ...packageJson.devDependencies,
-          },
-          installCommand: `yarn set version berry && yarn config set pnpFallbackMode none && yarn config set enableGlobalCache true && YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install`,
-          buildCommand: `yarn build --no-lint`,
-          startCommand: (global as any).isNextDev
-            ? `yarn next`
-            : `yarn next start`,
-        })
+      next = await createNext({
+        files: srcFiles.reduce((prev, file) => {
+          prev[file] = new FileRef(join(srcDir, file))
+          return prev
+        }, {} as { [key: string]: FileRef }),
+        dependencies: {
+          ...packageJson.dependencies,
+          ...packageJson.devDependencies,
+        },
+        installCommand: `yarn set version berry && yarn config set pnpFallbackMode none && yarn config set enableGlobalCache true && YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install`,
+        buildCommand: `yarn build --no-lint`,
+        startCommand: (global as any).isNextDev
+          ? `yarn next`
+          : `yarn next start`,
+      })
 
-        const res = await fetchViaHTTP(next.url, '/')
-        expect(res.status).toBe(200)
-        expect(await res.text()).toContain('<html')
-      } finally {
-        await next.destroy()
-      }
+      const res = await fetchViaHTTP(next.url, '/')
+      expect(res.status).toBe(200)
+      expect(await res.text()).toContain('<html')
     })
   }
 })
