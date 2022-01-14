c2e6d03285c0bfcab3e25c0ed8f1018fa4fd2201
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-01-14 19:53:14 -0300

    docs: add skynexui to examples

---
+++ examples/with-skynexui-components/pages/_app.js
@@ -0,0 +1,15 @@
+import { Provider } from '@skynexui/components';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider theme={{
+        components: {
+            textField: {
+                variant: "basicBordered", // or put "bottomBorder"
+            }
+        }
+    }}>
+        <Component {...pageProps} />
+    </Provider>
+  );
+}
