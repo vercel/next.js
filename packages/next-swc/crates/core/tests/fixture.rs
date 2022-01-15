b6d924fa712315297cb67eec45eb06179c6774ef
Terence Bezman <terencebezman@gmail.com>
2022-01-15 01:53:45 -0800

    Remove artifact dir test since that's handled by the relay packages

--- packages/next-swc/crates/core/tests/fixture.rs
+++ packages/next-swc/crates/core/tests/fixture.rs
@@ -1,4 +1,4 @@
-use next_swc::relay::{relay, RelayLanguageConfig};
+use next_swc::relay::{relay, test_relay, RelayLanguageConfig};
 use next_swc::{
     amp_attributes::amp_attributes,
     next_dynamic::next_dynamic,
@@ -9,6 +9,9 @@ use next_swc::{
     shake_exports::{shake_exports, Config as ShakeExportsConfig},
     styled_jsx::styled_jsx,
 };
+use relay_compiler::config::Config;
+use relay_compiler::ProjectConfig;
+use relay_config::TypegenLanguage;
 use std::path::PathBuf;
 use swc_common::{chain, comments::SingleThreadedComments, FileName, Mark, Span, DUMMY_SP};
 use swc_ecma_transforms_testing::{test, test_fixture};
@@ -151,43 +154,16 @@ fn page_config_fixture(input: PathBuf) {
     test_fixture(syntax(), &|_tr| page_config_test(), &input, &output);
 }
 
-#[fixture("tests/fixture/relay/no-artifact-dir/**/input.ts*")]
+#[fixture("tests/fixture/relay/**/input.ts*")]
 fn relay_no_artifact_dir_fixture(input: PathBuf) {
     let output = input.parent().unwrap().join("output.js");
     test_fixture(
         syntax(),
         &|_tr| {
-            relay(
-                next_swc::relay::Config {
-                    artifact_directory: None,
-                    language: RelayLanguageConfig::Typescript,
-                },
-                FileName::Real(PathBuf::from(input.clone())),
-            )
-        },
-        &input,
-        &output,
-    );
-}
+            let mut config = ProjectConfig::default();
+            config.typegen_config.language = TypegenLanguage::TypeScript;
 
-#[fixture("tests/fixture/relay/artifact-dir/**/input.ts*")]
-fn relay_fixture(input: PathBuf) {
-    let output = input.parent().unwrap().join("output.js");
-    test_fixture(
-        Syntax::Typescript(TsConfig {
-            tsx: true,
-            decorators: false,
-            dts: false,
-            no_early_errors: false,
-        }),
-        &|_tr| {
-            relay(
-                next_swc::relay::Config {
-                    artifact_directory: Some(PathBuf::from("some/generated/dir")),
-                    language: RelayLanguageConfig::Typescript,
-                },
-                FileName::Real(PathBuf::from(input.clone())),
-            )
+            test_relay(FileName::Real(PathBuf::from(input.clone())), config)
         },
         &input,
         &output,
