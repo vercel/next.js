27057e5377174aa2423791609e40c945b5520451
Terence Bezman <terencebezman@gmail.com>
2022-01-17 23:07:09 -0800

    Fix unit tests

--- test/integration/relay-graphql-swc-multi-project/relay.config.js
+++ test/integration/relay-graphql-swc-multi-project/relay.config.js
@@ -1,15 +1,10 @@
 module.exports = {
   root: './',
   sources: {
-    'project-a/pages': ['project-a', 'should-fail'],
+    'project-a/pages': 'project-a',
     'project-b/pages': 'project-b',
   },
   projects: {
-    'should-fail': {
-      schema: 'schema.graphql',
-      language: 'typescript',
-      output: '__generated__',
-    },
     'project-a': {
       schema: 'schema.graphql',
       language: 'typescript',
