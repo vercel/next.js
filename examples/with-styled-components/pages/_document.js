ac42c6ecdefb619accc08e4a57a0066a50ba52ce
JJ Kasper <jj@jjsweb.site>
2022-02-05 15:58:41 -0600

    lint-fix

--- examples/with-styled-components/pages/_document.js
+++ examples/with-styled-components/pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from "next/document"
-import { ServerStyleSheet } from "styled-components"
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   render() {
