9081cf870b14844da189007287a4de7ee2932fd8
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2022-02-07 03:01:14 +0530

    progressive web app example converted to typescript  (#33100)
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Errors have helpful link attached, see `contributing.md`
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the feature request has been accepted for implementation before opening a PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have helpful link attached, see `contributing.md`
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `yarn lint`
    
    
    Co-authored-by: JJ Kasper <22380829+ijjk@users.noreply.github.com>

--- examples/progressive-web-app/pages/_app.tsx
+++ examples/progressive-web-app/pages/_app.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+import { AppProps } from 'next/app'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
