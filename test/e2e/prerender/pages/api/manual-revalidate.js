a44187b3791fa91ac40f3a9b87485588d00d5c4e
JJ Kasper <jj@jjsweb.site>
2022-02-08 10:19:19 -0600

    update util

--- test/e2e/prerender/pages/api/manual-revalidate.js
+++ test/e2e/prerender/pages/api/manual-revalidate.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
   // WARNING: don't use user input in production
   // make sure to use trusted value for revalidating
-  const revalidateRes = await res.unstable_revalidate(req.query.pathname)
+  let revalidated = false
+  try {
+    await res.unstable_revalidate(req.query.pathname)
+    revalidated = true
+  } catch (err) {
+    console.error(err)
+  }
   res.json({
-    revalidated: true,
-    status: revalidateRes.status,
-    text: await revalidateRes.text(),
+    revalidated,
   })
 }
