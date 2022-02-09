f8c23729856941fde32def40a02b715c821992d4
nkzawa <naoyuki.kanezawa@gmail.com>
2022-02-09 17:02:17 +0700

    use iterator instead of generator

--- packages/next/shared/lib/router/utils/routing-items.ts
+++ packages/next/shared/lib/router/utils/routing-items.ts
@@ -12,29 +12,49 @@ export interface RoutingItem {
   isMiddleware?: boolean
 }
 
-export function* getRoutingItems(
+export function getRoutingItems(
   pages: string[],
   middleware: { page: string; ssr: boolean }[]
-): Generator<RoutingItem> {
+): IterableIterator<RoutingItem> {
   // append the suffix so that `getSortedRoutes()` can handle middleware properly
   const middlewarePages = middleware.map((m) => `${m.page}${MIDDLEWARE_SUFFIX}`)
 
   const middlewareMap = new Map(middleware.map((m) => [m.page, m]))
+  const sortedRoutes = getSortedRoutes([...pages, ...middlewarePages])
+  let i = 0
 
-  for (const page of getSortedRoutes([...pages, ...middlewarePages]))
-    if (page.endsWith(MIDDLEWARE_SUFFIX)) {
-      const p = page.slice(0, -MIDDLEWARE_SUFFIX.length) || '/'
-      const { ssr } = middlewareMap.get(p)!
-      yield {
-        match: getRouteMatcher(getMiddlewareRegex(p, !ssr)),
-        page: p,
-        ssr,
-        isMiddleware: true,
+  return {
+    [Symbol.iterator](): IterableIterator<RoutingItem> {
+      return this
+    },
+
+    next(): IteratorResult<RoutingItem> {
+      if (i >= sortedRoutes.length) {
+        return { done: true, value: null }
       }
-    } else {
-      yield {
-        match: getRouteMatcher(getRouteRegex(page)),
-        page,
+
+      const page = sortedRoutes[i++]
+      if (page.endsWith(MIDDLEWARE_SUFFIX)) {
+        const p = page.slice(0, -MIDDLEWARE_SUFFIX.length) || '/'
+        const { ssr } = middlewareMap.get(p)!
+        return {
+          done: false,
+          value: {
+            match: getRouteMatcher(getMiddlewareRegex(p, !ssr)),
+            page: p,
+            ssr,
+            isMiddleware: true,
+          },
+        }
+      } else {
+        return {
+          done: false,
+          value: {
+            match: getRouteMatcher(getRouteRegex(page)),
+            page,
+          },
+        }
       }
-    }
+    },
+  }
 }
