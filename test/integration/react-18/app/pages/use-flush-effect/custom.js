c51b271ebbd4f3ec5f6532c4036ac82e6a5c37b3
Gerald Monaco <gbmonaco@google.com>
2022-02-15 20:18:29 +0000

    Fix test

--- test/integration/react-18/app/pages/use-flush-effect/custom.js
+++ test/integration/react-18/app/pages/use-flush-effect/custom.js
@@ -4,8 +4,8 @@ export default function Custom() {
   if (typeof window === 'undefined') {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useFlushEffects([
-      () => <script id="custom-flush-effect-1">foo</script>,
-      () => <script id="custom-flush-effect-2">bar</script>,
+      () => <span id="custom-flush-effect-1">foo</span>,
+      () => <span id="custom-flush-effect-2">bar</span>,
     ])
   }
   return null
