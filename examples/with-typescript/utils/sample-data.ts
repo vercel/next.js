ba8334fdcb79a2518e82f6186bc9129eb5c1629d
Balázs Orbán <info@balazsorban.com>
2022-02-16 22:39:47 +0100

    chore: delete old example files

--- examples/with-typescript/utils/sample-data.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../interfaces'
-
-/** Dummy user data. */
-export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
-]
