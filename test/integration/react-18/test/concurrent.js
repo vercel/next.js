a90bffa7a1625cc81b9d1113bd078b767cd43c0a
Gerald Monaco <gbmonaco@google.com>
2022-02-16 00:49:31 +0000

    Fix tests

--- test/integration/react-18/test/concurrent.js
+++ test/integration/react-18/test/concurrent.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 
 import webdriver from 'next-webdriver'
-import { check } from 'next-test-utils'
+import { check, renderViaHTTP } from 'next-test-utils'
 
 export default (context, _render) => {
   async function withBrowser(path, cb) {
