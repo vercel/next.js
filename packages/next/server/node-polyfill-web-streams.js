bfabc523308a4d18284c87491fc09f56cf9d59ff
Peter Mbanugo <p.mbanugo@yahoo.com>
2022-02-21 10:28:58 +0100

    Polyfill TransformStream in Node.js
    Extend conditional check for WebStream in Node.js environment

--- packages/next/server/node-polyfill-web-streams.js
+++ packages/next/server/node-polyfill-web-streams.js
@@ -2,7 +2,7 @@ import { TransformStream } from 'next/dist/compiled/web-streams-polyfill'
 import { ReadableStream } from './web/sandbox/readable-stream'
 
 // Polyfill Web Streams in the Node.js environment
-if (!global.ReadableStream) {
+if (!global.ReadableStream || !global.TransformStream) {
   global.ReadableStream = ReadableStream
   global.TransformStream = TransformStream
 }
