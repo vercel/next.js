d118d024f726492a4d6ff592d13dfb5684da1030
JJ Kasper <jj@jjsweb.site>
2022-02-25 14:08:08 -0800

    Clean-up error when starting next with non-existent dir (#34830)

--- packages/next/lib/get-project-dir.ts
+++ packages/next/lib/get-project-dir.ts
@@ -3,17 +3,29 @@ import path from 'path'
 import * as Log from '../build/output/log'
 
 export function getProjectDir(dir?: string) {
-  const resolvedDir = path.resolve(dir || '.')
-  const realDir = fs.realpathSync.native(resolvedDir)
+  try {
+    const resolvedDir = path.resolve(dir || '.')
+    const realDir = fs.realpathSync.native(resolvedDir)
 
-  if (
-    resolvedDir !== realDir &&
-    resolvedDir.toLowerCase() === realDir.toLowerCase()
-  ) {
-    Log.warn(
-      `Invalid casing detected for project dir, received ${resolvedDir} actual path ${realDir}, see more info here https://nextjs.org/docs/messages/invalid-project-dir-casing`
-    )
+    if (
+      resolvedDir !== realDir &&
+      resolvedDir.toLowerCase() === realDir.toLowerCase()
+    ) {
+      Log.warn(
+        `Invalid casing detected for project dir, received ${resolvedDir} actual path ${realDir}, see more info here https://nextjs.org/docs/messages/invalid-project-dir-casing`
+      )
+    }
+
+    return realDir
+  } catch (err: any) {
+    if (err.code === 'ENOENT') {
+      Log.error(
+        `Invalid project directory provided, no such directory: ${path.resolve(
+          dir || '.'
+        )}`
+      )
+      process.exit(1)
+    }
+    throw err
   }
-
-  return realDir
 }
