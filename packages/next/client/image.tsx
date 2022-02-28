57702cb2a9a9dba4b552e0007c16449cf36cfb44
Jiachi Liu <inbox@huozhi.im>
2022-02-28 23:39:51 +0100

    Only warn styles and scripts under next head in concurrent mode (#34897)
    x-ref: #34021 , #34004
    
    Only log each warning once and only trigger in concurrent mode
    
    
    ## Bug
    
    - [x] Related issues linked using `fixes #number`
    - [x] Integration tests added
    - [x] Errors have helpful link attached, see `contributing.md`

--- packages/next/client/image.tsx
+++ packages/next/client/image.tsx
@@ -8,6 +8,7 @@ import {
 } from '../shared/lib/image-config'
 import { useIntersection } from './use-intersection'
 import { ImageConfigContext } from '../shared/lib/image-config-context'
+import { warnOnce } from '../shared/lib/utils'
 
 const configEnv = process.env.__NEXT_IMAGE_OPTS as any as ImageConfigComplete
 const loadedImageURLs = new Set<string>()
@@ -21,17 +22,6 @@ const emptyDataURL =
 
 if (typeof window === 'undefined') {
   ;(global as any).__NEXT_IMAGE_IMPORTED = true
-}
-
-let warnOnce = (_: string) => {}
-if (process.env.NODE_ENV !== 'production') {
-  const warnings = new Set<string>()
-  warnOnce = (msg: string) => {
-    if (!warnings.has(msg)) {
-      console.warn(msg)
-    }
-    warnings.add(msg)
-  }
 }
 
 const VALID_LOADING_VALUES = ['lazy', 'eager', undefined] as const
