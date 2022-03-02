6bc20d666ed6e00adadf7e835b8b705ef5e6a6b4
Lydia Hallie <lydiajuliettehallie@gmail.com>
2022-03-01 17:30:15 -0700

    Add replaceable var in images domains array

--- examples/cms-wordpress/next.config.js
+++ examples/cms-wordpress/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
     domains: [
-      "vercel.wpengine.com",
+      // "[yourapp].wpengine.com" (Update this to be your Wordpress application name in order to load images connected to your posts)
       "secure.gravatar.com"
     ]
   }
