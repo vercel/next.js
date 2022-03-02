7b2fb70a67ac114414941ea93a1351871bfe46ea
Gal Schlezinger <gal@spitfire.co.il>
2022-03-02 17:09:36 +0200

    Expose WASM bindings in Middleware (#34437)
    This PR introduces a way to use WASM in middlewares.
    Next.js will find all `.wasm` imports in middlewares and load them as `WebAssembly.Module` objects, which then can be later instantiated.
    The metadata will be stored in `middleware-manifest.json`
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Errors have helpful link attached, see `contributing.md`
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the feature request has been accepted for implementation before opening a PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have helpful link attached, see `contributing.md`
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `yarn lint`

--- packages/next/build/webpack/plugins/functions-manifest-plugin.ts
+++ packages/next/build/webpack/plugins/functions-manifest-plugin.ts
@@ -3,7 +3,7 @@ import { sources, webpack5 } from 'next/dist/compiled/webpack/webpack'
 import { normalizePagePath } from '../../../server/normalize-page-path'
 import { FUNCTIONS_MANIFEST } from '../../../shared/lib/constants'
 import { getPageFromPath } from '../../entries'
-import { collectAssets, getEntrypointInfo } from './middleware-plugin'
+import { collectAssets, getEntrypointInfo, PerRoute } from './middleware-plugin'
 
 const PLUGIN_NAME = 'FunctionsManifestPlugin'
 export interface FunctionsManifest {
@@ -52,7 +52,7 @@ export default class FunctionsManifestPlugin {
   createAssets(
     compilation: webpack5.Compilation,
     assets: any,
-    envPerRoute: Map<string, string[]>,
+    perRoute: PerRoute,
     isEdgeRuntime: boolean
   ) {
     const functionsManifest: FunctionsManifest = {
@@ -60,7 +60,7 @@ export default class FunctionsManifestPlugin {
       pages: {},
     }
 
-    const infos = getEntrypointInfo(compilation, envPerRoute, isEdgeRuntime)
+    const infos = getEntrypointInfo(compilation, perRoute, isEdgeRuntime)
     infos.forEach((info) => {
       const { page } = info
       // TODO: use global default runtime instead of 'web'
