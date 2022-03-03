72a1ad0856e96305a339b7d39189409078a2f7f6
Shu Ding <g@shud.in>
2022-03-03 16:57:11 +0100

    fix test

--- test/integration/react-streaming-and-server-components/test/runtime.js
+++ test/integration/react-streaming-and-server-components/test/runtime.js
@@ -3,10 +3,10 @@ import { renderViaHTTP } from 'next-test-utils'
 export default async function runtime(context, { runtime }) {
   if (runtime === 'edge') {
     it('should support per-page runtime configuration', async () => {
-      const html = await renderViaHTTP(context.appPort, '/runtime')
-      expect(html).toContain('Runtime: Node.js')
-      const html = await renderViaHTTP(context.appPort, '/runtime-rsc')
-      expect(html).toContain('Runtime: Node.js')
+      const html1 = await renderViaHTTP(context.appPort, '/runtime')
+      expect(html1).toContain('Runtime: Node.js')
+      const html2 = await renderViaHTTP(context.appPort, '/runtime-rsc')
+      expect(html2).toContain('Runtime: Node.js')
     })
   }
 }
