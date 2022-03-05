c3a8ab46054bb2ebff7b1037e46f42c7a485eca0
James Perkins <jamesperkins@hey.com>
2022-03-04 21:20:45 -0500

    Update the width height
    Change from ) to }

--- examples/blog-starter/components/avatar.js
+++ examples/blog-starter/components/avatar.js
@@ -4,8 +4,8 @@ export default function Avatar({ name, picture }) {
     <div className="flex items-center">
       <Image
         src={picture}
-        width={48)
-        height={48)
+        width={48}
+        height={48}
         className="w-12 h-12 rounded-full mr-4"
         alt={name}
       />
