6a6902e9479668ffaac375c7d52f95536fbb65e0
Jiachi Liu <inbox@huozhi.im>
2022-03-06 01:07:12 +0100

    fix format with new version of prettier

--- examples/cms-tina/.tina/__generated__/types.ts
+++ examples/cms-tina/.tina/__generated__/types.ts
@@ -7,9 +7,13 @@ export type Exact<T extends { [key: string]: unknown }> = {
   [K in keyof T]: T[K]
 }
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> }
+  {
+    [SubKey in K]?: Maybe<T[SubKey]>
+  }
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> }
+  {
+    [SubKey in K]: Maybe<T[SubKey]>
+  }
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string
