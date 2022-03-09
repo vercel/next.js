abf2d9ac94339136a90352f15dd78b0de589c78c
JJ Kasper <jj@jjsweb.site>
2022-03-09 16:23:55 -0600

    Apply suggestions from code review

--- packages/next/server.js
+++ packages/next/server.js
@@ -1,2 +1,6 @@
-exports.NextRequest = require('next/dist/server/web/spec-extension/request').NextRequest;
-exports.NextResponse = require('next/dist/server/web/spec-extension/response').NextResponse;
+module.exports = {
+  NextRequest: require('next/dist/server/web/spec-extension/request').NextRequest,
+  NextResponse: require('next/dist/server/web/spec-extension/response').NextResponse
+}
+
+
