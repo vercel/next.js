9b40fbd1108a7066353ba7e8df181fffee4396b3
Lee Robinson <lrobinson2011@gmail.com>
2022-03-10 21:56:41 -0600

    Update create-next-app template.

--- packages/create-next-app/templates/typescript/pages/api/hello.ts
+++ packages/create-next-app/templates/typescript/pages/api/hello.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  framework: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ framework: 'Next.js' })
 }
