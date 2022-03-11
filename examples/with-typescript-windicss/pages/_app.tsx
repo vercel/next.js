15cd8c090de78cf1ad2703a3f2ebbe158247babe
Avi Avinav <aviavinav2004@gmail.com>
2022-03-11 19:55:37 +0530

    add windicss example

---
+++ examples/with-typescript-windicss/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import 'windi.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
