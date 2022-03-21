e918fee69d42536cff7167eaace1ae182efaf811
JJ Kasper <jj@jjsweb.site>
2022-03-21 13:45:23 -0500

    add with-react-18 for debugging

--- test/e2e/root-dir/app/next.config.js
+++ test/e2e/root-dir/app/next.config.js
@@ -1,8 +1,20 @@
-module.exports = {
+const path = require('path')
+
+let withReact18 = (config) => config
+
+try {
+  // only used when running inside of the monorepo not when isolated
+  withReact18 = require(path.join(
+    __dirname,
+    '../../../integration/react-18/test/with-react-18'
+  ))
+} catch (_) {}
+
+module.exports = withReact18({
   experimental: {
     rootDir: true,
     runtime: 'nodejs',
     reactRoot: true,
     serverComponents: true,
   },
-}
+})
