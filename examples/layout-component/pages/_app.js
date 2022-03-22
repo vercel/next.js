956f84a3794ad82bbf1f6bc62c4e9c834b15a720
eejdan <65033345+eejdan@users.noreply.github.com>
2022-03-22 08:38:13 +0200

    _app.js bug fix
    Edited so if there is no layout defined at page level, the default layout will ensue and render the page, nested.

--- examples/layout-component/pages/_app.js
+++ examples/layout-component/pages/_app.js
@@ -1,8 +1,9 @@
 import '../global.css'
+import Layout from '../components/layout'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => Layout(page))
 
   return getLayout(<Component {...pageProps} />)
 }
