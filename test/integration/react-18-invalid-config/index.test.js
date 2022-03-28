41a2eb2303bd338b110a1dc170ad5fdc4b805c0e
Jiachi Liu <inbox@huozhi.im>
2022-03-28 14:32:34 +0200

    Clean up legacy react 18 tests (#35615)
    ### source changes
    * Error dynamic suspense option in non concurrent mode, don't check react root anymore since they should be bound
    
    ### tests changes
    
    * Remove duplicated rsc client test
    * Merge type checking cases (`next/dynamic`)
    * Remove blocking rendering tests cases since we should opt-in Fizz rendering when using React.lazy
    * Remove some thrown promises cases, leverage the streaming component we had in RSC test

--- test/integration/react-18-invalid-config/index.test.js
+++ test/integration/react-18-invalid-config/index.test.js
@@ -6,6 +6,7 @@ import { File, nextBuild } from 'next-test-utils'
 
 const appDir = __dirname
 const nextConfig = new File(join(appDir, 'next.config.js'))
+const reactDomPackagePah = join(appDir, 'node_modules/react-dom')
 
 function writeNextConfig(config) {
   const content = `module.exports = { experimental: ${JSON.stringify(config)} }`
@@ -24,21 +25,36 @@ describe('Invalid react 18 webpack config', () => {
       '`experimental.runtime` requires `experimental.reactRoot` to be enabled along with React 18.'
     )
   })
+})
 
-  it('should warn user when not using react 18 and `experimental.reactRoot` is enabled', async () => {
-    const reactDomPackagePah = join(appDir, 'node_modules/react-dom')
+describe('React 17 with React 18 config', () => {
+  beforeAll(async () => {
     await fs.mkdirp(reactDomPackagePah)
     await fs.writeFile(
       join(reactDomPackagePah, 'package.json'),
       JSON.stringify({ name: 'react-dom', version: '17.0.0' })
     )
     writeNextConfig({ reactRoot: true })
-    const { stderr } = await nextBuild(appDir, [], { stderr: true })
+  })
+  afterAll(async () => {
     await fs.remove(reactDomPackagePah)
     nextConfig.restore()
+  })
 
+  it('should warn user when not using react 18 and `experimental.reactRoot` is enabled', async () => {
+    const { stderr } = await nextBuild(appDir, [], { stderr: true })
     expect(stderr).toContain(
       'You have to use React 18 to use `experimental.reactRoot`.'
     )
+  })
+
+  it('suspense is not allowed in blocking rendering mode', async () => {
+    const { stderr, code } = await nextBuild(appDir, [], {
+      stderr: true,
+    })
+    expect(stderr).toContain(
+      'Invalid suspense option usage in next/dynamic. Read more: https://nextjs.org/docs/messages/invalid-dynamic-suspense'
+    )
+    expect(code).toBe(1)
   })
 })
