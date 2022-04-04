c773bb7c6ae5c3be8ebf93271e2bfd167d5c1228
Balázs Orbán <info@balazsorban.com>
2022-04-04 11:00:26 +0200

    test: add test

---
+++ test/e2e/interpolate-dynamic-path-with-search-param/index.test.ts
@@ -0,0 +1,51 @@
+import { createNext } from 'e2e-utils'
+import webdriver from 'next-webdriver'
+import { BrowserInterface } from 'test/lib/browsers/base'
+import { NextInstance } from 'test/lib/next-modes/base'
+
+describe('Interpolate dynamic path with search param', () => {
+  let next: NextInstance
+  let browser: BrowserInterface
+  beforeAll(async () => {
+    next = await createNext({
+      files: {
+        'pages/index.js': `
+          import Link from "next/link"
+
+          export default function Page() {
+            return (
+              <Link
+                href={{
+                  pathname: "/days/[day]",
+                  query: { day: "foo" },
+                  search: "foo=bar",
+                }}
+              >
+                Day
+              </Link>
+            )
+          }
+        `,
+        'pages/days/[day].js': `
+          import { useRouter } from "next/router"
+
+          export default function Page() {
+            const router = useRouter()
+            return <p>{router.asPath}</p>
+          }`,
+      },
+      dependencies: {},
+    })
+  })
+  afterAll(async () => {
+    await next.destroy()
+    await browser.close()
+  })
+
+  it('should work', async () => {
+    browser = await webdriver(next.appPort, '/')
+    await browser.elementByCss('a').click()
+    const path = await browser.elementByCss('p').text()
+    expect(path).toBe('/days/foo?foo=bar')
+  })
+})
