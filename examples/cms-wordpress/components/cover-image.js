33e169c3c79217eb1aceab3d2e342fffaf483447
indicozy <berkutkarlibai@gmail.com>
2022-04-05 13:52:32 +0600

    Updated graphql API to support v1.7.2

--- examples/cms-wordpress/components/cover-image.js
+++ examples/cms-wordpress/components/cover-image.js
@@ -8,7 +8,7 @@ export default function CoverImage({ title, coverImage, slug }) {
       width={2000}
       height={1000}
       alt={`Cover Image for ${title}`}
-      src={coverImage?.sourceUrl}
+      src={coverImage?.node.sourceUrl}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
