204497248fd3c7f6b8a958806c1271b422957ed8
Tobias Koppers <tobias.koppers@googlemail.com>
2022-04-05 21:23:42 +0200

    add node_file_trace benchmark

--- crates/turbopack/benches/mod.rs
+++ crates/turbopack/benches/mod.rs
@@ -1,6 +1,8 @@
 use criterion::{criterion_group, criterion_main};
 
 mod analyzer;
+mod node_file_trace;
 
-criterion_group!(benches, analyzer::benchmark);
-criterion_main!(benches);
+criterion_group!(analyzer_benches, analyzer::benchmark);
+criterion_group!(node_file_trace_benches, node_file_trace::benchmark);
+criterion_main!(analyzer_benches, node_file_trace_benches);
