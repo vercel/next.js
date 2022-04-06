e190ba2540fba757c9a3aa672304cf9a1ace5c80
John Tarus <47330228+johntarus@users.noreply.github.com>
2022-04-06 12:51:52 +0300

    Add 'unoptimized' to the Image className
    For tailwind, you can just add 'unoptimized' to the Image className and you are ready to go:

--- examples/image-component/next.config.js
+++ examples/image-component/next.config.js
@@ -4,3 +4,13 @@ module.exports = {
     formats: ['image/avif', 'image/webp'],
   },
 }
+
+//For tailwind you can just add 'unoptimized' to the Image className and you are ready to go:
+<Image
+          alt={author.name}
+          unoptimized
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+          src={author.photo.url}
+        />
