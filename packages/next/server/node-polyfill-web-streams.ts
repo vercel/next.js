4e2fa7bff6ca0dc762628c7e5d18398be23e2fec
Shu Ding <g@shud.in>
2022-04-06 16:35:14 +0200

    type fix

--- packages/next/server/node-polyfill-web-streams.ts
+++ packages/next/server/node-polyfill-web-streams.ts
@@ -1,12 +1,12 @@
-// @ts-nocheck
-
 import {
   ReadableStream,
   TransformStream,
 } from 'next/dist/compiled/web-streams-polyfill'
 
 // Polyfill Web Streams for the Node.js runtime.
+// @ts-ignore
 global.ReadableStream = ReadableStream
+// @ts-ignore
 global.TransformStream = TransformStream
 
 export { ReadableStream, TransformStream }
