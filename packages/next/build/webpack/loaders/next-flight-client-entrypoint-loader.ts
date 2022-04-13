997c0834f4a831ea63a1d1e9c4e509a0c9c6480a
Tim Neutkens <tim@timneutkens.nl>
2022-04-13 16:21:12 +0200

    Fix client component entrypoints not being handled

--- packages/next/build/webpack/loaders/next-flight-client-entrypoint-loader.ts
+++ packages/next/build/webpack/loaders/next-flight-client-entrypoint-loader.ts
@@ -1,7 +1,7 @@
 export default async function transformSource(this: any): Promise<string> {
   const { absolutePagePath } = this.getOptions()
   return `
-    import ComponentRef from '${absolutePagePath}?__sc_client__';
+    import ComponentRef from 'next-flight-client-loader!${absolutePagePath}';
 
     export const noop = () => require('${absolutePagePath}');
 
