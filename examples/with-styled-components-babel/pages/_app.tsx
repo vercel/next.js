9f2f79dc2e7c0285f2baf8c0296bb49c0f253a5c
Rafo <53544360+rafae2k@users.noreply.github.com>
2022-04-13 12:07:00 -0300

    refactor: change files to .tsx and add TS types

--- examples/with-styled-components-babel/pages/_app.tsx
+++ examples/with-styled-components-babel/pages/_app.tsx
@@ -8,7 +8,13 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const theme = {
+interface ThemeInterface {
+  colors: {
+    primary: string
+  }
+}
+
+const theme: ThemeInterface = {
   colors: {
     primary: '#0070f3',
   },
