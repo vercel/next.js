9474bfe74fcfd39125876a505295534464ed39fc
ViolanteCodes <maria@buttercms.com>
2022-04-14 10:01:06 -0400

    Fix eslint

--- examples/cms-buttercms/components/author-card.js
+++ examples/cms-buttercms/components/author-card.js
@@ -1,14 +1,6 @@
 export default function AuthorCard({ author }) {
-  const authorAvatar = author.profile_image
-    ? author.profile_image
-    : '/images/team/team-1.png'
   return (
     <a>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={authorAvatar}
-        alt={`Profile image ${author.first_name} ${author.last_name}`}
-      />
       {author.first_name} {author.last_name}
     </a>
   )
