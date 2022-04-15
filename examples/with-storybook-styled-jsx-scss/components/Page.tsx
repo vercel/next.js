0cf155c105c29f57732c991e46a847a800af7d6f
Johan Mattsson <johan.mattsson@hotmail.se>
2022-04-15 20:57:24 +0200

    examples/with-storybook-styled-jsx-scss: Escape quotes

--- examples/with-storybook-styled-jsx-scss/components/Page.tsx
+++ examples/with-storybook-styled-jsx-scss/components/Page.tsx
@@ -96,7 +96,7 @@ export const Page: React.FC<PageProps> = ({
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            such data from the &quot;args&quot; of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
