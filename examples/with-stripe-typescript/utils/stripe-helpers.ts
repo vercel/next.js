75bfdc0a7888784a2703151f2857c6703988f161
Johan Mattsson <johan.mattsson@hotmail.se>
2022-04-15 21:06:09 +0200

    examples/with-stripe-typescript: Use 'const' instead of 'let'

--- examples/with-stripe-typescript/utils/stripe-helpers.ts
+++ examples/with-stripe-typescript/utils/stripe-helpers.ts
@@ -2,7 +2,7 @@ export function formatAmountForDisplay(
   amount: number,
   currency: string
 ): string {
-  let numberFormat = new Intl.NumberFormat(['en-US'], {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
     style: 'currency',
     currency: currency,
     currencyDisplay: 'symbol',
@@ -14,14 +14,14 @@ export function formatAmountForStripe(
   amount: number,
   currency: string
 ): number {
-  let numberFormat = new Intl.NumberFormat(['en-US'], {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
     style: 'currency',
     currency: currency,
     currencyDisplay: 'symbol',
   })
   const parts = numberFormat.formatToParts(amount)
   let zeroDecimalCurrency: boolean = true
-  for (let part of parts) {
+  for (const part of parts) {
     if (part.type === 'decimal') {
       zeroDecimalCurrency = false
     }
@@ -33,14 +33,14 @@ export function formatAmountFromStripe(
   amount: number,
   currency: string
 ): number {
-  let numberFormat = new Intl.NumberFormat(['en-US'], {
+  const numberFormat = new Intl.NumberFormat(['en-US'], {
     style: 'currency',
     currency: currency,
     currencyDisplay: 'symbol',
   })
   const parts = numberFormat.formatToParts(amount)
   let zeroDecimalCurrency: boolean = true
-  for (let part of parts) {
+  for (const part of parts) {
     if (part.type === 'decimal') {
       zeroDecimalCurrency = false
     }
