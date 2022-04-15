80c99ee4faa36786d9282ea6804b40e87c0b0e28
Johan Mattsson <johan.mattsson@hotmail.se>
2022-04-15 20:20:23 +0200

    examples/with-redux-persist: Fix duplicated import

--- examples/with-redux-persist/components/data-list.js
+++ examples/with-redux-persist/components/data-list.js
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { loadExampleData, loadingExampleDataFailure } from '../store'
-import { useEffect } from 'react'
 
 const DataList = () => {
   const dispatch = useDispatch()
