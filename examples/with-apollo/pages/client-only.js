cc62476036ba507a35628e851e07996e6e09c086
Johan Mattsson <johan.mattsson@hotmail.se>
2022-04-15 15:53:05 +0200

    examples/with-apollo: Escape apostrophes

--- examples/with-apollo/pages/client-only.js
+++ examples/with-apollo/pages/client-only.js
@@ -10,8 +10,8 @@ const ClientOnlyPage = (props) => (
     <InfoBox>
       ℹ️ This page shows how to use Apollo only in the client. If you{' '}
       <a href="/client-only">reload</a> this page, you will see a loader since
-      Apollo didn't fetch any data on the server. This is useful when the page
-      doesn't have SEO requirements or blocking data fetching requirements.
+      Apollo didn&#39;t fetch any data on the server. This is useful when the page
+      doesn&#39;t have SEO requirements or blocking data fetching requirements.
     </InfoBox>
     <Submit />
     <PostList />
