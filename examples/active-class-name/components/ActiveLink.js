63ecd46850ea361c0a7c75c6dfa0d87697a9c0cf
Johan Mattsson <johan.mattsson@hotmail.se>
2022-04-15 12:42:20 +0200

    examples/active-class-name: Fix duplicated import

--- examples/active-class-name/components/ActiveLink.js
+++ examples/active-class-name/components/ActiveLink.js
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react'
+import React, { Children, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
-import React, { Children } from 'react'
 
 const ActiveLink = ({ children, activeClassName, ...props }) => {
   const { asPath, isReady } = useRouter()
