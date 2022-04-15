6ef4193f3a8338d2346f291d6ceec7cb32d9378f
Maedah Batool <me@MaedahBatool.com>
2022-04-15 22:11:42 +0500

    Lint tests

--- examples/cms-tina/.tina/__generated__/types.ts
+++ examples/cms-tina/.tina/__generated__/types.ts
@@ -6,12 +6,14 @@ export type InputMaybe<T> = Maybe<T>
 export type Exact<T extends { [key: string]: unknown }> = {
   [K in keyof T]: T[K]
 }
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>
-}
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>
-}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  {
+    [SubKey in K]?: Maybe<T[SubKey]>
+  }
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  {
+    [SubKey in K]: Maybe<T[SubKey]>
+  }
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string
