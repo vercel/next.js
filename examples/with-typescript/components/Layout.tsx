73b05af1fbad8865891828173e292c2dd2c946fa
Johan Mattsson <johan.mattsson@hotmail.se>
2022-04-15 21:21:51 +0200

    examples/with-typescript: Escape apostrophe

--- examples/with-typescript/components/Layout.tsx
+++ examples/with-typescript/components/Layout.tsx
@@ -33,7 +33,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     {children}
     <footer>
       <hr />
-      <span>I'm here to stay (Footer)</span>
+      <span>I&#39;m here to stay (Footer)</span>
     </footer>
   </div>
 )
