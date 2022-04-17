a01786bb525a27a42b0338dc7afeb7d678ec8052
Challa Sharath <csharath@outlook.com>
2022-04-17 21:33:07 +0530

    remove duplicate eslint rule(script in head)

--- packages/eslint-plugin-next/lib/index.js
+++ packages/eslint-plugin-next/lib/index.js
@@ -42,7 +42,6 @@ module.exports = {
         '@next/next/no-document-import-in-page': 2,
         '@next/next/no-head-import-in-document': 2,
         '@next/next/no-script-in-document': 2,
-        '@next/next/no-script-in-head': 2,
         '@next/next/no-script-component-in-head': 2,
         '@next/next/no-styled-jsx-in-document': 2,
         '@next/next/no-server-import-in-page': 2,
