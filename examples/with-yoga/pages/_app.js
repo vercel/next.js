279142b5d9f06aa447b65a6d624ea31f094ce3cd
Matheus Martins <mathews_2010@outlook.com>
2022-04-19 08:18:09 -0300

    fix: add config to styled-components

--- examples/with-yoga/pages/_app.js
+++ examples/with-yoga/pages/_app.js
@@ -1,8 +1,18 @@
 import { ThemeProvider, FontLoader } from '@gympass/yoga'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <GlobalStyle />
       <FontLoader />
       <Component {...pageProps} />
     </ThemeProvider>
