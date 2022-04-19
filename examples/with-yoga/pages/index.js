62b94cc6c8e631d6c4ce4392e9a7e017f09ea8f4
JJ Kasper <jj@jjsweb.site>
2022-04-19 16:59:54 -0500

    Apply suggestions from code review

--- examples/with-yoga/pages/index.js
+++ examples/with-yoga/pages/index.js
@@ -3,9 +3,9 @@ import { Box, Text, Button, Divider } from '@gympass/yoga'
 export default function Home() {
   return (
     <Box d="flex" flexDirection="column" alignItems="center">
-      <Text.H2> @gympass/yoga with Next.js </Text.H2>
+      <Text.H2>@gympass/yoga with Next.js</Text.H2>
       <Divider />
-      <Button> Yoga Design System </Button>
+      <Button>Yoga Design System</Button>
     </Box>
   )
 }
