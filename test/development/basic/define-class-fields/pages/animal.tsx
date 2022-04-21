473748625a057438284a6f6b303feb032d1ed315
machenjie <309272985@qq.com>
2022-04-21 14:24:35 +0800

    test:add test for useDefineForClassFields option

---
+++ test/development/basic/define-class-fields/pages/animal.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+interface Animal {
+  animalStuff: any
+}
+interface Dog extends Animal {
+  dogStuff: any
+}
+
+class AnimalHouse {
+  resident: Animal
+  constructor(animal: Animal) {
+    this.resident = animal
+  }
+}
+class DogHouse extends AnimalHouse {
+  // Initializes 'resident' to 'undefined'
+  // after the call to 'super()' when
+  // using 'useDefineForClassFields'!
+  // @ts-ignore
+  resident: Dog
+  constructor(dog: Dog) {
+    super(dog)
+  }
+}
+
+class DogHouseWithDeclare extends AnimalHouse {
+  declare resident: Dog
+  constructor(dog: Dog) {
+    super(dog)
+  }
+}
+
+export default function Animal() {
+  const dog = new DogHouse({
+    animalStuff: 'animal',
+    dogStuff: 'dog',
+  })
+  const dogDeclare = new DogHouseWithDeclare({
+    animalStuff: 'animal',
+    dogStuff: 'dog',
+  })
+  return (
+    <>
+      <div id={'dog'}>{dog.resident}</div>
+      <div id={'dogDecl'}>{dogDeclare.resident?.dogStuff}</div>
+    </>
+  )
+}
