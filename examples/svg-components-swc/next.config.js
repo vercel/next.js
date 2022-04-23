8edd09ff50f0b9fb7e80e504e7ceba367e295177
Rubens Armôa Lopes <rubens@y520-15ikbn.localdomain>
2022-04-23 14:21:51 -0400

    docs: add svg as component example
    feat: add svg as component example

---
+++ examples/svg-components-swc/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
+}
