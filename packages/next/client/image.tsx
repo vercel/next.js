2671a53d066135190fa720c92491a6e2b9161c5b
Alex Castle <atcastle@gmail.com>
2022-04-27 14:17:20 -0700

    Update packages/next/client/image.tsx
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/next/client/image.tsx
+++ packages/next/client/image.tsx
@@ -338,7 +338,7 @@ function handleLoading(
           (!heightModified && widthModified)
         ) {
           warnOnce(
-            `Image with src ${src} has either height or width modified, but not the other. If you use CSS to change the size of your image, also include the styles 'height: "auto"' or 'width: "auto"' to maintain the aspect ratio.`
+            `Image with src "${src}" has either width or height modified, but not the other. If you use CSS to change the size of your image, also include the styles 'width: "auto"' or 'height: "auto"' to maintain the aspect ratio.`
           )
         }
       }
