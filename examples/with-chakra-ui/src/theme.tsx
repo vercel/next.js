cc35859fe74ad4b7db801ae552e0a79ffc05fc96
Lazar Nikolov <hello@nikolovlazar.com>
2022-04-28 14:36:33 +0200

    docs(examples): Updated Chakra UI examples (#36333)
    ## Overview
    
    This PR updates both Chakra UI examples by updating the Chakra UI packages to the latest versions, and added better examples for Semantic Tokens (new feature in Chakra UI), and pseudo props.
    
    ## Documentation / Examples
    
    - [x] Make sure the linting passes by running `yarn lint`
    
    
    Co-authored-by: Balázs Orbán <18369201+balazsorban44@users.noreply.github.com>

--- examples/with-chakra-ui/src/theme.tsx
+++ examples/with-chakra-ui/src/theme.tsx
@@ -11,6 +11,25 @@ const breakpoints = createBreakpoints({
 })
 
 const theme = extendTheme({
+  semanticTokens: {
+    colors: {
+      text: {
+        default: '#16161D',
+        _dark: '#ade3b8',
+      },
+      heroGradientStart: {
+        default: '#7928CA',
+        _dark: '#e3a7f9',
+      },
+      heroGradientEnd: {
+        default: '#FF0080',
+        _dark: '#fbec8f',
+      },
+    },
+    radii: {
+      button: '12px',
+    },
+  },
   colors: {
     black: '#16161D',
   },
