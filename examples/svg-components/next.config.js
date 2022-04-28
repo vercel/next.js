20b54e352e80ae54ed04cc2575a97801a60729a6
Rubens Armôa Lopes <rubens@y520-15ikbn.localdomain>
2022-04-27 20:11:05 -0400

    move svg-components-swc into svg-components

--- examples/svg-components/next.config.js
+++ examples/svg-components/next.config.js
@@ -3,7 +3,8 @@ module.exports = {
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     })
 
