5cd09949ff4156ddcd4fc0c52f1069de02b30440
Freddy Montes <freddymontes@gmail.com>
2022-04-29 08:12:03 -0600

    Fix the media queries

--- examples/cms-dotcms/tailwind.config.js
+++ examples/cms-dotcms/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      container: {
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+        }
+      },
       colors: {
         'accent-1': '#FAFAFA',
         'accent-2': '#EAEAEA',
