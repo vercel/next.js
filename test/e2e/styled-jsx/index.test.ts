485f86bb6f7cdcb418ac5f37fa3ed22fbf6faa1c
Tim Neutkens <tim@timneutkens.nl>
2022-04-30 21:36:49 +0200

    Update test to run both npm / pnpm

--- test/e2e/styled-jsx/index.test.ts
+++ test/e2e/styled-jsx/index.test.ts
@@ -3,33 +3,40 @@ import { NextInstance } from 'test/lib/next-modes/base'
 import { renderViaHTTP } from 'next-test-utils'
 import cheerio from 'cheerio'
 
-describe('styled-jsx', () => {
-  let next: NextInstance
+function runTest(packageManager?: string) {
+  describe(`styled-jsx${packageManager ? ' ' + packageManager : ''}`, () => {
+    let next: NextInstance
 
-  beforeAll(async () => {
-    next = await createNext({
-      files: {
-        'pages/index.js': `
-          export default function Page() {
-            return (
-              <div>
-                <style jsx>{'p { color: red; }'}</style>
-                <p>hello world</p>
-              </div>
-            )
-          }
-        `,
-      },
-      ...(!!process.env.PKG_MANAGER && {
-        installCommand: `${process.env.PKG_MANAGER} install`,
-      }),
+    beforeAll(async () => {
+      next = await createNext({
+        files: {
+          'pages/index.js': `
+            export default function Page() {
+              return (
+                <div>
+                  <style jsx>{'p { color: red; }'}</style>
+                  <p>hello world</p>
+                </div>
+              )
+            }
+          `,
+        },
+        ...(packageManager
+          ? {
+              installCommand: `${packageManager} install`,
+            }
+          : {}),
+      })
+    })
+    afterAll(() => next.destroy())
+
+    it('should contain styled-jsx styles in html', async () => {
+      const html = await renderViaHTTP(next.url, '/')
+      const $ = cheerio.load(html)
+      expect($('head').text()).toContain('color:red')
     })
   })
-  afterAll(() => next.destroy())
+}
 
-  it('should contain styled-jsx styles in html', async () => {
-    const html = await renderViaHTTP(next.url, '/')
-    const $ = cheerio.load(html)
-    expect($('head').text()).toContain('color:red')
-  })
-})
+runTest()
+runTest('pnpm')
