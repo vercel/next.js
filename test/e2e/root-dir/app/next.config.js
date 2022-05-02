9670c977356b6db5b57a265ab641cf5d7328fd10
Tim Neutkens <tim@timneutkens.nl>
2022-05-02 11:55:29 +0200

    Remove react18 helper

--- test/e2e/root-dir/app/next.config.js
+++ test/e2e/root-dir/app/next.config.js
@@ -1,20 +1,8 @@
-const path = require('path')
-
-let withReact18 = (config) => config
-
-try {
-  // only used when running inside of the monorepo not when isolated
-  withReact18 = require(path.join(
-    __dirname,
-    '../../../integration/react-18/test/with-react-18'
-  ))
-} catch (_) {}
-
-module.exports = withReact18({
+module.exports = {
   experimental: {
     rootDir: true,
     runtime: 'nodejs',
     reactRoot: true,
     serverComponents: true,
   },
-})
+}
