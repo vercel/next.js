f6af0bc2d8edd30415d910ef9e239af99172debd
Jiachi Liu <inbox@huozhi.im>
2022-05-06 01:16:24 +0200

    test: merge rsc custom app cases (#36713)
    the rsc & streaming test app is too big to build which is time consuming, move custom _app cases to switchable runtime test suite

--- test/integration/react-streaming-and-server-components/test/switchable-runtime.test.js
+++ test/integration/react-streaming-and-server-components/test/switchable-runtime.test.js
@@ -22,6 +22,48 @@ function splitLines(text) {
     .filter(Boolean)
 }
 
+function getOccurrence(text, matcher) {
+  return (text.match(matcher) || []).length
+}
+
+function flight(context) {
+  describe('flight response', () => {
+    it('should contain _app.server in flight response (node)', async () => {
+      const html = await renderViaHTTP(context.appPort, '/node-rsc')
+      const flightResponse = await renderViaHTTP(
+        context.appPort,
+        '/node-rsc?__flight__=1'
+      )
+      expect(
+        getOccurrence(html, new RegExp(`class="app-server-root"`, 'g'))
+      ).toBe(1)
+      expect(
+        getOccurrence(
+          flightResponse,
+          new RegExp(`"className":\\s*"app-server-root"`, 'g')
+        )
+      ).toBe(1)
+    })
+  })
+
+  it('should contain _app.server in flight response (edge)', async () => {
+    const html = await renderViaHTTP(context.appPort, '/edge-rsc')
+    const flightResponse = await renderViaHTTP(
+      context.appPort,
+      '/edge-rsc?__flight__=1'
+    )
+    expect(
+      getOccurrence(html, new RegExp(`class="app-server-root"`, 'g'))
+    ).toBe(1)
+    expect(
+      getOccurrence(
+        flightResponse,
+        new RegExp(`"className":\\s*"app-server-root"`, 'g')
+      )
+    ).toBe(1)
+  })
+}
+
 async function testRoute(appPort, url, { isStatic, isEdge, isRSC }) {
   const html1 = await renderViaHTTP(appPort, url)
   const renderedAt1 = +html1.match(/Time: (\d+)/)[1]
@@ -44,9 +86,8 @@ async function testRoute(appPort, url, { isStatic, isEdge, isRSC }) {
   const [rootClass, oppositeRootClass] = isRSC
     ? [rootClasses[0], rootClasses[1]]
     : [rootClasses[1], rootClasses[0]]
-  const appServerOccurrence =
-    html1.match(new RegExp(`class="${rootClass}"`, 'g')) || []
-  expect(appServerOccurrence.length).toBe(1)
+
+  expect(getOccurrence(html1, new RegExp(`class="${rootClass}"`, 'g'))).toBe(1)
   expect(html1).not.toContain(`"${oppositeRootClass}"`)
 }
 
@@ -66,6 +107,8 @@ describe('Switchable runtime (prod)', () => {
   afterAll(async () => {
     await killApp(context.server)
   })
+
+  flight(context)
 
   it('should build /static as a static page with the nodejs runtime', async () => {
     await testRoute(context.appPort, '/static', {
@@ -281,6 +324,7 @@ describe('Switchable runtime (dev)', () => {
     await killApp(context.server)
   })
 
+  flight(context)
   it('should support client side navigation to ssr rsc pages', async () => {
     let flightRequest = null
 
