aa933a722efa2dc339ba0285332206273eb9954e
JJ Kasper <jj@jjsweb.site>
2022-05-09 13:52:12 -0500

    Update moduleNameMapper jest config and remove extra deps

--- packages/next/build/jest/jest.ts
+++ packages/next/build/jest/jest.ts
@@ -76,10 +76,6 @@ export default function nextJest(options: { dir?: string } = {}) {
         ...resolvedJestConfig,
 
         moduleNameMapper: {
-          // Custom config will be able to override the default mappings
-          // moduleNameMapper is matched top to bottom hence why this has to be before Next.js internal rules
-          ...(resolvedJestConfig.moduleNameMapper || {}),
-
           // Handle CSS imports (with CSS modules)
           // https://jestjs.io/docs/webpack#mocking-css-modules
           '^.+\\.module\\.(css|sass|scss)$':
@@ -92,6 +88,11 @@ export default function nextJest(options: { dir?: string } = {}) {
           '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$': require.resolve(
             `./__mocks__/fileMock.js`
           ),
+
+          // custom config comes last to ensure the above rules are matched,
+          // fixes the case where @pages/(.*) -> src/pages/$! doesn't break
+          // CSS/image mocks
+          ...(resolvedJestConfig.moduleNameMapper || {}),
         },
         testPathIgnorePatterns: [
           // Don't look for tests in node_modules
