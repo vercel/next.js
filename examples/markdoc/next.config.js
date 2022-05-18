f7f2f98ad3019a282e14adb8bf0b4aacd7ad24d6
Mike Fix <mfix@stripe.com>
2022-05-17 17:59:50 -0700

    update next.config.js

--- examples/markdoc/next.config.js
+++ examples/markdoc/next.config.js
@@ -1,5 +1,5 @@
 const withMarkdoc = require('@markdoc/next.js');
 
 module.exports = withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
-  pageExtensions: ['js', 'md']
+  pageExtensions: ['js', 'md', 'mdoc']
 });
