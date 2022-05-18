016a0958f7fbd7d941f5900094ecf049080dee3e
Mike Fix <mfix@stripe.com>
2022-05-18 14:50:47 -0700

    rename _app.ts -> _app.tsx

--- examples/markdoc/next.config.js
+++ examples/markdoc/next.config.js
@@ -2,5 +2,5 @@ const withMarkdoc = require('@markdoc/next.js')
 
 module.exports =
   withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
-    pageExtensions: ['js', 'md', 'mdoc'],
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
   })
