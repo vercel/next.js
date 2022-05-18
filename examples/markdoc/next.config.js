31f9feb5ff0a8d686236ae957e46d17ea067f672
Mike Fix <mfix@stripe.com>
2022-05-18 09:33:58 -0700

    fix prettier

--- examples/markdoc/next.config.js
+++ examples/markdoc/next.config.js
@@ -1,5 +1,6 @@
-const withMarkdoc = require('@markdoc/next.js');
+const withMarkdoc = require('@markdoc/next.js')
 
-module.exports = withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
-  pageExtensions: ['js', 'md', 'mdoc']
-});
+module.exports =
+  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
+    pageExtensions: ['js', 'md', 'mdoc'],
+  })
