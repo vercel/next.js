265d02e69a854d49d408a27ad2b7724773778a63
feugy <damien.feugas@gmail.com>
2022-05-19 23:32:33 +0200

    chore: remove undesired changes

--- examples/damien/pages/_middleware.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export default async function middleware(req) {
-  console.log('body:', await req.json())
-  console.log('-------')
-  return NextResponse.next()
-}
