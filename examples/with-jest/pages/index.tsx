278fd264d77db2740c24937d3586ba992d5fe354
Vladimir Trotsenko <74984425+keeeparis@users.noreply.github.com>
2022-05-22 14:39:01 +0600

    Fix failed tests for with-jest example

--- examples/with-jest/pages/index.tsx
+++ examples/with-jest/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import styles from '@/pages/index.module.css'
+import styles from '@/styles/index.module.css'
 
 export default function Home() {
   return (
