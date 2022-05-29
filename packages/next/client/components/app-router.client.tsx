a2a2e4c7127794003ebc2be9a93b9332a51438a2
Tim Neutkens <tim@timneutkens.nl>
2022-05-29 13:28:36 +0200

    Add router as client component

--- packages/next/client/components/app-router.client.tsx
+++ packages/next/client/components/app-router.client.tsx
@@ -1,75 +1,74 @@
 import React from 'react'
-// import { createFromFetch } from 'next/dist/compiled/react-server-dom-webpack'
+import { createFromFetch } from 'next/dist/compiled/react-server-dom-webpack'
+import { RouterContext } from '../../shared/lib/router-context'
 
-// function createResponseCache() {
-//   return new Map<string, any>()
-// }
-// const rscCache = createResponseCache()
+function createResponseCache() {
+  return new Map<string, any>()
+}
+const rscCache = createResponseCache()
 
-// const getCacheKey = () => {
-//   const { pathname, search } = location
-//   return pathname + search
-// }
+const getCacheKey = () => {
+  const { pathname, search } = location
+  return pathname + search
+}
 
-// function fetchFlight(href: string) {
-//   const url = new URL(href, location.origin)
-//   const searchParams = url.searchParams
-//   searchParams.append('__flight__', '1')
+function fetchFlight(href: string) {
+  const url = new URL(href, location.origin)
+  const searchParams = url.searchParams
+  searchParams.append('__flight__', '1')
 
-//   return fetch(url.toString())
-// }
+  return fetch(url.toString())
+}
 
-// function fetchServerResponse(cacheKey: string) {
-//   let response = rscCache.get(cacheKey)
-//   if (response) return response
+function fetchServerResponse(cacheKey: string) {
+  let response = rscCache.get(cacheKey)
+  if (response) return response
 
-//   response = createFromFetch(fetchFlight(getCacheKey()))
+  response = createFromFetch(fetchFlight(getCacheKey()))
 
-//   rscCache.set(cacheKey, response)
-//   return response
-// }
-
-// const AppRouterContext = React.createContext({})
+  rscCache.set(cacheKey, response)
+  return response
+}
 
 // TODO: move to client component when handling is implemented
 export default function AppRouter({ initialUrl, children }: any) {
+  const initialState = {
+    url: initialUrl,
+  }
+  const previousUrlRef = React.useRef(initialState)
+  const [current, setCurrent] = React.useState(initialState)
+  const appRouter = React.useMemo(() => {
+    return {
+      prefetch: () => {},
+      replace: () => {},
+      push: (url: string) => {
+        previousUrlRef.current = current
+        setCurrent({ ...current, url })
+        // TODO: update url eagerly or not?
+        window.history.pushState(current, '', url)
+      },
+      url: current.url,
+    }
+  }, [current])
+  if (typeof window !== 'undefined') {
+    // @ts-ignore TODO: for testing
+    window.appRouter = appRouter
+    console.log({
+      appRouter,
+      previous: previousUrlRef.current,
+      current,
+    })
+  }
+
+  let root
+  if (current.url !== previousUrlRef.current?.url) {
+    // eslint-disable-next-line
+    const data = fetchServerResponse(current.url)
+    root = data.readRoot()
+  }
   return (
-    <>
-      <h1>Router</h1>
-    </>
+    <RouterContext.Provider value={appRouter}>
+      {root ? root : children}
+    </RouterContext.Provider>
   )
-  // const initialState = {
-  //   url: initialUrl,
-  // }
-  // const previousUrlRef = React.useRef(initialState)
-  // const [current, setCurrent] = React.useState(initialState)
-  // const appRouter = React.useMemo(() => {
-  //   return {
-  //     push: (url: string) => {
-  //       previousUrlRef.current = current
-  //       setCurrent({ ...current, url })
-  //       // TODO: update url eagerly or not?
-  //       window.history.pushState(current, '', url)
-  //     },
-  //     url: current.url,
-  //   }
-  // }, [current])
-  // // @ts-ignore TODO: for testing
-  // window.appRouter = appRouter
-  // console.log({
-  //   appRouter,
-  //   previous: previousUrlRef.current,
-  //   current,
-  // })
-  // let root
-  // if (current.url !== previousUrlRef.current?.url) {
-  //   // eslint-disable-next-line
-  //   const data = fetchServerResponse(current.url)
-  //   root = data.readRoot()
-  // }
-  // return (
-  //   <AppRouterContext.Provider value={appRouter}>
-  //     {root ? root : children}
-  //   </AppRouterContext.Provider>
-  // )
 }
