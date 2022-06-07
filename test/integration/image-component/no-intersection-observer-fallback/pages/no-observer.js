d4be460a1e6b91c28749b7355e759eef0b981988
SukkaW <isukkaw@gmail.com>
2022-06-07 12:20:06 +0800

    Test: properly mock the IntersectionObserver

--- test/integration/image-component/no-intersection-observer-fallback/pages/no-observer.js
+++ test/integration/image-component/no-intersection-observer-fallback/pages/no-observer.js
@@ -1,14 +1,8 @@
 import Image from 'next/image'
-import Script from 'next/script'
 
 const Lazy = () => {
   return (
     <div>
-      <Script
-        id="IntersectionObserver-null"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{ __html: 'IntersectionObserver = null' }}
-      />
       <p id="stubtext">
         This is a page with one lazy-loaded image, to be used in the test for
         browsers without intersection observer.
