53d315234d3b06f1a32e599baab5db8deaebbcab
Shu Ding <g@shud.in>
2022-06-07 13:45:07 +0200

    check hasOwnProperty

--- packages/next/shared/lib/router/router.ts
+++ packages/next/shared/lib/router/router.ts
@@ -123,11 +123,15 @@ function compareRouterStates(a: Router['state'], b: Router['state']) {
       }
       for (let j = queryKeys.length; j--; ) {
         const queryKey = queryKeys[j]
-        if (a.query[queryKey] !== b.query[queryKey]) {
+        if (
+          !b.query.hasOwnProperty(queryKey) ||
+          a.query[queryKey] !== b.query[queryKey]
+        ) {
           return false
         }
       }
     } else if (
+      !b.hasOwnProperty(key) ||
       a[key as keyof Router['state']] !== b[key as keyof Router['state']]
     ) {
       return false
