553e80211995287eef4661f5c5061a30f561a22e
Ilya Taratukhin <darkilfa@gmail.com>
2022-06-08 14:50:49 +0700

    feat: add with-fingerprintjs example

---
+++ examples/with-fingerprintjs/components/Nav.tsx
@@ -0,0 +1,34 @@
+import { PropsWithChildren } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { RouterProps } from './types'
+
+export const Nav: React.FC = () => {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <nav className='nav'>
+      <CustomLink to='home'>Home</CustomLink>
+      <CustomLink to='signin'>Sign in</CustomLink>
+    </nav>
+  )
+}
+
+const CustomLink: React.FC<PropsWithChildren<{ to: string }>> = ({to, children}) => {
+  const router = useRouter();
+  const { pathname } = router;
+  const { cacheStrategy } = router.query as RouterProps;
+  const linkPathname = `/${to}/[cacheStrategy]`
+  const className = `nav-link${pathname === linkPathname ? ' active' : ''}`
+  console.log(pathname, linkPathname, router)
+  return (
+    <div>
+      <Link href={{
+        pathname: linkPathname,
+        query: { cacheStrategy },
+      }} scroll={false}>
+        <a className={className}>{children}</a>
+      </Link>
+    </div>
+  )
+}
---
+++ examples/with-fingerprintjs/components/types.ts
@@ -0,0 +1,6 @@
+
+export type CacheStrategyPath = 'no-cache' | 'memory-cache' | 'session-storage-cache' | 'ls-cache';
+
+export type RouterProps = {
+  cacheStrategy: CacheStrategyPath;
+}
