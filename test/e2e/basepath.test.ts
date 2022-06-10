9af590e4eabdc0bd2d2925bdffbdba0550aaa7be
JJ Kasper <jj@jjsweb.site>
2022-06-10 18:10:52 -0500

    another flakey test

--- test/e2e/basepath.test.ts
+++ test/e2e/basepath.test.ts
@@ -757,7 +757,9 @@ describe('basePath', () => {
           .waitForElementByCss('#other-page-title')
 
         const eventLog = await browser.eval('window._getEventLog()')
-        expect(eventLog).toEqual([
+        expect(
+          eventLog.filter((item) => item[1]?.endsWith('/other-page'))
+        ).toEqual([
           ['routeChangeStart', `${basePath}/other-page`, { shallow: false }],
           ['beforeHistoryChange', `${basePath}/other-page`, { shallow: false }],
           ['routeChangeComplete', `${basePath}/other-page`, { shallow: false }],
