c5a3b8a96b794c1d46f8e3b726f3b8570eed35aa
Alabhya Jindal <52493077+alabhyajindal@users.noreply.github.com>
2022-06-11 15:06:03 +0530

    Corrected default export

--- examples/with-context-api/pages/_app.js
+++ examples/with-context-api/pages/_app.js
@@ -1,9 +1,11 @@
 import { CounterProvider } from '../components/Counter'
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <CounterProvider>
       <Component {...pageProps} />
     </CounterProvider>
   )
 }
+
+export default MyApp
