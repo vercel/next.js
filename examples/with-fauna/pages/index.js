182addf4b666f1edabffcf53e82741340750421b
JJ Kasper <jj@jjsweb.site>
2022-06-12 21:48:39 -0500

    remove extra dep and fix text color

--- examples/with-fauna/pages/index.js
+++ examples/with-fauna/pages/index.js
@@ -37,8 +37,8 @@ const useEntriesFlow = ({ fallback }) => {
 
 const AppHead = () => (
   <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.png" />
   </Head>
 )
@@ -149,15 +149,10 @@ const Guestbook = ({ fallback }) => {
             'dark:bg-blue-opaque'
           )}
         >
-          <h5
-            className={cn(
-              'text-lg md:text-xl font-bold',
-              'text-gray-900 dark:text-gray-100'
-            )}
-          >
+          <h5 className={cn('text-lg md:text-xl font-bold', 'text-gray-900')}>
             Sign the Guestbook
           </h5>
-          <p className="my-1 text-gray-800 dark:text-gray-200">
+          <p className="my-1 text-gray-800">
             Share a message for a future visitor.
           </p>
           <EntryForm onSubmit={onSubmit} />
