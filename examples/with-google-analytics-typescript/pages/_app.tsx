5701893d562a3a6a2974bbfccc156a0ce4d5398b
tarunama <sakeharami@gmail.com>
2022-06-14 18:46:56 +0900

    chore: add loading of google analytics

--- examples/with-google-analytics-typescript/pages/_app.tsx
+++ examples/with-google-analytics-typescript/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
+import '../styles/globals.css'
 import { GA_TRACKING_ID, pageview } from '../lib/gtags'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -10,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   useEffect(() => {
-    if (!GA_TRACKING_ID) return
+    if (GA_TRACKING_ID === undefined) return
 
     const handleRouteChange = (url: string) => {
       pageview(url)
@@ -21,7 +22,29 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
