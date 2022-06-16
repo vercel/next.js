a371f2f51f1ea0437a9c26632e1a4db5dd40cc74
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-17 01:56:04 +0900

    FIx

--- packages/next-swc/crates/styled_jsx/src/transform_css.rs
+++ packages/next-swc/crates/styled_jsx/src/transform_css.rs
@@ -501,21 +501,15 @@ where
     }
 
     let span = node.span();
-    let mut lexer = swc_css::parser::lexer::Lexer::new(
+    let lexer = swc_css::parser::lexer::Lexer::new(
         StringInput::new(&s, span.lo, span.hi),
         ParserConfig {
             allow_wrong_line_comments: true,
         },
     );
 
-    let mut tokens = vec![];
-
-    while let Some(t) = lexer.next() {
-        tokens.push(t);
-    }
-
     Tokens {
         span: Span::new(span.lo, span.hi, Default::default()),
-        tokens,
+        tokens: lexer.collect(),
     }
 }
