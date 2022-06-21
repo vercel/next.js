258e44796fbc26c4fe371af754af77a315ecff12
JJ Kasper <jj@jjsweb.site>
2022-06-21 12:53:59 -0500

    remove old tests

--- test/unit/web-runtime/next-response.test.ts
+++ test/unit/web-runtime/next-response.test.ts
@@ -4,47 +4,6 @@
 
 import { NextResponse } from 'next/server/web/spec-extension/response'
 
-const toJSON = async (response) => ({
-  body: await response.json(),
-  contentType: response.headers.get('content-type'),
-  status: response.status,
-})
-
-it('automatically parses and formats JSON', async () => {
-  expect(await toJSON(NextResponse.json({ message: 'hello!' }))).toMatchObject({
-    contentType: 'application/json',
-    body: { message: 'hello!' },
-  })
-
-  expect(
-    await toJSON(NextResponse.json({ status: 'success' }, { status: 201 }))
-  ).toMatchObject({
-    contentType: 'application/json',
-    body: { status: 'success' },
-    status: 201,
-  })
-
-  expect(
-    await toJSON(
-      NextResponse.json({ error: { code: 'bad_request' } }, { status: 400 })
-    )
-  ).toMatchObject({
-    contentType: 'application/json',
-    body: { error: { code: 'bad_request' } },
-    status: 400,
-  })
-
-  expect(await toJSON(NextResponse.json(null))).toMatchObject({
-    contentType: 'application/json',
-    body: null,
-  })
-
-  expect(await toJSON(NextResponse.json(''))).toMatchObject({
-    contentType: 'application/json',
-    body: '',
-  })
-})
-
 it('can be cloned', async () => {
   const fetchResponse = await fetch('https://example.vercel.sh')
   const newResponse = new NextResponse(fetchResponse.body, fetchResponse)
