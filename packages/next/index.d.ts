ef4fb5e5424b51774e456799dc670abbd27ba84b
JJ Kasper <jj@jjsweb.site>
2022-06-23 13:58:23 -0500

    remove ext for latest TypeScript version

--- packages/next/index.d.ts
+++ packages/next/index.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="./types/global.d.ts" />
+/// <reference types="./types/global" />
 /// <reference path="./amp.d.ts" />
 /// <reference path="./app.d.ts" />
 /// <reference path="./config.d.ts" />
@@ -11,7 +11,7 @@
 /// <reference path="./router.d.ts" />
 /// <reference path="./script.d.ts" />
 /// <reference path="./server.d.ts" />
-/// <reference path="./dist/styled-jsx-types/global.d.ts" />
+/// <reference path="./dist/styled-jsx-types/global" />
 
 export { default } from './types'
 export * from './types'
