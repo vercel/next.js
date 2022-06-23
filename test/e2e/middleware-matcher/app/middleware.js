57c5e2a0245f6b36663812f88f0de4641ca1e9b9
JJ Kasper <jj@jjsweb.site>
2022-06-23 15:47:58 -0500

    Update middleware matcher test for root (#37961)

--- test/e2e/middleware-matcher/app/middleware.js
+++ test/e2e/middleware-matcher/app/middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 
 export const config = {
   matcher: [
+    '/',
     '/with-middleware/:path*',
     '/another-middleware/:path*',
     // the below is testing special characters don't break the build
