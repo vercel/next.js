faf7d9c26e606267020f1ef324e1e5b24af10fba
JJ Kasper <jj@jjsweb.site>
2022-06-24 18:48:51 -0500

    fix test

--- test/integration/custom-server/test/index.test.js
+++ test/integration/custom-server/test/index.test.js
@@ -4,6 +4,7 @@ import webdriver from 'next-webdriver'
 import { join } from 'path'
 import getPort from 'get-port'
 import cheerio from 'cheerio'
+import https from 'https'
 import {
   initNextServerScript,
   killApp,
@@ -26,9 +27,19 @@ describe.each([
   { title: 'using HTTP', useHttps: false },
   { title: 'using HTTPS', useHttps: true },
 ])('Custom Server $title', ({ useHttps }) => {
+  let nextUrl
+
+  const agent = useHttps
+    ? new https.Agent({
+        rejectUnauthorized: false,
+      })
+    : undefined
+
   const startServer = async (optEnv = {}, opts) => {
     const scriptPath = join(appDir, 'server.js')
     context.appPort = appPort = await getPort()
+    nextUrl = `http${useHttps ? 's' : ''}://127.0.0.1:${context.appPort}`
+
     const env = Object.assign(
       { ...process.env },
       { PORT: `${appPort}`, __NEXT_TEST_MODE: 'true', USE_HTTPS: useHttps },
@@ -49,29 +60,42 @@ describe.each([
     afterAll(() => killApp(server))
 
     it('should serve internal file from render', async () => {
-      const data = await renderViaHTTP(appPort, '/static/hello.txt')
+      const data = await renderViaHTTP(
+        nextUrl,
+        '/static/hello.txt',
+        undefined,
+        { agent }
+      )
       expect(data).toMatch(/hello world/)
     })
 
     it('should handle render with undefined query', async () => {
-      expect(await renderViaHTTP(appPort, '/no-query')).toMatch(/"query":/)
+      expect(
+        await renderViaHTTP(nextUrl, '/no-query', undefined, { agent })
+      ).toMatch(/"query":/)
     })
 
     it('should set the assetPrefix dynamically', async () => {
-      const normalUsage = await renderViaHTTP(appPort, '/asset')
+      const normalUsage = await renderViaHTTP(nextUrl, '/asset', undefined, {
+        agent,
+      })
       expect(normalUsage).not.toMatch(/127\.0\.0\.1/)
 
       const dynamicUsage = await renderViaHTTP(
-        appPort,
-        '/asset?setAssetPrefix=1'
+        nextUrl,
+        '/asset?setAssetPrefix=1',
+        undefined,
+        { agent }
       )
       expect(dynamicUsage).toMatch(/127\.0\.0\.1/)
     })
 
     it('should handle null assetPrefix accordingly', async () => {
       const normalUsage = await renderViaHTTP(
-        appPort,
-        '/asset?setEmptyAssetPrefix=1'
+        nextUrl,
+        '/asset?setEmptyAssetPrefix=1',
+        undefined,
+        { agent }
       )
       expect(normalUsage).toMatch(/"\/_next/)
     })
@@ -79,8 +103,10 @@ describe.each([
     it('should set the assetPrefix to a given request', async () => {
       for (let lc = 0; lc < 1000; lc++) {
         const [normalUsage, dynamicUsage] = await Promise.all([
-          await renderViaHTTP(appPort, '/asset'),
-          await renderViaHTTP(appPort, '/asset?setAssetPrefix=1'),
+          await renderViaHTTP(nextUrl, '/asset', undefined, { agent }),
+          await renderViaHTTP(nextUrl, '/asset?setAssetPrefix=1', undefined, {
+            agent,
+          }),
         ])
 
         expect(normalUsage).not.toMatch(/127\.0\.0\.1/)
@@ -89,12 +115,14 @@ describe.each([
     })
 
     it('should render nested index', async () => {
-      const html = await renderViaHTTP(appPort, '/dashboard')
+      const html = await renderViaHTTP(nextUrl, '/dashboard', undefined, {
+        agent,
+      })
       expect(html).toMatch(/made it to dashboard/)
     })
 
     it('should contain customServer in NEXT_DATA', async () => {
-      const html = await renderViaHTTP(appPort, '/')
+      const html = await renderViaHTTP(nextUrl, '/', undefined, { agent })
       const $ = cheerio.load(html)
       expect(JSON.parse($('#__NEXT_DATA__').text()).customServer).toBe(true)
     })
@@ -108,7 +136,7 @@ describe.each([
     afterAll(() => killApp(server))
 
     it('response includes etag header', async () => {
-      const response = await fetchViaHTTP(appPort, '/')
+      const response = await fetchViaHTTP(nextUrl, '/', undefined, { agent })
       expect(response.headers.get('etag')).toBeTruthy()
     })
   })
@@ -118,45 +146,48 @@ describe.each([
     afterAll(() => killApp(server))
 
     it('response does not include etag header', async () => {
-      const response = await fetchViaHTTP(appPort, '/')
+      const response = await fetchViaHTTP(nextUrl, '/', undefined, { agent })
       expect(response.headers.get('etag')).toBeNull()
     })
   })
 
-  describe('HMR with custom server', () => {
-    beforeAll(() => startServer())
-    afterAll(() => {
-      killApp(server)
-      indexPg.restore()
-    })
+  // playwright fails with SSL error due to self-signed cert
+  if (!useHttps) {
+    describe('HMR with custom server', () => {
+      beforeAll(() => startServer())
+      afterAll(() => {
+        killApp(server)
+        indexPg.restore()
+      })
 
-    it('Should support HMR when rendering with /index pathname', async () => {
-      let browser
-      try {
-        browser = await webdriver(context.appPort, '/test-index-hmr')
-        const text = await browser.elementByCss('#go-asset').text()
-        const logs = await browser.log()
-        expect(text).toBe('Asset')
+      it('Should support HMR when rendering with /index pathname', async () => {
+        let browser
+        try {
+          browser = await webdriver(nextUrl, '/test-index-hmr')
+          const text = await browser.elementByCss('#go-asset').text()
+          const logs = await browser.log()
+          expect(text).toBe('Asset')
 
-        // Hydrates with react 18 is correct as expected
-        expect(
-          logs.some((log) =>
-            log.message.includes(
-              'ReactDOM.hydrate is no longer supported in React 18'
+          // Hydrates with react 18 is correct as expected
+          expect(
+            logs.some((log) =>
+              log.message.includes(
+                'ReactDOM.hydrate is no longer supported in React 18'
+              )
             )
-          )
-        ).toBe(false)
+          ).toBe(false)
 
-        indexPg.replace('Asset', 'Asset!!')
+          indexPg.replace('Asset', 'Asset!!')
 
-        await check(() => browser.elementByCss('#go-asset').text(), /Asset!!/)
-      } finally {
-        if (browser) {
-          await browser.close()
+          await check(() => browser.elementByCss('#go-asset').text(), /Asset!!/)
+        } finally {
+          if (browser) {
+            await browser.close()
+          }
         }
-      }
+      })
     })
-  })
+  }
 
   describe('Error when rendering without starting slash', () => {
     afterEach(() => killApp(server))
@@ -171,7 +202,9 @@ describe.each([
           },
         }
       )
-      const html = await renderViaHTTP(appPort, '/no-slash')
+      const html = await renderViaHTTP(nextUrl, '/no-slash', undefined, {
+        agent,
+      })
       expect(html).toContain('made it to dashboard')
       expect(stderr).toContain('Cannot render page with path "dashboard"')
     })
@@ -191,7 +224,9 @@ describe.each([
         }
       )
 
-      const html = await renderViaHTTP(appPort, '/no-slash')
+      const html = await renderViaHTTP(nextUrl, '/no-slash', undefined, {
+        agent,
+      })
       expect(html).toContain('made it to dashboard')
       expect(stderr).toContain('Cannot render page with path "dashboard"')
     })
@@ -204,7 +239,9 @@ describe.each([
     it.each(['/', '/no-query'])(
       'should handle compression for route %s',
       async (route) => {
-        const response = await fetchViaHTTP(appPort, route)
+        const response = await fetchViaHTTP(nextUrl, route, undefined, {
+          agent,
+        })
         expect(response.headers.get('Content-Encoding')).toBe('gzip')
       }
     )
@@ -215,7 +252,12 @@ describe.each([
     afterAll(() => killApp(server))
 
     it('should serve internal file from render', async () => {
-      const data = await renderViaHTTP(appPort, '/static/hello.txt')
+      const data = await renderViaHTTP(
+        nextUrl,
+        '/static/hello.txt',
+        undefined,
+        { agent }
+      )
       expect(data).toMatch(/hello world/)
     })
   })
@@ -233,7 +275,7 @@ describe.each([
           },
         }
       )
-      await fetchViaHTTP(appPort, '/unhandled-rejection')
+      await fetchViaHTTP(nextUrl, '/unhandled-rejection', undefined, { agent })
       await check(() => stderr, /unhandledRejection/)
       expect(stderr).toContain(
         'error - unhandledRejection: Error: unhandled rejection'
@@ -248,7 +290,7 @@ describe.each([
 
     it('should read the expected url protocol in middleware', async () => {
       const path = '/middleware-augmented'
-      const response = await fetchViaHTTP(appPort, path)
+      const response = await fetchViaHTTP(nextUrl, path, undefined, { agent })
       expect(response.headers.get('x-original-url')).toBe(
         `${useHttps ? 'https' : 'http'}://localhost:${appPort}${path}`
       )
