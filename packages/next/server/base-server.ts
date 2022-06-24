ebc2a0d68a7e43d5e48fabe07e68b0cba3023061
Gal Schlezinger <gal@spitfire.co.il>
2022-06-24 10:18:35 +0300

    If we have a Middleware we always return 200 OK for data requests

--- packages/next/server/base-server.ts
+++ packages/next/server/base-server.ts
@@ -779,10 +779,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
             _parsedUrl.query.__nextDefaultLocale =
               defaultLocale || this.nextConfig.i18n.defaultLocale
 
-            if (
-              !detectedLocale &&
-              !(pathname === '/api' || pathname.startsWith('/api/'))
-            ) {
+            if (!detectedLocale && !this.router.catchAllMiddleware[0]) {
               _parsedUrl.query.__nextLocale =
                 _parsedUrl.query.__nextDefaultLocale
               await this.render404(req, res, _parsedUrl)
