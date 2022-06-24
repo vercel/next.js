933edcf8a81791ff74cbca60e5959ef6b08dd265
Steven <steven@ceriously.com>
2022-06-24 18:03:16 -0400

    Change static import to url string

--- test/production/typescript-basic/app/pages/image-import.tsx
+++ test/production/typescript-basic/app/pages/image-import.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image'
 import FutureImage from 'next/future/image'
 
-import jpg from '../public/test.jpg'
-import png from '../public/test.png'
-
 export default function Page() {
   return (
     <>
       <h1>Example Image Usage</h1>
-      <Image src={jpg} />
+      <Image src="/test.jpg" width={200} height={200} />
       <hr />
-      <FutureImage src={png} />
+      <FutureImage src="/test.png" width={200} height={200} />
     </>
   )
 }
