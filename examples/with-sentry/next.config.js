d2b13cae28bd1d3929188ef70a778b20f591b4da
chaiwattsw <tongbboy29@hotmail.com>
2022-06-28 00:54:08 +0700

    update to latest example from @sentry/nextjs doc

--- examples/with-sentry/next.config.js
+++ examples/with-sentry/next.config.js
@@ -6,17 +6,21 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
 const moduleExports = {
-  // Your existing module.exports
+  // your existing module.exports
 }
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, {
+const sentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
-})
+}
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
