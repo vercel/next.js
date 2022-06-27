92502ce7febc0caf6f221f1abae8fb558b7c0708
JJ Kasper <jj@jjsweb.site>
2022-06-27 15:27:37 -0500

    update test for node v18

--- test/e2e/proxy-request-with-middleware/app/pages/api/index.js
+++ test/e2e/proxy-request-with-middleware/app/pages/api/index.js
@@ -9,12 +9,20 @@ export const config = {
 export default function handler(req, res) {
   return req
     .pipe(
-      request(`http://${req.headers.host}${req.url}/post`, {
-        followAllRedirects: false,
-        followRedirect: false,
-        gzip: true,
-        json: false,
-      })
+      request(
+        `http://${
+          // node v18 resolves to IPv6 by default so force IPv4
+          process.version.startsWith('v18.')
+            ? `127.0.0.1:${req.headers.host.split(':').pop() || ''}`
+            : req.headers.host
+        }${req.url}/post`,
+        {
+          followAllRedirects: false,
+          followRedirect: false,
+          gzip: true,
+          json: false,
+        }
+      )
     )
     .pipe(res)
 }
