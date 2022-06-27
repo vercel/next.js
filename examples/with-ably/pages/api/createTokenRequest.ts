22542ecb46eebe61cf9bf093f6e381cc3fa24bec
Jo Franchetti <jofranchetti@gmail.com>
2022-06-27 12:14:23 +0100

    lint-fix

--- examples/with-ably/pages/api/createTokenRequest.ts
+++ examples/with-ably/pages/api/createTokenRequest.ts
@@ -1,8 +1,13 @@
-import Ably from "ably/promises";
-import type { NextApiRequest, NextApiResponse } from "next";
+import Ably from 'ably/promises'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: req.query.clientId as string });
-  res.status(200).json(tokenRequestData);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const client = new Ably.Realtime(process.env.ABLY_API_KEY)
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: req.query.clientId as string,
+  })
+  res.status(200).json(tokenRequestData)
 }
