f957f6ddf07a2f03e80219572d6ed527d25860ee
Max Proske <max@mproske.com>
2022-06-27 19:39:49 -0700

    Replace all occurrences of placeholder.com with vercel.app in tests

--- test/integration/image-future/typescript/pages/valid.tsx
+++ test/integration/image-future/typescript/pages/valid.tsx
@@ -12,26 +12,26 @@ const Page = () => {
       <p>Valid TS</p>
       <Image
         id="width-and-height-num"
-        src="https://via.placeholder.com/500"
+        src="https://image-optimization-test.vercel.app/test.jpg"
         width={500}
         height={500}
       />
       <Image
         id="width-and-height-str"
-        src="https://via.placeholder.com/500"
+        src="https://image-optimization-test.vercel.app/test.jpg"
         width="500"
         height="500"
       />
       <Image
         id="quality-num"
-        src="https://via.placeholder.com/500"
+        src="https://image-optimization-test.vercel.app/test.jpg"
         quality={80}
         width={500}
         height={500}
       />
       <Image
         id="quality-str"
-        src="https://via.placeholder.com/500"
+        src="https://image-optimization-test.vercel.app/test.jpg"
         quality="80"
         width={500}
         height={500}
@@ -44,7 +44,7 @@ const Page = () => {
       />
       <Image
         id="placeholder-and-blur-data-url"
-        src="https://via.placeholder.com/500"
+        src="https://image-optimization-test.vercel.app/test.jpg"
         width={500}
         height={500}
         placeholder="blur"
@@ -61,7 +61,7 @@ const Page = () => {
       <ImageCard id="image-card" src="https://via.placeholder.com/300" />
       <DynamicSrcImage
         id="dynamic-src"
-        src="https://via.placeholder.com/400"
+        src="https://image-optimization-test.vercel.app/test.jpg"
         width={400}
         height={400}
       />
