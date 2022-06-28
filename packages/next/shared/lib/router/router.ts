def37b2c7140ad60f8be433c165b669df54739ba
JJ Kasper <jj@jjsweb.site>
2022-06-27 20:41:42 -0500

    Ensure history navigation is correct after query update

--- packages/next/shared/lib/router/router.ts
+++ packages/next/shared/lib/router/router.ts
@@ -903,6 +903,8 @@ export default class Router implements BaseRouter {
       Object.assign<{}, TransitionOptions, TransitionOptions>({}, options, {
         shallow: options.shallow && this._shallow,
         locale: options.locale || this.defaultLocale,
+        // @ts-ignore internal value not exposed on types
+        _h: 0,
       }),
       forcedScroll
     )
