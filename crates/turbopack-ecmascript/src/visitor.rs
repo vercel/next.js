15de8b76af988d913b0c94138e5799fd27fd8cc2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-06-29 15:16:43 +0900

    visitor

--- crates/turbopack-ecmascript/src/visitor.rs
+++ crates/turbopack-ecmascript/src/visitor.rs
@@ -1,5 +1,12 @@
-use swc_ecmascript::visit::VisitMut;
+use swc_common::Span;
+use swc_ecmascript::visit::{VisitMut, noop_visit_mut_type};
 
-pub struct ApplyVisitors {}
+pub struct ApplyVisitors {
+    /// `VisitMut` should be shallow. In other words, it should not visit
+    /// children of the node.
+    pub visitors: Vec<(Span, Box<dyn VisitMut + Send + Sync>)>,
+}
 
-impl VisitMut for ApplyVisitors {}
+impl VisitMut for ApplyVisitors {
+    noop_visit_mut_type!();
+}
