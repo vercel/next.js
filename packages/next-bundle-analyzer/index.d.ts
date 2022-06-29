872375c2268d7b6341c137d9537d08526a5113b9
Richard Borcsik <git@richardb.me>
2022-06-29 21:09:26 +0200

    fix: declare NextBundleAnalyzer as a type

--- packages/next-bundle-analyzer/index.d.ts
+++ packages/next-bundle-analyzer/index.d.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from 'next'
 
-const NextBundleAnalyzer =
+type NextBundleAnalyzer =
   (options?: { enabled?: boolean; openAnalyzer?: boolean }) =>
   (config?: NextConfig) =>
     NextConfig
