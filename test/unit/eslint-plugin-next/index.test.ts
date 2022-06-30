6d0e9ae52d88cba9c1031d49482259c98db08a4c
Michael Novotny <manovotny@gmail.com>
2022-06-29 20:59:24 -0500

    Adds tests to ensure `eslint-plugin-next`'s available rules are properly exported and recommended rules are defined correctly.

---
+++ test/unit/eslint-plugin-next/index.test.ts
@@ -0,0 +1,30 @@
+import { basename } from 'path'
+
+import glob from 'glob'
+
+import index from '@next/eslint-plugin-next'
+
+const getRuleNameFromRulePath = (path) => basename(path, '.js')
+const rulePaths = glob.sync('packages/eslint-plugin-next/lib/rules/*js', {
+  absolute: true,
+})
+
+describe('@next/eslint-plugin-next index', () => {
+  it('should include all defined rules and no extra / undefined rules', () => {
+    const rules = rulePaths.map((rulePath) => getRuleNameFromRulePath(rulePath))
+
+    expect(index.rules).toContainAllKeys(rules)
+  })
+
+  rulePaths.forEach((rulePath) => {
+    const rule = require(rulePath)
+    const ruleName = getRuleNameFromRulePath(rulePath)
+    const { recommended } = rule.meta.docs
+
+    it(`${ruleName}: recommend should be \`${recommended}\``, () => {
+      expect(
+        Object.hasOwn(index.configs.recommended.rules, `@next/next/${ruleName}`)
+      ).toBe(recommended)
+    })
+  })
+})
