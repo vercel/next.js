6da19ff9eaf25edf93a9f413e476bcd745dd1343
Eduardo Ciciliato <eduardo.ciciliato@gmail.com>
2022-07-01 13:25:25 -0300

    feat(examples): adds middleware example

---
+++ examples/middleware/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  console.log('Middleware called from', request.url)
+  if (request.nextUrl.pathname === '/about') {
+    return NextResponse.redirect(new URL('/redirected', request.url))
+  }
+  if (request.nextUrl.pathname === '/another') {
+    return NextResponse.rewrite(new URL('/rewrite', request.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/about/:path*', '/another/:path*'],
+}
