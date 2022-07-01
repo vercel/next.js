b0332d71618f09d8df64cb3e8003ff63ca0eac35
Jiachi Liu <inbox@huozhi.im>
2022-07-01 23:50:31 +0200

    revert test next config

--- test/integration/react-18-invalid-config/next.config.js
+++ test/integration/react-18-invalid-config/next.config.js
@@ -1,8 +1,1 @@
-const path = require('path')
-const withReactChannel =
-  'exp' === '18'
-    ? (v, conf) => conf
-    : require(path.join(__dirname, '../../lib/with-react-channel.js'))
-module.exports = withReactChannel('exp', {
-  experimental: { serverComponents: true },
-})
+module.exports = {}
