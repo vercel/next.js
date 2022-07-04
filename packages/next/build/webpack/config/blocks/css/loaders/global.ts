b5658965c0311d970f620a075c063484ae41904e
Shu Ding <g@shud.in>
2022-07-04 23:49:02 +0200

    css modules support

--- packages/next/build/webpack/config/blocks/css/loaders/global.ts
+++ packages/next/build/webpack/config/blocks/css/loaders/global.ts
@@ -16,6 +16,7 @@ export function getGlobalCssLoader(
     // loader
     loaders.push(
       getClientStyleLoader({
+        isAppDir: !!ctx.experimental.appDir,
         isDevelopment: ctx.isDevelopment,
         assetPrefix: ctx.assetPrefix,
       })
