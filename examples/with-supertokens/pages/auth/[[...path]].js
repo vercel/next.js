d7f9672c8d85ea710db6fa429b97218d0ce865f4
Mihaly Lengyel <mihaly@lengyel.tech>
2022-07-04 03:40:16 +0200

    feat: update&improve ssr in with-supertokens

---
+++ examples/with-supertokens/pages/auth/[[...path]].js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import React, { useEffect } from 'react'
+import styles from '../../styles/Home.module.css'
+import dynamic from 'next/dynamic'
+import SuperTokens, { SuperTokensWrapper } from 'supertokens-auth-react'
+import { redirectToAuth } from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
+
+const SuperTokensComponentNoSSR = dynamic(
+  new Promise((res) => res(SuperTokens.getRoutingComponent)),
+  { ssr: false }
+)
+
+export default function Auth() {
+  useEffect(() => {
+    if (SuperTokens.canHandleRoute() === false) {
+      redirectToAuth()
+    }
+  }, [])
+
+  return (
+    <SuperTokensWrapper>
+      <div className={styles.container}>
+        <Head>
+          <title>SuperTokens 💫</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <SuperTokensComponentNoSSR />
+        </main>
+      </div>
+    </SuperTokensWrapper>
+  )
+}
