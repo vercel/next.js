118e9222a1d24c47ac5c6c48b2f7e98dffa91670
JJ Kasper <jj@jjsweb.site>
2022-07-05 16:08:54 -0500

    lint-fix

--- test/e2e/link-with-api-rewrite/app/next.config.js
+++ test/e2e/link-with-api-rewrite/app/next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-        return {
-            beforeFiles: [
-                {
-                    source: '/:path(.*)',
-                    has: [{ type: 'query', key: 'json', value: 'true' }],
-                    destination: '/api/json?from=/:path',
-                },
-            ],
-        }
-    },
+  reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path(.*)',
+          has: [{ type: 'query', key: 'json', value: 'true' }],
+          destination: '/api/json?from=/:path',
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig
