86b201189f2b292eeaf048c321098ebd8b074194
julio@theowl.solutions <julio@theowl.solutions>
2022-07-06 15:01:40 -0500

    Remove: files and directories from trpc example

--- examples/with-trpc/src/utils/trpc.ts
+++
@@ -1,5 +0,0 @@
-import { createReactQueryHooks } from '@trpc/react'
-import type { AppRouter } from '../pages/api/trpc/[trpc]'
-
-export const trpc = createReactQueryHooks<AppRouter>()
-// => { useQuery: ..., useMutation: ...}
