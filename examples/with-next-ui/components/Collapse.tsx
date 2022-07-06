b6c3915c8fb20a273f02d7c3a3570ef6d8136df0
Cupid Valentine <westerngod@126.com>
2022-07-06 22:23:14 +0800

    remove some unrelated file and code

--- examples/with-next-ui/components/Collapse.tsx
+++ examples/with-next-ui/components/Collapse.tsx
@@ -5,26 +5,21 @@ const CustomCollapse = () => {
     <Collapse.Group>
       <Collapse title="Option A">
         <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          I have had my invitation to this world's festival, and thus my life
+          has been blessed.
         </Text>
       </Collapse>
       <Collapse title="Option B">
         <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          In the meanwhile I smile and I sing all alone. In the meanwhile the
+          air is filling with the perfume of promise.
         </Text>
       </Collapse>
       <Collapse title="Option C">
         <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          I came out on the chariot of the first gleam of light, and pursued my
+          voyage through the wildernesses of worlds leaving my track on many a
+          star and planet.
         </Text>
       </Collapse>
     </Collapse.Group>
--- examples/with-next-ui/next.config.js
+++ examples/with-next-ui/next.config.js
@@ -1,7 +1,15 @@
+const withPlugins = require('next-compose-plugins')
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: false,
 }
 
-module.exports = nextConfig
+const plugins = [[withBundleAnalyzer]]
+
+module.exports = withPlugins(plugins, nextConfig)
