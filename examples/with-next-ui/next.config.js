f490b5d3d2c25262da9fed2d759f9a2a669882fd
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2022-07-08 21:06:30 +0800

    Update next.config.js

--- examples/with-next-ui/next.config.js
+++ examples/with-next-ui/next.config.js
@@ -1,15 +1,7 @@
-const withPlugins = require('next-compose-plugins')
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: false,
 }
 
-const plugins = [[withBundleAnalyzer]]
-
-module.exports = withPlugins(plugins, nextConfig)
+module.exports = nextConfig
