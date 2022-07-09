cfdbcf1148695027cc16e49db9278611d503aac3
Cupid Valentine <westerngod@126.com>
2022-07-09 12:30:10 +0800

    Revert "Add TypeScript to the ant-design example"
    This reverts commit 898fca3d4d11048100a60c7c3aaa4f6ce0e826d0.

--- examples/with-ant-design/next.config.js
+++ examples/with-ant-design/next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer()
--- examples/with-ant-design/pages/_app.js
+++ examples/with-ant-design/pages/_app.js
@@ -1,10 +1,7 @@
-import type { AppProps } from 'next/app'
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/global.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
