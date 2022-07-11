d3f833fe77217fb83b829f72d9e8e9f8d14e1626
Shu Ding <g@shud.in>
2022-07-12 01:38:35 +0200

    add test

--- test/e2e/app-dir/app/app/css/layout.server.js
+++ test/e2e/app-dir/app/app/css/layout.server.js
@@ -4,7 +4,9 @@ import styles from './style.module.css'
 export default function ServerLayout({ children }) {
   return (
     <>
-      <div className={styles['server-css']}>Server Layout: CSS Modules</div>
+      <div id="server-cssm" className={styles['server-css']}>
+        Server Layout: CSS Modules
+      </div>
       <div className="server-css">Server Layout: Global CSS</div>
       {children}
     </>
