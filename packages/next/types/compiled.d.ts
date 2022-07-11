26700539f028fd304f0b21fa9cad590fc5272ad2
JJ Kasper <jj@jjsweb.site>
2022-07-10 21:58:15 -0500

    update lib type

--- packages/next/types/compiled.d.ts
+++ packages/next/types/compiled.d.ts
@@ -1,7 +1,7 @@
 declare module 'next/dist/compiled/ajv' {
   // eslint-disable-next-line
   export type JSONSchemaType<T> = any
-  function Ajv(..._args: any[]): any
+  function Ajv(..._args: any[]): void
   export default Ajv
 }
 declare module 'next/dist/compiled/ajv-keywords'
