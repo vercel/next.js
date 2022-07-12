d8239ec7b5586438e55b92a4c08c0e415cf44941
Wyatt Johnson <accounts+github@wyattjoh.ca>
2022-07-12 12:37:24 -0600

    tests: linting

--- test/e2e/app-dir/app/app/param-and-query/[slug]/page.client.js
+++ test/e2e/app-dir/app/app/param-and-query/[slug]/page.client.js
@@ -1,5 +1,3 @@
-import { useRouter } from 'next/dist/client/components/hooks-client'
-
 export default function Page({ params, query }) {
   return (
     <h1 id="params-and-query" data-params={params.slug} data-query={query.slug}>
