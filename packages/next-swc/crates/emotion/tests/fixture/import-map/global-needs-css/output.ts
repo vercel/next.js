7c2dfab7ba7a2bea1b8fd37a92a29d159a8147d3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-13 20:27:25 +0900

    Update test refs

---
+++ packages/next-swc/crates/emotion/tests/fixture/import-map/global-needs-css/output.ts
@@ -0,0 +1,15 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import * as React from 'react';
+import { SomeGlobalFromCore } from 'package-two';
+const getBgColor = ()=>({
+        backgroundColor: '#fff'
+    });
+export default (()=>/*#__PURE__*/ _jsx(SomeGlobalFromCore, {
+        styles: [
+            {
+                color: 'hotpink',
+                ...getBgColor()
+            },
+            "/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5wdXQudHMiLCJzb3VyY2VzIjpbImlucHV0LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIFJlYWN0IGZyb20gJ3JlYWN0J1xuaW1wb3J0IHsgU29tZUdsb2JhbEZyb21Db3JlIH0gZnJvbSAncGFja2FnZS10d28nXG5cbmNvbnN0IGdldEJnQ29sb3IgPSAoKSA9PiAoeyBiYWNrZ3JvdW5kQ29sb3I6ICcjZmZmJyB9KVxuXG5leHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gIDxTb21lR2xvYmFsRnJvbUNvcmUgc3R5bGVzPXt7IGNvbG9yOiAnaG90cGluaycsIC4uLmdldEJnQ29sb3IoKSB9fSAvPlxuKVxuIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQU1HIn0= */"
+        ]
+    }));
