a1e66c5f2719f4009e88d03939f58b6a9702f360
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-13 19:25:11 +0900

    Types

--- packages/next-swc/crates/emotion/src/import_map.rs
+++ packages/next-swc/crates/emotion/src/import_map.rs
@@ -1,1 +1,18 @@
+use serde::Deserialize;
+use swc_atoms::JsWord;
+use swc_common::collections::AHashMap;
 
+/// key: `importSource`
+pub(crate) type ImportMap = AHashMap<String, ImportMapValue>;
+
+/// key: `localExportName`
+pub(crate) type ImportMapValue = AHashMap<String, Config>;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct Config {
+    canonicalImport: ImportItem,
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct ImportItem(JsWord, JsWord);
