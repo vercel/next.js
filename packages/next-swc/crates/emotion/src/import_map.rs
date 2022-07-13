7eb7a2514bd9a6b12edccf815061793f81977739
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-13 19:35:34 +0900

    API

--- packages/next-swc/crates/emotion/src/import_map.rs
+++ packages/next-swc/crates/emotion/src/import_map.rs
@@ -1,19 +1,28 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use swc_atoms::JsWord;
 use swc_common::collections::AHashMap;
 
+use crate::EmotionModuleConfig;
+
 /// key: `importSource`
-pub(crate) type ImportMap = AHashMap<String, ImportMapValue>;
+pub type ImportMap = AHashMap<String, ImportMapValue>;
 
 /// key: `localExportName`
-pub(crate) type ImportMapValue = AHashMap<String, Config>;
+pub type ImportMapValue = AHashMap<String, Config>;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub(crate) struct Config {
+pub struct Config {
     canonical_import: ImportItem,
 }
 
 /// `(packageName, exportName)`
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct ImportItem(JsWord, JsWord);
+
+pub(crate) fn expand_import_map(
+    map: ImportMap,
+    imports: Vec<EmotionModuleConfig>,
+) -> Vec<EmotionModuleConfig> {
+    imports
+}
