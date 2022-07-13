2fdfd1f86420d7d2022cc33876b44867049fc908
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-07-13 15:09:45 +0900

    Fix api

--- packages/next-swc/crates/emotion/src/lib.rs
+++ packages/next-swc/crates/emotion/src/lib.rs
@@ -10,7 +10,7 @@ use sourcemap::{RawToken, SourceMap as RawSourcemap};
 use swc_atoms::JsWord;
 use swc_common::comments::Comments;
 use swc_common::util::take::Take;
-use swc_common::{BytePos, SourceMap, DUMMY_SP};
+use swc_common::{BytePos, SourceMap, SourceMapperDyn, DUMMY_SP};
 use swc_ecmascript::ast::{
     ArrayLit, CallExpr, JSXAttr, JSXAttrName, JSXAttrOrSpread, JSXAttrValue, JSXElementName,
     JSXExpr, JSXExprContainer, JSXObject, SourceMapperExt,
@@ -142,7 +142,7 @@ enum PackageMeta {
 pub fn emotion<C: Comments>(
     emotion_options: EmotionOptions,
     path: &Path,
-    cm: Arc<SourceMap>,
+    cm: Arc<SourceMapperDyn>,
     comments: C,
 ) -> impl Fold {
     EmotionTransformer::new(emotion_options, path, cm, comments)
