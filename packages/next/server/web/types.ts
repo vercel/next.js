2a6b590d98648ebdc75ce4ae7c3f97fec38996ae
Balázs Orbán <info@balazsorban.com>
2022-07-14 01:08:15 +0200

    allow `void` to be returned from Middleware

--- packages/next/server/web/types.ts
+++ packages/next/server/web/types.ts
@@ -47,7 +47,12 @@ export interface FetchEventResult {
   waitUntil: Promise<any>
 }
 
-export type NextMiddlewareResult = NextResponse | Response | null | undefined
+export type NextMiddlewareResult =
+  | NextResponse
+  | Response
+  | null
+  | undefined
+  | void
 
 export type NextMiddleware = (
   request: NextRequest,
