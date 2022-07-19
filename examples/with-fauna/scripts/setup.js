4648002382a1617139e9781679684d5613ccbdcc
Trevor Finn <104657098+trevor-fauna@users.noreply.github.com>
2022-07-19 16:57:07 -0400

    Fix export issue in with-fauna example

--- examples/with-fauna/scripts/setup.js
+++ examples/with-fauna/scripts/setup.js
@@ -184,6 +184,8 @@ const main = async () => {
   console.log('- Created .env.local file with secret')
 }
 
+exports.resolveDbDomain = resolveDbDomain;
+
 main().catch((err) => {
   console.error(err)
   process.exit(1)
