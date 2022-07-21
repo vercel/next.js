cc9c366f0b526dad81b277b9b7d9b3c95e5814de
Han Yeong-woo <nix6839@washnix.com>
2022-07-22 08:45:22 +0900

    refactor: change arrow to declaration

--- examples/with-stitches/pages/_document.tsx
+++ examples/with-stitches/pages/_document.tsx
@@ -1,16 +1,19 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 import { getCssText } from '../stitches.config'
 
-const MyDocument = () => (
-  <Html lang="en">
-    <Head>
-      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-    </Head>
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-)
-
-export default MyDocument
+export default function MyDocument() {
+  return (
+    <Html lang="en">
+      <Head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
