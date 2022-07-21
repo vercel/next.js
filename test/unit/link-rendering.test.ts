8d647790379d122cbc293608e071149089e5935c
Yuddomack <darr880406@gmail.com>
2022-07-22 01:40:27 +0900

    Update rendering test

--- test/unit/link-rendering.test.ts
+++ test/unit/link-rendering.test.ts
@@ -2,15 +2,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/server'
 import Link from 'next/link'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { NextRouter } from 'next/dist/shared/lib/router/router'
 
 describe('Link rendering', () => {
   it('should render Link on its own', async () => {
     const element = React.createElement(
-      Link,
-      {
-        href: '/my-path',
-      },
-      React.createElement('a', {}, 'to another page')
+      RouterContext.Provider,
+      { value: {} as NextRouter },
+      React.createElement(
+        Link,
+        {
+          href: '/my-path',
+        },
+        React.createElement('a', {}, 'to another page')
+      )
     )
     const html = ReactDOM.renderToString(element)
     expect(html).toMatchInlineSnapshot(
