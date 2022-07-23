1257fa788603ec44f3962abb307de145062727a6
Max Proske <max@mproske.com>
2022-07-23 14:28:27 -0700

    Remove prettier, add env d.ts, functional Document

--- examples/blog-with-comment/pages/_app.tsx
+++ examples/blog-with-comment/pages/_app.tsx
@@ -8,11 +8,15 @@ import { Auth0Provider } from '@auth0/auth0-react'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Auth0Provider
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
     >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Clone and deploy your own Next.js portfolio in minutes."
+        />
         <title>My awesome blog</title>
       </Head>
 
--- examples/blog-with-comment/pages/_document.tsx
+++ examples/blog-with-comment/pages/_document.tsx
@@ -1,35 +1,13 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  static getInitialProps(ctx: DocumentContext) {
-    return Document.getInitialProps(ctx)
-  }
-
-  render() {
-    const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    }
-
-    return (
-      <Html lang="en" className="bg-white text-gray-700 antialiased">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="robots" content="follow, index" />
-          <meta name="description" content={meta.description} />
-        </Head>
-
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-white text-gray-700 antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
