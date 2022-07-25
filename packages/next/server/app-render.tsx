9639c23931dee21ed134d8f2f699d3e4ac02e515
Tim Neutkens <tim@timneutkens.nl>
2022-07-25 16:29:55 +0200

    Refactor dynamic segment handling

--- packages/next/server/app-render.tsx
+++ packages/next/server/app-render.tsx
@@ -316,6 +316,9 @@ export type ChildProp = {
   segment: Segment
 }
 
+/**
+ * Parse dynamic route segment to type of parameter
+ */
 function getSegmentParam(segment: string): {
   param: string
   type: DynamicParamTypes
@@ -344,6 +347,9 @@ function getSegmentParam(segment: string): {
   return null
 }
 
+/**
+ * Get inline <link> tags based on __next_rsc_css__ manifest. Only used when rendering to HTML.
+ */
 function getCssInlinedLinkTags(
   ComponentMod: any,
   serverComponentManifest: any
@@ -392,6 +398,7 @@ export async function renderToHTMLOrFlight(
 
   const isFlight = query.__flight__ !== undefined
 
+  // Handle client-side navigation to pages directory
   if (isFlight && isPagesDir) {
     stripInternalQueries(query)
     const search = stringifyQuery(query)
@@ -408,6 +415,9 @@ export async function renderToHTMLOrFlight(
   // TODO-APP: verify the tree is valid
   // TODO-APP: verify query param is single value (not an array)
   // TODO-APP: verify tree can't grow out of control
+  /**
+   * Router state provided from the client-side router. Used to handle rendering from the common layout down.
+   */
   const providedFlightRouterState: FlightRouterState = isFlight
     ? query.__flight_router_state_tree__
       ? JSON.parse(query.__flight_router_state_tree__ as string)
@@ -416,9 +426,7 @@ export async function renderToHTMLOrFlight(
 
   stripInternalQueries(query)
 
-  const hasConcurrentFeatures = !!runtime
   const pageIsDynamic = isDynamicRoute(pathname)
-
   const LayoutRouter =
     ComponentMod.LayoutRouter as typeof import('../client/components/layout-router.client').default
   const HotReloader = ComponentMod.HotReloader as
@@ -426,10 +434,14 @@ export async function renderToHTMLOrFlight(
     | null
 
   const headers = req.headers
-  // @ts-expect-error TODO-APP: fix type of req
+  // TODO-APP: fix type of req
+  // @ts-expect-error
   const cookies = req.cookies
 
-  const tree: LoaderTree = ComponentMod.tree
+  /**
+   * The tree created in next-app-loader that holds component segments and modules
+   */
+  const loaderTree: LoaderTree = ComponentMod.tree
 
   // Reads of this are cached on the `req` object, so this should resolve
   // instantly. There's no need to pass this data down from a previous
@@ -440,6 +452,11 @@ export async function renderToHTMLOrFlight(
     (renderOpts as any).previewProps
   )
   const isPreview = previewData !== false
+  /**
+   * Server Context is specifically only available in Server Components.
+   * It has to hold values that can't change while rendering from the common layout down.
+   * An example of this would be that `headers` are available but `searchParams` are not because that'd mean we have to render from the root layout down on all requests.
+   */
   const serverContexts: Array<[string, any]> = [
     ['WORKAROUND', null], // TODO-APP: First value has a bug currently where the value is not set on the second request: https://github.com/facebook/react/issues/24849
     ['HeadersContext', headers],
@@ -447,19 +464,28 @@ export async function renderToHTMLOrFlight(
     ['PreviewDataContext', previewData],
   ]
 
+  /**
+   * Used to keep track of in-flight / resolved data fetching Promises.
+   */
   const dataCache = new Map<string, Record>()
 
   type CreateSegmentPath = (child: FlightSegmentPath) => FlightSegmentPath
 
+  /**
+   * Dynamic parameters. E.g. when you visit `/dashboard/vercel` which is rendered by `/dashboard/[slug]` the value will be {"slug": "vercel"}.
+   */
   const pathParams = (renderOpts as any).params as ParsedUrlQuery
 
+  /**
+   * Parse the dynamic segment and return the associated value.
+   */
   const getDynamicParamFromSegment = (
-    // [id] or [slug]
+    // [slug] / [[slug]] / [...slug]
     segment: string
   ): {
     param: string
     value: string | string[] | null
-    treeValue: string
+    treeSegment: Segment
     type: DynamicParamTypesShort
   } | null => {
     const segmentParam = getSegmentParam(segment)
@@ -471,22 +497,29 @@ export async function renderToHTMLOrFlight(
     const value = pathParams[key]
 
     if (!value) {
+      // Handle case where optional catchall does not have a value, e.g. `/dashboard/[...slug]` when requesting `/dashboard`
       if (segmentParam.type === 'optional-catchall') {
+        const type = getShortDynamicParamType(segmentParam.type)
         return {
           param: key,
           value: null,
-          type: getShortDynamicParamType(segmentParam.type),
-          treeValue: '',
+          type: type,
+          // This value always has to be a string.
+          treeSegment: [key, '', type],
         }
       }
       return null
     }
 
+    const type = getShortDynamicParamType(segmentParam.type)
+
     return {
       param: key,
+      // The value that is passed to user code.
       value: value,
-      treeValue: Array.isArray(value) ? value.join('/') : value,
-      type: getShortDynamicParamType(segmentParam.type),
+      // The value that is rendered in the router tree.
+      treeSegment: [key, Array.isArray(value) ? value.join('/') : value, type],
+      type: type,
     }
   }
 
@@ -499,9 +532,7 @@ export async function renderToHTMLOrFlight(
     const dynamicParam = getDynamicParamFromSegment(segment)
 
     const segmentTree: FlightRouterState = [
-      dynamicParam
-        ? [dynamicParam.param, dynamicParam.treeValue, dynamicParam.type]
-        : segment,
+      dynamicParam ? dynamicParam.treeSegment : segment,
       {},
     ]
 
@@ -523,15 +554,18 @@ export async function renderToHTMLOrFlight(
     return segmentTree
   }
 
+  /**
+   * Use the provided loader tree to create the React Component tree.
+   */
   const createComponentTree = async ({
     createSegmentPath,
-    tree: [segment, parallelRoutes, { layout, loading, page }],
+    loaderTree: [segment, parallelRoutes, { layout, loading, page }],
     parentParams,
     firstItem,
     rootLayoutIncluded,
   }: {
     createSegmentPath: CreateSegmentPath
-    tree: LoaderTree
+    loaderTree: LoaderTree
     parentParams: { [key: string]: any }
     rootLayoutIncluded?: boolean
     firstItem?: boolean
@@ -544,10 +578,19 @@ export async function renderToHTMLOrFlight(
       : isPage
       ? await page()
       : undefined
+    /**
+     * Checks if the current segment is a root layout.
+     */
     const rootLayoutAtThisLevel = isLayout && !rootLayoutIncluded
+    /**
+     * Checks if the current segment or any level above it has a root layout.
+     */
     const rootLayoutIncludedAtThisLevelOrAbove =
       rootLayoutIncluded || rootLayoutAtThisLevel
 
+    /**
+     * Check if the current layout/page is a client component
+     */
     const isClientComponentModule =
       layoutOrPageMod && !layoutOrPageMod.hasOwnProperty('__next_rsc__')
 
@@ -565,11 +608,18 @@ export async function renderToHTMLOrFlight(
       }
     }
 
+    /**
+     * The React Component to render.
+     */
     const Component = layoutOrPageMod
       ? interopDefault(layoutOrPageMod)
       : undefined
 
+    // Handle dynamic segment params.
     const segmentParam = getDynamicParamFromSegment(segment)
+    /**
+     * Create object holding the parent params and current params, this is passed to getServerSideProps and getStaticProps.
+     */
     const currentParams =
       // Handle null case where dynamic param is optional
       segmentParam && segmentParam.value !== null
@@ -577,10 +627,10 @@ export async function renderToHTMLOrFlight(
             ...parentParams,
             [segmentParam.param]: segmentParam.value,
           }
-        : parentParams
-    const actualSegment = segmentParam
-      ? [segmentParam.param, segmentParam.treeValue]
-      : segment
+        : // Pass through parent params to children
+          parentParams
+    // Resolve the segment param
+    const actualSegment = segmentParam ? segmentParam.treeSegment : segment
 
     // This happens outside of rendering in order to eagerly kick off data fetching for layouts / the page further down
     const parallelRouteMap = await Promise.all(
@@ -590,11 +640,12 @@ export async function renderToHTMLOrFlight(
             ? [parallelRouteKey]
             : [actualSegment, parallelRouteKey]
 
+          // Create the child component
           const { Component: ChildComponent } = await createComponentTree({
             createSegmentPath: (child) => {
               return createSegmentPath([...currentSegmentPath, ...child])
             },
-            tree: parallelRoutes[parallelRouteKey],
+            loaderTree: parallelRoutes[parallelRouteKey],
             parentParams: currentParams,
             rootLayoutIncluded: rootLayoutIncludedAtThisLevelOrAbove,
           })
@@ -605,11 +656,7 @@ export async function renderToHTMLOrFlight(
           const childProp: ChildProp = {
             current: <ChildComponent />,
             segment: childSegmentParam
-              ? [
-                  childSegmentParam.param,
-                  childSegmentParam.treeValue,
-                  childSegmentParam.type,
-                ]
+              ? childSegmentParam.treeSegment
               : parallelRoutes[parallelRouteKey][0],
           }
 
@@ -775,7 +822,7 @@ export async function renderToHTMLOrFlight(
             }
           : parentParams
       const actualSegment: Segment = segmentParam
-        ? [segmentParam.param, segmentParam.treeValue, segmentParam.type]
+        ? segmentParam.treeSegment
         : segment
 
       const renderComponentsOnThisLevel =
@@ -796,7 +843,7 @@ export async function renderToHTMLOrFlight(
                 // This ensures flightRouterPath is valid and filters down the tree
                 {
                   createSegmentPath: (child) => child,
-                  tree: treeToFilter,
+                  loaderTree: treeToFilter,
                   parentParams: currentParams,
                   firstItem: true,
                 }
@@ -826,7 +873,11 @@ export async function renderToHTMLOrFlight(
     const flightData: FlightData = [
       // TODO-APP: change walk to output without ''
       (
-        await walkTreeWithFlightRouterState(tree, {}, providedFlightRouterState)
+        await walkTreeWithFlightRouterState(
+          loaderTree,
+          {},
+          providedFlightRouterState
+        )
       ).slice(1),
     ]
 
@@ -839,7 +890,7 @@ export async function renderToHTMLOrFlight(
 
   const { Component: ComponentTree } = await createComponentTree({
     createSegmentPath: (child) => child,
-    tree,
+    loaderTree: loaderTree,
     parentParams: {},
     firstItem: true,
   })
@@ -862,7 +913,7 @@ export async function renderToHTMLOrFlight(
 
   const ServerComponentsRenderer = createServerComponentRenderer(
     () => {
-      const initialTree = createFlightRouterStateFromLoaderTree(tree)
+      const initialTree = createFlightRouterStateFromLoaderTree(loaderTree)
 
       return (
         <AppRouter
@@ -928,6 +979,8 @@ export async function renderToHTMLOrFlight(
       const flushed = ReactDOMServer.renderToString(styledJsxFlushEffect())
       return flushed
     }
+
+    const hasConcurrentFeatures = !!runtime
 
     return await continueFromInitialStream(renderStream, {
       dataStream: serverComponentsInlinedTransformStream?.readable,
