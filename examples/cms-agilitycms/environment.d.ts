cfe01dbb3230c52cec9500b0ed190cf32e7663fc
Max Proske <max@mproske.com>
2022-07-25 07:39:41 -0700

    Update examples/cms-agilitycms/environment.d.ts
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- examples/cms-agilitycms/environment.d.ts
+++ examples/cms-agilitycms/environment.d.ts
@@ -1,6 +1,5 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test'
     readonly AGILITY_CMS_GUID: string
     readonly AGILITY_CMS_API_FETCH_KEY: string
   }
