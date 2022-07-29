79513de86254ffeeac68d4f1821c969240613226
Jiachi Liu <inbox@huozhi.im>
2022-07-29 12:21:34 +0200

    remove loadable

--- packages/next/client/components/shared/dynamic/index.tsx
+++ packages/next/client/components/shared/dynamic/index.tsx
@@ -1,50 +1,21 @@
 import React from 'react'
-import Loadable from './loadable'
 
-export type LoaderComponent<P = {}> = Promise<
-  React.ComponentType<P> | { default: React.ComponentType<P> }
->
+export type LoaderComponent<P = {}> = Promise<{
+  default: React.ComponentType<P>
+}>
 
-export type Loader<P = {}> = (() => LoaderComponent<P>) | LoaderComponent<P>
+export type Loader<P = {}> = () => LoaderComponent<P>
 
-export type LoaderMap = { [mdule: string]: () => Loader<any> }
-
-export type LoadableGeneratedOptions = {
-  webpack?(): any
-  modules?(): LoaderMap
-}
-
-export type DynamicOptions<P = {}> = LoadableGeneratedOptions & {
+export type DynamicOptions<P = {}> = {
   loader?: Loader<P>
-  loadableGenerated?: LoadableGeneratedOptions
 }
-
-export type LoadableOptions<P = {}> = DynamicOptions<P>
-
-export type LoadableFn<P = {}> = (
-  opts: LoadableOptions<P>
-) => React.ComponentType<P>
 
 export type LoadableComponent<P = {}> = React.ComponentType<P>
 
 export default function dynamic<P = {}>(
-  loader: Loader<P>,
-  options?: DynamicOptions<P>
+  loader: Loader<P>
 ): React.ComponentType<P> {
-  let loadableFn: LoadableFn<P> = Loadable
-  let loadableOptions: LoadableOptions<P> = {
-    ...options,
-    loader,
-  }
+  const LazyLoadable = React.lazy(loader)
 
-  // coming from build/babel/plugins/react-loadable-plugin.js
-  if (loadableOptions.loadableGenerated) {
-    loadableOptions = {
-      ...loadableOptions,
-      ...loadableOptions.loadableGenerated,
-    }
-    delete loadableOptions.loadableGenerated
-  }
-
-  return loadableFn(loadableOptions)
+  return LazyLoadable
 }
