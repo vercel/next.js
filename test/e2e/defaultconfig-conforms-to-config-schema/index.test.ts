8a81352e64ceaa0f0dc74f0463d5258df3cff953
Balázs Orbán <info@balazsorban.com>
2022-08-01 11:08:27 +0200

    add test

---
+++ test/e2e/defaultconfig-conforms-to-config-schema/index.test.ts
@@ -0,0 +1,30 @@
+import { createNext } from 'e2e-utils'
+import { NextInstance } from 'test/lib/next-modes/base'
+import { renderViaHTTP } from 'next-test-utils'
+
+describe('defaultConfig conforms to config schema', () => {
+  let next: NextInstance
+
+  beforeAll(async () => {
+    next = await createNext({
+      files: {
+        'pages/index.js': `
+          export default function Page() {
+            return
+          }
+        `,
+        'next.config.js': `
+        module.exports = (_, c) => c.defaultConfig`,
+      },
+      dependencies: {},
+    })
+  })
+  afterAll(() => next.destroy())
+
+  it('should work', async () => {
+    await renderViaHTTP(next.url, '/')
+    expect(next.cliOutput).not.toContain(
+      /Invalid next.config.js options detected:/
+    )
+  })
+})
