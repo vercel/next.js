41c2dfd2de9f6ff876cb14d1ba567acbc122076a
Tobias Koppers <tobias.koppers@googlemail.com>
2022-08-01 09:02:53 +0200

    introduce ContentSource as source for the dev server (vercel/turbo#185)
    and potentially a next build in future

---
+++ packages/next-swc/crates/next-dev/src/turbo_tasks_viz.rs
@@ -0,0 +1,62 @@
+use std::{str::FromStr, sync::Arc};
+
+use anyhow::Result;
+use mime::Mime;
+use turbo_tasks::TurboTasks;
+use turbo_tasks_fs::{File, FileContent, FileContentVc};
+use turbo_tasks_memory::{stats::Stats, viz, MemoryBackend};
+use turbopack_dev_server::source::{ContentSource, ContentSourceVc};
+
+#[turbo_tasks::value(ContentSource, serialization: none, eq: manual, cell: new, into: new)]
+pub struct TurboTasksSource {
+    #[trace_ignore]
+    pub turbo_tasks: Arc<TurboTasks<MemoryBackend>>,
+}
+
+impl TurboTasksSourceVc {
+    pub fn new(turbo_tasks: Arc<TurboTasks<MemoryBackend>>) -> Self {
+        Self::cell(TurboTasksSource { turbo_tasks })
+    }
+}
+
+#[turbo_tasks::value_impl]
+impl ContentSource for TurboTasksSource {
+    #[turbo_tasks::function]
+    fn get(&self, path: &str) -> Result<FileContentVc> {
+        let tt = &self.turbo_tasks;
+        if path == "graph" {
+            let mut stats = Stats::new();
+            let b = tt.backend();
+            b.with_all_cached_tasks(|task| {
+                stats.add_id(b, task);
+            });
+            let tree = stats.treeify();
+            let graph = viz::graph::visualize_stats_tree(tree);
+            return Ok(FileContent::Content(
+                File::from_source(viz::graph::wrap_html(&graph))
+                    .with_content_type(Mime::from_str("text/html")?),
+            )
+            .into());
+        }
+        if path == "table" {
+            let mut stats = Stats::new();
+            let b = tt.backend();
+            b.with_all_cached_tasks(|task| {
+                stats.add_id(b, task);
+            });
+            let tree = stats.treeify();
+            let table = viz::table::create_table(tree);
+            return Ok(FileContent::Content(
+                File::from_source(viz::table::wrap_html(&table))
+                    .with_content_type(Mime::from_str("text/html")?),
+            )
+            .into());
+        }
+        Ok(FileContent::NotFound.into())
+    }
+
+    #[turbo_tasks::function]
+    fn get_by_id(&self, _id: &str) -> FileContentVc {
+        FileContent::NotFound.into()
+    }
+}
