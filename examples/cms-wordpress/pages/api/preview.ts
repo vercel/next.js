85c230a1ef5e3e33dd7174327792f52ca92e666b
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2022-08-02 18:24:44 +0700

    style: convert files to typescript

--- examples/cms-wordpress/pages/api/preview.ts
+++ examples/cms-wordpress/pages/api/preview.ts
@@ -1,6 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { getPreviewPost } from '../../lib/api'
 
-export default async function preview(req, res) {
+export default async function preview(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { secret, id, slug } = req.query
 
   // Check the secret and next parameters
