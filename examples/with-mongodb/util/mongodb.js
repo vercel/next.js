1c56857f5dbfb6c0f5d5e4583e5b4d4e33809c05
githubrana <39762049+githubrana@users.noreply.github.com>
2022-08-05 18:21:29 +0530

    Create mongodb.js
    Module not found: Can’t resolve ‘../util/mongodb’ - Error When Running Next.js
    
    as lot, many people find it difficult to run the sample program without this file, even mentioned on the official site of MongoDB https://www.mongodb.com/community/forums/t/module-not-found-cant-resolve-util-mongodb-error-when-running-next-js/136438
    
    this will resolve the above mentioned issued

---
+++ examples/with-mongodb/util/mongodb.js
@@ -0,0 +1,48 @@
+import { MongoClient } from 'mongodb';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_DB = process.env.MONGODB_DB;
+
+// check the MongoDB URI
+if (!MONGODB_URI) {
+    throw new Error('Define the MONGODB_URI environmental variable');
+}
+
+// check the MongoDB DB
+if (!MONGODB_DB) {
+    throw new Error('Define the MONGODB_DB environmental variable');
+}
+
+let cachedClient = null;
+let cachedDb = null;
+
+export async function connectToDatabase() {
+    // check the cached.
+    if (cachedClient && cachedDb) {
+        // load from cache
+        return {
+            client: cachedClient,
+            db: cachedDb,
+        };
+    }
+
+    // set the connection options
+    const opts = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    // Connect to cluster
+    let client = new MongoClient(MONGODB_URI, opts);
+    await client.connect();
+    let db = client.db(MONGODB_DB);
+
+    // set cache
+    cachedClient = client;
+    cachedDb = db;
+
+    return {
+        client: cachedClient,
+        db: cachedDb,
+    };
+}
