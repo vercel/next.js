0c6eabaaec4d90ed26d47878a8bd89f79092ef79
LongYinan <lynweklm@gmail.com>
2022-08-08 23:30:19 +0800

    Fix wrong hooks usage in webpack-node-module-trace (#214)

--- packages/webpack-nmt/src/index.ts
+++ packages/webpack-nmt/src/index.ts
@@ -1,10 +1,12 @@
-import { spawn } from 'child_process'
+import { spawnSync } from 'child_process'
 import { join } from 'path'
 
 import { Compilation, WebpackPluginInstance, Compiler } from 'webpack'
 
 export interface NodeModuleTracePluginOptions {
   cwd?: string
+  // relative to cwd
+  contextDirectory?: string
   // additional PATH environment variable to use for spawning the `node-file-trace` process
   path?: string
 }
@@ -12,13 +14,15 @@ export interface NodeModuleTracePluginOptions {
 export class NodeModuleTracePlugin implements WebpackPluginInstance {
   static PluginName = 'NodeModuleTracePlugin'
 
+  private readonly chunksToTrace = new Set<string>()
+
   constructor(private readonly options?: NodeModuleTracePluginOptions) {}
 
   apply(compiler: Compiler) {
     compiler.hooks.compilation.tap(
       NodeModuleTracePlugin.PluginName,
       (compilation) => {
-        compilation.hooks.processAssets.tapPromise(
+        compilation.hooks.processAssets.tap(
           {
             name: NodeModuleTracePlugin.PluginName,
             stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
@@ -27,67 +31,57 @@ export class NodeModuleTracePlugin implements WebpackPluginInstance {
         )
       },
     )
+    compiler.hooks.afterDone.tap(NodeModuleTracePlugin.PluginName, () =>
+      this.runTrace(),
+    )
   }
 
   private createTraceAssets(compilation: Compilation) {
     const outputPath = compilation.outputOptions.path!
-    const entryFilesMap = new Map<any, Set<string>>()
-    const chunksToTrace = new Set<string>()
-    const isTraceable = (file: string) => !file.endsWith('.wasm')
+
+    const isTraceable = (file: string) =>
+      !file.endsWith('.wasm') && !file.endsWith('.map')
 
     for (const entrypoint of compilation.entrypoints.values()) {
-      const entryFiles = new Set<string>()
-
       for (const chunk of entrypoint
         .getEntrypointChunk()
         .getAllReferencedChunks()) {
         for (const file of chunk.files) {
           if (isTraceable(file)) {
             const filePath = join(outputPath, file)
-            chunksToTrace.add(filePath)
-            entryFiles.add(filePath)
+            this.chunksToTrace.add(filePath)
           }
         }
         for (const file of chunk.auxiliaryFiles) {
           if (isTraceable(file)) {
             const filePath = join(outputPath, file)
-            chunksToTrace.add(filePath)
-            entryFiles.add(filePath)
+            this.chunksToTrace.add(filePath)
           }
         }
       }
-      entryFilesMap.set(entrypoint, entryFiles)
     }
-    return this.runTrace(chunksToTrace)
   }
 
-  private runTrace(files: Set<string>) {
-    const child = spawn('node-file-trace', ['annotate', '--exact', ...files], {
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        PATH: `${this.options?.path ?? ''}${
-          process.platform === 'win32' ? ';' : ':'
-        }${process.env.PATH}`,
+  private runTrace() {
+    spawnSync(
+      'node-file-trace',
+      [
+        'annotate',
+        '--context-directory',
+        this.options?.contextDirectory ?? '.',
+        '--exact',
+        ...this.chunksToTrace,
+      ],
+      {
+        stdio: 'inherit',
+        env: {
+          ...process.env,
+          PATH: `${this.options?.path ?? ''}${
+            process.platform === 'win32' ? ';' : ':'
+          }${process.env.PATH}`,
+        },
+        cwd: this.options?.cwd ?? process.cwd(),
       },
-      cwd: this.options?.cwd ?? process.cwd(),
-    })
-    return new Promise<void>((resolve, reject) => {
-      child.once('close', (code) => {
-        if (!code) {
-          resolve()
-        } else {
-          reject(new Error(`node-file-trace exited with code ${code}`))
-        }
-      })
-      child.once('error', reject)
-      child.once('exit', (code) => {
-        if (!code) {
-          resolve()
-        } else {
-          reject(new Error(`node-file-trace exited with code ${code}`))
-        }
-      })
-    })
+    )
   }
 }
