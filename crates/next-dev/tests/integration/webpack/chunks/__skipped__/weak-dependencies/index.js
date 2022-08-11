d2892518a61345109a6d0e8b08e0146bbce5217c
Will Binns-Smith <wbinnssmith@gmail.com>
2022-08-11 09:33:25 -0700

    next-dev test runner: Check in and use webpack's chunk test cases (#212)
    This commits webpack's chunk tests (test/cases/chunks) and skips those
    that do not pass yet.
    
    Test Plan: `cargo test -p next-dev -- --nocapture` and verify the
    non-skipped tests run.

---
+++ crates/next-dev/tests/integration/webpack/chunks/__skipped__/weak-dependencies/index.js
@@ -0,0 +1,13 @@
+it('should not include a module with a weak dependency', function () {
+  var a = !!__webpack_modules__[require.resolveWeak('./a')]
+  var b = !!__webpack_modules__[require.resolve('./b')]
+  var c = !!__webpack_modules__[require.resolveWeak('./c')]
+  var d = !!__webpack_modules__[require.resolveWeak('./d')]
+  require(['./c'])
+  require('./d')
+
+  expect(a).toBe(false)
+  expect(b).toBe(true)
+  expect(c).toBe(false)
+  expect(d).toBe(true)
+})
