0ff547ac5d27f3d015571ff23fe4095d490ffda5
Tobias Koppers <tobias.koppers@googlemail.com>
2022-08-12 13:47:05 +0200

    upgrade SWC (#234)

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -81,7 +81,11 @@ fn main() {
             let swc_packages = lock
                 .packages
                 .iter()
-                .filter(|p| p.name.as_str().starts_with("swc_") || p.name.as_str() == "testing")
+                .filter(|p| {
+                    p.name.as_str().starts_with("swc_")
+                        || p.name.as_str() == "swc"
+                        || p.name.as_str() == "testing"
+                })
                 .collect::<Vec<_>>();
             let only_swc_set = swc_packages
                 .iter()
