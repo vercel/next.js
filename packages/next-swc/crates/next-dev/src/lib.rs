c7ff4150c8e3e3e03fd058fd10ca2c5db27e6d03
Tobias Koppers <tobias.koppers@googlemail.com>
2022-08-16 15:12:09 +0200

    fix consistency issues (vercel/turbo#224)
    There are a bunch or problems with invalidations:
    
    * The fs impl watches path case-insenstive. This means two paths might conflict when on a case-sensitive filesystem. It uses an array of Invalidators now
    * Move the next-dev bootstrapping logic out of the run_once scope (which is not updated when invalidations occur). Instead it's executed inside the request handling resp. update stream where changes can be handled.
    * `TransientValue` was not an Value type actually. This fixes that.
    * Adds a new `TransientInstance` wrapper to pass transient by reference.
    * `strongly_consistent` was broken when using nested TaskScopes. This fixes that.

--- packages/next-swc/crates/next-dev/src/lib.rs
+++ packages/next-swc/crates/next-dev/src/lib.rs
@@ -1,19 +1,19 @@
 #![feature(future_join)]
 #![feature(min_specialization)]
 
-use std::{net::IpAddr, sync::Arc};
+use std::{net::IpAddr, path::MAIN_SEPARATOR, sync::Arc};
 
 use anyhow::{anyhow, Context, Result};
 use next_core::{create_server_rendered_source, create_web_entry_source};
-use turbo_tasks::{CollectiblesSource, TransientValue, TurboTasks};
-use turbo_tasks_fs::{DiskFileSystemVc, FileSystemPathVc};
+use turbo_tasks::{CollectiblesSource, TransientInstance, TurboTasks};
+use turbo_tasks_fs::{DiskFileSystemVc, FileSystemPathVc, FileSystemVc};
 use turbo_tasks_memory::MemoryBackend;
 use turbopack_cli_utils::issue::issue_to_styled_string;
 use turbopack_core::issue::{IssueSeverity, IssueVc};
 use turbopack_dev_server::{
     fs::DevServerFileSystemVc,
-    source::{combined::CombinedContentSource, router::RouterContentSource},
-    DevServerListening, DevServerVc,
+    source::{combined::CombinedContentSource, router::RouterContentSource, ContentSourceVc},
+    DevServer,
 };
 
 mod turbo_tasks_viz;
@@ -75,95 +75,117 @@ impl NextDevServerBuilder {
         self
     }
 
-    pub async fn build(self) -> Result<DevServerListening> {
+    pub async fn build(self) -> Result<DevServer> {
         let turbo_tasks = self.turbo_tasks.context("turbo_tasks must be set")?;
 
-        async fn handle_issues<T: CollectiblesSource + Copy>(source: T) -> Result<()> {
-            let issues = IssueVc::peek_issues_with_path(source).await?;
-            let issues = issues.await?;
-            let mut fatal = false;
-            for (issue, path) in issues.iter_with_shortest_path() {
-                println!("{}\n", &*issue_to_styled_string(issue, path).await?);
-                if *issue.severity().await? >= IssueSeverity::Fatal {
-                    fatal = true;
-                }
-            }
+        let project_dir = self.project_dir.context("project_dir must be set")?;
+        let entry_assets = self.entry_assets;
+        let eager_compile = self.eager_compile;
 
-            if fatal {
-                Err(anyhow!("Fatal issue(s) occurred"))
-            } else {
-                Ok(())
-            }
-        }
+        let server = DevServer::listen(
+            turbo_tasks.clone(),
+            move || {
+                source(
+                    project_dir.clone(),
+                    entry_assets.clone(),
+                    eager_compile,
+                    turbo_tasks.clone().into(),
+                )
+            },
+            (
+                self.hostname.context("hostname must be set")?,
+                self.port.context("port must be set")?,
+            )
+                .into(),
+        );
 
-        turbo_tasks
-            .clone()
-            .run_once(async move {
-                let project_dir = self.project_dir.context("project_dir must be set")?;
-                let output_disk_fs = DiskFileSystemVc::new(
-                    "output".to_string(),
-                    format!("{project_dir}/.next/server"),
-                );
-                handle_issues(output_disk_fs).await?;
-                output_disk_fs.await?.start_watching()?;
-                let output_fs = output_disk_fs.into();
-
-                let disk_fs = DiskFileSystemVc::new("project".to_string(), project_dir);
-                handle_issues(disk_fs).await?;
-                disk_fs.await?.start_watching()?;
-                let fs = disk_fs.into();
-
-                let dev_server_fs = DevServerFileSystemVc::new().as_file_system();
-                let web_source = create_web_entry_source(
-                    FileSystemPathVc::new(fs, ""),
-                    self.entry_assets
-                        .iter()
-                        .map(|a| FileSystemPathVc::new(fs, a))
-                        .collect(),
-                    dev_server_fs,
-                    self.eager_compile,
-                );
-                let rendered_source = create_server_rendered_source(
-                    FileSystemPathVc::new(fs, ""),
-                    FileSystemPathVc::new(output_fs, ""),
-                    FileSystemPathVc::new(dev_server_fs, ""),
-                );
-                let viz = turbo_tasks_viz::TurboTasksSource {
-                    turbo_tasks: turbo_tasks.clone(),
-                }
-                .cell()
-                .into();
-                let source = RouterContentSource {
-                    routes: vec![("__turbo_tasks__/".to_string(), viz)],
-                    fallback: CombinedContentSource {
-                        sources: vec![rendered_source, web_source],
-                    }
-                    .cell()
-                    .into(),
-                }
-                .cell()
-                .into();
-
-                let server = DevServerVc::new(
-                    source,
-                    TransientValue::new(
-                        (
-                            self.hostname.context("hostname must be set")?,
-                            self.port.context("port must be set")?,
-                        )
-                            .into(),
-                    ),
-                );
-
-                handle_issues(dev_server_fs).await?;
-                handle_issues(web_source).await?;
-                handle_issues(rendered_source).await?;
-                handle_issues(server).await?;
-
-                server.listen().await
-            })
-            .await
+        Ok(server)
     }
+}
+
+async fn handle_issues<T: CollectiblesSource + Copy>(source: T) -> Result<()> {
+    let issues = IssueVc::peek_issues_with_path(source).await?;
+    let issues = issues.await?;
+    let mut fatal = false;
+    for (issue, path) in issues.iter_with_shortest_path() {
+        println!("{}\n", &*issue_to_styled_string(issue, path).await?);
+        if *issue.severity().await? >= IssueSeverity::Fatal {
+            fatal = true;
+        }
+    }
+
+    if fatal {
+        Err(anyhow!("Fatal issue(s) occurred"))
+    } else {
+        Ok(())
+    }
+}
+
+#[turbo_tasks::function]
+async fn project_fs(project_dir: &str) -> Result<FileSystemVc> {
+    let disk_fs = DiskFileSystemVc::new("project".to_string(), project_dir.to_string());
+    handle_issues(disk_fs).await?;
+    disk_fs.await?.start_watching()?;
+    Ok(disk_fs.into())
+}
+
+#[turbo_tasks::function]
+async fn output_fs(project_dir: &str) -> Result<FileSystemVc> {
+    let disk_fs = DiskFileSystemVc::new(
+        "output".to_string(),
+        format!("{project_dir}{s}.next{s}server", s = MAIN_SEPARATOR),
+    );
+    handle_issues(disk_fs).await?;
+    disk_fs.await?.start_watching()?;
+    Ok(disk_fs.into())
+}
+
+#[turbo_tasks::function]
+async fn source(
+    project_dir: String,
+    entry_assets: Vec<String>,
+    eager_compile: bool,
+    turbo_tasks: TransientInstance<TurboTasks<MemoryBackend>>,
+) -> Result<ContentSourceVc> {
+    let output_fs = output_fs(&project_dir);
+    let fs = project_fs(&project_dir);
+
+    let dev_server_fs = DevServerFileSystemVc::new().as_file_system();
+    let web_source = create_web_entry_source(
+        FileSystemPathVc::new(fs, ""),
+        entry_assets
+            .iter()
+            .map(|a| FileSystemPathVc::new(fs, a))
+            .collect(),
+        dev_server_fs,
+        eager_compile,
+    );
+    let rendered_source = create_server_rendered_source(
+        FileSystemPathVc::new(fs, ""),
+        FileSystemPathVc::new(output_fs, ""),
+        FileSystemPathVc::new(dev_server_fs, ""),
+    );
+    let viz = turbo_tasks_viz::TurboTasksSource {
+        turbo_tasks: turbo_tasks.into(),
+    }
+    .cell()
+    .into();
+    let source = RouterContentSource {
+        routes: vec![("__turbo_tasks__/".to_string(), viz)],
+        fallback: CombinedContentSource {
+            sources: vec![rendered_source, web_source],
+        }
+        .cell()
+        .into(),
+    }
+    .cell()
+    .into();
+
+    handle_issues(dev_server_fs).await?;
+    handle_issues(web_source).await?;
+    handle_issues(rendered_source).await?;
+
+    Ok(source)
 }
 
 pub fn register() {
