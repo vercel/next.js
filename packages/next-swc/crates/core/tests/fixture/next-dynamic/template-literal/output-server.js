69f8c9b73c58d5b911c62dce0c788b353a086879
magic-akari <akari.ccino@gmail.com>
2022-08-16 12:15:06 +0800

    chore: update test case

--- packages/next-swc/crates/core/tests/fixture/next-dynamic/template-literal/output-server.js
+++ packages/next-swc/crates/core/tests/fixture/next-dynamic/template-literal/output-server.js
@@ -6,3 +6,6 @@ const DynamicComponent = dynamic(()=>import(`../components/hello`), {
         ]
     }
 });
+const componentRoot = '@/some-components';
+const Component1 = dynamic(()=>import(`${componentRoot}/component1`));
+const Component2 = dynamic(()=>import(`${componentRoot}/component2`));
