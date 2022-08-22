842793636cf31c9dceca5ca5f5a060c5a409bba4
Tim Neutkens <tim@timneutkens.nl>
2022-08-22 17:34:11 +0200

    Remove throw

--- test/e2e/app-dir/app/app/dashboard/client-comp.client.jsx
+++ test/e2e/app-dir/app/app/dashboard/client-comp.client.jsx
@@ -3,7 +3,6 @@ import styles from './client-comp.module.css'
 import { useEffect, useState } from 'react'
 
 export default function ClientComp() {
-  throw new Error('test')
   const [state, setState] = useState({})
   useEffect(() => {
     setState({ test: 'HELLOOOO' })
