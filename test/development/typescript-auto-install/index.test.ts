e57ba22eea23d1e618f97b2284c2527738e84bcd
JJ Kasper <jj@jjsweb.site>
2022-08-22 18:51:35 -0500

    update test

--- test/development/typescript-auto-install/index.test.ts
+++ test/development/typescript-auto-install/index.test.ts
@@ -1,6 +1,9 @@
 import { createNext } from 'e2e-utils'
 import { NextInstance } from 'test/lib/next-modes/base'
-import { renderViaHTTP } from 'next-test-utils'
+import { check, renderViaHTTP } from 'next-test-utils'
+import webdriver from 'next-webdriver'
+// @ts-expect-error missing types
+import stripAnsi from 'strip-ansi'
 
 describe('typescript-auto-install', () => {
   let next: NextInstance
@@ -22,5 +25,34 @@ describe('typescript-auto-install', () => {
   it('should work', async () => {
     const html = await renderViaHTTP(next.url, '/')
     expect(html).toContain('hello world')
+  })
+
+  it('should detect TypeScript being added and auto setup', async () => {
+    const browser = await webdriver(next.url, '/')
+    const pageContent = await next.readFile('pages/index.js')
+
+    expect(await browser.eval('document.documentElement.innerHTML')).toContain(
+      'hello world'
+    )
+
+    await next.renameFile('pages/index.js', 'pages/index.tsx')
+
+    await check(
+      () => stripAnsi(next.cliOutput),
+      /We detected TypeScript in your project and created a tsconfig\.json file for you/i
+    )
+
+    expect(await browser.eval('document.documentElement.innerHTML')).toContain(
+      'hello world'
+    )
+    await next.patchFile(
+      'pages/index.tsx',
+      pageContent.replace('hello world', 'hello again')
+    )
+
+    await check(
+      () => browser.eval('document.documentElement.innerHTML'),
+      /hello again/
+    )
   })
 })
