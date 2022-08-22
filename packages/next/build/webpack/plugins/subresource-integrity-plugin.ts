aa698afbc382c61dd37bc106c19b3f67411bc214
Wyatt Johnson <accounts+github@wyattjoh.ca>
2022-08-22 10:09:31 +0100

    fix: moved function into closure to avoid typing

--- packages/next/build/webpack/plugins/subresource-integrity-plugin.ts
+++ packages/next/build/webpack/plugins/subresource-integrity-plugin.ts
@@ -6,10 +6,6 @@ const PLUGIN_NAME = 'SubresourceIntegrityPlugin'
 
 export type SubresourceIntegrityAlgorithm = 'sha256' | 'sha384' | 'sha512'
 
-export type CompilationAssets = Parameters<
-  Parameters<webpack.Compilation['hooks']['afterOptimizeAssets']['tap']>[1]
->[0]
-
 export class SubresourceIntegrityPlugin {
   constructor(private readonly algorithm: SubresourceIntegrityAlgorithm) {}
 
@@ -20,51 +16,48 @@ export class SubresourceIntegrityPlugin {
           name: PLUGIN_NAME,
           stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
         },
-        (assets) => this.createAsset(assets, compilation)
+        (assets) => {
+          // Collect all the entrypoint files.
+          let files = new Set<string>()
+          for (const entrypoint of compilation.entrypoints.values()) {
+            const iterator = entrypoint?.getFiles()
+            if (!iterator) {
+              continue
+            }
+
+            for (const file of iterator) {
+              files.add(file)
+            }
+          }
+
+          // For each file, deduped, calculate the file hash.
+          const hashes: Record<string, string> = {}
+          for (const file of files.values()) {
+            // Get the buffer for the asset.
+            const asset = assets[file]
+            if (!asset) {
+              throw new Error(`could not get asset: ${file}`)
+            }
+
+            // Get the buffer for the asset.
+            const buffer = asset.buffer()
+
+            // Create the hash for the content.
+            const hash = crypto
+              .createHash(this.algorithm)
+              .update(buffer)
+              .digest()
+              .toString('base64')
+
+            hashes[file] = `${this.algorithm}-${hash}`
+          }
+
+          const json = JSON.stringify(hashes, null, 2)
+          assets[SUBRESOURCE_INTEGRITY_MANIFEST] = new sources.RawSource(
+            json
+          ) as any
+        }
       )
     })
-  }
-
-  private createAsset(
-    assets: CompilationAssets,
-    compilation: webpack.Compilation
-  ) {
-    // Collect all the entrypoint files.
-    let files = new Set<string>()
-    for (const entrypoint of compilation.entrypoints.values()) {
-      const iterator = entrypoint?.getFiles()
-      if (!iterator) {
-        continue
-      }
-
-      for (const file of iterator) {
-        files.add(file)
-      }
-    }
-
-    // For each file, deduped, calculate the file hash.
-    const hashes: Record<string, string> = {}
-    for (const file of files.values()) {
-      // Get the buffer for the asset.
-      const asset = assets[file]
-      if (!asset) {
-        throw new Error(`could not get asset: ${file}`)
-      }
-
-      // Get the buffer for the asset.
-      const buffer = asset.buffer()
-
-      // Create the hash for the content.
-      const hash = crypto
-        .createHash(this.algorithm)
-        .update(buffer)
-        .digest()
-        .toString('base64')
-
-      hashes[file] = `${this.algorithm}-${hash}`
-    }
-
-    const json = JSON.stringify(hashes, null, 2)
-    assets[SUBRESOURCE_INTEGRITY_MANIFEST] = new sources.RawSource(json) as any
   }
 }
