73bffd6fd77a577293ff2bf152d8724a4fa28e84
Steven <steven@ceriously.com>
2022-08-23 19:56:52 -0400

    Fix `next/future/image` blur-up placeholder  (#39785)
    This PR is a follow up to PR #39190 so that we can dynamically set the `feComponentTransfer` when we know the image doesn't have transparency (at this time its just jpeg).
    
    We also set the stdDeviation to 1 and the viewbox to the placeholder's width/height to avoid any rounding issues.
    
    Finally, we also fix the conversion from `objectPosition` to `backgroundPosition` because they have different default values according to the spec.

--- test/integration/image-future/default/test/static.test.ts
+++ test/integration/image-future/default/test/static.test.ts
@@ -9,6 +9,7 @@ import {
   launchApp,
 } from 'next-test-utils'
 import webdriver from 'next-webdriver'
+import cheerio from 'cheerio'
 import { join } from 'path'
 
 const appDir = join(__dirname, '../')
@@ -16,10 +17,11 @@ let appPort
 let app
 let browser
 let html
+let $
 
 const indexPage = new File(join(appDir, 'pages/static-img.js'))
 
-const runTests = (isDev = false) => {
+const runTests = (isDev) => {
   it('Should allow an image with a static src to omit height and width', async () => {
     expect(await browser.elementById('basic-static')).toBeTruthy()
     expect(await browser.elementById('blur-png')).toBeTruthy()
@@ -68,22 +70,51 @@ const runTests = (isDev = false) => {
     })
   }
   it('Should automatically provide an image height and width', async () => {
-    expect(html).toContain('width="400" height="300"')
+    const img = $('#basic-non-static')
+    expect(img.attr('width')).toBe('400')
+    expect(img.attr('height')).toBe('300')
   })
   it('Should allow provided width and height to override intrinsic', async () => {
-    expect(html).toContain('width="150" height="150"')
+    const img = $('#defined-size-static')
+    expect(img.attr('width')).toBe('150')
+    expect(img.attr('height')).toBe('150')
   })
 
-  it('Should add a blur to a statically imported image in "raw" mode', async () => {
-    // next-image-loader uses different blur behavior for dev/prod mode
-    // See next/build/webpack/loaders/next-image-loader
+  it('Should add a blur placeholder a statically imported jpg', async () => {
+    const style = $('#basic-static').attr('style')
     if (isDev) {
-      expect(html).toContain(
-        `style="background-size:cover;background-position:0% 0%;filter:blur(20px);background-image:url(&quot;/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest-rect.f323a148.jpg&amp;w=8&amp;q=70&quot;)"`
+      expect(style).toBe(
+        `color:transparent;background-size:cover;background-position:50% 50%;background-repeat:no-repeat;filter:blur(20px);background-image:url("/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest-rect.f323a148.jpg&w=8&q=70")`
       )
     } else {
-      expect(html).toContain(
-        `style="background-size:cover;background-position:0% 0%;background-image:url(&quot;data:image/svg+xml;charset=utf-8,%3Csvg xmlns=&#x27;http%3A//www.w3.org/2000/svg&#x27; viewBox=&#x27;0 0 400 300&#x27;%3E%3Cfilter id=&#x27;b&#x27; color-interpolation-filters=&#x27;sRGB&#x27;%3E%3CfeGaussianBlur stdDeviation=&#x27;50&#x27;/%3E%3C/filter%3E%3Cimage filter=&#x27;url(%23b)&#x27; x=&#x27;0&#x27; y=&#x27;0&#x27; height=&#x27;100%25&#x27; width=&#x27;100%25&#x27; href=&#x27;data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/sBCgoKCgoKCwwMCw8QDhAPFhQTExQWIhgaGBoYIjMgJSAgJSAzLTcsKSw3LVFAODhAUV5PSk9ecWVlcY+Ij7u7+//CABEIAAYACAMBIgACEQEDEQH/xAAnAAEBAAAAAAAAAAAAAAAAAAAABwEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEAMQAAAAmgP/xAAcEAACAQUBAAAAAAAAAAAAAAASFBMAAQMFERX/2gAIAQEAAT8AZ1HjrKZX55JysIc4Ff/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z&#x27;/%3E%3C/svg%3E&quot;)`
+      expect(style).toBe(
+        `color:transparent;background-size:cover;background-position:50% 50%;background-repeat:no-repeat;background-image:url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http%3A//www.w3.org/2000/svg' viewBox='0 0 8 6'%3E%3Cfilter id='b' color-interpolation-filters='sRGB'%3E%3CfeGaussianBlur stdDeviation='1'/%3E%3CfeComponentTransfer%3E%3CfeFuncA type='discrete' tableValues='1 1'/%3E%3C/feComponentTransfer%3E%%3C/filter%3E%3Cimage filter='url(%23b)' x='0' y='0' height='100%25' width='100%25' href='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/sBCgoKCgoKCwwMCw8QDhAPFhQTExQWIhgaGBoYIjMgJSAgJSAzLTcsKSw3LVFAODhAUV5PSk9ecWVlcY+Ij7u7+//CABEIAAYACAMBIgACEQEDEQH/xAAnAAEBAAAAAAAAAAAAAAAAAAAABwEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEAMQAAAAmgP/xAAcEAACAQUBAAAAAAAAAAAAAAASFBMAAQMFERX/2gAIAQEAAT8AZ1HjrKZX55JysIc4Ff/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQIBAT8Af//EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQMBAT8Af//Z'/%3E%3C/svg%3E")`
+      )
+    }
+  })
+
+  it('Should add a blur placeholder a statically imported png', async () => {
+    const style = $('#blur-png').attr('style')
+    if (isDev) {
+      expect(style).toBe(
+        `color:transparent;background-size:cover;background-position:50% 50%;background-repeat:no-repeat;filter:blur(20px);background-image:url("/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=8&q=70")`
+      )
+    } else {
+      expect(style).toBe(
+        `color:transparent;background-size:cover;background-position:50% 50%;background-repeat:no-repeat;background-image:url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http%3A//www.w3.org/2000/svg' viewBox='0 0 8 8'%3E%3Cfilter id='b' color-interpolation-filters='sRGB'%3E%3CfeGaussianBlur stdDeviation='1'/%3E%3C/filter%3E%3Cimage filter='url(%23b)' x='0' y='0' height='100%25' width='100%25' href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAAAAADhZOFXAAAAOklEQVR42iWGsQkAIBDE0iuIdiLOJjiGIzjiL/Meb4okiNYIlLjK3hJMzCQG1/0qmXXOUkjAV+m9wAMe3QiV6Ne8VgAAAABJRU5ErkJggg=='/%3E%3C/svg%3E")`
+      )
+    }
+  })
+
+  it('Should add a blur placeholder a statically imported png with fill', async () => {
+    const style = $('#blur-png-fill').attr('style')
+    if (isDev) {
+      expect(style).toBe(
+        `position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent;background-size:cover;background-position:50% 50%;background-repeat:no-repeat;filter:blur(20px);background-image:url("/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=8&q=70")`
+      )
+    } else {
+      expect(style).toBe(
+        `position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent;background-size:cover;background-position:50% 50%;background-repeat:no-repeat;background-image:url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http%3A//www.w3.org/2000/svg' viewBox='0 0 8 8'%3E%3Cfilter id='b' color-interpolation-filters='sRGB'%3E%3CfeGaussianBlur stdDeviation='1'/%3E%3C/filter%3E%3Cimage filter='url(%23b)' x='0' y='0' height='100%25' width='100%25' href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAAAAADhZOFXAAAAOklEQVR42iWGsQkAIBDE0iuIdiLOJjiGIzjiL/Meb4okiNYIlLjK3hJMzCQG1/0qmXXOUkjAV+m9wAMe3QiV6Ne8VgAAAABJRU5ErkJggg=='/%3E%3C/svg%3E")`
       )
     }
   })
@@ -115,6 +146,7 @@ describe('Future Static Image Component Tests', () => {
       appPort = await findPort()
       app = await nextStart(appDir, appPort)
       html = await renderViaHTTP(appPort, '/static-img')
+      $ = cheerio.load(html)
       browser = await webdriver(appPort, '/static-img')
     })
     afterAll(() => {
@@ -128,6 +160,7 @@ describe('Future Static Image Component Tests', () => {
       appPort = await findPort()
       app = await launchApp(appDir, appPort)
       html = await renderViaHTTP(appPort, '/static-img')
+      $ = cheerio.load(html)
       browser = await webdriver(appPort, '/static-img')
     })
     afterAll(() => {
