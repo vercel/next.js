1e23ba38e1b40314894b6f27fec8707b3596b4ce
LongYinan <lynweklm@gmail.com>
2022-08-24 21:44:49 +0800

    Only apply tracing on server prod builds (#270)

--- packages/turbo-tracing-next-plugin/src/index.ts
+++ packages/turbo-tracing-next-plugin/src/index.ts
@@ -4,18 +4,20 @@ import {
 } from '@vercel/webpack-node-module-trace'
 import type { NextConfig } from 'next'
 
-export function withTurboTracing(options?: NodeModuleTracePluginOptions) {
+export function turboTracing(options?: NodeModuleTracePluginOptions) {
   return function createTurboTracingConfig(config: NextConfig = {}) {
     const createWebpackConfig = config.webpack
     config.outputFileTracing = false
     config.webpack = (webpackConfig, context) => {
       const config =
         createWebpackConfig?.(webpackConfig, context) ?? webpackConfig
-      const plugin = new NodeModuleTracePlugin(options)
-      if (config.plugins) {
-        config.plugins.push(plugin)
-      } else {
-        config.plugins = [plugin]
+      if (context.isServer && !context.dev) {
+        const plugin = new NodeModuleTracePlugin(options)
+        if (config.plugins) {
+          config.plugins.push(plugin)
+        } else {
+          config.plugins = [plugin]
+        }
       }
 
       return config
