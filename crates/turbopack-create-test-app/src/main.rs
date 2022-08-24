ccde07d30325bd004408e93b880a4e8fa7d76bc4
Tobias Koppers <tobias.koppers@googlemail.com>
2022-08-24 14:09:44 +0200

    create sierpinski triangle in test app (#269)
    The test app generator creates a sierpinski triangle based on the specified
    * modules count
    * directories count
    * flatness
    
    Examples:
    `--modules 30 --directories 3`
    ![image](https://user-images.githubusercontent.com/1365881/186281117-a4708c41-a00d-4657-990d-94e41fed791e.png)
    
    `--modules 300 --directories 30`
    ![image](https://user-images.githubusercontent.com/1365881/186281172-ff0daf60-34d9-403f-ac9a-792873c23089.png)
    
    `--modules 2000 --flatness 100`
    ![image](https://user-images.githubusercontent.com/1365881/186281264-69798b4f-7b43-4ff7-8116-31e79e32f695.png)
    
    `--modules 2000 --flatness 10`
    ![image](https://user-images.githubusercontent.com/1365881/186281326-f1e7173b-e973-4846-ae17-551727244993.png)
    
    `--modules 2000 --flatness 3`
    ![image](https://user-images.githubusercontent.com/1365881/186281493-7bf1f674-d3be-4ee2-9713-deb50845e415.png)

--- crates/turbopack-create-test-app/src/main.rs
+++ crates/turbopack-create-test-app/src/main.rs
@@ -1,8 +1,39 @@
+use std::env::current_dir;
+
 use anyhow::Result;
+use clap::Parser;
 use turbopack_create_test_app::test_app_builder::TestAppBuilder;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The number of modules to generate
+    #[clap(short, long, value_parser, default_value_t = 1000)]
+    modules: usize,
+
+    /// The number of directories to generate
+    #[clap(short, long, value_parser, default_value_t = 50)]
+    directories: usize,
+
+    /// How flat should be the component tree
+    #[clap(short, long, value_parser, default_value_t = 4)]
+    flatness: usize,
+}
+
 fn main() -> Result<()> {
-    println!("{}", TestAppBuilder { module_count: 10 }.build()?.display());
+    let args = Args::parse();
+
+    println!(
+        "{}",
+        TestAppBuilder {
+            target: Some(current_dir()?),
+            module_count: args.modules,
+            directories_count: args.directories,
+            flatness: args.flatness
+        }
+        .build()?
+        .display()
+    );
 
     Ok(())
 }
--- crates/turbopack-create-test-app/src/test_app_builder.rs
+++ crates/turbopack-create-test-app/src/test_app_builder.rs
@@ -1,37 +1,151 @@
 use std::{
-    fs::{self, File},
+    collections::VecDeque,
+    fs::{create_dir_all, File},
     io::prelude::*,
     path::PathBuf,
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+
+fn decide(remaining: usize, min_remaining_decisions: usize) -> bool {
+    if remaining == 0 {
+        false
+    } else if min_remaining_decisions <= remaining {
+        true
+    } else {
+        let urgentness = min_remaining_decisions / remaining;
+        (min_remaining_decisions * 11 * 7 * 5) % urgentness == 0
+    }
+}
 
 #[derive(Default)]
 pub struct TestAppBuilder {
-    pub module_count: u32,
+    pub target: Option<PathBuf>,
+    pub module_count: usize,
+    pub directories_count: usize,
+    pub flatness: usize,
 }
 
 impl TestAppBuilder {
     pub fn build(&self) -> Result<PathBuf> {
-        let path = tempfile::tempdir()?.into_path();
+        let path = if let Some(target) = self.target.clone() {
+            target
+        } else {
+            tempfile::tempdir().context("creating tempdir")?.into_path()
+        };
         let src = path.join("src");
-        fs::create_dir(&src)?;
+        create_dir_all(&src).context("creating src dir")?;
 
-        for i in 0..self.module_count {
-            File::create(src.join(format!("mod{}.js", i)))?
-                .write_all(format!("export default {};\n", i).as_bytes())?;
+        let mut remaining_modules = self.module_count - 1;
+        let mut remaining_directories = self.directories_count;
+
+        let mut queue = VecDeque::new();
+        queue.push_back(src.join("triangle.js"));
+        remaining_modules -= 1;
+
+        while let Some(file) = queue.pop_front() {
+            let leaf = remaining_modules == 0
+                || (!queue.is_empty()
+                    && (queue.len() + remaining_modules) % (self.flatness + 1) == 0);
+            if leaf {
+                File::create(file)
+                    .context("creating file")?
+                    .write_all(
+                        r#"export default function Triangle({ style }) {
+    return <polygon points="-5,4.33 0,-4.33 5,4.33" style={style} />;
+}
+"#
+                        .as_bytes(),
+                    )
+                    .context("writing file")?;
+            } else {
+                let in_subdirectory = decide(remaining_directories, remaining_modules / 3);
+
+                let import_path;
+                let base_file = file.with_extension("");
+                let base_file = if in_subdirectory {
+                    remaining_directories -= 1;
+                    create_dir_all(&base_file).context("creating subdirectory")?;
+                    import_path = format!(
+                        "./{}/triangle_",
+                        base_file.file_name().unwrap().to_str().unwrap()
+                    );
+                    base_file.join("triangle")
+                } else {
+                    import_path =
+                        format!("./{}_", base_file.file_name().unwrap().to_str().unwrap());
+                    base_file
+                };
+
+                for i in 1..=3 {
+                    let mut f = base_file.clone();
+                    f.set_file_name(format!(
+                        "{}_{}.js",
+                        f.file_name().unwrap().to_str().unwrap(),
+                        i
+                    ));
+                    queue.push_back(f);
+                }
+                remaining_modules = remaining_modules.saturating_sub(3);
+
+                File::create(&file)
+                    .with_context(|| format!("creating file with children {}", file.display()))?
+                    .write_all(
+                        format!(
+                            r#"// Container for 3 child triangles
+import A from "{import_path}1";
+import B from "{import_path}2";
+import C from "{import_path}3";
+export default function Container({{ style }}) {{
+    return <>
+        <g transform="translate(0 -2.16)   scale(0.5 0.5)"><A style={{style}}/></g>
+        <g transform="translate(-2.5 2.16) scale(0.5 0.5)"><B style={{style}}/></g>
+        <g transform="translate(2.5 2.16)  scale(0.5 0.5)"><C style={{style}}/></g>
+    </>;
+}}
+"#
+                        )
+                        .as_bytes(),
+                    )
+                    .with_context(|| format!("writing file with children {}", file.display()))?;
+            }
         }
 
-        let mods = (0..self.module_count)
-            .map(|i| format!("mod{}", i))
-            .collect::<Vec<String>>();
+        let bootstrap = r#"import { createRoot } from "react-dom/client";
+import Triangle from "./triangle.js";
 
-        let mut entry_asset = File::create(src.join("index.js"))?;
-        for m in &mods {
-            writeln!(entry_asset, "import {} from './{}.js';", m, m)?;
-        }
+function App() {
+    return <svg height="100%" viewBox="-5 -4.33 10 8.66" style={{ }}>
+        <Triangle style={{ fill: "white" }}/>
+    </svg>
+}
 
-        writeln!(entry_asset, "console.log([{}]);", mods.join(", "))?;
+document.body.style.backgroundColor = "black";
+let root = document.createElement("main");
+document.body.appendChild(root);
+createRoot(root).render(<App />);
+"#;
+        File::create(src.join("index.js"))
+            .context("creating bootstrap file")?
+            .write_all(bootstrap.as_bytes())
+            .context("writing bootstrap file")?;
+
+        let pages = src.join("pages");
+        create_dir_all(&pages)?;
+
+        let bootstrap_page = r#"
+import Triangle from "../triangle.js";
+
+export default function Page() {
+    return <svg height="100%" viewBox="-5 -4.33 10 8.66" style={{ backgroundColor: "black" }}>
+    <Triangle style={{ fill: "white" }}/>
+    </svg>
+}
+"#;
+        File::create(pages.join("page.js"))
+            .context("creating bootstrap page")?
+            .write_all(bootstrap_page.as_bytes())
+            .context("writing bootstrap page")?;
 
         Ok(path)
     }
