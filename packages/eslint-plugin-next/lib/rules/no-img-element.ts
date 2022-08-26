63dd610fd3377a97ca0f09be7b1d5d843581b32b
Balázs Orbán <info@balazsorban.com>
2022-08-26 18:08:29 +0100

    simplify

--- packages/eslint-plugin-next/lib/rules/no-img-element.ts
+++ packages/eslint-plugin-next/lib/rules/no-img-element.ts
@@ -2,7 +2,7 @@ import type { Rule } from 'eslint'
 
 const url = 'https://nextjs.org/docs/messages/no-img-element'
 
-module.exports = {
+export default {
   meta: {
     docs: {
       description: 'Prevent usage of `<img>` element to prevent layout shift.',
