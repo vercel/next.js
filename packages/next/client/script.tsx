8e3e58fda11dfbdafce8bc5740075f9022378b19
npgfh <112116470+npgfh@users.noreply.github.com>
2022-08-28 20:56:42 +0700

    Revert "fix(#39993): avoid race condition for next/script onReady (#40002)"
    This reverts commit e6862e4061e61efa3d51d22703a6cec3b285f395.

--- packages/next/client/script.tsx
+++ packages/next/client/script.tsx
@@ -61,9 +61,6 @@ const loadScript = (props: ScriptProps): void => {
 
   const loadPromise = new Promise<void>((resolve, reject) => {
     el.addEventListener('load', function (e) {
-      // add cacheKey to LoadCache when load successfully
-      LoadCache.add(cacheKey)
-
       resolve()
       if (onLoad) {
         onLoad.call(this, e)
@@ -82,11 +79,13 @@ const loadScript = (props: ScriptProps): void => {
     }
   })
 
+  if (src) {
+    ScriptCache.set(src, loadPromise)
+  }
+  LoadCache.add(cacheKey)
+
   if (dangerouslySetInnerHTML) {
     el.innerHTML = dangerouslySetInnerHTML.__html || ''
-
-    // add cacheKey to LoadCache for inline script
-    LoadCache.add(cacheKey)
   } else if (children) {
     el.textContent =
       typeof children === 'string'
@@ -94,15 +93,8 @@ const loadScript = (props: ScriptProps): void => {
         : Array.isArray(children)
         ? children.join('')
         : ''
-
-    // add cacheKey to LoadCache for inline script
-    LoadCache.add(cacheKey)
   } else if (src) {
     el.src = src
-    // do not add cacheKey into LoadCache for remote script here
-    // cacheKey will be added to LoadCache when it is actually loaded (see loadPromise above)
-
-    ScriptCache.set(src, loadPromise)
   }
 
   for (const [k, value] of Object.entries(props)) {
