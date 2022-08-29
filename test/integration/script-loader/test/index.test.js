8a318fcf997cddaff61cb7c24b6c01141e05b20f
SukkaW <isukkaw@gmail.com>
2022-08-29 23:12:15 +0800

    test(next/script): add more cases to strict mode test

--- test/integration/script-loader/test/index.test.js
+++ test/integration/script-loader/test/index.test.js
@@ -23,78 +23,78 @@ let appPort
 const runTests = (isDev = false) => {
   // TODO: We will refactor the next/script to be strict mode resilient
   // Don't skip the test case for dev mode (strict mode) once refactoring is finished
-  if (!isDev) {
-    it('priority afterInteractive', async () => {
-      let browser
-      try {
-        browser = await webdriver(appPort, '/')
-        await waitFor(1000)
+  it('priority afterInteractive', async () => {
+    let browser
+    try {
+      browser = await webdriver(appPort, '/')
+      await waitFor(1000)
 
-        async function test(id) {
-          const script = await browser.elementById(id)
-          const dataAttr = await script.getAttribute('data-nscript')
-          const endScripts = await browser.elementsByCss(
-            `#__NEXT_DATA__ ~ #${id}`
-          )
-
-          // Renders script tag
-          expect(script).toBeDefined()
-          expect(dataAttr).toBeDefined()
-
-          // Script is inserted at the end
-          expect(endScripts.length).toBe(1)
-        }
-
-        // afterInteractive script in page
-        await test('scriptAfterInteractive')
-        // afterInteractive script in _document
-        await test('documentAfterInteractive')
-      } finally {
-        if (browser) await browser.close()
-      }
-    })
-
-    it('priority lazyOnload', async () => {
-      let browser
-      try {
-        browser = await webdriver(appPort, '/page3')
-
-        await browser.waitForElementByCss('#onload-div')
-        await waitFor(1000)
-
-        const logs = await browser.log('browser')
-        const filteredLogs = logs.filter(
-          (log) =>
-            !log.message.includes('Failed to load resource') &&
-            !log.message === 'error' &&
-            !log.message === 'Event'
+      async function test(id) {
+        const script = await browser.elementById(id)
+        const dataAttr = await script.getAttribute('data-nscript')
+        const endScripts = await browser.elementsByCss(
+          `#__NEXT_DATA__ ~ #${id}`
         )
-        expect(filteredLogs.length).toBe(0)
 
-        async function test(id) {
-          const script = await browser.elementById(id)
-          const dataAttr = await script.getAttribute('data-nscript')
-          const endScripts = await browser.elementsByCss(
-            `#__NEXT_DATA__ ~ #${id}`
-          )
+        // Renders script tag
+        expect(script).toBeDefined()
+        expect(dataAttr).toBeDefined()
 
-          // Renders script tag
-          expect(script).toBeDefined()
-          expect(dataAttr).toBeDefined()
-
-          // Script is inserted at the end
-          expect(endScripts.length).toBe(1)
-        }
-
-        // lazyOnload script in page
-        await test('scriptLazyOnload')
-        // lazyOnload script in _document
-        await test('documentLazyOnload')
-      } finally {
-        if (browser) await browser.close()
+        // Script is inserted at the end
+        expect(endScripts.length).toBe(1)
       }
-    })
 
+      // afterInteractive script in page
+      await test('scriptAfterInteractive')
+      // afterInteractive script in _document
+      await test('documentAfterInteractive')
+    } finally {
+      if (browser) await browser.close()
+    }
+  })
+
+  it('priority lazyOnload', async () => {
+    let browser
+    try {
+      browser = await webdriver(appPort, '/page3')
+
+      await browser.waitForElementByCss('#onload-div')
+      await waitFor(1000)
+
+      const logs = await browser.log('browser')
+      const filteredLogs = logs.filter(
+        (log) =>
+          !log.message.includes('Failed to load resource') &&
+          !log.message === 'error' &&
+          !log.message === 'Event'
+      )
+      expect(filteredLogs.length).toBe(0)
+
+      async function test(id) {
+        const script = await browser.elementById(id)
+        const dataAttr = await script.getAttribute('data-nscript')
+        const endScripts = await browser.elementsByCss(
+          `#__NEXT_DATA__ ~ #${id}`
+        )
+
+        // Renders script tag
+        expect(script).toBeDefined()
+        expect(dataAttr).toBeDefined()
+
+        // Script is inserted at the end
+        expect(endScripts.length).toBe(1)
+      }
+
+      // lazyOnload script in page
+      await test('scriptLazyOnload')
+      // lazyOnload script in _document
+      await test('documentLazyOnload')
+    } finally {
+      if (browser) await browser.close()
+    }
+  })
+
+  if (!isDev) {
     it('priority beforeInteractive', async () => {
       const html = await renderViaHTTP(appPort, '/page1')
       const $ = cheerio.load(html)
@@ -161,7 +161,9 @@ const runTests = (isDev = false) => {
         if (browser) await browser.close()
       }
     })
+  }
 
+  if (!isDev) {
     it('onload fires correctly', async () => {
       let browser
       try {
@@ -205,47 +207,43 @@ const runTests = (isDev = false) => {
         if (browser) await browser.close()
       }
     })
-
-    it('Does not duplicate inline scripts', async () => {
-      let browser
-      try {
-        browser = await webdriver(appPort, '/')
-
-        // Navigate away and back to page
-        await browser.waitForElementByCss('[href="/page5"]')
-        await browser.click('[href="/page5"]')
-        await browser.waitForElementByCss('[href="/"]')
-        await browser.click('[href="/"]')
-        await browser.waitForElementByCss('[href="/page5"]')
-        await browser.click('[href="/page5"]')
-
-        await browser.waitForElementByCss('.container')
-        await waitFor(1000)
-
-        const text = await browser.elementById('text').text()
-
-        expect(text).toBe('abc')
-      } finally {
-        if (browser) await browser.close()
-      }
-    })
-
-    it('Error message is shown if Partytown is not installed locally', async () => {
-      const { stdout, stderr } = await nextBuild(
-        appWithPartytownMissingDir,
-        [],
-        {
-          stdout: true,
-          stderr: true,
-        }
-      )
-      const output = stdout + stderr
-
-      expect(output.replace(/\n|\r/g, '')).toMatch(
-        /It looks like you're trying to use Partytown with next\/script but do not have the required package\(s\) installed.Please install Partytown by running:.*?(npm|pnpm|yarn) (install|add) (--save-dev|--dev) @builder.io\/partytownIf you are not trying to use Partytown, please disable the experimental "nextScriptWorkers" flag in next.config.js./
-      )
-    })
   }
+
+  it('Does not duplicate inline scripts', async () => {
+    let browser
+    try {
+      browser = await webdriver(appPort, '/')
+
+      // Navigate away and back to page
+      await browser.waitForElementByCss('[href="/page5"]')
+      await browser.click('[href="/page5"]')
+      await browser.waitForElementByCss('[href="/"]')
+      await browser.click('[href="/"]')
+      await browser.waitForElementByCss('[href="/page5"]')
+      await browser.click('[href="/page5"]')
+
+      await browser.waitForElementByCss('.container')
+      await waitFor(1000)
+
+      const text = await browser.elementById('text').text()
+
+      expect(text).toBe('abc')
+    } finally {
+      if (browser) await browser.close()
+    }
+  })
+
+  it('Error message is shown if Partytown is not installed locally', async () => {
+    const { stdout, stderr } = await nextBuild(appWithPartytownMissingDir, [], {
+      stdout: true,
+      stderr: true,
+    })
+    const output = stdout + stderr
+
+    expect(output.replace(/\n|\r/g, '')).toMatch(
+      /It looks like you're trying to use Partytown with next\/script but do not have the required package\(s\) installed.Please install Partytown by running:.*?(npm|pnpm|yarn) (install|add) (--save-dev|--dev) @builder.io\/partytownIf you are not trying to use Partytown, please disable the experimental "nextScriptWorkers" flag in next.config.js./
+    )
+  })
 
   it('onReady fires after load event and then on every subsequent re-mount', async () => {
     let browser
