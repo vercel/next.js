481950c34b9c7b2eeb051eb4dc966d0ed3bb9605
Jiachi Liu <inbox@huozhi.im>
2022-08-31 15:13:47 +0200

    Error for ssg and ssr exports from client components in build time (#40106)
    Follow up for #39953
    
    Detect invalid gSSP/gSP exports in page or layout client components in build time instead of checking them in runtime, in this way we can:
    * Error to user eariler with traced file path, help user find the incorrect usage easier
    * Make the flight client loader simpler, headless, aligned with react
    
    Co-authored-by: JJ Kasper <22380829+ijjk@users.noreply.github.com>

--- packages/next/build/webpack/loaders/next-flight-client-loader/index.ts
+++ packages/next/build/webpack/loaders/next-flight-client-loader/index.ts
@@ -5,31 +5,50 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import path from 'path'
 import { checkExports } from '../../../analysis/get-page-static-info'
 import { parse } from '../../../swc'
+
+function containsPath(parent: string, child: string) {
+  const relation = path.relative(parent, child)
+  return !!relation && !relation.startsWith('..') && !path.isAbsolute(relation)
+}
 
 export default async function transformSource(
   this: any,
   source: string
 ): Promise<string> {
-  const { resourcePath } = this
-
-  const transformedSource = source
-  if (typeof transformedSource !== 'string') {
+  if (typeof source !== 'string') {
     throw new Error('Expected source to have been transformed to a string.')
   }
 
-  const swcAST = await parse(transformedSource, {
-    filename: resourcePath,
-    isModule: 'unknown',
-  })
-  const { ssg, ssr } = checkExports(swcAST)
+  const appDir = path.join(this.rootContext, 'app')
+  const isUnderAppDir = containsPath(appDir, this.resourcePath)
+  const filename = path.basename(this.resourcePath)
+  const isPageOrLayoutFile = /^(page|layout)\.client\.\w+$/.test(filename)
+
+  const createError = (name: string) =>
+    new Error(
+      `${name} is not supported in client components.\nFrom: ${this.resourcePath}`
+    )
+
+  if (isUnderAppDir && isPageOrLayoutFile) {
+    const swcAST = await parse(source, {
+      filename: this.resourcePath,
+      isModule: 'unknown',
+    })
+    const { ssg, ssr } = checkExports(swcAST)
+    if (ssg) {
+      this.emitError(createError('getStaticProps'))
+    }
+    if (ssr) {
+      this.emitError(createError('getServerSideProps'))
+    }
+  }
 
   const output = `
 const { createProxy } = require("next/dist/build/webpack/loaders/next-flight-client-loader/module-proxy")\n
-module.exports = createProxy(${JSON.stringify(
-    resourcePath
-  )}, { ssr: ${ssr}, ssg: ${ssg} })
+module.exports = createProxy(${JSON.stringify(this.resourcePath)})
 `
   return output
 }
