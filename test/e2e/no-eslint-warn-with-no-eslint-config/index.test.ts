vercel/next.js

commit b85628392bd66e8d2d3ff86924886b33deec48da

Remove eslint warning when no eslint config is present (#39872)
We shouldn't be warning when no eslint config file is present as it's valid not to use eslint. The warning is still shown if an empty eslint config file is added as this gives intent to using eslint. 

x-ref: [slack thread](https://vercel.slack.com/archives/CGU8HUTUH/p1661268593890619?thread_ts=1661266342.496699&cid=CGU8HUTUH)

## Bug

- [x] Related issues linked using `fixes #number`
- [x] Integration tests added
- [ ] Errors have helpful link attached, see `contributing.md`

packages/next/lib/eslint/hasEslintConfiguration.ts
@@ -31,6 +31,7 @@ export async function hasEslintConfiguration(
     ) {
       return { ...configObject, emptyEslintrc: true }
     }
+    return { ...configObject, exists: true }
   } else if (packageJsonConfig?.eslintConfig) {
     if (Object.entries(packageJsonConfig?.eslintConfig).length === 0) {
       return {
@@ -38,9 +39,6 @@ export async function hasEslintConfiguration(
         emptyPkgJsonConfig: true,
       }
     }
-  } else {
-    return configObject
   }
-
-  return { ...configObject, exists: true }
+  return configObject
 }

test/e2e/no-eslint-warn-with-no-eslint-config/index.test.ts
@@ -1,0 +1,69 @@
+import { createNext } from 'e2e-utils'
+import { NextInstance } from 'test/lib/next-modes/base'
+import { renderViaHTTP } from 'next-test-utils'
+
+describe('no-eslint-warn-with-no-eslint-config', () => {
+  let next: NextInstance
+
+  if ((global as any).isNextDeploy) {
+    it('should skip for deploy', () => {})
+    return
+  }
+
+  beforeAll(async () => {
+    next = await createNext({
+      files: {
+        'pages/index.js': `
+          export default function Page() { 
+            return <p>hello world</p>
+          } 
+        `,
+      },
+      dependencies: {},
+    })
+  })
+  afterAll(() => next.destroy())
+
+  it('should render', async () => {
+    const html = await renderViaHTTP(next.url, '/')
+    expect(html).toContain('hello world')
+  })
+
+  it('should not have eslint warnings when no eslint config', async () => {
+    expect(next.cliOutput).not.toContain(
+      'No ESLint configuration detected. Run next lint to begin setup'
+    )
+    expect(next.cliOutput).not.toBe('warn')
+  })
+
+  if (!(global as any).isNextDev) {
+    it('should warn with empty eslintrc', async () => {
+      await next.stop()
+      await next.patchFile('.eslintrc.json', '{}')
+      await next.start()
+
+      expect(next.cliOutput).toContain(
+        'No ESLint configuration detected. Run next lint to begin setup'
+      )
+    })
+
+    it('should warn with empty eslint config in package.json', async () => {
+      await next.stop()
+      await next.deleteFile('.eslintrc.json')
+      const origPkgJson = await next.readFile('package.json')
+      const pkgJson = JSON.parse(origPkgJson)
+      pkgJson.eslintConfig = {}
+
+      try {
+        await next.patchFile('package.json', JSON.stringify(pkgJson))
+        await next.start()
+
+        expect(next.cliOutput).toContain(
+          'No ESLint configuration detected. Run next lint to begin setup'
+        )
+      } finally {
+        await next.patchFile('package.json', origPkgJson)
+      }
+    })
+  }
+})

packages/next/lib/eslint/runLintCheck.ts
@@ -321,8 +321,13 @@ export async function runLintCheck(
         outputFile
       )
     } else {
-      // Display warning if no ESLint configuration is present during "next build"
-      if (lintDuringBuild) {
+      // Display warning if no ESLint configuration is present inside
+      // config file during "next build", no warning is shown when
+      // no eslintrc file is present
+      if (
+        lintDuringBuild &&
+        (config.emptyPkgJsonConfig || config.emptyEslintrc)
+      ) {
         Log.warn(
           `No ESLint configuration detected. Run ${chalk.bold.cyan(
             'next lint'

commit 2de7b43b7819b4148f019ec560651e54e9ba562c

fix: detect ESLint config in package.json (#40158)
Fixes #40133

Fixes a small regression introduced in #39872. We should be able to detect if a non-empty `package.json#eslintConfig` property is present.

## Bug

- [ ] Related issues linked using `fixes #number`
- [ ] Integration tests added
- [ ] Errors have helpful link attached, see `contributing.md`

## Feature

- [ ] Implements an existing feature request or RFC. Make sure the feature request has been accepted for implementation before opening a PR.
- [ ] Related issues linked using `fixes #number`
- [ ] Integration tests added
- [ ] Documentation added
- [ ] Telemetry added. In case of a feature if it's used or not.
- [ ] Errors have helpful link attached, see `contributing.md`

## Documentation / Examples

- [ ] Make sure the linting passes by running `pnpm lint`
- [ ] The examples guidelines are followed from [our contributing doc](https://github.com/vercel/next.js/blob/canary/contributing.md#adding-examples)

packages/next/lib/eslint/hasEslintConfiguration.ts
@@ -33,12 +33,10 @@ export async function hasEslintConfiguration(
     }
     return { ...configObject, exists: true }
   } else if (packageJsonConfig?.eslintConfig) {
-    if (Object.entries(packageJsonConfig?.eslintConfig).length === 0) {
-      return {
-        ...configObject,
-        emptyPkgJsonConfig: true,
-      }
+    if (Object.keys(packageJsonConfig?.eslintConfig).length) {
+      return { ...configObject, exists: true }
     }
+    return { ...configObject, emptyPkgJsonConfig: true }
   }
   return configObject
 }

test/e2e/no-eslint-warn-with-no-eslint-config/index.test.ts
@@ -65,5 +65,23 @@ describe('no-eslint-warn-with-no-eslint-config', () => {
         await next.patchFile('package.json', origPkgJson)
       }
     })
+
+    it('should not warn with eslint config in package.json', async () => {
+      await next.stop()
+      const origPkgJson = await next.readFile('package.json')
+      const pkgJson = JSON.parse(origPkgJson)
+      pkgJson.eslintConfig = { rules: { semi: 'off' } }
+
+      try {
+        await next.patchFile('package.json', JSON.stringify(pkgJson))
+        await next.start()
+
+        expect(next.cliOutput).not.toContain(
+          'No ESLint configuration detected. Run next lint to begin setup'
+        )
+      } finally {
+        await next.patchFile('package.json', origPkgJson)
+      }
+    })
   }
 })