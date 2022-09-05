9bf0d600ea6a73f200d3449fe1d790a77c51defb
Hannes Bornö <hannes.borno@vercel.com>
2022-09-05 10:27:35 +0200

    add next-font-loaders to core

--- packages/next-swc/crates/core/src/lib.rs
+++ packages/next-swc/crates/core/src/lib.rs
@@ -51,6 +51,7 @@ mod auto_cjs;
 pub mod disallow_re_export_all_in_page;
 pub mod hook_optimizer;
 pub mod next_dynamic;
+pub mod next_font_loaders;
 pub mod next_ssg;
 pub mod page_config;
 pub mod react_remove_properties;
@@ -105,6 +106,9 @@ pub struct TransformOptions {
 
     #[serde(default)]
     pub modularize_imports: Option<modularize_imports::Config>,
+
+    #[serde(default)]
+    pub font_loaders: Option<Vec<String>>,
 }
 
 pub fn custom_before_pass<'a, C: Comments + 'a>(
@@ -194,6 +198,11 @@ pub fn custom_before_pass<'a, C: Comments + 'a>(
             .unwrap_or_else(|| Either::Right(noop())),
         match &opts.modularize_imports {
             Some(config) => Either::Left(modularize_imports::modularize_imports(config.clone())),
+            None => Either::Right(noop()),
+        },
+        match &opts.font_loaders {
+            Some(font_loaders) =>
+                Either::Left(next_font_loaders::next_font_loaders(font_loaders.clone())),
             None => Either::Right(noop()),
         }
     )
