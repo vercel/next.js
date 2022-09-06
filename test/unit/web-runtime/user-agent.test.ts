cc4d43cc4696d1e16d4cd3dcc9fb858de2cc6470
Seiya Nuta <nuta@seiya.me>
2022-09-06 15:47:52 +0900

    [web-runtime] Add a test case for the user agent parser

--- test/unit/web-runtime/user-agent.test.ts
+++ test/unit/web-runtime/user-agent.test.ts
@@ -6,12 +6,12 @@ import { userAgentFromString, userAgent, NextRequest } from 'next/server'
 
 const UA_STRING =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
+const MOBULE_UA_STRING =
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
 
 it('parse an user agent', () => {
   const parser = userAgentFromString(UA_STRING)
-  expect(parser.ua).toBe(
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
-  )
+  expect(parser.ua).toBe(UA_STRING)
   expect(parser.browser).toStrictEqual({
     name: 'Chrome',
     version: '89.0.4389.90',
@@ -23,6 +23,28 @@ it('parse an user agent', () => {
   })
   expect(parser.os).toStrictEqual({ name: 'Mac OS', version: '11.2.3' })
   expect(parser.cpu).toStrictEqual({ architecture: undefined })
+  expect(parser.isBot).toBe(false)
+})
+
+it('parse an user agent (mobile)', () => {
+  const parser = userAgentFromString(MOBULE_UA_STRING)
+  expect(parser.ua).toBe(MOBULE_UA_STRING)
+  expect(parser.browser).toStrictEqual({
+    major: '14',
+    name: 'Mobile Safari',
+    version: '14.0',
+  })
+  expect(parser.engine).toStrictEqual({
+    name: 'WebKit',
+    version: '605.1.15',
+  })
+  expect(parser.os).toStrictEqual({ name: 'iOS', version: '14.0' })
+  expect(parser.cpu).toStrictEqual({ architecture: undefined })
+  expect(parser.device).toStrictEqual({
+    model: 'iPhone',
+    type: 'mobile',
+    vendor: 'Apple',
+  })
   expect(parser.isBot).toBe(false)
 })
 
