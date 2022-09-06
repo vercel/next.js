29968552b07a5a0d4279e3c069e93270fc099ce1
Justin Ridgewell <justin@ridgewell.name>
2022-09-06 13:50:11 -0400

    Switch to ESM exports

--- packages/eslint-plugin-next/lib/rules/no-img-element.ts
+++ packages/eslint-plugin-next/lib/rules/no-img-element.ts
@@ -2,39 +2,36 @@ import type { Rule } from 'eslint'
 
 const url = 'https://nextjs.org/docs/messages/no-img-element'
 
-const rule: Rule.RuleModule = {
-  meta: {
-    docs: {
-      description: 'Prevent usage of `<img>` element to prevent layout shift.',
-      category: 'HTML',
-      recommended: true,
-      url,
-    },
-    type: 'problem',
-    schema: [],
+export const meta: Rule.RuleModule['meta'] = {
+  docs: {
+    description: 'Prevent usage of `<img>` element to prevent layout shift.',
+    category: 'HTML',
+    recommended: true,
+    url,
   },
-  create(context) {
-    return {
-      JSXOpeningElement(node) {
-        if (node.name.name !== 'img') {
-          return
-        }
-
-        if (node.attributes.length === 0) {
-          return
-        }
-
-        if (node.parent?.parent?.openingElement?.name?.name === 'picture') {
-          return
-        }
-
-        context.report({
-          node,
-          message: `Do not use \`<img>\` element. Use \`<Image />\` from \`next/image\` instead. See: ${url}`,
-        })
-      },
-    }
-  },
+  type: 'problem',
+  schema: [],
 }
 
-module.exports = rule
+export const create: Rule.RuleModule['create'] = (context) => {
+  return {
+    JSXOpeningElement(node) {
+      if (node.name.name !== 'img') {
+        return
+      }
+
+      if (node.attributes.length === 0) {
+        return
+      }
+
+      if (node.parent?.parent?.openingElement?.name?.name === 'picture') {
+        return
+      }
+
+      context.report({
+        node,
+        message: `Do not use \`<img>\` element. Use \`<Image />\` from \`next/image\` instead. See: ${url}`,
+      })
+    },
+  }
+}
