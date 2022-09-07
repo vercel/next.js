fbf1c97eac7769be7a6fc038217176298bcfd1ee
Ben Heidemann <56122437+bcheidemann@users.noreply.github.com>
2022-09-07 04:14:08 +0100

    Add experimental proxy timeout option (#40289)
    <!--
    Thanks for opening a PR! Your contribution is much appreciated.
    In order to make sure your PR is handled as smoothly as possible we
    request that you follow the checklist sections below.
    Choose the right checklist for the change that you're making:
    -->
    
    ## Bug
    
    - [X] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Errors have helpful link attached, see `contributing.md`
    
    Fixes #36251

--- packages/next/server/next-server.ts
+++ packages/next/server/next-server.ts
@@ -697,7 +697,10 @@ export default class NextNodeServer extends BaseServer {
       ws: true,
       // we limit proxy requests to 30s by default, in development
       // we don't time out WebSocket requests to allow proxying
-      proxyTimeout: upgradeHead && this.renderOpts.dev ? undefined : 30_000,
+      proxyTimeout:
+        upgradeHead && this.renderOpts.dev
+          ? undefined
+          : this.nextConfig.experimental.proxyTimeout || 30_000,
     })
 
     await new Promise((proxyResolve, proxyReject) => {
