28a09b8be0c989e44a47c9db037e8d1c2e99f103
Ethan Arrowood <ethan.arrowood@vercel.com>
2022-09-07 11:16:59 -0600

    move polyfill to inside !global.fetch

--- packages/next/server/node-polyfill-fetch.js
+++ packages/next/server/node-polyfill-fetch.js
@@ -1,21 +1,21 @@
-let fetch, Headers, Request, Response
-
-if (process.env.NEXT_USE_UNDICI) {
-  const undici = require('next/dist/compiled/undici')
-  fetch = undici.fetch
-  Headers = undici.Headers
-  Request = undici.Request
-  Response = undici.Response
-} else {
-  const nodeFetch = require('next/dist/compiled/node-fetch')
-  fetch = nodeFetch.fetch
-  Headers = nodeFetch.Headers
-  Request = nodeFetch.Request
-  Response = nodeFetch.Response
-}
-
 // Polyfill fetch() in the Node.js environment
 if (!global.fetch) {
+  let fetch, Headers, Request, Response
+
+  if (process.env.NEXT_USE_UNDICI) {
+    const undici = require('next/dist/compiled/undici')
+    fetch = undici.fetch
+    Headers = undici.Headers
+    Request = undici.Request
+    Response = undici.Response
+  } else {
+    const nodeFetch = require('next/dist/compiled/node-fetch')
+    fetch = nodeFetch.fetch
+    Headers = nodeFetch.Headers
+    Request = nodeFetch.Request
+    Response = nodeFetch.Response
+  }
+
   const agent = ({ protocol }) =>
     protocol === 'http:' ? global.__NEXT_HTTP_AGENT : global.__NEXT_HTTPS_AGENT
   const fetchWithAgent = (url, opts, ...rest) => {
