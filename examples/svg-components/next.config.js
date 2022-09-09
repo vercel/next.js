b85f1c99008430e4c521afaad4976f0e4c3e0d4d
Alexander Shipulin <sania8600@gmail.com>
2022-09-09 12:12:46 +0700

    fix(svg-components): add custom config for svgr

--- examples/svg-components/next.config.js
+++ examples/svg-components/next.config.js
@@ -5,7 +5,27 @@ module.exports = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      // use this if you don't need custom options.
+      // use: ['@svgr/webpack'],
+      // docs options @svgr - https://react-svgr.com/docs/options
+      use: {
+        loader: "@svgr/webpack",
+        options: {
+          dimensions: false,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "preset-default",
+                params: {
+                  overrides: {
+                    removeViewBox: false,
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
     })
 
     return config
