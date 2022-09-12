90c284fc6452e4b423fca3f7000304d19d8c1037
Tim Neutkens <tim@timneutkens.nl>
2022-09-12 12:10:49 +0200

    Remove ts from test

--- test/e2e/app-dir/app/app/loading-bug/[categorySlug]/page.server.js
+++ test/e2e/app-dir/app/app/loading-bug/[categorySlug]/page.server.js
@@ -1,18 +1,14 @@
 // @ts-ignore
 import { experimental_use as use } from 'react'
 
-const fetchCategory = async (categorySlug: string): Promise<{}> => {
+const fetchCategory = async (categorySlug) => {
   // artificial delay
   await new Promise((resolve) => setTimeout(resolve, 3000))
 
   return categorySlug + 'abc'
 }
 
-export default function Page({
-  params,
-}: {
-  params: { [key: string]: string }
-}) {
+export default function Page({ params }) {
   const category = use(fetchCategory(params.categorySlug))
 
   return <>{category}</>
