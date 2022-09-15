a11d11749125f5e39d69a2221ccf65adeec4f8c1
Balázs Orbán <info@balazsorban.com>
2022-09-15 19:04:12 +0200

    require fetch implementation conditionally

--- packages/next/server/node-polyfill-fetch.js
+++ packages/next/server/node-polyfill-fetch.js
@@ -1,40 +1,46 @@
 // Polyfill fetch() in the Node.js environment
-if (!global.fetch) {
-  // Due to limitation of global configuartion. we have to do this resolution at runtime
-  const nodeFetch = require('next/dist/compiled/node-fetch')
-  const undici = require('next/dist/compiled/undici')
-  global.fetch = (...args) => {
-    if (global.__NEXT_USE_UNDICI) {
-      return undici.fetch(...args)
-    } else {
-      const agent = ({ protocol }) =>
-        protocol === 'http:'
-          ? global.__NEXT_HTTP_AGENT
-          : global.__NEXT_HTTPS_AGENT
 
-      if (!args[1]) {
-        args[1] = { agent }
-      } else if (!args[1].agent) {
-        args[1].agent = agent
-      }
-      return nodeFetch(...args)
+if (!global.fetch) {
+  function getFetchImpl() {
+    return global.__NEXT_USE_UNDICI
+      ? require('next/dist/compiled/undici')
+      : require('next/dist/compiled/node-fetch')
+  }
+  // Due to limitation of global configuartion, we have to do this resolution at runtime
+  global.fetch = (...args) => {
+    const fetchImpl = getFetchImpl()
+
+    if (global.__NEXT_USE_UNDICI) {
+      return fetchImpl.fetch(...args)
     }
+    const agent = ({ protocol }) =>
+      protocol === 'http:'
+        ? global.__NEXT_HTTP_AGENT
+        : global.__NEXT_HTTPS_AGENT
+
+    if (!args[1]) {
+      args[1] = { agent }
+    } else if (!args[1].agent) {
+      args[1].agent = agent
+    }
+
+    return fetchImpl(...args)
   }
 
   Object.defineProperties(global, {
     Headers: {
       get() {
-        return global.__NEXT_USE_UNDICI ? undici.Headers : nodeFetch.Headers
+        return getFetchImpl().Headers
       },
     },
     Request: {
       get() {
-        return global.__NEXT_USE_UNDICI ? undici.Request : nodeFetch.Request
+        return getFetchImpl().Request
       },
     },
     Response: {
       get() {
-        return global.__NEXT_USE_UNDICI ? undici.Response : nodeFetch.Response
+        return getFetchImpl().Response
       },
     },
   })
