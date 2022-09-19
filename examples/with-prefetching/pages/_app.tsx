24b20dd13a52ecf602d7ddba5ce01ffba0e8f634
Henrik Wenz <HaNdTriX@users.noreply.github.com>
2022-09-19 20:15:54 +0200

    chore: Migrate with-prefetching example to typescript (#40671)
    ## Documentation / Examples
    
    - [x] Make sure the linting passes by running `pnpm lint`
    - [x] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- examples/with-prefetching/pages/_app.tsx
+++ examples/with-prefetching/pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from 'next/app'
 import Nav from '../components/Nav'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Nav />
