99a1bbbef131c75edbc4de58446e42d94fcd88b2
Tim Neutkens <tim@timneutkens.nl>
2022-09-19 19:52:47 +0200

    Leverage use to get router instance

--- packages/next/client/components/redirect-client.ts
+++ packages/next/client/components/redirect-client.ts
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { experimental_use as use } from 'react'
+import { AppRouterContext } from '../../shared/lib/app-router-context'
 import { createInfinitePromise } from './infinite-promise'
 
 export function redirect(url: string) {
+  const router = use(AppRouterContext)
   setTimeout(() => {
     // @ts-ignore startTransition exists
     React.startTransition(() => {
-      // TODO-APP: handle redirect
-      // @ts-expect-error TODO:-APP: fix this
-      window.nd.router.replace(url, {})
+      router.replace(url, {})
     })
   })
   // setTimeout is used to start a new transition during render, this is an intentional hack around React.
