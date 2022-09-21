d85f7b10775e3db67a2170f6e6fa956a6e6e8724
Hannes Bornö <hannes.borno@vercel.com>
2022-09-21 14:30:29 +0200

    temp add swc plugin

---
+++ packages/next-swc/crates/core/tests/errors/next-font-loaders/options-object/output.js
@@ -0,0 +1,18 @@
+import a from "@next/font/google?ABeeZee;{}";
+import a from "@next/font/google?ABeeZee;{}";
+import a from "@next/font/google?ABeeZee;{}";
+import a from "@next/font/google?ABeeZee;{}";
+const a = fn({
+    10: 'hello'
+});
+const a = fn({
+    variant: [
+        i1
+    ]
+});
+const a = fn({
+    variant: ()=>{}
+});
+const a = fn({
+    ...{}
+});
