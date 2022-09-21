9f4f412cf05f407580d46d9e1578c711ae4da08b
Jiachi Liu <inbox@huozhi.im>
2022-09-22 01:43:57 +0200

    update config files

--- test/.stats-app/stats-config.js
+++ test/.stats-app/stats-config.js
@@ -32,12 +32,11 @@ const clientGlobs = [
     globs: ['fetched-pages/**/*.html'],
   },
   {
-    name: 'Pages edge SSR bundle Size',
-    globs: ['.next/server/pages/edge-ssr.js'],
-  },
-  {
-    name: 'App edge SSR bundle Size',
-    globs: ['.next/server/app/app-edge-ssr/page.js'],
+    name: 'Edge SSR bundle Size',
+    globs: [
+      '.next/server/pages/edge-ssr.js',
+      '.next/server/app/app-edge-ssr/page.js',
+    ],
   },
   {
     name: 'Middleware size',
@@ -93,6 +92,9 @@ module.exports = {
           path: 'next.config.js',
           content: `
             module.exports = {
+              experimental: {
+                appDir: true,
+              },
               generateBuildId: () => 'BUILD_ID',
               webpack(config) {
                 config.optimization.minimize = false
@@ -113,7 +115,10 @@ module.exports = {
         {
           path: 'next.config.js',
           content: `
-            module.exports = {
+          module.exports = {
+              experimental: {
+                appDir: true,
+              },
               generateBuildId: () => 'BUILD_ID'
             }
           `,
@@ -148,6 +153,9 @@ module.exports = {
           path: 'next.config.js',
           content: `
             module.exports = {
+              experimental: {
+                appDir: true,
+              },
               generateBuildId: () => 'BUILD_ID',
               swcMinify: true,
               webpack(config) {
@@ -170,6 +178,9 @@ module.exports = {
           path: 'next.config.js',
           content: `
             module.exports = {
+              experimental: {
+                appDir: true,
+              },
               swcMinify: true,
               generateBuildId: () => 'BUILD_ID'
             }
