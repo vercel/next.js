04b51d8801d4f4613d29b5b60659543b424ab773
Henrik Wenz <henrik.wenz@gmail.com>
2022-09-21 10:08:25 +0200

    Use function based Document

--- examples/with-typestyle/pages/_document.js
+++ examples/with-typestyle/pages/_document.js
@@ -1,24 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 import { getStyles } from 'typestyle'
 
-export default class MyDocument extends Document {
-  static async getInitialProps({ renderPage }) {
-    const page = await renderPage()
-    const styleTags = getStyles()
-    return { ...page, styleTags }
-  }
+export default function Document({ styleTags }) {
+  return (
+    <Html>
+      <Head>
+        <style id="styles-target">{styleTags}</style>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <style id="styles-target">{this.props.styleTags}</style>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+Document.getInitialProps = async ({ renderPage }) => {
+  const page = await renderPage()
+  const styleTags = getStyles()
+  return { ...page, styleTags }
 }
