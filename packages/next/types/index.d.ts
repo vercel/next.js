3e87013a33325b36c26d500e3c85c018f5e29d3f
Hannes Bornö <hannes.borno@vercel.com>
2022-09-22 14:36:54 +0200

    Move font loader types to next

--- packages/next/types/index.d.ts
+++ packages/next/types/index.d.ts
@@ -182,6 +182,21 @@ export type InferGetServerSidePropsType<T extends (args: any) => any> = Awaited<
   Extract<Awaited<ReturnType<T>>, { props: any }>['props']
 >
 
+export type FontModule = {
+  className: string
+  variable: string
+  style: { fontFamily: string; fontWeight?: number; fontStyle?: string }
+}
+
+export type FontLoader = (options: {
+  functionName: string
+  data: any[]
+  config: any
+  emitFontFile: (content: Buffer, ext: string, preload: boolean) => string
+  resolve: (src: string) => string
+  fs: any
+}) => Promise<{ css: string; fallbackFonts?: string[] }>
+
 declare global {
   interface Crypto {
     readonly subtle: SubtleCrypto
