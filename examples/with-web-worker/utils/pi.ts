eef8271d819235ca1d597da8c1d8177debc69019
Henrik Wenz <henrik.wenz@gmail.com>
2022-09-23 17:57:14 +0200

    chore: refactor with-web-worker example

--- examples/with-web-worker/utils/pi.ts
+++ examples/with-web-worker/utils/pi.ts
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/39575124
-export default function pi(n) {
-  var v = 0
+export default function pi(n: number) {
+  let v = 0
   for (let i = 1; i <= n; i += 4) {
     // increment by 4
     v += 1 / i - 1 / (i + 2) // add the value of the series
