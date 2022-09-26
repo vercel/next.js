2af6b63dd1b54f5ecfc93a50389a9cb1b709ef0b
Jiachi Liu <inbox@huozhi.im>
2022-09-27 00:46:01 +0200

    Should resolve esm external module imports on server (#40865)
    ### Issue
    
    When import an esm package in client component, and use it in server
    component page, it will fail to SSR but render successfully on client.
    It's because the import to esm package will make the client chunk become
    an **async module** since esm module will be treated as **async**.
    
    ```
    page (serve component) -> local module (client) -> external dependency (esm)
    ```
    
    Then in react SSR layer, it need the module type information of that
    chunk, async or not for react so that react could unwrap the async
    module from `Promise` properly when SSR.
    
    ### Solution
    
    We need to mark the client entries which are effected by async/esm
    modules that becoming **async** as `async: true` in SSR manifest.
    
    Since flight manifest plugin is only running against client compiler,
    which doesn't have those module information from server compiler. So we
    collect the async modules from the **server** compiler **client** layer
    from flight entry client plugin, then leverage the collection to detect
    if a module is async in flight manifest plugin for react.
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [x] Integration tests added
    - [ ] Errors have a helpful link attached, see `contributing.md`

--- test/e2e/app-dir/rsc-basic/app/page.js
+++ test/e2e/app-dir/rsc-basic/app/page.js
@@ -1,10 +1,11 @@
 import Nav from '../components/nav'
 import { headers } from 'next/dist/client/components/hooks-server'
+import { name } from 'random-module-instance'
 
 const envVar = process.env.ENV_VAR_TEST
 const headerKey = 'x-next-test-client'
 
-export default function Index(props) {
+export default function Index() {
   const headersList = headers()
   const header = headersList.get(headerKey)
 
@@ -13,6 +14,7 @@ export default function Index(props) {
       <h1>{`component:index.server`}</h1>
       <div>{'env:' + envVar}</div>
       <div>{'header:' + header}</div>
+      <p>{name}</p>
       <Nav />
     </div>
   )
