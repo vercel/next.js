563c4db90c3b2ea63c52b1ebc82efd1d80860d76
Bruno Crosier <bruno.crosier@gmail.com>
2022-09-26 23:16:47 +0100

    add valid fixtures for `numeric-string-types`

--- test/integration/image-future/typescript/pages/valid.tsx
+++ test/integration/image-future/typescript/pages/valid.tsx
@@ -47,6 +47,14 @@ const Page = () => {
         height={500}
       />
       <Image
+        id="numeric-string-types"
+        alt="numeric-string-types"
+        src="https://image-optimization-test.vercel.app/test.jpg"
+        quality="80"
+        width="500"
+        height="500"
+      />
+      <Image
         id="data-protocol"
         alt="data-protocol"
         src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
