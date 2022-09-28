df6595441371f0146ba101a7ed06c12bc8735e3f
Steven <steven@ceriously.com>
2022-09-28 18:38:27 -0400

    Add support for dynamic import expressions

--- packages/next-codemod/transforms/next-image-to-legacy-image.ts
+++ packages/next-codemod/transforms/next-image-to-legacy-image.ts
@@ -8,33 +8,58 @@ export default function transformer(
   const j = api.jscodeshift
   const root = j(file.source)
 
-  // Rename each import from "next/image" to "next/legacy/image"
-  const imageImports = root.find(j.ImportDeclaration, {
-    source: { value: 'next/image' },
-  })
-
-  imageImports.forEach((imageImport) => {
-    j(imageImport).replaceWith(
-      j.importDeclaration(
-        imageImport.node.specifiers,
-        j.stringLiteral('next/legacy/image')
+  // Before: import Image from "next/image"
+  //  After: import Image from "next/legacy/image"
+  root
+    .find(j.ImportDeclaration, {
+      source: { value: 'next/image' },
+    })
+    .forEach((imageImport) => {
+      j(imageImport).replaceWith(
+        j.importDeclaration(
+          imageImport.node.specifiers,
+          j.stringLiteral('next/legacy/image')
+        )
       )
-    )
-  })
-
-  // Rename each import from "next/future/image" to "next/image"
-  const imageFutureImports = root.find(j.ImportDeclaration, {
-    source: { value: 'next/future/image' },
-  })
-
-  imageFutureImports.forEach((imageFutureImport) => {
-    j(imageFutureImport).replaceWith(
-      j.importDeclaration(
-        imageFutureImport.node.specifiers,
-        j.stringLiteral('next/image')
+    })
+  // Before: const Image = await import("next/image")
+  //  After: const Image = await import("next/legacy/image")
+  root
+    .find(j.ImportExpression, {
+      source: { value: 'next/image' },
+    })
+    .forEach((imageImport) => {
+      j(imageImport).replaceWith(
+        j.importExpression(j.stringLiteral('next/legacy/image'))
       )
-    )
-  })
+    })
+
+  // Before: import Image from "next/future/image"
+  //  After: import Image from "next/image"
+  root
+    .find(j.ImportDeclaration, {
+      source: { value: 'next/future/image' },
+    })
+    .forEach((imageFutureImport) => {
+      j(imageFutureImport).replaceWith(
+        j.importDeclaration(
+          imageFutureImport.node.specifiers,
+          j.stringLiteral('next/image')
+        )
+      )
+    })
+
+  // Before: const Image = await import("next/future/image")
+  //  After: const Image = await import("next/image")
+  root
+    .find(j.ImportExpression, {
+      source: { value: 'next/future/image' },
+    })
+    .forEach((imageFutureImport) => {
+      j(imageFutureImport).replaceWith(
+        j.importExpression(j.stringLiteral('next/image'))
+      )
+    })
 
   // Learn more about renaming an import declaration here:
   // https://www.codeshiftcommunity.com/docs/import-manipulation/#replacerename-an-import-declaration
