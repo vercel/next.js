e6f899c858011a7e988e1c869d8fa568fea1a08d
Rafael <rjvelazco21@gmail.com>
2022-09-28 11:14:40 -0400

    clean up

--- examples/cms-dotcms/components/date.tsx
+++ examples/cms-dotcms/components/date.tsx
@@ -3,7 +3,7 @@ import { format } from 'date-fns'
 export default function DateComponent({ dateString }) {
   return (
     <time dateTime={dateString}>
-      {format(new Date(dateString), 'LLLL	d, yyyy')}
+      {format(new Date(dateString), 'LLLL d, yyyy')}
     </time>
   )
 }
