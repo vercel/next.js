4b7900a7b3997addd45b605b9cbb5d465ef9369b
Miguel Oller <migueloller@users.noreply.github.com>
2022-09-28 14:33:08 -0400

    Update cms-makeswift example (#41005)
    Makeswift now uses Next.js' Preview Mode so there's no need for a
    preview route. It also supports automatic on-demand revalidation with
    the introduction of the Makeswift API handler.
    
    ## Documentation / Examples
    
    - [x] Make sure the linting passes by running `pnpm lint`
    - [x] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- examples/cms-makeswift/next.config.js
+++ examples/cms-makeswift/next.config.js
@@ -3,6 +3,7 @@ const withMakeswift = require('@makeswift/runtime/next/plugin')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = withMakeswift(nextConfig)
