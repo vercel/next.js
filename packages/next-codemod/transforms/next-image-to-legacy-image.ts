16f1abb36d7d85f6ae0c5651bb9b2c916ee5cf03
Steven <steven@ceriously.com>
2022-09-28 17:39:57 -0400

    Make it work both ways

--- packages/next-codemod/transforms/next-image-to-legacy-image.ts
+++ packages/next-codemod/transforms/next-image-to-legacy-image.ts
@@ -8,20 +8,36 @@ export default function transformer(
   const j = api.jscodeshift
   const root = j(file.source)
 
+  // Rename each import from "next/image" to "next/legacy/image"
   const imageImports = root.find(j.ImportDeclaration, {
     source: { value: 'next/image' },
   })
 
   imageImports.forEach((imageImport) => {
-    //console.log(imageImport.node.source)
-    //console.log(j.stringLiteral('next/legacy/image'))
     j(imageImport).replaceWith(
       j.importDeclaration(
-        imageImport.node.specifiers, // copy over the existing import specificers
-        j.stringLiteral('next/legacy/image') // Replace with our new source
+        imageImport.node.specifiers,
+        j.stringLiteral('next/legacy/image')
       )
     )
   })
+
+  // Rename each import from "next/future/image" to "next/image"
+  const imageFutureImports = root.find(j.ImportDeclaration, {
+    source: { value: 'next/future/image' },
+  })
+
+  imageFutureImports.forEach((imageFutureImport) => {
+    j(imageFutureImport).replaceWith(
+      j.importDeclaration(
+        imageFutureImport.node.specifiers,
+        j.stringLiteral('next/image')
+      )
+    )
+  })
+
+  // Learn more about renaming an import declaration here:
+  // https://www.codeshiftcommunity.com/docs/import-manipulation/#replacerename-an-import-declaration
 
   return root.toSource(options)
 }
