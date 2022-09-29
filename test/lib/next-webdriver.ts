9810cd7e7811be3503015df711a4df76c49da8e8
Jaril <jarilvalenciano@gmail.com>
2022-09-29 07:52:49 -0700

    Only record with replay on failing tests

--- test/lib/next-webdriver.ts
+++ test/lib/next-webdriver.ts
@@ -84,7 +84,7 @@ export default async function webdriver(
     const { Selenium, quit } = await import('./browsers/selenium')
     CurrentInterface = Selenium
     browserQuit = quit
-  } else if (process.env.RECORD_REPLAY) {
+  } else if (process.env.RECORD_REPLAY === 'true') {
     const { Replay, quit } = await require('./browsers/replay')
     CurrentInterface = Replay
     browserQuit = quit
