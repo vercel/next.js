41f6c12a8113e0e412db356637160ede96d0c0eb
Balázs Orbán <info@balazsorban.com>
2022-10-01 07:12:41 +0200

    chore(examples): fix lint

--- examples/authsignal/passwordless-login/pages/api/login.ts
+++ examples/authsignal/passwordless-login/pages/api/login.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { authsignal } from '../../lib'
 
+const redirectUrl =
+  process.env.REDIRECT_URL ?? 'http://localhost:3000/api/finalize-login'
+
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).send({ message: 'Only POST requests allowed' })
@@ -12,6 +15,3 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
 
   res.redirect(303, url)
 }
-
-const redirectUrl =
-  process.env.REDIRECT_URL ?? 'http://localhost:3000/api/finalize-login'
