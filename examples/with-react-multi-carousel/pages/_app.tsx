8112c032bb1ae0e890d1e8461da918394338a1d3
Balázs Orbán <info@balazsorban.com>
2022-10-01 15:09:31 +0200

    Delete _app.tsx

--- examples/with-react-multi-carousel/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
