e34d47dd011699edcc8bbd982b89936f497c2763
Ben Read <endymion1818@gmail.com>
2022-10-05 15:39:07 +0100

    chore(examples): add webiny cms example (#41159)
    ## Documentation / Examples
    
    - [X] Make sure the linting passes by running `pnpm lint`
    - [X] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

---
+++ examples/cms-webiny/components/post-title.tsx
@@ -0,0 +1,7 @@
+export default function PostTitle({ children }) {
+  return (
+    <h1 className="mb-12 text-6xl font-bold leading-tight tracking-tighter text-center md:text-7xl lg:text-8xl md:leading-none md:text-left">
+      {children}
+    </h1>
+  )
+}
