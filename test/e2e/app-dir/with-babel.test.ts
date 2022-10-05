328a47cb74d8c1340a72e9e003a68d1b84c3c786
JJ Kasper <jj@jjsweb.site>
2022-10-05 12:42:37 -0700

    unpin experimental react

--- test/e2e/app-dir/with-babel.test.ts
+++ test/e2e/app-dir/with-babel.test.ts
@@ -20,8 +20,8 @@ describe('with babel', () => {
     next = await createNext({
       files: new FileRef(path.join(__dirname, 'with-babel')),
       dependencies: {
-        react: '0.0.0-experimental-cb5084d1c-20220924',
-        'react-dom': '0.0.0-experimental-cb5084d1c-20220924',
+        react: 'experimental',
+        'react-dom': 'experimental',
       },
     })
   })
