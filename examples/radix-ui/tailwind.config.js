20767f3c25354dfd4d1e3153ddf828c9a3587402
Balázs Orbán <info@balazsorban.com>
2022-10-05 16:13:19 +0200

    Update tailwind.config.js

--- examples/radix-ui/tailwind.config.js
+++ examples/radix-ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
