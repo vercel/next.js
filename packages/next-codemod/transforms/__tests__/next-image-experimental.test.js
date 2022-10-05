0a06c85309fbdb19406aa3569a6f35826db8f32a
Steven <steven@ceriously.com>
2022-10-05 18:05:18 -0400

    Rename to "next-image-experimental"

--- packages/next-codemod/transforms/__tests__/next-image-experimental.test.js
+++ packages/next-codemod/transforms/__tests__/next-image-experimental.test.js
@@ -4,7 +4,7 @@ const defineTest = require('jscodeshift/dist/testUtils').defineTest
 const { readdirSync } = require('fs')
 const { join } = require('path')
 
-const fixtureDir = 'next-image-upgrade-best-effort'
+const fixtureDir = 'next-image-experimental'
 const fixtureDirPath = join(__dirname, '..', '__testfixtures__', fixtureDir)
 const fixtures = readdirSync(fixtureDirPath)
   .filter(file => file.endsWith('.input.js'))
