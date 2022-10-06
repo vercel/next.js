98445e7af0b73c3d348a3cf02356d3427037cc86
Jiachi Liu <inbox@huozhi.im>
2022-10-06 15:57:21 +0200

    remove unused

--- test/e2e/app-dir/rsc-external.test.ts
+++ test/e2e/app-dir/rsc-external.test.ts
@@ -19,7 +19,6 @@ async function resolveStreamResponse(response: any, onData?: any) {
 
 describe('app dir - rsc external dependency', () => {
   let next: NextInstance
-  let distDir: string
 
   if ((global as any).isNextDeploy) {
     it('should skip for deploy mode for now', () => {})
@@ -45,11 +44,10 @@ describe('app dir - rsc external dependency', () => {
       startCommand: (global as any).isNextDev ? 'yarn dev' : 'yarn start',
       buildCommand: 'yarn build',
     })
-    distDir = path.join(next.testDir, '.next')
   })
   afterAll(() => next.destroy())
 
-  const { isNextDeploy, isNextDev } = global as any
+  const { isNextDeploy } = global as any
   const isReact17 = process.env.NEXT_TEST_REACT_VERSION === '^17'
   if (isNextDeploy || isReact17) {
     it('should skip tests for next-deploy and react 17', () => {})
