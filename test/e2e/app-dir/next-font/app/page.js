3b3d3b6a68b056b0c78b0277d897353d62d411e9
Hannes Bornö <hannes.borno@vercel.com>
2022-10-07 17:22:57 +0200

    Add duplicate font, preloads shouldn't change

--- test/e2e/app-dir/next-font/app/page.js
+++ test/e2e/app-dir/next-font/app/page.js
@@ -1,9 +1,11 @@
 import Comp from './Comp'
+import font1 from '../fonts/font1'
 import font2 from '../fonts/font2'
 
 export default function HomePage() {
   return (
     <>
+      <p className={font1.className}>Hello world</p>
       <p id="root-page" className={font2.className}>
         {JSON.stringify(font2)}
       </p>
