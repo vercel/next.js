c3b6b0f790b69f83960cc1c891ac9b29e80cbe42
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2022-10-07 14:49:09 +0200

    Add support for pages/_document (vercel/turbo#456)
    This adds preliminary support for pages/_document. The API is more complex than pages/_app, so this required a few more things in order to work.
    
    Right now, our `next/document` shim does much, much less than the original. We will eventually have to figure out whether we should distribute our own module, or find a way to reuse Next.js' internals. There's *a lot* of stuff in there so the second option is probably best.

--- packages/next-swc/crates/next-core/src/next_js/internal/next-hydrate.js
+++ packages/next-swc/crates/next-core/src/next_js/internal/next-hydrate.js
@@ -1,7 +1,4 @@
 import App from "@vercel/turbopack-next/pages/_app";
-// TODO(alexkirsz) We might need yet another transition that makes sure "." and
-// its dependencies cannot use the next-alias transition, by removing it from
-// the transition map.
 import Component from ".";
 import { hydrateRoot } from "react-dom/client";
 
