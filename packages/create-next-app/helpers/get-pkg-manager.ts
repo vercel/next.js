634d2e37a4c6fe098798e4a8db85ad226eec0aa3
Nestor <elnestinatorHD@gmail.com>
2022-10-08 12:54:28 +0200

    Add npm to environment package parser

--- packages/create-next-app/helpers/get-pkg-manager.ts
+++ packages/create-next-app/helpers/get-pkg-manager.ts
@@ -5,19 +5,19 @@ export type PackageManager = 'npm' | 'pnpm' | 'yarn'
 export function getPkgManager(): PackageManager {
   try {
     const userAgent = process.env.npm_config_user_agent
+
     if (userAgent) {
       if (userAgent.startsWith('yarn')) {
+        execSync('yarn --version', { stdio: 'ignore' })
         return 'yarn'
       } else if (userAgent.startsWith('pnpm')) {
+        execSync('pnpm --version', { stdio: 'ignore' })
         return 'pnpm'
+      } else {
+        return 'npm'
       }
-    }
-    try {
-      execSync('yarn --version', { stdio: 'ignore' })
-      return 'yarn'
-    } catch {
-      execSync('pnpm --version', { stdio: 'ignore' })
-      return 'pnpm'
+    } else {
+      return 'npm'
     }
   } catch {
     return 'npm'
