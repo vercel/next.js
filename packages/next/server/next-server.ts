d2693033112fd5020d1c6a9fa5d74475660573bd
Jimmy Lai <laijimmy0@gmail.com>
2022-10-11 12:59:32 +0200

    lazyload the edge runtime

--- packages/next/server/next-server.ts
+++ packages/next/server/next-server.ts
@@ -54,7 +54,7 @@ import HttpProxy from 'next/dist/compiled/http-proxy'
 import { getPathMatch } from '../shared/lib/router/utils/path-match'
 import { createHeaderRoute, createRedirectRoute } from './server-route-utils'
 import getRouteFromAssetPath from '../shared/lib/router/utils/get-route-from-asset-path'
-import { run } from './web/sandbox'
+
 import { detectDomainLocale } from '../shared/lib/i18n/detect-domain-locale'
 
 import { NodeNextRequest, NodeNextResponse } from './base-http/node'
@@ -68,7 +68,6 @@ import { ParsedUrl, parseUrl } from '../shared/lib/router/utils/parse-url'
 import { parse as nodeParseUrl } from 'url'
 import * as Log from '../build/output/log'
 import loadRequireHook from '../build/webpack/require-hook'
-import { consumeUint8ArrayReadableStream } from 'next/dist/compiled/edge-runtime'
 
 import BaseServer, {
   Options,
@@ -1767,7 +1766,7 @@ export default class NextNodeServer extends BaseServer {
 
     const method = (params.request.method || 'GET').toUpperCase()
 
-    const result = await run({
+    const result = await require('./web/sandbox').run({
       distDir: this.distDir,
       name: middlewareInfo.name,
       paths: middlewareInfo.paths,
@@ -2089,7 +2088,7 @@ export default class NextNodeServer extends BaseServer {
       )
     }
 
-    const result = await run({
+    const result = await require('./web/sandbox').run({
       distDir: this.distDir,
       name: edgeInfo.name,
       paths: edgeInfo.paths,
@@ -2129,6 +2128,9 @@ export default class NextNodeServer extends BaseServer {
     if (result.response.body) {
       // TODO(gal): not sure that we always need to stream
       const nodeResStream = (params.res as NodeNextResponse).originalResponse
+      const {
+        consumeUint8ArrayReadableStream,
+      } = require('next/dist/compiled/edge-runtime')
       try {
         for await (const chunk of consumeUint8ArrayReadableStream(
           result.response.body
