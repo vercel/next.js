e1e64e072ee5592087b1e41f9040e5e5401c5113
Hannes Bornö <hannes.borno@vercel.com>
2022-10-12 01:00:14 +0200

    Test for unused css modules with layout (#41018)
    Similar to #40996.
    
    This PR only adds a failing test that should pass. Also updates wrong url of previous test.
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Errors have a helpful link attached, see `contributing.md`
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the feature request has been accepted for implementation before opening a PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have a helpful link attached, see `contributing.md`
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `pnpm lint`
    - [ ] The "examples guidelines" are followed from [our contributing doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- test/e2e/app-dir/index.test.ts
+++ test/e2e/app-dir/index.test.ts
@@ -1261,10 +1261,23 @@ describe('app dir', () => {
 
         if (!isDev) {
           it('should not include unused css modules in the page in prod', async () => {
-            const browser = await webdriver(next.url, '/css/css-page')
+            const browser = await webdriver(next.url, '/css/css-page/unused')
             expect(
               await browser.eval(
                 `[...document.styleSheets].some(({ rules }) => [...rules].some(rule => rule.selectorText.includes('this_should_not_be_included')))`
+              )
+            ).toBe(false)
+          })
+
+          // TODO-APP: Should not include unused css modules in pages
+          it.skip('should not include unused css modules in nested pages in prod', async () => {
+            const browser = await webdriver(
+              next.url,
+              '/css/css-page/unused-nested/inner'
+            )
+            expect(
+              await browser.eval(
+                `[...document.styleSheets].some(({ rules }) => [...rules].some(rule => rule.selectorText.includes('this_should_not_be_included_in_inner_path')))`
               )
             ).toBe(false)
           })
