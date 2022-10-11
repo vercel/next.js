53b76a1bc77eccf3ad13ebb52e1ef60925829e26
Leticijak <65724560+Leticijak@users.noreply.github.com>
2022-10-11 12:49:36 +0200

    hmtl lang, NextScript
    html is set to "en"
    ability to easily add external scripts

--- examples/with-styled-components/pages/_document.tsx
+++ examples/with-styled-components/pages/_document.tsx
@@ -1,25 +1,43 @@
-import Document, { DocumentContext } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: [initialProps.styles, sheet.getStyleElement()],
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
