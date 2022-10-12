f3ca765b237ffacf199ec1e74cceddc7704ca621
Tim Neutkens <tim@timneutkens.nl>
2022-10-12 21:39:50 +0200

    Add exports for new router

--- test/e2e/app-dir/app/app/not-found/clientcomponent/a.js
+++ test/e2e/app-dir/app/app/not-found/clientcomponent/a.js
@@ -1,6 +1,6 @@
 // TODO-APP: enable when flight error serialization is implemented
 import ClientComp from './client-component'
-import { headers } from 'next/dist/client/components/hooks-server'
+import { headers } from 'next/headers'
 
 export default function Page() {
   // Opt-in to SSR.
