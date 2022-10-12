dad00bddca725cd0de4114a74bba459fcad959e0
Jody LeCompte <jody.lecompte@vincit.fi>
2022-10-12 18:52:43 -0500

    Improve error message for invalid types in get static paths path params

--- packages/next/build/utils.ts
+++ packages/next/build/utils.ts
@@ -969,13 +969,17 @@ export async function buildStaticPaths({
         ) {
           paramValue = []
         }
+
         if (
           (repeat && !Array.isArray(paramValue)) ||
+          (repeat &&
+            Array.isArray(paramValue) &&
+            !paramValue.every((param) => typeof param === 'string')) ||
           (!repeat && typeof paramValue !== 'string')
         ) {
-          throw new Error(
+          throw new TypeError(
             `A required parameter (${validParamKey}) was not provided as ${
-              repeat ? 'an array' : 'a string'
+              repeat ? 'an array of string' : 'a string'
             } in getStaticPaths for ${page}`
           )
         }
