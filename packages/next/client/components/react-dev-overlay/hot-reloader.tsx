4d6acb12e5190da0569ed8293eca612bdbbe89d6
Tim Neutkens <tim@timneutkens.nl>
2022-10-15 18:46:36 +0200

    Don't show error overlay for not found and redirect (#41438)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/next/client/components/react-dev-overlay/hot-reloader.tsx
+++ packages/next/client/components/react-dev-overlay/hot-reloader.tsx
@@ -445,6 +445,16 @@ export default function HotReload({
   })
 
   const handleOnUnhandledError = useCallback((ev) => {
+    if (
+      ev.error &&
+      ev.error.digest &&
+      (ev.error.digest.startsWith('NEXT_REDIRECT') ||
+        ev.error.digest === 'NEXT_NOT_FOUND')
+    ) {
+      ev.preventDefault()
+      return
+    }
+
     hadRuntimeError = true
     onUnhandledError(dispatch, ev)
   }, [])
