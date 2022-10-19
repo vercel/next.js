c0221c2e040ea1db919205e7f016628f24f6cba5
Shu Ding <g@shud.in>
2022-10-19 18:35:59 +0200

    update example

--- test/e2e/app-dir/app-typescript/app/layout.tsx
+++ test/e2e/app-dir/app-typescript/app/layout.tsx
@@ -2,7 +2,7 @@ export const config = {
   revalidate: 0,
 }
 
-// export const revalidate = -1
+export const revalidate = -1
 
 export default function Root({ children }) {
   return (
--- test/e2e/app-dir/app-typescript/pages/index.tsx
+++ test/e2e/app-dir/app-typescript/pages/index.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
 export default function Page() {
-  useState
   return <div>hello</div>
 }
