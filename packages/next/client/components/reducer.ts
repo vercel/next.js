dcaa8163ca52c26cae4dbcdcf39f0448d38f7161
Hannes Bornö <hannes.borno@vercel.com>
2022-10-19 11:20:47 +0200

    add comments

--- packages/next/client/components/reducer.ts
+++ packages/next/client/components/reducer.ts
@@ -506,8 +506,12 @@ function isNavigatingToNewRootLayout(
   // Compare segments
   const currentTreeSegment = currentTree[0]
   const nextTreeSegment = nextTree[0]
+  // If any segment is different before we find the root layout, the root layout has changed.
+  // E.g. /same/(group1)/layout.js -> /same/(group2)/layout.js
+  // First segment is 'same' for both, keep looking. (group1) changed to (group2) before the root layout was found, it must have changed.
   if (Array.isArray(currentTreeSegment) && Array.isArray(nextTreeSegment)) {
-    // Compare dynamic param name and type but ignore the value
+    // Compare dynamic param name and type but ignore the value, different values would not affect the current root layout
+    // /[name] - /slug1 and /slug2, both values (slug1 & slug2) still has the same layout /[name]/layout.js
     if (
       currentTreeSegment[0] !== nextTreeSegment[0] ||
       currentTreeSegment[2] !== nextTreeSegment[2]
@@ -520,10 +524,10 @@ function isNavigatingToNewRootLayout(
 
   // Current tree root layout found
   if (currentTree[4]) {
-    // Is navigating to new root layout if next tree doesn't have its root layout in the same segment
+    // If the next tree doesn't have the root layout flag, it must have changed.
     return !nextTree[4]
   }
-  // Next tree has root layout where current tree doesn't
+  // Current tree  didn't have its root layour here, must have changed.
   if (nextTree[4]) {
     return true
   }
