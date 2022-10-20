e1c58f239cf220ccb0f81fbf704f7cab332ead06
Steven <steven@ceriously.com>
2022-10-20 09:25:39 -0400

    Use next/dist/shared/lib/image-loader
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- packages/next/client/image.tsx
+++ packages/next/client/image.tsx
@@ -17,7 +17,7 @@ import {
 import { ImageConfigContext } from '../shared/lib/image-config-context'
 import { warnOnce } from '../shared/lib/utils'
 // @ts-ignore - This is replaced by webpack alias
-import defaultLoader from 'next/image/loader'
+import defaultLoader from 'next/dist/shared/lib/image-loader'
 
 const configEnv = process.env.__NEXT_IMAGE_OPTS as any as ImageConfigComplete
 const allImgs = new Map<
