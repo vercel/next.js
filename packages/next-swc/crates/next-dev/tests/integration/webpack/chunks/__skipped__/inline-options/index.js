276c844152526189244ed029d27003d3a8c63ef9
Leah <github.leah@hrmny.sh>
2022-10-21 01:46:26 +0200

    prettier fixes (vercel/turbo#41)

--- packages/next-swc/crates/next-dev/tests/integration/webpack/chunks/__skipped__/inline-options/index.js
+++ packages/next-swc/crates/next-dev/tests/integration/webpack/chunks/__skipped__/inline-options/index.js
@@ -14,7 +14,10 @@ it("should be able to use lazy-once mode", function () {
 
 it("should be able to use lazy-once mode with name", function () {
   function load(name) {
-    return import(/* webpackMode: "lazy-once", webpackChunkName: "name-lazy-once" */ "./dir3/" + name);
+    return import(
+      /* webpackMode: "lazy-once", webpackChunkName: "name-lazy-once" */ "./dir3/" +
+        name
+    );
   }
   return testChunkLoading(load, false, true);
 });
@@ -28,14 +31,19 @@ it("should be able to use lazy mode", function () {
 
 it("should be able to use lazy mode with name", function () {
   function load(name) {
-    return import(/* webpackMode: "lazy", webpackChunkName: "name-lazy" */ "./dir5/" + name);
+    return import(
+      /* webpackMode: "lazy", webpackChunkName: "name-lazy" */ "./dir5/" + name
+    );
   }
   return testChunkLoading(load, false, false);
 });
 
 it("should be able to use lazy mode with name and placeholder", function () {
   function load(name) {
-    return import(/* webpackMode: "lazy", webpackChunkName: "name-lazy-[request]" */ "./dir6/" + name);
+    return import(
+      /* webpackMode: "lazy", webpackChunkName: "name-lazy-[request]" */ "./dir6/" +
+        name
+    );
   }
   return testChunkLoading(load, false, false);
 });
@@ -89,7 +97,9 @@ it("should be able to use weak mode (without context)", function () {
 
 it("should not find module when mode is weak and chunk not served elsewhere", function () {
   var name = "a";
-  return import(/* webpackMode: "weak" */ "./dir10/" + name).catch(function (e) {
+  return import(/* webpackMode: "weak" */ "./dir10/" + name).catch(function (
+    e
+  ) {
     expect(e).toMatchObject({
       message: /not available/,
       code: /MODULE_NOT_FOUND/,
@@ -108,13 +118,17 @@ it("should not find module when mode is weak and chunk not served elsewhere (wit
 
 if (process.env.NODE_ENV === "production") {
   it("should contain only one export from webpackExports from module", function () {
-    return import(/* webpackExports: "usedExports" */ "./dir12/a?1").then((module) => {
-      expect(module.usedExports).toEqual(["usedExports"]);
-    });
+    return import(/* webpackExports: "usedExports" */ "./dir12/a?1").then(
+      (module) => {
+        expect(module.usedExports).toEqual(["usedExports"]);
+      }
+    );
   });
 
   it("should contain only webpackExports from module", function () {
-    return import(/* webpackExports: ["a", "usedExports", "b"] */ "./dir12/a?2").then((module) => {
+    return import(
+      /* webpackExports: ["a", "usedExports", "b"] */ "./dir12/a?2"
+    ).then((module) => {
       expect(module.usedExports).toEqual(["a", "b", "usedExports"]);
     });
   });
@@ -143,22 +157,28 @@ if (process.env.NODE_ENV === "production") {
   });
 
   it("should not mangle webpackExports from module", function () {
-    return import(/* webpackExports: "longnameforexport" */ "./dir12/a?5").then((module) => {
-      expect(module).toHaveProperty("longnameforexport");
-    });
+    return import(/* webpackExports: "longnameforexport" */ "./dir12/a?5").then(
+      (module) => {
+        expect(module).toHaveProperty("longnameforexport");
+      }
+    );
   });
 
   it("should not mangle default webpackExports from module", function () {
-    return import(/* webpackExports: "default" */ "./dir12/a?6").then((module) => {
-      expect(module).toHaveProperty("default");
-    });
+    return import(/* webpackExports: "default" */ "./dir12/a?6").then(
+      (module) => {
+        expect(module).toHaveProperty("default");
+      }
+    );
   });
 
   it("should contain only webpackExports from module in context mode", function () {
     const x = "b";
-    return import(/* webpackExports: "usedExports" */ `./dir13/${x}`).then((module) => {
-      expect(module.usedExports).toEqual(["usedExports"]);
-    });
+    return import(/* webpackExports: "usedExports" */ `./dir13/${x}`).then(
+      (module) => {
+        expect(module.usedExports).toEqual(["usedExports"]);
+      }
+    );
   });
 }
 
--- packages/next-swc/crates/next-dev/tests/integration/webpack/chunks/weird-reference-to-entry/errors.js
+++ packages/next-swc/crates/next-dev/tests/integration/webpack/chunks/weird-reference-to-entry/errors.js
@@ -1,3 +1,5 @@
 module.exports = [
-  [/It's not allowed to load an initial chunk on demand\. The chunk name "main" is already used by an entrypoint\./],
+  [
+    /It's not allowed to load an initial chunk on demand\. The chunk name "main" is already used by an entrypoint\./,
+  ],
 ];
