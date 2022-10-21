22e8892d07710ba2688b331cabfeb3b51e1e73f5
Hannes Bornö <hannes.borno@vercel.com>
2022-10-21 18:24:58 +0200

    msg

--- packages/font/google/index.js
+++ packages/font/google/index.js
@@ -1,4 +1,4 @@
-let message = '@next/font/google failed to run.'
+let message = '@next/font/google failed to run or is incorrectly configured.'
 if (process.env.NODE_ENV === 'development') {
   message +=
     '\nIf you just installed `@next/font`, please try restarting `next dev` and resaving your file.'
