b586b8cad24452c444e1702ffa1a2c8b411d0897
Steven <steven@ceriously.com>
2022-10-21 17:01:10 -0400

    Add missing loaders

--- packages/next-codemod/transforms/__testfixtures__/next-image-experimental-loader/akamai/output/akamai-loader.js
+++ packages/next-codemod/transforms/__testfixtures__/next-image-experimental-loader/akamai/output/akamai-loader.js
@@ -1,1 +1,4 @@
-// TODO
+const normalizeSrc = (src) => src[0] === '/' ? src.slice(1) : src
+export default function akamaiLoader({ src, width, quality }) {
+return 'https://example.com/' + normalizeSrc(src) + '?imwidth=' + width
+}
--- packages/next-codemod/transforms/__testfixtures__/next-image-experimental-loader/cloudinary/output/cloudinary-loader.js
+++ packages/next-codemod/transforms/__testfixtures__/next-image-experimental-loader/cloudinary/output/cloudinary-loader.js
@@ -1,1 +1,6 @@
-// TODO
+const normalizeSrc = (src) => src[0] === '/' ? src.slice(1) : src
+export default function cloudinaryLoader({ src, width, quality }) {
+const params = ['f_auto', 'c_limit', 'w_' + width, 'q_' + (quality || 'auto')]
+const paramsString = params.join(',') + '/'
+return 'https://example.com/' + paramsString + normalizeSrc(src)
+}
