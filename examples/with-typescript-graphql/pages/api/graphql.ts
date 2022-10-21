3976aeebd85a9a6cbd382e10e7fef746c0633846
Charly POLY <cpoly55@gmail.com>
2022-10-21 10:01:01 +0200

    style(examples): prettier

--- examples/with-typescript-graphql/pages/api/graphql.ts
+++ examples/with-typescript-graphql/pages/api/graphql.ts
@@ -16,5 +16,5 @@ export default createYoga<{
 }>({
   schema,
   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
-  graphqlEndpoint: '/api/graphql'
+  graphqlEndpoint: '/api/graphql',
 })
