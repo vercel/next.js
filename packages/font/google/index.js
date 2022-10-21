bf4aceda0f537d6f00ad8d3cf7e615440b8e5e58
Hannes Bornö <hannes.borno@vercel.com>
2022-10-21 16:07:37 +0200

    Improve warnings

--- packages/font/google/index.js
+++ packages/font/google/index.js
@@ -1,1 +1,7 @@
-throw new Error('@next/font/google is not correctly configured')
+let message = '@next/font/google failed to run.'
+if (process.env.NODE_ENV === 'development') {
+  message +=
+    '\nIf you just installed `@next/font`, please try rerunning `next dev`'
+}
+
+throw new Error(message)
--- packages/font/local/index.js
+++ packages/font/local/index.js
@@ -1,1 +1,7 @@
-throw new Error('@next/font/local is not correctly configured')
+let message = '@next/font/local failed to run.'
+if (process.env.NODE_ENV === 'development') {
+  message +=
+    '\nIf you just installed `@next/font`, please try rerunning `next dev`'
+}
+
+throw new Error(message)
