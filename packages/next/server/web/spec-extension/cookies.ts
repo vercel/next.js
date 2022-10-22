5a0195fbbc0ceb7fbec32336c95744dc13cbfc17
Balázs Orbán <info@balazsorban.com>
2022-10-22 21:16:15 +0200

    use `CookieStore` for `NextCookies`

--- packages/next/server/web/spec-extension/cookies.ts
+++ packages/next/server/web/spec-extension/cookies.ts
@@ -1,8 +1,4 @@
-export { Cookie } from 'next/dist/compiled/@edge-runtime/cookies'
-import { ResponseCookies } from 'next/dist/compiled/@edge-runtime/cookies'
-
-export class NextCookies extends ResponseCookies {
-  constructor(response: Response | Request) {
-    super(response as any) // TODO: Make `ResponseCookies` more general in `@edge-runtime/cookies`?
-  }
-}
+export {
+  CookieStore as NextCookies,
+  Cookie,
+} from 'next/dist/compiled/@edge-runtime/cookies'
