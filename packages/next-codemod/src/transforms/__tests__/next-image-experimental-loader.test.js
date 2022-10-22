6d3d1e3a697875ce65907f17f61df0087638210b
Balázs Orbán <info@balazsorban.com>
2022-10-22 20:35:57 +0200

    fix formatting

--- packages/next-codemod/src/transforms/__tests__/next-image-experimental-loader.test.js
+++ packages/next-codemod/src/transforms/__tests__/next-image-experimental-loader.test.js
@@ -1,23 +1,13 @@
 /* global jest */
 jest.autoMockOff()
-const Runner = require('jscodeshift/dist/Runner')
+const Runner = require('jscodeshift/dist/Runner');
 const { cp, mkdir, rm, readdir, readFile } = require('fs/promises')
 const { join } = require('path')
 
-const fixtureDir = join(
-  __dirname,
-  '..',
-  '__testfixtures__',
-  'next-image-experimental-loader'
-)
-const transform = join(
-  __dirname,
-  '../../../dist/transforms',
-  'next-image-experimental.js'
-)
+const fixtureDir = join(__dirname, '..', '__testfixtures__', 'next-image-experimental-loader')
+const transform = join(__dirname, '../../../dist/transforms', 'next-image-experimental.js')
 const opts = { recursive: true }
 
-console.log(transform)
 async function toObj(dir) {
   const obj = {}
   const files = await readdir(dir)
@@ -26,20 +16,19 @@ async function toObj(dir) {
   }
   return obj
 }
-it.each(['imgix', 'cloudinary', 'akamai'])(
-  'should transform loader %s',
-  async (loader) => {
-    try {
-      await mkdir(join(fixtureDir, 'tmp'), opts)
-      await cp(join(fixtureDir, loader, 'input'), join(fixtureDir, 'tmp'), opts)
-      process.chdir(join(fixtureDir, 'tmp'))
-      const result = await Runner.run(transform, [`.`], {})
-      expect(result.error).toBe(0)
-      expect(await toObj(join(fixtureDir, 'tmp'))).toStrictEqual(
-        await toObj(join(fixtureDir, loader, 'output'))
-      )
-    } finally {
-      await rm(join(fixtureDir, 'tmp'), opts)
-    }
+it.each(['imgix', 'cloudinary', 'akamai'])('should transform loader %s', async (loader) => {
+  try {
+    await mkdir(join(fixtureDir, 'tmp'), opts)
+    await cp(join(fixtureDir, loader, 'input'), join(fixtureDir, 'tmp'), opts)
+    process.chdir(join(fixtureDir, 'tmp'))
+    const result = await Runner.run(transform, [`.`], {})
+    expect(result.error).toBe(0)
+    expect(
+      await toObj(join(fixtureDir, 'tmp'))
+    ).toStrictEqual(
+      await toObj(join(fixtureDir, loader, 'output'))
+    )
+  } finally {
+    await rm(join(fixtureDir, 'tmp'), opts)
   }
-)
+})
