78138af50548b3050c08d3edd0afaac8f91fccfa
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-23 04:59:06 +0900

    chore: Update swc_core to `v0.40.7` (#41613)

--- packages/next-swc/crates/emotion/tests/fixture/issues/39672/output.ts
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-const SelectedComponent = /*#__PURE__*/ styled("p", {
-    target: "ekie5mj0",
-    label: "SelectedComponent"
-})("color:red;&:after{content:' | ';}", "/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5wdXQudHMiLCJzb3VyY2VzIjpbImlucHV0LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBzdHlsZWQgZnJvbSAnQGVtb3Rpb24vc3R5bGVkJ1xuXG5jb25zdCBTZWxlY3RlZENvbXBvbmVudCA9IHN0eWxlZC5wYFxuICBjb2xvcjogcmVkO1xuXG4gICY6YWZ0ZXIge1xuICAgIGNvbnRlbnQ6ICcgfCAnO1xuICB9XG5gXG4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBRTBCIn0= */");
