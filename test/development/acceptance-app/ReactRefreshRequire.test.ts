9dae7a89d097ef17df04f90ffd2d650c5f815201
Tim Neutkens <tim@timneutkens.nl>
2022-10-22 14:26:57 +0200

    Enable ReactRefreshLogBox-builtins.test.ts, remove loader from import trace (#41645)

--- test/development/acceptance-app/ReactRefreshRequire.test.ts
+++ test/development/acceptance-app/ReactRefreshRequire.test.ts
@@ -10,6 +10,10 @@ describe('ReactRefreshRequire app', () => {
   beforeAll(async () => {
     next = await createNext({
       files: new FileRef(path.join(__dirname, 'fixtures', 'default-template')),
+      dependencies: {
+        react: '0.0.0-experimental-9cdf8a99e-20221018',
+        'react-dom': '0.0.0-experimental-9cdf8a99e-20221018',
+      },
       skipStart: true,
     })
   })
