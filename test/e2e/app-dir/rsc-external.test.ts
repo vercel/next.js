4c95aa52c6bb2f5b50622b98306fb3ce2252edd4
JJ Kasper <jj@jjsweb.site>
2022-10-21 23:11:15 -0700

    update tests

--- test/e2e/app-dir/rsc-external.test.ts
+++ test/e2e/app-dir/rsc-external.test.ts
@@ -29,8 +29,8 @@ describe('app dir - rsc external dependency', () => {
     next = await createNext({
       files: new FileRef(path.join(__dirname, './rsc-external')),
       dependencies: {
-        react: 'experimental',
-        'react-dom': 'experimental',
+        react: '0.0.0-experimental-9cdf8a99e-20221018',
+        'react-dom': '0.0.0-experimental-9cdf8a99e-20221018',
       },
       packageJson: {
         scripts: {
