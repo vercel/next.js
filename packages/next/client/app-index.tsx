fdb1470ed220ee29a50eeaf2687f57821e627eb9
Jiachi Liu <inbox@huozhi.im>
2022-10-24 23:13:52 -0700

    use default dev overlay export, match specific loader name

--- packages/next/client/app-index.tsx
+++ packages/next/client/app-index.tsx
@@ -175,8 +175,8 @@ function RSCComponent(props: any): JSX.Element {
 
 export function hydrate() {
   if (process.env.NODE_ENV !== 'production') {
-    const HotReload = require('./components/react-dev-overlay/hot-reloader-client')
-    typeof import('./components/react-dev-overlay/hot-reloader-client')
+    const HotReload: typeof import('./components/react-dev-overlay/hot-reloader-client').default = require('./components/react-dev-overlay/hot-reloader-client')
+
     const rootLayoutMissingTagsError = (self as any)
       .__next_root_layout_missing_tags_error
 
