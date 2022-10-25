cb00772a1beccfebdc33f9a06ec852fe0b812d67
Jared Palmer <jared@jaredpalmer.com>
2022-10-25 09:30:42 -0700

    Add turbopack example

---
+++ examples/with-turbopack/app/route-groups/(shop)/layout.tsx
@@ -0,0 +1,26 @@
+import { use } from 'react';
+import {
+  fetchCategories,
+  getCategories,
+  type Category,
+} from '@/lib/getCategories';
+import { Boundary } from '@/ui/Boundary';
+import ClickCounter from '@/ui/ClickCounter';
+import React from 'react';
+import CategoryNav from '../CategoryNav';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const categories = use(fetchCategories());
+  return (
+    <Boundary labels={['shop layout']} color="cyan" animateRerendering={false}>
+      <div className="space-y-9">
+        <div className="flex items-center justify-between">
+          <CategoryNav categories={categories} />
+          <ClickCounter />
+        </div>
+
+        <div>{children}</div>
+      </div>
+    </Boundary>
+  );
+}
---
+++ examples/with-turbopack/tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './page/**/*.{js,ts,jsx,tsx}',
+    './ui/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      // https://vercel.com/design/color
+      colors: {
+        vercel: {
+          pink: '#FF0080',
+          blue: '#0070F3',
+          cyan: '#50E3C2',
+          orange: '#F5A623',
+          violet: '#7928CA',
+        },
+      },
+      keyframes: ({ theme }) => ({
+        rerender: {
+          '0%': {
+            ['border-color']: theme('colors.vercel.pink'),
+          },
+          '40%': {
+            ['border-color']: theme('colors.vercel.pink'),
+          },
+        },
+        highlight: {
+          '0%': {
+            background: theme('colors.vercel.pink'),
+            color: theme('colors.white'),
+          },
+          '40%': {
+            background: theme('colors.vercel.pink'),
+            color: theme('colors.white'),
+          },
+        },
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
+        translateXReset: {
+          '100%': {
+            transform: 'translateX(0)',
+          },
+        },
+        fadeToTransparent: {
+          '0%': {
+            opacity: 1,
+          },
+          '40%': {
+            opacity: 1,
+          },
+          '100%': {
+            opacity: 0,
+          },
+        },
+      }),
+    },
+  },
+};
