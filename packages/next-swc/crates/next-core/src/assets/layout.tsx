dc74ca31c0a8322075a277ad037e8782c25979b9
Will Binns-Smith <wbinnssmith@gmail.com>
2022-10-24 17:30:04 -0700

    Create an app root layout if app/page.{js,tsx} exists but layout does not (vercel/turbo#264)
    This makes turbopack automatically create `app/layout.js` if
    `app/page.js` exists, or `app/layout.tsx` if `app/page.tsx` exists, and
    neither layout file already exists.
    
    **Note that I'd prefer this issue to have info severity, but even with a
    higher log level of info, the issue is never shown**. Filed vercel/turbo#265 to
    track this.
    
    Test Plan:
    In a new `create-next-app`, opt into appDirectory and create
    `app/page.js`. Run turbopack and verify:
    * [x] the index page renders correctly
    * [x] An issue with Warning level is logged notifying the user an
    `app/layout.js` was created
    * [x] `app/layout.js` is actually created with basic root layout
    
    In a new `create-next-app`, opt into appDirectory and create
    `app/page.tsx`. Run turbopack and verify:
    * [x] the index page renders correctly
    * [x] An issue with Warning level is logged notifying the user an
    `app/layout.tsx` was created
    * [x] `app/layout.tsx` is actually created with basic root layout
    
    In a new `create-next-app`, opt into appDirectory and create
    `app/page.tsx`. Also create a basic `app/layout.js` (note js layout with
    tsx page). Add a custom `<title>` to the layout. Run turbopack and
    verify:
    * [x] the index page renders correctly
    * [x] No issue is logged
    * [x] No files are modified
    * [x] The custom `<title>` is reflected.

---
+++ packages/next-swc/crates/next-core/src/assets/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>{children}</body>
+    </html>
+  );
+}
