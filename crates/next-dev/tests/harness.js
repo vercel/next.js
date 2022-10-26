b2f1695d06c8895766bcc444ee7e0dbca5815af1
Will Binns-Smith <wbinnssmith@gmail.com>
2022-10-26 16:22:58 -0700

    Implement nsObj helper from webpack tests and pass basic import test (vercel/turbo#2385)

--- crates/next-dev/tests/harness.js
+++ crates/next-dev/tests/harness.js
@@ -5,3 +5,11 @@ globalThis.__jest__ = jest;
 globalThis.expect = expect;
 globalThis.describe = jest.describe;
 globalThis.it = jest.it;
+
+// From https://github.com/webpack/webpack/blob/9fcaa243573005d6fdece9a3f8d89a0e8b399613/test/TestCases.template.js#L422
+globalThis.nsObj = function nsObj(obj) {
+  Object.defineProperty(obj, Symbol.toStringTag, {
+    value: "Module",
+  });
+  return obj;
+};
