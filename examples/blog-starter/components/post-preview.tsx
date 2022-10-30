6b10d17ef12a9dfa2c493997bcbd8f115c637321
Yusuke Ebihara <dev.biibiebi@gmail.com>
2022-10-30 23:40:00 +0900

    Fix the error, `Error: Invalid with <a> child.`.
    Step to reproduce:
    1. Run `yarn create next-app --example blog-starter blog`
    2. Run `cd blog; yarn dev`
    3. The error occurs and says `Error: Invalid <Link> with <a> child.
    Please remove <a> or use <Link legacyBehavior>.`
    
    This error comes from a change of next/link in Next.js 13.
    ref: https://nextjs.org/docs/messages/invalid-new-link-with-extra-anchor
    
    I fix it as the document says, and I confirm the blog is up.

--- examples/blog-starter/components/post-preview.tsx
+++ examples/blog-starter/components/post-preview.tsx
@@ -27,8 +27,8 @@ const PostPreview = ({
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+        <Link as={`/posts/${slug}`} className="hover:underline" href="/posts/[slug]">
+          {title}
         </Link>
       </h3>
       <div className="text-lg mb-4">
