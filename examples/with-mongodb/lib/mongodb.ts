cfb1e551e385342cf385762fbeff128caa5871da
汪心禾 <wangxinhe06@gmail.com>
2022-10-31 14:17:50 +0800

    Simplify examples/with-mongodb/lib
    Remove unjustified code and simplify assignment condition

--- examples/with-mongodb/lib/mongodb.ts
+++ examples/with-mongodb/lib/mongodb.ts
@@ -1,32 +1,21 @@
 import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid environment variable: "MONGODB_URI"')
-}
+if (!process.env.MONGODB_URI)
+  throw new Error(
+    'Missing environment variable: "MONGODB_URI". Did you forget to add your MongoDB connection URI to .env.local?'
+  )
 
-const uri = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI
 const options = {}
 
-let client
-let clientPromise: Promise<MongoClient>
-
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
-
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
-}
+const clientPromise =
+  process.env.NODE_ENV === 'development'
+    ? // In development mode, use a global variable so that the value
+      // is preserved across module reloads caused by HMR (Hot Module Replacement).
+      global._mongoClientPromise ||
+      (global._mongoClientPromise = new MongoClient(url, options).connect())
+    : // In production mode, it's best to not use a global variable.
+      new MongoClient(url, options).connect()
 
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
