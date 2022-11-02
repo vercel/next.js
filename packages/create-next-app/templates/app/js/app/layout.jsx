e15b2ad37026471d01438a3bb76ab461f8f824ae
Wyatt Johnson <accounts+github@wyattjoh.ca>
2022-11-02 09:24:04 -0600

    fix: enhance comment on `layout.{t,j}sx` files

--- packages/create-next-app/templates/app/js/app/layout.jsx
+++ packages/create-next-app/templates/app/js/app/layout.jsx
@@ -3,6 +3,11 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
       <body>{children}</body>
     </html>
   )
--- packages/create-next-app/templates/app/ts/app/layout.tsx
+++ packages/create-next-app/templates/app/ts/app/layout.tsx
@@ -7,6 +7,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
       <body>{children}</body>
     </html>
   )
