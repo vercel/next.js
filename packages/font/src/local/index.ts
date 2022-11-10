5cd13c84663b3a343d6553dec0c7fd0581e66223
Hannes Bornö <hannes.borno@vercel.com>
2022-11-10 15:59:24 +0100

    Return correct type in local

--- packages/font/src/local/index.ts
+++ packages/font/src/local/index.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { FontModule } from 'next/font'
-type Display = 'auto' | 'block' | 'swap' | 'fallback' | 'optional'
-type CssVariable = `--${string}`
-type LocalFont = {
+import type {
+  CssVariable,
+  Display,
+  NextFont,
+  NextFontWithVariable,
+} from '../types'
+
+type LocalFont<T extends CssVariable | undefined = undefined> = {
   src:
     | string
     | Array<{
@@ -16,10 +20,14 @@ type LocalFont = {
   adjustFontFallback?: 'Arial' | 'Times New Roman' | false
   fallback?: string[]
   preload?: boolean
-  variable?: CssVariable
+  variable?: T
   declarations?: Array<{ prop: string; value: string }>
 }
 
-export default function localFont(options: LocalFont): FontModule {
+export default function localFont<
+  T extends CssVariable | undefined = undefined
+>(
+  options: LocalFont<T>
+): T extends undefined ? NextFont : NextFontWithVariable {
   throw new Error()
 }
