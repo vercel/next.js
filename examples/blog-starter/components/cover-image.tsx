8089659a343fa75c1ec3d9ef51b3f73093653baf
Steven Tey <stevensteel97@gmail.com>
2022-11-14 14:30:11 -0600

    Update cover-image.tsx

--- examples/blog-starter/components/cover-image.tsx
+++ examples/blog-starter/components/cover-image.tsx
@@ -13,7 +13,7 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm', {
+      className={cn('shadow-sm w-full', {
         'hover:shadow-lg transition-shadow duration-200': slug,
       })}
       width={1300}
