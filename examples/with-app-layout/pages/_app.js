54fd44f71fd7ed660e372a44ba9946b2e77f2cd6
Max Proske <max@mproske.com>
2022-11-14 22:13:43 -0800

    Convert `with-app-layout` example to TypeScript

--- examples/with-app-layout/pages/_app.js
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }) => <div className="layout">{children}</div>
-
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
