f3c365f9e5a26718b836fe3c2645ff29fe4e76f2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-16 14:04:22 +0900

    Update test refs

--- packages/next-swc/crates/core/tests/loader/issue-30389/output.js
+++ packages/next-swc/crates/core/tests/loader/issue-30389/output.js
@@ -1,2 +1,2 @@
-var ref;
-var test = (ref = my_array === null || my_array === void 0 ? void 0 : my_array[0]) !== null && ref !== void 0 ? ref : [];
+var _ref;
+var test = (_ref = my_array === null || my_array === void 0 ? void 0 : my_array[0]) !== null && _ref !== void 0 ? _ref : [];
