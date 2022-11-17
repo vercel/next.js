fd2688b98403142ab811bcb175bf8148b10f90c5
Tobias Koppers <tobias.koppers@googlemail.com>
2022-11-17 15:58:19 +0100

    add benchmarks for Vite SSR and SWC (vercel/turbo#2751)

--- packages/next-swc/crates/next-dev/benches/bundlers/vite/mod.rs
+++ packages/next-swc/crates/next-dev/benches/bundlers/vite/mod.rs
@@ -15,50 +15,81 @@ use crate::{
     },
 };
 
-pub struct Vite;
+pub struct Vite {
+    swc: bool,
+    ssr: bool,
+}
 
 impl Vite {
-    pub fn new() -> Self {
-        Vite {}
+    pub fn new(swc: bool, ssr: bool) -> Self {
+        Vite { swc, ssr }
     }
 }
 
 impl Bundler for Vite {
     fn get_name(&self) -> &str {
-        "Vite CSR"
+        if self.ssr {
+            if self.swc {
+                "Vite SWC SSR"
+            } else {
+                "Vite SSR"
+            }
+        } else {
+            if self.swc {
+                "Vite SWC CSR"
+            } else {
+                "Vite CSR"
+            }
+        }
     }
 
     fn prepare(&self, install_dir: &Path) -> Result<()> {
-        npm::install(
-            install_dir,
-            &[
-                NpmPackage::new("vite", "^3.2.4"),
-                NpmPackage::new("@vitejs/plugin-react", "^2.2.0"),
-            ],
-        )
-        .context("failed to install from npm")?;
+        let mut packages = vec![NpmPackage::new("vite", "^3.2.4")];
+        if self.swc {
+            packages.push(NpmPackage::new("vite-plugin-swc-react-refresh", "^2.2.0"));
+        } else {
+            packages.push(NpmPackage::new("@vitejs/plugin-react", "^2.2.0"));
+        };
+        if self.ssr {
+            packages.push(NpmPackage::new("express", "^4.18.2"));
+        }
+        npm::install(install_dir, &packages).context("failed to install from npm")?;
 
         fs::write(
             install_dir.join("vite.config.js"),
-            include_bytes!("vite.config.js"),
+            if self.swc {
+                include_bytes!("vite.swc.config.js") as &[u8]
+            } else {
+                include_bytes!("vite.config.js") as &[u8]
+            },
         )?;
 
         Ok(())
     }
 
     fn start_server(&self, test_dir: &Path) -> Result<(Child, String)> {
-        let mut proc = Command::new("node")
-            .args([
-                (test_dir
+        let args = if self.ssr {
+            vec![test_dir
+                .join("vite-server.mjs")
+                .to_str()
+                .unwrap()
+                .to_string()]
+        } else {
+            vec![
+                test_dir
                     .join("node_modules")
                     .join("vite")
                     .join("bin")
                     .join("vite.js")
                     .to_str()
-                    .unwrap()),
-                "--port",
-                "0",
-            ])
+                    .unwrap()
+                    .to_string(),
+                "--port".to_string(),
+                "0".to_string(),
+            ]
+        };
+        let mut proc = Command::new("node")
+            .args(args)
             .env("NO_COLOR", "1")
             .current_dir(test_dir)
             .stdout(Stdio::piped())
