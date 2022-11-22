67c6bf9d5c9fd34d0f1af76a1f9b6ab67e31afe4
nutlope <he55@drexel.edu>
2022-11-22 12:35:52 -0500

    updated generateBlurPlacerholder

--- examples/with-cloudinary/utils/generateBlurPlaceholder.ts
+++ examples/with-cloudinary/utils/generateBlurPlaceholder.ts
@@ -1,10 +1,10 @@
-import { ImageProps } from "./imageType";
+import { ImageProps } from './imageType'
 
 export default async function getBase64ImageUrl(image: ImageProps) {
   const response = await fetch(
-    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/w_75/e_blur:500,q_auto,f_webp/${image.public_id}.${image.format}`
-  );
-  const buffer = await response.arrayBuffer();
-  const data = Buffer.from(buffer).toString("base64");
-  return `data:image/webp;base64,${data}`;
+    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/w_100/e_blur:1000,q_auto,f_webp/${image.public_id}.${image.format}`
+  )
+  const buffer = await response.arrayBuffer()
+  const data = Buffer.from(buffer).toString('base64')
+  return `data:image/webp;base64,${data}`
 }
