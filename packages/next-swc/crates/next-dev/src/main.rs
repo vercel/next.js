0b841ad61f36a8db5cff22866beb9a27b8f3ea91
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2022-11-22 08:50:27 -0800

    feat(next/dev): allow to display version (vercel/turbo#2793)

--- packages/next-swc/crates/next-dev/src/main.rs
+++ packages/next-swc/crates/next-dev/src/main.rs
@@ -18,5 +18,42 @@ fn main() -> Result<()> {
 async fn main() -> Result<()> {
     let options = next_dev::devserver_options::DevServerOptions::parse();
 
+    if options.display_version {
+        println!(
+            "Build Timestamp\t\t{:#?}",
+            option_env!("VERGEN_BUILD_TIMESTAMP").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Build Version\t\t{:#?}",
+            option_env!("VERGEN_BUILD_SEMVER").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Commit SHA\t\t{:#?}",
+            option_env!("VERGEN_GIT_SHA").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Commit Date\t\t{:#?}",
+            option_env!("VERGEN_GIT_COMMIT_TIMESTAMP").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Commit Branch\t\t{:#?}",
+            option_env!("VERGEN_GIT_BRANCH").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Commit Message\t\t{:#?}",
+            option_env!("VERGEN_GIT_COMMIT_MESSAGE").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Cargo Target Triple\t{:#?}",
+            option_env!("VERGEN_CARGO_TARGET_TRIPLE").unwrap_or_else(|| "N/A")
+        );
+        println!(
+            "Cargo Profile\t\t{:#?}",
+            option_env!("VERGEN_CARGO_PROFILE").unwrap_or_else(|| "N/A")
+        );
+
+        return Ok(());
+    }
+
     next_dev::start_server(&options).await
 }
