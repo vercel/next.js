c8a0b7fe5c0566c30ad7528e56ede670b306ddd5
Max Proske <max@mproske.com>
2022-11-22 10:46:55 -0800

    Use `check` util

--- test/e2e/examples/test/with-styled-jsx.test.ts
+++ test/e2e/examples/test/with-styled-jsx.test.ts
@@ -1,14 +1,19 @@
 import { init, next } from './utils'
 import webdriver from 'next-webdriver'
+import { check } from 'next-test-utils'
 
 describe('with-styled-jsx example', () => {
   init('with-styled-jsx')
 
   it('should render styles', async () => {
     const browser = await webdriver(next.url, '/')
-    const color = await browser.eval(
-      `getComputedStyle(document.querySelector('.hello')).backgroundColor`
+
+    await check(
+      () =>
+        browser.eval(
+          `window.getComputedStyle(document.querySelector('.hello')).backgroundColor`
+        ),
+      'rgb(238, 238, 238)'
     )
-    expect(color).toMatch('rgb(238, 238, 238)')
   })
 })
