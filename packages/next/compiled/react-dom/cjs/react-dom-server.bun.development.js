718ea15b9ba1a8665ca006decd847ab72c556c3e
Shu Ding <g@shud.in>
2022-11-23 19:49:57 +0100

    update

--- packages/next/compiled/react-dom/cjs/react-dom-server.bun.development.js
+++ packages/next/compiled/react-dom/cjs/react-dom-server.bun.development.js
@@ -10,8 +10,8 @@
 
 'use strict';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('next/dist/compiled/react');
+var ReactDOM = require('next/dist/compiled/react-dom');
 
 var ReactVersion = '18.3.0-next-2655c9354-20221121';
 
