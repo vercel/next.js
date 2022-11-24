d4e21d0b18edee8c6f4f7ae619a1bb35231c648f
JJ Kasper <jj@jjsweb.site>
2022-11-23 18:09:33 -0800

    Add test case and add missing files

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -61,6 +61,9 @@ import {
   FONT_LOADER_MANIFEST,
   CLIENT_STATIC_FILES_RUNTIME_MAIN_APP,
   APP_CLIENT_INTERNALS,
+  SUBRESOURCE_INTEGRITY_MANIFEST,
+  MIDDLEWARE_BUILD_MANIFEST,
+  MIDDLEWARE_REACT_LOADABLE_MANIFEST,
 } from '../shared/lib/constants'
 import { getSortedRoutes, isDynamicRoute } from '../shared/lib/router/utils'
 import { __ApiPreviewProps } from '../server/api-utils'
@@ -887,14 +890,29 @@ export default async function build(
           files: [
             ROUTES_MANIFEST,
             path.relative(distDir, manifestPath),
-            ...(isAppDirEnabled
-              ? [path.relative(distDir, appManifestPath)]
-              : []),
             BUILD_MANIFEST,
             PRERENDER_MANIFEST,
             path.join(SERVER_DIRECTORY, MIDDLEWARE_MANIFEST),
+            path.join(SERVER_DIRECTORY, MIDDLEWARE_BUILD_MANIFEST + '.js'),
+            path.join(
+              SERVER_DIRECTORY,
+              MIDDLEWARE_REACT_LOADABLE_MANIFEST + '.js'
+            ),
             ...(appDir
               ? [
+                  ...(config.experimental.sri
+                    ? [
+                        path.join(
+                          SERVER_DIRECTORY,
+                          SUBRESOURCE_INTEGRITY_MANIFEST + '.js'
+                        ),
+                        path.join(
+                          SERVER_DIRECTORY,
+                          SUBRESOURCE_INTEGRITY_MANIFEST + '.json'
+                        ),
+                      ]
+                    : []),
+                  path.relative(distDir, appManifestPath),
                   path.join(SERVER_DIRECTORY, FLIGHT_MANIFEST + '.js'),
                   path.join(SERVER_DIRECTORY, FLIGHT_MANIFEST + '.json'),
                   path.join(
@@ -2759,28 +2777,32 @@ export default async function build(
           })
           await promises.copyFile(filePath, outputPath)
         }
-        await recursiveCopy(
-          path.join(distDir, SERVER_DIRECTORY, 'pages'),
-          path.join(
-            distDir,
-            'standalone',
-            path.relative(outputFileTracingRoot, distDir),
-            SERVER_DIRECTORY,
-            'pages'
-          ),
-          { overwrite: true }
-        )
-        await recursiveCopy(
-          path.join(distDir, SERVER_DIRECTORY, 'app'),
-          path.join(
-            distDir,
-            'standalone',
-            path.relative(outputFileTracingRoot, distDir),
-            SERVER_DIRECTORY,
-            'app'
-          ),
-          { overwrite: true }
-        )
+        if (pagesDir) {
+          await recursiveCopy(
+            path.join(distDir, SERVER_DIRECTORY, 'pages'),
+            path.join(
+              distDir,
+              'standalone',
+              path.relative(outputFileTracingRoot, distDir),
+              SERVER_DIRECTORY,
+              'pages'
+            ),
+            { overwrite: true }
+          )
+        }
+        if (appDir) {
+          await recursiveCopy(
+            path.join(distDir, SERVER_DIRECTORY, 'app'),
+            path.join(
+              distDir,
+              'standalone',
+              path.relative(outputFileTracingRoot, distDir),
+              SERVER_DIRECTORY,
+              'app'
+            ),
+            { overwrite: true }
+          )
+        }
       }
 
       staticPages.forEach((pg) => allStaticPages.add(pg))
