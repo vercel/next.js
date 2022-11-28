e2bc04a0e09fc65fa21f8b683695e71ea6e1ae63
jueungrace <graceyunn@gmail.com>
2022-11-28 14:42:12 -0800

    clean up /app

--- packages/create-next-app/templates/app/ts/app/page.tsx
+++ packages/create-next-app/templates/app/ts/app/page.tsx
@@ -21,109 +21,101 @@ const spaceMono = Space_Mono({
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.content}>
-        <div className={styles.description}>
-          <p className={spaceMono.className}>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              className={spaceMono.className}
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <span className={styles['vercel-logo']}>
-                <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  width={100}
-                  height={24}
-                  priority
-                />
-              </span>
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <div className={styles.logo}>
-            <Image
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={180}
-              height={37}
-              priority
-            />
-          </div>
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
+    <div className={styles.content}>
+      <div className={styles.description}>
+        <p className={spaceMono.className}>
+          Get started by editing&nbsp;
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+        <div>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={spaceGrotesk.className}>
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className={spaceMono.className}
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={spaceGrotesk.className}>
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={spaceGrotesk.className}>
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={spaceGrotesk.className}>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
+            By{' '}
+            <span className={styles['vercel-logo']}>
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width={100}
+                height={24}
+                priority
+              />
+            </span>
           </a>
         </div>
       </div>
-    </>
+
+      <div className={styles.center}>
+        <div className={styles.logo}>
+          <Image
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+        <div className={styles.thirteen}>
+          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h2 className={inter.className}>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p className={spaceGrotesk.className}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p className={spaceGrotesk.className}>
+            Learn about Next.js in an interactive course with quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={styles.card}
+        >
+          <h2 className={inter.className}>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p className={spaceGrotesk.className}>
+            Discover and deploy boilerplate example Next.js projects.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.card}
+        >
+          <h2 className={inter.className}>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p className={spaceGrotesk.className}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </div>
   )
 }
