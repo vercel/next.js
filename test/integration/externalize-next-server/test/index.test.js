9f0b9ad2183f4a06647044efa89b1b442e883b60
Jiachi Liu <inbox@huozhi.im>
2022-11-28 17:47:43 +0100

    fix tests

--- test/integration/externalize-next-server/test/index.test.js
+++ test/integration/externalize-next-server/test/index.test.js
@@ -10,27 +10,27 @@ describe('externalize next/dist/shared', () => {
     await nextBuild(appDir)
   })
 
-  it('Bundle next/dist/shared/lib/head.js but not next/dist/shared/lib/head-manager.context.js in _error', async () => {
+  it('Bundle next/dist/shared/lib/head.js but not next/dist/shared/lib/head-manager-context.js in _error', async () => {
     const content = readNextBuildServerPageFile(appDir, '/_error')
     expect(content).toMatch(
       new RegExp(
         '^' +
           escapeStringRegexp(
-            `module.exports = require("next/dist/shared/lib/head-context-manager.js");`
+            `module.exports = require("next/dist/shared/lib/head-manager-context.js");`
+          ) +
+          ';?$',
+        'm'
+      )
+    )
+    expect(content).not.toMatch(
+      new RegExp(
+        '^' +
+          escapeStringRegexp(
+            `module.exports = require("next/dist/shared/lib/head.js");`
           ) +
           ';?$',
         'm'
       )
     )
   })
-  expect(content).not.toMatch(
-    new RegExp(
-      '^' +
-        escapeStringRegexp(
-          `module.exports = require("next/dist/shared/lib/head.js");`
-        ) +
-        ';?$',
-      'm'
-    )
-  )
 })
