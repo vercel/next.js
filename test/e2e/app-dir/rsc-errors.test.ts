59466f8a15d8af0887568ea1d55b84981862a794
Shu Ding <g@shud.in>
2022-11-28 01:15:56 +0100

    Add gSP and gSSP checks for both server and client layers in the SWC transform (#43391)
    We currently have these as a build runtime check. This PR ensures that
    it happens during the SWC compilation time, and applies to both server
    and client entries.
    
    Also adds link to the docs:
    
    <img width="768" alt="CleanShot 2022-11-25 at 18 14 54@2x"
    src="https://user-images.githubusercontent.com/3676859/204031972-51447236-77f3-4c78-8e53-a7708aa3952a.png">
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [x] Integration tests added
    - [x] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the
    feature request has been accepted for implementation before opening a
    PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ]
    [e2e](https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs)
    tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `pnpm build && pnpm lint`
    - [ ] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- test/e2e/app-dir/rsc-errors.test.ts
+++ test/e2e/app-dir/rsc-errors.test.ts
@@ -52,7 +52,7 @@ describe('app dir - rsc errors', () => {
 
     expect(res.status).toBe(500)
     expect(await res.text()).toContain(
-      '`getServerSideProps` is not allowed in Client Components'
+      '"getServerSideProps\\" is not supported in app/'
     )
   })
 
@@ -79,7 +79,7 @@ describe('app dir - rsc errors', () => {
 
     expect(res.status).toBe(500)
     expect(await res.text()).toContain(
-      '`getStaticProps` is not allowed in Client Components'
+      '"getStaticProps\\" is not supported in app/'
     )
   })
 
