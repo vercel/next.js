237e594235b3ee5c918c4a0ea29dbf0949de232d
Mert Can Altın <mertgold60@gmail.com>
2022-12-04 02:13:26 +0300

    first commit revert & getStaticProps function refactored

--- examples/data-fetch/pages/index.tsx
+++ examples/data-fetch/pages/index.tsx
@@ -2,21 +2,31 @@ import Link from 'next/link'
 import type { InferGetStaticPropsType } from 'next'
 import type { Repository } from '../types/github'
 
-export const getStaticProps = async () => {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const data: Repository = await res.json()
-  return {
-    props: {
-      stars: data.stargazers_count,
-    },
+export async function getStaticProps() {
+  try {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const data: Repository = await res.json()
+    return {
+      props: {
+        stars: data.stargazers_count,
+      },
+    }
+  } catch (err) {
+    return {
+      props: {
+        error: err.message,
+      },
+    }
   }
 }
 
-const IndexPage = ({ stars }: InferGetStaticPropsType<typeof getStaticProps>) => (
-  <>
-    <p>Next.js has {stars} ⭐️</p>
-    <Link href="/preact-stars">How about preact?</Link>
-  </>
-)
-
-export default IndexPage
+export default function IndexPage({
+  stars,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <p>Next.js has {stars} ⭐️</p>
+      <Link href="/preact-stars">How about preact?</Link>
+    </>
+  )
+}
