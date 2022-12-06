5757bf2144faef60597f8df406aad5a5c5397747
Jan Kaifer <jan@kaifer.cz>
2022-12-06 10:24:59 +0100

    test imports

---
+++ test/e2e/app-dir/import/app/jsx/page.jsx
@@ -0,0 +1,15 @@
+import CompJs from '../../components/compJs'
+import CompJsx from '../../components/compJsx'
+import CompTs from '../../components/compTs'
+import CompTsx from '../../components/compTsx'
+
+export default function Page() {
+  return (
+    <>
+      <CompJs />
+      <CompJsx />
+      <CompTs />
+      <CompTsx />
+    </>
+  )
+}
