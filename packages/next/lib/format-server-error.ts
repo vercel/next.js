856b9459f8928fcf1f2837b1ed046afb55f28fd8
Hannes Bornö <hannes.borno@vercel.com>
2022-12-07 15:28:02 +0100

    Use error instead of link

--- packages/next/lib/format-server-error.ts
+++ packages/next/lib/format-server-error.ts
@@ -1,7 +1,7 @@
 export function formatServerError(error: Error): void {
   if (error.message.includes('createContext is not a function')) {
     const message =
-      'createContext only works in Client Components. Add the "use client" directive at the top of the file to use it. Read more: https://beta.nextjs.org/docs/rendering/server-and-client-components#context'
+      'createContext only works in Client Components. Add the "use client" directive at the top of the file to use it. Read more: https://nextjs.org/docs/messages/context-in-server-component'
     error.message = message
     if (error.stack) {
       const lines = error.stack.split('\n')
