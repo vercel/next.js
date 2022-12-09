9d03416cc588d3ce90f374369964a12757ff35ba
Will Binns-Smith <wbinnssmith@gmail.com>
2022-12-09 14:20:15 -0800

    Remove next_static_source (vercel/turbo#2971)

--- packages/next-swc/crates/next-dev/src/lib.rs
+++ packages/next-swc/crates/next-dev/src/lib.rs
@@ -331,11 +331,6 @@ async fn source(
     .into();
     let static_source =
         StaticAssetsContentSourceVc::new(String::new(), project_path.join("public")).into();
-    let next_static_source = StaticAssetsContentSourceVc::new(
-        "_next/static/".to_owned(),
-        project_path.join(".next/static"),
-    )
-    .into();
     let manifest_source = DevManifestContentSource {
         page_roots: vec![app_source, rendered_source],
     }
@@ -343,7 +338,6 @@ async fn source(
     .into();
     let main_source = CombinedContentSourceVc::new(vec![
         manifest_source,
-        next_static_source,
         static_source,
         app_source,
         rendered_source,
