ef016c1e3673870ba62640313cc450ed11233d92
Jiachi Liu <inbox@huozhi.im>
2022-12-09 15:08:23 +0100

    hoist log and add test for pure client case

--- packages/next/client/components/noop-head.tsx
+++ packages/next/client/components/noop-head.tsx
@@ -1,10 +1,11 @@
 import { warnOnce } from '../../shared/lib/utils/warn-once'
 
+if (process.env.NODE_ENV !== 'production') {
+  warnOnce(
+    `You're using \`next/head\` inside app directory, please migrate to \`head.js\`. Checkout https://beta.nextjs.org/docs/api-reference/file-conventions/head for details.`
+  )
+}
+
 export default function NoopHead() {
-  if (process.env.NODE_ENV !== 'production') {
-    warnOnce(
-      `You're using \`next/head\` inside app directory, please migrate to \`head.js\`. Checkout https://beta.nextjs.org/docs/api-reference/file-conventions/head for details.`
-    )
-  }
   return null
 }
