6dbcf1aba0ef3a2200b171b92d875554c3d8041f
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-12-09 17:46:26 +0100

    Move back event since is necessary

--- examples/with-google-analytics/lib/gtag.js
+++ examples/with-google-analytics/lib/gtag.js
@@ -6,3 +6,12 @@ export const pageview = (url) => {
     page_path: url,
   })
 }
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
