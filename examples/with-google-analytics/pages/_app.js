d7138c07fcab520d3fcf743ad27ef5d1f1208f38
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-12-12 16:33:17 +0100

    Solve webVitals issue with no _document.js

--- examples/with-google-analytics/pages/_app.js
+++ examples/with-google-analytics/pages/_app.js
@@ -1,7 +1,8 @@
-import { useEffect } from 'react'
-import Script from 'next/script'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
+import { useEffect } from 'react';
+import Script from 'next/script';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter()
@@ -17,18 +18,17 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <>
+    <Head>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        strategy="afterInteractive"
+      <script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+
+            // Disable any default-enabled advertising features (turn them on later when we get consent)
+            gtag('set', 'allow_google_signals', false);
 
             gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
@@ -36,8 +36,13 @@ const App = ({ Component, pageProps }) => {
           `,
         }}
       />
-      <Component {...pageProps} />
-    </>
+    </Head>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+    />
+    <Component {...pageProps} />;
+  </>
   )
 }
 
