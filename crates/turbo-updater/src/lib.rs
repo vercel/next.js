f1db13ebe560a933e13de0106cd2c6fccce68b81
Thomas Knickman <tom.knickman@vercel.com>
2022-12-15 13:14:04 -0500

    feat(turbo-updater): notifications per channel (vercel/turbo#3028)
    We can overload name because update-informer allows name in an
    owner/name format and then splits the name but includes the owner in the
    cached file. So we can do something like canary/turbo, latest/turbo etc.
    
    We should still send a fix upstream to support this more formally. But
    this should work for now.
    
    Updated version of https://github.com/vercel/turbo/pull/3017

--- crates/turbo-updater/src/lib.rs
+++ crates/turbo-updater/src/lib.rs
@@ -1,7 +1,8 @@
-use std::time::Duration;
+use std::{fmt, time::Duration};
 
 use colored::*;
 use is_terminal::IsTerminal;
+use semver::Version as SemVerVersion;
 use serde::Deserialize;
 use thiserror::Error as ThisError;
 use update_informer::{Check, Package, Registry, Result as UpdateResult, Version};
@@ -20,37 +21,60 @@ const ENVIRONMENTAL_DISABLE_VARS: [&str; 1] = ["CI"];
 pub enum UpdateNotifierError {
     #[error("Failed to write to terminal")]
     RenderError(#[from] ui::utils::GetDisplayLengthError),
+    #[error("Failed to parse current version")]
+    VersionError(#[from] semver::Error),
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct NpmVersionData {
     version: String,
+}
+
+#[derive(Debug)]
+enum VersionTag {
+    Latest,
+    Canary,
+}
+
+impl fmt::Display for VersionTag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            VersionTag::Latest => write!(f, "latest"),
+            VersionTag::Canary => write!(f, "canary"),
+        }
+    }
 }
 
 struct NPMRegistry;
 
 impl Registry for NPMRegistry {
-    const NAME: &'static str = "npm_registry";
+    const NAME: &'static str = "npm-registry";
     fn get_latest_version(
         pkg: &Package,
         version: &Version,
         timeout: Duration,
     ) -> UpdateResult<Option<String>> {
         // determine tag to request
-        let tag = match &version.get().pre {
-            t if t.contains("canary") => "canary",
-            t if t.contains("next") => "next",
-            _ => "latest",
-        };
-
+        let tag = get_tag_from_version(&version.get().pre);
+        // since we're overloading tag within name, we need to split it back out
+        let full_name = pkg.to_string();
+        let split_name: Vec<&str> = full_name.split('/').collect();
+        let name = split_name[1];
         let url = format!(
             "https://turbo.build/api/binaries/version?name={name}&tag={tag}",
-            name = pkg,
+            name = name,
             tag = tag
         );
         let resp = ureq::get(&url).timeout(timeout).call()?;
         let result = resp.into_json::<NpmVersionData>().unwrap();
         Ok(Some(result.version))
+    }
+}
+
+fn get_tag_from_version(pre: &semver::Prerelease) -> VersionTag {
+    match pre {
+        t if t.contains("canary") => VersionTag::Canary,
+        _ => VersionTag::Latest,
     }
 }
 
@@ -88,6 +112,14 @@ pub fn check_for_updates(
     if should_skip_notification() {
         return Ok(());
     }
+
+    // we want notifications per channel (latest, canary, etc) so we need to ensure
+    // we have one cached latest version per channel. UpdateInformer does not
+    // support this out of the box, so we hack it into the name by overloading
+    // owner (in the supported owner/name format) to be channel/name.
+    let parsed_version = SemVerVersion::parse(current_version)?;
+    let tag = get_tag_from_version(&parsed_version.pre);
+    let package_name = format!("{}/{}", tag, package_name);
 
     let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT);
     let interval = interval.unwrap_or(DEFAULT_INTERVAL);
