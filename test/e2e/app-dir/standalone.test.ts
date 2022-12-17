6b3442f9840d38e9d0dee4a9795033c72e4a0bae
Tim Neutkens <tim@timneutkens.nl>
2022-12-17 15:32:27 +0100

    Convert app-dir tests to createNextDescribe where possible

--- test/e2e/app-dir/standalone.test.ts
+++ test/e2e/app-dir/standalone.test.ts
@@ -1,4 +1,4 @@
-import { createNext, FileRef } from 'e2e-utils'
+import { createNextDescribe, FileRef } from 'e2e-utils'
 import { NextInstance } from 'test/lib/next-modes/base'
 import fs from 'fs-extra'
 import os from 'os'
@@ -10,37 +10,32 @@ import {
   killApp,
 } from 'next-test-utils'
 
-describe('output: standalone with app dir', () => {
-  let next: NextInstance
+createNextDescribe(
+  'output: standalone with app dir',
+  {
+    files: new FileRef(path.join(__dirname, 'app')),
+    dependencies: {
+      swr: '2.0.0-rc.0',
+      react: 'latest',
+      'react-dom': 'latest',
+      sass: 'latest',
+    },
+    skipStart: true,
+  },
+  ({ next, isNextStart }) => {
+    if (!isNextStart) {
+      it('should skip for non-next start', () => {})
+      return
+    }
 
-  if (!(global as any).isNextStart) {
-    it('should skip for non-next start', () => {})
-    return
-  }
-
-  beforeAll(async () => {
-    next = await createNext({
-      files: new FileRef(path.join(__dirname, 'app')),
-      dependencies: {
-        swr: '2.0.0-rc.0',
-        react: 'latest',
-        'react-dom': 'latest',
-        sass: 'latest',
-      },
-      skipStart: true,
+    beforeAll(async () => {
+      await next.patchFile(
+        'next.config.js',
+        (await next.readFile('next.config.js')).replace('// output', 'output')
+      )
+      await next.start()
     })
 
-    await next.patchFile(
-      'next.config.js',
-      (await next.readFile('next.config.js')).replace('// output', 'output')
-    )
-    await next.start()
-  })
-  afterAll(async () => {
-    await next.destroy()
-  })
-
-  if ((global as any).isNextStart) {
     it('should handle trace files correctly for route groups (nodejs only)', async () => {
       expect(next.cliOutput).not.toContain('Failed to copy traced files')
       const serverDirPath = path.join(
@@ -67,48 +62,48 @@ describe('output: standalone with app dir', () => {
         }
       }
     })
-  }
 
-  it('should work correctly with output standalone', async () => {
-    const tmpFolder = path.join(os.tmpdir(), 'next-standalone-' + Date.now())
-    await fs.move(path.join(next.testDir, '.next/standalone'), tmpFolder)
-    let server: any
+    it('should work correctly with output standalone', async () => {
+      const tmpFolder = path.join(os.tmpdir(), 'next-standalone-' + Date.now())
+      await fs.move(path.join(next.testDir, '.next/standalone'), tmpFolder)
+      let server: any
 
-    try {
-      const testServer = path.join(tmpFolder, 'server.js')
-      const appPort = await findPort()
-      server = await initNextServerScript(
-        testServer,
-        /Listening on/,
-        {
-          ...process.env,
-          PORT: appPort,
-        },
-        undefined,
-        {
-          cwd: tmpFolder,
+      try {
+        const testServer = path.join(tmpFolder, 'server.js')
+        const appPort = await findPort()
+        server = await initNextServerScript(
+          testServer,
+          /Listening on/,
+          {
+            ...process.env,
+            PORT: appPort,
+          },
+          undefined,
+          {
+            cwd: tmpFolder,
+          }
+        )
+
+        for (const testPath of [
+          '/',
+          '/api/hello',
+          '/blog/first',
+          '/dashboard',
+          '/dashboard/another',
+          '/dashboard/changelog',
+          '/dashboard/deployments/breakdown',
+          '/dashboard/deployments/123',
+          '/dashboard/hello',
+          '/dashboard/project/123',
+          '/catch-all/first',
+        ]) {
+          const res = await fetchViaHTTP(appPort, testPath)
+          expect(res.status).toBe(200)
         }
-      )
-
-      for (const testPath of [
-        '/',
-        '/api/hello',
-        '/blog/first',
-        '/dashboard',
-        '/dashboard/another',
-        '/dashboard/changelog',
-        '/dashboard/deployments/breakdown',
-        '/dashboard/deployments/123',
-        '/dashboard/hello',
-        '/dashboard/project/123',
-        '/catch-all/first',
-      ]) {
-        const res = await fetchViaHTTP(appPort, testPath)
-        expect(res.status).toBe(200)
+      } finally {
+        if (server) await killApp(server)
+        await fs.remove(tmpFolder)
       }
-    } finally {
-      if (server) await killApp(server)
-      await fs.remove(tmpFolder)
-    }
-  })
-})
+    })
+  }
+)
