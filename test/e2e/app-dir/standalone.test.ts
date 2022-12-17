2532228ba7d572e741b461e616bc3333bb02eaa1
Tim Neutkens <tim@timneutkens.nl>
2022-12-17 19:53:03 +0100

    Fix error in standalone

--- test/e2e/app-dir/standalone.test.ts
+++ test/e2e/app-dir/standalone.test.ts
@@ -1,5 +1,4 @@
 import { createNextDescribe, FileRef } from 'e2e-utils'
-import { NextInstance } from 'test/lib/next-modes/base'
 import fs from 'fs-extra'
 import os from 'os'
 import path from 'path'
@@ -10,100 +9,102 @@ import {
   killApp,
 } from 'next-test-utils'
 
-createNextDescribe(
-  'output: standalone with app dir',
-  {
-    files: new FileRef(path.join(__dirname, 'app')),
-    dependencies: {
-      swr: '2.0.0-rc.0',
-      react: 'latest',
-      'react-dom': 'latest',
-      sass: 'latest',
+if (!(globalThis as any).isNextStart) {
+  it('should skip for non-next start', () => {})
+} else {
+  createNextDescribe(
+    'output: standalone with app dir',
+    {
+      files: new FileRef(path.join(__dirname, 'app')),
+      dependencies: {
+        swr: '2.0.0-rc.0',
+        react: 'latest',
+        'react-dom': 'latest',
+        sass: 'latest',
+      },
+      skipStart: true,
     },
-    skipStart: true,
-  },
-  ({ next, isNextStart }) => {
-    if (!isNextStart) {
-      it('should skip for non-next start', () => {})
-      return
-    }
-
-    beforeAll(async () => {
-      await next.patchFile(
-        'next.config.js',
-        (await next.readFile('next.config.js')).replace('// output', 'output')
-      )
-      await next.start()
-    })
-
-    it('should handle trace files correctly for route groups (nodejs only)', async () => {
-      expect(next.cliOutput).not.toContain('Failed to copy traced files')
-      const serverDirPath = path.join(
-        next.testDir,
-        '.next/standalone/.next/server'
-      )
-      for (const page of [
-        '(newroot)/dashboard/another',
-        '(newroot)/dashboard/project/[projectId]',
-        '(rootonly)/dashboard/changelog',
-      ]) {
-        const pagePath = path.join(serverDirPath, 'app', page)
-
-        expect(
-          await fs.pathExists(path.join(pagePath, 'page.js.nft.json'))
-        ).toBe(true)
-
-        const files = (
-          await fs.readJSON(path.join(pagePath, 'page.js.nft.json'))
-        ).files as string[]
-
-        for (const file of files) {
-          expect(await fs.pathExists(path.join(pagePath, file))).toBe(true)
-        }
-      }
-    })
-
-    it('should work correctly with output standalone', async () => {
-      const tmpFolder = path.join(os.tmpdir(), 'next-standalone-' + Date.now())
-      await fs.move(path.join(next.testDir, '.next/standalone'), tmpFolder)
-      let server: any
-
-      try {
-        const testServer = path.join(tmpFolder, 'server.js')
-        const appPort = await findPort()
-        server = await initNextServerScript(
-          testServer,
-          /Listening on/,
-          {
-            ...process.env,
-            PORT: appPort,
-          },
-          undefined,
-          {
-            cwd: tmpFolder,
-          }
+    ({ next }) => {
+      beforeAll(async () => {
+        await next.patchFile(
+          'next.config.js',
+          (await next.readFile('next.config.js')).replace('// output', 'output')
         )
+        await next.start()
+      })
 
-        for (const testPath of [
-          '/',
-          '/api/hello',
-          '/blog/first',
-          '/dashboard',
-          '/dashboard/another',
-          '/dashboard/changelog',
-          '/dashboard/deployments/breakdown',
-          '/dashboard/deployments/123',
-          '/dashboard/hello',
-          '/dashboard/project/123',
-          '/catch-all/first',
+      it('should handle trace files correctly for route groups (nodejs only)', async () => {
+        expect(next.cliOutput).not.toContain('Failed to copy traced files')
+        const serverDirPath = path.join(
+          next.testDir,
+          '.next/standalone/.next/server'
+        )
+        for (const page of [
+          '(newroot)/dashboard/another',
+          '(newroot)/dashboard/project/[projectId]',
+          '(rootonly)/dashboard/changelog',
         ]) {
-          const res = await fetchViaHTTP(appPort, testPath)
-          expect(res.status).toBe(200)
+          const pagePath = path.join(serverDirPath, 'app', page)
+
+          expect(
+            await fs.pathExists(path.join(pagePath, 'page.js.nft.json'))
+          ).toBe(true)
+
+          const files = (
+            await fs.readJSON(path.join(pagePath, 'page.js.nft.json'))
+          ).files as string[]
+
+          for (const file of files) {
+            expect(await fs.pathExists(path.join(pagePath, file))).toBe(true)
+          }
         }
-      } finally {
-        if (server) await killApp(server)
-        await fs.remove(tmpFolder)
-      }
-    })
-  }
-)
+      })
+
+      it('should work correctly with output standalone', async () => {
+        const tmpFolder = path.join(
+          os.tmpdir(),
+          'next-standalone-' + Date.now()
+        )
+        await fs.move(path.join(next.testDir, '.next/standalone'), tmpFolder)
+        let server: any
+
+        try {
+          const testServer = path.join(tmpFolder, 'server.js')
+          const appPort = await findPort()
+          server = await initNextServerScript(
+            testServer,
+            /Listening on/,
+            {
+              ...process.env,
+              PORT: appPort,
+            },
+            undefined,
+            {
+              cwd: tmpFolder,
+            }
+          )
+
+          for (const testPath of [
+            '/',
+            '/api/hello',
+            '/blog/first',
+            '/dashboard',
+            '/dashboard/another',
+            '/dashboard/changelog',
+            '/dashboard/deployments/breakdown',
+            '/dashboard/deployments/123',
+            '/dashboard/hello',
+            '/dashboard/project/123',
+            '/catch-all/first',
+          ]) {
+            const res = await fetchViaHTTP(appPort, testPath)
+            expect(res.status).toBe(200)
+          }
+        } finally {
+          if (server) await killApp(server)
+          await fs.remove(tmpFolder)
+        }
+      })
+    }
+  )
+}
