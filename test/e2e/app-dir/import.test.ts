ef21b622fcd0baaf34860c8ef7ffebce12f8dda4
Tim Neutkens <tim@timneutkens.nl>
2022-12-17 09:41:10 +0100

    Refactor import.test.ts to use createNextDescribe

--- test/e2e/app-dir/import.test.ts
+++ test/e2e/app-dir/import.test.ts
@@ -1,30 +1,24 @@
 import path from 'path'
-import cheerio from 'cheerio'
-import { createNext, FileRef } from 'e2e-utils'
-import { NextInstance } from 'test/lib/next-modes/base'
-import { renderViaHTTP } from 'next-test-utils'
+import { createNextDescribe } from 'e2e-utils'
 
-describe('app dir imports', () => {
-  let next: NextInstance
-
-  beforeAll(async () => {
-    next = await createNext({
-      files: new FileRef(path.join(__dirname, 'import')),
-      dependencies: {
-        react: 'latest',
-        'react-dom': 'latest',
-        typescript: 'latest',
-        '@types/react': 'latest',
-        '@types/node': 'latest',
-      },
+createNextDescribe(
+  'app dir imports',
+  {
+    files: path.join(__dirname, 'import'),
+    dependencies: {
+      react: 'latest',
+      'react-dom': 'latest',
+      typescript: 'latest',
+      '@types/react': 'latest',
+      '@types/node': 'latest',
+    },
+  },
+  ({ next }) => {
+    ;['js', 'jsx', 'ts', 'tsx'].forEach((ext) => {
+      it(`we can import all components from .${ext}`, async () => {
+        const $ = await next.render$(`/${ext}`)
+        expect($('#js').text()).toBe('CompJs')
+      })
     })
-  })
-  afterAll(() => next.destroy())
-  ;['js', 'jsx', 'ts', 'tsx'].forEach((ext) => {
-    it(`we can import all components from .${ext}`, async () => {
-      const html = await renderViaHTTP(next.url, `/${ext}`)
-      const $ = cheerio.load(html)
-      expect($('#js').text()).toBe('CompJs')
-    })
-  })
-})
+  }
+)
