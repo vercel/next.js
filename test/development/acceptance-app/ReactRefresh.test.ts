69d11e2bc512474c92ce805b6fca201f2dc2e414
Tim Neutkens <tim@timneutkens.nl>
2022-12-18 19:31:28 +0100

    Debug

--- test/development/acceptance-app/ReactRefresh.test.ts
+++ test/development/acceptance-app/ReactRefresh.test.ts
@@ -24,7 +24,7 @@ describe('ReactRefresh app', () => {
   })
   afterAll(() => next.destroy())
 
-  test('can edit a component without losing state', async () => {
+  test.only('can edit a component without losing state', async () => {
     const { session, cleanup } = await sandbox(next)
     await session.patch(
       'index.js',
