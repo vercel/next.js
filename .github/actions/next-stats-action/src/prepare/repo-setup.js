ddb594e9fda5feea17e8c6e804275610158ed73f
Jan Kaifer <jan@kaifer.cz>
2022-12-19 12:06:26 +0100

    use turbo package from next.js project

--- .github/actions/next-stats-action/src/prepare/repo-setup.js
+++ .github/actions/next-stats-action/src/prepare/repo-setup.js
@@ -191,7 +191,7 @@ module.exports = (actionInfo) => {
                   .traceAsyncFn(async () => {
                     const { pkgPath } = pkgDatas.get(pkgName)
                     await exec(
-                      `cd ${pkgPath} && turbo run test-pack --cache-dir="${turboCacheLocation}"`,
+                      `cd ${pkgPath} && pnpm run --dir=${nextjsRepoRoot} turbo run test-pack --cache-dir="${turboCacheLocation}"`,
                       true
                     )
                   })
