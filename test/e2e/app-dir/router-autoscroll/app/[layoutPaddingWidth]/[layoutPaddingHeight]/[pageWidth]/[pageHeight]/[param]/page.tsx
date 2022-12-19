3f04e47586d3d7dd2cbcc6f8f8417337d905aafa
Jan Kaifer <jan@kaifer.cz>
2022-12-19 13:44:47 +0100

    improve test example for easier visual inspection

--- test/e2e/app-dir/router-autoscroll/app/[layoutPaddingWidth]/[layoutPaddingHeight]/[pageWidth]/[pageHeight]/[param]/page.tsx
+++ test/e2e/app-dir/router-autoscroll/app/[layoutPaddingWidth]/[layoutPaddingHeight]/[pageWidth]/[pageHeight]/[param]/page.tsx
@@ -1,9 +1,10 @@
 export default function Page() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16)
   return (
     <div
       id="page"
       style={{
-        background: 'green',
+        background: `#${randomColor}`,
         flexGrow: 1,
       }}
     />
