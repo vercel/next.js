b4da5e00a80aac89bcfb8383db33787084ae3389
Tobias Koppers <tobias.koppers@googlemail.com>
2022-12-21 20:44:18 +0100

    update to latest next.js (vercel/turbo#3112)
    * add GlobalError export to layout entry (see
    https://github.com/vercel/next.js/pull/44066)
    * update app manifest for CSS (see
    https://github.com/vercel/next.js/pull/44168)
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/next-swc/crates/next-core/js/src/entry/app/layout-entry.tsx
+++ packages/next-swc/crates/next-core/js/src/entry/app/layout-entry.tsx
@@ -1,6 +1,7 @@
 export { default as AppRouter } from "next/dist/client/components/app-router.js";
 export { default as LayoutRouter } from "next/dist/client/components/layout-router.js";
 export { default as RenderFromTemplateContext } from "next/dist/client/components/render-from-template-context.js";
+export { default as GlobalError } from "next/dist/client/components/error-boundary.js";
 export { staticGenerationAsyncStorage } from "next/dist/esm/client/components/static-generation-async-storage.js";
 export { requestAsyncStorage } from "next/dist/esm/client/components/request-async-storage.js";
 import * as serverHooks from "next/dist/esm/client/components/hooks-server-context.js";
