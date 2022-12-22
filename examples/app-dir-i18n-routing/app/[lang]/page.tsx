2ab6d0be47e955839e3db19412d10848ce9e9dfc
Jan Kaifer <jan@kaifer.cz>
2022-12-22 10:21:43 +0100

    Added redirect from /

---
+++ examples/app-dir-i18n-routing/app/[lang]/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation'
+
+export default function Page() {
+  // We redorect to this path and let our middleware to prepend correct locale
+  redirect('/some/custom/path')
+}
