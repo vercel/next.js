85ce4fa81314f9ae12cab338db994ef3a42155a5
Jan Kaifer <jan@kaifer.cz>
2022-12-22 10:34:53 +0100

    fix lint error

--- examples/app-dir-i18n-routing/app/[lang]/page.tsx
+++ examples/app-dir-i18n-routing/app/[lang]/page.tsx
@@ -3,4 +3,5 @@ import { redirect } from 'next/navigation'
 export default function Page() {
   // We redorect to this path and let our middleware to prepend correct locale
   redirect('/some/custom/path')
+  return null
 }
