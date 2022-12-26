4f88a492405cb7a2c86134889127be139cc78f10
Jan Kaifer <jan@kaifer.cz>
2022-12-26 14:06:08 +0100

    Update examples/app-dir-i18n-routing/app/[lang]/page.tsx
    Co-authored-by: Vu Van Dung <joulev.vvd@yahoo.com>

--- examples/app-dir-i18n-routing/app/[lang]/page.tsx
+++ examples/app-dir-i18n-routing/app/[lang]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 
 export default function Page() {
-  // We redorect to this path and let our middleware to prepend correct locale
+  // We redirect to this path and let our middleware prepend the correct locale
   redirect('/some/custom/path')
   return null
 }
