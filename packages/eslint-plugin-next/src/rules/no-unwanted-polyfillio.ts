553fad7253d8d94dcefd4bcf3af004fd5a64fe2d
Sloane Sturzenegger <sloane@increase.com>
2022-12-29 14:59:46 -0800

    Polyfill Array.prototype.at

--- packages/eslint-plugin-next/src/rules/no-unwanted-polyfillio.ts
+++ packages/eslint-plugin-next/src/rules/no-unwanted-polyfillio.ts
@@ -3,6 +3,7 @@ import { defineRule } from '../utils/define-rule'
 // Keep in sync with next.js polyfills file : https://github.com/vercel/next.js/blob/master/packages/next-polyfill-nomodule/src/index.js
 const NEXT_POLYFILLED_FEATURES = [
   'Array.prototype.@@iterator',
+  'Array.prototype.at',
   'Array.prototype.copyWithin',
   'Array.prototype.fill',
   'Array.prototype.find',
