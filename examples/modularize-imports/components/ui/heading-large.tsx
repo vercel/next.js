28ba45989139cd6dda5478763eab6c43a251dd96
Shusaku Uesugi <shu.chibicode@gmail.com>
2023-01-03 06:19:45 -0800

    Illustrate skipDefaultConversion

--- examples/modularize-imports/components/ui/heading-large.tsx
+++ examples/modularize-imports/components/ui/heading-large.tsx
@@ -1,7 +1,3 @@
-export default function HeadingLarge({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export function HeadingLarge({ children }: { children: React.ReactNode }) {
   return <h1>{children}</h1>
 }
--- examples/modularize-imports/next.config.js
+++ examples/modularize-imports/next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
     '../components/ui': {
       // Use `kebabCase` helper to convert `HeadingLarge` to `heading-large`
       transform: '../components/ui/{{ kebabCase member }}',
+      // Use `skipDefaultConversion`, since `HeadingLarge` is exported
+      // using named exports instead of default exports
+      skipDefaultConversion: true,
     },
   },
 }
