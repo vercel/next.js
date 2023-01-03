3014a91ed0b5abff475441747534c5768214b92e
Jan Kaifer <jan@kaifer.cz>
2023-01-03 16:30:31 +0100

    Added simple custom server test

---
+++ test/production/custom-server/custom-server.test.ts
@@ -0,0 +1,19 @@
+import { createNextDescribe } from 'e2e-utils'
+
+createNextDescribe(
+  'custom server',
+  {
+    files: __dirname,
+    dependencies: {
+      http: 'latest',
+      url: 'latest',
+    },
+    startCommand: 'node server.js',
+  },
+  ({ next }) => {
+    it.each(['a', 'b', 'c'])('can navigate to /%s', async (page) => {
+      const $ = await next.render$(`/${page}`)
+      expect($('p').text()).toBe(`Page ${page}`)
+    })
+  }
+)
