05bf838464056a7e7eb788180a1bcde5f6ddc298
Hannes Bornö <hannes.borno@vercel.com>
2023-01-03 17:29:25 +0100

    Add tests

--- test/development/acceptance-app/server-components.test.ts
+++ test/development/acceptance-app/server-components.test.ts
@@ -162,4 +162,136 @@ describe('Error Overlay for server components', () => {
       await cleanup()
     })
   })
+
+  describe.only('React component hooks called in Server Component', () => {
+    it('should show error when React.<client-hook> is called', async () => {
+      const { session, cleanup } = await sandbox(
+        next,
+        new Map([
+          [
+            'app/page.js',
+            `
+        import React from 'react'
+        export default function Page() {
+          const ref = React.useRef()
+          return "Hello world" 
+        }`,
+          ],
+        ])
+      )
+
+      expect(await session.hasRedbox(true)).toBe(true)
+
+      await check(async () => {
+        expect(await session.getRedboxSource(true)).toContain(
+          `Error: useRef only works in Client Components. Add the "use client" directive at the top of the file to use it. Read more: https://nextjs.org/docs/messages/react-client-hook-in-server-component`
+        )
+        return 'success'
+      }, 'success')
+
+      expect(next.cliOutput).toContain('useRef only works in Client Components')
+
+      await cleanup()
+    })
+
+    it('should show error when React.<client-hook> is called in external package', async () => {
+      const { session, cleanup } = await sandbox(
+        next,
+        new Map([
+          [
+            'node_modules/my-package/index.js',
+            `
+          const React = require('react')
+          module.exports = function Component() {
+            const [state, useState] = React.useState()
+            return "Hello world"
+          }
+        `,
+          ],
+          [
+            'node_modules/my-package/package.json',
+            `
+          {
+            "name": "my-package",
+            "version": "0.0.1"
+          }
+        `,
+          ],
+          [
+            'app/page.js',
+            `
+        import Component from 'my-package'
+        export default function Page() {
+          return <Component />
+        }`,
+          ],
+        ])
+      )
+
+      expect(await session.hasRedbox(true)).toBe(true)
+
+      await check(async () => {
+        expect(await session.getRedboxSource(true)).toContain(
+          `Error: useState only works in Client Components. Add the "use client" directive at the top of the file to use it. Read more: https://nextjs.org/docs/messages/react-client-hook-in-server-component`
+        )
+        return 'success'
+      }, 'success')
+
+      expect(next.cliOutput).toContain(
+        'useState only works in Client Components'
+      )
+
+      await cleanup()
+    })
+
+    it('should show error when React client hook is called in external package', async () => {
+      const { session, cleanup } = await sandbox(
+        next,
+        new Map([
+          [
+            'node_modules/my-package/index.js',
+            `
+          const { useEffect } = require('react')
+          module.exports = function Component() {
+            useEffect(() => {}, [])
+            return "Hello world"
+          }
+        `,
+          ],
+          [
+            'node_modules/my-package/package.json',
+            `
+          {
+            "name": "my-package",
+            "version": "0.0.1"
+          }
+        `,
+          ],
+          [
+            'app/page.js',
+            `
+            import Component from 'my-package'
+            export default function Page() {
+              return <Component />
+            }`,
+          ],
+        ])
+      )
+
+      expect(await session.hasRedbox(true)).toBe(true)
+
+      await check(async () => {
+        expect(await session.getRedboxSource(true)).toContain(
+          `Error: useEffect only works in Client Components. Add the "use client" directive at the top of the file to use it. Read more: https://nextjs.org/docs/messages/react-client-hook-in-server-component`
+        )
+        return 'success'
+      }, 'success')
+
+      expect(next.cliOutput).toContain(
+        'useEffect only works in Client Components'
+      )
+
+      await cleanup()
+    })
+  })
 })
