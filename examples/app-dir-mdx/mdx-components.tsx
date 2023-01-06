4795d52755d5aa74aa0d910634162f148bcd1a0f
Tim Neutkens <tim@timneutkens.nl>
2023-01-06 12:56:56 +0100

    Add support for rendering MDX as a server component

---
+++ examples/app-dir-mdx/mdx-components.tsx
@@ -0,0 +1,9 @@
+export function useMDXComponents(components: {
+  [component: string]: React.ComponentType
+}) {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    ...components,
+  }
+}
