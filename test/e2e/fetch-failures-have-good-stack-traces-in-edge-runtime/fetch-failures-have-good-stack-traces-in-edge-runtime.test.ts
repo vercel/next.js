2d8156e6be3f61ecd3e83598c9c005ea78aaf5eb
Gal Schlezinger <gal@spitfire.co.il>
2023-01-10 18:17:26 +0200

    use red box helpers and test both dev and start

--- test/e2e/fetch-failures-have-good-stack-traces-in-edge-runtime/fetch-failures-have-good-stack-traces-in-edge-runtime.test.ts
+++ test/e2e/fetch-failures-have-good-stack-traces-in-edge-runtime/fetch-failures-have-good-stack-traces-in-edge-runtime.test.ts
@@ -1,18 +1,34 @@
 import { createNextDescribe } from 'e2e-utils'
+import webdriver from 'next-webdriver'
+import {
+  hasRedbox,
+  getRedboxSource,
+  getRedboxDescription,
+} from 'next-test-utils'
 
 createNextDescribe(
   'fetch failures have good stack traces in edge runtime',
   {
     files: __dirname,
   },
-  ({ next }) => {
+  ({ next, isNextStart, isNextDev }) => {
     it('when awaiting `fetch` using an unknown domain, stack traces are preserved', async () => {
-      const res = await next.fetch('/api/unknown-domain')
-      expect(res.status).toBe(500)
-      const html = await res.text()
-      expect(html).toContain(`(middleware)/./pages/api/unknown-domain.js`)
-      expect(html).toContain(`(middleware)/./src/fetcher.js`)
-      expect(html).toContain(`anotherFetcher`)
+      const browser = await webdriver(next.appPort, '/api/unknown-domain')
+
+      if (isNextStart) {
+        expect(next.cliOutput).toMatch(/at.+\/pages\/api\/unknown-domain.js/)
+      } else if (isNextDev) {
+        expect(next.cliOutput).toContain('src/fetcher.js')
+
+        expect(await hasRedbox(browser, true)).toBe(true)
+        const source = await getRedboxSource(browser)
+
+        expect(source).toContain('async function anotherFetcher(...args)')
+        expect(source).toContain(`fetch(...args)`)
+
+        const description = await getRedboxDescription(browser)
+        expect(description).toEqual('TypeError: fetch failed')
+      }
     })
   }
 )
