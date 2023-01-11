a0d39e929f075603709aa6aeb6a3e21db366834c
Tim Neutkens <tim@timneutkens.nl>
2023-01-11 16:00:23 +0100

    Fix state reset / suspense triggering on router.refresh()

--- packages/next/src/server/app-render.tsx
+++ packages/next/src/server/app-render.tsx
@@ -1342,7 +1342,6 @@ export async function renderToHTMLOrFlight(
                   notFound={NotFound ? <NotFound /> : undefined}
                   notFoundStyles={notFoundStyles}
                   childProp={childProp}
-                  rootLayoutIncluded={rootLayoutIncludedAtThisLevelOrAbove}
                 />,
               ]
             }
