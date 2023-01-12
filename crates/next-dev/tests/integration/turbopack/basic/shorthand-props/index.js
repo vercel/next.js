36f2caae07115d8d3adc9769d7ccea0873641de3
Tobias Koppers <tobias.koppers@googlemail.com>
2023-01-12 09:09:01 +0100

    fix code generation in shorthand properties (vercel/turbo#3259)

---
+++ crates/next-dev/tests/integration/turbopack/basic/shorthand-props/index.js
@@ -0,0 +1,52 @@
+import def, { named } from "./module.js";
+import {
+  nested,
+  nested2,
+  nested_with_identity,
+  nested_with_identity2,
+  double_nested,
+  double_nested2,
+  double_nested_with_identity,
+  double_nested_with_identity2,
+} from "./reexport.js";
+
+it("support imports in shorthand properties", () => {
+  expect(def).toBe("default");
+  expect(named).toBe("named");
+  expect({ def }).toStrictEqual({ def: "default" });
+  expect({ named }).toStrictEqual({ named: "named" });
+  expect(nested).toStrictEqual({ def: "default", named: "named" });
+  expect(nested2).toStrictEqual({ def: "default", named: "named" });
+  expect(nested_with_identity).toStrictEqual({
+    def: "default",
+    named: "named",
+  });
+  expect(nested_with_identity2).toStrictEqual({
+    def: "default",
+    named: "named",
+  });
+  expect(double_nested).toStrictEqual({
+    nested: {
+      def: "default",
+      named: "named",
+    },
+  });
+  expect(double_nested2).toStrictEqual({
+    nested: {
+      def: "default",
+      named: "named",
+    },
+  });
+  expect(double_nested_with_identity).toStrictEqual({
+    nested: {
+      def: "default",
+      named: "named",
+    },
+  });
+  expect(double_nested_with_identity2).toStrictEqual({
+    nested: {
+      def: "default",
+      named: "named",
+    },
+  });
+});
