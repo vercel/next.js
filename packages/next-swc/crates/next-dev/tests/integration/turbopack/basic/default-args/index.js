2df0f4838e69f8dfba6336d18ff6e3e4270c5030
Tobias Koppers <tobias.koppers@googlemail.com>
2023-01-12 17:34:08 +0100

    fix imports in default arguments in functions (vercel/turbo#3292)

---
+++ packages/next-swc/crates/next-dev/tests/integration/turbopack/basic/default-args/index.js
@@ -0,0 +1,9 @@
+import { named } from "./module.js";
+
+function Fun({ value = named }) {
+  return value;
+}
+
+it("support imports in default arguments", () => {
+  expect(Fun({})).toBe("named");
+});
