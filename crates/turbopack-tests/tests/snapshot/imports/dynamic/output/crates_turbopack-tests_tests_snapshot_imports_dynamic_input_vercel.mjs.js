031deb51ce6ad178a6ddbba3e8810dbce05c9430
Justin Ridgewell <justin@ridgewell.name>
2023-01-13 00:48:52 -0500

    Fix deduplication when importing module statically and dynamically (vercel/turbo#3297)
    The `processed_assets` `IndexSet` was performing deduplication on the `AssetVc`, which is the same between static (`import ... from "foo"`) and dynamic (`import("foo")`) imports of the same specifier. But, the _reference_ to those assets is different, and generates different chunking semantics. In order for both to succeed, we need to process both assets.
    
    Coupled with #3193's ability to load chunks on the server side, this fixes fixes WEB-381.

---
+++ crates/turbopack-tests/tests/snapshot/imports/dynamic/output/crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs.js
@@ -0,0 +1,14 @@
+(self.TURBOPACK = self.TURBOPACK || []).push(["output/crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs.js", {
+
+"[project]/crates/turbopack-tests/tests/snapshot/imports/dynamic/input/vercel.mjs (ecmascript)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, p: process, g: global, __dirname }) => (() => {
+
+__turbopack_esm__({
+    "default": ()=>__TURBOPACK__default__export__
+});
+const __TURBOPACK__default__export__ = "turbopack";
+
+})()),
+}]);
+
+
+//# sourceMappingURL=crates_turbopack-tests_tests_snapshot_imports_dynamic_input_vercel.mjs.js.map
