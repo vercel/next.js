1d3e78284197d58eb31988ad7ec8f67794a2caa2
Leah <github.leah@hrmny.sh>
2023-01-19 21:54:47 +0100

    add full next.js config (vercel/turbo#3343)

--- packages/next-swc/crates/next-core/js/src/entry/config/next.js
+++ packages/next-swc/crates/next-core/js/src/entry/config/next.js
@@ -3,8 +3,16 @@ import { PHASE_DEVELOPMENT_SERVER } from "next/dist/shared/lib/constants";
 
 const loadNextConfig = async () => {
   const nextConfig = await loadConfig(PHASE_DEVELOPMENT_SERVER, process.cwd());
-  nextConfig.rewrites = await nextConfig.rewrites?.();
+
+  nextConfig.generateBuildId = await nextConfig.generateBuildId?.();
+  nextConfig.headers = await nextConfig.headers?.();
   nextConfig.redirects = await nextConfig.redirects?.();
+  nextConfig.rewrites = await nextConfig.rewrites?.();
+
+  // TODO: these functions takes arguments, have to be supported in a different way
+  nextConfig.exportPathMap = nextConfig.exportPathMap && {};
+  nextConfig.webpack = nextConfig.webpack && {};
+
   return nextConfig;
 };
 
