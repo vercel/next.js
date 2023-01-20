e0865b10e06f258e0bd6297965de58490a961a8d
JJ Kasper <jj@jjsweb.site>
2023-01-20 11:57:18 -0800

    Revert "Initial support for metadata (#44729)" (#45111)

---
+++ packages/next/src/client/components/head.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function DefaultHead() {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  )
+}
