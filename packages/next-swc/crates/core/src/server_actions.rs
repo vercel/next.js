84e49a8c0bc0ca3ec39dfb2f73ed3e8a1a4b1ef9
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-20 16:33:10 +0900

    init visitor

--- packages/next-swc/crates/core/src/server_actions.rs
+++ packages/next-swc/crates/core/src/server_actions.rs
@@ -1,5 +1,18 @@
+use next_binding::swc::core::ecma::visit::{as_folder, noop_visit_mut_type, Fold, VisitMut};
 use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct Config {}
+
+pub fn server_actions(config: Config) -> impl VisitMut + Fold {
+    as_folder(ServerActions { config })
+}
+
+struct ServerActions {
+    config: Config,
+}
+
+impl VisitMut for ServerActions {
+    noop_visit_mut_type!();
+}
