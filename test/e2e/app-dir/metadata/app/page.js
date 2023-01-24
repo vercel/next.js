2e251cb85fd4104aa473bb2538276bb671c63d65
JJ Kasper <jj@jjsweb.site>
2023-01-24 05:21:59 -0800

    Revert "Revert "Revert "Revert "Initial support for metadata (#44729)"" (#45113)"" (#45196)
    Hopefully last time
    
    Reverts vercel/next.js#45130

---
+++ test/e2e/app-dir/metadata/app/page.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <>
+      <p id="index">index page</p>
+
+      <Link href="/basic" id="to-basic">
+        to /basic
+      </Link>
+      <br />
+
+      <Link href="/title" id="to-title">
+        to /title
+      </Link>
+      <br />
+    </>
+  )
+}
+
+export const metadata = {
+  title: 'index page',
+}
