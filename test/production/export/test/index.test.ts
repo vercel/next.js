b758dbb11577208a209bb9fb5390337fc3185796
Jan Kaifer <jan@kaifer.cz>
2023-01-24 13:32:36 +0100

    fixup ssr

--- test/production/export/test/index.test.ts
+++ test/production/export/test/index.test.ts
@@ -1,14 +1,8 @@
 import path from 'path'
 import { createNextDescribe } from 'e2e-utils'
-
-import { startStaticServer } from 'next-test-utils'
+import { renderViaHTTP, startStaticServer } from 'next-test-utils'
 import { AddressInfo, Server } from 'net'
-
-import ssr from './ssr'
-import browser from './browser'
-import dev from './dev'
-import dynamic from './dynamic'
-import apiRoutes from './api-routes'
+import cheerio from 'cheerio'
 
 const files = path.join(__dirname, '..')
 
@@ -21,7 +15,9 @@ createNextDescribe(
     const outdir = 'out'
     const outNoTrailSlash = 'outNoTrailSlash'
     let server: Server
+    let port: number
     let serverNoTrailSlash: Server
+    let portNoTrailSlash: number
 
     beforeAll(async () => {
       const nextConfigPath = 'next.config.js'
@@ -44,6 +40,8 @@ createNextDescribe(
       serverNoTrailSlash = await startStaticServer(
         path.join(next.testDir, outNoTrailSlash)
       )
+      port = (server.address() as AddressInfo).port
+      portNoTrailSlash = (serverNoTrailSlash.address() as AddressInfo).port
     })
 
     afterAll(async () => {
@@ -103,6 +101,87 @@ createNextDescribe(
     // browser(context)
     // dev(devContext)
     // dynamic(context)
+
+    describe('Render via SSR', () => {
+      it('should render the home page', async () => {
+        const html = await renderViaHTTP(port, '/')
+        expect(html).toMatch(/This is the home page/)
+      })
+
+      it('should render the about page', async () => {
+        const html = await renderViaHTTP(port, '/about')
+        expect(html).toMatch(/This is the About page foobar/)
+      })
+
+      it('should render links correctly', async () => {
+        const html = await renderViaHTTP(port, '/')
+        const $ = cheerio.load(html)
+        const dynamicLink = $('#dynamic-1').prop('href')
+        const filePathLink = $('#path-with-extension').prop('href')
+        expect(dynamicLink).toEqual('/dynamic/one/')
+        expect(filePathLink).toEqual('/file-name.md')
+      })
+
+      it('should render a page with getInitialProps', async () => {
+        const html = await renderViaHTTP(port, '/dynamic')
+        expect(html).toMatch(/cool dynamic text/)
+      })
+
+      it('should render a dynamically rendered custom url page', async () => {
+        const html = await renderViaHTTP(port, '/dynamic/one')
+        expect(html).toMatch(/next export is nice/)
+      })
+
+      it('should render pages with dynamic imports', async () => {
+        const html = await renderViaHTTP(port, '/dynamic-imports')
+        expect(html).toMatch(/Welcome to dynamic imports/)
+      })
+
+      it('should render paths with extensions', async () => {
+        const html = await renderViaHTTP(port, '/file-name.md')
+        expect(html).toMatch(/this file has an extension/)
+      })
+
+      it('should give empty object for query if there is no query', async () => {
+        const html = await renderViaHTTP(
+          port,
+          '/get-initial-props-with-no-query'
+        )
+        expect(html).toMatch(/Query is: {}/)
+      })
+
+      it('should render _error on 404.html even if not provided in exportPathMap', async () => {
+        const html = await renderViaHTTP(port, '/404.html')
+        // The default error page from the test server
+        // contains "404", so need to be specific here
+        expect(html).toMatch(/404.*page.*not.*found/i)
+      })
+
+      // since exportTrailingSlash is enabled we should allow this
+      it('should render _error on /404/index.html', async () => {
+        const html = await renderViaHTTP(port, '/404/index.html')
+        // The default error page from the test server
+        // contains "404", so need to be specific here
+        expect(html).toMatch(/404.*page.*not.*found/i)
+      })
+
+      it('Should serve static files', async () => {
+        const data = await renderViaHTTP(port, '/static/data/item.txt')
+        expect(data).toBe('item')
+      })
+
+      it('Should serve public files', async () => {
+        const html = await renderViaHTTP(port, '/about')
+        const data = await renderViaHTTP(port, '/about/data.txt')
+        expect(html).toMatch(/This is the About page foobar/)
+        expect(data).toBe('data')
+      })
+
+      it('Should render dynamic files with query', async () => {
+        const html = await renderViaHTTP(port, '/blog/nextjs/comment/test')
+        expect(html).toMatch(/Blog post nextjs comment test/)
+      })
+    })
 
     describe('API routes export', () => {
       it('Should throw if a route is matched', async () => {
