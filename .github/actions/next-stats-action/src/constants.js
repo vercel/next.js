7ece64f99e9c6f6858d91a96c0dda42c381a76ec
Jan Kaifer <jan@kaifer.cz>
2023-01-25 11:08:48 +0100

    fix tmp dir creation

--- .github/actions/next-stats-action/src/constants.js
+++ .github/actions/next-stats-action/src/constants.js
@@ -1,8 +1,9 @@
 const path = require('path')
+const os = require('os')
 const fs = require('fs')
 
 const benchTitle = 'Page Load Tests'
-const workDir = path.join(fs.mkdtempSync(), 'next-stats')
+const workDir = fs.mkdtempSync(path.join(os.tmpdir(), 'next-stats'))
 const mainRepoDir = path.join(workDir, 'main-repo')
 const diffRepoDir = path.join(workDir, 'diff-repo')
 const statsAppDir = path.join(workDir, 'stats-app')
