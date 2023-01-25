a004d9e1e01d358a80a832a97a97ea18e661f280
Jan Kaifer <jan@kaifer.cz>
2023-01-25 17:35:40 +0100

    silence logs in CI

--- test/jest-setup-after-env.ts
+++ test/jest-setup-after-env.ts
@@ -4,3 +4,10 @@ expect.extend(matchers)
 // A default max-timeout of 90 seconds is allowed
 // per test we should aim to bring this down though
 jest.setTimeout((process.platform === 'win32' ? 180 : 90) * 1000)
+
+// Silence logs in CI
+const origLog = console.log
+console.log = (...args) => {
+  if (process.env.CI) return
+  origLog(...args)
+}
