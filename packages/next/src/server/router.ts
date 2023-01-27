e66d288014a1cebca13081c74210c1e47a958d18
JJ Kasper <jj@jjsweb.site>
2023-01-26 16:17:36 -0800

    fix edge runtime

--- packages/next/src/server/router.ts
+++ packages/next/src/server/router.ts
@@ -504,98 +504,107 @@ export default class Router {
   }
 }
 
-export async function makeResolver(dir: string, nextConfig: NextConfig) {
-  // TODO: why are there double defaults here?
-  const { default: DevServer } =
-    require('./dev/next-dev-server.js') as typeof import('./dev/next-dev-server')
+let _makeResolver: any = () => {}
 
-  const { default: loadCustomRoutes } =
-    require('../lib/load-custom-routes') as typeof import('../lib/load-custom-routes')
-
-  const { NodeNextRequest, NodeNextResponse } =
-    require('./base-http/node') as typeof import('./base-http/node')
-
-  const url = require('url') as typeof import('url')
-
-  const devServer = new DevServer({
-    dir,
-    conf: nextConfig,
-  }) as any as {
-    customRoutes: UnwrapPromise<ReturnType<typeof loadCustomRoutes>>
-    router: Router
-    generateRoutes: any
-  }
-  devServer.customRoutes = await loadCustomRoutes(nextConfig)
-  const routes = devServer.generateRoutes.bind(devServer)()
-  devServer.router = new Router(routes)
-  const routeResults = new Map<string, string>()
-
-  // @ts-expect-error internal field
-  devServer.router.catchAllRoute = {
-    match: getPathMatch('/:path*'),
-    name: 'catchall route',
-    fn: async (req, _res, _params, parsedUrl) => {
-      // clean up internal query values
-      for (const key of Object.keys(parsedUrl.query || {})) {
-        if (key.startsWith('_next')) {
-          delete parsedUrl.query[key]
-        }
-      }
-
-      routeResults.set(
-        (req as any)._initUrl,
-        url.format({
-          pathname: parsedUrl.pathname,
-          query: parsedUrl.query,
-          hash: parsedUrl.hash,
-        })
-      )
-      return { finished: true }
-    },
-  } as Route
-
-  // @ts-expect-error internal field
-  devServer.router.compiledRoutes = devServer.router.compiledRoutes.filter(
-    (route) => {
-      return (
-        route.type === 'rewrite' ||
-        route.type === 'redirect' ||
-        route.type === 'header' ||
-        route.name === 'catchall route' ||
-        route.name?.includes('check')
-      )
-    }
-  )
-
-  return async function resolveRoute(
-    _req: IncomingMessage,
-    _res: ServerResponse
+if (process.env.NEXT_RUNTIME !== 'edge') {
+  _makeResolver = async function makeResolver(
+    dir: string,
+    nextConfig: NextConfig
   ) {
-    const req = new NodeNextRequest(_req)
-    const res = new NodeNextResponse(_res)
-    ;(req as any)._initUrl = req.url
+    // TODO: why are there double defaults here?
+    const { default: DevServer } =
+      require('./dev/next-dev-server.js') as typeof import('./dev/next-dev-server')
 
-    await devServer.router.execute.bind(devServer.router)(
-      req,
-      res,
-      url.parse(req.url!, true)
+    const { default: loadCustomRoutes } =
+      require('../lib/load-custom-routes') as typeof import('../lib/load-custom-routes')
+
+    const { NodeNextRequest, NodeNextResponse } =
+      require('./base-http/node') as typeof import('./base-http/node')
+
+    const url = require('url') as typeof import('url')
+
+    const devServer = new DevServer({
+      dir,
+      conf: nextConfig,
+    }) as any as {
+      customRoutes: UnwrapPromise<ReturnType<typeof loadCustomRoutes>>
+      router: Router
+      generateRoutes: any
+    }
+    devServer.customRoutes = await loadCustomRoutes(nextConfig)
+    const routes = devServer.generateRoutes.bind(devServer)()
+    devServer.router = new Router(routes)
+    const routeResults = new Map<string, string>()
+
+    // @ts-expect-error internal field
+    devServer.router.catchAllRoute = {
+      match: getPathMatch('/:path*'),
+      name: 'catchall route',
+      fn: async (req, _res, _params, parsedUrl) => {
+        // clean up internal query values
+        for (const key of Object.keys(parsedUrl.query || {})) {
+          if (key.startsWith('_next')) {
+            delete parsedUrl.query[key]
+          }
+        }
+
+        routeResults.set(
+          (req as any)._initUrl,
+          url.format({
+            pathname: parsedUrl.pathname,
+            query: parsedUrl.query,
+            hash: parsedUrl.hash,
+          })
+        )
+        return { finished: true }
+      },
+    } as Route
+
+    // @ts-expect-error internal field
+    devServer.router.compiledRoutes = devServer.router.compiledRoutes.filter(
+      (route) => {
+        return (
+          route.type === 'rewrite' ||
+          route.type === 'redirect' ||
+          route.type === 'header' ||
+          route.name === 'catchall route' ||
+          route.name?.includes('check')
+        )
+      }
     )
 
-    if (!res.originalResponse.headersSent) {
-      res.setHeader('x-nextjs-route-result', '1')
-      const resolvedUrl = routeResults.get((req as any)._initUrl) || req.url!
-      const routeResult: {
-        url: string
-        statusCode: number
-        headers: Record<string, undefined | number | string | string[]>
-        isRedirect?: boolean
-      } = {
-        url: resolvedUrl,
-        statusCode: 200,
-        headers: {},
-      }
+    return async function resolveRoute(
+      _req: IncomingMessage,
+      _res: ServerResponse
+    ) {
+      const req = new NodeNextRequest(_req)
+      const res = new NodeNextResponse(_res)
+      ;(req as any)._initUrl = req.url
 
-      res.body(JSON.stringify(routeResult)).send()
+      await devServer.router.execute.bind(devServer.router)(
+        req,
+        res,
+        url.parse(req.url!, true)
+      )
+
+      if (!res.originalResponse.headersSent) {
+        res.setHeader('x-nextjs-route-result', '1')
+        const resolvedUrl = routeResults.get((req as any)._initUrl) || req.url!
+        const routeResult: {
+          url: string
+          statusCode: number
+          headers: Record<string, undefined | number | string | string[]>
+          isRedirect?: boolean
+        } = {
+          url: resolvedUrl,
+          statusCode: 200,
+          headers: {},
+        }
+
+        res.body(JSON.stringify(routeResult)).send()
+      }
     }
   }
 }
+
+export const makeResolver = _makeResolver
