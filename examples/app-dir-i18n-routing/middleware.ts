2c01a05d228f8862c7ea52ac91bd35e1569101a9
Jan Kaifer <jan@kaifer.cz>
2023-01-27 10:43:05 +0100

    Update examples/app-dir-i18n-routing/middleware.ts
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- examples/app-dir-i18n-routing/middleware.ts
+++ examples/app-dir-i18n-routing/middleware.ts
@@ -49,5 +49,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: '/((?!_next)(?!api)(/.*)?',
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
