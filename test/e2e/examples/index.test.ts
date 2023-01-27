07ba14d106287be3dfef65dd932079582e655dc9
Jan Kaifer <jan@kaifer.cz>
2023-01-27 14:54:15 +0100

    add better logs

--- test/e2e/examples/index.test.ts
+++ test/e2e/examples/index.test.ts
@@ -83,26 +83,32 @@ testedExamples.forEach((example) => {
   )
 
   const packageJson = fs.readJsonSync(path.join(exampleFiles, 'package.json'))
-
-  createNextDescribe(
-    `example '${example}'`,
-    {
-      files: exampleFiles,
-      dependencies: {
-        // We need to make sure that these default dependencies are not installed by default
-        // for our examples to ensure that they have all their dependencies in package.json
-        '@types/node': undefined,
-        '@types/react': undefined,
-        next: undefined,
-        react: undefined,
-        'react-dom': undefined,
-        typescript: undefined,
-        ...packageJson.dependencies,
-        ...packageJson.devDependencies,
+  describe(`example '${example}'`, () => {
+    // If there is an issue during a build, jest won't tell us which example caused it
+    // we need to log it ourselfs
+    beforeAll(() => {
+      require('console').log(`Running example '${example}'`)
+    })
+    createNextDescribe(
+      `example '${example}'`,
+      {
+        files: exampleFiles,
+        dependencies: {
+          // We need to make sure that these default dependencies are not installed by default
+          // for our examples to ensure that they have all their dependencies in package.json
+          '@types/node': undefined,
+          '@types/react': undefined,
+          next: undefined,
+          react: undefined,
+          'react-dom': undefined,
+          typescript: undefined,
+          ...packageJson.dependencies,
+          ...packageJson.devDependencies,
+        },
       },
-    },
-    () => {
-      it('builds', () => {})
-    }
-  )
+      () => {
+        it('builds', () => {})
+      }
+    )
+  })
 })
