ff28749a923f8c1d976f919d0332fd5678c5ac9c
Sean Massa <endangeredmassa@gmail.com>
2023-01-27 16:02:03 -0600

    try the one in the middle

--- test/lib/next-modes/next-deploy.ts
+++ test/lib/next-modes/next-deploy.ts
@@ -30,7 +30,7 @@ export class NextDeployInstance extends NextInstance {
       require('console').log(`Using Vercel CLI version:`, res.stdout)
     } catch (_) {
       require('console').log(`Installing Vercel CLI`)
-      await execa('npm', ['i', '-g', 'vercel@28.13.0'], {
+      await execa('npm', ['i', '-g', 'vercel@28.13.1'], {
         stdio: 'inherit',
       })
     }
