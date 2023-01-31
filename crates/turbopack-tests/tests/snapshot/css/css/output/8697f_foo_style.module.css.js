f1e816f0c3c13074180ebdb75be41e37524d19b4
Will Binns-Smith <wbinnssmith@gmail.com>
2023-01-31 10:13:55 -0800

    css modules: use a terser selector format that requires less escaping (vercel/turbo#3437)
    Fixes WEB-447.
    
    Previously, generated classnames would include literal `/` characters,
    which need escaping when generating selectors. While users should still
    escape selectors, this avoids it for this case and makes generated code
    easier to read. This format is also more aligned with what webpack-based
    css modules generates.
    
    This also removes the square character as a separator, since it breaks
    words and makes double-clicking for selection more difficult.

--- crates/turbopack-tests/tests/snapshot/css/css/output/8697f_foo_style.module.css.js
+++ crates/turbopack-tests/tests/snapshot/css/css/output/8697f_foo_style.module.css.js
@@ -3,7 +3,7 @@
 "[project]/crates/turbopack-tests/tests/snapshot/css/css/input/node_modules/foo/style.module.css (css module)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_cjs__, p: process, g: global, __dirname }) => (() => {
 
 __turbopack_export_value__({
-  "foo-module-style": "foo-module-style◽[project]/crates/turbopack-tests/tests/snapshot/css/css/input/node_modules/foo/style.module.css",
+  "foo-module-style": "foo-module-style__style__abf9e738",
 });
 
 })()),
