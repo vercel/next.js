b967bdfe89dafa1575a37af8c0dc58a9dec7afa0
Jimmy Lai <laijimmy0@gmail.com>
2023-02-01 18:34:08 +0100

    misc: remove IIFE from webpack build fn (5/6) (#45459)
    This is mostly useless now that the compilation is self contained to a
    file.
    
    <!--
    Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change that you're making:
    -->
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the
    feature request has been accepted for implementation before opening a
    PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ]
    [e2e](https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs)
    tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `pnpm build && pnpm lint`
    - [ ] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- packages/next/src/build/webpack-build.ts
+++ packages/next/src/build/webpack-build.ts
@@ -6,6 +6,7 @@ import {
   COMPILER_NAMES,
   CLIENT_STATIC_FILES_RUNTIME_MAIN_APP,
   APP_CLIENT_INTERNALS,
+  PHASE_PRODUCTION_BUILD,
 } from '../shared/lib/constants'
 import { runCompiler } from './compiler'
 import * as Log from './output/log'
@@ -14,8 +15,10 @@ import { NextError } from '../lib/is-error'
 import { injectedClientEntries } from './webpack/plugins/flight-client-entry-plugin'
 import { TelemetryPlugin } from './webpack/plugins/telemetry-plugin'
 import { NextBuildContext } from './build-context'
-
+import { isMainThread, parentPort, Worker, workerData } from 'worker_threads'
 import { createEntrypoints } from './entries'
+import loadConfig from '../server/config'
+import { trace } from '../trace'
 
 type CompilerResult = {
   errors: webpack.StatsError[]
@@ -33,7 +36,7 @@ function isTelemetryPlugin(plugin: unknown): plugin is TelemetryPlugin {
   return plugin instanceof TelemetryPlugin
 }
 
-export async function webpackBuild(): Promise<number> {
+async function webpackBuildImpl(): Promise<number> {
   let result: CompilerResult | null = {
     warnings: [],
     errors: [],
@@ -44,155 +47,151 @@ export async function webpackBuild(): Promise<number> {
   const buildSpinner = NextBuildContext.buildSpinner
   const dir = NextBuildContext.dir!
 
-  await (async () => {
-    // IIFE to isolate locals and avoid retaining memory too long
-    const runWebpackSpan = nextBuildSpan.traceChild('run-webpack-compiler')
+  const runWebpackSpan = nextBuildSpan.traceChild('run-webpack-compiler')
+  const entrypoints = await nextBuildSpan
+    .traceChild('create-entrypoints')
+    .traceAsyncFn(() =>
+      createEntrypoints({
+        buildId: NextBuildContext.buildId!,
+        config: NextBuildContext.config!,
+        envFiles: NextBuildContext.loadedEnvFiles!,
+        isDev: false,
+        rootDir: dir,
+        pageExtensions: NextBuildContext.config!.pageExtensions!,
+        pagesDir: NextBuildContext.pagesDir!,
+        appDir: NextBuildContext.appDir!,
+        pages: NextBuildContext.mappedPages!,
+        appPaths: NextBuildContext.mappedAppPages!,
+        previewMode: NextBuildContext.previewProps!,
+        rootPaths: NextBuildContext.mappedRootPaths!,
+      })
+    )
 
-    const entrypoints = await nextBuildSpan
-      .traceChild('create-entrypoints')
-      .traceAsyncFn(() =>
-        createEntrypoints({
-          buildId: NextBuildContext.buildId!,
-          config: NextBuildContext.config!,
-          envFiles: NextBuildContext.loadedEnvFiles!,
-          isDev: false,
-          rootDir: dir,
-          pageExtensions: NextBuildContext.config!.pageExtensions!,
-          pagesDir: NextBuildContext.pagesDir!,
-          appDir: NextBuildContext.appDir!,
-          pages: NextBuildContext.mappedPages!,
-          appPaths: NextBuildContext.mappedAppPages!,
-          previewMode: NextBuildContext.previewProps!,
-          rootPaths: NextBuildContext.mappedRootPaths!,
-        })
-      )
+  const commonWebpackOptions = {
+    isServer: false,
+    buildId: NextBuildContext.buildId!,
+    config: NextBuildContext.config!,
+    target: NextBuildContext.config!.target!,
+    appDir: NextBuildContext.appDir!,
+    pagesDir: NextBuildContext.pagesDir!,
+    rewrites: NextBuildContext.rewrites!,
+    reactProductionProfiling: NextBuildContext.reactProductionProfiling!,
+    noMangling: NextBuildContext.noMangling!,
+  }
 
-    const commonWebpackOptions = {
-      isServer: false,
-      buildId: NextBuildContext.buildId!,
-      config: NextBuildContext.config!,
-      target: NextBuildContext.config!.target!,
-      appDir: NextBuildContext.appDir!,
-      pagesDir: NextBuildContext.pagesDir!,
-      rewrites: NextBuildContext.rewrites!,
-      reactProductionProfiling: NextBuildContext.reactProductionProfiling!,
-      noMangling: NextBuildContext.noMangling!,
-    }
+  const configs = await runWebpackSpan
+    .traceChild('generate-webpack-config')
+    .traceAsyncFn(() =>
+      Promise.all([
+        getBaseWebpackConfig(dir, {
+          ...commonWebpackOptions,
+          middlewareMatchers: entrypoints.middlewareMatchers,
+          runWebpackSpan,
+          compilerType: COMPILER_NAMES.client,
+          entrypoints: entrypoints.client,
+        }),
+        getBaseWebpackConfig(dir, {
+          ...commonWebpackOptions,
+          runWebpackSpan,
+          middlewareMatchers: entrypoints.middlewareMatchers,
+          compilerType: COMPILER_NAMES.server,
+          entrypoints: entrypoints.server,
+        }),
+        getBaseWebpackConfig(dir, {
+          ...commonWebpackOptions,
+          runWebpackSpan,
+          middlewareMatchers: entrypoints.middlewareMatchers,
+          compilerType: COMPILER_NAMES.edgeServer,
+          entrypoints: entrypoints.edgeServer,
+        }),
+      ])
+    )
 
-    const configs = await runWebpackSpan
-      .traceChild('generate-webpack-config')
-      .traceAsyncFn(() =>
-        Promise.all([
-          getBaseWebpackConfig(dir, {
-            ...commonWebpackOptions,
-            middlewareMatchers: entrypoints.middlewareMatchers,
-            runWebpackSpan,
-            compilerType: COMPILER_NAMES.client,
-            entrypoints: entrypoints.client,
-          }),
-          getBaseWebpackConfig(dir, {
-            ...commonWebpackOptions,
-            runWebpackSpan,
-            middlewareMatchers: entrypoints.middlewareMatchers,
-            compilerType: COMPILER_NAMES.server,
-            entrypoints: entrypoints.server,
-          }),
-          getBaseWebpackConfig(dir, {
-            ...commonWebpackOptions,
-            runWebpackSpan,
-            middlewareMatchers: entrypoints.middlewareMatchers,
-            compilerType: COMPILER_NAMES.edgeServer,
-            entrypoints: entrypoints.edgeServer,
-          }),
-        ])
-      )
+  const clientConfig = configs[0]
 
-    const clientConfig = configs[0]
+  if (
+    clientConfig.optimization &&
+    (clientConfig.optimization.minimize !== true ||
+      (clientConfig.optimization.minimizer &&
+        clientConfig.optimization.minimizer.length === 0))
+  ) {
+    Log.warn(
+      `Production code optimization has been disabled in your project. Read more: https://nextjs.org/docs/messages/minification-disabled`
+    )
+  }
 
-    if (
-      clientConfig.optimization &&
-      (clientConfig.optimization.minimize !== true ||
-        (clientConfig.optimization.minimizer &&
-          clientConfig.optimization.minimizer.length === 0))
-    ) {
-      Log.warn(
-        `Production code optimization has been disabled in your project. Read more: https://nextjs.org/docs/messages/minification-disabled`
-      )
-    }
+  webpackBuildStart = process.hrtime()
 
-    webpackBuildStart = process.hrtime()
+  // We run client and server compilation separately to optimize for memory usage
+  await runWebpackSpan.traceAsyncFn(async () => {
+    // Run the server compilers first and then the client
+    // compiler to track the boundary of server/client components.
+    let clientResult: SingleCompilerResult | null = null
 
-    // We run client and server compilation separately to optimize for memory usage
-    await runWebpackSpan.traceAsyncFn(async () => {
-      // Run the server compilers first and then the client
-      // compiler to track the boundary of server/client components.
-      let clientResult: SingleCompilerResult | null = null
+    // During the server compilations, entries of client components will be
+    // injected to this set and then will be consumed by the client compiler.
+    injectedClientEntries.clear()
 
-      // During the server compilations, entries of client components will be
-      // injected to this set and then will be consumed by the client compiler.
-      injectedClientEntries.clear()
+    const serverResult = await runCompiler(configs[1], {
+      runWebpackSpan,
+    })
+    const edgeServerResult = configs[2]
+      ? await runCompiler(configs[2], { runWebpackSpan })
+      : null
 
-      const serverResult = await runCompiler(configs[1], {
+    // Only continue if there were no errors
+    if (!serverResult.errors.length && !edgeServerResult?.errors.length) {
+      injectedClientEntries.forEach((value, key) => {
+        const clientEntry = clientConfig.entry as webpack.EntryObject
+        if (key === APP_CLIENT_INTERNALS) {
+          clientEntry[CLIENT_STATIC_FILES_RUNTIME_MAIN_APP] = [
+            // TODO-APP: cast clientEntry[CLIENT_STATIC_FILES_RUNTIME_MAIN_APP] to type EntryDescription once it's available from webpack
+            // @ts-expect-error clientEntry['main-app'] is type EntryDescription { import: ... }
+            ...clientEntry[CLIENT_STATIC_FILES_RUNTIME_MAIN_APP].import,
+            value,
+          ]
+        } else {
+          clientEntry[key] = {
+            dependOn: [CLIENT_STATIC_FILES_RUNTIME_MAIN_APP],
+            import: value,
+          }
+        }
+      })
+
+      clientResult = await runCompiler(clientConfig, {
         runWebpackSpan,
       })
-      const edgeServerResult = configs[2]
-        ? await runCompiler(configs[2], { runWebpackSpan })
-        : null
+    }
 
-      // Only continue if there were no errors
-      if (!serverResult.errors.length && !edgeServerResult?.errors.length) {
-        injectedClientEntries.forEach((value, key) => {
-          const clientEntry = clientConfig.entry as webpack.EntryObject
-          if (key === APP_CLIENT_INTERNALS) {
-            clientEntry[CLIENT_STATIC_FILES_RUNTIME_MAIN_APP] = [
-              // TODO-APP: cast clientEntry[CLIENT_STATIC_FILES_RUNTIME_MAIN_APP] to type EntryDescription once it's available from webpack
-              // @ts-expect-error clientEntry['main-app'] is type EntryDescription { import: ... }
-              ...clientEntry[CLIENT_STATIC_FILES_RUNTIME_MAIN_APP].import,
-              value,
-            ]
-          } else {
-            clientEntry[key] = {
-              dependOn: [CLIENT_STATIC_FILES_RUNTIME_MAIN_APP],
-              import: value,
-            }
-          }
-        })
+    result = {
+      warnings: ([] as any[])
+        .concat(
+          clientResult?.warnings,
+          serverResult?.warnings,
+          edgeServerResult?.warnings
+        )
+        .filter(nonNullable),
+      errors: ([] as any[])
+        .concat(
+          clientResult?.errors,
+          serverResult?.errors,
+          edgeServerResult?.errors
+        )
+        .filter(nonNullable),
+      stats: [
+        clientResult?.stats,
+        serverResult?.stats,
+        edgeServerResult?.stats,
+      ],
+    }
+  })
+  result = nextBuildSpan
+    .traceChild('format-webpack-messages')
+    .traceFn(() => formatWebpackMessages(result, true)) as CompilerResult
 
-        clientResult = await runCompiler(clientConfig, {
-          runWebpackSpan,
-        })
-      }
-
-      result = {
-        warnings: ([] as any[])
-          .concat(
-            clientResult?.warnings,
-            serverResult?.warnings,
-            edgeServerResult?.warnings
-          )
-          .filter(nonNullable),
-        errors: ([] as any[])
-          .concat(
-            clientResult?.errors,
-            serverResult?.errors,
-            edgeServerResult?.errors
-          )
-          .filter(nonNullable),
-        stats: [
-          clientResult?.stats,
-          serverResult?.stats,
-          edgeServerResult?.stats,
-        ],
-      }
-    })
-    result = nextBuildSpan
-      .traceChild('format-webpack-messages')
-      .traceFn(() => formatWebpackMessages(result, true))
-
-    NextBuildContext.telemetryPlugin = (
-      clientConfig as webpack.Configuration
-    ).plugins?.find(isTelemetryPlugin)
-  })()
+  NextBuildContext.telemetryPlugin = (
+    clientConfig as webpack.Configuration
+  ).plugins?.find(isTelemetryPlugin)
 
   const webpackBuildEnd = process.hrtime(webpackBuildStart)
   if (buildSpinner) {
@@ -246,5 +245,72 @@ export async function webpackBuild(): Promise<number> {
       Log.info('Compiled successfully')
     }
     return webpackBuildEnd[0]
+  }
+}
+
+// the main function when this file is run as a worker
+async function workerMain() {
+  const { buildContext } = workerData
+  // setup new build context from the serialized data passed from the parent
+  Object.assign(NextBuildContext, buildContext)
+
+  /// load the config because it's not serializable
+  NextBuildContext.config = await loadConfig(
+    PHASE_PRODUCTION_BUILD,
+    NextBuildContext.dir!,
+    undefined,
+    undefined,
+    true
+  )
+  NextBuildContext.nextBuildSpan = trace('next-build')
+
+  try {
+    const result = await webpackBuildImpl()
+    parentPort!.postMessage(result)
+  } catch (e) {
+    parentPort!.postMessage(e)
+  } finally {
+    process.exit(0)
+  }
+}
+
+if (!isMainThread) {
+  workerMain()
+}
+
+async function webpackBuildWithWorker() {
+  const {
+    config,
+    telemetryPlugin,
+    buildSpinner,
+    nextBuildSpan,
+    ...prunedBuildContext
+  } = NextBuildContext
+  const worker = new Worker(new URL(import.meta.url), {
+    workerData: {
+      buildContext: prunedBuildContext,
+    },
+  })
+
+  const result = await new Promise((resolve, reject) => {
+    worker.on('message', resolve)
+    worker.on('error', reject)
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Worker stopped with exit code ${code}`))
+      }
+    })
+  })
+
+  return result as number
+}
+
+export async function webpackBuild() {
+  const config = NextBuildContext.config!
+
+  if (config.experimental.webpackBuildWorker) {
+    return await webpackBuildWithWorker()
+  } else {
+    return await webpackBuildImpl()
   }
 }
