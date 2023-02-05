0c6b08a4a33eb48932dbf8145340929429b63946
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-06 07:07:28 +0900

    Harden

--- packages/next-swc/crates/core/tests/fixture/server-actions/7/input.js
+++ packages/next-swc/crates/core/tests/fixture/server-actions/7/input.js
@@ -3,7 +3,7 @@ import deleteFromDb from 'db'
 export function Item(product) {
     async function deleteItem() {
         "use server";
-        await deleteFromDb(product.id);
+        await deleteFromDb(product.id,product?.foo,product.bar.baz);
     }
     return <Button action={deleteItem}>Delete</Button>;
 }
