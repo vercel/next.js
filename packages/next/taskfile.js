a5f68b3c6c43f6b88b00722d7d887e8f13890395
Jimmy Lai <laijimmy0@gmail.com>
2023-02-06 21:49:23 +0100

    misc: fix precompiled script generation (#45616)
    It seems this was broken when we changed the source folder.
    
    <!--
    Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change(s) that you're making:
    -->
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [ ] Integration tests added
    - [ ] Errors have a helpful link attached, see [`contributing.md`]
    - [ ] (https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the
    feature request has been accepted for implementation before opening a
    PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ]
    [e2e](https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs)
    tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `pnpm build && pnpm lint`
    - [ ] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- packages/next/taskfile.js
+++ packages/next/taskfile.js
@@ -1080,7 +1080,7 @@ export async function ncc_babel_bundle(task, opts) {
     delete bundleExternals[pkg]
   }
   await task
-    .source('bundles/babel/bundle.js')
+    .source('src/bundles/babel/bundle.js')
     .ncc({
       packageName: '@babel/core',
       bundleName: 'babel',
@@ -1106,7 +1106,7 @@ export async function ncc_babel_bundle_packages(task, opts) {
   await fs.writeFile(eslintParseFile, replacedContent)
 
   await task
-    .source('bundles/babel/packages-bundle.js')
+    .source('src/bundles/babel/packages-bundle.js')
     .ncc({
       externals: externals,
     })
@@ -1117,7 +1117,7 @@ export async function ncc_babel_bundle_packages(task, opts) {
     JSON.stringify({ name: 'babel-packages', main: './packages-bundle.js' })
   )
 
-  await task.source('bundles/babel/packages/*').target('src/compiled/babel')
+  await task.source('src/bundles/babel/packages/*').target('src/compiled/babel')
 }
 
 // eslint-disable-next-line camelcase
@@ -1919,7 +1919,7 @@ export async function ncc_webpack_bundle5(task, opts) {
     delete bundleExternals[pkg]
   }
   await task
-    .source('bundles/webpack/bundle5.js')
+    .source('src/bundles/webpack/bundle5.js')
     .ncc({
       packageName: 'webpack',
       bundleName: 'webpack',
@@ -1943,7 +1943,7 @@ Object.assign(externals, webpackBundlePackages)
 
 export async function ncc_webpack_bundle_packages(task, opts) {
   await task
-    .source('bundles/webpack/packages/*')
+    .source('src/bundles/webpack/packages/*')
     .target('src/compiled/webpack/')
 }
 
