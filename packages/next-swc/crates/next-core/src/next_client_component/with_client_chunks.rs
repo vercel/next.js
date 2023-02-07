f83691f700a90ba3699228fa2ef7abce623da628
Tobias Koppers <tobias.koppers@googlemail.com>
2023-02-07 23:56:06 +0100

    refactor ResolveResult to allow mixing special and assets in alternatives (vercel/turbo#3675)
    Co-authored-by: Justin Ridgewell <justin@ridgewell.name>

--- packages/next-swc/crates/next-core/src/next_client_component/with_client_chunks.rs
+++ packages/next-swc/crates/next-core/src/next_client_component/with_client_chunks.rs
@@ -189,7 +189,7 @@ impl ValueToString for WithClientChunksAssetReference {
 impl AssetReference for WithClientChunksAssetReference {
     #[turbo_tasks::function]
     fn resolve_reference(&self) -> ResolveResultVc {
-        ResolveResult::Single(self.asset, Vec::new()).cell()
+        ResolveResult::asset(self.asset).cell()
     }
 }
 
