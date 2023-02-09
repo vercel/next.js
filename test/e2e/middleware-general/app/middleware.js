ac8123ddd75d680fa824909905c3cd029fbec5fd
JJ Kasper <jj@jjsweb.site>
2023-02-08 21:35:20 -0800

    fix lint

--- test/e2e/middleware-general/app/middleware.js
+++ test/e2e/middleware-general/app/middleware.js
@@ -49,7 +49,9 @@ export async function middleware(request) {
     return NextResponse.next()
   }
 
-  if (url.pathname.includes('/_next/static/chunks/pages/_app-non-existent.js')) {
+  if (
+    url.pathname.includes('/_next/static/chunks/pages/_app-non-existent.js')
+  ) {
     return NextResponse.rewrite('https://example.vercel.sh')
   }
 
