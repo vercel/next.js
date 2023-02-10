b99c2f995d63fb6a0190b1822be338177c326d78
Tobias Koppers <tobias.koppers@googlemail.com>
2023-02-10 16:31:45 +0100

    update to 13.1.7-canary.8 (vercel/turbo#3712)

--- crates/next-dev/benches/bundlers/turbopack/mod.rs
+++ crates/next-dev/benches/bundlers/turbopack/mod.rs
@@ -49,7 +49,7 @@ impl Bundler for Turbopack {
         npm::install(
             install_dir,
             &[
-                NpmPackage::new("next", "13.1.7-canary.2"),
+                NpmPackage::new("next", "13.1.7-canary.8"),
                 // Dependency on this is inserted by swc's preset_env
                 NpmPackage::new("@swc/helpers", "^0.4.11"),
             ],
