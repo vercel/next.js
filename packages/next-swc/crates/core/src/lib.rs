3b77f1dccd3b36d9f42cf7ff7877c2143408b7b2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-13 07:23:58 +0900

    fixup

--- packages/next-swc/crates/core/src/lib.rs
+++ packages/next-swc/crates/core/src/lib.rs
@@ -288,7 +288,7 @@ impl TransformOptions {
 
         if should_enable_commonjs {
             self.swc.config.module =
-                Some(serde_json::from_str(r##"{ "ignoreDynamic": true, }"##).unwrap());
+                Some(serde_json::from_str(r##"{ "ignoreDynamic": true }"##).unwrap());
         }
 
         self
