ea7ae358ab9da279354982eb13a15c03fda1bc31
Jan Kaifer <jan@kaifer.cz>
2023-02-14 18:53:51 +0100

    Revert "temporarily make server error out"
    This reverts commit 19d688b1b24d61649f0277de08104817f6e7d7f3.

--- test/development/acceptance-app/fixtures/default-template/app/server/page.js
+++ test/development/acceptance-app/fixtures/default-template/app/server/page.js
@@ -1,9 +1,4 @@
 import Component from '../../index'
-import { cookies } from 'next/server'
-
 export default function Page() {
-  cookies()
   return <Component />
 }
-
-export const dynamic = 'error'
