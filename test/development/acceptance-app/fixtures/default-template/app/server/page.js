19d688b1b24d61649f0277de08104817f6e7d7f3
Jan Kaifer <jan@kaifer.cz>
2023-02-14 16:53:25 +0100

    temporarily make server error out

--- test/development/acceptance-app/fixtures/default-template/app/server/page.js
+++ test/development/acceptance-app/fixtures/default-template/app/server/page.js
@@ -1,4 +1,9 @@
 import Component from '../../index'
+import { cookies } from 'next/server'
+
 export default function Page() {
+  cookies()
   return <Component />
 }
+
+export const dynamic = 'error'
