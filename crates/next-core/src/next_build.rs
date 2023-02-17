d76fa3ca5a05afe12ff6d245c1df0bdf9c6045b4
Tobias Koppers <tobias.koppers@googlemail.com>
2023-02-17 22:23:43 +0100

    alias @swc/helpers to the version within next.js (vercel/turbo#3865)

--- crates/next-core/src/next_build.rs
+++ crates/next-core/src/next_build.rs
@@ -1,39 +1,8 @@
-use anyhow::{Context, Result};
-use turbo_tasks::Value;
+use anyhow::Result;
 use turbo_tasks_fs::FileSystemPathVc;
-use turbopack::{resolve_options, resolve_options_context::ResolveOptionsContext};
-use turbopack_core::{
-    asset::Asset,
-    resolve::{
-        options::{ImportMapping, ImportMappingVc},
-        parse::RequestVc,
-        pattern::Pattern,
-        resolve,
-    },
-};
+use turbopack_core::resolve::options::{ImportMapping, ImportMappingVc};
 
-#[turbo_tasks::function]
-pub async fn get_next_package(project_root: FileSystemPathVc) -> Result<FileSystemPathVc> {
-    let result = resolve(
-        project_root,
-        RequestVc::parse(Value::new(Pattern::Constant(
-            "next/package.json".to_string(),
-        ))),
-        resolve_options(
-            project_root,
-            ResolveOptionsContext {
-                enable_node_modules: true,
-                enable_node_native_modules: true,
-                custom_conditions: vec!["development".to_string()],
-                ..Default::default()
-            }
-            .cell(),
-        ),
-    );
-    let assets = result.primary_assets().await?;
-    let asset = assets.first().context("Next.js package not found")?;
-    Ok(asset.path().parent())
-}
+use crate::next_import_map::get_next_package;
 
 #[turbo_tasks::function]
 pub async fn get_postcss_package_mapping(
