0345d9e120931b317dadeac5941413c6e998594a
Tobias Koppers <tobias.koppers@googlemail.com>
2023-02-17 22:23:43 +0100

    alias @swc/helpers to the version within next.js (vercel/turbo#3865)

--- packages/next-swc/crates/next-core/src/next_import_map.rs
+++ packages/next-swc/crates/next-core/src/next_import_map.rs
@@ -1,14 +1,20 @@
 use std::collections::{BTreeMap, HashMap};
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use turbo_tasks::Value;
 use turbo_tasks_fs::{glob::GlobVc, FileSystemPathVc};
-use turbopack_core::resolve::{
-    options::{
-        ConditionValue, ImportMap, ImportMapVc, ImportMapping, ImportMappingVc, ResolvedMap,
-        ResolvedMapVc,
+use turbopack::{resolve_options, resolve_options_context::ResolveOptionsContext};
+use turbopack_core::{
+    asset::Asset,
+    resolve::{
+        options::{
+            ConditionValue, ImportMap, ImportMapVc, ImportMapping, ImportMappingVc,
+            ResolveOptionsVc, ResolvedMap, ResolvedMapVc,
+        },
+        parse::RequestVc,
+        pattern::Pattern,
+        resolve, AliasPattern, ExportsValue, ResolveAliasMapVc,
     },
-    AliasPattern, ExportsValue, ResolveAliasMapVc,
 };
 
 use crate::{
@@ -371,7 +377,58 @@ pub async fn insert_next_shared_aliases(
         ImportMapping::Dynamic(NextFontGoogleCssModuleReplacerVc::new(project_path).into()).into(),
     );
 
+    import_map.insert_wildcard_alias(
+        "@swc/helpers/",
+        ImportMapping::PrimaryAlternative(
+            "./*".to_string(),
+            Some(get_swc_helpers_package(project_path)),
+        )
+        .cell(),
+    );
+
     Ok(())
+}
+
+#[turbo_tasks::function]
+fn package_lookup_resolve_options(project_root: FileSystemPathVc) -> ResolveOptionsVc {
+    resolve_options(
+        project_root,
+        ResolveOptionsContext {
+            enable_node_modules: true,
+            enable_node_native_modules: true,
+            custom_conditions: vec!["development".to_string()],
+            ..Default::default()
+        }
+        .cell(),
+    )
+}
+
+#[turbo_tasks::function]
+pub async fn get_next_package(project_root: FileSystemPathVc) -> Result<FileSystemPathVc> {
+    let result = resolve(
+        project_root,
+        RequestVc::parse(Value::new(Pattern::Constant(
+            "next/package.json".to_string(),
+        ))),
+        package_lookup_resolve_options(project_root),
+    );
+    let assets = result.primary_assets().await?;
+    let asset = assets.first().context("Next.js package not found")?;
+    Ok(asset.path().parent())
+}
+
+#[turbo_tasks::function]
+pub async fn get_swc_helpers_package(project_root: FileSystemPathVc) -> Result<FileSystemPathVc> {
+    let result = resolve(
+        get_next_package(project_root),
+        RequestVc::parse(Value::new(Pattern::Constant(
+            "@swc/helpers/package.json".to_string(),
+        ))),
+        package_lookup_resolve_options(project_root),
+    );
+    let assets = result.primary_assets().await?;
+    let asset = assets.first().context("Next.js package not found")?;
+    Ok(asset.path().parent())
 }
 
 pub async fn insert_alias_option<const N: usize>(
