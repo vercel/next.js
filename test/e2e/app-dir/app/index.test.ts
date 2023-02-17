1fda7a8e079c4dfa495ccccd534fbbb7c8dbe093
Shu Ding <g@shud.in>
2023-02-17 23:33:36 +0100

    make sure edge ssr returns vary header

--- test/e2e/app-dir/app/index.test.ts
+++ test/e2e/app-dir/app/index.test.ts
@@ -100,6 +100,14 @@ createNextDescribe(
       expect(res.headers.get('Content-Type')).toBe('text/x-component')
     })
 
+    it('should return the `vary` header from edge runtime', async () => {
+      const res = await next.fetch('/dashboard')
+      expect(res.headers.get('x-edge-runtime')).toBe('1')
+      expect(res.headers.get('vary')).toBe(
+        'RSC, Next-Router-State-Tree, Next-Router-Prefetch'
+      )
+    })
+
     it('should pass props from getServerSideProps in root layout', async () => {
       const $ = await next.render$('/dashboard')
       expect($('title').first().text()).toBe('hello world')
