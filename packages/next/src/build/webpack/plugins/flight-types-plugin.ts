f64ee1ee9b6ff099755860eaad689000f7f8a397
imranbarbhuiya <imranbarbhuiya.fsd@gmail.com>
2023-02-18 16:46:56 +0000

    fix: typedroute routes for window

--- packages/next/src/build/webpack/plugins/flight-types-plugin.ts
+++ packages/next/src/build/webpack/plugins/flight-types-plugin.ts
@@ -220,15 +220,19 @@ export class FlightTypesPlugin {
       return
     }
 
+    // Filter out non-page files in pages dir
+    if (!isApp && !/[/\\](?:_app|_document|404|500)\.[^.]+$/.test(filePath)) {
+      return
+    }
+
     const page = isApp
       ? normalizeAppPath(path.relative(this.appDir, filePath))
       : '/' + path.relative(this.pagesDir, filePath)
 
     let route =
       (isApp
-        ? page.replace(/\/page\.[^./]+$/, '')
-        : page.replace(/\.[^./]+$/, '')
-      ).replace(/\/index$/, '') || '/'
+        ? page.replace(/[/\\]page\.[^./]+$/, '')
+        : page.replace(/\.[^./]+$/, '').replace(/[/\\]index$/, '')) || '/'
 
     if (isDynamicRoute(route)) {
       route = route
