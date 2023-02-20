37958afb5d16ce63cc1bea83115d30bf3c8ac4ea
Vasu Singh <vasucp1207@gmail.com>
2023-02-20 14:43:40 +0530

    Remove font

--- examples/with-valtio/src/pages/index.tsx
+++ examples/with-valtio/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import Todo from '../../components/Todo'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
