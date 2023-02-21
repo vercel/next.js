4d81492cf4d2652cabbc5323ea91328c5dd28640
vasucp1207 <vasucp1207@gmail.com>
2023-02-21 00:05:20 +0000

    arranging files

--- examples/with-valtio/src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
