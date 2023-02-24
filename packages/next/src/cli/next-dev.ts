9d4ae6294705340df95ac8d7132d44175fb94800
Hannes Bornö <hannes.borno@vercel.com>
2023-02-24 11:42:17 +0100

    Use command for current packet manager in next/font warning

--- packages/next/src/cli/next-dev.ts
+++ packages/next/src/cli/next-dev.ts
@@ -22,6 +22,7 @@ import Watchpack from 'next/dist/compiled/watchpack'
 import stripAnsi from 'next/dist/compiled/strip-ansi'
 import { warn } from '../build/output/log'
 import { getPossibleInstrumentationHookFilenames } from '../build/utils'
+import { getNpxCommand } from '../lib/helpers/get-npx-command'
 
 let isTurboSession = false
 let sessionStopHandled = false
@@ -197,10 +198,11 @@ const nextDev: CliCommand = async (argv) => {
       (devDependencies['@next/font'] &&
         devDependencies['@next/font'] !== 'workspace:*')
     ) {
+      const command = getNpxCommand(dir)
       Log.warn(
         'Your project has `@next/font` installed as a dependency, please use the built-in `next/font` instead. ' +
           'The `@next/font` package will be removed in Next.js 14. ' +
-          'You can migrate by running `npx @next/codemod built-in-next-font .`. Read more: https://nextjs.org/docs/messages/built-in-next-font'
+          `You can migrate by running \`${command} @next/codemod built-in-next-font .\`. Read more: https://nextjs.org/docs/messages/built-in-next-font`
       )
     }
   }
