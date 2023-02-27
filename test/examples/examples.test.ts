e2f938d75bba2c4cc0577feb5f58ba5c326b75c2
Jan Kaifer <jan@kaifer.cz>
2023-02-27 10:49:59 +0100

    remove needles function

--- test/examples/examples.test.ts
+++ test/examples/examples.test.ts
@@ -72,38 +72,34 @@ const testedExamples = [
   'with-vercel-fetch',
 ]
 
-const testExamples = (examples) => {
-  describe.each(examples)(`example '%s'`, (example) => {
-    // If there is an issue during a build, jest won't tell us which example caused it
-    // we need to log it ourselfs
-    beforeAll(() => {
-      require('console').log(`Running example '${example}'`)
-    })
-
-    const exampleFiles = path.join(__dirname, '..', '..', 'examples', example)
-    const packageJson = fs.readJsonSync(path.join(exampleFiles, 'package.json'))
-    createNextDescribe(
-      `example '${example}'`,
-      {
-        files: exampleFiles,
-        dependencies: {
-          // We need to make sure that these default dependencies are not installed by default
-          // for our examples to ensure that they have all their dependencies in package.json
-          '@types/node': undefined,
-          '@types/react': undefined,
-          next: undefined,
-          react: undefined,
-          'react-dom': undefined,
-          typescript: undefined,
-          ...packageJson.dependencies,
-          ...packageJson.devDependencies,
-        },
-      },
-      () => {
-        it('builds', () => {})
-      }
-    )
+describe.each(testedExamples)(`example '%s'`, (example) => {
+  // If there is an issue during a build, jest won't tell us which example caused it
+  // we need to log it ourselfs
+  beforeAll(() => {
+    require('console').log(`Running example '${example}'`)
   })
-}
 
-testExamples(testedExamples)
+  const exampleFiles = path.join(__dirname, '..', '..', 'examples', example)
+  const packageJson = fs.readJsonSync(path.join(exampleFiles, 'package.json'))
+  createNextDescribe(
+    `example '${example}'`,
+    {
+      files: exampleFiles,
+      dependencies: {
+        // We need to make sure that these default dependencies are not installed by default
+        // for our examples to ensure that they have all their dependencies in package.json
+        '@types/node': undefined,
+        '@types/react': undefined,
+        next: undefined,
+        react: undefined,
+        'react-dom': undefined,
+        typescript: undefined,
+        ...packageJson.dependencies,
+        ...packageJson.devDependencies,
+      },
+    },
+    () => {
+      it('builds', () => {})
+    }
+  )
+})
