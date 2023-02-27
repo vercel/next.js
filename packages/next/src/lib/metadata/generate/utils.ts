63d2aff005c113e9ae107a9a596a7dbfe6468519
Jiachi Liu <inbox@huozhi.im>
2023-02-27 17:25:02 +0100

    Add theme color media support for metadata

--- packages/next/src/lib/metadata/generate/utils.ts
+++ packages/next/src/lib/metadata/generate/utils.ts
@@ -1,6 +1,12 @@
-export function resolveAsArrayOrUndefined<
-  T extends unknown | readonly unknown[]
->(value: T | T[] | undefined | null): undefined | T[] {
+function resolveAsArrayOrUndefined<
+  T extends (null | undefined) | readonly (null | undefined)[]
+>(value: T | T[]): undefined
+function resolveAsArrayOrUndefined<T extends unknown | readonly unknown[]>(
+  value: T | T[]
+): T[]
+function resolveAsArrayOrUndefined<T extends unknown | readonly unknown[]>(
+  value: T | T[] | undefined | null
+): undefined | T[] {
   if (typeof value === 'undefined' || value === null) {
     return undefined
   }
@@ -9,3 +15,5 @@ export function resolveAsArrayOrUndefined<
   }
   return [value]
 }
+
+export { resolveAsArrayOrUndefined }
