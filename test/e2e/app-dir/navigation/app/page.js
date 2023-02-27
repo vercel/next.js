555234ca56219d60ec55dddd2a7b4149e2a04ffb
Hannes Bornö <hannes.borno@vercel.com>
2023-02-27 11:53:57 +0100

    Add missing test file

--- test/e2e/app-dir/navigation/app/page.js
+++ test/e2e/app-dir/navigation/app/page.js
@@ -10,6 +10,18 @@ export default function Page() {
       <Link id="set-query" href="/?a=b&c=d">
         set Query
       </Link>
+      <Link id="to-client-redirect" href="/redirect/clientcomponent">
+        To client redirect
+      </Link>
+      <Link id="to-server-redirect" href="/redirect/servercomponent">
+        To server redirect
+      </Link>
+      <Link id="to-client-not-found" href="/not-found/clientcomponent">
+        To client not found
+      </Link>
+      <Link id="to-server-not-found" href="/not-found/servercomponent">
+        To server not found
+      </Link>
       <div id="query">{params.toString()}</div>
     </>
   )
