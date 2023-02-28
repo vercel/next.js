31d2b48ec2ca0f88fe182f9bd54dc22d35001ffb
Jiachi Liu <inbox@huozhi.im>
2023-02-28 18:01:41 +0100

    Fix viewport typing

--- packages/next/src/lib/metadata/types/extra-types.ts
+++ packages/next/src/lib/metadata/types/extra-types.ts
@@ -59,6 +59,7 @@ export type Viewport = {
   initialScale?: number
   minimumScale?: number
   maximumScale?: number
+  viewportFit?: 'auto' | 'cover' | 'contain'
   interactiveWidget?: 'resizes-visual' | 'resizes-content' | 'overlays-content'
 }
 
