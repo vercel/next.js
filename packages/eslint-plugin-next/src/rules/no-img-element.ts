d58db13849a47cdb9c51298204954504d9de7b4d
Steven Tey <stevensteel97@gmail.com>
2023-03-01 12:07:19 -0600

    Improved Next Image ESLint Warning

--- packages/eslint-plugin-next/src/rules/no-img-element.ts
+++ packages/eslint-plugin-next/src/rules/no-img-element.ts
@@ -31,7 +31,7 @@ export = defineRule({
 
         context.report({
           node,
-          message: `Using \`<img>\` could result in slower LCP and higher bandwidth. Use \`<Image />\` from \`next/image\` instead to utilize Image Optimization. See: ${url}`,
+          message: `Using \`<img>\` could result in slower LCP and higher bandwidth. Consider using \`<Image />\` from \`next/image\` to automatically optimize images. Note: when self-hosting, Image Optimization runs on your Next.js server; if using an external loader, additional usage or cost could be incurred, depending on your provider. For more details: ${url}`,
         })
       },
     }
