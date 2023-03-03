35d8e8379ea0a3248bf9ea1a76c9a7a7ee0c3e96
JJ Kasper <jj@jjsweb.site>
2023-03-03 14:17:07 -0800

    Ensure app chunk URLs are encoded properly (#46749
    This ensures we properly encode chunks for app dir the same we do for
    pages.
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [x] Integration tests added
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    x-ref: https://github.com/vercel/next.js/pull/8435

---
+++ test/e2e/app-dir/app/app/dynamic-client/[category]/[id]/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+export default function IdPage({ children, params }) {
+  return (
+    <>
+      <p>
+        Id Page. Params:{' '}
+        <span id="id-page-params">{JSON.stringify(params)}</span>
+      </p>
+      {children}
+    </>
+  )
+}
