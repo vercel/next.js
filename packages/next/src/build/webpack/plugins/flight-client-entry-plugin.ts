088c19f709c8b355d23535290d1908beac356577
Shu Ding <g@shud.in>
2023-03-05 19:00:48 +0100

    Correct server reference manifest in prod build (#46683)
    Since we have 2 server compilers (node and edge), the `serverActions`
    object will always get overridden by the second compilation during `next
    build`. Like the client reference manifest, we need to have 2 objects
    for each compiler and merge them when outputting the asset.
    
    Besides that, to avoid `export default` being tree-shaken as unused
    export, I changed the loader to use CJS `module.exports` instead. Can't
    easily figure out a proper way for now but we plan to create separate
    worker for the action endpoint and this will be gone then.
    
    Fixes NEXT-759, read NEXT-761 for the full future plan.
    
    ## Bug
    
    - [ ] Related issues linked using `fixes #number`
    - [x] Integration tests added
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Feature
    
    - [ ] Implements an existing feature request or RFC. Make sure the
    feature request has been accepted for implementation before opening a
    PR.
    - [ ] Related issues linked using `fixes #number`
    - [ ]
    [e2e](https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs)
    tests added
    - [ ] Documentation added
    - [ ] Telemetry added. In case of a feature if it's used or not.
    - [ ] Errors have a helpful link attached, see
    [`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)
    
    ## Documentation / Examples
    
    - [ ] Make sure the linting passes by running `pnpm build && pnpm lint`
    - [ ] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)

--- packages/next/src/build/webpack/plugins/flight-client-entry-plugin.ts
+++ packages/next/src/build/webpack/plugins/flight-client-entry-plugin.ts
@@ -52,6 +52,8 @@ export type ActionManifest = {
 const pluginState = getProxiedPluginState({
   // A map to track "action" -> "list of bundles".
   serverActions: {} as ActionManifest,
+  edgeServerActions: {} as ActionManifest,
+  actionModId: {} as Record<string, string | number>,
 
   // Manifest of CSS entry files for server/edge server.
   serverCSSManifest: {} as FlightCSSManifest,
@@ -283,6 +285,12 @@ export class FlightClientEntryPlugin {
       )
 
       // Create action entry
+      if (this.isEdgeServer) {
+        pluginState.edgeServerActions = {}
+      } else {
+        pluginState.serverActions = {}
+      }
+
       if (actionEntryImports.size > 0) {
         addActionEntryList.push(
           this.injectActionEntry({
@@ -698,15 +706,18 @@ export class FlightClientEntryPlugin {
       actions: JSON.stringify(actionsArray),
     })}!`
 
+    let currentCompilerServerActions = this.isEdgeServer
+      ? pluginState.serverActions
+      : pluginState.edgeServerActions
     for (const [p, names] of actionsArray) {
       for (const name of names) {
         const id = generateActionId(p, name)
-        if (typeof pluginState.serverActions[id] === 'undefined') {
-          pluginState.serverActions[id] = {
+        if (typeof currentCompilerServerActions[id] === 'undefined') {
+          currentCompilerServerActions[id] = {
             workers: {},
           }
         }
-        pluginState.serverActions[id].workers[bundlePath] = ''
+        currentCompilerServerActions[id].workers[bundlePath] = ''
       }
     }
 
@@ -760,8 +771,6 @@ export class FlightClientEntryPlugin {
     compilation: webpack.Compilation,
     assets: webpack.Compilation['assets']
   ) {
-    const actionModId: Record<string, string | number> = {}
-
     traverseModules(compilation, (mod, _chunk, chunkGroup, modId) => {
       // Go through all action entries and record the module ID for each entry.
       if (
@@ -769,19 +778,23 @@ export class FlightClientEntryPlugin {
         mod.request &&
         /next-flight-action-entry-loader/.test(mod.request)
       ) {
-        actionModId[chunkGroup.name] = modId
+        pluginState.actionModId[chunkGroup.name] = modId
       }
     })
 
-    for (let id in pluginState.serverActions) {
-      const action = pluginState.serverActions[id]
+    const fullServerActions = {
+      ...pluginState.serverActions,
+      ...pluginState.edgeServerActions,
+    }
+    for (let id in fullServerActions) {
+      const action = fullServerActions[id]
       for (let name in action.workers) {
-        action.workers[name] = actionModId[name]
+        action.workers[name] = pluginState.actionModId[name]
       }
     }
 
     const json = JSON.stringify(
-      pluginState.serverActions,
+      fullServerActions,
       null,
       this.dev ? 2 : undefined
     )
