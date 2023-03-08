ddff41a9082f2016fda9e16c638c0d1ccef3f4f3
Shu Ding <g@shud.in>
2023-03-09 00:07:31 +0100

    Support re-exporting unnamed function expression (#46936)
    Add support for cases like:
    
    ```js
    const foo = async function () {}
    export default foo
    
    const bar = async function() {}
    export { bar }
    ```
    
    Also fix a bug where nested async function declarations are mistakenly
    counted as actions.
    
    Fixes NEXT-800.

--- packages/next-swc/crates/core/tests/errors/server-actions/1/output.js
+++ packages/next-swc/crates/core/tests/errors/server-actions/1/output.js
@@ -1,4 +1,1 @@
-/* __next_internal_action_entry_do_not_use__ foo */ export function foo() {}
-foo.$$typeof = Symbol.for("react.server.reference");
-foo.$$id = "ab21efdafbe611287bc25c0462b1e0510d13e48b";
-foo.$$bound = [];
+/* __next_internal_action_entry_do_not_use__  */ export function foo() {}
