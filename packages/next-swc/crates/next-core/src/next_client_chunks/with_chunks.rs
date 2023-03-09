511b244f4a5182931b7a963ced031eb883d76a46
Tobias Koppers <tobias.koppers@googlemail.com>
2023-03-09 17:08:03 +0100

    improve introspection (vercel/turbo#4135)
    ### Description
    
    Add more details to introspection and make it more robust to errors
    during introspection
    
    ### Testing Instructions
    
    go to http://localhost:3000/__turbopack__/

--- packages/next-swc/crates/next-core/src/next_client_chunks/with_chunks.rs
+++ packages/next-swc/crates/next-core/src/next_client_chunks/with_chunks.rs
@@ -222,7 +222,7 @@ impl AssetReference for WithChunksAssetReference {
 #[turbo_tasks::value_impl]
 impl ChunkableAssetReference for WithChunksAssetReference {
     #[turbo_tasks::function]
-    fn chunking_type(&self, _context: ChunkingContextVc) -> ChunkingTypeOptionVc {
+    fn chunking_type(&self) -> ChunkingTypeOptionVc {
         ChunkingTypeOptionVc::cell(Some(ChunkingType::Separate))
     }
 }
