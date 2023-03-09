a415501a24f6962ade3b466da3ff901713fab94a
Tobias Koppers <tobias.koppers@googlemail.com>
2023-03-09 17:05:29 +0100

    fix app dir with latest next.js (vercel/turbo#4134)
    ### Description
    
    * [fix manifest for
    __ssr_module_mapping__](https://github.com/vercel/turbo/commit/4abd87312d05a74ee858a8f556bfe567f3a9e008)
    * [add missing styled-jsx
    alias](https://github.com/vercel/turbo/commit/d9b4a4df7f81b18e657689f4e182bc9ef2259633)
    
    ### Testing Instructions
    
    layout-playground

--- crates/next-core/src/next_import_map.rs
+++ crates/next-core/src/next_import_map.rs
@@ -396,6 +396,7 @@ pub async fn insert_next_shared_aliases(
     );
 
     import_map.insert_singleton_alias("@swc/helpers", get_next_package(project_path));
+    import_map.insert_singleton_alias("styled-jsx", get_next_package(project_path));
     import_map.insert_singleton_alias("next", project_path);
     import_map.insert_singleton_alias("react", project_path);
     import_map.insert_singleton_alias("react-dom", project_path);
