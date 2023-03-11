115f44b63fe75fa8525551f533556c1de4d3fc5a
Bradley Dirheimer <53783914+dirheimerb@users.noreply.github.com>
2023-03-11 11:37:48 -0500

    Update cloudinary.ts
    This change will remove the typescript error.  cloudinary.config doesn't exist, it should be updated to cloudinary.v2.config

--- examples/with-cloudinary/utils/cloudinary.ts
+++ examples/with-cloudinary/utils/cloudinary.ts
@@ -1,7 +1,8 @@
 import cloudinary from 'cloudinary'
 
+// The @ts-ignore can be removed if you add cloudinary.v2.config
 // @ts-ignore
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
