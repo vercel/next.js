ea9550e04048f8ad56f28675e954ed468bee88de
Balázs Orbán <info@balazsorban.com>
2023-03-13 15:39:33 +0100

    add top padding

--- packages/create-next-app/templates/app-tw/ts/app/page.tsx
+++ packages/create-next-app/templates/app-tw/ts/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div
         className={`z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex`}
       >
-        <p className="pt- fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800 dark:bg-opacity-30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800 lg:dark:bg-opacity-30">
+        <p className="pt- fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800 dark:bg-opacity-30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800 lg:dark:bg-opacity-30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
