daad35757732bfb77c996357ea72fe9d6c6c8f1a
Jan Kaifer <jan@kaifer.cz>
2023-03-13 15:04:39 +0100

    Moved instrumentation to instrumentation-node with a proper imports

--- examples/with-opentelemetry/instrumentation.ts
+++ examples/with-opentelemetry/instrumentation.ts
@@ -1,32 +1,7 @@
 export function register() {
+  // We need to make sure that we import these files only in Node.js environment.
+  // OpenTelemetry is **not** supported on Edge or Client side at the moment.
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    // We need to make sure that we import these files only in Node.js environment.
-    // OpenTelemetry is **not** supported on Edge or Client side at the moment.
-    const { Resource } = require('@opentelemetry/resources')
-    const {
-      SemanticResourceAttributes,
-    } = require('@opentelemetry/semantic-conventions')
-    const { NodeTracerProvider, SimpleSpanProcessor } =
-      require('@opentelemetry/sdk-trace-node') as typeof import('@opentelemetry/sdk-trace-node')
-
-    // You can use gRPC exporter instead
-    const {
-      OTLPTraceExporter,
-    } = require('@opentelemetry/exporter-trace-otlp-http')
-
-    // Next.js expects you to use to register TraceProvider. It won't work if you use NodeSDK instead.
-    // We use registered provider to create traces inside of Next.js internals.
-    const provider = new NodeTracerProvider({
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: 'next-app',
-      }),
-    })
-
-    provider.addSpanProcessor(
-      new SimpleSpanProcessor(new OTLPTraceExporter({}))
-    )
-
-    // Make sure to register you provider
-    provider.register()
+    require('./instrumentation-node').register()
   }
 }
