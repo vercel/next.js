ad2bb66d6ae8dabd8aa2053ccf2b2d89fab1261a
Jan Kaifer <jan@kaifer.cz>
2023-03-13 16:17:14 +0100

    run instrumentation in main scope instead of register function

--- examples/with-opentelemetry/instrumentation.ts
+++ examples/with-opentelemetry/instrumentation.ts
@@ -2,6 +2,6 @@ export function register() {
   // We need to make sure that we import these files only in Node.js environment.
   // OpenTelemetry is **not** supported on Edge or Client side at the moment.
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    require('./instrumentation-node').register()
+    require('./instrumentation-node')
   }
 }
