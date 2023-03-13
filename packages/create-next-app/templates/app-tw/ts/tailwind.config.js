4489bb131890658266860b8713fb9908f4b3322e
Balázs Orbán <info@balazsorban.com>
2023-03-13 15:19:27 +0100

    more styling

--- packages/create-next-app/templates/app-tw/ts/tailwind.config.js
+++ packages/create-next-app/templates/app-tw/ts/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
   plugins: [],
 }
