b146da60370d36af0ffce9c8417ce9fef85a03ba
Shu Ding <g@shud.in>
2023-03-13 18:53:43 +0100

    Refactor renderers and RenderResult (#46955)
    This PR does two major changes:
    
    1. Make sure both pages renderer and app renderer return `RenderResult`,
    no more `null`. This was achieved with a new `null` type in the
    constructor `new RenderResult(null)`, and a `.isNull()` method.
    2. Remove all mutations of the `renderOpts` object inside renderers. To
    pass extra information out, they need to be attached to the
    `RenderResult` now. This also requires 1) to be done.
    
    These changes are the initial steps to the isolated rendering worker
    architecture. Besides those there're also some type improvements.
    
    Fixes NEXT-807.
    
    ---------

--- packages/next/src/client/components/router-reducer/invalidate-cache-below-flight-segmentpath.ts
+++ packages/next/src/client/components/router-reducer/invalidate-cache-below-flight-segmentpath.ts
@@ -1,5 +1,5 @@
-import { CacheNode } from '../../../shared/lib/app-router-context'
-import { FlightSegmentPath } from '../../../server/app-render'
+import type { CacheNode } from '../../../shared/lib/app-router-context'
+import type { FlightSegmentPath } from '../../../server/app-render'
 
 /**
  * Fill cache up to the end of the flightSegmentPath, invalidating anything below it.
