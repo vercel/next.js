3e30c4d57bc3c3d6b89d6867b8de8646a3ae82d0
Balázs Orbán <info@balazsorban.com>
2023-03-13 15:26:37 +0100

    Apply suggestions from code review

--- examples/with-cloudinary/utils/cloudinary.ts
+++ examples/with-cloudinary/utils/cloudinary.ts
@@ -1,7 +1,5 @@
 import cloudinary from 'cloudinary'
 
-// The @ts-ignore can be removed if you add cloudinary.v2.config
-// @ts-ignore
 cloudinary.v2.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
