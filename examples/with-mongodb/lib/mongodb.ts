ca4056761c2ff2a4876bc79f073cf84e78486698
JJ Kasper <jj@jjsweb.site>
2023-03-13 09:36:26 -0700

    lint fix

--- examples/with-mongodb/lib/mongodb.ts
+++ examples/with-mongodb/lib/mongodb.ts
@@ -13,9 +13,9 @@ let clientPromise: Promise<MongoClient>
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-	let globalWithMongo = global as typeof globalThis & {
-		_mongoClientPromise?: Promise<MongoClient>
-	}
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>
+  }
 
   if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options)
@@ -31,4 +31,3 @@ if (process.env.NODE_ENV === 'development') {
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default clientPromise
-
