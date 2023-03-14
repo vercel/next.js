2579ad7648a1c247ae2c99013b37cb2f24d2296f
Mojtaba <45169963+Gladowar@users.noreply.github.com>
2023-03-13 21:26:08 -0700

    updated zustand example to 4.3.6, changed out deprecated methods (#46911)
    updated zustand example to 4.3.6
    changed out zustand's deprecated methods (createContext, create)
    converted the example to typescript
    
    ## Why
    zustand's example in nextjs repo is for zustand v3 which is quite
    different to how things are done in v4, it was also in javascript.
    back when when I started to use zustand in my nextjs app, this example
    helped me a lot and now, I wanna do the same for devs that come here to
    see how they can integrate next and zustand.
    
    ## Documentation / Examples
    [✓] Make sure the linting passes by running pnpm build && pnpm lint
    [✓] The "examples guidelines" are followed from [our contributing
    doc](https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md)
    
    ---------
    
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- examples/with-zustand/src/pages/index.tsx
+++ examples/with-zustand/src/pages/index.tsx
@@ -1,15 +1,17 @@
+import type { GetServerSideProps } from 'next'
 import Page from '../components/page'
 import { initializeStore } from '../lib/store'
 
-export default function SSG() {
+export default function Home() {
   return <Page />
 }
 
-// If you build and start the app, the date returned here will have the same
-// value for all requests, as this method gets executed at build time.
-// You will not see this while in development mode though.
-export function getStaticProps() {
+// The date returned here will be different for every request that hits the page,
+// that is because the page becomes a serverless function instead of being statically
+// exported when you use `getServerSideProps` or `getInitialProps`
+export const getServerSideProps: GetServerSideProps = async () => {
   const zustandStore = initializeStore()
+
   return {
     props: {
       // the "stringify and then parse again" piece is required as next.js
