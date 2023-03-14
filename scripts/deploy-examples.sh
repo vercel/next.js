e5c123907aee95271215ca27a728d31c1a44c145
Steven <steven@ceriously.com>
2023-03-14 18:13:51 -0400

    Fix typo

--- scripts/deploy-examples.sh
+++ scripts/deploy-examples.sh
@@ -3,7 +3,7 @@
 CHANGED_EXAMPLES=$(node scripts/run-for-change.js --type deploy-examples --listChangedDirectories)
 
 for CWD in $CHANGED_EXAMPLES ; do
-  PROJECT=$(echo $CWD | | tr '/' '-')
+  PROJECT=$(echo $CWD | tr '/' '-')
   echo "Deploying directory $CWD as $PROJECT to Vercel..."
   vercel link --cwd $CWD --scope vercel --project "$PROJECT" --token "$VERCEL_API_TOKEN"
   vercel deploy --cwd $CWD --token "$VERCEL_API_TOKEN"
