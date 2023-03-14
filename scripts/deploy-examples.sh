557c275f5682d65d5b9c9b200608546685a8cca5
Steven <steven@ceriously.com>
2023-03-14 18:32:05 -0400

    Add missing `--yes` option

--- scripts/deploy-examples.sh
+++ scripts/deploy-examples.sh
@@ -7,6 +7,6 @@ for CWD in $CHANGED_EXAMPLES ; do
   HYPHENS=$(echo "$CWD" | tr '/' '-')
   PROJECT="nextjs-$HYPHENS"
   echo "Deploying directory $CWD as $PROJECT to Vercel..."
-  vercel link --cwd "$CWD" --scope vercel --project "$PROJECT" --token "$VERCEL_API_TOKEN"
+  vercel link --cwd "$CWD" --scope vercel --project "$PROJECT" --token "$VERCEL_API_TOKEN" --yes
   vercel deploy --cwd "$CWD" --token "$VERCEL_API_TOKEN"
 done;
