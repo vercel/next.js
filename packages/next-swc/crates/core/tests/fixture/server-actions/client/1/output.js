4c3f59f88d8821675f5a63de6d9a7de4a4fbc397
Shu Ding <g@shud.in>
2023-03-15 23:58:18 +0100

    Implement server entry creation in client layer (#47127
    In short, this PR adds a 3rd layer to the server compiler. This extra
    layer is for marking the modules when re-entering the server layer from
    a client component. It is almost identical to the existing server layer
    and it should have all the same bundling and runtime behaviors, but it's
    still special because it's not allowed to enter the client layer again
    from there.
    
    Because of that, we create the extra entry for that new layer when the
    client layer compilation finishes in the `finishModules` phase. The new
    entry is handled normally as it's in the server layer. But the original
    module in the client layer will be compiled specially as special no-op
    exports, and will then be connected via the `callServer` wrapper.
    
    fix NEXT-809 ([link](https://linear.app/vercel/issue/NEXT-809)).

---
+++ packages/next-swc/crates/core/tests/fixture/server-actions/client/1/output.js
@@ -0,0 +1,13 @@
+// app/send.ts
+/* __next_internal_action_entry_do_not_use__ myAction,default */ export async function myAction(...args) {
+    return __build_action__(myAction, args);
+}
+export default async function $$ACTION_0(...args) {
+    return __build_action__($$ACTION_0, args);
+};
+myAction.$$typeof = Symbol.for("react.server.reference");
+myAction.$$id = "e10665baac148856374b2789aceb970f66fec33e";
+myAction.$$bound = [];
+$$ACTION_0.$$typeof = Symbol.for("react.server.reference");
+$$ACTION_0.$$id = "c18c215a6b7cdc64bf709f3a714ffdef1bf9651d";
+$$ACTION_0.$$bound = [];
