21538ef59349c58fc03aca4c418cc82788928023
Shu Ding <g@shud.in>
2023-03-15 23:49:15 +0100

    support inlined functions

--- packages/next-swc/crates/core/tests/fixture/server-actions/16/output.js
+++ packages/next-swc/crates/core/tests/fixture/server-actions/16/output.js
@@ -2,13 +2,10 @@
 const v1 = 'v1';
 export function Item({ id1 , id2  }) {
     const v2 = id2;
-    const deleteItem = $$ACTION_0 = async ()=>$$ACTION_1($$ACTION_0.$$bound);
-    $$ACTION_0.$$typeof = Symbol.for("react.server.reference");
-    $$ACTION_0.$$id = "188d5d945750dc32e2c842b93c75a65763d4a922";
-    $$ACTION_0.$$bound = [
+    const deleteItem = ($$ACTION_0 = async ()=>$$ACTION_1($$ACTION_0.$$bound), $$ACTION_0.$$typeof = Symbol.for("react.server.reference"), $$ACTION_0.$$id = "188d5d945750dc32e2c842b93c75a65763d4a922", $$ACTION_0.$$bound = [
         id1,
         v2
-    ];
+    ], $$ACTION_0);
     return <Button action={deleteItem}>Delete</Button>;
 }
 export const $$ACTION_1 = async (closure)=>{
