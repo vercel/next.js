5f3bb3db6217ce6ef635aaa017b03a494eaef3d0
JJ Kasper <jj@jjsweb.site>
2023-03-16 16:24:28 -0700

    Fix failing CI checks

--- scripts/deploy-examples.sh
+++ scripts/deploy-examples.sh
@@ -8,9 +8,13 @@ if [ "$DEPLOY_ENVIRONMENT" = "production" ]; then
 fi
 
 for CWD in $CHANGED_EXAMPLES ; do
-  HYPHENS=$(echo "$CWD" | tr '/' '-')
-  PROJECT="nextjs-$HYPHENS"
-  echo "Deploying directory $CWD as $PROJECT to Vercel..."
-  vercel link --cwd "$CWD" --scope vercel --project "$PROJECT" --token "$VERCEL_API_TOKEN" --yes
-  vercel deploy --cwd "$CWD" --token "$VERCEL_API_TOKEN" $PROD
+  if [[ $CWD = examples* ]]; then
+    HYPHENS=$(echo "$CWD" | tr '/' '-')
+    PROJECT="nextjs-$HYPHENS"
+    echo "Deploying directory $CWD as $PROJECT to Vercel..."
+    vercel link --cwd "$CWD" --scope vercel --project "$PROJECT" --token "$VERCEL_API_TOKEN" --yes
+    vercel deploy --cwd "$CWD" --token "$VERCEL_API_TOKEN" $PROD
+  else
+    echo "Ignoring non-example $CWD"
+  fi
 done;
