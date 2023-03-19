2b00913e8caecbf393dd4f6dd94efe3b4f17bc23
Balázs Orbán <info@balazsorban.com>
2023-03-19 12:12:44 +0100

    remove 13

--- packages/create-next-app/templates/app/ts/app/page.tsx
+++ packages/create-next-app/templates/app/ts/app/page.tsx
@@ -40,9 +40,6 @@ export default function Home() {
           height={37}
           priority
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
       </div>
 
       <div className={styles.grid}>
