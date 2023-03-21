1d325d4bbe6758e42600e0916566b74c6fcc43e8
Shu Ding <g@shud.in>
2023-03-21 16:33:15 +0100

    update test

--- test/e2e/app-dir/not-found/not-found.test.ts
+++ test/e2e/app-dir/not-found/not-found.test.ts
@@ -6,20 +6,22 @@ createNextDescribe(
     files: __dirname,
     skipDeployment: true,
   },
-  ({ next }) => {
+  ({ next, isNextDev }) => {
     describe('root not-found page', () => {
       it('should use the not-found page for non-matching routes', async () => {
         const html = await next.render('/random-content')
         expect(html).toContain('This Is The Not Found Page')
       })
 
-      it('should create the 404 mapping and copy the file to pages', async () => {
-        const html = await next.readFile('.next/server/pages/404.html')
-        expect(html).toContain('This Is The Not Found Page')
-        expect(
-          await next.readFile('.next/server/pages-manifest.json')
-        ).toContain('"pages/404.html"')
-      })
+      if (!isNextDev) {
+        it('should create the 404 mapping and copy the file to pages', async () => {
+          const html = await next.readFile('.next/server/pages/404.html')
+          expect(html).toContain('This Is The Not Found Page')
+          expect(
+            await next.readFile('.next/server/pages-manifest.json')
+          ).toContain('"pages/404.html"')
+        })
+      }
     })
   }
 )
