922d0c336a574f04f5fb8739e29ba1946f7f0f9c
abhishekpatilnet <116247925+abhishekpatilnet@users.noreply.github.com>
2023-03-21 10:11:10 +0530

    Update header.tsx

--- examples/cms-wordpress/components/header.tsx
+++ examples/cms-wordpress/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
-        Blog
+        Webstar Infotech
       </Link>
       .
     </h2>
