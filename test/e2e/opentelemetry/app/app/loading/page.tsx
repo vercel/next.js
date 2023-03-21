d69189dae6ee24d0e424481a3452db7dd6966f06
Jan Kaifer <jan@kaifer.cz>
2023-03-21 17:09:10 +0100

    fix loading example

--- test/e2e/opentelemetry/app/app/loading/page.tsx
+++
@@ -1,7 +0,0 @@
-// We want to trace this fetch in runtime
-export const dynamic = 'force-dynamic'
-
-export default async function Page() {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  return <p>app/loading/page</p>
-}
