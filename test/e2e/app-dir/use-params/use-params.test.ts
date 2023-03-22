f029023a3c8f1caf65109f208c1b666fe51d2e21
Tim Neutkens <tim@timneutkens.nl>
2023-03-22 10:26:19 +0100

    Add useParams() (#47373)
    ### What?
    
    Adds `useParams` to read the canonical parameters.
    
    ```tsx
    'use client'
    
    import { useParams } from 'next/navigation'
    
    export default function Page() {
      const { id } = useParams()
      return (
        <div>
          <div id="param-id">{id}</div>
        </div>
      )
    }
    ```
    
    ### Why?
    
    Allows for accessing segment parameters for the entire page.
    
    ### How?
    
    It uses a similar resolving of `useSelectedLayoutSegments` which resolve `children` and if `children` is not available the first available parallel route instead.
    
    Closes NEXT-99

---
+++ test/e2e/app-dir/use-params/use-params.test.ts
@@ -0,0 +1,40 @@
+import { createNextDescribe } from 'e2e-utils'
+
+createNextDescribe(
+  'use-params',
+  {
+    files: __dirname,
+  },
+  ({ next }) => {
+    it('should work for single dynamic param', async () => {
+      const $ = await next.render$('/a/b')
+      expect($('#param-id').text()).toBe('a')
+    })
+    it('should work for nested dynamic params', async () => {
+      const $ = await next.render$('/a/b')
+      expect($('#param-id').text()).toBe('a')
+      expect($('#param-id2').text()).toBe('b')
+    })
+
+    it('should work for single dynamic param client navigating', async () => {
+      const browser = await next.browser('/')
+      expect(
+        await browser
+          .elementByCss('#to-a')
+          .click()
+          .waitForElementByCss('#param-id')
+          .text()
+      ).toBe('a')
+    })
+
+    it('should work for nested dynamic params client navigating', async () => {
+      const browser = await next.browser('/')
+      await browser
+        .elementByCss('#to-a-b')
+        .click()
+        .waitForElementByCss('#param-id')
+      expect(await browser.elementByCss('#param-id').text()).toBe('a')
+      expect(await browser.elementByCss('#param-id2').text()).toBe('b')
+    })
+  }
+)
