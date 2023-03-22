20cde1910fc5ef48f756ec3a19f63119767e2683
Tobias Koppers <tobias.koppers@googlemail.com>
2023-03-22 08:23:54 +0000

    allow to start an integration test

--- packages/next-swc/crates/next-dev-tests/test-harness/harness.ts
+++ packages/next-swc/crates/next-dev-tests/test-harness/harness.ts
@@ -1,69 +1,77 @@
-import * as jest from "jest-circus-browser/dist/umd/jest-circus";
-import expectMod from "expect/build-es5/index";
+import * as jest from 'jest-circus-browser/dist/umd/jest-circus'
+import expectMod from 'expect/build-es5/index'
 
 type CallSignature<T extends (...a: any[]) => unknown> = (
   ...a: Parameters<T>
-) => ReturnType<T>;
+) => ReturnType<T>
 
 declare global {
-  var __jest__: typeof jest;
-  var expect: typeof expectMod;
+  var __jest__: typeof jest
+  var expect: typeof expectMod
   // We need to extract only the call signature as `autoReady(jest.describe)` drops all the other properties
-  var describe: CallSignature<typeof jest.describe>;
-  var it: CallSignature<typeof jest.it>;
-  var READY: (arg: string) => void;
-  var nsObj: (obj: any) => any;
+  var describe: CallSignature<typeof jest.describe>
+  var it: CallSignature<typeof jest.it>
+  var READY: (arg: string) => void
+  var nsObj: (obj: any) => any
 
   interface Window {
-    NEXT_HYDRATED?: boolean;
-    onNextHydrated?: () => void;
+    NEXT_HYDRATED?: boolean
+    onNextHydrated?: () => void
   }
 }
 
-let isReady = false;
+let isReady = false
 function autoReady<T extends (...a: any[]) => unknown>(
   fn: (...args: Parameters<T>) => ReturnType<T>
 ): (...args: Parameters<T>) => ReturnType<T> {
   return (...args) => {
     if (!isReady) {
-      isReady = true;
+      isReady = true
       requestIdleCallback(
         () => {
-          READY("");
+          if (typeof READY === 'function') {
+            READY('')
+          } else {
+            console.info(
+              '%cTurbopack tests:',
+              'font-weight: bold;',
+              'Entering debug mode. Run `await __jest__.run()` in the browser console to run tests.'
+            )
+          }
         },
         { timeout: 20000 }
-      );
+      )
     }
-    return fn(...args);
-  };
+    return fn(...args)
+  }
 }
 
-globalThis.__jest__ = jest;
-globalThis.expect = expectMod;
-globalThis.describe = autoReady(jest.describe);
-globalThis.it = autoReady(jest.it);
+globalThis.__jest__ = jest
+globalThis.expect = expectMod
+globalThis.describe = autoReady(jest.describe)
+globalThis.it = autoReady(jest.it)
 
 // From https://github.com/webpack/webpack/blob/9fcaa243573005d6fdece9a3f8d89a0e8b399613/test/TestCases.template.js#L422
 globalThis.nsObj = function nsObj(obj) {
   Object.defineProperty(obj, Symbol.toStringTag, {
-    value: "Module",
-  });
-  return obj;
-};
+    value: 'Module',
+  })
+  return obj
+}
 
 export function wait(ms: number): Promise<void> {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    setTimeout(resolve, ms)
+  })
 }
 
 async function waitForPath(contentWindow: Window, path: string): Promise<void> {
   while (true) {
     if (contentWindow.location.pathname === path) {
-      break;
+      break
     }
 
-    await wait(1);
+    await wait(1)
   }
 }
 
@@ -124,37 +132,37 @@ export function waitForHydration(
   return new Promise((resolve) => {
     if (
       iframe.contentDocument != null &&
-      iframe.contentDocument.readyState === "complete"
+      iframe.contentDocument.readyState === 'complete'
     ) {
-      waitForHydrationAndResolve(iframe.contentWindow!, path).then(resolve);
+      waitForHydrationAndResolve(iframe.contentWindow!, path).then(resolve)
     } else {
-      iframe.addEventListener("load", () => {
-        waitForHydrationAndResolve(iframe.contentWindow!, path).then(resolve);
-      });
+      iframe.addEventListener('load', () => {
+        waitForHydrationAndResolve(iframe.contentWindow!, path).then(resolve)
+      })
     }
-  });
+  })
 }
 
 async function waitForHydrationAndResolve(
   contentWindow: Window,
   path: string
 ): Promise<void> {
-  await waitForPath(contentWindow, path);
+  await waitForPath(contentWindow, path)
 
   return await new Promise((resolve) => {
     if (contentWindow.NEXT_HYDRATED) {
-      resolve();
+      resolve()
     } else {
       contentWindow.onNextHydrated = () => {
-        resolve();
-      };
+        resolve()
+      }
     }
-  });
+  })
 }
 
 export function markAsHydrated() {
-  window.NEXT_HYDRATED = true;
-  if (typeof window.onNextHydrated === "function") {
-    window.onNextHydrated();
+  window.NEXT_HYDRATED = true
+  if (typeof window.onNextHydrated === 'function') {
+    window.onNextHydrated()
   }
 }
