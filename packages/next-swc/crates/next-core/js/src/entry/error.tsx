6d6f91eadb52e944152861fa5ca49005eaf2aeee
Leah <github.leah@hrmny.sh>
2023-03-22 19:43:08 +0100

    fix typescript errors in next-core

--- packages/next-swc/crates/next-core/js/src/entry/error.tsx
+++ packages/next-swc/crates/next-core/js/src/entry/error.tsx
@@ -1,7 +1,2 @@
 // TODO(alexkirsz) export * would be preferrable here once supported.
-export {
-  default,
-  getStaticProps,
-  __N_SSG,
-  __N_SSP,
-} from "@vercel/turbopack-next/internal/_error";
+export { default } from "@vercel/turbopack-next/internal/_error";
