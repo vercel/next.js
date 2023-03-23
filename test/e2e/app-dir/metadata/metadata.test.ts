d187c365036234397d95f1b73ec1a87c7d925b87
Jiachi Liu <inbox@huozhi.im>
2023-03-23 17:38:42 +0100

    Add content hash for dynamic og image routes (#47440)
    * Add `contenthash` as query (`?<hash>`) for dynamic image and icon
    routes as well.
    * Merge the metadata image info extration into 1 single loader, dynamic
    and static image route will all pass through
    `next-metadata-image-loader` to extract the image info such as size,
    content type, etc.
    * Applying same cache header from `@vercel/og`, for development we use
    "no cache" header, for production, we use "long cache" header
    
    Closes NEXT-882

--- test/e2e/app-dir/metadata/metadata.test.ts
+++ test/e2e/app-dir/metadata/metadata.test.ts
@@ -658,12 +658,16 @@ createNextDescribe(
         expect(resAppleIcon.status).toBe(200)
         expect(resAppleIcon.headers.get('content-type')).toBe('image/png')
         expect(resAppleIcon.headers.get('cache-control')).toBe(
-          'public, max-age=0, must-revalidate'
+          isNextDev
+            ? 'no-cache, no-store'
+            : 'public, immutable, no-transform, max-age=31536000'
         )
         expect(resIcon.status).toBe(200)
         expect(resIcon.headers.get('content-type')).toBe('image/png')
         expect(resIcon.headers.get('cache-control')).toBe(
-          'public, max-age=0, must-revalidate'
+          isNextDev
+            ? 'no-cache, no-store'
+            : 'public, immutable, no-transform, max-age=31536000'
         )
       })
 
