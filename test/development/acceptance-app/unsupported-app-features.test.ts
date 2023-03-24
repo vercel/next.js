f63088c7284ea2654746e293ea49448f0443d60e
JJ Kasper <jj@jjsweb.site>
2023-03-24 15:19:41 -0700

    skip

--- test/development/acceptance-app/unsupported-app-features.test.ts
+++ test/development/acceptance-app/unsupported-app-features.test.ts
@@ -3,32 +3,35 @@ import path from 'path'
 import { createNextDescribe, FileRef } from 'e2e-utils'
 import { getRedboxDescription, hasRedbox } from 'next-test-utils'
 
-createNextDescribe(
-  'Error Overlay unsupported app features',
-  {
-    files: new FileRef(path.join(__dirname, 'fixtures', 'default-template')),
-  },
-  ({ next }) => {
-    it('should show error exporting AMP config in app dir', async () => {
-      const browser = await next.browser('/')
+// TODO: investigate this test flaking in CI
+describe.skip('should skip for now', () => {
+  createNextDescribe(
+    'Error Overlay unsupported app features',
+    {
+      files: new FileRef(path.join(__dirname, 'fixtures', 'default-template')),
+    },
+    ({ next }) => {
+      it('should show error exporting AMP config in app dir', async () => {
+        const browser = await next.browser('/')
 
-      // Add AMP export
-      await next.patchFile(
-        'app/page.js',
+        // Add AMP export
+        await next.patchFile(
+          'app/page.js',
+          `
+          export const config = { amp: true }
+
+          import Component from '../index'
+          export default function Page() {
+            return <Component />
+          }
         `
-        export const config = { amp: true }
+        )
 
-        import Component from '../index'
-        export default function Page() {
-          return <Component />
-        }
-      `
-      )
-
-      expect(await hasRedbox(browser, true)).toBe(true)
-      expect(await getRedboxDescription(browser)).toInclude(
-        'AMP is not supported in the app directory. If you need to use AMP it will continue to be supported in the pages directory.'
-      )
-    })
-  }
-)
+        expect(await hasRedbox(browser, true)).toBe(true)
+        expect(await getRedboxDescription(browser)).toInclude(
+          'AMP is not supported in the app directory. If you need to use AMP it will continue to be supported in the pages directory.'
+        )
+      })
+    }
+  )
+})
