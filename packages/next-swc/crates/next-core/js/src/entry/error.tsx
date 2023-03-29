e88f693668174b6d7ba0396d9640f864f2d4194f
Leah <github.leah@hrmny.sh>
2023-03-29 03:48:25 +0200

    add back exports even if they should not exist

--- packages/next-swc/crates/next-core/js/src/entry/error.tsx
+++ packages/next-swc/crates/next-core/js/src/entry/error.tsx
@@ -1,2 +1,10 @@
 // TODO(alexkirsz) export * would be preferrable here once supported.
-export { default } from "@vercel/turbopack-next/internal/_error";
+export {
+  default,
+  // @ts-expect-error
+  getStaticProps,
+  // @ts-expect-error
+  __N_SSG,
+  // @ts-expect-error
+  __N_SSP,
+} from "@vercel/turbopack-next/internal/_error";
