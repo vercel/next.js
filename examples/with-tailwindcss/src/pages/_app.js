dd32fdb42acc12b43aa4586d92d3dfdcd3db0e6e
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-03-29 21:25:58 +0500

    Init

---
+++ examples/with-tailwindcss/src/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/globals.css'
+import '@/styles/tailwind.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ examples/with-tailwindcss/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
