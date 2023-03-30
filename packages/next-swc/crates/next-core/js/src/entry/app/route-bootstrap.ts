f31f897915cfeb00a5c33ac39b04e5a2d1bef5b1
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-03-29 22:30:54 -0600

    fix: added the rust TS typings for the imported route module

--- packages/next-swc/crates/next-core/js/src/entry/app/route-bootstrap.ts
+++ packages/next-swc/crates/next-core/js/src/entry/app/route-bootstrap.ts
@@ -1,13 +1,9 @@
-// PAGE is set from rust code
-declare const PAGE: string;
-// PATHNAME is set from rust code
-declare const PATHNAME: string;
+// PAGE and PATHNAME is set from rust code
+declare const PAGE: string, PATHNAME: string;
 
 import { EdgeModuleWrapper } from "next/dist/build/webpack/loaders/next-edge-app-route-loader/edge-module-wrapper";
 
-// @ts-expect-error - ROUTE_MODULE is set from rust code
 import RouteModule from "ROUTE_MODULE";
-
 import * as userland from "ENTRY";
 
 // TODO: (wyattjoh) - perform the option construction in Rust to allow other modules to accept different options
