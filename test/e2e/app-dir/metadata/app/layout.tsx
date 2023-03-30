efa81500dfb65ffff04b20000dabefc49e9bb356
Jiachi Liu <inbox@huozhi.im>
2023-03-30 13:33:55 +0200

    add test

--- test/e2e/app-dir/metadata/app/layout.tsx
+++ test/e2e/app-dir/metadata/app/layout.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from 'react'
-
 export default function Layout({ children }) {
   return (
     <html>
       <head></head>
-      <body>
-        <Suspense fallback={<div>loading...</div>}>{children}</Suspense>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
