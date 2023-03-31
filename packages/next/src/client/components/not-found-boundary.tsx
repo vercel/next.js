vercel/next.js

commit de9278114547c6d7238b05e90c840b3a9739df47

Add default meta tags to error page (#47404)
Default tags of `charset` and `viewport` should also be rendered in app
router error pages like not found pages.

Closes NEXT-869

This PR:
* Extract not found boundary to single component
* Add the default tags to `NotFoundErrorBoundary` rendering
* Fix duplicated `noindex` tag for streaming error rendering

packages/next/src/client/components/not-found-boundary.tsx
@@ -1,0 +1,61 @@
+import React from 'react'
+import { DEFAULT_METADATA_TAGS } from '../../lib/metadata/default-metadata'
+
+interface NotFoundBoundaryProps {
+  notFound?: React.ReactNode
+  notFoundStyles?: React.ReactNode
+  asNotFound?: boolean
+  children: React.ReactNode
+}
+
+class NotFoundErrorBoundary extends React.Component<
+  NotFoundBoundaryProps,
+  { notFoundTriggered: boolean }
+> {
+  constructor(props: NotFoundBoundaryProps) {
+    super(props)
+    this.state = { notFoundTriggered: !!props.asNotFound }
+  }
+
+  static getDerivedStateFromError(error: any) {
+    if (error?.digest === 'NEXT_NOT_FOUND') {
+      return { notFoundTriggered: true }
+    }
+    // Re-throw if error is not for 404
+    throw error
+  }
+
+  render() {
+    if (this.state.notFoundTriggered) {
+      return (
+        <>
+          {DEFAULT_METADATA_TAGS}
+          <meta name="robots" content="noindex" />
+          {this.props.notFoundStyles}
+          {this.props.notFound}
+        </>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export function NotFoundBoundary({
+  notFound,
+  notFoundStyles,
+  asNotFound,
+  children,
+}: NotFoundBoundaryProps) {
+  return notFound ? (
+    <NotFoundErrorBoundary
+      notFound={notFound}
+      notFoundStyles={notFoundStyles}
+      asNotFound={asNotFound}
+    >
+      {children}
+    </NotFoundErrorBoundary>
+  ) : (
+    <>{children}</>
+  )
+}

packages/next/src/lib/metadata/default-metadata.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { ResolvedMetadata } from './types/metadata-interface'
 
 export const createDefaultMetadata = (): ResolvedMetadata => {
@@ -42,3 +43,12 @@ export const createDefaultMetadata = (): ResolvedMetadata => {
     other: {},
   }
 }
+
+export const DEFAULT_METADATA_TAGS = [
+  <meta charSet="utf-8" key="charset" />,
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1"
+    key="viewport"
+  />,
+]

test/e2e/app-dir/navigation/navigation.test.ts
@@ -261,14 +261,24 @@ createNextDescribe(
 
     describe('SEO', () => {
       it('should emit noindex meta tag for not found page when streaming', async () => {
+        const noIndexTag = '<meta name="robots" content="noindex"/>'
         const html = await next.render('/not-found/suspense')
-        expect(html).toContain('<meta name="robots" content="noindex"/>')
+        expect(html).toContain(noIndexTag)
+        expect(html.split(noIndexTag).length).toBe(2) // only contain once
       })
 
       it('should emit refresh meta tag for redirect page when streaming', async () => {
         const html = await next.render('/redirect/suspense')
         expect(html).toContain(
           '<meta http-equiv="refresh" content="0;url=/redirect/result"/>'
+        )
+      })
+
+      it('should contain default meta tags in error page', async () => {
+        const html = await next.render('/not-found/servercomponent')
+        expect(html).toContain('<meta name="robots" content="noindex"/>')
+        expect(html).toContain(
+          '<meta name="viewport" content="width=device-width, initial-scale=1"/>'
         )
       })
     })

packages/next/src/client/components/layout-router.tsx
@@ -24,6 +24,7 @@ import { matchSegment } from './match-segments'
 import { handleSmoothScroll } from '../../shared/lib/router/utils/handle-smooth-scroll'
 import { findHeadInCache } from './router-reducer/reducers/find-head-in-cache'
 import { RedirectBoundary } from './redirect-boundary'
+import { NotFoundBoundary } from './not-found-boundary'
 
 /**
  * Add refetch marker to router state at the point of the current layout segment.
@@ -416,64 +417,6 @@ function LoadingBoundary({
   }
 
   return <>{children}</>
-}
-
-interface NotFoundBoundaryProps {
-  notFound?: React.ReactNode
-  notFoundStyles?: React.ReactNode
-  asNotFound?: boolean
-  children: React.ReactNode
-}
-
-class NotFoundErrorBoundary extends React.Component<
-  NotFoundBoundaryProps,
-  { notFoundTriggered: boolean }
-> {
-  constructor(props: NotFoundBoundaryProps) {
-    super(props)
-    this.state = { notFoundTriggered: !!props.asNotFound }
-  }
-
-  static getDerivedStateFromError(error: any) {
-    if (error?.digest === 'NEXT_NOT_FOUND') {
-      return { notFoundTriggered: true }
-    }
-    // Re-throw if error is not for 404
-    throw error
-  }
-
-  render() {
-    if (this.state.notFoundTriggered) {
-      return (
-        <>
-          <meta name="robots" content="noindex" />
-          {this.props.notFoundStyles}
-          {this.props.notFound}
-        </>
-      )
-    }
-
-    return this.props.children
-  }
-}
-
-function NotFoundBoundary({
-  notFound,
-  notFoundStyles,
-  asNotFound,
-  children,
-}: NotFoundBoundaryProps) {
-  return notFound ? (
-    <NotFoundErrorBoundary
-      notFound={notFound}
-      notFoundStyles={notFoundStyles}
-      asNotFound={asNotFound}
-    >
-      {children}
-    </NotFoundErrorBoundary>
-  ) : (
-    <>{children}</>
-  )
 }
 
 /**

packages/next/src/server/app-render/index.tsx
@@ -73,6 +73,7 @@ import {
   createFlightRouterStateFromLoaderTree,
 } from './create-flight-router-state-from-loader-tree'
 import { PAGE_SEGMENT_KEY } from '../../shared/lib/constants'
+import { DEFAULT_METADATA_TAGS } from '../../lib/metadata/default-metadata'
 
 export const isEdgeRuntime = process.env.NEXT_RUNTIME === 'edge'
 
@@ -1212,7 +1213,7 @@ export async function renderToHTMLOrFlight(
         const getServerInsertedHTML = () => {
           // Loop through all the errors that have been captured but not yet
           // flushed.
-          const errorMetaTags = []
+          const errorMetaTags = [...DEFAULT_METADATA_TAGS]
           for (
             ;
             flushedErrorMetaTagsUntilIndex < allCapturedErrors.length;
@@ -1294,7 +1295,6 @@ export async function renderToHTMLOrFlight(
 
           return result
         } catch (err: any) {
-          const shouldNotIndex = isNotFoundError(err)
           if (isNotFoundError(err)) {
             res.statusCode = 404
           }
@@ -1306,11 +1306,7 @@ export async function renderToHTMLOrFlight(
             ReactDOMServer,
             element: (
               <html id="__next_error__">
-                <head>
-                  {shouldNotIndex ? (
-                    <meta name="robots" content="noindex" />
-                  ) : null}
-                </head>
+                <head></head>
                 <body></body>
               </html>
             ),

commit 2d796ed5940ed82d74170f23d3ac882802dba18c

Fix duplicated default metadata tags (#47690)
Adding default metadata before causes duplicated metadata tags when
adding server inserted html. Remove the ones in the not found boundary
Now we use same way to render metadata to render the default metadata in
error html

Fixes the issue introduced in #47404

---------

Co-authored-by: JJ Kasper <jj@jjsweb.site>

packages/next/src/client/components/not-found-boundary.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { DEFAULT_METADATA_TAGS } from '../../lib/metadata/default-metadata'
 
 interface NotFoundBoundaryProps {
   notFound?: React.ReactNode
@@ -29,7 +28,6 @@ class NotFoundErrorBoundary extends React.Component<
     if (this.state.notFoundTriggered) {
       return (
         <>
-          {DEFAULT_METADATA_TAGS}
           <meta name="robots" content="noindex" />
           {this.props.notFoundStyles}
           {this.props.notFound}

packages/next/src/lib/metadata/default-metadata.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import type { ResolvedMetadata } from './types/metadata-interface'
 
 export function createDefaultMetadata(): ResolvedMetadata {
@@ -48,12 +47,3 @@ export function createDefaultMetadata(): ResolvedMetadata {
     other: {},
   }
 }
-
-export const DEFAULT_METADATA_TAGS = [
-  <meta charSet="utf-8" key="charset" />,
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1"
-    key="viewport"
-  />,
-]

packages/next/src/server/app-render/app-render.tsx
@@ -71,7 +71,6 @@ import {
 } from './create-flight-router-state-from-loader-tree'
 import { handleAction } from './action-handler'
 import { PAGE_SEGMENT_KEY } from '../../shared/lib/constants'
-import { DEFAULT_METADATA_TAGS } from '../../lib/metadata/default-metadata'
 import { NEXT_DYNAMIC_NO_SSR_CODE } from '../../shared/lib/lazy-dynamic/no-ssr-error'
 import { warn } from '../../build/output/log'
 
@@ -1255,7 +1254,7 @@ export async function renderToHTMLOrFlight(
         const getServerInsertedHTML = () => {
           // Loop through all the errors that have been captured but not yet
           // flushed.
-          const errorMetaTags = [...DEFAULT_METADATA_TAGS]
+          const errorMetaTags = []
           for (
             ;
             flushedErrorMetaTagsUntilIndex < allCapturedErrors.length;
@@ -1354,7 +1353,10 @@ export async function renderToHTMLOrFlight(
             ReactDOMServer,
             element: (
               <html id="__next_error__">
-                <head></head>
+                <head>
+                  {/* @ts-expect-error allow to use async server component */}
+                  <MetadataTree key={requestId} metadata={[]} />
+                </head>
                 <body></body>
               </html>
             ),

test/e2e/app-dir/navigation/navigation.test.ts
@@ -262,9 +262,13 @@ createNextDescribe(
     describe('SEO', () => {
       it('should emit noindex meta tag for not found page when streaming', async () => {
         const noIndexTag = '<meta name="robots" content="noindex"/>'
+        const defaultViewportTag =
+          '<meta name="viewport" content="width=device-width, initial-scale=1"/>'
         const html = await next.render('/not-found/suspense')
         expect(html).toContain(noIndexTag)
-        expect(html.split(noIndexTag).length).toBe(2) // only contain once
+        // only contain once
+        expect(html.split(noIndexTag).length).toBe(2)
+        expect(html.split(defaultViewportTag).length).toBe(2)
       })
 
       it('should emit refresh meta tag for redirect page when streaming', async () => {