9cd59a3c36b0d7264aa2030f00a2f74d40bb2dd0
Howard Tai <howard@howardism.dev>
2023-04-01 00:23:32 +0800

    fix: add relative dist types check before adding tsconfig.include

--- packages/next/src/lib/typescript/writeConfigurationDefaults.ts
+++ packages/next/src/lib/typescript/writeConfigurationDefaults.ts
@@ -184,13 +184,31 @@ export async function writeConfigurationDefaults(
             : `['next-env.d.ts', '**/*.ts', '**/*.tsx']`
         )
     )
-  } else if (isAppDirEnabled && !rawConfig.include.includes(nextAppTypes)) {
-    userTsConfig.include.push(nextAppTypes)
-    suggestedActions.push(
-      chalk.cyan('include') +
-        ' was updated to add ' +
-        chalk.bold(`'${nextAppTypes}'`)
+  } else if (isAppDirEnabled) {
+    const nextAppTypesIndex = rawConfig.include.indexOf(nextAppTypes)
+    const relativeNextAppTypesIndex = rawConfig.include.indexOf(
+      `./${nextAppTypes}`
     )
+
+    if (nextAppTypesIndex >= 0 && relativeNextAppTypesIndex >= 0) {
+      userTsConfig.include.splice(nextAppTypesIndex)
+      suggestedActions.push(
+        chalk.cyan('include') +
+          ' was set to ' +
+          chalk.bold(
+            `['next-env.d.ts', './${nextAppTypes}', '**/*.ts', '**/*.tsx']`
+          )
+      )
+    } else if (nextAppTypesIndex < 0 && relativeNextAppTypesIndex < 0) {
+      userTsConfig.include.push(nextAppTypes)
+      suggestedActions.push(
+        chalk.cyan('include') +
+          ' was set to ' +
+          chalk.bold(
+            `['next-env.d.ts', '${nextAppTypes}', '**/*.ts', '**/*.tsx']`
+          )
+      )
+    }
   }
 
   // Enable the Next.js typescript plugin.
