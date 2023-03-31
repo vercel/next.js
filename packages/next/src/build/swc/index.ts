66c2300710a9b27b60354b62d7303a9fe2041bb0
Tim Neutkens <tim@timneutkens.nl>
2023-03-31 10:42:55 +0200

    Remove testing code

--- packages/next/src/build/swc/index.ts
+++ packages/next/src/build/swc/index.ts
@@ -352,41 +352,6 @@ function loadNative(isCustomTurbopack = false) {
   }
 
   if (bindings) {
-    // ;(async () => {
-    //   const turboTasks = bindings.createTurboTasks()
-    //   const entrypoints = await bindings.getEntrypoints(
-    //     turboTasks,
-    //     '/Users/timneutkens/projects/next.js',
-    //     '/Users/timneutkens/projects/next.js/test/e2e/app-dir/parallel-routes-and-interception',
-    //     ['ts', 'tsx', 'js', 'jsx']
-    //   )
-    //   console.log({ entrypoints })
-    //   console.dir({ test: entrypoints['/parallel-tab-bar'] }, { depth: null })
-    //   // bindings.streamEntrypoints(
-    //   //   turboTasks,
-    //   //   '/Users/timneutkens/projects/next.js',
-    //   //   '/Users/timneutkens/projects/next.js/test/e2e/app-dir/parallel-routes-and-interception',
-    //   //   ['ts', 'tsx', 'js', 'jsx'],
-    //   //   (err, entrypoints) => {
-    //   //     if (err) console.log({ err })
-    //   //     console.log({ entrypoints })
-    //   //     console.dir({ test: entrypoints['/parallel-tab-bar'] }, { depth: null })
-    //   //   }
-    //   // )
-    // })().catch((e) => console.error(e))
-
-    // // Initialize crash reporter, as earliest as possible from any point of import.
-    // // The first-time import to next-swc is not predicatble in the import tree of next.js, which makes
-    // // we can't rely on explicit manual initialization as similar to trace reporter.
-    // if (!swcCrashReporterFlushGuard) {
-    //   // Crash reports in next-swc should be treated in the same way we treat telemetry to opt out.
-    //   /* TODO: temporarily disable initialization while confirming logistics.
-    //   let telemetry = new Telemetry({ distDir: process.cwd() })
-    //   if (telemetry.isEnabled) {
-    //     swcCrashReporterFlushGuard = bindings.initCrashReporter?.()
-    //   }*/
-    // }
-
     nativeBindings = {
       isWasm: false,
       transform(src: string, options: any) {
