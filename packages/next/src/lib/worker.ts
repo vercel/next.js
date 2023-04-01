vercel/next.js

commit 1356088df7c92e9f67732fee7e02e2216ae8627f

Update static build workers (#46705)
Ensures we log when a static build worker exits unexpectedly and fixes
passing down `NODE_OPTIONS` unconditionally as this can cause too much
memory to be leveraged when it shouldn't be.

x-ref: [slack
thread](https://vercel.slack.com/archives/C04S835KUC9/p1677788484200189)

## Bug

- [ ] Related issues linked using `fixes #number`
- [ ] Integration tests added
- [ ] Errors have a helpful link attached, see
[`contributing.md`](https://github.com/vercel/next.js/blob/canary/contributing.md)

packages/next/src/lib/worker.ts
@@ -1,5 +1,6 @@
+import { ChildProcess } from 'child_process'
 import { Worker as JestWorker } from 'next/dist/compiled/jest-worker'
-
+import { getNodeOptionsWithoutInspect } from '../server/lib/utils'
 type FarmOptions = ConstructorParameters<typeof JestWorker>[1]
 
 const RESTARTED = Symbol('restarted')
@@ -24,10 +25,37 @@ export class Worker {
     this._worker = undefined
 
     const createWorker = () => {
-      this._worker = new JestWorker(workerPath, farmOptions) as JestWorker
+      this._worker = new JestWorker(workerPath, {
+        ...farmOptions,
+        forkOptions: {
+          ...farmOptions.forkOptions,
+          env: {
+            ...((farmOptions.forkOptions?.env || {}) as any),
+            ...process.env,
+            // we don't pass down NODE_OPTIONS as it can
+            // extra memory usage
+            NODE_OPTIONS: getNodeOptionsWithoutInspect()
+              .replace(/--max-old-space-size=[\d]{1,}/, '')
+              .trim(),
+          } as any,
+        },
+      }) as JestWorker
       restartPromise = new Promise(
         (resolve) => (resolveRestartPromise = resolve)
       )
+
+      for (const worker of ((this._worker as any)._workerPool?._workers ||
+        []) as {
+        _child: ChildProcess
+      }[]) {
+        worker._child.on('exit', (code, signal) => {
+          if (code || signal) {
+            console.error(
+              `Static worker unexpectedly exited with code: ${code} and signal: ${signal}`
+            )
+          }
+        })
+      }
 
       this._worker.getStdout().pipe(process.stdout)
       this._worker.getStderr().pipe(process.stderr)

commit b132efc5c4ff36b569ff972e09e712e69952788a

fix(export): make static export work with worker_threads (#47784)
Fixes #46993.

The issue is introduced in #46705 by @ijjk 

Next.js uses `jest-worker` for multiprocessing. `jest-worker` supports both `child_process` and `worker_threads` modes. Next.js use `child_process` mode by default, but can also switch to using `worker_threads` mode with an experimental flag `config.experimental.workerThreads`.

In #46705, @ijjk applies a fix that works for the `child_process` mode, which breaks the `worker_threads` mode (`jest-worker`'s `NodeThreadWorker` interface doesn't have the private property `_child`), causing static HTML export to fail with the following error (#46993):

```
> Build error occurred
TypeError: Cannot read properties of undefined (reading 'on')
    at createWorker (/Users/[redacted]/node_modules/next/dist/lib/worker.js:32:31)
    at new Worker (/Users/[redacted]/node_modules/next/dist/lib/worker.js:42:9)
    at /Users/[redacted]/node_modules/next/dist/build/index.js:629:35
    at async Span.traceAsyncFn (/Users/[redacted]/node_modules/next/dist/trace/trace.js:79:20)
    at async Object.build [as default] (/Users/[redacted]/node_modules/next/dist/build/index.js:74:29)
```

The PR fixes that.

packages/next/src/lib/worker.ts
@@ -14,6 +14,7 @@ export class Worker {
       timeout?: number
       onRestart?: (method: string, args: any[], attempts: number) => void
       exposedMethods: ReadonlyArray<string>
+      enableWorkerThreads?: boolean
     }
   ) {
     let { timeout, onRestart, ...farmOptions } = options
@@ -44,18 +45,29 @@ export class Worker {
         (resolve) => (resolveRestartPromise = resolve)
       )
 
-      for (const worker of ((this._worker as any)._workerPool?._workers ||
-        []) as {
-        _child: ChildProcess
-      }[]) {
-        worker._child.on('exit', (code, signal) => {
-          // log unexpected exit if .end() wasn't called
-          if ((code || signal) && this._worker) {
-            console.error(
-              `Static worker unexpectedly exited with code: ${code} and signal: ${signal}`
-            )
-          }
-        })
+      /**
+       * Jest Worker has two worker types, ChildProcessWorker (uses child_process) and NodeThreadWorker (uses worker_threads)
+       * Next.js uses ChildProcessWorker by default, but it can be switched to NodeThreadWorker with an experimental flag
+       *
+       * We only want to handle ChildProcessWorker's orphan process issue, so we access the private property "_child":
+       * https://github.com/facebook/jest/blob/b38d7d345a81d97d1dc3b68b8458b1837fbf19be/packages/jest-worker/src/workers/ChildProcessWorker.ts
+       *
+       * But this property is not available in NodeThreadWorker, so we need to check if we are using ChildProcessWorker
+       */
+      if (!farmOptions.enableWorkerThreads) {
+        for (const worker of ((this._worker as any)._workerPool?._workers ||
+          []) as {
+          _child?: ChildProcess
+        }[]) {
+          worker._child?.on('exit', (code, signal) => {
+            // log unexpected exit if .end() wasn't called
+            if ((code || signal) && this._worker) {
+              console.error(
+                `Static worker unexpectedly exited with code: ${code} and signal: ${signal}`
+              )
+            }
+          })
+        }
       }
 
       this._worker.getStdout().pipe(process.stdout)