26a56ade9c54f19619706c21cbda417793a9558c
wesleyara <wes1gom@gmail.com>
2023-04-03 11:56:01 -0300

    feat: adding tailwind in example

---
+++ examples/with-tailwindcss/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
---
+++ examples/with-tailwindcss/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
