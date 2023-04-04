5da81e0114965c5191b6ca6ed621a1b07ee168ac
Jiachi Liu <inbox@huozhi.im>
2023-04-04 16:27:53 +0200

    rm trace

--- packages/next/src/server/lib/find-vercel-config.ts
+++ packages/next/src/server/lib/find-vercel-config.ts
@@ -1,7 +1,6 @@
 import findUp from 'next/dist/compiled/find-up'
 
 export async function hasVercelConfigFiles(dir: string) {
-  console.trace('hasVercelConfigFiles:dir', dir)
   const vercelJsonPath = await findUp('vercel.json', { cwd: dir })
   const dotVercelPath = await findUp('.vercel', { cwd: dir })
   return !!(vercelJsonPath || dotVercelPath)
