27ba02422f7c7d8f81c76e0c1d9038efd3bd3e02
Jan Kaifer <jan@kaifer.cz>
2023-04-04 15:58:37 +0200

    Remove two files from exampe added by mistake

--- examples/reproduction-template/next.config.js
+++ examples/reproduction-template/next.config.js
@@ -1,5 +1,4 @@
 /** @type {import("next").NextConfig} */
-/** test */
 module.exports = {
   reactStrictMode: true,
 }
--- examples/with-opentelemetry/next-env.d.ts
+++ examples/with-opentelemetry/next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
