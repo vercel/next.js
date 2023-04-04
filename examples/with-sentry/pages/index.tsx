18a70acba36e42d6bce9e4c6fdd837dbc01df5b8
Lazar Nikolov <hello@nikolovlazar.com>
2023-04-04 10:06:17 -0400

    update(examples): updating with-sentry example content and styling

--- examples/with-sentry/pages/index.tsx
+++ examples/with-sentry/pages/index.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>pages/index.js</code>
+          New users instrument with:&nbsp;
+          <code className={styles.code}>npx @sentry/wizard -s -i nextjs</code>
         </p>
         <div>
           <a
@@ -46,38 +46,40 @@ export default function Home() {
           <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
         </div>
       </div>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/sentry.svg"
-          alt="Sentry Logo"
-          width={360}
-          height={74}
-          priority
-        />
-      </div>
-      <p className={inter.className}>
-        See how Sentry records unhandled exceptions in your code.
-      </p>
-      <br />
-      <div className={styles.description}>
+      <Image
+        src="/sentry.svg"
+        alt="Sentry Logo"
+        width={360}
+        height={74}
+        priority
+      />
+      <div
+        className={styles.justCenter}
+        style={{ display: 'flex', flexDirection: 'column', rowGap: '1rem' }}
+      >
         <p className={inter.className}>
-          <strong>Important:</strong> exceptions in development mode take a
-          different path than in production. These tests should be run on a
-          production build (i.e. <code className={styles.code}>next build</code>
-          ).{' '}
-          <Link
-            className={styles.link}
-            target="_blank"
-            href="https://nextjs.org/docs/advanced-features/custom-error-page#customizing-the-error-page"
-          >
-            Read more
-          </Link>
+          See how Sentry records unhandled exceptions in your code.
         </p>
+        <div className={styles.description}>
+          <p className={inter.className}>
+            <strong>Important:</strong> exceptions in development mode take a
+            different path than in production. These tests should be run on a
+            production build (i.e.{' '}
+            <code className={styles.code}>next build</code>
+            ).{' '}
+            <Link
+              className={styles.link}
+              target="_blank"
+              href="https://nextjs.org/docs/advanced-features/custom-error-page#customizing-the-error-page"
+            >
+              Read more
+            </Link>
+          </p>
+        </div>
       </div>
       <br />
       <div className={styles.grid}>
-        <a className={styles.card} target="_blank" rel="noopener noreferrer">
+        <div className={styles.card}>
           <h2 className={inter.className}>
             API routes <span>-&gt;</span>
           </h2>
@@ -88,147 +90,161 @@ export default function Home() {
           <p className={inter.className}>
             Top-of-module promise that rejects, but its result is not awaited.
             {'  '}
-            <a href="/api/test1" target="_blank">
-              API Test 1
-            </a>
+            <code className={styles.code}>
+              <a className={styles.link} href="/api/test1" target="_blank">
+                API Test 1
+              </a>
+            </code>
           </p>
           <p className={inter.className}>
             API has a top-of-module exception.{' '}
-            <a href="/api/test2" target="_blank">
-              API Test 2
-            </a>
+            <code className={styles.code}>
+              <a className={styles.link} href="/api/test2" target="_blank">
+                API Test 2
+              </a>
+            </code>
           </p>
           <p className={inter.className}>
             API with an exception in its request handler.{' '}
-            <a href="/api/test3" target="_blank">
-              API Test 3
-            </a>
+            <code className={styles.code}>
+              <a className={styles.link} href="/api/test3" target="_blank">
+                API Test 3
+              </a>
+            </code>
           </p>
           <p className={inter.className}>
             API uses a try/catch to handle an exception and records it.{' '}
-            <a href="/api/test4" target="_blank">
-              API Test 4
-            </a>
+            <code className={styles.code}>
+              <a className={styles.link} href="/api/test4" target="_blank">
+                API Test 4
+              </a>
+            </code>
           </p>
-        </a>
-        <a className={styles.card} target="_blank" rel="noopener noreferrer">
+        </div>
+        <div className={styles.card}>
           <h2 className={inter.className}>
             SSR <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Note that there are currently two known bugs with respect to SSR
-            transactions: they don't get recorded on Vercel, and ones that are
-            recorded and have an error are grouped in the Sentry UI by the error
-            page name rather than the requested page name.
+            There are currently two known bugs with respect to SSR transactions:
+            they don't get recorded on Vercel, and ones that are recorded and
+            have an error are grouped in the Sentry UI by the error page name
+            rather than the requested page name.
           </p>
 
           <p className={inter.className}>
             <code className={styles.code}>getServerSideProps </code>
             &nbsp;throws an Error. This should cause _error.js to render and
-            record Error('SSR Test 1') in Sentry.{' '}
-            <a href="/ssr/test1" target="_blank">
-              or open a new one
-            </a>{' '}
-            or <Link href="/ssr/test1">Navigate in this tab</Link>
+            record and Error in Sentry.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/ssr/test1">
+                SSR Test 1
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
-            getServerSideProps returns a Promise that rejects. This should cause
-            _error.js to render and record Error('SSR Test 2') in Sentry.{' '}
-            <a href="/ssr/test2" target="_blank">
-              or open a new one
-            </a>
+            <code className={styles.code}>getServerSideProps </code>
+            returns a Promise that rejects. This should cause _error.js to
+            render and record an Error in Sentry.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/ssr/test2">
+                SSR Test 2
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
-            getServerSideProps calls a Promise that rejects, but does not handle
-            the rejection or await its result (returning synchronously). Sentry
-            should record Error('SSR Test 3'), but <strong>will not</strong>{' '}
-            when deployed to Vercel because the serverless function will already
-            have exited.{' '}
-            <a href="/ssr/test3" target="_blank">
-              or open a new one
-            </a>
+            <code className={styles.code}>getServerSideProps </code>
+            calls a Promise that rejects, but does not handle the rejection or
+            await its result (returning synchronously). Sentry records an Error
+            but <strong>will not</strong> when deployed to Vercel because the
+            serverless function will already have exited.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/ssr/test3">
+                SSR Test 3
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
-            getServerSideProps manually captures an exception from a try/catch.
-            This should record Error('SSR Test 4') in Sentry.{' '}
-            <a href="/ssr/test4" target="_blank">
-              or open a new one
-            </a>
+            <code className={styles.code}>getServerSideProps </code>
+            manually captures an exception from a try/catch. This should record
+            Error in Sentry.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/ssr/test4">
+                SSR Test 4
+              </Link>
+            </code>
           </p>
-        </a>
+        </div>
 
-        <a className={styles.card} target="_blank" rel="noopener noreferrer">
+        <div className={styles.card}>
           <h2 className={inter.className}>
             Client exceptions <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
             There is a top-of-module Promise that rejects, but its result is not
-            awaited. Sentry should record Error('Client Test 1').{' '}
-            <Link href="/client/test1">Navigate in this tab</Link> or{' '}
-            <a href="/client/test1" target="_blank">
-              or open a new one
-            </a>
+            awaited. Sentry records an Error.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/client/test1">
+                Client Test 1
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
             There is a top-of-module exception. _error.js should render and
             record ReferenceError('process is not defined') in Sentry.{' '}
-            <Link href="/client/test2">Navigate in this tab</Link> or{' '}
-            <a href="/client/test2" target="_blank">
-              or open a new one
-            </a>
+            <code className={styles.code}>
+              <Link className={styles.link} href="/client/test2">
+                Client Test 2
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
             There is an exception during React lifecycle that is caught by
             Next.js's React Error Boundary. In this case, when the component
-            mounts. This should cause _error.js to render and record
-            Error('Client Test 3') in Sentry.{' '}
-            <Link href="/client/test3">Navigate in this tab</Link> or{' '}
-            <a href="/client/test3" target="_blank">
-              or open a new one
-            </a>
+            mounts. This causes _error.js to render and records Error in Sentry.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/client/test3">
+                Client Test 3
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
             There is an unhandled Promise rejection during React lifecycle. In
-            this case, when the component mounts. Sentry should record
-            Error('Client Test 4').{' '}
-            <Link href="/client/test4"> Navigate in this tab</Link> or{' '}
-            <a href="/client/test4" target="_blank">
-              or open a new one
-            </a>
+            this case, when the component mounts. Sentry records an Error.{' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/client/test4">
+                {' '}
+                Client Test 4
+              </Link>
+            </code>
           </p>
           <p className={inter.className}>
-            An Error is thrown from an event handler. Sentry should record
-            Error('Client Test 5'). (This page also demonstrates how to manually
-            instrument your code for performance monitoring.){' '}
-            <Link href="/client/test5">Navigate in this tab</Link> or{' '}
-            <a href="/client/test5" target="_blank">
-              or open a new one
-            </a>
+            An Error is thrown from an event handler. Sentry records an Error.
+            (This page also demonstrates how to manually instrument your code
+            for performance monitoring.){' '}
+            <code className={styles.code}>
+              <Link className={styles.link} href="/client/test5">
+                Client Test 5
+              </Link>
+            </code>
           </p>
-        </a>
-        <a className={styles.card} target="_blank" rel="noopener noreferrer">
+        </div>
+        <div className={styles.card}>
           <h2 className={inter.className}>
             NextJS 13 Features <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            NextjS 13 continues to bring many new features to developers,
+            NextJS 13 continues to bring many new features to developers,
             especially those depoying on Vercel. We are trying to keep up, we
             promise!
           </p>
-          <p className={inter.className}>
-            Add Edge Function example){' '}
-            <Link href="/client/test5">Navigate in this tab</Link> or{' '}
-            <a href="/client/test5" target="_blank">
-              or open a new one
-            </a>
-          </p>
-        </a>
-        <a className={styles.card} target="_blank" rel="noopener noreferrer">
+        </div>
+        <div className={styles.card}>
           <h2 className={inter.className}>
             Performance <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Why should I care about Performance?
+            <strong>Why should I care about Performance?</strong>
           </p>
           <p className={inter.className}>
             Front-end transactions are recorded for each pageload or navigation.
@@ -238,23 +254,32 @@ export default function Home() {
           </p>
           <p className={inter.className}>
             Sentry creates links between errors and transactions, and can be
-            seen in the [trace
-            navigator](https://docs.sentry.io/product/sentry-basics/tracing/trace-view/).
+            seen in the{' '}
+            <strong>
+              <Link
+                className={styles.link}
+                href="https://docs.sentry.io/product/sentry-basics/tracing/trace-view/"
+              >
+                trace navigator docs
+              </Link>
+            </strong>
+            .
           </p>
           <p className={inter.className}>
             Manual performance instrumentation is demonstrated in the final
             example below (throwing an error from an event handler).
           </p>
           <p className={inter.className}>
-            Add Edge Function example){' '}
-            <Link href="/client/test5">Navigate in this tab</Link> or{' '}
-            <a href="/client/test5" target="_blank">
-              or open a new one
-            </a>
+            Add Edge Function example
+            <br />
+            <code className={styles.code}>
+              <Link className={styles.link} href="/client/test5">
+                Trigger Edge Function
+              </Link>
+            </code>
           </p>
-        </a>
+        </div>
       </div>
-      <div style={{ maxWidth: 700, margin: '0 auto' }}></div>
     </main>
   )
 }
