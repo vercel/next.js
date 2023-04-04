35ac5728198811869e7071b5d71da7eb45576b77
Shu Ding <g@shud.in>
2023-04-04 23:56:00 +0200

    implement route type for router apis

--- packages/next/src/build/webpack/plugins/next-types-plugin.ts
+++ packages/next/src/build/webpack/plugins/next-types-plugin.ts
@@ -432,7 +432,44 @@ declare module 'next/link' {
   }
 
   export default function Link<RouteType>(props: LinkProps<RouteType>): JSX.Element
-}`
+}
+
+declare module 'next/navigation' {
+  export * from 'next/dist/client/components/navigation'
+
+  import type { NavigateOptions } from 'next/dist/shared/lib/app-router-context'
+  interface AppRouterInstance {
+    /**
+     * Navigate to the previous history entry.
+     */
+    back(): void
+    /**
+     * Navigate to the next history entry.
+     */
+    forward(): void
+    /**
+     * Refresh the current page.
+     */
+    refresh(): void
+    /**
+     * Navigate to the provided href.
+     * Pushes a new history entry.
+     */
+    push<RouteType>(href: __next_route_internal_types__.RouteImpl<RouteType>, options?: NavigateOptions): void
+    /**
+     * Navigate to the provided href.
+     * Replaces the current history entry.
+     */
+    replace<RouteType>(href: __next_route_internal_types__.RouteImpl<RouteType>, options?: NavigateOptions): void
+    /**
+     * Prefetch the provided href.
+     */
+    prefetch<RouteType>(href: __next_route_internal_types__.RouteImpl<RouteType>): void
+  }
+
+  export declare function useRouter(): AppRouterInstance;
+}
+`
 }
 
 const appTypesBasePath = path.join('types', 'app')
