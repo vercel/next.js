e3e22f5beded84a342d7a8cc7dd1a98ebdf1b041
JJ Kasper <jj@jjsweb.site>
2023-04-05 14:14:40 -0700

    Update search params/route params handling on deploy (#47930)
          This ensures we prefix the dynamic route params in the query so that
    they can be kept separate from actual query params from the initial
    request.
    
    Fixes: https://github.com/vercel/next.js/issues/43139

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -190,7 +190,7 @@ async function generateClientSsgManifest(
 }
 
 function pageToRoute(page: string) {
-  const routeRegex = getNamedRouteRegex(page)
+  const routeRegex = getNamedRouteRegex(page, true)
   return {
     page,
     regex: normalizeRouteRegex(routeRegex.re.source),
@@ -1975,7 +1975,8 @@ export default async function build(
 
           if (isDynamicRoute(page)) {
             const routeRegex = getNamedRouteRegex(
-              dataRoute.replace(/\.json$/, '')
+              dataRoute.replace(/\.json$/, ''),
+              true
             )
 
             dataRouteRegex = normalizeRouteRegex(
@@ -2392,7 +2393,7 @@ export default async function build(
               // dynamicParams for non-static paths?
               finalDynamicRoutes[page] = {
                 routeRegex: normalizeRouteRegex(
-                  getNamedRouteRegex(page).re.source
+                  getNamedRouteRegex(page, false).re.source
                 ),
                 dataRoute,
                 // if dynamicParams are enabled treat as fallback:
@@ -2404,7 +2405,8 @@ export default async function build(
                   ? null
                   : normalizeRouteRegex(
                       getNamedRouteRegex(
-                        dataRoute.replace(/\.rsc$/, '')
+                        dataRoute.replace(/\.rsc$/, ''),
+                        false
                       ).re.source.replace(/\(\?:\\\/\)\?\$$/, '\\.rsc$')
                     ),
               }
@@ -2769,7 +2771,7 @@ export default async function build(
 
           finalDynamicRoutes[tbdRoute] = {
             routeRegex: normalizeRouteRegex(
-              getNamedRouteRegex(tbdRoute).re.source
+              getNamedRouteRegex(tbdRoute, false).re.source
             ),
             dataRoute,
             fallback: ssgBlockingFallbackPages.has(tbdRoute)
@@ -2779,7 +2781,8 @@ export default async function build(
               : false,
             dataRouteRegex: normalizeRouteRegex(
               getNamedRouteRegex(
-                dataRoute.replace(/\.json$/, '')
+                dataRoute.replace(/\.json$/, ''),
+                false
               ).re.source.replace(/\(\?:\\\/\)\?\$$/, '\\.json$')
             ),
           }
