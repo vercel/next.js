37bd62fde10b464b372b19ed394f5d3670b3787d
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-04-05 15:51:29 -0700

    Revert "feat(ecmascript): support configurable emotion options" (vercel/turbo#4481)
    Reverts vercel/turbo#4458

--- crates/turbopack-tests/tests/snapshot.rs
+++ crates/turbopack-tests/tests/snapshot.rs
@@ -20,10 +20,7 @@ use turbo_tasks_memory::MemoryBackend;
 use turbopack::{
     condition::ContextCondition,
     ecmascript::EcmascriptModuleAssetVc,
-    module_options::{
-        EmotionTransformOptions, EmotionTransformOptionsVc, JsxTransformOptions,
-        JsxTransformOptionsVc, ModuleOptionsContext,
-    },
+    module_options::{JsxTransformOptions, JsxTransformOptionsVc, ModuleOptionsContext},
     resolve_options_context::ResolveOptionsContext,
     transition::TransitionsByNameVc,
     ModuleAssetContextVc,
@@ -196,11 +193,7 @@ async fn run_test(resource: &str) -> Result<FileSystemPathVc> {
             enable_jsx: Some(JsxTransformOptionsVc::cell(JsxTransformOptions {
                 ..Default::default()
             })),
-            enable_emotion: Some(EmotionTransformOptionsVc::cell(EmotionTransformOptions {
-                enabled: true,
-                sourcemap: Some(false),
-                ..Default::default()
-            })),
+            enable_emotion: true,
             enable_styled_components: true,
             preset_env_versions: Some(env),
             rules: vec![(
