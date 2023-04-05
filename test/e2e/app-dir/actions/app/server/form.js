5190a4cf63088a0fe74b0d595e03bdcf8c47b20c
Shu Ding <g@shud.in>
2023-04-05 22:51:25 +0200

    warn to use experimental react to enable actions

--- test/e2e/app-dir/actions/app/server/form.js
+++ test/e2e/app-dir/actions/app/server/form.js
@@ -14,16 +14,14 @@ export default function Form() {
   return (
     <>
       <hr />
-      <form action="" method="POST">
+      <form method="POST" action={action}>
         <input type="text" name="name" id="name" required />
-        <input type="text" name="$$id" value={action.$$id} hidden readOnly />
         <button type="submit" id="submit">
           Submit
         </button>
       </form>
       <hr />
-      <form action="" method="POST">
-        <input type="text" name="$$id" value={nowhere.$$id} hidden readOnly />
+      <form method="POST" action={nowhere}>
         <button type="submit" id="nowhere">
           Go nowhere
         </button>
