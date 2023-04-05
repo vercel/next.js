a94de4c7d0fffde3b08e0df21186a09f3f478558
Tim Neutkens <tim@timneutkens.nl>
2023-04-05 23:51:52 +0200

    Check for null before reading value in useParams (#47875)
    Ensures the `null` value is checked instead of destructuring the value
    as it caused an error.
    Also added a test to check for `useParams` in `pages`.
    
    <!-- Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change(s) that you're making:
    
    ## For Contributors
    
    ### Improving Documentation or adding/fixing Examples
    
    - The "examples guidelines" are followed from our contributing doc
    https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
    - Make sure the linting passes by running `pnpm build && pnpm lint`. See
    https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md
    
    ### Fixing a bug
    
    - Related issues linked using `fixes #number`
    - Tests added. See:
    https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    ### Adding a feature
    
    - Implements an existing feature request or RFC. Make sure the feature
    request has been accepted for implementation before opening a PR. (A
    discussion must be opened, see
    https://github.com/vercel/next.js/discussions/new?category=ideas)
    - Related issues/discussions are linked using `fixes #number`
    - e2e tests added
    (https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Documentation added
    - Telemetry added. In case of a feature if it's used or not.
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    
    
    ## For Maintainers
    
    - Minimal description (aim for explaining to someone not on the team to
    understand the PR)
    - When linking to a Slack thread, you might want to share details of the
    conclusion
    - Link both the Linear (Fixes NEXT-xxx) and the GitHub issues
    - Add review comments if necessary to explain to the reviewer the logic
    behind a change
    
    ### What?
    
    ### Why?
    
    ### How?
    
    Closes NEXT-
    Fixes #
    
    -->
    
    ---------
    
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- test/e2e/app-dir/use-params/pages/use-params-pages.js
+++ test/e2e/app-dir/use-params/pages/use-params-pages.js
@@ -2,13 +2,9 @@
 import { useParams } from 'next/navigation'
 export default function Page() {
   const params = useParams()
-  if (params === null) {
-    return null
-  }
   return (
     <div>
-      <div id="param-id">{params.id}</div>
-      <div id="param-id2">{params.id2}</div>
+      <div id="params">{JSON.stringify(params)}</div>
     </div>
   )
 }
