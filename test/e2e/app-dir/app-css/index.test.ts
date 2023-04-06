vercel/next.js

commit 0616f1b4321d4df36d92c37f2832531f2dca522c

Add not found boundary and move head cache to app router (#47688)
### What?

- Add not found boundary to app router
- Move `head` cache back to app router

### Why?

We want the head to be rendered separately from body, previously to be
able to use `redirect()` and `notFound()` in `generateMetadata` we move
the head cache into layout-router to be wrapped by not found and
redirect boundaries. Since redirect boudary is already moved to
app-router, so we only need to add not found boundary and move head
cache to app router.

Notice: there's a limitation that we can't find the corresponding not
found of page if you throw notFound in generateMetadata, the root layout
+ root/default not found will be used to generate the 404 page

### How?

Closes NEXT-864
Fixes #46738

fix NEXT-888 ([link](https://linear.app/vercel/issue/NEXT-888))

---------

packages/next/src/client/components/app-router.tsx
@@ -42,6 +42,8 @@ import { isBot } from '../../shared/lib/router/utils/is-bot'
 import { addBasePath } from '../add-base-path'
 import { AppRouterAnnouncer } from './app-router-announcer'
 import { RedirectBoundary } from './redirect-boundary'
+import { NotFoundBoundary } from './not-found-boundary'
+import { findHeadInCache } from './router-reducer/reducers/find-head-in-cache'
 
 const isServer = typeof window === 'undefined'
 
@@ -70,6 +72,10 @@ type AppRouterProps = Omit<
 > & {
   initialHead: ReactNode
   assetPrefix: string
+  // Top level boundaries props
+  notFound: React.ReactNode | undefined
+  notFoundStyles: React.ReactNode | undefined
+  asNotFound?: boolean
 }
 
 function isExternalURL(url: URL) {
@@ -85,6 +91,9 @@ function Router({
   initialCanonicalUrl,
   children,
   assetPrefix,
+  notFound,
+  notFoundStyles,
+  asNotFound,
 }: AppRouterProps) {
   const initialState = useMemo(
     () =>
@@ -311,13 +320,22 @@ function Router({
     }
   }, [onPopState])
 
+  const head = useMemo(() => {
+    return findHeadInCache(cache, tree[1])
+  }, [cache, tree])
+
   const content = (
-    <>
+    <NotFoundBoundary
+      notFound={notFound}
+      notFoundStyles={notFoundStyles}
+      asNotFound={asNotFound}
+    >
       <RedirectBoundary>
+        {head}
         {cache.subTreeData}
         <AppRouterAnnouncer tree={tree} />
       </RedirectBoundary>
-    </>
+    </NotFoundBoundary>
   )
 
   return (
@@ -338,7 +356,6 @@ function Router({
                 // Root node always has `url`
                 // Provided in AppTreeContext to ensure it can be overwritten in layout-router
                 url: canonicalUrl,
-                headRenderedAboveThisLevel: false,
               }}
             >
               {HotReloader ? (

packages/next/src/server/app-render/app-render.tsx
@@ -1124,16 +1124,34 @@ export async function renderToHTMLOrFlight(
     }>(
       async (props) => {
         // Create full component tree from root to leaf.
+        const injectedCSS = new Set<string>()
         const { Component: ComponentTree } = await createComponentTree({
           createSegmentPath: (child) => child,
-          loaderTree: loaderTree,
+          loaderTree,
           parentParams: {},
           firstItem: true,
-          injectedCSS: new Set(),
+          injectedCSS,
           injectedFontPreloadTags: new Set(),
           rootLayoutIncluded: false,
           asNotFound: props.asNotFound,
         })
+
+        const { 'not-found': notFound, layout } = loaderTree[2]
+        const isLayout = typeof layout !== 'undefined'
+        const rootLayoutModule = layout?.[0]
+        const RootLayout = rootLayoutModule
+          ? interopDefault(await rootLayoutModule())
+          : null
+        const rootLayoutAtThisLevel = isLayout
+        const [NotFound, notFoundStyles] = notFound
+          ? await createComponentAndStyles({
+              filePath: notFound[1],
+              getComponent: notFound[0],
+              injectedCSS,
+            })
+          : rootLayoutAtThisLevel
+          ? [DefaultNotFound]
+          : []
 
         const initialTree = createFlightRouterStateFromLoaderTree(
           loaderTree,
@@ -1155,6 +1173,15 @@ export async function renderToHTMLOrFlight(
                 </>
               }
               globalErrorComponent={GlobalError}
+              notFound={
+                NotFound && RootLayout ? (
+                  <RootLayout params={{}}>
+                    <NotFound />
+                  </RootLayout>
+                ) : undefined
+              }
+              notFoundStyles={notFoundStyles}
+              asNotFound={props.asNotFound}
             >
               <ComponentTree />
             </AppRouter>

test/e2e/app-dir/metadata-suspense/index.test.ts
@@ -1,0 +1,18 @@
+import { createNextDescribe } from 'e2e-utils'
+
+createNextDescribe(
+  'app dir - metadata dynamic routes',
+  {
+    files: __dirname,
+    skipDeployment: true,
+  },
+  ({ next }) => {
+    it('should render metadata in head even root layout is wrapped with Suspense', async () => {
+      const $ = await next.render$('/')
+      expect($('head title').text()).toBe('My title')
+      expect($('head meta[name="theme-color"]').attr('content')).toBe('#eee')
+
+      expect($('body meta').length).toBe(0)
+    })
+  }
+)

test/e2e/app-dir/not-found/app/not-found/not-found.js
@@ -1,0 +1,3 @@
+export default function notFound() {
+  return <h1>custom not found</h1>
+}

packages/next/src/client/components/layout-router.tsx
@@ -9,7 +9,7 @@ import type {
 import type { ErrorComponent } from './error-boundary'
 import { FocusAndScrollRef } from './router-reducer/router-reducer-types'
 
-import React, { useContext, useMemo, use } from 'react'
+import React, { useContext, use } from 'react'
 import ReactDOM from 'react-dom'
 import {
   CacheStates,
@@ -22,7 +22,6 @@ import { createInfinitePromise } from './infinite-promise'
 import { ErrorBoundary } from './error-boundary'
 import { matchSegment } from './match-segments'
 import { handleSmoothScroll } from '../../shared/lib/router/utils/handle-smooth-scroll'
-import { findHeadInCache } from './router-reducer/reducers/find-head-in-cache'
 import { RedirectBoundary } from './redirect-boundary'
 import { NotFoundBoundary } from './not-found-boundary'
 
@@ -232,7 +231,6 @@ function InnerLayoutRouter({
   // TODO-APP: implement `<Offscreen>` when available.
   // isActive,
   path,
-  headRenderedAboveThisLevel,
 }: {
   parallelRouterKey: string
   url: string
@@ -242,7 +240,6 @@ function InnerLayoutRouter({
   tree: FlightRouterState
   isActive: boolean
   path: string
-  headRenderedAboveThisLevel: boolean
 }) {
   const context = useContext(GlobalLayoutRouterContext)
   if (!context) {
@@ -250,13 +247,6 @@ function InnerLayoutRouter({
   }
 
   const { changeByServerResponse, tree: fullTree, focusAndScrollRef } = context
-
-  const head = useMemo(() => {
-    if (headRenderedAboveThisLevel) {
-      return null
-    }
-    return findHeadInCache(childNodes, tree[1])
-  }, [childNodes, tree, headRenderedAboveThisLevel])
 
   // Read segment path from the parallel router cache node.
   let childNode = childNodes.get(path)
@@ -370,10 +360,8 @@ function InnerLayoutRouter({
         childNodes: childNode.parallelRoutes,
         // TODO-APP: overriding of url for parallel routes
         url: url,
-        headRenderedAboveThisLevel: true,
       }}
     >
-      {head}
       {childNode.subTreeData}
     </LayoutRouterContext.Provider>
   )
@@ -457,7 +445,7 @@ export default function OuterLayoutRouter({
     throw new Error('invariant expected layout router to be mounted')
   }
 
-  const { childNodes, tree, url, headRenderedAboveThisLevel } = context
+  const { childNodes, tree, url } = context
 
   // Get the current parallelRouter cache node
   let childNodesForParallelRouter = childNodes.get(parallelRouterKey)
@@ -528,7 +516,6 @@ export default function OuterLayoutRouter({
                         segmentPath={segmentPath}
                         path={preservedSegment}
                         isActive={currentChildSegment === preservedSegment}
-                        headRenderedAboveThisLevel={headRenderedAboveThisLevel}
                       />
                     </RedirectBoundary>
                   </NotFoundBoundary>

packages/next/src/client/components/router-reducer/reducers/find-head-in-cache.test.tsx
@@ -95,10 +95,7 @@ describe('findHeadInCache', () => {
       ]),
     }
 
-    const result = findHeadInCache(
-      cache.parallelRoutes.get('children')!,
-      routerTree[1]
-    )
+    const result = findHeadInCache(cache, routerTree[1])
 
     expect(result).toMatchObject(
       <>

packages/next/src/client/components/router-reducer/reducers/find-head-in-cache.ts
@@ -1,16 +1,20 @@
 import type { FlightRouterState } from '../../../../server/app-render/types'
-import type { ChildSegmentMap } from '../../../../shared/lib/app-router-context'
+import type { CacheNode } from '../../../../shared/lib/app-router-context'
 
 export function findHeadInCache(
-  childSegmentMap: ChildSegmentMap,
+  cache: CacheNode,
   parallelRoutes: FlightRouterState[1]
 ): React.ReactNode {
-  if (!childSegmentMap) {
-    return undefined
+  const isLastItem = Object.keys(parallelRoutes).length === 0
+  if (isLastItem) {
+    return cache.head
   }
   for (const key in parallelRoutes) {
     const [segment, childParallelRoutes] = parallelRoutes[key]
-    const isLastItem = Object.keys(childParallelRoutes).length === 0
+    const childSegmentMap = cache.parallelRoutes.get(key)
+    if (!childSegmentMap) {
+      continue
+    }
 
     const cacheKey = Array.isArray(segment) ? segment[1] : segment
 
@@ -19,14 +23,9 @@ export function findHeadInCache(
       continue
     }
 
-    if (isLastItem && cacheNode.head) return cacheNode.head
-
-    const segmentMap = cacheNode.parallelRoutes.get(key)
-    if (segmentMap) {
-      const item = findHeadInCache(segmentMap, childParallelRoutes)
-      if (item) {
-        return item
-      }
+    const item = findHeadInCache(cacheNode, childParallelRoutes)
+    if (item) {
+      return item
     }
   }
 

packages/next/src/shared/lib/app-router-context.ts
@@ -103,7 +103,6 @@ export const LayoutRouterContext = React.createContext<{
   childNodes: CacheNode['parallelRoutes']
   tree: FlightRouterState
   url: string
-  headRenderedAboveThisLevel: boolean
 }>(null as any)
 export const GlobalLayoutRouterContext = React.createContext<{
   tree: FlightRouterState

commit e4e5c1674aa2ce203042b512d31cabbc5e69b576

Fix css applying for root not found (#47992)
### What

This issue is introduced in #47688, we need to do the same work for
rendering single component which collecting the assets and then render
with root layout + root not found

Fix #47970
Related #47862 (partially fix the css issue but not link issue)

### How

This PR encapsulates the preload and stylesheets assets collection and
rendering process, and move them into a helper, and share between the
component rendering and the root not found rendering

packages/next/src/build/index.ts
@@ -2541,11 +2541,14 @@ export default async function build(
                 const updatedRelativeDest = path
                   .join('pages', '404.html')
                   .replace(/\\/g, '/')
-                await promises.copyFile(
-                  orig,
-                  path.join(distDir, 'server', updatedRelativeDest)
-                )
-                pagesManifest['/404'] = updatedRelativeDest
+
+                if (await fileExists(orig)) {
+                  await promises.copyFile(
+                    orig,
+                    path.join(distDir, 'server', updatedRelativeDest)
+                  )
+                  pagesManifest['/404'] = updatedRelativeDest
+                }
               })
           }
 

packages/next/src/client/components/app-router.tsx
@@ -75,7 +75,7 @@ type AppRouterProps = Omit<
   assetPrefix: string
   // Top level boundaries props
   notFound: React.ReactNode | undefined
-  notFoundStyles: React.ReactNode | undefined
+  notFoundStyles?: React.ReactNode | undefined
   asNotFound?: boolean
 }
 

packages/next/src/server/app-render/app-render.tsx
@@ -459,6 +459,89 @@ export async function renderToHTMLOrFlight(
       return [Comp, styles]
     }
 
+    const createStaticAssets = async ({
+      layoutOrPagePath,
+      injectedCSS: injectedCSSWithCurrentLayout,
+      injectedFontPreloadTags: injectedFontPreloadTagsWithCurrentLayout,
+    }: {
+      layoutOrPagePath: string | undefined
+      injectedCSS: Set<string>
+      injectedFontPreloadTags: Set<string>
+    }) => {
+      const stylesheets: string[] = layoutOrPagePath
+        ? getCssInlinedLinkTags(
+            clientReferenceManifest,
+            serverCSSManifest!,
+            layoutOrPagePath,
+            serverCSSForEntries,
+            injectedCSSWithCurrentLayout,
+            true
+          )
+        : []
+
+      const preloadedFontFiles = layoutOrPagePath
+        ? getPreloadedFontFilesInlineLinkTags(
+            serverCSSManifest!,
+            nextFontManifest,
+            serverCSSForEntries,
+            layoutOrPagePath,
+            injectedFontPreloadTagsWithCurrentLayout
+          )
+        : []
+
+      return (
+        <>
+          {preloadedFontFiles?.length === 0 ? (
+            <link
+              data-next-font={
+                nextFontManifest?.appUsingSizeAdjust ? 'size-adjust' : ''
+              }
+              rel="preconnect"
+              href="/"
+              crossOrigin="anonymous"
+            />
+          ) : null}
+          {preloadedFontFiles
+            ? preloadedFontFiles.map((fontFile) => {
+                const ext = /\.(woff|woff2|eot|ttf|otf)$/.exec(fontFile)![1]
+                return (
+                  <link
+                    key={fontFile}
+                    rel="preload"
+                    href={`${assetPrefix}/_next/${fontFile}`}
+                    as="font"
+                    type={`font/${ext}`}
+                    crossOrigin="anonymous"
+                    data-next-font={
+                      fontFile.includes('-s') ? 'size-adjust' : ''
+                    }
+                  />
+                )
+              })
+            : null}
+          {stylesheets
+            ? stylesheets.map((href, index) => (
+                <link
+                  rel="stylesheet"
+                  // In dev, Safari will wrongly cache the resource if you preload it:
+                  // - https://github.com/vercel/next.js/issues/5860
+                  // - https://bugs.webkit.org/show_bug.cgi?id=187726
+                  // We used to add a `?ts=` query for resources in `pages` to bypass it,
+                  // but in this case it is fine as we don't need to preload the styles.
+                  href={`${assetPrefix}/_next/${href}`}
+                  // `Precedence` is an opt-in signal for React to handle
+                  // resource loading and deduplication, etc:
+                  // https://github.com/facebook/react/pull/25060
+                  // @ts-ignore
+                  precedence="next.js"
+                  key={index}
+                />
+              ))
+            : null}
+        </>
+      )
+    }
+
     /**
      * Use the provided loader tree to create the React Component tree.
      */
@@ -498,29 +581,15 @@ export async function renderToHTMLOrFlight(
       const layoutOrPagePath = layout?.[1] || page?.[1]
 
       const injectedCSSWithCurrentLayout = new Set(injectedCSS)
-      const stylesheets: string[] = layoutOrPagePath
-        ? getCssInlinedLinkTags(
-            clientReferenceManifest,
-            serverCSSManifest!,
-            layoutOrPagePath,
-            serverCSSForEntries,
-            injectedCSSWithCurrentLayout,
-            true
-          )
-        : []
-
       const injectedFontPreloadTagsWithCurrentLayout = new Set(
         injectedFontPreloadTags
       )
-      const preloadedFontFiles = layoutOrPagePath
-        ? getPreloadedFontFilesInlineLinkTags(
-            serverCSSManifest!,
-            nextFontManifest,
-            serverCSSForEntries,
-            layoutOrPagePath,
-            injectedFontPreloadTagsWithCurrentLayout
-          )
-        : []
+
+      const assets = createStaticAssets({
+        layoutOrPagePath,
+        injectedCSS: injectedCSSWithCurrentLayout,
+        injectedFontPreloadTags: injectedFontPreloadTagsWithCurrentLayout,
+      })
 
       const [Template, templateStyles] = template
         ? await createComponentAndStyles({
@@ -855,53 +924,7 @@ export async function renderToHTMLOrFlight(
               ) : (
                 <Component {...props} />
               )}
-              {preloadedFontFiles?.length === 0 ? (
-                <link
-                  data-next-font={
-                    nextFontManifest?.appUsingSizeAdjust ? 'size-adjust' : ''
-                  }
-                  rel="preconnect"
-                  href="/"
-                  crossOrigin="anonymous"
-                />
-              ) : null}
-              {preloadedFontFiles
-                ? preloadedFontFiles.map((fontFile) => {
-                    const ext = /\.(woff|woff2|eot|ttf|otf)$/.exec(fontFile)![1]
-                    return (
-                      <link
-                        key={fontFile}
-                        rel="preload"
-                        href={`${assetPrefix}/_next/${fontFile}`}
-                        as="font"
-                        type={`font/${ext}`}
-                        crossOrigin="anonymous"
-                        data-next-font={
-                          fontFile.includes('-s') ? 'size-adjust' : ''
-                        }
-                      />
-                    )
-                  })
-                : null}
-              {stylesheets
-                ? stylesheets.map((href, index) => (
-                    <link
-                      rel="stylesheet"
-                      // In dev, Safari will wrongly cache the resource if you preload it:
-                      // - https://github.com/vercel/next.js/issues/5860
-                      // - https://bugs.webkit.org/show_bug.cgi?id=187726
-                      // We used to add a `?ts=` query for resources in `pages` to bypass it,
-                      // but in this case it is fine as we don't need to preload the styles.
-                      href={`${assetPrefix}/_next/${href}`}
-                      // `Precedence` is an opt-in signal for React to handle
-                      // resource loading and deduplication, etc:
-                      // https://github.com/facebook/react/pull/25060
-                      // @ts-ignore
-                      precedence="next.js"
-                      key={index}
-                    />
-                  ))
-                : null}
+              {assets}
             </>
           )
         },
@@ -1201,13 +1224,15 @@ export async function renderToHTMLOrFlight(
       async (props) => {
         // Create full component tree from root to leaf.
         const injectedCSS = new Set<string>()
+        const injectedFontPreloadTags = new Set<string>()
+
         const { Component: ComponentTree } = await createComponentTree({
           createSegmentPath: (child) => child,
           loaderTree,
           parentParams: {},
           firstItem: true,
           injectedCSS,
-          injectedFontPreloadTags: new Set(),
+          injectedFontPreloadTags,
           rootLayoutIncluded: false,
           asNotFound: props.asNotFound,
         })
@@ -1228,6 +1253,12 @@ export async function renderToHTMLOrFlight(
           : rootLayoutAtThisLevel
           ? [DefaultNotFound]
           : []
+
+        const assets = createStaticAssets({
+          layoutOrPagePath: layout?.[1],
+          injectedCSS,
+          injectedFontPreloadTags,
+        })
 
         const initialTree = createFlightRouterStateFromLoaderTree(
           loaderTree,
@@ -1255,12 +1286,15 @@ export async function renderToHTMLOrFlight(
               globalErrorComponent={GlobalError}
               notFound={
                 NotFound && RootLayout ? (
-                  <RootLayout params={{}}>
-                    <NotFound />
-                  </RootLayout>
+                  <>
+                    {assets}
+                    <RootLayout params={{}}>
+                      {notFoundStyles}
+                      <NotFound />
+                    </RootLayout>
+                  </>
                 ) : undefined
               }
-              notFoundStyles={notFoundStyles}
               asNotFound={props.asNotFound}
             >
               <ComponentTree />

test/e2e/app-dir/app-css/app/not-found.js
@@ -1,0 +1,3 @@
+export default function RootNotFound() {
+  return <h1 className="not-found">Root not found</h1>
+}

test/e2e/app-dir/app-css/app/style.css
@@ -1,3 +1,7 @@
 body {
   font-size: large;
 }
+
+.not-found {
+  color: rgb(210, 105, 30); /* chocolate */
+}

test/e2e/app-dir/app-css/index.test.ts
@@ -194,6 +194,15 @@ createNextDescribe(
           ).toBe('rgb(255, 0, 0)')
         })
 
+        it('should include root layout css for root not-found.js', async () => {
+          const browser = await next.browser('/this-path-does-not-exist')
+          expect(
+            await browser.eval(
+              `window.getComputedStyle(document.querySelector('h1')).color`
+            )
+          ).toBe('rgb(210, 105, 30)')
+        })
+
         it('should include css imported in error.js', async () => {
           const browser = await next.browser('/error/client-component')
           await browser.elementByCss('button').click()