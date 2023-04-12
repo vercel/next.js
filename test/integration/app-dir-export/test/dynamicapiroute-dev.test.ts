805183e3e93e15db7bc6c66837925431791a2250
Jiachi Liu <inbox@huozhi.im>
2023-04-12 15:53:18 +0200

    Temporarily disable app dir export integration test (#48291)
    As the integration tests are keeping failed on canary, turns out that
    the require hook didn't set up properly so when running the
    app-dir-export integration test it breaks others. Will re-enable it once
    migrated to e2e tests
    
    x-ref:
    https://github.com/vercel/next.js/actions/runs/4668919790/jobs/8266728783?pr=48252#step:6:149
    x-ref:
    https://github.com/vercel/next.js/actions/runs/4668707070/jobs/8266215431?pr=48252#step:6:325
    x-ref:
    https://github.com/vercel/next.js/actions/runs/4675947717/jobs/8281764592?pr=48276

--- test/integration/app-dir-export/test/dynamicapiroute-dev.test.ts
+++ test/integration/app-dir-export/test/dynamicapiroute-dev.test.ts
@@ -1,17 +1,19 @@
 import { runTests } from './utils'
 
-it.each([
-  { dynamicApiRoute: 'undefined' },
-  { dynamicApiRoute: "'error'" },
-  { dynamicApiRoute: "'force-static'" },
-  {
-    dynamicApiRoute: "'force-dynamic'",
-    expectedErrMsg:
-      'export const dynamic = "force-dynamic" on page "/api/json" cannot be used with "output: export".',
-  },
-])(
-  'should work in dev with dynamicApiRoute $dynamicApiRoute',
-  async ({ dynamicApiRoute, expectedErrMsg }) => {
-    await runTests({ isDev: true, dynamicApiRoute, expectedErrMsg })
-  }
-)
+describe.skip('dynamic api route dev', () => {
+  it.each([
+    { dynamicApiRoute: 'undefined' },
+    { dynamicApiRoute: "'error'" },
+    { dynamicApiRoute: "'force-static'" },
+    {
+      dynamicApiRoute: "'force-dynamic'",
+      expectedErrMsg:
+        'export const dynamic = "force-dynamic" on page "/api/json" cannot be used with "output: export".',
+    },
+  ])(
+    'should work in dev with dynamicApiRoute $dynamicApiRoute',
+    async ({ dynamicApiRoute, expectedErrMsg }) => {
+      await runTests({ isDev: true, dynamicApiRoute, expectedErrMsg })
+    }
+  )
+})
