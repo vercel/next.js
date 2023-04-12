72ec23ad80e2beea7c919a919cbf555ae836d459
Jan Kaifer <jan@kaifer.cz>
2023-04-12 19:52:55 +0200

    don't copy .tgz files in our tests

--- test/lib/create-next-install.js
+++ test/lib/create-next-install.js
@@ -79,7 +79,8 @@ async function createNextInstall({
                     !item.includes('.DS_Store') &&
                     // Exclude Rust compilation files
                     !/next[\\/]build[\\/]swc[\\/]target/.test(item) &&
-                    !/next-swc[\\/]target/.test(item)
+                    !/next-swc[\\/]target/.test(item) &&
+                    !item.endsWith('.tgz')
                   )
                 },
               }
