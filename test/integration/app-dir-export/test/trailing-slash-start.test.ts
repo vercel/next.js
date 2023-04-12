276784e21e7470b845ae45ecb9f407d640b38d55
Jiachi Liu <inbox@huozhi.im>
2023-04-12 13:29:47 +0200

    disable

--- test/integration/app-dir-export/test/trailing-slash-start.test.ts
+++
@@ -1,8 +0,0 @@
-import { runTests } from './utils'
-
-it.each([{ trailingSlash: false }, { trailingSlash: true }])(
-  "should work in prod with trailingSlash '$trailingSlash'",
-  async ({ trailingSlash }) => {
-    await runTests({ isDev: false, trailingSlash })
-  }
-)
