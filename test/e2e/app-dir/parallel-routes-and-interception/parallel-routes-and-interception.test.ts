vercel/next.js

commit 17e44d29074e880ec249023edc9112ac35d6fa87

app-router: add batching support for RSC router payloads (#48253)
This PR basically adds support for processing multiple router payloads.
Previously we were only handling one payload at a time but now that we
introduced parallel routes, we need to be able to render and return
separate parts of the layout separately.

Before, the single payload was generated in a DFS manner: we traversed
the router state and rendered the first segment of the tree that we
found needed rendering. In practice, this meant that we could miss
adjacent segments that might need to be re-rendered as well.

Now, we iterate all branches of the tree and return an array of flight
patches to be applied on the client correctly.

fixes NEXT-971

<!-- Thanks for opening a PR! Your contribution is much appreciated.
To make sure your PR is handled as smoothly as possible we request that
you follow the checklist sections below.
Choose the right checklist for the change(s) that you're making:

## For Contributors

### Improving Documentation or adding/fixing Examples

- The "examples guidelines" are followed from our contributing doc
https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
- Make sure the linting passes by running `pnpm build && pnpm lint`. See
https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md

### Fixing a bug

- Related issues linked using `fixes #number`
- Tests added. See:
https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
- Errors have a helpful link attached, see
https://github.com/vercel/next.js/blob/canary/contributing.md

### Adding a feature

- Implements an existing feature request or RFC. Make sure the feature
request has been accepted for implementation before opening a PR. (A
discussion must be opened, see
https://github.com/vercel/next.js/discussions/new?category=ideas)

packages/next/src/server/app-render/app-render.tsx
@@ -988,24 +988,10 @@ export async function renderToHTMLOrFlight(
         injectedFontPreloadTags: Set<string>
         rootLayoutIncluded: boolean
         asNotFound?: boolean
-      }): Promise<FlightDataPath> => {
+      }): Promise<FlightDataPath[]> => {
         const [segment, parallelRoutes, components] = loaderTreeToFilter
 
-        // if there's a refetch key, it'll probably take precedence when rendering
-        // so we sort the parallel routes so that the refetch key is first
-        // Sort the routes so that a route marked for refetching is at the beginning of the array
-        const parallelRoutesKeys = Object.keys(parallelRoutes).sort(
-          (parallelRouteKeyA, parallelRouteKeyB) => {
-            if (flightRouterState?.[1][parallelRouteKeyA][3] === 'refetch') {
-              return -1
-            }
-            if (flightRouterState?.[1][parallelRouteKeyB][3] === 'refetch') {
-              return 1
-            }
-
-            return 0
-          }
-        )
+        const parallelRoutesKeys = Object.keys(parallelRoutes)
 
         const { layout } = components
         const isLayout = typeof layout !== 'undefined'
@@ -1046,19 +1032,7 @@ export async function renderToHTMLOrFlight(
           // Last item in the tree
           parallelRoutesKeys.length === 0 ||
           // Explicit refresh
-          flightRouterState[3] === 'refetch' ||
-          // if any of the parallel routes segments differ or have a refresh marker
-          Object.entries(parallelRoutes).some(
-            ([parallelRouteKey, [parallelRouteSegment]]) => {
-              const [incomingParallelRouteSegment] =
-                flightRouterState[1][parallelRouteKey] ?? []
-
-              return (
-                incomingParallelRouteSegment === '__DEFAULT__' &&
-                parallelRouteSegment !== '__DEFAULT__'
-              )
-            }
-          )
+          flightRouterState[3] === 'refetch'
 
         if (!parentRendered && renderComponentsOnThisLevel) {
           const overriddenSegment =
@@ -1066,41 +1040,44 @@ export async function renderToHTMLOrFlight(
             canSegmentBeOverridden(actualSegment, flightRouterState[0])
               ? flightRouterState[0]
               : null
-
           return [
-            overriddenSegment ?? actualSegment,
-            createFlightRouterStateFromLoaderTree(
-              // Create router state using the slice of the loaderTree
-              loaderTreeToFilter,
-              getDynamicParamFromSegment,
-              query
-            ),
-            // Check if one level down from the common layout has a loading component. If it doesn't only provide the router state as part of the Flight data.
-            isPrefetch && !Boolean(components.loading)
-              ? null
-              : // Create component tree using the slice of the loaderTree
-                // @ts-expect-error TODO-APP: fix async component type
-                React.createElement(async () => {
-                  const { Component } = await createComponentTree(
-                    // This ensures flightRouterPath is valid and filters down the tree
-                    {
-                      createSegmentPath: (child) => {
-                        return createSegmentPath(child)
-                      },
-                      loaderTree: loaderTreeToFilter,
-                      parentParams: currentParams,
-                      firstItem: isFirst,
-                      injectedCSS,
-                      injectedFontPreloadTags,
-                      // This is intentionally not "rootLayoutIncludedAtThisLevelOrAbove" as createComponentTree starts at the current level and does a check for "rootLayoutAtThisLevel" too.
-                      rootLayoutIncluded: rootLayoutIncluded,
-                      asNotFound,
-                    }
-                  )
+            [
+              overriddenSegment ?? actualSegment,
+              createFlightRouterStateFromLoaderTree(
+                // Create router state using the slice of the loaderTree
+                loaderTreeToFilter,
+                getDynamicParamFromSegment,
+                query
+              ),
+              // Check if one level down from the common layout has a loading component. If it doesn't only provide the router state as part of the Flight data.
+              isPrefetch && !Boolean(components.loading)
+                ? null
+                : // Create component tree using the slice of the loaderTree
+                  // @ts-expect-error TODO-APP: fix async component type
+                  React.createElement(async () => {
+                    const { Component } = await createComponentTree(
+                      // This ensures flightRouterPath is valid and filters down the tree
+                      {
+                        createSegmentPath: (child) => {
+                          return createSegmentPath(child)
+                        },
+                        loaderTree: loaderTreeToFilter,
+                        parentParams: currentParams,
+                        firstItem: isFirst,
+                        injectedCSS,
+                        injectedFontPreloadTags,
+                        // This is intentionally not "rootLayoutIncludedAtThisLevelOrAbove" as createComponentTree starts at the current level and does a check for "rootLayoutAtThisLevel" too.
+                        rootLayoutIncluded: rootLayoutIncluded,
+                        asNotFound,
+                      }
+                    )
 
-                  return <Component />
-                }),
-            isPrefetch && !Boolean(components.loading) ? null : rscPayloadHead,
+                    return <Component />
+                  }),
+              isPrefetch && !Boolean(components.loading)
+                ? null
+                : rscPayloadHead,
+            ],
           ]
         }
 
@@ -1131,36 +1108,53 @@ export async function renderToHTMLOrFlight(
         }
 
         // Walk through all parallel routes.
-        for (const parallelRouteKey of parallelRoutesKeys) {
-          const parallelRoute = parallelRoutes[parallelRouteKey]
+        const paths: FlightDataPath[] = (
+          await Promise.all(
+            parallelRoutesKeys.map(async (parallelRouteKey) => {
+              // for (const parallelRouteKey of parallelRoutesKeys) {
+              const parallelRoute = parallelRoutes[parallelRouteKey]
 
-          const currentSegmentPath: FlightSegmentPath = isFirst
-            ? [parallelRouteKey]
-            : [actualSegment, parallelRouteKey]
+              const currentSegmentPath: FlightSegmentPath = isFirst
+                ? [parallelRouteKey]
+                : [actualSegment, parallelRouteKey]
 
-          const path = await walkTreeWithFlightRouterState({
-            createSegmentPath: (child) => {
-              return createSegmentPath([...currentSegmentPath, ...child])
-            },
-            loaderTreeToFilter: parallelRoute,
-            parentParams: currentParams,
-            flightRouterState:
-              flightRouterState && flightRouterState[1][parallelRouteKey],
-            parentRendered: parentRendered || renderComponentsOnThisLevel,
-            isFirst: false,
-            rscPayloadHead,
-            injectedCSS: injectedCSSWithCurrentLayout,
-            injectedFontPreloadTags: injectedFontPreloadTagsWithCurrentLayout,
-            rootLayoutIncluded: rootLayoutIncludedAtThisLevelOrAbove,
-            asNotFound,
-          })
+              const path = await walkTreeWithFlightRouterState({
+                createSegmentPath: (child) => {
+                  return createSegmentPath([...currentSegmentPath, ...child])
+                },
+                loaderTreeToFilter: parallelRoute,
+                parentParams: currentParams,
+                flightRouterState:
+                  flightRouterState && flightRouterState[1][parallelRouteKey],
+                parentRendered: parentRendered || renderComponentsOnThisLevel,
+                isFirst: false,
+                rscPayloadHead,
+                injectedCSS: injectedCSSWithCurrentLayout,
+                injectedFontPreloadTags:
+                  injectedFontPreloadTagsWithCurrentLayout,
+                rootLayoutIncluded: rootLayoutIncludedAtThisLevelOrAbove,
+                asNotFound,
+              })
 
-          if (typeof path[path.length - 1] !== 'string') {
-            return [actualSegment, parallelRouteKey, ...path]
-          }
-        }
+              return path
+                .map((item) => {
+                  // we don't need to send over default routes in the flight data
+                  // because they are always ignored by the client
+                  if (
+                    item[0] === '__DEFAULT__' &&
+                    flightRouterState &&
+                    !!flightRouterState[1][parallelRouteKey][0]
+                  ) {
+                    return null
+                  }
+                  return [actualSegment, parallelRouteKey, ...item]
+                })
+                .filter(Boolean) as FlightDataPath[]
+            })
+          )
+        ).flat()
 
-        return [actualSegment]
+        return paths
       }
 
       const metadataItems = await resolveMetadata({
@@ -1170,33 +1164,31 @@ export async function renderToHTMLOrFlight(
       })
       // Flight data that is going to be passed to the browser.
       // Currently a single item array but in the future multiple patches might be combined in a single request.
-      const flightData: FlightData = [
-        (
-          await walkTreeWithFlightRouterState({
-            createSegmentPath: (child) => child,
-            loaderTreeToFilter: loaderTree,
-            parentParams: {},
-            flightRouterState: providedFlightRouterState,
-            isFirst: true,
-            // For flight, render metadata inside leaf page
-            rscPayloadHead: (
-              <>
-                {/* Adding key={requestId} to make metadata remount for each render */}
-                {/* @ts-expect-error allow to use async server component */}
-                <MetadataTree
-                  key={requestId}
-                  metadata={metadataItems}
-                  pathname={pathname}
-                />
-              </>
-            ),
-            injectedCSS: new Set(),
-            injectedFontPreloadTags: new Set(),
-            rootLayoutIncluded: false,
-            asNotFound: pathname === '/404',
-          })
-        ).slice(1),
-      ]
+      const flightData: FlightData = (
+        await walkTreeWithFlightRouterState({
+          createSegmentPath: (child) => child,
+          loaderTreeToFilter: loaderTree,
+          parentParams: {},
+          flightRouterState: providedFlightRouterState,
+          isFirst: true,
+          // For flight, render metadata inside leaf page
+          rscPayloadHead: (
+            <>
+              {/* Adding key={requestId} to make metadata remount for each render */}
+              {/* @ts-expect-error allow to use async server component */}
+              <MetadataTree
+                key={requestId}
+                metadata={metadataItems}
+                pathname={pathname}
+              />
+            </>
+          ),
+          injectedCSS: new Set(),
+          injectedFontPreloadTags: new Set(),
+          rootLayoutIncluded: false,
+          asNotFound: pathname === '/404',
+        })
+      ).map((path) => path.slice(1)) // remove the '' (root) segment
 
       // For app dir, use the bundled version of Fizz renderer (renderToReadableStream)
       // which contains the subset React.

test/e2e/app-dir/parallel-routes-and-interception/parallel-routes-and-interception.test.ts
@@ -206,6 +206,43 @@ createNextDescribe(
         const html = await res.text()
         expect(html).toContain('page could not be found')
       })
+
+      it('should render nested parallel routes', async () => {
+        const browser = await next.browser('/parallel-side-bar/nested/deeper')
+        await check(
+          () => browser.waitForElementByCss('#nested-deeper-main').text(),
+          'Nested deeper page'
+        )
+
+        await check(
+          () => browser.waitForElementByCss('#nested-deeper-sidebar').text(),
+          'Nested deeper sidebar here'
+        )
+
+        await browser.elementByCss('[href="/parallel-side-bar/nested"]').click()
+
+        await check(
+          () => browser.waitForElementByCss('#nested-main').text(),
+          'Nested page'
+        )
+
+        await check(
+          () => browser.waitForElementByCss('#nested-sidebar').text(),
+          'Nested sidebar here'
+        )
+
+        await browser.elementByCss('[href="/parallel-side-bar"]').click()
+
+        await check(
+          () => browser.waitForElementByCss('#main').text(),
+          'homepage'
+        )
+
+        await check(
+          () => browser.waitForElementByCss('#sidebar-main').text(),
+          'root sidebar here'
+        )
+      })
     })
 
     describe('route intercepting', () => {

packages/next/src/client/components/router-reducer/apply-flight-data.ts
@@ -2,10 +2,9 @@ import { CacheNode, CacheStates } from '../../../shared/lib/app-router-context'
 import { FlightDataPath } from '../../../server/app-render/types'
 import { fillLazyItemsTillLeafWithHead } from './fill-lazy-items-till-leaf-with-head'
 import { fillCacheWithNewSubTreeData } from './fill-cache-with-new-subtree-data'
-import { ReadonlyReducerState } from './router-reducer-types'
 
 export function applyFlightData(
-  state: ReadonlyReducerState,
+  existingCache: CacheNode,
   cache: CacheNode,
   flightDataPath: FlightDataPath,
   wasPrefetched?: boolean
@@ -23,7 +22,7 @@ export function applyFlightData(
     cache.subTreeData = subTreeData
     fillLazyItemsTillLeafWithHead(
       cache,
-      state.cache,
+      existingCache,
       treePatch,
       head,
       wasPrefetched
@@ -31,10 +30,10 @@ export function applyFlightData(
   } else {
     // Copy subTreeData for the root node of the cache.
     cache.status = CacheStates.READY
-    cache.subTreeData = state.cache.subTreeData
-    cache.parallelRoutes = new Map(state.cache.parallelRoutes)
+    cache.subTreeData = existingCache.subTreeData
+    cache.parallelRoutes = new Map(existingCache.parallelRoutes)
     // Create a copy of the existing cache with the subTreeData applied.
-    fillCacheWithNewSubTreeData(cache, state.cache, flightDataPath)
+    fillCacheWithNewSubTreeData(cache, existingCache, flightDataPath)
   }
 
   return true

packages/next/src/client/components/router-reducer/reducers/fast-refresh-reducer.ts
@@ -54,50 +54,54 @@ function fastRefreshReducerImpl(
   // Remove cache.data as it has been resolved at this point.
   cache.data = null
 
-  // TODO-APP: Currently the Flight data can only have one item but in the future it can have multiple paths.
-  const flightDataPath = flightData[0]
+  let currentTree = state.tree
+  let currentCache = state.cache
 
-  // FlightDataPath with more than two items means unexpected Flight data was returned
-  if (flightDataPath.length !== 3) {
-    // TODO-APP: handle this case better
-    console.log('REFRESH FAILED')
-    return state
+  for (const flightDataPath of flightData) {
+    // FlightDataPath with more than two items means unexpected Flight data was returned
+    if (flightDataPath.length !== 3) {
+      // TODO-APP: handle this case better
+      console.log('REFRESH FAILED')
+      return state
+    }
+
+    // Given the path can only have two items the items are only the router state and subTreeData for the root.
+    const [treePatch] = flightDataPath
+    const newTree = applyRouterStatePatchToTree(
+      // TODO-APP: remove ''
+      [''],
+      currentTree,
+      treePatch
+    )
+
+    if (newTree === null) {
+      throw new Error('SEGMENT MISMATCH')
+    }
+
+    if (isNavigatingToNewRootLayout(currentTree, newTree)) {
+      return handleExternalUrl(state, mutable, href, state.pushRef.pendingPush)
+    }
+
+    const canonicalUrlOverrideHref = canonicalUrlOverride
+      ? createHrefFromUrl(canonicalUrlOverride)
+      : undefined
+
+    if (canonicalUrlOverride) {
+      mutable.canonicalUrl = canonicalUrlOverrideHref
+    }
+    const applied = applyFlightData(currentCache, cache, flightDataPath)
+
+    if (applied) {
+      mutable.cache = cache
+      currentCache = cache
+    }
+
+    mutable.previousTree = currentTree
+    mutable.patchedTree = newTree
+    mutable.canonicalUrl = href
+
+    currentTree = newTree
   }
-
-  // Given the path can only have two items the items are only the router state and subTreeData for the root.
-  const [treePatch] = flightDataPath
-  const newTree = applyRouterStatePatchToTree(
-    // TODO-APP: remove ''
-    [''],
-    state.tree,
-    treePatch
-  )
-
-  if (newTree === null) {
-    throw new Error('SEGMENT MISMATCH')
-  }
-
-  if (isNavigatingToNewRootLayout(state.tree, newTree)) {
-    return handleExternalUrl(state, mutable, href, state.pushRef.pendingPush)
-  }
-
-  const canonicalUrlOverrideHref = canonicalUrlOverride
-    ? createHrefFromUrl(canonicalUrlOverride)
-    : undefined
-
-  if (canonicalUrlOverride) {
-    mutable.canonicalUrl = canonicalUrlOverrideHref
-  }
-  const applied = applyFlightData(state, cache, flightDataPath)
-
-  if (applied) {
-    mutable.cache = cache
-  }
-
-  mutable.previousTree = state.tree
-  mutable.patchedTree = newTree
-  mutable.canonicalUrl = href
-
   return handleMutable(state, mutable)
 }
 

commit ccaa7d6482ba06aaf44c60301116c009bdf33d5a

parallel route: fix payload not being sent on refetch (#48294)
This PR fixes a bug introduced in #48253 where I inadvertently didn't
return the refetch payloads anymore

<!-- Thanks for opening a PR! Your contribution is much appreciated.
To make sure your PR is handled as smoothly as possible we request that
you follow the checklist sections below.
Choose the right checklist for the change(s) that you're making:

## For Contributors

### Improving Documentation or adding/fixing Examples

- The "examples guidelines" are followed from our contributing doc
https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
- Make sure the linting passes by running `pnpm build && pnpm lint`. See
https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md

### Fixing a bug

- Related issues linked using `fixes #number`
- Tests added. See:
https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
- Errors have a helpful link attached, see
https://github.com/vercel/next.js/blob/canary/contributing.md

### Adding a feature

- Implements an existing feature request or RFC. Make sure the feature
request has been accepted for implementation before opening a PR. (A
discussion must be opened, see
https://github.com/vercel/next.js/discussions/new?category=ideas)
- Related issues/discussions are linked using `fixes #number`
- e2e tests added
(https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
- Documentation added
- Telemetry added. In case of a feature if it's used or not.
- Errors have a helpful link attached, see
https://github.com/vercel/next.js/blob/canary/contributing.md



## For Maintainers

- Minimal description (aim for explaining to someone not on the team to
understand the PR)
- When linking to a Slack thread, you might want to share details of the
conclusion

packages/next/src/server/app-render/app-render.tsx
@@ -1137,11 +1137,12 @@ export async function renderToHTMLOrFlight(
               return path
                 .map((item) => {
                   // we don't need to send over default routes in the flight data
-                  // because they are always ignored by the client
+                  // because they are always ignored by the client, unless it's a refetch
                   if (
                     item[0] === '__DEFAULT__' &&
                     flightRouterState &&
-                    !!flightRouterState[1][parallelRouteKey][0]
+                    !!flightRouterState[1][parallelRouteKey][0] &&
+                    flightRouterState[1][parallelRouteKey][3] !== 'refetch'
                   ) {
                     return null
                   }

test/e2e/app-dir/parallel-routes-and-interception/parallel-routes-and-interception.test.ts
@@ -407,6 +407,16 @@ createNextDescribe(
           'Photo MODAL 1'
         )
 
+        await check(
+          () =>
+            browser
+              .elementByCss('[href="/intercepting-parallel-modal/photo/2"]')
+              .click()
+              .waitForElementByCss('#photo-modal-2')
+              .text(),
+          'Photo MODAL 2'
+        )
+
         // Check if modal was rendered while existing page content is preserved.
         await check(
           () => browser.elementByCss('#user-page').text(),
@@ -416,19 +426,19 @@ createNextDescribe(
         // Check if url matches even though it was intercepted.
         await check(
           () => browser.url(),
-          next.url + '/intercepting-parallel-modal/photo/1'
+          next.url + '/intercepting-parallel-modal/photo/2'
         )
 
         // Trigger a refresh, this should load the normal page, not the modal.
         await check(
-          () => browser.refresh().waitForElementByCss('#photo-page-1').text(),
-          'Photo PAGE 1'
+          () => browser.refresh().waitForElementByCss('#photo-page-2').text(),
+          'Photo PAGE 2'
         )
 
         // Check if the url matches still.
         await check(
           () => browser.url(),
-          next.url + '/intercepting-parallel-modal/photo/1'
+          next.url + '/intercepting-parallel-modal/photo/2'
         )
       })
     })