ea40492b5bf3aeb253a5734640fa49f820b1b84b
JJ Kasper <jj@jjsweb.site>
2023-04-13 16:50:33 -0700

    Update font handling in CNA templates

--- packages/create-next-app/templates/app-tw/js/app/layout.js
+++ packages/create-next-app/templates/app-tw/js/app/layout.js
@@ -1,4 +1,7 @@
 import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
