fcda11a6eb7c3a27c26fb8a13e69d751a8999839
Tobias Koppers <tobias.koppers@googlemail.com>
2023-04-14 16:11:32 +0200

    make node bootstrap asset lazy (vercel/turbo#4579)
    ### Description
    
    lazy creation of the chunk group to make walking introspection faster

--- crates/turbopack-node/src/bootstrap.rs
+++ crates/turbopack-node/src/bootstrap.rs
@@ -5,6 +5,7 @@ use turbo_tasks::primitives::StringVc;
 use turbo_tasks_fs::{File, FileSystemPathVc};
 use turbopack_core::{
     asset::{Asset, AssetContentVc, AssetVc, AssetsVc},
+    chunk::{ChunkVc, ChunkingContext, ChunkingContextVc, EvaluatableAssetsVc},
     ident::AssetIdentVc,
     reference::{AssetReferencesVc, SingleAssetReferenceVc},
 };
@@ -13,12 +14,21 @@ use turbopack_ecmascript::utils::StringifyJs;
 #[turbo_tasks::value(shared)]
 pub(super) struct NodeJsBootstrapAsset {
     pub(super) path: FileSystemPathVc,
-    pub(super) chunks: AssetsVc,
+    pub(super) chunking_context: ChunkingContextVc,
+    pub(super) entry: ChunkVc,
+    pub(super) evaluatable_assets: EvaluatableAssetsVc,
 }
 
 #[turbo_tasks::function]
 fn node_js_bootstrap_chunk_reference_description() -> StringVc {
     StringVc::cell("node.js bootstrap chunk".to_string())
+}
+
+impl NodeJsBootstrapAsset {
+    fn chunks(&self) -> AssetsVc {
+        self.chunking_context
+            .evaluated_chunk_group(self.entry, self.evaluatable_assets)
+    }
 }
 
 #[turbo_tasks::value_impl]
@@ -36,7 +46,7 @@ impl Asset for NodeJsBootstrapAsset {
         // but until then this is a simple hack to make it work for now
         let mut output = "Error.stackTraceLimit = 100;\nglobal.self = global;\n".to_string();
 
-        for chunk in self.chunks.await?.iter() {
+        for chunk in self.chunks().await?.iter() {
             let path = &*chunk.ident().path().await?;
             if let Some(p) = context_path.get_relative_path_to(path) {
                 if p.ends_with(".js") {
@@ -50,7 +60,7 @@ impl Asset for NodeJsBootstrapAsset {
 
     #[turbo_tasks::function]
     async fn references(&self) -> Result<AssetReferencesVc> {
-        let chunks = self.chunks.await?;
+        let chunks = self.chunks().await?;
         let mut references = Vec::new();
         for chunk in chunks.iter() {
             references.push(
