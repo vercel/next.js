b7cc62bee72b17a22f52ef6a518525f0569902c5
Jimmy Lai <laijimmy0@gmail.com>
2023-04-15 16:56:54 +0200

    add test

---
+++ test/e2e/app-dir/parallel-routes-and-interception/app/dynamic-interception/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <Link href="/dynamic-interception/1">Go to dynamic-interception 1</Link>
+  )
+}
