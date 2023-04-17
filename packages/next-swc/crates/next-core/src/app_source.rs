1edc8a21ece11342adfde63750d3ffe703d13f2d
Tobias Koppers <tobias.koppers@googlemail.com>
2023-04-17 09:54:37 +0000

    fix relative path

--- packages/next-swc/crates/next-core/src/app_source.rs
+++ packages/next-swc/crates/next-core/src/app_source.rs
@@ -709,7 +709,7 @@ import {}, {{ chunks as {} }} from "COMPONENT_{}";
         let mut result =
             RopeBuilder::from("import { IPC } from \"@vercel/turbopack-node/ipc/index\";\n");
 
-        let import_path = next_js_file_path("entry/app-renderer.tsx")
+        let import_path = next_js_file_path("entry")
             .await?
             .get_relative_path_to(&*next_js_file_path("polyfill/app-polyfills.ts").await?)
             .unwrap();
