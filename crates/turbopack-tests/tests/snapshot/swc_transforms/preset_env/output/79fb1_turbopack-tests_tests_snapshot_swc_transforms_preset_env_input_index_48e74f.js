1c52ca02dd67edb15cf9a05ce5410af38d43ca0b
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-18 13:27:30 +0900

    Update swc_core to `v0.75.22` (vercel/turbo#4470)
    ### Description
    
    This update includes an improvement of `@swc/helpers`, so we need to
    update `@swc/helpers` too when updating next-swc.
    
    ### Testing Instructions
    
    Fix WEB-744
    Fix WEB-859
    
    ---
    
     - Fixes #2825.
     - Fixes #4092.

--- crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_48e74f.js
+++ crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_48e74f.js
@@ -1,11 +1,11 @@
 (globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_21f811.js",
+    "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_48e74f.js",
     {},
 ]);
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-  "path": "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_21f811.js",
+  "path": "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_48e74f.js",
   "chunks": [
-    "output/63a02_@swc_helpers_src__class_call_check.mjs_f682d0._.js",
+    "output/63a02_@swc_helpers____class_call_check_f08d44.js",
     "output/79fb1_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_b53fce.js"
   ],
   "source": "entry"
