57f001bc2745c7c6bde9ad44227d341ba66319f6
Tim Neutkens <tim@timneutkens.nl>
2023-04-18 10:57:30 +0200

    Update test

--- test/development/app-dir/multiple-compiles-single-route/multiple-compiles-single-route.test.ts
+++ test/development/app-dir/multiple-compiles-single-route/multiple-compiles-single-route.test.ts
@@ -9,12 +9,12 @@ createNextDescribe(
     // Recommended for tests that check HTML. Cheerio is a HTML parser that has a jQuery like API.
     it('should not compile additional matching paths', async () => {
       const logs: string[] = []
-      next.on('stderr', (log) => {
+      next.on('stdout', (log) => {
         logs.push(log)
       })
       await next.render('/about')
-      //
-      expect(logs.some((log) => log.includes(`/[slug]`))).toBe(false)
+      // Check if `/[slug]` is mentioned in the logs as being compiled
+      expect(logs.some((log) => log.includes('/[slug]'))).toBe(false)
     })
   }
 )
