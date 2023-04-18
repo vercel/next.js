6987aaf4facec0e040184bb2b8cd4655c0650b22
Tobias Koppers <tobias.koppers@googlemail.com>
2023-04-18 13:21:45 +0000

    link

--- packages/next-swc/crates/next-core/src/next_client/transforms.rs
+++ packages/next-swc/crates/next-core/src/next_client/transforms.rs
@@ -6,10 +6,8 @@ use crate::{
     next_client::context::ClientContextType,
     next_config::NextConfigVc,
     next_shared::transforms::{
-        get_next_dynamic_transform_rule, get_next_font_transform_rule,
-        get_next_modularize_imports_rule, get_next_pages_transforms_rule,
         get_next_dynamic_transform_rule, get_next_font_transform_rule, get_next_image_rule,
-        get_next_pages_transforms_rule,
+        get_next_modularize_imports_rule, get_next_pages_transforms_rule,
     },
 };
 
