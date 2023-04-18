91833448579b7eeb2865f515fcea61c7b3faaa37
Tim Neutkens <tim@timneutkens.nl>
2023-04-18 10:54:35 +0200

    Update test

--- test/development/app-dir/multiple-compiles-single-route/multiple-compiles-single-route.test.ts
+++ test/development/app-dir/multiple-compiles-single-route/multiple-compiles-single-route.test.ts
@@ -7,28 +7,14 @@ createNextDescribe(
   },
   ({ next }) => {
     // Recommended for tests that check HTML. Cheerio is a HTML parser that has a jQuery like API.
-    it('should work using cheerio', async () => {
-      const $ = await next.render$('/')
-      expect($('p').text()).toBe('hello world')
-    })
-
-    // Recommended for tests that need a full browser
-    it('should work using browser', async () => {
-      const browser = await next.browser('/')
-      expect(await browser.elementByCss('p').text()).toBe('hello world')
-    })
-
-    // In case you need the full HTML. Can also use $.html() with cheerio.
-    it('should work with html', async () => {
-      const html = await next.render('/')
-      expect(html).toContain('hello world')
-    })
-
-    // In case you need to test the response object
-    it('should work with fetch', async () => {
-      const res = await next.fetch('/')
-      const html = await res.text()
-      expect(html).toContain('hello world')
+    it('should not compile additional matching paths', async () => {
+      const logs: string[] = []
+      next.on('stderr', (log) => {
+        logs.push(log)
+      })
+      await next.render('/about')
+      //
+      expect(logs.some((log) => log.includes(`/[slug]`))).toBe(false)
     })
   }
 )
