189e6a3687924de3172438a2912973bd49c1e37e
Tobias Koppers <tobias.koppers@googlemail.com>
2023-04-20 02:18:59 +0200

    use structured images with metainfo (blur placeholder) (#48531)
    ### What?
    
    add support for blur placeholder generation to turbopack
    
    add `StructuredImageModuleType` which is used with `ModuleType::Custom`
    to allow importing an image as `{ url, width, height, blurDataURL,
    blurWidth, blurHeight }`
    
    in contrast to next.js with webpack this will also generate blur
    placeholder in development instead of using a _next/image reference.
    This should lead to more production-like experience (at the cost of a
    little bit of compilation time).
    
    turbo PR: https://github.com/vercel/turbo/pull/4621
    
    ### Why?
    
    Turbopack was crashing on `placeholder="blur"` before.
    
    fixes WEB-534
    
    ### Turbopack changes
    
    * https://github.com/vercel/turbo/pull/4521 <!-- OJ Kwon -
    feat(contextcondition): support InPath contextcondition -->
    * https://github.com/vercel/turbo/pull/4601 <!-- Alex Kirszenberg -
    Chunking Context Refactor pt. 3: Address PR comments from pt. 2 -->
    * https://github.com/vercel/turbo/pull/4623 <!-- Tobias Koppers -
    exclude turborepo from turbopack bench tests -->
    * https://github.com/vercel/turbo/pull/4399 <!-- Leah - support
    require.context -->
    * https://github.com/vercel/turbo/pull/4610 <!-- OJ Kwon - test(subset):
    add mdx test into subset -->
    * https://github.com/vercel/turbo/pull/4624 <!-- Tobias Koppers - run
    benchmarks on windows and macOS too -->
    * https://github.com/vercel/turbo/pull/4620 <!-- Alex Kirszenberg - Make
    ContainmentTree fully generic -->
    * https://github.com/vercel/turbo/pull/4600 <!-- Tobias Koppers - add
    getChunkPath method -->
    * https://github.com/vercel/turbo/pull/4621 <!-- Tobias Koppers - add
    turbopack-image -->
    * https://github.com/vercel/turbo/pull/4639 <!-- Tobias Koppers -
    restrict snapshot path for windows path length limit -->
    * https://github.com/vercel/turbo/pull/4641 <!-- Tobias Koppers - put
    webp behind a feature flag -->

---
+++ packages/next-swc/crates/next-core/src/image/mod.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use indexmap::indexmap;
+use turbo_binding::{
+    turbo::tasks::Value,
+    turbopack::{
+        core::{
+            asset::AssetVc,
+            context::{AssetContext, AssetContextVc},
+            plugin::{CustomModuleType, CustomModuleTypeVc},
+            resolve::ModulePartVc,
+        },
+        ecmascript::{
+            EcmascriptInputTransformsVc, EcmascriptModuleAssetType, EcmascriptModuleAssetVc,
+            EcmascriptOptions, InnerAssetsVc,
+        },
+        r#static::StaticModuleAssetVc,
+    },
+};
+
+use self::source::StructuredImageSourceAsset;
+
+pub(crate) mod source;
+
+/// Module type that analyzes images and offers some meta information like
+/// width, height and blur placeholder as export from the module.
+#[turbo_tasks::value]
+pub struct StructuredImageModuleType {}
+
+#[turbo_tasks::value_impl]
+impl StructuredImageModuleTypeVc {
+    #[turbo_tasks::function]
+    pub fn new() -> Self {
+        StructuredImageModuleTypeVc::cell(StructuredImageModuleType {})
+    }
+}
+
+#[turbo_tasks::value_impl]
+impl CustomModuleType for StructuredImageModuleType {
+    #[turbo_tasks::function]
+    async fn create_module(
+        &self,
+        source: AssetVc,
+        context: AssetContextVc,
+        _part: Option<ModulePartVc>,
+    ) -> Result<AssetVc> {
+        let static_asset = StaticModuleAssetVc::new(source, context);
+        Ok(EcmascriptModuleAssetVc::new_with_inner_assets(
+            StructuredImageSourceAsset { image: source }.cell().into(),
+            context,
+            Value::new(EcmascriptModuleAssetType::Ecmascript),
+            EcmascriptInputTransformsVc::empty(),
+            Value::new(EcmascriptOptions {
+                ..Default::default()
+            }),
+            context.compile_time_info(),
+            InnerAssetsVc::cell(indexmap!(
+                "IMAGE".to_string() => static_asset.into()
+            )),
+        )
+        .into())
+    }
+}
