f053ec7d21dbb89b8ff4b16c44d5d44b8fcd7f00
Oliver Benns <oliverbenns@gmail.com>
2023-04-22 16:22:26 +0100

    Add safety

--- examples/auth-http-basic/middleware.ts
+++ examples/auth-http-basic/middleware.ts
@@ -3,6 +3,10 @@ import basicAuth from './lib/basicAuth'
 // Note: This is an example. In a production environment you will likely want to get your
 // credentials from a more secure source, as well as compare hashes rather than plaintext.
 export const middleware = basicAuth.createMiddleware((credentials) => {
+  if (!process.env.HTTP_BASIC_AUTH_USER || !process.env.HTTP_BASIC_AUTH_PASS) {
+    return false
+  }
+
   return (
     credentials.username === process.env.HTTP_BASIC_AUTH_USER &&
     credentials.password === process.env.HTTP_BASIC_AUTH_PASS
