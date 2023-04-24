vercel/next.js

commit 958150d15be0bbd6a05fad99fc301461b8864734

Add suffix to static metadata images (#48202)
### What?

The change in #47985 breaks the URLs of static image files like
`/(group)/opengraph-image.png` to `/opengraph-image.png-012345`.
References from `/` are also broken.

### Why?

This is because only `opengraph-image.ts` and `opengraph-image.tsx` are
considered.

### How?

In this Pull Request, we are trying to solve the problem by including
similar support for `opengraph-image.png` and `opengraph-image.jpeg`.

---------

Co-authored-by: Jiachi Liu <inbox@huozhi.im>

packages/next/src/lib/metadata/get-metadata-route.ts
@@ -47,7 +47,13 @@ export function normalizeMetadataRoute(page: string) {
     // Support both /<metadata-route.ext> and custom routes /<metadata-route>/route.ts.
     // If it's a metadata file route, we need to append /route to the page.
     if (!route.endsWith('/route')) {
-      route = `${route}${suffix ? `-${suffix}` : ''}/route`
+      const { dir, name, ext } = path.parse(route)
+
+      route = path.join(
+        dir,
+        `${name}${suffix ? `-${suffix}` : ''}${ext}`,
+        'route'
+      )
     }
   }
   return route

packages/next/src/build/webpack/loaders/next-metadata-image-loader.ts
@@ -114,19 +114,24 @@ async function nextMetadataImageLoader(this: any, content: Buffer) {
   import path from 'next/dist/shared/lib/isomorphic/path'
   import { interpolateDynamicPath } from 'next/dist/server/server-utils'
   import { getNamedRouteRegex } from 'next/dist/shared/lib/router/utils/route-regex'
+  import { getMetadataRouteSuffix } from 'next/dist/lib/metadata/get-metadata-route'
 
   export default (props) => {
     const pathname = ${JSON.stringify(route)}
     const routeRegex = getNamedRouteRegex(pathname, false)
+    const segment = ${JSON.stringify(segment)}
     const route = interpolateDynamicPath(pathname, props.params, routeRegex)
+    const suffix = getMetadataRouteSuffix(segment)
+    const routeSuffix = suffix ? \`-\${suffix}\` : ''
+    const { name, ext } = path.parse(${JSON.stringify(pageRoute)})
 
     const imageData = ${JSON.stringify(imageData)};
 
     return {
       ...imageData,
-      url: path.join(route, ${JSON.stringify(pageRoute)} + ${JSON.stringify(
-    hashQuery
-  )}),
+      url: path.join(route, name + routeSuffix + ext + ${JSON.stringify(
+        hashQuery
+      )}),
     }
   }`
 }

test/e2e/app-dir/metadata-dynamic-routes/app/(group)/static/page.tsx
@@ -1,0 +1,3 @@
+export default function page() {
+  return <>static image</>
+}

test/e2e/app-dir/metadata-dynamic-routes/index.test.ts
@@ -137,6 +137,15 @@ createNextDescribe(
         expect([sizeBig.width, sizeBig.height]).toEqual([1200, 630])
         expect([sizeSmall.width, sizeSmall.height]).toEqual([600, 315])
       })
+
+      it('should fill params into routes groups url of static images', async () => {
+        const $ = await next.render$('/static')
+        const ogImageUrl = $('meta[property="og:image"]').attr('content')
+        expect(ogImageUrl).toMatch(hashRegex)
+        expect(ogImageUrl).toMatch('/static/opengraph-image')
+        // should already normalize the parallel routes segment to url
+        expect(ogImageUrl).not.toContain('(group)')
+      })
     })
 
     describe('icon image routes', () => {

commit 05d486f68898881ed1a1defed21cf7c708d2d1e8

Fix Windows path in route join (#48603)
A change here: (#48202) 958150d
Caused a URL to be joined with `path.join`, which on Windows inserts a
backslash character. Changing to `path.posix.join` fixes this.

Breaks next build on Windows when paths like `favicon.ico` are in
the source dir.

packages/next/src/lib/metadata/get-metadata-route.ts
@@ -56,7 +56,7 @@ export function normalizeMetadataRoute(page: string) {
         page.startsWith('/manifest') ||
         isStaticMetadataFile
 
-      route = path.join(
+      route = path.posix.join(
         dir,
         `${baseName}${suffix ? `-${suffix}` : ''}${ext}`,
         isSingleRoute ? '' : '[[...__metadata_id__]]',