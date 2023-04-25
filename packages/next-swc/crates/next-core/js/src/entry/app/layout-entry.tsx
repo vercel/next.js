6298e683a3ae325229605e6fb72b7e5afa21bc97
Shu Ding <g@shud.in>
2023-04-25 16:26:22 +0200

    revert change in next-swc

--- packages/next-swc/crates/next-core/js/src/entry/app/layout-entry.tsx
+++ packages/next-swc/crates/next-core/js/src/entry/app/layout-entry.tsx
@@ -3,7 +3,6 @@ export { default as LayoutRouter } from 'next/dist/client/components/layout-rout
 export { default as RenderFromTemplateContext } from 'next/dist/client/components/render-from-template-context.js'
 export { default as GlobalError } from 'next/dist/client/components/error-boundary.js'
 export { staticGenerationAsyncStorage } from 'next/dist/client/components/static-generation-async-storage.js'
-export { actionAsyncStorage } from 'next/dist/client/components/action-async-storage.js'
 export { requestAsyncStorage } from 'next/dist/client/components/request-async-storage.js'
 import * as serverHooks from 'next/dist/client/components/hooks-server-context.js'
 export { serverHooks }
