9556fe7599778e8ba890ae4ec9cd77d528b0504c
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-04-27 19:11:00 -0700

    refactor(next-core): consolidate swc_relay

--- packages/next-swc/crates/next-core/src/next_shared/transforms/relay.rs
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/relay.rs
@@ -1,93 +1,15 @@
 use std::path::PathBuf;
 
 use anyhow::Result;
-use async_trait::async_trait;
-use swc_core::{
-    common::{util::take::Take, FileName},
-    ecma::{
-        ast::{Module, Program},
-        transforms::base::resolver,
-        visit::{FoldWith, VisitMutWith},
-    },
-};
-use swc_relay::RelayLanguageConfig;
 use turbo_binding::{
-    turbo::tasks_fs::FileSystem,
-    turbopack::ecmascript::{
-        CustomTransformer, OptionTransformPluginVc, TransformContext, TransformPluginVc,
+    swc::custom_transform::relay::{Config, RelayLanguageConfig},
+    turbopack::{
+        ecmascript::{OptionTransformPluginVc, TransformPluginVc},
+        ecmascript_plugin::transform::relay::RelayTransformer,
     },
 };
 
-use crate::next_config::{NextConfigVc, RelayConfig, RelayLanguage};
-
-#[derive(Debug)]
-pub struct RelayTransformer {
-    config: swc_relay::Config,
-}
-
-impl RelayTransformer {
-    pub fn new(config: &RelayConfig) -> Self {
-        // [TODO]: There are config mismatches between RelayConfig to swc_relay::Config
-        let swc_relay_config = swc_relay::Config {
-            artifact_directory: config.artifact_directory.as_ref().map(PathBuf::from),
-            language: config.language.as_ref().map_or(
-                RelayLanguageConfig::TypeScript,
-                |v| match v {
-                    RelayLanguage::JavaScript => RelayLanguageConfig::JavaScript,
-                    RelayLanguage::TypeScript => RelayLanguageConfig::TypeScript,
-                    RelayLanguage::Flow => RelayLanguageConfig::Flow,
-                },
-            ),
-            ..Default::default()
-        };
-
-        Self {
-            config: swc_relay_config,
-        }
-    }
-}
-
-#[async_trait]
-impl CustomTransformer for RelayTransformer {
-    async fn transform(
-        &self,
-        program: &mut Program,
-        ctx: &TransformContext<'_>,
-    ) -> Option<Program> {
-        let root = if let Ok(file_path) = ctx.file_path.await {
-            file_path
-                .fs
-                .root()
-                .await
-                .map_or(PathBuf::new(), |v| PathBuf::from(v.path.to_string()))
-        } else {
-            PathBuf::new()
-        };
-
-        let (root, config) = if self.config.artifact_directory.is_some() {
-            (root, None)
-        } else {
-            let config = swc_relay::Config {
-                artifact_directory: Some(PathBuf::from("__generated__")),
-                ..self.config
-            };
-            (PathBuf::from("."), Some(config))
-        };
-
-        let p = std::mem::replace(program, Program::Module(Module::dummy()));
-        *program = p.fold_with(&mut swc_relay::relay(
-            config.as_ref().unwrap_or_else(|| &self.config),
-            FileName::Real(PathBuf::from(ctx.file_name_str)),
-            root,
-            // [TODO]: pages_dir comes through next-swc-loader
-            // https://github.com/vercel/next.js/blob/ea472e8058faea8ebdab2ef6d3aab257a1f0d11c/packages/next/src/build/webpack-config.ts#L792
-            None,
-            Some(ctx.unresolved_mark),
-        ));
-
-        None
-    }
-}
+use crate::next_config::{NextConfigVc, RelayLanguage};
 
 /// Returns a transform plugin for the relay graphql transform.
 #[turbo_tasks::function]
@@ -102,9 +24,22 @@ pub async fn get_relay_transform_plugin(
             value
                 .relay
                 .as_ref()
-                .map(|value| {
+                .map(|config| {
+                    // [TODO]: There are config mismatches between RelayConfig to swc_relay::Config
+                    let swc_relay_config = Config {
+                        artifact_directory: config.artifact_directory.as_ref().map(PathBuf::from),
+                        language: config.language.as_ref().map_or(
+                            RelayLanguageConfig::TypeScript,
+                            |v| match v {
+                                RelayLanguage::JavaScript => RelayLanguageConfig::JavaScript,
+                                RelayLanguage::TypeScript => RelayLanguageConfig::TypeScript,
+                                RelayLanguage::Flow => RelayLanguageConfig::Flow,
+                            },
+                        ),
+                        ..Default::default()
+                    };
                     OptionTransformPluginVc::cell(Some(TransformPluginVc::cell(Box::new(
-                        RelayTransformer::new(value),
+                        RelayTransformer::new(swc_relay_config),
                     ))))
                 })
                 .unwrap_or(Default::default())
