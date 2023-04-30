ad21ee8283a726f908d1e5a2867a58897a9db806
Shu Ding <g@shud.in>
2023-04-30 19:57:10 +0200

    fix behavior

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -394,8 +394,11 @@ export async function renderToHTMLOrFlight(
             // During HMR, it's critical to use different `precedence` values
             // for different stylesheets, so their order will be kept.
             // https://github.com/facebook/react/pull/25060
-            const precedence =
-              process.env.NODE_ENV === 'development' ? 'next_' + href : 'next'
+            const precedence = shouldPreload
+              ? process.env.NODE_ENV === 'development'
+                ? 'next_' + href
+                : 'next'
+              : undefined
 
             return (
               <link
