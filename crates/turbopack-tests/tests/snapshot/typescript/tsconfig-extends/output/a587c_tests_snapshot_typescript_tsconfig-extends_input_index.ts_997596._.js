2dc5a34efdf70fcd21c15822f7046aa98960ea5a
Justin Ridgewell <justin@ridgewell.name>
2023-05-01 04:47:22 -0400

    Match TS's extends resolution algorithm (vercel/turbo#4754)
    ### Description
    
    Implements TS's `extends` [resolution
    algorithm](https://github.com/microsoft/TypeScript/blob/611a912d/src/compiler/commandLineParser.ts#L3294-L3326).
    In particular, there are a few cases that need to be properly handled:
    
    - Relative paths: if the file doesn't exist and the path doesn't end in
    `.json`, try again with `.json` extension
    - Windows paths: same thing
    - Posix paths: same thing
    - `.` and `..` (but not `./` nor `../` because they're relative paths):
    join with `/tsconfig.json` before looking up
    - `foo` module import: join with `/tsconfig.json` before looking up
    
    ### Testing Instructions
    
    Snapshot tests
    
    Fixes WEB-963
    fix vercel/turbo#2682
    
    ---------
    
    Co-authored-by: Tobias Koppers <tobias.koppers@googlemail.com>
    Co-authored-by: Alex Kirszenberg <alex.kirszenberg@vercel.com>

---
+++ crates/turbopack-tests/tests/snapshot/typescript/tsconfig-extends/output/a587c_tests_snapshot_typescript_tsconfig-extends_input_index.ts_997596._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/a587c_tests_snapshot_typescript_tsconfig-extends_input_index.ts_997596._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "output/a587c_tests_snapshot_typescript_tsconfig-extends_input_index.ts_997596._.js",
+  "chunks": [
+    "output/a587c_tests_snapshot_typescript_tsconfig-extends_input_index.ts_b53fce._.js"
+  ],
+  "source": "entry"
+});
