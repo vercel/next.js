cefd3e6e34f1abfc7141320bca22a5a8e573dd67
eps1lon <silbermann.sebastian@gmail.com>
2023-05-01 20:08:05 +0200

    Ensure latest react-dom types for experimental release channel are used

--- packages/next/types/index.d.ts
+++ packages/next/types/index.d.ts
@@ -2,6 +2,7 @@
 /// <reference types="react" />
 /// <reference types="react/experimental" />
 /// <reference types="react-dom" />
+/// <reference types="react-dom/experimental" />
 
 import React from 'react'
 import { ParsedUrlQuery } from 'querystring'
