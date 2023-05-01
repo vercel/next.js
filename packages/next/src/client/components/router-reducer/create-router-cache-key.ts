b998eb98d6aa3d0d67224318e08363484a118956
Jimmy Lai <laijimmy0@gmail.com>
2023-05-01 18:18:49 +0200

    Update packages/next/src/client/components/router-reducer/create-router-cache-key.ts
    Co-authored-by: Tim Neutkens <tim@timneutkens.nl>

--- packages/next/src/client/components/router-reducer/create-router-cache-key.ts
+++ packages/next/src/client/components/router-reducer/create-router-cache-key.ts
@@ -2,7 +2,7 @@ import { Segment } from '../../../server/app-render/types'
 
 export function createRouterCacheKey(
   segment: Segment,
-  stripSearchParameters: boolean = false
+  withoutSearchParameters: boolean = false
 ) {
   return Array.isArray(segment)
     ? `${segment[0]}|${segment[1]}|${segment[2]}`
