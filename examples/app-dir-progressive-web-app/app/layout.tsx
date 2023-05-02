09602b755252e6d0f6fc467f6c24ec20279795f6
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-05-02 03:10:49 +0000

    fix(examples/app-dir-progressive-web-app): addlayout children type

--- examples/app-dir-progressive-web-app/app/layout.tsx
+++ examples/app-dir-progressive-web-app/app/layout.tsx
@@ -1,8 +1,12 @@
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  );
+  )
 }
