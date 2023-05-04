7f6e8dbeed1651667e6167975d59a99f2b3f5385
Ivan Kryvorotenko <ikryvorotenko@gmail.com>
2023-05-04 21:37:29 +0200

    Support incrementalCacheHandlerPath for standalone output (#48694)
    <!-- Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change(s) that you're making:
    
    ## For Contributors
    
    ### Improving Documentation or adding/fixing Examples
    
    - The "examples guidelines" are followed from our contributing doc
    https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
    - Make sure the linting passes by running `pnpm build && pnpm lint`. See
    https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md
    
    ### Fixing a bug
    
    - Related issues linked using `fixes #number`
    - Tests added. See:
    https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    ### Adding a feature
    
    - Implements an existing feature request or RFC. Make sure the feature
    request has been accepted for implementation before opening a PR. (A
    discussion must be opened, see
    https://github.com/vercel/next.js/discussions/new?category=ideas)
    - Related issues/discussions are linked using `fixes #number`
    - e2e tests added
    (https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Documentation added
    - Telemetry added. In case of a feature if it's used or not.
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    
    
    ## For Maintainers
    
    - Minimal description (aim for explaining to someone not on the team to
    understand the PR)
    - When linking to a Slack thread, you might want to share details of the
    conclusion
    - Link both the Linear (Fixes NEXT-xxx) and the GitHub issues
    - Add review comments if necessary to explain to the reviewer the logic
    behind a change
    
    
    -->
    ### What?
    This is a follow-up to https://github.com/vercel/next.js/pull/46290 by
    @ijjk
    
    Currently, nextjs build resolves provided incrementalCacheHandlerPath
    over destDir (.next by default). And for standalone builds the custom
    cache can't be resolved and leads to runtime error.
    
    ### Why?
    This fix basically introduces support of incrementalCacheHandlerPath for
    the standalone build.
    
    ### How?
    incrementalCacheHandlerPath is now always resolved relatively to
    distDir, which should work for both default and standalone builds.
    
    Also, for convenience (and better testability)
    incrementalCacheHandlerPath can now be provided as a relative path to
    the project directory.

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -30,7 +30,7 @@ import type { MiddlewareRouteMatch } from '../shared/lib/router/utils/middleware
 import type { RouteMatch } from './future/route-matches/route-match'
 
 import fs from 'fs'
-import { join, relative, resolve, sep } from 'path'
+import { join, relative, resolve, sep, isAbsolute } from 'path'
 import { IncomingMessage, ServerResponse } from 'http'
 import { addRequestMeta, getRequestMeta } from './request-meta'
 import {
@@ -385,9 +385,9 @@ export default class NextNodeServer extends BaseServer {
     const { incrementalCacheHandlerPath } = this.nextConfig.experimental
 
     if (incrementalCacheHandlerPath) {
-      CacheHandler = require(this.minimalMode
-        ? join(this.distDir, incrementalCacheHandlerPath)
-        : incrementalCacheHandlerPath)
+      CacheHandler = require(isAbsolute(incrementalCacheHandlerPath)
+        ? incrementalCacheHandlerPath
+        : join(this.distDir, incrementalCacheHandlerPath))
       CacheHandler = CacheHandler.default || CacheHandler
     }
 
