43c8c1bb9a471aa0a6340cb44d47d5f1b0d4216e
JJ Kasper <jj@jjsweb.site>
2023-05-04 14:24:40 -0700

    cleanup more

--- test/development/app-hmr/next.config.js
+++ test/development/app-hmr/next.config.js
@@ -1,6 +1,4 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-  experimental: {},
-}
+module.exports = {}
--- test/e2e/app-dir/dynamic/next.config.js
+++ test/e2e/app-dir/dynamic/next.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  experimental: {},
 }
