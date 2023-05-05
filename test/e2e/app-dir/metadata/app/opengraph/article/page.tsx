00d35f55ab33d1c9fe4a954385d2938aa92624a2
Jiachi Liu <inbox@huozhi.im>
2023-05-06 01:20:52 +0200

    fix test

--- test/e2e/app-dir/metadata/app/opengraph/article/page.tsx
+++ test/e2e/app-dir/metadata/app/opengraph/article/page.tsx
@@ -6,6 +6,7 @@ export async function generateMetadata(_props, parentResolvingMetadata) {
   const parentMetadata = await parentResolvingMetadata
   return {
     openGraph: {
+      ...parentMetadata.openGraph,
       title: `My custom title | ${parentMetadata.openGraph.title.absolute}`,
       // merging parent images URL instance should work
       images: [...parentMetadata.openGraph.images],
