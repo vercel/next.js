75eccf7c7f64ef158220db47c35d2aa101288513
Leah <github.leah@hrmny.sh>
2023-05-05 19:33:30 +0200

    make sure server component externals only apply to files resolvable by node (#49147)
    Fixes WEB-959

--- packages/next-swc/crates/next-core/src/next_import_map.rs
+++ packages/next-swc/crates/next-core/src/next_import_map.rs
@@ -232,10 +232,6 @@ pub async fn get_next_server_import_map(
                 request_to_import_mapping(project_path, "next/dist/shared/lib/app-dynamic"),
             );
 
-            for name in next_config.server_component_externals().await?.iter() {
-                import_map.insert_exact_alias(name, external);
-                import_map.insert_wildcard_alias(format!("{name}/"), external);
-            }
             // The sandbox can't be bundled and needs to be external
             import_map.insert_exact_alias("next/dist/server/web/sandbox", external);
         }
