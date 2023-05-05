c881224eaec1f3aa5aca41b1c5a8e7837b148393
JJ Kasper <jj@jjsweb.site>
2023-05-05 00:05:51 -0700

    Add experimental verbose logging flag (#49250)
    Only shows fetch request logs by default and allows opting into verbose
    request timings via experimental config.
    
    x-ref: [slack
    thread](https://vercel.slack.com/archives/C03KAR5DCKC/p1683236293282549)

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -1789,7 +1789,7 @@ export default class NextNodeServer extends BaseServer {
               )
             }
             process.stdout.write('\n')
-          } else {
+          } else if (this.nextConfig.experimental.logging === 'verbose') {
             process.stdout.write(
               `- ${chalk.cyan(req.method || 'GET')} ${req.url} ${
                 res.statusCode
