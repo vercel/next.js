32bf90616b935ee2c6d29a2cb2d5b20c86c9ec16
Shu Ding <g@shud.in>
2023-05-05 21:37:25 +0200

    extend disable server react-dom apis

--- packages/next-swc/crates/core/tests/errors/react-server-components/server-graph/react-dom-api/output.js
+++ packages/next-swc/crates/core/tests/errors/react-server-components/server-graph/react-dom-api/output.js
@@ -1,4 +1,5 @@
 import { findDOMNode, flushSync, unstable_batchedUpdates } from 'react-dom';
+import { experimental_useOptimistic as useOptimistic, experimental_useFormStatus } from 'react-dom';
 export default function() {
     return null;
 }
