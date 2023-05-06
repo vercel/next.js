1edc343ce42bd4e989c1911f8ae02dfe995aa74d
remorses <beats.by.morse@gmail.com>
2023-05-06 13:07:59 +0200

    remove trim

--- test/e2e/app-dir/dynamic-params-react-key/layout-params.test.ts
+++ test/e2e/app-dir/dynamic-params-react-key/layout-params.test.ts
@@ -10,17 +10,15 @@ createNextDescribe(
     it('should keep the React router instance the same when changing dynamic params', async () => {
       const browser = await next.browser('/en')
 
-      const params = await browser.waitForElementByCss('#params').text().trim()
+      const params = await browser.waitForElementByCss('#params').text()
 
       await browser.elementByCss('#increment').click()
       await browser.elementByCss('#increment').click()
 
       await browser.elementByCss('#push').click()
       await check(async () => {
-        const newParams = await browser
-          .waitForElementByCss('#params')
-          .text()
-          .trim()
+        const newParams = await browser.waitForElementByCss('#params').text()
+
         return newParams !== params ? 'success' : 'retry'
       }, 'success')
       await check(async () => {
