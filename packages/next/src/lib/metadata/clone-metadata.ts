05d24bb2d1e3b78b10067847601e8d09551495cb
Jiachi Liu <inbox@huozhi.im>
2023-05-08 17:53:48 +0200

    use string token

--- packages/next/src/lib/metadata/clone-metadata.ts
+++ packages/next/src/lib/metadata/clone-metadata.ts
@@ -1,17 +1,17 @@
 import type { ResolvedMetadata } from './types/metadata-interface'
 
-const SYM_URL = Symbol('METADATA_URL')
+const TYPE_URL = '__METADATA_URL'
 
 function replacer(_key: string, val: any) {
   // clone URL as string but recover it as URL
   if (val instanceof URL) {
-    return { _type: SYM_URL, value: val.href }
+    return { _type: TYPE_URL, value: val.href }
   }
   return val
 }
 
 function reviver(_key: string, val: any) {
-  if (typeof val === 'object' && val !== null && val._type === SYM_URL) {
+  if (typeof val === 'object' && val !== null && val._type === TYPE_URL) {
     return new URL(val.value)
   }
   return val
