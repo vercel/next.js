71d8064b3d048454c620796bc4b80b02bf183f17
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-05-09 11:09:34 -0700

    feat(next-core): relay transform plugin (#48899)
    <!-- Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change(s) that you're making:
    
    ## For Contributors
    
    ### Improving Documentation or adding/fixing Examples
    
    - The "examples guidelines" are followed from our contributing doc
    https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
    - Make sure the linting passes by running `pnpm build && pnpm lint`. See
    https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md
    
    ### Fixing a bug
    
    - Related issues linked using `fixes #number`
    - Tests added. See:
    https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    ### Adding a feature
    
    - Implements an existing feature request or RFC. Make sure the feature
    request has been accepted for implementation before opening a PR. (A
    discussion must be opened, see
    https://github.com/vercel/next.js/discussions/new?category=ideas)
    - Related issues/discussions are linked using `fixes #number`
    - e2e tests added
    (https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Documentation added
    - Telemetry added. In case of a feature if it's used or not.
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    
    
    ## For Maintainers
    
    - Minimal description (aim for explaining to someone not on the team to
    understand the PR)
    - When linking to a Slack thread, you might want to share details of the
    conclusion
    - Link both the Linear (Fixes NEXT-xxx) and the GitHub issues
    - Add review comments if necessary to explain to the reviewer the logic
    behind a change
    
    ### Why?
    
    ### How?
    
    Closes NEXT-
    Fixes #
    
    -->
    
    #### What
    Part 1 for WEB-848. 
    
    This PR implements initial path to enable relay compiler option in
    Turbopack. PR is based on approach WEB-955, that passing custom plugin
    transform itself instead of trying to pass down configuration into
    turbopack. In result, this PR requires counterpart turbopack PR at
    https://github.com/vercel/turbo/pull/4721.
    
    PR also refactors next-shared's transform bit, as we have grown number
    of custom transforms.
    
    Unfortunately there are some runtime errors with this transforms, so it
    is not possible to use relay actually yet. WEB-956 tracks this.
    (https://github.com/swc-project/plugins/pull/179)
    
    ---------
    
    Co-authored-by: Tobias Koppers <tobias.koppers@googlemail.com>
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- packages/next/src/lib/turbopack-warning.ts
+++ packages/next/src/lib/turbopack-warning.ts
@@ -8,6 +8,7 @@ const supportedTurbopackNextConfigOptions = [
   'env',
   'modularizeImports',
   'compiler.emotion',
+  'compiler.relay',
   'compiler.styledComponents',
   'images',
   'pageExtensions',
