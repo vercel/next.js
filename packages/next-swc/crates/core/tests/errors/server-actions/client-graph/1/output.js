000d022b4e83b7e9022af085911abd9bbf85f7e2
Shu Ding <g@shud.in>
2023-05-09 19:05:20 +0200

    Support `.bind` syntax with Action functions (#49422)
    This PR implements initial support for `.bind` syntax for inline actions
    (not top-level `"use server"` files yet). For example:
    
    ```js
    export default function Page({ foo, x, y }) {
      async function action(a, b, c, d) {
        'use server'
        console.log(a, b, c, d)
      }
    
      return <ClientComp action={action.bind(null, foo[0], foo[1], foo.x, foo[y])} />
    }
    ```
    
    Is now valid.
    
    Fixes NEXT-1109.

--- packages/next-swc/crates/core/tests/errors/server-actions/client-graph/1/output.js
+++ packages/next-swc/crates/core/tests/errors/server-actions/client-graph/1/output.js
@@ -1,10 +1,9 @@
-/* __next_internal_client_entry_do_not_use__ default auto */ /* __next_internal_action_entry_do_not_use__ $$ACTION_0 */ export default function App() {
-    async function fn() {
-        return $$ACTION_0(fn.$$bound);
+/* __next_internal_client_entry_do_not_use__ default auto */ /* __next_internal_action_entry_do_not_use__ $$ACTION_0 */ import __create_action_proxy__ from "private-next-rsc-action-proxy";
+export default function App() {
+    async function fn(...args) {
+        return $$ACTION_0.apply(null, (fn.$$bound || []).concat(args));
     }
-    fn.$$typeof = Symbol.for("react.server.reference");
-    fn.$$id = "6d53ce510b2e36499b8f56038817b9bad86cabb4";
-    fn.$$bound = null;
+    __create_action_proxy__("6d53ce510b2e36499b8f56038817b9bad86cabb4", null, fn, $$ACTION_0);
     return <div>App</div>;
 }
-export async function $$ACTION_0(closure) {}
+export async function $$ACTION_0() {}
--- packages/next-swc/crates/core/tests/errors/server-actions/server-graph/3/output.js
+++ packages/next-swc/crates/core/tests/errors/server-actions/server-graph/3/output.js
@@ -1,9 +1,7 @@
-/* __next_internal_action_entry_do_not_use__ x */ export const x = 1;
-import ensureServerEntryExports from "private-next-rsc-action-proxy";
+/* __next_internal_action_entry_do_not_use__ x */ import __create_action_proxy__ from "private-next-rsc-action-proxy";
+export const x = 1;
+import ensureServerEntryExports from "private-next-rsc-action-validate";
 ensureServerEntryExports([
     x
 ]);
-x.$$typeof = Symbol.for("react.server.reference");
-x.$$id = "b78c261f135a7a852508c2920bd7228020ff4bd7";
-x.$$bound = null;
-x.$$with_bound = false;
+__create_action_proxy__("b78c261f135a7a852508c2920bd7228020ff4bd7", null, x);
