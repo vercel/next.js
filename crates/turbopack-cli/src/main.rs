cb24d435a690d1ca6b3e3e8dcdf98b132618aa0b
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-05-10 14:46:54 +0200

    Add support for logging events and intervals in the macOS profiler (vercel/turbo#4636)
    ### Description
    
    Surprisingly, I couldn't find an adequate out-of-the-box crate for
    adding signposts to the macOS profiler. I had to dig in to semi-private
    macOS APIs to add this capability.
    
    Here's what signposts look like in the profiler view:
    
    
    ![image](https://user-images.githubusercontent.com/1621758/233111302-8257f996-df62-4443-a3a8-435651e7b623.png)
    
    ### Testing Instructions
    
    N/A
    
    link WEB-931

--- crates/turbopack-cli/src/main.rs
+++ crates/turbopack-cli/src/main.rs
@@ -3,6 +3,7 @@
 
 use anyhow::Result;
 use clap::Parser;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
 use turbopack_cli::{arguments::Arguments, register};
 
 #[global_allocator]
@@ -10,6 +11,18 @@ static ALLOC: turbo_malloc::TurboMalloc = turbo_malloc::TurboMalloc;
 
 fn main() {
     use turbo_malloc::TurboMalloc;
+
+    let subscriber = Registry::default();
+
+    #[cfg(target_os = "macos")]
+    let subscriber = subscriber.with(tracing_signpost::SignpostLayer::new());
+
+    let stdout_log = tracing_subscriber::fmt::layer().pretty();
+    let subscriber = subscriber.with(stdout_log);
+
+    let subscriber = subscriber.with(EnvFilter::from_default_env());
+
+    subscriber.init();
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
