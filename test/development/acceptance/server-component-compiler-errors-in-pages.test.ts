7eb8b4b6fedd897cb467b3ea327f3f2935e479a3
JJ Kasper <jj@jjsweb.site>
2023-05-11 10:23:11 -0700

    update error link test

--- test/development/acceptance/server-component-compiler-errors-in-pages.test.ts
+++ test/development/acceptance/server-component-compiler-errors-in-pages.test.ts
@@ -52,7 +52,7 @@ createNextDescribe(
         "./components/Comp.js
         ReactServerComponentsError:
 
-        You're importing a component that needs next/headers. That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/app/api-reference/functions/headers
+        You're importing a component that needs next/headers. That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/getting-started/react-essentials#server-components
 
            ,-[1:1]
          1 | 
