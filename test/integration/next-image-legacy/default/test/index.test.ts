94fb6594b1d7ed46b14360ceaa6f587eb92a3590
Fonger <5862369+Fonger@users.noreply.github.com>
2023-05-12 18:25:52 +0800

    test(next-image-legacy): correct test case

--- test/integration/next-image-legacy/default/test/index.test.ts
+++ test/integration/next-image-legacy/default/test/index.test.ts
@@ -132,6 +132,11 @@ function runTests(mode) {
             '/_next/image?url=%2Ftest.jpg&w=640&q=75 1x, /_next/image?url=%2Ftest.jpg&w=828&q=75 2x',
         },
         {
+          imagesizes: '',
+          imagesrcset:
+            '/_next/image?url=%2Ftest.png&w=640&q=75 1x, /_next/image?url=%2Ftest.png&w=828&q=75 2x',
+        },
+        {
           imagesizes: '100vw',
           imagesrcset:
             '/_next/image?url=%2Fwide.png&w=640&q=75 640w, /_next/image?url=%2Fwide.png&w=750&q=75 750w, /_next/image?url=%2Fwide.png&w=828&q=75 828w, /_next/image?url=%2Fwide.png&w=1080&q=75 1080w, /_next/image?url=%2Fwide.png&w=1200&q=75 1200w, /_next/image?url=%2Fwide.png&w=1920&q=75 1920w, /_next/image?url=%2Fwide.png&w=2048&q=75 2048w, /_next/image?url=%2Fwide.png&w=3840&q=75 3840w',
