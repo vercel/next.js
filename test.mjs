88a677335c94a42267bc84390c5f423fddd37809
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-05-12 15:17:28 -0600

    fix: deleted test file

--- test.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
-
-const metadata = {
-  'Content-Type': 'text/plain',
-}
-
-await fs.writeFile(path.join('test.txt'), metadata, 'utf-8')
