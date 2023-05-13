8ad61d0943520aeb2f1b978c95a6355c4ee3d561
JJ Kasper <jj@jjsweb.site>
2023-05-13 00:55:51 -0700

    update warning

--- packages/next/src/shared/lib/bloom-filter.ts
+++ packages/next/src/shared/lib/bloom-filter.ts
@@ -25,21 +25,6 @@ export class BloomFilter {
     )
     this.numHashes = Math.ceil((this.numBits / numItems) * Math.log(2))
     this.bitArray = new Array(this.numBits).fill(0)
-
-    if (typeof window === 'undefined') {
-      if (errorRate < 0.01) {
-        const filterData = JSON.stringify(this.export())
-        const gzipSize = require('next/dist/compiled/gzip-size').sync(
-          filterData
-        )
-
-        if (gzipSize > 1024) {
-          console.warn(
-            `Creating filter with error rate less than 1% (0.01) can increase the size dramatically proceed with caution. Received error rate ${errorRate} resulted in size ${gzipSize} bytes (gzip)`
-          )
-        }
-      }
-    }
   }
 
   static from(items: string[], errorRate = 0.01) {
@@ -52,13 +37,30 @@ export class BloomFilter {
   }
 
   export() {
-    return {
+    const data = {
       numItems: this.numItems,
       errorRate: this.errorRate,
       numBits: this.numBits,
       numHashes: this.numHashes,
       bitArray: this.bitArray,
     }
+
+    if (typeof window === 'undefined' && process.env.NEXT_RUNTIME !== 'edge') {
+      if (this.errorRate < 0.01) {
+        const filterData = JSON.stringify(data)
+        const gzipSize = require('next/dist/compiled/gzip-size').sync(
+          filterData
+        )
+
+        if (gzipSize > 1024) {
+          console.warn(
+            `Creating filter with error rate less than 1% (0.01) can increase the size dramatically proceed with caution. Received error rate ${this.errorRate} resulted in size ${filterData.length} bytes, ${gzipSize} bytes (gzip)`
+          )
+        }
+      }
+    }
+
+    return data
   }
 
   import(data: ReturnType<typeof this['export']>) {
