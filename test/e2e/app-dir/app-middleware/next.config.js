5147fd68925aad47a9bf0d88336cda80285babab
Khue Nguyen <khuezy.nguyen@gmail.com>
2023-05-15 09:52:58 -0700

    add top level await

--- test/e2e/app-dir/app-middleware/next.config.js
+++ test/e2e/app-dir/app-middleware/next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
   experimental: {
     clientRouterFilter: true,
     clientRouterFilterRedirects: true,
