af9371391070ae9fa2585d2824150d68e4a894ae
Nicolas Figueroa <figueroanicolas.a@gmail.com>
2023-05-16 16:15:27 -0400

    feat(with-facebook-pixel): add links between pages for testing pixel propouse

--- examples/with-facebook-pixel/app/about/page.js
+++ examples/with-facebook-pixel/app/about/page.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <>
       <h1>About</h1>
+
+      <Link href="/">
+        back to home
+      </Link>
     </>
   );
 }
