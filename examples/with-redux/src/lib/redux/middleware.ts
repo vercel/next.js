46c7c17c0ca1d6c0a2c16e5bca6d642c50485175
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2023-05-17 00:07:06 +0300

    🐛 fix(examples/with-redux): change redux-logger predicate to check if window is defined instead of process.browser to fix logger not working in server-side rendering

--- examples/with-redux/src/lib/redux/middleware.ts
+++ examples/with-redux/src/lib/redux/middleware.ts
@@ -4,7 +4,6 @@ import { Middleware } from '@reduxjs/toolkit'
 const middleware: Middleware[] = []
 
 if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line
   const { createLogger } = require('redux-logger')
 
   const logger = createLogger({
@@ -18,7 +17,7 @@ if (process.env.NODE_ENV === 'development') {
       nextState: () => '#A47104',
       error: () => '#ff0005',
     },
-    predicate: () => process.browser,
+    predicate: () => typeof window !== 'undefined',
   })
 
   middleware.push(logger)
