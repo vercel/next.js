df0c6e08c1d213e386f47c1552e22e20448a686c
Nicolas Figueroa <figueroanicolas.a@gmail.com>
2023-05-16 16:32:02 -0400

    fix(with-facebook-pixel): add fbq to window in public script

--- examples/with-facebook-pixel/public/scripts/pixel.js
+++ examples/with-facebook-pixel/public/scripts/pixel.js
@@ -8,4 +8,4 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', PIXEL_ID);
+window.fbq('init', PIXEL_ID);
