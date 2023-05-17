5bba0cd18d175a4bfeb9f599ee4b42893d845bf5
Ashish Rawat <eashish93@gmail.com>
2023-05-17 18:22:45 +0530

    simplified mongodb initialization.

--- examples/with-mongodb/pages/index.tsx
+++ examples/with-mongodb/pages/index.tsx
@@ -1,18 +1,12 @@
 import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
+import db from '../lib/mongodb'
 import { InferGetServerSidePropsType } from 'next'
 
 export async function getServerSideProps(context) {
   try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
+    
+    // First call to `db` with await will invoke `connect()` automatically.
+    // await db.find({}) or any of the MongoDB Node Driver commands
 
     return {
       props: { isConnected: true },
