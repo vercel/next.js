7e586a773a8d8f0753565c1a88a67858e7dcc786
Jiachi Liu <inbox@huozhi.im>
2023-05-17 22:04:39 +0200

    use await require

--- packages/next/src/server/future/helpers/module-loader/node-module-loader.ts
+++ packages/next/src/server/future/helpers/module-loader/node-module-loader.ts
@@ -1,17 +1,13 @@
-import { interopDefault } from '../../../../lib/interop-default'
 import { ModuleLoader } from './module-loader'
 
 /**
- * Loads a module using `require(id)`.
+ * Loads a module using `await require(id)`.
  */
 export class NodeModuleLoader implements ModuleLoader {
   public async load<M>(id: string): Promise<M> {
     if (process.env.NEXT_RUNTIME !== 'edge') {
-      // Use dynamic import to cover the case that route is marked ESM modules by ESM escalation.
-      const mod = await import(/* webpackMode: "eager" */ id).then((m) =>
-        interopDefault(m)
-      )
-      return mod
+      // Need to `await` to cover the case that route is marked ESM modules by ESM escalation.
+      return await require(id)
     }
 
     throw new Error('NodeModuleLoader is not supported in edge runtime.')
