75c962f3934e9fe13c3995ff416e690daf88b85d
Nicholas Yang <nicholas.yang@vercel.com>
2023-05-17 10:25:15 -0400

    chore: Standardize turborepo prefix (vercel/turbo#4982)
    ## Description
    
    To make filtering for CI and other tasks easier, we're prefixing all
    turborepo crates with turborepo-. That way, filtering can be done as
    crates/turborepo-*.
    
    ## Testing Instructions

--- crates/glob-match/fuzz/fuzz_targets/both_fuzz.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-
-use fuzz_local::Data;
-use glob_match::glob_match;
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: Data<'_>| {
-  _ = glob_match(data.pat, data.input);
-});
--- crates/glob-match/fuzz/fuzz_targets/pattern_on_itself.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use glob_match::glob_match;
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &str| {
-  _ = glob_match(data, data);
-});
