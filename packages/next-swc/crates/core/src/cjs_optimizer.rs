2ebe0c2594b358885fda69fa0abb0d7ccba52929
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-18 09:59:44 +0900

    Data

--- packages/next-swc/crates/core/src/cjs_optimizer.rs
+++ packages/next-swc/crates/core/src/cjs_optimizer.rs
@@ -1,1 +1,12 @@
+use turbopack_binding::swc::core::ecma::visit::Visit;
 
+pub struct Optimizer {
+    data: Data,
+}
+
+struct Analyzer<'a> {
+    data: &'a mut Data,
+}
+
+#[derive(Debug, Default)]
+struct Data {}
