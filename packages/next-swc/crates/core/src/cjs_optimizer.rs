ab5a0d6374a3da83b876606e074c81a6a5bd4806
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-18 10:02:01 +0900

    Use visitor

--- packages/next-swc/crates/core/src/cjs_optimizer.rs
+++ packages/next-swc/crates/core/src/cjs_optimizer.rs
@@ -1,6 +1,18 @@
-use turbopack_binding::swc::core::ecma::visit::Visit;
+use turbopack_binding::swc::core::ecma::{
+    ast::{Module, Script},
+    visit::{
+        as_folder, noop_visit_mut_type, noop_visit_type, Fold, Visit, VisitMut, VisitMutWith,
+        VisitWith,
+    },
+};
 
-pub struct Optimizer {
+pub fn cjs_optimizer() -> impl Fold + VisitMut {
+    as_folder(Optimizer {
+        data: Data::default(),
+    })
+}
+
+struct Optimizer {
     data: Data,
 }
 
@@ -10,3 +22,27 @@ struct Analyzer<'a> {
 
 #[derive(Debug, Default)]
 struct Data {}
+
+impl VisitMut for Optimizer {
+    fn visit_mut_module(&mut self, n: &mut Module) {
+        n.visit_with(&mut Analyzer {
+            data: &mut self.data,
+        });
+
+        n.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_script(&mut self, n: &mut Script) {
+        n.visit_with(&mut Analyzer {
+            data: &mut self.data,
+        });
+
+        n.visit_mut_children_with(self);
+    }
+
+    noop_visit_mut_type!();
+}
+
+impl Visit for Analyzer<'_> {
+    noop_visit_type!();
+}
