20ac12d576938942440c33f05db7369fd89314db
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-18 10:08:55 +0900

    config

--- packages/next-swc/crates/core/src/cjs_optimizer.rs
+++ packages/next-swc/crates/core/src/cjs_optimizer.rs
@@ -1,32 +1,48 @@
-use turbopack_binding::swc::core::ecma::{
-    ast::{Module, Script},
-    visit::{
-        as_folder, noop_visit_mut_type, noop_visit_type, Fold, Visit, VisitMut, VisitMutWith,
-        VisitWith,
+use turbopack_binding::swc::core::{
+    common::SyntaxContext,
+    ecma::{
+        ast::{CallExpr, Callee, Expr, Lit, Module, Script, VarDeclarator},
+        visit::{
+            as_folder, noop_visit_mut_type, noop_visit_type, Fold, Visit, VisitMut, VisitMutWith,
+            VisitWith,
+        },
     },
 };
 
-pub fn cjs_optimizer() -> impl Fold + VisitMut {
+pub fn cjs_optimizer(config: Config, unresolved_ctxt: SyntaxContext) -> impl Fold + VisitMut {
     as_folder(Optimizer {
         data: Data::default(),
+        config,
+        unresolved_ctxt,
     })
 }
 
+#[derive(Debug)]
+pub struct Config {}
+
 struct Optimizer {
     data: Data,
+    config: Config,
+    unresolved_ctxt: SyntaxContext,
 }
 
 struct Analyzer<'a> {
     data: &'a mut Data,
+    config: &'a Config,
+    unresolved_ctxt: SyntaxContext,
 }
 
 #[derive(Debug, Default)]
 struct Data {}
 
 impl VisitMut for Optimizer {
+    noop_visit_mut_type!();
+
     fn visit_mut_module(&mut self, n: &mut Module) {
         n.visit_with(&mut Analyzer {
             data: &mut self.data,
+            config: &self.config,
+            unresolved_ctxt: self.unresolved_ctxt,
         });
 
         n.visit_mut_children_with(self);
@@ -35,14 +51,36 @@ impl VisitMut for Optimizer {
     fn visit_mut_script(&mut self, n: &mut Script) {
         n.visit_with(&mut Analyzer {
             data: &mut self.data,
+            config: &self.config,
+            unresolved_ctxt: self.unresolved_ctxt,
         });
 
         n.visit_mut_children_with(self);
     }
-
-    noop_visit_mut_type!();
 }
 
 impl Visit for Analyzer<'_> {
     noop_visit_type!();
+
+    fn visit_var_declarator(&mut self, n: &VarDeclarator) {
+        n.visit_children_with(self);
+
+        // Find `require('foo')`
+        if let Some(Expr::Call(CallExpr {
+            callee: Callee::Expr(callee),
+            args,
+            ..
+        })) = n.init.as_deref()
+        {
+            if let Expr::Ident(ident) = &**callee {
+                if ident.sym == *"require" {
+                    if let Some(arg) = args.get(0) {
+                        if let Expr::Lit(Lit::Str(v)) = &*arg.expr {
+                            //
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
