2e7061534cdf07cad09bcbdc6b5262ec45e46d9f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-19 13:49:06 +0900

    `ImportRecord`

--- packages/next-swc/crates/core/src/cjs_optimizer.rs
+++ packages/next-swc/crates/core/src/cjs_optimizer.rs
@@ -1,5 +1,10 @@
+use convert_case::{Case, Casing};
+use handlebars::{Context, Handlebars, Helper, HelperResult, Output, RenderContext};
 use rustc_hash::FxHashMap;
+use serde::Deserialize;
 use turbopack_binding::swc::core::{
+    bundler::ModuleRecord,
+    cached::regex::CachedRegex,
     common::{SyntaxContext, DUMMY_SP},
     ecma::{
         ast::{
@@ -13,28 +18,73 @@ use turbopack_binding::swc::core::{
 };
 
 pub fn cjs_optimizer(config: Config, unresolved_ctxt: SyntaxContext) -> impl Fold + VisitMut {
-    as_folder(Optimizer {
+    let mut folder = CjsOptimizer {
         data: Data::default(),
-        config,
+        renderer: handlebars::Handlebars::new(),
+        packages: vec![],
         unresolved_ctxt,
-    })
+    };
+    folder
+        .renderer
+        .register_helper("lowerCase", Box::new(helper_lower_case));
+    folder
+        .renderer
+        .register_helper("upperCase", Box::new(helper_upper_case));
+    folder
+        .renderer
+        .register_helper("camelCase", Box::new(helper_camel_case));
+    folder
+        .renderer
+        .register_helper("kebabCase", Box::new(helper_kebab_case));
+    for (mut k, v) in config.packages {
+        // XXX: Should we keep this hack?
+        if !k.starts_with('^') && !k.ends_with('$') {
+            k = format!("^{}$", k);
+        }
+        folder.packages.push((
+            CachedRegex::new(&k).expect("transform-imports: invalid regex"),
+            v,
+        ));
+    }
+
+    as_folder(folder)
 }
 
-#[derive(Debug)]
-pub struct Config {}
+#[derive(Clone, Debug, Deserialize)]
+pub struct Config {
+    pub packages: FxHashMap<String, PackageConfig>,
+}
 
-struct Optimizer {
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PackageConfig {
+    pub transform: String,
+    #[serde(default)]
+    pub prevent_full_import: bool,
+    #[serde(default)]
+    pub skip_default_conversion: bool,
+}
+
+struct Rewriter<'a> {
+    renderer: &'a handlebars::Handlebars<'static>,
+    key: &'a str,
+    config: &'a PackageConfig,
+    group: Vec<&'a str>,
+}
+
+struct CjsOptimizer {
     data: Data,
-    config: Config,
+    renderer: handlebars::Handlebars<'static>,
+    packages: Vec<(CachedRegex, PackageConfig)>,
     unresolved_ctxt: SyntaxContext,
 }
 
 #[derive(Debug, Default)]
 struct Data {
-    /// List of `require` calls
+    /// List of `require` calls **which should be replaced**.
     ///
-    ///  `(identifier): (module_specifier)`
-    imports: FxHashMap<Id, Atom>,
+    ///  `(identifier): (module_record)`
+    imports: FxHashMap<Id, ImportRecord>,
 
     /// `(module_specifier, property): (identifier)`
     replaced: FxHashMap<(Atom, JsWord), Id>,
@@ -44,7 +94,33 @@ struct Data {
     rename_map: FxHashMap<Id, Id>,
 }
 
-impl VisitMut for Optimizer {
+#[derive(Debug)]
+struct ImportRecord {
+    module_specifier: Atom,
+}
+
+impl CjsOptimizer {
+    fn should_rewrite<'a>(&'a self, name: &'a str) -> Option<Rewriter<'a>> {
+        for (regex, config) in &self.packages {
+            let group = regex.captures(name);
+            if let Some(group) = group {
+                let group = group
+                    .iter()
+                    .map(|x| x.map(|x| x.as_str()).unwrap_or_default())
+                    .collect::<Vec<&str>>();
+                return Some(Rewriter {
+                    renderer: &self.renderer,
+                    key: name,
+                    config,
+                    group,
+                });
+            }
+        }
+        None
+    }
+}
+
+impl VisitMut for CjsOptimizer {
     noop_visit_mut_type!();
 
     fn visit_mut_expr(&mut self, e: &mut Expr) {
@@ -53,11 +129,11 @@ impl VisitMut for Optimizer {
         if let Expr::Member(n) = e {
             if let MemberProp::Ident(prop) = &n.prop {
                 if let Expr::Ident(obj) = &*n.obj {
-                    if let Some(module_specifier) = self.data.imports.get(&obj.to_id()) {
+                    if let Some(record) = self.data.imports.get(&obj.to_id()) {
                         let new_id = self
                             .data
                             .replaced
-                            .entry((module_specifier.clone(), prop.sym.clone()))
+                            .entry((record.module_specifier.clone(), prop.sym.clone()))
                             .or_insert_with(|| private_ident!(prop.sym.clone()).to_id())
                             .clone();
 
@@ -72,9 +148,10 @@ impl VisitMut for Optimizer {
                                     DUMMY_SP.with_ctxt(self.unresolved_ctxt),
                                 )
                                 .as_callee(),
-                                args: vec![
-                                    Expr::Lit(Lit::Str(module_specifier.clone().into())).as_arg()
-                                ],
+                                args: vec![Expr::Lit(Lit::Str(
+                                    record.module_specifier.clone().into(),
+                                ))
+                                .as_arg()],
                                 type_args: None,
                             }))),
                             definite: false,
@@ -113,9 +190,12 @@ impl VisitMut for Optimizer {
                             // TODO: Config
 
                             if let Pat::Ident(name) = &n.name {
-                                self.data
-                                    .imports
-                                    .insert(name.to_id(), v.value.clone().into());
+                                self.data.imports.insert(
+                                    name.to_id(),
+                                    ImportRecord {
+                                        module_specifier: v.value.clone().into(),
+                                    },
+                                );
                             }
                         }
                     }
@@ -142,4 +222,58 @@ impl VisitMut for Optimizer {
 
         n.visit_mut_children_with(&mut IdentRenamer::new(&self.data.rename_map));
     }
+}
+
+fn helper_lower_case(
+    h: &Helper<'_, '_>,
+    _: &Handlebars<'_>,
+    _: &Context,
+    _: &mut RenderContext<'_, '_>,
+    out: &mut dyn Output,
+) -> HelperResult {
+    // get parameter from helper or throw an error
+    let param = h.param(0).and_then(|v| v.value().as_str()).unwrap_or("");
+    out.write(param.to_lowercase().as_ref())?;
+    Ok(())
+}
+
+fn helper_upper_case(
+    h: &Helper<'_, '_>,
+    _: &Handlebars<'_>,
+    _: &Context,
+    _: &mut RenderContext<'_, '_>,
+    out: &mut dyn Output,
+) -> HelperResult {
+    // get parameter from helper or throw an error
+    let param = h.param(0).and_then(|v| v.value().as_str()).unwrap_or("");
+    out.write(param.to_uppercase().as_ref())?;
+    Ok(())
+}
+
+fn helper_camel_case(
+    h: &Helper<'_, '_>,
+    _: &Handlebars<'_>,
+    _: &Context,
+    _: &mut RenderContext<'_, '_>,
+    out: &mut dyn Output,
+) -> HelperResult {
+    // get parameter from helper or throw an error
+    let param = h.param(0).and_then(|v| v.value().as_str()).unwrap_or("");
+
+    out.write(param.to_case(Case::Camel).as_ref())?;
+    Ok(())
+}
+
+fn helper_kebab_case(
+    h: &Helper<'_, '_>,
+    _: &Handlebars<'_>,
+    _: &Context,
+    _: &mut RenderContext<'_, '_>,
+    out: &mut dyn Output,
+) -> HelperResult {
+    // get parameter from helper or throw an error
+    let param = h.param(0).and_then(|v| v.value().as_str()).unwrap_or("");
+
+    out.write(param.to_case(Case::Kebab).as_ref())?;
+    Ok(())
 }
