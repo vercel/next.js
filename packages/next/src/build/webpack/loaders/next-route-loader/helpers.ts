dcbdb585eaf4cc0e4708c31de78a77533780a11f
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-05-19 15:13:02 -0600

    fix: support commonjs exports

--- packages/next/src/build/webpack/loaders/next-route-loader/helpers.ts
+++ packages/next/src/build/webpack/loaders/next-route-loader/helpers.ts
@@ -14,7 +14,13 @@ export function hoist(module: any, name: string) {
   // If a property called `then` exists, assume it's a promise and
   // return a promise that resolves to the name.
   if ('then' in module && typeof module.then === 'function') {
-    return module.then((mod: any) => mod[name])
+    return module.then((mod: any) => hoist(mod, name))
+  }
+
+  // If we're trying to hoise the default export, and the module is a function,
+  // return the module itself.
+  if (typeof module === 'function' && name === 'default') {
+    return module
   }
 
   // Otherwise, return undefined.
