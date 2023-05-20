59d44973cb35f3d8f2c1d8a20496f3e16509c67e
Ibrahem Mahyob <ibrahemid@gmail.com>
2023-05-20 21:31:24 +0800

    Fix TypeScript error in with-mongodb example
    The context parameter in the getServerSideProps function lacked a type, which led to a compile-time error. To resolve this, the GetServerSidePropsContext type from next was used to properly type the context parameter. This ensures type safety and allows TypeScript to catch potential errors at compile time.

--- examples/with-mongodb/pages/index.tsx
+++ examples/with-mongodb/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
-import { InferGetServerSidePropsType } from 'next'
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   try {
     await clientPromise
     // `await clientPromise` will use the default database passed in the MONGODB_URI
