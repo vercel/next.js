ac121a19b26df2e3a371096af43b89187e6737c5
Jiachi Liu <inbox@huozhi.im>
2023-05-20 15:26:34 +0200

    fix ts checking

--- packages/next/src/server/lib/trace/tracer.ts
+++ packages/next/src/server/lib/trace/tracer.ts
@@ -25,7 +25,7 @@ try {
   }
 }
 
-const { context, trace, SpanStatusCode, SpanKind } = api
+const { context, trace, SpanStatusCode, SpanKind } = api!
 
 const isPromise = <T>(p: any): p is Promise<T> => {
   return p !== null && typeof p === 'object' && typeof p.then === 'function'
