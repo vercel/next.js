045633a96305711bba0da6e0d35715e6c7b3ba8f
조상현 <05lazy.dev@gmail.com>
2023-05-22 11:32:19 +0900

    Update index.ts

--- packages/create-next-app/templates/index.ts
+++ packages/create-next-app/templates/index.ts
@@ -1,5 +1,4 @@
 import { install } from '../helpers/install'
-import { makeDir } from '../helpers/make-dir'
 
 import cpy from 'cpy'
 import globOrig from 'glob'
@@ -11,6 +10,8 @@ import util from 'util'
 import { Sema } from 'async-sema'
 
 import { GetTemplateFileArgs, InstallTemplateArgs } from './types'
+
+import { makeDir } from '../helpers/make-dir'
 
 const glob = util.promisify(globOrig)
 
