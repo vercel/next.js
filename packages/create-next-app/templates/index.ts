af6fb560ac269e48692bc76a57b61e00cf3e429a
Josanghyeon <05lazy.dev@gmail.com>
2023-05-23 11:28:57 +0900

    Revert "makeDir should use func"
    This reverts commit 8c016a27bdae34468a78bf5221f200a614b18b1b.

--- packages/create-next-app/templates/index.ts
+++ packages/create-next-app/templates/index.ts
@@ -1,5 +1,4 @@
 import { install } from '../helpers/install'
-import { makeDir } from '../helpers/make-dir'
 
 import cpy from 'cpy'
 import globOrig from 'glob'
@@ -112,7 +111,7 @@ export const installTemplate = async ({
   }
 
   if (srcDir) {
-    await makeDir(path.join(root, 'src'))
+    await fs.promises.mkdir(path.join(root, 'src'), { recursive: true })
     await Promise.all(
       SRC_DIR_NAMES.map(async (file) => {
         await fs.promises
