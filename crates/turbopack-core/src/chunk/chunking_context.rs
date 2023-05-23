3cf5734a6657d0bb0cfefd144c71786bf4e23c7d
Tobias Koppers <tobias.koppers@googlemail.com>
2023-05-23 18:00:17 +0200

    improve asset path (vercel/turbo#5072)
    ### Description
    
    * include basename in static path
    * only use 8 chars for hash
    
    This aligns with next.js, which seem to be a reasonable default.
    
    Production mode can omit the basename and only include the hash.

--- crates/turbopack-core/src/chunk/chunking_context.rs
+++ crates/turbopack-core/src/chunk/chunking_context.rs
@@ -33,7 +33,11 @@ pub trait ChunkingContext {
 
     fn can_be_in_same_chunk(&self, asset_a: AssetVc, asset_b: AssetVc) -> BoolVc;
 
-    fn asset_path(&self, content_hash: &str, extension: &str) -> FileSystemPathVc;
+    fn asset_path(
+        &self,
+        content_hash: &str,
+        original_asset_ident: AssetIdentVc,
+    ) -> FileSystemPathVc;
 
     fn is_hot_module_replacement_enabled(&self) -> BoolVc {
         BoolVc::cell(false)
--- crates/turbopack-static/src/lib.rs
+++ crates/turbopack-static/src/lib.rs
@@ -138,7 +138,6 @@ struct StaticAsset {
 impl Asset for StaticAsset {
     #[turbo_tasks::function]
     async fn ident(&self) -> Result<AssetIdentVc> {
-        let source_path = self.source.ident().path();
         let content = self.source.content();
         let content_hash = if let AssetContent::File(file) = &*content.await? {
             if let FileContent::Content(file) = &*file.await? {
@@ -150,10 +149,9 @@ impl Asset for StaticAsset {
             return Err(anyhow!("StaticAsset::path: unsupported file content"));
         };
         let content_hash_b16 = turbo_tasks_hash::encode_hex(content_hash);
-        let asset_path = match source_path.await?.extension() {
-            Some(ext) => self.context.asset_path(&content_hash_b16, ext),
-            None => self.context.asset_path(&content_hash_b16, "bin"),
-        };
+        let asset_path = self
+            .context
+            .asset_path(&content_hash_b16, self.source.ident());
         Ok(AssetIdentVc::from_path(asset_path))
     }
 
