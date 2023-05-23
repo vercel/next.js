e534bbf45413c9a16edbc0253f9c0d12f49666e5
Jiachi Liu <inbox@huozhi.im>
2023-05-23 17:26:40 +0200

    revert changes

--- packages/next/server.d.ts
+++ packages/next/server.d.ts
@@ -4,12 +4,14 @@ declare global {
   var AsyncLocalStorage: typeof NodeAsyncLocalStorage
 }
 
-export { NextResponse } from 'next/dist/server/web/spec-extension/response'
-export { userAgentFromString } from 'next/dist/server/web/spec-extension/user-agent'
-export { userAgent } from 'next/dist/server/web/spec-extension/user-agent'
-export { URLPattern } from 'next/dist/compiled/@edge-runtime/primitives/url'
-export { ImageResponse } from 'next/dist/server/web/spec-extension/image-response'
-export { NextRequest } from 'next/dist/server/web/spec-extension/request'
 export type { NextMiddleware } from 'next/dist/server/web/types'
 export type { NextFetchEvent } from 'next/dist/server/web/spec-extension/fetch-event'
 export type { ImageResponseOptions } from 'next/dist/compiled/@vercel/og/types'
+export { NextResponse } from 'next/dist/server/web/spec-extension/response'
+export {
+  userAgent,
+  userAgentFromString,
+} from 'next/dist/server/web/spec-extension/user-agent'
+export { URLPattern } from 'next/dist/compiled/@edge-runtime/primitives/url'
+export { ImageResponse } from 'next/dist/server/web/spec-extension/image-response'
+export { NextRequest } from 'next/dist/server/web/spec-extension/request'
