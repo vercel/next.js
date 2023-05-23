c92da11c94438c00dd4cae9e0d704a2081c0c01d
Jiachi Liu <inbox@huozhi.im>
2023-05-23 19:18:57 +0200

    revert modularize import

--- packages/next-swc/crates/core/src/lib.rs
+++ packages/next-swc/crates/core/src/lib.rs
@@ -163,6 +163,21 @@ where
         }
     };
 
+    let mut modularize_imports_config = match &opts.modularize_imports {
+        Some(config) => config.clone(),
+        None => turbopack_binding::swc::custom_transform::modularize_imports::Config {
+            packages: std::collections::HashMap::new(),
+        },
+    };
+    modularize_imports_config.packages.insert(
+        "next/server".to_string(),
+        turbopack_binding::swc::custom_transform::modularize_imports::PackageConfig {
+            transform: "next/dist/server/web/exports/{{ kebabCase member }}".to_string(),
+            prevent_full_import: false,
+            skip_default_conversion: false,
+        },
+    );
+
     chain!(
         disallow_re_export_all_in_page::disallow_re_export_all_in_page(opts.is_page_file),
         match &opts.server_components {
@@ -255,14 +270,9 @@ where
                 }
             })
             .unwrap_or_else(|| Either::Right(noop())),
-        match &opts.modularize_imports {
-            Some(config) => Either::Left(
-                turbopack_binding::swc::custom_transform::modularize_imports::modularize_imports(
-                    config.clone()
-                )
-            ),
-            None => Either::Right(noop()),
-        },
+        turbopack_binding::swc::custom_transform::modularize_imports::modularize_imports(
+            modularize_imports_config
+        ),
         match &opts.font_loaders {
             Some(config) => Either::Left(next_font_loaders(config.clone())),
             None => Either::Right(noop()),
