212b230fd4650f54cb40850052b0914b36cb6f5f
Jiachi Liu <inbox@huozhi.im>
2023-05-25 19:42:41 +0200

    fix test and lint

--- test/e2e/app-dir/app-edge-root-layout/index.test.ts
+++ test/e2e/app-dir/app-edge-root-layout/index.test.ts
@@ -1,5 +1,4 @@
 import { createNextDescribe } from 'e2e-utils'
-import { check } from 'next-test-utils'
 
 createNextDescribe(
   'app-dir edge runtime root layout',
@@ -10,20 +9,20 @@ createNextDescribe(
   ({ next, isNextStart }) => {
     it('should not emit metadata files into bad paths', async () => {
       await next.fetch('/favicon.ico')
-      const middlewareManifest = await next.readFile(
-        '.next/server/middleware-manifest.json'
-      )
-      expect(middlewareManifest).not.toContain('favicon')
+      // issue: If metadata files are not filter out properly with image-loader,
+      // an incorrect static/media folder will be generated
+
+      // Check that the static folder is not generated
+      const incorrectGeneratedStaticFolder = await next.hasFile('static')
+      expect(incorrectGeneratedStaticFolder).toBe(false)
     })
 
     if (isNextStart) {
       it('should not emit metadata files into bad paths', async () => {
-        // issue: If metadata files are not filter out properly with image-loader,
-        // an incorrect static/media folder will be generated
-
-        // Check that the static folder is not generated
-        const incorrectGeneratedStaticFolder = await next.hasFile('static')
-        expect(incorrectGeneratedStaticFolder).toBe(false)
+        const middlewareManifest = await next.readFile(
+          '.next/server/middleware-manifest.json'
+        )
+        expect(middlewareManifest).not.toContain('favicon')
       })
     }
   }
