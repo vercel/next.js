6c98409a28fe9af79631699b4b64a763a5b0012b
vinay <94120295+vinaykulk621@users.noreply.github.com>
2023-05-26 12:45:20 +0530

    better get-pkg logic

--- packages/create-next-app/helpers/get-pkg-manager.ts
+++ packages/create-next-app/helpers/get-pkg-manager.ts
@@ -3,15 +3,13 @@ export type PackageManager = 'npm' | 'pnpm' | 'yarn'
 export function getPkgManager(): PackageManager {
   const userAgent = process.env.npm_config_user_agent
 
-  if (userAgent) {
-    if (userAgent.startsWith('yarn')) {
-      return 'yarn'
-    } else if (userAgent.startsWith('pnpm')) {
-      return 'pnpm'
-    } else {
-      return 'npm'
-    }
-  } else {
-    return 'npm'
+  if (userAgent && userAgent.startsWith('yarn')) {
+    return 'yarn'
   }
+  
+  if (userAgent && userAgent.startsWith('pnpm')) {
+    return 'pnpm'
+  } 
+  
+  return 'npm'
 }
