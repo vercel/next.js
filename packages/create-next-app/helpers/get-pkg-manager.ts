94bcf4738f7906f37e080b8c7f1a3c4a7744b65d
Steven <steven@ceriously.com>
2023-05-26 10:56:27 -0400

    Apply suggestions from code review

--- packages/create-next-app/helpers/get-pkg-manager.ts
+++ packages/create-next-app/helpers/get-pkg-manager.ts
@@ -1,13 +1,13 @@
 export type PackageManager = 'npm' | 'pnpm' | 'yarn'
 
 export function getPkgManager(): PackageManager {
-  const userAgent = process.env.npm_config_user_agent
+  const userAgent = process.env.npm_config_user_agent || ''
 
-  if (userAgent && userAgent.startsWith('yarn')) {
+  if (userAgent.startsWith('yarn')) {
     return 'yarn'
   }
   
-  if (userAgent && userAgent.startsWith('pnpm')) {
+  if (userAgent.startsWith('pnpm')) {
     return 'pnpm'
   } 
   
