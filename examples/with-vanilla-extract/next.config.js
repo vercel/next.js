6978b06d1429b1196771317c879243ffdb8b27a8
SuttonJack <jack@momenthouse.com>
2023-05-26 16:26:35 -0400

    Update 'vanilla-extract/next-plugin' => v2.1.3

--- examples/with-vanilla-extract/next.config.js
+++ examples/with-vanilla-extract/next.config.js
@@ -1,12 +1,4 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin')
-
 const withVanillaExtract = createVanillaExtractPlugin()
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = withVanillaExtract(nextConfig)
+module.exports = withVanillaExtract()
