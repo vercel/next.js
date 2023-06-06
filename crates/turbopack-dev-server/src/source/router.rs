c976d3b0e1aa71bf7fd8b2f6c141629826181912
Justin Ridgewell <justin@ridgewell.name>
2023-06-06 16:14:21 -0400

    Fix `PrefixedRouterContentSourceVc`'s `debug_assertion` (vercel/turbo#5227)
    ### Description
    
    `ContentSource` paths do not begin with a `/`, and the prefix should
    match that. 🤦
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->

--- crates/turbopack-dev-server/src/source/router.rs
+++ crates/turbopack-dev-server/src/source/router.rs
@@ -36,7 +36,7 @@ impl PrefixedRouterContentSourceVc {
         if cfg!(debug_assertions) {
             let prefix_string = prefix.await?;
             debug_assert!(prefix_string.is_empty() || prefix_string.ends_with('/'));
-            debug_assert!(prefix_string.starts_with('/'));
+            debug_assert!(!prefix_string.starts_with('/'));
         }
         Ok(PrefixedRouterContentSource {
             prefix,
