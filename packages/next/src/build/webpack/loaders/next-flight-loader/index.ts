ed782381a6e94a889ed1e3c566a9c0e021ea5cc2
Scott Martin <92527868+sm-kc@users.noreply.github.com>
2023-06-08 14:05:10 +0100

    Fix vercel/next.js#50844
    Link to correct metadata docs in warning about using next/head in app directory.

--- packages/next/src/build/webpack/loaders/next-flight-loader/index.ts
+++ packages/next/src/build/webpack/loaders/next-flight-loader/index.ts
@@ -95,7 +95,7 @@ export { e${cnt++} as ${ref} };`
   if (buildInfo.rsc?.type !== RSC_MODULE_TYPES.client) {
     if (noopHeadPath === this.resourcePath) {
       warnOnce(
-        `Warning: You're using \`next/head\` inside the \`app\` directory, please migrate to the Metadata API. See https://nextjs.org/docs/app/api-reference/file-conventions/metadata for more details.`
+        `Warning: You're using \`next/head\` inside the \`app\` directory, please migrate to the Metadata API. See https://nextjs.org/docs/app/building-your-application/optimizing/metadata for more details.`
       )
     }
   }
