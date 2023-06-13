cb90783f1a21e83dd6d0840277bdcadf2d1007e2
Steven <steven@ceriously.com>
2023-06-13 19:42:14 -0400

    Ensure test checks for the order

--- test/unit/next-image-get-img-props.test.ts
+++ test/unit/next-image-get-img-props.test.ts
@@ -1,28 +1,30 @@
 /* eslint-env jest */
 import { unstable_getImgProps } from 'next/image'
 
-describe('Image rendering', () => {
-  it('should render Image on its own', async () => {
+describe('getImgProps()', () => {
+  it('should get props', async () => {
     const { props } = unstable_getImgProps({
-      alt: 'a nice image',
+      alt: 'a nice desc',
       id: 'my-image',
       src: '/test.png',
       width: 100,
-      height: 100,
+      height: 200,
       loading: 'eager',
     })
     // order matters here
-    expect(props).toStrictEqual({
-      alt: 'a nice image',
-      id: 'my-image',
-      loading: 'eager',
-      width: 100,
-      height: 100,
-      decoding: 'async',
-      style: { color: 'transparent' },
-      srcSet:
+    expect(Object.entries(props)).toStrictEqual([
+      ['alt', 'a nice desc'],
+      ['id', 'my-image'],
+      ['loading', 'eager'],
+      ['width', 100],
+      ['height', 200],
+      ['decoding', 'async'],
+      ['style', { color: 'transparent' }],
+      [
+        'srcSet',
         '/_next/image?url=%2Ftest.png&w=128&q=75 1x, /_next/image?url=%2Ftest.png&w=256&q=75 2x',
-      src: '/_next/image?url=%2Ftest.png&w=256&q=75',
-    })
+      ],
+      ['src', '/_next/image?url=%2Ftest.png&w=256&q=75'],
+    ])
   })
 })
