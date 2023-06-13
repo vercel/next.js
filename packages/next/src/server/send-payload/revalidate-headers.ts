2b90620d23f8222da29f49be80e6f45f4b6d2cb1
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-06-13 17:07:21 -0600

    fix: revert render changes

--- packages/next/src/server/send-payload/revalidate-headers.ts
+++ packages/next/src/server/send-payload/revalidate-headers.ts
@@ -2,33 +2,13 @@ import type { ServerResponse } from 'http'
 import type { BaseNextResponse } from '../base-http'
 import type { PayloadOptions } from './index'
 
-function isResponse(
-  res: ServerResponse | BaseNextResponse | Headers
-): res is ServerResponse | BaseNextResponse {
-  return 'setHeader' in res && typeof res.setHeader === 'function'
-}
-
-function adapt(
-  res: ServerResponse | BaseNextResponse | Headers
-): Pick<Headers, 'has' | 'set'> {
-  if (isResponse(res)) {
-    return {
-      has: res.hasHeader.bind(res),
-      set: res.setHeader.bind(res),
-    }
-  }
-
-  return res
-}
-
 export function setRevalidateHeaders(
-  res: ServerResponse | BaseNextResponse | Headers,
+  res: ServerResponse | BaseNextResponse,
   options: PayloadOptions
 ) {
-  const headers = adapt(res)
   if (options.private || options.stateful) {
-    if (options.private || !headers.has('Cache-Control')) {
-      headers.set(
+    if (options.private || !res.hasHeader('Cache-Control')) {
+      res.setHeader(
         'Cache-Control',
         'private, no-cache, no-store, max-age=0, must-revalidate'
       )
@@ -40,11 +20,11 @@ export function setRevalidateHeaders(
       )
     }
 
-    headers.set(
+    res.setHeader(
       'Cache-Control',
       `s-maxage=${options.revalidate}, stale-while-revalidate`
     )
   } else if (options.revalidate === false) {
-    headers.set('Cache-Control', 's-maxage=31536000, stale-while-revalidate')
+    res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate')
   }
 }
