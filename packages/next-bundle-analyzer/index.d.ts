1422c5e0f3bf2a482cc170a13198983851c275ad
JJ Kasper <jj@jjsweb.site>
2023-06-14 14:34:44 -0700

    update type more

--- packages/next-bundle-analyzer/index.d.ts
+++ packages/next-bundle-analyzer/index.d.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next'
 
-type NextBundleAnalyzer = (options?: {
+declare function NextBundleAnalyzer(options?: {
   enabled?: boolean
   openAnalyzer?: boolean
   analyzerMode?: 'json' | 'static'
-}) => (config?: NextConfig) => NextConfig
+}): (config?: NextConfig) => NextConfig
 
 export = NextBundleAnalyzer
