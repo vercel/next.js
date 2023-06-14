af68c3d1a572648d8654dd00f04625f4a6793bab
Shu Ding <g@shud.in>
2023-06-15 01:13:21 +0200

    fix server actions compilation

--- packages/next-swc/crates/core/tests/fixture/server-actions/server/2/output.js
+++ packages/next-swc/crates/core/tests/fixture/server-actions/server/2/output.js
@@ -10,5 +10,5 @@ export default function Page() {
     return <Button action={myAction}>Delete</Button>;
 }
 export const action = withValidate(($$ACTION_1 = async (...args)=>$$ACTION_2.apply(null, ($$ACTION_1.$$bound || []).concat(args)), __create_action_proxy__("9878bfa39811ca7650992850a8751f9591b6a557", null, $$ACTION_1, $$ACTION_2), $$ACTION_1));
-export const $$ACTION_2 = async ()=>{};
+export var $$ACTION_2 = async ()=>{};
 var $$ACTION_1;
