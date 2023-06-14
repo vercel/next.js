aa8400c2ca95667cff306fddea09be93b23ecf00
JJ Kasper <jj@jjsweb.site>
2023-06-13 23:37:19 -0700

    Update flakey rsc streaming test (#51274)
    This is checking for only alpha-numerical chars for the build_id value
    although they can also contain dashes
    
    x-ref:
    https://github.com/vercel/next.js/actions/runs/5263773644/jobs/9514284188#step:25:454

--- test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
+++ test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
@@ -399,7 +399,7 @@ createNextDescribe(
           const result = await resolveStreamResponse(response)
           expect(result).toContain('component:index.server')
           expect(result).toMatch(
-            isNextDev ? /0:\["development",/ : /0:\["\w+",/
+            isNextDev ? /0:\["development",/ : /0:\[".*?",/
           )
         })
     })
