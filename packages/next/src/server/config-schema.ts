710d8107390f11e8aed31ae77410368d9df4df89
devjiwonchoi <devjeanmarlon@gmail.com>
2023-06-15 14:56:03 +0900

    chore: add serverActionsSizeLimit type to config-schema
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- packages/next/src/server/config-schema.ts
+++ packages/next/src/server/config-schema.ts
@@ -304,6 +304,16 @@ const configSchema = {
         serverActions: {
           type: 'boolean',
         },
+        serverActionsSizeLimit: {
+          oneOf: [
+            {
+              type: 'number',
+            },
+            {
+              type: 'string',
+            },
+          ] as any,
+        },
         extensionAlias: {
           type: 'object',
         },
