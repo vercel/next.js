b5f51a89abed2bd167708781402570dc795ce889
Balázs Orbán <info@balazsorban.com>
2023-06-15 17:13:55 +0200

    Apply suggestions from code review

--- examples/with-supabase/tailwind.config.js
+++ examples/with-supabase/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
