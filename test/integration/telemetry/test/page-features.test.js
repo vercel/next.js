4b48a5b3636fee5d9258f2e98ea0982dedf39166
Tobias Koppers <tobias.koppers@googlemail.com>
2023-06-15 15:24:01 +0000

    update telemetry test

--- test/integration/telemetry/test/page-features.test.js
+++ test/integration/telemetry/test/page-features.test.js
@@ -34,283 +34,287 @@ const setupAppDir = async () => {
 }
 
 describe('page features telemetry', () => {
-  it('detects --turbo correctly for `next dev`', async () => {
-    let port = await findPort()
-    let stderr = ''
+  if (process.env.TURBOPACK) {
+    it('detects --turbo correctly for `next dev`', async () => {
+      let port = await findPort()
+      let stderr = ''
 
-    const teardown = await setupAppDir()
-
-    try {
-      const handleStderr = (msg) => {
-        stderr += msg
-      }
-      let app = await launchApp(appDir, port, {
-        onStderr: handleStderr,
-        env: {
-          NEXT_TELEMETRY_DEBUG: 1,
-        },
-        turbo: true,
-      })
-      await check(() => stderr, /NEXT_CLI_SESSION_STARTED/)
-
-      if (app) {
-        await killApp(app)
-      }
+      const teardown = await setupAppDir()
 
       try {
-        expect(stderr).toContain('NEXT_CLI_SESSION_STARTED')
-        const event1 = /NEXT_CLI_SESSION_STARTED[\s\S]+?{([\s\S]+?)}/
+        const handleStderr = (msg) => {
+          stderr += msg
+        }
+        let app = await launchApp(appDir, port, {
+          onStderr: handleStderr,
+          env: {
+            NEXT_TELEMETRY_DEBUG: 1,
+          },
+          turbo: true,
+        })
+        await check(() => stderr, /NEXT_CLI_SESSION_STARTED/)
+
+        if (app) {
+          await killApp(app)
+        }
+
+        try {
+          expect(stderr).toContain('NEXT_CLI_SESSION_STARTED')
+          const event1 = /NEXT_CLI_SESSION_STARTED[\s\S]+?{([\s\S]+?)}/
+            .exec(stderr)
+            .pop()
+
+          expect(event1).toMatch(/"pagesDir": true/)
+          expect(event1).toMatch(/"turboFlag": true/)
+        } catch (err) {
+          require('console').error('failing stderr', stderr, err)
+          throw err
+        }
+      } finally {
+        await teardown()
+      }
+    })
+
+    it('detects --turbo correctly for `next dev` stopped', async () => {
+      let port = await findPort()
+      let stderr = ''
+
+      const teardown = await setupAppDir()
+
+      try {
+        const handleStderr = (msg) => {
+          stderr += msg
+        }
+        let app = await launchApp(appDir, port, {
+          onStderr: handleStderr,
+          env: {
+            NEXT_TELEMETRY_DEBUG: 1,
+          },
+          turbo: true,
+        })
+
+        await check(() => stderr, /NEXT_CLI_SESSION_STARTED/)
+
+        if (app) {
+          await killApp(app)
+        }
+        await check(() => stderr, /NEXT_CLI_SESSION_STOPPED/)
+
+        expect(stderr).toContain('NEXT_CLI_SESSION_STOPPED')
+        const event1 = /NEXT_CLI_SESSION_STOPPED[\s\S]+?{([\s\S]+?)}/
           .exec(stderr)
           .pop()
 
         expect(event1).toMatch(/"pagesDir": true/)
         expect(event1).toMatch(/"turboFlag": true/)
-      } catch (err) {
-        require('console').error('failing stderr', stderr, err)
-        throw err
+
+        expect(
+          await fs.pathExists(path.join(appDir, '.next/_events.json'))
+        ).toBe(false)
+      } finally {
+        await teardown()
       }
-    } finally {
-      await teardown()
-    }
-  })
+    })
+  } else {
+    it('detects correctly for `next dev` stopped (no turbo)', async () => {
+      let port = await findPort()
+      let stderr = ''
 
-  it('detects --turbo correctly for `next dev` stopped', async () => {
-    let port = await findPort()
-    let stderr = ''
+      const teardown = await setupAppDir()
 
-    const teardown = await setupAppDir()
+      try {
+        const handleStderr = (msg) => {
+          stderr += msg
+        }
+        let app = await launchApp(appDir, port, {
+          onStderr: handleStderr,
+          env: {
+            NEXT_TELEMETRY_DEBUG: 1,
+          },
+        })
 
-    try {
-      const handleStderr = (msg) => {
-        stderr += msg
+        await check(() => stderr, /NEXT_CLI_SESSION_STARTED/)
+
+        if (app) {
+          await killApp(app)
+        }
+
+        await check(() => stderr, /NEXT_CLI_SESSION_STOPPED/)
+
+        expect(stderr).toContain('NEXT_CLI_SESSION_STOPPED')
+        const event1 = /NEXT_CLI_SESSION_STOPPED[\s\S]+?{([\s\S]+?)}/
+          .exec(stderr)
+          .pop()
+
+        expect(event1).toMatch(/"turboFlag": false/)
+        expect(event1).toMatch(/"pagesDir": true/)
+        expect(event1).toMatch(/"appDir": true/)
+
+        expect(
+          await fs.pathExists(path.join(appDir, '.next/_events.json'))
+        ).toBe(false)
+      } finally {
+        await teardown()
       }
-      let app = await launchApp(appDir, port, {
-        onStderr: handleStderr,
-        env: {
-          NEXT_TELEMETRY_DEBUG: 1,
-        },
-        turbo: true,
+    })
+
+    it('should detect app page counts', async () => {
+      const teardown = await setupAppDir()
+
+      try {
+        await fs.ensureFile(path.join(__dirname, '../app/ssr/page.js'))
+        await fs.writeFile(
+          path.join(__dirname, '../app/ssr/page.js'),
+          `
+          export const revalidate = 0
+          export default function Page() {
+            return <p>ssr page</p>
+          }
+        `
+        )
+        await fs.ensureFile(path.join(__dirname, '../app/edge-ssr/page.js'))
+        await fs.writeFile(
+          path.join(__dirname, '../app/edge-ssr/page.js'),
+          `
+          export const runtime = 'experimental-edge'
+          export default function Page() {
+            return <p>edge-ssr page</p>
+          }
+        `
+        )
+        await fs.ensureFile(
+          path.join(__dirname, '../app/app-ssg/[slug]/page.js')
+        )
+        await fs.writeFile(
+          path.join(__dirname, '../app/app-ssg/[slug]/page.js'),
+          `
+          export function generateStaticParams() {
+            return [
+              { slug: 'post-1' },
+              { slug: 'post-2' },
+            ]
+          }
+          export default function Page() {
+            return <p>ssg page</p>
+          }
+        `
+        )
+        const { stderr } = await nextBuild(appDir, [], {
+          stderr: true,
+          env: { NEXT_TELEMETRY_DEBUG: 1 },
+        })
+
+        try {
+          expect(stderr).toContain('NEXT_BUILD_OPTIMIZED')
+          const event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
+            .exec(stderr)
+            .pop()
+          expect(event1).toMatch(/"staticPropsPageCount": 2/)
+          expect(event1).toMatch(/"serverPropsPageCount": 2/)
+          expect(event1).toMatch(/"ssrPageCount": 3/)
+          expect(event1).toMatch(/"staticPageCount": 4/)
+          expect(event1).toMatch(/"totalPageCount": 11/)
+          expect(event1).toMatch(/"totalAppPagesCount": 4/)
+          expect(event1).toMatch(/"serverAppPagesCount": 2/)
+          expect(event1).toMatch(/"edgeRuntimeAppCount": 1/)
+          expect(event1).toMatch(/"edgeRuntimePagesCount": 2/)
+
+          expect(stderr).toContain('NEXT_BUILD_COMPLETED')
+          const event2 = /NEXT_BUILD_COMPLETED[\s\S]+?{([\s\S]+?)}/
+            .exec(stderr)
+            .pop()
+
+          expect(event2).toMatch(/"totalAppPagesCount": 4/)
+        } catch (err) {
+          require('console').error('failing stderr', stderr, err)
+          throw err
+        }
+      } finally {
+        await teardown()
+      }
+    })
+
+    it('detects reportWebVitals with no _app correctly for `next build`', async () => {
+      // Case 1: When _app.js does not exist.
+      let build = await nextBuild(appDir, [], {
+        stderr: 'log',
+        stdout: 'log',
+        env: { NEXT_TELEMETRY_DEBUG: 1 },
       })
 
-      await check(() => stderr, /NEXT_CLI_SESSION_STARTED/)
-
-      if (app) {
-        await killApp(app)
-      }
-      await check(() => stderr, /NEXT_CLI_SESSION_STOPPED/)
-
-      expect(stderr).toContain('NEXT_CLI_SESSION_STOPPED')
-      const event1 = /NEXT_CLI_SESSION_STOPPED[\s\S]+?{([\s\S]+?)}/
-        .exec(stderr)
+      expect(build.stderr).toContain('NEXT_BUILD_OPTIMIZED')
+      let event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
+        .exec(build.stderr)
         .pop()
+      expect(event1).toMatch(/hasReportWebVitals.*?false/)
+    })
 
-      expect(event1).toMatch(/"pagesDir": true/)
-      expect(event1).toMatch(/"turboFlag": true/)
-
-      expect(await fs.pathExists(path.join(appDir, '.next/_events.json'))).toBe(
-        false
+    it('detect with reportWebVitals correctly for `next build`', async () => {
+      // Case 2: When _app.js exist with reportWebVitals function.
+      await fs.utimes(
+        path.join(appDir, 'pages', '_app_withreportwebvitals.empty'),
+        new Date(),
+        new Date()
       )
-    } finally {
-      await teardown()
-    }
-  })
-
-  it('detects correctly for `next dev` stopped (no turbo)', async () => {
-    let port = await findPort()
-    let stderr = ''
-
-    const teardown = await setupAppDir()
-
-    try {
-      const handleStderr = (msg) => {
-        stderr += msg
-      }
-      let app = await launchApp(appDir, port, {
-        onStderr: handleStderr,
-        env: {
-          NEXT_TELEMETRY_DEBUG: 1,
-        },
-      })
-
-      await check(() => stderr, /NEXT_CLI_SESSION_STARTED/)
-
-      if (app) {
-        await killApp(app)
-      }
-
-      await check(() => stderr, /NEXT_CLI_SESSION_STOPPED/)
-
-      expect(stderr).toContain('NEXT_CLI_SESSION_STOPPED')
-      const event1 = /NEXT_CLI_SESSION_STOPPED[\s\S]+?{([\s\S]+?)}/
-        .exec(stderr)
-        .pop()
-
-      expect(event1).toMatch(/"turboFlag": false/)
-      expect(event1).toMatch(/"pagesDir": true/)
-      expect(event1).toMatch(/"appDir": true/)
-
-      expect(await fs.pathExists(path.join(appDir, '.next/_events.json'))).toBe(
-        false
+      await fs.rename(
+        path.join(appDir, 'pages', '_app_withreportwebvitals.empty'),
+        path.join(appDir, 'pages', '_app.js')
       )
-    } finally {
-      await teardown()
-    }
-  })
 
-  it('should detect app page counts', async () => {
-    const teardown = await setupAppDir()
-
-    try {
-      await fs.ensureFile(path.join(__dirname, '../app/ssr/page.js'))
-      await fs.writeFile(
-        path.join(__dirname, '../app/ssr/page.js'),
-        `
-        export const revalidate = 0
-        export default function Page() {
-          return <p>ssr page</p>
-        }
-      `
-      )
-      await fs.ensureFile(path.join(__dirname, '../app/edge-ssr/page.js'))
-      await fs.writeFile(
-        path.join(__dirname, '../app/edge-ssr/page.js'),
-        `
-        export const runtime = 'experimental-edge'
-        export default function Page() {
-          return <p>edge-ssr page</p>
-        }
-      `
-      )
-      await fs.ensureFile(path.join(__dirname, '../app/app-ssg/[slug]/page.js'))
-      await fs.writeFile(
-        path.join(__dirname, '../app/app-ssg/[slug]/page.js'),
-        `
-        export function generateStaticParams() {
-          return [
-            { slug: 'post-1' },
-            { slug: 'post-2' },
-          ]
-        }
-        export default function Page() {
-          return <p>ssg page</p>
-        }
-      `
-      )
-      const { stderr } = await nextBuild(appDir, [], {
+      const build = await nextBuild(appDir, [], {
         stderr: true,
         env: { NEXT_TELEMETRY_DEBUG: 1 },
       })
 
+      await fs.rename(
+        path.join(appDir, 'pages', '_app.js'),
+        path.join(appDir, 'pages', '_app_withreportwebvitals.empty')
+      )
+
       try {
-        expect(stderr).toContain('NEXT_BUILD_OPTIMIZED')
+        expect(build.stderr).toContain('NEXT_BUILD_OPTIMIZED')
         const event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
-          .exec(stderr)
+          .exec(build.stderr)
           .pop()
-        expect(event1).toMatch(/"staticPropsPageCount": 2/)
-        expect(event1).toMatch(/"serverPropsPageCount": 2/)
-        expect(event1).toMatch(/"ssrPageCount": 3/)
-        expect(event1).toMatch(/"staticPageCount": 4/)
-        expect(event1).toMatch(/"totalPageCount": 11/)
-        expect(event1).toMatch(/"totalAppPagesCount": 4/)
-        expect(event1).toMatch(/"serverAppPagesCount": 2/)
-        expect(event1).toMatch(/"edgeRuntimeAppCount": 1/)
-        expect(event1).toMatch(/"edgeRuntimePagesCount": 2/)
-
-        expect(stderr).toContain('NEXT_BUILD_COMPLETED')
-        const event2 = /NEXT_BUILD_COMPLETED[\s\S]+?{([\s\S]+?)}/
-          .exec(stderr)
-          .pop()
-
-        expect(event2).toMatch(/"totalAppPagesCount": 4/)
+        expect(event1).toMatch(/hasReportWebVitals.*?true/)
       } catch (err) {
-        require('console').error('failing stderr', stderr, err)
+        require('console').error(build.stderr)
         throw err
       }
-    } finally {
-      await teardown()
-    }
-  })
-
-  it('detects reportWebVitals with no _app correctly for `next build`', async () => {
-    // Case 1: When _app.js does not exist.
-    let build = await nextBuild(appDir, [], {
-      stderr: 'log',
-      stdout: 'log',
-      env: { NEXT_TELEMETRY_DEBUG: 1 },
     })
 
-    expect(build.stderr).toContain('NEXT_BUILD_OPTIMIZED')
-    let event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
-      .exec(build.stderr)
-      .pop()
-    expect(event1).toMatch(/hasReportWebVitals.*?false/)
-  })
+    it('detect without reportWebVitals correctly for `next build`', async () => {
+      // Case 3: When _app.js exist without reportWebVitals function.
+      await fs.utimes(
+        path.join(appDir, 'pages', '_app_withoutreportwebvitals.empty'),
+        new Date(),
+        new Date()
+      )
+      await fs.rename(
+        path.join(appDir, 'pages', '_app_withoutreportwebvitals.empty'),
+        path.join(appDir, 'pages', '_app.js')
+      )
 
-  it('detect with reportWebVitals correctly for `next build`', async () => {
-    // Case 2: When _app.js exist with reportWebVitals function.
-    await fs.utimes(
-      path.join(appDir, 'pages', '_app_withreportwebvitals.empty'),
-      new Date(),
-      new Date()
-    )
-    await fs.rename(
-      path.join(appDir, 'pages', '_app_withreportwebvitals.empty'),
-      path.join(appDir, 'pages', '_app.js')
-    )
+      const build = await nextBuild(appDir, [], {
+        stderr: true,
+        env: { NEXT_TELEMETRY_DEBUG: 1 },
+      })
 
-    const build = await nextBuild(appDir, [], {
-      stderr: true,
-      env: { NEXT_TELEMETRY_DEBUG: 1 },
+      await fs.rename(
+        path.join(appDir, 'pages', '_app.js'),
+        path.join(appDir, 'pages', '_app_withoutreportwebvitals.empty')
+      )
+
+      try {
+        expect(build.stderr).toContain('NEXT_BUILD_OPTIMIZED')
+        const event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
+          .exec(build.stderr)
+          .pop()
+        expect(event1).toMatch(/hasReportWebVitals.*?false/)
+      } catch (err) {
+        require('console').error(build.stderr)
+        throw err
+      }
     })
-
-    await fs.rename(
-      path.join(appDir, 'pages', '_app.js'),
-      path.join(appDir, 'pages', '_app_withreportwebvitals.empty')
-    )
-
-    try {
-      expect(build.stderr).toContain('NEXT_BUILD_OPTIMIZED')
-      const event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
-        .exec(build.stderr)
-        .pop()
-      expect(event1).toMatch(/hasReportWebVitals.*?true/)
-    } catch (err) {
-      require('console').error(build.stderr)
-      throw err
-    }
-  })
-
-  it('detect without reportWebVitals correctly for `next build`', async () => {
-    // Case 3: When _app.js exist without reportWebVitals function.
-    await fs.utimes(
-      path.join(appDir, 'pages', '_app_withoutreportwebvitals.empty'),
-      new Date(),
-      new Date()
-    )
-    await fs.rename(
-      path.join(appDir, 'pages', '_app_withoutreportwebvitals.empty'),
-      path.join(appDir, 'pages', '_app.js')
-    )
-
-    const build = await nextBuild(appDir, [], {
-      stderr: true,
-      env: { NEXT_TELEMETRY_DEBUG: 1 },
-    })
-
-    await fs.rename(
-      path.join(appDir, 'pages', '_app.js'),
-      path.join(appDir, 'pages', '_app_withoutreportwebvitals.empty')
-    )
-
-    try {
-      expect(build.stderr).toContain('NEXT_BUILD_OPTIMIZED')
-      const event1 = /NEXT_BUILD_OPTIMIZED[\s\S]+?{([\s\S]+?)}/
-        .exec(build.stderr)
-        .pop()
-      expect(event1).toMatch(/hasReportWebVitals.*?false/)
-    } catch (err) {
-      require('console').error(build.stderr)
-      throw err
-    }
-  })
+  }
 })
