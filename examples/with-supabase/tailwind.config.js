3dccbe41931bafd042e196349dc2e5834682938f
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-06-16 11:51:56 +1000

    remove unused tailwind extend

--- examples/with-supabase/tailwind.config.js
+++ examples/with-supabase/tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
