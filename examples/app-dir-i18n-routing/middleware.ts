2101ae6474c3a5b652bae0ec806f64da85a2ddab
CHEN Xian-an <xianan.chen@gmail.com>
2023-06-21 00:26:18 +0800

    fix: avoid double slashes on redirected path

--- examples/app-dir-i18n-routing/middleware.ts
+++ examples/app-dir-i18n-routing/middleware.ts
@@ -47,7 +47,12 @@ export function middleware(request: NextRequest) {
 
     // e.g. incoming request is /products
     // The new URL is now /en-US/products
-    return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url))
+    return NextResponse.redirect(
+      new URL(
+        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+        request.url
+      )
+    )
   }
 }
 
