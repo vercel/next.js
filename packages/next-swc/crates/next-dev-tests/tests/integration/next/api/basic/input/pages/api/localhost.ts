f3a87d47e9cea4e2144f89c0b0cee51063da546c
vinay <94120295+vinaykulk621@users.noreply.github.com>
2023-06-20 20:34:47 +0530

    Lint

--- packages/next-swc/crates/next-dev-tests/tests/integration/next/api/basic/input/pages/api/localhost.ts
+++ packages/next-swc/crates/next-dev-tests/tests/integration/next/api/basic/input/pages/api/localhost.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/pages/building-your-application/routing/api-routes
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   hello: string
