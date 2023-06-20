835eb33fd5c672b4bc958f39e53a3ae9102d8419
Ty Hopp <hopp.ty.c@gmail.com>
2023-06-20 16:37:44 +0800

    test: Add test cases for new eslint rule

---
+++ test/unit/eslint-plugin-next/no-async-client-component.test.ts
@@ -0,0 +1,71 @@
+import rule from '@next/eslint-plugin-next/dist/rules/no-async-client-component'
+import { RuleTester } from 'eslint'
+;(RuleTester as any).setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      jsx: true,
+    },
+  },
+})
+const ruleTester = new RuleTester()
+
+ruleTester.run('no-async-client-component single line', rule, {
+  valid: [
+    `
+    export default async function MyComponent() {
+      return <></>
+    }
+    `,
+  ],
+  invalid: [
+    {
+      code: `
+      "use client"
+
+      export default async function MyComponent() {
+        return <></>
+      }
+      `,
+      errors: [
+        {
+          message:
+            'Client components cannot be async functions. See: https://nextjs.org/docs/messages/no-async-client-component',
+        },
+      ],
+    },
+  ],
+})
+
+ruleTester.run('no-async-client-component multiple line', rule, {
+  valid: [
+    `
+    async function MyComponent() {
+      return <></>
+    }
+
+    export default MyComponent
+    `,
+  ],
+  invalid: [
+    {
+      code: `
+      "use client"
+
+      async function MyComponent() {
+        return <></>
+      }
+
+      export default MyComponent
+      `,
+      errors: [
+        {
+          message:
+            'Client components cannot be async functions. See: https://nextjs.org/docs/messages/no-async-client-component',
+        },
+      ],
+    },
+  ],
+})
