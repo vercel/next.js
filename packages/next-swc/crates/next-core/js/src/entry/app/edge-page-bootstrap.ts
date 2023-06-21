1b2f1bf75f3a27a7300bfee4245c290707179047
Tobias Koppers <tobias.koppers@googlemail.com>
2023-06-21 21:12:34 +0000

    avoid Object.hasOwn

--- packages/next-swc/crates/next-core/js/src/entry/app/edge-page-bootstrap.ts
+++ packages/next-swc/crates/next-core/js/src/entry/app/edge-page-bootstrap.ts
@@ -1,4 +1,3 @@
-import 'next/dist/esm/server/web/globals'
 import { adapter } from 'next/dist/server/web/adapter'
 import { RSC_VARY_HEADER } from 'next/dist/client/components/app-router-headers'
 import { IncrementalCache } from 'next/dist/server/lib/incremental-cache'
