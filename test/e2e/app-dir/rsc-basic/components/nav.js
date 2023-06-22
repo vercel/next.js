ae32589779faa16bfbfa6d33818463f69711b8c8
Jiachi Liu <inbox@huozhi.im>
2023-06-23 01:05:53 +0200

    disable prefetch

--- test/e2e/app-dir/rsc-basic/components/nav.js
+++ test/e2e/app-dir/rsc-basic/components/nav.js
@@ -4,17 +4,17 @@ export default function Nav() {
   return (
     <>
       <div>
-        <Link href={'/next-api/link'} id="goto-next-link">
+        <Link prefetch={false} href={'/next-api/link'} id="goto-next-link">
           next link
         </Link>
       </div>
       <div>
-        <Link href={'/streaming-rsc'} id="goto-streaming-rsc">
+        <Link prefetch={false} href={'/streaming-rsc'} id="goto-streaming-rsc">
           streaming rsc
         </Link>
       </div>
       <div>
-        <Link href={'/root'} id="goto-home">
+        <Link prefetch={false} href={'/root'} id="goto-home">
           home
         </Link>
       </div>
