f2c01e246ebc2e29135e3da3ec943460dfcb241f
Justin Ridgewell <justin@ridgewell.name>
2023-06-23 14:53:30 -0400

    Fix bug with `imports` field from a nested directory (vercel/turbo#5366)
    ### Description
    
    When importing a `#foo` subpath import, the resolved file is relative
    the the `package.json` containing the `imports` field, not the file with
    the import statement. This fixes subpath imports to use the proper
    context path during that resolve.
    
    ### Testing Instructions
    
    ```
    RUST_BACKTRACE=1 cargo nextest run -p turbopack-tests -E 'test(snapshot__imports__subpath_imports_nested)' --no-capture
    ```
    
    Fixes WEB-1216

---
+++ crates/turbopack-tests/tests/snapshot/imports/subpath-imports-nested/output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_5771e1.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_5771e1.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_5771e1.js",
+  "chunks": [
+    "output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_b53fce.js"
+  ],
+  "source": "entry"
+});
---
+++ crates/turbopack-tests/tests/snapshot/imports/subpath-imports-nested/output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_dc7802.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_dc7802.js",
+    {},
+    {"otherChunks":[{"path":"output/a587c_tests_snapshot_imports_subpath-imports-nested_input_index_b53fce.js","included":["[project]/crates/turbopack-tests/tests/snapshot/imports/subpath-imports-nested/input/index.js (ecmascript)"]}],"runtimeModuleIds":["[project]/crates/turbopack-tests/tests/snapshot/imports/subpath-imports-nested/input/index.js (ecmascript)"]}
+]);
+// Dummy runtime
