49cda981e18a68b42057e93bd1676a6434312ef8
Milo <49202538+milovangudelj@users.noreply.github.com>
2023-06-25 17:31:47 +0200

    Avoiding try-catch block
    `Negotiator.languages()` accepts an array of available locales and returns only results that are present in it. 
    
    This effectively eliminates the need to use a try-catch block when calling `matchLocale`.

--- examples/app-dir-i18n-routing/middleware.ts
+++ examples/app-dir-i18n-routing/middleware.ts
@@ -10,16 +10,18 @@ function getLocale(request: NextRequest): string | undefined {
   // Negotiator expects plain object so we need to transform headers
   const negotiatorHeaders: Record<string, string> = {}
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-
-  // Use negotiator and intl-localematcher to get best locale
-  let languages = new Negotiator({ headers: negotiatorHeaders }).languages()
+  
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales
-  try {
-    return matchLocale(languages, locales, i18n.defaultLocale)
-  } catch (_e) {
-    return i18n.defaultLocale
-  }
+
+  // Use negotiator and intl-localematcher to get best locale
+  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales
+  )
+
+  const locale = matchLocale(languages, locales, i18n.defaultLocale);
+
+  return locale
 }
 
 export function middleware(request: NextRequest) {
