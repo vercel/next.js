30020fedee88dbfec585215096fa4cf2ff8b536b
Shu Ding <g@shud.in>
2023-06-25 16:57:52 +0200

    fix ssr page

--- test/e2e/app-dir/app-compilation/app/page-with-loading/page.js
+++ test/e2e/app-dir/app-compilation/app/page-with-loading/page.js
@@ -1,7 +1,7 @@
 import { use } from 'react'
 
 async function getData() {
-  await new Promise((resolve) => setTimeout(resolve, 4000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   return {
     message: 'hello from slow page',
   }
@@ -12,3 +12,5 @@ export default function SlowPage() {
 
   return <h1 id="slow-page-message">{data.message}</h1>
 }
+
+export const dynamic = 'force-dynamic'
