469332340a994ce9d88fc2d8937c3032f657bdf9
Max Leiter <max.leiter@vercel.com>
2023-06-27 12:06:58 -0700

    next/node-polyfill-web-streams: fix ReadableStream polyfill

--- packages/next/src/server/node-polyfill-web-streams.ts
+++ packages/next/src/server/node-polyfill-web-streams.ts
@@ -1,8 +1,14 @@
 // Polyfill Web Streams for the Node.js runtime.
 if (!global.ReadableStream) {
-  const { ReadableStream } =
-    require('next/dist/compiled/@edge-runtime/ponyfill') as typeof import('next/dist/compiled/@edge-runtime/ponyfill')
-  global.ReadableStream = ReadableStream
+  // In Node v16, ReadableStream is available natively but under the `stream` namespace.
+  // In Node v18+, it's available under global.
+  if (require('stream').ReadableStream) {
+    global.ReadableStream = require('stream').ReadableStream
+  } else {
+    const { ReadableStream } =
+      require('next/dist/compiled/@edge-runtime/ponyfill') as typeof import('next/dist/compiled/@edge-runtime/ponyfill')
+    global.ReadableStream = ReadableStream
+  }
 }
 if (!global.TransformStream) {
   const { TransformStream } =
