vercel/next.js

commit aeed1c770aee6e4765d90419ba4d4671dc759930

add support for globally providing mdx components to turbopack (#49818)
### What?

Adds support for `mdx-components` at the root/src directory.
This is a feature `next-mdx` already has:
https://github.com/vercel/next.js/tree/canary/packages/next-mdx#usage-1

Fixes WEB-1012

Depends on https://github.com/vercel/turbo/pull/4944

### Turbopack updates

* https://github.com/vercel/turbo/pull/4949 <!-- OJ Kwon -
refactor(turbopack): deprecate single react_refresh options -->
* https://github.com/vercel/turbo/pull/4944 <!-- Leah - add mdx
`providerImportSource` option -->

packages/next-swc/crates/next-core/src/next_import_map.rs
@@ -43,7 +43,13 @@ pub async fn get_next_client_import_map(
 ) -> Result<ImportMapVc> {
     let mut import_map = ImportMap::empty();
 
-    insert_next_shared_aliases(&mut import_map, project_path, execution_context).await?;
+    insert_next_shared_aliases(
+        &mut import_map,
+        project_path,
+        execution_context,
+        next_config,
+    )
+    .await?;
 
     insert_alias_option(
         &mut import_map,
@@ -187,7 +193,13 @@ pub async fn get_next_server_import_map(
 ) -> Result<ImportMapVc> {
     let mut import_map = ImportMap::empty();
 
-    insert_next_shared_aliases(&mut import_map, project_path, execution_context).await?;
+    insert_next_shared_aliases(
+        &mut import_map,
+        project_path,
+        execution_context,
+        next_config,
+    )
+    .await?;
 
     insert_alias_option(
         &mut import_map,
@@ -251,7 +263,13 @@ pub async fn get_next_edge_import_map(
 ) -> Result<ImportMapVc> {
     let mut import_map = ImportMap::empty();
 
-    insert_next_shared_aliases(&mut import_map, project_path, execution_context).await?;
+    insert_next_shared_aliases(
+        &mut import_map,
+        project_path,
+        execution_context,
+        next_config,
+    )
+    .await?;
 
     insert_alias_option(
         &mut import_map,
@@ -398,12 +416,29 @@ pub async fn insert_next_server_special_aliases(
     Ok(())
 }
 
+pub fn mdx_import_source_file() -> String {
+    format!("{VIRTUAL_PACKAGE_NAME}/mdx-import-source")
+}
+
 pub async fn insert_next_shared_aliases(
     import_map: &mut ImportMap,
     project_path: FileSystemPathVc,
     execution_context: ExecutionContextVc,
+    next_config: NextConfigVc,
 ) -> Result<()> {
     let package_root = next_js_fs().root();
+
+    if *next_config.mdx_rs().await? {
+        insert_alias_to_alternatives(
+            import_map,
+            mdx_import_source_file(),
+            vec![
+                request_to_import_mapping(project_path, "./mdx-components"),
+                request_to_import_mapping(project_path, "./src/mdx-components"),
+                external_request_to_import_mapping("@mdx-js/react"),
+            ],
+        );
+    }
 
     // we use the next.js hydration code, so we replace the error overlay with our
     // own

Cargo.toml
@@ -42,11 +42,11 @@ swc_core = { version = "0.75.41" }
 testing = { version = "0.33.4" }
 
 # Turbo crates
-turbopack-binding = { git = "https://github.com/vercel/turbo.git", tag = "turbopack-230515.2" }
+turbopack-binding = { git = "https://github.com/vercel/turbo.git", tag = "turbopack-230515.3" }
 # [TODO]: need to refactor embed_directory! macro usages, as well as resolving turbo_tasks::function, macros..
-turbo-tasks = { git = "https://github.com/vercel/turbo.git", tag = "turbopack-230515.2" }
+turbo-tasks = { git = "https://github.com/vercel/turbo.git", tag = "turbopack-230515.3" }
 # [TODO]: need to refactor embed_directory! macro usage in next-core
-turbo-tasks-fs = { git = "https://github.com/vercel/turbo.git", tag = "turbopack-230515.2" }
+turbo-tasks-fs = { git = "https://github.com/vercel/turbo.git", tag = "turbopack-230515.3" }
 
 # General Deps
 

packages/next-swc/crates/next-core/src/fallback.rs
@@ -57,7 +57,13 @@ pub async fn get_fallback_page(
     let entries = get_client_runtime_entries(project_path, env, ty, next_config, execution_context);
 
     let mut import_map = ImportMap::empty();
-    insert_next_shared_aliases(&mut import_map, project_path, execution_context).await?;
+    insert_next_shared_aliases(
+        &mut import_map,
+        project_path,
+        execution_context,
+        next_config,
+    )
+    .await?;
 
     let context: AssetContextVc = ModuleAssetContextVc::new(
         TransitionsByNameVc::cell(HashMap::new()),

packages/next-swc/crates/next-core/src/next_client/context.rs
@@ -30,8 +30,8 @@ use turbopack_binding::{
             module_options::{
                 module_options_context::{ModuleOptionsContext, ModuleOptionsContextVc},
                 CustomEcmascriptTransformPlugins, CustomEcmascriptTransformPluginsVc,
-                JsxTransformOptions, PostCssTransformOptions, TypescriptTransformOptions,
-                WebpackLoadersOptions,
+                JsxTransformOptions, MdxTransformModuleOptions, PostCssTransformOptions,
+                TypescriptTransformOptions, WebpackLoadersOptions,
             },
             resolve_options_context::{ResolveOptionsContext, ResolveOptionsContextVc},
             transition::TransitionsByNameVc,
@@ -50,7 +50,7 @@ use crate::{
     next_config::NextConfigVc,
     next_import_map::{
         get_next_client_fallback_import_map, get_next_client_import_map,
-        get_next_client_resolved_map,
+        get_next_client_resolved_map, mdx_import_source_file,
     },
     next_shared::{
         resolve::UnsupportedModulesResolvePluginVc, transforms::get_relay_transform_plugin,
@@ -173,7 +173,16 @@ pub async fn get_client_module_options_context(
 
     let tsconfig = get_typescript_transform_options(project_path);
     let decorators_options = get_decorators_transform_options(project_path);
-    let mdx_rs_options = *next_config.mdx_rs().await?;
+    let enable_mdx_rs = if *next_config.mdx_rs().await? {
+        Some(
+            MdxTransformModuleOptions {
+                provider_import_source: Some(mdx_import_source_file()),
+            }
+            .cell(),
+        )
+    } else {
+        None
+    };
     let jsx_runtime_options =
         get_jsx_transform_options(project_path, Some(resolve_options_context));
     let enable_webpack_loaders = {
@@ -242,7 +251,7 @@ pub async fn get_client_module_options_context(
         enable_postcss_transform: postcss_transform_options,
         enable_webpack_loaders,
         enable_typescript_transform: Some(tsconfig),
-        enable_mdx_rs: mdx_rs_options,
+        enable_mdx_rs,
         decorators: Some(decorators_options),
         rules: vec![
             (

packages/next-swc/crates/next-core/src/next_server/context.rs
@@ -25,8 +25,9 @@ use turbopack_binding::{
             condition::ContextCondition,
             module_options::{
                 CustomEcmascriptTransformPlugins, CustomEcmascriptTransformPluginsVc,
-                JsxTransformOptions, ModuleOptionsContext, ModuleOptionsContextVc,
-                PostCssTransformOptions, TypescriptTransformOptions, WebpackLoadersOptions,
+                JsxTransformOptions, MdxTransformModuleOptions, ModuleOptionsContext,
+                ModuleOptionsContextVc, PostCssTransformOptions, TypescriptTransformOptions,
+                WebpackLoadersOptions,
             },
             resolve_options_context::{ResolveOptionsContext, ResolveOptionsContextVc},
         },
@@ -41,7 +42,7 @@ use crate::{
     embed_js::next_js_fs,
     next_build::{get_external_next_compiled_package_mapping, get_postcss_package_mapping},
     next_config::NextConfigVc,
-    next_import_map::get_next_server_import_map,
+    next_import_map::{get_next_server_import_map, mdx_import_source_file},
     next_server::resolve::ExternalPredicate,
     next_shared::{
         resolve::UnsupportedModulesResolvePluginVc, transforms::get_relay_transform_plugin,
@@ -293,7 +294,16 @@ pub async fn get_server_module_options_context(
 
     let tsconfig = get_typescript_transform_options(project_path);
     let decorators_options = get_decorators_transform_options(project_path);
-    let mdx_rs_options = *next_config.mdx_rs().await?;
+    let enable_mdx_rs = if *next_config.mdx_rs().await? {
+        Some(
+            MdxTransformModuleOptions {
+                provider_import_source: Some(mdx_import_source_file()),
+            }
+            .cell(),
+        )
+    } else {
+        None
+    };
     let jsx_runtime_options = get_jsx_transform_options(project_path, None);
     let enable_emotion = *get_emotion_compiler_config(next_config).await?;
     let enable_styled_components = *get_styled_components_compiler_config(next_config).await?;
@@ -332,7 +342,7 @@ pub async fn get_server_module_options_context(
                 enable_postcss_transform,
                 enable_webpack_loaders,
                 enable_typescript_transform: Some(tsconfig),
-                enable_mdx_rs: mdx_rs_options,
+                enable_mdx_rs,
                 decorators: Some(decorators_options),
                 rules: vec![
                     (
@@ -390,7 +400,7 @@ pub async fn get_server_module_options_context(
                 enable_postcss_transform,
                 enable_webpack_loaders,
                 enable_typescript_transform: Some(tsconfig),
-                enable_mdx_rs: mdx_rs_options,
+                enable_mdx_rs,
                 decorators: Some(decorators_options),
                 rules: vec![
                     (
@@ -448,7 +458,7 @@ pub async fn get_server_module_options_context(
                 enable_postcss_transform,
                 enable_webpack_loaders,
                 enable_typescript_transform: Some(tsconfig),
-                enable_mdx_rs: mdx_rs_options,
+                enable_mdx_rs,
                 decorators: Some(decorators_options),
                 rules: vec![
                     (
@@ -478,7 +488,7 @@ pub async fn get_server_module_options_context(
                 enable_postcss_transform,
                 enable_webpack_loaders,
                 enable_typescript_transform: Some(tsconfig),
-                enable_mdx_rs: mdx_rs_options,
+                enable_mdx_rs,
                 decorators: Some(decorators_options),
                 rules: vec![
                     (
@@ -512,7 +522,7 @@ pub async fn get_server_module_options_context(
                 enable_postcss_transform,
                 enable_webpack_loaders,
                 enable_typescript_transform: Some(tsconfig),
-                enable_mdx_rs: mdx_rs_options,
+                enable_mdx_rs,
                 decorators: Some(decorators_options),
                 rules: vec![
                     (

pnpm-lock.yaml
@@ -1014,8 +1014,8 @@ importers:
       '@types/react': 18.2.5
       '@types/react-dom': 18.2.3
       '@vercel/ncc': ^0.36.0
-      '@vercel/turbopack-dev': https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230511.2
-      '@vercel/turbopack-node': https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230511.2
+      '@vercel/turbopack-dev': https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230515.3
+      '@vercel/turbopack-node': https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230515.3
       anser: ^2.1.1
       css.escape: ^1.5.1
       find-up: ^6.3.0
@@ -1027,8 +1027,8 @@ importers:
       stacktrace-parser: ^0.1.10
       strip-ansi: ^7.0.1
     dependencies:
-      '@vercel/turbopack-dev': '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230511.2_react-refresh@0.12.0'
-      '@vercel/turbopack-node': '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230511.2'
+      '@vercel/turbopack-dev': '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230515.3_react-refresh@0.12.0'
+      '@vercel/turbopack-node': '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230515.3'
       anser: 2.1.1
       css.escape: 1.5.1
       next: link:../../../../next
@@ -5998,7 +5998,7 @@ packages:
     dependencies:
       '@mdx-js/mdx': 2.2.1
       source-map: 0.7.3
-      webpack: 5.74.0
+      webpack: 5.74.0_@swc+core@1.3.55
     transitivePeerDependencies:
       - supports-color
 
@@ -6668,7 +6668,6 @@ packages:
     cpu: [arm64]
     os: [darwin]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-darwin-x64/1.3.55:
@@ -6677,7 +6676,6 @@ packages:
     cpu: [x64]
     os: [darwin]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-linux-arm-gnueabihf/1.3.55:
@@ -6686,7 +6684,6 @@ packages:
     cpu: [arm]
     os: [linux]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-linux-arm64-gnu/1.3.55:
@@ -6695,7 +6692,6 @@ packages:
     cpu: [arm64]
     os: [linux]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-linux-arm64-musl/1.3.55:
@@ -6704,7 +6700,6 @@ packages:
     cpu: [arm64]
     os: [linux]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-linux-x64-gnu/1.3.55:
@@ -6713,7 +6708,6 @@ packages:
     cpu: [x64]
     os: [linux]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-linux-x64-musl/1.3.55:
@@ -6722,7 +6716,6 @@ packages:
     cpu: [x64]
     os: [linux]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-win32-arm64-msvc/1.3.55:
@@ -6731,7 +6724,6 @@ packages:
     cpu: [arm64]
     os: [win32]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-win32-ia32-msvc/1.3.55:
@@ -6740,7 +6732,6 @@ packages:
     cpu: [ia32]
     os: [win32]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core-win32-x64-msvc/1.3.55:
@@ -6749,7 +6740,6 @@ packages:
     cpu: [x64]
     os: [win32]
     requiresBuild: true
-    dev: true
     optional: true
 
   /@swc/core/1.3.55_@swc+helpers@0.5.1:
@@ -6774,7 +6764,6 @@ packages:
       '@swc/core-win32-arm64-msvc': 1.3.55
       '@swc/core-win32-ia32-msvc': 1.3.55
       '@swc/core-win32-x64-msvc': 1.3.55
-    dev: true
 
   /@swc/helpers/0.4.14:
     resolution: {integrity: sha512-4C7nX/dvpzB7za4Ql9K81xK3HPxCpHMgwTZVyf+9JQ6VUbn9jjZVN7/Nkdz/Ugzs2CSjqnL/UPXroiVBVHUWUw==}
@@ -23777,7 +23766,6 @@ packages:
       source-map: 0.6.1
       terser: 5.14.1
       webpack: 5.74.0_@swc+core@1.3.55
-    dev: true
 
   /terser-webpack-plugin/5.2.4_webpack@5.74.0:
     resolution: {integrity: sha512-E2CkNMN+1cho04YpdANyRrn8CyN4yMy+WdFKZIySFZrGXZxJwJP6PMNGGc/Mcr6qygQHUUqRxnAPmi0M9f00XA==}
@@ -25183,7 +25171,6 @@ packages:
       - '@swc/core'
       - esbuild
       - uglify-js
-    dev: true
 
   /websocket-driver/0.7.3:
     resolution: {integrity: sha512-bpxWlvbbB459Mlipc5GBzzZwhoZgGEZLuqPaR0INBGnPAY1vdBX6hPnoFXiw+3yWxDuHyQjO2oXTMyS8A5haFg==}
@@ -25589,9 +25576,9 @@ packages:
   /zwitch/2.0.4:
     resolution: {integrity: sha512-bXE4cR/kVZhKZX/RjPEflHaKVhUVl85noU3v6b8apfQEc1x4A+zBxjZ4lN8LqGd6WZ3dl98pY4o717VFmoPp+A==}
 
-  '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230511.2_react-refresh@0.12.0':
-    resolution: {tarball: https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230511.2}
-    id: '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230511.2'
+  '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230515.3_react-refresh@0.12.0':
+    resolution: {tarball: https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230515.3}
+    id: '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-dev/js?turbopack-230515.3'
     name: '@vercel/turbopack-dev'
     version: 0.0.0
     dependencies:
@@ -25601,8 +25588,8 @@ packages:
       - webpack
     dev: false
 
-  '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230511.2':
-    resolution: {tarball: https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230511.2}
+  '@gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230515.3':
+    resolution: {tarball: https://gitpkg.vercel.app/vercel/turbo/crates/turbopack-node/js?turbopack-230515.3}
     name: '@vercel/turbopack-node'
     version: 0.0.0
     dependencies:

test/integration/plugin-mdx-rs/components/marker.js
@@ -1,0 +1,3 @@
+export function Marker() {
+  return <span>Marker was rendered!</span>
+}

test/integration/plugin-mdx-rs/mdx-components.js
@@ -1,0 +1,8 @@
+import { Marker } from './components/marker'
+
+export function useMDXComponents(components) {
+  return {
+    ...components,
+    Marker,
+  }
+}

commit f1a4f7ad7d2f77bfa189fe66665e999f4b41d6d0

fix turbopack mdx import map (#51881)
I accidentally broke this with #49818

packages/next-swc/crates/next-core/src/next_import_map.rs
@@ -33,6 +33,7 @@ use crate::{
     next_server::context::ServerContextType,
 };
 
+// Make sure to not add any external requests here.
 /// Computes the Next-specific client import map.
 #[turbo_tasks::function]
 pub async fn get_next_client_import_map(
@@ -426,6 +427,7 @@ pub fn mdx_import_source_file() -> String {
     format!("{VIRTUAL_PACKAGE_NAME}/mdx-import-source")
 }
 
+// Make sure to not add any external requests here.
 pub async fn insert_next_shared_aliases(
     import_map: &mut ImportMap,
     project_path: FileSystemPathVc,
@@ -441,7 +443,7 @@ pub async fn insert_next_shared_aliases(
             vec![
                 request_to_import_mapping(project_path, "./mdx-components"),
                 request_to_import_mapping(project_path, "./src/mdx-components"),
-                external_request_to_import_mapping("@mdx-js/react"),
+                request_to_import_mapping(project_path, "@mdx-js/react"),
             ],
         );
     }