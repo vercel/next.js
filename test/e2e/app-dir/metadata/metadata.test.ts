3b9ad16a23aaa4d555b69eff0eb62ad38c7d98b0
Jiachi Liu <inbox@huozhi.im>
2023-06-30 11:38:32 +0200

    fix matcher

--- test/e2e/app-dir/metadata/metadata.test.ts
+++ test/e2e/app-dir/metadata/metadata.test.ts
@@ -31,7 +31,9 @@ createNextDescribe(
         if (Array.isArray(expected)) {
           expect(values).toEqual(expected)
         } else {
-          expect(values.includes(expected)).toBe(true)
+          // If expected is undefined, then it should not exist.
+          // Otherwise, it should exist in the matched values.
+          expect(values.includes(expected)).toBe(expected !== undefined)
         }
       }
     }
