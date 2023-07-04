0e178ae6568a59a73992f88c6289fb72cf2919a3
Jimmy Lai <laijimmy0@gmail.com>
2023-07-05 00:35:56 +0200

    revert lazy polyfill

--- packages/next/src/server/node-polyfill-form.ts
+++ packages/next/src/server/node-polyfill-form.ts
@@ -2,22 +2,12 @@
  * Polyfills `FormData` and `Blob` in the Node.js runtime.
  */
 
-if (!(global as any).FormData) {
-  Object.defineProperty(global, 'FormData', {
-    get() {
-      const { FormData } =
-        require('next/dist/compiled/@edge-runtime/ponyfill') as typeof import('next/dist/compiled/@edge-runtime/ponyfill')
-      return FormData
-    },
-  })
+if (!global.FormData) {
+  const { FormData } = require('next/dist/compiled/@edge-runtime/ponyfill')
+  global.FormData = FormData
 }
 
-if (!(global as any).Blob) {
-  Object.defineProperty(global, 'Blob', {
-    get() {
-      const { Blob } =
-        require('next/dist/compiled/@edge-runtime/ponyfill') as typeof import('next/dist/compiled/@edge-runtime/ponyfill')
-      return Blob
-    },
-  })
+if (!global.Blob) {
+  const { Blob } = require('next/dist/compiled/@edge-runtime/ponyfill')
+  global.Blob = Blob
 }
