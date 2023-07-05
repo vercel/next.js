2e486f4f980c3ad725468886c86a9a67026a3916
Shu Ding <g@shud.in>
2023-07-05 14:34:36 +0200

    Improve error message when using per function "use server" in Client Components (#52214)
    The current message isn't very clear about `"use server" function` and
    `"use server" file`, and there's no link to docs to explain it further:
    
    ```
    "use server" functions are not allowed in client components. 
    You can import them from a "use server" file instead.
    ```
    
    This PR makes it a bit more verbose:
    
    ```
    It is not allowed to define inline "use server" annotated Server Actions in Client Components.
    You can either mark the entire file by putting "use server" at the top, or pass them down through props from a Server Component.
    
    Read more: https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions#with-client-components
    ```

--- packages/next-swc/crates/core/tests/fixture.rs
+++ packages/next-swc/crates/core/tests/fixture.rs
@@ -325,7 +325,10 @@ fn server_actions_server_fixture(input: PathBuf) {
                 resolver(Mark::new(), Mark::new(), false),
                 server_actions(
                     &FileName::Real("/app/item.js".into()),
-                    server_actions::Config { is_server: true },
+                    server_actions::Config {
+                        is_server: true,
+                        enabled: true
+                    },
                     _tr.comments.as_ref().clone(),
                 )
             )
@@ -346,7 +349,10 @@ fn server_actions_client_fixture(input: PathBuf) {
                 resolver(Mark::new(), Mark::new(), false),
                 server_actions(
                     &FileName::Real("/app/item.js".into()),
-                    server_actions::Config { is_server: false },
+                    server_actions::Config {
+                        is_server: false,
+                        enabled: true
+                    },
                     _tr.comments.as_ref().clone(),
                 )
             )
