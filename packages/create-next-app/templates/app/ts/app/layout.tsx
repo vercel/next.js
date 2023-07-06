d9b8e60e5ce4897a6806ae9f587541708dd5f34c
Yiğithan <yigithanyucedag@gmail.com>
2023-07-06 17:52:30 +0300

    Apply suggestions from code review
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/create-next-app/templates/app/ts/app/layout.tsx
+++ packages/create-next-app/templates/app/ts/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Metadata } from "next";
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
