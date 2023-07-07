803e89fc03b3b9a8dd69c79282110ff91e366b09
Jiachi Liu <inbox@huozhi.im>
2023-07-07 18:59:03 +0200

    Simplify react require hooks

--- packages/next/src/server/require-hook.ts
+++ packages/next/src/server/require-hook.ts
@@ -9,8 +9,6 @@ const mod = require('module')
 const resolveFilename = mod._resolveFilename
 const hookPropertyMap = new Map()
 
-let aliasedPrebundledReact = false
-
 export function addHookAliases(aliases: [string, string][] = []) {
   for (const [key, value] of aliases) {
     hookPropertyMap.set(key, value)
@@ -25,159 +23,26 @@ addHookAliases([
   ['styled-jsx/style', require.resolve('styled-jsx/style')],
 ])
 
-// Override built-in React packages if necessary
-function overrideReact() {
-  if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT) {
-    aliasedPrebundledReact = true
+function mapReactRequest(request: string) {
+  if (
+    process.env.__NEXT_PRIVATE_PREBUNDLED_REACT &&
+    /^(react|react-dom|react-server-dom-webpack)\/?/.test(request)
+  ) {
+    const [packageName] = request.split('/')
+    const packagePath = request.slice(packageName.length) // eg: /server
+    const aliasedPackage =
+      process.env.__NEXT_PRIVATE_PREBUNDLED_REACT === 'experimental'
+        ? `${packageName}-experimental`
+        : packageName
+    const mapped = `next/dist/compiled/${aliasedPackage}${packagePath}`
 
-    // Require these modules with static paths to make sure they are tracked by
-    // NFT when building the app in standalone mode, as we are now conditionally
-    // aliasing them it's tricky to track them in build time.
-    if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT === 'experimental') {
-      addHookAliases([
-        ['react', require.resolve(`next/dist/compiled/react-experimental`)],
-        [
-          'react/jsx-runtime',
-          require.resolve(`next/dist/compiled/react-experimental/jsx-runtime`),
-        ],
-        [
-          'react/jsx-dev-runtime',
-          require.resolve(
-            `next/dist/compiled/react-experimental/jsx-dev-runtime`
-          ),
-        ],
-        [
-          'react-dom',
-          require.resolve(
-            `next/dist/compiled/react-dom-experimental/server-rendering-stub`
-          ),
-        ],
-        [
-          'react/package.json',
-          require.resolve(`next/dist/compiled/react-experimental/package.json`),
-        ],
-        [
-          'react-dom/package.json',
-          require.resolve(
-            `next/dist/compiled/react-dom-experimental/package.json`
-          ),
-        ],
-        [
-          'react-dom/client',
-          require.resolve(`next/dist/compiled/react-dom-experimental/client`),
-        ],
-        [
-          'react-dom/server',
-          require.resolve(`next/dist/compiled/react-dom-experimental/server`),
-        ],
-        [
-          'react-dom/server.browser',
-          require.resolve(
-            `next/dist/compiled/react-dom-experimental/server.browser`
-          ),
-        ],
-        [
-          'react-dom/server.edge',
-          require.resolve(
-            `next/dist/compiled/react-dom-experimental/server.edge`
-          ),
-        ],
-        [
-          'react-server-dom-webpack/client',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack-experimental/client`
-          ),
-        ],
-        [
-          'react-server-dom-webpack/client.edge',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack-experimental/client.edge`
-          ),
-        ],
-        [
-          'react-server-dom-webpack/server.edge',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack-experimental/server.edge`
-          ),
-        ],
-        [
-          'react-server-dom-webpack/server.node',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack-experimental/server.node`
-          ),
-        ],
-      ])
-    } else {
-      addHookAliases([
-        ['react', require.resolve(`next/dist/compiled/react`)],
-        [
-          'react/package.json',
-          require.resolve(`next/dist/compiled/react/package.json`),
-        ],
-        [
-          'react/jsx-runtime',
-          require.resolve(`next/dist/compiled/react/jsx-runtime`),
-        ],
-        [
-          'react/jsx-dev-runtime',
-          require.resolve(`next/dist/compiled/react/jsx-dev-runtime`),
-        ],
-        [
-          'react-dom',
-          require.resolve(`next/dist/compiled/react-dom/server-rendering-stub`),
-        ],
-        [
-          'react-dom/package.json',
-          require.resolve(`next/dist/compiled/react-dom/package.json`),
-        ],
-        [
-          'react-dom/client',
-          require.resolve(`next/dist/compiled/react-dom/client`),
-        ],
-        [
-          'react-dom/server',
-          require.resolve(`next/dist/compiled/react-dom/server`),
-        ],
-        [
-          'react-dom/server.browser',
-          require.resolve(`next/dist/compiled/react-dom/server.browser`),
-        ],
-        [
-          'react-dom/server.edge',
-          require.resolve(`next/dist/compiled/react-dom/server.edge`),
-        ],
-        [
-          'react-server-dom-webpack/client',
-          require.resolve(`next/dist/compiled/react-server-dom-webpack/client`),
-        ],
-        [
-          'react-server-dom-webpack/client.edge',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack/client.edge`
-          ),
-        ],
-        [
-          'react-server-dom-webpack/server.edge',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack/server.edge`
-          ),
-        ],
-        [
-          'react-server-dom-webpack/server.node',
-          require.resolve(
-            `next/dist/compiled/react-server-dom-webpack/server.node`
-          ),
-        ],
-      ])
-    }
-  } else {
-    addHookAliases([
-      ['react/jsx-runtime', require.resolve(`react/jsx-runtime`)],
-      ['react/jsx-dev-runtime', require.resolve(`react/jsx-dev-runtime`)],
-    ])
+    // Cache the resolved request for future lookups
+    hookPropertyMap.set(request, mapped)
+
+    return mapped
   }
+  return null
 }
-overrideReact()
 
 mod._resolveFilename = function (
   originalResolveFilename: typeof resolveFilename,
@@ -187,14 +52,14 @@ mod._resolveFilename = function (
   isMain: boolean,
   options: any
 ) {
-  if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT && !aliasedPrebundledReact) {
-    // In case the environment variable is set after the module is loaded.
-    overrideReact()
+  const hookResolved = requestMap.get(request)
+
+  if (hookResolved) request = hookResolved
+  else {
+    const mappedReactRequest = mapReactRequest(request)
+    if (mappedReactRequest) request = mappedReactRequest
   }
 
-  const hookResolved = requestMap.get(request)
-  if (hookResolved) request = hookResolved
   return originalResolveFilename.call(mod, request, parent, isMain, options)
-
   // We use `bind` here to avoid referencing outside variables to create potential memory leaks.
 }.bind(null, resolveFilename, hookPropertyMap)
