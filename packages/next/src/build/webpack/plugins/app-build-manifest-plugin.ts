vercel/next.js

commit 990c58c5ef20b3c382fc395976d873daac61e336

Split the client reference manifest file to be generated per-entry (#52450)
This PR changes client manifest generation process. Instead of one big
manifest file that contains client references for the entire app, we're
now generating one manifest file per entry which only covers client
components that can be reached in the module graph.

packages/next/src/build/webpack/plugins/app-build-manifest-plugin.ts
@@ -1,6 +1,7 @@
 import { webpack, sources } from 'next/dist/compiled/webpack/webpack'
 import {
   APP_BUILD_MANIFEST,
+  CLIENT_REFERENCE_MANIFEST,
   CLIENT_STATIC_FILES_RUNTIME_MAIN_APP,
   SYSTEM_ENTRYPOINTS,
 } from '../../../shared/lib/constants'
@@ -76,8 +77,22 @@ export class AppBuildManifestPlugin {
       }
 
       const filesForPage = getEntrypointFiles(entrypoint)
+      const manifestsForPage =
+        pagePath.endsWith('/page') ||
+        pagePath === '/not-found' ||
+        pagePath === '/_not-found'
+          ? [
+              'server/app' +
+                pagePath.replace(/%5F/g, '_') +
+                '_' +
+                CLIENT_REFERENCE_MANIFEST +
+                '.js',
+            ]
+          : []
 
-      manifest.pages[pagePath] = [...new Set([...mainFiles, ...filesForPage])]
+      manifest.pages[pagePath] = [
+        ...new Set([...mainFiles, ...manifestsForPage, ...filesForPage]),
+      ]
     }
 
     const json = JSON.stringify(manifest, null, 2)

packages/next/src/server/base-server.ts
@@ -270,7 +270,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   protected router: Router
   protected appPathRoutes?: Record<string, string[]>
   protected customRoutes: CustomRoutes
-  protected clientReferenceManifest?: ClientReferenceManifest
   protected nextFontManifest?: NextFontManifest
   public readonly hostname?: string
   public readonly port?: number
@@ -294,7 +293,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   }): Promise<FindComponentsResult | null>
   protected abstract getFontManifest(): FontManifest | undefined
   protected abstract getPrerenderManifest(): PrerenderManifest
-  protected abstract getServerComponentManifest(): any
+  // protected abstract getServerComponentManifest(): any
   protected abstract getNextFontManifest(): NextFontManifest | undefined
   protected abstract attachRequestMeta(
     req: BaseNextRequest,
@@ -419,9 +418,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     this.hasAppDir =
       !!this.nextConfig.experimental.appDir && this.getHasAppDir(dev)
     const serverComponents = this.hasAppDir
-    this.clientReferenceManifest = serverComponents
-      ? this.getServerComponentManifest()
-      : undefined
+
     this.nextFontManifest = this.getNextFontManifest()
 
     if (process.env.NEXT_RUNTIME !== 'edge') {
@@ -1389,9 +1386,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     }
 
     // Don't delete headers[RSC] yet, it still needs to be used in renderToHTML later
-    const isFlightRequest = Boolean(
-      this.clientReferenceManifest && req.headers[RSC.toLowerCase()]
-    )
+    const isFlightRequest = Boolean(req.headers[RSC.toLowerCase()])
 
     // For pages we need to ensure the correct Vary header is set too, to avoid
     // caching issues when navigating between pages and app
@@ -1781,9 +1776,10 @@ export default abstract class Server<ServerOptions extends Options = Options> {
         components.routeModule
       ) {
         const module = components.routeModule as PagesRouteModule
+        renderOpts.clientReferenceManifest = components.clientReferenceManifest
 
         // Due to the way we pass data by mutating `renderOpts`, we can't extend
-        // the object here but only updating its `clientReferenceManifest`
+        // the object here but only updating its `nextFontManifest`
         // field.
         // https://github.com/vercel/next.js/blob/df7cbd904c3bd85f399d1ce90680c0ecf92d2752/packages/next/server/render.tsx#L947-L952
         renderOpts.nextFontManifest = this.nextFontManifest

test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
@@ -30,7 +30,13 @@ createNextDescribe(
         await check(async () => {
           // Check that the client-side manifest is correct before any requests
           const clientReferenceManifest = JSON.parse(
-            await next.readFile('.next/server/client-reference-manifest.json')
+            JSON.parse(
+              (
+                await next.readFile(
+                  '.next/server/app/page_client-reference-manifest.js'
+                )
+              ).match(/]=(.+)$/)[1]
+            )
           )
           const clientModulesNames = Object.keys(
             clientReferenceManifest.clientModules
@@ -546,7 +552,6 @@ createNextDescribe(
         const files = [
           'middleware-build-manifest.js',
           'middleware-manifest.json',
-          'client-reference-manifest.json',
         ]
 
         let promises = files.map(async (file) => {
@@ -558,6 +563,16 @@ createNextDescribe(
           expect(await next.hasFile(file)).toBe(true)
         })
         await Promise.all(promises)
+      })
+
+      it('should generate client reference manifest for edge SSR pages', async () => {
+        const buildManifest = JSON.parse(
+          await next.readFile('.next/app-build-manifest.json')
+        )
+
+        expect(buildManifest.pages['/edge/dynamic/page']).toInclude(
+          'server/app/edge/dynamic/page_client-reference-manifest.js'
+        )
       })
     }
   }

packages/next/src/build/index.ts
@@ -52,7 +52,6 @@ import {
   PAGES_MANIFEST,
   PHASE_PRODUCTION_BUILD,
   PRERENDER_MANIFEST,
-  CLIENT_REFERENCE_MANIFEST,
   REACT_LOADABLE_MANIFEST,
   ROUTES_MANIFEST,
   SERVER_DIRECTORY,
@@ -905,14 +904,6 @@ export default async function build(
                   APP_BUILD_MANIFEST,
                   path.join(
                     SERVER_DIRECTORY,
-                    CLIENT_REFERENCE_MANIFEST + '.js'
-                  ),
-                  path.join(
-                    SERVER_DIRECTORY,
-                    CLIENT_REFERENCE_MANIFEST + '.json'
-                  ),
-                  path.join(
-                    SERVER_DIRECTORY,
                     SERVER_REFERENCE_MANIFEST + '.js'
                   ),
                   path.join(
@@ -1487,7 +1478,6 @@ export default async function build(
                             pageRuntime,
                             edgeInfo,
                             pageType,
-                            hasServerComponents: !!appDir,
                             incrementalCacheHandlerPath:
                               config.experimental.incrementalCacheHandlerPath,
                             isrFlushToDisk: config.experimental.isrFlushToDisk,

packages/next/src/build/utils.ts
@@ -1339,7 +1339,6 @@ export async function isPageStatic({
   pageRuntime,
   edgeInfo,
   pageType,
-  hasServerComponents,
   originalAppPath,
   isrFlushToDisk,
   maxMemoryCacheSize,
@@ -1356,7 +1355,6 @@ export async function isPageStatic({
   edgeInfo?: any
   pageType?: 'pages' | 'app'
   pageRuntime?: ServerRuntime
-  hasServerComponents?: boolean
   originalAppPath?: string
   isrFlushToDisk?: boolean
   maxMemoryCacheSize?: number
@@ -1425,7 +1423,6 @@ export async function isPageStatic({
         componentsResult = await loadComponents({
           distDir,
           pathname: originalAppPath || page,
-          hasServerComponents: !!hasServerComponents,
           isAppPath: pageType === 'app',
         })
       }
@@ -1669,7 +1666,6 @@ export async function hasCustomGetInitialProps(
   const components = await loadComponents({
     distDir,
     pathname: page,
-    hasServerComponents: false,
     isAppPath: false,
   })
   let mod = components.ComponentMod
@@ -1692,7 +1688,6 @@ export async function getDefinedNamedExports(
   const components = await loadComponents({
     distDir,
     pathname: page,
-    hasServerComponents: false,
     isAppPath: false,
   })
 

packages/next/src/build/webpack/loaders/next-edge-ssr-loader/index.ts
@@ -216,7 +216,9 @@ const edgeSSRLoader: webpack.LoaderDefinitionFunction<EdgeSSRLoaderQuery> =
     const buildManifest = self.__BUILD_MANIFEST
     const prerenderManifest = maybeJSONParse(self.__PRERENDER_MANIFEST)
     const reactLoadableManifest = maybeJSONParse(self.__REACT_LOADABLE_MANIFEST)
-    const rscManifest = maybeJSONParse(self.__RSC_MANIFEST)
+    const rscManifest = maybeJSONParse(self.__RSC_MANIFEST?.[${JSON.stringify(
+      page
+    )}])
     const rscServerManifest = maybeJSONParse(self.__RSC_SERVER_MANIFEST)
     const subresourceIntegrityManifest = ${
       sriEnabled

packages/next/src/build/webpack/loaders/next-edge-ssr-loader/render.ts
@@ -70,6 +70,7 @@ export function getRender({
     nextFontManifest,
     Document,
     App: appMod?.default as AppType,
+    clientReferenceManifest,
   }
 
   const server = new WebServer({
@@ -86,7 +87,6 @@ export function getRender({
         runtime: SERVER_RUNTIME.experimentalEdge,
         supportsDynamicHTML: true,
         disableOptimizedLoading: true,
-        clientReferenceManifest,
         serverActionsManifest,
         serverActionsBodySizeLimit,
       },

packages/next/src/build/webpack/plugins/flight-manifest-plugin.ts
@@ -71,6 +71,43 @@ export type ClientReferenceManifest = {
   }
 }
 
+function getAppPathRequiredChunks(chunkGroup: webpack.ChunkGroup) {
+  return chunkGroup.chunks
+    .map((requiredChunk: webpack.Chunk) => {
+      if (SYSTEM_ENTRYPOINTS.has(requiredChunk.name || '')) {
+        return null
+      }
+
+      // Get the actual chunk file names from the chunk file list.
+      // It's possible that the chunk is generated via `import()`, in
+      // that case the chunk file name will be '[name].[contenthash]'
+      // instead of '[name]-[chunkhash]'.
+      return [...requiredChunk.files].map((file) => {
+        // It's possible that a chunk also emits CSS files, that will
+        // be handled separatedly.
+        if (!file.endsWith('.js')) return null
+        if (file.endsWith('.hot-update.js')) return null
+
+        return requiredChunk.id + ':' + file
+      })
+    })
+    .flat()
+    .filter(nonNullable)
+}
+
+function mergeManifest(
+  manifest: ClientReferenceManifest,
+  manifestToMerge: ClientReferenceManifest
+) {
+  Object.assign(manifest.clientModules, manifestToMerge.clientModules)
+  Object.assign(manifest.ssrModuleMapping, manifestToMerge.ssrModuleMapping)
+  Object.assign(
+    manifest.edgeSSRModuleMapping,
+    manifestToMerge.edgeSSRModuleMapping
+  )
+  Object.assign(manifest.entryCSSFiles, manifestToMerge.entryCSSFiles)
+}
+
 const PLUGIN_NAME = 'ClientReferenceManifestPlugin'
 
 export class ClientReferenceManifestPlugin {
@@ -116,43 +153,21 @@ export class ClientReferenceManifestPlugin {
     compilation: webpack.Compilation,
     context: string
   ) {
-    const manifest: ClientReferenceManifest = {
-      ssrModuleMapping: {},
-      edgeSSRModuleMapping: {},
-      clientModules: {},
-      entryCSSFiles: {},
-    }
+    const manifestsPerGroup = new Map<string, ClientReferenceManifest[]>()
 
     compilation.chunkGroups.forEach((chunkGroup) => {
-      function getAppPathRequiredChunks() {
-        return chunkGroup.chunks
-          .map((requiredChunk: webpack.Chunk) => {
-            if (SYSTEM_ENTRYPOINTS.has(requiredChunk.name || '')) {
-              return null
-            }
-
-            // Get the actual chunk file names from the chunk file list.
-            // It's possible that the chunk is generated via `import()`, in
-            // that case the chunk file name will be '[name].[contenthash]'
-            // instead of '[name]-[chunkhash]'.
-            return [...requiredChunk.files].map((file) => {
-              // It's possible that a chunk also emits CSS files, that will
-              // be handled separatedly.
-              if (!file.endsWith('.js')) return null
-              if (file.endsWith('.hot-update.js')) return null
-
-              return requiredChunk.id + ':' + file
-            })
-          })
-          .flat()
-          .filter(nonNullable)
+      // By default it's the shared chunkGroup (main-app) for every page.
+      let entryName = ''
+      const manifest: ClientReferenceManifest = {
+        ssrModuleMapping: {},
+        edgeSSRModuleMapping: {},
+        clientModules: {},
+        entryCSSFiles: {},
       }
-      const requiredChunks = getAppPathRequiredChunks()
 
-      let chunkEntryName: string | null = null
       if (chunkGroup.name && /^app[\\/]/.test(chunkGroup.name)) {
         // Absolute path without the extension
-        chunkEntryName = (this.appDirBase + chunkGroup.name).replace(
+        const chunkEntryName = (this.appDirBase + chunkGroup.name).replace(
           /[\\/]/g,
           path.sep
         )
@@ -161,8 +176,11 @@ export class ClientReferenceManifestPlugin {
           .filter(
             (f) => !f.startsWith('static/css/pages/') && f.endsWith('.css')
           )
+
+        entryName = chunkGroup.name
       }
 
+      const requiredChunks = getAppPathRequiredChunks(chunkGroup)
       const recordModule = (id: ModuleId, mod: webpack.NormalModule) => {
         // Skip all modules from the pages folder.
         if (mod.layer !== WEBPACK_LAYERS.appClient) {
@@ -311,22 +329,95 @@ export class ClientReferenceManifestPlugin {
           }
         }
       })
+
+      // A page's entry name can have extensions. For example, these are both valid:
+      // - app/foo/page
+      // - app/foo/page.page
+      // Let's normalize the entry name to remove the extra extension
+      const groupName = /\/page(\.[^/]+)?$/.test(entryName)
+        ? entryName.replace(/\/page(\.[^/]+)?$/, '/page')
+        : entryName.slice(0, entryName.lastIndexOf('/'))
+
+      if (!manifestsPerGroup.has(groupName)) {
+        manifestsPerGroup.set(groupName, [])
+      }
+      manifestsPerGroup.get(groupName)!.push(manifest)
+
+      if (entryName.includes('/@')) {
+        // Remove parallel route labels:
+        // - app/foo/@bar/page -> app/foo
+        // - app/foo/@bar/layout -> app/foo/layout -> app/foo
+        const entryNameWithoutNamedSegments = entryName.replace(/\/@[^/]+/g, '')
+        const groupNameWithoutNamedSegments =
+          entryNameWithoutNamedSegments.slice(
+            0,
+            entryNameWithoutNamedSegments.lastIndexOf('/')
+          )
+        if (!manifestsPerGroup.has(groupNameWithoutNamedSegments)) {
+          manifestsPerGroup.set(groupNameWithoutNamedSegments, [])
+        }
+        manifestsPerGroup.get(groupNameWithoutNamedSegments)!.push(manifest)
+      }
+
+      // Special case for the root not-found page.
+      if (/^app\/not-found(\.[^.]+)?$/.test(entryName)) {
+        if (!manifestsPerGroup.has('app/not-found')) {
+          manifestsPerGroup.set('app/not-found', [])
+        }
+        manifestsPerGroup.get('app/not-found')!.push(manifest)
+      }
     })
 
-    const file = 'server/' + CLIENT_REFERENCE_MANIFEST
-    const json = JSON.stringify(manifest, null, this.dev ? 2 : undefined)
+    // Generate per-page manifests.
+    for (const [groupName] of manifestsPerGroup) {
+      if (groupName.endsWith('/page') || groupName === 'app/not-found') {
+        const mergedManifest: ClientReferenceManifest = {
+          ssrModuleMapping: {},
+          edgeSSRModuleMapping: {},
+          clientModules: {},
+          entryCSSFiles: {},
+        }
+
+        const segments = groupName.split('/')
+        let group = ''
+        for (const segment of segments) {
+          if (segment.startsWith('@')) continue
+          for (const manifest of manifestsPerGroup.get(group) || []) {
+            mergeManifest(mergedManifest, manifest)
+          }
+          group += (group ? '/' : '') + segment
+        }
+        for (const manifest of manifestsPerGroup.get(groupName) || []) {
+          mergeManifest(mergedManifest, manifest)
+        }
+
+        const json = JSON.stringify(mergedManifest)
+
+        const pagePath = groupName.replace(/%5F/g, '_')
+        assets['server/' + pagePath + '_' + CLIENT_REFERENCE_MANIFEST + '.js'] =
+          new sources.RawSource(
+            `globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST[${JSON.stringify(
+              pagePath.slice('app'.length)
+            )}]=${JSON.stringify(json)}`
+          ) as unknown as webpack.sources.RawSource
+
+        if (pagePath === 'app/not-found') {
+          // Create a separate special manifest for the root not-found page.
+          assets[
+            'server/' +
+              'app/_not-found' +
+              '_' +
+              CLIENT_REFERENCE_MANIFEST +
+              '.js'
+          ] = new sources.RawSource(
+            `globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST[${JSON.stringify(
+              '/_not-found'
+            )}]=${JSON.stringify(json)}`
+          ) as unknown as webpack.sources.RawSource
+        }
+      }
+    }
 
     pluginState.ASYNC_CLIENT_MODULES = []
-
-    assets[file + '.js'] = new sources.RawSource(
-      `self.__RSC_MANIFEST=${JSON.stringify(json)}`
-      // Work around webpack 4 type of RawSource being used
-      // TODO: use webpack 5 type by default
-    ) as unknown as webpack.sources.RawSource
-    assets[file + '.json'] = new sources.RawSource(
-      json
-      // Work around webpack 4 type of RawSource being used
-      // TODO: use webpack 5 type by default
-    ) as unknown as webpack.sources.RawSource
   }
 }

commit 0fe6e850fee5fec4ccac57021149f7da41098ff2

Fix tracking of client reference manifest (#52505)
The problem was introduced in #52450, that the client reference manifest isn't being tracked and included in the function.

Verified that this fixes the issue.

packages/next/src/build/webpack/plugins/app-build-manifest-plugin.ts
@@ -1,7 +1,6 @@
 import { webpack, sources } from 'next/dist/compiled/webpack/webpack'
 import {
   APP_BUILD_MANIFEST,
-  CLIENT_REFERENCE_MANIFEST,
   CLIENT_STATIC_FILES_RUNTIME_MAIN_APP,
   SYSTEM_ENTRYPOINTS,
 } from '../../../shared/lib/constants'
@@ -77,22 +76,7 @@ export class AppBuildManifestPlugin {
       }
 
       const filesForPage = getEntrypointFiles(entrypoint)
-      const manifestsForPage =
-        pagePath.endsWith('/page') ||
-        pagePath === '/not-found' ||
-        pagePath === '/_not-found'
-          ? [
-              'server/app' +
-                pagePath.replace(/%5F/g, '_') +
-                '_' +
-                CLIENT_REFERENCE_MANIFEST +
-                '.js',
-            ]
-          : []
-
-      manifest.pages[pagePath] = [
-        ...new Set([...mainFiles, ...manifestsForPage, ...filesForPage]),
-      ]
+      manifest.pages[pagePath] = [...new Set([...mainFiles, ...filesForPage])]
     }
 
     const json = JSON.stringify(manifest, null, 2)

packages/next/src/build/webpack/plugins/next-trace-entrypoints-plugin.ts
@@ -6,7 +6,10 @@ import {
   nodeFileTrace,
   NodeFileTraceReasons,
 } from 'next/dist/compiled/@vercel/nft'
-import { TRACE_OUTPUT_VERSION } from '../../../shared/lib/constants'
+import {
+  CLIENT_REFERENCE_MANIFEST,
+  TRACE_OUTPUT_VERSION,
+} from '../../../shared/lib/constants'
 import { webpack, sources } from 'next/dist/compiled/webpack/webpack'
 import {
   NODE_ESM_RESOLVE_OPTIONS,
@@ -284,6 +287,27 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
         })
         // don't include the entry itself in the trace
         entryFiles.delete(nodePath.join(outputPath, `../${entrypoint.name}.js`))
+
+        if (entrypoint.name.startsWith('app/')) {
+          // include the client reference manifest
+          const clientManifestsForPage =
+            entrypoint.name.endsWith('/page') ||
+            entrypoint.name === '/not-found' ||
+            entrypoint.name === '/_not-found'
+              ? nodePath.join(
+                  outputPath,
+                  '..',
+                  entrypoint.name.replace(/%5F/g, '_') +
+                    '_' +
+                    CLIENT_REFERENCE_MANIFEST +
+                    '.js'
+                )
+              : null
+
+          if (clientManifestsForPage !== null) {
+            entryFiles.add(clientManifestsForPage)
+          }
+        }
 
         const finalFiles: string[] = []
 

packages/next/src/server/base-server.ts
@@ -293,7 +293,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   }): Promise<FindComponentsResult | null>
   protected abstract getFontManifest(): FontManifest | undefined
   protected abstract getPrerenderManifest(): PrerenderManifest
-  // protected abstract getServerComponentManifest(): any
   protected abstract getNextFontManifest(): NextFontManifest | undefined
   protected abstract attachRequestMeta(
     req: BaseNextRequest,

test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
@@ -564,16 +564,6 @@ createNextDescribe(
         })
         await Promise.all(promises)
       })
-
-      it('should generate client reference manifest for edge SSR pages', async () => {
-        const buildManifest = JSON.parse(
-          await next.readFile('.next/app-build-manifest.json')
-        )
-
-        expect(buildManifest.pages['/edge/dynamic/page']).toInclude(
-          'server/app/edge/dynamic/page_client-reference-manifest.js'
-        )
-      })
     }
   }
 )