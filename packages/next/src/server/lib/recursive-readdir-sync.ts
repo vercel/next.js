1c55f8f6903308a9c63c14acf8a1d35523cca687
Shu Ding <g@shud.in>
2023-07-10 18:56:31 +0200

    perf: use fs.opendir for recursiveReadDirSync

--- packages/next/src/server/lib/recursive-readdir-sync.ts
+++ packages/next/src/server/lib/recursive-readdir-sync.ts
@@ -13,17 +13,19 @@ export function recursiveReadDirSync(
   /** Used to replace the initial path, only the relative path is left, it's faster than path.relative. */
   rootDir = dir
 ): string[] {
-  const result = fs.readdirSync(dir, { withFileTypes: true })
+  // Use opendirSync for better memory usage
+  const result = fs.opendirSync(dir)
 
-  result.forEach((part) => {
+  let part: fs.Dirent | null
+  while ((part = result.readSync())) {
     const absolutePath = join(dir, part.name)
-
     if (part.isDirectory()) {
       recursiveReadDirSync(absolutePath, arr, rootDir)
-      return
+    } else {
+      arr.push(absolutePath.slice(rootDir.length))
     }
-    arr.push(absolutePath.replace(rootDir, ''))
-  })
+  }
 
+  result.closeSync()
   return arr
 }
