a54c1ba124c81b31561a8bd184382547326bf110
Jiachi Liu <inbox@huozhi.im>
2023-07-13 13:59:22 +0200

    add test

---
+++ test/e2e/app-dir/root-layout-render-once/app/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+let value = 0
+export default function Layout({ children }) {
+  return (
+    <html>
+      <head></head>
+      <body>
+        <div id="render-once">{children}</div>
+        <p id="counter">{value++}</p>
+      </body>
+    </html>
+  )
+}
