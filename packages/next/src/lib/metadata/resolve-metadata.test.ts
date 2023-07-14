vercel/next.js

commit 85033e3add4ed12a679eb46776870ab2d4eb87d6

Override file based images with social images property (#52416)
Metadata API should provide a way to override the filebased metadata
images. As usually for child routes, if there's new social images or
icons are provided, the ones from parent routes should be overridden /
skipped.

The `metadata` object export or `generateMetadata` should be able to do
that. Sometimes users still add other og info (besides images) to
metadata export (both object and `generateMetadata`).
I think we should check if they really have returned images property,
then decide to override.

- For the same level of routes:
- If there's no `openGraph.images` in the returned value, merge with
file based images
- If there's any `openGraph.images` in the returned value, ignore file
based ones

- For child level of routes:
Always override the parent level, ignoring parent level file based
images unless they use `generateMetadata` to merge from
`resolvingParentMetadata` value, then the parent level's file based ones
will present there


Closes NEXT-1418

packages/next/src/lib/metadata/resolve-metadata.ts
@@ -51,54 +51,61 @@ type TitleTemplates = {
 }
 
 function mergeStaticMetadata(
-  metadata: ResolvedMetadata,
+  source: Metadata | null,
+  target: ResolvedMetadata,
   staticFilesMetadata: StaticMetadata,
   metadataContext: MetadataContext,
   titleTemplates: TitleTemplates
 ) {
   if (!staticFilesMetadata) return
   const { icon, apple, openGraph, twitter, manifest } = staticFilesMetadata
-  if (icon || apple) {
-    metadata.icons = {
+  // file based metadata is specified and current level metadata icons is not specified
+  if (
+    (icon && !source?.icons && !source?.icons?.hasOwnProperty('icon')) ||
+    (apple && !source?.icons?.hasOwnProperty('apple'))
+  ) {
+    target.icons = {
       icon: icon || [],
       apple: apple || [],
     }
   }
-  if (twitter) {
+  // file based metadata is specified and current level metadata twitter.images is not specified
+  if (twitter && !source?.twitter?.hasOwnProperty('images')) {
     const resolvedTwitter = resolveTwitter(
-      { ...metadata.twitter, images: twitter } as Twitter,
-      metadata.metadataBase,
+      { ...target.twitter, images: twitter } as Twitter,
+      target.metadataBase,
       titleTemplates.twitter
     )
-    metadata.twitter = resolvedTwitter
+    target.twitter = resolvedTwitter
   }
 
-  if (openGraph) {
+  // file based metadata is specified and current level metadata openGraph.images is not specified
+  if (openGraph && !source?.openGraph?.hasOwnProperty('images')) {
     const resolvedOpenGraph = resolveOpenGraph(
-      { ...metadata.openGraph, images: openGraph } as OpenGraph,
-      metadata.metadataBase,
+      { ...target.openGraph, images: openGraph } as OpenGraph,
+      target.metadataBase,
       metadataContext,
       titleTemplates.openGraph
     )
-    metadata.openGraph = resolvedOpenGraph
+    target.openGraph = resolvedOpenGraph
   }
   if (manifest) {
-    metadata.manifest = manifest
+    target.manifest = manifest
   }
 
-  return metadata
+  return target
 }
 
 // Merge the source metadata into the resolved target metadata.
 function merge({
-  target,
   source,
+  target,
   staticFilesMetadata,
   titleTemplates,
   metadataContext,
 }: {
-  target: ResolvedMetadata
   source: Metadata | null
+  target: ResolvedMetadata
   staticFilesMetadata: StaticMetadata
   titleTemplates: TitleTemplates
   metadataContext: MetadataContext
@@ -211,6 +218,7 @@ function merge({
     }
   }
   mergeStaticMetadata(
+    source,
     target,
     staticFilesMetadata,
     metadataContext,
@@ -388,7 +396,9 @@ function postProcessMetadata(
     }> = {}
     const hasTwTitle = twitter?.title.absolute
     const hasTwDescription = twitter?.description
-    const hasTwImages = twitter?.images
+    const hasTwImages = Boolean(
+      twitter?.hasOwnProperty('images') && twitter.images
+    )
     if (!hasTwTitle) autoFillProps.title = openGraph.title
     if (!hasTwDescription) autoFillProps.description = openGraph.description
     if (!hasTwImages) autoFillProps.images = openGraph.images

packages/next/src/lib/metadata/resolve-metadata.test.ts
@@ -235,6 +235,68 @@ describe('accumulateMetadata', () => {
         },
       })
     })
+
+    it('should override openGraph or twitter images when current layer specifies social images properties', async () => {
+      const metadataItems1: MetadataItems = [
+        [
+          {
+            openGraph: {
+              images: 'https://test.com/og.png',
+            },
+            twitter: {
+              images: 'https://test.com/twitter.png',
+            },
+          },
+          // has static metadata files
+          {
+            icon: undefined,
+            apple: undefined,
+            twitter: ['/filebased/twitter.png'],
+            openGraph: ['/filebased/og.png'],
+            manifest: undefined,
+          },
+        ],
+      ]
+      const metadata1 = await accumulateMetadata(metadataItems1)
+      expect(metadata1).toMatchObject({
+        openGraph: {
+          images: [{ url: new URL('https://test.com/og.png') }],
+        },
+        twitter: {
+          images: [{ url: new URL('https://test.com/twitter.png ') }],
+        },
+      })
+
+      const metadataItems2: MetadataItems = [
+        [
+          function gM2() {
+            return {
+              openGraph: {
+                images: undefined,
+              },
+              // twitter is not specified, supposed to merged with openGraph but images should not be picked up
+            }
+          },
+          // has static metadata files
+          {
+            icon: undefined,
+            apple: undefined,
+            twitter: undefined,
+            openGraph: ['/filebased/og.png'],
+            manifest: undefined,
+          },
+        ],
+      ]
+      const metadata2 = await accumulateMetadata(metadataItems2)
+      expect(metadata2).toMatchObject({
+        openGraph: {
+          images: undefined,
+        },
+        twitter: {
+          images: undefined,
+        },
+      })
+    })
   })
 
   describe('themeColor', () => {

test/e2e/app-dir/metadata/metadata.test.ts
@@ -89,7 +89,7 @@ createNextDescribe(
         tag: string,
         queryKey: string,
         domAttributeField: string,
-        expected: Record<string, string | string[]>
+        expected: Record<string, string | string[] | undefined>
       ) => {
         const res = {}
         for (const key of Object.keys(expected)) {
@@ -569,8 +569,27 @@ createNextDescribe(
         })
 
         // favicon shouldn't be overridden
-        const $icon = $('link[rel="icon"]')
-        expect($icon.attr('href')).toBe('/favicon.ico')
+        expect($('link[rel="icon"]').attr('href')).toBe('/favicon.ico')
+      })
+
+      it('should override file based images when opengraph-image and twitter-image specify images property', async () => {
+        const $ = await next.render$('/opengraph/static/override')
+
+        const match = createMultiHtmlMatcher($)
+        await match('meta', 'property', 'content', {
+          'og:title': 'no-og-image',
+          'og:image': undefined,
+        })
+
+        await match('meta', 'name', 'content', {
+          'twitter:image': undefined,
+          'twitter:title': 'no-tw-image',
+        })
+
+        // icon should be overridden
+        expect($('link[rel="icon"]').attr('href')).toBe(
+          'https://custom-icon-1.png'
+        )
       })
     })
 

test/e2e/app-dir/metadata/app/opengraph/static/override/page.tsx
@@ -1,0 +1,15 @@
+export default function Page() {
+  return 'opengraph-static-override'
+}
+
+export const metadata = {
+  icons: ['https://custom-icon-1.png'],
+  openGraph: {
+    title: 'no-og-image',
+    images: undefined,
+  },
+  twitter: {
+    title: 'no-tw-image',
+    images: null,
+  },
+}

commit d10f105b74d4af6408ee7917498f38b257ba1f04

Fix icons metadata missing from layout (#52673)
### What

This was an issue introduced in #52416 when we check the images and icons property while merging with static file conventions. Where we should check if the properties are present but we only checked if they value is falsy. So when you're using `icons` with array value it breaks.

Now we properly check all the possible case of `metadata.icons` value, so then decide if we are using the static file convention or the icons property of defined metadata.

Also add few more tests cases to cover icons resolving.

packages/next/src/lib/metadata/resolve-metadata.ts
@@ -50,6 +50,25 @@ type TitleTemplates = {
   openGraph: string | null
 }
 
+function hasIconsProperty(
+  icons: Metadata['icons'],
+  prop: 'icon' | 'apple'
+): boolean {
+  if (!icons) return false
+  if (prop === 'icon') {
+    // Detect if icons.icon will be presented, icons array and icons string will all be merged into icons.icon
+    return !!(
+      typeof icons === 'string' ||
+      icons instanceof URL ||
+      Array.isArray(icons) ||
+      (prop in icons && icons[prop])
+    )
+  } else {
+    // Detect if icons.apple will be presented, only icons.apple will be merged into icons.apple
+    return !!(typeof icons === 'object' && prop in icons && icons[prop])
+  }
+}
+
 function mergeStaticMetadata(
   source: Metadata | null,
   target: ResolvedMetadata,
@@ -61,8 +80,8 @@ function mergeStaticMetadata(
   const { icon, apple, openGraph, twitter, manifest } = staticFilesMetadata
   // file based metadata is specified and current level metadata icons is not specified
   if (
-    (icon && !source?.icons && !source?.icons?.hasOwnProperty('icon')) ||
-    (apple && !source?.icons?.hasOwnProperty('apple'))
+    (icon && !hasIconsProperty(source?.icons, 'icon')) ||
+    (apple && !hasIconsProperty(source?.icons, 'apple'))
   ) {
     target.icons = {
       icon: icon || [],

packages/next/src/lib/metadata/resolve-metadata.test.ts
@@ -59,6 +59,130 @@ describe('accumulateMetadata', () => {
     })
   })
 
+  describe('icon', () => {
+    it('should resolve icons.icon correctly', async () => {
+      // array icons
+      expect(
+        await accumulateMetadata([
+          [
+            {
+              icons: [
+                {
+                  url: 'favicon-light.png',
+                  rel: 'icon',
+                  media: '(prefers-color-scheme: light)',
+                },
+                {
+                  url: 'favicon-dark.png',
+                  rel: 'icon',
+                  media: '(prefers-color-scheme: dark)',
+                },
+              ],
+            },
+            null,
+          ],
+        ])
+      ).toMatchObject({
+        icons: {
+          icon: [
+            {
+              url: 'favicon-light.png',
+              rel: 'icon',
+              media: '(prefers-color-scheme: light)',
+            },
+            {
+              url: 'favicon-dark.png',
+              rel: 'icon',
+              media: '(prefers-color-scheme: dark)',
+            },
+          ],
+        },
+      })
+
+      // string icons
+      expect(
+        await accumulateMetadata([
+          [
+            {
+              icons: 'favicon-light.png',
+            },
+            null,
+          ],
+        ])
+      ).toMatchObject({
+        icons: {
+          icon: [
+            {
+              url: 'favicon-light.png',
+              rel: 'icon',
+            },
+          ],
+        },
+      })
+
+      // icon.icons array
+      expect(
+        await accumulateMetadata([
+          [
+            {
+              icons: {
+                icon: [
+                  {
+                    url: 'favicon-light.png',
+                  },
+                  {
+                    url: 'favicon-dark.png',
+                  },
+                ],
+              },
+            },
+            null,
+          ],
+        ])
+      ).toMatchObject({
+        icons: {
+          icon: [
+            {
+              url: 'favicon-light.png',
+            },
+            {
+              url: 'favicon-dark.png',
+            },
+          ],
+        },
+      })
+    })
+
+    it('should resolve icons.apple', async () => {
+      expect(
+        await accumulateMetadata([
+          [
+            {
+              icons: {
+                apple: [
+                  {
+                    url: 'apple-touch-icon-light.png',
+                    media: '(prefers-color-scheme: light)',
+                  },
+                ],
+              },
+            },
+            null,
+          ],
+        ])
+      ).toMatchObject({
+        icons: {
+          apple: [
+            {
+              url: 'apple-touch-icon-light.png',
+              media: '(prefers-color-scheme: light)',
+            },
+          ],
+        },
+      })
+    })
+  })
+
   describe('itunes', () => {
     it('should resolve relative url starting with ./ with pathname for itunes.appArgument', async () => {
       const metadataItems: MetadataItems = [

test/e2e/app-dir/metadata/app/icons/descriptor/from-layout/layout.tsx
@@ -1,0 +1,18 @@
+export default function layout({ children }) {
+  return children
+}
+
+export const metadata = {
+  icons: [
+    {
+      url: 'favicon-light.png',
+      rel: 'icon',
+      media: '(prefers-color-scheme: light)',
+    },
+    {
+      url: 'favicon-dark.png',
+      rel: 'icon',
+      media: '(prefers-color-scheme: dark)',
+    },
+  ],
+}

test/e2e/app-dir/metadata/app/icons/descriptor/from-layout/page.tsx
@@ -1,0 +1,3 @@
+export default function page() {
+  return 'icons-from-layout'
+}

test/e2e/app-dir/metadata/metadata.test.ts
@@ -631,6 +631,18 @@ createNextDescribe(
         })
       })
 
+      it('should merge icons from layout if no static icons files are specified', async () => {
+        const browser = await next.browser('/icons/descriptor/from-layout')
+        const matchDom = createDomMatcher(browser)
+
+        await matchDom('link', 'href="favicon-light.png"', {
+          media: '(prefers-color-scheme: light)',
+        })
+        await matchDom('link', 'href="favicon-dark.png"', {
+          media: '(prefers-color-scheme: dark)',
+        })
+      })
+
       it('should not hoist meta[itemProp] to head', async () => {
         const $ = await next.render$('/')
         expect($('head meta[itemProp]').length).toBe(0)