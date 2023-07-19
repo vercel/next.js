91c31c69b0bb5d63154906a3598700d0c8a45cf3
Jimmy Lai <laijimmy0@gmail.com>
2023-07-19 14:00:54 +0200

    Revert "stuff"
    This reverts commit 85bd7cc11f94341f2ecfc2436931bcff9fe08688.

--- packages/next/src/client/components/request-async-storage.ts
+++ packages/next/src/client/components/request-async-storage.ts
@@ -1,23 +1,16 @@
 import type { AsyncLocalStorage } from 'async_hooks'
-import type { IncomingMessage } from 'http'
 import type { DraftModeProvider } from '../../server/async-storage/draft-mode-provider'
 import type { ResponseCookies } from '../../server/web/spec-extension/cookies'
 import type { ReadonlyHeaders } from '../../server/web/spec-extension/adapters/headers'
 import type { ReadonlyRequestCookies } from '../../server/web/spec-extension/adapters/request-cookies'
 
 import { createAsyncLocalStorage } from './async-local-storage'
-import { NextRequest } from '../../server/web/spec-extension/request'
-import { BaseNextRequest } from '../../server/base-http'
 
 export interface RequestStore {
   readonly headers: ReadonlyHeaders
   readonly cookies: ReadonlyRequestCookies
   readonly mutableCookies: ResponseCookies
   readonly draftMode: DraftModeProvider
-  readonly DO_NOT_USE_OR_YOU_WILL_BE_FIRED_request:
-    | IncomingMessage
-    | BaseNextRequest
-    | NextRequest
 }
 
 export type RequestAsyncStorage = AsyncLocalStorage<RequestStore>
