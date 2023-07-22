f0b6045e4292d5eba7efc959b6a66353279116cf
Jiachi Liu <inbox@huozhi.im>
2023-07-22 22:44:37 +0200

    update

--- test/e2e/app-dir/app-static/app-fetch-logging.test.ts
+++ test/e2e/app-dir/app-static/app-fetch-logging.test.ts
@@ -10,14 +10,34 @@ createNextDescribe(
       'app/default-cache/page.js': new FileRef(
         path.join(__dirname, 'app/default-cache/page.js')
       ),
+      'next.config.js': `module.exports = { experimental: { logging: 'verbose' } }`,
     },
   },
-  ({ next }) => {
-    it('should not log fetching hits in dev mode by default', async () => {
-      await next.fetch('/default-cache')
+  ({ next, isNextDev }) => {
+    function runTests({ isVerbose }: { isVerbose: boolean }) {
+      it('should not log fetching hits in dev mode by default', async () => {
+        await next.fetch('/default-cache')
 
-      const logs = stripAnsi(next.cliOutput)
-      expect(logs).not.toContain('GET /default-cache 200')
+        const logs = stripAnsi(next.cliOutput)
+        if (isVerbose && isNextDev) {
+          expect(logs).toContain('GET /default-cache 200')
+        } else {
+          expect(logs).not.toContain('GET /default-cache 200')
+        }
+      })
+    }
+
+    describe('with logging verbose', () => {
+      runTests({ isVerbose: true })
+    })
+
+    describe('with default logging', () => {
+      beforeAll(async () => {
+        await next.stop()
+        await next.deleteFile('next.config.js')
+        await next.start()
+      })
+      runTests({ isVerbose: false })
     })
   }
 )
