fc50e42384444996cdb9ac71689ee9cd27ed4888
lijianan <574980606@qq.com>
2023-07-23 09:00:49 +0800

    Update _document.tsx

--- examples/with-ant-design/pages/_document.tsx
+++ examples/with-ant-design/pages/_document.tsx
@@ -19,12 +19,11 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
   const originalRenderPage = ctx.renderPage
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App) => (props) =>
-        (
-          <StyleProvider cache={cache}>
-            <App {...props} />
-          </StyleProvider>
-        ),
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
     })
   const initialProps = await Document.getInitialProps(ctx)
   const style = extractStyle(cache, true)
