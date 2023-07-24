595b4b2bec7c1472bec07e746a584a53ee6085c5
Tobias Koppers <tobias.koppers@googlemail.com>
2023-07-24 16:25:00 +0000

    update next build setup code

--- packages/next-swc/crates/next-core/js/src/build/client/bootstrap.ts
+++ packages/next-swc/crates/next-core/js/src/build/client/bootstrap.ts
@@ -3,5 +3,24 @@
  */
 
 import '../shims'
-import './setup'
-import 'next/dist/client/next'
+import { initialize, hydrate, version, router, emitter } from 'next/dist/client'
+
+declare global {
+  interface Window {
+    next: any
+  }
+}
+
+window.next = {
+  version: `${version}-turbo`,
+  // router is initialized later so it has to be live-binded
+  get router() {
+    return router
+  },
+  emitter,
+}
+;(self as any).__next_set_public_path__ = () => {}
+
+initialize({})
+  .then(() => hydrate())
+  .catch(console.error)
--- packages/next-swc/crates/next-core/js/src/build/client/setup.ts
+++
@@ -1,1 +0,0 @@
-;(self as any).__next_set_public_path__ = () => {}
