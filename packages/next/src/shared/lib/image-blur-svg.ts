4eedbe0b9e522d24cdaf1bef1b64e3d0f1737ff7
Artur Bień <artur.bien@gmail.com>
2023-07-24 07:00:06 +0000

    fix blur value at 20

--- packages/next/src/shared/lib/image-blur-svg.ts
+++ packages/next/src/shared/lib/image-blur-svg.ts
@@ -16,15 +16,13 @@ export function getImageBlurSvg({
   blurDataURL: string
   objectFit?: string
 }): string {
-  const std = blurWidth && blurHeight ? 0.5 : 20
-  const svgWidth = blurWidth || widthInt
-  const svgHeight = blurHeight || heightInt
+  const std = 20
+  const svgWidth = blurWidth ? blurWidth * 40 : widthInt
+  const svgHeight = blurHeight ? blurHeight * 40 : heightInt
 
-  const svgDimensions = svgWidth && svgHeight
-  const svgSizeAttributes = svgDimensions
-    ? `viewBox='0 0 ${svgWidth} ${svgHeight}'`
-    : ''
-  const preserveAspectRatio = svgDimensions
+  const viewBox =
+    svgWidth && svgHeight ? `viewBox='0 0 ${svgWidth} ${svgHeight}'` : ''
+  const preserveAspectRatio = viewBox
     ? 'none'
     : objectFit === 'contain'
     ? 'xMidYMid'
@@ -32,5 +30,5 @@ export function getImageBlurSvg({
     ? 'xMidYMid slice'
     : 'none'
 
-  return `%3csvg xmlns='http://www.w3.org/2000/svg' ${svgSizeAttributes}%3e%3cfilter id='b' color-interpolation-filters='sRGB'%3e%3cfeGaussianBlur stdDeviation='${std}'/%3e%3cfeColorMatrix values='1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 100 -1' result='s'/%3e%3cfeFlood x='0' y='0' width='100%25' height='100%25'/%3e%3cfeComposite operator='out' in='s'/%3e%3cfeComposite in2='SourceGraphic'/%3e%3cfeGaussianBlur stdDeviation='${std}'/%3e%3c/filter%3e%3cimage width='100%25' height='100%25' x='0' y='0' preserveAspectRatio='${preserveAspectRatio}' style='filter: url(%23b);' href='${blurDataURL}'/%3e%3c/svg%3e`
+  return `%3csvg xmlns='http://www.w3.org/2000/svg' ${viewBox}%3e%3cfilter id='b' color-interpolation-filters='sRGB'%3e%3cfeGaussianBlur stdDeviation='${std}'/%3e%3cfeColorMatrix values='1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 100 -1' result='s'/%3e%3cfeFlood x='0' y='0' width='100%25' height='100%25'/%3e%3cfeComposite operator='out' in='s'/%3e%3cfeComposite in2='SourceGraphic'/%3e%3cfeGaussianBlur stdDeviation='${std}'/%3e%3c/filter%3e%3cimage width='100%25' height='100%25' x='0' y='0' preserveAspectRatio='${preserveAspectRatio}' style='filter: url(%23b);' href='${blurDataURL}'/%3e%3c/svg%3e`
 }
