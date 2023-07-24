212ff0eb9cfa67880423ea1389e3fb68e468fd67
Jimmy Lai <laijimmy0@gmail.com>
2023-07-24 20:42:45 +0200

    add other ctx

--- packages/next/src/server/future/route-modules/app-page/externals.ts
+++ packages/next/src/server/future/route-modules/app-page/externals.ts
@@ -1,4 +1,12 @@
+// the name of the export has to be the camelCase version of the file name (without the extension)
 export * as headManagerContext from '../../../../shared/lib/head-manager-context.external'
 export * as serverInsertedHtml from '../../../../shared/lib/server-inserted-html.external'
 export * as appRouterContext from '../../../../shared/lib/app-router-context.external'
 export * as hooksClientContext from '../../../../shared/lib/hooks-client-context.external'
+export * as routerContext from '../../../../shared/lib/router-context.external'
+export * as htmlContext from '../../../../shared/lib/html-context.external'
+export * as ampContext from '../../../../shared/lib/amp-context.external'
+export * as adapters from '../../../../shared/lib/router/adapters.external'
+export * as loadableContext from '../../../../shared/lib/loadable-context.external'
+export * as imageConfigContext from '../../../../shared/lib/image-config-context.external'
+export * as runtimeConfig from '../../../../shared/lib/runtime-config.external'
