cc7ce7c1db5982d84bfffcb6eaecf7e08534b515
Ian Macartney <ianhmacartney@gmail.com>
2023-07-24 03:31:21 -0700

    prettier

--- examples/convex/convex/messages.ts
+++ examples/convex/convex/messages.ts
@@ -1,6 +1,6 @@
 import { query, mutation } from './_generated/server'
 import { Doc } from './_generated/dataModel'
-import { v } from 'convex/values';
+import { v } from 'convex/values'
 
 export const list = query({
   handler: async (ctx): Promise<Doc<'messages'>[]> => {
@@ -10,8 +10,7 @@ export const list = query({
 
 export const send = mutation({
   args: { body: v.string(), author: v.string() },
-  handler:
-  async (ctx, { body, author }) => {
+  handler: async (ctx, { body, author }) => {
     const message = { body, author }
     await ctx.db.insert('messages', message)
   },
