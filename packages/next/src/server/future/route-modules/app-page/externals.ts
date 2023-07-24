b03853513cf8f32944d67361e413104e8a4c6fd3
Jimmy Lai <laijimmy0@gmail.com>
2023-07-24 19:55:38 +0200

    fix router externals

--- packages/next/src/server/future/route-modules/app-page/externals.ts
+++ packages/next/src/server/future/route-modules/app-page/externals.ts
@@ -1,2 +1,4 @@
 export * as headManagerContext from '../../../../shared/lib/head-manager-context.external'
 export * as serverInsertedHtml from '../../../../shared/lib/server-inserted-html.external'
+export * as appRouterContext from '../../../../shared/lib/app-router-context.external'
+export * as hooksClientContext from '../../../../shared/lib/hooks-client-context.external'
