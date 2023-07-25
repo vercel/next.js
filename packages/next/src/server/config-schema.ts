6510934c79689d8b1a8055fe42f134d9fb01afa7
SukkaW <isukkaw@gmail.com>
2023-07-25 17:30:09 +0800

    refactor: wip migrating config schemas

--- packages/next/src/server/config-schema.ts
+++ packages/next/src/server/config-schema.ts
@@ -108,6 +108,97 @@ export const configSchemaZod: zod.ZodType<NextConfig> = z.lazy(() =>
       dirs: z.array(z.string()).optional(),
       ignoreDuringBuilds: z.boolean().optional(),
     }),
+    excludeDefaultMomentLocales: z.boolean().optional(),
+    exprimental: z.object({
+      appDocumentPreloading: z.boolean().optional(),
+      adjustFontFallbacks: z.boolean().optional(),
+      adjustFontFallbacksWithSizeAdjust: z.boolean().optional(),
+      allowedRevalidateHeaderKeys: z.array(z.string()).optional(),
+      amp: z
+        .object({
+          optimizer: z.any(),
+          validator: z.string().optional(),
+          skipValidation: z.boolean().optional(),
+        })
+        .optional(),
+      clientRouterFilter: z.boolean().optional(),
+      clientRouterFilterRedirects: z.boolean().optional(),
+      clientRouterFilterAllowedRate: z.number().optional(),
+      cpus: z.number().optional(),
+      memoryBasedWorkersCount: z.boolean().optional(),
+      craCompat: z.boolean().optional(),
+      caseSensitiveRoutes: z.boolean().optional(),
+      useDeploymenId: z.boolean().optional(),
+      deploymentId: z.string().optional(),
+      disableOptimizedLoading: z.boolean().optional(),
+      disablePostCssPresetEnv: z.boolean().optional(),
+      esmExternals: z.union([z.boolean(), z.literal('loose')]).optional(),
+      appDir: z.boolean().optional(),
+      serverActions: z.boolean().optional(),
+      serverActionsBodySizeLimit: z.union([z.number(), z.string()]).optional(),
+      extensionAlias: z.record(z.string()).optional(),
+      externalDir: z.boolean().optional(),
+      externalMiddlewareRewritesResolve: z.boolean().optional(),
+      fallbackNodePolyfills: z.boolean().optional(),
+      fetchCacheKeyPrefix: z.string().optional(),
+      forceSwcTransforms: z.boolean().optional(),
+      fullySpecified: z.boolean().optional(),
+      gzipSize: z.boolean().optional(),
+      incrementalCacheHandlerPath: z.string().optional(),
+      isrFlushToDisk: z.boolean().optional(),
+      isrMemoryCacheSize: z.number().optional(),
+      largePageDataBytes: z.number().optional(),
+      legacyBrowsers: z.boolean().optional(),
+      manualClientBasePath: z.boolean().optional(),
+      middleware: z
+        .union([z.literal('strict'), z.literal('flexible')])
+        .optional(),
+      newNextLinkBehavior: z.boolean().optional(),
+      nextScriptWorkers: z.boolean().optional(),
+      optimizeCss: z.union([z.boolean(), z.any()]).optional(),
+      optimisticClientCache: z.boolean().optional(),
+      outputFileTracingRoot: z.string().optional(),
+      outputFileTracingExcludes: z
+        .record(z.string(), z.array(z.string()))
+        .optional(),
+      outputFileTracingIgnores: z.array(z.string()).optional(),
+      outputFileTracingIncludes: z
+        .record(z.string(), z.array(z.string()))
+        .optional(),
+      swcTraceProfiling: z.boolean().optional(),
+      pageEnv: z.boolean().optional(),
+      proxyTimeout: z.number().min(0).optional(),
+      serverComponentsExternalPackages: z.array(z.string()).optional(),
+      scrollRestoration: z.boolean().optional(),
+      sharedPool: z.boolean().optional(),
+      srt: z.object({
+        algorithm: z
+          .union([
+            z.literal('sha256'),
+            z.literal('sha384'),
+            z.literal('sha512'),
+          ])
+          .optional(),
+      }),
+      strictNextHead: z.boolean().optional(),
+      swcFileReading: z.boolean().optional(),
+      swcMinify: z.boolean().optional(),
+      swcPlugins: z
+        .array(z.tuple([z.string(), z.record(z.string(), z.any()).optional()]))
+        .optional(),
+      urlImports: z.any(),
+      workerThreads: z.boolean().optional(),
+      webVitalsAttribution: z.array(
+        z.union([
+          z.literal('CLS'),
+          z.literal('FCP'),
+          z.literal('FID'),
+          z.literal('INP'),
+          z.literal('LCP'),
+          z.literal('TTFB'),
+        ])
+      ),
+    }),
   })
 )
 
@@ -115,239 +206,9 @@ const configSchema = {
   type: 'object',
   additionalProperties: false,
   properties: {
-    excludeDefaultMomentLocales: {
-      type: 'boolean',
-    },
     experimental: {
       additionalProperties: false,
       properties: {
-        appDocumentPreloading: {
-          type: 'boolean',
-        },
-        adjustFontFallbacks: {
-          type: 'boolean',
-        },
-        adjustFontFallbacksWithSizeAdjust: {
-          type: 'boolean',
-        },
-        allowedRevalidateHeaderKeys: {
-          type: 'array',
-        },
-        amp: {
-          additionalProperties: false,
-          properties: {
-            optimizer: {
-              type: 'object',
-            },
-            skipValidation: {
-              type: 'boolean',
-            },
-            validator: {
-              type: 'string',
-            },
-          },
-          type: 'object',
-        },
-        clientRouterFilter: {
-          type: 'boolean',
-        },
-        clientRouterFilterRedirects: {
-          type: 'boolean',
-        },
-        clientRouterFilterAllowedRate: {
-          type: 'number',
-        },
-        cpus: {
-          type: 'number',
-        },
-        memoryBasedWorkersCount: {
-          type: 'boolean',
-        },
-        craCompat: {
-          type: 'boolean',
-        },
-        caseSensitiveRoutes: {
-          type: 'boolean',
-        },
-        useDeploymentId: {
-          type: 'boolean',
-        },
-        useDeploymentIdServerActions: {
-          type: 'boolean',
-        },
-        deploymentId: {
-          type: 'string',
-        },
-        disableOptimizedLoading: {
-          type: 'boolean',
-        },
-        disablePostcssPresetEnv: {
-          type: 'boolean',
-        },
-        esmExternals: {
-          oneOf: [
-            {
-              type: 'boolean',
-            },
-            {
-              const: 'loose',
-            },
-          ] as any,
-        },
-        appDir: {
-          type: 'boolean',
-        },
-        serverActions: {
-          type: 'boolean',
-        },
-        serverActionsBodySizeLimit: {
-          oneOf: [
-            {
-              type: 'number',
-            },
-            {
-              type: 'string',
-            },
-          ] as any,
-        },
-        extensionAlias: {
-          type: 'object',
-        },
-        externalDir: {
-          type: 'boolean',
-        },
-        externalMiddlewareRewritesResolve: {
-          type: 'boolean',
-        },
-        fallbackNodePolyfills: {
-          type: 'boolean',
-        },
-        fetchCacheKeyPrefix: {
-          type: 'string',
-        },
-        forceSwcTransforms: {
-          type: 'boolean',
-        },
-        fullySpecified: {
-          type: 'boolean',
-        },
-        gzipSize: {
-          type: 'boolean',
-        },
-        incrementalCacheHandlerPath: {
-          type: 'string',
-        },
-        isrFlushToDisk: {
-          type: 'boolean',
-        },
-        isrMemoryCacheSize: {
-          type: 'number',
-        },
-        largePageDataBytes: {
-          type: 'number',
-        },
-        legacyBrowsers: {
-          type: 'boolean',
-        },
-        manualClientBasePath: {
-          type: 'boolean',
-        },
-        middlewarePrefetch: {
-          // automatic typing doesn't like enum
-          enum: ['strict', 'flexible'] as any,
-          type: 'string',
-        },
-        newNextLinkBehavior: {
-          type: 'boolean',
-        },
-        nextScriptWorkers: {
-          type: 'boolean',
-        },
-        optimizeCss: {
-          oneOf: [
-            {
-              type: 'boolean',
-            },
-            {
-              type: 'object',
-            },
-          ] as any,
-        },
-        optimisticClientCache: {
-          type: 'boolean',
-        },
-        outputFileTracingRoot: {
-          nullable: true,
-          type: 'string',
-        },
-        outputFileTracingExcludes: {
-          type: 'object',
-        },
-        outputFileTracingIgnores: {
-          type: 'array',
-        },
-        outputFileTracingIncludes: {
-          type: 'object',
-        },
-        pageEnv: {
-          type: 'boolean',
-        },
-        proxyTimeout: {
-          minimum: 0,
-          type: 'number',
-        },
-        serverComponentsExternalPackages: {
-          items: {
-            type: 'string',
-          },
-          type: 'array',
-        },
-        scrollRestoration: {
-          type: 'boolean',
-        },
-        sharedPool: {
-          type: 'boolean',
-        },
-        sri: {
-          properties: {
-            algorithm: {
-              enum: ['sha256', 'sha384', 'sha512'] as any,
-              type: 'string',
-            },
-          },
-          type: 'object',
-        },
-        strictNextHead: {
-          type: 'boolean',
-        },
-        swcFileReading: {
-          type: 'boolean',
-        },
-        swcMinify: {
-          type: 'boolean',
-        },
-        swcPlugins: {
-          type: 'array',
-        },
-        swcTraceProfiling: {
-          type: 'boolean',
-        },
-        urlImports: {
-          items: {
-            type: 'string',
-          },
-          type: 'array',
-        },
-        workerThreads: {
-          type: 'boolean',
-        },
-        webVitalsAttribution: {
-          type: 'array',
-          items: {
-            type: 'string',
-            enum: ['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB'],
-          } as any,
-        },
         mdxRs: {
           type: 'boolean',
         },
