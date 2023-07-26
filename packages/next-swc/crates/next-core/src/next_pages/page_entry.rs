e9d3a37ba2c56bca7beeee7e2c48ad1bc6f28666
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-07-26 14:54:38 +0200

    Fmt + fix API pathname starting with //

--- packages/next-swc/crates/next-core/src/next_pages/page_entry.rs
+++ packages/next-swc/crates/next-core/src/next_pages/page_entry.rs
@@ -29,7 +29,7 @@ pub async fn create_page_ssr_entry_module(
     source: Vc<Box<dyn Source>>,
     next_original_name: Vc<String>,
 ) -> Result<Vc<Box<dyn EcmascriptChunkPlaceable>>> {
-    let definition_page = format!("/{}", next_original_name.await?);
+    let definition_page = next_original_name.await?;
     let definition_pathname = pathname.await?;
 
     let ssr_module = ssr_module_context.process(source, reference_type.clone());
@@ -42,45 +42,45 @@ pub async fn create_page_ssr_entry_module(
             writedoc!(
                 result,
                 r#"
-                import RouteModule from "next/dist/server/future/route-modules/pages/module"
-                import {{ hoist }} from "next/dist/build/webpack/loaders/next-route-loader/helpers"
+                    import RouteModule from "next/dist/server/future/route-modules/pages/module"
+                    import {{ hoist }} from "next/dist/build/webpack/loaders/next-route-loader/helpers"
 
-                import Document from "@vercel/turbopack-next/pages/_document"
-                import App from "@vercel/turbopack-next/pages/_app"
+                    import Document from "@vercel/turbopack-next/pages/_document"
+                    import App from "@vercel/turbopack-next/pages/_app"
 
-                import * as userland from "INNER"
+                    import * as userland from "INNER"
 
-                export default hoist(userland, "default")
+                    export default hoist(userland, "default")
 
-                export const getStaticProps = hoist(userland, "getStaticProps")
-                export const getStaticPaths = hoist(userland, "getStaticPaths")
-                export const getServerSideProps = hoist(userland, "getServerSideProps")
-                export const config = hoist(userland, "config")
-                export const reportWebVitals = hoist(userland, "reportWebVitals")
+                    export const getStaticProps = hoist(userland, "getStaticProps")
+                    export const getStaticPaths = hoist(userland, "getStaticPaths")
+                    export const getServerSideProps = hoist(userland, "getServerSideProps")
+                    export const config = hoist(userland, "config")
+                    export const reportWebVitals = hoist(userland, "reportWebVitals")
 
-                export const unstable_getStaticProps = hoist(userland, "unstable_getStaticProps")
-                export const unstable_getStaticPaths = hoist(userland, "unstable_getStaticPaths")
-                export const unstable_getStaticParams = hoist(userland, "unstable_getStaticParams")
-                export const unstable_getServerProps = hoist(userland, "unstable_getServerProps")
-                export const unstable_getServerSideProps = hoist(userland, "unstable_getServerSideProps")
-                
-                export const routeModule = new RouteModule({{
-                    definition: {{
-                        kind: "PAGES",
-                        page: {definition_page},
-                        pathname: {definition_pathname},
-                        // The following aren't used in production, but are
-                        // required for the RouteModule constructor.
-                        bundlePath: "",
-                        filename: "",
-                    }},
-                    components: {{
-                        App,
-                        Document,
-                    }},
-                    userland,
-                }})
-            "#,
+                    export const unstable_getStaticProps = hoist(userland, "unstable_getStaticProps")
+                    export const unstable_getStaticPaths = hoist(userland, "unstable_getStaticPaths")
+                    export const unstable_getStaticParams = hoist(userland, "unstable_getStaticParams")
+                    export const unstable_getServerProps = hoist(userland, "unstable_getServerProps")
+                    export const unstable_getServerSideProps = hoist(userland, "unstable_getServerSideProps")
+                    
+                    export const routeModule = new RouteModule({{
+                        definition: {{
+                            kind: "PAGES",
+                            page: {definition_page},
+                            pathname: {definition_pathname},
+                            // The following aren't used in production, but are
+                            // required for the RouteModule constructor.
+                            bundlePath: "",
+                            filename: "",
+                        }},
+                        components: {{
+                            App,
+                            Document,
+                        }},
+                        userland,
+                    }})
+                "#,
                 definition_page = StringifyJs(&definition_page),
                 definition_pathname = StringifyJs(&definition_pathname),
             )?;
@@ -88,7 +88,9 @@ pub async fn create_page_ssr_entry_module(
             // When we're building the instrumentation page (only when the
             // instrumentation file conflicts with a page also labeled
             // /instrumentation) hoist the `register` method.
-            if definition_page == "/instrumentation" || definition_page == "/src/instrumentation" {
+            if definition_page.as_str() == "/instrumentation"
+                || definition_page.as_str() == "/src/instrumentation"
+            {
                 writeln!(
                     result,
                     r#"export const register = hoist(userland, "register")"#
@@ -102,27 +104,27 @@ pub async fn create_page_ssr_entry_module(
             writedoc!(
                 result,
                 r#"
-                import RouteModule from "next/dist/server/future/route-modules/pages-api/module"
-                import {{ hoist }} from "next/dist/build/webpack/loaders/next-route-loader/helpers"
+                    import RouteModule from "next/dist/server/future/route-modules/pages-api/module"
+                    import {{ hoist }} from "next/dist/build/webpack/loaders/next-route-loader/helpers"
 
-                import * as userland from "INNER"
+                    import * as userland from "INNER"
 
-                export default hoist(userland, "default")
-                export const config = hoist(userland, "config")
-                
-                export const routeModule = new RouteModule({{
-                    definition: {{
-                        kind: "PAGES_API",
-                        page: {definition_page},
-                        pathname: {definition_pathname},
-                        // The following aren't used in production, but are
-                        // required for the RouteModule constructor.
-                        bundlePath: "",
-                        filename: "",
-                    }},
-                    userland,
-                }})
-            "#,
+                    export default hoist(userland, "default")
+                    export const config = hoist(userland, "config")
+                    
+                    export const routeModule = new RouteModule({{
+                        definition: {{
+                            kind: "PAGES_API",
+                            page: {definition_page},
+                            pathname: {definition_pathname},
+                            // The following aren't used in production, but are
+                            // required for the RouteModule constructor.
+                            bundlePath: "",
+                            filename: "",
+                        }},
+                        userland,
+                    }})
+                "#,
                 definition_page = StringifyJs(&definition_page),
                 definition_pathname = StringifyJs(&definition_pathname),
             )?;
