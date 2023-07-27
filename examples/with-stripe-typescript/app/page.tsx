1811e4cbf59f7d61109365dd556b3908d7890917
Jonathan Steele <jsteele@stripe.com>
2023-07-27 21:56:43 +0100

    feat: Move / page to App Router

---
+++ examples/with-stripe-typescript/app/page.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next'
+
+import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'Home | Next.js + TypeScript Example'
+}
+
+export default function IndexPage(): JSX.Element {
+  return (
+    <ul className="card-list">
+      <li>
+        <Link
+          href="/donate-with-checkout"
+          className="card checkout-style-background"
+        >
+          <h2 className="bottom">Donate with Checkout</h2>
+          <img src="/checkout-one-time-payments.svg" />
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/donate-with-elements"
+          className="card elements-style-background"
+        >
+          <h2 className="bottom">Donate with Elements</h2>
+          <img src="/elements-card-payment.svg" />
+        </Link>
+      </li>
+      <li>
+        <Link href="/use-shopping-cart" className="card cart-style-background">
+          <h2 className="bottom">Use Shopping Cart</h2>
+          <img src="/use-shopping-cart.png" />
+        </Link>
+      </li>
+    </ul>
+  )
+}
