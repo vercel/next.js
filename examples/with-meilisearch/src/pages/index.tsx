f72783092bcf35fc7cc3153e1ee40bb73586c835
Strift <laurent@meilisearch.com>
2023-07-27 11:47:31 +0200

    Polish home page look

--- examples/with-meilisearch/src/pages/index.tsx
+++ examples/with-meilisearch/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { Poppins } from 'next/font/google'
+import Link from 'next/link'
 import {
   InstantSearch,
   Hits,
@@ -8,16 +8,12 @@ import {
   connectSearchBox,
 } from 'react-instantsearch-dom'
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
-
-const poppins = Poppins({
-  weight: ['700'],
-  subsets: ['latin'],
-  variable: '--font-poppins',
-})
+import logo from '../assets/meilisearch.svg'
 
 const searchClient = instantMeiliSearch(
   process.env.NEXT_PUBLIC_MEILISEARCH_HOST,
-  process.env.NEXT_PUBLIC_MEILISEARCH_SEARCH_API_KEY
+  process.env.NEXT_PUBLIC_MEILISEARCH_SEARCH_API_KEY,
+  { primaryKey: 'id' }
 )
 
 interface HitProps {
@@ -60,13 +56,13 @@ const CustomSearchBox = connectSearchBox(SearchBox)
 
 const Hit = ({ hit }: HitProps) => {
   return (
-    <div className="flex p-5 mb-5 space-x-8 bg-gray-100 border border-white/25 bg-opacity-5 backdrop-blur-sm rounded-xl">
+    <div className="p-5 mb-5 space-y-6 bg-gray-100 border sm:flex sm:space-y-0 sm:space-x-8 sm:flex-row border-white/25 bg-opacity-5 backdrop-blur-sm rounded-xl">
       <Image
         src={hit.image}
         width={230}
         height={107.5}
         alt={hit.name + ' steam banner'}
-        className="flex-shrink-0 rounded-xl"
+        className="w-full sm:max-w-[230px] rounded-xl"
       ></Image>
       <div className="flex flex-col justify-center">
         <div className="mb-3 text-lg leading-5 text-gray-100">
@@ -82,16 +78,25 @@ const Hit = ({ hit }: HitProps) => {
 
 export default function Home() {
   return (
-    <main className="py-12 mx-3 font-sans sm:mx-auto sm:max-w-md">
-      <h1
-        className={`mb-12 text-4xl text-center text-primary font-title ${poppins.variable}`}
-      >
-        Meilisearch Starter
-      </h1>
-      <InstantSearch indexName="steam-video-games" searchClient={searchClient}>
-        <CustomSearchBox />
-        <Hits hitComponent={Hit} />
-      </InstantSearch>
-    </main>
+    <div>
+      <main className="py-12 mx-3 font-sans sm:mx-auto sm:max-w-md">
+        <h1 className="mb-12">
+          <Link href="https://meilisearch.com/docs" target="_blank">
+            <Image
+              src={logo}
+              alt="Meilisearch"
+              title="Meilisearch docs"
+            ></Image>
+          </Link>
+        </h1>
+        <InstantSearch
+          indexName="steam-video-games"
+          searchClient={searchClient}
+        >
+          <CustomSearchBox />
+          <Hits hitComponent={Hit} />
+        </InstantSearch>
+      </main>
+    </div>
   )
 }
