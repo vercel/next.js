ab22de0e775f5e57d958878f3324b195b9c72b33
Strift <laurent@meilisearch.com>
2023-07-27 11:10:01 +0200

    Clean up Tailwind config

--- examples/with-meilisearch/tailwind.config.js
+++ examples/with-meilisearch/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
