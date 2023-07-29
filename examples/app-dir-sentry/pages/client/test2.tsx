3326e2de575ba7cc63aa29dce7128e97200a36d8
Mayank <mayank.srmu@gmail.com>
2023-07-29 17:14:35 +0000

    copy existing sentry example and update dependencies

---
+++ examples/app-dir-sentry/pages/client/test2.tsx
@@ -0,0 +1,15 @@
+// This code will run just fine on the server in Node.js, but process will be
+// undefined in a browser. Note that `isProd = process.env.NODE_ENV` would have
+// worked because Webpack's DefinePlugin will replace it with a string at build
+// time
+const env = process.env
+const isProd = env.NODE_ENV === 'production'
+
+const Test2 = () => (
+  <>
+    <h1>Client Test 2</h1>
+    <p>isProd: {isProd}</p>
+  </>
+)
+
+export default Test2
---
+++ examples/app-dir-sentry/pages/client/test5.tsx
@@ -0,0 +1,30 @@
+import * as Sentry from '@sentry/nextjs'
+
+const Test5 = () => (
+  <>
+    <h1>Client Test 5</h1>
+    <button
+      onClick={() => {
+        const transaction = Sentry.startTransaction({
+          name: 'Testing performance',
+        })
+        Sentry.configureScope((scope) => {
+          scope.setSpan(transaction)
+        })
+
+        try {
+          // Some operation the button does, but fails
+          throw new Error('Client Test 5')
+        } catch (error) {
+          Sentry.captureException(error)
+        } finally {
+          transaction.finish()
+        }
+      }}
+    >
+      Click me to create a transaction and throw an Error
+    </button>
+  </>
+)
+
+export default Test5
