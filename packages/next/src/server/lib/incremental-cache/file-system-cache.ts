65fa94c1993394541c2eb62949672893700f189e
JJ Kasper <jj@jjsweb.site>
2023-07-28 17:27:05 -0700

    fix type

--- packages/next/src/server/lib/incremental-cache/file-system-cache.ts
+++ packages/next/src/server/lib/incremental-cache/file-system-cache.ts
@@ -114,7 +114,7 @@ export default class FileSystemCache implements CacheHandler {
     }: {
       fetchCache?: boolean
       tags?: string[]
-    }
+    } = {}
   ) {
     let data = memoryCache?.get(key)
 
