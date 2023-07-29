f4d0e79f412c1bfa811754f572659be5e39c02dd
JJ Kasper <jj@jjsweb.site>
2023-07-28 23:22:48 -0700

    lint-fix

--- examples/app-dir-i18n-routing/get-dictionary.ts
+++ examples/app-dir-i18n-routing/get-dictionary.ts
@@ -10,4 +10,4 @@ const dictionaries = {
 }
 
 export const getDictionary = async (locale: Locale) =>
-  dictionaries[locale]?.() ?? dictionaries.en();
+  dictionaries[locale]?.() ?? dictionaries.en()
