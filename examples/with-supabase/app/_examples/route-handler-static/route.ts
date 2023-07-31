9ff8ca0f373eb635d37893a787103e7d8d68d01a
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-07-31 20:22:21 +1000

    add examples for edge and static

---
+++ examples/with-supabase/app/_examples/route-handler-static/route.ts
@@ -0,0 +1,23 @@
+// TODO: Duplicate or move this file outside the `_examples` folder to make it a route
+
+import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  // This uses `supabase-js` directly as the request happens
+  // at build-time, rather than when the user requests the route,
+  // therefore, there are no cookies to pass through with the request
+  // to Supabase.
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+
+  // This assumes you have a `todos` table in Supabase. Check out
+  // the `Create Table and seed with data` section of the README 👇
+  // https://github.com/vercel/next.js/blob/canary/examples/with-supabase/README.md
+  // If using RLS, you will need to ensure there is a policy to enable the `select` action.
+  const { data: todos } = await supabase.from('todos').select()
+
+  return NextResponse.json(todos)
+}
