f6a2cd07f5d5759e7a2665b0e9b536eb706182b5
Tobias Koppers <tobias.koppers@googlemail.com>
2023-08-01 13:03:59 +0000

    run experimental-turbo tests

---
+++ test/turbopack-tests-manifest.js
@@ -0,0 +1,6 @@
+// Tests that are currently enabled with experimental Turbopack in CI.
+// Only tests that are actively testing against Turbopack should
+// be enabled here
+const enabledTests = []
+
+module.exports = { enabledTests }
