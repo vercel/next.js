f21a7a1ffb0424064f1a82b2a1878f99060ff6c4
Tobias Koppers <tobias.koppers@googlemail.com>
2023-08-02 09:14:11 +0000

    updates test runner for experimental turbopack, add test case

--- test/turbopack-tests-manifest.js
+++ test/turbopack-tests-manifest.js
@@ -1,6 +1,9 @@
 // Tests that are currently enabled with experimental Turbopack in CI.
 // Only tests that are actively testing against Turbopack should
 // be enabled here
-const enabledTests = ['test/integration/bigint/test/index.test.js']
+const enabledTests = [
+  'test/development/api-cors-with-rewrite/index.test.ts',
+  'test/integration/bigint/test/index.test.js',
+]
 
 module.exports = { enabledTests }
