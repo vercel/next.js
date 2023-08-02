b05f5f1cfe2a952bba7814e419c6aa914a1e8c6f
Ralph <janicklas@google.com>
2023-08-02 13:10:26 -0700

    Fix test

--- test/e2e/app-dir/third-parties/app/google-maps-embed/page.js
+++ test/e2e/app-dir/third-parties/app/google-maps-embed/page.js
@@ -3,7 +3,7 @@ import { GoogleMapsEmbed } from '@next/third-parties/google'
 const Page = () => {
   return (
     <div class="container">
-      <h1>Youtube Embed</h1>
+      <h1>Google Maps Embed</h1>
       <GoogleMapsEmbed
         apiKey="XYZ"
         height={200}
