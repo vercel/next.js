a90a61bf0b4144e4b45b8f9ad6d429e37420cccb
Tobias Koppers <tobias.koppers@googlemail.com>
2023-08-02 07:07:41 +0000

    enable only one test

--- test/turbopack-tests-manifest.js
+++ test/turbopack-tests-manifest.js
@@ -1,6 +1,6 @@
 // Tests that are currently enabled with experimental Turbopack in CI.
 // Only tests that are actively testing against Turbopack should
 // be enabled here
-const enabledTests = []
+const enabledTests = ['test/integration/bigint/test/index.test.js']
 
 module.exports = { enabledTests }
