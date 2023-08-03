e408b7d9a5111d1f4d611efb02f34bceed603df7
Balázs Orbán <info@balazsorban.com>
2023-08-03 20:03:01 +0200

    chore: add GH action to notify about pending PRs

---
+++ .github/actions/pr-approved-open/src/index.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { context, getOctokit } from '@actions/github'
+import { setFailed } from '@actions/core'
+const { WebClient } = require('@slack/web-api')
+
+async function run() {
+  try {
+    if (!process.env.GITHUB_TOKEN) throw new TypeError('GITHUB_TOKEN not set')
+    if (!process.env.SLACK_TOKEN) throw new TypeError('SLACK_TOKEN not set')
+
+    const octoClient = getOctokit(process.env.GITHUB_TOKEN)
+    const slackClient = new WebClient(process.env.SLACK_TOKEN)
+
+    const prs = await octoClient.rest.pulls.list({
+      repo: context.repo.repo,
+      owner: context.repo.owner,
+    })
+
+    await slackClient.chat.postMessage({
+      channel: '#team-next-js',
+      text: `${prs.data.length} PR(s) are awaiting merge.`,
+      username: 'GitHub Notifier',
+      icon_emoji: ':github:',
+    })
+  } catch (error) {
+    setFailed(error)
+  }
+}
+
+run()
