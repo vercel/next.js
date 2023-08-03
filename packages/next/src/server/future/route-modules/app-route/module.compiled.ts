112ef014e0a487789d8d0608b73d688a739fa5bf
Jimmy Lai <laijimmy0@gmail.com>
2023-08-03 13:18:31 +0200

    fix edge

--- packages/next/src/server/future/route-modules/app-route/module.compiled.ts
+++ packages/next/src/server/future/route-modules/app-route/module.compiled.ts
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('next/dist/compiled/next-server/app-route.runtime.prod.js')
-} else {
-  module.exports = require('next/dist/compiled/next-server/app-route.runtime.dev.js')
+if (process.env.NEXT_RUNTIME !== 'edge') {
+  if (process.env.NODE_ENV === 'production') {
+    module.exports = require('next/dist/compiled/next-server/app-route.runtime.prod.js')
+  } else {
+    module.exports = require('next/dist/compiled/next-server/app-route.runtime.dev.js')
+  }
 }
