3e6fbb57e300ea32209e64afc8f1c1f93b6eaf5f
Jimmy Lai <laijimmy0@gmail.com>
2023-08-03 16:20:59 +0200

    revert changes to start-server

--- packages/next/src/server/lib/start-server.ts
+++ packages/next/src/server/lib/start-server.ts
@@ -242,7 +242,7 @@ export async function startServer({
   })
 
   try {
-    if (false) {
+    if (useWorkers) {
       const httpProxy =
         require('next/dist/compiled/http-proxy') as typeof import('next/dist/compiled/http-proxy')
 
