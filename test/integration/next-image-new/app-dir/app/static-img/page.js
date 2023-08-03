77dd4112a77d8d578a3197f13667359cfa05ba62
Artur Bień <artur.bien@gmail.com>
2023-08-03 16:34:57 +0000

    fixup! add static image tests

--- test/integration/next-image-new/app-dir/app/static-img/page.js
+++ test/integration/next-image-new/app-dir/app/static-img/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import testImg from '../../public/foo/test-rect-broken.jpg'
+import testImg from '../../public/foo/test-rect.jpg'
 import Image from 'next/image'
 
 import testJPG from '../../public/test.jpg'
