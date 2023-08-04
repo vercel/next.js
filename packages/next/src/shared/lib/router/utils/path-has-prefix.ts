30a5c97679e1e71da668b02f100d1ca78f230edb
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-08-04 11:06:52 -0600

    fix: improve `pathHasPrefix` performance

--- packages/next/src/shared/lib/router/utils/path-has-prefix.ts
+++ packages/next/src/shared/lib/router/utils/path-has-prefix.ts
@@ -8,7 +8,7 @@ import { parsePath } from './parse-path'
  * @param prefix The prefix to check against.
  */
 export function pathHasPrefix(path: string, prefix: string) {
-  if (typeof path !== 'string') {
+  if (typeof path !== 'string' || prefix.length === 0) {
     return false
   }
 
