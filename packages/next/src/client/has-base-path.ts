158c0368a49177157dc572cad0abf1f5d7fa22f3
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-08-04 13:18:49 -0700

    fix: revert changes to basePath optimization

--- packages/next/src/client/has-base-path.ts
+++ packages/next/src/client/has-base-path.ts
@@ -3,10 +3,5 @@ import { pathHasPrefix } from '../shared/lib/router/utils/path-has-prefix'
 const basePath = (process.env.__NEXT_ROUTER_BASEPATH as string) || ''
 
 export function hasBasePath(path: string): boolean {
-  // If there is no base path, the pathname can't have one!
-  if (basePath === '') {
-    return false
-  }
-
   return pathHasPrefix(path, basePath)
 }
