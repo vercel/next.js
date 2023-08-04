47e0059191bee2c13cc55e20af5fc460c75b8911
Tim Neutkens <tim@timneutkens.nl>
2023-08-04 11:11:51 +0200

    Use fixture for test

--- test/development/watch-config-file/index.test.ts
+++ test/development/watch-config-file/index.test.ts
@@ -1,23 +1,10 @@
 import { createNextDescribe } from 'e2e-utils'
 import { check } from 'next-test-utils'
-
+import { join } from 'path'
 createNextDescribe(
   'watch-config-file',
   {
-    files: {
-      'pages/index.js': `
-          export default function Page() { 
-            return <p>hello world</p>
-          } 
-        `,
-      'next.config.js': `
-        const nextConfig = {
-          reactStrictMode: true,
-        }
-        
-        module.exports = nextConfig        
-        `,
-    },
+    files: join(__dirname, 'fixture'),
   },
   ({ next }) => {
     it('should output config file change', async () => {
