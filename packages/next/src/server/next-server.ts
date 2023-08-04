a1886a1fef358adbe6c792a42788898aa8a0bbb9
Jiachi Liu <inbox@huozhi.im>
2023-08-04 17:39:48 +0200

    rm log

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -1276,12 +1276,6 @@ export default class NextNodeServer extends BaseServer {
           .catch(() => {})
       }
 
-      console.log(
-        'this.getEdgeFunctionsPages()',
-        this.getEdgeFunctionsPages(),
-        'notFoundPathname',
-        notFoundPathname
-      )
       if (this.getEdgeFunctionsPages().includes(notFoundPathname)) {
         await this.runEdgeFunction({
           req: req as BaseNextRequest,
