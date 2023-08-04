4c097e166fafb41464771e7ba9042639be19b143
Tim Neutkens <tim@timneutkens.nl>
2023-08-04 12:28:08 +0200

    Reset test

--- test/development/watch-config-file/fixture/next.config.js
+++ test/development/watch-config-file/fixture/next.config.js
@@ -1,13 +1,5 @@
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
 }
+
 module.exports = nextConfig
