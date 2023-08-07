a2a672340126ed5f6c6020b8316a09bc855359bd
tknickman <tom.knickman@vercel.com>
2023-08-07 12:20:10 -0400

    use types from plop directly in helpers

--- turbo/generators/helpers.ts
+++ turbo/generators/helpers.ts
@@ -1,9 +1,9 @@
-import type { PlopTypes } from '@turbo/gen'
+import type { NodePlopAPI } from 'node-plop'
 
 export function toFileName(str: string) {
   return str.toLowerCase().replace(/ /g, '-')
 }
 
-export function init(plop: PlopTypes.NodePlopAPI): void {
+export function init(plop: NodePlopAPI): void {
   plop.setHelper('toFileName', toFileName)
 }
