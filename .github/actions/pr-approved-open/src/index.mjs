dac2a551c9403e1f60b1a3b73dcf844467842a32
Balázs Orbán <info@balazsorban.com>
2023-08-07 14:28:44 +0200

    fix link

--- .github/actions/pr-approved-open/src/index.mjs
+++ .github/actions/pr-approved-open/src/index.mjs
@@ -21,7 +21,7 @@ async function run() {
     if (pendingPRs) {
       await slackClient.chat.postMessage({
         channel: '#coord-next-turbopack',
-        text: `🤖 Pending PRs for Next.js: There are <https://github.com/vercel/next.js/pulls?q=is%3Apr+is%3Aopen+review%3Aapproved|${prs.data.items.length} PRs> awaiting merge.`,
+        text: `🤖 Pending PRs for Next.js: There are <https://github.com/vercel/next.js/pulls?q=is%3Apr+is%3Aopen+review%3Aapproved+-is%3Adraft|${prs.data.items.length} PRs> awaiting merge.`,
         username: 'GitHub Notifier',
         icon_emoji: ':github:',
       })
