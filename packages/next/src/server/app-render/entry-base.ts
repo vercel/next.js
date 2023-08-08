0d4dbdce421e7f4d614171a9a915a90935944b6d
Jimmy Lai <laijimmy0@gmail.com>
2023-08-08 14:19:54 +0200

    cleanup entry-base

--- packages/next/src/server/app-render/entry-base.ts
+++ packages/next/src/server/app-render/entry-base.ts
@@ -1,36 +1,9 @@
-// const { default: AppRouter } =
-//   require('next/dist/client/components/app-router') as typeof import('../../client/components/app-router')
-// const { default: LayoutRouter } =
-//   require('next/dist/client/components/layout-router') as typeof import('../../client/components/layout-router')
-// const { default: RenderFromTemplateContext } =
-//   require('next/dist/client/components/render-from-template-context') as typeof import('../../client/components/render-from-template-context')
-
-// const { staticGenerationAsyncStorage } =
-//   require('next/dist/client/components/static-generation-async-storage.shared-runtime') as typeof import('../../client/components/static-generation-async-storage.shared-runtime')
-
-// const { requestAsyncStorage } =
-//   require('next/dist/client/components/request-async-storage.shared-runtime') as typeof import('../../client/components/request-async-storage.shared-runtime')
-// const { actionAsyncStorage } =
-//   require('next/dist/client/components/action-async-storage.shared-runtime') as typeof import('../../client/components/action-async-storage.shared-runtime')
-
-// const { staticGenerationBailout } =
-//   require('next/dist/client/components/static-generation-bailout') as typeof import('../../client/components/static-generation-bailout')
-// const { default: StaticGenerationSearchParamsBailoutProvider } =
-//   require('next/dist/client/components/static-generation-searchparams-bailout-provider') as typeof import('../../client/components/static-generation-searchparams-bailout-provider')
-// const { createSearchParamsBailoutProxy } =
-//   require('next/dist/client/components/searchparams-bailout-proxy') as typeof import('../../client/components/searchparams-bailout-proxy')
-
-// const serverHooks =
-//   require('next/dist/client/components/hooks-server-context') as typeof import('../../client/components/hooks-server-context')
-
 const {
   renderToReadableStream,
   decodeReply,
   decodeAction,
   // eslint-disable-next-line import/no-extraneous-dependencies
 } = require('react-server-dom-webpack/server.edge')
-// const { preloadStyle, preloadFont, preconnect } =
-//   require('next/dist/server/app-render/rsc/preloads') as typeof import('../../server/app-render/rsc/preloads')
 
 import AppRouter from '../../client/components/app-router'
 import LayoutRouter from '../../client/components/layout-router'
