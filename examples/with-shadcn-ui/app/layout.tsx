bad0eff68695468965d19ce58b979c4fd5abe42b
vinay <94120295+vinaykulk621@users.noreply.github.com>
2023-08-08 11:18:57 +0530

    update-code

--- examples/with-shadcn-ui/app/layout.tsx
+++ examples/with-shadcn-ui/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'shadcn ui',
   description: 'shadcn ui',
 }
