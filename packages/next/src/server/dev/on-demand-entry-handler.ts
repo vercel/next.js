12bb0d9cf11ab31a456f5d8dac1f12c89a434f8d
Zack Tanner <zacktanner@gmail.com>
2023-08-14 10:37:36 -0700

    lint

--- packages/next/src/server/dev/on-demand-entry-handler.ts
+++ packages/next/src/server/dev/on-demand-entry-handler.ts
@@ -101,7 +101,7 @@ export function getEntryKey(
   // TODO: handle the /children slot better
   // this is a quick hack to handle when children is provided as children/page instead of /page
   return `${compilerType}@${pageBundleType}@${page.replace(
-    /(@[^\/]+)\/children/g,
+    /(@[^/]+)\/children/g,
     '$1'
   )}`
 }
