cd95fe408500c647cd0a4aebb06091937f1a6122
Shu Ding <g@shud.in>
2023-08-15 01:16:28 +0200

    Adjust internal action proxy export (#54004)
    This PR changes the internal `createActionProxy` util from a default
    export to a named export, as the default export could be problematic if
    it's built without the "esModuleInterop" step.
    
    ---------
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- packages/next-swc/crates/core/tests/fixture/server-actions/server/3/output.js
+++ packages/next-swc/crates/core/tests/fixture/server-actions/server/3/output.js
@@ -1,5 +1,5 @@
 // app/send.ts
-/* __next_internal_action_entry_do_not_use__ myAction */ import __create_action_proxy__ from "private-next-rsc-action-proxy";
+/* __next_internal_action_entry_do_not_use__ myAction */ import { createActionProxy } from "private-next-rsc-action-proxy";
 export async function myAction(a, b, c) {
     console.log('a');
 }
@@ -7,4 +7,4 @@ import ensureServerEntryExports from "private-next-rsc-action-validate";
 ensureServerEntryExports([
     myAction
 ]);
-__create_action_proxy__("e10665baac148856374b2789aceb970f66fec33e", null, myAction);
+createActionProxy("e10665baac148856374b2789aceb970f66fec33e", null, myAction);
