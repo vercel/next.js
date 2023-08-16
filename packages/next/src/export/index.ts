f944e984bd0d614be6b87ac870f961ede3d7cdec
Steven <steven@ceriously.com>
2023-08-16 11:30:30 -0400

    chore: refactor to use `fs.promises.rm()` (#54076)
    Since Next.js already requires [`node@16.8.0`](https://github.com/vercel/next.js/blob/88b8d15f41f894011cf33af0b1f4a3fb24aed46b/packages/next/package.json#L319) or newer, we can drop old techniques for removing a directory recursively and instead rely on the native [`fs.promises.rm()`](https://nodejs.org/api/fs.html#fspromisesrmpath-options) function (available since `node@14.14.0`).
    
    x-ref: https://github.com/vercel/next.js/pull/34210

--- packages/next/src/export/index.ts
+++ packages/next/src/export/index.ts
@@ -17,7 +17,6 @@ import * as Log from '../build/output/log'
 import createSpinner from '../build/spinner'
 import { SSG_FALLBACK_EXPORT_ERROR } from '../lib/constants'
 import { recursiveCopy } from '../lib/recursive-copy'
-import { recursiveDelete } from '../lib/recursive-delete'
 import {
   BUILD_ID_FILE,
   CLIENT_PUBLIC_FILES_PATH,
@@ -354,7 +353,7 @@ export default async function exportApp(
       )
     }
 
-    await recursiveDelete(join(outDir))
+    await promises.rm(outDir, { recursive: true, force: true })
     await promises.mkdir(join(outDir, '_next', buildId), { recursive: true })
 
     writeFileSync(
