94ee83c0c056046300234169a603440d050f344a
SukkaW <isukkaw@gmail.com>
2023-08-16 14:28:26 +0800

    fix(#52216): add export interop to `next/image`

--- packages/next/image.js
+++ packages/next/image.js
@@ -1,1 +1,5 @@
-module.exports = require('./dist/shared/lib/image-external')
+const imageExternalExports = require('./dist/shared/lib/image-external')
+
+module.exports = imageExternalExports.default || imageExternalExports
+module.exports.unstable_getImgProps = imageExternalExports.unstable_getImgProps
+module.exports.default = module.exports
