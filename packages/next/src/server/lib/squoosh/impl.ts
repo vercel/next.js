a063ce51b4f61b9ab4a9d38a0c0d5f0310198135
Steven <steven@ceriously.com>
2023-08-16 11:29:30 -0400

    chore: remove workaround for versions older than `node@16.5.0`

--- packages/next/src/server/lib/squoosh/impl.ts
+++ packages/next/src/server/lib/squoosh/impl.ts
@@ -4,31 +4,6 @@ import ImageData from './image_data'
 
 type EncoderKey = keyof typeof supportedFormats
 
-// Fixed in Node.js 16.5.0 and newer.
-// See https://github.com/nodejs/node/pull/39337
-// Eventually, remove this delay when engines is updated.
-// See https://github.com/vercel/next.js/blob/1bcc923439f495a1717421e06af7e64c6003072c/packages/next/package.json#L249-L251
-const FIXED_VERSION = '16.5.0'
-const DELAY_MS = 1000
-let _promise: Promise<void> | undefined
-
-function delayOnce(ms: number): Promise<void> {
-  if (!_promise) {
-    _promise = new Promise((resolve) => {
-      setTimeout(resolve, ms)
-    })
-  }
-  return _promise
-}
-
-function maybeDelay(): Promise<void> {
-  const isAppleM1 = process.arch === 'arm64' && process.platform === 'darwin'
-  if (isAppleM1 && semver.lt(process.version, FIXED_VERSION)) {
-    return delayOnce(DELAY_MS)
-  }
-  return Promise.resolve()
-}
-
 export async function decodeBuffer(
   _buffer: Buffer | Uint8Array
 ): Promise<ImageData> {
@@ -70,7 +45,6 @@ export async function resize({ image, width, height }: ResizeOpts) {
 
   const p = preprocessors['resize']
   const m = await p.instantiate()
-  await maybeDelay()
   return await m(image.data, image.width, image.height, {
     ...p.defaultOptions,
     width,
