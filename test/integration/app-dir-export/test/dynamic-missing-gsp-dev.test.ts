60438b18ae89ed3b21d69165c3c660857e2867a3
Steven <steven@ceriously.com>
2023-08-18 17:32:38 -0400

    fix tests

--- test/integration/app-dir-export/test/dynamic-missing-gsp-dev.test.ts
+++ test/integration/app-dir-export/test/dynamic-missing-gsp-dev.test.ts
@@ -3,8 +3,9 @@ import { runTests } from './utils'
 it('should error when dynamic route is missing generateStaticParams', async () => {
   await runTests({
     isDev: true,
+    dynamicPage: 'undefined',
     generateStaticParamsEnabled: false,
     expectedErrMsg:
-      'Page "/another/[slug]" is missing exported function "generateStaticParams()", which is required with "output: export" config.',
+      'Page "/another/[slug]/page" is missing exported function "generateStaticParams()", which is required with "output: export" config.',
   })
 })
--- test/integration/app-dir-export/test/dynamic-missing-gsp-prod.test.ts
+++ test/integration/app-dir-export/test/dynamic-missing-gsp-prod.test.ts
@@ -3,6 +3,7 @@ import { runTests } from './utils'
 it('should error when dynamic route is missing generateStaticParams', async () => {
   await runTests({
     isDev: false,
+    dynamicPage: 'undefined',
     generateStaticParamsEnabled: false,
     expectedErrMsg:
       'Page "/another/[slug]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.',
