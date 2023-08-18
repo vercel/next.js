0c0b2f2c1366a0f19f4328c10c4d4f5394f1b7ec
Jiachi Liu <inbox@huozhi.im>
2023-08-18 17:39:27 +0200

    fix test

--- test/e2e/app-dir/not-found-default/index.test.ts
+++ test/e2e/app-dir/not-found-default/index.test.ts
@@ -74,19 +74,20 @@ createNextDescribe(
     })
 
     it('should render default not found for group routes if not found is not defined', async () => {
-      const groupDynamicBrowser = await next.browser('/group-dynamic/123')
-      const groupDynamic404Browser = await next.browser('/group-dynamic/404')
-
-      expect(await groupDynamicBrowser.elementByCss('#page').text()).toBe(
+      const browser = await next.browser('/group-dynamic/123')
+      console.group('0')
+      expect(await browser.elementByCss('#page').text()).toBe(
         'group-dynamic [id]'
       )
 
-      expect(
-        await groupDynamic404Browser.elementByCss('.next-error-h1').text()
-      ).toBe('404')
-      expect(
-        await groupDynamic404Browser.elementByCss('html').attr('class')
-      ).toBe('group-root-layout')
+      console.group('1')
+      await browser.loadPage(next.url + '/group-dynamic/404')
+      console.group('2')
+      expect(await browser.elementByCss('.next-error-h1').text()).toBe('404')
+      console.group('3')
+      expect(await browser.elementByCss('html').getAttribute('class')).toBe(
+        'group-root-layout'
+      )
     })
   }
 )
