8b47000a9b157e1adf338da5fc328353ad0d2610
Nego <61694826+kardespro@users.noreply.github.com>
2023-08-21 23:16:43 +0400

    Create tailwind.config.js

---
+++ examples/with-tailwindcss-framermotion/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
