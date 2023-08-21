ef9f034a355decc4deb9ee3105c2114f58cd2733
Nego <61694826+kardespro@users.noreply.github.com>
2023-08-21 23:31:54 +0400

    Create index.tsx

---
+++ examples/with-tailwindcss-framermotion/pages/index.tsx
@@ -0,0 +1,22 @@
+import Modal from '../components/Modal'
+import Button from '../components/Button'
+import { useState } from 'react';
+
+export default function Test() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalToggle = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  return (
+    <>
+      <Button text="Open Modal" backgroundColor="blue-600" onClick={handleModalToggle} />
+      <Modal isOpen={modalOpen} onClose={handleModalToggle} closeButtonColor="blue-600" animationType="easeInOut" blurPx="10px">
+        <h1 className="text-center font-extrabold bg-gradient-to-r to-white from-black  text-transparent  bg-clip-text delay-150 duration-100 animate-ping">Welcome to Next.js Application</h1>
+      </Modal>
+
+      
+    </>
+  );
+}
