8612845b46f5fd56627134036328a6d70009da5b
indraantoor <indraantoor@gmail.com>
2023-08-21 20:23:43 +0530

    fix: weight above 900 not working with google fonts
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- packages/font/src/google/get-google-fonts-url.ts
+++ packages/font/src/google/get-google-fonts-url.ts
@@ -50,10 +50,31 @@ export function getGoogleFontsUrl(
     '+'
   )}`
 
+  function sortVariantValues(valA: string, valB: string) {
+    // If both values contain commas, it indicates they are in "ital,wght" format
+    if (valA.includes(',') && valB.includes(',')) {
+      // Split the values into prefix and suffix
+      const [aPrefix, aSuffix] = valA.split(',')
+      const [bPrefix, bSuffix] = valB.split(',')
+
+      // Compare the prefixes (ital values)
+      if (aPrefix === bPrefix) {
+        // If prefixes are equal, then compare the suffixes (wght values)
+        return parseInt(aSuffix) - parseInt(bSuffix)
+      } else {
+        // If prefixes are different, then compare the prefixes directly
+        return parseInt(aPrefix) - parseInt(bPrefix)
+      }
+    }
+
+    // If values are not in "ital,wght" format, then directly compare them as integers
+    return parseInt(valA) - parseInt(valB)
+  }
+
   if (variants.length > 0) {
     url = `${url}:${variants[0].map(([key]) => key).join(',')}@${variants
       .map((variant) => variant.map(([, val]) => val).join(','))
-      .sort()
+      .sort(sortVariantValues)
       .join(';')}`
   }
 
@@ -61,3 +82,4 @@ export function getGoogleFontsUrl(
 
   return url
 }
+
