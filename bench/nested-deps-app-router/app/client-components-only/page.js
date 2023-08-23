e08da1e17efa42120d65fb2c79b01172fdba39af
Tim Neutkens <tim@timneutkens.nl>
2023-08-23 13:44:36 +0200

    Fix package.json

--- bench/nested-deps-app-router/app/client-components-only/page.js
+++ bench/nested-deps-app-router/app/client-components-only/page.js
@@ -3,5 +3,10 @@
 import Comp from '../../components/index.jsx'
 
 export default function Home() {
-  return <Comp />
+  return (
+    <>
+      <h1>Hello!!!!!!!!!!!!</h1>
+      <Comp />
+    </>
+  )
 }
