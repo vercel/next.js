b3c2c636e02d8397c9e0211648318bb52714b17d
Tobias Koppers <tobias.koppers@googlemail.com>
2023-08-30 12:29:18 +0000

    fix import

--- packages/next/src/client/next-dev-turbopack.ts
+++ packages/next/src/client/next-dev-turbopack.ts
@@ -5,8 +5,8 @@ import initWebpackHMR from './dev/webpack-hot-middleware-client'
 import './setup-hydration-warning'
 import { pageBootrap } from './page-bootstrap'
 import { addMessageListener, sendMessage } from './dev/error-overlay/websocket'
-//@ts-expect-error requires "moduleResolution": "node16" in tsconfig.json
-import { connect } from '@vercel/turbopack-ecmascript-runtime/dev/client/hmr-client'
+//@ts-expect-error requires "moduleResolution": "node16" in tsconfig.json and not .ts extension
+import { connect } from '@vercel/turbopack-ecmascript-runtime/dev/client/hmr-client.ts'
 
 window.next = {
   version: `${version}-turbo`,
