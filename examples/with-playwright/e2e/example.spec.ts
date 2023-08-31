8f4a1ad22b149160f6f4542ad8deeeadde8059ad
Balázs Orbán <info@balazsorban.com>
2023-08-31 17:52:42 +0200

    Update example.spec.ts

--- examples/with-playwright/e2e/example.spec.ts
+++ examples/with-playwright/e2e/example.spec.ts
@@ -8,5 +8,7 @@ test('should navigate to the about page', async ({ page }) => {
   // The new url should be "/about" (baseURL is used there)
   await expect(page).toHaveURL('/about')
   // The new page should contain an h1 with "About Page"
-  await expect(page.getByRole('heading', { level: 1 })).toContainText('About Page')
+  await expect(page.getByRole('heading', { level: 1 })).toContainText(
+    'About Page'
+  )
 })
