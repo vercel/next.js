ef33018a3d7e11c5de376f21637c7db969045a89
Zack Tanner <zacktanner@gmail.com>
2023-09-01 11:56:51 -0700

    fix next/headers when used in src/middleware (#54889)
    This updates the check to see if a request is made in middleware to be a bit less brittle so it doesn't break if middleware is located in `/src/middleware` (`params.page` would be `/src/middleware` rather than `/middleware` in that case)
    
    fixes https://github.com/vercel/next.js/discussions/54853

--- test/e2e/app-dir/app-middleware/app-middleware.test.ts
+++ test/e2e/app-dir/app-middleware/app-middleware.test.ts
@@ -166,3 +166,37 @@ createNextDescribe(
     })
   }
 )
+
+createNextDescribe(
+  'app dir middleware with middleware in src dir',
+  {
+    files: {
+      'src/app': new FileRef(path.join(__dirname, 'app')),
+      'next.config.js': new FileRef(path.join(__dirname, 'next.config.js')),
+      'src/middleware.js': `
+      import { NextResponse } from 'next/server'
+      import { cookies } from 'next/headers'
+
+      export async function middleware(request) {
+        const cookie = cookies().get('test-cookie')
+        return NextResponse.json({ cookie })
+      }
+    `,
+    },
+    skipDeployment: true,
+  },
+  ({ next }) => {
+    it('works without crashing when using requestAsyncStorage', async () => {
+      const browser = await next.browser('/')
+      await browser.addCookie({
+        name: 'test-cookie',
+        value: 'test-cookie-response',
+      })
+      await browser.refresh()
+
+      const html = await browser.eval('document.documentElement.innerHTML')
+
+      expect(html).toContain('test-cookie-response')
+    })
+  }
+)
