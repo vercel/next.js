65f309dafa300c2bf8c8713c47207d47e47c8e26
Tim Neutkens <tim@timneutkens.nl>
2023-09-04 11:27:04 +0200

    Remove pong event as it is not used

--- packages/next/src/server/dev/hot-reloader-types.ts
+++ packages/next/src/server/dev/hot-reloader-types.ts
@@ -15,7 +15,6 @@ export const enum HMR_ACTIONS_SENT_TO_BROWSER {
   SERVER_ONLY_CHANGES = 'serverOnlyChanges',
   BUILT = 'built',
   BUILDING = 'building',
-  PONG = 'pong',
   DEV_PAGES_MANIFEST_UPDATE = 'devPagesManifestUpdate',
   TURBOPACK_MESSAGE = 'turbopack-message',
   SERVER_ERROR = 'serverError',
@@ -69,16 +68,6 @@ interface ServerOnlyChangesAction {
   pages: ReadonlyArray<string>
 }
 
-interface PongActionAppRouter {
-  action: HMR_ACTIONS_SENT_TO_BROWSER.PONG
-  success: boolean
-}
-
-interface PongActionPagesRouter {
-  event: HMR_ACTIONS_SENT_TO_BROWSER.PONG
-  success: boolean
-}
-
 interface DevPagesManifestUpdateAction {
   action: HMR_ACTIONS_SENT_TO_BROWSER.DEV_PAGES_MANIFEST_UPDATE
   data: [
@@ -88,11 +77,8 @@ interface DevPagesManifestUpdateAction {
   ]
 }
 
-type PongAction = PongActionAppRouter | PongActionPagesRouter
-
 export type HMR_ACTION_TYPES =
   | TurboPackMessageAction
-  | PongAction
   | BuildingAction
   | BuiltAction
   | AddedPageAction
--- packages/next/src/server/dev/on-demand-entry-handler.ts
+++ packages/next/src/server/dev/on-demand-entry-handler.ts
@@ -615,89 +615,6 @@ export function onDemandEntryHandler({
     disposeInactiveEntries(curEntries, maxInactiveAge)
   }, pingIntervalTime + 1000).unref()
 
-  function handleAppDirPing(
-    tree: FlightRouterState
-  ): { success: true } | { invalid: true } {
-    const pages = getEntrypointsFromTree(tree, true)
-    let toSend: { invalid: true } | { success: true } = { invalid: true }
-
-    for (const page of pages) {
-      for (const compilerType of [
-        COMPILER_NAMES.client,
-        COMPILER_NAMES.server,
-        COMPILER_NAMES.edgeServer,
-      ]) {
-        const entryKey = getEntryKey(compilerType, 'app', `/${page}`)
-        const entryInfo = curEntries[entryKey]
-
-        // If there's no entry, it may have been invalidated and needs to be re-built.
-        if (!entryInfo) {
-          // if (page !== lastEntry) client pings, but there's no entry for page
-          continue
-        }
-
-        // We don't need to maintain active state of anything other than BUILT entries
-        if (entryInfo.status !== BUILT) continue
-
-        // If there's an entryInfo
-        if (!lastServerAccessPagesForAppDir.includes(entryKey)) {
-          lastServerAccessPagesForAppDir.unshift(entryKey)
-
-          // Maintain the buffer max length
-          // TODO: verify that the current pageKey is not at the end of the array as multiple entrypoints can exist
-          if (lastServerAccessPagesForAppDir.length > pagesBufferLength) {
-            lastServerAccessPagesForAppDir.pop()
-          }
-        }
-        entryInfo.lastActiveTime = Date.now()
-        entryInfo.dispose = false
-        toSend = { success: true }
-      }
-    }
-
-    return toSend
-  }
-
-  function handlePing(pg: string) {
-    const page = normalizePathSep(pg)
-    let toSend: { invalid: true } | { success: true } = { invalid: true }
-
-    for (const compilerType of [
-      COMPILER_NAMES.client,
-      COMPILER_NAMES.server,
-      COMPILER_NAMES.edgeServer,
-    ]) {
-      const entryKey = getEntryKey(compilerType, 'pages', page)
-      const entryInfo = curEntries[entryKey]
-
-      // If there's no entry, it may have been invalidated and needs to be re-built.
-      if (!entryInfo) {
-        // if (page !== lastEntry) client pings, but there's no entry for page
-        if (compilerType === COMPILER_NAMES.client) {
-          return { invalid: true }
-        }
-        continue
-      }
-
-      // We don't need to maintain active state of anything other than BUILT entries
-      if (entryInfo.status !== BUILT) continue
-
-      // If there's an entryInfo
-      if (!lastClientAccessPages.includes(entryKey)) {
-        lastClientAccessPages.unshift(entryKey)
-
-        // Maintain the buffer max length
-        if (lastClientAccessPages.length > pagesBufferLength) {
-          lastClientAccessPages.pop()
-        }
-      }
-      entryInfo.lastActiveTime = Date.now()
-      entryInfo.dispose = false
-      toSend = { success: true }
-    }
-    return toSend
-  }
-
   async function ensurePageImpl({
     page,
     clientOnly,
@@ -945,7 +862,7 @@ export function onDemandEntryHandler({
       client.addEventListener('close', () => {
         bufferedHmrServerError = null
       })
-      client.addEventListener('message', ({ data }) => {
+      client.addEventListener('message', () => {
         try {
           const error = getHmrServerError()
 
@@ -956,22 +873,6 @@ export function onDemandEntryHandler({
               errorJSON: stringifyError(error),
             })
             bufferedHmrServerError = null
-          }
-
-          const parsedData = JSON.parse(
-            typeof data !== 'string' ? data.toString() : data
-          )
-
-          if (parsedData.event === 'ping') {
-            const result = parsedData.appDirRoute
-              ? handleAppDirPing(parsedData.tree)
-              : handlePing(parsedData.page)
-            client.send(
-              JSON.stringify({
-                ...result,
-                [parsedData.appDirRoute ? 'action' : 'event']: 'pong',
-              })
-            )
           }
         } catch {}
       })
