330944e45f5018c31aae0f4241f4a9c1853f2088
Balázs Orbán <info@balazsorban.com>
2023-09-13 16:15:43 +0200

    use undici's proxy Agent

--- packages/font/src/google/get-proxy-agent.ts
+++ packages/font/src/google/get-proxy-agent.ts
@@ -1,20 +1,13 @@
 // @ts-ignore
-import HttpsProxyAgent from 'next/dist/compiled/https-proxy-agent'
-// @ts-ignore
-import HttpProxyAgent from 'next/dist/compiled/http-proxy-agent'
-import { Agent } from 'https'
+import { Agent } from 'next/src/compiled/undici'
 
-/**
- * If the http(s)_proxy environment variables is set, return a proxy agent.
- */
+/* If the http(s)_proxy environment variables is set, return a proxy agent. */
 export function getProxyAgent(): Agent | undefined {
-  const httpsProxy = process.env['https_proxy'] || process.env['HTTPS_PROXY']
-  if (httpsProxy) {
-    return new HttpsProxyAgent(httpsProxy)
-  }
+  const proxy =
+    process.env['https_proxy'] ??
+    process.env['HTTPS_PROXY'] ??
+    process.env['http_proxy'] ??
+    process.env['HTTP_PROXY']
 
-  const httpProxy = process.env['http_proxy'] || process.env['HTTP_PROXY']
-  if (httpProxy) {
-    return new HttpProxyAgent(httpProxy)
-  }
+  if (proxy) return new Agent(proxy)
 }
