2251d4efc81366f50f211a4a276162115e4fd71e
Jiachi Liu <inbox@huozhi.im>
2023-09-19 15:29:58 +0200

    add React.cache test

--- test/e2e/app-dir/metadata-dynamic-routes/app/client-ref-dependency/sitemap.tsx
+++ test/e2e/app-dir/metadata-dynamic-routes/app/client-ref-dependency/sitemap.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { clientRef } from './client-component'
 
 export const contentType = 'image/png'
+const cachedNoop = React.cache(() => null)
 
 function noopCall(value) {
   return value
@@ -9,5 +11,6 @@ function noopCall(value) {
 export default function sitemap() {
   // keep the variable from being tree-shaken
   noopCall(clientRef)
+  cachedNoop()
   return []
 }
