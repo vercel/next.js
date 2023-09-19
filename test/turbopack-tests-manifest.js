8ad2f0542d7c899e0ef0ed9a6af3e491ad43f2d3
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-09-19 14:49:43 -0700

    test(turbopack): update test manifests

--- test/turbopack-tests-manifest.js
+++ test/turbopack-tests-manifest.js
@@ -89,6 +89,7 @@ const enabledTests = [
   'test/e2e/typescript-version-warning/typescript-version-warning.test.ts',
   'test/e2e/undici-fetch/index.test.ts',
   'test/integration/api-support/test/index.test.js',
+  'test/integration/app-dir-export/test/config.test.ts',
   'test/integration/404-page/test/index.test.js',
   'test/integration/404-page-app/test/index.test.js',
   'test/integration/404-page-custom-error/test/index.test.js',
@@ -123,9 +124,6 @@ const enabledTests = [
   'test/production/jest/rsc/lib/utils.test.js',
   'test/production/jest/transpile-packages.test.ts',
   'test/production/postcss-plugin-config-as-string/index.test.ts',
-
-  // disabled due to nextConfig.distDir
-  //'test/integration/trailing-slash-dist/test/index.test.js',
 ]
 
 /// Naive check to ensure that the enabled tests actually exist
