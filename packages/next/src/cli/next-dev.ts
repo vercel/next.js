vercel/next.js

commit 15980a66d4bdbcd5e9f3494bcaa1a286f6ec9aee

remove --turbo, use --experimental-turbo as --turbo (#55063)
### What?

Switch the default for `--turbo` to the new `--experimental-turbo`, remove the old code in next.js

### Why?

The new approach will be used in future


Closes WEB-1506

packages/next/src/cli/next-dev.ts
@@ -16,11 +16,10 @@ import { setGlobal, traceGlobals } from '../trace/shared'
 import { Telemetry } from '../telemetry/storage'
 import loadConfig, { getEnabledExperimentalFeatures } from '../server/config'
 import { findPagesDir } from '../lib/find-pages-dir'
-import { findRootDir } from '../lib/find-root'
 import { fileExists, FileType } from '../lib/file-exists'
 import { getNpxCommand } from '../lib/helpers/get-npx-command'
 import Watchpack from 'watchpack'
-import { resetEnv, initialEnv } from '@next/env'
+import { initialEnv } from '@next/env'
 import { getValidatedArgs } from '../lib/get-validated-args'
 import { Worker } from 'next/dist/compiled/jest-worker'
 import type { ChildProcess } from 'child_process'
@@ -144,7 +143,7 @@ async function createRouterWorker(fullConfig: NextConfigComplete): Promise<{
           ? { __NEXT_PRIVATE_CPU_PROFILE: `CPU.router` }
           : {}),
         WATCHPACK_WATCHER_LIMIT: '20',
-        EXPERIMENTAL_TURBOPACK: process.env.EXPERIMENTAL_TURBOPACK,
+        TURBOPACK: process.env.TURBOPACK,
         __NEXT_PRIVATE_PREBUNDLED_REACT: !!fullConfig.experimental.serverActions
           ? 'experimental'
           : 'next',
@@ -348,161 +347,83 @@ const nextDev: CliCommand = async (argv) => {
   if (args['--turbo']) {
     process.env.TURBOPACK = '1'
   }
-  if (args['--experimental-turbo']) {
-    process.env.EXPERIMENTAL_TURBOPACK = '1'
-  }
 
   const distDir = path.join(dir, config.distDir ?? '.next')
   setGlobal('phase', PHASE_DEVELOPMENT_SERVER)
   setGlobal('distDir', distDir)
 
-  if (process.env.TURBOPACK) {
-    isTurboSession = true
+  const runDevServer = async (reboot: boolean) => {
+    try {
+      const workerInit = await createRouterWorker(config)
 
-    const { validateTurboNextConfig } =
-      require('../lib/turbopack-warning') as typeof import('../lib/turbopack-warning')
-    const { loadBindings, __isCustomTurbopackBinary, teardownHeapProfiler } =
-      require('../build/swc') as typeof import('../build/swc')
-    const { eventCliSession } =
-      require('../telemetry/events/version') as typeof import('../telemetry/events/version')
-    require('../telemetry/storage') as typeof import('../telemetry/storage')
-    const findUp =
-      require('next/dist/compiled/find-up') as typeof import('next/dist/compiled/find-up')
+      if (!!args['--experimental-https']) {
+        Log.warn(
+          'Self-signed certificates are currently an experimental feature, use at your own risk.'
+        )
 
-    const isCustomTurbopack = await __isCustomTurbopackBinary()
-    const rawNextConfig = await validateTurboNextConfig({
-      isCustomTurbopack,
-      ...devServerOptions,
-      isDev: true,
-    })
+        let certificate: { key: string; cert: string } | undefined
 
-    const { pagesDir, appDir } = findPagesDir(dir)
-    const telemetry = new Telemetry({
-      distDir,
-    })
-    setGlobal('appDir', appDir)
-    setGlobal('pagesDir', pagesDir)
-    setGlobal('telemetry', telemetry)
+        if (
+          args['--experimental-https-key'] &&
+          args['--experimental-https-cert']
+        ) {
+          certificate = {
+            key: path.resolve(args['--experimental-https-key']),
+            cert: path.resolve(args['--experimental-https-cert']),
+          }
+        } else {
+          certificate = await createSelfSignedCertificate(host)
+        }
 
-    if (!isCustomTurbopack) {
-      telemetry.record(
-        eventCliSession(distDir, rawNextConfig as NextConfigComplete, {
-          webpackVersion: 5,
-          cliCommand: 'dev',
-          isSrcDir: path
-            .relative(dir, pagesDir || appDir || '')
-            .startsWith('src'),
-          hasNowJson: !!(await findUp('now.json', { cwd: dir })),
-          isCustomServer: false,
-          turboFlag: true,
-          pagesDir: !!pagesDir,
-          appDir: !!appDir,
+        await workerInit.worker.startServer({
+          ...devServerOptions,
+          selfSignedCertificate: certificate,
         })
+      } else {
+        await workerInit.worker.startServer(devServerOptions)
+      }
+
+      await preflight(reboot)
+      return {
+        cleanup: workerInit.cleanup,
+      }
+    } catch (err) {
+      console.error(err)
+      process.exit(1)
+    }
+  }
+
+  let runningServer: Awaited<ReturnType<typeof runDevServer>> | undefined
+
+  watchConfigFiles(devServerOptions.dir, async (filename) => {
+    if (process.env.__NEXT_DISABLE_MEMORY_WATCHER) {
+      Log.info(
+        `Detected change, manual restart required due to '__NEXT_DISABLE_MEMORY_WATCHER' usage`
       )
+      return
     }
-
-    // Turbopack need to be in control over reading the .env files and watching them.
-    // So we need to start with a initial env to know which env vars are coming from the user.
-    resetEnv()
-    let server
-    trace('start-dev-server').traceAsyncFn(async (_) => {
-      let bindings = await loadBindings()
-
-      server = bindings.turbo.startDev({
-        ...devServerOptions,
-        showAll: args['--show-all'] ?? false,
-        root: args['--root'] ?? findRootDir(dir),
-      })
-
-      // Start preflight after server is listening and ignore errors:
-      preflight(false).catch(() => {})
-    })
-
-    if (!isCustomTurbopack) {
-      await telemetry.flush()
-    }
-
-    // There are some cases like test fixtures teardown that normal flush won't hit.
-    // Force flush those on those case, but don't wait for it.
-    ;['SIGTERM', 'SIGINT', 'beforeExit', 'exit'].forEach((event) =>
-      process.on(event, () => teardownHeapProfiler())
+    // Adding a new line to avoid the logs going directly after the spinner in `next build`
+    Log.warn('')
+    Log.warn(
+      `Found a change in ${path.basename(
+        filename
+      )}. Restarting the server to apply the changes...`
     )
 
-    return server
-  } else {
-    const runDevServer = async (reboot: boolean) => {
-      try {
-        const workerInit = await createRouterWorker(config)
-
-        if (!!args['--experimental-https']) {
-          Log.warn(
-            'Self-signed certificates are currently an experimental feature, use at your own risk.'
-          )
-
-          let certificate: { key: string; cert: string } | undefined
-
-          if (
-            args['--experimental-https-key'] &&
-            args['--experimental-https-cert']
-          ) {
-            certificate = {
-              key: path.resolve(args['--experimental-https-key']),
-              cert: path.resolve(args['--experimental-https-cert']),
-            }
-          } else {
-            certificate = await createSelfSignedCertificate(host)
-          }
-
-          await workerInit.worker.startServer({
-            ...devServerOptions,
-            selfSignedCertificate: certificate,
-          })
-        } else {
-          await workerInit.worker.startServer(devServerOptions)
-        }
-
-        await preflight(reboot)
-        return {
-          cleanup: workerInit.cleanup,
-        }
-      } catch (err) {
-        console.error(err)
-        process.exit(1)
+    try {
+      if (runningServer) {
+        await runningServer.cleanup()
       }
+      runningServer = await runDevServer(true)
+    } catch (err) {
+      console.error(err)
+      process.exit(1)
     }
+  })
 
-    let runningServer: Awaited<ReturnType<typeof runDevServer>> | undefined
-
-    watchConfigFiles(devServerOptions.dir, async (filename) => {
-      if (process.env.__NEXT_DISABLE_MEMORY_WATCHER) {
-        Log.info(
-          `Detected change, manual restart required due to '__NEXT_DISABLE_MEMORY_WATCHER' usage`
-        )
-        return
-      }
-      // Adding a new line to avoid the logs going directly after the spinner in `next build`
-      Log.warn('')
-      Log.warn(
-        `Found a change in ${path.basename(
-          filename
-        )}. Restarting the server to apply the changes...`
-      )
-
-      try {
-        if (runningServer) {
-          await runningServer.cleanup()
-        }
-        runningServer = await runDevServer(true)
-      } catch (err) {
-        console.error(err)
-        process.exit(1)
-      }
-    })
-
-    await trace('start-dev-server').traceAsyncFn(async (_) => {
-      runningServer = await runDevServer(false)
-    })
-  }
+  await trace('start-dev-server').traceAsyncFn(async (_) => {
+    runningServer = await runDevServer(false)
+  })
 }
 
 export { nextDev }

.github/workflows/build_and_test.yml
@@ -124,26 +124,17 @@ jobs:
       afterBuild: turbo run rust-check
     secrets: inherit
 
-  test-experimental-turbopack-dev:
-    name: test experimental turbopack dev
-    needs: ['build-native', 'build-next']
-    uses: ./.github/workflows/build_reusable.yml
-    with:
-      skipForDocsOnly: 'yes'
-      afterBuild: RUST_BACKTRACE=0 NEXT_EXTERNAL_TESTS_FILTERS="$(pwd)/test/turbopack-tests-manifest.js" EXPERIMENTAL_TURBOPACK=1 NEXT_E2E_TEST_TIMEOUT=240000 NEXT_TEST_MODE=dev node run-tests.js --test-pattern '^(test\/development)/.*\.test\.(js|jsx|ts|tsx)$' --timings -c ${TEST_CONCURRENCY}
-    secrets: inherit
-
   test-turbopack-dev:
     name: test turbopack dev
     needs: ['build-native', 'build-next']
     uses: ./.github/workflows/build_reusable.yml
     with:
       skipForDocsOnly: 'yes'
-      afterBuild: RUST_BACKTRACE=0 NEXT_EXTERNAL_TESTS_FILTERS="$(pwd)/packages/next-swc/crates/next-dev-tests/tests-manifest.js" TURBOPACK=1 __INTERNAL_CUSTOM_TURBOPACK_BINDINGS="$(pwd)/packages/next-swc/native/next-swc.linux-x64-gnu.node" NEXT_E2E_TEST_TIMEOUT=240000 NEXT_TEST_MODE=dev node run-tests.js --test-pattern '^(test\/development)/.*\.test\.(js|jsx|ts|tsx)$' --timings -c ${TEST_CONCURRENCY}
+      afterBuild: RUST_BACKTRACE=0 NEXT_EXTERNAL_TESTS_FILTERS="$(pwd)/test/turbopack-tests-manifest.js" TURBOPACK=1 NEXT_E2E_TEST_TIMEOUT=240000 NEXT_TEST_MODE=dev node run-tests.js --test-pattern '^(test\/development)/.*\.test\.(js|jsx|ts|tsx)$' --timings -c ${TEST_CONCURRENCY}
     secrets: inherit
 
-  test-experimental-turbopack-integration:
-    name: test experimental turbopack integration
+  test-turbopack-integration:
+    name: test turbopack integration
     needs: ['build-native', 'build-next']
     strategy:
       fail-fast: false
@@ -154,23 +145,9 @@ jobs:
     with:
       nodeVersion: 16
       skipForDocsOnly: 'yes'
-      afterBuild: RUST_BACKTRACE=0 NEXT_EXTERNAL_TESTS_FILTERS="$(pwd)/test/turbopack-tests-manifest.js" EXPERIMENTAL_TURBOPACK=1 node run-tests.js --timings -g ${{ matrix.group }}/1 -c ${TEST_CONCURRENCY} --type integration
+      afterBuild: RUST_BACKTRACE=0 NEXT_EXTERNAL_TESTS_FILTERS="$(pwd)/test/turbopack-tests-manifest.js" TURBOPACK=1 node run-tests.js --timings -g ${{ matrix.group }}/1 -c ${TEST_CONCURRENCY} --type integration
     secrets: inherit
 
-  test-turbopack-integration:
-    name: test turbopack integration
-    needs: ['build-native', 'build-next']
-    strategy:
-      fail-fast: false
-      matrix:
-        group: [1, 2, 3, 4, 5]
-
-    uses: ./.github/workflows/build_reusable.yml
-    with:
-      nodeVersion: 16
-      skipForDocsOnly: 'yes'
-      afterBuild: RUST_BACKTRACE=0 NEXT_EXTERNAL_TESTS_FILTERS="$(pwd)/packages/next-swc/crates/next-dev-tests/tests-manifest.js" TURBOPACK=1 __INTERNAL_CUSTOM_TURBOPACK_BINDINGS="$(pwd)/packages/next-swc/native/next-swc.linux-x64-gnu.node" node run-tests.js --timings -g ${{ matrix.group }}/5 -c ${TEST_CONCURRENCY} --type integration
-    secrets: inherit
   test-next-swc-wasm:
     name: test next-swc wasm
     needs: ['build-native', 'build-next']
@@ -235,14 +212,7 @@ jobs:
     secrets: inherit
 
   report-test-results:
-    needs:
-      [
-        'test-dev',
-        'test-prod',
-        'test-integration',
-        'test-turbopack-dev',
-        'test-turbopack-integration',
-      ]
+    needs: ['test-dev', 'test-prod', 'test-integration']
     uses: ./.github/workflows/build_reusable.yml
     with:
       skipForDocsOnly: 'yes'
@@ -266,9 +236,7 @@ jobs:
         'rust-check',
         'test-next-swc-wasm',
         'test-turbopack-dev',
-        'test-experimental-turbopack-dev',
         'test-turbopack-integration',
-        'test-experimental-turbopack-integration',
       ]
 
     if: always()

jest.config.js
@@ -37,10 +37,9 @@ if (shouldEnableTestTrace) {
     customJestConfig.reporters = ['default']
   }
 
-  const outputDirectory =
-    process.env.TURBOPACK || process.env.EXPERIMENTAL_TURBOPACK
-      ? '<rootDir>/turbopack-test-junit-report'
-      : '<rootDir>/test-junit-report'
+  const outputDirectory = process.env.TURBOPACK
+    ? '<rootDir>/turbopack-test-junit-report'
+    : '<rootDir>/test-junit-report'
 
   customJestConfig.reporters.push([
     'jest-junit',

packages/next-swc/crates/napi/src/turbopack.rs
@@ -9,15 +9,8 @@ use next_build::{
     build as turbo_next_build, build_options::BuildContext, BuildOptions as NextBuildOptions,
 };
 use next_core::next_config::{Rewrite, Rewrites, RouteHas};
-use next_dev::{devserver_options::DevServerOptions, start_server};
 
 use crate::util::MapErr;
-
-#[napi]
-pub async fn start_turbo_dev(options: Buffer) -> napi::Result<()> {
-    let options: DevServerOptions = serde_json::from_slice(&options)?;
-    start_server(&options).await.convert_err()
-}
 
 #[napi(object, object_to_js = false)]
 #[derive(Debug)]

packages/next-swc/crates/next-dev-tests/print-enabled-tests.js
@@ -1,30 +1,0 @@
-/// A script that prints a list of test cases that are enabled
-
-const _glob = require('glob')
-const { promisify } = require('util')
-const glob = promisify(_glob)
-const path = require('path')
-
-const { disabledTests } = require('./tests-manifest.js')
-
-const main = async () => {
-  // Collect all test files
-  const testFiles = new Set(
-    (
-      await glob('**/*.test.{js,ts,tsx}', {
-        nodir: true,
-        cwd: path.resolve(__dirname, '../../../../test'),
-      })
-    ).map((file) => `test/${file}`)
-  )
-
-  for (const testFile of disabledTests) {
-    testFiles.delete(testFile)
-  }
-
-  for (const testFile of testFiles) {
-    console.log(testFile)
-  }
-}
-
-main().catch((e) => console.error(e))

packages/next-swc/crates/next-dev-tests/tests-manifest.js
@@ -1,214 +1,0 @@
-// Tests that are currently enabled with Turbopack in CI.
-// Only tests that are actively testing against Turbopack should
-// be enabled here
-const enabledTests = [
-  'test/development/acceptance-app/ReactRefresh.test.ts',
-  'test/development/acceptance-app/ReactRefreshLogBoxMisc.test.ts',
-  'test/development/acceptance-app/ReactRefreshRequire.test.ts',
-  // 'test/development/acceptance-app/app-hmr-changes.test.ts', (FLAKY)
-  'test/development/acceptance-app/dynamic-error.test.ts',
-  'test/development/acceptance-app/version-staleness.test.ts',
-  'test/development/acceptance/ReactRefreshLogBox-scss.test.ts',
-  'test/development/acceptance/ReactRefreshLogBoxMisc.test.ts',
-  'test/development/api-cors-with-rewrite/index.test.ts',
-  'test/development/app-dir/multiple-compiles-single-route/multiple-compiles-single-route.test.ts',
-  // x-ref: below test is flakey and needs to be investigated further
-  // 'test/development/app-hmr/hmr.test.ts',
-  'test/development/basic/define-class-fields.test.ts',
-  'test/development/basic/emotion-swc.test.ts',
-  'test/development/basic/legacy-decorators.test.ts',
-  'test/development/basic/project-directory-rename.test.ts',
-  'test/development/basic/tailwind-jit.test.ts',
-  'test/development/basic/theme-ui.test.ts',
-  'test/development/dotenv-default-expansion/index.test.ts',
-  'test/development/jsconfig-path-reloading/index.test.ts',
-  'test/development/middleware-warnings/index.test.ts',
-  'test/development/project-directory-with-styled-jsx-suffix/index.test.ts',
-  'test/development/repeated-dev-edits/repeated-dev-edits.test.ts',
-  'test/development/tsconfig-path-reloading/index.test.ts',
-  'test/e2e/app-dir/_allow-underscored-root-directory/_allow-underscored-root-directory.test.ts',
-  'test/e2e/app-dir/actions/app-action-invalid.test.ts',
-  'test/e2e/app-dir/actions/app-action-size-limit-invalid.test.ts',
-  'test/e2e/app-dir/app-alias/app-alias.test.ts',
-  'test/e2e/app-dir/app-client-cache/client-cache.test.ts',
-  'test/e2e/app-dir/app-css-pageextensions/index.test.ts',
-  'test/e2e/app-dir/app-edge-root-layout/index.test.ts',
-  'test/e2e/app-dir/app-edge/app-edge.test.ts',
-  'test/e2e/app-dir/app-prefetch-false/app-prefetch-false.test.ts',
-  'test/e2e/app-dir/app-prefetch/prefetching.test.ts',
-  'test/e2e/app-dir/app-validation/validation.test.ts',
-  'test/e2e/app-dir/asset-prefix/asset-prefix.test.ts',
-  'test/e2e/app-dir/async-component-preload/async-component-preload.test.ts',
-  'test/e2e/app-dir/autoscroll-with-css-modules/index.test.ts',
-  'test/e2e/app-dir/back-button-download-bug/back-button-download-bug.test.ts',
-  'test/e2e/app-dir/build-size/index.test.ts',
-  'test/e2e/app-dir/global-error/global-error.test.ts',
-  'test/e2e/app-dir/hello-world/hello-world.test.ts',
-  'test/e2e/app-dir/import/import.test.ts',
-  'test/e2e/app-dir/layout-params/layout-params.test.ts',
-  'test/e2e/app-dir/metadata-suspense/index.test.ts',
-  'test/e2e/app-dir/rewrites-redirects/rewrites-redirects.test.ts',
-  'test/e2e/app-dir/route-page-manifest-bug/route-page-manifest-bug.test.ts',
-  'test/e2e/app-dir/router-autoscroll/router-autoscroll.test.ts',
-  'test/e2e/app-dir/router-stuck-dynamic-static-segment/router-stuck-dynamic-static-segment.test.ts',
-  'test/e2e/app-dir/rsc-basic/rsc-basic.test.ts',
-  'test/e2e/app-dir/search-params-react-key/layout-params.test.ts',
-  'test/e2e/app-dir/searchparams-static-bailout/searchparams-static-bailout.test.ts',
-  'test/e2e/app-dir/test-template/{{ toFileName name }}/{{ toFileName name }}.test.ts',
-  'test/e2e/app-dir/use-selected-layout-segment-s/use-selected-layout-segment-s.test.ts',
-  'test/e2e/app-dir/crypto-globally-available/crypto-globally-available.test.ts',
-  'test/e2e/browserslist-extends/index.test.ts',
-  'test/e2e/config-promise-export/async-function.test.ts',
-  'test/e2e/config-promise-export/promise.test.ts',
-  'test/e2e/conflicting-app-page-error/index.test.ts',
-  'test/e2e/disable-js-preload/test/index.test.js',
-  'test/e2e/hello-world/hello-world.test.ts',
-  'test/e2e/i18n-api-support/index.test.ts',
-  'test/e2e/i18n-disallow-multiple-locales/i18n-disallow-multiple-locales.test.ts',
-  'test/e2e/i18n-ignore-rewrite-source-locale/rewrites-with-basepath.test.ts',
-  'test/e2e/i18n-ignore-rewrite-source-locale/rewrites.test.ts',
-  'test/e2e/link-with-api-rewrite/index.test.ts',
-  'test/e2e/middleware-fetches-with-body/index.test.ts',
-  'test/e2e/middleware-shallow-link/index.test.ts',
-  'test/e2e/new-link-behavior/child-a-tag-error.test.ts',
-  'test/e2e/new-link-behavior/index.test.ts',
-  'test/e2e/new-link-behavior/material-ui.test.ts',
-  'test/e2e/new-link-behavior/stitches.test.ts',
-  'test/e2e/new-link-behavior/typescript.test.ts',
-  'test/e2e/next-head/index.test.ts',
-  'test/e2e/next-image-forward-ref/index.test.ts',
-  'test/e2e/no-eslint-warn-with-no-eslint-config/index.test.ts',
-  'test/e2e/nonce-head-manager/index.test.ts',
-  'test/e2e/postcss-config-cjs/index.test.ts',
-  'test/e2e/proxy-request-with-middleware/test/index.test.ts',
-  'test/e2e/repeated-forward-slashes-error/repeated-forward-slashes-error.test.ts',
-  'test/e2e/styled-jsx/index.test.ts',
-  'test/e2e/test-template/{{ toFileName name }}/{{ toFileName name }}.test.ts',
-  'test/e2e/test-utils-tests/basic/basic.test.ts',
-  'test/e2e/trailingslash-with-rewrite/index.test.ts',
-  'test/e2e/transpile-packages/index.test.ts',
-  'test/e2e/type-module-interop/index.test.ts',
-  'test/e2e/typescript-version-no-warning/typescript-version-no-warning.test.ts',
-  'test/e2e/typescript-version-warning/typescript-version-warning.test.ts',
-  'test/e2e/undici-fetch/index.test.ts',
-  'test/e2e/yarn-pnp/test/with-eslint.test.ts',
-  'test/e2e/yarn-pnp/test/with-next-sass.test.ts',
-  'test/integration/404-page-custom-error/test/index.test.js',
-  'test/integration/amp-export-validation/test/index.test.js',
-  'test/integration/amphtml-custom-validator/test/index.test.js',
-  'test/integration/amphtml-fragment-style/test/index.test.js',
-  'test/integration/api-body-parser/test/index.test.js',
-  'test/integration/api-catch-all/test/index.test.js',
-  'test/integration/app-aspath/test/index.test.js',
-  'test/integration/app-config-asset-prefix/test/index.test.js',
-  'test/integration/app-functional/test/index.test.js',
-  'test/integration/auto-export-error-bail/test/index.test.js',
-  'test/integration/auto-export-query-error/test/index.test.js',
-  'test/integration/bigint/test/index.test.js',
-  'test/integration/catches-missing-getStaticProps/test/index.test.js',
-  'test/integration/clean-distdir/test/index.test.js',
-  'test/integration/client-navigation-a11y/test/index.test.js',
-  // TODO: re-enable once the logging is aligned
-  // 'test/integration/config-experimental-warning/test/index.test.js',
-  'test/integration/config-schema-check/test/index.test.js',
-  'test/integration/config-syntax-error/test/index.test.js',
-  'test/integration/config-validation/test/index.test.ts',
-  'test/integration/conflicting-ssg-paths/test/index.test.js',
-  'test/integration/create-next-app/index.test.ts',
-  'test/integration/create-next-app/package-manager.test.ts',
-  'test/integration/create-next-app/templates-app.test.ts',
-  'test/integration/create-next-app/templates-pages.test.ts',
-  'test/integration/css/test/dev-css-handling.test.js',
-  'test/integration/custom-error-page-exception/test/index.test.js',
-  'test/integration/custom-server-types/test/index.test.js',
-  'test/integration/custom-server/test/index.test.js',
-  'test/integration/dedupes-scripts/test/index.test.js',
-  'test/integration/development-hmr-refresh/test/index.test.js',
-  'test/integration/disable-js/test/index.test.js',
-  'test/integration/document-head-warnings/test/index.test.js',
-  'test/integration/duplicate-pages/test/index.test.js',
-  'test/integration/dynamic-require/test/index.test.js',
-  'test/integration/dynamic-route-rename/test/index.test.js',
-  'test/integration/empty-object-getInitialProps/test/index.test.js',
-  'test/integration/errors-on-output-to-public/test/index.test.js',
-  'test/integration/errors-on-output-to-static/test/index.test.js',
-  'test/integration/eslint/test/lint-cache.test.js',
-  'test/integration/eslint/test/next-lint.test.js',
-  'test/integration/export-404/test/index.test.js',
-  'test/integration/export-default-map/test/index.test.js',
-  'test/integration/export-dynamic-pages/test/index.test.js',
-  'test/integration/export-fallback-true-error/test/index.test.js',
-  'test/integration/export-getInitialProps-warn/test/index.test.js',
-  'test/integration/export-image-default/test/index.test.js',
-  'test/integration/export-image-loader-legacy/test/index.test.js',
-  'test/integration/export-index-not-found-gsp/test/index.test.ts',
-  'test/integration/export-intent/test/index.test.js',
-  'test/integration/export-no-build/test/index.test.js',
-  'test/integration/export-progress-status-message/test/index.test.js',
-  'test/integration/export-subfolders/test/index.test.js',
-  'test/integration/fallback-modules/test/index.test.js',
-  'test/integration/filesystempublicroutes/test/index.test.js',
-  'test/integration/firebase-grpc/test/index.test.js',
-  'test/integration/future/test/index.test.js',
-  'test/integration/gsp-extension/test/index.test.js',
-  'test/integration/handles-export-errors/test/index.test.js',
-  'test/integration/hashbang/test/index.test.js',
-  'test/integration/index-index/test/index.test.js',
-  'test/integration/initial-ref/test/index.test.js',
-  'test/integration/invalid-config-values/test/index.test.js',
-  'test/integration/invalid-page-automatic-static-optimization/test/index.test.js',
-  'test/integration/invalid-revalidate-values/test/index.test.js',
-  'test/integration/invalid-server-options/test/index.test.js',
-  'test/integration/json-serialize-original-error/test/index.test.js',
-  'test/integration/legacy-ssg-methods-error/test/index.test.js',
-  'test/integration/link-ref/test/index.test.js',
-  'test/integration/link-with-multiple-child/test/index.test.js',
-  'test/integration/link-without-router/test/index.test.js',
-  'test/integration/middleware-build-errors/test/index.test.js',
-  'test/integration/middleware-overrides-node.js-api/test/index.test.ts',
-  'test/integration/missing-document-component-error/test/index.test.js',
-  'test/integration/next-image-legacy/custom-resolver/test/index.test.ts',
-  'test/integration/next-image-legacy/no-intersection-observer-fallback/test/index.test.ts',
-  'test/integration/next-image-legacy/noscript/test/index.test.ts',
-  'test/integration/next-image-legacy/react-virtualized/test/index.test.ts',
-  'test/integration/next-image-legacy/unoptimized/test/index.test.ts',
-  'test/integration/next-image-new/react-virtualized/test/index.test.ts',
-  // 'test/integration/next-image-new/unoptimized/test/index.test.ts', (FLAKY)
-  'test/integration/no-op-export/test/index.test.js',
-  'test/integration/non-next-dist-exclude/test/index.test.js',
-  'test/integration/non-standard-node-env-warning/test/index.test.js',
-  'test/integration/ondemand/test/index.test.js',
-  'test/integration/optional-chaining-nullish-coalescing/test/index.test.js',
-  'test/integration/plugin-mdx-rs/test/index.test.js',
-  'test/integration/prerender-invalid-catchall-params/test/index.test.js',
-  'test/integration/prerender-invalid-paths/test/index.test.js',
-  'test/integration/prerender-legacy/test/index.test.js',
-  'test/integration/production-build-dir/test/index.test.js',
-  'test/integration/production-start-no-build/test/index.test.js',
-  'test/integration/router-hash-navigation/test/index.test.js',
-  'test/integration/router-prefetch/test/index.test.js',
-  'test/integration/router-rerender/test/index.test.js',
-  'test/integration/scss/test/dev-css-handling.test.js',
-  'test/integration/src-dir-support-double-dir/test/index.test.js',
-  'test/integration/ssg-dynamic-routes-404-page/test/index.test.js',
-  'test/integration/static-404/test/index.test.js',
-  'test/integration/static-page-name/test/index.test.js',
-  'test/integration/trailing-slashes-href-resolving/test/index.test.js',
-  'test/integration/tsconfig-verifier/test/index.test.js',
-  'test/integration/typescript-baseurl/test/index.test.js',
-  'test/integration/typescript-external-dir/project/test/index.test.js',
-  'test/integration/typescript-filtered-files/test/index.test.js',
-  // 'test/integration/typescript-hmr/test/index.test.js', (FLAKY)
-  'test/integration/webpack-config-mainjs/test/index.test.js',
-  'test/integration/with-electron/test/index.test.js',
-  'test/production/ci-missing-typescript-deps/index.test.ts',
-  'test/production/enoent-during-require/index.test.ts',
-  'test/production/eslint-plugin-deps/index.test.ts',
-  'test/production/fallback-export-error/index.test.ts',
-  'test/production/jest/new-link-behavior.test.ts',
-  'test/production/jest/transpile-packages.test.ts',
-  'test/production/postcss-plugin-config-as-string/index.test.ts',
-  'test/production/supports-module-resolution-nodenext/supports-moduleresolution-nodenext.test.ts',
-]
-
-module.exports = { enabledTests }

commit 50a1ebc2c2e6dec2e95403d4a99ccb7c3e25d160

Correctly mark turbo sessions in telemetry
This regressed in #55063

packages/next/src/cli/next-dev.ts
@@ -231,6 +231,7 @@ const nextDev: CliCommand = async (args) => {
   }
 
   if (process.env.TURBOPACK) {
+    isTurboSession = true
     await validateTurboNextConfig({
       ...devServerOptions,
       isDev: true,