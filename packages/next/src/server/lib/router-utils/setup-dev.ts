24452b6f465e822a64d3e8404d660ef01669d63b
Tobias Koppers <tobias.koppers@googlemail.com>
2023-09-26 15:04:33 +0000

    improve issue reporting fix race condition

--- packages/next/src/server/lib/router-utils/setup-dev.ts
+++ packages/next/src/server/lib/router-utils/setup-dev.ts
@@ -219,7 +219,10 @@ async function startWatcher(opts: SetupOpts) {
     })
     const iter = project.entrypointsSubscribe()
     const curEntries: Map<string, Route> = new Map()
-    const changeSubscriptions: Map<string, AsyncIterator<any>> = new Map()
+    const changeSubscriptions: Map<
+      string,
+      Promise<AsyncIterator<any>>
+    > = new Map()
     let prevMiddleware: boolean | undefined = undefined
     const globalEntries: {
       app: Endpoint | undefined
@@ -298,24 +301,28 @@ async function startWatcher(opts: SetupOpts) {
       const newSet = new Map<string, Issue>()
       issues.set(name, newSet)
 
+      const relevantIssues = new Set()
+
       for (const issue of result.issues) {
         // TODO better formatting
         if (issue.severity !== 'error' && issue.severity !== 'fatal') continue
         const key = issueKey(issue)
         const formatted = formatIssue(issue)
-        if (!throwIssue && !oldSet.has(key)) {
-          console.error(`  ⚠ ${key} ${formatted}\n\n`)
+        if (!oldSet.has(key) && !newSet.has(key)) {
+          console.error(`  ⚠ ${name} ${key} ${formatted}\n\n`)
         }
         newSet.set(key, issue)
-        if (throwIssue) {
-          throw new ModuleBuildError(formatted)
-        }
+        relevantIssues.add(formatted)
       }
 
       for (const issue of oldSet.keys()) {
         if (!newSet.has(issue)) {
           console.error(`✅ ${name} fixed ${issue}`)
         }
+      }
+
+      if (relevantIssues.size && throwIssue) {
+        throw new ModuleBuildError([...relevantIssues].join('\n\n'))
       }
     }
 
@@ -503,8 +510,9 @@ async function startWatcher(opts: SetupOpts) {
     ) {
       if (!endpoint || changeSubscriptions.has(page)) return
 
-      const changed = await endpoint.changed()
-      changeSubscriptions.set(page, changed)
+      const changedPromise = endpoint.changed()
+      changeSubscriptions.set(page, changedPromise)
+      const changed = await changedPromise
 
       for await (const change of changed) {
         consoleStore.setState(
@@ -521,8 +529,8 @@ async function startWatcher(opts: SetupOpts) {
       }
     }
 
-    function clearChangeSubscription(page: string) {
-      const subscription = changeSubscriptions.get(page)
+    async function clearChangeSubscription(page: string) {
+      const subscription = await changeSubscriptions.get(page)
       if (subscription) {
         subscription.return?.()
         changeSubscriptions.delete(page)
@@ -560,13 +568,14 @@ async function startWatcher(opts: SetupOpts) {
             }
           }
 
-          for (const [pathname, subscription] of changeSubscriptions) {
+          for (const [pathname, subscriptionPromise] of changeSubscriptions) {
             if (pathname === '') {
               // middleware is handled below
               continue
             }
 
             if (!curEntries.has(pathname)) {
+              const subscription = await subscriptionPromise
               subscription.return?.()
               changeSubscriptions.delete(pathname)
             }
@@ -578,7 +587,7 @@ async function startWatcher(opts: SetupOpts) {
           // unnecessary during the first serve)
           if (prevMiddleware === true && !middleware) {
             // Went from middleware to no middleware
-            clearChangeSubscription('middleware')
+            await clearChangeSubscription('middleware')
             sendHmr('entrypoint-change', 'middleware', {
               event: HMR_ACTIONS_SENT_TO_BROWSER.MIDDLEWARE_CHANGES,
             })
@@ -881,7 +890,7 @@ async function startWatcher(opts: SetupOpts) {
       }
 
       for await (const data of subscription) {
-        processIssues(id, data)
+        processIssues('hmr', data)
         sendTurbopackMessage(data)
       }
     }
