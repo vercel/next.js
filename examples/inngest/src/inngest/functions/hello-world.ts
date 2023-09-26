5ddc3d49491d0c0fd2c3f7c4f4b69f667b5c0e55
joel <joelhooks@gmail.com>
2023-09-26 16:38:07 -0700

    add id to helloWorld function

--- examples/inngest/src/inngest/functions/hello-world.ts
+++ examples/inngest/src/inngest/functions/hello-world.ts
@@ -1,7 +1,7 @@
 import { inngest } from '../inngest.client'
 
 export const helloWorld = inngest.createFunction(
-  { name: 'Hello World' },
+  { id: 'hello-world', name: 'Hello World' },
   { event: 'test/hello.world' },
   async ({ event, step }) => {
     await step.sleep('1s')
