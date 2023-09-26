5e4244819d6817233319d5e1526169f0b8fe9247
Shu Ding <g@shud.in>
2023-09-26 17:09:52 +0200

    update test

--- test/e2e/app-dir/actions/app-action-form-state.test.ts
+++ test/e2e/app-dir/actions/app-action-form-state.test.ts
@@ -31,20 +31,10 @@ createNextDescribe(
       await browser.eval(`document.getElementById('name-input').value = 'test'`)
       await browser.elementByCss('#submit-form').click()
 
+      // It should inline the form state into HTML so it can still be hydrated.
       await check(() => {
         return browser.elementByCss('#form-state').text()
       }, 'initial-state:test')
-
-      // It should inline the form state into HTML so it can still be hydrated.
-      // Is there a better way to test this? It seems like we can't toggle the
-      // disableJavaScript flag after the browser has been created.
-      await check(async () => {
-        return (await browser.eval('document.body.innerHTML')).includes(
-          'self.__next_f.push([2,["initial-state:test"'
-        )
-          ? 'true'
-          : 'false'
-      }, 'true')
     })
 
     it('should support hydrating the app from progressively enhanced form request', async () => {
