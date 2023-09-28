06705a4815e117b6b9be94588a2fcf894866b25a
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-09-28 08:36:17 +0800

    Chore/update with supabase demo deploy button (#52483)
    ### What?
    
    - added correct deploy button urls
      - uses new supabase integration
    - updated readme for local development, more clearer instructions
    - added opengraph image
    
    ### Why?
    
    - old supabase integration is buggy
    - readme is more clear for local development
    
    ---------
    
    Co-authored-by: Steven Tey <stevensteel97@gmail.com>
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- examples/with-supabase/app/page.tsx
+++ examples/with-supabase/app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import LogoutButton from '../components/LogoutButton'
 import SupabaseLogo from '../components/SupabaseLogo'
 import NextJsLogo from '../components/NextJsLogo'
+import DeployButton from '../components/DeployButton'
 
 export const dynamic = 'force-dynamic'
 
@@ -49,22 +50,20 @@ export default async function Index() {
     <div className="w-full flex flex-col items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
-          <div />
-          <div>
-            {user ? (
-              <div className="flex items-center gap-4">
-                Hey, {user.email}!
-                <LogoutButton />
-              </div>
-            ) : (
-              <Link
-                href="/login"
-                className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-              >
-                Login
-              </Link>
-            )}
-          </div>
+          <DeployButton />
+          {user ? (
+            <div className="flex items-center gap-4">
+              Hey, {user.email}!
+              <LogoutButton />
+            </div>
+          ) : (
+            <Link
+              href="/login"
+              className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+            >
+              Login
+            </Link>
+          )}
         </div>
       </nav>
 
