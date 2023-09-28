b03d6ff7c31a81101a6289d092e07a186f2e4f58
Tobias Koppers <tobias.koppers@googlemail.com>
2023-09-28 06:30:30 +0000

    fixup for turbo api change

--- packages/next-swc/crates/next-core/src/next_server/context.rs
+++ packages/next-swc/crates/next-core/src/next_server/context.rs
@@ -611,6 +611,7 @@ pub fn get_server_chunking_context(
     BuildChunkingContext::builder(
         project_path,
         node_root,
+        client_root,
         node_root.join("server/chunks".to_string()),
         client_root.join("_next/static/media".to_string()),
         environment,
