e78eee25c518dcad08500e17555146fe154dba09
JJ Kasper <jj@jjsweb.site>
2023-09-29 15:11:45 -0700

    Add additional handling for experimental-compile (#56224)
    This ensures we properly set the `isReady` flag when building with `experimental-compile` and enables our main app dir test suite to ensure we don't regress on it.

--- test/e2e/app-dir/app/index.test.ts
+++ test/e2e/app-dir/app/index.test.ts
@@ -7,14 +7,19 @@ createNextDescribe(
   'app dir',
   {
     files: __dirname,
+    buildCommand: process.env.NEXT_EXPERIMENTAL_COMPILE
+      ? 'pnpm next experimental-compile'
+      : undefined,
   },
   ({ next, isNextDev: isDev, isNextStart, isNextDeploy }) => {
     if (isNextStart) {
-      it('should have correct size in build output', async () => {
-        expect(next.cliOutput).toMatch(
-          /\/dashboard\/another.*? [^0]{1,} [\w]{1,}B/
-        )
-      })
+      if (!process.env.NEXT_EXPERIMENTAL_COMPILE) {
+        it('should have correct size in build output', async () => {
+          expect(next.cliOutput).toMatch(
+            /\/dashboard\/another.*? [^0]{1,} [\w]{1,}B/
+          )
+        })
+      }
 
       it('should have correct preferredRegion values in manifest', async () => {
         const middlewareManifest = JSON.parse(
