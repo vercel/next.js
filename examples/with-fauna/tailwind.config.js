7b425d5aa86daa6ab17cb0e6e223d1c5c835ba1b
Shadid2 <shadid@salussafety.io>
2023-10-02 17:04:07 -0400

    refactor tailwind

--- examples/with-fauna/tailwind.config.js
+++ examples/with-fauna/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
