2732ea5f805e65205338f5b3fece36cb888d340d
SukkaW <isukkaw@gmail.com>
2023-10-05 02:15:39 +0800

    test: update config validation case

--- test/integration/config-validation/test/index.test.ts
+++ test/integration/config-validation/test/index.test.ts
@@ -19,9 +19,10 @@ describe('next.config.js validation', () => {
         }
       `,
         outputs: [
-          'The value at .images.loader must be one of',
-          'The value at .rewrites must be a function that returns a Promise',
-          'The value at .swcMinify must be a boolean but it was a string',
+          'Invalid enum value. Expected',
+          `received 'something' at "images.loader"`,
+          'Expected function, received boolean at "rewrites"',
+          'Expected boolean, received string at "swcMinify"',
         ],
       },
       {
@@ -35,8 +36,8 @@ describe('next.config.js validation', () => {
         }
       `,
         outputs: [
-          'The root value has an unexpected property, nonExistent,',
-          'The value at .experimental has an unexpected property, anotherNonExistent',
+          `Unrecognized key(s) in object: 'nonExistent'`,
+          `Unrecognized key(s) in object: 'anotherNonExistent' at "experimental"`,
         ],
       },
     ])(
