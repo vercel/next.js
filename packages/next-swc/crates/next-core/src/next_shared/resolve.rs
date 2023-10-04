1d04870b706beedc7b0f0fd1983703c41f84f824
Tobias Koppers <tobias.koppers@googlemail.com>
2023-10-04 11:36:14 +0000

    Revert "make glob more specific"
    This reverts commit 58830ac3dce34cc4c5a84ed617ead6a9a2297124.

--- packages/next-swc/crates/next-core/src/next_shared/resolve.rs
+++ packages/next-swc/crates/next-core/src/next_shared/resolve.rs
@@ -122,9 +122,7 @@ impl ResolvePlugin for NextExternalResolvePlugin {
     fn after_resolve_condition(&self) -> Vc<ResolvePluginCondition> {
         ResolvePluginCondition::new(
             self.root.root(),
-            Glob::new(
-                "**/node_modules/next/dist/**/*.{external,runtime.dev,runtime.prod}.js".to_string(),
-            ),
+            Glob::new("**/next/dist/**/*.{external,runtime.dev,runtime.prod}.js".to_string()),
         )
     }
 
