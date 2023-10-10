093af1850a17b480fc34ebe4bd3e65c47329fc50
Will Binns-Smith <wbinnssmith@gmail.com>
2023-10-10 10:00:14 -0700

    Turbopack: detect `spawn(process.argv[0], ['-e', ...])` (vercel/turbo#6118)
    This detects a specific pattern for invoking a node process to evaluate
    a string of JavaScript code. Since a filepath can't (or shouldn't) be
    passed here, this exempts it from static analysis.
    
    This pattern is used by the package `xmlhttprequest-ssl`, which is
    required transitively by socket.io-client:
    https://github.com/mjwwit/node-XMLHttpRequest/blob/b0271d5e52692d9f48da6088b27d5bf2a6f50d86/lib/XMLHttpRequest.js#L544
    
    Fixes WEB-1554
    Resolves vercel/next.js#54787
    
    Test Plan:
    - Added snapshot test
    - Manual test with a project using socket.io-client

---
+++ crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_e27a00.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_e27a00.js",
+    {},
+    {"otherChunks":[{"path":"output/crates_turbopack-tests_tests_snapshot_node_spawn_node_eval_input_index_b53fce.js","included":["[project]/crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/input/index.js (ecmascript)"]},{"path":"output/134fc_child_process_index_64e768.js","included":["[project]/crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/input/node_modules/child_process/index.js (ecmascript)"]}],"runtimeModuleIds":["[project]/crates/turbopack-tests/tests/snapshot/node/spawn_node_eval/input/index.js (ecmascript)"]}
+]);
+// Dummy runtime
