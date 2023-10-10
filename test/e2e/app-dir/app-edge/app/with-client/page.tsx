vercel/next.js

commit db4b9858cac1bed5a5043b68207c808b2286b58a

Add esm entry for next/server and alias @vercel/og (#51651)
The next-metadata-route-loader emitted content with `"next/server"` is still using cjs version which is not get tree-shaked on edge runtime, This PR adds a new esm entry for it and mapped it that on edge runtime so unused exports can be tree-shaked. This case happened when users are using ImageResponse from `"@vercel/og"` instead of `"next/server"`.

2nd change is adding another alias to map `@vercel/og` package to vendored og package inside next.js, this way we could merge the two package instead of bundled both of them


Fixes: NEXT-1303

changed `require.reoslve("next/dist/xxx")` to `${NEXT_PROJECT_ROOT}/xxx}` to avoid to be traced by nft.

packages/next/src/build/webpack-config.ts
@@ -1077,6 +1077,9 @@ export default async function getBaseWebpackConfig(
       ...nodePathList, // Support for NODE_PATH environment variable
     ],
     alias: {
+      // Alias 3rd party @vercel/og package to vendored og image package to reduce bundle size
+      '@vercel/og': 'next/dist/server/web/spec-extension/image-response',
+
       // Alias next/dist imports to next/dist/esm assets,
       // let this alias hit before `next` alias.
       ...(isEdgeServer
@@ -1088,25 +1091,27 @@ export default async function getBaseWebpackConfig(
             'next/dist/server': 'next/dist/esm/server',
 
             // Alias the usage of next public APIs
-            [require.resolve('next/dist/client/link')]:
+            [`${NEXT_PROJECT_ROOT}/server`]:
+              'next/dist/esm/server/web/exports/index',
+            [`${NEXT_PROJECT_ROOT}/dist/client/link`]:
               'next/dist/esm/client/link',
-            [require.resolve('next/dist/client/image')]:
+            [`${NEXT_PROJECT_ROOT}/dist/client/image`]:
               'next/dist/esm/client/image',
-            [require.resolve('next/dist/client/script')]:
+            [`${NEXT_PROJECT_ROOT}/dist/client/script`]:
               'next/dist/esm/client/script',
-            [require.resolve('next/dist/client/router')]:
+            [`${NEXT_PROJECT_ROOT}/dist/client/router`]:
               'next/dist/esm/client/router',
-            [require.resolve('next/dist/shared/lib/head')]:
+            [`${NEXT_PROJECT_ROOT}/dist/shared/lib/head`]:
               'next/dist/esm/shared/lib/head',
-            [require.resolve('next/dist/shared/lib/dynamic')]:
+            [`${NEXT_PROJECT_ROOT}/dist/shared/lib/dynamic`]:
               'next/dist/esm/shared/lib/dynamic',
-            [require.resolve('next/dist/pages/_document')]:
+            [`${NEXT_PROJECT_ROOT}/dist/pages/_document`]:
               'next/dist/esm/pages/_document',
-            [require.resolve('next/dist/pages/_app')]:
+            [`${NEXT_PROJECT_ROOT}/dist/pages/_app`]:
               'next/dist/esm/pages/_app',
-            [require.resolve('next/dist/client/components/navigation')]:
+            [`${NEXT_PROJECT_ROOT}/dist/client/components/navigation`]:
               'next/dist/esm/client/components/navigation',
-            [require.resolve('next/dist/client/components/headers')]:
+            [`${NEXT_PROJECT_ROOT}/dist/client/components/headers`]:
               'next/dist/esm/client/components/headers',
           }
         : undefined),
@@ -1433,8 +1438,8 @@ export default async function getBaseWebpackConfig(
       resolveResult.res = require.resolve(request)
     }
 
-    // Don't bundle @vercel/og nodejs bundle for nodejs runtime
-    // TODO-APP: bundle route.js with different layer that externals common node_module deps
+    // Don't bundle @vercel/og nodejs bundle for nodejs runtime.
+    // TODO-APP: bundle route.js with different layer that externals common node_module deps.
     if (
       layer === WEBPACK_LAYERS.server &&
       request === 'next/dist/compiled/@vercel/og/index.node.js'

packages/next/src/build/webpack/plugins/middleware-plugin.ts
@@ -570,7 +570,7 @@ function getExtractMetadata(params: {
           const resource = module.resource
           const hasOGImageGeneration =
             resource &&
-            /[\\/]node_modules[\\/]@vercel[\\/]og[\\/]dist[\\/]index\.(edge|node)\.js$|[\\/]next[\\/]dist[\\/]server[\\/]web[\\/]spec-extension[\\/]image-response\.js$/.test(
+            /[\\/]node_modules[\\/]@vercel[\\/]og[\\/]dist[\\/]index\.(edge|node)\.js$|[\\/]next[\\/]dist[\\/](esm[\\/])?server[\\/]web[\\/]spec-extension[\\/]image-response\.js$/.test(
               resource
             )
 

packages/next/src/server/web/exports/index.ts
@@ -1,0 +1,8 @@
+// Alias index file of next/server for edge runtime for tree-shaking purpose
+
+export { default as ImageResponse } from './image-response'
+export { default as NextRequest } from './next-request'
+export { default as NextResponse } from './next-response'
+export { default as userAgent } from './user-agent'
+export { default as userAgentFromString } from './user-agent-from-string'
+export { default as URLPattern } from './url-pattern'

packages/next/src/server/web/spec-extension/image-response.ts
@@ -1,4 +1,5 @@
 export class ImageResponse {
+  public static displayName = 'NextImageResponse'
   constructor(
     ...args: ConstructorParameters<
       typeof import('next/dist/compiled/@vercel/og').ImageResponse

test/e2e/app-dir/metadata-dynamic-routes/app/twitter-image2.tsx
@@ -2,13 +2,13 @@ import { ImageResponse } from 'next/server'
 // @ts-ignore
 import { ImageResponse as ImageResponse2 } from '@vercel/og'
 
-// Node.js: Using @vercel/og external package, and should be aliased to "next/server" ImageResponse
+// Edge: Using @vercel/og external package, and should be aliased to "next/server" ImageResponse
 if (ImageResponse.displayName !== 'NextImageResponse')
   throw new Error('ImageResponse mismatch: ' + ImageResponse.displayName)
 // @ts-ignore
 if (ImageResponse2.displayName !== 'NextImageResponse')
   // @ts-ignore
-  throw new Error('ImageResponse mismatch: ' + ImageResponse2.displayName)
+  throw new Error('ImageResponse2 mismatch: ' + ImageResponse2.displayName)
 
 export const alt = 'Twitter'
 export const size = { width: 1600, height: 900 }
@@ -33,3 +33,5 @@ export default function twitter() {
     size
   )
 }
+
+export const runtime = 'edge'

test/e2e/app-dir/metadata-dynamic-routes/index.test.ts
@@ -14,6 +14,9 @@ createNextDescribe(
   'app dir - metadata dynamic routes',
   {
     files: __dirname,
+    dependencies: {
+      '@vercel/og': 'latest',
+    },
   },
   ({ next, isNextDev, isNextStart, isNextDeploy }) => {
     describe('text routes', () => {
@@ -101,8 +104,16 @@ createNextDescribe(
       })
 
       it('should render og image with twitter-image dynamic routes', async () => {
-        const res = await next.fetch('/twitter-image')
+        // nodejs runtime
+        let res = await next.fetch('/twitter-image')
 
+        expect(res.headers.get('content-type')).toBe('image/png')
+        expect(res.headers.get('cache-control')).toBe(
+          isNextDev ? CACHE_HEADERS.NONE : CACHE_HEADERS.LONG
+        )
+
+        // edge runtime
+        res = await next.fetch('/twitter-image2')
         expect(res.headers.get('content-type')).toBe('image/png')
         expect(res.headers.get('cache-control')).toBe(
           isNextDev ? CACHE_HEADERS.NONE : CACHE_HEADERS.LONG

test/e2e/app-dir/metadata-dynamic-routes/next.config.js
@@ -1,1 +1,9 @@
 module.exports = {}
+
+// For development: analyze the bundled chunks for stats app
+if (process.env.ANALYZE) {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true,
+  })
+  module.exports = withBundleAnalyzer(module.exports)
+}

commit 35f507242cbb10d17ea70e1716635246aa2a7998

fix: next dev with edge runtime on windows (#56502)
- Reverts https://github.com/vercel/next.js/pull/44616
- Regression introduced in https://github.com/vercel/next.js/pull/51651
- Fixes https://github.com/vercel/next.js/issues/55013

.github/actions/next-stats-action/src/index.js
@@ -127,9 +127,6 @@ if (!allowedActions.has(actionInfo.actionName) && !actionInfo.isRelease) {
         )
         .catch(console.error)
 
-      console.log(await exec(`ls ${path.join(__dirname, '../native')}`))
-      console.log(await exec(`cd ${dir} && ls ${dir}/packages/next-swc/native`))
-
       logger(`Linking packages in ${dir}`)
       const isMainRepo = dir === mainRepoDir
       const pkgPaths = await linkPackages({

azure-pipelines.yml
@@ -111,76 +111,76 @@ stages:
             env:
               NEXT_TEST_MODE: 'dev'
 
-      # - job: test_e2e_dev
-      #   pool:
-      #     vmImage: 'windows-2019'
-      #   steps:
-      #     - task: NodeTool@0
-      #       inputs:
-      #         versionSpec: $(node_16_version)
-      #       displayName: 'Install Node.js'
+      - job: test_e2e_dev
+        pool:
+          vmImage: 'windows-2019'
+        steps:
+          - task: NodeTool@0
+            inputs:
+              versionSpec: $(node_16_version)
+            displayName: 'Install Node.js'
 
-      #     - bash: |
-      #         node scripts/run-for-change.js --not --type docs --exec echo "##vso[task.setvariable variable=isDocsOnly]No"
-      #       displayName: 'Check Docs Only Change'
+          - bash: |
+              node scripts/run-for-change.js --not --type docs --exec echo "##vso[task.setvariable variable=isDocsOnly]No"
+            displayName: 'Check Docs Only Change'
 
-      #     - script: corepack enable
-      #       condition: eq(variables['isDocsOnly'], 'No')
-      #       displayName: 'Enable Corepack'
+          - script: corepack enable
+            condition: eq(variables['isDocsOnly'], 'No')
+            displayName: 'Enable Corepack'
 
-      #     - script: pnpm config set store-dir $(PNPM_CACHE_FOLDER)
-      #       condition: eq(variables['isDocsOnly'], 'No')
+          - script: pnpm config set store-dir $(PNPM_CACHE_FOLDER)
+            condition: eq(variables['isDocsOnly'], 'No')
 
-      #     - script: pnpm store path
-      #       condition: eq(variables['isDocsOnly'], 'No')
+          - script: pnpm store path
+            condition: eq(variables['isDocsOnly'], 'No')
 
-      #     - script: pnpm install && pnpm run build
-      #       condition: eq(variables['isDocsOnly'], 'No')
-      #       displayName: 'Install and build'
+          - script: pnpm install && pnpm run build
+            condition: eq(variables['isDocsOnly'], 'No')
+            displayName: 'Install and build'
 
-      #     - script: npx playwright@1.35.1 install chromium
-      #       condition: eq(variables['isDocsOnly'], 'No')
+          - script: npx playwright@1.35.1 install chromium
+            condition: eq(variables['isDocsOnly'], 'No')
 
-      #     - script: |
-      #         node run-tests.js -c 1 --debug test/e2e/app-dir/app/index.test.ts
-      #       condition: eq(variables['isDocsOnly'], 'No')
-      #       displayName: 'Run tests (E2E Development)'
-      #       env:
-      #         NEXT_TEST_MODE: 'dev'
+          - script: |
+              node run-tests.js -c 1 --debug test/e2e/app-dir/app/index.test.ts test/e2e/app-dir/app-edge/app-edge.test.ts
+            condition: eq(variables['isDocsOnly'], 'No')
+            displayName: 'Run tests (E2E Development)'
+            env:
+              NEXT_TEST_MODE: 'dev'
 
-      # - job: test_e2e_prod
-      #   pool:
-      #     vmImage: 'windows-2019'
-      #   steps:
-      #     - task: NodeTool@0
-      #       inputs:
-      #         versionSpec: $(node_16_version)
-      #       displayName: 'Install Node.js'
+      - job: test_e2e_prod
+        pool:
+          vmImage: 'windows-2019'
+        steps:
+          - task: NodeTool@0
+            inputs:
+              versionSpec: $(node_16_version)
+            displayName: 'Install Node.js'
 
-      #     - bash: |
-      #         node scripts/run-for-change.js --not --type docs --exec echo "##vso[task.setvariable variable=isDocsOnly]No"
-      #       displayName: 'Check Docs Only Change'
+          - bash: |
+              node scripts/run-for-change.js --not --type docs --exec echo "##vso[task.setvariable variable=isDocsOnly]No"
+            displayName: 'Check Docs Only Change'
 
-      #     - script: corepack enable
-      #       condition: eq(variables['isDocsOnly'], 'No')
-      #       displayName: 'Enable Corepack'
+          - script: corepack enable
+            condition: eq(variables['isDocsOnly'], 'No')
+            displayName: 'Enable Corepack'
 
-      #     - script: pnpm config set store-dir $(PNPM_CACHE_FOLDER)
-      #       condition: eq(variables['isDocsOnly'], 'No')
+          - script: pnpm config set store-dir $(PNPM_CACHE_FOLDER)
+            condition: eq(variables['isDocsOnly'], 'No')
 
-      #     - script: pnpm store path
-      #       condition: eq(variables['isDocsOnly'], 'No')
+          - script: pnpm store path
+            condition: eq(variables['isDocsOnly'], 'No')
 
-      #     - script: pnpm install && pnpm run build
-      #       condition: eq(variables['isDocsOnly'], 'No')
-      #       displayName: 'Install and build'
+          - script: pnpm install && pnpm run build
+            condition: eq(variables['isDocsOnly'], 'No')
+            displayName: 'Install and build'
 
-      #     - script: npx playwright@1.35.1 install chromium
-      #       condition: eq(variables['isDocsOnly'], 'No')
+          - script: npx playwright@1.35.1 install chromium
+            condition: eq(variables['isDocsOnly'], 'No')
 
-      #     - script: |
-      #         node run-tests.js -c 1 --debug test/e2e/app-dir/app/index.test.ts
-      #       condition: eq(variables['isDocsOnly'], 'No')
-      #       displayName: 'Run tests (E2E Production)'
-      #       env:
-      #         NEXT_TEST_MODE: 'start'
+          - script: |
+              node run-tests.js -c 1 --debug test/e2e/app-dir/app/index.test.ts test/e2e/app-dir/app-edge/app-edge.test.ts
+            condition: eq(variables['isDocsOnly'], 'No')
+            displayName: 'Run tests (E2E Production)'
+            env:
+              NEXT_TEST_MODE: 'start'

packages/next/src/build/webpack-config.ts
@@ -822,28 +822,41 @@ export default async function getBaseWebpackConfig(
             'next/dist/server': 'next/dist/esm/server',
 
             // Alias the usage of next public APIs
-            [`${NEXT_PROJECT_ROOT}/server`]:
+            [path.join(NEXT_PROJECT_ROOT, 'server')]:
               'next/dist/esm/server/web/exports/index',
-            [`${NEXT_PROJECT_ROOT}/dist/client/link`]:
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'client', 'link')]:
               'next/dist/esm/client/link',
-            [`${NEXT_PROJECT_ROOT}/dist/shared/lib/image-external`]:
-              'next/dist/esm/shared/lib/image-external',
-            [`${NEXT_PROJECT_ROOT}/dist/client/script`]:
+            [path.join(
+              NEXT_PROJECT_ROOT,
+              'dist',
+              'shared',
+              'lib',
+              'image-external'
+            )]: 'next/dist/esm/shared/lib/image-external',
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'client', 'script')]:
               'next/dist/esm/client/script',
-            [`${NEXT_PROJECT_ROOT}/dist/client/router`]:
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'client', 'router')]:
               'next/dist/esm/client/router',
-            [`${NEXT_PROJECT_ROOT}/dist/shared/lib/head`]:
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'shared', 'lib', 'head')]:
               'next/dist/esm/shared/lib/head',
-            [`${NEXT_PROJECT_ROOT}/dist/shared/lib/dynamic`]:
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'shared', 'lib', 'dynamic')]:
               'next/dist/esm/shared/lib/dynamic',
-            [`${NEXT_PROJECT_ROOT}/dist/pages/_document`]:
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'pages', '_document')]:
               'next/dist/esm/pages/_document',
-            [`${NEXT_PROJECT_ROOT}/dist/pages/_app`]:
+            [path.join(NEXT_PROJECT_ROOT_DIST, 'pages', '_app')]:
               'next/dist/esm/pages/_app',
-            [`${NEXT_PROJECT_ROOT}/dist/client/components/navigation`]:
-              'next/dist/esm/client/components/navigation',
-            [`${NEXT_PROJECT_ROOT}/dist/client/components/headers`]:
-              'next/dist/esm/client/components/headers',
+            [path.join(
+              NEXT_PROJECT_ROOT_DIST,
+              'client',
+              'components',
+              'navigation'
+            )]: 'next/dist/esm/client/components/navigation',
+            [path.join(
+              NEXT_PROJECT_ROOT_DIST,
+              'client',
+              'components',
+              'headers'
+            )]: 'next/dist/esm/client/components/headers',
           }
         : undefined),
 

test/e2e/app-dir/app-edge/app-edge.test.ts
@@ -55,10 +55,20 @@ createNextDescribe(
         next.on('stderr', (log) => {
           logs.push(log)
         })
-        await next.render('app-edge')
+        await next.render('/app-edge')
         expect(
           logs.some((log) => log.includes(`Attempted import error:`))
         ).toBe(false)
+      })
+
+      it('should resolve client component without error', async () => {
+        const logs = []
+        next.on('stderr', (log) => {
+          logs.push(log)
+        })
+        const html = await next.render('/with-client')
+        expect(html).toContain('My Button')
+        expect(logs).toEqual([])
       })
 
       it('should handle edge rsc hmr', async () => {

test/e2e/app-dir/app-edge/app/with-client/page.tsx
@@ -1,0 +1,13 @@
+import Button from '../../components/button'
+import React from 'react'
+
+export const runtime = 'edge'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Import a client component</h1>
+      <Button />
+    </>
+  )
+}

test/e2e/app-dir/app-edge/components/button.tsx
@@ -1,0 +1,9 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+export default function Button() {
+  const router = useRouter()
+  const onClick = () => router.push('/')
+  return <button onClick={onClick}>My Button</button>
+}