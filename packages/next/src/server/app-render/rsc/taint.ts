41926d11fd43d831fa88275a0cdd35e6fcecb732
Sebastian Markbage <sebastian@calyptus.eu>
2023-10-11 12:02:13 -0400

    Check for experimental React using process.env.__NEXT_EXPERIMENTAL_REACT

--- packages/next/src/server/app-render/rsc/taint.ts
+++ packages/next/src/server/app-render/rsc/taint.ts
@@ -6,7 +6,11 @@ Files in the rsc directory are meant to be packaged as part of the RSC graph usi
 
 import * as React from 'react'
 
-// @ts-ignore
-export const taintObjectReference = React.experimental_taintObjectReference
-// @ts-ignore
-export const taintUniqueValue = React.experimental_taintUniqueValue
+export const taintObjectReference = process.env.__NEXT_EXPERIMENTAL_REACT
+  ? // @ts-ignore
+    React.experimental_taintObjectReference
+  : null
+export const taintUniqueValue = process.env.__NEXT_EXPERIMENTAL_REACT
+  ? // @ts-ignore
+    React.experimental_taintUniqueValue
+  : null
