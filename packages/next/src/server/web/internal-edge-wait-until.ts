5dc9f3752efd0e0fda96bf5e7042662578e83367
Shu Ding <g@shud.in>
2023-10-11 21:41:00 +0200

    fix impl

--- packages/next/src/server/web/internal-edge-wait-until.ts
+++ packages/next/src/server/web/internal-edge-wait-until.ts
@@ -1,22 +1,36 @@
 // An internal module to expose the "waitUntil" API to Edge SSR and Edge Route Handler functions.
 // This is highly experimental and subject to change.
 
-let currentWaitUntilCounter = 0
-let currentWaitUntilResolve: () => void
-let currentWaitUntilPromise: null | Promise<void> = null
+// We still need a global key to bypass Webpack's layering of modules.
+const GLOBAL_KEY = Symbol.for('__next_internal_waitUntil__')
+
+const state: {
+  waitUntilCounter: number
+  waitUntilResolve: () => void
+  waitUntilPromise: Promise<void> | null
+} =
+  // @ts-ignore
+  globalThis[GLOBAL_KEY] ||
+  // @ts-ignore
+  (globalThis[GLOBAL_KEY] = {
+    waitUntilCounter: 0,
+    waitUntilResolve: undefined,
+    waitUntilPromise: null,
+  })
 
 // No matter how many concurrent requests are being handled, we want to make sure
 // that the final promise is only resolved once all of the waitUntil promises have
 // settled.
 function resolveOnePromise() {
-  currentWaitUntilCounter--
-  if (currentWaitUntilCounter === 0) {
-    currentWaitUntilResolve()
+  state.waitUntilCounter--
+  if (state.waitUntilCounter === 0) {
+    state.waitUntilResolve()
+    state.waitUntilPromise = null
   }
 }
 
 export function internal_getCurrentFunctionWaitUntil() {
-  return currentWaitUntilPromise
+  return state.waitUntilPromise
 }
 
 export function internal_runWithWaitUntil<T>(fn: () => T): T {
@@ -29,13 +43,13 @@ export function internal_runWithWaitUntil<T>(fn: () => T): T {
     typeof result.then === 'function' &&
     typeof result.finally === 'function'
   ) {
-    if (!currentWaitUntilCounter) {
+    if (!state.waitUntilCounter) {
       // Create the promise for the next batch of waitUntil calls.
-      currentWaitUntilPromise = new Promise<void>((resolve) => {
-        currentWaitUntilResolve = resolve
+      state.waitUntilPromise = new Promise<void>((resolve) => {
+        state.waitUntilResolve = resolve
       })
     }
-    currentWaitUntilCounter++
+    state.waitUntilCounter++
     return result.finally(() => {
       resolveOnePromise()
     })
