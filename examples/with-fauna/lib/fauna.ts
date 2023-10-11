216d968c9fbe54a183ac706c52e39d927116d06a
Shadid12 <Shadid12@users.noreply.github.com>
2023-10-11 13:39:25 -0400

    examples: Update Fauna example (#56185)

---
+++ examples/with-fauna/lib/fauna.ts
@@ -0,0 +1,37 @@
+import 'server-only'
+import {
+  Client,
+  fql,
+  QuerySuccess,
+  QueryValueObject,
+  QueryFailure,
+} from 'fauna'
+
+const client = new Client({
+  secret: process.env.FAUNA_CLIENT_SECRET,
+})
+
+export const getAllEntries = async () => {
+  try {
+    const dbresponse: QuerySuccess<QueryValueObject> = await client.query(fql`
+		Entry.all()
+	`)
+    return dbresponse.data.data
+  } catch (error: QueryFailure | any) {
+    throw new Error(error.message)
+  }
+}
+
+export const createEntry = async (name: string, message: string) => {
+  try {
+    const dbresponse = await client.query(fql`
+			Entry.create({
+				name: ${name},
+				message: ${message},
+				createdAt: Time.now(),
+			})`)
+    return dbresponse.data
+  } catch (error: QueryFailure | any) {
+    throw new Error(error.message)
+  }
+}
