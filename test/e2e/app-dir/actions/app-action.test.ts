89a672bd1a536843e1fa83b2f51ba539599d748f
Quentin <contact@yovach.dev>
2023-10-12 14:00:30 +0200

    perf(tests): use "limit" param split

--- test/e2e/app-dir/actions/app-action.test.ts
+++ test/e2e/app-dir/actions/app-action.test.ts
@@ -45,7 +45,7 @@ createNextDescribe(
       await browser.elementByCss('#cookie').click()
       await check(async () => {
         const res = (await browser.elementByCss('h1').text()) || ''
-        const id = res.split(':')
+        const id = res.split(':', 2)
         return id[0] === id[1] && id[0] ? 'same' : 'different'
       }, 'same')
 
@@ -59,7 +59,7 @@ createNextDescribe(
       await browser.elementByCss('#setCookie').click()
       await check(async () => {
         const res = (await browser.elementByCss('h1').text()) || ''
-        const id = res.split(':')
+        const id = res.split(':', 2)
         return id[0] === id[1] && id[0] === id[2] && id[0]
           ? 'same'
           : 'different'
