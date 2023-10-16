vercel/next.js

commit f0f87629c3210131bf9e2a8f69525ef39c8c2d1d

Drop ipc server headers filters (#56226)
It's merged into single process after #54813 , we don't need to filter ipc server headers anymore. Still need to keep header filters for server actions

packages/next/src/server/lib/router-utils/resolve-routes.ts
@@ -9,7 +9,6 @@ import url from 'url'
 import { Redirect } from '../../../../types'
 import setupDebug from 'next/dist/compiled/debug'
 import { getCloneableBody } from '../../body-streams'
-import { filterReqHeaders, ipcForbiddenHeaders } from '../server-ipc/utils'
 import { Header } from '../../../lib/load-custom-routes'
 import { stringifyQuery } from '../../server-route-utils'
 import { formatHostname } from '../format-hostname'
@@ -457,7 +456,7 @@ export function getResolveRoutes(
               const { res: mockedRes } = await createRequestResponseMocks({
                 url: req.url || '/',
                 method: req.method || 'GET',
-                headers: filterReqHeaders(invokeHeaders, ipcForbiddenHeaders),
+                headers: invokeHeaders,
                 resWriter(chunk) {
                   readableController.enqueue(Buffer.from(chunk))
                   return true
@@ -560,7 +559,7 @@ export function getResolveRoutes(
             delete middlewareHeaders['x-middleware-next']
 
             for (const [key, value] of Object.entries({
-              ...filterReqHeaders(middlewareHeaders, ipcForbiddenHeaders),
+              ...middlewareHeaders,
             })) {
               if (
                 [

packages/next/src/server/lib/server-ipc/invoke-request.ts
@@ -1,6 +1,5 @@
 import type { IncomingMessage } from 'http'
 import type { Readable } from 'stream'
-import { filterReqHeaders, ipcForbiddenHeaders } from './utils'
 
 export const invokeRequest = async (
   targetUrl: string,
@@ -11,13 +10,10 @@ export const invokeRequest = async (
   },
   readableBody?: Readable | ReadableStream
 ) => {
-  const invokeHeaders = filterReqHeaders(
-    {
-      'cache-control': '',
-      ...requestInit.headers,
-    },
-    ipcForbiddenHeaders
-  ) as IncomingMessage['headers']
+  const invokeHeaders = {
+    'cache-control': '',
+    ...requestInit.headers,
+  }
 
   return await fetch(targetUrl, {
     headers: invokeHeaders as any as Headers,

packages/next/src/server/lib/server-ipc/utils.ts
@@ -1,4 +1,4 @@
-export const ipcForbiddenHeaders = [
+export const actionsForbiddenHeaders = [
   'accept-encoding',
   'keepalive',
   'keep-alive',
@@ -8,10 +8,7 @@ export const ipcForbiddenHeaders = [
   'connection',
   // marked as unsupported by undici: https://github.com/nodejs/undici/blob/c83b084879fa0bb8e0469d31ec61428ac68160d5/lib/core/request.js#L354
   'expect',
-]
-
-export const actionsForbiddenHeaders = [
-  ...ipcForbiddenHeaders,
+  // action specific
   'content-length',
 ]
 

packages/next/src/server/next-server.ts
@@ -93,7 +93,6 @@ import { nodeFs } from './lib/node-fs-methods'
 import { getRouteRegex } from '../shared/lib/router/utils/route-regex'
 import { invokeRequest } from './lib/server-ipc/invoke-request'
 import { pipeReadable } from './pipe-readable'
-import { filterReqHeaders, ipcForbiddenHeaders } from './lib/server-ipc/utils'
 import { createRequestResponseMocks } from './lib/mock-request'
 import { NEXT_RSC_UNION_QUERY } from '../client/components/app-router-headers'
 import { signalFromNodeResponse } from './web/spec-extension/adapters/next-request'
@@ -539,13 +538,12 @@ export default class NextNodeServer extends BaseServer {
               signal: signalFromNodeResponse(res.originalResponse),
             }
           )
-          const filteredResHeaders = filterReqHeaders(
-            toNodeOutgoingHttpHeaders(invokeRes.headers),
-            ipcForbiddenHeaders
+          const nodeOutgoingHttpHeaders = toNodeOutgoingHttpHeaders(
+            invokeRes.headers
           )
 
-          for (const key of Object.keys(filteredResHeaders)) {
-            newRes.setHeader(key, filteredResHeaders[key] || '')
+          for (const key of Object.keys(nodeOutgoingHttpHeaders)) {
+            newRes.setHeader(key, nodeOutgoingHttpHeaders[key] || '')
           }
           newRes.statusCode = invokeRes.status || 200
 

commit d32121b736098ac78544996fb2c5935d0c3128fd

chore(test): test remote image from proxy (#56895)
This PR adds a test to ensure we don't regress again when using proxies
in front of Next.js with `connection: 'upgrade'`.

- Regression introduced in https://github.com/vercel/next.js/pull/56226
- Issue reported in https://github.com/vercel/next.js/issues/56038
- Rolled back in https://github.com/vercel/next.js/pull/56836

test/e2e/app-dir/next-image/app/page.js
@@ -7,6 +7,13 @@ export default function Page() {
     <>
       <h2>app-page</h2>
       <Image id="app-page" src={testPng} quality={90} />
+      <Image
+        id="remote-app-page"
+        src="https://image-optimization-test.vercel.app/test.jpg"
+        width="200"
+        height="200"
+        quality={90}
+      />
       <Comp />
     </>
   )

test/e2e/app-dir/next-image/next-image-proxy.test.ts
@@ -73,19 +73,34 @@ createNextDescribe(
         },
       })
 
-      const image = browser.elementByCss('#app-page')
-      const src = await image.getAttribute('src')
-
-      expect(src).toContain(
+      const local = await browser.elementByCss('#app-page').getAttribute('src')
+      expect(local).toContain(
         '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=828&q=90'
       )
 
-      await check(() => {
-        // we expect 3 images to load and for none of them to have errors
-        if (fulfilledCount === 3 && failCount === 0) {
-          return 'success'
-        }
-      }, 'success')
+      const remote = await browser
+        .elementByCss('#remote-app-page')
+        .getAttribute('src')
+      expect(remote).toBe(
+        '/_next/image?url=https%3A%2F%2Fimage-optimization-test.vercel.app%2Ftest.jpg&w=640&q=90'
+      )
+
+      const expected = JSON.stringify({ fulfilledCount: 4, failCount: 0 })
+      await check(() => JSON.stringify({ fulfilledCount, failCount }), expected)
+    })
+
+    it('should work with connection upgrade by removing it via filterReqHeaders()', async () => {
+      const opts = { headers: { connection: 'upgrade' } }
+      const res1 = await next.fetch(
+        '/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftest.3f1a293b.png&w=828&q=90',
+        opts
+      )
+      expect(res1.status).toBe(200)
+      const res2 = await next.fetch(
+        '/_next/image?url=https%3A%2F%2Fimage-optimization-test.vercel.app%2Ftest.jpg&w=640&q=90',
+        opts
+      )
+      expect(res2.status).toBe(200)
     })
 
     afterAll(() => {

test/e2e/app-dir/next-image/next.config.js
@@ -1,1 +1,5 @@
-module.exports = {}
+module.exports = {
+  images: {
+    remotePatterns: [{ hostname: 'image-optimization-test.vercel.app' }],
+  },
+}