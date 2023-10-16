9dac19281101f3fb4507dccc9a58cc3ec1cf82d8
Balázs Orbán <info@balazsorban.com>
2023-10-16 21:03:59 +0200

    update tests

--- test/e2e/app-dir/actions/app-action-progressive-enhancement.test.ts
+++ test/e2e/app-dir/actions/app-action-progressive-enhancement.test.ts
@@ -39,7 +39,7 @@ createNextDescribe(
 
       await check(() => {
         return browser.eval('window.location.pathname + window.location.search')
-      }, '/header?name=test&constructor=FormData&hidden-info=hi')
+      }, '/header?name=test&constructor=_FormData&hidden-info=hi')
     })
   }
 )
