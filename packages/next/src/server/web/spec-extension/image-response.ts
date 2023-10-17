9017d1cb968a4d871c003c9347263b9d0b2ae2be
Jiachi Liu <inbox@huozhi.im>
2023-10-17 07:27:21 -0700

    update migration error msg

--- packages/next/src/server/web/spec-extension/image-response.ts
+++ packages/next/src/server/web/spec-extension/image-response.ts
@@ -1,5 +1,5 @@
 export function ImageResponse() {
   throw new Error(
-    'ImageResponse export is moved to next/og since Next.js 14, please import from "next/og" instead'
+    'ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead'
   )
 }
