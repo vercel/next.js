154dda40969dac312433ca1d2bb11a1f02111a11
Sebastian Markbage <sebastian@calyptus.eu>
2023-10-17 16:38:45 -0400

    Type the taint apis internally
    This pretends that they always exist so that they error at runtime if we
    use them without the flag check.

--- packages/next/src/server/app-render/rsc/taint.ts
+++ packages/next/src/server/app-render/rsc/taint.ts
@@ -6,11 +6,25 @@ Files in the rsc directory are meant to be packaged as part of the RSC graph usi
 
 import * as React from 'react'
 
-export const taintObjectReference = process.env.__NEXT_EXPERIMENTAL_REACT
+type Reference = object
+type TaintableUniqueValue = string | bigint | ArrayBufferView
+
+function notImplemented() {
+  throw new Error('Taint can only be used with the taint flag.')
+}
+
+export const taintObjectReference: (
+  message: string | undefined,
+  object: Reference
+) => void = process.env.__NEXT_EXPERIMENTAL_REACT
   ? // @ts-ignore
     React.experimental_taintObjectReference
-  : null
-export const taintUniqueValue = process.env.__NEXT_EXPERIMENTAL_REACT
+  : notImplemented
+export const taintUniqueValue: (
+  message: string | undefined,
+  lifetime: Reference,
+  value: TaintableUniqueValue
+) => void = process.env.__NEXT_EXPERIMENTAL_REACT
   ? // @ts-ignore
     React.experimental_taintUniqueValue
-  : null
+  : notImplemented
