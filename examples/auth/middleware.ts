1520b5afaa3b7ed04852bd1e6871fbadc1fbff9c
Lee Robinson <lrobinson2011@gmail.com>
2023-10-16 21:49:15 -0500

    Middleware

--- examples/auth/middleware.ts
+++ examples/auth/middleware.ts
@@ -1,5 +1,5 @@
 export { auth as middleware } from './auth'
 
-// export const config = {
-//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-// }
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
