b04a847a2bd1881643036990e7c8b17eb023a206
Kevin Martensson <kevinmartensson@gmail.com>
2023-10-18 12:51:43 +0200

    chore: remove unneeded script code

--- test/e2e/app-dir/app/app/script-nonce/page.js
+++ test/e2e/app-dir/app/app/script-nonce/page.js
@@ -5,10 +5,7 @@ export default function Page() {
     <>
       <Script strategy="afterInteractive" src="/test1.js" />
       <Script strategy="beforeInteractive" src="/test2.js" />
-      <Script strategy="beforeInteractive" id="3">{`
-        ;(window._script_order = window._script_order || []).push(3)
-        console.log(window._script_order)
-      `}</Script>
+      <Script strategy="beforeInteractive" id="3" />
     </>
   )
 }
