920a7e8ba6d029042e73df94c0041ee206639dbe
Josh Story <story@hey.com>
2023-10-20 10:42:27 -0700

    Update React from a41957507 to d803f519e (#57015)
    ## React upstream Changes
    
    - https://github.com/facebook/react/pull/27542
    - https://github.com/facebook/react/pull/27551
    - https://github.com/facebook/react/pull/27537
    - https://github.com/facebook/react/pull/27549
    - https://github.com/facebook/react/pull/27544
    - https://github.com/facebook/react/pull/27543
    - https://github.com/facebook/react/pull/27541
    - https://github.com/facebook/react/pull/27538
    - https://github.com/facebook/react/pull/27536

--- packages/next/src/compiled/react-dom/cjs/react-dom-server.node.development.js
+++ packages/next/src/compiled/react-dom/cjs/react-dom-server.node.development.js
@@ -20,7 +20,7 @@ var crypto = require('crypto');
 var async_hooks = require('async_hooks');
 var ReactDOM = require('react-dom');
 
-var ReactVersion = '18.3.0-canary-a41957507-20231017';
+var ReactVersion = '18.3.0-canary-d803f519e-20231020';
 
 var ReactSharedInternals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
@@ -10985,7 +10985,10 @@ function flushCompletedQueues(request, destination) {
         } // We're done.
 
 
-        close(destination);
+        close(destination); // We need to stop flowing now because we do not want any async contexts which might call
+        // float methods to initiate any flushes after this point
+
+        stopFlowing(request);
       } else {
       completeWriting(destination);
       flushBuffered(destination);
@@ -11008,10 +11011,17 @@ function enqueueFlush(request) {
   request.pingedTasks.length === 0 && // If there is no destination there is nothing we can flush to. A flush will
   // happen when we start flowing again
   request.destination !== null) {
-    var destination = request.destination;
     request.flushScheduled = true;
     scheduleWork(function () {
-      return flushCompletedQueues(request, destination);
+      // We need to existence check destination again here because it might go away
+      // in between the enqueueFlush call and the work execution
+      var destination = request.destination;
+
+      if (destination) {
+        flushCompletedQueues(request, destination);
+      } else {
+        request.flushScheduled = false;
+      }
     });
   }
 }
