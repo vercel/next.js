46a5210d029a71aa453dcb75516017348995e903
Tobias Koppers <tobias.koppers@googlemail.com>
2023-10-22 13:46:18 -0700

    avoid cloning list (vercel/turbo#6244)
    ### Description
    
    Improved performance
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->
    
    
    Closes WEB-1821

--- crates/turbo-tasks/src/read_ref.rs
+++ crates/turbo-tasks/src/read_ref.rs
@@ -3,6 +3,7 @@ use std::{
     fmt::{Debug, Display},
     hash::Hash,
     marker::PhantomData,
+    mem::transmute_copy,
     sync::Arc,
 };
 
@@ -150,6 +151,52 @@ where
 
     fn into_iter(self) -> Self::IntoIter {
         (&**self).into_iter()
+    }
+}
+
+impl<T, I: 'static, J: Iterator<Item = I>> IntoIterator for ReadRef<T>
+where
+    T: VcValueType,
+    &'static <T::Read as VcRead<T>>::Target: IntoIterator<Item = I, IntoIter = J>,
+{
+    type Item = I;
+
+    type IntoIter = ReadRefIter<T, I, J>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let r = &*self;
+        // # Safety
+        // The reference will we valid as long as the ReadRef is valid.
+        let r = unsafe {
+            transmute_copy::<
+                &'_ <T::Read as VcRead<T>>::Target,
+                &'static <T::Read as VcRead<T>>::Target,
+            >(&r)
+        };
+        ReadRefIter {
+            read_ref: self,
+            iter: r.into_iter(),
+        }
+    }
+}
+
+pub struct ReadRefIter<T, I: 'static, J: Iterator<Item = I>>
+where
+    T: VcValueType,
+{
+    iter: J,
+    #[allow(dead_code)]
+    read_ref: ReadRef<T>,
+}
+
+impl<T, I: 'static, J: Iterator<Item = I>> Iterator for ReadRefIter<T, I, J>
+where
+    T: VcValueType,
+{
+    type Item = I;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
     }
 }
 
