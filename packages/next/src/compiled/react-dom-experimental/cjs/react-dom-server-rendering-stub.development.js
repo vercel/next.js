c7acb0fa042cd1b43eb645a0c36ce4999d455433
Andrew Clark <git@andrewclark.io>
2023-10-23 15:15:15 -0700

    Update React from d803f519e to b8e47d988
    React upstream changes:
    
    - https://github.com/facebook/react/pull/27570
    - https://github.com/facebook/react/pull/27569
    - https://github.com/facebook/react/pull/27550
    - https://github.com/facebook/react/pull/27559
    - https://github.com/facebook/react/pull/27552
    - https://github.com/facebook/react/pull/27504
    - https://github.com/facebook/react/pull/27522

--- packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
+++ packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
@@ -55,7 +55,7 @@ function printWarning(level, format, args) {
   }
 }
 
-var ReactVersion = '18.3.0-experimental-d803f519e-20231020';
+var ReactVersion = '18.3.0-experimental-b8e47d988-20231023';
 
 var Internals = {
   usingClientEntryPoint: false,
--- packages/next/src/compiled/react/cjs/react.shared-subset.development.js
+++ packages/next/src/compiled/react/cjs/react.shared-subset.development.js
@@ -281,7 +281,7 @@ var ReactServerSharedInternals = {
   ReactCurrentCache: ReactCurrentCache
 };
 
-var ReactVersion = '18.3.0-canary-d803f519e-20231020';
+var ReactVersion = '18.3.0-canary-b8e47d988-20231023';
 
 // ATTENTION
 // When adding new symbols to this file,
