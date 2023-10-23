7e215f07210ac0446fb4cdc3119ef736114f2e67
Lewis <lewis@spellcraft.org>
2023-10-22 17:12:18 -0700

    feat(cna): standardize viewport sizing
    Original fix from #28096 was overwritten in #43482.
    
    see https://twitter.com/rauchg/status/1716226300602655119

--- packages/create-next-app/templates/app-tw/js/app/page.js
+++ packages/create-next-app/templates/app-tw/js/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between gap-8 p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
