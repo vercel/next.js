32c257f0b71073ca5b3e06ea8247a7ddfaac9f84
Jiachi Liu <inbox@huozhi.im>
2023-10-24 16:28:19 -0700

    types: mark ImageResponse from next/server deprecated

--- packages/next/src/server/web/spec-extension/image-response.ts
+++ packages/next/src/server/web/spec-extension/image-response.ts
@@ -1,5 +1,13 @@
-export function ImageResponse() {
+/**
+ * @deprecated ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead
+ */
+export function ImageResponse(): never {
   throw new Error(
     'ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead'
   )
 }
+
+/**
+ * @deprecated ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead
+ */
+export type ImageResponse = never
