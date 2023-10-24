26277b6cc8f6016aad1c7cf0fff5e5501569dfd2
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-10-24 13:03:50 +1100

    simplify SQL snippet

--- examples/with-supabase/components/SignUpUserSteps.tsx
+++ examples/with-supabase/components/SignUpUserSteps.tsx
@@ -4,12 +4,15 @@ import Code from '@/components/Code'
 
 const create = `
 create table notes (
-  id uuid default gen_random_uuid() primary key,
+  id serial primary key,
   title text
 );
 
 insert into notes(title)
-values('Today I connected Next.js to Supabase. It was awesome!');
+values
+  ('Today I created a Supabase project.'),
+  ('I added some data and queried it from Next.js.'),
+  ('It was awesome!');
 `.trim()
 
 const server = `
