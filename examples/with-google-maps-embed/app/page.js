a1b9c8046d510cb073baa7f0190ff055ed9f8a5b
Houssein Djirdeh <houssein@google.com>
2023-10-24 17:47:09 -0400

    change page function name to Page

--- examples/with-google-maps-embed/app/page.js
+++ examples/with-google-maps-embed/app/page.js
@@ -2,7 +2,7 @@ import { GoogleMapsEmbed } from '@next/third-parties/google'
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 
-export default function Home() {
+export default function Page() {
   return (
     <div>
       <GoogleMapsEmbed
