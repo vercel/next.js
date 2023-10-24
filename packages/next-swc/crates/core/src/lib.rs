7e14022f4fd7e383d6dcd42ac702fac92d0e39a2
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-10-23 23:23:57 -0700

    Change order

--- packages/next-swc/crates/core/src/lib.rs
+++ packages/next-swc/crates/core/src/lib.rs
@@ -190,7 +190,6 @@ where
     };
 
     chain!(
-        pure::pure_magic(comments.clone()),
         disallow_re_export_all_in_page::disallow_re_export_all_in_page(opts.is_page_file),
         match &opts.server_components {
             Some(config) if config.truthy() =>
@@ -313,7 +312,7 @@ where
             Some(config) => Either::Left(server_actions::server_actions(
                 &file.name,
                 config.clone(),
-                comments,
+                comments.clone(),
             )),
             None => Either::Right(noop()),
         },
@@ -323,6 +322,7 @@ where
             },
             None => Either::Right(noop()),
         },
+        pure::pure_magic(comments),
     )
 }
 
