a3cbda090611ab2ec2d1f500bf67ea05a89a2d82
Jiachi Liu <inbox@huozhi.im>
2023-10-24 16:45:52 -0700

    rm duplicated type

--- packages/next/src/server/web/spec-extension/image-response.ts
+++ packages/next/src/server/web/spec-extension/image-response.ts
@@ -6,8 +6,3 @@ export function ImageResponse(): never {
     'ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead'
   )
 }
-
-/**
- * @deprecated ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead
- */
-export type ImageResponse = never
