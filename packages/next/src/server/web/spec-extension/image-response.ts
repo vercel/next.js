bc4fd866934bcf08d89401f026460d91741fb39e
Jiachi Liu <inbox@huozhi.im>
2023-10-25 12:37:49 -0700

    update docs with link in derepcation msg

--- packages/next/src/server/web/spec-extension/image-response.ts
+++ packages/next/src/server/web/spec-extension/image-response.ts
@@ -1,5 +1,6 @@
 /**
- * @deprecated ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead
+ * @deprecated ImageResponse moved from "next/server" to "next/og" since Next.js 14, please import from "next/og" instead.
+ * Migration with codemods: https://nextjs.org/docs/app/building-your-application/upgrading/codemods#next-og-import
  */
 export function ImageResponse(): never {
   throw new Error(
