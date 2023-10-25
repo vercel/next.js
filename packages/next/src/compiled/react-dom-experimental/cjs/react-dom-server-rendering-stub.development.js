fcc28999bfa803caaa1ec4fb324a070c48fa6bd4
Josh Story <story@hey.com>
2023-10-25 13:29:47 -0700

    Update React from b8e47d988 to a9985529f. (#57442)
    React upstream changes:
    
    - https://github.com/facebook/react/pull/27586
    - https://github.com/facebook/react/pull/27577
    - https://github.com/facebook/react/pull/27576
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
+++ packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
@@ -55,7 +55,7 @@ function printWarning(level, format, args) {
   }
 }
 
-var ReactVersion = '18.3.0-experimental-b8e47d988-20231023';
+var ReactVersion = '18.3.0-experimental-a9985529f-20231025';
 
 var Internals = {
   usingClientEntryPoint: false,
