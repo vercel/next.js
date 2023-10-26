00df11b0a7b07e20cdc6b99b1f57fc61d6980ddc
Andrew Clark <git@andrewclark.io>
2023-10-26 00:10:27 -0700

    Update React from a9985529f to 8c8ee9ee6 (#57483)
    React upstream changes:
    
    - https://github.com/facebook/react/pull/27594
    - https://github.com/facebook/react/pull/27584
    
    Co-authored-by: Jiachi Liu <4800338+huozhi@users.noreply.github.com>

--- packages/next/src/compiled/react/cjs/react.development.js
+++ packages/next/src/compiled/react/cjs/react.development.js
@@ -23,7 +23,7 @@ if (
 ) {
   __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStart(new Error());
 }
-          var ReactVersion = '18.3.0-canary-a9985529f-20231025';
+          var ReactVersion = '18.3.0-canary-8c8ee9ee6-20231026';
 
 // ATTENTION
 // When adding new symbols to this file,
