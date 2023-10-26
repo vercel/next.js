b9dd6c4ff6b025453504c6a5669094260c23ca1b
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-10-27 03:18:17 +1100

    Fix: Call cookies function from route to flag as dynamic (#57494)
    ### What?
    
    Move calling the `cookies` function up to the route
    
    ### Why?
    
    This flags the route as `dynamic`, meaning we don't need to explicitly declare `export const dynamic = 'force-dynamic'`
    
    ### How?
    
    Passing the returned `cookieStore` into Supabase helper functions, rather than the `cookies` function itself

--- examples/with-supabase/utils/supabase/server.ts
+++ examples/with-supabase/utils/supabase/server.ts
@@ -1,9 +1,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export const createClient = () => {
-  const cookieStore = cookies()
-
+export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
