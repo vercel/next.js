4bf19d9da8772f8a5e4f51fefc2d100646c55b3c
Tobias Koppers <tobias.koppers@googlemail.com>
2023-10-25 20:34:28 -0700

    normalize pattern correctly to avoid slow path in matching (vercel/turbo#6288)
    ### Description
    
    fixes a performance problem with read_matches
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->
    
    
    Closes WEB-1875

---
+++ crates/turbopack-tests/tests/snapshot/imports/order/output/crates_turbopack-tests_tests_snapshot_imports_order_input_index_6fef85.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/crates_turbopack-tests_tests_snapshot_imports_order_input_index_6fef85.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "output/crates_turbopack-tests_tests_snapshot_imports_order_input_index_6fef85.js",
+  "chunks": [
+    "output/crates_turbopack-tests_tests_snapshot_imports_order_input_17c023._.js"
+  ],
+  "source": "entry"
+});
