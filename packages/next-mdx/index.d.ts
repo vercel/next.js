42aa6d5760e4ea3abf3eb8082e81175225aa3582
Remco Haszing <remcohaszing@gmail.com>
2023-10-27 10:12:47 +0200

    Fix @next/mdx types
    It’s not possible to combine `export =` type definitions with ESM-style
    exports. To export types for a module that uses `module.exports =`, one
    needs to use a namespace.
    
    Also `Options` is now imported from `@mdx-js/loader`. The imports need
    to come from a direct dependency or peer dependency. Also
    `@mdx-js/loader` is more accurate here, because the options are passed
    into `@mdx-js/loader`.

--- packages/next-mdx/index.d.ts
+++ packages/next-mdx/index.d.ts
@@ -1,33 +1,35 @@
 /* eslint-disable */
 import { NextConfig } from 'next'
-import { CompileOptions } from '@mdx-js/mdx'
+import { Options } from '@mdx-js/loader'
 import { RuleSetConditionAbsolute } from 'webpack'
 
 type WithMDX = (config: NextConfig) => NextConfig
 
-export interface NextMDXOptions {
-  /**
-   * A webpack rule test to match files to treat as MDX.
-   *
-   * @default /\.mdx$/
-   * @example
-   * // Support both .md and .mdx files.
-   * /\.mdx?$/
-   */
-  extension?: RuleSetConditionAbsolute
+declare namespace nextMDX {
+  interface NextMDXOptions {
+    /**
+     * A webpack rule test to match files to treat as MDX.
+     *
+     * @default /\.mdx$/
+     * @example
+     * // Support both .md and .mdx files.
+     * /\.mdx?$/
+     */
+    extension?: RuleSetConditionAbsolute
 
-  /**
-   * The options to pass to MDX.
-   *
-   * @see https://mdxjs.com/packages/mdx/#api
-   */
-  options?: CompileOptions
+    /**
+     * The options to pass to MDX.
+     *
+     * @see https://mdxjs.com/packages/mdx/#api
+     */
+    options?: Options
+  }
 }
 
 /**
  * Use [MDX](https://github.com/mdx-js/mdx) with [Next.js](https://github.com/vercel/next.js)
  */
-declare function nextMDX(options?: NextMDXOptions): WithMDX
+declare function nextMDX(options?: nextMDX.NextMDXOptions): WithMDX
 
 export = nextMDX
 /* eslint=enable */
