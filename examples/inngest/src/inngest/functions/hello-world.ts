d08c38c0b34c5a2815aa614fc606a4616ea4faf0
joel <joelhooks@gmail.com>
2023-10-27 18:26:03 -0700

    bump the inngest version

--- examples/inngest/src/inngest/functions/hello-world.ts
+++ examples/inngest/src/inngest/functions/hello-world.ts
@@ -4,7 +4,7 @@ export const helloWorld = inngest.createFunction(
   { id: 'hello-world', name: 'Hello World' },
   { event: 'test/hello.world' },
   async ({ event, step }) => {
-    await step.sleep('1s')
+    await step.sleep('sleep for a second', '1s')
     return { event, body: event.data.message }
   }
 )
