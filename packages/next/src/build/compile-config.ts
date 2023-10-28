3075e2a805e6190ca0fab89fa62bab7281df8b6a
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-28 14:32:42 +0900

    chore: compile as mjs

--- packages/next/src/build/compile-config.ts
+++ packages/next/src/build/compile-config.ts
@@ -6,15 +6,18 @@ import { transformSync } from './swc'
 export function compileConfig(filePath: string): string {
   const content = readFileSync(filePath, 'utf-8')
   const compiled = transformSync(content, {
-    module: {
-      type: 'commonjs',
+    jsc: {
+      target: 'es5',
+      parser: {
+        syntax: 'typescript',
+      },
     },
   })
 
   const cwd = filePath.replace('/next.config.ts', '')
   mkdirSync(join(cwd, '.next'), { recursive: true })
 
-  const compiledFilePath = join(cwd, '.next', 'next.config.js')
+  const compiledFilePath = join(cwd, '.next', 'next.config.mjs')
   writeFileSync(compiledFilePath, compiled.code)
 
   return compiledFilePath
