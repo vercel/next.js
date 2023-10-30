f3a46fc309b99384831000adf2d62e0877ad1ff4
Josh Story <story@hey.com>
2023-10-30 10:52:25 -0700

    Update React from 8c8ee9ee6 to 0c6348758
    Successfully updated React from 8c8ee9ee6 to 0c6348758.
    
    - https://github.com/facebook/react/pull/27627
    - https://github.com/facebook/react/pull/27624
    - https://github.com/facebook/react/pull/27610
    - https://github.com/facebook/react/pull/27592
    - https://github.com/facebook/react/pull/27593
    - https://github.com/facebook/react/pull/27588
    - https://github.com/facebook/react/pull/27591
    - https://github.com/facebook/react/pull/27587

--- packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server.edge.development.js
+++ packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server.edge.development.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== "production") {
 var React = require("next/dist/compiled/react-experimental");
 var ReactDOM = require('react-dom');
 
-var ReactVersion = '18.3.0-experimental-8c8ee9ee6-20231026';
+var ReactVersion = '18.3.0-experimental-0c6348758-20231030';
 
 var ReactSharedInternals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
