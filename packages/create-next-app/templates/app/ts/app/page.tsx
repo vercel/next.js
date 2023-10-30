874ab2898b354b1972e3b40e2b22e4637b632432
Huy <laiquanghuy24122001@gmail.com>
2023-10-30 09:24:44 +0700

    change every Next.js 13 in sample app to Next.js 14

--- packages/create-next-app/templates/app/ts/app/page.tsx
+++ packages/create-next-app/templates/app/ts/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           <h2>
             Templates <span>-&gt;</span>
           </h2>
-          <p>Explore the Next.js 13 playground.</p>
+          <p>Explore the Next.js 14 playground.</p>
         </a>
 
         <a
