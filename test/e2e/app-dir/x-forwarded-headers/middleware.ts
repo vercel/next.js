vercel/next.js

commit f57eecde5e173d89b614936517cc798e95f1ad0f

Separate routing code from render servers (#52492)
This breaks out routing handling from `next-server`, `next-dev-server`,
and `base-server` so that these are only handling the "render" work and
eventually these will be deleted completely in favor of the bundling
work being done.

The `router` process and separate `render` processes are still
maintained here although will be investigated further in follow-up to
see if we can reduce the need for these.

We are also changing the `require-cache` IPC to a single call instead of
call per entry to reduce overhead and also de-dupes handling for
starting the server between the standalone server and normal server.

To maintain support for existing turbopack route resolving this
implements the new route resolving in place of the existing
`route-resolver` until the new nextturbo API is fully landed.

After these initial changes we should continue to eliminate non-render
related code from `next-server`, `base-server`, and `next-dev-server`.

packages/next/src/server/base-server.ts
@@ -1,7 +1,5 @@
 import type { __ApiPreviewProps } from './api-utils'
-import type { CustomRoutes } from '../lib/load-custom-routes'
 import type { DomainLocale } from './config'
-import type { RouterOptions } from './router'
 import type { FontManifest, FontConfig } from './font-utils'
 import type { LoadComponentsReturnType } from './load-components'
 import type { RouteMatchFn } from '../shared/lib/router/utils/route-matcher'
@@ -51,7 +49,6 @@ import {
   checkIsOnDemandRevalidate,
 } from './api-utils'
 import { setConfig } from '../shared/lib/runtime-config'
-import Router from './router'
 
 import { setRevalidateHeaders } from './send-payload/revalidate-headers'
 import { execOnce } from '../shared/lib/utils'
@@ -117,6 +114,7 @@ import {
   parsedUrlQueryToParams,
   type RouteMatch,
 } from './future/route-matches/route-match'
+import { normalizeLocalePath } from '../shared/lib/i18n/normalize-locale-path'
 
 export type FindComponentsResult = {
   components: LoadComponentsReturnType
@@ -268,9 +266,8 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   }
   protected serverOptions: ServerOptions
   private responseCache: ResponseCacheBase
-  protected router: Router
   protected appPathRoutes?: Record<string, string[]>
-  protected customRoutes: CustomRoutes
+  protected clientReferenceManifest?: ClientReferenceManifest
   protected nextFontManifest?: NextFontManifest
   public readonly hostname?: string
   public readonly port?: number
@@ -282,7 +279,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   protected abstract getAppPathsManifest(): PagesManifest | undefined
   protected abstract getBuildId(): string
 
-  protected abstract getFilesystemPaths(): Set<string>
   protected abstract findPageComponents(params: {
     pathname: string
     query: NextParsedUrlQuery
@@ -300,10 +296,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     parsedUrl: NextUrlWithParsedQuery
   ): void
   protected abstract getFallback(page: string): Promise<string>
-  protected abstract getCustomRoutes(): CustomRoutes
   protected abstract hasPage(pathname: string): Promise<boolean>
-
-  protected abstract generateRoutes(dev?: boolean): RouterOptions
 
   protected abstract sendRenderResult(
     req: BaseNextRequest,
@@ -334,11 +327,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     renderOpts: RenderOpts
   ): Promise<RenderResult>
 
-  protected abstract handleCompression(
-    req: BaseNextRequest,
-    res: BaseNextResponse
-  ): void
-
   protected abstract getIncrementalCache(options: {
     requestHeaders: Record<string, undefined | string | string[]>
     requestProtocol: 'http' | 'https'
@@ -358,7 +346,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   protected readonly handlers: RouteHandlerManager
   protected readonly i18nProvider?: I18NProvider
   protected readonly localeNormalizer?: LocaleRouteNormalizer
-  protected readonly isRouterWorker?: boolean
   protected readonly isRenderWorker?: boolean
 
   public constructor(options: ServerOptions) {
@@ -373,7 +360,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
       port,
     } = options
     this.serverOptions = options
-    this.isRouterWorker = options._routerWorker
     this.isRenderWorker = options._renderWorker
 
     this.dir =
@@ -473,6 +459,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
 
     this.pagesManifest = this.getPagesManifest()
     this.appPathsManifest = this.getAppPathsManifest()
+    this.appPathRoutes = this.getAppPathRoutes()
 
     // Configure the routes.
     const { matchers, handlers } = this.getRoutes()
@@ -483,12 +470,40 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     // because we use the `waitTillReady` promise below in `handleRequest` to
     // wait. Also we can't `await` in the constructor.
     matchers.reload()
-
-    this.customRoutes = this.getCustomRoutes()
-    this.router = new Router(this.generateRoutes(dev))
     this.setAssetPrefix(assetPrefix)
-
     this.responseCache = this.getResponseCache({ dev })
+  }
+
+  protected async normalizeNextData(
+    _req: BaseNextRequest,
+    _res: BaseNextResponse,
+    _parsedUrl: NextUrlWithParsedQuery
+  ): Promise<{ finished: boolean }> {
+    return { finished: false }
+  }
+
+  protected async handleNextImageRequest(
+    _req: BaseNextRequest,
+    _res: BaseNextResponse,
+    _parsedUrl: NextUrlWithParsedQuery
+  ): Promise<{ finished: boolean }> {
+    return { finished: false }
+  }
+
+  protected async handleCatchallRenderRequest(
+    _req: BaseNextRequest,
+    _res: BaseNextResponse,
+    _parsedUrl: NextUrlWithParsedQuery
+  ): Promise<{ finished: boolean }> {
+    return { finished: false }
+  }
+
+  protected async handleCatchallMiddlewareRequest(
+    _req: BaseNextRequest,
+    _res: BaseNextResponse,
+    _parsedUrl: NextUrlWithParsedQuery
+  ): Promise<{ finished: boolean }> {
+    return { finished: false }
   }
 
   protected getRoutes(): {
@@ -564,8 +579,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
           'http.method': method,
           'http.target': req.url,
         },
-        // We will fire this from the renderer worker
-        hideSpan: this.isRouterWorker,
       },
       async (span) =>
         this.handleRequestImpl(req, res, parsedUrl).finally(() => {
@@ -708,6 +721,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
         addRequestMeta(req, '_nextHadBasePath', true)
       }
 
+      // TODO: merge handling with x-invoke-path
       if (
         this.minimalMode &&
         typeof req.headers['x-matched-path'] === 'string'
@@ -787,7 +801,11 @@ export default abstract class Server<ServerOptions extends Options = Options> {
             page: srcPathname,
             i18n: this.nextConfig.i18n,
             basePath: this.nextConfig.basePath,
-            rewrites: this.customRoutes.rewrites,
+            rewrites: this.getRoutesManifest()?.rewrites || {
+              beforeFiles: [],
+              afterFiles: [],
+              fallback: [],
+            },
             caseSensitive: !!this.nextConfig.experimental.caseSensitiveRoutes,
           })
 
@@ -926,29 +944,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
         }
       }
 
-      addRequestMeta(req, '__nextHadTrailingSlash', pathnameInfo.trailingSlash)
-      addRequestMeta(req, '__nextIsLocaleDomain', Boolean(domainLocale))
-
-      if (pathnameInfo.locale) {
-        req.url = formatUrl(url)
-        addRequestMeta(req, '__nextStrippedLocale', true)
-      }
-
-      // If we aren't in minimal mode or there is no locale in the query
-      // string, add the locale to the query string.
-      if (!this.minimalMode || !parsedUrl.query.__nextLocale) {
-        // If the locale is in the pathname, add it to the query string.
-        if (pathnameInfo.locale) {
-          parsedUrl.query.__nextLocale = pathnameInfo.locale
-        }
-        // If the default locale is available, add it to the query string and
-        // mark it as inferred rather than implicit.
-        else if (defaultLocale) {
-          parsedUrl.query.__nextLocale = defaultLocale
-          parsedUrl.query.__nextInferredLocaleFromDefault = '1'
-        }
-      }
-
       if (
         // Edge runtime always has minimal mode enabled.
         process.env.NEXT_RUNTIME !== 'edge' &&
@@ -977,6 +972,174 @@ export default abstract class Server<ServerOptions extends Options = Options> {
             .body(redirect)
             .send()
         }
+      }
+
+      addRequestMeta(req, '__nextIsLocaleDomain', Boolean(domainLocale))
+
+      if (pathnameInfo.locale) {
+        req.url = formatUrl(url)
+        addRequestMeta(req, '__nextStrippedLocale', true)
+      }
+
+      // If we aren't in minimal mode or there is no locale in the query
+      // string, add the locale to the query string.
+      if (!this.minimalMode || !parsedUrl.query.__nextLocale) {
+        // If the locale is in the pathname, add it to the query string.
+        if (pathnameInfo.locale) {
+          parsedUrl.query.__nextLocale = pathnameInfo.locale
+        }
+        // If the default locale is available, add it to the query string and
+        // mark it as inferred rather than implicit.
+        else if (defaultLocale) {
+          parsedUrl.query.__nextLocale = defaultLocale
+          parsedUrl.query.__nextInferredLocaleFromDefault = '1'
+        }
+      }
+
+      // set incremental cache to request meta so it can
+      // be passed down for edge functions and the fetch disk
+      // cache can be leveraged locally
+      if (
+        !(this.serverOptions as any).webServerConfig &&
+        !getRequestMeta(req, '_nextIncrementalCache')
+      ) {
+        let protocol: 'http:' | 'https:' = 'https:'
+
+        try {
+          const parsedFullUrl = new URL(
+            getRequestMeta(req, '__NEXT_INIT_URL') || '/',
+            'http://n'
+          )
+          protocol = parsedFullUrl.protocol as 'https:' | 'http:'
+        } catch (_) {}
+
+        const incrementalCache = this.getIncrementalCache({
+          requestHeaders: Object.assign({}, req.headers),
+          requestProtocol: protocol.substring(0, protocol.length - 1) as
+            | 'http'
+            | 'https',
+        })
+        addRequestMeta(req, '_nextIncrementalCache', incrementalCache)
+        ;(globalThis as any).__incrementalCache = incrementalCache
+      }
+
+      // when x-invoke-path is specified we can short short circuit resolving
+      // we only honor this header if we are inside of a render worker to
+      // prevent external users coercing the routing path
+      const matchedPath = req.headers['x-invoke-path'] as string
+
+      if (
+        !(req.headers['x-matched-path'] && this.minimalMode) &&
+        process.env.NEXT_RUNTIME !== 'edge' &&
+        process.env.__NEXT_PRIVATE_RENDER_WORKER &&
+        matchedPath
+      ) {
+        if (req.headers['x-invoke-status']) {
+          const invokeQuery = req.headers['x-invoke-query']
+
+          if (typeof invokeQuery === 'string') {
+            Object.assign(
+              parsedUrl.query,
+              JSON.parse(decodeURIComponent(invokeQuery))
+            )
+          }
+
+          res.statusCode = Number(req.headers['x-invoke-status'])
+          let err = null
+
+          if (typeof req.headers['x-invoke-error'] === 'string') {
+            const invokeError = JSON.parse(
+              req.headers['x-invoke-error'] || '{}'
+            )
+            err = new Error(invokeError.message)
+          }
+
+          return this.renderError(err, req, res, '/_error', parsedUrl.query)
+        }
+
+        const parsedMatchedPath = new URL(matchedPath || '/', 'http://n')
+        const invokePathnameInfo = getNextPathnameInfo(
+          parsedMatchedPath.pathname,
+          {
+            nextConfig: this.nextConfig,
+            parseData: false,
+          }
+        )
+
+        if (invokePathnameInfo.locale) {
+          parsedUrl.query.__nextLocale = invokePathnameInfo.locale
+        }
+
+        if (parsedUrl.pathname !== parsedMatchedPath.pathname) {
+          parsedUrl.pathname = parsedMatchedPath.pathname
+          addRequestMeta(req, '_nextRewroteUrl', invokePathnameInfo.pathname)
+          addRequestMeta(req, '_nextDidRewrite', true)
+        }
+        const normalizeResult = normalizeLocalePath(
+          removePathPrefix(parsedUrl.pathname, this.nextConfig.basePath || ''),
+          this.nextConfig.i18n?.locales || []
+        )
+
+        if (normalizeResult.detectedLocale) {
+          parsedUrl.query.__nextLocale = normalizeResult.detectedLocale
+        }
+        parsedUrl.pathname = normalizeResult.pathname
+
+        for (const key of Object.keys(parsedUrl.query)) {
+          if (!key.startsWith('__next') && !key.startsWith('_next')) {
+            delete parsedUrl.query[key]
+          }
+        }
+        const invokeQuery = req.headers['x-invoke-query']
+
+        if (typeof invokeQuery === 'string') {
+          Object.assign(
+            parsedUrl.query,
+            JSON.parse(decodeURIComponent(invokeQuery))
+          )
+        }
+
+        if (parsedUrl.pathname.startsWith('/_next/image')) {
+          const imageResult = await this.handleNextImageRequest(
+            req,
+            res,
+            parsedUrl
+          )
+
+          if (imageResult.finished) {
+            return
+          }
+        }
+        const nextDataResult = await this.normalizeNextData(req, res, parsedUrl)
+
+        if (nextDataResult.finished) {
+          return
+        }
+        await this.handleCatchallRenderRequest(req, res, parsedUrl)
+        return
+      }
+
+      if (
+        process.env.NEXT_RUNTIME !== 'edge' &&
+        process.env.__NEXT_PRIVATE_RENDER_WORKER &&
+        req.headers['x-middleware-invoke']
+      ) {
+        const nextDataResult = await this.normalizeNextData(req, res, parsedUrl)
+
+        if (nextDataResult.finished) {
+          return
+        }
+        const result = await this.handleCatchallMiddlewareRequest(
+          req,
+          res,
+          parsedUrl
+        )
+        if (!result.finished) {
+          res.setHeader('x-middleware-next', '1')
+          res.body('')
+          res.send()
+        }
+        return
       }
 
       res.statusCode = 200
@@ -1036,15 +1199,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
   // Backwards compatibility
   protected async close(): Promise<void> {}
 
-  protected async _beforeCatchAllRender(
-    _req: BaseNextRequest,
-    _res: BaseNextResponse,
-    _params: Params,
-    _parsedUrl: UrlWithParsedQuery
-  ): Promise<boolean> {
-    return false
-  }
-
   protected getAppPathRoutes(): Record<string, string[]> {
     const appPathRoutes: Record<string, string[]> = {}
 
@@ -1073,49 +1227,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     res: BaseNextResponse,
     parsedUrl: UrlWithParsedQuery
   ): Promise<void> {
-    this.handleCompression(req, res)
-
-    // set incremental cache to request meta so it can
-    // be passed down for edge functions and the fetch disk
-    // cache can be leveraged locally
-    if (
-      !(this.serverOptions as any).webServerConfig &&
-      !getRequestMeta(req, '_nextIncrementalCache')
-    ) {
-      let protocol: 'http:' | 'https:' = 'https:'
-
-      try {
-        const parsedFullUrl = new URL(
-          getRequestMeta(req, '__NEXT_INIT_URL') || '/',
-          'http://n'
-        )
-        protocol = parsedFullUrl.protocol as 'https:' | 'http:'
-      } catch (_) {}
-
-      const incrementalCache = this.getIncrementalCache({
-        requestHeaders: Object.assign({}, req.headers),
-        requestProtocol: protocol.substring(0, protocol.length - 1) as
-          | 'http'
-          | 'https',
-      })
-      addRequestMeta(req, '_nextIncrementalCache', incrementalCache)
-      ;(globalThis as any).__incrementalCache = incrementalCache
-    }
-
-    try {
-      const matched = await this.router.execute(req, res, parsedUrl)
-      if (matched) {
-        return
-      }
-    } catch (err) {
-      if (err instanceof DecodeError || err instanceof NormalizeError) {
-        res.statusCode = 400
-        return this.renderError(null, req, res, '/_error', {})
-      }
-      throw err
-    }
-
-    await this.render404(req, res, parsedUrl)
+    await this.handleCatchallRenderRequest(req, res, parsedUrl)
   }
 
   private async pipe(
@@ -1229,11 +1341,6 @@ export default abstract class Server<ServerOptions extends Options = Options> {
         (this.hasStaticDir && req.url!.match(/^\/static\//)))
     ) {
       return this.handleRequest(req, res, parsedUrl)
-    }
-
-    // Custom server users can run `app.render()` which needs compression.
-    if (this.renderOpts.customServer) {
-      this.handleCompression(req, res)
     }
 
     if (isBlockedPage(pathname)) {
@@ -2263,6 +2370,28 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     )
   }
 
+  protected getMiddleware(): MiddlewareRoutingItem | undefined {
+    return undefined
+  }
+
+  protected getRoutesManifest():
+    | {
+        dynamicRoutes: {
+          page: string
+          regex: string
+          namedRegex?: string
+          routeKeys?: { [key: string]: string }
+        }
+        rewrites: {
+          beforeFiles: any[]
+          afterFiles: any[]
+          fallback: any[]
+        }
+      }
+    | undefined {
+    return undefined
+  }
+
   private async renderToResponseImpl(
     ctx: RequestContext
   ): Promise<ResponsePayload | null> {
@@ -2278,6 +2407,19 @@ export default abstract class Server<ServerOptions extends Options = Options> {
 
     try {
       for await (const match of this.matchers.matchAll(pathname, options)) {
+        // when a specific invoke-output is meant to be matched
+        // ensure a prior dynamic route/page doesn't take priority
+        const invokeOutput = ctx.req.headers['x-invoke-output']
+        if (
+          !this.minimalMode &&
+          this.isRenderWorker &&
+          typeof invokeOutput === 'string' &&
+          isDynamicRoute(invokeOutput || '') &&
+          invokeOutput !== match.definition.pathname
+        ) {
+          continue
+        }
+
         const result = await this.renderPageComponent(
           {
             ...ctx,
@@ -2366,7 +2508,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
     }
 
     if (
-      this.router.hasMiddleware &&
+      this.getMiddleware() &&
       !!ctx.req.headers['x-nextjs-data'] &&
       (!res.statusCode || res.statusCode === 200 || res.statusCode === 404)
     ) {

commit 1caa58087a428666bb14d40dbc159b9a768b075d

fix: correctly set `x-forwarded-*` in Middleware (#57815)
### What?

Follow-up of #56797

While working on this, I noticed that some logic around stripping internal headers was duplicated, so I did some cleanup too.

### Why?

In #56797 we set these headers, but it only affected Route Handlers, Middleware is still missing them, which is a regression introduced in #52492

(Related: https://github.com/vercel/next-learn/issues/252)

### How?

Move to set these headers up to `base-server.ts` so they are present in Middleware too.

> Note: All headers are set with `??=` to respect the original value if set (with other words, only add these headers if they aren't set yet)

Closes NEXT-
Fixes #52266

packages/next/src/server/base-server.ts
@@ -127,6 +127,7 @@ import getRouteFromAssetPath from '../shared/lib/router/utils/get-route-from-ass
 import { stripInternalHeaders } from './internal-utils'
 import { RSCPathnameNormalizer } from './future/normalizers/request/rsc'
 import { PostponedPathnameNormalizer } from './future/normalizers/request/postponed'
+import type { TLSSocket } from 'tls'
 
 export type FindComponentsResult = {
   components: LoadComponentsReturnType
@@ -893,6 +894,15 @@ export default abstract class Server<ServerOptions extends Options = Options> {
           delete req.headers[param.toString().toLowerCase()]
         }
       }
+
+      req.headers['x-forwarded-host'] ??= `${this.hostname}:${this.port}`
+      req.headers['x-forwarded-port'] ??= this.port?.toString()
+      const { originalRequest } = req as NodeNextRequest
+      req.headers['x-forwarded-proto'] ??= (originalRequest.socket as TLSSocket)
+        ?.encrypted
+        ? 'https'
+        : 'http'
+      req.headers['x-forwarded-for'] ??= originalRequest.socket?.remoteAddress
 
       this.attachRequestMeta(req, parsedUrl)
 

packages/next/src/server/internal-utils.ts
@@ -2,6 +2,7 @@ import type { IncomingHttpHeaders } from 'http'
 import type { NextParsedUrlQuery } from './request-meta'
 
 import { NEXT_RSC_UNION_QUERY } from '../client/components/app-router-headers'
+import { INTERNAL_HEADERS } from '../shared/lib/constants'
 
 const INTERNAL_QUERY_NAMES = [
   '__nextFallback',
@@ -38,19 +39,6 @@ export function stripInternalSearchParams<T extends string | URL>(
 
   return (isStringUrl ? instance.toString() : instance) as T
 }
-
-/**
- * Headers that are set by the Next.js server and should be stripped from the
- * request headers going to the user's application.
- */
-const INTERNAL_HEADERS = [
-  'x-invoke-path',
-  'x-invoke-status',
-  'x-invoke-error',
-  'x-invoke-query',
-  'x-invoke-output',
-  'x-middleware-invoke',
-] as const
 
 /**
  * Strip internal headers from the request headers.

packages/next/src/server/lib/mock-request.ts
@@ -37,13 +37,15 @@ export class MockedRequest extends Stream.Readable implements IncomingMessage {
   private bodyReadable?: Stream.Readable
 
   // If we don't actually have a socket, we'll just use a mock one that
-  // always returns false for the `encrypted` property.
+  // always returns false for the `encrypted` property and undefined for the
+  // `remoteAddress` property.
   public socket: Socket = new Proxy<TLSSocket>({} as TLSSocket, {
     get: (_target, prop) => {
-      if (prop !== 'encrypted') {
+      if (prop !== 'encrypted' && prop !== 'remoteAddress') {
         throw new Error('Method not implemented')
       }
 
+      if (prop === 'remoteAddress') return undefined
       // For this mock request, always ensure we just respond with the encrypted
       // set to false to ensure there's no odd leakages.
       return false

packages/next/src/server/lib/router-server.ts
@@ -32,7 +32,6 @@ import {
   PERMANENT_REDIRECT_STATUS,
 } from '../../shared/lib/constants'
 import { DevBundlerService } from './dev-bundler-service'
-import type { TLSSocket } from 'tls'
 
 const debug = setupDebug('next:router-server:main')
 
@@ -225,17 +224,6 @@ export async function initialize(opts: {
         'x-middleware-invoke': '',
         'x-invoke-path': invokePath,
         'x-invoke-query': encodeURIComponent(JSON.stringify(parsedUrl.query)),
-        'x-forwarded-host':
-          req.headers['x-forwarded-host'] ?? req.headers.host ?? opts.hostname,
-        'x-forwarded-port':
-          req.headers['x-forwarded-port'] ?? opts.port.toString(),
-        'x-forwarded-proto':
-          req.headers['x-forwarded-proto'] ??
-          (req.socket as TLSSocket).encrypted
-            ? 'https'
-            : 'http',
-        'x-forwarded-for':
-          req.headers['x-forwarded-for'] ?? req.socket.remoteAddress,
         ...(additionalInvokeHeaders || {}),
       }
       Object.assign(req.headers, invokeHeaders)

packages/next/src/server/lib/router-utils/proxy-request.ts
@@ -24,11 +24,13 @@ export async function proxyRequest(
     target,
     changeOrigin: true,
     ignorePath: true,
-    xfwd: true,
     ws: true,
     // we limit proxy requests to 30s by default, in development
     // we don't time out WebSocket requests to allow proxying
     proxyTimeout: proxyTimeout === null ? undefined : proxyTimeout || 30_000,
+    headers: {
+      'x-forwarded-host': req.headers.host || '',
+    },
   })
 
   await new Promise((proxyResolve, proxyReject) => {

packages/next/src/server/lib/router-utils/resolve-routes.ts
@@ -1,4 +1,3 @@
-import type { TLSSocket } from 'tls'
 import type { FsOutput } from './filesystem'
 import type { IncomingMessage, ServerResponse } from 'http'
 import type { NextConfigComplete } from '../../config-shared'
@@ -38,6 +37,7 @@ import {
   prepareDestination,
 } from '../../../shared/lib/router/utils/prepare-destination'
 import { createRequestResponseMocks } from '../mock-request'
+import type { TLSSocket } from 'tls'
 
 const debug = setupDebug('next:router-server:resolve-routes')
 

packages/next/src/server/next-server.ts
@@ -2,7 +2,6 @@ import './node-environment'
 import './require-hook'
 import './node-polyfill-crypto'
 
-import type { TLSSocket } from 'tls'
 import type { CacheFs } from '../shared/lib/utils'
 import {
   DecodeError,
@@ -41,7 +40,6 @@ import {
   SERVER_DIRECTORY,
   NEXT_FONT_MANIFEST,
   PHASE_PRODUCTION_BUILD,
-  INTERNAL_HEADERS,
 } from '../shared/lib/constants'
 import { findDir } from '../lib/find-pages-dir'
 import { NodeNextRequest, NodeNextResponse } from './base-http/node'
@@ -1616,10 +1614,6 @@ export default class NextNodeServer extends BaseServer {
     >
     let bubblingResult = false
 
-    for (const key of INTERNAL_HEADERS) {
-      delete req.headers[key]
-    }
-
     // Strip the internal headers.
     this.stripInternalHeaders(req)
 
@@ -1746,11 +1740,8 @@ export default class NextNodeServer extends BaseServer {
     parsedUrl: NextUrlWithParsedQuery,
     isUpgradeReq?: boolean
   ) {
-    const protocol =
-      ((req as NodeNextRequest).originalRequest?.socket as TLSSocket)
-        ?.encrypted || req.headers['x-forwarded-proto']?.includes('https')
-        ? 'https'
-        : 'http'
+    // Injected in base-server.ts
+    const protocol = req.headers['x-forwarded-proto'] as 'https' | 'http'
 
     // When there are hostname and port we build an absolute URL
     const initUrl =

packages/next/src/server/web/adapter.ts
@@ -11,11 +11,7 @@ import { waitUntilSymbol } from './spec-extension/fetch-event'
 import { NextURL } from './next-url'
 import { stripInternalSearchParams } from '../internal-utils'
 import { normalizeRscURL } from '../../shared/lib/router/utils/app-paths'
-import {
-  NEXT_ROUTER_PREFETCH,
-  NEXT_ROUTER_STATE_TREE,
-  RSC,
-} from '../../client/components/app-router-headers'
+import { FLIGHT_PARAMETERS } from '../../client/components/app-router-headers'
 import { NEXT_QUERY_PARAM_PREFIX } from '../../lib/constants'
 import { ensureInstrumentationRegistered } from './globals'
 import { RequestAsyncStorageWrapper } from '../async-storage/request-async-storage-wrapper'
@@ -46,12 +42,6 @@ class NextRequestHint extends NextRequest {
     throw new PageSignatureError({ page: this.sourcePage })
   }
 }
-
-const FLIGHT_PARAMETERS = [
-  [RSC],
-  [NEXT_ROUTER_STATE_TREE],
-  [NEXT_ROUTER_PREFETCH],
-] as const
 
 export type AdapterOptions = {
   handler: NextMiddleware

packages/next/src/shared/lib/constants.ts
@@ -10,11 +10,16 @@ export const COMPILER_NAMES = {
   edgeServer: 'edge-server',
 } as const
 
+/**
+ * Headers that are set by the Next.js server and should be stripped from the
+ * request headers going to the user's application.
+ */
 export const INTERNAL_HEADERS = [
-  'x-invoke-path',
-  'x-invoke-status',
   'x-invoke-error',
+  'x-invoke-output',
+  'x-invoke-path',
   'x-invoke-query',
+  'x-invoke-status',
   'x-middleware-invoke',
 ] as const
 

test/e2e/app-dir/x-forwarded-headers/middleware.ts
@@ -1,0 +1,14 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req: Request) {
+  return NextResponse.next({
+    headers: {
+      'middleware-x-forwarded-host':
+        req.headers.get('x-forwarded-host') ?? 'wrong',
+      'middleware-x-forwarded-port':
+        req.headers.get('x-forwarded-port') ?? 'wrong',
+      'middleware-x-forwarded-proto':
+        req.headers.get('x-forwarded-proto') ?? 'wrong',
+    },
+  })
+}

test/e2e/app-dir/x-forwarded-headers/x-forwarded-headers.test.ts
@@ -8,6 +8,10 @@ createNextDescribe('x-forwarded-headers', { files: __dirname }, ({ next }) => {
     expect(headers['x-forwarded-host']).toBe(url.host)
     expect(headers['x-forwarded-port']).toBe(url.port)
     expect(headers['x-forwarded-proto']).toBe(url.protocol.replace(':', ''))
-    console.log(headers)
+    expect(headers['middleware-x-forwarded-host']).toBe(url.host)
+    expect(headers['middleware-x-forwarded-port']).toBe(url.port)
+    expect(headers['middleware-x-forwarded-proto']).toBe(
+      url.protocol.replace(':', '')
+    )
   })
 })