4bd0f781113e3775da4ca602f6bc395610c0bda6
lijianan <574980606@qq.com>
2023-11-03 01:15:26 +0800

    docs: add information about workaround for Nextjs 14

--- examples/with-ant-design/app/AntdRegistry.tsx
+++ examples/with-ant-design/app/AntdRegistry.tsx
@@ -3,13 +3,11 @@
 import React from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs'
+// if you are using Nextjs 14, use below import instead. More info: https://github.com/ant-design/ant-design/issues/45567
+// import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs/lib'
 import type Entity from '@ant-design/cssinjs/es/Cache'
 
-interface StyledRegistryProps {
-  children: React.ReactNode
-}
-
-const StyledComponentsRegistry = ({ children }: StyledRegistryProps) => {
+const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
   const cache = React.useMemo<Entity>(() => createCache(), [])
   useServerInsertedHTML(() => (
     <style
