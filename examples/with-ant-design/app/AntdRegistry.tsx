b5251bac6b919ec3bc2648e22114781fd085e18c
lijianan <574980606@qq.com>
2023-11-04 03:58:42 +0800

    Update AntdRegistry.tsx

--- examples/with-ant-design/app/AntdRegistry.tsx
+++ examples/with-ant-design/app/AntdRegistry.tsx
@@ -2,9 +2,7 @@
 
 import React from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
-import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs'
-// if you are using Nextjs 14, use below import instead. More info: https://github.com/ant-design/ant-design/issues/45567
-// import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs/lib'
+import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs/lib'
 import type Entity from '@ant-design/cssinjs/es/Cache'
 
 const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
