03184dd8de2c0c810d7f9f8cf295097478857b0d
Tim Neutkens <tim@timneutkens.nl>
2023-11-04 19:43:19 +0100

    Bring back dependencies

--- test/development/basic/emotion-swc/emotion-swc.test.ts
+++ test/development/basic/emotion-swc/emotion-swc.test.ts
@@ -5,6 +5,11 @@ createNextDescribe(
   'emotion SWC option',
   {
     files: join(__dirname, 'fixture'),
+    dependencies: {
+      '@emotion/cache': '^10.0.29',
+      '@emotion/core': '^10.0.35',
+      '@emotion/styled': '^10.0.27',
+    },
   },
   ({ next }) => {
     it('should have styling from the css prop', async () => {
