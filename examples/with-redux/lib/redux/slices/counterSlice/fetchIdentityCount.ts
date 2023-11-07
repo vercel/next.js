0e74898cf2baa281cd45e6762b66c422bc0fef7b
Alex Ausch <aausch@users.noreply.github.com>
2023-11-07 16:45:01 -0500

    "Switch to using ${window.location.hostname} instead of localhost for the with-redux example for nextjs"

--- examples/with-redux/lib/redux/slices/counterSlice/fetchIdentityCount.ts
+++ examples/with-redux/lib/redux/slices/counterSlice/fetchIdentityCount.ts
@@ -1,7 +1,7 @@
 export const fetchIdentityCount = async (
   amount = 1
 ): Promise<{ data: number }> => {
-  const response = await fetch('http://localhost:3000/api/identity-count', {
+  const response = await fetch(`http://${$window.location.hostname}:3000/api/identity-count`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ amount }),
