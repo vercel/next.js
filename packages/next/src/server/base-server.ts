0278af4e031d19ec8b57942d5c40d593a35ce522
JJ Kasper <jj@jjsweb.site>
2023-11-06 20:33:16 -0800

    re-add other normalize

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -958,6 +958,15 @@ export default abstract class Server<ServerOptions extends Options = Options> {
             'http://localhost'
           )
 
+          if (this.normalizers.rsc.match(matchedPath)) {
+            matchedPath = this.normalizers.rsc.normalize(matchedPath, true)
+          } else if (this.normalizers.postponed.match(matchedPath)) {
+            matchedPath = this.normalizers.postponed.normalize(
+              matchedPath,
+              true
+            )
+          }
+
           const { pathname: urlPathname } = new URL(req.url, 'http://localhost')
 
           // For ISR  the URL is normalized to the prerenderPath so if
