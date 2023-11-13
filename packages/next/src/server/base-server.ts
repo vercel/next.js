afeeab6eeb6edd563a7391999f803fd163a2c70f
James Moore <jamesemoore@gmail.com>
2023-11-13 20:13:07 +0000

    Revert to behavior before #57815

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -911,7 +911,7 @@ export default abstract class Server<ServerOptions extends Options = Options> {
         )
       }
 
-      req.headers['x-forwarded-host'] ??= req.headers["host"] || `${this.hostname}:${this.port}`
+      req.headers['x-forwarded-host'] ??= req.headers["host"] ?? this.hostname
       req.headers['x-forwarded-port'] ??= this.port?.toString()
       const { originalRequest } = req as NodeNextRequest
       req.headers['x-forwarded-proto'] ??= (originalRequest.socket as TLSSocket)
