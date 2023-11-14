vercel/next.js

commit 0f945637b927965623d46895f3d42dc4d25230e1

perf: optimize flat-readdir to use `fs.opendir` (#52361)
This pull request replaces `readdir` with `opendir` and simplifies the
implementation.

Co-authored-by: Steven <steven@ceriously.com>
Co-authored-by: Tim Neutkens <tim@timneutkens.nl>

packages/next/src/lib/flat-readdir.ts
@@ -1,29 +1,23 @@
 import { join } from 'path'
-import { nonNullable } from './non-nullable'
-import { promises } from 'fs'
+import fs from 'fs/promises'
 
 export async function flatReaddir(dir: string, includes: RegExp[]) {
-  const dirents = await promises.readdir(dir, { withFileTypes: true })
-  const result = await Promise.all(
-    dirents.map(async (part) => {
-      const absolutePath = join(dir, part.name)
-      if (part.isSymbolicLink()) {
-        const stats = await promises.stat(absolutePath)
-        if (stats.isDirectory()) {
-          return null
-        }
-      }
+  const dirents = await fs.opendir(dir)
+  const result = []
 
-      if (
-        part.isDirectory() ||
-        !includes.some((include) => include.test(part.name))
-      ) {
-        return null
-      }
+  for await (const part of dirents) {
+    let shouldOmit =
+      part.isDirectory() || !includes.some((include) => include.test(part.name))
 
-      return absolutePath
-    })
-  )
+    if (part.isSymbolicLink()) {
+      const stats = await fs.stat(join(dir, part.name))
+      shouldOmit = stats.isDirectory()
+    }
 
-  return result.filter(nonNullable)
+    if (!shouldOmit) {
+      result.push(join(dir, part.name))
+    }
+  }
+
+  return result
 }

commit 5ecf39a62868ca1d985ae0527ae9a6e5c4bfa7a6

Fix `flatReaddir` ignoring include condition on symbolic files
This resolves an issue introduced in #52361 where `shouldOmit` is overwritten based on if it is a directory or not without considering include conditions.

In an environment where the the application being built is orchestrated and managed by bazel, all files are symbolic links and thus without this the file includes predicate checks aren't considered - and the build fails under certain conditions.

packages/next/src/lib/flat-readdir.ts
@@ -9,7 +9,7 @@ export async function flatReaddir(dir: string, includes: RegExp[]) {
     let shouldOmit =
       part.isDirectory() || !includes.some((include) => include.test(part.name))
 
-    if (part.isSymbolicLink()) {
+    if (!shouldOmit && part.isSymbolicLink()) {
       const stats = await fs.stat(join(dir, part.name))
       shouldOmit = stats.isDirectory()
     }