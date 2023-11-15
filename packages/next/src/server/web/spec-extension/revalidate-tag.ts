30c3357d445f03b20aa2ee2c3db1d24786c48cfd
Zack Tanner <zacktanner@gmail.com>
2023-11-15 07:22:52 -0800

    use staticGenerationBailout

--- packages/next/src/server/web/spec-extension/revalidate-tag.ts
+++ packages/next/src/server/web/spec-extension/revalidate-tag.ts
@@ -2,7 +2,7 @@ import type {
   StaticGenerationAsyncStorage,
   StaticGenerationStore,
 } from '../../../client/components/static-generation-async-storage.external'
-import { DynamicServerError } from '../../../client/components/hooks-server-context'
+import { staticGenerationBailout } from '../../app-render/entry-base'
 
 export function revalidateTag(tag: string) {
   const staticGenerationAsyncStorage = (
@@ -20,15 +20,7 @@ export function revalidateTag(tag: string) {
 
   // a route that makes use of revalidation APIs should be considered dynamic
   // as otherwise it would be impossible to revalidate
-  if (store.isStaticGeneration) {
-    const dynamicUsageReason = `revalidateTag ${tag}`
-    const err = new DynamicServerError(dynamicUsageReason)
-    store.dynamicUsageDescription = `revalidateTag ${tag}`
-    store.dynamicUsageStack = err.stack
-    store.revalidate = 0
-
-    throw err
-  }
+  staticGenerationBailout(`revalidateTag ${tag}`)
 
   if (!store.revalidatedTags) {
     store.revalidatedTags = []
