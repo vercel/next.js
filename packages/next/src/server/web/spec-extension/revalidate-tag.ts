9e764b104e67cc557293ba09193ff4cd22b728c5
Zack Tanner <zacktanner@gmail.com>
2023-11-14 20:14:04 -0800

    revalidate APIs should make route handlers dynamic

--- packages/next/src/server/web/spec-extension/revalidate-tag.ts
+++ packages/next/src/server/web/spec-extension/revalidate-tag.ts
@@ -2,6 +2,7 @@ import type {
   StaticGenerationAsyncStorage,
   StaticGenerationStore,
 } from '../../../client/components/static-generation-async-storage.external'
+import { DynamicServerError } from '../../../client/components/hooks-server-context'
 
 export function revalidateTag(tag: string) {
   const staticGenerationAsyncStorage = (
@@ -16,6 +17,19 @@ export function revalidateTag(tag: string) {
       `Invariant: static generation store missing in revalidateTag ${tag}`
     )
   }
+
+  // a route that makes use of revalidation APIs should be considered dynamic
+  // as otherwise it would be impossible to revalidate
+  if (store.isStaticGeneration) {
+    const dynamicUsageReason = `revalidateTag ${tag}`
+    const err = new DynamicServerError(dynamicUsageReason)
+    store.dynamicUsageDescription = `revalidateTag ${tag}`
+    store.dynamicUsageStack = err.stack
+    store.revalidate = 0
+
+    throw err
+  }
+
   if (!store.revalidatedTags) {
     store.revalidatedTags = []
   }
---
+++ test/e2e/app-dir/revalidate-dynamic/revalidate-dynamic.test.ts
@@ -0,0 +1,37 @@
+import { createNextDescribe } from '../../../lib/e2e-utils'
+
+createNextDescribe(
+  'app-dir revalidate-dynamic',
+  {
+    files: __dirname,
+  },
+  ({ next, isNextStart }) => {
+    if (isNextStart) {
+      it('should correctly mark a route handler that uses revalidateTag as dynamic', async () => {
+        expect(next.cliOutput).toContain('λ /api/revalidate')
+      })
+    }
+
+    describe('revalidatePath', () => {
+      it.each(['/api/revalidate-path', '/api/revalidate-tag'])(
+        `should revalidate the data with %s`,
+        async (path) => {
+          const browser = await next.browser('/')
+          const randomNumber = await browser.elementById('data-value').text()
+          await browser.refresh()
+          const randomNumber2 = await browser.elementById('data-value').text()
+
+          expect(randomNumber).toEqual(randomNumber2)
+
+          const revalidateRes = await next.fetch(path)
+          expect((await revalidateRes.json()).revalidated).toBe(true)
+
+          await browser.refresh()
+
+          const randomNumber3 = await browser.elementById('data-value').text()
+          expect(randomNumber).not.toEqual(randomNumber3)
+        }
+      )
+    })
+  }
+)
