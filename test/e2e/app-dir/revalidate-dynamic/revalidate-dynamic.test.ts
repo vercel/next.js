e4de115c0d03af8a24244727e2d05922604a7f63
Zack Tanner <zacktanner@gmail.com>
2023-11-14 20:20:24 -0800

    test tweak

--- test/e2e/app-dir/revalidate-dynamic/revalidate-dynamic.test.ts
+++ test/e2e/app-dir/revalidate-dynamic/revalidate-dynamic.test.ts
@@ -12,26 +12,24 @@ createNextDescribe(
       })
     }
 
-    describe('revalidatePath', () => {
-      it.each(['/api/revalidate-path', '/api/revalidate-tag'])(
-        `should revalidate the data with %s`,
-        async (path) => {
-          const browser = await next.browser('/')
-          const randomNumber = await browser.elementById('data-value').text()
-          await browser.refresh()
-          const randomNumber2 = await browser.elementById('data-value').text()
+    it.each(['/api/revalidate-path', '/api/revalidate-tag'])(
+      `should revalidate the data with %s`,
+      async (path) => {
+        const browser = await next.browser('/')
+        const randomNumber = await browser.elementById('data-value').text()
+        await browser.refresh()
+        const randomNumber2 = await browser.elementById('data-value').text()
 
-          expect(randomNumber).toEqual(randomNumber2)
+        expect(randomNumber).toEqual(randomNumber2)
 
-          const revalidateRes = await next.fetch(path)
-          expect((await revalidateRes.json()).revalidated).toBe(true)
+        const revalidateRes = await next.fetch(path)
+        expect((await revalidateRes.json()).revalidated).toBe(true)
 
-          await browser.refresh()
+        await browser.refresh()
 
-          const randomNumber3 = await browser.elementById('data-value').text()
-          expect(randomNumber).not.toEqual(randomNumber3)
-        }
-      )
-    })
+        const randomNumber3 = await browser.elementById('data-value').text()
+        expect(randomNumber).not.toEqual(randomNumber3)
+      }
+    )
   }
 )
