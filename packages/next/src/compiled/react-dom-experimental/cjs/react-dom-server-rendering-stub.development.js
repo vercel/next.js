e6d4aaa59d52e7b87fcbc02654a06536b1e8b4bb
Shu Ding <g@shud.in>
2023-11-15 23:35:45 +0900

    upgrade react

--- packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
+++ packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
@@ -55,7 +55,7 @@ function printWarning(level, format, args) {
   }
 }
 
-var ReactVersion = '18.3.0-experimental-0e352ea01-20231109';
+var ReactVersion = '18.3.0-experimental-593ecee66-20231114';
 
 var Internals = {
   usingClientEntryPoint: false,
