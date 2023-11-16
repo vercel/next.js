2a4b3e88e71beb170962af96f37fb814448f9f76
Josh Story <story@hey.com>
2023-11-16 14:06:57 -0800

    Updated React from 593ecee66 to 2c338b16f.
    - https://github.com/facebook/react/pull/27692
    - https://github.com/facebook/react/pull/27712
    - https://github.com/facebook/react/pull/27659

--- packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
+++ packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server-rendering-stub.development.js
@@ -55,7 +55,7 @@ function printWarning(level, format, args) {
   }
 }
 
-var ReactVersion = '18.3.0-experimental-593ecee66-20231114';
+var ReactVersion = '18.3.0-experimental-2c338b16f-20231116';
 
 var Internals = {
   usingClientEntryPoint: false,
