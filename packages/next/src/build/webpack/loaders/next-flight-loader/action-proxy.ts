3f3cdfb52ef36fefe63838fccfc8cff3765c29d9
Shu Ding <g@shud.in>
2023-11-21 00:41:16 +0900

    fix failing case

--- packages/next/src/build/webpack/loaders/next-flight-loader/action-proxy.ts
+++ packages/next/src/build/webpack/loaders/next-flight-loader/action-proxy.ts
@@ -2,5 +2,5 @@
 import { registerServerReference } from 'react-server-dom-webpack/server.edge'
 
 export function createActionProxy(id: string, action: any) {
-  return registerServerReference(action, id)
+  return registerServerReference(action, id, null)
 }
