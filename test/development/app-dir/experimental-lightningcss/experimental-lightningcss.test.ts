036ed41fe6d89bb56b09fc372a3e1ff564648724
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-20 17:11:01 +0900

    lint

--- test/development/app-dir/experimental-lightningcss/experimental-lightningcss.test.ts
+++ test/development/app-dir/experimental-lightningcss/experimental-lightningcss.test.ts
@@ -7,13 +7,15 @@ createNextDescribe(
   },
   ({ next }) => {
     // Recommended for tests that check HTML. Cheerio is a HTML parser that has a jQuery like API.
-    ;(process.env.TURBOPACK ? it : it.skip)(
-      'should support css modules',
-      async () => {
-        const $ = await next.render$('/')
-        expect($('p').text()).toBe('hello world')
-        // swc_css does not include `-module` in the class name, while lightningcss does.
-        expect($('p').attr('class')).toBe('style-module__hlQ3RG__blue')
+    ;(process.env.TURBOPACK ? describe : describe.skip)(
+      'with it enabled',
+      () => {
+        it('should support css modules', async () => {
+          const $ = await next.render$('/')
+          expect($('p').text()).toBe('hello world')
+          // swc_css does not include `-module` in the class name, while lightningcss does.
+          expect($('p').attr('class')).toBe('style-module__hlQ3RG__blue')
+        })
       }
     )
   }
