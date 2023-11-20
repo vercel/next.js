6e14d5d3a003c5287edfa73addddfa10786e45f4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-20 12:01:48 +0900

    WIP: test

--- test/development/app-dir/experimental-lightningcss/app/page.tsx
+++ test/development/app-dir/experimental-lightningcss/app/page.tsx
@@ -1,3 +1,5 @@
+import styles from './style.module.css'
+
 export default function Page() {
-  return <p>hello world</p>
+  return <p className={styles.blue}>hello world</p>
 }
--- test/development/app-dir/experimental-lightningcss/experimental-lightningcss.test.ts
+++ test/development/app-dir/experimental-lightningcss/experimental-lightningcss.test.ts
@@ -10,25 +10,9 @@ createNextDescribe(
     it('should work using cheerio', async () => {
       const $ = await next.render$('/')
       expect($('p').text()).toBe('hello world')
-    })
-
-    // Recommended for tests that need a full browser
-    it('should work using browser', async () => {
-      const browser = await next.browser('/')
-      expect(await browser.elementByCss('p').text()).toBe('hello world')
-    })
-
-    // In case you need the full HTML. Can also use $.html() with cheerio.
-    it('should work with html', async () => {
-      const html = await next.render('/')
-      expect(html).toContain('hello world')
-    })
-
-    // In case you need to test the response object
-    it('should work with fetch', async () => {
-      const res = await next.fetch('/')
-      const html = await res.text()
-      expect(html).toContain('hello world')
+      expect($('p').html()).toMatchInlineSnapshot(`"hello world"`)
+      expect($('p').attr('class')).toMatchInlineSnapshot(`"style_blue__2oYLK"`)
+      expect($('p').css('color')).toBe('red')
     })
   }
 )
