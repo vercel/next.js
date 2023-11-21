aadbd4e7b2735d1765c7233fcbb39dc67aa79b28
Tobias Koppers <tobias.koppers@googlemail.com>
2023-11-21 20:49:35 +0000

    trigger hot update after sync event

--- packages/next/src/client/components/react-dev-overlay/hot-reloader-client.tsx
+++ packages/next/src/client/components/react-dev-overlay/hot-reloader-client.tsx
@@ -338,7 +338,9 @@ function processMessage(
         })
       )
 
-      if (!process.env.TURBOPACK) {
+      if (process.env.TURBOPACK) {
+        handleHotUpdate()
+      } else {
         const isHotUpdate =
           obj.action !== HMR_ACTIONS_SENT_TO_BROWSER.SYNC &&
           (!window.__NEXT_DATA__ || window.__NEXT_DATA__.page !== '/_error') &&
