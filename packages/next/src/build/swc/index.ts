50c59116f7e83d5b19345480e8e56a7dce52e331
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-11-23 07:57:20 +0900

    WIP

--- packages/next/src/build/swc/index.ts
+++ packages/next/src/build/swc/index.ts
@@ -1463,6 +1463,8 @@ export async function parse(src: string, options: any): Promise<any> {
     .then((astStr: any) => JSON.parse(astStr))
 }
 
+export async function transformCss() {}
+
 export function getBinaryMetadata() {
   let bindings
   try {
