95ecfd67affbb4af5f494ba00c5c73fa26c64bd4
hrmny <github.leah@hrmny.sh>
2023-11-28 15:31:17 +0100

    feat(turbopack): support loading WebAssembly in the edge runtime

--- packages/next-swc/crates/next-api/src/pages.rs
+++ packages/next-swc/crates/next-api/src/pages.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use anyhow::{bail, Context, Result};
 use indexmap::IndexMap;
 use next_core::{
-    create_page_loader_entry_module, get_asset_path_from_pathname,
+    all_assets_from_entries, create_page_loader_entry_module, get_asset_path_from_pathname,
     get_edge_resolve_options_context,
     mode::NextMode,
     next_client::{
@@ -70,6 +70,7 @@ use crate::{
         collect_chunk_group, collect_evaluated_chunk_group, collect_next_dynamic_imports,
         DynamicImportedChunks,
     },
+    middleware::{get_js_paths_from_root, get_wasm_paths_from_root, wasm_paths_to_bindings},
     project::Project,
     route::{Endpoint, Route, Routes, WrittenEndpoint},
     server_paths::all_server_paths,
@@ -992,22 +993,17 @@ impl PageEndpoint {
                     "server/middleware-react-loadable-manifest.js".to_string(),
                     "server/next-font-manifest.js".to_string(),
                 ];
+                let mut wasm_paths_from_root = vec![];
 
                 let node_root_value = node_root.await?;
-                let middleware_paths_from_root: Vec<String> = files_value
-                    .iter()
-                    .map(move |&file| {
-                        let node_root_value = node_root_value.clone();
-                        async move {
-                            Ok(node_root_value
-                                .get_path_to(&*file.ident().path().await?)
-                                .map(|path| path.to_string()))
-                        }
-                    })
-                    .try_flat_join()
-                    .await?;
 
-                file_paths_from_root.extend(middleware_paths_from_root);
+                file_paths_from_root
+                    .extend(get_js_paths_from_root(&node_root_value, &files_value).await?);
+
+                let all_output_assets = all_assets_from_entries(files).await?;
+
+                wasm_paths_from_root
+                    .extend(get_wasm_paths_from_root(&node_root_value, &all_output_assets).await?);
 
                 let pathname = this.pathname.await?;
                 let named_regex = get_named_middleware_regex(&pathname);
@@ -1019,6 +1015,7 @@ impl PageEndpoint {
                 let original_name = this.original_name.await?;
                 let edge_function_definition = EdgeFunctionDefinition {
                     files: file_paths_from_root,
+                    wasm: wasm_paths_to_bindings(wasm_paths_from_root),
                     name: pathname.to_string(),
                     page: original_name.to_string(),
                     regions: None,
