b122be57637d6140c360b076792c78e9b193d089
Will Binns-Smith <wbinnssmith@gmail.com>
2023-12-04 12:01:07 -0800

    Turbopack css: treat `composes` and `@import` as urls when resolving (vercel/turbo#6666)
    This allows for bare specifiers to be resolved first as relative, then
    using a node-style resolution
    
    Test Plan: Added snapshot tests.
    
    
    Closes PACK-2064

---
+++ crates/turbopack-tests/tests/snapshot/cssmodules/relative-uri-import/output/79fb1_turbopack-tests_tests_snapshot_cssmodules_relative-uri-import_input_f065a6._.js
@@ -0,0 +1,27 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/79fb1_turbopack-tests_tests_snapshot_cssmodules_relative-uri-import_input_f065a6._.js", {
+
+"[project]/crates/turbopack-tests/tests/snapshot/cssmodules/relative-uri-import/input/other.module.css [test] (css module)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, p: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname }) => (() => {
+
+__turbopack_export_value__({
+  "foo": "foo__other__4a675be2",
+});
+
+})()),
+"[project]/crates/turbopack-tests/tests/snapshot/cssmodules/relative-uri-import/input/index.module.css [test] (css module)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, p: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname }) => (() => {
+
+__turbopack_export_value__({
+  "bar": "bar__index__c520c638" + " " + __turbopack_import__("[project]/crates/turbopack-tests/tests/snapshot/cssmodules/relative-uri-import/input/other.module.css [test] (css module)")["foo"],
+});
+
+})()),
+"[project]/crates/turbopack-tests/tests/snapshot/cssmodules/relative-uri-import/input/index.js [test] (ecmascript)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, p: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname, k: __turbopack_refresh__ }) => (() => {
+"use strict";
+
+var __TURBOPACK__imported__module__$5b$project$5d2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$cssmodules$2f$relative$2d$uri$2d$import$2f$input$2f$index$2e$module$2e$css__$5b$test$5d$__$28$css__module$29$__ = __turbopack_import__("[project]/crates/turbopack-tests/tests/snapshot/cssmodules/relative-uri-import/input/index.module.css [test] (css module)");
+"__TURBOPACK__ecmascript__hoisting__location__";
+;
+
+})()),
+}]);
+
+//# sourceMappingURL=79fb1_turbopack-tests_tests_snapshot_cssmodules_relative-uri-import_input_f065a6._.js.map
