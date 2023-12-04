b089529caff6a610b549e5a26543801d85439d94
Tim Neutkens <tim@timneutkens.nl>
2023-12-04 05:13:28 +0100

    Failing test for 59195 (#59210)
    Added a failing test based on the reproduction in #59195
    
    <!-- Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change(s) that you're making:
    
    ## For Contributors
    
    ### Improving Documentation
    
    - Run `pnpm prettier-fix` to fix formatting issues before opening the
    PR.
    - Read the Docs Contribution Guide to ensure your contribution follows
    the docs guidelines:
    https://nextjs.org/docs/community/contribution-guide
    
    ### Adding or Updating Examples
    
    - The "examples guidelines" are followed from our contributing doc
    https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
    - Make sure the linting passes by running `pnpm build && pnpm lint`. See
    https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md
    
    ### Fixing a bug
    
    - Related issues linked using `fixes #number`
    - Tests added. See:
    https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    ### Adding a feature
    
    - Implements an existing feature request or RFC. Make sure the feature
    request has been accepted for implementation before opening a PR. (A
    discussion must be opened, see
    https://github.com/vercel/next.js/discussions/new?category=ideas)
    - Related issues/discussions are linked using `fixes #number`
    - e2e tests added
    (https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs)
    - Documentation added
    - Telemetry added. In case of a feature if it's used or not.
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    
    ## For Maintainers
    
    - Minimal description (aim for explaining to someone not on the team to
    understand the PR)
    - When linking to a Slack thread, you might want to share details of the
    conclusion
    - Link both the Linear (Fixes NEXT-xxx) and the GitHub issues
    - Add review comments if necessary to explain to the reviewer the logic
    behind a change
    
    ### What?
    
    ### Why?
    
    ### How?
    
    Closes NEXT-
    Fixes #
    
    -->
    
    
    Closes NEXT-1781

---
+++ test/e2e/react-dnd-compile/react-dnd-compile.test.ts
@@ -0,0 +1,26 @@
+import { createNextDescribe } from 'e2e-utils'
+
+createNextDescribe(
+  'react-dnd-compile',
+  {
+    files: __dirname,
+    dependencies: {
+      '@react-dnd/asap': '^5.0.1',
+      '@react-dnd/invariant': '^4.0.2',
+      'dnd-core': '14.0.1',
+      'react-dnd': '^16.0.1',
+      'react-dnd-html5-backend': '^16.0.1',
+    },
+  },
+  ({ next }) => {
+    it('should work', async () => {
+      const $ = await next.render$('/')
+      expect($('#index-page-title').text()).toBe('Hello, Next.js!')
+    })
+
+    it('should work on react-dnd import page', async () => {
+      const $ = await next.render$('/oom')
+      expect($('#oom-page-title').text()).toBe('Hello, Next.js!')
+    })
+  }
+)
