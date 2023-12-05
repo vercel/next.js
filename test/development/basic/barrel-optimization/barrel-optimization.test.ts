vercel/next.js

commit 305bb015060e82be3e6ae59a3d063c11a3e207f9

Refactor swc options (#57369)
* Rename swc option `is_server` to `is_server_compiler`
* Rename swc plugin `config.is_server` to `config.is_react_server_layer`
* Remove unused `bundle_target`, which could be covered by composing `is_server_compiler` and `is_react_server_layer`

packages/next/src/build/webpack-config.ts
@@ -390,7 +390,6 @@ export default async function getBaseWebpackConfig(
         pagesDir,
         cwd: dir,
         development: dev,
-        hasServerComponents: hasAppDir,
         hasReactRefresh: dev && isClient,
         hasJsxRuntime: true,
       },
@@ -398,12 +397,7 @@ export default async function getBaseWebpackConfig(
   }
 
   let swcTraceProfilingInitialized = false
-  const getSwcLoader = (
-    extraOptions: Partial<SWCLoaderOptions> & {
-      bundleTarget: SWCLoaderOptions['bundleTarget']
-      isServerLayer: SWCLoaderOptions['isServerLayer']
-    }
-  ) => {
+  const getSwcLoader = (extraOptions: Partial<SWCLoaderOptions>) => {
     if (
       config?.experimental?.swcTraceProfiling &&
       !swcTraceProfilingInitialized
@@ -426,7 +420,6 @@ export default async function getBaseWebpackConfig(
         pagesDir,
         appDir,
         hasReactRefresh: dev && isClient,
-        hasServerComponents: true,
         nextConfig: config,
         jsConfig,
         supportedBrowsers,
@@ -438,28 +431,38 @@ export default async function getBaseWebpackConfig(
 
   const defaultLoaders = {
     babel: useSWCLoader
-      ? getSwcLoader({ bundleTarget: 'client', isServerLayer: false })
+      ? getSwcLoader({
+          serverComponents: true,
+          isReactServerLayer: false,
+        })
       : getBabelLoader(),
   }
 
   const swcLoaderForServerLayer = hasAppDir
     ? useSWCLoader
-      ? [getSwcLoader({ isServerLayer: true, bundleTarget: 'server' })]
+      ? [
+          getSwcLoader({
+            isReactServerLayer: true,
+            serverComponents: true,
+          }),
+        ]
       : // When using Babel, we will have to add the SWC loader
         // as an additional pass to handle RSC correctly.
         // This will cause some performance overhead but
         // acceptable as Babel will not be recommended.
         [
-          getSwcLoader({ isServerLayer: true, bundleTarget: 'server' }),
+          getSwcLoader({
+            isReactServerLayer: true,
+            serverComponents: true,
+          }),
           getBabelLoader(),
         ]
     : []
 
   const swcLoaderForMiddlewareLayer = useSWCLoader
     ? getSwcLoader({
-        isServerLayer: false,
-        hasServerComponents: false,
-        bundleTarget: 'default',
+        serverComponents: false,
+        isReactServerLayer: false,
       })
     : // When using Babel, we will have to use SWC to do the optimization
       // for middleware to tree shake the unused default optimized imports like "next/server".
@@ -467,9 +470,8 @@ export default async function getBaseWebpackConfig(
       // acceptable as Babel will not be recommended.
       [
         getSwcLoader({
-          isServerLayer: false,
-          hasServerComponents: false,
-          bundleTarget: 'default',
+          serverComponents: false,
+          isReactServerLayer: false,
         }),
         getBabelLoader(),
       ]
@@ -492,9 +494,8 @@ export default async function getBaseWebpackConfig(
       ? useSWCLoader
         ? [
             getSwcLoader({
-              hasServerComponents: hasAppDir,
-              isServerLayer: false,
-              bundleTarget: 'client',
+              isReactServerLayer: false,
+              serverComponents: true,
             }),
           ]
         : // When using Babel, we will have to add the SWC loader
@@ -503,8 +504,8 @@ export default async function getBaseWebpackConfig(
           // acceptable as Babel will not be recommended.
           [
             getSwcLoader({
-              isServerLayer: false,
-              bundleTarget: 'client',
+              isReactServerLayer: false,
+              serverComponents: false,
             }),
             getBabelLoader(),
           ]
@@ -517,9 +518,8 @@ export default async function getBaseWebpackConfig(
   const loaderForAPIRoutes =
     hasAppDir && useSWCLoader
       ? getSwcLoader({
-          isServerLayer: false,
-          bundleTarget: 'default',
-          hasServerComponents: false,
+          serverComponents: false,
+          isReactServerLayer: false,
         })
       : defaultLoaders.babel
 
@@ -1671,12 +1671,11 @@ export default async function getBaseWebpackConfig(
         ? (() => {
             // Even though require.cache is server only we have to clear assets from both compilations
             // This is because the client compilation generates the build manifest that's used on the server side
-            const {
-              NextJsRequireCacheHotReloader,
-            } = require('./webpack/plugins/nextjs-require-cache-hot-reloader')
-            const devPlugins = [
+            const { NextJsRequireCacheHotReloader } =
+              require('./webpack/plugins/nextjs-require-cache-hot-reloader') as typeof import('./webpack/plugins/nextjs-require-cache-hot-reloader')
+            const devPlugins: any[] = [
               new NextJsRequireCacheHotReloader({
-                hasServerComponents: hasAppDir,
+                serverComponents: hasAppDir,
               }),
             ]
 

packages/next-swc/crates/core/src/lib.rs
@@ -47,8 +47,7 @@ use turbopack_binding::swc::{
             SyntaxContext,
         },
         ecma::{
-            ast::EsVersion, atoms::JsWord, parser::parse_file_as_module,
-            transforms::base::pass::noop, visit::Fold,
+            ast::EsVersion, parser::parse_file_as_module, transforms::base::pass::noop, visit::Fold,
         },
     },
     custom_transform::modularize_imports,
@@ -94,10 +93,7 @@ pub struct TransformOptions {
     pub is_development: bool,
 
     #[serde(default)]
-    pub is_server: bool,
-
-    #[serde(default)]
-    pub bundle_target: JsWord,
+    pub is_server_compiler: bool,
 
     #[serde(default)]
     pub server_components: Option<react_server_components::Config>,
@@ -198,7 +194,6 @@ where
                     config.clone(),
                     comments.clone(),
                     opts.app_dir.clone(),
-                    opts.bundle_target.clone()
                 )),
             _ => Either::Right(noop()),
         },
@@ -230,7 +225,7 @@ where
         amp_attributes::amp_attributes(),
         next_dynamic(
             opts.is_development,
-            opts.is_server,
+            opts.is_server_compiler,
             match &opts.server_components {
                 Some(config) if config.truthy() => match config {
                     // Always enable the Server Components mode for both

packages/next-swc/crates/core/src/react_server_components.rs
@@ -37,11 +37,11 @@ impl Config {
 #[derive(Clone, Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Options {
-    pub is_server: bool,
+    pub is_react_server_layer: bool,
 }
 
 struct ReactServerComponents<C: Comments> {
-    is_server: bool,
+    is_react_server_layer: bool,
     filepath: String,
     app_dir: Option<PathBuf>,
     comments: C,
@@ -50,7 +50,6 @@ struct ReactServerComponents<C: Comments> {
     invalid_client_imports: Vec<JsWord>,
     invalid_server_react_apis: Vec<JsWord>,
     invalid_server_react_dom_apis: Vec<JsWord>,
-    bundle_target: String,
 }
 
 struct ModuleImports {
@@ -66,11 +65,11 @@ impl<C: Comments> VisitMut for ReactServerComponents<C> {
             self.collect_top_level_directives_and_imports(module);
         let is_cjs = contains_cjs(module);
 
-        if self.is_server {
+        if self.is_react_server_layer {
             if is_client_entry {
                 self.to_module_ref(module, is_cjs);
                 return;
-            } else if self.bundle_target == "server" {
+            } else {
                 // Only assert server graph if file's bundle target is "server", e.g.
                 // * server components pages
                 // * pages bundles on SSR layer
@@ -83,7 +82,7 @@ impl<C: Comments> VisitMut for ReactServerComponents<C> {
             // and bundle target is "client" e.g.
             // * client components pages
             // * pages bundles on browser layer
-            if !is_action_file && self.bundle_target == "client" {
+            if !is_action_file {
                 self.assert_client_graph(&imports);
                 self.assert_invalid_api(module, true);
             }
@@ -139,7 +138,7 @@ impl<C: Comments> ReactServerComponents<C> {
                                             if is_action_file {
                                                 panic_both_directives(expr_stmt.span)
                                             }
-                                        } else if self.bundle_target != "default" {
+                                        } else {
                                             HANDLER.with(|handler| {
                                                 handler
                                                     .struct_span_err(
@@ -588,17 +587,15 @@ pub fn server_components<C: Comments>(
     config: Config,
     comments: C,
     app_dir: Option<PathBuf>,
-    bundle_target: JsWord,
 ) -> impl Fold + VisitMut {
-    let is_server: bool = match &config {
-        Config::WithOptions(x) => x.is_server,
-        _ => true,
+    let is_react_server_layer: bool = match &config {
+        Config::WithOptions(x) => x.is_react_server_layer,
+        _ => false,
     };
     as_folder(ReactServerComponents {
-        is_server,
+        is_react_server_layer,
         comments,
         filepath: filename.to_string(),
-        bundle_target: bundle_target.to_string(),
         app_dir,
         export_names: vec![],
         invalid_server_imports: vec![

packages/next-swc/crates/core/src/server_actions.rs
@@ -24,7 +24,7 @@ use turbopack_binding::swc::core::{
 #[derive(Clone, Debug, Deserialize)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct Config {
-    pub is_server: bool,
+    pub is_react_server_layer: bool,
     pub enabled: bool,
 }
 
@@ -141,7 +141,7 @@ impl<C: Comments> ServerActions<C> {
                     self.config.enabled,
                 );
 
-                if is_action_fn && !self.config.is_server {
+                if is_action_fn && !self.config.is_react_server_layer {
                     HANDLER.with(|handler| {
                         handler
                             .struct_span_err(
@@ -1029,7 +1029,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
 
             stmt.visit_mut_with(self);
 
-            if self.config.is_server || !self.in_action_file {
+            if self.config.is_react_server_layer || !self.in_action_file {
                 new.push(stmt);
                 new.extend(self.annotations.drain(..).map(ModuleItem::Stmt));
                 new.append(&mut self.extra_items);
@@ -1041,7 +1041,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
             // If it's compiled in the client layer, each export field needs to be
             // wrapped by a reference creation call.
             let create_ref_ident = private_ident!("createServerReference");
-            if !self.config.is_server {
+            if !self.config.is_react_server_layer {
                 // import { createServerReference } from
                 // 'private-next-rsc-action-client-wrapper'
                 // createServerReference("action_id")
@@ -1066,7 +1066,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
             for (id, export_name) in self.exported_idents.iter() {
                 let ident = Ident::new(id.0.clone(), DUMMY_SP.with_ctxt(id.1));
 
-                if !self.config.is_server {
+                if !self.config.is_react_server_layer {
                     let action_id = generate_action_id(&self.file_name, export_name);
 
                     if export_name == "default" {
@@ -1123,7 +1123,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
                 }
             }
 
-            if self.config.is_server {
+            if self.config.is_react_server_layer {
                 new.append(&mut self.extra_items);
 
                 // Ensure that the exports are valid by appending a check
@@ -1219,7 +1219,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
             })));
 
             // Encryption and decryption only happens on the server layer.
-            if self.config.is_server {
+            if self.config.is_react_server_layer {
                 // import { encryptActionBoundArgs, decryptActionBoundArgs } from
                 // 'private-next-rsc-action-encryption'
                 new.push(ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {

commit 6790004d79ad17d1d512ee24d8cb5abead591e6e

Fix barrel optimization to ignore layers (#59254)
Fixes #57624. The recent issue was an unexpected side effect caused by
https://github.com/vercel/next.js/commit/305bb015060e82be3e6ae59a3d063c11a3e207f9,
which only affects specific packages like `@mui/material`.

The problem was that the entry file of `@mui/material` has `"use
client"` at top, which affects the compilation result to output
reference info only (when on the RSC layer), instead of keeping the
original export statements. And the fix here is to ignore all layer info
and React specific transforms here, as barrel optimization isn't related
to all these framework features at all. To keep all directives
unchanged, the SWC transform needs to parse and pass that info to the
Webpack loader.

This PR adds a test to ensure that `@mui/material` is working as
expected (less than 1500 modules compiled). Without this feature it'll
be ~2400 modules.

Closes NEXT-1793, closes NEXT-1762.

packages/next-swc/crates/core/src/optimize_barrel.rs
@@ -73,11 +73,16 @@ impl Fold for OptimizeBarrel {
 
         // We only apply this optimization to barrel files. Here we consider
         // a barrel file to be a file that only exports from other modules.
+
         // Besides that, lit expressions are allowed as well ("use client", etc.).
+        let mut allowed_directives = true;
+        let mut directives = vec![];
+
         let mut is_barrel = true;
         for item in &items {
             match item {
                 ModuleItem::ModuleDecl(decl) => {
+                    allowed_directives = false;
                     match decl {
                         ModuleDecl::Import(_) => {}
                         // export { foo } from './foo';
@@ -198,10 +203,17 @@ impl Fold for OptimizeBarrel {
                 }
                 ModuleItem::Stmt(stmt) => match stmt {
                     Stmt::Expr(expr) => match &*expr.expr {
-                        Expr::Lit(_) => {
-                            new_items.push(item.clone());
+                        Expr::Lit(l) => {
+                            if let Lit::Str(s) = l {
+                                if allowed_directives && s.value.starts_with("use ") {
+                                    directives.push(s.value.to_string());
+                                }
+                            } else {
+                                allowed_directives = false;
+                            }
                         }
                         _ => {
+                            allowed_directives = false;
                             if !self.wildcard {
                                 is_barrel = false;
                                 break;
@@ -209,6 +221,7 @@ impl Fold for OptimizeBarrel {
                         }
                     },
                     _ => {
+                        allowed_directives = false;
                         if !self.wildcard {
                             is_barrel = false;
                             break;
@@ -257,6 +270,31 @@ impl Fold for OptimizeBarrel {
                     }),
                     with: None,
                     type_only: false,
+                })));
+            }
+
+            // Push directives.
+            if !directives.is_empty() {
+                new_items.push(ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(ExportDecl {
+                    span: DUMMY_SP,
+                    decl: Decl::Var(Box::new(VarDecl {
+                        span: DUMMY_SP,
+                        kind: VarDeclKind::Const,
+                        declare: false,
+                        decls: vec![VarDeclarator {
+                            span: DUMMY_SP,
+                            name: Pat::Ident(BindingIdent {
+                                id: private_ident!("__next_private_directive_list__"),
+                                type_ann: None,
+                            }),
+                            init: Some(Box::new(Expr::Lit(Lit::Str(Str {
+                                span: DUMMY_SP,
+                                value: serde_json::to_string(&directives).unwrap().into(),
+                                raw: None,
+                            })))),
+                            definite: false,
+                        }],
+                    })),
                 })));
             }
         }

packages/next/src/build/webpack-config.ts
@@ -746,7 +746,7 @@ export default async function getBaseWebpackConfig(
     config.experimental.externalDir || !!config.transpilePackages
 
   const codeCondition = {
-    test: /\.(tsx|ts|js|cjs|mjs|jsx)$/,
+    test: { or: [/\.(tsx|ts|js|cjs|mjs|jsx)$/, /__barrel_optimize__/] },
     ...(shouldIncludeExternalDirs
       ? // Allowing importing TS/TSX files from outside of the root dir.
         {}
@@ -1124,37 +1124,6 @@ export default async function getBaseWebpackConfig(
     },
     module: {
       rules: [
-        {
-          // This loader rule works like a bridge between user's import and
-          // the target module behind a package's barrel file. It reads SWC's
-          // analysis result from the previous loader, and directly returns the
-          // code that only exports values that are asked by the user.
-          test: /__barrel_optimize__/,
-          use: ({ resourceQuery }: { resourceQuery: string }) => {
-            const names = (
-              resourceQuery.match(/\?names=([^&]+)/)?.[1] || ''
-            ).split(',')
-
-            return [
-              {
-                loader: 'next-barrel-loader',
-                options: {
-                  names,
-                  swcCacheDir: path.join(
-                    dir,
-                    config?.distDir ?? '.next',
-                    'cache',
-                    'swc'
-                  ),
-                },
-                // This is part of the request value to serve as the module key.
-                // The barrel loader are no-op re-exported modules keyed by
-                // export names.
-                ident: 'next-barrel-loader:' + resourceQuery,
-              },
-            ]
-          },
-        },
         // Alias server-only and client-only to proper exports based on bundling layers
         {
           issuerLayer: {
@@ -1579,6 +1548,40 @@ export default async function getBaseWebpackConfig(
           // tree-shake it if not used.
           test: /[\\/]next[\\/]dist[\\/](esm[\\/])?server[\\/]og[\\/]image-response\.js/,
           sideEffects: false,
+        },
+        {
+          // This loader rule should be before other rules, as it can output code
+          // that still contains `"use client"` or `"use server"` statements that
+          // needs to be re-transformed by the RSC compilers.
+          // This loader rule works like a bridge between user's import and
+          // the target module behind a package's barrel file. It reads SWC's
+          // analysis result from the previous loader, and directly returns the
+          // code that only exports values that are asked by the user.
+          test: /__barrel_optimize__/,
+          use: ({ resourceQuery }: { resourceQuery: string }) => {
+            const names = (
+              resourceQuery.match(/\?names=([^&]+)/)?.[1] || ''
+            ).split(',')
+
+            return [
+              {
+                loader: 'next-barrel-loader',
+                options: {
+                  names,
+                  swcCacheDir: path.join(
+                    dir,
+                    config?.distDir ?? '.next',
+                    'cache',
+                    'swc'
+                  ),
+                },
+                // This is part of the request value to serve as the module key.
+                // The barrel loader are no-op re-exported modules keyed by
+                // export names.
+                ident: 'next-barrel-loader:' + resourceQuery,
+              },
+            ]
+          },
         },
       ],
     },

packages/next/src/build/webpack/loaders/next-barrel-loader.ts
@@ -88,7 +88,6 @@ import type webpack from 'webpack'
 
 import path from 'path'
 import { transform } from '../../swc'
-import { WEBPACK_LAYERS } from '../../../lib/constants'
 
 // This is a in-memory cache for the mapping of barrel exports. This only applies
 // to the packages that we optimize. It will never change (e.g. upgrading packages)
@@ -97,14 +96,13 @@ import { WEBPACK_LAYERS } from '../../../lib/constants'
 const barrelTransformMappingCache = new Map<
   string,
   {
-    prefix: string
     exportList: [string, string, string][]
     wildcardExports: string[]
+    isClientEntry: boolean
   } | null
 >()
 
 async function getBarrelMapping(
-  layer: string | null | undefined,
   resourcePath: string,
   swcCacheDir: string,
   resolve: (context: string, request: string) => Promise<string>,
@@ -135,9 +133,6 @@ async function getBarrelMapping(
         optimizeBarrelExports: {
           wildcard: isWildcard,
         },
-        serverComponents: {
-          isReactServerLayer: layer === WEBPACK_LAYERS.reactServerComponents,
-        },
         jsc: {
           parser: {
             syntax: isTypeScript ? 'typescript' : 'ecmascript',
@@ -155,7 +150,11 @@ async function getBarrelMapping(
 
   // Avoid circular `export *` dependencies
   const visited = new Set<string>()
-  async function getMatches(file: string, isWildcard: boolean) {
+  async function getMatches(
+    file: string,
+    isWildcard: boolean,
+    isClientEntry: boolean
+  ) {
     if (visited.has(file)) {
       return null
     }
@@ -180,7 +179,15 @@ async function getBarrelMapping(
       return null
     }
 
-    const prefix = matches[1]
+    const matchedDirectives = output.match(
+      /^([^]*)export (const|var) __next_private_directive_list__ = '([^']+)'/
+    )
+    const directiveList = matchedDirectives
+      ? JSON.parse(matchedDirectives[3])
+      : []
+    // "use client" in barrel files has to be transferred to the target file.
+    isClientEntry = directiveList.includes('use client')
+
     let exportList = JSON.parse(matches[3].slice(1, -1)) as [
       string,
       string,
@@ -209,7 +216,11 @@ async function getBarrelMapping(
             req.replace('__barrel_optimize__?names=__PLACEHOLDER__!=!', '')
           )
 
-          const targetMatches = await getMatches(targetPath, true)
+          const targetMatches = await getMatches(
+            targetPath,
+            true,
+            isClientEntry
+          )
           if (targetMatches) {
             // Merge the export list
             exportList = exportList.concat(targetMatches.exportList)
@@ -219,13 +230,13 @@ async function getBarrelMapping(
     }
 
     return {
-      prefix,
       exportList,
       wildcardExports,
+      isClientEntry,
     }
   }
 
-  const res = await getMatches(resourcePath, false)
+  const res = await getMatches(resourcePath, false, false)
   barrelTransformMappingCache.set(resourcePath, res)
 
   return res
@@ -249,7 +260,6 @@ const NextBarrelLoader = async function (
   })
 
   const mapping = await getBarrelMapping(
-    this._module?.layer,
     this.resourcePath,
     swcCacheDir,
     resolve,
@@ -271,15 +281,14 @@ const NextBarrelLoader = async function (
     return
   }
 
-  // It needs to keep the prefix for comments and directives like "use client".
-  const prefix = mapping.prefix
   const exportList = mapping.exportList
+  const isClientEntry = mapping.isClientEntry
   const exportMap = new Map<string, [string, string]>()
   for (const [name, filePath, orig] of exportList) {
     exportMap.set(name, [filePath, orig])
   }
 
-  let output = prefix
+  let output = ''
   let missedNames: string[] = []
   for (const name of names) {
     // If the name matches
@@ -311,6 +320,12 @@ const NextBarrelLoader = async function (
         req.replace('__PLACEHOLDER__', missedNames.join(',') + '&wildcard')
       )}`
     }
+  }
+
+  // When it has `"use client"` inherited from its barrel files, we need to
+  // prefix it to this target file as well.
+  if (isClientEntry) {
+    output = `"use client";\n${output}`
   }
 
   this.callback(null, output)

packages/next/src/build/webpack/loaders/next-flight-client-entry-loader.ts
@@ -1,4 +1,7 @@
-import { RSC_MODULE_TYPES } from '../../../shared/lib/constants'
+import {
+  BARREL_OPTIMIZATION_PREFIX,
+  RSC_MODULE_TYPES,
+} from '../../../shared/lib/constants'
 import { getModuleBuildInfo } from './get-module-build-info'
 import { regexCSS } from './utils'
 
@@ -26,7 +29,11 @@ export default function transformSource(this: any) {
     .filter((request) => (isServer ? !regexCSS.test(request) : true))
     .map(
       (request) =>
-        `import(/* webpackMode: "eager" */ ${JSON.stringify(request)})`
+        `import(/* webpackMode: "eager" */ ${JSON.stringify(
+          request.startsWith(BARREL_OPTIMIZATION_PREFIX)
+            ? request.replace(':', '!=!')
+            : request
+        )})`
     )
     .join(';\n')
 

packages/next/src/build/webpack/plugins/flight-client-entry-plugin.ts
@@ -193,7 +193,11 @@ export class FlightClientEntryPlugin {
         const modQuery = mod.resourceResolveData?.query || ''
         // query is already part of mod.resource
         // so it's only necessary to add it for matchResource or mod.resourceResolveData
-        const modResource = modPath ? modPath + modQuery : mod.resource
+        const modResource = modPath
+          ? modPath.startsWith(BARREL_OPTIMIZATION_PREFIX)
+            ? mod.resource
+            : modPath + modQuery
+          : mod.resource
 
         if (mod.layer !== WEBPACK_LAYERS.serverSideRendering) {
           return

packages/next-swc/crates/core/tests/fixture/optimize-barrel/normal/4/input.js
@@ -1,3 +1,9 @@
+/* Test */
+/* Test directives inside comments
+'use server'
+*/
+
+// This should be kept
 'use client'
 
 import foo, { a, b } from 'foo'
@@ -7,3 +13,7 @@ export { a as x }
 export { y } from '1'
 export { b }
 export { foo as default, z }
+
+// This should be removed as it's not on top
+// prettier-ignore
+'use strict'

packages/next-swc/crates/core/tests/fixture/optimize-barrel/normal/4/output.js
@@ -1,2 +1,5 @@
-'use client';
+/* Test */ /* Test directives inside comments
+'use server'
+*/ // This should be kept
 export const __next_private_export_map__ = '[["x","foo","a"],["y","1","y"],["b","foo","b"],["default","foo","default"],["z","bar","default"]]';
+export const __next_private_directive_list__ = '["use client"]';

test/development/basic/barrel-optimization/barrel-optimization.test.ts
@@ -28,6 +28,9 @@ createNextDescribe(
       '@heroicons/react': '2.0.18',
       '@visx/visx': '3.3.0',
       'recursive-barrel': '1.0.0',
+      '@mui/material': '5.14.19',
+      '@emotion/styled': '11.11.0',
+      '@emotion/react': '11.11.1',
     },
   },
   ({ next }) => {
@@ -125,6 +128,25 @@ createNextDescribe(
     it('should support visx', async () => {
       const html = await next.render('/visx')
       expect(html).toContain('<linearGradient')
+    })
+
+    it('should support MUI', async () => {
+      let logs = ''
+      next.on('stdout', (log) => {
+        logs += log
+      })
+
+      // Ensure that MUI is working
+      const html = await next.render('/mui')
+      expect(html).toContain('test_mui')
+
+      const modules = [...logs.matchAll(/\((\d+) modules\)/g)]
+      expect(modules.length).toBeGreaterThanOrEqual(1)
+      for (const [, moduleCount] of modules) {
+        // Ensure that the number of modules is less than 1500 - otherwise we're
+        // importing the entire library.
+        expect(parseInt(moduleCount)).toBeLessThan(1500)
+      }
     })
 
     it('should not break "use client" directive in optimized packages', async () => {

test/development/basic/barrel-optimization/fixture/app/mui/page.js
@@ -1,0 +1,5 @@
+import { Button } from '@mui/material'
+
+export default function Page() {
+  return <Button>test_mui</Button>
+}