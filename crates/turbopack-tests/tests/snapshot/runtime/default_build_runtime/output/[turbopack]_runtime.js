40e4a80924939f8c0ca9977943621e642ddba603
Leah <8845940+ForsakenHarmony@users.noreply.github.com>
2023-12-07 22:25:37 +0100

    fix(turbopack-ecmascript-runtime): don't use `path.relative` for absolute paths (vercel/turbo#6731)
    ### Description
    
    This was causing an issue with `import.meta.url` used in `@vercel/og`
    trying to get the font file from `project/app/node_modules` instead of
    `project/node_modules`
    
    Closes PACK-2118

--- crates/turbopack-tests/tests/snapshot/runtime/default_build_runtime/output/[turbopack]_runtime.js
+++ crates/turbopack-tests/tests/snapshot/runtime/default_build_runtime/output/[turbopack]_runtime.js
@@ -346,9 +346,7 @@ const ABSOLUTE_ROOT = path.resolve(__filename, relativePathToDistRoot);
  * See ImportMetaBinding::code_generation for the usage.
  */ function resolveAbsolutePath(modulePath) {
     if (modulePath) {
-        // Module path can contain common relative path to the root, recalaute to avoid duplicated joined path.
-        const relativePathToRoot = path.relative(ABSOLUTE_ROOT, modulePath);
-        return path.join(ABSOLUTE_ROOT, relativePathToRoot);
+        return path.join(ABSOLUTE_ROOT, modulePath);
     }
     return ABSOLUTE_ROOT;
 }
