73fc04c32981453534efb60f59d9f542a816b307
Ralph <janicklas@google.com>
2023-12-09 02:50:40 +0530

    Adding dataLayerName prop to GA component

--- packages/third-parties/src/google/ga.tsx
+++ packages/third-parties/src/google/ga.tsx
@@ -3,20 +3,29 @@
 import React, { useEffect } from 'react'
 import Script from 'next/script'
 
+import type { GAParams } from '../types/google'
+
 declare global {
   interface Window {
     dataLayer?: Object[]
   }
 }
 
-type GAParams = {
-  gaId: string
-}
+let currDataLayerName: string | undefined = undefined
 
 export function GoogleAnalytics(props: GAParams) {
-  const { gaId } = props
+  const { gaId, dataLayerName = 'dataLayer' } = props
+
+  if (currDataLayerName === undefined) {
+    currDataLayerName = dataLayerName
+  }
 
   useEffect(() => {
+    // performance.mark is being used as a feature use signal. While it is traditionally used for performance
+    // benchmarking it is low overhead and thus considered safe to use in production and it is a widely available
+    // existing API.
+    // The performance measurement will be done at the Browser side by Chrome
+
     performance.mark('mark_feature_usage', {
       detail: {
         feature: 'next-third-parties-ga',
@@ -30,8 +39,8 @@ export function GoogleAnalytics(props: GAParams) {
         id="_next-ga-init"
         dangerouslySetInnerHTML={{
           __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          window['${dataLayerName}'] = window['${dataLayerName}'] || [];
+          function gtag(){window['${dataLayerName}'].push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${gaId}');`,
@@ -46,6 +55,16 @@ export function GoogleAnalytics(props: GAParams) {
 }
 
 export const sendGAEvent = (...args: Object[]) => {
-  window.dataLayer = window.dataLayer || []
-  window.dataLayer.push(...args)
+  if (currDataLayerName === undefined) {
+    console.warn(`@next/third-parties: GA has not been initialized`)
+    return
+  }
+
+  if (window[currDataLayerName]) {
+    window[currDataLayerName].push(...args)
+  } else {
+    console.warn(
+      `@next/third-parties: GA dataLayer ${currDataLayerName} does not exist`
+    )
+  }
 }
