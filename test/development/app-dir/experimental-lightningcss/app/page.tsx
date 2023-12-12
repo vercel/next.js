3c3d33ced6060cf92711c7490641626e6128e80d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-12 13:12:08 +0900

    log

--- test/development/app-dir/experimental-lightningcss/app/page.tsx
+++ test/development/app-dir/experimental-lightningcss/app/page.tsx
@@ -1,7 +1,7 @@
 import styles from './style.module.css'
 
 export default function Page() {
-  console.log(styles)
+  console.log('CSSModules.styles', styles)
   return (
     <>
       <p className={`search-keyword ${styles.blue}`}>hello world</p>
