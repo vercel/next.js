a470c9d7fb8006b15855154fb689cbd8903198e0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-12 13:20:14 +0900

    import

--- packages/next/src/build/webpack/loaders/lightningcss-loader/src/loader.ts
+++ packages/next/src/build/webpack/loaders/lightningcss-loader/src/loader.ts
@@ -20,6 +20,7 @@ import {
   getPreRequester,
   isUrlRequestable,
 } from '../../css-loader/src/utils'
+import { stringifyRequest } from '../../../stringify-request'
 
 function createVisitor(
   options: ILightningCssLoaderConfig,
@@ -192,63 +193,3 @@ export async function LightningCssLoader(
 }
 
 export const raw = true
-
-const matchRelativePath = /^\.\.?[/\\]/
-
-function isAbsolutePath(str: string) {
-  return path.posix.isAbsolute(str) || path.win32.isAbsolute(str)
-}
-
-function isRelativePath(str: string) {
-  return matchRelativePath.test(str)
-}
-
-// TODO simplify for the next major release
-function stringifyRequest(
-  loaderContext: LoaderContext<ILightningCssLoaderConfig>,
-  request: string
-) {
-  if (
-    typeof loaderContext.utils !== 'undefined' &&
-    typeof loaderContext.utils.contextify === 'function'
-  ) {
-    return JSON.stringify(
-      loaderContext.utils.contextify(
-        loaderContext.context || loaderContext.rootContext,
-        request
-      )
-    )
-  }
-
-  const splitted = request.split('!')
-  const { context } = loaderContext
-
-  return JSON.stringify(
-    splitted
-      .map((part) => {
-        // First, separate singlePath from query, because the query might contain paths again
-        const splittedPart = part.match(/^(.*?)(\?.*)/)
-        const query = splittedPart ? splittedPart[2] : ''
-        let singlePath = splittedPart ? splittedPart[1] : part
-
-        if (isAbsolutePath(singlePath) && context) {
-          singlePath = path.relative(context, singlePath)
-
-          if (isAbsolutePath(singlePath)) {
-            // If singlePath still matches an absolute path, singlePath was on a different drive than context.
-            // In this case, we leave the path platform-specific without replacing any separators.
-            // @see https://github.com/webpack/loader-utils/pull/14
-            return singlePath + query
-          }
-
-          if (isRelativePath(singlePath) === false) {
-            // Ensure that the relative path starts at least with ./ otherwise it would be a request into the modules directory (like node_modules).
-            singlePath = `./${singlePath}`
-          }
-        }
-
-        return singlePath.replace(/\\/g, '/') + query
-      })
-      .join('!')
-  )
-}
