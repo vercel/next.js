f654eb2305379df71adfccbc50f2cb6b1d28b783
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-12 12:06:09 +0900

    WIP: Test for url loader

--- test/development/app-dir/experimental-lightningcss/app/page.tsx
+++ test/development/app-dir/experimental-lightningcss/app/page.tsx
@@ -2,5 +2,10 @@ import styles from './style.module.css'
 
 export default function Page() {
   console.log(styles)
-  return <p className={`search-keyword ${styles.blue}`}>hello world</p>
+  return (
+    <>
+      <p className={`search-keyword ${styles.blue}`}>hello world</p>
+      <div className="red-text">This text should be red.</div>
+    </>
+  )
 }
