53ffe832c3b269a8fbfd7d38c156b891d75a633b
Delba de Oliveira <delbabrown@gmail.com>
2023-12-12 14:51:31 +0000

    Fix route conflict

--- examples/with-vitest/pages/home/index.tsx
+++ examples/with-vitest/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
