b116e5edc01c2b3fc30aa04bda4568832b40d07b
Delba de Oliveira <delbabrown@gmail.com>
2023-12-12 14:33:53 +0000

    Clean up vitest example
    - Move 'server-only' into its own file/test (e.g. server-only shouldn't be in a server component)
    - Delete extra folders

--- examples/with-vitest/app/counter.test.tsx
+++ examples/with-vitest/app/counter.test.tsx
@@ -1,9 +1,9 @@
 import { expect, test } from 'vitest'
 import { render, screen, fireEvent } from '@testing-library/react'
-import Component from './component'
+import Counter from './counter'
 
-test('App Router: Works with Client Components (React State)', () => {
-  render(<Component />)
+test('App Router: Works with Client Components', () => {
+  render(<Counter />)
   expect(screen.getByRole('heading', { level: 2, name: '0' })).toBeDefined()
   fireEvent.click(screen.getByRole('button'))
   expect(screen.getByRole('heading', { level: 2, name: '1' })).toBeDefined()
--- examples/with-vitest/app/counter.tsx
+++ examples/with-vitest/app/counter.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 
 export default function Counter() {
