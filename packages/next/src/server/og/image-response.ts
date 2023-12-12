9a3e3ece4645a9277b0ef8fcb5bd9ac8a286d07c
Jiachi Liu <inbox@huozhi.im>
2023-12-12 20:53:25 +0100

    fix import

--- packages/next/src/server/og/image-response.ts
+++ packages/next/src/server/og/image-response.ts
@@ -1,10 +1,10 @@
 import {
-  ImageResponse as OriginalImageResponse,
+  ImageResponse,
   experimental_FigmaImageResponse,
 } from 'next/dist/compiled/@vercel/og'
 
 // Set the `displayName` property to the original `ImageResponse` class.
 // @ts-ignore
-OriginalImageResponse.displayName = 'ImageResponse'
+ImageResponse.displayName = 'ImageResponse'
 
-export { OriginalImageResponse, experimental_FigmaImageResponse }
+export { ImageResponse, experimental_FigmaImageResponse }
