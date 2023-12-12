56e39c9d349a617b1fcd0b656118a19cafd9853d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-12 11:47:17 +0900

    test

--- test/development/app-dir/experimental-lightningcss/experimental-lightningcss.test.ts
+++ test/development/app-dir/experimental-lightningcss/experimental-lightningcss.test.ts
@@ -1,6 +1,24 @@
 import { nextTestSetup } from 'e2e-utils'
+import { describeVariants as describe } from 'next-test-utils'
 
-describe('experimental-lightningcss', () => {
+describe.each(['turbo'])('experimental-lightningcss', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should support css modules', async () => {
+    // Recommended for tests that check HTML. Cheerio is a HTML parser that has a jQuery like API.
+    const $ = await next.render$('/')
+    expect($('p').text()).toBe('hello world')
+    // swc_css does not include `-module` in the class name, while lightningcss does.
+    expect($('p').attr('class')).toBe(
+      'search-keyword style-module__hlQ3RG__blue'
+    )
+  })
+})
+
+// lightningcss produces different class names in turbo mode
+describe.each(['default'])('experimental-lightningcss', () => {
   const { next } = nextTestSetup({
     files: __dirname,
     dependencies: { lightningcss: '^1' },
@@ -12,7 +30,7 @@ describe('experimental-lightningcss', () => {
     expect($('p').text()).toBe('hello world')
     // swc_css does not include `-module` in the class name, while lightningcss does.
     expect($('p').attr('class')).toBe(
-      'search-keyword style-module__hlQ3RG__blue'
+      'search-keyword style-module__wDjWCG__blue'
     )
   })
 })
