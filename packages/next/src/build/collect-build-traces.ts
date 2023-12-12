ed12b552de3739c386bdcecf22fd1fd9faefe3ad
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-12-12 14:37:00 -0700

    Page Info Cleanup (#59430)
    This updates the some of the logic around updating `PageInfo` entries in
    the `pageInfos` map. This is a followup to #59420.
    
    Closes NEXT-1838

--- packages/next/src/build/collect-build-traces.ts
+++ packages/next/src/build/collect-build-traces.ts
@@ -14,7 +14,7 @@ import {
 
 import path from 'path'
 import fs from 'fs/promises'
-import type { PageInfo } from './utils'
+import type { PageInfo } from './page-info'
 import { loadBindings } from './swc'
 import { nonNullable } from '../lib/non-nullable'
 import * as ciEnvironment from '../telemetry/ci-info'
@@ -79,7 +79,7 @@ export async function collectBuildTraces({
   staticPages: string[]
   hasSsrAmpPages: boolean
   outputFileTracingRoot: string
-  pageInfos: [string, PageInfo][]
+  pageInfos: Map<string, PageInfo> | undefined
   nextBuildSpan?: Span
   config: NextConfigComplete
   buildTraceContext?: BuildTraceContext
@@ -642,10 +642,8 @@ export async function collectBuildTraces({
         }
 
         // edge routes have no trace files
-        const [, pageInfo] = pageInfos.find((item) => item[0] === route) || []
-        if (pageInfo?.runtime === 'edge') {
-          return
-        }
+        const pageInfo = pageInfos?.get(route)
+        if (pageInfo?.runtime === 'edge') return
 
         const combinedIncludes = new Set<string>()
         const combinedExcludes = new Set<string>()
