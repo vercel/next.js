2a698761eab841ade7143eb64b4a19186ee39a0f
OJ Kwon <1210596+kwonoj@users.noreply.github.com.>
2023-12-14 10:35:10 -0800

    feat(next-core): apply transform

--- packages/next-swc/crates/next-core/src/next_server/transforms.rs
+++ packages/next-swc/crates/next-core/src/next_server/transforms.rs
@@ -11,7 +11,9 @@ use crate::{
     next_shared::transforms::{
         get_next_dynamic_transform_rule, get_next_font_transform_rule, get_next_image_rule,
         get_next_modularize_imports_rule, get_next_pages_transforms_rule,
-        get_server_actions_transform_rule, server_actions::ActionsTransform,
+        get_server_actions_transform_rule,
+        next_react_server_components::get_next_react_server_components_transform_rule,
+        server_actions::ActionsTransform,
     },
 };
 
@@ -60,6 +62,11 @@ pub async fn get_next_server_transforms_rules(
                 ActionsTransform::Server,
                 mdx_rs,
             ));
+
+            rules.push(get_next_react_server_components_transform_rule(
+                true, mdx_rs,
+            ));
+
             if let Some(client_transition) = client_transition {
                 rules.push(get_next_css_client_reference_transforms_rule(
                     client_transition,
--- packages/next-swc/crates/next-core/src/next_shared/transforms/next_react_server_components.rs
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/next_react_server_components.rs
@@ -1,25 +1,39 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use async_trait::async_trait;
+use next_transform_react_server_components::{server_components, Config, Options};
 use swc_core::{
-    common::util::take::Take,
-    ecma::ast::{Module, Program},
+    common::{util::take::Take, FileName},
+    ecma::{
+        ast::{Module, Program},
+        visit::FoldWith,
+    },
 };
+use turbo_tasks::Vc;
 use turbopack_binding::turbopack::{
-    ecmascript::{CustomTransformer, TransformContext},
-    turbopack::module_options::ModuleRule,
+    ecmascript::{CustomTransformer, EcmascriptInputTransform, TransformContext},
+    turbopack::module_options::{ModuleRule, ModuleRuleEffect},
 };
 
 use super::module_rule_match_js_no_url;
 
 /// Returns a rule which applies the Next.js react server components transform.
-pub async fn get_next_react_server_components_transform_rule(
+pub fn get_next_react_server_components_transform_rule(
     is_react_server_layer: bool,
     enable_mdx_rs: bool,
-) -> Result<ModuleRule> {
-    Ok(ModuleRule::new(
+) -> ModuleRule {
+    let transformer =
+        EcmascriptInputTransform::Plugin(Vc::cell(Box::new(NextJsReactServerComponents {
+            is_react_server_layer,
+        }) as _));
+
+    ModuleRule::new(
         module_rule_match_js_no_url(enable_mdx_rs),
-        vec![],
-    ))
+        vec![ModuleRuleEffect::AddEcmascriptTransforms(Vc::cell(vec![
+            transformer,
+        ]))],
+    )
 }
 
 #[derive(Debug)]
@@ -31,6 +45,17 @@ struct NextJsReactServerComponents {
 impl CustomTransformer for NextJsReactServerComponents {
     async fn transform(&self, program: &mut Program, ctx: &TransformContext<'_>) -> Result<()> {
         let p = std::mem::replace(program, Program::Module(Module::dummy()));
+
+        let mut visitor = server_components(
+            FileName::Custom(ctx.file_path_str.to_string()),
+            Config::WithOptions(Options {
+                is_react_server_layer: self.is_react_server_layer,
+            }),
+            ctx.comments,
+            Some(PathBuf::from(ctx.file_path.parent().await?.path.clone())),
+        );
+
+        *program = p.fold_with(&mut visitor);
 
         Ok(())
     }
