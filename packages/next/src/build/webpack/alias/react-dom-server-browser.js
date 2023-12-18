45f78dfa1fc275f77d1afcaa114bbac513fc8d90
Jiachi Liu <inbox@huozhi.im>
2023-12-18 17:16:51 +0100

    fix browser alias

--- packages/next/src/build/webpack/alias/react-dom-server-browser.js
+++ packages/next/src/build/webpack/alias/react-dom-server-browser.js
@@ -5,19 +5,25 @@ function error() {
   throw new Error(ERROR_MESSAGE)
 }
 
-var b
+// 'use strict';
+
+var l
+var s
 if (process.env.NODE_ENV === 'production') {
-  b = require('next/dist/compiled/react-dom/cjs/react-dom-server.edge.production.min.js')
+  // l = require('next/dist/compiled/react-compiled/cjs/react-dom-server-legacy.browser.production.min.js');
+  s = require('next/dist/compiled/react-compiled/cjs/react-dom-server.browser.production.min.js')
 } else {
-  b = require('next/dist/compiled/react-dom/cjs/react-dom-server.edge.development.js')
+  // l = require('next/dist/compiled/react-compiled/cjs/react-dom-server-legacy.browser.development.js');
+  s = require('next/dist/compiled/react-compiled/cjs/react-dom-server.browser.development.js')
 }
 
-exports.version = b.version
-exports.renderToReadableStream = b.renderToReadableStream
-exports.renderToNodeStream = b.renderToNodeStream
-exports.renderToStaticNodeStream = b.renderToStaticNodeStream
-exports.renderToString = error
-exports.renderToStaticMarkup = error
-if (b.resume) {
-  exports.resume = b.resume
+exports.renderToString = error //l.renderToString;
+exports.renderToStaticMarkup = error //l.renderToStaticMarkup;
+exports.renderToNodeStream = error //l.renderToNodeStream;
+exports.renderToStaticNodeStream = error //l.renderToStaticNodeStream;
+
+exports.version = s.version
+exports.renderToReadableStream = s.renderToReadableStream
+if (s.resume) {
+  exports.resume = s.resume
 }
