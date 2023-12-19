6f8a4396eaf276e3ff3dda289f4b87bc32d6b086
Zane Bauman <zane.bauman@upstart.com>
2023-12-19 14:41:32 -0500

    Pass on all props to next/script in app router

--- packages/next/src/client/script.tsx
+++ packages/next/src/client/script.tsx
@@ -334,7 +334,7 @@ function Script(props: ScriptProps): JSX.Element | null {
             dangerouslySetInnerHTML={{
               __html: `(self.__next_s=self.__next_s||[]).push(${JSON.stringify([
                 0,
-                { ...restProps },
+                { ...restProps, id },
               ])})`,
             }}
           />
@@ -353,6 +353,7 @@ function Script(props: ScriptProps): JSX.Element | null {
             dangerouslySetInnerHTML={{
               __html: `(self.__next_s=self.__next_s||[]).push(${JSON.stringify([
                 src,
+                { ...restProps, id }
               ])})`,
             }}
           />
