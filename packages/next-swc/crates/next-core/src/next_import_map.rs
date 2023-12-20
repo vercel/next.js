e974719316df87939a87a46efced3ad5b5d8b643
강동윤 (Donny) <kdy1997.dev@gmail.com>
2023-12-20 19:38:09 +0900

    I need a review

--- packages/next-swc/crates/next-core/src/next_import_map.rs
+++ packages/next-swc/crates/next-core/src/next_import_map.rs
@@ -827,8 +827,38 @@ async fn insert_next_shared_aliases(
     import_map.insert_singleton_alias("@swc/helpers", get_next_package(project_path));
     import_map.insert_singleton_alias("styled-jsx", get_next_package(project_path));
     import_map.insert_singleton_alias("next", project_path);
-    import_map.insert_singleton_alias("react", project_path);
-    import_map.insert_singleton_alias("react-dom", project_path);
+    // import_map.insert_singleton_alias("react", project_path);
+    // import_map.insert_singleton_alias("react-dom", project_path);
+
+    let ppr = *next_config.enable_ppr().await?;
+    let taint = *next_config.enable_taint().await?;
+    let react_channel = if ppr || taint { "-experimental" } else { "" };
+
+    let mut alias = indexmap! {
+        "react" => format!("next/dist/compiled/react{react_channel}"),
+        "react-dom" => format!("next/dist/compiled/react-dom{react_channel}"),
+        "react/jsx-runtime" => format!("next/dist/compiled/react{react_channel}/jsx-runtime"),
+        "react/jsx-dev-runtime" => format!("next/dist/compiled/react{react_channel}/jsx-dev-runtime"),
+        "react-dom/client" => format!("next/dist/compiled/react-dom{react_channel}/client"),
+        "react-dom/static" => format!("next/dist/compiled/react-dom-experimental/static"),
+        "react-dom/static.edge" => format!("next/dist/compiled/react-dom-experimental/static.edge"),
+        "react-dom/static.browser" => format!("next/dist/compiled/react-dom-experimental/static.browser"),
+        "react-dom/server" => format!("next/dist/compiled/react-dom{react_channel}/server"),
+        "react-dom/server.edge" => format!("next/dist/compiled/react-dom{react_channel}/server.edge"),
+        "react-dom/server.browser" => format!("next/dist/compiled/react-dom{react_channel}/server.browser"),
+        "react-server-dom-webpack/client" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/client"),
+        "react-server-dom-webpack/client.edge" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/client.edge"),
+        "react-server-dom-webpack/server.edge" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/server.edge"),
+        "react-server-dom-webpack/server.node" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/server.node"),
+        "react-server-dom-turbopack/client" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/client"),
+        "react-server-dom-turbopack/client.edge" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/client.edge"),
+        "react-server-dom-turbopack/server.edge" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/server.edge"),
+        "react-server-dom-turbopack/server.node" => format!("next/dist/compiled/react-server-dom-turbopack{react_channel}/server.node"),
+    };
+
+    for (key, value) in alias.iter_mut() {
+        import_map.insert_exact_alias(*key, request_to_import_mapping(project_path, value));
+    }
 
     //https://github.com/vercel/next.js/blob/f94d4f93e4802f951063cfa3351dd5a2325724b3/packages/next/src/build/webpack-config.ts#L1196
     import_map.insert_exact_alias(
