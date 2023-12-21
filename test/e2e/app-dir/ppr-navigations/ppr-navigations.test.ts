746052767ab757059fb22b45f679110955fea59b
Andrew Clark <git@andrewclark.io>
2023-12-21 01:13:00 -0500

    Add regression test for stale prefetch PPR bug

--- test/e2e/app-dir/ppr-navigations/ppr-navigations.test.ts
+++ test/e2e/app-dir/ppr-navigations/ppr-navigations.test.ts
@@ -28,27 +28,23 @@ describe('ppr-navigations', () => {
     const port = await findPort()
     server.listen(port)
     next = await createNext({
-      files: __dirname,
+      files: __dirname + '/loading-tsx-no-partial-rendering',
       env: { TEST_DATA_SERVICE_URL: `http://localhost:${port}` },
     })
 
     // There should have been no data requests during build
     TestLog.assert([])
 
-    const browser = await next.browser(
-      '/loading-tsx-no-partial-rendering/start'
-    )
+    const browser = await next.browser('/start')
 
     // Use a text input to set the target URL.
     const input = await browser.elementByCss('input')
-    await input.fill('/loading-tsx-no-partial-rendering/yay')
+    await input.fill('/yay')
 
     // This causes a <Link> to appear. (We create the Link after initial render
     // so we can control when the prefetch happens.)
     const link = await browser.elementByCss('a')
-    expect(await link.getAttribute('href')).toBe(
-      '/loading-tsx-no-partial-rendering/yay'
-    )
+    expect(await link.getAttribute('href')).toBe('/yay')
 
     // The <Link> triggers a prefetch. Even though this route has a loading.tsx
     // boundary, we're still able to prefetch the static data in the page.
@@ -94,6 +90,92 @@ describe('ppr-navigations', () => {
     const loading = await browser.elementById('loading-tsx')
     expect(await loading.innerHTML()).toEqual('Loading [inner loading.tsx]...')
   })
+
+  // This is a regression test where a condition that checked whether a cached
+  // route entry was stale accidentally caused the router to treat prefetched
+  // data as if it were complete data that didn't contain dynamic holes. This
+  // prevented the dynamic data from ever streaming in.
+  test(
+    'works if a prefetched route entry has become stale (too much ' +
+      'time has elapsed since it was prefetched)',
+    async () => {
+      const TestLog = createTestLog()
+      let autoresolveRequests = true
+      let pendingRequests = new Map()
+      server = createTestDataServer(async (key, res) => {
+        TestLog.log('REQUEST: ' + key)
+        if (autoresolveRequests) {
+          res.resolve()
+          return
+        }
+        if (pendingRequests.has(key)) {
+          throw new Error('Request already pending for ' + key)
+        }
+        pendingRequests.set(key, res)
+      })
+      const port = await findPort()
+      server.listen(port)
+      next = await createNext({
+        files: __dirname + '/stale-prefetch-entry',
+        env: { TEST_DATA_SERVICE_URL: `http://localhost:${port}` },
+      })
+      TestLog.assert(['REQUEST: Some data [static]'])
+      autoresolveRequests = false
+
+      const browser = await next.browser('/', {
+        // Override Date.now so we can simulate time passing to expire a
+        // prefetch entry.
+        async beforePageLoad(page) {
+          await page.addInitScript(`
+            __FAKE_NOW__ = 0
+            Date = class extends Date {
+              constructor(...args) {
+                if (args.length === 0) {
+                  super(__FAKE_NOW__)
+                } else {
+                  super(...args)
+                }
+              }
+              static now() {
+                return __FAKE_NOW__
+              }
+            }
+          `)
+        },
+      })
+
+      const startTime = await browser.eval(() => Date.now())
+
+      // Explicitly hover over the link to trigger a prefetch.
+      const link = await browser.elementByCss('a[href="/some-page"]')
+      await link.hover()
+
+      // Increment time by 3 minutes. This is longer than the default expiration
+      // interval for prefetch entries.
+      await browser.eval(`__FAKE_NOW__ = 60 * 1000 * 3`)
+      const endTime = await browser.eval(() => Date.now())
+      // Sanity check.
+      expect(endTime - startTime).toBe(60 * 1000 * 3)
+
+      // Navigate. The prefetch entry will be stale.
+      await link.click()
+
+      // The static UI appears immediately because it was prerendered at
+      // build time.
+      const staticContainer = await browser.elementById('static')
+      expect(await staticContainer.innerText()).toBe('Some data [static]')
+
+      // The dynamic data is fetched on navigation. (In the regression case that
+      // this test simulates, the dynamic data was never requested, and the page
+      // got stuck in a loading state.)
+      await TestLog.waitFor(['REQUEST: Some data [dynamic]'])
+      pendingRequests.get('Some data [dynamic]').resolve()
+
+      // Now the dynamic data appears.
+      const dynamicContainer = await browser.elementById('dynamic')
+      expect(await dynamicContainer.innerText()).toBe('Some data [dynamic]')
+    }
+  )
 })
 
 // NOTE: I've intentionally not yet moved these helpers into a separate
@@ -103,7 +185,7 @@ describe('ppr-navigations', () => {
 
 type TestDataResponse = {
   _res: http.ServerResponse
-  resolve: (value: string) => any
+  resolve: (value?: string) => any
   reject: (value: any) => any
 }
 
@@ -147,7 +229,7 @@ function createTestDataServer(
 
     const response: TestDataResponse = {
       _res: res,
-      resolve(value: string | void) {
+      resolve(value?: string) {
         res.end(value === undefined ? key : value)
       },
       reject(error: Error, status?: number) {
@@ -208,13 +290,14 @@ function createTestLog() {
   }
 
   function assert(expectedEvents: any[]) {
-    const actualEvents = events
-
     if (pendingExpectation !== null) {
       const error = new Error('Cannot assert while a waitFor() is pending.')
       Error.captureStackTrace(error, assert)
       throw error
     }
+
+    const actualEvents = events
+    events = []
 
     if (!areLogsEqual(expectedEvents, actualEvents)) {
       // Capture the stack trace of `assert` so that Jest will report the
@@ -264,6 +347,8 @@ function createTestLog() {
         reject(error)
       }
     }, timeout)
+
+    pingExpectation()
 
     return promise
   }
