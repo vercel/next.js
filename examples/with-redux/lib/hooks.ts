ed3bae2c34cf3d1a10b27c294926b62929624e95
Arya Emami <aryaemami59@yahoo.com>
2023-12-21 16:04:43 -0600

    Add `hooks.ts` to match RTK docs

---
+++ examples/with-redux/lib/hooks.ts
@@ -0,0 +1,8 @@
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useDispatch, useSelector, useStore } from 'react-redux'
+import type { AppDispatch, AppStore, RootState } from './store'
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppStore: () => AppStore = useStore
