97541c641909a50c70d0c4b2e2af0ca9ece6590d
Arya Emami <aryaemami59@yahoo.com>
2023-12-21 16:51:14 -0600

    Rename and update `providers.tsx` to `StoreProvider.tsx` to match RTK docs

---
+++ examples/with-redux/app/StoreProvider.tsx
@@ -0,0 +1,20 @@
+'use client'
+import type { ReactNode } from 'react'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import type { AppStore } from '../lib/store'
+import { makeStore } from '../lib/store'
+
+interface Props {
+  children: ReactNode
+}
+
+export const StoreProvider = ({ children }: Props) => {
+  const storeRef = useRef<AppStore | null>(null)
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore()
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>
+}
