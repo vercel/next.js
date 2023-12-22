59b64aad0195d0e27651e6c1241d6762a23d429c
Arya Emami <aryaemami59@yahoo.com>
2023-12-22 03:42:32 -0600

    Add `Quotes.tsx` component file

---
+++ examples/with-redux/app/components/Quotes/Quotes.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { useGetQuotesQuery } from '@/lib/features/quotes/quotesApiSlice'
+import { useState } from 'react'
+import styles from './quotes.module.css'
+
+export const Quotes = () => {
+  const [numberOfQuotes, setNumberOfQuotes] = useState(10)
+  const { data, isError, isLoading, isSuccess } =
+    useGetQuotesQuery(numberOfQuotes)
+
+  if (isError) {
+    return (
+      <div>
+        <h1>There was an error!!!</h1>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    return (
+      <div className={styles.container}>
+        <h3>Select the Quantity of Quotes to Fetch:</h3>
+        <select
+          className={styles.select}
+          value={numberOfQuotes}
+          onChange={(e) => setNumberOfQuotes(Number(e.target.value))}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+        </select>
+        {data.quotes.map(({ author, quote, id }) => (
+          <blockquote key={id}>
+            "{quote}"
+            <footer>
+              <cite>{author}</cite>
+            </footer>
+          </blockquote>
+        ))}
+      </div>
+    )
+  }
+
+  return null
+}
