ecd908d2d2555b65b1a4253fdbc5e637be43c56d
Arya Emami <aryaemami59@yahoo.com>
2023-12-24 08:30:29 -0600

    Add `combineSlices`

--- examples/with-redux/lib/store.ts
+++ examples/with-redux/lib/store.ts
@@ -1,14 +1,15 @@
 import type { Action, ThunkAction } from '@reduxjs/toolkit'
-import { configureStore } from '@reduxjs/toolkit'
+import { combineSlices, configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './features/counter/counterSlice'
 import { quotesApiSlice } from './features/quotes/quotesApiSlice'
 
+// `combineSlices` automatically combines the reducers using
+// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
+const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+
 export const makeStore = () => {
   return configureStore({
-    reducer: {
-      [counterSlice.reducerPath]: counterSlice.reducer,
-      [quotesApiSlice.reducerPath]: quotesApiSlice.reducer,
-    },
+    reducer: rootReducer,
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: (getDefaultMiddleware) => {
