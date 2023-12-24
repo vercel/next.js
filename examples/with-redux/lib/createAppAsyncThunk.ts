b9894c0e2d64f2fbbe1e2fe623aaebcca4a564be
Arya Emami <aryaemami59@yahoo.com>
2023-12-24 00:54:10 -0600

    slightly modify JSDocs for `createAppAsyncThunk`

--- examples/with-redux/lib/createAppAsyncThunk.ts
+++ examples/with-redux/lib/createAppAsyncThunk.ts
@@ -3,10 +3,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import type { AppDispatch, RootState } from './store'
 
 /**
- * A utility function that provides a pre-typed version of `createAsyncThunk`
+ * A utility function that provides a pre-typed version of {@linkcode createAsyncThunk}
  * that has the types for `state` and `dispatch` built in.
  * This lets you set up those types once, so you don't have to repeat
- * them each time you call `createAsyncThunk`.
+ * them each time you call {@linkcode createAsyncThunk}.
  */
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
   state: RootState
