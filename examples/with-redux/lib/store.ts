65766e72e0ba789c38da6273c5cd4451d51a0693
Arya Emami <aryaemami59@yahoo.com>
2023-12-24 09:54:34 -0600

    Rename generic type parameter `ReturnType` in `AppThunk` to avoid confusion with `ReturnType` the utility type

--- examples/with-redux/lib/store.ts
+++ examples/with-redux/lib/store.ts
@@ -23,8 +23,8 @@ export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
   RootState,
   unknown,
   Action
