4af4eaa1d57b9fb73a9bd8d189249f59acbe9e09
Vordgi <sasha2822222@gmail.com>
2023-12-26 22:37:02 +0400

    not-found-params add potential changes

--- packages/next/src/server/app-render/create-component-tree.tsx
+++ packages/next/src/server/app-render/create-component-tree.tsx
@@ -250,7 +250,7 @@ export async function createComponentTree({
               {layerAssets}
               <RootLayoutComponent>
                 {notFoundStyles}
-                <NotFoundComponent />
+                <NotFoundComponent params={currentParams} />
               </RootLayoutComponent>
             </>
           }
@@ -327,7 +327,9 @@ export async function createComponentTree({
         const parallelRoute = parallelRoutes[parallelRouteKey]
 
         const notFoundComponent =
-          NotFound && isChildrenRouteKey ? <NotFound /> : undefined
+          NotFound && isChildrenRouteKey ? (
+            <NotFound params={currentParams} />
+          ) : undefined
 
         // if we're prefetching and that there's a Loading component, we bail out
         // otherwise we keep rendering for the prefetch.
@@ -486,7 +488,7 @@ export async function createComponentTree({
             <meta name="next-error" content="not-found" />
           )}
           {notFoundStyles}
-          <NotFound />
+          <NotFound params={currentParams} />
         </>
       ),
     }
