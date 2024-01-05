ff3dd1938abedcf9d58cfceb35138868a328108d
Jiachi Liu <inbox@huozhi.im>
2024-01-05 17:06:58 +0100

    bypass webpack://

--- packages/react-dev-overlay/src/middleware.ts
+++ packages/react-dev-overlay/src/middleware.ts
@@ -320,8 +320,9 @@ function getOverlayMiddleware(options: OverlayMiddlewareOptions) {
 
       if (
         !(
-          (frame.file?.startsWith('webpack-internal:///') ||
-            frame.file?.startsWith('file://')) &&
+          (frame.file?.startsWith('webpack-internal://') ||
+            frame.file?.startsWith('file://') ||
+            frame.file?.startsWith('webpack://')) &&
           Boolean(parseInt(frame.lineNumber?.toString() ?? '', 10))
         )
       ) {
@@ -331,11 +332,11 @@ function getOverlayMiddleware(options: OverlayMiddlewareOptions) {
       }
 
       const moduleId: string = frame.file.replace(
-        /^(webpack-internal:\/\/\/|file:\/\/)/,
+        /webpack-internal:(\/)+|file:\/\//,
         ''
       )
       const modulePath = frame.file.replace(
-        /^(webpack-internal:\/\/\/|file:\/\/)(\(.*\)\/)?/,
+        /webpack-internal:(\/)+|file:\/\/(\(.*\)\/)?/,
         ''
       )
 
