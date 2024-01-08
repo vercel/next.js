60750e8e1927a5fb2b02e155960d810206fd16f8
Jiachi Liu <inbox@huozhi.im>
2024-01-08 18:56:48 +0100

    simplify

--- packages/next/src/lib/picocolors.ts
+++ packages/next/src/lib/picocolors.ts
@@ -37,42 +37,40 @@ const replaceClose = (
     : start + end
 }
 
-const formatter =
-  (open: string, close: string, replace = open) =>
-  (input: string) => {
+const formatter = (open: string, close: string, replace = open) => {
+  if (!enabled) return String
+  return (input: string) => {
     const string = '' + input
     const index = string.indexOf(close, open.length)
     return ~index
       ? open + replaceClose(string, close, replace, index) + close
       : open + string + close
   }
-
-const color = (...args: Parameters<typeof formatter>) =>
-  enabled ? formatter(...args) : String
+}
 
 export const reset = enabled ? (s: string) => `\x1b[0m${s}\x1b[0m` : String
-export const bold = color('\x1b[1m', '\x1b[22m', '\x1b[22m\x1b[1m')
-export const dim = color('\x1b[2m', '\x1b[22m', '\x1b[22m\x1b[2m')
-export const italic = color('\x1b[3m', '\x1b[23m')
-export const underline = color('\x1b[4m', '\x1b[24m')
-export const inverse = color('\x1b[7m', '\x1b[27m')
-export const hidden = color('\x1b[8m', '\x1b[28m')
-export const strikethrough = color('\x1b[9m', '\x1b[29m')
-export const black = color('\x1b[30m', '\x1b[39m')
-export const red = color('\x1b[31m', '\x1b[39m')
-export const green = color('\x1b[32m', '\x1b[39m')
-export const yellow = color('\x1b[33m', '\x1b[39m')
-export const blue = color('\x1b[34m', '\x1b[39m')
-export const magenta = color('\x1b[35m', '\x1b[39m')
-export const purple = color('\x1b[38;2;173;127;168m', '\x1b[39m')
-export const cyan = color('\x1b[36m', '\x1b[39m')
-export const white = color('\x1b[37m', '\x1b[39m')
-export const gray = color('\x1b[90m', '\x1b[39m')
-export const bgBlack = color('\x1b[40m', '\x1b[49m')
-export const bgRed = color('\x1b[41m', '\x1b[49m')
-export const bgGreen = color('\x1b[42m', '\x1b[49m')
-export const bgYellow = color('\x1b[43m', '\x1b[49m')
-export const bgBlue = color('\x1b[44m', '\x1b[49m')
-export const bgMagenta = color('\x1b[45m', '\x1b[49m')
-export const bgCyan = color('\x1b[46m', '\x1b[49m')
-export const bgWhite = color('\x1b[47m', '\x1b[49m')
+export const bold = formatter('\x1b[1m', '\x1b[22m', '\x1b[22m\x1b[1m')
+export const dim = formatter('\x1b[2m', '\x1b[22m', '\x1b[22m\x1b[2m')
+export const italic = formatter('\x1b[3m', '\x1b[23m')
+export const underline = formatter('\x1b[4m', '\x1b[24m')
+export const inverse = formatter('\x1b[7m', '\x1b[27m')
+export const hidden = formatter('\x1b[8m', '\x1b[28m')
+export const strikethrough = formatter('\x1b[9m', '\x1b[29m')
+export const black = formatter('\x1b[30m', '\x1b[39m')
+export const red = formatter('\x1b[31m', '\x1b[39m')
+export const green = formatter('\x1b[32m', '\x1b[39m')
+export const yellow = formatter('\x1b[33m', '\x1b[39m')
+export const blue = formatter('\x1b[34m', '\x1b[39m')
+export const magenta = formatter('\x1b[35m', '\x1b[39m')
+export const purple = formatter('\x1b[38;2;173;127;168m', '\x1b[39m')
+export const cyan = formatter('\x1b[36m', '\x1b[39m')
+export const white = formatter('\x1b[37m', '\x1b[39m')
+export const gray = formatter('\x1b[90m', '\x1b[39m')
+export const bgBlack = formatter('\x1b[40m', '\x1b[49m')
+export const bgRed = formatter('\x1b[41m', '\x1b[49m')
+export const bgGreen = formatter('\x1b[42m', '\x1b[49m')
+export const bgYellow = formatter('\x1b[43m', '\x1b[49m')
+export const bgBlue = formatter('\x1b[44m', '\x1b[49m')
+export const bgMagenta = formatter('\x1b[45m', '\x1b[49m')
+export const bgCyan = formatter('\x1b[46m', '\x1b[49m')
+export const bgWhite = formatter('\x1b[47m', '\x1b[49m')
