74e196dadcfdae7f3c7d44ec892aa52af1bb83c4
Anton Novik <tehbi4.dev@gmail.com>
2024-01-08 15:07:47 +0300

    rm some files

--- examples/with-expo-typescript/next.config.js
+++
@@ -1,8 +0,0 @@
-// @generated: @expo/next-adapter@2.1.9
-// Learn more: https://github.com/expo/expo/blob/master/docs/pages/guides/using-nextjs.md
-
-const { withExpo } = require('@expo/next-adapter')
-
-module.exports = withExpo({
-  projectRoot: __dirname,
-})
--- examples/with-expo-typescript/pages/_document.tsx
+++
@@ -1,2 +0,0 @@
-// @generated: @expo/next-adapter@2.1.9
-export { default } from '@expo/next-adapter/document'
