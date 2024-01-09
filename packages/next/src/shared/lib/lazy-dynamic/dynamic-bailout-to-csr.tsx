d042d4836d1060b548222326536e0b3e34e1ff1a
Balázs Orbán <info@balazsorban.com>
2024-01-09 23:24:03 +0100

    add comment to address review

--- packages/next/src/shared/lib/lazy-dynamic/dynamic-bailout-to-csr.tsx
+++ packages/next/src/shared/lib/lazy-dynamic/dynamic-bailout-to-csr.tsx
@@ -8,6 +8,10 @@ interface BailoutToCSRProps {
   children: ReactElement
 }
 
+/**
+ * If rendered on the server, this component throws an error
+ * to signal to Next.js to bail out to client-side rendering instead.
+ */
 export function BailoutToCSR({ reason, children }: BailoutToCSRProps) {
   if (typeof window === 'undefined') {
     throw new BailoutToCSRError(reason)
