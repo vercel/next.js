db1be4ec21470ac370e16e267af191f6ba402d56
Wyatt Johnson <accounts+github@wyattjoh.ca>
2024-01-11 08:13:02 -0700

    Reapply "feat(app-router): introduce `experimental.missingSuspenseWithCSRBailout` flag" (#60508)
    This reverts commit 9cb0387681fb4ee928ab6056e3215afe08bd39d3.

--- test/e2e/app-dir/app-static/next.config.js
+++ test/e2e/app-dir/app-static/next.config.js
@@ -13,7 +13,7 @@ module.exports = {
       afterFiles: [
         {
           source: '/rewritten-use-search-params',
-          destination: '/hooks/use-search-params/static-bailout',
+          destination: '/hooks/use-search-params/with-suspense',
         },
         {
           source: '/rewritten-use-pathname',
