b32260538ab0683ca0caac46336b470242a30b90
OJ Kwon <1210596+kwonoj@users.noreply.github.com.>
2024-01-11 11:35:55 -0800

    feat(next-core): apply transform rules

--- packages/next-swc/crates/next-core/src/next_shared/transforms/next_cjs_optimizer.rs
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/next_cjs_optimizer.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use next_custom_transforms::transforms::cjs_optimizer::{cjs_optimizer, Config};
+use next_custom_transforms::transforms::cjs_optimizer::{cjs_optimizer, Config, PackageConfig};
+use rustc_hash::FxHashMap;
 use turbo_tasks::Vc;
 use turbopack_binding::{
     swc::core::{
@@ -15,7 +16,40 @@ use turbopack_binding::{
 
 use super::module_rule_match_js_no_url;
 
-pub fn get_next_cjs_optimizer_rule(enable_mdx_rs: bool, config: Config) -> ModuleRule {
+pub fn get_next_cjs_optimizer_rule(enable_mdx_rs: bool) -> ModuleRule {
+    // [NOTE]: This isn't user configurable config
+    // (https://github.com/vercel/next.js/blob/a1d0259ea06592c5ca6df882e9b1d0d0121c5083/packages/next/src/build/swc/options.ts#L395)
+    // build it internally without accepting customization.
+    let config = Config {
+        packages: FxHashMap::from_iter([(
+            "next/server".to_string(),
+            PackageConfig {
+                transforms: FxHashMap::from_iter([
+                    (
+                        "NextRequest".into(),
+                        "next/dist/server/web/spec-extension/request".into(),
+                    ),
+                    (
+                        "NextResponse".into(),
+                        "next/dist/server/web/spec-extension/response".into(),
+                    ),
+                    (
+                        "ImageResponse".into(),
+                        "next/dist/server/web/spec-extension/image-response".into(),
+                    ),
+                    (
+                        "userAgentFromString".into(),
+                        "next/dist/server/web/spec-extension/user-agent".into(),
+                    ),
+                    (
+                        "userAgent".into(),
+                        "next/dist/server/web/spec-extension/user-agent".into(),
+                    ),
+                ]),
+            },
+        )]),
+    };
+
     let transformer =
         EcmascriptInputTransform::Plugin(Vc::cell(Box::new(NextCjsOptimizer { config }) as _));
     ModuleRule::new(
