5d72bfe87dc32372c4d17acdc15ce3428dbaa077
lijianan <574980606@qq.com>
2024-01-12 06:37:46 +0800

    docs: update docs

--- examples/with-ant-design/app/AntdRegistry.tsx
+++ examples/with-ant-design/app/AntdRegistry.tsx
@@ -5,10 +5,6 @@ import { useServerInsertedHTML } from 'next/navigation'
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs'
 import type Entity from '@ant-design/cssinjs/es/Cache'
 
-/**
- * @descCN 注意：当你安装 `@ant-design/cssinjs` 时，必须确保版本号跟 `antd` 的 `node_modules` 中的 `@ant-design/cssinjs` 版本保持一致，否则会出现多个 React 实例，导致无法正确的读取 ctx。
- * @descEN Notes: when you install `@ant-design/cssinjs`, you must ensure that the version is consistent with the version of `@ant-design/cssinjs` in `node_modules` of `antd`, otherwise, multiple React instances will appear, resulting in ctx being unable to be read correctly.
- */
 const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
   const cache = React.useMemo<Entity>(() => createCache(), [])
   const isServerInserted = React.useRef<boolean>(false);
