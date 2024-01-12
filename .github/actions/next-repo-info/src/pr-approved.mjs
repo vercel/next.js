b2dd254356dfe650d7a6a85c517bff44107050a7
Sam Ko <sam@vercel.com>
2024-01-11 18:54:18 -0800

    fix: fix repo name

--- .github/actions/next-repo-info/src/pr-approved.mjs
+++ .github/actions/next-repo-info/src/pr-approved.mjs
@@ -20,7 +20,7 @@ async function run() {
 
     if (pendingPRs) {
       await slackClient.chat.postMessage({
-        channel: '#next-js-repo-udpates',
+        channel: '#next-js-repo-updates',
         text: `Pending PRs for Next.js: There are <https://github.com/vercel/next.js/pulls?q=is%3Apr+is%3Aopen+review%3Aapproved+-is%3Adraft|${prs.data.items.length} PRs> awaiting merge.`,
         username: 'GitHub',
         icon_emoji: ':github:',
