f768037c2f045e2c2e0ee7374a16867af03915d9
Arya Emami <aryaemami59@yahoo.com>
2024-01-12 18:55:59 -0600

    Add `hooks.withTypes()` to `hooks.ts`

--- examples/with-redux/lib/hooks.ts
+++ examples/with-redux/lib/hooks.ts
@@ -1,8 +1,7 @@
-import type { TypedUseSelectorHook } from 'react-redux'
 import { useDispatch, useSelector, useStore } from 'react-redux'
 import type { AppDispatch, AppStore, RootState } from './store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppStore: () => AppStore = useStore
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppStore = useStore.withTypes<AppStore>()
