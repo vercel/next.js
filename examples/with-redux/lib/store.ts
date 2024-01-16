cd81f860337302ae2f3c6fbdfff278dc474a647d
Arya Emami <aryaemami59@yahoo.com>
2024-01-16 12:56:59 -0600

    Make setting up types resemble other Redux templates for consistency

--- examples/with-redux/lib/store.ts
+++ examples/with-redux/lib/store.ts
@@ -6,6 +6,8 @@ import { quotesApiSlice } from './features/quotes/quotesApiSlice'
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 const rootReducer = combineSlices(counterSlice, quotesApiSlice)
+// Infer the `RootState` type from the root reducer
+export type RootState = ReturnType<typeof rootReducer>
 
 // `makeStore` encapsulates the store configuration to allow
 // creating unique store instances, which is particularly important for
@@ -24,8 +26,7 @@ export const makeStore = () => {
 
 // Infer the return type of `makeStore`
 export type AppStore = ReturnType<typeof makeStore>
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
+// Infer the `AppDispatch` type from the store itself
 export type AppDispatch = AppStore['dispatch']
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
