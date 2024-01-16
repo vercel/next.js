732de306658166ca46f997bc776b354c6d383d03
Arya Emami <aryaemami59@yahoo.com>
2024-01-16 12:52:05 -0600

    Add explanation comment above `makeStore`

--- examples/with-redux/lib/store.ts
+++ examples/with-redux/lib/store.ts
@@ -7,6 +7,10 @@ import { quotesApiSlice } from './features/quotes/quotesApiSlice'
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
 const rootReducer = combineSlices(counterSlice, quotesApiSlice)
 
+// `makeStore` encapsulates the store configuration to allow
+// creating unique store instances, which is particularly important for
+// server-side rendering (SSR) scenarios. In SSR, separate store instances
+// are needed for each request to prevent cross-request state pollution.
 export const makeStore = () => {
   return configureStore({
     reducer: rootReducer,
