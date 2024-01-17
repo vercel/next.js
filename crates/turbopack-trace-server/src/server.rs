c0b0badbc4318ccd9a3bcc7ad5818781a0da4acb
Tobias Koppers <tobias.koppers@googlemail.com>
2024-01-17 18:11:54 +0100

    Bottom up tracing and some tracing improvements (vercel/turbo#6923)
    ### Description
    
    Add bottom up view to tracing
    Tracing refactorings
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->
    
    
    Closes PACK-2198

--- crates/turbopack-trace-server/src/server.rs
+++ crates/turbopack-trace-server/src/server.rs
@@ -20,7 +20,7 @@ use crate::{
     store::SpanId,
     store_container::StoreContainer,
     u64_string,
-    viewer::{ViewLineUpdate, ViewMode, Viewer},
+    viewer::{Update, ViewLineUpdate, ViewMode, Viewer},
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -33,6 +33,7 @@ pub enum ServerToClientMessage {
     },
     ViewLinesCount {
         count: usize,
+        max: u64,
     },
     #[serde(rename_all = "camelCase")]
     QueryResult {
@@ -44,6 +45,8 @@ pub enum ServerToClientMessage {
         duration: u64,
         allocations: u64,
         deallocations: u64,
+        allocation_count: u64,
+        persistent_allocations: u64,
         args: Vec<(String, String)>,
         path: Vec<String>,
     },
@@ -161,14 +164,17 @@ pub fn serve(store: Arc<StoreContainer>) -> Result<()> {
                         return Ok(());
                     }
                     state.last_update_generation = store.generation();
-                    let updates = state.viewer.compute_update(&store, &state.view_rect);
+                    let Update {
+                        lines: updates,
+                        max,
+                    } = state.viewer.compute_update(&store, &state.view_rect);
                     let count = updates.len();
                     for update in updates {
                         let message = ServerToClientMessage::ViewLine { update };
                         let message = serde_json::to_string(&message).unwrap();
                         state.writer.send_message(&OwnedMessage::Text(message))?;
                     }
-                    let message = ServerToClientMessage::ViewLinesCount { count };
+                    let message = ServerToClientMessage::ViewLinesCount { count, max };
                     let message = serde_json::to_string(&message).unwrap();
                     state.writer.send_message(&OwnedMessage::Text(message))?;
                     ready_for_update.store(false, Ordering::SeqCst);
@@ -212,39 +218,36 @@ pub fn serve(store: Arc<StoreContainer>) -> Result<()> {
                                     )?;
                                 }
                                 ClientToServerMessage::ViewMode { id, mode, inherit } => {
-                                    match mode.as_str() {
+                                    let (mode, sorted) =
+                                        if let Some(mode) = mode.strip_suffix("-sorted") {
+                                            (mode, true)
+                                        } else {
+                                            (mode.as_str(), false)
+                                        };
+                                    match mode {
                                         "raw-spans" => {
                                             state.viewer.set_view_mode(
                                                 id,
-                                                Some((
-                                                    ViewMode::RawSpans { sorted: false },
-                                                    inherit,
-                                                )),
-                                            );
-                                        }
-                                        "raw-spans-sorted" => {
-                                            state.viewer.set_view_mode(
-                                                id,
-                                                Some((
-                                                    ViewMode::RawSpans { sorted: true },
-                                                    inherit,
-                                                )),
+                                                Some((ViewMode::RawSpans { sorted }, inherit)),
                                             );
                                         }
                                         "aggregated" => {
                                             state.viewer.set_view_mode(
                                                 id,
-                                                Some((
-                                                    ViewMode::Aggregated { sorted: false },
-                                                    inherit,
-                                                )),
+                                                Some((ViewMode::Aggregated { sorted }, inherit)),
                                             );
                                         }
-                                        "aggregated-sorted" => {
+                                        "bottom-up" => {
+                                            state.viewer.set_view_mode(
+                                                id,
+                                                Some((ViewMode::BottomUp { sorted }, inherit)),
+                                            );
+                                        }
+                                        "aggregated-bottom-up" => {
                                             state.viewer.set_view_mode(
                                                 id,
                                                 Some((
-                                                    ViewMode::Aggregated { sorted: true },
+                                                    ViewMode::AggregatedBottomUp { sorted },
                                                     inherit,
                                                 )),
                                             );
@@ -278,6 +281,9 @@ pub fn serve(store: Arc<StoreContainer>) -> Result<()> {
                                         let duration = span.corrected_total_time();
                                         let allocations = span.total_allocations();
                                         let deallocations = span.total_deallocations();
+                                        let allocation_count = span.total_allocation_count();
+                                        let persistent_allocations =
+                                            span.total_persistent_allocations();
                                         let args = span
                                             .args()
                                             .map(|(k, v)| (k.to_string(), v.to_string()))
@@ -297,6 +303,8 @@ pub fn serve(store: Arc<StoreContainer>) -> Result<()> {
                                             duration,
                                             allocations,
                                             deallocations,
+                                            allocation_count,
+                                            persistent_allocations,
                                             args,
                                             path,
                                         }
@@ -309,6 +317,8 @@ pub fn serve(store: Arc<StoreContainer>) -> Result<()> {
                                             duration: 0,
                                             allocations: 0,
                                             deallocations: 0,
+                                            allocation_count: 0,
+                                            persistent_allocations: 0,
                                             args: Vec::new(),
                                             path: Vec::new(),
                                         }
