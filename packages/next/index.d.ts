f5526a9e34b12690c3bf5fb892b9f398e2d4a54d
Balázs Orbán <info@balazsorban.com>
2024-01-18 12:23:16 +0100

    sort

--- packages/next/index.d.ts
+++ packages/next/index.d.ts
@@ -1,8 +1,10 @@
+/// <reference types="./types/global" />
+/// <reference types="./types/compiled" />
+/// <reference path="./dist/styled-jsx/types/global.d.ts" />
 /// <reference path="./amp.d.ts" />
 /// <reference path="./app.d.ts" />
 /// <reference path="./cache.d.ts" />
 /// <reference path="./config.d.ts" />
-/// <reference path="./dist/styled-jsx/types/global.d.ts" />
 /// <reference path="./document.d.ts" />
 /// <reference path="./dynamic.d.ts" />
 /// <reference path="./error.d.ts" />
@@ -14,8 +16,6 @@
 /// <reference path="./router.d.ts" />
 /// <reference path="./script.d.ts" />
 /// <reference path="./server.d.ts" />
-/// <reference types="./types/compiled" />
-/// <reference types="./types/global" />
 
 export { default } from './types'
 export * from './types'
