94fd4fe2db504af3b45a54714fd9f29e48b1b368
Tim Neutkens <tim@timneutkens.nl>
2024-01-18 10:59:07 +0100

    Fix http (NaN:NaN)

--- test/development/acceptance-app/component-stack.test.ts
+++ test/development/acceptance-app/component-stack.test.ts
@@ -25,56 +25,32 @@ describe('Component Stack in error overlay', () => {
         Component
         main
         InnerLayoutRouter
-        http (NaN:NaN)
         RedirectErrorBoundary
-        http (NaN:NaN)
         RedirectBoundary
-        http (NaN:NaN)
         NotFoundErrorBoundary
-        http (NaN:NaN)
         NotFoundBoundary
-        http (NaN:NaN)
         LoadingBoundary
-        http (NaN:NaN)
         ErrorBoundary
-        http (NaN:NaN)
         InnerScrollAndFocusHandler
-        http (NaN:NaN)
         ScrollAndFocusHandler
-        http (NaN:NaN)
         RenderFromTemplateContext
-        http (NaN:NaN)
         OuterLayoutRouter
-        http (NaN:NaN)
         body
         html
         RedirectErrorBoundary
-        http (NaN:NaN)
         RedirectBoundary
-        http (NaN:NaN)
         NotFoundErrorBoundary
-        http (NaN:NaN)
         NotFoundBoundary
-        http (NaN:NaN)
         DevRootNotFoundBoundary
-        http (NaN:NaN)
         ReactDevOverlay
-        http (NaN:NaN)
         HotReload
-        http (NaN:NaN)
         Router
-        http (NaN:NaN)
         ErrorBoundaryHandler
-        http (NaN:NaN)
         ErrorBoundary
-        http (NaN:NaN)
         AppRouter
-        http (NaN:NaN)
         ServerRoot
-        http (NaN:NaN)
         RSCComponent
-        Root
-        http (NaN:NaN)"
+        Root"
       `)
     } else {
       expect(await session.getRedboxComponentStack()).toMatchInlineSnapshot(`
