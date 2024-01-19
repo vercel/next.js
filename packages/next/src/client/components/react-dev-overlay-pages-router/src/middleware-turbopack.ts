d0cee511c73ad2450e0bf5300652e1ec1a330647
Tim Neutkens <tim@timneutkens.nl>
2024-01-19 15:55:04 +0100

    Update codeframe usage

--- packages/next/src/client/components/react-dev-overlay-pages-router/src/middleware-turbopack.ts
+++ packages/next/src/client/components/react-dev-overlay-pages-router/src/middleware-turbopack.ts
@@ -4,7 +4,7 @@ import type { OriginalStackFrameResponse } from './middleware'
 
 import fs, { constants as FS } from 'fs/promises'
 import url from 'url'
-import { codeFrameColumns } from '@babel/code-frame'
+import { codeFrameColumns } from 'next/dist/compiled/babel/code-frame'
 import { launchEditor } from './internal/helpers/launchEditor'
 
 interface Project {
