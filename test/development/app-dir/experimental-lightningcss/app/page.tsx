c1932f6975f988071735fb619e67770f62cbad54
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-20 15:00:01 +0900

    Harden test

--- test/development/app-dir/experimental-lightningcss/app/page.tsx
+++ test/development/app-dir/experimental-lightningcss/app/page.tsx
@@ -5,6 +5,9 @@ export default function Page() {
   return (
     <>
       <p className={`search-keyword ${styles.blue}`}>hello world</p>
+      <p className={`${styles.blue}`}>
+        <div className="nested">Red due to nesting</div>
+      </p>
       <div className="red-text">This text should be red.</div>
     </>
   )
