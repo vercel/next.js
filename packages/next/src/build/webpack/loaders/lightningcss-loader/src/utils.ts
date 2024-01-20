3bb1003c9403c5d5516ca4bbfe0a236c1b0cd25f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-20 15:08:05 +0900

    Fix build

--- packages/next/src/build/webpack/loaders/lightningcss-loader/src/utils.ts
+++ packages/next/src/build/webpack/loaders/lightningcss-loader/src/utils.ts
@@ -3,11 +3,11 @@ import type { Targets } from '../../../../swc/lightningcss/targets'
 import type { ECacheKey } from './interface'
 
 let targetsCache: Record<string, Targets> = {}
-export const getTargets = (opts: { targets: string[]; key: ECacheKey }) => {
+export const getTargets = (opts: { targets?: string[]; key: ECacheKey }) => {
   const cache = targetsCache[opts.key]
   if (cache) {
     return cache
   }
 
-  return (targetsCache[opts.key] = browserslistToTargets(opts.targets))
+  return (targetsCache[opts.key] = browserslistToTargets(opts.targets ?? []))
 }
