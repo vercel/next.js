215417bccaa03397a79bd42352ef735df90bc936
Jiachi Liu <inbox@huozhi.im>
2024-01-22 14:54:02 +0100

    Update the start phase

--- test/e2e/next-phase/index.test.ts
+++ test/e2e/next-phase/index.test.ts
@@ -11,7 +11,7 @@ createNextDescribe(
       'pages/foo.js': `export default function Page() { return <p>{'pages'}</p> }`,
       'next.config.js': `
         module.exports = (phase, { defaultConfig }) => {
-          console.log('phase:', phase)
+          console.log(phase)
           return defaultConfig
         }
       `,
@@ -22,6 +22,7 @@ createNextDescribe(
       const phases = {
         dev: 'phase-development-server',
         build: 'phase-production-build',
+        start: 'phase-production-server',
       }
       const currentPhase = isNextDev ? phases.dev : phases.build
       const nonExistedPhase = isNextDev ? phases.build : phases.dev
@@ -33,9 +34,9 @@ createNextDescribe(
       await next.fetch('/foo')
 
       if (isNextDev) {
-        expect(next.cliOutput).not.toContain('phase-production-server')
+        expect(next.cliOutput).not.toContain(phases.start)
       } else {
-        expect(next.cliOutput).toContain('phase-production-server')
+        expect(next.cliOutput).toContain(phases.start)
       }
     })
   }
