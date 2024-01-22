71c0c56ff5f0b936e426c3dfb7df9b618a408392
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-22 22:09:21 +0900

    Revert "Update test refs"
    This reverts commit 5f9013f5ce6ec79fbe582eea58faa4ce3861cee8.

--- test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
+++ test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
@@ -343,7 +343,7 @@ createNextDescribe(
       expect(head).toMatch(/{color:(\s*)#ff69b4;?}/) // styled-jsx/css
 
       // from styled-components
-      expect(head).toMatch(/{color:(\s*)#00f;?}/)
+      expect(head).toMatch(/{color:(\s*)blue;?}/)
     })
 
     it('should render initial styles of css-in-js in edge SSR correctly', async () => {
@@ -355,7 +355,7 @@ createNextDescribe(
       expect(head).toMatch(/{color:(\s*)#ff69b4;?}/) // styled-jsx/css
 
       // from styled-components
-      expect(head).toMatch(/{color:(\s*)#00f;?}/)
+      expect(head).toMatch(/{color:(\s*)blue;?}/)
     })
 
     it('should render css-in-js suspense boundary correctly', async () => {
