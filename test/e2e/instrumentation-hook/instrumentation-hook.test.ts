5858313f22e57dab607829b1c12c0c843b033b95
Jiachi Liu <inbox@huozhi.im>
2024-01-22 17:11:39 +0100

    fix test

--- test/e2e/instrumentation-hook/instrumentation-hook.test.ts
+++ test/e2e/instrumentation-hook/instrumentation-hook.test.ts
@@ -21,11 +21,6 @@ describe('Instrumentation Hook', () => {
   //   'with-esm-import',
   //   {
   //     files: path.join(__dirname, 'with-esm-import'),
-  //     nextConfig: {
-  //       experimental: {
-  //         instrumentationHook: true,
-  //       },
-  //     },
   //     dependencies: {
   //       // This test is mostly for compatibility with this package
   //       '@vercel/otel': 'latest',
