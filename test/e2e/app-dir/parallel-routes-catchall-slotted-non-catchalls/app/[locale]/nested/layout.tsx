0c35ee20ba608b986f5e5dbbe4b4a973fce2064c
Willi#m ⬣ <william@vercel.com>
2024-01-23 10:35:26 +0800

    Update test/e2e/app-dir/parallel-routes-catchall-slotted-non-catchalls/app/[locale]/nested/layout.tsx
    Co-authored-by: Zack Tanner <zacktanner@gmail.com>

--- test/e2e/app-dir/parallel-routes-catchall-slotted-non-catchalls/app/[locale]/nested/layout.tsx
+++ test/e2e/app-dir/parallel-routes-catchall-slotted-non-catchalls/app/[locale]/nested/layout.tsx
@@ -3,7 +3,7 @@ export default function Layout({ children, slot0, slot1 }) {
     <>
       Children: <div id="nested-children">{children}</div>
       Slot0: <div id="slot0">{slot0}</div>
-      Slot0: <div id="slot1">{slot1}</div>
+      Slot1: <div id="slot1">{slot1}</div>
     </>
   )
 }
