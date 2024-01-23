622dcf1ce9afa4d7766fbda0b68afe50fbd0965f
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-23 11:29:46 +0900

    Update tests

--- test/development/pages-dir/client-navigation/rendering.ts
+++ test/development/pages-dir/client-navigation/rendering.ts
@@ -226,7 +226,10 @@ export default function (next: NextInstance, render, fetch, ctx) {
       const styleId = $('#blue-box').attr('class')
       const style = $('style')
 
-      expect(style.text().includes(`p.${styleId}{color:blue`)).toBeTruthy()
+      expect(
+        style.text().includes(`p.${styleId}{color:blue`) ||
+          style.text().includes(`p.${styleId}{color:#00f`)
+      ).toBeTruthy()
     })
 
     test('renders styled jsx external', async () => {
@@ -234,7 +237,10 @@ export default function (next: NextInstance, render, fetch, ctx) {
       const styleId = $('#blue-box').attr('class')
       const style = $('style')
 
-      expect(style.text().includes(`p.${styleId}{color:blue`)).toBeTruthy()
+      expect(
+        style.text().includes(`p.${styleId}{color:blue`) ||
+          style.text().includes(`p.${styleId}{color:#00f`)
+      ).toBeTruthy()
     })
 
     test('renders properties populated asynchronously', async () => {
