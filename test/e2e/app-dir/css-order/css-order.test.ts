5375bb93a68cf3da67b3113441992ccfd081eced
Tobias Koppers <tobias.koppers@googlemail.com>
2024-01-26 12:20:44 +0000

    make linting happy

--- test/e2e/app-dir/css-order/css-order.test.ts
+++ test/e2e/app-dir/css-order/css-order.test.ts
@@ -52,80 +52,88 @@ createNextDescribe(
   },
   ({ next, isNextDev, isTurbopack }) => {
     for (const ordering of allPairs) {
+      const name = `should load correct styles navigating back again ${ordering.join(
+        ' -> '
+      )} -> ${ordering.join(' -> ')}`
       // TODO fix this case
       const broken = isNextDev && !isTurbopack
-      ;(broken ? it.skip : it)(
-        `should load correct styles navigating back again ${ordering.join(
-          ' -> '
-        )} -> ${ordering.join(' -> ')}`,
-        async () => {
-          const start = PAGES[ordering[0]]
-          const browser = await next.browser(start.url)
-          const check = async (pageInfo) => {
-            expect(
-              await browser
-                .waitForElementByCss(pageInfo.selector)
-                .getComputedCss('color')
-            ).toBe(pageInfo.color)
-          }
-          const navigate = async (page) => {
-            await browser.waitForElementByCss('#' + page).click()
-          }
-          await check(start)
-          for (const page of ordering.slice(1)) {
-            await navigate(page)
-            await check(PAGES[page])
-          }
-          for (const page of ordering) {
-            await navigate(page)
-            await check(PAGES[page])
-          }
-        }
-      )
-    }
-    for (const ordering of allPairs) {
-      // TODO fix this case
-      const broken =
-        isNextDev &&
-        !isTurbopack &&
-        ordering.some((page) => page.includes('client'))
-      ;(broken ? it.skip : it)(
-        `should load correct styles navigating ${ordering.join(' -> ')}`,
-        async () => {
-          const start = PAGES[ordering[0]]
-          const browser = await next.browser(start.url)
-          const check = async (pageInfo) => {
-            expect(
-              await browser
-                .waitForElementByCss(pageInfo.selector)
-                .getComputedCss('color')
-            ).toBe(pageInfo.color)
-          }
-          const navigate = async (page) => {
-            await browser.waitForElementByCss('#' + page).click()
-          }
-          await check(start)
-          for (const page of ordering.slice(1)) {
-            await navigate(page)
-            await check(PAGES[page])
-          }
-        }
-      )
-    }
-    for (const [page, pageInfo] of Object.entries(PAGES)) {
-      // TODO fix this case
-      const broken = isNextDev && !isTurbopack && page.includes('client')
-      ;(broken ? it.skip : it)(
-        `should load correct styles on ${page}`,
-        async () => {
-          const browser = await next.browser(pageInfo.url)
+      if (broken) {
+        it.skip(name)
+        continue
+      }
+      it(name, async () => {
+        const start = PAGES[ordering[0]]
+        const browser = await next.browser(start.url)
+        const check = async (pageInfo) => {
           expect(
             await browser
               .waitForElementByCss(pageInfo.selector)
               .getComputedCss('color')
           ).toBe(pageInfo.color)
         }
-      )
+        const navigate = async (page) => {
+          await browser.waitForElementByCss('#' + page).click()
+        }
+        await check(start)
+        for (const page of ordering.slice(1)) {
+          await navigate(page)
+          await check(PAGES[page])
+        }
+        for (const page of ordering) {
+          await navigate(page)
+          await check(PAGES[page])
+        }
+      })
+    }
+    for (const ordering of allPairs) {
+      const name = `should load correct styles navigating ${ordering.join(
+        ' -> '
+      )}`
+      // TODO fix this case
+      const broken =
+        isNextDev &&
+        !isTurbopack &&
+        ordering.some((page) => page.includes('client'))
+      if (broken) {
+        it.skip(name)
+        continue
+      }
+      it(name, async () => {
+        const start = PAGES[ordering[0]]
+        const browser = await next.browser(start.url)
+        const check = async (pageInfo) => {
+          expect(
+            await browser
+              .waitForElementByCss(pageInfo.selector)
+              .getComputedCss('color')
+          ).toBe(pageInfo.color)
+        }
+        const navigate = async (page) => {
+          await browser.waitForElementByCss('#' + page).click()
+        }
+        await check(start)
+        for (const page of ordering.slice(1)) {
+          await navigate(page)
+          await check(PAGES[page])
+        }
+      })
+    }
+    for (const [page, pageInfo] of Object.entries(PAGES)) {
+      const name = `should load correct styles on ${page}`
+      // TODO fix this case
+      const broken = isNextDev && !isTurbopack && page.includes('client')
+      if (broken) {
+        it.skip(name)
+        continue
+      }
+      it(name, async () => {
+        const browser = await next.browser(pageInfo.url)
+        expect(
+          await browser
+            .waitForElementByCss(pageInfo.selector)
+            .getComputedCss('color')
+        ).toBe(pageInfo.color)
+      })
     }
   }
 )
