ef81b1099856bc7918dfa39e6d272cce53451eaf
Shu Ding <g@shud.in>
2024-01-27 05:15:24 -0800

    Fix Server Reference being double registered (#61244)
    When we apply `createActionProxy` twice to the same value, it currently
    errors because we can't override the ID of it.
    
    This PR makes sure that 1) when the value already have an ID defined, we
    skip setting it again; 2) all the action IDs are being tracked even for
    duplicated ones.
    
    Note that it's technically impossible to "detect" the duplication here
    (via static analyzation) and only set it once. For example:
    
    ```ts
    'use server'
    
    export async function foo () {}
    export const bar = a_value_we_dont_know_yet ? foo : async () => {}
    ```
    
    So, the compiler will always wrap `createActionProxy` for every exported
    value and assume they're different Server Actions (hence different IDs).
    With this fix, if we find that it's already defined before, we just
    return the defined reference as they're strictly identical so the ID
    does't matter.
    
    Closes #54655, closes #61183.
    
    Closes NEXT-2264

--- packages/next-swc/crates/next-custom-transforms/tests/fixture/server-actions/server/17/output.js
+++ packages/next-swc/crates/next-custom-transforms/tests/fixture/server-actions/server/17/output.js
@@ -1,4 +1,4 @@
-/* __next_internal_action_entry_do_not_use__ {"ab21efdafbe611287bc25c0462b1e0510d13e48b":"foo","ac840dcaf5e8197cb02b7f3a43c119b7a770b272":"bar"} */ import { createActionProxy } from "private-next-rsc-action-proxy";
+/* __next_internal_action_entry_do_not_use__ {"6d53ce510b2e36499b8f56038817b9bad86cabb4":"$$ACTION_0","ab21efdafbe611287bc25c0462b1e0510d13e48b":"foo","ac840dcaf5e8197cb02b7f3a43c119b7a770b272":"bar"} */ import { createActionProxy } from "private-next-rsc-action-proxy";
 import { encryptActionBoundArgs, decryptActionBoundArgs } from "private-next-rsc-action-encryption";
 export const foo = createActionProxy("6d53ce510b2e36499b8f56038817b9bad86cabb4", $$ACTION_0);
 export async function $$ACTION_0() {}
