2c3ed36bc9c600ea1187437a6927213855a0f988
Jiachi Liu <inbox@huozhi.im>
2024-01-29 19:28:34 +0100

    fix test

--- test/e2e/app-dir/ppr/ppr.test.ts
+++ test/e2e/app-dir/ppr/ppr.test.ts
@@ -9,7 +9,7 @@ createNextDescribe(
   ({ next, isNextDev, isNextStart }) => {
     it('should indicate the feature is experimental', async () => {
       await check(() => {
-        return next.cliOutput.includes('Experiments (with caution)') &&
+        return next.cliOutput.includes('Experiments (use with caution)') &&
           next.cliOutput.includes('ppr')
           ? 'success'
           : 'fail'
