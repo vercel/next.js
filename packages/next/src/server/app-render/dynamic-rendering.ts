7271d26e2aeddff5c79976a14bf7a22a55972af7
Jiachi Liu <inbox@huozhi.im>
2024-01-29 16:57:10 +0100

    Update format

--- packages/next/src/server/app-render/dynamic-rendering.ts
+++ packages/next/src/server/app-render/dynamic-rendering.ts
@@ -26,6 +26,7 @@ import React from 'react'
 import type { StaticGenerationStore } from '../../client/components/static-generation-async-storage.external'
 import { DynamicServerError } from '../../client/components/hooks-server-context'
 import { StaticGenBailoutError } from '../../client/components/static-generation-bailout'
+import { getPathname } from '../../lib/url'
 
 const hasPostpone = typeof React.unstable_postpone === 'function'
 
@@ -39,6 +40,7 @@ export function markCurrentScopeAsDynamic(
   store: StaticGenerationStore,
   expression: string
 ): void {
+  const pathname = getPathname(store.urlPathname)
   if (store.isUnstableCacheCallback) {
     // inside cache scopes marking a scope as dynamic has no effect because the outer cache scope
     // creates a cache boundary. This is subtly different from reading a dynamic data source which is
@@ -46,7 +48,7 @@ export function markCurrentScopeAsDynamic(
     return
   } else if (store.dynamicShouldError) {
     throw new StaticGenBailoutError(
-      `Route ${store.urlPathname} with \`dynamic = "error"\` couldn't be rendered statically because it used \`${expression}\`. See more info here: https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic#dynamic-rendering`
+      `Route ${pathname} with \`dynamic = "error"\` couldn't be rendered statically because it used \`${expression}\`. See more info here: https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic#dynamic-rendering`
     )
   } else if (
     // We are in a prerender (PPR enabled, during build)
@@ -57,14 +59,14 @@ export function markCurrentScopeAsDynamic(
     // This will be used by the renderer to decide whether
     // the prerender requires a resume
     store.prerenderState.hasDynamic = true
-    React.unstable_postpone(createPostponeReason(expression, store.urlPathname))
+    React.unstable_postpone(createPostponeReason(expression, pathname))
   } else {
     store.revalidate = 0
 
     if (store.isStaticGeneration) {
       // We aren't prerendering but we are generating a static page. We need to bail out of static generation
       const err = new DynamicServerError(
-        `Route couldn't be rendered statically because it used ${expression}. See more info here: https://nextjs.org/docs/messages/dynamic-server-error`
+        `Route ${pathname} couldn't be rendered statically because it used ${expression}. See more info here: https://nextjs.org/docs/messages/dynamic-server-error`
       )
       store.dynamicUsageDescription = expression
       store.dynamicUsageStack = err.stack
@@ -87,7 +89,7 @@ export function trackDynamicDataAccessed(
   store: StaticGenerationStore,
   expression: string
 ): void {
-  const pathname = store.urlPathname
+  const pathname = getPathname(store.urlPathname)
   if (store.isUnstableCacheCallback) {
     throw new Error(
       `Route ${pathname} used "${expression}" inside a function cached with "unstable_cache(...)". Accessing Dynamic data sources inside a cache scope is not supported. If you need this data inside a cached function use "${expression}" oustide of the cached function and pass the required dynamic data in as an argument. See more info here: https://nextjs.org/docs/app/api-reference/functions/unstable_cache`
@@ -149,7 +151,7 @@ export function trackDynamicFetch(
 }
 
 function createPostponeReason(expression: string, urlPathname: string) {
-  const pathname = new URL(urlPathname, 'http://n').pathname // remove queries such like `_rsc` for flight
+  const pathname = getPathname(urlPathname) // remove queries such like `_rsc` for flight
   return (
     `Route ${pathname} needs to bail out of prerendering at this point because it used ${expression}. ` +
     `React throws this special object to indicate where. It should not be caught by ` +
