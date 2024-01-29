56d17c4e775a9cf04059c5898ac45e86b9f5d3c1
Tim Neutkens <tim@timneutkens.nl>
2024-01-29 10:51:44 +0100

    Skip browserslistrc tests (#61294)
    ## What?
    
    Fixes the condition to correctly test if browserslist is applied in both
    webpack and Turbopack.
    
    Skips the test because `.browserslistrc` is not supported yet and will
    be added after stable.
    
    <!-- Thanks for opening a PR! Your contribution is much appreciated.
    To make sure your PR is handled as smoothly as possible we request that
    you follow the checklist sections below.
    Choose the right checklist for the change(s) that you're making:
    
    ## For Contributors
    
    ### Improving Documentation
    
    - Run `pnpm prettier-fix` to fix formatting issues before opening the
    PR.
    - Read the Docs Contribution Guide to ensure your contribution follows
    the docs guidelines:
    https://nextjs.org/docs/community/contribution-guide
    
    ### Adding or Updating Examples
    
    - The "examples guidelines" are followed from our contributing doc
    https://github.com/vercel/next.js/blob/canary/contributing/examples/adding-examples.md
    - Make sure the linting passes by running `pnpm build && pnpm lint`. See
    https://github.com/vercel/next.js/blob/canary/contributing/repository/linting.md
    
    ### Fixing a bug
    
    - Related issues linked using `fixes #number`
    - Tests added. See:
    https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    ### Adding a feature
    
    - Implements an existing feature request or RFC. Make sure the feature
    request has been accepted for implementation before opening a PR. (A
    discussion must be opened, see
    https://github.com/vercel/next.js/discussions/new?category=ideas)
    - Related issues/discussions are linked using `fixes #number`
    - e2e tests added
    (https://github.com/vercel/next.js/blob/canary/contributing/core/testing.md#writing-tests-for-nextjs)
    - Documentation added
    - Telemetry added. In case of a feature if it's used or not.
    - Errors have a helpful link attached, see
    https://github.com/vercel/next.js/blob/canary/contributing.md
    
    
    ## For Maintainers
    
    - Minimal description (aim for explaining to someone not on the team to
    understand the PR)
    - When linking to a Slack thread, you might want to share details of the
    conclusion
    - Link both the Linear (Fixes NEXT-xxx) and the GitHub issues
    - Add review comments if necessary to explain to the reviewer the logic
    behind a change
    
    ### What?
    
    ### Why?
    
    ### How?
    
    Closes NEXT-
    Fixes #
    
    -->
    
    
    Closes NEXT-2277

--- test/e2e/browserslist/default-target.test.ts
+++ test/e2e/browserslist/default-target.test.ts
@@ -6,21 +6,19 @@ import cheerio from 'cheerio'
 const appDir = path.join(__dirname, 'app')
 
 // TODO: This test needs to check multiple files and syntax features.
-describe.skip('Browserslist', () => {
+describe.skip('default browserslist target', () => {
   let next: NextInstance
 
   beforeAll(async () => {
     next = await createNext({
       files: {
         pages: new FileRef(path.join(appDir, 'pages')),
-        // In Chrome 45 arrow functions are introduced, by
-        '.browserslistrc': 'Chrome 42',
       },
     })
   })
   afterAll(() => next.destroy())
 
-  it('should apply browserslist target', async () => {
+  it('should apply default browserslist target', async () => {
     const html = await renderViaHTTP(next.url, '/')
     const $ = cheerio.load(html)
 
@@ -33,14 +31,16 @@ describe.skip('Browserslist', () => {
               const src = $(el).attr('src')
               const res = await fetchViaHTTP(next.url, src)
               const code = await res.text()
+              // Default compiles for ES Modules output
               if (code.includes('async ()=>')) {
                 return src
               }
               return false
             })
         )
-      ) // Filter out false values
+      )
+        // Filter out false values
         .filter((item) => item)
-    ).toBeEmpty()
+    ).not.toBeEmpty()
   })
 })
