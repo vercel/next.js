4561f495762fec497baee5e50b9ab77e4283f7f9
Adrian Hartanto <adrian.hartanto@traveloka.com>
2024-01-29 21:51:59 +0700

    set swr delta

--- packages/next/src/server/lib/revalidate.ts
+++ packages/next/src/server/lib/revalidate.ts
@@ -13,8 +13,8 @@ export function formatRevalidate(revalidate: Revalidate): string {
   if (revalidate === 0) {
     return 'private, no-cache, no-store, max-age=0, must-revalidate'
   } else if (typeof revalidate === 'number') {
-    return `s-maxage=${revalidate}, stale-while-revalidate`
+    return `s-maxage=${revalidate}, stale-while-revalidate=${CACHE_ONE_YEAR}`
   }
 
-  return `s-maxage=${CACHE_ONE_YEAR}, stale-while-revalidate`
+  return `s-maxage=${CACHE_ONE_YEAR}, stale-while-revalidate=${CACHE_ONE_YEAR}`
 }
