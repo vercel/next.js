181891fcbb0ca0a562e1e155dacd71c39b00c993
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-01-29 11:38:46 +0900

    next.config.js

--- test/integration/css/test/css-compilation.test.js
+++ test/integration/css/test/css-compilation.test.js
@@ -27,19 +27,14 @@ describe('CSS Support', () => {
       describe.each([true, false])(`useLightnincsss(%s)`, (useLightningcss) => {
         beforeAll(async () => {
           nextConfig.write(
-            `module.exports = {
-              onDemandEntries: {
-                // Make sure entries are not getting disposed.
-                maxInactiveAge: 1000 * 60 * 60,
-              },
-              webpack(cfg) {
-                cfg.devtool = 'source-map'
-                return cfg
-              },
-              experimental: {
-                useLightningcss: ${useLightningcss}
-              }
-            }`
+            `
+const config = require('../next.config.js');
+module.exports = {
+  ..config
+  experimental: {
+    useLightningcss: ${useLightningcss}
+  }
+}`
           )
         })
 
@@ -96,158 +91,230 @@ describe('CSS Support', () => {
 
     describe('React Lifecyce Order (production)', () => {
       const appDir = join(fixturesDir, 'transition-react')
+      const nextConfig = new File(join(appDir, 'next.config.js'))
       beforeAll(async () => {
         await remove(join(appDir, '.next'))
       })
 
-      let appPort
-      let app
-      let code
-      let stdout
-      beforeAll(async () => {
-        ;({ code, stdout } = await nextBuild(appDir, [], {
-          stdout: true,
-        }))
-        appPort = await findPort()
-        app = await nextStart(appDir, appPort)
-      })
-      afterAll(async () => {
-        await killApp(app)
-      })
+      describe.each([true, false])(`useLightnincsss(%s)`, (useLightningcss) => {
+        beforeAll(async () => {
+          nextConfig.write(
+            `
+const config = require('../next.config.js');
+module.exports = {
+  ..config
+  experimental: {
+    useLightningcss: ${useLightningcss}
+  }
+}`
+          )
+        })
 
-      it('should have compiled successfully', () => {
-        expect(code).toBe(0)
-        expect(stdout).toMatch(/Compiled successfully/)
-      })
+        let appPort
+        let app
+        let code
+        let stdout
+        beforeAll(async () => {
+          ;({ code, stdout } = await nextBuild(appDir, [], {
+            stdout: true,
+          }))
+          appPort = await findPort()
+          app = await nextStart(appDir, appPort)
+        })
+        afterAll(async () => {
+          await killApp(app)
+        })
 
-      it('should have the correct color on mount after navigation', async () => {
-        let browser
-        try {
-          browser = await webdriver(appPort, '/')
+        it('should have compiled successfully', () => {
+          expect(code).toBe(0)
+          expect(stdout).toMatch(/Compiled successfully/)
+        })
 
-          // Navigate to other:
-          await browser.waitForElementByCss('#link-other').click()
-          const text = await browser.waitForElementByCss('#red-title').text()
-          expect(text).toMatchInlineSnapshot(`"rgb(255, 0, 0)"`)
-        } finally {
-          if (browser) {
-            await browser.close()
+        it('should have the correct color on mount after navigation', async () => {
+          let browser
+          try {
+            browser = await webdriver(appPort, '/')
+
+            // Navigate to other:
+            await browser.waitForElementByCss('#link-other').click()
+            const text = await browser.waitForElementByCss('#red-title').text()
+            expect(text).toMatchInlineSnapshot(`"rgb(255, 0, 0)"`)
+          } finally {
+            if (browser) {
+              await browser.close()
+            }
           }
-        }
+        })
       })
     })
 
     describe('Has CSS in computed styles in Production', () => {
       const appDir = join(fixturesDir, 'multi-page')
+      const nextConfig = new File(join(appDir, 'next.config.js'))
 
-      let appPort
-      let app
-      let stdout
-      let code
-      beforeAll(async () => {
-        await remove(join(appDir, '.next'))
-        ;({ code, stdout } = await nextBuild(appDir, [], {
-          stdout: true,
-        }))
-        appPort = await findPort()
-        app = await nextStart(appDir, appPort)
-      })
-      afterAll(async () => {
-        await killApp(app)
-      })
+      describe.each([true, false])(`useLightnincsss(%s)`, (useLightningcss) => {
+        beforeAll(async () => {
+          nextConfig.write(
+            `
+const config = require('../next.config.js');
+module.exports = {
+  ..config
+  experimental: {
+    useLightningcss: ${useLightningcss}
+  }
+}`
+          )
+        })
 
-      it('should have compiled successfully', () => {
-        expect(code).toBe(0)
-        expect(stdout).toMatch(/Compiled successfully/)
-      })
+        let appPort
+        let app
+        let stdout
+        let code
+        beforeAll(async () => {
+          await remove(join(appDir, '.next'))
+          ;({ code, stdout } = await nextBuild(appDir, [], {
+            stdout: true,
+          }))
+          appPort = await findPort()
+          app = await nextStart(appDir, appPort)
+        })
+        afterAll(async () => {
+          await killApp(app)
+        })
 
-      it('should have CSS for page', async () => {
-        const browser = await webdriver(appPort, '/page2')
+        it('should have compiled successfully', () => {
+          expect(code).toBe(0)
+          expect(stdout).toMatch(/Compiled successfully/)
+        })
 
-        const currentColor = await browser.eval(
-          `window.getComputedStyle(document.querySelector('.blue-text')).color`
-        )
-        expect(currentColor).toMatchInlineSnapshot(`"rgb(0, 0, 255)"`)
-      })
+        it('should have CSS for page', async () => {
+          const browser = await webdriver(appPort, '/page2')
 
-      it(`should've preloaded the CSS file and injected it in <head>`, async () => {
-        const content = await renderViaHTTP(appPort, '/page2')
-        const $ = cheerio.load(content)
+          const currentColor = await browser.eval(
+            `window.getComputedStyle(document.querySelector('.blue-text')).color`
+          )
+          expect(currentColor).toMatchInlineSnapshot(`"rgb(0, 0, 255)"`)
+        })
 
-        const cssPreload = $('link[rel="preload"][as="style"]')
-        expect(cssPreload.length).toBe(1)
-        expect(cssPreload.attr('href')).toMatch(
-          /^\/_next\/static\/css\/.*\.css$/
-        )
+        it(`should've preloaded the CSS file and injected it in <head>`, async () => {
+          const content = await renderViaHTTP(appPort, '/page2')
+          const $ = cheerio.load(content)
 
-        const cssSheet = $('link[rel="stylesheet"]')
-        expect(cssSheet.length).toBe(1)
-        expect(cssSheet.attr('href')).toMatch(/^\/_next\/static\/css\/.*\.css$/)
+          const cssPreload = $('link[rel="preload"][as="style"]')
+          expect(cssPreload.length).toBe(1)
+          expect(cssPreload.attr('href')).toMatch(
+            /^\/_next\/static\/css\/.*\.css$/
+          )
 
-        /* ensure CSS preloaded first */
-        const allPreloads = [].slice.call($('link[rel="preload"]'))
-        const styleIndexes = allPreloads.flatMap((p, i) =>
-          p.attribs.as === 'style' ? i : []
-        )
-        expect(styleIndexes).toEqual([0])
+          const cssSheet = $('link[rel="stylesheet"]')
+          expect(cssSheet.length).toBe(1)
+          expect(cssSheet.attr('href')).toMatch(
+            /^\/_next\/static\/css\/.*\.css$/
+          )
+
+          /* ensure CSS preloaded first */
+          const allPreloads = [].slice.call($('link[rel="preload"]'))
+          const styleIndexes = allPreloads.flatMap((p, i) =>
+            p.attribs.as === 'style' ? i : []
+          )
+          expect(styleIndexes).toEqual([0])
+        })
       })
     })
 
     describe('Good CSS Import from node_modules', () => {
       const appDir = join(fixturesDir, 'npm-import')
+      const nextConfig = new File(join(appDir, 'next.config.js'))
 
-      beforeAll(async () => {
-        await remove(join(appDir, '.next'))
-      })
-
-      it('should compile successfully', async () => {
-        const { code, stdout } = await nextBuild(appDir, [], {
-          stdout: true,
+      describe.each([true, false])(`useLightnincsss(%s)`, (useLightningcss) => {
+        beforeAll(async () => {
+          nextConfig.write(
+            `
+const config = require('../next.config.js');
+module.exports = {
+  ..config
+  experimental: {
+    useLightningcss: ${useLightningcss}
+  }
+}`
+          )
         })
-        expect(code).toBe(0)
-        expect(stdout).toMatch(/Compiled successfully/)
-      })
 
-      it(`should've emitted a single CSS file`, async () => {
-        const cssFolder = join(appDir, '.next/static/css')
+        beforeAll(async () => {
+          await remove(join(appDir, '.next'))
+        })
 
-        const files = await readdir(cssFolder)
-        const cssFiles = files.filter((f) => /\.css$/.test(f))
+        it('should compile successfully', async () => {
+          const { code, stdout } = await nextBuild(appDir, [], {
+            stdout: true,
+          })
+          expect(code).toBe(0)
+          expect(stdout).toMatch(/Compiled successfully/)
+        })
 
-        expect(cssFiles.length).toBe(1)
-        const cssContent = await readFile(join(cssFolder, cssFiles[0]), 'utf8')
-        expect(cssContent.replace(/\/\*.*?\*\//g, '').trim()).toMatch(
-          /nprogress/
-        )
+        it(`should've emitted a single CSS file`, async () => {
+          const cssFolder = join(appDir, '.next/static/css')
+
+          const files = await readdir(cssFolder)
+          const cssFiles = files.filter((f) => /\.css$/.test(f))
+
+          expect(cssFiles.length).toBe(1)
+          const cssContent = await readFile(
+            join(cssFolder, cssFiles[0]),
+            'utf8'
+          )
+          expect(cssContent.replace(/\/\*.*?\*\//g, '').trim()).toMatch(
+            /nprogress/
+          )
+        })
       })
     })
 
     describe('Good Nested CSS Import from node_modules', () => {
       const appDir = join(fixturesDir, 'npm-import-nested')
+      const nextConfig = new File(join(appDir, 'next.config.js'))
 
-      beforeAll(async () => {
-        await remove(join(appDir, '.next'))
-      })
-
-      it('should compile successfully', async () => {
-        const { code, stdout } = await nextBuild(appDir, [], {
-          stdout: true,
+      describe.each([true, false])(`useLightnincsss(%s)`, (useLightningcss) => {
+        beforeAll(async () => {
+          nextConfig.write(
+            `
+const config = require('../next.config.js');
+module.exports = {
+  ..config
+  experimental: {
+    useLightningcss: ${useLightningcss}
+  }
+}`
+          )
         })
-        expect(code).toBe(0)
-        expect(stdout).toMatch(/Compiled successfully/)
-      })
 
-      it(`should've emitted a single CSS file`, async () => {
-        const cssFolder = join(appDir, '.next/static/css')
+        beforeAll(async () => {
+          await remove(join(appDir, '.next'))
+        })
 
-        const files = await readdir(cssFolder)
-        const cssFiles = files.filter((f) => /\.css$/.test(f))
+        it('should compile successfully', async () => {
+          const { code, stdout } = await nextBuild(appDir, [], {
+            stdout: true,
+          })
+          expect(code).toBe(0)
+          expect(stdout).toMatch(/Compiled successfully/)
+        })
 
-        expect(cssFiles.length).toBe(1)
-        const cssContent = await readFile(join(cssFolder, cssFiles[0]), 'utf8')
-        expect(
-          cssContent.replace(/\/\*.*?\*\//g, '').trim()
-        ).toMatchInlineSnapshot(`".other{color:blue}.test{color:red}"`)
+        it(`should've emitted a single CSS file`, async () => {
+          const cssFolder = join(appDir, '.next/static/css')
+
+          const files = await readdir(cssFolder)
+          const cssFiles = files.filter((f) => /\.css$/.test(f))
+
+          expect(cssFiles.length).toBe(1)
+          const cssContent = await readFile(
+            join(cssFolder, cssFiles[0]),
+            'utf8'
+          )
+          expect(
+            cssContent.replace(/\/\*.*?\*\//g, '').trim()
+          ).toMatchInlineSnapshot(`".other{color:blue}.test{color:red}"`)
+        })
       })
     })
   })
@@ -257,40 +324,56 @@ describe('CSS Support', () => {
 describe('CSS Property Ordering', () => {
   ;(process.env.TURBOPACK ? describe.skip : describe)('production mode', () => {
     const appDir = join(fixturesDir, 'next-issue-15468')
+    const nextConfig = new File(join(appDir, 'next.config.js'))
 
-    let appPort
-    let app
-    let stdout
-    let code
-    beforeAll(async () => {
-      await remove(join(appDir, '.next'))
-      ;({ code, stdout } = await nextBuild(appDir, [], {
-        stdout: true,
-      }))
-      appPort = await findPort()
-      app = await nextStart(appDir, appPort)
-    })
-    afterAll(async () => {
-      await killApp(app)
-    })
+    describe.each([true, false])(`useLightnincsss(%s)`, (useLightningcss) => {
+      beforeAll(async () => {
+        nextConfig.write(
+          `
+const config = require('../next.config.js');
+module.exports = {
+  ..config
+  experimental: {
+    useLightningcss: ${useLightningcss}
+  }
+}`
+        )
+      })
 
-    it('should have compiled successfully', () => {
-      expect(code).toBe(0)
-      expect(stdout).toMatch(/Compiled successfully/)
-    })
+      let appPort
+      let app
+      let stdout
+      let code
+      beforeAll(async () => {
+        await remove(join(appDir, '.next'))
+        ;({ code, stdout } = await nextBuild(appDir, [], {
+          stdout: true,
+        }))
+        appPort = await findPort()
+        app = await nextStart(appDir, appPort)
+      })
+      afterAll(async () => {
+        await killApp(app)
+      })
 
-    it('should have the border width (property ordering)', async () => {
-      const browser = await webdriver(appPort, '/')
+      it('should have compiled successfully', () => {
+        expect(code).toBe(0)
+        expect(stdout).toMatch(/Compiled successfully/)
+      })
 
-      const width1 = await browser.eval(
-        `window.getComputedStyle(document.querySelector('.test1')).borderWidth`
-      )
-      expect(width1).toMatchInlineSnapshot(`"0px"`)
+      it('should have the border width (property ordering)', async () => {
+        const browser = await webdriver(appPort, '/')
 
-      const width2 = await browser.eval(
-        `window.getComputedStyle(document.querySelector('.test2')).borderWidth`
-      )
-      expect(width2).toMatchInlineSnapshot(`"5px"`)
+        const width1 = await browser.eval(
+          `window.getComputedStyle(document.querySelector('.test1')).borderWidth`
+        )
+        expect(width1).toMatchInlineSnapshot(`"0px"`)
+
+        const width2 = await browser.eval(
+          `window.getComputedStyle(document.querySelector('.test2')).borderWidth`
+        )
+        expect(width2).toMatchInlineSnapshot(`"5px"`)
+      })
     })
   })
 })
