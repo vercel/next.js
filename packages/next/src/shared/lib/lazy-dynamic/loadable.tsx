4874f28f800a2c3d805de1fd5aaad17278c4fe55
Jiachi Liu <inbox@huozhi.im>
2024-01-30 13:01:11 +0100

    Fix dynamic import named export from client components

--- packages/next/src/shared/lib/lazy-dynamic/loadable.tsx
+++ packages/next/src/shared/lib/lazy-dynamic/loadable.tsx
@@ -1,12 +1,22 @@
 import { Suspense, lazy } from 'react'
 import { BailoutToCSR } from './dynamic-bailout-to-csr'
 import type { ComponentModule } from './types'
+import { isClientReference } from '../../../lib/client-reference'
 
 // Normalize loader to return the module as form { default: Component } for `React.lazy`.
 // Also for backward compatible since next/dynamic allows to resolve a component directly with loader
 // Client component reference proxy need to be converted to a module.
-function convertModule<P>(mod: React.ComponentType<P> | ComponentModule<P>) {
-  return { default: (mod as ComponentModule<P>)?.default ?? mod }
+function convertModule<P>(mod: React.ComponentType<P> | ComponentModule<P>): {
+  default: React.ComponentType<P>
+} {
+  // If it's client component, it's already a component type. We don't access the default property into the module,
+  // client module proxy can manage it and map to the right property.
+  const isClientComponentModule = isClientReference(mod)
+  return {
+    default: isClientComponentModule
+      ? (mod as React.ComponentType<P>)
+      : (mod as ComponentModule<P>)?.default ?? mod,
+  }
 }
 
 const defaultOptions = {
