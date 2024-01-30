e268f00f2e92879b354866fb97cf14b960e61671
Federico Orlandau <federico.orlandau@gmail.com>
2024-01-30 15:43:04 +0100

    fix: update tests

--- test/e2e/app-dir/metadata-dynamic-routes/index.test.ts
+++ test/e2e/app-dir/metadata-dynamic-routes/index.test.ts
@@ -78,13 +78,11 @@ createNextDescribe(
           "<?xml version="1.0" encoding="UTF-8"?>
           <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
           <sitemap>
-          <loc>https://example.com</loc>
+          <loc>https://example.com/sitemap.xml</loc>
           <lastmod>2021-01-01</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.5</priority>
           </sitemap>
           <sitemap>
-          <loc>https://example.com/about</loc>
+          <loc>https://example.com/sitemap2.xml</loc>
           <lastmod>2021-01-01</lastmod>
           </sitemap>
           </sitemapindex>
