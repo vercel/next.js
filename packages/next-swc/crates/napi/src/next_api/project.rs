cbc4e8f4ae247f23d11ad71f27776959147aab3e
Tim Neutkens <tim@timneutkens.nl>
2024-01-30 10:14:10 +0100

    Revert "Same as #61360"
    This reverts commit 18eed3693629000356779255722a9fb0be878585.

--- packages/next-swc/crates/napi/src/next_api/project.rs
+++ packages/next-swc/crates/napi/src/next_api/project.rs
@@ -30,14 +30,12 @@ use turbopack_binding::{
     },
     turbopack::{
         core::{
-            chunk::ModuleId,
             diagnostics::PlainDiagnostic,
             error::PrettyPrintError,
             issue::PlainIssue,
             source_map::{GenerateSourceMap, Token},
             version::{PartialUpdate, TotalUpdate, Update, VersionState},
         },
-        dev::ecmascript::EcmascriptDevChunkContent,
         ecmascript_hmr_protocol::{ClientUpdateInstruction, ResourceIdentifier},
         trace_utils::{
             exit::ExitGuard,
@@ -806,16 +804,12 @@ pub async fn project_trace_source(
     let turbo_tasks = project.turbo_tasks.clone();
     let traced_frame = turbo_tasks
         .run_once(async move {
-            let (file, module) = match Url::parse(&frame.file) {
+            let file = match Url::parse(&frame.file) {
                 Ok(url) => match url.scheme() {
-                    "file" => {
-                        let path = urlencoding::decode(url.path())?.to_string();
-                        let module = url.query_pairs().find(|(k, _)| k == "id");
-                        (path, module.map(|(_, m)| m.into_owned()))
-                    }
+                    "file" => urlencoding::decode(url.path())?.to_string(),
                     _ => bail!("Unknown url scheme"),
                 },
-                Err(_) => (frame.file.to_string(), None),
+                Err(_) => frame.file.to_string(),
             };
 
             let Some(chunk_base) = file.strip_prefix(
@@ -843,37 +837,18 @@ pub async fn project_trace_source(
                     .join(chunk_base.to_owned())
             };
 
-            let content_vc = project.container.get_versioned_content(path);
-            let map = match module {
-                Some(module) => {
-                    let Some(content) =
-                        Vc::try_resolve_downcast_type::<EcmascriptDevChunkContent>(content_vc)
-                            .await?
-                    else {
-                        bail!("Was not EcmascriptDevChunkContent")
-                    };
-
-                    let entries = content.entries().await?;
-                    let entry = entries.get(&ModuleId::String(module).cell().await?);
-                    let map = match entry {
-                        Some(entry) => *entry.code.generate_source_map().await?,
-                        None => None,
-                    };
-                    map.context("Entry is missing sourcemap")?
-                }
-                None => {
-                    let Some(versioned) =
-                        Vc::try_resolve_sidecast::<Box<dyn GenerateSourceMap>>(content_vc).await?
-                    else {
-                        bail!("Could not GenerateSourceMap")
-                    };
-
-                    versioned
-                        .generate_source_map()
-                        .await?
-                        .context("Chunk is missing a sourcemap")?
-                }
+            let Some(versioned) = Vc::try_resolve_sidecast::<Box<dyn GenerateSourceMap>>(
+                project.container.get_versioned_content(path),
+            )
+            .await?
+            else {
+                bail!("Could not GenerateSourceMap")
             };
+
+            let map = versioned
+                .generate_source_map()
+                .await?
+                .context("Chunk is missing a sourcemap")?;
 
             let token = map
                 .lookup_token(frame.line as usize, frame.column.unwrap_or(0) as usize)
