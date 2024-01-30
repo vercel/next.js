4b80ba8123e884adef0927d4913933bd0c12ee18
samcx <sam@vercel.com>
2024-01-29 18:08:57 -0800

    chore: update next export

--- packages/next/src/bin/future-next.ts
+++ packages/next/src/bin/future-next.ts
@@ -4,6 +4,7 @@ import { Command, Option } from '@commander-js/extra-typings'
 
 import semver from 'next/dist/compiled/semver'
 import { bold, cyan } from '../lib/picocolors'
+import { nextExport } from '../cli/future-next-export'
 import { nextTelemetry } from '../cli/future-next-telemetry'
 
 if (
@@ -24,28 +25,22 @@ program
   )
   .version(`Next.js v${process.env.__NEXT_VERSION}`, '-v, --version')
 
-program.command('export').error(
-  `The \`next export\` command has been removed in favor of \`output: export\` in next.config.js.\nLearn more: ${cyan(
-    'https://nextjs.org/docs/advanced-features/static-html-export'
-  )}
-`,
-  { exitCode: 1 }
-)
+program.command('export', { hidden: true }).action(nextExport).helpOption(false)
 
 program
   .command('telemetry')
   .addHelpText('after', `\nLearn more: ${cyan('https://nextjs.org/telemetry')}`)
-  .description(
-    `This command allows you to enable or disable Next.js' ${bold(
-      'completely anonymous'
-    )} telemetry collection.`
-  )
   .addOption(
     new Option('--enable', `enables Next.js' telemetry collection`).conflicts(
       'disable'
     )
   )
   .addOption(new Option('--disable', `disables Next.js' telemetry collection`))
+  .description(
+    `This \`next telemetry\` command allows you to enable or disable Next.js' ${bold(
+      'completely anonymous'
+    )} telemetry collection.`
+  )
   .action((options) => nextTelemetry(options))
 
 program.parse(process.argv)
---
+++ packages/next/src/cli/future-next-export.ts
@@ -0,0 +1,12 @@
+import { cyan } from '../lib/picocolors'
+import * as Log from '../build/output/log'
+
+const nextExport = () => {
+  Log.error(`
+    The "next export" command has been removed in favor of "output: export" in next.config.js.\nLearn more: ${cyan(
+      'https://nextjs.org/docs/advanced-features/static-html-export'
+    )}
+  `)
+}
+
+export { nextExport }
