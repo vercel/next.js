838d15e86ddabdab80e31d0d214ec253ab108222
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-01-31 16:42:30 +0900

    feat: Re-enable `preset-env` mode of `styled-jsx` in turbopack (vercel/turbo#7182)
    ### Description
    
    Re-enable `preset-env` mode of `styled-jsx`
    
    
    ### Testing Instructions
    
    Tested with https://github.com/swc-project/plugins/pull/259
    
    Closes PACK-2330

--- crates/turbopack-ecmascript-plugins/src/transform/styled_jsx.rs
+++ crates/turbopack-ecmascript-plugins/src/transform/styled_jsx.rs
@@ -1,10 +1,14 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use async_trait::async_trait;
+use lightningcss::{
+    stylesheet::{PrinterOptions, StyleSheet},
+    targets::{Browsers, Targets},
+};
 use swc_core::{
     common::{util::take::Take, FileName},
     ecma::{
         ast::{Module, Program},
-        preset_env::Versions,
+        preset_env::{Version, Versions},
         visit::FoldWith,
     },
 };
@@ -37,9 +41,57 @@ impl CustomTransformer for StyledJsxTransformer {
                 use_lightningcss: self.use_lightningcss,
                 browsers: self.target_browsers,
             },
-            styled_jsx::visitor::NativeConfig { process_css: None },
+            styled_jsx::visitor::NativeConfig {
+                process_css: if self.use_lightningcss || self.target_browsers.is_any_target() {
+                    None
+                } else {
+                    let targets = Targets {
+                        browsers: Some(convert_browsers(&self.target_browsers)),
+                        ..Default::default()
+                    };
+
+                    Some(Box::new(move |css| {
+                        let ss = StyleSheet::parse(css, Default::default());
+
+                        let ss = match ss {
+                            Ok(v) => v,
+                            Err(err) => {
+                                bail!("failed to parse css using lightningcss: {}", err)
+                            }
+                        };
+
+                        let output = ss.to_css(PrinterOptions {
+                            minify: true,
+                            source_map: None,
+                            project_root: None,
+                            targets,
+                            analyze_dependencies: None,
+                            pseudo_classes: None,
+                        })?;
+                        Ok(output.code)
+                    }))
+                },
+            },
         ));
 
         Ok(())
+    }
+}
+
+fn convert_browsers(browsers: &Versions) -> Browsers {
+    fn convert(v: Option<Version>) -> Option<u32> {
+        v.map(|v| v.major << 16 | v.minor << 8 | v.patch)
+    }
+
+    Browsers {
+        android: convert(browsers.android),
+        chrome: convert(browsers.chrome),
+        edge: convert(browsers.edge),
+        firefox: convert(browsers.firefox),
+        ie: convert(browsers.ie),
+        ios_saf: convert(browsers.ios),
+        opera: convert(browsers.opera),
+        safari: convert(browsers.safari),
+        samsung: convert(browsers.samsung),
     }
 }
