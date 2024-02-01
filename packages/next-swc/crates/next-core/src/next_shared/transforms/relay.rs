d5dbc9bb832dd9386293d008b12f1c6689ce472c
OJ Kwon <1210596+kwonoj@users.noreply.github.com.>
2024-01-31 16:24:25 -0800

    featn(next-core): relay to custom rule

--- packages/next-swc/crates/next-core/src/next_shared/transforms/relay.rs
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/relay.rs
@@ -2,8 +2,10 @@ use anyhow::Result;
 use turbo_tasks::Vc;
 use turbopack_binding::turbopack::{
     ecmascript::OptionTransformPlugin, ecmascript_plugin::transform::relay::RelayTransformer,
+    turbopack::module_options::ModuleRule,
 };
 
+use super::get_ecma_transform_rule;
 use crate::next_config::NextConfig;
 
 /// Returns a transform plugin for the relay graphql transform.
@@ -27,4 +29,20 @@ pub async fn get_relay_transform_plugin(
         .unwrap_or_default();
 
     Ok(transform_plugin)
+}
+
+pub async fn get_relay_transform_rule(next_config: Vc<NextConfig>) -> Result<Option<ModuleRule>> {
+    let enable_mdx_rs = *next_config.mdx_rs().await?;
+    let module_rule = next_config
+        .await?
+        .compiler
+        .as_ref()
+        .map(|value| {
+            value.relay.as_ref().map(|config| {
+                get_ecma_transform_rule(Box::new(RelayTransformer::new(config)), enable_mdx_rs)
+            })
+        })
+        .flatten();
+
+    Ok(module_rule)
 }
