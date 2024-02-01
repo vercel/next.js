a56050bfe473ed8b90f640f81c54e0b50a98053a
OJ Kwon <1210596+kwonoj@users.noreply.github.com.>
2024-01-31 16:11:19 -0800

    feat(next-core): emotion to custom rule

--- packages/next-swc/crates/next-core/src/next_shared/transforms/emotion.rs
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/emotion.rs
@@ -3,9 +3,32 @@ use turbo_tasks::Vc;
 use turbopack_binding::turbopack::{
     ecmascript::OptionTransformPlugin,
     ecmascript_plugin::transform::emotion::{EmotionTransformConfig, EmotionTransformer},
+    turbopack::module_options::ModuleRule,
 };
 
+use super::get_ecma_transform_rule;
 use crate::next_config::{EmotionTransformOptionsOrBoolean, NextConfig};
+
+pub async fn get_emotion_transform_rule(next_config: Vc<NextConfig>) -> Result<Option<ModuleRule>> {
+    let enable_mdx_rs = *next_config.mdx_rs().await?;
+    let module_rule = next_config
+        .await?
+        .compiler
+        .as_ref()
+        .map(|value| value.emotion.as_ref())
+        .flatten()
+        .map(|config| match config {
+            EmotionTransformOptionsOrBoolean::Boolean(true) => {
+                EmotionTransformer::new(&Default::default())
+            }
+            EmotionTransformOptionsOrBoolean::Options(value) => EmotionTransformer::new(value),
+            _ => None,
+        })
+        .flatten()
+        .map(|transformer| get_ecma_transform_rule(Box::new(transformer), enable_mdx_rs));
+
+    Ok(module_rule)
+}
 
 #[turbo_tasks::function]
 pub async fn get_emotion_transform_plugin(
