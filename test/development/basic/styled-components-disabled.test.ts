904aa43af5ba7cca93839c23c6c500b88584f98e
Tim Neutkens <tim@timneutkens.nl>
2024-02-01 10:47:53 +0100

    Skip disabled test

--- test/development/basic/styled-components-disabled.test.ts
+++ test/development/basic/styled-components-disabled.test.ts
@@ -4,58 +4,59 @@ import { createNext, FileRef } from 'e2e-utils'
 import { NextInstance } from 'test/lib/next-modes/base'
 import { check, fetchViaHTTP } from 'next-test-utils'
 
-describe.each([
-  ['dev', false],
-  // TODO: re-enable when this is no longer flakey with turbo
-  // ['turbo', true],
-])('styled-components SWC transform', (_name, turbo) => {
-  let next: NextInstance
+// TODO: Somehow the warning doesn't show up with Turbopack, even though the transform is not enabled.
+;(process.env.TURBOPACK ? describe.skip : describe)(
+  'styled-components SWC transform',
+  () => {
+    let next: NextInstance
 
-  beforeAll(async () => {
-    next = await createNext({
-      turbo: !!turbo,
-      files: {
-        'next.config.js': new FileRef(
-          join(__dirname, 'styled-components-disabled/next.config.js')
-        ),
-        pages: new FileRef(join(__dirname, 'styled-components-disabled/pages')),
-      },
-      dependencies: {
-        'styled-components': '5.3.3',
-      },
+    beforeAll(async () => {
+      next = await createNext({
+        files: {
+          'next.config.js': new FileRef(
+            join(__dirname, 'styled-components-disabled/next.config.js')
+          ),
+          pages: new FileRef(
+            join(__dirname, 'styled-components-disabled/pages')
+          ),
+        },
+        dependencies: {
+          'styled-components': '5.3.3',
+        },
+      })
     })
-  })
-  afterAll(() => next.destroy())
+    afterAll(() => next.destroy())
 
-  async function matchLogs$(browser) {
-    let foundLog = false
+    async function matchLogs$(browser) {
+      let foundLog = false
 
-    const browserLogs = await browser.log('browser')
+      const browserLogs = await browser.log('browser')
 
-    browserLogs.forEach((log) => {
-      if (log.message.includes('Warning: Prop `%s` did not match.')) {
-        foundLog = true
-      }
-    })
-    return foundLog
-  }
-  it('should have hydration mismatch with styled-components transform disabled', async () => {
-    let browser
-    try {
-      browser = await webdriver(next.url, '/')
-
-      // Compile /_error
-      await fetchViaHTTP(next.url, '/404')
-
-      await check(async () => {
-        await browser.refresh()
-        const foundLog = await matchLogs$(browser)
-        return foundLog ? 'success' : await browser.log('browser')
-      }, 'success')
-    } finally {
-      if (browser) {
-        await browser.close()
-      }
+      browserLogs.forEach((log) => {
+        if (log.message.includes('Warning: Prop `%s` did not match.')) {
+          foundLog = true
+        }
+      })
+      return foundLog
     }
-  })
-})
+    it('should have hydration mismatch with styled-components transform disabled', async () => {
+      let browser
+      try {
+        browser = await webdriver(next.url, '/')
+
+        // Compile /_error
+        await fetchViaHTTP(next.url, '/404')
+
+        await check(async () => {
+          await browser.refresh()
+          const foundLog = await matchLogs$(browser)
+          return foundLog ? 'success' : await browser.log('browser')
+        }, 'success')
+      } finally {
+        if (browser) {
+          await browser.close()
+        }
+      }
+    })
+  }
+)
