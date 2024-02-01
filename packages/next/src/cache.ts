4b6add9bed2c919704a1cb698b404bb9795f471f
Balázs Orbán <info@balazsorban.com>
2024-02-01 20:14:27 +0100

    add missing module

---
+++ packages/next/src/cache.ts
@@ -0,0 +1,4 @@
+export { unstable_cache } from './server/web/spec-extension/unstable-cache'
+export { revalidateTag } from './server/web/spec-extension/revalidate'
+export { revalidatePath } from './server/web/spec-extension/revalidate'
+export { unstable_noStore } from './server/web/spec-extension/unstable-no-store'
