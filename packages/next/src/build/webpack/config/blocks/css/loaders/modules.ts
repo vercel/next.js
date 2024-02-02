ebf0a07b4b0a3ba4a2b5405c5a6bf384e41377fc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-02 17:55:41 +0900

    Revert "postcss first"
    This reverts commit 587af9b833fce40fd405a3278ca108b3342c7407.

--- packages/next/src/build/webpack/config/blocks/css/loaders/modules.ts
+++ packages/next/src/build/webpack/config/blocks/css/loaders/modules.ts
@@ -24,14 +24,6 @@ export function getCssModuleLoader(
     )
   }
 
-  // Compile CSS
-  loaders.push({
-    loader: require.resolve('../../../../loaders/postcss-loader/src'),
-    options: {
-      postcss,
-    },
-  })
-
   if (ctx.experimental.useLightningcss) {
     loaders.push({
       loader: require.resolve('../../../../loaders/lightningcss-loader/src'),
@@ -81,6 +73,14 @@ export function getCssModuleLoader(
       },
     })
   }
+
+  // Compile CSS
+  loaders.push({
+    loader: require.resolve('../../../../loaders/postcss-loader/src'),
+    options: {
+      postcss,
+    },
+  })
 
   loaders.push(
     // Webpack loaders run like a stack, so we need to reverse the natural
