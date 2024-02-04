a4869257b0911a25c1c0aaf01e973332e9321061
JJ Kasper <jj@jjsweb.site>
2024-02-04 13:13:13 -0800

    Remove extra edge-runtime/primitives override

--- packages/next/src/compiled/@edge-runtime/primitives/encoding.d.ts
+++
@@ -1,8 +0,0 @@
-declare const TextEncoderConstructor: typeof TextEncoder
-declare const TextDecoderConstructor: typeof TextDecoder
-
-
-declare const atob: (encoded: string) => string
-declare const btoa: (input: any) => string
-
-export { TextDecoderConstructor as TextDecoder, TextEncoderConstructor as TextEncoder, atob, btoa };
