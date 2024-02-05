65e96ebb8b17a78f03cf9937573cfba4b669d76c
Adrian Hartanto <s.adrianh@gmail.com>
2024-02-05 07:10:28 +0700

    Update revalidate.ts
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- packages/next/src/server/lib/revalidate.ts
+++ packages/next/src/server/lib/revalidate.ts
@@ -13,7 +13,7 @@ export function formatRevalidate(revalidate: Revalidate): string {
   if (revalidate === 0) {
     return 'private, no-cache, no-store, max-age=0, must-revalidate'
   } else if (typeof revalidate === 'number') {
-    return `s-maxage=${revalidate}, stale-while-revalidate=${revalidate}`
+    return `s-maxage=${revalidate}, stale-while-revalidate=${CACHE_ONE_YEAR}`
   }
 
   return `s-maxage=${CACHE_ONE_YEAR}, stale-while-revalidate=${CACHE_ONE_YEAR}`
