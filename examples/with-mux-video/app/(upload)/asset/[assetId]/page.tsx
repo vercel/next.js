ce95e19055160ab0ea123ef054e6842c459a68ef
Darius Cepulis <dcepulis@mux.com>
2024-02-08 08:06:36 -0600

    Oh you know, this should be force-dynamic

--- examples/with-mux-video/app/(upload)/asset/[assetId]/page.tsx
+++ examples/with-mux-video/app/(upload)/asset/[assetId]/page.tsx
@@ -22,6 +22,7 @@ const checkAssetStatus = async (assetId: string): Promise<Status> => {
   };
 };
 
+export const dynamic = "force-dynamic";
 export default async function Page({
   params: { assetId },
 }: {
