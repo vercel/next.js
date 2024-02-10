429c06961047e6126d3cbf37ad4762cf3ede4204
Jamie Barton <jamie@notrab.dev>
2024-02-10 06:55:54 +0000

    Update examples/with-turso/tailwind.config.ts
    Co-authored-by: Michael Novotny <manovotny@gmail.com>

--- examples/with-turso/tailwind.config.ts
+++ examples/with-turso/tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
