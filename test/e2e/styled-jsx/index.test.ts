c82e8ada52517608b0276f9ea57d62825ffb9a70
Jiachi Liu <inbox@huozhi.im>
2024-02-14 22:50:19 +0100

    rewrite styled-jsx test

--- test/e2e/styled-jsx/index.test.ts
+++ test/e2e/styled-jsx/index.test.ts
@@ -1,49 +1,23 @@
-import path from 'path'
-import { createNext, FileRef } from 'e2e-utils'
-import { NextInstance } from 'test/lib/next-modes/base'
-import { renderViaHTTP, shouldRunTurboDevTest } from 'next-test-utils'
-import webdriver from 'next-webdriver'
+import { createNextDescribe } from 'e2e-utils'
 
-const appDir = path.join(__dirname, 'app')
-
-function runTest() {
-  describe(`styled-jsx`, () => {
-    let next: NextInstance
-
-    beforeAll(async () => {
-      const devCommand = shouldRunTurboDevTest() ? 'dev --turbo' : 'dev'
-
-      next = await createNext({
-        files: {
-          pages: new FileRef(path.join(appDir, 'pages')),
-          '.npmrc': new FileRef(path.join(appDir, '.npmrc')),
-          node_modules: new FileRef('node_modules'),
-        },
-        packageJson: {
-          scripts: {
-            build: `next build`,
-            dev: `next ${devCommand}`,
-            start: 'next start',
-          },
-        },
-        dependencies: {
-          'styled-jsx': '5.0.0', // styled-jsx on user side
-        },
-        installCommand: 'pnpm i',
-        startCommand: (global as any).isNextDev ? 'pnpm dev' : 'pnpm start',
-        buildCommand: `pnpm build`,
-      })
-    })
-    afterAll(() => next.destroy())
-
+createNextDescribe(
+  'styled-jsx',
+  {
+    files: __dirname,
+    skipDeployment: true,
+    dependencies: {
+      'styled-jsx': '5.0.0', // styled-jsx on user side
+    },
+  },
+  ({ next }) => {
     it('should contain styled-jsx styles during SSR', async () => {
-      const html = await renderViaHTTP(next.url, '/')
+      const html = await next.render('/')
       expect(html).toMatch(/color:.*?red/)
       expect(html).toMatch(/color:.*?cyan/)
     })
 
     it('should render styles during CSR', async () => {
-      const browser = await webdriver(next.url, '/')
+      const browser = await next.browser('/')
       const color = await browser.eval(
         `getComputedStyle(document.querySelector('button')).color`
       )
@@ -52,7 +26,7 @@ function runTest() {
     })
 
     it('should render styles during CSR (AMP)', async () => {
-      const browser = await webdriver(next.url, '/amp')
+      const browser = await next.browser('/amp')
       const color = await browser.eval(
         `getComputedStyle(document.querySelector('button')).color`
       )
@@ -61,10 +35,8 @@ function runTest() {
     })
 
     it('should render styles during SSR (AMP)', async () => {
-      const html = await renderViaHTTP(next.url, '/amp')
+      const html = await next.render('/amp')
       expect(html).toMatch(/color:.*?cyan/)
     })
-  })
-}
-
-runTest()
+  }
+)
