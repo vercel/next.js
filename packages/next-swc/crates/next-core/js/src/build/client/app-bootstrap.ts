3e57b52336b75eccbae684d731db8a093e801f63
Tim Neutkens <tim@timneutkens.nl>
2024-02-15 11:26:48 +0100

    Remove unused app-turbopack file

--- packages/next-swc/crates/next-core/js/src/build/client/app-bootstrap.ts
+++ packages/next-swc/crates/next-core/js/src/build/client/app-bootstrap.ts
@@ -6,7 +6,6 @@ import '../shims'
 import { appBootstrap } from 'next/dist/client/app-bootstrap'
 
 appBootstrap(() => {
-  require('./app-turbopack')
   const { hydrate } = require('./app-index')
   hydrate()
 })
