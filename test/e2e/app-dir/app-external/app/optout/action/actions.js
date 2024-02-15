72aba0a753e70032f4a2310c763b14ce4413882c
Jiachi Liu <inbox@huozhi.im>
2024-02-15 14:23:33 +0100

    test require

--- test/e2e/app-dir/app-external/app/optout/action/actions.js
+++ test/e2e/app-dir/app-external/app/optout/action/actions.js
@@ -1,20 +1,8 @@
 'use server'
 
-import * as dualPkgOptout from 'dual-pkg-optout'
-import { value as dualPkgOptoutValue } from 'dual-pkg-optout'
-
-// console.log(
-//   'server import dualPkgOptoutValue',
-//   dualPkgOptoutValue,
-//   'dualPkgOptout',
-//   dualPkgOptout,
-// )
-// if (dualPkgOptout?.then && typeof dualPkgOptout.then === 'function') {
-
-//   dualPkgOptout?.then(v => {
-//     console.log('dualPkgOptout.then', v)
-//   })
-// }
+// import * as dualPkgOptout from 'dual-pkg-optout'
+// import { value as dualPkgOptoutValue } from 'dual-pkg-optout'
+const { value: dualPkgOptoutValue } = require('dual-pkg-optout')
 
 export async function getDualOptoutValue() {
   return dualPkgOptoutValue
