1b4cf9b94100ab24ce68f6d62f30cf0b78f6df72
Tobias Koppers <tobias.koppers@googlemail.com>
2024-02-16 20:39:57 +0000

    update for working webpack test

--- test/e2e/esm-externals/esm-externals.test.ts
+++ test/e2e/esm-externals/esm-externals.test.ts
@@ -1,68 +1,56 @@
-import { createNextDescribe, FileRef } from 'e2e-utils'
-import { join } from 'path'
+import { nextTestSetup } from 'e2e-utils'
 
-createNextDescribe(
-  'esm-externals',
+describe('esm-externals', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+  // Pages
   {
-    files: {
-      pages: new FileRef(join(__dirname, 'pages')),
-      node_modules: new FileRef(join(__dirname, 'node_modules')),
-      'next.config.js': new FileRef(join(__dirname, 'next.config.js')),
-      app: new FileRef(
-        process.env.TURBOPACK
-          ? join(__dirname, 'app')
-          : // TODO webpack doesn't handle serverComponentsExternalPackages in SSR of client components correctly
-            join(__dirname, 'app-webpack')
-      ),
-    },
-  },
-  ({ next, isTurbopack }) => {
-    // Pages
-    {
-      const expectedHtml =
-        /Hello <!-- -->World<!-- -->\+<!-- -->World<!-- -->\+<!-- -->World<!-- -->\+<!-- -->World\+World\+World/
-      const expectedText = /Hello World\+World\+World\+World\+World\+World/
-      const urls = ['/static', '/ssr', '/ssg']
+    const expectedHtml =
+      /Hello <!-- -->World<!-- -->\+<!-- -->World<!-- -->\+<!-- -->World<!-- -->\+<!-- -->World\+World\+World/
+    const expectedText = /Hello World\+World\+World\+World\+World\+World/
+    const urls = ['/static', '/ssr', '/ssg']
 
-      for (const url of urls) {
-        it(`should return the correct SSR HTML for ${url}`, async () => {
-          const res = await next.fetch(url)
-          const html = await res.text()
-          expect(html).toMatch(expectedHtml)
-        })
+    for (const url of urls) {
+      it(`should return the correct SSR HTML for ${url}`, async () => {
+        const res = await next.fetch(url)
+        const html = await res.text()
+        expect(html).toMatch(expectedHtml)
+      })
 
-        it(`should render the correct page for ${url}`, async () => {
-          const browser = await next.browser(url)
-          expect(await browser.elementByCss('body').text()).toMatch(
-            expectedText
-          )
-        })
-      }
-    }
-
-    // App dir
-    {
-      // TODO App Dir doesn't use esmExternals: true correctly for webpack and Turbopack
-      // so we only verify that the page doesn't crash, but ignore the actual content
-      const expectedHtml = /Hello/
-      const expectedText = /Hello/
-      // TODO webpack doesn't handle serverComponentsExternalPackages in SSR of client components correctly
-      const urls = isTurbopack ? ['/server', '/client'] : ['/server']
-
-      for (const url of urls) {
-        it(`should return the correct SSR HTML for ${url}`, async () => {
-          const res = await next.fetch(url)
-          const html = await res.text()
-          expect(html).toMatch(expectedHtml)
-        })
-
-        it(`should render the correct page for ${url}`, async () => {
-          const browser = await next.browser(url)
-          expect(await browser.elementByCss('body').text()).toMatch(
-            expectedText
-          )
-        })
-      }
+      it(`should render the correct page for ${url}`, async () => {
+        const browser = await next.browser(url)
+        expect(await browser.elementByCss('body').text()).toMatch(expectedText)
+      })
     }
   }
-)
+
+  // App dir
+  {
+    // TODO App Dir doesn't use esmExternals: true correctly for webpack and Turbopack
+    // so we only verify that the page doesn't crash, but ignore the actual content
+    // const expectedHtml =
+    //   /Hello <!-- -->World<!-- -->\+<!-- -->World<!-- -->\+<!-- -->World/
+    const expectedHtml =
+      /Hello <!-- -->Wrong<!-- -->\+<!-- -->Wrong<!-- -->\+<!-- -->Wrong/
+    // const expectedText = /Hello World\+World\+World/
+    const urls = ['/server', '/client']
+
+    for (const url of urls) {
+      const expectedText =
+        url === '/server'
+          ? /Hello Wrong\+Wrong\+Wrong/
+          : /Hello World\+World\+World/
+      it(`should return the correct SSR HTML for ${url}`, async () => {
+        const res = await next.fetch(url)
+        const html = await res.text()
+        expect(html).toMatch(expectedHtml)
+      })
+
+      it(`should render the correct page for ${url}`, async () => {
+        const browser = await next.browser(url)
+        expect(await browser.elementByCss('body').text()).toMatch(expectedText)
+      })
+    }
+  }
+})
