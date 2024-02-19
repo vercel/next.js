840286f3d7879fee26a0915e11dcbefeab49a40c
Tobias Koppers <tobias.koppers@googlemail.com>
2024-02-19 21:15:10 +0100

    disable manifest chunks by default (vercel/turbo#7417)
    ### Description
    
    We only need manifest chunks for lazy compilation, but next.js doesn't
    support that yet. So put that behind a flag and disable it by default.
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->
    
    
    Closes PACK-2537

---
+++ crates/turbopack-tests/tests/snapshot/basic/top-level-await/output/crates_turbopack-tests_tests_snapshot_basic_top-level-await_input_index_0c23c8.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/crates_turbopack-tests_tests_snapshot_basic_top-level-await_input_index_0c23c8.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "output/crates_turbopack-tests_tests_snapshot_basic_top-level-await_input_index_0c23c8.js",
+  "chunks": [
+    "output/crates_turbopack-tests_tests_snapshot_basic_top-level-await_input_3adb52._.js",
+    "output/crates_turbopack-tests_tests_snapshot_basic_top-level-await_input_UserAPI_98de3b.js"
+  ],
+  "source": "entry"
+});
---
+++ crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_e30503._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_e30503._.js", {
+
+"[project]/crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/input/vercel.mjs [test] (ecmascript, async loader)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, p: __turbopack_lookup__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname }) => (() => {
+
+__turbopack_export_value__((__turbopack_import__) => {
+    return Promise.all([
+  "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_2c23f8._.js",
+  "output/a587c_tests_snapshot_imports_static-and-dynamic_input_vercel_mjs_d426d2._.js"
+].map((chunk) => __turbopack_load__(chunk))).then(() => {
+        return __turbopack_import__("[project]/crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/input/vercel.mjs [test] (ecmascript)");
+    });
+});
+
+})()),
+}]);
