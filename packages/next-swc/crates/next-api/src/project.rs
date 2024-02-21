79cb2b2256878fc18b1f78e5e2daf4c4ab72eb87
Tobias Koppers <tobias.koppers@googlemail.com>
2024-02-21 15:29:03 +0100

    Revert "Turbopack: reduce tasks needed for emitting" (#62324)
    Reverts vercel/next.js#62291
    
    Closes PACK-2563

--- packages/next-swc/crates/next-api/src/project.rs
+++ packages/next-swc/crates/next-api/src/project.rs
@@ -5,7 +5,8 @@ use indexmap::{map::Entry, IndexMap};
 use next_core::{
     all_assets_from_entries,
     app_structure::find_app_dir,
-    get_edge_chunking_context, get_edge_compile_time_info, get_edge_resolve_options_context,
+    emit_assets, get_edge_chunking_context, get_edge_compile_time_info,
+    get_edge_resolve_options_context,
     instrumentation::instrumentation_files,
     middleware::middleware_files,
     mode::NextMode,
@@ -797,14 +798,17 @@ impl Project {
             let client_relative_path = self.client_relative_path();
             let node_root = self.node_root();
 
-            let completion = self.await?.versioned_content_map.insert_output_assets(
-                all_output_assets,
-                node_root,
+            self.await?
+                .versioned_content_map
+                .insert_output_assets(all_output_assets, client_relative_path, node_root)
+                .await?;
+
+            Ok(emit_assets(
+                *all_output_assets.await?,
+                self.node_root(),
                 client_relative_path,
                 node_root,
-            );
-
-            Ok(completion)
+            ))
         }
         .instrument(span)
         .await
