bf196f5e5a324487ab3257a6e39edbff82c7d29a
Tobias Koppers <tobias.koppers@googlemail.com>
2024-02-21 13:36:57 +0100

    Turbopack: reduce tasks needed for emitting (#62291)
    ### What?
    
    * reduce tasks needed for emitting
    * add user level tracing
    
    
    Closes PACK-2552

--- packages/next-swc/crates/next-api/src/project.rs
+++ packages/next-swc/crates/next-api/src/project.rs
@@ -5,8 +5,7 @@ use indexmap::{map::Entry, IndexMap};
 use next_core::{
     all_assets_from_entries,
     app_structure::find_app_dir,
-    emit_assets, get_edge_chunking_context, get_edge_compile_time_info,
-    get_edge_resolve_options_context,
+    get_edge_chunking_context, get_edge_compile_time_info, get_edge_resolve_options_context,
     instrumentation::instrumentation_files,
     middleware::middleware_files,
     mode::NextMode,
@@ -798,17 +797,14 @@ impl Project {
             let client_relative_path = self.client_relative_path();
             let node_root = self.node_root();
 
-            self.await?
-                .versioned_content_map
-                .insert_output_assets(all_output_assets, client_relative_path, node_root)
-                .await?;
-
-            Ok(emit_assets(
-                *all_output_assets.await?,
-                self.node_root(),
+            let completion = self.await?.versioned_content_map.insert_output_assets(
+                all_output_assets,
+                node_root,
                 client_relative_path,
                 node_root,
-            ))
+            );
+
+            Ok(completion)
         }
         .instrument(span)
         .await
