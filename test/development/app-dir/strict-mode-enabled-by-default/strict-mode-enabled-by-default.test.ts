66645119aae202b9fd4e85a75e87a0b3a6425888
Jiachi Liu <inbox@huozhi.im>
2024-02-21 15:58:48 +0100

    add retry

--- test/development/app-dir/strict-mode-enabled-by-default/strict-mode-enabled-by-default.test.ts
+++ test/development/app-dir/strict-mode-enabled-by-default/strict-mode-enabled-by-default.test.ts
@@ -1,5 +1,6 @@
+import { type BrowserInterface } from 'test/lib/browsers/base'
 import { createNextDescribe } from 'e2e-utils'
-import { BrowserInterface } from 'test/lib/browsers/base'
+import { retry } from 'next-test-utils'
 
 createNextDescribe(
   'Strict Mode enabled by default',
@@ -10,13 +11,15 @@ createNextDescribe(
     // Recommended for tests that need a full browser
     it('should work using browser', async () => {
       const browser: BrowserInterface = await next.browser('/')
-      const logs = await browser.log()
-      const userLogs = logs.filter(
-        (log) => log.source === 'log' && log.message.match(/logged \d times/)
-      )
-      expect(userLogs.length).toBe(2)
-      userLogs.forEach((log, i) => {
-        expect(log.message).toBe(`logged ${i + 1} times`)
+      await retry(async () => {
+        const logs = await browser.log()
+        const userLogs = logs.filter(
+          (log) => log.source === 'log' && log.message.match(/logged \d times/)
+        )
+        expect(userLogs.length).toBe(2)
+        userLogs.forEach((log, i) => {
+          expect(log.message).toBe(`logged ${i + 1} times`)
+        })
       })
     })
   }
