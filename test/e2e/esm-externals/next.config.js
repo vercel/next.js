1f5c6f1ee6de4b874bb9cf8e641f040af05eb0a0
Jiachi Liu <inbox@huozhi.im>
2024-02-22 13:36:12 +0100

    revert alias externals

--- test/e2e/esm-externals/next.config.js
+++ test/e2e/esm-externals/next.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+  // Testing that externals still work after aliasing
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'preact/compat': 'react',
+    }
+    return config
+  },
   experimental: {
+    turbo: {
+      resolveAlias: {
+        'preact/compat': 'react',
+      },
+    },
     serverComponentsExternalPackages: [
       'app-esm-package1',
       'app-esm-package2',
