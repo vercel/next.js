e628785bccb589d058bd9753d43eb810d2c87c00
Jiachi Liu <inbox@huozhi.im>
2024-02-23 15:49:57 +0100

    still throw when its simply server-client mismatch

--- packages/next/src/client/components/is-hydration-error.ts
+++ packages/next/src/client/components/is-hydration-error.ts
@@ -1,9 +1,5 @@
 import isError from '../../lib/is-error'
 
 export function isHydrationError(error: unknown): boolean {
-  return (
-    isError(error) &&
-    error.message.match(/(hydration|content does not match|did not match)/i) !=
-      null
-  )
+  return isError(error) && error.message.match(/hydration failed/i) != null
 }
