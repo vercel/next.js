419d1a96abfb8afc389895951568f45ada28325b
devjiwonchoi <devjiwonchoi@gmail.com>
2024-02-25 03:24:28 +0900

    test: add pkg type module esm project

---
+++ test/e2e/app-dir/next-config-ts/pkg-type-module-esm/index.test.ts
@@ -0,0 +1,12 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('pkg-type-module-esm', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should support ESM project package.json type module', async () => {
+    const $ = await next.render$('/')
+    expect($('p').text()).toBe('foo')
+  })
+})
