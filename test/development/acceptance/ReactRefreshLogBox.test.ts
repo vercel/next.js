ffa3fdd7b255eed3e67b8c92835399c325524147
Tobias Koppers <tobias.koppers@googlemail.com>
2024-02-26 08:32:17 +0000

    update snapshots

--- test/development/acceptance/ReactRefreshLogBox.test.ts
+++ test/development/acceptance/ReactRefreshLogBox.test.ts
@@ -212,7 +212,7 @@ describe.each(['default', 'turbo'])('ReactRefreshLogBox %s', () => {
     if (process.env.TURBOPACK) {
       expect(source).toMatchInlineSnapshot(`
         "./index.js:7:1
-        Ecmascript file had an error
+        Parsing ecmascript source code failed
           5 |     div
           6 |   )
         > 7 | }
