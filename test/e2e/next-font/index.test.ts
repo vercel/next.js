800626a3e4a0bac9e37876868e239df445cdcda1
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-02-27 15:18:04 +0900

    Update tests

--- test/e2e/next-font/index.test.ts
+++ test/e2e/next-font/index.test.ts
@@ -12,7 +12,7 @@ const mockedGoogleFontResponses = require.resolve(
 function getClassNameRegex(className: string): RegExp {
   // Turbopack uses a different format for its css modules than webpack-based Next.js
   return shouldRunTurboDevTest()
-    ? new RegExp(`^${className}__.*__.{8}$`) // e.g. `className__inter_c6e282f1__a8cc5613`
+    ? new RegExp(`.*__.{6}__${className}$`) // e.g. `className__inter_c6e282f1__a8cc5613`
     : new RegExp(`^__${className}_.{6}$`) // e.g. `__className_a8cc56`
 }
 
