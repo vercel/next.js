vercel/next.js

commit b08fb216bf45c1a679802edc6364fd7ea8474368

Fix instrument bundling as client components (#60984)
### What

When users're using nextjs server API that break the build, and error
with incorrect client components usage.
Instrument should be treated as server components or server-only build
target, instead of being treated as client components.

### How

We added a new layer `instrument` for instrument hook bundling, and
apply it with the rsc server webpack loaders

Fixes #57563
Closes NEXT-2181
Closes NEXT-1994

packages/next/src/build/webpack-config.ts
@@ -1429,6 +1429,11 @@ export default async function getBaseWebpackConfig(
               issuerLayer: WEBPACK_LAYERS.middleware,
               use: middlewareLayerLoaders,
             },
+            {
+              test: codeCondition.test,
+              issuerLayer: WEBPACK_LAYERS.instrument,
+              use: appServerLayerLoaders,
+            },
             ...(hasAppDir
               ? [
                   {

packages/next/src/build/entries.ts
@@ -40,6 +40,7 @@ import {
   isMiddlewareFile,
   isMiddlewareFilename,
   isInstrumentationHookFile,
+  isInstrumentationHookFilename,
 } from './utils'
 import { getPageStaticInfo } from './analysis/get-page-static-info'
 import { normalizePathSep } from '../shared/lib/page-path/normalize-path-sep'
@@ -391,13 +392,6 @@ export function getEdgeServerEntry(opts: {
     return `next-edge-function-loader?${stringify(loaderParams)}!`
   }
 
-  if (isInstrumentationHookFile(opts.page)) {
-    return {
-      import: opts.absolutePagePath,
-      filename: `edge-${INSTRUMENTATION_HOOK_FILENAME}.js`,
-    }
-  }
-
   const loaderParams: EdgeSSRLoaderQuery = {
     absolute500Path: opts.pages['/500'] || '',
     absoluteAppPath: opts.pages['/_app'],
@@ -428,6 +422,23 @@ export function getEdgeServerEntry(opts: {
     // be in the SSR layer — we later convert the page request to the RSC layer
     // via a webpack rule.
     layer: opts.appDirLoader ? WEBPACK_LAYERS.serverSideRendering : undefined,
+  }
+}
+
+export function getInstrumentationEntry(opts: {
+  absolutePagePath: string
+  isEdgeServer: boolean
+  isDev: boolean
+}) {
+  // the '../' is needed to make sure the file is not chunked
+  const filename = `${
+    opts.isEdgeServer ? 'edge-' : opts.isDev ? '' : '../'
+  }${INSTRUMENTATION_HOOK_FILENAME}.js`
+
+  return {
+    import: opts.absolutePagePath,
+    filename,
+    layer: WEBPACK_LAYERS.instrument,
   }
 }
 
@@ -605,6 +616,8 @@ export async function createEntrypoints(
         ]
       }
 
+      const isInstrumentation =
+        isInstrumentationHookFile(page) && pagesType === PAGE_TYPES.ROOT
       runDependingOnPageType({
         page,
         pageRuntime: staticInfo.runtime,
@@ -636,15 +649,13 @@ export async function createEntrypoints(
               preferredRegion: staticInfo.preferredRegion,
               middlewareConfig: encodeToBase64(staticInfo.middleware || {}),
             })
-          } else if (
-            isInstrumentationHookFile(page) &&
-            pagesType === PAGE_TYPES.ROOT
-          ) {
-            server[serverBundlePath.replace('src/', '')] = {
-              import: absolutePagePath,
-              // the '../' is needed to make sure the file is not chunked
-              filename: `../${INSTRUMENTATION_HOOK_FILENAME}.js`,
-            }
+          } else if (isInstrumentation) {
+            server[serverBundlePath.replace('src/', '')] =
+              getInstrumentationEntry({
+                absolutePagePath,
+                isEdgeServer: false,
+                isDev: false,
+              })
           } else if (isAPIRoute(page)) {
             server[serverBundlePath] = [
               getRouteLoaderEntry({
@@ -676,44 +687,49 @@ export async function createEntrypoints(
         },
         onEdgeServer: () => {
           let appDirLoader: string = ''
-          if (pagesType === 'app') {
-            const matchedAppPaths = appPathsPerRoute[normalizeAppPath(page)]
-            appDirLoader = getAppEntry({
-              name: serverBundlePath,
+          if (isInstrumentation) {
+            edgeServer[serverBundlePath.replace('src/', '')] =
+              getInstrumentationEntry({
+                absolutePagePath,
+                isEdgeServer: true,
+                isDev: false,
+              })
+          } else {
+            if (pagesType === 'app') {
+              const matchedAppPaths = appPathsPerRoute[normalizeAppPath(page)]
+              appDirLoader = getAppEntry({
+                name: serverBundlePath,
+                page,
+                pagePath: absolutePagePath,
+                appDir: appDir!,
+                appPaths: matchedAppPaths,
+                pageExtensions,
+                basePath: config.basePath,
+                assetPrefix: config.assetPrefix,
+                nextConfigOutput: config.output,
+                // This isn't used with edge as it needs to be set on the entry module, which will be the `edgeServerEntry` instead.
+                // Still passing it here for consistency.
+                preferredRegion: staticInfo.preferredRegion,
+                middlewareConfig: Buffer.from(
+                  JSON.stringify(staticInfo.middleware || {})
+                ).toString('base64'),
+              }).import
+            }
+            edgeServer[serverBundlePath] = getEdgeServerEntry({
+              ...params,
+              rootDir,
+              absolutePagePath: absolutePagePath,
+              bundlePath: clientBundlePath,
+              isDev: false,
+              isServerComponent,
               page,
-              pagePath: absolutePagePath,
-              appDir: appDir!,
-              appPaths: matchedAppPaths,
-              pageExtensions,
-              basePath: config.basePath,
-              assetPrefix: config.assetPrefix,
-              nextConfigOutput: config.output,
-              // This isn't used with edge as it needs to be set on the entry module, which will be the `edgeServerEntry` instead.
-              // Still passing it here for consistency.
+              middleware: staticInfo?.middleware,
+              pagesType,
+              appDirLoader,
               preferredRegion: staticInfo.preferredRegion,
-              middlewareConfig: Buffer.from(
-                JSON.stringify(staticInfo.middleware || {})
-              ).toString('base64'),
-            }).import
+              middlewareConfig: staticInfo.middleware,
+            })
           }
-          const normalizedServerBundlePath =
-            isInstrumentationHookFile(page) && pagesType === PAGE_TYPES.ROOT
-              ? serverBundlePath.replace('src/', '')
-              : serverBundlePath
-          edgeServer[normalizedServerBundlePath] = getEdgeServerEntry({
-            ...params,
-            rootDir,
-            absolutePagePath: absolutePagePath,
-            bundlePath: clientBundlePath,
-            isDev: false,
-            isServerComponent,
-            page,
-            middleware: staticInfo?.middleware,
-            pagesType,
-            appDirLoader,
-            preferredRegion: staticInfo.preferredRegion,
-            middlewareConfig: staticInfo.middleware,
-          })
         },
       })
     }
@@ -766,24 +782,29 @@ export function finalizeEntrypoint({
       : value
 
   const isApi = name.startsWith('pages/api/')
+  const isInstrumentation = isInstrumentationHookFilename(name)
 
   switch (compilerType) {
     case COMPILER_NAMES.server: {
+      const layer = isApi
+        ? WEBPACK_LAYERS.api
+        : isInstrumentation
+        ? WEBPACK_LAYERS.instrument
+        : isServerComponent
+        ? WEBPACK_LAYERS.reactServerComponents
+        : undefined
+
       return {
         publicPath: isApi ? '' : undefined,
         runtime: isApi ? 'webpack-api-runtime' : 'webpack-runtime',
-        layer: isApi
-          ? WEBPACK_LAYERS.api
-          : isServerComponent
-          ? WEBPACK_LAYERS.reactServerComponents
-          : undefined,
+        layer,
         ...entry,
       }
     }
     case COMPILER_NAMES.edgeServer: {
       return {
         layer:
-          isMiddlewareFilename(name) || isApi
+          isMiddlewareFilename(name) || isApi || isInstrumentation
             ? WEBPACK_LAYERS.middleware
             : undefined,
         library: { name: ['_ENTRIES', `middleware_[name]`], type: 'assign' },

packages/next/src/lib/constants.ts
@@ -130,6 +130,10 @@ const WEBPACK_LAYERS_NAMES = {
    */
   middleware: 'middleware',
   /**
+   * The layer for the instrumentation hooks.
+   */
+  instrument: 'instrument',
+  /**
    * The layer for assets on the edge.
    */
   edgeAsset: 'edge-asset',
@@ -158,9 +162,10 @@ const WEBPACK_LAYERS = {
       WEBPACK_LAYERS_NAMES.actionBrowser,
       WEBPACK_LAYERS_NAMES.appMetadataRoute,
       WEBPACK_LAYERS_NAMES.appRouteHandler,
+      WEBPACK_LAYERS_NAMES.instrument,
     ],
     nonClientServerTarget: [
-      // plus middleware and pages api
+      // middleware and pages api
       WEBPACK_LAYERS_NAMES.middleware,
       WEBPACK_LAYERS_NAMES.api,
     ],
@@ -172,6 +177,7 @@ const WEBPACK_LAYERS = {
       WEBPACK_LAYERS_NAMES.serverSideRendering,
       WEBPACK_LAYERS_NAMES.appPagesBrowser,
       WEBPACK_LAYERS_NAMES.shared,
+      WEBPACK_LAYERS_NAMES.instrument,
     ],
   },
 }

packages/next/src/server/dev/hot-reloader-webpack.ts
@@ -19,6 +19,7 @@ import {
   getAppEntry,
   runDependingOnPageType,
   getStaticInfoIncludingLayouts,
+  getInstrumentationEntry,
 } from '../../build/entries'
 import { watchCompilers } from '../../build/output'
 import * as Log from '../../build/output/log'
@@ -840,6 +841,9 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
               this.hasAppRouterEntrypoints = true
             }
 
+            const isInstrumentation =
+              isInstrumentationHookFile(page) && pageType === PAGE_TYPES.ROOT
+
             runDependingOnPageType({
               page,
               pageRuntime: staticInfo.runtime,
@@ -847,6 +851,23 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
               onEdgeServer: () => {
                 // TODO-APP: verify if child entry should support.
                 if (!isEdgeServerCompilation || !isEntry) return
+                entries[entryKey].status = BUILDING
+
+                if (isInstrumentation) {
+                  const normalizedBundlePath = bundlePath.replace('src/', '')
+                  entrypoints[normalizedBundlePath] = finalizeEntrypoint({
+                    compilerType: COMPILER_NAMES.edgeServer,
+                    name: normalizedBundlePath,
+                    value: getInstrumentationEntry({
+                      absolutePagePath: entryData.absolutePagePath,
+                      isEdgeServer: true,
+                      isDev: true,
+                    }),
+                    isServerComponent: true,
+                    hasAppDir,
+                  })
+                  return
+                }
                 const appDirLoader = isAppPath
                   ? getAppEntry({
                       name: bundlePath,
@@ -874,7 +895,6 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
                     }).import
                   : undefined
 
-                entries[entryKey].status = BUILDING
                 entrypoints[bundlePath] = finalizeEntrypoint({
                   compilerType: COMPILER_NAMES.edgeServer,
                   name: bundlePath,
@@ -934,7 +954,20 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
                 }
 
                 let value: { import: string; layer?: string } | string
-                if (isAppPath) {
+                if (isInstrumentation) {
+                  value = getInstrumentationEntry({
+                    absolutePagePath: entryData.absolutePagePath,
+                    isEdgeServer: false,
+                    isDev: true,
+                  })
+                  entrypoints[bundlePath] = finalizeEntrypoint({
+                    compilerType: COMPILER_NAMES.server,
+                    name: bundlePath,
+                    isServerComponent: true,
+                    value,
+                    hasAppDir,
+                  })
+                } else if (isAppPath) {
                   value = getAppEntry({
                     name: bundlePath,
                     page,
@@ -971,7 +1004,7 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
                   !isMiddlewareFile(page) &&
                   !isInternalComponent(relativeRequest) &&
                   !isNonRoutePagesPage(page) &&
-                  !(isInstrumentationHookFile(page) && pageType === 'root')
+                  !isInstrumentation
                 ) {
                   value = getRouteLoaderEntry({
                     kind: RouteKind.PAGES,

test/e2e/instrumentation-hook/general/instrumentation.js
@@ -1,3 +1,6 @@
+// Should not error
+import 'server-only'
+
 export function register() {
   if (process.env.NEXT_RUNTIME === 'edge') {
     console.log('instrumentation hook on the edge')

commit eaa7606de19b759b4fc439333a6d6cc104c1ad85

Fix instrumentation with only pages (#62622)
This ensures we properly transpile `instrumentation.ts` when only
`pages` is being used as previously we were relying on the `app`
specific loaders which aren't configured when an `app` directory isn't
present. We regressed on this in
https://github.com/vercel/next.js/pull/60984 as it was working as
expected prior to this commit

x-ref: [slack
thread](https://vercel.slack.com/archives/C011GK1JUBA/p1709075846401649?thread_ts=1706643408.233909&cid=C011GK1JUBA)

Closes NEXT-2632

packages/next/src/build/webpack-config.ts
@@ -500,6 +500,15 @@ export default async function getBaseWebpackConfig(
       ].filter(Boolean)
     : []
 
+  const instrumentLayerLoaders = [
+    // When using Babel, we will have to add the SWC loader
+    // as an additional pass to handle RSC correctly.
+    // This will cause some performance overhead but
+    // acceptable as Babel will not be recommended.
+    swcServerLayerLoader,
+    babelLoader,
+  ].filter(Boolean)
+
   const middlewareLayerLoaders = [
     // When using Babel, we will have to use SWC to do the optimization
     // for middleware to tree shake the unused default optimized imports like "next/server".
@@ -1478,7 +1487,7 @@ export default async function getBaseWebpackConfig(
             {
               test: codeCondition.test,
               issuerLayer: WEBPACK_LAYERS.instrument,
-              use: appServerLayerLoaders,
+              use: instrumentLayerLoaders,
             },
             ...(hasAppDir
               ? [

test/e2e/multi-zone/multi-zone.test.ts
@@ -11,9 +11,13 @@ createNextDescribe(
     startCommand: (global as any).isNextDev ? 'pnpm dev' : 'pnpm start',
     packageJson: {
       scripts: {
+        dev: 'node server.js',
+        build: 'yarn next build apps/host && yarn next build apps/guest',
+        start: 'NODE_ENV=production node server.js',
         'post-build': 'echo done',
       },
     },
+    dependencies: require('./app/package.json').dependencies,
   },
   ({ next, isNextDev }) => {
     it.each([

test/e2e/opentelemetry/app/app/[param]/rsc-fetch/edge/page.tsx
@@ -8,6 +8,6 @@ export async function generateMetadata() {
 }
 
 export default async function Page() {
-  const data = await fetch('https://vercel.com')
+  const data = await fetch('https://example.vercel.sh')
   return <pre>RESONSE: {data.status}</pre>
 }

test/e2e/opentelemetry/app/app/[param]/rsc-fetch/page.tsx
@@ -6,6 +6,11 @@ export async function generateMetadata() {
 }
 
 export default async function Page() {
-  const data = await fetch('https://vercel.com')
-  return <pre>{await data.text()}</pre>
+  const data = await fetch('https://example.vercel.sh')
+  return (
+    <>
+      <p>Page</p>
+      <pre>{await data.text()}</pre>
+    </>
+  )
 }

test/e2e/opentelemetry/instrumentation-minimal.ts
@@ -1,0 +1,3 @@
+export async function register() {
+  console.log('instrumentation log')
+}

test/e2e/opentelemetry/instrumentation-pages-app-only.test.ts
@@ -1,0 +1,67 @@
+import { nextTestSetup } from 'e2e-utils'
+import { retry } from 'next-test-utils'
+
+for (const { app, src, pathname, text } of [
+  {
+    pages: true,
+    src: false,
+    pathname: '/pages/param/getServerSideProps',
+    text: 'Page',
+  },
+  {
+    pages: true,
+    src: true,
+    pathname: '/pages/param/getServerSideProps',
+    text: 'Page',
+  },
+  {
+    app: true,
+    src: false,
+    pathname: '/app/param/rsc-fetch',
+    text: 'Page',
+  },
+  {
+    app: true,
+    src: true,
+    pathname: '/app/param/rsc-fetch',
+    text: 'Page',
+  },
+]) {
+  describe(`instrumentation ${app ? 'app' : 'pages'}${
+    src ? ' src/' : ''
+  }`, () => {
+    const curDir = app ? 'app' : 'pages'
+    const oppositeDir = app ? 'pages' : 'app'
+
+    const { next } = nextTestSetup({
+      files: __dirname,
+      env: {
+        NEXT_PUBLIC_SIMPLE_INSTRUMENT: '1',
+      },
+      skipDeployment: true,
+      packageJson: {
+        scripts: {
+          'setup-dir': `mv instrumentation-minimal.ts instrumentation.ts; rm -rf ${oppositeDir}${
+            src
+              ? `; mkdir src; mv ${curDir} src/; mv instrumentation.ts src/`
+              : ''
+          }`,
+          dev: 'pnpm setup-dir && next dev',
+          build: 'pnpm setup-dir && next build',
+          start: 'next start',
+        },
+      },
+      startCommand: `pnpm ${(global as any).isNextDev ? 'dev' : 'start'}`,
+      buildCommand: `pnpm build`,
+      dependencies: require('./package.json').dependencies,
+    })
+
+    it('should start and serve correctly', async () => {
+      const html = await next.render(pathname)
+      expect(html).toContain(text)
+      retry(() => {
+        expect(next.cliOutput).toContain('instrumentation log')
+      })
+    })
+  })
+}

test/e2e/opentelemetry/instrumentation.ts
@@ -1,6 +1,6 @@
-import { register as registerForTest } from './instrumentation-test'
-
 export async function register() {
+  const { register: registerForTest } = await import('./instrumentation-test')
+
   if (process.env.__NEXT_TEST_MODE) {
     registerForTest()
   } else if (process.env.NEXT_RUNTIME === 'nodejs') {

test/e2e/opentelemetry/opentelemetry.test.ts
@@ -129,12 +129,13 @@ createNextDescribe(
                         ],
                       },
                       {
-                        name: 'fetch GET https://vercel.com/',
+                        name: 'fetch GET https://example.vercel.sh/',
                         attributes: {
                           'http.method': 'GET',
-                          'http.url': 'https://vercel.com/',
-                          'net.peer.name': 'vercel.com',
-                          'next.span_name': 'fetch GET https://vercel.com/',
+                          'http.url': 'https://example.vercel.sh/',
+                          'net.peer.name': 'example.vercel.sh',
+                          'next.span_name':
+                            'fetch GET https://example.vercel.sh/',
                           'next.span_type': 'AppRender.fetch',
                         },
                         kind: 2,
@@ -265,14 +266,15 @@ createNextDescribe(
                         ],
                       },
                       {
-                        name: 'fetch GET https://vercel.com/',
+                        name: 'fetch GET https://example.vercel.sh/',
                         kind: 2,
                         attributes: {
-                          'next.span_name': 'fetch GET https://vercel.com/',
+                          'next.span_name':
+                            'fetch GET https://example.vercel.sh/',
                           'next.span_type': 'AppRender.fetch',
-                          'http.url': 'https://vercel.com/',
+                          'http.url': 'https://example.vercel.sh/',
                           'http.method': 'GET',
-                          'net.peer.name': 'vercel.com',
+                          'net.peer.name': 'example.vercel.sh',
                         },
                         status: { code: 0 },
                       },

test/lib/next-modes/base.ts
@@ -73,6 +73,8 @@ export class NextInstance {
     this.env = {}
     Object.assign(this, opts)
 
+    require('console').log('packageJson??', this.packageJson)
+
     if (!(global as any).isNextDeploy) {
       this.env = {
         ...this.env,
@@ -102,7 +104,17 @@ export class NextInstance {
         )
       }
 
-      await fs.cp(files.fsPath, this.testDir, { recursive: true })
+      await fs.cp(files.fsPath, this.testDir, {
+        recursive: true,
+        filter(source) {
+          // we don't copy a package.json as it's manually written
+          // via the createNextInstall process
+          if (path.relative(files.fsPath, source) === 'package.json') {
+            return false
+          }
+          return true
+        },
+      })
     } else {
       for (const filename of Object.keys(files)) {
         const item = files[filename]