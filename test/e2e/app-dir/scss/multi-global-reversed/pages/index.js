cc79a3f21316891b7f5000c49253aa1de49386fb
Tim Neutkens <tim@timneutkens.nl>
2024-03-01 19:03:43 +0100

    Revert "Migrate Sass tests to test/e2e (#62321)"
    This reverts commit c0b1383c394613999cdd69d75d3c299de0e95ae5.

--- test/e2e/app-dir/scss/multi-global-reversed/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="red-text blue-text" id="verify-blue">
-      This text should be blue.
-    </div>
-  )
-}
--- test/lib/e2e-utils.ts
+++ test/lib/e2e-utils.ts
@@ -254,11 +254,6 @@ export function nextTestSetup(
 
   const nextProxy = new Proxy<NextInstance>({} as NextInstance, {
     get: function (_target, property) {
-      if (!next) {
-        throw new Error(
-          'next instance is not initialized yet, make sure you call methods on next instance in test body.'
-        )
-      }
       const prop = next[property]
       return typeof prop === 'function' ? prop.bind(next) : prop
     },
