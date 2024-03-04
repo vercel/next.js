8b45317dde30eb19bb2155a0d078bbd1e112bd32
Ethan Arrowood <ethan@arrowood.dev>
2024-03-04 14:32:05 -0700

    simplify streamToString method

--- packages/next/src/server/stream-utils/node-web-streams-helper.ts
+++ packages/next/src/server/stream-utils/node-web-streams-helper.ts
@@ -2,7 +2,6 @@ import type { FlightRouterState } from '../app-render/types'
 
 import { getTracer } from '../lib/trace/tracer'
 import { AppRenderSpan } from '../lib/trace/constants'
-import { createDecodeTransformStream } from './encode-decode'
 import { DetachedPromise } from '../../lib/detached-promise'
 import { scheduleImmediate, atLeastOneTask } from '../../lib/scheduler'
 
@@ -73,20 +72,17 @@ export function streamFromString(str: string): ReadableStream<Uint8Array> {
 export async function streamToString(
   stream: ReadableStream<Uint8Array>
 ): Promise<string> {
-  let buffer = ''
+  const decoder = new TextDecoder()
+  let string = ''
 
-  await stream
-    // Decode the streamed chunks to turn them into strings.
-    .pipeThrough(createDecodeTransformStream())
-    .pipeTo(
-      new WritableStream<string>({
-        write(chunk) {
-          buffer += chunk
-        },
-      })
-    )
+  // @ts-expect-error TypeScript gets this wrong (https://nodejs.org/api/webstreams.html#async-iteration)
+  for await (const chunk of stream) {
+    string += decoder.decode(chunk, { stream: true })
+  }
 
-  return buffer
+  string += decoder.decode()
+
+  return string
 }
 
 export function createBufferedTransformStream(): TransformStream<
