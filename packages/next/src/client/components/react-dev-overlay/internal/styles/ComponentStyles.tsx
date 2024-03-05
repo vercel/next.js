1b4350925142b178ebad3983549961f6d45949c4
Balázs Orbán <info@balazsorban.com>
2024-03-05 14:44:27 +0100

    fix build

--- packages/next/src/client/components/react-dev-overlay/internal/styles/ComponentStyles.tsx
+++ packages/next/src/client/components/react-dev-overlay/internal/styles/ComponentStyles.tsx
@@ -8,7 +8,6 @@ import { styles as terminal } from '../components/Terminal/styles'
 import { styles as toast } from '../components/Toast'
 import { styles as versionStaleness } from '../components/VersionStalenessInfo'
 import { styles as buildErrorStyles } from '../container/BuildError'
-import { styles as rootLayoutErrorStyles } from '../container/RootLayoutError'
 import { styles as containerErrorStyles } from '../container/Errors'
 import { styles as containerRuntimeErrorStyles } from '../container/RuntimeError'
 import { noop as css } from '../helpers/noop-template'
@@ -25,7 +24,6 @@ export function ComponentStyles() {
         ${terminal}
         
         ${buildErrorStyles}
-        ${rootLayoutErrorStyles}
         ${containerErrorStyles}
         ${containerRuntimeErrorStyles}
         ${versionStaleness}
