b0b97786f7f4097efba61a9fec8eb8c5f58ad195
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-03-05 17:56:52 +0100

    WIP "vendor" compiled types

--- packages/next/types/compiled.d.ts
+++ packages/next/types/compiled.d.ts
@@ -4,12 +4,15 @@
 
 declare module 'next/dist/compiled/webpack/webpack' {
   export function init(): void
+
   export let BasicEvaluatedExpression: any
   export let GraphHelpers: any
   export let sources: any
   export let StringXor: any
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export type LoaderDefinitionFunction<T> = any
+
   namespace webpack {
     export type Compiler = any
     export type WebpackPluginInstance = any
@@ -35,6 +38,7 @@ declare module 'next/dist/compiled/webpack/webpack' {
       export type RawSource = any
     }
   }
+
   export var webpack: any
 }
 
@@ -53,3 +57,89 @@ declare module 'VAR_MODULE_GLOBAL_ERROR'
 declare module 'VAR_USERLAND'
 declare module 'VAR_MODULE_DOCUMENT'
 declare module 'VAR_MODULE_APP'
+
+declare module 'next/dist/compiled/@hapi/accept'
+declare module 'next/dist/compiled/@mswjs/interceptors/ClientRequest'
+declare module 'next/dist/compiled/@napi-rs/triples'
+declare module 'next/dist/compiled/@next/font/dist/google'
+declare module 'next/dist/compiled/@next/font/dist/local'
+declare module 'next/dist/compiled/@next/react-refresh-utils/dist/ReactRefreshWebpackPlugin'
+declare module 'next/dist/compiled/@opentelemetry/api'
+declare module 'next/dist/compiled/@vercel/nft'
+declare module 'next/dist/compiled/amphtml-validator'
+declare module 'next/dist/compiled/anser'
+declare module 'next/dist/compiled/arg/index.js'
+declare module 'next/dist/compiled/async-retry'
+declare module 'next/dist/compiled/async-sema'
+declare module 'next/dist/compiled/babel/code-frame'
+declare module 'next/dist/compiled/babel/core'
+declare module 'next/dist/compiled/babel/core-lib-block-hoist-plugin'
+declare module 'next/dist/compiled/babel/core-lib-config'
+declare module 'next/dist/compiled/babel/core-lib-normalize-file'
+declare module 'next/dist/compiled/babel/core-lib-normalize-opts'
+declare module 'next/dist/compiled/babel/core-lib-plugin-pass'
+declare module 'next/dist/compiled/babel/generator'
+declare module 'next/dist/compiled/babel/plugin-syntax-jsx'
+declare module 'next/dist/compiled/babel/plugin-transform-modules-commonjs'
+declare module 'next/dist/compiled/babel/traverse'
+declare module 'next/dist/compiled/browserslist'
+declare module 'next/dist/compiled/bytes'
+declare module 'next/dist/compiled/ci-info'
+declare module 'next/dist/compiled/comment-json'
+declare module 'next/dist/compiled/compression'
+declare module 'next/dist/compiled/conf'
+declare module 'next/dist/compiled/content-disposition'
+declare module 'next/dist/compiled/content-type'
+declare module 'next/dist/compiled/cookie'
+declare module 'next/dist/compiled/cross-spawn'
+declare module 'next/dist/compiled/cssnano-simple'
+declare module 'next/dist/compiled/data-uri-to-buffer'
+declare module 'next/dist/compiled/debug'
+declare module 'next/dist/compiled/devalue'
+declare module 'next/dist/compiled/edge-runtime'
+declare module 'next/dist/compiled/find-up'
+declare module 'next/dist/compiled/fresh'
+declare module 'next/dist/compiled/get-orientation'
+declare module 'next/dist/compiled/gzip-size'
+declare module 'next/dist/compiled/icss-utils'
+declare module 'next/dist/compiled/image-size'
+declare module 'next/dist/compiled/is-animated'
+declare module 'next/dist/compiled/is-docker'
+declare module 'next/dist/compiled/is-wsl'
+declare module 'next/dist/compiled/jest-worker'
+declare module 'next/dist/compiled/json5'
+declare module 'next/dist/compiled/loader-utils2'
+declare module 'next/dist/compiled/loader-utils3'
+declare module 'next/dist/compiled/lodash.curry'
+declare module 'next/dist/compiled/lru-cache'
+declare module 'next/dist/compiled/nanoid/index.cjs'
+declare module 'next/dist/compiled/node-fetch'
+declare module 'next/dist/compiled/node-html-parser'
+declare module 'next/dist/compiled/ora'
+declare module 'next/dist/compiled/p-limit'
+declare module 'next/dist/compiled/path-to-regexp'
+declare module 'next/dist/compiled/picomatch'
+declare module 'next/dist/compiled/postcss-modules-extract-imports'
+declare module 'next/dist/compiled/postcss-modules-local-by-default'
+declare module 'next/dist/compiled/postcss-modules-scope'
+declare module 'next/dist/compiled/postcss-modules-values'
+declare module 'next/dist/compiled/postcss-scss'
+declare module 'next/dist/compiled/postcss-value-parser'
+declare module 'next/dist/compiled/react-is'
+declare module 'next/dist/compiled/semver'
+declare module 'next/dist/compiled/send'
+declare module 'next/dist/compiled/shell-quote'
+declare module 'next/dist/compiled/source-map'
+declare module 'next/dist/compiled/source-map08'
+declare module 'next/dist/compiled/stacktrace-parser'
+declare module 'next/dist/compiled/string-hash'
+declare module 'next/dist/compiled/strip-ansi'
+declare module 'next/dist/compiled/tar'
+declare module 'next/dist/compiled/terser'
+declare module 'next/dist/compiled/text-table'
+declare module 'next/dist/compiled/ua-parser-js'
+declare module 'next/dist/compiled/unistore'
+declare module 'next/dist/compiled/watchpack'
+declare module 'next/dist/compiled/web-vitals'
+declare module 'next/dist/compiled/ws'
+declare module 'next/dist/compiled/zod'
