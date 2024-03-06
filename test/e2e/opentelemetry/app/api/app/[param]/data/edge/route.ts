439ae34165a3ddc94d002e607f674f758b9d49ad
JJ Kasper <jj@jjsweb.site>
2024-03-06 10:43:15 -0800

    move performance check to module scope

--- test/e2e/opentelemetry/app/api/app/[param]/data/edge/route.ts
+++ test/e2e/opentelemetry/app/api/app/[param]/data/edge/route.ts
@@ -1,7 +1,7 @@
-export async function GET() {
-  // ensure performance is available in edge
-  console.log(performance.now())
+// ensure performance is available in edge
+console.log(performance.now())
 
+export async function GET() {
   return new Response(JSON.stringify({ test: 'data-edge' }))
 }
 
--- test/e2e/opentelemetry/pages/api/pages/[param]/edge.ts
+++ test/e2e/opentelemetry/pages/api/pages/[param]/edge.ts
@@ -1,10 +1,9 @@
 export const config = {
   runtime: 'edge',
 }
+// ensure performance is available in edge
+console.log(performance.now())
 
 export default function handler(req) {
-  // ensure performance is available in edge
-  console.log(performance.now())
-
   return Response.json({ text: 'hello world' })
 }
