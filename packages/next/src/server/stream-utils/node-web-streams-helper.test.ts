1e473adeb9660c5934d9cba339abd6618c9d0ad4
Ethan Arrowood <ethan@arrowood.dev>
2024-03-07 09:22:55 -0700

    Initialize node-web-stream-helper.ts tests (#62973)
    This PR adds `node-web-stream-helper.test.ts`, adds some tests for a
    couple of the methods and stubs out the rest for future completion.
    
    
    
    Closes NEXT-2736

---
+++ packages/next/src/server/stream-utils/node-web-streams-helper.test.ts
@@ -0,0 +1,112 @@
+import {
+  chainStreams,
+  streamFromString,
+  streamToString,
+} from './node-web-streams-helper'
+
+describe('node-web-stream-helpers', () => {
+  describe('streamFromString', () => {
+    it('should encode the string into a stream', async () => {
+      const stream = streamFromString('abc')
+      const reader = stream.getReader()
+      let { done, value } = await reader.read()
+      expect(done).toStrictEqual(false)
+      expect(value).toStrictEqual(new Uint8Array([97, 98, 99]))
+      ;({ done, value } = await reader.read())
+      expect(done).toStrictEqual(true)
+      expect(value).toStrictEqual(undefined)
+    })
+  })
+  describe('streamToString', () => {
+    it('should decode the stream into a string', async () => {
+      const input = 'abc'
+      const stream = new TextEncoderStream()
+      const p = streamToString(stream.readable)
+      const writer = stream.writable.getWriter()
+      await writer.write(input)
+      await writer.close()
+      const output = await p
+      expect(output).toStrictEqual(input)
+    })
+  })
+  it('streamFromString and streamToString should be reflective', async () => {
+    const input = 'abcdefghijklmnopqrstuvwxyz'
+    const stream = streamFromString(input)
+    const output = await streamToString(stream)
+    expect(output).toBe(input)
+  })
+
+  describe('chainStreams', () => {
+    it('should throw error on 0 args', () => {
+      expect(() => chainStreams()).toThrow(
+        'Invariant: chainStreams requires at least one stream'
+      )
+    })
+    it('should return singular stream argument', () => {
+      const stream = new ReadableStream()
+      const actual = chainStreams(stream)
+      expect(actual).toStrictEqual(stream)
+    })
+    it('should chain streams in order', async () => {
+      const createReadableStream = (data: string) => {
+        return new ReadableStream({
+          start(controller) {
+            controller.enqueue(data)
+            controller.close()
+          },
+        })
+      }
+      const inputs = ['abcd', 'efgh', 'ijkl', 'mnop', 'qrst', 'uvwx', 'yz00']
+      const streams = inputs.map((input) => createReadableStream(input))
+      const stream = chainStreams(...streams)
+      const reader = stream.getReader()
+      for (let i = 0; i < inputs.length; i++) {
+        const { done, value } = await reader.read()
+        expect(done).toStrictEqual(false)
+        expect(value).toStrictEqual(inputs[i])
+      }
+      const { done, value } = await reader.read()
+      expect(done).toStrictEqual(true)
+      expect(value).toStrictEqual(undefined)
+    })
+    it('should throw errors from chained streams', async () => {
+      const r1 = new ReadableStream({
+        start(controller) {
+          controller.enqueue('abcd')
+          controller.close()
+        },
+      })
+      const r2 = new ReadableStream({
+        start(controller) {
+          controller.error(new Error('Error from ReadableStream 2'))
+        },
+      })
+      const chained = chainStreams(r1, r2)
+      const reader = chained.getReader()
+      let { done, value } = await reader.read()
+      expect(done).toStrictEqual(false)
+      expect(value).toStrictEqual('abcd')
+      try {
+        await reader.read()
+      } catch (err) {
+        expect(err).toStrictEqual(new Error('Error from ReadableStream 2'))
+      }
+    })
+  })
+
+  describe('createBufferedTransformStream', () => {})
+  describe('createInsertedHTMLStream', () => {})
+  describe('renderToInitialFizzStream', () => {})
+  describe('createHeadInsertionTransformStream', () => {})
+  describe('createDeferredSuffixStream', () => {})
+  describe('createMergedTransformStream', () => {})
+  describe('createMoveSuffixStream', () => {})
+  describe('createStripDocumentClosingTagsTransform', () => {})
+  describe('createRootLayoutValidatorStream', () => {})
+  describe('chainTransformers', () => {})
+  describe('continueFizzStream', () => {})
+  describe('continueDynamicPrerender', () => {})
+  describe('continueStaticPrerender', () => {})
+  describe('continueDynamicHTMLResume', () => {})
+  describe('continueDynamicDataResume', () => {})
+})
