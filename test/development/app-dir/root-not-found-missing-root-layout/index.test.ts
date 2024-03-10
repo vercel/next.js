d5ffaac4d9967d332364f99e50f63eaa6868955e
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-10 15:17:09 +0900

    refactor: clearer conditional for skip test if turbo

--- test/development/app-dir/root-not-found-missing-root-layout/index.test.ts
+++ test/development/app-dir/root-not-found-missing-root-layout/index.test.ts
@@ -5,14 +5,14 @@ describe('root-not-found-missing-root-layout', () => {
     files: __dirname,
   })
 
-  if (!isTurbopack) {
-    it('should not conflict with generated layout on dev server', async () => {
+  // Skip test for turbo dev for now since generating a missing root layout is not supported yet.
+  // See https://github.com/vercel/next.js/pull/63053#issuecomment-1987101666
+  ;(isTurbopack ? it.skip : it)(
+    'should not conflict with generated layout on dev server',
+    async () => {
       const browser = await next.browser('/')
+      // eslint-disable-next-line jest/no-standalone-expect
       expect(await browser.elementByCss('p').text()).toBe('not found')
-    })
-  } else {
-    // skip test for turbo dev server
-    // See https://github.com/vercel/next.js/pull/63053#issuecomment-1987101666
-    it.skip('should not conflict with generated layout on turbo dev server', () => {})
-  }
+    }
+  )
 })
