b70e245eb2305250480368f1355592cd0bfadfb1
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-10 15:02:46 +0900

    test: skip test for turbo

--- test/development/app-dir/root-not-found-missing-root-layout/index.test.ts
+++ test/development/app-dir/root-not-found-missing-root-layout/index.test.ts
@@ -1,12 +1,18 @@
 import { nextTestSetup } from 'e2e-utils'
 
 describe('root-not-found-missing-root-layout', () => {
-  const { next } = nextTestSetup({
+  const { next, isTurbopack } = nextTestSetup({
     files: __dirname,
   })
 
-  it('should not conflict with generated layout on dev server', async () => {
-    const browser = await next.browser('/')
-    expect(await browser.elementByCss('p').text()).toBe('not found')
-  })
+  if (!isTurbopack) {
+    it('should not conflict with generated layout on dev server', async () => {
+      const browser = await next.browser('/')
+      expect(await browser.elementByCss('p').text()).toBe('not found')
+    })
+  } else {
+    // skip test for turbo dev server
+    // See https://github.com/vercel/next.js/pull/63053#issuecomment-1987101666
+    it.skip('should not conflict with generated layout on turbo dev server', () => {})
+  }
 })
