41019c23142021e1cc605bf69b930e726cb0eb6d
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-03-11 03:49:18 +0100

    Update Sanity example for App Router (#63045)
    This PR updates the `cms-sanity` example to use:
    - App Router
    - TypeScript
    - Sanity Studio v3 instead of v2
    - Embeds the Studio inside the next app on the `/studio` route.
    - ISR / Data Cache (revalidations through `revalidatePath` while in Live
    Visual Editing, time-based to match the Sanity API CDN in production).
    - Support Vercel Visual Editing out of the box.
    - The new `next-sanity/image` component.
    - Vercel Speed Insights.
    - The Sanity Presentation Tool for live content previews.
    - Sanity Portable Text setup to fully support `@tailwindcss/typography`.
    - [AI Assist](https://www.sanity.io/docs/ai-assist)
      - Auto fill in `alt` text on images.
      - Preset prompts for content creation

---
+++ examples/cms-sanity/sanity/schemas/documents/author.ts
@@ -0,0 +1,38 @@
+import { UserIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "author",
+  title: "Author",
+  icon: UserIcon,
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "picture",
+      title: "Picture",
+      type: "image",
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+          description: "Important for SEO and accessiblity.",
+          validation: (rule) => rule.required(),
+        },
+      ],
+      options: {
+        hotspot: true,
+        aiAssist: {
+          imageDescriptionField: "alt",
+        },
+      },
+      validation: (rule) => rule.required(),
+    }),
+  ],
+});
