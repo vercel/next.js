759435bdc9d9fbd581fd1c8d362e1e96e1071e5f
Cody Olsen <stipsan@gmail.com>
2024-03-11 11:38:13 +0100

    Update Sanity example for App Router

--- examples/cms-sanity/sanity/schemas/documents/author.ts
+++ examples/cms-sanity/sanity/schemas/documents/author.ts
@@ -23,7 +23,14 @@ export default defineType({
           type: "string",
           title: "Alternative text",
           description: "Important for SEO and accessiblity.",
-          validation: (rule) => rule.required(),
+          validation: (rule) => {
+            return rule.custom((alt, context) => {
+              if ((context.document?.picture as any)?.asset?._ref && !alt) {
+                return "Required";
+              }
+              return true;
+            });
+          },
         },
       ],
       options: {
