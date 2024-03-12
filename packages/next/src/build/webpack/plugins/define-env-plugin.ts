a36fbab713ddafd7cb9832b8bce74f2efcc79e23
Tim Neutkens <tim@timneutkens.nl>
2024-03-12 11:29:40 +0100

    Fix

--- packages/next/src/build/webpack/plugins/define-env-plugin.ts
+++ packages/next/src/build/webpack/plugins/define-env-plugin.ts
@@ -10,7 +10,7 @@ function errorIfEnvConflicted(config: NextConfigComplete, key: string) {
   const isPrivateKey = /^(?:NODE_.+)|^(?:__.+)$/i.test(key)
   const hasNextRuntimeKey = key === 'NEXT_RUNTIME'
 
-  if (isPrivateKey ?? hasNextRuntimeKey) {
+  if (isPrivateKey || hasNextRuntimeKey) {
     throw new Error(
       `The key "${key}" under "env" in ${config.configFileName} is not allowed. https://nextjs.org/docs/messages/env-key-not-allowed`
     )
