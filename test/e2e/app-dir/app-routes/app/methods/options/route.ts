ad40d4913402dc22945c9dba9c88e8d784834d3f
samcx <sam@vercel.com>
2024-03-13 16:53:26 -0700

    test(app-dir/app-routes): fix comment

--- test/e2e/app-dir/app-routes/app/methods/options/route.ts
+++ test/e2e/app-dir/app-routes/app/methods/options/route.ts
@@ -1,3 +1,3 @@
-// This route exports GET, POST, and DELETE. The test verifies that this route
+// This route exports  POST. The test verifies that this route
 // will handle the OPTIONS request.
 export { POST } from '../../../handlers/hello'
