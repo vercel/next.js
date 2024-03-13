84da16e21da2540f23f2c92b835ef1d4131ec9e8
samcx <sam@vercel.com>
2024-03-13 11:02:47 -0700

    chore(examples): prettier file

--- examples/with-mongodb/lib/mongodb.ts
+++ examples/with-mongodb/lib/mongodb.ts
@@ -1,17 +1,17 @@
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-}
+  },
+};
 
 let client;
 let clientPromise: Promise<MongoClient>;
