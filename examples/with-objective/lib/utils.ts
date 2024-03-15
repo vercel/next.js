19500dc8bacfc83075d71b0c3d096f519ed7713d
David Parks <Davidthomasparks@gmail.com>
2024-03-15 09:59:42 -0500

    Remove errors

--- examples/with-objective/lib/utils.ts
+++ examples/with-objective/lib/utils.ts
@@ -1,6 +1,5 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { ErrorMessage } from "./error"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
