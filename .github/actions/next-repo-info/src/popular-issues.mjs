e307f9760f4a6150ec432fb5f1ea8fe655b127ef
samcx <sam@vercel.com>
2024-03-18 14:40:32 -0700

    Update popular-issues

--- .github/actions/next-repo-info/src/popular-issues.mjs
+++ .github/actions/next-repo-info/src/popular-issues.mjs
@@ -1,39 +1,39 @@
 // @ts-check
 import { context, getOctokit } from '@actions/github'
-import { setFailed, info } from '@actions/core'
-import { WebClient } from '@slack/web-api'
+import { setFailed } from '@actions/core'
+// import { WebClient } from '@slack/web-api'
 
-function generateBlocks(issues) {
-  const blocks = [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: '*A list of the top 15 issues sorted by most :+1: reactions over the last 90 days.*\n_Note: This :github2: workflow will run every Monday at 1PM UTC (9AM EST)._',
-      },
-    },
-    {
-      type: 'divider',
-    },
-  ]
+// function generateBlocks(issues) {
+//   const blocks = [
+//     {
+//       type: 'section',
+//       text: {
+//         type: 'mrkdwn',
+//         text: '*A list of the top 15 issues sorted by most :+1: reactions over the last 90 days.*\n_Note: This :github2: <https://github.com/vercel/next.js/blob/canary/.github/workflows/issue_popular.yml|workflow> → <https://github.com/vercel/next.js/blob/canary/.github/actions/next-repo-info/src/popular-issues.mjs|action> will run every Monday at 1PM UTC (9AM EST)._',
+//       },
+//     },
+//     {
+//       type: 'divider',
+//     },
+//   ]
 
-  let text = ''
-  issues.forEach((issue, i) => {
-    text += `${i + 1}. [<${issue.html_url}|#${issue.number}>, :+1: ${
-      issue.reactions['+1']
-    }]: ${issue.title}\n`
-  })
+//   let text = ''
+//   issues.forEach((issue, i) => {
+//     text += `${i + 1}. [<${issue.html_url}|#${issue.number}>, :+1: ${
+//       issue.reactions['+1']
+//     }]: ${issue.title}\n`
+//   })
 
-  blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: text,
-    },
-  })
+//   blocks.push({
+//     type: 'section',
+//     text: {
+//       type: 'mrkdwn',
+//       text: text,
+//     },
+//   })
 
-  return blocks
-}
+//   return blocks
+// }
 
 async function run() {
   try {
@@ -41,7 +41,7 @@ async function run() {
     if (!process.env.SLACK_TOKEN) throw new TypeError('SLACK_TOKEN not set')
 
     const octoClient = getOctokit(process.env.GITHUB_TOKEN)
-    const slackClient = new WebClient(process.env.SLACK_TOKEN)
+    // const slackClient = new WebClient(process.env.SLACK_TOKEN)
 
     // Get the date 90 days ago (YYYY-MM-DD)
     const date = new Date()
@@ -56,18 +56,20 @@ async function run() {
       sort: 'reactions-+1',
     })
 
-    if (data.items.length > 0) {
-      await slackClient.chat.postMessage({
-        blocks: generateBlocks(data.items),
-        channel: '#team-next-js',
-        icon_emoji: ':github:',
-        username: 'GitHub Notifier',
-      })
+    console.log('[test] data =', data)
 
-      info(`Posted to Slack!`)
-    } else {
-      info(`No popular issues`)
-    }
+    // if (data.items.length > 0) {
+    //   await slackClient.chat.postMessage({
+    //     blocks: generateBlocks(data.items),
+    //     channel: '#team-next-js',
+    //     icon_emoji: ':github:',
+    //     username: 'GitHub Notifier',
+    //   })
+
+    //   info(`Posted to Slack!`)
+    // } else {
+    //   info(`No popular issues`)
+    // }
   } catch (error) {
     setFailed(error)
   }
