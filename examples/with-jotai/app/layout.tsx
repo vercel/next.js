642357ed83f2f37665fd2469744c42e4d7330555
arian <ariannargesi@gmail.com>
2024-03-19 00:46:47 +0330

    prettier files

--- examples/with-jotai/app/layout.tsx
+++ examples/with-jotai/app/layout.tsx
@@ -1,22 +1,20 @@
-import { Provider } from "jotai"
-import { Metadata } from "next"
+import { Provider } from "jotai";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  icons: '/favicon.ico'
-}
+  icons: "/favicon.ico",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Provider>
-          {children}
-        </Provider>
+        <Provider>{children}</Provider>
       </body>
     </html>
-  )
+  );
 }
--- examples/with-jotai/components/Canvas.tsx
+++ examples/with-jotai/components/Canvas.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { atom, useAtom } from "jotai";
 
