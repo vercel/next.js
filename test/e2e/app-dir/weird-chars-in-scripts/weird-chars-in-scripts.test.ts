8e542992398cef96dc204fdd33f7d34b01da6dc1
Will Binns-Smith <wbinnssmith@gmail.com>
2024-03-25 15:52:28 -0700

    fixup! Merge remote-tracking branch 'origin/canary' into sokra/improve-escape

--- test/e2e/app-dir/weird-chars-in-scripts/weird-chars-in-scripts.test.ts
+++ test/e2e/app-dir/weird-chars-in-scripts/weird-chars-in-scripts.test.ts
@@ -1,36 +1,34 @@
-import { createNextDescribe } from 'e2e-utils'
+/* eslint-disable jest/no-standalone-expect */
+import { nextTestSetup } from 'e2e-utils'
 
-createNextDescribe(
-  'weird chars in scripts',
-  {
+describe('weird chars in scripts', () => {
+  const { next } = nextTestSetup({
     files: __dirname,
-  },
-  ({ next }) => {
-    // TODO: fix test case in webpack
-    // It's failing with `Could not find the module ".../app/client#component.tsx#" in the React Client Manifest. This is probably a bug in the React Server Components bundler.`
-    ;(process.env.TURBOPACK ? it : it.skip)(
-      'should load in the browser',
-      async () => {
-        const browser = await next.browser('/')
-        expect(await browser.elementByCss('p').text()).toBe('hello world')
-        const scripts = await browser.elementsByCss('script')
-        for (const script of scripts) {
-          const src = await script.evaluate((script) => script.src)
-          expect(src).not.toContain('#')
-        }
+  })
+  // TODO: fix test case in webpack
+  // It's failing with `Could not find the module ".../app/client#component.tsx#" in the React Client Manifest. This is probably a bug in the React Server Components bundler.`
+  ;(process.env.TURBOPACK ? it : it.skip)(
+    'should load in the browser',
+    async () => {
+      const browser = await next.browser('/')
+      expect(await browser.elementByCss('p').text()).toBe('hello world')
+      const scripts = await browser.elementsByCss('script')
+      for (const script of scripts) {
+        const src = await script.evaluate((script) => script.src)
+        expect(src).not.toContain('#')
       }
-    )
-    ;(process.env.TURBOPACK ? it : it.skip)(
-      'should load in the browser',
-      async () => {
-        const browser = await next.browser('/pages')
-        expect(await browser.elementByCss('p').text()).toBe('hello world')
-        const scripts = await browser.elementsByCss('script')
-        for (const script of scripts) {
-          const src = await script.evaluate((script) => script.src)
-          expect(src).not.toContain('#')
-        }
+    }
+  )
+  ;(process.env.TURBOPACK ? it : it.skip)(
+    'should load in the browser',
+    async () => {
+      const browser = await next.browser('/pages')
+      expect(await browser.elementByCss('p').text()).toBe('hello world')
+      const scripts = await browser.elementsByCss('script')
+      for (const script of scripts) {
+        const src = await script.evaluate((script) => script.src)
+        expect(src).not.toContain('#')
       }
-    )
-  }
-)
+    }
+  )
+})
