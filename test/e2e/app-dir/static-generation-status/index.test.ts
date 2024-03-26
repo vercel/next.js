b85d08e7f126b54c46e1a38ff9dec7ed694cfc0c
Jiachi Liu <inbox@huozhi.im>
2024-03-26 22:26:31 +0100

    move to e2e test, update turbo build manifest

--- test/e2e/app-dir/static-generation-status/index.test.ts
+++ test/e2e/app-dir/static-generation-status/index.test.ts
@@ -5,7 +5,7 @@ createNextDescribe(
   {
     files: __dirname,
   },
-  ({ next, isTurbopack }) => {
+  ({ next }) => {
     it('should render the page using notFound with status 404', async () => {
       const { status } = await next.fetch('/not-found-page')
       expect(status).toBe(404)
@@ -17,9 +17,7 @@ createNextDescribe(
     })
 
     it('should render the non existed route redirect with status 404', async () => {
-      expect((await next.fetch('/_not-found')).status).toBe(
-        isTurbopack ? 200 : 404
-      )
+      expect((await next.fetch('/_not-found')).status).toBe(404)
       expect((await next.fetch('/does-not-exist')).status).toBe(404)
     })
   }
