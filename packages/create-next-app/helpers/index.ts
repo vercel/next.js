241fa6fc49c23706caaaf1ae0ec9a7ec0a5db19e
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-27 15:28:14 +0900

    refactor: barrel export helpers

---
+++ packages/create-next-app/helpers/index.ts
@@ -0,0 +1,16 @@
+export { copy } from './copy'
+export {
+  isUrlOk,
+  getRepoInfo,
+  hasRepo,
+  existsInRepo,
+  downloadAndExtractRepo,
+  downloadAndExtractExample,
+} from './examples'
+export { getPkgManager } from './get-pkg-manager'
+export { tryGitInit } from './git'
+export { install } from './install'
+export { isFolderEmpty } from './is-folder-empty'
+export { getOnline } from './is-online'
+export { isWriteable } from './is-writeable'
+export { validateNpmName } from './validate-pkg'
