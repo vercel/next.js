782672008745cbba32d1291964d4dff33916b7cd
Jiachi Liu <inbox@huozhi.im>
2024-03-27 11:16:12 +0100

    add parallel routes static generation test

---
+++ test/production/app-dir/parallel-routes-static/index.test.ts
@@ -0,0 +1,17 @@
+import { createNextDescribe } from 'e2e-utils'
+
+createNextDescribe(
+  'app-dir parallel-routes-static',
+  {
+    files: __dirname,
+  },
+  ({ next }) => {
+    it('should static generate parallel routes', async () => {
+      expect(await next.hasFile('.next/server/app/nested/foo.html')).toBe(true)
+      expect(await next.hasFile('.next/server/app/nested/foo.meta')).toBe(true)
+
+      expect(await next.hasFile('.next/server/app/nested/bar.html')).toBe(true)
+      expect(await next.hasFile('.next/server/app/nested/bar.meta')).toBe(true)
+    })
+  }
+)
