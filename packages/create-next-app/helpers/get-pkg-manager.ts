61f36c5b22814cc1b018c5e8e8fcc6b140d23dd9
devjiwonchoi <devjiwonchoi@gmail.com>
2024-03-29 03:26:50 +0900

    refactor: return 'npm' once
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/create-next-app/helpers/get-pkg-manager.ts
+++ packages/create-next-app/helpers/get-pkg-manager.ts
@@ -1,8 +1,7 @@
 import type { PackageManager } from '../types'
 
 export function getPkgManager(): PackageManager {
-  const userAgent = process.env.npm_config_user_agent
-  if (!userAgent) return 'npm'
+  const userAgent = process.env.npm_config_user_agent ?? ''
   if (userAgent.startsWith('pnpm')) return 'pnpm'
   if (userAgent.startsWith('yarn')) return 'yarn'
   if (userAgent.startsWith('bun')) return 'bun'
