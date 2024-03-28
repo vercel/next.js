4ff7b7abeafd03e82378a15b78a5daa9378c78c4
Will Binns-Smith <wbinnssmith@gmail.com>
2024-03-28 11:23:46 -0700

    fixup! fix encoding for filenames containing `?` or `#`

--- packages/next-swc/crates/next-core/src/next_manifests/utils.rs
+++
@@ -1,9 +0,0 @@
-use std::borrow::Cow;
-
-pub fn encode_url(file_path: &str) -> String {
-    file_path
-        .split('/')
-        .map(|s| urlencoding::encode(s))
-        .intersperse(Cow::Borrowed("/"))
-        .collect()
-}
