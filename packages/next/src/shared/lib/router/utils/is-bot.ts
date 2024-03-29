ca06702749c6557754f91ab6de8dc56319713680
Maxime <maxime.vast@ambler.fr>
2024-03-29 10:19:10 +0100

    Add 'Google-InspectionTool' to bot UA list

--- packages/next/src/shared/lib/router/utils/is-bot.ts
+++ packages/next/src/shared/lib/router/utils/is-bot.ts
@@ -1,5 +1,5 @@
 export function isBot(userAgent: string): boolean {
-  return /Googlebot|Mediapartners-Google|AdsBot-Google|googleweblight|Storebot-Google|Google-PageRenderer|Bingbot|BingPreview|Slurp|DuckDuckBot|baiduspider|yandex|sogou|LinkedInBot|bitlybot|tumblr|vkShare|quora link preview|facebookexternalhit|facebookcatalog|Twitterbot|applebot|redditbot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|ia_archiver/i.test(
+  return /Googlebot|Mediapartners-Google|AdsBot-Google|googleweblight|Storebot-Google|Google-PageRenderer|Google-InspectionTool|GoogleAssociationService|Bingbot|BingPreview|Slurp|DuckDuckBot|baiduspider|yandex|sogou|LinkedInBot|bitlybot|tumblr|vkShare|quora link preview|facebookexternalhit|facebookcatalog|Twitterbot|applebot|redditbot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|ia_archiver/i.test(
     userAgent
   )
 }
