d15ea5cd0cfff638923202d7c6c59d196a082724
Will Binns-Smith <wbinnssmith@gmail.com>
2024-04-02 09:58:45 -0700

    fixup! fix encoding for filenames containing `?` or `#`

--- test/production/deployment-id-handling/deployment-id-handling.test.ts
+++ test/production/deployment-id-handling/deployment-id-handling.test.ts
@@ -62,6 +62,7 @@ describe.each(['NEXT_DEPLOYMENT_ID', 'CUSTOM_DEPLOYMENT_ID'])(
         )
 
         try {
+          console.log('!! requests', urlPath, requests)
           expect(
             requests.every((item) => item.includes('dpl=' + deploymentId))
           ).toBe(true)
