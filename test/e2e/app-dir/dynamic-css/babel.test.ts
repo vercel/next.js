a4cdeef21285e5cc481c7e14890708ae9ccde2a4
Jiachi Liu <inbox@huozhi.im>
2024-04-03 16:27:27 +0200

    skip test

---
+++ test/e2e/app-dir/dynamic-css/babel.test.ts
@@ -0,0 +1,2 @@
+process.env.TEST_NEXT_BABEL = '1'
+require('./index.test')
--- test/e2e/app-dir/dynamic-css/index.test.ts
+++ test/e2e/app-dir/dynamic-css/index.test.ts
@@ -1,12 +1,32 @@
-import { createNextDescribe } from 'e2e-utils'
+import { FileRef, createNextDescribe } from 'e2e-utils'
+import path from 'path'
 
 createNextDescribe(
   'app dir - dynamic css',
   {
-    files: __dirname,
+    files: {
+      app: new FileRef(path.join(__dirname, 'app')),
+      components: new FileRef(path.join(__dirname, 'components')),
+      ...(!process.env.TURBOPACK && process.env.TEST_NEXT_BABEL === '1'
+        ? {
+            '.babelrc': `
+          {
+            "presets": ["next/babel"]
+          }
+        `,
+          }
+        : {}),
+    },
     skipDeployment: true,
   },
   ({ next }) => {
+    if (!process.env.TURBOPACK && process.env.TEST_NEXT_BABEL === '1') {
+      it('should build successfully with babel preset', async () => {
+        const output = next.cliOutput
+        expect(output).toContain('Using external babel configuration')
+      })
+    }
+
     it('should preload css of dynamic component during SSR', async () => {
       const $ = await next.render$('/ssr')
       const cssLinks = $('link[rel="preload stylesheet"]')
