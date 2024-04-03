ca9cd216fd55259e0053b29b45ccff51fe1d1b17
Jiachi Liu <inbox@huozhi.im>
2024-04-03 16:24:21 +0200

    update swc snapshot

--- test/e2e/app-dir/dynamic-css/babel.test.ts
+++
@@ -1,2 +0,0 @@
-process.env.TEST_NEXT_BABEL = '1'
-require('./index.test')
--- test/e2e/app-dir/dynamic-css/index.test.ts
+++ test/e2e/app-dir/dynamic-css/index.test.ts
@@ -1,32 +1,12 @@
-import { FileRef, createNextDescribe } from 'e2e-utils'
-import path from 'path'
+import { createNextDescribe } from 'e2e-utils'
 
 createNextDescribe(
   'app dir - dynamic css',
   {
-    files: {
-      app: new FileRef(path.join(__dirname, 'app')),
-      components: new FileRef(path.join(__dirname, 'components')),
-      ...(process.env.TEST_NEXT_BABEL === '1'
-        ? {
-            '.babelrc': `
-          {
-            "presets": ["next/babel"]
-          }
-        `,
-          }
-        : {}),
-    },
+    files: __dirname,
     skipDeployment: true,
   },
   ({ next }) => {
-    if (process.env.TEST_NEXT_BABEL === '1') {
-      it('should build successfully with babel preset', async () => {
-        const output = next.cliOutput
-        expect(output).toContain('Using external babel configuration')
-      })
-    }
-
     it('should preload css of dynamic component during SSR', async () => {
       const $ = await next.render$('/ssr')
       const cssLinks = $('link[rel="preload stylesheet"]')
