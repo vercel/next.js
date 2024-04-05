7891fc83e26fd761041ddb488582996e7c09ca71
eps1lon <sebastian.silbermann@vercel.com>
2024-04-05 14:33:06 +0200

    Fix TS2309

--- packages/next/types/compiled.d.ts
+++ packages/next/types/compiled.d.ts
@@ -2,15 +2,6 @@
 // it is not meant to be used with local type checking and is ignored in our
 // local tsconfig.json
 
-declare module 'next/dist/compiled/superstruct' {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  export type Struct<T, S> = any
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  export type Infer<T = any> = any
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  export type Describe<T> = any
-}
-
 declare module 'react-server-dom-webpack/server.edge'
 
 declare module 'VAR_MODULE_GLOBAL_ERROR'
