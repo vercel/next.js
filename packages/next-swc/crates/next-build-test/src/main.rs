9c35371526af111d8b8fa1883b6d599a6dceaa01
Alexander Lyon <arlyon@me.com>
2024-04-05 13:55:49 +0100

    add next-build-test binary / library

---
+++ packages/next-swc/crates/next-build-test/src/main.rs
@@ -0,0 +1,50 @@
+use std::str::FromStr;
+
+use next_build_test::{main_inner, Strategy};
+use turbo_tasks::TurboTasks;
+use turbo_tasks_malloc::TurboMalloc;
+use turbopack_binding::turbo::tasks_memory::MemoryBackend;
+
+// #[global_allocator]
+// static ALLOC: turbo_tasks_malloc::TurboMalloc =
+// turbo_tasks_malloc::TurboMalloc;
+
+fn main() {
+    tracing_subscriber::fmt::init();
+
+    let mut factor = std::env::args()
+        .nth(2)
+        .map(|s| s.parse().unwrap())
+        .unwrap_or(num_cpus::get());
+    let strat = std::env::args()
+        .nth(1)
+        .map(|s| Strategy::from_str(&s))
+        .transpose()
+        .unwrap()
+        .unwrap_or(Strategy::Sequential);
+
+    let limit = std::env::args()
+        .nth(3)
+        .map(|s| s.parse().unwrap())
+        .unwrap_or(1);
+
+    if strat == Strategy::Sequential {
+        factor = 1;
+    }
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .on_thread_stop(|| {
+            TurboMalloc::thread_stop();
+            tracing::debug!("threads stopped");
+        })
+        .build()
+        .unwrap()
+        .block_on(async {
+            let tt = TurboTasks::new(MemoryBackend::new(usize::MAX));
+            let x = tt.run_once(main_inner(strat, factor, limit)).await;
+            tracing::debug!("done");
+            x
+        })
+        .unwrap();
+}
