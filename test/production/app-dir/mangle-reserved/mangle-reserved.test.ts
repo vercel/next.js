9638b9d46456a98b48fba3fdb01e4ef4cc197b56
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-04-09 10:40:23 +0900

    chore: Fix CI (#64233)
    ### What?
    
    Fix CI, which is broken due to https://github.com/vercel/next.js/pull/58534
    
    ### Why?
    
    The PR was out of sync and CI didn't check turbo build
    
    ### How?
    
    
    Closes PACK-2920

--- test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
+++ test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
@@ -5,9 +5,10 @@ createNextDescribe(
   {
     files: __dirname,
   },
-  ({ next }) => {
-    it('should work using cheerio', async () => {
+  ({ next, isTurbopack }) => {
+    ;(isTurbopack ? it.skip : it)('should work using cheerio', async () => {
       const $ = await next.render$('/')
+      // eslint-disable-next-line jest/no-standalone-expect
       expect($('p').text()).toBe('AbortSignal')
     })
   }
