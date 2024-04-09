e6f53f007d2658f440f86b2a1c775815a688d28b
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-09 13:05:44 +0900

    it

--- test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
+++ test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
@@ -7,10 +7,9 @@ import { createNextDescribe } from 'e2e-utils'
       {
         files: __dirname,
       },
-      ({ next, isTurbopack }) => {
-        ;(isTurbopack ? it.skip : it)('should work using cheerio', async () => {
+      ({ next }) => {
+        it('should work using cheerio', async () => {
           const $ = await next.render$('/')
-          // eslint-disable-next-line jest/no-standalone-expect
           expect($('p').text()).toBe('AbortSignal')
         })
       }
