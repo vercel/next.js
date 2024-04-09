054a2026d983c912f3ef74833fcf0b9a17a121d0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-09 10:23:22 +0900

    ignore in another way

--- test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
+++ test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
@@ -6,9 +6,11 @@ createNextDescribe(
     files: __dirname,
   },
   ({ next, isTurbopack }) => {
-    ;(isTurbopack ? it.skip : it)('should work using cheerio', async () => {
-      const $ = await next.render$('/')
-      expect($('p').text()).toBe('AbortSignal')
+    it('should work using cheerio', async () => {
+      if (!isTurbopack) {
+        const $ = await next.render$('/')
+        expect($('p').text()).toBe('AbortSignal')
+      }
     })
   }
 )
