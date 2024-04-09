36c80d9b993a463eaa30b85b4db1ee8c9694d3e8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-09 15:02:30 +0900

    Enable

--- test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
+++ test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
@@ -1,18 +1,14 @@
 import { createNextDescribe } from 'e2e-utils'
-;(process.env.TURBOPACK ? describe.skip : describe)(
-  'Skipped in Turbopack',
-  () => {
-    createNextDescribe(
-      'mangle-reserved',
-      {
-        files: __dirname,
-      },
-      ({ next }) => {
-        it('should work using cheerio', async () => {
-          const $ = await next.render$('/')
-          expect($('p').text()).toBe('AbortSignal')
-        })
-      }
-    )
+
+createNextDescribe(
+  'mangle-reserved',
+  {
+    files: __dirname,
+  },
+  ({ next }) => {
+    it('should preserve the name', async () => {
+      const $ = await next.render$('/')
+      expect($('p').text()).toBe('AbortSignal')
+    })
   }
 )
