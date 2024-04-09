49f44114f57d30188e9324e37954ee4474b6785a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-09 13:04:36 +0900

    fix ci

--- test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
+++ test/production/app-dir/mangle-reserved/mangle-reserved.test.ts
@@ -1,15 +1,19 @@
 import { createNextDescribe } from 'e2e-utils'
-
-createNextDescribe(
-  'mangle-reserved',
-  {
-    files: __dirname,
-  },
-  ({ next, isTurbopack }) => {
-    ;(isTurbopack ? it.skip : it)('should work using cheerio', async () => {
-      const $ = await next.render$('/')
-      // eslint-disable-next-line jest/no-standalone-expect
-      expect($('p').text()).toBe('AbortSignal')
-    })
+;(process.env.TURBOPACK ? describe.skip : describe)(
+  'Skipped in Turbopack',
+  () => {
+    createNextDescribe(
+      'mangle-reserved',
+      {
+        files: __dirname,
+      },
+      ({ next, isTurbopack }) => {
+        ;(isTurbopack ? it.skip : it)('should work using cheerio', async () => {
+          const $ = await next.render$('/')
+          // eslint-disable-next-line jest/no-standalone-expect
+          expect($('p').text()).toBe('AbortSignal')
+        })
+      }
+    )
   }
 )
