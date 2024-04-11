0f0549191dbd5df7fa6bd275555d9d173c70828a
Luca Forstner <luca.forstner@sentry.io>
2024-04-11 12:48:48 +0200

    Update tests to cover a few cases

--- test/e2e/app-dir/adsf/app/page.tsx
+++ test/e2e/app-dir/adsf/app/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'
-
 export default function Page() {
   return <p>hello world</p>
 }
--- test/e2e/client-trace-metadata/instrumentation.ts
+++
@@ -1,5 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    require('./instrumentation-node').register()
-  }
-}
