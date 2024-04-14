708ec7f73e393bb5070b9ee5503259f718e901d2
Jiachi Liu <inbox@huozhi.im>
2024-04-14 21:16:24 +0200

    enable turbopack test

--- test/production/app-dir/barrel-optimization/basic/index.test.ts
+++ test/production/app-dir/barrel-optimization/basic/index.test.ts
@@ -1,21 +1,17 @@
 import { createNextDescribe } from 'e2e-utils'
 
-// Skipped in Turbopack, will be added later.
-;(process.env.TURBOPACK ? describe.skip : describe)(
-  'Skipped in Turbopack',
-  () => {
-    createNextDescribe(
-      'app-dir - optimizePackageImports - basic',
-      {
-        files: __dirname,
-      },
-      ({ next }) => {
-        it('should build successfully', async () => {
-          // Ensure that MUI is working
-          const $ = await next.render$('/')
-          expect(await $('#client-mod').text()).toContain('client:default')
-        })
-      }
-    )
-  }
-)
+describe('Skipped in Turbopack', () => {
+  createNextDescribe(
+    'app-dir - optimizePackageImports - basic',
+    {
+      files: __dirname,
+    },
+    ({ next }) => {
+      it('should build successfully', async () => {
+        // Ensure that MUI is working
+        const $ = await next.render$('/')
+        expect(await $('#client-mod').text()).toContain('client:default')
+      })
+    }
+  )
+})
--- test/production/app-dir/barrel-optimization/mui/index.test.ts
+++ test/production/app-dir/barrel-optimization/mui/index.test.ts
@@ -1,27 +1,23 @@
 import { createNextDescribe } from 'e2e-utils'
 
-// Skipped in Turbopack, will be added later.
-;(process.env.TURBOPACK ? describe.skip : describe)(
-  'Skipped in Turbopack',
-  () => {
-    createNextDescribe(
-      'app-dir - optimizePackageImports - mui',
-      {
-        files: __dirname,
+describe('Skipped in Turbopack', () => {
+  createNextDescribe(
+    'app-dir - optimizePackageImports - mui',
+    {
+      files: __dirname,
 
-        dependencies: {
-          '@mui/material': '5.15.15',
-          '@emotion/react': '11.11.1',
-          '@emotion/styled': '11.11.0',
-        },
+      dependencies: {
+        '@mui/material': '5.15.15',
+        '@emotion/react': '11.11.1',
+        '@emotion/styled': '11.11.0',
       },
-      ({ next }) => {
-        it('should build successfully', async () => {
-          // Ensure that MUI is working
-          const $ = await next.render$('/')
-          expect(await $('#typography').text()).toContain('typography')
-        })
-      }
-    )
-  }
-)
+    },
+    ({ next }) => {
+      it('should build successfully', async () => {
+        // Ensure that MUI is working
+        const $ = await next.render$('/')
+        expect(await $('#typography').text()).toContain('typography')
+      })
+    }
+  )
+})
