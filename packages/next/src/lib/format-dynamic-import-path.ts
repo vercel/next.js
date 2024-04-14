fa7b1a13abb8f7d27d1d3e2562dc9fe1766edd60
Vordgi <sasha2822222@gmail.com>
2024-04-14 23:27:08 +0400

    supports different path types as cacheHandler path

--- packages/next/src/lib/format-dynamic-import-path.ts
+++ packages/next/src/lib/format-dynamic-import-path.ts
@@ -1,8 +1,21 @@
 import path from 'path'
-import { pathToFileURL } from 'url'
+
+function parseFileUrl(path: string) {
+  try {
+    const fileUrl = new URL(path)
+
+    if (fileUrl.protocol === 'file:') {
+      return fileUrl
+    }
+  } catch {
+    //
+  }
+
+  return false
+}
 
 /**
- * The path for a dynamic route must be URLs with a valid scheme.
+ * The path for a dynamic import must be URLs with a valid file scheme.
  *
  * When an absolute Windows path is passed to it, it interprets the beginning of the path as a protocol (`C:`).
  * Therefore, it is important to always construct a complete path.
@@ -10,10 +23,21 @@ import { pathToFileURL } from 'url'
  * @param filePath Absolute or relative path
  */
 export const formatDynamicImportPath = (dir: string, filePath: string) => {
+  const fileUrl = parseFileUrl(filePath)
+
+  if (fileUrl && path.isAbsolute(fileUrl.pathname)) {
+    return fileUrl.href
+  }
+
   const absoluteFilePath = path.isAbsolute(filePath)
     ? filePath
     : path.join(dir, filePath)
-  const formattedFilePath = pathToFileURL(absoluteFilePath).toString()
+  const normalizedAbsolutePath = path
+    .normalize(absoluteFilePath)
+    .replaceAll(path.sep, '/')
+  const fullFileUrlPath = normalizedAbsolutePath.startsWith('/')
+    ? `file://${normalizedAbsolutePath}`
+    : `file:///${normalizedAbsolutePath}`
 
-  return formattedFilePath
+  return fullFileUrlPath
 }
