0e8fffe67e3d7f5e571acae027c1f7425830dfeb
Jiachi Liu <inbox@huozhi.im>
2024-04-14 20:00:50 +0200

    add specific test case

--- test/production/app-dir/barrel-optimization/basic/index.test.ts
+++ test/production/app-dir/barrel-optimization/basic/index.test.ts
@@ -5,21 +5,15 @@ import { createNextDescribe } from 'e2e-utils'
   'Skipped in Turbopack',
   () => {
     createNextDescribe(
-      'app-dir - optimizePackageImports - mui',
+      'app-dir - optimizePackageImports - basic',
       {
         files: __dirname,
-
-        dependencies: {
-          '@mui/material': '5.15.15',
-          '@emotion/react': '11.11.1',
-          '@emotion/styled': '11.11.0',
-        },
       },
       ({ next }) => {
         it('should build successfully', async () => {
           // Ensure that MUI is working
           const $ = await next.render$('/')
-          expect(await $('#typography').text()).toContain('typography')
+          expect(await $('#client-mod').text()).toContain('client:default')
         })
       }
     )
