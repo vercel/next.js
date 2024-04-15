f2a73f94e0dccaf7b08981030ca4bf4dd60d4bb5
Will Binns-Smith <wbinnssmith@gmail.com>
2024-04-15 12:19:53 -0700

    fixup! Turbopack: Allow client components to be imported in app routes

--- test/e2e/app-dir/app-routes-client-component/app-routes-client-component.test.ts
+++ test/e2e/app-dir/app-routes-client-component/app-routes-client-component.test.ts
@@ -12,6 +12,7 @@ describe('referencing a client component in an app route', () => {
 
   it('responds without error', async () => {
     expect(JSON.parse(await next.render('/runtime'))).toEqual({
+      // Turbopack's proxy components are functions
       clientComponent: process.env.TURBOPACK ? 'function' : 'object',
     })
   })
