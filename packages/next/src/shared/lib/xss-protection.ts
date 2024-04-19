b84a128438972bb8248284fbcb3bac895ed182a2
eps1lon <sebastian.silbermann@vercel.com>
2024-04-19 16:34:20 +0200

    Don't need to handle special chars
    See https://yp39gm.csb.app/

--- packages/next/src/shared/lib/xss-protection.ts
+++ packages/next/src/shared/lib/xss-protection.ts
@@ -5,7 +5,6 @@ export interface TrustedHref {
   }
 }
 
-// TODO: Should this handle special chars?
 const javaScriptProtocol =
   // eslint-disable-next-line no-useless-escape, no-control-regex
   /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i
