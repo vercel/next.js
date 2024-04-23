8adbcc8fd1d6ad42ce15afc5c9f3b7c385c0508d
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-04-23 09:59:22 +0900

    feat(turbopack): Handle fragment in requests (vercel/turbo#7935)
    ### Description
    
    This reverts commit 0d1ba8acac572b9510c65d0f3458d5c02bf59fa4.
    
    ### Testing Instructions
    
    https://github.com/vercel/next.js/pull/64232

--- crates/turbopack-ecmascript/src/typescript/mod.rs
+++ crates/turbopack-ecmascript/src/typescript/mod.rs
@@ -145,6 +145,7 @@ impl Module for TsConfigModuleAsset {
                         name,
                         Value::new("".to_string().into()),
                         Vc::<String>::default(),
+                        Vc::<String>::default(),
                     ),
                 )));
             }
