2042718ef9beff9128652a807358ce807d835ed2
Janka Uryga <lolzatu2@gmail.com>
2024-04-23 13:22:24 +0200

    test: add basic tests for after()

--- test/e2e/app-dir/next-after/utils/log.js
+++ test/e2e/app-dir/next-after/utils/log.js
@@ -1,6 +1,28 @@
 import * as fs from 'fs'
+import * as path from 'path'
 
-export function persistentLog(/** @type {Record<string, any>} */ data) {
+const LOG_FILE =
+  process.env.PERSISTENT_LOG_FILE ??
+  path.resolve(process.cwd(), 'after-output.jsonl')
+
+export function persistentLog(
+  /** @type {Record<string, any>} */ data,
+  file = LOG_FILE
+) {
   console.log(data)
-  fs.appendFileSync('after-output.jsonl', JSON.stringify(data) + '\n')
+  fs.appendFileSync(file, JSON.stringify(data) + '\n')
+}
+
+export function clearPersistentLog(file = LOG_FILE) {
+  if (fs.existsSync(file)) {
+    fs.rmSync(file)
+  }
+}
+
+export function readPersistentLog(file = LOG_FILE) {
+  const contents = fs.readFileSync(file, 'utf-8')
+  return contents
+    .split('\n')
+    .slice(0, -1)
+    .map((line) => JSON.parse(line))
 }
