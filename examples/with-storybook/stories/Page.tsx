704fd7010f67bbd7f62c876a54074e9a4cc52ccb
zahra shahrouzi <z.shahrouzi@gmail.com>
2024-04-23 14:14:31 +0330

    upgraded storybook to version 8

--- examples/with-storybook/stories/Page.tsx
+++ examples/with-storybook/stories/Page.tsx
@@ -19,7 +19,7 @@ export const Page: React.FC = () => {
         onCreateAccount={() => setUser({ name: "Jane Doe" })}
       />
 
-      <section>
+      <section className="storybook-page">
         <h2>Pages in Storybook</h2>
         <p>
           We recommend building UIs with a{" "}
@@ -33,22 +33,22 @@ export const Page: React.FC = () => {
           process starting with atomic components and ending with pages.
         </p>
         <p>
-          Render pages with mock data. This makes it easy to build and review
-          page states without needing to navigate to them in your app. Here are
-          some handy patterns for managing page data in Storybook:
+          Render pages with mock data. This makes it easy to build and review page states without
+          needing to navigate to them in your app. Here are some handy patterns for managing page
+          data in Storybook:
         </p>
         <ul>
           <li>
-            Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            Use a higher-level connected component. Storybook helps you compose such data from the
+            "args" of child component stories
           </li>
           <li>
-            Assemble data in the page component from your services. You can mock
-            these services out using Storybook.
+            Assemble data in the page component from your services. You can mock these services out
+            using Storybook.
           </li>
         </ul>
         <p>
-          Get a guided tutorial on component-driven development at{" "}
+          Get a guided tutorial on component-driven development at{' '}
           <a
             href="https://storybook.js.org/tutorials/"
             target="_blank"
@@ -67,8 +67,7 @@ export const Page: React.FC = () => {
           .
         </p>
         <div className="tip-wrapper">
-          <span className="tip">Tip</span> Adjust the width of the canvas with
-          the{" "}
+          <span className="tip">Tip</span> Adjust the width of the canvas with the{' '}
           <svg
             width="10"
             height="10"
