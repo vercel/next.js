51ab986597ade9097fbf16ada6bc8002d6ee16ed
Wyatt Johnson <accounts+github@wyattjoh.ca>
2024-04-24 15:07:13 -0600

    feat: remove regexp for explicit argument parsing

--- packages/next/src/server/lib/utils.ts
+++ packages/next/src/server/lib/utils.ts
@@ -1,3 +1,4 @@
+import { parseArgs } from 'node:util'
 import { InvalidArgumentError } from 'next/dist/compiled/commander'
 
 export function printAndExit(message: string, code = 1) {
@@ -10,22 +11,134 @@ export function printAndExit(message: string, code = 1) {
   return process.exit(code)
 }
 
-export const getDebugPort = () => {
-  const debugPortStr =
-    process.execArgv
-      .find(
-        (localArg) =>
-          localArg.startsWith('--inspect') ||
-          localArg.startsWith('--inspect-brk')
-      )
-      ?.split('=', 2)[1] ??
-    process.env.NODE_OPTIONS?.match?.(/--inspect(-brk)?(=(\S+))?( |$)/)?.[3]
-  return debugPortStr ? parseInt(debugPortStr, 10) : 9229
+/**
+ * Get the node options from the environment variable `NODE_OPTIONS` and returns
+ * them as an array of strings.
+ *
+ * @returns An array of strings with the node options.
+ */
+const getNodeOptionsArgs = () =>
+  process.env.NODE_OPTIONS?.split(' ').map((arg) => arg.trim()) ?? []
+
+/**
+ * The debug address is in the form of `[host:]port`. The host is optional.
+ */
+type DebugAddress = {
+  host: string | undefined
+  port: number
 }
 
-const NODE_INSPECT_RE = /--inspect(-brk)?(=\S+)?( |$)/
-export function getNodeOptionsWithoutInspect() {
-  return (process.env.NODE_OPTIONS || '').replace(NODE_INSPECT_RE, '')
+/**
+ * Formats the debug address into a string.
+ */
+export const formatDebugAddress = ({ host, port }: DebugAddress): string => {
+  if (host) return `${host}:${port}`
+  return `${port}`
+}
+
+/**
+ * Get's the debug address from the `NODE_OPTIONS` environment variable. If the
+ * address is not found, it returns the default host (`undefined`) and port
+ * (`9229`).
+ *
+ * @returns An object with the host and port of the debug address.
+ */
+export const getParsedDebugAddress = (): DebugAddress => {
+  const args = getNodeOptionsArgs()
+  if (args.length === 0) return { host: undefined, port: 9229 }
+
+  const { values } = parseArgs({ args, strict: false })
+
+  // We expect to find the debug port in one of these options. The first one
+  // found will be used.
+  const address =
+    values.inspect ??
+    values['inspect-brk'] ??
+    values['inspect_brk'] ??
+    values['inspect-port'] ??
+    values['inspect_port']
+
+  if (!address || typeof address !== 'string') {
+    return { host: undefined, port: 9229 }
+  }
+
+  // The address is in the form of `[host:]port`. Let's parse the address.
+  if (address.includes(':')) {
+    const [host, port] = address.split(':')
+    return { host, port: parseInt(port, 10) }
+  }
+
+  return { host: undefined, port: parseInt(address, 10) }
+}
+
+/**
+ * Get the debug address from the `NODE_OPTIONS` environment variable and format
+ * it into a string.
+ *
+ * @returns A string with the formatted debug address.
+ */
+export const getFormattedDebugAddress = () =>
+  formatDebugAddress(getParsedDebugAddress())
+
+/**
+ * Stringify the arguments to be used in a command line. It will ignore any
+ * argument that has a value of `undefined`.
+ *
+ * @param args The arguments to be stringified.
+ * @returns A string with the arguments.
+ */
+export function formatNodeOptions(
+  args: Record<string, string | boolean | undefined>
+): string {
+  return Object.entries(args)
+    .map(([key, value]) => {
+      if (value === true) {
+        return `--${key}`
+      }
+
+      if (value) {
+        return `--${key}=${value}`
+      }
+
+      return null
+    })
+    .filter((arg) => arg !== null)
+    .join(' ')
+}
+
+/**
+ * Get the node options from the `NODE_OPTIONS` environment variable and parse
+ * them into an object without the inspect options.
+ *
+ * @returns An object with the parsed node options.
+ */
+export function getParsedNodeOptionsWithoutInspect() {
+  const args = getNodeOptionsArgs()
+  if (args.length === 0) return {}
+
+  const { values } = parseArgs({ args, strict: false })
+
+  // Remove inspect options.
+  delete values.inspect
+  delete values['inspect-brk']
+  delete values['inspect_brk']
+  delete values['inspect-port']
+  delete values['inspect_port']
+
+  return values
+}
+
+/**
+ * Get the node options from the `NODE_OPTIONS` environment variable and format
+ * them into a string without the inspect options.
+ *
+ * @returns A string with the formatted node options.
+ */
+export function getFormattedNodeOptionsWithoutInspect() {
+  const args = getParsedNodeOptionsWithoutInspect()
+  if (Object.keys(args).length === 0) return ''
+
+  return formatNodeOptions(args)
 }
 
 export function myParseInt(value: string) {
@@ -40,30 +153,34 @@ export function myParseInt(value: string) {
 
 export const RESTART_EXIT_CODE = 77
 
-export function checkNodeDebugType() {
-  let nodeDebugType = undefined
+/**
+ * Get the debug type from the `NODE_OPTIONS` environment variable.
+ */
+export function getNodeDebugType() {
+  const args = [...process.execArgv, ...getNodeOptionsArgs()]
+  if (args.length === 0) return
 
-  if (
-    process.execArgv.some((localArg) => localArg.startsWith('--inspect')) ||
-    process.env.NODE_OPTIONS?.match?.(/--inspect(=\S+)?( |$)/)
-  ) {
-    nodeDebugType = 'inspect'
-  }
+  const { values } = parseArgs({ args, strict: false })
 
-  if (
-    process.execArgv.some((localArg) => localArg.startsWith('--inspect-brk')) ||
-    process.env.NODE_OPTIONS?.match?.(/--inspect-brk(=\S+)?( |$)/)
-  ) {
-    nodeDebugType = 'inspect-brk'
-  }
-
-  return nodeDebugType
+  if (values.inspect) return 'inspect'
+  if (values['inspect-brk'] || values['inspect_brk']) return 'inspect-brk'
+  if (values['inspect-port'] || values['inspect_port']) return 'inspect-port'
 }
 
+/**
+ * Get the `max-old-space-size` value from the `NODE_OPTIONS` environment
+ * variable.
+ *
+ * @returns The value of the `max-old-space-size` option as a number.
+ */
 export function getMaxOldSpaceSize() {
-  const maxOldSpaceSize = process.env.NODE_OPTIONS?.match(
-    /--max[-_]old[-_]space[-_]size=(\d+)/
-  )?.[1]
+  const args = getNodeOptionsArgs()
+  if (args.length === 0) return
 
-  return maxOldSpaceSize ? parseInt(maxOldSpaceSize, 10) : undefined
+  const { values } = parseArgs({ args, strict: false })
+
+  const size = values['max-old-space-size'] || values['max_old_space_size']
+  if (!size || typeof size !== 'string') return
+
+  return parseInt(size, 10)
 }
