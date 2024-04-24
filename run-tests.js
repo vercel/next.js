cf8a71222ad0915634169d45bb2491e5bc9a5abf
JJ Kasper <jj@jjsweb.site>
2024-04-23 18:15:58 -0700

    special case windows

--- run-tests.js
+++ run-tests.js
@@ -529,7 +529,8 @@ ${ENDGROUP}`)
           ...process.env,
           ...env,
         },
-        shell: true,
+        // See: https://nodejs.org/en/blog/vulnerability/april-2024-security-releases-2
+        shell: process.platform === 'win32',
       })
       child.stdout.on('data', stdout)
       child.stderr.on('data', handleOutput('stderr'))
