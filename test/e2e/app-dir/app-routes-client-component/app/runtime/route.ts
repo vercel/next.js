33c6b42c538344719e9f38800b4c3d5425bc2456
Will Binns-Smith <wbinnssmith@gmail.com>
2024-04-25 14:59:06 -0700

    fixup! Turbopack: Allow client components from foreign code in app routes

--- test/e2e/app-dir/app-routes-client-component/app/runtime/route.ts
+++ test/e2e/app-dir/app-routes-client-component/app/runtime/route.ts
@@ -3,7 +3,6 @@ import { ClientComponent } from '../../ClientComponent'
 import { MyModuleClientComponent } from 'my-module/MyModuleClientComponent'
 
 export function GET() {
-  console.log('!! test GET')
   return NextResponse.json({
     clientComponent: typeof ClientComponent,
     myModuleClientComponent: typeof MyModuleClientComponent,
