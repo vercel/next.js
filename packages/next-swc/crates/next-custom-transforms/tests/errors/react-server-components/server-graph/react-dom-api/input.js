e115ee5294e52f145072bae2564d2a904f30acb0
eps1lon <sebastian.silbermann@vercel.com>
2024-04-25 21:38:54 +0200

    useFormState -> useActionState
    Kept the transform support since useFormState still exists.

--- packages/next-swc/crates/next-custom-transforms/tests/errors/react-server-components/server-graph/react-dom-api/input.js
+++ packages/next-swc/crates/next-custom-transforms/tests/errors/react-server-components/server-graph/react-dom-api/input.js
@@ -1,5 +1,7 @@
 import { flushSync, unstable_batchedUpdates } from 'react-dom'
 
+import { useActionState } from 'react'
+
 import { useFormStatus, useFormState } from 'react-dom'
 
 export default function () {
