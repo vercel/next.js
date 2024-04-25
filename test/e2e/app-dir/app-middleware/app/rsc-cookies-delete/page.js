d99d229f8d080d13e465502d64cf61c4877b6ccf
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-04-25 09:47:22 -0700

    initialize ALS with cookies in middleware (#65008)
    ### What
    Cookies set/updated/removed in middleware won't be accessible during the
    render in which they were set
    
    ### Why
    Middleware will properly set a `set-cookie` header to inform the client
    of the cookie change, but this means the `AsyncLocalStorage` context
    containing the cookies value wouldn't be updated until the next time the
    request headers were parsed. In other words, on the first request the
    cookie would be sent but wouldn't be available in the `cookies()`
    context. And then the following request would properly have the cookie
    values.
    
    ### How
    This uses a proxy on the `ResponseCookies` used in middleware to add a
    middleware override header with the cookie value. When we instantiate
    the cached cookies, we merge in whatever headers would have been set by
    middleware, so that they're available in the same render that invoked
    middleware.
    
    ### Test Plan
    This changeset adds a test to confirm cookies set/deleted in middleware
    are available in a single pass. Verified with a deployment
    [here](https://vtest314-e2e-tests-ldx7olfl1-ztanner.vercel.app/rsc-cookies).
    
    Fixes #49442
    Closes NEXT-1126

---
+++ test/e2e/app-dir/app-middleware/app/rsc-cookies-delete/page.js
@@ -0,0 +1,13 @@
+import { cookies } from 'next/headers'
+
+export default function Page() {
+  const rscCookie1 = cookies().get('rsc-cookie-value-1')?.value
+  const rscCookie2 = cookies().get('rsc-cookie-value-2')?.value
+
+  return (
+    <div>
+      <p id="rsc-cookie-1">Cookie 1: {rscCookie1}</p>
+      <p id="rsc-cookie-2">Cookie 2: {rscCookie2}</p>
+    </div>
+  )
+}
