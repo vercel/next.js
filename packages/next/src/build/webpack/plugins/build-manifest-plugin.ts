42c8b10b7a4d8366cf50d5d4287df5cdb20734d4
Jiachi Liu <inbox@huozhi.im>
2024-05-03 17:36:13 +0200

    fix edge build manifest

--- packages/next/src/build/webpack/plugins/build-manifest-plugin.ts
+++ packages/next/src/build/webpack/plugins/build-manifest-plugin.ts
@@ -29,32 +29,36 @@ export type ClientBuildManifest = {
 // generated).
 export const srcEmptySsgManifest = `self.__SSG_MANIFEST=new Set;self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()`
 
-// Return different path for edge runtime and nodejs runtime
-// edge: '"/static/" + process.env.__NEXT_BUILD_ID + "/low-priority.js"'
 // nodejs: '/static/<build id>/low-priority.js'
-function buildLowPriorityPath(
-  filename: string,
-  buildId: string,
-  isEdgeRuntime: boolean
-) {
-  return isEdgeRuntime
-    ? `"${CLIENT_STATIC_FILES_PATH}/" + process.env.__NEXT_BUILD_ID + "/${filename}"`
-    : `${CLIENT_STATIC_FILES_PATH}/${buildId}/${filename}`
+function buildNodejsLowPriorityPath(filename: string, buildId: string) {
+  return `${CLIENT_STATIC_FILES_PATH}/${buildId}/${filename}`
 }
 
-function createEdgeRuntimeManifest(
-  originAssetMap: BuildManifest,
-  buildId: string
-): string {
-  const assetMap = {
+function createEdgeRuntimeManifest(originAssetMap: BuildManifest): string {
+  const manifestFilenames = ['_buildManifest.js', '_ssgManifest.js']
+
+  const assetMap: BuildManifest = {
     ...originAssetMap,
-    lowPriorityFiles: [
-      buildLowPriorityPath('_buildManifest.js', buildId, true),
-      buildLowPriorityPath('_ssgManifest.js', buildId, true),
-    ],
+    lowPriorityFiles: [],
   }
 
-  return JSON.stringify(assetMap, null, 2)
+  const manifestDefCode = `self.__BUILD_MANIFEST = ${JSON.stringify(
+    assetMap,
+    null,
+    2
+  )};\n`
+  // edge lowPriorityFiles item: '"/static/" + process.env.__NEXT_BUILD_ID + "/low-priority.js"'.
+  // Since lowPriorityFiles is not fixed and relying on `process.env.__NEXT_BUILD_ID`, we'll produce code creating it dynamically.
+  const lowPriorityFilesCode =
+    `self.__BUILD_MANIFEST.lowPriorityFiles = [\n` +
+    manifestFilenames
+      .map((filename) => {
+        return `"/static/" + process.env.__NEXT_BUILD_ID + "/${filename}",\n`
+      })
+      .join(',') +
+    `\n];`
+
+  return manifestDefCode + lowPriorityFilesCode
 }
 
 function normalizeRewrite(item: {
@@ -259,15 +263,13 @@ export default class BuildManifestPlugin {
         // Add the runtime build manifest file (generated later in this file)
         // as a dependency for the app. If the flag is false, the file won't be
         // downloaded by the client.
-        const buildManifestPath = buildLowPriorityPath(
+        const buildManifestPath = buildNodejsLowPriorityPath(
           '_buildManifest.js',
-          this.buildId,
-          false
+          this.buildId
         )
-        const ssgManifestPath = buildLowPriorityPath(
+        const ssgManifestPath = buildNodejsLowPriorityPath(
           '_ssgManifest.js',
-          this.buildId,
-          false
+          this.buildId
         )
         assetMap.lowPriorityFiles.push(buildManifestPath, ssgManifestPath)
         assets[ssgManifestPath] = new sources.RawSource(srcEmptySsgManifest)
@@ -292,10 +294,7 @@ export default class BuildManifestPlugin {
       )
 
       assets[`server/${MIDDLEWARE_BUILD_MANIFEST}.js`] = new sources.RawSource(
-        `self.__BUILD_MANIFEST=${createEdgeRuntimeManifest(
-          assetMap,
-          this.buildId
-        )}`
+        `${createEdgeRuntimeManifest(assetMap)}`
       )
 
       if (!this.isDevFallback) {
