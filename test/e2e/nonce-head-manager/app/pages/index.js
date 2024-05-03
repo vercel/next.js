330b523fa50284667afb00727871e759813e6429
eps1lon <sebastian.silbermann@vercel.com>
2024-05-03 10:48:56 +0200

    Stop using next/head for scripts
    We warn specifically because it's not compatible with Suspense and streaming.
    Using next/head results in `window.scriptExecutionIds` to resolve to ['src-1.js', 'src-1.js']
    
    For inline scripts we document this: https://nextjs.org/docs/messages/inline-script-id

--- test/e2e/nonce-head-manager/app/pages/index.js
+++ test/e2e/nonce-head-manager/app/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Head from 'next/head'
+import Script from 'next/script'
 
 const Page = () => {
   const [counter, setCounter] = React.useState(0)
@@ -7,9 +7,7 @@ const Page = () => {
 
   return (
     <>
-      <Head>
-        <script nonce="abc123" src={useSrc1 ? '/src-1.js' : '/src-2.js'} />
-      </Head>
+      <Script nonce="abc123" src={useSrc1 ? '/src-1.js' : '/src-2.js'} />
       <h1 id="h1">{'Count ' + counter}</h1>
       <button id="force-rerender" onClick={() => setCounter(counter + 1)}>
         Re-render
