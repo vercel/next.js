21e79fcade360afa62a9067b4b93aeee2413a679
abhishake1 <abhi9848038283@gmail.com>
2024-05-04 11:55:18 +0000

    add comment

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -1845,6 +1845,7 @@ export default abstract class Server<
 
       staticPaths = pathsResult.staticPaths
 
+      // change [fallbackMode] if staticPaths is defined and not empty
       if (!!staticPaths?.length) pathsResult.fallbackMode = 'static'
 
       fallbackMode = pathsResult.fallbackMode
