3736e5fb40e07c59caea4543bb2c3da62d97f49c
Jiachi Liu <inbox@huozhi.im>
2024-05-06 22:17:23 +0200

    add react assertions

--- test/e2e/module-layer/pages/api/hello.js
+++ test/e2e/module-layer/pages/api/hello.js
@@ -1,5 +1,9 @@
 import 'server-only'
+import React from 'react'
 
 export default function handler(req, res) {
+  if (React.useState) {
+    throw new Error('React.useState should not be defined in server layer')
+  }
   return res.send('pages/api/hello.js:')
 }
