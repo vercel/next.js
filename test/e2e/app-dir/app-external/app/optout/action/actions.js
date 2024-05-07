6dfce23ed8859091f8b3c7a702bb0b119786bc7f
Jiachi Liu <inbox@huozhi.im>
2024-05-07 20:21:04 +0200

    rm unused

--- test/e2e/app-dir/app-external/app/optout/action/actions.js
+++ test/e2e/app-dir/app-external/app/optout/action/actions.js
@@ -1,7 +1,6 @@
 'use server'
 
 import { value as dualPkgOptoutValue } from 'dual-pkg-optout'
-import * as mod from 'dual-pkg-optout'
 
 export async function getDualOptoutValue() {
   return dualPkgOptoutValue
