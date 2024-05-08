354f1def425949c79e384f2c88108bc282eacc30
samcx <sam@vercel.com>
2024-05-08 14:30:15 -0700

    chore(create-next-app): add app-empty, default-empty templates

--- packages/create-next-app/templates/types.ts
+++ packages/create-next-app/templates/types.ts
@@ -1,6 +1,12 @@
 import { PackageManager } from "../helpers/get-pkg-manager";
 
-export type TemplateType = "default" | "app" | "default-tw" | "app-tw";
+export type TemplateType =
+  | "app"
+  | "app-empty"
+  | "app-tw"
+  | "default"
+  | "default-empty"
+  | "default-tw";
 export type TemplateMode = "js" | "ts";
 
 export interface GetTemplateFileArgs {
