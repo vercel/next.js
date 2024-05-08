a483c98ca92faf420cb60756f0ad25d07678cc83
samcx <sam@vercel.com>
2024-05-08 15:20:29 -0700

    test(create-next-app): fix create-next-app bun integration test

--- test/integration/create-next-app/package-manager/bun.test.ts
+++ test/integration/create-next-app/package-manager/bun.test.ts
@@ -30,6 +30,7 @@ describe('create-next-app with package manager bun', () => {
           '--no-src-dir',
           '--no-tailwind',
           '--no-import-alias',
+          '--no-empty',
         ],
         {
           cwd,
@@ -58,6 +59,7 @@ it('should use bun when user-agent is bun', async () => {
         '--no-src-dir',
         '--no-tailwind',
         '--no-import-alias',
+        '--no-empty',
       ],
       {
         cwd,
