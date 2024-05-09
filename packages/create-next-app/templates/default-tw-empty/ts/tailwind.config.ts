04517a97dad11cd13ab6d05edcf7b94429c6d57d
Sam Ko <sam@vercel.com>
2024-05-09 08:42:27 -0700

    chore(create-next-app): remove tailwind themes
    Co-authored-by: Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>

--- packages/create-next-app/templates/default-tw-empty/ts/tailwind.config.ts
+++ packages/create-next-app/templates/default-tw-empty/ts/tailwind.config.ts
@@ -6,15 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
