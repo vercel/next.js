8919524b4c6f17646c594bd9789e70d5bd0108db
samcx <sam@vercel.com>
2024-05-10 10:36:17 -0700

    chore(create-next-app): update index page to be equal to app router empty templates

--- packages/create-next-app/templates/default-empty/js/pages/index.js
+++ packages/create-next-app/templates/default-empty/js/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>Hello World!</div>
+        <div>Hello world!</div>
       </main>
     </>
   );
