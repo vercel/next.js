70124aa87f61794af2a919b07b49fbc57fcd167b
Tim Neutkens <tim@timneutkens.nl>
2024-05-10 16:48:10 +0200

    Add Ecmascript features tests

---
+++ test/e2e/app-dir/ecmascript-features/app/page.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { abc } from './export-as'
+import json from './file.json' with { type: 'json' }
+// Test both server and client compilation of ECMAScript features.
+
+class ClassWithPrivate {
+  #privateField
+  #privateFieldWithInitializer = 11
+
+  #privateMethod() {
+    this.#privateField = 10
+  }
+
+  static #privateStaticFieldWithInitializer = 12
+
+  static #privateStaticMethod() {
+    return this.#privateStaticFieldWithInitializer
+  }
+
+  constructor() {
+    this.#privateMethod()
+  }
+
+  getPrivateField() {
+    return this.#privateField
+  }
+  getPrivateFieldWithInitializer() {
+    return this.#privateFieldWithInitializer
+  }
+  getPrivateStaticFieldWithInitializer() {
+    return ClassWithPrivate.#privateStaticFieldWithInitializer
+  }
+  getPrivateStaticMethod() {
+    return ClassWithPrivate.#privateStaticMethod()
+  }
+}
+
+// Not supported in Node.js yet.
+// let regex = /abc/v
+
+export default function Page() {
+  const instance = new ClassWithPrivate()
+
+  return (
+    <>
+      <h1>Ecmascript features test</h1>
+      <pre id="values-to-check">
+        {JSON.stringify(
+          {
+            privateField: instance.getPrivateField(),
+            privateFieldWithInitializer:
+              instance.getPrivateFieldWithInitializer(),
+            privateStaticFieldWithInitializer:
+              instance.getPrivateStaticFieldWithInitializer(),
+            privateStaticMethod: instance.getPrivateStaticMethod(),
+            exportAs: abc,
+            importWith: json.message,
+            // Not supported in Node.js yet.
+            // regex: regex instanceof RegExp
+          },
+          null,
+          2
+        )}
+      </pre>
+    </>
+  )
+}
