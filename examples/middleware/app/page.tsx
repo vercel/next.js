dc3a787796994d9b0c86ad5b7493456d5cbd22c5
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-05-10 22:50:16 +0700

    Update  to App Router

--- examples/middleware/app/page.tsx
+++ examples/middleware/app/page.tsx
@@ -1,27 +1,24 @@
-import type { NextPage } from "next";
 import Link from "next/link";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <div>
       <h1>Index</h1>
       <p>
-        <Link href="/about" passHref>
+        <Link href="/about">
           Go to about page (will redirect)
         </Link>
       </p>
       <p>
-        <Link href="/another" passHref>
+        <Link href="/another">
           Go to another page (will rewrite)
         </Link>
       </p>
       <p>
-        <Link href="/about2" passHref>
+        <Link href="/about2">
           Go to about 2 page (no redirect or rewrite)
         </Link>
       </p>
     </div>
   );
-};
-
-export default Home;
+}
