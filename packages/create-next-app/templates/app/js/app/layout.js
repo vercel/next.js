0557d080cc17740c5fa23cd3ca225bb891b5c189
yasmin p <yasmin.pessoa@vercel.com>
2024-05-14 13:24:56 -0400

    Update App Router vanilla CSS templates

--- packages/create-next-app/templates/app/js/app/layout.js
+++ packages/create-next-app/templates/app/js/app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
