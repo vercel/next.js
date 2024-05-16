a543ecae8e124d0b950f87a74c65f57f4a2d08fd
Jiachi Liu <inbox@huozhi.im>
2024-05-16 16:20:02 +0200

    lint

--- test/e2e/app-dir/navigation/pages/api/navigation.js
+++ test/e2e/app-dir/navigation/pages/api/navigation.js
@@ -2,9 +2,6 @@ import { useParams } from 'next/navigation'
 import { useRouter } from 'next/router'
 
 export default function handle(_, res) {
-  const values = [
-    typeof useRouter,
-    typeof useParams
-  ]
+  const values = [typeof useRouter, typeof useParams]
   res.send(values.join())
 }
