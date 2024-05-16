f461b914143ca7db751a9e47e0e246499f15353b
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-17 03:47:06 +0900

    refactor: root glob * matches all

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '**/*.{js,jsx,mjs,ts,tsx,mts}': [
+  '*.{js,jsx,mjs,ts,tsx,mts}': [
     'prettier --write',
     'eslint --no-ignore --max-warnings=0 --fix',
   ],
-  '**/*.{json,md,mdx,css,html,yml,yaml,scss}': ['prettier --write'],
-  '**/*.rs': ['cargo fmt'],
+  '*.{json,md,mdx,css,html,yml,yaml,scss}': ['prettier --write'],
+  '*.rs': ['cargo fmt'],
 }
