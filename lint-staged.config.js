684749718030d5f59b8ca568db0bd9f01aab9a49
devjiwonchoi <devjiwonchoi@gmail.com>
2024-05-17 03:12:25 +0900

    refactor: remove unnecessarily targeting staged files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,43 +1,8 @@
-const { quote } = require('shell-quote')
-const { ESLint } = require('eslint')
-
-const eslint = new ESLint()
-
-/**
- * Escape filenames to ensure that spaces and such aren't interpreted as
- * separators.
- *
- * @param {string[]} filenames
- * @returns {string[]}
- */
-function escape(filenames) {
-  if (process.platform === 'win32') {
-    return filenames
-  }
-
-  return filenames.map((filename) => quote([filename]).replace(/\\@/g, '@'))
-}
-
 module.exports = {
-  '**/*.{js,jsx,mjs,ts,tsx,mts}': async (filenames) => {
-    const escapedFileNames = escape(filenames).join(' ')
-    const eslintFileNames = await Promise.all(
-      filenames.map(async (filename) => {
-        const ignored = await eslint.isPathIgnored(filename)
-        return ignored ? null : filename
-      })
-    )
-    return [
-      `prettier --write ${escapedFileNames}`,
-      `eslint --no-ignore --max-warnings=0 --fix ${escape(eslintFileNames.filter((filename) => filename !== null)).join(' ')}`,
-    ]
-  },
-  '**/*.{json,md,mdx,css,html,yml,yaml,scss}': (filenames) => {
-    const escapedFileNames = escape(filenames).join(' ')
-    return [`prettier --write ${escapedFileNames}`]
-  },
-  '**/*.rs': (filenames) => {
-    const escapedFileNames = escape(filenames).join(' ')
-    return [`cargo fmt -- ${escapedFileNames}`]
-  },
+  '**/*.{js,jsx,mjs,ts,tsx,mts}': [
+    'prettier --write',
+    'eslint --no-ignore --max-warnings=0 --fix',
+  ],
+  '**/*.{json,md,mdx,css,html,yml,yaml,scss}': ['prettier --write'],
+  '**/*.rs': ['cargo fmt'],
 }
