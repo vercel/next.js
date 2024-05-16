vercel/next.js

commit 186afe9fb95130b638b8f686cbde18751047c615

Reland Remove ineffective webpack rules and unused app-page context modules (#65694)
Reland #65321 

Added a test to make sure this change will not fail in `pages/api` like
the error mentioned in #65558

packages/next/src/build/webpack-config.ts
@@ -1276,14 +1276,6 @@ export default async function getBaseWebpackConfig(
         ...(hasAppDir
           ? [
               {
-                layer: WEBPACK_LAYERS.appRouteHandler,
-                test: new RegExp(
-                  `private-next-app-dir\\/.*\\/route\\.(${pageExtensions.join(
-                    '|'
-                  )})$`
-                ),
-              },
-              {
                 // Make sure that AsyncLocalStorage module instance is shared between server and client
                 // layers.
                 layer: WEBPACK_LAYERS.shared,
@@ -1708,23 +1700,17 @@ export default async function getBaseWebpackConfig(
               '.shared-runtime'
             )
             const layer = resource.contextInfo.issuerLayer
-
             let runtime
 
-            switch (layer) {
-              case WEBPACK_LAYERS.appRouteHandler:
-                runtime = 'app-route'
-                break
-              case WEBPACK_LAYERS.serverSideRendering:
-              case WEBPACK_LAYERS.reactServerComponents:
-              case WEBPACK_LAYERS.appPagesBrowser:
-              case WEBPACK_LAYERS.actionBrowser:
-                runtime = 'app-page'
-                break
-              default:
-                runtime = 'pages'
+            if (layer === WEBPACK_LAYERS.serverSideRendering) {
+              runtime = 'app-page'
+            } else if (!layer || layer === WEBPACK_LAYERS.api) {
+              runtime = 'pages'
+            } else {
+              throw new Error(
+                `shared-runtime module ${moduleName} cannot be used in ${layer} layer`
+              )
             }
-
             resource.request = `next/dist/server/future/route-modules/${runtime}/vendored/contexts/${moduleName}`
           }
         ),

test/e2e/app-dir/navigation/navigation.test.ts
@@ -909,4 +909,11 @@ describe('app dir - navigation', () => {
       })
     })
   })
+
+  describe('pages api', () => {
+    it('should not error if just import the navigation api in pages/api', async () => {
+      const res = await next.fetch('/api/navigation')
+      expect(res.status).toBe(200)
+    })
+  })
 })

test/e2e/app-dir/navigation/pages/api/navigation.js
@@ -1,0 +1,5 @@
+import { useParams } from 'next/navigation'
+
+export default function handle(_, res) {
+  res.send(`${typeof useParams}`)
+}

packages/next/src/build/webpack/plugins/next-types-plugin/index.ts
@@ -613,11 +613,7 @@ export class NextTypesPlugin {
         }
         return
       }
-      if (
-        mod.layer !== WEBPACK_LAYERS.reactServerComponents &&
-        mod.layer !== WEBPACK_LAYERS.appRouteHandler
-      )
-        return
+      if (mod.layer !== WEBPACK_LAYERS.reactServerComponents) return
 
       const IS_LAYOUT = /[/\\]layout\.[^./\\]+$/.test(mod.resource)
       const IS_PAGE = !IS_LAYOUT && /[/\\]page\.[^.]+$/.test(mod.resource)

packages/next/src/lib/constants.ts
@@ -115,7 +115,8 @@ const WEBPACK_LAYERS_NAMES = {
    */
   shared: 'shared',
   /**
-   * React Server Components layer (rsc).
+   * The layer for server-only runtime and picking up `react-server` export conditions.
+   * Including app router RSC pages and app router custom routes.
    */
   reactServerComponents: 'rsc',
   /**
@@ -150,10 +151,6 @@ const WEBPACK_LAYERS_NAMES = {
    * The server bundle layer for metadata routes.
    */
   appMetadataRoute: 'app-metadata-route',
-  /**
-   * The layer for the server bundle for App Route handlers.
-   */
-  appRouteHandler: 'app-route-handler',
 } as const
 
 export type WebpackLayerName =
@@ -166,7 +163,6 @@ const WEBPACK_LAYERS = {
       WEBPACK_LAYERS_NAMES.reactServerComponents,
       WEBPACK_LAYERS_NAMES.actionBrowser,
       WEBPACK_LAYERS_NAMES.appMetadataRoute,
-      WEBPACK_LAYERS_NAMES.appRouteHandler,
       WEBPACK_LAYERS_NAMES.instrument,
       WEBPACK_LAYERS_NAMES.middleware,
     ],
@@ -182,7 +178,6 @@ const WEBPACK_LAYERS = {
       WEBPACK_LAYERS_NAMES.reactServerComponents,
       WEBPACK_LAYERS_NAMES.actionBrowser,
       WEBPACK_LAYERS_NAMES.appMetadataRoute,
-      WEBPACK_LAYERS_NAMES.appRouteHandler,
       WEBPACK_LAYERS_NAMES.serverSideRendering,
       WEBPACK_LAYERS_NAMES.appPagesBrowser,
       WEBPACK_LAYERS_NAMES.shared,

packages/next/src/server/future/route-modules/app-page/vendored/contexts/entrypoints.ts
@@ -3,8 +3,5 @@ export * as ServerInsertedHtml from '../../../../../../shared/lib/server-inserte
 export * as AppRouterContext from '../../../../../../shared/lib/app-router-context.shared-runtime'
 export * as HooksClientContext from '../../../../../../shared/lib/hooks-client-context.shared-runtime'
 export * as RouterContext from '../../../../../../shared/lib/router-context.shared-runtime'
-export * as HtmlContext from '../../../../../../shared/lib/html-context.shared-runtime'
 export * as AmpContext from '../../../../../../shared/lib/amp-context.shared-runtime'
-export * as LoadableContext from '../../../../../../shared/lib/loadable-context.shared-runtime'
 export * as ImageConfigContext from '../../../../../../shared/lib/image-config-context.shared-runtime'
-export * as Loadable from '../../../../../../shared/lib/loadable.shared-runtime'

packages/next/src/server/future/route-modules/app-page/vendored/contexts/html-context.ts
@@ -1,3 +1,0 @@
-module.exports = require('../../module.compiled').vendored[
-  'contexts'
-].HtmlContext

packages/next/src/server/future/route-modules/app-page/vendored/contexts/loadable-context.ts
@@ -1,3 +1,0 @@
-module.exports = require('../../module.compiled').vendored[
-  'contexts'
-].LoadableContext

commit afe4f2fe13490cf2cc6a14bc4b5364ea75659283

Fix the runtime for rsc layer (#65850)
### What

Fix a bug introduced in #65694 , use app-page runtime for app router
layers

### Why

This is basically reverted the route context picking up logic we had
before.

During the test we found the error thrown
> Module not found: shared-runtime module router-context cannot be used
in rsc layer

Which is caused by a `next/router` imports in rsc page. Decided to
revert to what it was before as the most safe way to load share module
contexts.

It's caused by `next-contentlayer` usage that they're using
`next/router` in server component MDX, but we cannot lint error that
from node_modules. (We actually can, but disabled that due to various
mis-usage of server/client hooks we had before)

packages/next/src/build/webpack-config.ts
@@ -1710,14 +1710,15 @@ export default async function getBaseWebpackConfig(
             const layer = resource.contextInfo.issuerLayer
             let runtime
 
-            if (layer === WEBPACK_LAYERS.serverSideRendering) {
-              runtime = 'app-page'
-            } else if (!layer || layer === WEBPACK_LAYERS.api) {
-              runtime = 'pages'
-            } else {
-              throw new Error(
-                `shared-runtime module ${moduleName} cannot be used in ${layer} layer`
-              )
+            switch (layer) {
+              case WEBPACK_LAYERS.serverSideRendering:
+              case WEBPACK_LAYERS.reactServerComponents:
+              case WEBPACK_LAYERS.appPagesBrowser:
+              case WEBPACK_LAYERS.actionBrowser:
+                runtime = 'app-page'
+                break
+              default:
+                runtime = 'pages'
             }
             resource.request = `next/dist/server/future/route-modules/${runtime}/vendored/contexts/${moduleName}`
           }

test/e2e/app-dir/navigation/navigation.test.ts
@@ -909,11 +909,4 @@ describe('app dir - navigation', () => {
       })
     })
   })
-
-  describe('pages api', () => {
-    it('should not error if just import the navigation api in pages/api', async () => {
-      const res = await next.fetch('/api/navigation')
-      expect(res.status).toBe(200)
-    })
-  })
 })

test/e2e/app-dir/navigation/pages/api/navigation.js
@@ -1,5 +1,0 @@
-import { useParams } from 'next/navigation'
-
-export default function handle(_, res) {
-  res.send(`${typeof useParams}`)
-}

test/e2e/app-dir/rsc-basic/app/shared-context/server/page.js
@@ -1,0 +1,5 @@
+require('next/router')
+
+export default function Page() {
+  return <p>just work</p>
+}

test/e2e/app-dir/rsc-basic/pages/api/navigation.js
@@ -1,0 +1,6 @@
+// Use `require` to skip the api check
+require('next/navigation')
+
+export default function handle(_, res) {
+  res.send('just work')
+}

test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
@@ -59,6 +59,20 @@ describe('app dir - rsc basics', () => {
     })
   }
 
+  describe('next internal shared context', () => {
+    it('should not error if just load next/navigation module in pages/api', async () => {
+      const res = await next.fetch('/api/navigation')
+      expect(res.status).toBe(200)
+      expect(await res.text()).toBe('just work')
+    })
+
+    it('should not error if just load next/router module in app page', async () => {
+      const res = await next.fetch('/shared-context/server')
+      expect(res.status).toBe(200)
+      expect(await res.text()).toContain('just work')
+    })
+  })
+
   it('should correctly render page returning null', async () => {
     const homeHTML = await next.render('/return-null/page')
     const $ = cheerio.load(homeHTML)