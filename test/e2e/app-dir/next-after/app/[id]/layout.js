0da119c2ccddd1630c8a659958726a05bc7a5d44
Janka Uryga <lolzatu2@gmail.com>
2024-05-17 21:24:30 +0200

    split e2e tests for unstable_after into app and pages, run app tests for both nodejs and edge runtimes

--- test/e2e/app-dir/next-after/app/[id]/layout.js
+++
@@ -1,9 +0,0 @@
-import { unstable_after as after } from 'next/server'
-import { persistentLog } from '../../utils/log'
-
-export default function Layout({ children }) {
-  after(async () => {
-    persistentLog({ source: '[layout] /[id]' })
-  })
-  return <>{children}</>
-}
