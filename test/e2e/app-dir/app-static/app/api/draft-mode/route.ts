01a2c79c55084b9d19a54fec03d6ff81cdb0e554
JJ Kasper <jj@jjsweb.site>
2024-05-20 16:36:20 -0500

    another extra revalidate

--- test/e2e/app-dir/app-static/app/api/draft-mode/route.ts
+++ test/e2e/app-dir/app-static/app/api/draft-mode/route.ts
@@ -1,8 +1,6 @@
 import { draftMode } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
-export const revalidate = 1
-
 export function GET(req: NextRequest) {
   const status = req.nextUrl.searchParams.get('status')
 
