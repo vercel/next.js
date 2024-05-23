59ce87e52266a6d3d50f2f7d17c0f4c6f21e4d0a
Sebastian Silbermann <sebastian.silbermann@vercel.com>
2024-05-23 14:39:27 +0200

    Don't fail the build if `@types/` packages appear to be missing (#66127)
    Co-authored-by: Alessio Gravili <alessio@gravili.de>

--- test/production/ci-missing-typescript-deps/index.test.ts
+++ test/production/ci-missing-typescript-deps/index.test.ts
@@ -35,4 +35,51 @@ describe('ci-missing-typescript-deps', () => {
       await next.destroy()
     }
   })
+
+  it('should not throw an error if beta version of @types/react and @types/react-dom is installed', async () => {
+    const next = await createNext({
+      files: {
+        'pages/index.tsx': `
+          export default function Page() {
+            return <p>hello world</p>
+          }
+        `,
+      },
+      env: {
+        CI: '1',
+      },
+      skipStart: true,
+      dependencies: {
+        '@types/react': 'npm:types-react@beta',
+        '@types/react-dom': 'npm:types-react-dom@beta',
+      },
+      packageJson: {
+        overrides: {
+          '@types/react': 'npm:types-react@beta',
+          '@types/react-dom': 'npm:types-react-dom@beta',
+        },
+        pnpm: {
+          overrides: {
+            '@types/react': 'npm:types-react@beta',
+            '@types/react-dom': 'npm:types-react-dom@beta',
+          },
+        },
+      },
+    })
+    try {
+      const nextBuild = await next.build()
+      // build doesn't fail because types/ may not be installed.
+      expect(nextBuild.cliOutput.split('\n')).toEqual(
+        expect.arrayContaining([
+          'This may be a bug in Next.js. Please file an issue.',
+          'Make sure the following type packages are installed:',
+          // In cyan color but this changes between CI and local.
+          // TODO: Use picocolors in tests to ensure we can test local and CI.
+          expect.stringContaining('@types/react'),
+        ])
+      )
+    } finally {
+      await next.destroy()
+    }
+  })
 })
