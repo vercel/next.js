c0a05bb34c06b5b30c5374efe05fc7557788b2cc
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-05-24 00:41:14 -0700

    chore: Remove unused `base16` dependency and dead `hex` wrapper function (vercel/turbo#8207)
    ### Description
    
    Noticed these dependencies/functions were unused.
    
    For the most part, it's better to do these conversions with the built-in
    `format!()`.
    
    Parsing base16 could make sense for this as a dependency, but we're not
    doing that.
    
    ### Testing Instructions
    
    ```
    cargo check
    ```
    
    Also grepped through next.js to check that we're not using it there.

--- crates/turbo-tasks-hash/src/lib.rs
+++ crates/turbo-tasks-hash/src/lib.rs
@@ -4,16 +4,14 @@
 //! invalidation, and encoding the hash to an hexadecimal string for use in a
 //! file name.
 
-mod base16;
 mod deterministic_hash;
 mod hex;
 mod md4;
 mod xxh3_hash64;
 
 pub use crate::{
-    base16::encode_base16,
     deterministic_hash::{DeterministicHash, DeterministicHasher},
-    hex::{encode_hex, encode_hex_string},
+    hex::encode_hex,
     md4::hash_md4,
     xxh3_hash64::{hash_xxh3_hash64, Xxh3Hash64Hasher},
 };
