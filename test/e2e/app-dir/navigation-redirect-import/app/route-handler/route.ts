bbe29f461cfed94bbe8ebf5b3cdba42d44dbd890
Tim Neutkens <tim@timneutkens.nl>
2024-05-24 17:20:38 +0200

    Update test/e2e/app-dir/navigation-redirect-import/app/route-handler/route.ts
    Co-authored-by: Hendrik Liebau <mail@hendrik-liebau.de>

--- test/e2e/app-dir/navigation-redirect-import/app/route-handler/route.ts
+++ test/e2e/app-dir/navigation-redirect-import/app/route-handler/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-// Redirect can't be called in middleware, but it should be able to be imported.
+// Redirect can't be called in a route handler, but it should be able to be imported.
 console.log({ redirect })
 
 export function GET() {
