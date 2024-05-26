8234fc0ad1f1120ed37df7134d9ae33dcc8c6d70
Tim Neutkens <tim@timneutkens.nl>
2024-05-26 11:45:27 +0200

    Ensure package is installed

--- test/e2e/app-dir/typeof-window/typeof-window.test.ts
+++ test/e2e/app-dir/typeof-window/typeof-window.test.ts
@@ -1,8 +1,12 @@
 import { nextTestSetup } from 'e2e-utils'
+import path from 'path'
 
 describe('typeof-window', () => {
   const { next } = nextTestSetup({
     files: __dirname,
+    dependencies: {
+      'my-differentiated-files': `file:${path.join(__dirname, 'my-differentiated-files.tar')}`,
+    },
   })
 
   it('should work using cheerio', async () => {
