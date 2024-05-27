b88982d79c30c78cd9045b636c123ab127aea40d
Hendrik Liebau <mail@hendrik-liebau.de>
2024-05-27 16:26:42 +0200

    Always print webpack warnings

--- packages/next/taskfile-webpack.js
+++ packages/next/taskfile-webpack.js
@@ -26,18 +26,18 @@ module.exports = function (task) {
           })
         }
 
+        if (stats.hasWarnings()) {
+          this.emit('plugin_warning', {
+            plugin: 'taskfile-webpack',
+            warning: `webpack compiled ${options.name} with warnings:\n${stats.toString('errors-warnings')}`,
+          })
+        }
+
         if (process.env.ANALYZE_STATS) {
           require('fs').writeFileSync(
             require('path').join(__dirname, options.name + '-stats.json'),
             JSON.stringify(stats.toJson())
           )
-        }
-
-        if (process.env.PRINT_WEBPACK_WARNINGS) {
-          this.emit('plugin_warning', {
-            plugin: 'taskfile-webpack',
-            warning: `webpack compiled ${options.name} with warnings:\n${stats.toString('errors-warnings')}`,
-          })
         }
 
         resolve()
