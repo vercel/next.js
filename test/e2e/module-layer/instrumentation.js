be955bb338aff056da2ed7e9c9c768cecf33c213
Jiachi Liu <inbox@huozhi.im>
2024-05-29 10:49:40 +0200

    update test

--- test/e2e/module-layer/instrumentation.js
+++ test/e2e/module-layer/instrumentation.js
@@ -1,8 +1,10 @@
-import { textValue } from './lib/mixed-lib/shared-module'
+import React from 'react'
+// TODO: support react client transform in turbopack
+// import { textValue } from './lib/mixed-lib/shared-module'
 
 export async function register() {
-  if (typeof textValue !== 'string') {
-    throw new Error('textValue is not a string')
+  if (Object(React).useState) {
+    throw new Error('instrumentation is not working correctly in server layer')
   }
   console.log('instrumentation:register')
 }
