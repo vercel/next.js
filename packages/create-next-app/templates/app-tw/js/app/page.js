f06e8c24080e0456a0114f1d18e4bb070e60c650
yasmin p <yasmin.pessoa@vercel.com>
2024-05-30 13:43:36 -0400

    Use classnames to set font family in Tailwind create-next-app templates (#66374)
    Replaces inline styles in the Tailwind `create-next-app` templates with
    classnames to set the font family.
    
    Co-authored-by: Sam Ko <sam@vercel.com>

--- packages/create-next-app/templates/app-tw/js/app/page.js
+++ packages/create-next-app/templates/app-tw/js/app/page.js
@@ -2,10 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
-      style={{ fontFamily: "var(--font-geist-sans)" }}
-    >
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -15,10 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol
-          className="list-inside list-decimal text-sm text-center sm:text-left"
-          style={{ fontFamily: "var(--font-geist-mono)" }}
-        >
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
