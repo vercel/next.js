a68e1ee3328492a9705d139ddb5b4b8491ea7bcc
Jiachi Liu <inbox@huozhi.im>
2024-05-30 09:52:24 +0200

    update test

--- test/e2e/rsc-layers-transform/instrumentation.js
+++ test/e2e/rsc-layers-transform/instrumentation.js
@@ -1,6 +1,5 @@
 import React from 'react'
-// TODO: support react client transform in turbopack
-// import { textValue } from './lib/mixed-lib/shared-module'
+import { textValue } from './lib/shared-module'
 
 export async function register() {
   // TODO: support react-server condition for instrumentation hook in turbopack
@@ -8,4 +7,5 @@ export async function register() {
     throw new Error('instrumentation is not working correctly in server layer')
   }
   console.log('instrumentation:register')
+  console.log('instrumentation:text:' + textValue)
 }
