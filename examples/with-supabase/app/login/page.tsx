c240fe599020205454836613274575eb7c596089
Terry Sutton <saltcod@gmail.com>
2024-06-02 09:11:17 -0230

    Prepare for easier shadcn ui install

--- examples/with-supabase/app/login/page.tsx
+++ examples/with-supabase/app/login/page.tsx
@@ -74,7 +74,7 @@ export default function Login({
         Back
       </Link>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
         <label className="text-md" htmlFor="email">
           Email
         </label>
--- examples/with-supabase/app/protected/page.tsx
+++ examples/with-supabase/app/protected/page.tsx
@@ -31,7 +31,7 @@ export default async function ProtectedPage() {
         </nav>
       </div>
 
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
