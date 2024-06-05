eebe3c542eb3d70db8acae406c201a018a026661
samcx <sam@vercel.com>
2024-06-05 00:00:39 -0700

    chore(github-workflow): update next-repo-actions workflow

--- .github/actions/next-repo-actions/src/bankrupt-issues.ts
+++ .github/actions/next-repo-actions/src/bankrupt-issues.ts
@@ -1,6 +1,3 @@
-// @ts-check
-import { Octokit } from '@actions/github'
-import { info, setFailed } from '@actions/core'
-import { WebClient } from '@slack/web-api'
+import { info } from '@actions/core'
 
-console.log('Hello, world!')
+info('Hello, world!')
