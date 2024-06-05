ac1895351e6feb6837bee036edbc3b50da37116e
Tobias Koppers <tobias.koppers@googlemail.com>
2024-06-05 22:51:45 +0200

    update test case

--- test/e2e/rsc-layers-transform/rsc-layers-transform.test.ts
+++ test/e2e/rsc-layers-transform/rsc-layers-transform.test.ts
@@ -1,26 +1,25 @@
 import { nextTestSetup } from 'e2e-utils'
 
 // TODO: support react-server condition for instrumentation hook in turbopack
-;(process.env.TURBOPACK ? describe.skip : describe)(
-  'rsc layers transform',
-  () => {
-    const { next } = nextTestSetup({
-      files: __dirname,
-    })
+describe('rsc layers transform', () => {
+  const { next, isTurbopack } = nextTestSetup({
+    files: __dirname,
+  })
 
-    it('should render installed react-server condition for middleware', async () => {
-      const json = await next.fetch('/middleware').then((res) => res.json())
+  it('should render installed react-server condition for middleware', async () => {
+    const json = await next.fetch('/middleware').then((res) => res.json())
 
-      expect(json).toEqual({
-        textValue: 'text-value',
-        clientReference: 'Symbol(react.client.reference)',
-      })
+    expect(json).toEqual({
+      textValue: 'text-value',
+      clientReference: isTurbopack
+        ? 'Using client components is not allowed in this environment.'
+        : 'Symbol(react.client.reference)',
     })
+  })
 
-    it('should call instrumentation hook without errors', async () => {
-      const output = next.cliOutput
-      expect(output).toContain('instrumentation:register')
-      expect(output).toContain('instrumentation:text:text-value')
-    })
-  }
-)
+  it('should call instrumentation hook without errors', async () => {
+    const output = next.cliOutput
+    expect(output).toContain('instrumentation:register')
+    expect(output).toContain('instrumentation:text:text-value')
+  })
+})
