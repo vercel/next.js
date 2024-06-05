651fed269e9a1a243bc623599f981c1b25acf87f
eps1lon <sebastian.silbermann@vercel.com>
2024-06-05 11:10:50 +0200

    Add missing `key` to default head

--- packages/next/src/shared/lib/head.tsx
+++ packages/next/src/shared/lib/head.tsx
@@ -12,9 +12,11 @@ type WithInAmpMode = {
 }
 
 export function defaultHead(inAmpMode = false): JSX.Element[] {
-  const head = [<meta charSet="utf-8" />]
+  const head = [<meta charSet="utf-8" key="charset" />]
   if (!inAmpMode) {
-    head.push(<meta name="viewport" content="width=device-width" />)
+    head.push(
+      <meta name="viewport" content="width=device-width" key="viewport" />
+    )
   }
   return head
 }
