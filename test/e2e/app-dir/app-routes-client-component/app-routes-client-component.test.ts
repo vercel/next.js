c41ec7cb9f4549b98ca5fd4aec3b9a95ed0386f1
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-06-10 15:22:46 -0700

    Revert "Fix esm property def in flight loader (#66286)"
    This reverts commit 38ac71b99312a7ec3bf5e28c08600fb83c115ee4.

--- test/e2e/app-dir/app-routes-client-component/app-routes-client-component.test.ts
+++ test/e2e/app-dir/app-routes-client-component/app-routes-client-component.test.ts
@@ -1,14 +1,16 @@
-import { nextTestSetup } from 'e2e-utils'
+import { FileRef, nextTestSetup } from 'e2e-utils'
+import path from 'path'
 
 describe('referencing a client component in an app route', () => {
   const { next } = nextTestSetup({
-    files: __dirname,
+    files: new FileRef(path.join(__dirname)),
   })
 
   it('responds without error', async () => {
     expect(JSON.parse(await next.render('/runtime'))).toEqual({
-      clientComponent: 'function',
-      myModuleClientComponent: 'function',
+      // Turbopack's proxy components are functions
+      clientComponent: process.env.TURBOPACK ? 'function' : 'object',
+      myModuleClientComponent: process.env.TURBOPACK ? 'function' : 'object',
     })
   })
 })
