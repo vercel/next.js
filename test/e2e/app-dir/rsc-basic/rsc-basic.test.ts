vercel/next.js

commit 38ac71b99312a7ec3bf5e28c08600fb83c115ee4

Fix esm property def in flight loader (#66286)
### What

Remove creating client proxy for each ESM export, instead for ESM we
create a CJS module proxy for itself and access the property with export
name as the actual export.

### Why

`proxy` is the module proxy that we treat the module as a client
boundary.
For ESM, we access the property of the module proxy directly for each
export.
This is bit hacky that treating using a CJS like module proxy for ESM's
exports,
but this will avoid creating nested proxies for each export. It will be
improved in the future.

Notice that for `next/dynamic`, if you're doing a dynamic import of
client component in server component, and trying to access the named
export directly, it will error. Instead you need to align the dynamic
import resolved value wrapping with a `default:` property (e.g. `{
default: resolved }`) like what `React.lazy` accepted.

Revert #57301
Fixes #66212

x-ref:
[slack](https://vercel.slack.com/archives/C04DUD7EB1B/p1716897764858829)

test/e2e/app-dir/dynamic/app/dynamic/named-export/page.js
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 
 const Button = dynamic(() =>
   import('./client').then((mod) => {
-    return mod.Button
+    return { default: mod.Button }
   })
 )
 

packages/next/src/build/webpack/loaders/next-flight-loader/index.ts
@@ -97,21 +97,24 @@ export default function transformSource(
         return
       }
 
+      // `proxy` is the module proxy that we treat the module as a client boundary.
+      // For ESM, we access the property of the module proxy directly for each export.
+      // This is bit hacky that treating using a CJS like module proxy for ESM's exports,
+      // but this will avoid creating nested proxies for each export. It will be improved in the future.
       let esmSource = `\
 import { createProxy } from "${MODULE_PROXY_PATH}"
+
+const proxy = createProxy(String.raw\`${resourceKey}\`)
 `
       let cnt = 0
       for (const ref of clientRefs) {
         if (ref === '') {
-          esmSource += `\nexports[''] = createProxy(String.raw\`${resourceKey}#\`);`
+          esmSource += `exports[''] = proxy['']\n`
         } else if (ref === 'default') {
-          esmSource += `\
-export default createProxy(String.raw\`${resourceKey}#default\`);
-`
+          esmSource += `export default createProxy(String.raw\`${resourceKey}#default\`);\n`
         } else {
-          esmSource += `
-const e${cnt} = createProxy(String.raw\`${resourceKey}#${ref}\`);
-export { e${cnt++} as ${ref} };`
+          esmSource += `const e${cnt} = proxy["${ref}"];\n`
+          esmSource += `export { e${cnt++} as ${ref} };\n`
         }
       }
 

packages/next/src/shared/lib/lazy-dynamic/loadable.tsx
@@ -15,7 +15,7 @@ function convertModule<P>(
   // Cases:
   // mod: { default: Component }
   // mod: Component
-  // mod: { $$typeof, default: proxy(Component) }
+  // mod: { default: proxy(Component) }
   // mod: proxy(Component)
   const hasDefault = mod && 'default' in mod
   return {

test/e2e/app-dir/app-routes-client-component/app-routes-client-component.test.ts
@@ -1,16 +1,14 @@
-import { FileRef, nextTestSetup } from 'e2e-utils'
-import path from 'path'
+import { nextTestSetup } from 'e2e-utils'
 
 describe('referencing a client component in an app route', () => {
   const { next } = nextTestSetup({
-    files: new FileRef(path.join(__dirname)),
+    files: __dirname,
   })
 
   it('responds without error', async () => {
     expect(JSON.parse(await next.render('/runtime'))).toEqual({
-      // Turbopack's proxy components are functions
-      clientComponent: process.env.TURBOPACK ? 'function' : 'object',
-      myModuleClientComponent: process.env.TURBOPACK ? 'function' : 'object',
+      clientComponent: 'function',
+      myModuleClientComponent: 'function',
     })
   })
 })

commit 62e8c9dd453839a40627a98803ecf1f0e401eacd

test: add name re-export from client components as page case (#66760)
### What

Add new test case where a named export from client component is being
exported as page

### Why

We found this case while investigating the errors triggered introduced
by #66286 , adding this test to avoid future regression

test/e2e/app-dir/app-routes-client-component/app/layout.js
@@ -1,0 +1,7 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/e2e/app-dir/rsc-basic/app/reexport-named/client.js
@@ -1,0 +1,9 @@
+'use client'
+
+export function PageComponent() {
+  return (
+    <div>
+      <h1 id="client-title">Client Title</h1>
+    </div>
+  )
+}

test/e2e/app-dir/rsc-basic/app/reexport-named/page.js
@@ -1,0 +1,3 @@
+import { PageComponent } from './client'
+
+export default PageComponent

test/e2e/app-dir/rsc-basic/app/reexport-namespace/client.js
@@ -1,5 +1,5 @@
 'use client'
 
 export function Foo() {
-  return <div>Foo</div>
+  return <div id="foo">Foo</div>
 }

test/e2e/app-dir/rsc-basic/rsc-basic.test.ts
@@ -109,6 +109,16 @@ describe('app dir - rsc basics', () => {
     expect($('#return-undefined-layout').html()).toBeEmpty()
   })
 
+  it('should handle named client components imported as page', async () => {
+    const $ = await next.render$('/reexport-named')
+    expect($('#client-title').text()).toBe('Client Title')
+  })
+
+  it('should handle client components imported as namespace', async () => {
+    const $ = await next.render$('/reexport-namespace')
+    expect($('#foo').text()).toBe('Foo')
+  })
+
   it('should render server components correctly', async () => {
     const homeHTML = await next.render('/', null, {
       headers: {