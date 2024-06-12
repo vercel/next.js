ea6bfde224053c62c9f166437a9e728a35caabd6
Tobias Koppers <tobias.koppers@googlemail.com>
2024-06-12 08:54:36 +0200

    add Hello to client component

--- bench/nested-deps-app-router/app/server-and-client-components/client-component.js
+++ bench/nested-deps-app-router/app/server-and-client-components/client-component.js
@@ -4,5 +4,10 @@ import React from 'react'
 import Comp from '../../components/index.jsx'
 
 export default function Home() {
-  return <Comp />
+  return (
+    <>
+      <h1>Hello!</h1>
+      <Comp />
+    </>
+  )
 }
