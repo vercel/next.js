6d00df5e0cf1fd6592b8590da22ffcfd328f6344
Mayank <mayank.srmu@gmail.com>
2024-06-12 09:17:02 +0000

    fix: support dark mode - update classes for dark mode for footer and alert - use classnames lib for adding multiple classes to layout

--- examples/blog-starter/src/app/layout.tsx
+++ examples/blog-starter/src/app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/app/_components/footer";
 import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import cn from "classnames";
 
 import "./globals.css";
 
@@ -56,10 +57,7 @@ export default function RootLayout({
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body
-        className={[
-          inter.className,
-          "dark:bg-slate-900 dark:text-slate-400",
-        ].join(" ")}
+        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
       >
         <div className="min-h-screen">{children}</div>
         <Footer />
