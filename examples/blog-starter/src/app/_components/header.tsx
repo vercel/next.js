60c209738ca44636caec9a94d58ee20e4b8bcc80
Mayank <mayank.srmu@gmail.com>
2024-06-12 09:48:03 +0000

    Add mode switch
    - Add Switch component from nextjs-darkmode
    - Add styles for the Switch component

--- examples/blog-starter/src/app/_components/header.tsx
+++ examples/blog-starter/src/app/_components/header.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import { Switch } from "nextjs-darkmode/switch";
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex justify-between items-center">
       <Link href="/" className="hover:underline">
-        Blog
+        Blog.
       </Link>
-      .
+      <Switch size={28} />
     </h2>
   );
 };
