b20978413c1c9263522effb1415eec3dcf79461f
Mayank <mayank.srmu@gmail.com>
2024-06-14 16:06:46 +0000

    Update tailwind config to respect both system preference and class

--- examples/blog-starter/tailwind.config.ts
+++ examples/blog-starter/tailwind.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: [
+    "variant",
+    [
+      "@media (prefers-color-scheme: dark) { &:not(.light *) }",
+      "&:is(.dark *)",
+    ],
+  ],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
