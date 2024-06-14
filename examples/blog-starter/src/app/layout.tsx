c77a794e4925eb3877884ba436c48c14e88ba986
Mayank <mayank.srmu@gmail.com>
2024-06-14 16:08:05 +0000

    Update nextjs-darkmode and set dont persist to true.

--- examples/blog-starter/src/app/layout.tsx
+++ examples/blog-starter/src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
       <body
         className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
       >
-        <Core />
+        <Core dp />
         <div className="min-h-screen">{children}</div>
         <Footer />
       </body>
