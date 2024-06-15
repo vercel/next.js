d09e14ccd7bfd4560bb71edb8212d6eaf46e0b42
Mayank <mayank.srmu@gmail.com>
2024-06-15 12:55:45 +0000

    fix: Apply mode by injecting script
    - No cookies

--- examples/blog-starter/tailwind.config.ts
+++ examples/blog-starter/tailwind.config.ts
@@ -1,13 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: [
-    "variant",
-    [
-      "@media (prefers-color-scheme: dark) { &:not(.light *) }",
-      "&:is(.dark *)",
-    ],
-  ],
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
