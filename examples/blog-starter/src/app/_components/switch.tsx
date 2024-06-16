60b7ee4153efdd21bfe48041ac78450a9847ce7b
Mayank <mayank.srmu@gmail.com>
2024-06-16 04:50:42 +0000

    simplify: remove extensibility from Switch component which is not required for this example

--- examples/blog-starter/src/app/_components/switch.tsx
+++ examples/blog-starter/src/app/_components/switch.tsx
@@ -1,76 +1,26 @@
-import { Dispatch, HTMLProps, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from "react";
 import styles from "./switch.module.css";
 import { ColorSchemePreference, ThemeState } from "./theme-switcher";
 
 export const modes: ColorSchemePreference[] = ["system", "dark", "light"];
 
-export interface SwitchProps extends HTMLProps<HTMLButtonElement> {
-  /** html tag @defaultValue 'button' */
-  tag?: "button" | "div";
-  /** Diameter of the color switch */
-  size?: number;
-  /** Skip system colorScheme while toggling */
-  skipSystem?: boolean;
+export interface SwitchProps {
   themeState: ThemeState;
   setThemeState: Dispatch<SetStateAction<ThemeState>>;
 }
 
 /**
  * Switch button to quickly toggle user preference.
- *
- * @example
- * ```tsx
- * <Switch [size={20} skipSystem]/>
- * ```
- *
- * @source - Source code
  */
-export const Switch = ({
-  tag: Tag = "button",
-  size = 24,
-  skipSystem,
-  children,
-  themeState: state,
-  setThemeState: setState,
-  ...props
-}: SwitchProps) => {
+export const Switch = ({ themeState, setThemeState }: SwitchProps) => {
   /** toggle mode */
   const handleModeSwitch = () => {
-    let index = modes.indexOf(state.mode);
+    let index = modes.indexOf(themeState.mode);
     const n = modes.length;
-    if (skipSystem && index === n - 1) index = 0;
-    setState({
-      ...state,
+    setThemeState({
+      ...themeState,
       mode: modes[(index + 1) % n],
     });
   };
-  if (children) {
-    return (
-      // @ts-expect-error -- too complex types
-      <Tag
-        suppressHydrationWarning
-        {...props}
-        data-testid="switch"
-        // skipcq: JS-0417
-        onClick={handleModeSwitch}
-      >
-        {/* @ts-expect-error -> we are setting the CSS variable */}
-        <div className={styles.switch} style={{ "--size": `${size}px` }} />
-        {children}
-      </Tag>
-    );
-  }
-  return (
-    <Tag
-      // Hydration warning is caused when the data from localStorage differs from the default data provided while rendering on server
-      suppressHydrationWarning
-      {...props}
-      className={[props.className, styles.switch].join(" ")}
-      // @ts-expect-error -> we are setting the CSS variable
-      style={{ "--size": `${size}px` }}
-      data-testid="switch"
-      // skipcq: JS-0417 -> tradeoff between size and best practices
-      onClick={handleModeSwitch}
-    />
-  );
+  return <button className={styles.switch} onClick={handleModeSwitch} />;
 };
