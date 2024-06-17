f16d9394154a5a758c0657133e7b61ebe860e3be
samcx <sam@vercel.com>
2024-06-17 18:43:34 +0200

    chore(github-workflow): test bankrupt-issues

--- .github/actions/next-repo-actions/src/bankrupt-issues.ts
+++ .github/actions/next-repo-actions/src/bankrupt-issues.ts
@@ -1,22 +1,24 @@
 import { context, getOctokit } from '@actions/github'
-import { info, setFailed } from '@actions/core'
+import { getInput, info, setFailed } from '@actions/core'
 
 async function main() {
   if (!process.env.GITHUB_TOKEN) throw new TypeError('GITHUB_TOKEN not set')
 
   const octokit = getOctokit(process.env.GITHUB_TOKEN)
   const { owner, repo } = context.repo
+  const created = getInput('created')
+  const year = created.slice(0, 4)
   const body = `
 
-We are in the process of closing issues dating back to 2019 to improve our focus on the most relevant and actionable problems.
+We are in the process of closing issues dating back to ${year} to improve our focus on the most relevant and actionable problems.
 
 **_Why are we doing this?_**
 
 Stale issues often lack recent updates and clear reproductions, making them difficult to address effectively. Our objective is to prioritize the most upvoted and actionable issues that have up-to-date reproductions, enabling us to resolve bugs more efficiently.
 
-**_Why 2019 issues?_**
+**_Why ${year} issues?_**
 
-Issues from 2019 are likely to be outdated and less relevant to the current state of the codebase. By closing these older stale issues, we can better focus our efforts on more recent and relevant problems, ensuring a more effective and streamlined workflow.
+Issues from ${year} are likely to be outdated and less relevant to the current state of the codebase. By closing these older stale issues, we can better focus our efforts on more recent and relevant problems, ensuring a more effective and streamlined workflow.
 
 If your issue is still relevant, please reopen it using our [bug report template](https://github.com/vercel/next.js/issues/new?assignees=&labels=bug&projects=&template=1.bug_report.yml). Be sure to include any important context from the original issue in your new report.
 
@@ -28,46 +30,50 @@ The Next.js Team
 
   let issues: number[] = []
 
-  try {
-    const { data } = await octokit.rest.search.issuesAndPullRequests({
-      q: `repo:${owner}/${repo} is:issue is:open created:2019-01-01..2019-12-31`,
-    })
+  info(`created = ${created}`)
+  info(`year = ${year}`)
+  info(`body = ${body}`)
 
-    issues = data.items.map((issue) => issue.number)
+  // try {
+  //   const { data } = await octokit.rest.search.issuesAndPullRequests({
+  //     q: `repo:${owner}/${repo} is:issue is:open created:${created}`,
+  //   })
 
-    info(`issues = ${issues}`)
-    info(`${issues.length} issues found!`)
+  //   issues = data.items.map((issue) => issue.number)
 
-    issues.forEach(async (issue_number) => {
-      // assign the issue to samcx
-      await octokit.rest.issues.addAssignees({
-        owner,
-        repo,
-        issue_number,
-        assignees: ['samcx'],
-      })
+  //   info(`issues = ${issues}`)
+  //   info(`${issues.length} issues found!`)
 
-      // add a comment
-      await octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number,
-        body,
-      })
+  //   issues.forEach(async (issue_number) => {
+  //     // assign the issue to samcx
+  //     await octokit.rest.issues.addAssignees({
+  //       owner,
+  //       repo,
+  //       issue_number,
+  //       assignees: ['samcx'],
+  //     })
 
-      // close the issue
-      await octokit.rest.issues.update({
-        owner,
-        repo,
-        issue_number,
-        state: 'closed',
-      })
-    })
+  //     // add a comment
+  //     await octokit.rest.issues.createComment({
+  //       owner,
+  //       repo,
+  //       issue_number,
+  //       body,
+  //     })
 
-    info(`${issues.length} issues closed.`)
-  } catch (error) {
-    setFailed(error)
-  }
+  //     // close the issue
+  //     await octokit.rest.issues.update({
+  //       owner,
+  //       repo,
+  //       issue_number,
+  //       state: 'closed',
+  //     })
+  //   })
+
+  //   info(`${issues.length} issues closed.`)
+  // } catch (error) {
+  //   setFailed(error)
+  // }
 }
 
 main()
