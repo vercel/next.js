d02d60d0859f6ea09cb7c899314ba7127453eb72
Jiachi Liu <inbox@huozhi.im>
2024-06-18 11:53:12 +0200

    externals

--- test/e2e/esm-externals/app/client/page.js
+++ test/e2e/esm-externals/app/client/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import World1 from 'app-esm-package1/entry'
+// import('app-esm-package1') SSR layer
 import World2 from 'app-esm-package2/entry'
 import World3 from 'app-cjs-esm-package/entry'
 
