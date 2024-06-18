c1a3d4f16a42d821f018e9d1149f3800f332cf78
archanaagivale30 <archana.agivale@qed42.com>
2024-06-19 00:17:47 +0530

    minor typo fixes for js and ts

--- examples/with-sitemap/app/sitemap.ts
+++ examples/with-sitemap/app/sitemap.ts
@@ -1,14 +1,14 @@
 const globby = require("globby");
 
  function addPage(page:string) {
-  const path = page.replace("app", "").replace(".js", "").replace(".mdx", "").replace("/page","");
+  const path = page.replace("app", "").replace(".tsx", "").replace(".mdx", "").replace("/page","");
   return path;
 }
 export default async function sitemap() {
   const pages = await globby([
-    "app/**/*{.js,jsx,tx,tsx,.mdx}",
+    "app/**/*{.js,jsx,ts,tsx,.mdx}",
     "!app/_*.js",
-    "!app/{sitemap,layout}.js",
+    "!app/{sitemap,layout}.{js,jsx,ts,tsx}",
     "!app/api",
   ]);
   const routes =pages.map((page:string) => ({
