feb91b18819ce7aefd3e9df18877d7de401d6c0c
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2024-06-18 08:19:40 +0530

    added example

---
+++ examples/with-imagekit/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
