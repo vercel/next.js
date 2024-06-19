d02dfc6e0578fcf2e6ca8ea69ae4a138dbba7ca2
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-06-19 16:49:48 +0200

    feat(turbopack): add support for esm externals in app dir (#64918)
    ### What?
    Writes the async flag to the client reference manifest to support ESM
    externals in app dir.
    
    The `app-ssr` context can't have esm externals as that might cause a
    module to be async in ssr but not on the client.
    
    Closes PACK-2967
    Fixes #64525

--- test/e2e/app-dir/app-external/app-external.test.ts
+++ test/e2e/app-dir/app-external/app-external.test.ts
@@ -16,7 +16,7 @@ async function resolveStreamResponse(response: any, onData?: any) {
 }
 
 describe('app dir - external dependency', () => {
-  const { next, skipped, isTurbopack } = nextTestSetup({
+  const { next, skipped } = nextTestSetup({
     files: __dirname,
     dependencies: {
       swr: 'latest',
@@ -286,10 +286,7 @@ describe('app dir - external dependency', () => {
       browser.elementByCss('#dual-pkg-outout button').click()
       await check(async () => {
         const text = await browser.elementByCss('#dual-pkg-outout p').text()
-        // TODO: enable esm externals for app router in turbopack for actions
-        expect(text).toBe(
-          isTurbopack ? 'dual-pkg-optout:cjs' : 'dual-pkg-optout:mjs'
-        )
+        expect(text).toBe('dual-pkg-optout:mjs')
         return 'success'
       }, /success/)
     })
