f844915b2f20426e3c9e6aad39f5e1d395b24d6d
Tim Neutkens <tim@timneutkens.nl>
2024-06-19 10:21:54 +0200

    Rename dev_runtime to browser_runtime (vercel/turbo#8396)
    ### Description
    
    Mirrors the changes from `get_dev_runtime_code` to `get_browser_runtime_code`, didn't rename the file back then.
    
    <!--
      ✍️ Write a short summary of your work.
      If necessary, include relevant screenshots.
    -->
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->

--- crates/turbopack-ecmascript-runtime/src/lib.rs
+++ crates/turbopack-ecmascript-runtime/src/lib.rs
@@ -1,15 +1,15 @@
 #![feature(arbitrary_self_types)]
 
 pub(crate) mod asset_context;
+pub(crate) mod browser_runtime;
 pub(crate) mod build_runtime;
-pub(crate) mod dev_runtime;
 #[cfg(feature = "test")]
 pub(crate) mod dummy_runtime;
 pub(crate) mod embed_js;
 pub(crate) mod runtime_type;
 
+pub use browser_runtime::get_browser_runtime_code;
 pub use build_runtime::get_nodejs_runtime_code;
-pub use dev_runtime::get_browser_runtime_code;
 #[cfg(feature = "test")]
 pub use dummy_runtime::get_dummy_runtime_code;
 pub use embed_js::{embed_file, embed_file_path, embed_fs};
