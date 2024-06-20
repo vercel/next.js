6bcc51fb8cf8c7b59f31f33df6e8a171f5fbf986
Mark Knichel <7355009+mknichel@users.noreply.github.com>
2024-06-20 15:31:29 -0700

    Fix unit tests

--- packages/next/src/diagnostics/build-diagnostics.test.ts
+++ packages/next/src/diagnostics/build-diagnostics.test.ts
@@ -2,7 +2,10 @@ import { mkdtemp, readFile } from 'fs/promises'
 import { tmpdir } from 'os'
 import { join } from 'path'
 import { setGlobal } from '../trace/shared'
-import { updateBuildDiagnostics } from './build-diagnostics'
+import {
+  recordFrameworkVersion,
+  updateBuildDiagnostics,
+} from './build-diagnostics'
 
 async function readBuildDiagnostics(dir: string) {
   return JSON.parse(
@@ -11,41 +14,51 @@ async function readBuildDiagnostics(dir: string) {
 }
 
 describe('build-diagnostics', () => {
+  it('records framework version to framework.json correctly', async () => {
+    const tmpDir = await mkdtemp(join(tmpdir(), 'build-diagnostics'))
+    setGlobal('distDir', tmpDir)
+
+    // Record the initial diagnostics and make sure it's correct.
+    await recordFrameworkVersion('14.2.3')
+    let diagnostics = JSON.parse(
+      await readFile(join(tmpDir, 'diagnostics', 'framework.json'), 'utf8')
+    )
+    expect(diagnostics.version).toEqual('14.2.3')
+  })
+
   it('records build diagnostics to a file correctly', async () => {
     const tmpDir = await mkdtemp(join(tmpdir(), 'build-diagnostics'))
     setGlobal('distDir', tmpDir)
 
     // Record the initial diagnostics and make sure it's correct.
     await updateBuildDiagnostics({
-      version: '14.2.3',
+      buildStage: 'compile',
     })
     let diagnostics = await readBuildDiagnostics(tmpDir)
-    expect(diagnostics.version).toEqual('14.2.3')
+    expect(diagnostics.buildStage).toEqual('compile')
 
     // Add a new build option. Make sure that existing fields are preserved.
     await updateBuildDiagnostics({
-      buildStage: 'compile',
+      buildStage: 'compile-server',
       buildOptions: {
         useBuildWorker: String(false),
       },
     })
     diagnostics = await readBuildDiagnostics(tmpDir)
-    expect(diagnostics.version).toEqual('14.2.3')
-    expect(diagnostics.buildStage).toEqual('compile')
+    expect(diagnostics.buildStage).toEqual('compile-server')
     expect(diagnostics.buildOptions).toEqual({
       useBuildWorker: 'false',
     })
 
     // Make sure that it keeps existing build options when adding a new one.
     await updateBuildDiagnostics({
-      buildStage: 'compile',
+      buildStage: 'compile-client',
       buildOptions: {
         experimentalBuildMode: 'compile',
       },
     })
     diagnostics = await readBuildDiagnostics(tmpDir)
-    expect(diagnostics.version).toEqual('14.2.3')
-    expect(diagnostics.buildStage).toEqual('compile')
+    expect(diagnostics.buildStage).toEqual('compile-client')
     expect(diagnostics.buildOptions).toEqual({
       experimentalBuildMode: 'compile',
       useBuildWorker: 'false',
