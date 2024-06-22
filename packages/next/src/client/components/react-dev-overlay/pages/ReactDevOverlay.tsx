e72aff0a03ae4ced599a254fc7bc3b39a5b10c55
Jiachi Liu <inbox@huozhi.im>
2024-06-22 22:32:48 +0200

    Add build error message to error overlay

--- packages/next/src/client/components/react-dev-overlay/pages/ReactDevOverlay.tsx
+++ packages/next/src/client/components/react-dev-overlay/pages/ReactDevOverlay.tsx
@@ -77,6 +77,7 @@ export default function ReactDevOverlay({
 
           {displayPrevented ? null : hasBuildError ? (
             <BuildError
+              errors={state.errors}
               message={state.buildError!}
               versionInfo={state.versionInfo}
             />
--- test/development/acceptance-app/rsc-build-errors.test.ts
+++ test/development/acceptance-app/rsc-build-errors.test.ts
@@ -315,6 +315,32 @@ describe('Error overlay - RSC build errors', () => {
     await cleanup()
   })
 
+  it('should display error message when module resolve error occurred', async () => {
+    const { session, cleanup } = await sandbox(
+      next,
+      new Map([
+        [
+          'app/unresolve/page.js',
+          `
+          import Mod from 'non-existing-module'
+
+          export default function Page() {
+            return <p>page</p>
+          }
+        `,
+        ],
+      ]),
+      '/unresolve'
+    )
+
+    await session.assertHasRedbox()
+    expect(await session.getRedboxDescription()).toInclude(
+      `Module not found: Can't resolve 'non-existing-module'`
+    )
+
+    await cleanup()
+  })
+
   it('should throw an error when error file is a server component', async () => {
     const { session, cleanup } = await sandbox(
       next,
