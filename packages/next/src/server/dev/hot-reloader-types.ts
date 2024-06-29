378d9c4a673b8c6056dc798a9bb05fcd770a610a
JJ Kasper <jj@jjsweb.site>
2024-06-29 09:46:18 -0700

    more types

--- packages/next/src/server/dev/hot-reloader-types.ts
+++ packages/next/src/server/dev/hot-reloader-types.ts
@@ -153,7 +153,7 @@ export interface NextJsHotReloaderInterface {
     req: IncomingMessage,
     _socket: Duplex,
     head: Buffer,
-    callback: (client: import('next/dist/compiled/ws').WebSocket) => void
+    callback: (client: { send(data: string): void }) => void
   ): void
   invalidate({
     reloadAfterInvalidation,
