b925435bc73b05fbf5230ed267bad1f8b8bf7670
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2024-07-03 10:32:30 +0300

    Fix issue with refreshing sessions

--- examples/with-supabase/utils/supabase/server.ts
+++ examples/with-supabase/utils/supabase/server.ts
@@ -12,6 +12,17 @@ export const createClient = () => {
         getAll() {
           return cookieStore.getAll();
         },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
       },
     },
   );
