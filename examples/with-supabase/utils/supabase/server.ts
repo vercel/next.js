7e975aff264a73daa8621429e41b153c1b68f55a
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2024-07-03 10:14:04 +0300

    chore: Update cookie handling in supabase middleware and server
    With latest version of supabase/ssr package we need to change how we set cookies. This commit updates the cookie handling in the `supabase` middleware and server files. It replaces the `get`, `set`, and `remove` methods with `getAll`, `setAll`, and `removeAll` methods respectively. This change ensures that all cookies are handled consistently and improves the overall cookie management in the codebase.

--- examples/with-supabase/utils/supabase/server.ts
+++ examples/with-supabase/utils/supabase/server.ts
@@ -1,4 +1,4 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = () => {
@@ -9,26 +9,8 @@ export const createClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+        getAll() {
+          return cookieStore.getAll();
         },
       },
     },
