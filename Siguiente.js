374b5dd5fb1f94e29483fdd207d3034f5732a99c
GreggJEduardoPH093 <greggjeduardophgnium@gmail.com>
2024-07-04 03:19:11 -0600

    Create Siguiente.js
    @toolmantim #20334 
    @sco #51030 
    @chrislloyd #56852 
    @benschwarz #67450 
    @Peeja #17641

---
+++ Siguiente.js
@@ -0,0 +1,37 @@
+
+
+Siguiente.js
+   
+
+Empezando
+Utilizado por algunas de las empresas más grandes del mundo, Next.js le permite crear aplicaciones web completas al ampliar las últimas características de React e integrar potentes herramientas de JavaScript basadas en Rust para compilaciones más rápidas.
+
+Visita nuestro curso Aprenda Next.js para comenzar a utilizar Next.js.
+Visita la presentación de Next.js para ver más sitios creados con Next.js.
+Documentación
+Visita https://nextjs.org/docs para ver la documentación completa.
+
+Comunidad
+La comunidad Next.js se puede encontrar en GitHub Discussions, donde puedes hacer preguntas, expresar ideas y compartir tus proyectos con otras personas.
+
+Para chatear con otros miembros de la comunidad puedes unirte al servidor Discord de Next.js.
+
+Tenga en cuenta que nuestro Código de conducta se aplica a todos los canales de la comunidad de Next.js. Se recomienda encarecidamente a los usuarios que lo lean y lo respeten para evitar repercusiones.
+
+Contribuyendo
+Las contribuciones a Next.js son bienvenidas y muy apreciadas. Sin embargo, antes de comenzar, nos gustaría que revisaras nuestras Pautas de contribución para asegurarte de que tengas una experiencia sin problemas al contribuir a Next.js.
+
+Buenos primeros problemas:
+Contamos con una lista de buenos primeros números que contienen errores de alcance relativamente limitado. Este es un excelente lugar para que tanto los principiantes como los recién llegados comiencen, adquieran experiencia y se familiaricen con nuestro proceso de contribución.
+
+Autores
+¡Una lista de los coautores originales de Next.js que ayudaron a darle vida a este increíble marco!
+
+Tim Neutkens ( @timneutkens )
+Naoyuki Kanezawa ( @nkzawa )
+Guillermo Rauch ( @rauchg )
+Arunoda Susiripala ( @arunoda )
+Tony Kovanen ( @tonykovanen )
+Dan Zajdband ( @impronunciable )
+Seguridad
+Si cree que ha encontrado una vulnerabilidad de seguridad en Next.js, le recomendamos que lo revele de manera responsable y NO abra un problema público . Investigaremos todos los informes legítimos. Envíe un correo electrónico security@vercel.compara informar sobre cualquier vulnerabilidad de seguridad. Alternativamente, puede visitar este enlace para obtener más información sobre la seguridad de Vercel e informar sobre cualquier vulnerabilidad de seguridad.
