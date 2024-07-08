6f663db4547b84e07faa1ec6d04e7ae62c3f614d
Jiachi Liu <inbox@huozhi.im>
2024-07-08 14:50:03 +0200

    test: update component to async to avoid TS error in build

--- test/e2e/app-dir/dedupe-rsc-error-log/app/component.tsx
+++ test/e2e/app-dir/dedupe-rsc-error-log/app/component.tsx
@@ -1,8 +1,4 @@
-async function getData(name: string) {
+export function ErrorComponent({ name }: { name: string }) {
   throw new Error('Custom error:' + name)
-}
-
-export async function ErrorComponent({ name }: { name: string }) {
-  await getData(name)
   return null
 }
