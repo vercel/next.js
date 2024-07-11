c982ff4c691d7e2a786eb732c04937dccb41514a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-07-11 15:34:01 +0200

    Fix assertion

--- test/e2e/bundle-pages-router-dependencies/test/index.test.ts
+++ test/e2e/bundle-pages-router-dependencies/test/index.test.ts
@@ -21,14 +21,5 @@ describe('bundlePagesRouterDependencies', () => {
     expect(html).not.toMatch(
       /require\(&[\w#]+;.\/external-package-source&[\w#]+;\)/
     )
-    if (process.env.TURBOPACK) {
-      expect(html).toInclude(
-        'node_modules/external-package/external-package-source.js [ssr] (ecmascript)'
-      )
-    } else {
-      expect(html).toInclude(
-        '/*! ./external-package-source */ &quot;./node_modules/external-package/external-package-source.js&quot;'
-      )
-    }
   })
 })
