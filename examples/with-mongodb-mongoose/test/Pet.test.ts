54456bf389e63e110dcbf656bd0b984626ef2876
Florian Bienefelt <florian.bienefelt@gmail.com>
2024-07-11 11:44:44 +0200

    Fix test

--- examples/with-mongodb-mongoose/test/Pet.test.ts
+++ examples/with-mongodb-mongoose/test/Pet.test.ts
@@ -5,21 +5,27 @@ import Pet from "../models/Pet";
 describe('Pet', () => {
   beforeAll(async () => {
     await dbConnect()
+    await Pet.deleteMany({})
+  });
+
+  afterAll(async () => {
+    const connection = await dbConnect();
+    connection.disconnect();
   });
 
   it('should create a pet', async () => {
     const result = await Pet.create({
-      name: "",
-      owner_name: "",
-      species: "",
-      age: 0,
+      name: "Rex",
+      owner_name: "John",
+      species: "Cat",
+      age: 10,
       poddy_trained: false,
       diet: [],
-      image_url: "",
+      image_url: "https://www.google.com",
       likes: [],
       dislikes: [],
     })
 
     expect(result._id).toBeDefined();
-  })
+  });
 });
