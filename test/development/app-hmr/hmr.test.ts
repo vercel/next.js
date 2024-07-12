6b52a1808e35fb54b0d98acdf71a2f274de967b6
eps1lon <sebastian.silbermann@vercel.com>
2024-07-12 15:40:15 +0200

    Current behavior of HMR to page with new bundler runtime

--- test/development/app-hmr/hmr.test.ts
+++ test/development/app-hmr/hmr.test.ts
@@ -245,5 +245,78 @@ describe(`app-dir-hmr`, () => {
     it('should have no unexpected action error for hmr', async () => {
       expect(next.cliOutput).not.toContain('Unexpected action')
     })
+
+    it('can navigate cleanly to a page that requires a change in the Webpack runtime', async () => {
+      // This isn't a very accurate test since the Webpack runtime is somewhat an implementation detail.
+      // To ensure this is still valid, check the `*/webpack.*.hot-update.js` network response content when the navigation is triggered.
+      // If there is new functionality added, the test is still valid.
+      // If not, the test doesn't cover anything new.
+      // TODO: Enforce console.error assertions or MPA navigation assertions in all tests instead.
+      const browser = await next.browser('/bundler-runtime-changes')
+      await browser.eval('window.__TEST_NO_RELOAD = true')
+
+      await browser
+        .elementByCss('a')
+        .click()
+        .waitForElementByCss('[data-testid="new-runtime-functionality-page"]')
+
+      const logs = await browser.log()
+      if (process.env.TURBOPACK) {
+        // FIXME: logging "rebuilding" multiple times instead of closing it of with "done in"
+        // Should just not branch here and have the same logs as Webpack.
+        expect(logs).toEqual(
+          expect.arrayContaining([
+            {
+              message: '[Fast Refresh] rebuilding',
+              source: 'log',
+            },
+            {
+              message: '[Fast Refresh] rebuilding',
+              source: 'log',
+            },
+            {
+              message: '[Fast Refresh] rebuilding',
+              source: 'log',
+            },
+          ])
+        )
+        expect(logs).not.toEqual(
+          expect.arrayContaining([
+            {
+              message: expect.stringContaining('[Fast Refresh] done in'),
+              source: 'log',
+            },
+          ])
+        )
+      } else {
+        // TODO: Should assert on all logs but these are cluttered with logs from our test utils (e.g. playwright tracing or webdriver)
+        expect(logs).toEqual(
+          expect.arrayContaining([
+            {
+              message: '[Fast Refresh] rebuilding',
+              source: 'log',
+            },
+            {
+              message: expect.stringContaining('[Fast Refresh] done in'),
+              source: 'log',
+            },
+          ])
+        )
+        expect(logs).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              source: 'error',
+            }),
+          ])
+        )
+      }
+      if (process.env.TURBOPACK) {
+        // No MPA navigation triggered
+        expect(await browser.eval('window.__TEST_NO_RELOAD')).toEqual(true)
+      } else {
+        // MPA navigation triggered
+        expect(await browser.eval('window.__TEST_NO_RELOAD')).toEqual(undefined)
+      }
+    })
   })
 })
