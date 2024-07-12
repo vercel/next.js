3335ec638dd5495b433dd6805ac67ad6fa35cfab
Tobias Koppers <tobias.koppers@googlemail.com>
2024-07-12 11:11:32 +0200

    add required Hash derives

--- packages/next-swc/crates/next-core/src/app_structure.rs
+++ packages/next-swc/crates/next-core/src/app_structure.rs
@@ -97,7 +97,9 @@ pub enum MetadataWithAltItem {
 }
 
 /// A single metadata file.
-#[derive(Copy, Clone, Debug, Serialize, Deserialize, PartialEq, Eq, TaskInput, TraceRawVcs)]
+#[derive(
+    Copy, Clone, Debug, Hash, Serialize, Deserialize, PartialEq, Eq, TaskInput, TraceRawVcs,
+)]
 pub enum MetadataItem {
     Static { path: Vc<FileSystemPath> },
     Dynamic { path: Vc<FileSystemPath> },
@@ -468,7 +470,16 @@ impl LoaderTree {
 }
 
 #[derive(
-    Clone, PartialEq, Eq, Serialize, Deserialize, TraceRawVcs, ValueDebugFormat, Debug, TaskInput,
+    Clone,
+    PartialEq,
+    Eq,
+    Hash,
+    Serialize,
+    Deserialize,
+    TraceRawVcs,
+    ValueDebugFormat,
+    Debug,
+    TaskInput,
 )]
 pub enum Entrypoint {
     AppPage {
