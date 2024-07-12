6d38386836a18ec2b1344291a238f40d55a199b1
eps1lon <sebastian.silbermann@vercel.com>
2024-07-12 14:19:27 +0200

    Only await Webpack Runtime update outside of prefetches

--- packages/next/src/client/components/router-reducer/fetch-server-response.ts
+++ packages/next/src/client/components/router-reducer/fetch-server-response.ts
@@ -93,11 +93,14 @@ export async function fetchServerResponse(
   }
 
   /**
-   * Three cases:
+   * Four cases:
    * - `prefetchKind` is `undefined`, it means it's a normal navigation, so we want to prefetch the page data fully
+   * - `prefetchKind` is `temporary` - it means it's a normal navigation, so we want to prefetch the page data fully
    * - `prefetchKind` is `full` - we want to prefetch the whole page so same as above
    * - `prefetchKind` is `auto` - if the page is dynamic, prefetch the page data partially, if static prefetch the page data fully
    */
+  const isNavigation =
+    prefetchKind === undefined || prefetchKind === PrefetchKind.TEMPORARY
   if (prefetchKind === PrefetchKind.AUTO) {
     headers[NEXT_ROUTER_PREFETCH_HEADER] = '1'
   }
@@ -185,7 +188,7 @@ export async function fetchServerResponse(
     // In prod, every page will have the same Webpack runtime.
     // In dev, the Webpack runtime is minimal for each page.
     // We need to ensure the Webpack runtime is updated before executing client-side JS of the new page.
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== 'production' && isNavigation) {
       await waitForWebpackRuntimeHotUpdate()
     }
 
