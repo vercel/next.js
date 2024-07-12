ce59af9db27054e2367179c32dd428e6d8c1c26b
Untitled <gnu.imm@outlook.com>
2024-07-12 20:44:54 +0800

    Add unit test case for ImageResponse headers

--- test/unit/image-response-header.test.ts
+++ test/unit/image-response-header.test.ts
@@ -3,8 +3,8 @@ import { ImageResponse } from 'next/og'
 import React from 'react'
 
 describe('new ImageResponse()', () => {
-  it('should merge headers correctly', () => {
-    const exactHeader = 'public, max-age=3600, s-maxage=3600'
+  const exactHeader = 'public, max-age=3600, s-maxage=3600'
+  it('should merge object literal headers correctly', () => {
     const res = new ImageResponse(
       React.createElement(
         'div',
@@ -19,6 +19,25 @@ describe('new ImageResponse()', () => {
         },
       }
     )
+    expect(res.headers.get('Content-Type')).toBeTruthy()
+    expect(res.headers.get('Cache-Control')).toBe(exactHeader)
+  })
+  it('should merge Headers instance correctly', () => {
+    const res = new ImageResponse(
+      React.createElement(
+        'div',
+        { style: { width: 10, height: 10 } },
+        'ImageResponse'
+      ),
+      {
+        width: 10,
+        height: 10,
+        headers: new Headers({
+          'Cache-Control': exactHeader,
+        }),
+      }
+    )
+    expect(res.headers.get('Content-Type')).toBeTruthy()
     expect(res.headers.get('Cache-Control')).toBe(exactHeader)
   })
   it('should have default Cache-Control header', () => {
