77b0d7dec9f2627d28d9843a02fd7a6abd72c759
samcx <sam@vercel.com>
2024-07-15 22:47:41 +0200

    chore: query last weeks issues

--- .github/actions/next-repo-actions/src/popular-issues.mjs
+++ .github/actions/next-repo-actions/src/popular-issues.mjs
@@ -2,78 +2,79 @@
 import { context, getOctokit } from '@actions/github'
 import { info, setFailed } from '@actions/core'
 import { WebClient } from '@slack/web-api'
-import { formattedDate, ninetyDaysAgo } from '../lib/util.mjs'
+import { formattedDate } from '../lib/util.mjs'
 
-function generateBlocks(issues) {
-  const blocks = [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: '*A list of the top 15 issues sorted by the most reactions over the last 90 days.*\n_Note: This :github2: <https://github.com/vercel/next.js/blob/canary/.github/workflows/popular.yml|workflow> → <https://github.com/vercel/next.js/blob/canary/.github/actions/next-repo-info/src/popular-issues.mjs|action> will run every Monday at 10AM UTC (6AM EST). These issues are automatically synced to Linear._',
-      },
-    },
-    {
-      type: 'divider',
-    },
-  ]
+// function generateBlocks(issues) {
+//   const blocks = [
+//     {
+//       type: 'section',
+//       text: {
+//         type: 'mrkdwn',
+//         text: '*A list of the top 15 issues sorted by the most reactions over the last 90 days.*\n_Note: This :github2: <https://github.com/vercel/next.js/blob/canary/.github/workflows/popular.yml|workflow> → <https://github.com/vercel/next.js/blob/canary/.github/actions/next-repo-info/src/popular-issues.mjs|action> will run every Monday at 10AM UTC (6AM EST). These issues are automatically synced to Linear._',
+//       },
+//     },
+//     {
+//       type: 'divider',
+//     },
+//   ]
 
-  let text = ''
+//   let text = ''
 
-  issues.forEach((issue, i) => {
-    text += `${i + 1}. [<${issue.html_url}|#${issue.number}>, ${
-      issue.reactions.total_count
-    } reactions, ${formattedDate(issue.created_at)}]: ${issue.title}\n`
-  })
+//   issues.forEach((issue, i) => {
+//     text += `${i + 1}. [<${issue.html_url}|#${issue.number}>, ${
+//       issue.reactions.total_count
+//     } reactions, ${formattedDate(issue.created_at)}]: ${issue.title}\n`
+//   })
 
-  blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: text,
-    },
-  })
+//   blocks.push({
+//     type: 'section',
+//     text: {
+//       type: 'mrkdwn',
+//       text: text,
+//     },
+//   })
 
-  return blocks
-}
+//   return blocks
+// }
 
 async function run() {
   try {
     if (!process.env.GITHUB_TOKEN) throw new TypeError('GITHUB_TOKEN not set')
-    if (!process.env.SLACK_TOKEN) throw new TypeError('SLACK_TOKEN not set')
+    // if (!process.env.SLACK_TOKEN) throw new TypeError('SLACK_TOKEN not set')
 
     const octoClient = getOctokit(process.env.GITHUB_TOKEN)
-    const slackClient = new WebClient(process.env.SLACK_TOKEN)
+    // const slackClient = new WebClient(process.env.SLACK_TOKEN)
 
     const { owner, repo } = context.repo
     const { data } = await octoClient.rest.search.issuesAndPullRequests({
       order: 'desc',
       per_page: 15,
-      q: `repo:${owner}/${repo} is:issue is:open created:>=${ninetyDaysAgo()}`,
-      sort: 'reactions',
+      q: `repo:${owner}/${repo} is:issue created:>=2024-07-08`,
     })
 
-    if (data.items.length > 0) {
-      data.items.forEach(async (item) => {
-        await octoClient.rest.issues.addLabels({
-          owner,
-          repo,
-          issue_number: item.number,
-          labels: ['linear: next'],
-        })
-      })
+    info(`issues = ${JSON.stringify(data.items)}`)
 
-      await slackClient.chat.postMessage({
-        blocks: generateBlocks(data.items),
-        channel: '#next-info',
-        icon_emoji: ':github:',
-        username: 'GitHub Notifier',
-      })
+    // if (data.items.length > 0) {
+    //   data.items.forEach(async (item) => {
+    //     await octoClient.rest.issues.addLabels({
+    //       owner,
+    //       repo,
+    //       issue_number: item.number,
+    //       labels: ['linear: next'],
+    //     })
+    //   })
 
-      info(`Posted to Slack!`)
-    } else {
-      info(`No popular issues`)
-    }
+    //   await slackClient.chat.postMessage({
+    //     blocks: generateBlocks(data.items),
+    //     channel: '#next-info',
+    //     icon_emoji: ':github:',
+    //     username: 'GitHub Notifier',
+    //   })
+
+    //   info(`Posted to Slack!`)
+    // } else {
+    //   info(`No popular issues`)
+    // }
   } catch (error) {
     setFailed(error)
   }
