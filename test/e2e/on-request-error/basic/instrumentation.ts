cffc96571983528979e6590b859b1ade3e417486
Jiachi Liu <inbox@huozhi.im>
2024-07-17 17:59:30 +0200

    Expose TS type for onRequestError

--- test/e2e/on-request-error/basic/instrumentation.ts
+++ test/e2e/on-request-error/basic/instrumentation.ts
@@ -1,8 +1,14 @@
-export function onRequestError(err, request, context) {
+import { type Instrumentation } from 'next'
+
+export const onRequestError: Instrumentation.onRequestError = (
+  err,
+  request,
+  context
+) => {
   fetch(`http://localhost:${process.env.PORT}/write-log`, {
     method: 'POST',
     body: JSON.stringify({
-      message: err.message,
+      message: (err as Error).message,
       request,
       context,
     }),
