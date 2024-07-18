b1642e69f4ce3ee08dc23255c3b1bd915c4323ea
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-07-18 09:07:05 +0900

    Update test refs

---
+++ packages/next-swc/crates/next-custom-transforms/tests/fixture/tla-iife/nested/output.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "foo", {
+    enumerable: true,
+    get: function() {
+        return foo;
+    }
+});
+const foo = async ()=>{
+    await Promise.resolve();
+};
