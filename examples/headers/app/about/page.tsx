843f2bf65fb395d2429c9d640cf61ad89432964d
Sam-Phillemon9493 <yphilemon9493@gmail.com>
2024-07-18 22:26:23 +0530

    updated the example of headers to utilize the App Router

--- examples/headers/app/about/page.tsx
+++ examples/headers/app/about/page.tsx
@@ -1,24 +1,22 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
-import styles from "../../styles.module.css";
-import Code from "../../components/Code";
+import styles from "/styles.module.css";
+import Code from "../_components/Code";
 
-export default function News() {
-  const { asPath } = useRouter();
-
+export default function About() {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h1>Path: {asPath}</h1>
+        <h1>Path: /about</h1>
         <hr className={styles.hr} />
         <p>
           The response contains a custom header{" "}
-          <Code>X-News-Custom-Header</Code> : <Code>news_header_value</Code>.
+          <Code>X-About-Custom-Header</Code> : <Code>about_header_value</Code>.
         </p>
         <p>
           To check the response headers of this page, open the Network tab
           inside your browser inspector.
         </p>
+
         <Link href="/">&larr; Back home</Link>
       </div>
     </div>
--- examples/headers/app/news/[...slug]/page.tsx
+++ examples/headers/app/news/[...slug]/page.tsx
@@ -1,22 +1,29 @@
+// import { useRouter } from "next/router";
 import Link from "next/link";
-import styles from "../styles.module.css";
-import Code from "../components/Code";
+import styles from "/styles.module.css";
+import Code from "../../_components/Code";
 
-export default function About() {
+type NewsProps = {
+  params: { slug: [] };
+};
+
+export default function News({ params }: NewsProps) {
+  console.log(params);
+  // const { asPath } = useRouter();
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h1>Path: /about</h1>
+        <h1>Path: {`/news/${params.slug.join("/")}`}</h1>
         <hr className={styles.hr} />
         <p>
           The response contains a custom header{" "}
-          <Code>X-About-Custom-Header</Code> : <Code>about_header_value</Code>.
+          <Code>X-News-Custom-Header</Code> : <Code>news_header_value</Code>.
         </p>
         <p>
           To check the response headers of this page, open the Network tab
           inside your browser inspector.
         </p>
-
         <Link href="/">&larr; Back home</Link>
       </div>
     </div>
