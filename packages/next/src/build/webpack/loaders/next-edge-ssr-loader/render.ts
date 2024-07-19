vercel/next.js

commit 7569c8087a7480334e02c789138fd67290097e9e

feat(log): improve dev/build logs (#62946)
Co-authored-by: Jiachi Liu <inbox@huozhi.im>

packages/next/src/build/webpack/loaders/next-edge-ssr-loader/render.ts
@@ -18,6 +18,7 @@ import { normalizeAppPath } from '../../../../shared/lib/router/utils/app-paths'
 import type { SizeLimit } from '../../../../../types'
 import { internal_getCurrentFunctionWaitUntil } from '../../../../server/web/internal-edge-wait-until'
 import type { PAGE_TYPES } from '../../../../lib/page-types'
+import type { NextRequestHint } from '../../../../server/web/adapter'
 
 export function getRender({
   dev,
@@ -151,23 +152,23 @@ export function getRender({
 
   const handler = server.getRequestHandler()
 
-  return async function render(request: Request, event: NextFetchEvent) {
+  return async function render(
+    request: NextRequestHint,
+    event?: NextFetchEvent
+  ) {
     const extendedReq = new WebNextRequest(request)
     const extendedRes = new WebNextResponse()
 
     handler(extendedReq, extendedRes)
     const result = await extendedRes.toResponse()
 
-    if (event && event.waitUntil) {
+    if (event?.waitUntil) {
       const waitUntilPromise = internal_getCurrentFunctionWaitUntil()
       if (waitUntilPromise) {
         event.waitUntil(waitUntilPromise)
       }
     }
 
-    // fetchMetrics is attached to the web request that going through the server,
-    // wait for the handler result is ready and attach it back to the original request.
-    ;(request as any).fetchMetrics = extendedReq.fetchMetrics
     return result
   }
 }

test/e2e/app-dir/logging/fetch-logging.test.ts
@@ -2,249 +2,251 @@ import path from 'path'
 import fs from 'fs'
 import stripAnsi from 'strip-ansi'
 import { retry } from 'next-test-utils'
-import { createNextDescribe } from 'e2e-utils'
+import { nextTestSetup } from 'e2e-utils'
+
+const cahceReasonRe = /Cache (missed|skipped) reason: /
+
+interface ParsedLog {
+  method: string
+  url: string
+  statusCode: number
+  responseTime: number
+  cache: string
+}
 
 function parseLogsFromCli(cliOutput: string) {
   const logs = stripAnsi(cliOutput)
     .split('\n')
-    .filter((log) => log.includes('Cache missed reason') || log.includes('GET'))
+    .filter((log) => cahceReasonRe.test(log) || log.includes('GET'))
 
-  return logs.reduce((parsedLogs, log) => {
-    if (log.includes('Cache missed reason')) {
-      // cache miss reason
-      const reasonSegment = log.split('Cache missed reason: ', 2)[1].trim()
+  return logs.reduce<ParsedLog[]>((parsedLogs, log) => {
+    if (cahceReasonRe.test(log)) {
+      // cache miss/skip reason
+      // Example of `log`: "│ │ Cache skipped reason: (cache: no-cache)"
+      const reasonSegment = log.split(cahceReasonRe, 3)[2].trim()
       const reason = reasonSegment.slice(1, -1)
       parsedLogs[parsedLogs.length - 1].cache = reason
     } else {
       // request info
       const trimmedLog = log.replace(/^[^a-zA-Z]+/, '')
-      const parts = trimmedLog.split(' ', 5)
-      const method = parts[0]
-      const url = parts[1]
-      const statusCode = parseInt(parts[2])
-      const responseTime = parseInt(parts[4])
+      const [method, url, statusCode, responseTime] = trimmedLog.split(' ', 5)
 
-      const parsedLog = {
+      parsedLogs.push({
         method,
         url,
-        statusCode,
-        responseTime,
+        statusCode: parseInt(statusCode, 10),
+        responseTime: parseInt(responseTime, 10),
         cache: undefined,
-      }
-      parsedLogs.push(parsedLog)
+      })
     }
     return parsedLogs
-  }, [] as any[])
+  }, [])
 }
 
-createNextDescribe(
-  'app-dir - logging',
-  {
+describe('app-dir - logging', () => {
+  const { next, isNextDev } = nextTestSetup({
     skipDeployment: true,
     files: __dirname,
-  },
-  ({ next, isNextDev }) => {
-    function runTests({
-      withFetchesLogging,
-      withFullUrlFetches = false,
-    }: {
-      withFetchesLogging: boolean
-      withFullUrlFetches?: boolean
-    }) {
-      if (withFetchesLogging) {
-        it('should only log requests in development mode', async () => {
-          const outputIndex = next.cliOutput.length
-          await next.fetch('/default-cache')
+  })
+  function runTests({
+    withFetchesLogging,
+    withFullUrlFetches = false,
+  }: {
+    withFetchesLogging: boolean
+    withFullUrlFetches?: boolean
+  }) {
+    if (withFetchesLogging) {
+      it('should only log requests in development mode', async () => {
+        const outputIndex = next.cliOutput.length
+        await next.fetch('/default-cache')
 
-          await retry(() => {
-            const logs = stripAnsi(next.cliOutput.slice(outputIndex))
-            const hasLogs = logs.includes('GET /default-cache 200')
-
-            expect(isNextDev ? hasLogs : !hasLogs).toBe(true)
-          })
-        })
-
-        if (isNextDev) {
-          it("should log 'skip' cache status with a reason when cache: 'no-cache' is used", async () => {
-            const outputIndex = next.cliOutput.length
-            await next.fetch('/default-cache')
-
-            await retry(() => {
-              const logs = parseLogsFromCli(next.cliOutput.slice(outputIndex))
-
-              const logEntry = logs.find((log) =>
-                log.url.includes('api/random?no-cache')
-              )
-
-              expect(logs.some((log) => log.url.includes('..'))).toBe(
-                !withFullUrlFetches
-              )
-
-              expect(logEntry?.cache).toBe('cache: no-cache')
-            })
-          })
-
-          it("should log 'skip' cache status with a reason when revalidate: 0 is used", async () => {
-            const outputIndex = next.cliOutput.length
-            await next.fetch('/default-cache')
-            await retry(() => {
-              const logs = parseLogsFromCli(next.cliOutput.slice(outputIndex))
-
-              const logEntry = logs.find((log) =>
-                log.url.includes('api/random?revalidate-0')
-              )
-
-              expect(logEntry?.cache).toBe('revalidate: 0')
-            })
-          })
-
-          it("should log 'skip' cache status with a reason when the browser indicates caching should be ignored", async () => {
-            const outputIndex = next.cliOutput.length
-            await next.fetch('/default-cache', {
-              headers: { 'Cache-Control': 'no-cache' },
-            })
-            await retry(() => {
-              const logs = parseLogsFromCli(next.cliOutput.slice(outputIndex))
-
-              const logEntry = logs.find((log) =>
-                log.url.includes('api/random?auto-cache')
-              )
-
-              expect(logEntry?.cache).toBe(
-                'cache-control: no-cache (hard refresh)'
-              )
-            })
-          })
-
-          it('should log requests with correct indentation', async () => {
-            const outputIndex = next.cliOutput.length
-            await next.fetch('/default-cache')
-
-            await retry(() => {
-              const logs = stripAnsi(next.cliOutput.slice(outputIndex))
-              const hasLogs =
-                logs.includes(' GET /default-cache') &&
-                logs.includes('  │ GET ') &&
-                logs.includes('  │  │ GET ') &&
-                logs.includes('  │  │  Cache missed reason')
-
-              expect(hasLogs).toBe(true)
-            })
-          })
-
-          it('should show cache reason of noStore when use with fetch', async () => {
-            const logLength = next.cliOutput.length
-            await next.fetch('/no-store')
-
-            await retry(() => {
-              const output = stripAnsi(next.cliOutput.slice(logLength))
-              expect(output).toContain('Cache missed reason: (noStore call)')
-            })
-          })
-
-          it('should respect request.init.cache when use with fetch input is instance', async () => {
-            const logLength = next.cliOutput.length
-            await next.fetch('/fetch-no-store')
-
-            await retry(() => {
-              const output = stripAnsi(next.cliOutput.slice(logLength))
-              expect(output).toContain('Cache missed reason: (cache: no-store)')
-            })
-          })
-        }
-      } else {
-        // No fetches logging enabled
-        it('should not log fetch requests at all', async () => {
-          const outputIndex = next.cliOutput.length
-          await next.fetch('/default-cache')
-
-          await retry(() => {
-            const logs = stripAnsi(next.cliOutput.slice(outputIndex))
+        await retry(() => {
+          const logs = stripAnsi(next.cliOutput.slice(outputIndex))
+          if (isNextDev) {
+            expect(logs).toContain('GET /default-cache 200')
+          } else {
             expect(logs).not.toContain('GET /default-cache 200')
-          })
+          }
         })
-      }
+      })
 
       if (isNextDev) {
-        it('should not contain trailing word page for app router routes', async () => {
-          const logLength = next.cliOutput.length
-          await next.fetch('/')
+        it("should log 'skip' cache status with a reason when cache: 'no-cache' is used", async () => {
+          const outputIndex = next.cliOutput.length
+          await next.fetch('/default-cache')
 
           await retry(() => {
-            const output = stripAnsi(next.cliOutput.slice(logLength))
-            expect(output).toContain('/')
-            expect(output).not.toContain('/page')
+            const logs = parseLogsFromCli(next.cliOutput.slice(outputIndex))
+
+            const logEntry = logs.find((log) =>
+              log.url.includes('api/random?no-cache')
+            )
+
+            expect(logs.some((log) => log.url.includes('..'))).toBe(
+              !withFullUrlFetches
+            )
+
+            expect(logEntry?.cache).toBe('cache: no-cache')
           })
         })
 
-        it('should not contain metadata internal segments for dynamic metadata routes', async () => {
+        it("should log 'skip' cache status with a reason when revalidate: 0 is used", async () => {
+          const outputIndex = next.cliOutput.length
+          await next.fetch('/default-cache')
+          await retry(() => {
+            const logs = parseLogsFromCli(next.cliOutput.slice(outputIndex))
+
+            const logEntry = logs.find((log) =>
+              log.url.includes('api/random?revalidate-0')
+            )
+
+            expect(logEntry?.cache).toBe('revalidate: 0')
+          })
+        })
+
+        it("should log 'skip' cache status with a reason when the browser indicates caching should be ignored", async () => {
+          const outputIndex = next.cliOutput.length
+          await next.fetch('/default-cache', {
+            headers: { 'Cache-Control': 'no-cache' },
+          })
+          await retry(() => {
+            const logs = parseLogsFromCli(next.cliOutput.slice(outputIndex))
+
+            const logEntry = logs.find((log) =>
+              log.url.includes('api/random?auto-cache')
+            )
+
+            expect(logEntry?.cache).toBe(
+              'cache-control: no-cache (hard refresh)'
+            )
+          })
+        })
+
+        it('should log requests with correct indentation', async () => {
+          const outputIndex = next.cliOutput.length
+          await next.fetch('/default-cache')
+
+          await retry(() => {
+            const logs = stripAnsi(next.cliOutput.slice(outputIndex))
+            expect(logs).toContain(' GET /default-cache')
+            expect(logs).toContain(' │ GET ')
+            expect(logs).toContain(' │ │ Cache skipped reason')
+            expect(logs).toContain(' │ │ GET ')
+          })
+        })
+
+        it('should show cache reason of noStore when use with fetch', async () => {
           const logLength = next.cliOutput.length
-          await next.fetch('/dynamic/big/icon')
+          await next.fetch('/no-store')
 
           await retry(() => {
             const output = stripAnsi(next.cliOutput.slice(logLength))
-            expect(output).toContain('/dynamic/[slug]/icon')
-            expect(output).not.toContain('/(group)')
-            expect(output).not.toContain('[[...__metadata_id__]]')
-            expect(output).not.toContain('/route')
+            expect(output).toContain('Cache skipped reason: (noStore call)')
+          })
+        })
+
+        it('should respect request.init.cache when use with fetch input is instance', async () => {
+          const logLength = next.cliOutput.length
+          await next.fetch('/fetch-no-store')
+
+          await retry(() => {
+            const output = stripAnsi(next.cliOutput.slice(logLength))
+            expect(output).toContain('Cache skipped reason: (cache: no-store)')
           })
         })
       }
+    } else {
+      // No fetches logging enabled
+      it('should not log fetch requests at all', async () => {
+        const outputIndex = next.cliOutput.length
+        await next.fetch('/default-cache')
+
+        await retry(() => {
+          const logs = stripAnsi(next.cliOutput.slice(outputIndex))
+          expect(logs).not.toContain('GET /default-cache 200')
+        })
+      })
     }
 
-    describe('with fetches verbose logging', () => {
-      runTests({ withFetchesLogging: true, withFullUrlFetches: true })
-    })
+    if (isNextDev) {
+      it('should not contain trailing word page for app router routes', async () => {
+        const logLength = next.cliOutput.length
+        await next.fetch('/')
 
-    describe('with fetches default logging', () => {
-      const curNextConfig = fs.readFileSync(
-        path.join(__dirname, 'next.config.js'),
-        { encoding: 'utf-8' }
-      )
-      beforeAll(async () => {
-        await next.stop()
-        await next.patchFile(
-          'next.config.js',
-          curNextConfig.replace('fullUrl: true', 'fullUrl: false')
-        )
-        await next.start()
-      })
-      afterAll(async () => {
-        await next.patchFile('next.config.js', curNextConfig)
+        await retry(() => {
+          const output = stripAnsi(next.cliOutput.slice(logLength))
+          expect(output).toContain('/')
+          expect(output).not.toContain('/page')
+        })
       })
 
-      runTests({ withFetchesLogging: true, withFullUrlFetches: false })
-    })
+      it('should not contain metadata internal segments for dynamic metadata routes', async () => {
+        const logLength = next.cliOutput.length
+        await next.fetch('/dynamic/big/icon')
 
-    describe('with verbose logging for edge runtime', () => {
-      beforeAll(async () => {
-        await next.stop()
-        const layoutContent = await next.readFile('app/layout.js')
-        await next.patchFile(
-          'app/layout.js',
-          layoutContent + `\nexport const runtime = 'edge'`
-        )
-        await next.start()
+        await retry(() => {
+          const output = stripAnsi(next.cliOutput.slice(logLength))
+          expect(output).toContain('/dynamic/[slug]/icon')
+          expect(output).not.toContain('/(group)')
+          expect(output).not.toContain('[[...__metadata_id__]]')
+          expect(output).not.toContain('/route')
+        })
       })
-
-      runTests({ withFetchesLogging: false })
-    })
-
-    describe('with default logging', () => {
-      const curNextConfig = fs.readFileSync(
-        path.join(__dirname, 'next.config.js'),
-        { encoding: 'utf-8' }
-      )
-      beforeAll(async () => {
-        await next.stop()
-        await next.deleteFile('next.config.js')
-        await next.start()
-      })
-      afterAll(async () => {
-        await next.patchFile('next.config.js', curNextConfig)
-      })
-
-      runTests({ withFetchesLogging: false })
-    })
+    }
   }
-)
+
+  describe('with fetches verbose logging', () => {
+    runTests({ withFetchesLogging: true, withFullUrlFetches: true })
+  })
+
+  describe('with fetches default logging', () => {
+    const curNextConfig = fs.readFileSync(
+      path.join(__dirname, 'next.config.js'),
+      { encoding: 'utf-8' }
+    )
+    beforeAll(async () => {
+      await next.stop()
+      await next.patchFile(
+        'next.config.js',
+        curNextConfig.replace('fullUrl: true', 'fullUrl: false')
+      )
+      await next.start()
+    })
+    afterAll(async () => {
+      await next.patchFile('next.config.js', curNextConfig)
+    })
+
+    runTests({ withFetchesLogging: true, withFullUrlFetches: false })
+  })
+
+  describe('with verbose logging for edge runtime', () => {
+    beforeAll(async () => {
+      await next.stop()
+      const layoutContent = await next.readFile('app/layout.js')
+      await next.patchFile(
+        'app/layout.js',
+        layoutContent + `\nexport const runtime = 'edge'`
+      )
+      await next.start()
+    })
+
+    runTests({ withFetchesLogging: false })
+  })
+
+  describe('with default logging', () => {
+    const curNextConfig = fs.readFileSync(
+      path.join(__dirname, 'next.config.js'),
+      { encoding: 'utf-8' }
+    )
+    beforeAll(async () => {
+      await next.stop()
+      await next.deleteFile('next.config.js')
+      await next.start()
+    })
+    afterAll(async () => {
+      await next.patchFile('next.config.js', curNextConfig)
+    })
+
+    runTests({ withFetchesLogging: false })
+  })
+})

packages/next/src/build/progress.ts
@@ -68,19 +68,16 @@ export const createProgress = (total: number, label: string) => {
     }
 
     const isFinished = curProgress === total
+    const message = `${label} (${curProgress}/${total})`
     if (progressSpinner && !isFinished) {
-      progressSpinner.setText(`${label} (${curProgress}/${total})`)
+      progressSpinner.setText(message)
     } else {
-      if (progressSpinner) {
-        progressSpinner.stop()
+      progressSpinner?.stop()
+      if (isFinished) {
+        Log.event(message)
+      } else {
+        Log.info(`${message} ${process.stdout.isTTY ? '\n' : '\r'}`)
       }
-      console.log(
-        ` ${
-          isFinished ? Log.prefixes.event : Log.prefixes.info
-        } ${label} (${curProgress}/${total}) ${
-          isFinished ? '' : process.stdout.isTTY ? '\n' : '\r'
-        }`
-      )
     }
   }
 }

packages/next/src/build/spinner.ts
@@ -47,18 +47,18 @@ export default function createSpinner(
       console.warn = origWarn
       console.error = origError
     }
-    spinner.setText = (newText: string) => {
+    spinner.setText = (newText) => {
       text = newText
       prefixText = ` ${Log.prefixes.info} ${newText} `
       spinner!.prefixText = prefixText
       return spinner!
     }
-    spinner.stop = (): ora.Ora => {
+    spinner.stop = () => {
       origStop()
       resetLog()
       return spinner!
     }
-    spinner.stopAndPersist = (): ora.Ora => {
+    spinner.stopAndPersist = () => {
       // Add \r at beginning to reset the current line of loading status text
       const suffixText = `\r ${Log.prefixes.event} ${text} `
       if (spinner) {

packages/next/src/build/utils.ts
@@ -481,7 +481,7 @@ export async function printTreeView(
       if (item === '/_app' || item === '/_app.server') {
         symbol = ' '
       } else if (isEdgeRuntime(pageInfo?.runtime)) {
-        symbol = 'ℇ'
+        symbol = 'ƒ'
       } else if (pageInfo?.isPPR) {
         if (
           // If the page has an empty prelude, then it's equivalent to a dynamic page
@@ -490,7 +490,7 @@ export async function printTreeView(
           // since in this case we're able to partially prerender it
           (pageInfo.isDynamicAppRoute && !pageInfo.hasPostponed)
         ) {
-          symbol = 'λ'
+          symbol = 'ƒ'
         } else if (!pageInfo?.hasPostponed) {
           symbol = '○'
         } else {
@@ -501,7 +501,7 @@ export async function printTreeView(
       } else if (pageInfo?.isSSG) {
         symbol = '●'
       } else {
-        symbol = 'λ'
+        symbol = 'ƒ'
       }
 
       usedSymbols.add(symbol)
@@ -749,16 +749,7 @@ export async function printTreeView(
           '(Partial Prerender)',
           'prerendered as static HTML with dynamic server-streamed content',
         ],
-        usedSymbols.has('λ') && [
-          'λ',
-          '(Dynamic)',
-          `server-rendered on demand using Node.js`,
-        ],
-        usedSymbols.has('ℇ') && [
-          'ℇ',
-          '(Edge Runtime)',
-          `server-rendered on demand using the Edge Runtime`,
-        ],
+        usedSymbols.has('ƒ') && ['ƒ', '(Dynamic)', `server-rendered on demand`],
       ].filter((x) => x) as [string, string, string][],
       {
         align: ['l', 'l', 'l'],

packages/next/src/server/async-storage/static-generation-async-storage-wrapper.ts
@@ -4,6 +4,7 @@ import type { AsyncLocalStorage } from 'async_hooks'
 import type { IncrementalCache } from '../lib/incremental-cache'
 
 import { createPrerenderState } from '../../server/app-render/dynamic-rendering'
+import type { FetchMetric } from '../base-http'
 
 export type StaticGenerationContext = {
   urlPathname: string
@@ -31,6 +32,8 @@ export type StaticGenerationContext = {
      */
     // TODO: remove this when we resolve accessing the store outside the execution context
     store?: StaticGenerationStore
+    /** Fetch metrics attached in patch-fetch.ts */
+    fetchMetrics?: FetchMetric[]
   }
 }
 

packages/next/src/server/base-http/index.ts
@@ -11,7 +11,7 @@ export interface BaseNextRequestConfig {
   trailingSlash?: boolean | undefined
 }
 
-export type FetchMetrics = Array<{
+export type FetchMetric = {
   url: string
   idx: number
   end: number
@@ -20,11 +20,14 @@ export type FetchMetrics = Array<{
   status: number
   cacheReason: string
   cacheStatus: 'hit' | 'miss' | 'skip'
-}>
+}
+
+export type FetchMetrics = Array<FetchMetric>
 
 export abstract class BaseNextRequest<Body = any> {
   protected _cookies: NextApiRequestCookies | undefined
   public abstract headers: IncomingHttpHeaders
+  public abstract fetchMetrics?: FetchMetric[]
 
   constructor(public method: string, public url: string, public body: Body) {}
 

packages/next/src/server/base-http/node.ts
@@ -7,16 +7,18 @@ import type { NextApiRequestCookies } from '../api-utils'
 import { NEXT_REQUEST_META } from '../request-meta'
 import type { RequestMeta } from '../request-meta'
 
-import { BaseNextRequest, BaseNextResponse } from './index'
+import { BaseNextRequest, BaseNextResponse, type FetchMetric } from './index'
 import type { OutgoingHttpHeaders } from 'node:http'
 
 type Req = IncomingMessage & {
   [NEXT_REQUEST_META]?: RequestMeta
   cookies?: NextApiRequestCookies
+  fetchMetrics?: FetchMetric[]
 }
 
 export class NodeNextRequest extends BaseNextRequest<Readable> {
-  public headers = this._req.headers;
+  public headers = this._req.headers
+  public fetchMetrics?: FetchMetric[] = this._req?.fetchMetrics;
 
   [NEXT_REQUEST_META]: RequestMeta = this._req[NEXT_REQUEST_META] || {}
 

commit dd59ef03479f77ee89c4269e26973233cd898bf1

Fix fetch logging in edge runtime
Fetch logging in edge runtime was originally implemented in #56108, and
then was accidentally broken in #62946.

This restores the previous behaviour, and also fixes the wrong test
config, which prevented use from catching the regression.

fixes #50439

packages/next/src/build/webpack/loaders/next-edge-ssr-loader/render.ts
@@ -162,6 +162,7 @@ export function getRender({
 
     handler(extendedReq, extendedRes)
     const result = await extendedRes.toResponse()
+    request.fetchMetrics = extendedReq.fetchMetrics
 
     if (event?.waitUntil) {
       // TODO(after):

test/e2e/app-dir/logging/fetch-logging.test.ts
@@ -267,7 +267,7 @@ describe('app-dir - logging', () => {
       await next.start()
     })
 
-    runTests({ withFetchesLogging: false })
+    runTests({ withFetchesLogging: true, withFullUrlFetches: true })
   })
 
   describe('with default logging', () => {