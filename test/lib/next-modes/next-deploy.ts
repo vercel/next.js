f0f84fb3bc1bbf69b67d9487c763a83fbac5428e
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-07-19 13:14:03 -0700

    fix retrieval of deploy test build logs (#67971)
    `vercel logs` now retrieves runtime logs in watch mode. This causes the
    `createNext` step to hang as it never resolves.
    
    These logs are intended to be build logs, which is available under
    `vercel inspect --logs`. I removed some arguments that are no longer
    valid for that CLI function.
    
    Separately, it looks like we're able to add runtime logs to
    `next.cliOutput`, which I'll do in a future PR.
    
    Related:
    - https://github.com/vercel/vercel/pull/11788
    
    [Failure
    example](https://github.com/vercel/next.js/actions/runs/10012728881/job/27679225925)
    
    ["Success"
    example](https://github.com/vercel/next.js/actions/runs/10013211758/job/27680965374?pr=67971)

--- test/lib/next-modes/next-deploy.ts
+++ test/lib/next-modes/next-deploy.ts
@@ -146,22 +146,24 @@ export class NextDeployInstance extends NextInstance {
 
     require('console').log(`Got buildId: ${this._buildId}`)
 
-    // Use the vercel logs command to get the CLI output from the build.
-    const logs = await execa(
+    // Use the vercel inspect command to get the CLI output from the build.
+    const buildLogs = await execa(
       'vercel',
-      ['logs', this._url, '--output', 'raw', ...vercelFlags],
+      ['inspect', '--logs', this._url, ...vercelFlags],
       {
         env: vercelEnv,
         reject: false,
       }
     )
-    if (logs.exitCode !== 0) {
-      throw new Error(`Failed to get build output logs: ${logs.stderr}`)
+    if (buildLogs.exitCode !== 0) {
+      throw new Error(`Failed to get build output logs: ${buildLogs.stderr}`)
     }
 
     // Use the stdout from the logs command as the CLI output. The CLI will
     // output other unrelated logs to stderr.
-    this._cliOutput = logs.stdout
+
+    // TODO: Combine with runtime logs (via `vercel logs`)
+    this._cliOutput = buildLogs.stdout
   }
 
   public get cliOutput() {
