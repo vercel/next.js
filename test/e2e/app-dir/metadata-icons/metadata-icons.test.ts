bc4548c6b0f92a2927970ada50cf6265093d7be5
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2024-07-25 01:54:46 +0200

    fix turbopack not adding favicon

--- test/e2e/app-dir/metadata-icons/metadata-icons.test.ts
+++ test/e2e/app-dir/metadata-icons/metadata-icons.test.ts
@@ -7,12 +7,12 @@ describe('app-dir - metadata-icons', () => {
 
   it('should only have 1 favicon link in root page', async () => {
     const $ = await next.render$('/')
-    expect($('link[href="/favicon.ico"]').length).toBe(1)
+    expect($('link[href^="/favicon.ico"]').length).toBe(1)
   })
 
   it('should only have 1 favicon link in nested page', async () => {
     const $ = await next.render$('/nested')
-    expect($('link[href="/favicon.ico"]').length).toBe(1)
+    expect($('link[href^="/favicon.ico"]').length).toBe(1)
   })
 
   it('should render custom icons along with favicon in root page', async () => {
