3652b6e79223597d3a970032a3f807c7bfddb18f
Alexander Lyon <arlyon@me.com>
2024-07-25 22:51:38 +0100

    add tests for webpackIgnore and turbopackIgnore

---
+++ turbopack/crates/turbopack-ecmascript/tests/analyzer/graph/turbopack-ignore/input.js
@@ -0,0 +1,4 @@
+const w_i = import(/* webpackIgnore: true */ "w_i");
+const t_i = import(/* turbopackIgnore: true */ "t_i");
+const w_r = require(/* webpackIgnore: true */ "w_r");
+const t_r = require(/* turbopackIgnore: true */ "t_r");
