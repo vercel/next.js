a0f83f8271a34cbd3fb531cffb7107aa8adb5e52
Sam Ko <sam@vercel.com>
2024-07-29 23:57:25 +0200

    chore(github): initialize triage_with_ai.yml workflow again (#68292)
    ## Why?
    
    There is no way to test opening an issue without clever hacks. So need
    to initialize again to see the payload object, which will be necessary
    when I set up a Tool Call → Zod Schema with the :ai-stonks: SDK.
    
    ---------
    
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- .github/actions/next-repo-actions/src/triage-issues-with-ai.ts
+++ .github/actions/next-repo-actions/src/triage-issues-with-ai.ts
@@ -1,7 +1,10 @@
 import { info } from '@actions/core'
+import { context } from '@actions/github'
 
 async function main() {
-  info('Triaging issues with AI!')
+  const issue = context.payload.issue
+  info(`Issue: ${issue}`)
+  info(`Issue: ${JSON.stringify(issue, null, 2)}`)
 }
 
 main()
