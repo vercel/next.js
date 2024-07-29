5e8bc775bb2700197569860747d30087bcc12900
samcx <sam@vercel.com>
2024-07-29 14:30:24 -0700

    chore(github): no choice but to merge again

--- .github/actions/next-repo-actions/src/triage-issues-with-ai.ts
+++ .github/actions/next-repo-actions/src/triage-issues-with-ai.ts
@@ -2,10 +2,8 @@ import { info } from '@actions/core'
 import { context } from '@actions/github'
 
 async function main() {
-  if (context.eventName === 'issues' && context.payload.action === 'opened') {
-    const issue = context.payload.issue
-    info(`Issue: ${JSON.stringify(issue)}`)
-  }
+  const issue = context.payload.issue
+  info(`Issue: ${JSON.stringify(issue)}`)
 }
 
 main()
