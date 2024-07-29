6d82cefef2ab22bbf6a1c13f5989c0e98bd04341
coderfin <coderfin@gmail.com>
2024-07-29 22:18:23 +0900

    Updates the manifest types to reflect the current specifications

--- packages/next/src/lib/metadata/types/manifest-types.ts
+++ packages/next/src/lib/metadata/types/manifest-types.ts
@@ -1,3 +1,17 @@
+type ClientModeEnum = 'auto' | 'focus-existing' | 'navigate-existing' | 'navigate-new'
+
+type File = {
+  name: string
+  accept: string | string[]
+}
+
+type Icon = {
+  src: string
+  type?: string
+  sizes?: string
+  purpose?: 'any' | 'maskable' | 'monochrome'
+}
+
 export type Manifest = {
   background_color?: string
   categories?: string[]
@@ -17,17 +31,11 @@ export type Manifest = {
       [mimeType: string]: string[]
     }[]
   }[]
-  icons?: {
-    src: string
-    type?: string
-    sizes?: string
-    purpose?: 'any' | 'maskable' | 'monochrome' | 'badge'
-  }[]
+  icons?: Icon[]
   id?: string
   lang?: string
   launch_handler?: {
-    platform?: 'windows' | 'macos' | 'linux'
-    url?: string
+    client_mode?: ClientModeEnum | ClientModeEnum[]
   }
   name?: string
   orientation?:
@@ -43,7 +51,6 @@ export type Manifest = {
   protocol_handlers?: {
     protocol: string
     url: string
-    title?: string
   }[]
   related_applications?: {
     platform: string
@@ -52,6 +59,22 @@ export type Manifest = {
   }[]
   scope?: string
   screenshots?: {
+    form_factor?: 'narrow' | 'wide'
+    label?: string
+    platform?: 
+      | 'android'
+      | 'chromeos'
+      | 'ipados'
+      | 'ios'
+      | 'kaios'
+      | 'macos'
+      | 'windows'
+      | 'xbox'
+      | 'chrome_web_store'
+      | 'itunes'
+      | 'microsoft-inbox'
+      | 'microsoft-store'
+      | 'play'
     src: string
     type?: string
     sizes?: string
@@ -70,16 +93,10 @@ export type Manifest = {
       | 'multipart/form-data'
       | 'text/plain'
     params?: {
-      name: string
-      value: string
-      required?: boolean
-    }[]
-    url?: string
-    title?: string
-    text?: string
-    files?: {
-      accept?: string[]
-      name?: string
+      title?: string
+      text?: string
+      url?: string
+      files?: File | File[]
     }[]
   }
   short_name?: string
@@ -88,12 +105,7 @@ export type Manifest = {
     short_name?: string
     description?: string
     url: string
-    icons?: {
-      src: string
-      type?: string
-      sizes?: string
-      purpose?: 'any' | 'maskable' | 'monochrome' | 'badge'
-    }[]
+    icons?: Icon[]
   }[]
   start_url?: string
   theme_color?: string
