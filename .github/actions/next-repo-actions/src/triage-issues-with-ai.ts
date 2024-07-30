6be2c8961c59230e46b958e8c4a95e90768901f7
samcx <sam@vercel.com>
2024-07-30 01:27:04 -0700

    chore(github): update triage-issues-with-ai workflow

--- .github/actions/next-repo-actions/src/triage-issues-with-ai.ts
+++ .github/actions/next-repo-actions/src/triage-issues-with-ai.ts
@@ -1,16 +1,20 @@
+import { WebClient } from '@slack/web-api'
 import * as path from 'node:path'
 import { readFileSync } from 'node:fs'
 import { setFailed } from '@actions/core'
 import { context } from '@actions/github'
 import { generateText, tool } from 'ai'
 import { openai } from '@ai-sdk/openai'
+import { BlockCollection, Section } from 'slack-block-builder'
 
 import { getLatestCanaryVersion, getLatestVersion } from '../lib/util.mjs'
 import { issueSchema } from '../lib/types'
 
 async function main() {
-  if (!process.env.SLACK_TOKEN) throw new TypeError('SLACK_TOKEN not set')
   if (!process.env.OPENAI_API_KEY) throw new TypeError('OPENAI_API_KEY not set')
+  if (!process.env.SLACK_TOKEN) throw new TypeError('SLACK_TOKEN not set')
+
+  const slackClient = new WebClient(process.env.SLACK_TOKEN)
 
   const issue = context.payload.issue
   const filePath = path.join(
@@ -34,13 +38,28 @@ async function main() {
           description:
             'Report to Slack if a GitHub issue is severe enough to flag.',
           parameters: issueSchema,
-          execute: async () => ({
-            asSevere: true,
+          execute: async ({ issue }) => ({
+            number: issue.number,
           }),
         }),
       },
       prompt: `${JSON.stringify(issue)}\n${fileText}\nlatestVersion: ${latestVersion}\nlatestCanaryVersion: ${latestCanaryVersion}\nith the above GitHub issue (JSON) and the triage guidelines for determining whether an issue is severe, can you determine whether the given issue is severe enough to flag on Slack? If severe enough, report to Slack with an approximately 300 character summary of why you think it is severe enough to flag on Slack. If not, do not report on Slack.`,
     })
+
+    if (result.roundtrips.length > 1) {
+      const blocks = BlockCollection([
+        Section({
+          text: `We just flagged *${result.roundtrips.length}* issues.`,
+        }),
+      ])
+
+      await slackClient.chat.postMessage({
+        blocks,
+        channel: '#next-info',
+        icon_emoji: ':github:',
+        username: 'GitHub Notifier',
+      })
+    }
   } catch (error) {
     setFailed(error)
   }
