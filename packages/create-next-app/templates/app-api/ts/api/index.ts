9160ea444789f165fb720cdb25741bba8248cdb3
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-07-30 10:47:22 +0000

    add params to ts example

--- packages/create-next-app/templates/app-api/ts/api/index.ts
+++ packages/create-next-app/templates/app-api/ts/api/index.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (req: VercelRequest, res: VercelResponse) => {
   return res.json({ message: "Hello World" });
--- packages/create-next-app/templates/app-api/ts/api/route.ts
+++ packages/create-next-app/templates/app-api/ts/api/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   return NextResponse.json(
