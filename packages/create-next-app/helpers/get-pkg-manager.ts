9927b66e9a3ba850241e7f69a36fb66d974f3344
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-01 00:17:34 +0900

    refactor(cna): improve getting package manager

--- packages/create-next-app/helpers/get-pkg-manager.ts
+++ packages/create-next-app/helpers/get-pkg-manager.ts
@@ -1,18 +1,31 @@
+import { existsSync } from 'fs'
+import { join } from 'path'
+
 export type PackageManager = 'npm' | 'pnpm' | 'yarn' | 'bun'
 
-export function getPkgManager(): PackageManager {
-  const userAgent = process.env.npm_config_user_agent || ''
-
-  if (userAgent.startsWith('yarn')) {
-    return 'yarn'
+export function getPkgManager(baseDir: string): PackageManager {
+  const userAgent = process.env.npm_config_user_agent
+  if (userAgent) {
+    if (userAgent.startsWith('yarn')) {
+      return 'yarn'
+    }
+    if (userAgent.startsWith('pnpm')) {
+      return 'pnpm'
+    }
+    if (userAgent.startsWith('bun')) {
+      return 'bun'
+    }
   }
 
-  if (userAgent.startsWith('pnpm')) {
-    return 'pnpm'
-  }
-
-  if (userAgent.startsWith('bun')) {
-    return 'bun'
+  for (const { lockFile, packageManager } of [
+    { lockFile: 'package-lock.json', packageManager: 'npm' },
+    { lockFile: 'yarn.lock', packageManager: 'yarn' },
+    { lockFile: 'pnpm-lock.yaml', packageManager: 'pnpm' },
+    { lockFile: 'bun.lock', packageManager: 'bun' },
+  ] as const) {
+    if (existsSync(join(baseDir, lockFile))) {
+      return packageManager
+    }
   }
 
   return 'npm'
