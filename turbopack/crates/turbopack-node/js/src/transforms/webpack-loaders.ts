514fad5cff89f659762aef170f77c9e642cc6ce6
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-08-01 12:04:30 +0200

    Support Buffer results from Webpack loaders

--- turbopack/crates/turbopack-node/js/src/transforms/webpack-loaders.ts
+++ turbopack/crates/turbopack-node/js/src/transforms/webpack-loaders.ts
@@ -468,7 +468,7 @@ const transform = (
         if (!result.result) return reject(new Error("No result from loaders"));
         const [source, map] = result.result;
         resolve({
-          source,
+          source: Buffer.isBuffer(source) ? source.toString("utf8") : source,
           map:
             typeof map === "string"
               ? map
