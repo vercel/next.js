a150dfc351556b1803705d7ffade2f0831a850a2
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-03 02:24:20 +0900

    remove unnecessary api/ [no ci]

--- packages/create-next-app/templates/app-api/js/api/[param]/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET(request, context) {
-  const { params } = context;
-  const { param } = params; // dynamic router
-
-  return new Response(JSON.stringify({ message: `Hello, ${param}` }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
