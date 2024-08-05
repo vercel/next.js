22eddd175902f08b692a5994f6e3bb396b0730c9
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-08-05 13:06:49 -0400

    Add `local_cells` argument to `turbo_tasks::function` macro
    And store that information in the native function object.

--- turbopack/crates/turbo-tasks-macros/src/value_trait_macro.rs
+++ turbopack/crates/turbo-tasks-macros/src/value_trait_macro.rs
@@ -120,6 +120,11 @@ pub fn value_trait(args: TokenStream, input: TokenStream) -> TokenStream {
                 &format!("{trait_ident}::{ident}"),
                 &inline_function_path,
                 turbo_fn.is_method(),
+                // `inline_cells` is currently unsupported here because:
+                // - The `#[turbo_tasks::function]` macro needs to be present for us to read this
+                //   argument.
+                // - This only makes sense when a default implementation is present.
+                false,
             );
 
             let native_function_ident = get_trait_default_impl_function_ident(trait_ident, ident);
