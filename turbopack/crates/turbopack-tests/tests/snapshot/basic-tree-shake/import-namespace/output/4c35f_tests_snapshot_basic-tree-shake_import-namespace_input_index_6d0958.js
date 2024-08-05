d7a9a1ba766b47d31710bddcda7b44a2f5aafa3d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-06 08:40:56 +0900

    Revert "Update test refs"
    This reverts commit d5fb018e304b548b7bbc1e464c08f1b895a4d04a.

--- turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-namespace/output/4c35f_tests_snapshot_basic-tree-shake_import-namespace_input_index_6d0958.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/4c35f_tests_snapshot_basic-tree-shake_import-namespace_input_index_6d0958.js",
-    {},
-    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_basic-tree-shake_import-namespace_input_ab57e2._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-namespace/input/index.js [test] (ecmascript) <facade>"]}
-]);
-// Dummy runtime
--- turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-side-effect/output/4c35f_tests_snapshot_basic-tree-shake_import-side-effect_input_index_ef3f12.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/4c35f_tests_snapshot_basic-tree-shake_import-side-effect_input_index_ef3f12.js",
-    {},
-    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_basic-tree-shake_import-side-effect_input_98e37c._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-side-effect/input/index.js [test] (ecmascript) <facade>"]}
-]);
-// Dummy runtime
