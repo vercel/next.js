0c96f11888fd37c0e807f72c1518e05e266c9f27
eps1lon <sebastian.silbermann@vercel.com>
2024-08-06 20:18:46 +0200

    Update React from `06d0b89e-20240801` to `187dd6a7-20240806`
    **breaking change for canary users**: Bumps peer dependency of React from `19.0.0-rc-06d0b89e-20240801` to `19.0.0-rc-187dd6a7-20240806``

--- packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server.browser.production.js
+++ packages/next/src/compiled/react-dom-experimental/cjs/react-dom-server.browser.production.js
@@ -6468,12 +6468,12 @@ function getPostponedState(request) {
 }
 function ensureCorrectIsomorphicReactVersion() {
   var isomorphicReactPackageVersion = React.version;
-  if ("19.0.0-experimental-06d0b89e-20240801" !== isomorphicReactPackageVersion)
+  if ("19.0.0-experimental-187dd6a7-20240806" !== isomorphicReactPackageVersion)
     throw Error(
       formatProdErrorMessage(
         527,
         isomorphicReactPackageVersion,
-        "19.0.0-experimental-06d0b89e-20240801"
+        "19.0.0-experimental-187dd6a7-20240806"
       )
     );
 }
@@ -6678,4 +6678,4 @@ exports.resume = function (children, postponedState, options) {
     startWork(request);
   });
 };
-exports.version = "19.0.0-experimental-06d0b89e-20240801";
+exports.version = "19.0.0-experimental-187dd6a7-20240806";
