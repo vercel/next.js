b4f1eb271620731d1bec67bbe1d94625c07b61cf
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-08-07 13:53:17 +0200

    skipDeployment

--- test/e2e/app-dir/self-importing-package/self-importing-package.test.ts
+++ test/e2e/app-dir/self-importing-package/self-importing-package.test.ts
@@ -4,6 +4,9 @@ import path from 'path'
 describe('self-importing-package', () => {
   const { next } = nextTestSetup({
     files: __dirname,
+    // This test is skipped when deployed because the local tarball appears corrupted
+    // It also doesn't seem particularly useful to test when deployed
+    skipDeployment: true,
     dependencies: {
       'internal-pkg': `file:${path.join(__dirname, 'internal-pkg.tar')}`,
     },
