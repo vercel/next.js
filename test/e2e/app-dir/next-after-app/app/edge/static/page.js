524345c1004e10f65a276e44a10133d31ef57d89
Janka Uryga <lolzatu2@gmail.com>
2024-08-07 12:34:57 +0200

    test: duplicate pages for edge instead of using next.patch

---
+++ test/e2e/app-dir/next-after-app/app/edge/static/page.js
@@ -0,0 +1,4 @@
+export { default } from '../../nodejs/static/page'
+
+// (patched in tests)
+// export const dynamic = 'REPLACE_ME'
---
+++ test/e2e/app-dir/next-after-app/app/nodejs/invalid-in-client/page.js
@@ -0,0 +1,14 @@
+// (replaced in tests)
+// 'use client'
+
+import { unstable_after as after } from 'next/server'
+import { cliLog } from '../../../utils/log'
+
+export const dynamic = 'force-dynamic'
+
+export default function Page() {
+  after(() => {
+    cliLog({ source: '[page] /invalid-in-client' })
+  })
+  return <div>Invalid: Client page with after()</div>
+}
