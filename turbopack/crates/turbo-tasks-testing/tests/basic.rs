a81a19d343139c6be96be7dd642cddb1974a03d2
Tobias Koppers <tobias.koppers@googlemail.com>
2024-08-07 11:59:40 +0200

    very basic operation working

---
+++ turbopack/crates/turbo-tasks-testing/tests/basic.rs
@@ -0,0 +1,31 @@
+#![feature(arbitrary_self_types)]
+
+use anyhow::Result;
+use turbo_tasks::Vc;
+use turbo_tasks_testing::{register, run, Registration};
+
+static REGISTRATION: Registration = register!();
+
+#[tokio::test]
+async fn basic() {
+    run(&REGISTRATION, || async {
+        // let input = Value { value: 42 }.cell();
+        // let output = func(input);
+        // assert_eq!(output.await?.value, 42);
+
+        anyhow::Ok(())
+    })
+    .await
+    .unwrap()
+}
+
+#[turbo_tasks::value]
+struct Value {
+    value: u32,
+}
+
+#[turbo_tasks::function]
+async fn func(input: Vc<Value>) -> Result<Vc<Value>> {
+    let value = input.await?.value;
+    Ok(Value { value }.cell())
+}
