66149a237cfc3422c2469d22b749da850d5d4d93
Develliot <elliotrevan@gmail.com>
2024-08-08 16:55:17 +0100

    Update gtm.tsx

--- packages/third-parties/src/google/gtm.tsx
+++ packages/third-parties/src/google/gtm.tsx
@@ -54,7 +54,7 @@ export function GoogleTagManager(props: GTMParams) {
 export const sendGTMEvent = (data: Object, dataLayerName?: string) => {
   // special case if we are sending events before GTM init and we have custom dataLayerName 
   const dataLayer = dataLayerName || currDataLayerName
-  // define dataLayer so we can still queue up events before GMT init
+  // define dataLayer so we can still queue up events before GTM init
   window[dataLayer] = window[dataLayer] || []
   window[dataLayer].push(data)
 }
