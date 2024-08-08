9a770f8fc02e867b7ce46d7194e75d69f2995163
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-08 12:11:48 +0900

    test: update to use createNext
    Co-authored-by: JJ Kasper <jj@jjsweb.site>

--- test/development/app-dir/hmr-asset-prefix-full-url/asset-prefix.test.ts
+++ test/development/app-dir/hmr-asset-prefix-full-url/asset-prefix.test.ts
@@ -1,20 +1,22 @@
-import { nextTestSetup } from 'e2e-utils'
-import { retry } from 'next-test-utils'
-
-// TODO: findPort not working anyway how
-const forcedPort = String(Math.floor(10000 + Math.random() * 90000))
+import { createNext } from 'e2e-utils'
+import { findPort, retry } from 'next-test-utils'
 
 describe('app-dir assetPrefix full URL', () => {
-  const { next } = nextTestSetup({
-    files: __dirname,
-    forcedPort,
-    skipDeployment: true,
-    nextConfig: {
-      assetPrefix: `http://localhost:${forcedPort}`,
-    },
-  })
+  let next, forcedPort
+  beforeAll(async () => {
+    forcedPort = ((await findPort()) ?? '54321').toString()
 
-  it('should not break when renaming a folder', async () => {
+    next = await createNext({
+      files: __dirname,
+      forcedPort,
+      nextConfig: {
+        assetPrefix: `http://localhost:${forcedPort}`,
+      },
+    })
+  })
+  afterAll(() => next.destroy())
+
+  it('should not break HMR when asset prefix set to full URL', async () => {
     const browser = await next.browser('/')
     const text = await browser.elementByCss('p').text()
     expect(text).toBe('before edit')
