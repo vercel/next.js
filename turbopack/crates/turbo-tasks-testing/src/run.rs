6054e0a750276b8d0bcd13bf5f674da1512a20e8
Tobias Koppers <tobias.koppers@googlemail.com>
2024-08-09 10:50:05 +0200

    pass test name to test_config

--- turbopack/crates/turbo-tasks-testing/src/run.rs
+++ turbopack/crates/turbo-tasks-testing/src/run.rs
@@ -10,12 +10,15 @@ use turbo_tasks::{run_once, trace::TraceRawVcs, TurboTasksApi};
 pub struct Registration {
     execution_lock: OnceLock<()>,
     func: fn(),
-    create_turbo_tasks: fn() -> Arc<dyn TurboTasksApi>,
+    create_turbo_tasks: fn(&str, bool) -> Arc<dyn TurboTasksApi>,
 }
 
 impl Registration {
     #[doc(hidden)]
-    pub const fn new(create_turbo_tasks: fn() -> Arc<dyn TurboTasksApi>, func: fn()) -> Self {
+    pub const fn new(
+        create_turbo_tasks: fn(&str, bool) -> Arc<dyn TurboTasksApi>,
+        func: fn(),
+    ) -> Self {
         Registration {
             execution_lock: OnceLock::new(),
             func,
@@ -30,8 +33,8 @@ impl Registration {
         self.execution_lock.get_or_init(self.func);
     }
 
-    pub fn create_turbo_tasks(&self) -> Arc<dyn TurboTasksApi> {
-        (self.create_turbo_tasks)()
+    pub fn create_turbo_tasks(&self, name: &str, initial: bool) -> Arc<dyn TurboTasksApi> {
+        (self.create_turbo_tasks)(name, initial)
     }
 }
 
@@ -40,11 +43,12 @@ macro_rules! register {
     ($($other_register_fns:expr),* $(,)?) => {{
         use turbo_tasks::TurboTasksApi;
         use std::sync::Arc;
-        fn create_turbo_tasks() -> Arc<dyn TurboTasksApi> {
-            include!(concat!(
+        fn create_turbo_tasks(name: &str, initial: bool) -> Arc<dyn TurboTasksApi> {
+            let inner = include!(concat!(
                 env!("CARGO_MANIFEST_DIR"),
                 "/tests/test_config.trs"
-            ))
+            ));
+            (inner)(name, initial)
         }
         fn register_impl() {
             $($other_register_fns();)*
@@ -68,8 +72,14 @@ where
     T: TraceRawVcs + Send + 'static,
 {
     registration.ensure_registered();
-    let tt = registration.create_turbo_tasks();
+    let name = closure_to_name(&fut);
+    let tt = registration.create_turbo_tasks(&name, true);
     run_once(tt, async move { Ok(fut.await) }).await.unwrap()
+}
+
+fn closure_to_name<T>(value: &T) -> String {
+    let name = std::any::type_name_of_val(value);
+    name.replace("::{{closure}}", "").replace("::", "_")
 }
 
 pub async fn run<T, F>(
@@ -81,12 +91,17 @@ where
     T: Debug + PartialEq + Eq + TraceRawVcs + Send + 'static,
 {
     registration.ensure_registered();
-    let tt = registration.create_turbo_tasks();
+
+    let name = closure_to_name(&fut);
+    let tt = registration.create_turbo_tasks(&name, true);
+    println!("Run #1 (without cache)");
     let first = run_once(tt.clone(), fut()).await?;
+    println!("Run #2 (with memory cache, same TurboTasks instance)");
     let second = run_once(tt, fut()).await?;
     assert_eq!(first, second);
-    let tt = registration.create_turbo_tasks();
-    let third = run_once(tt, fut()).await?;
+    let tt = registration.create_turbo_tasks(&name, false);
+    println!("Run #3 (with persistent cache if available, new TurboTasks instance)");
+    let third = run_once(tt.clone(), fut()).await?;
     assert_eq!(first, third);
     Ok(())
 }
