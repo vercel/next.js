a2ad391a9e8bc466d56ea0536e2a82477c63d735
Tobias Koppers <tobias.koppers@googlemail.com>
2024-08-13 07:18:26 +0200

    initial aggregation update

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/connect_child.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/connect_child.rs
@@ -1,11 +1,18 @@
 use serde::{Deserialize, Serialize};
 use turbo_tasks::TaskId;
 
-use super::{ExecuteContext, Operation};
-use crate::data::CachedDataItem;
+use super::{
+    aggregation_update::{AggregationUpdateJob, AggregationUpdateQueue},
+    ExecuteContext, Operation,
+};
+use crate::data::{CachedDataItem, CachedDataItemKey};
 
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub enum ConnectChildOperation {
+    UpdateAggregation {
+        task_id: TaskId,
+        aggregation_update: AggregationUpdateQueue,
+    },
     ScheduleTask {
         task_id: TaskId,
     },
@@ -21,10 +28,23 @@ impl ConnectChildOperation {
             task: child_task,
             value: (),
         }) {
-            // TODO add aggregated edge
-            // TODO check for active
-            ConnectChildOperation::ScheduleTask {
+            // Update the task aggregation
+            let upper_ids = parent_task
+                .iter()
+                .filter_map(|(key, _)| match *key {
+                    CachedDataItemKey::Upper { task } => Some(task),
+                    _ => None,
+                })
+                .collect::<Vec<_>>();
+            let mut queue = AggregationUpdateQueue::new();
+            queue.push(AggregationUpdateJob::InnerHasNewFollower {
+                upper_ids,
+                new_follower_id: child_task,
+                new_follower_data: (),
+            });
+            ConnectChildOperation::UpdateAggregation {
                 task_id: child_task,
+                aggregation_update: queue,
             }
             .execute(&ctx);
         }
@@ -36,6 +56,15 @@ impl Operation for ConnectChildOperation {
         loop {
             ctx.operation_suspend_point(&self);
             match self {
+                ConnectChildOperation::UpdateAggregation {
+                    task_id,
+                    ref mut aggregation_update,
+                } => {
+                    if aggregation_update.process(ctx) {
+                        // TODO check for active
+                        self = ConnectChildOperation::ScheduleTask { task_id }
+                    }
+                }
                 ConnectChildOperation::ScheduleTask { task_id } => {
                     {
                         let mut task = ctx.task(task_id);
@@ -44,8 +73,8 @@ impl Operation for ConnectChildOperation {
                     ctx.schedule(task_id);
 
                     self = ConnectChildOperation::Done;
-                    continue;
                 }
+
                 ConnectChildOperation::Done => {
                     return;
                 }
