b071d570358e4c93862f0adefc84d6588db93c2d
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-08-14 15:51:48 +0100

    fix error message for sign in

--- examples/with-supabase/app/login/page.tsx
+++ examples/with-supabase/app/login/page.tsx
@@ -45,7 +45,7 @@ export default function Login({
     });
 
     if (error) {
-      return redirect("/login?message=Could not authenticate user");
+      return redirect("/login?message=Could not sign up user");
     }
 
     return redirect("/login?message=Check email to continue sign in process");
