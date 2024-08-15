b465229dde23d4d2f63838aa86a9da3d35819295
Lee Robinson <me@leerob.io>
2024-08-15 10:54:19 -0500

    Update cache-handler.js

--- examples/cache-handler-redis/cache-handler.js
+++ examples/cache-handler-redis/cache-handler.js
@@ -20,7 +20,7 @@ CacheHandler.onCreation(async () => {
       // because it will prevent reconnection after a socket exception.
       client.on("error", (e) => {
         if (typeof process.env.NEXT_PRIVATE_DEBUG_CACHE !== 'undefined') {
-            console.warn("Redis error", e);
+          console.warn("Redis error", e);
         }
       });
     } catch (error) {
