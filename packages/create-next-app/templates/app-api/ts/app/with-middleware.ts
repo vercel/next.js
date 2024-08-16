d6a48aad4ee79f258becb1d33c45ebe071200e33
Arindam Majumder <109217591+Arindam200@users.noreply.github.com>
2024-08-16 13:00:59 +0530

    add `type` NextRequest

--- packages/create-next-app/templates/app-api/ts/app/with-middleware.ts
+++ packages/create-next-app/templates/app-api/ts/app/with-middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 type RouteHandler<T = Record<string, string | string[]>> = (
   req: NextRequest,
