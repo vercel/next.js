1ff313591c5c76570f01e0b506cfafb1b113f165
devjiwonchoi <devjiwonchoi@gmail.com>
2024-08-17 12:16:02 +0900

    test: check if react, react-dom does not exist

--- test/e2e/app-dir/no-react/no-react.test.ts
+++ test/e2e/app-dir/no-react/no-react.test.ts
@@ -6,6 +6,13 @@ describe('no-react', () => {
     installCommand: 'pnpm remove react react-dom',
   })
 
+  it('should not have react or react-dom in the package.json', async () => {
+    const packageJson = await next.readJSON('package.json')
+    expect(packageJson.dependencies).toHaveProperty('next')
+    expect(packageJson.dependencies).not.toHaveProperty('react')
+    expect(packageJson.dependencies).not.toHaveProperty('react-dom')
+  })
+
   it('should work using cheerio without react, react-dom', async () => {
     const $ = await next.render$('/')
     expect($('p').text()).toBe('hello world')
