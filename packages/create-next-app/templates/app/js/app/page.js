f6003310269dfd32ca3c3cbbbe9d3e11e3915e63
Sam Ko <sam@vercel.com>
2024-08-19 12:04:18 -0700

    chore(cna): add missing period (#69021)
    ## Why?
    
    I noticed the first `<li>` line is missing a period at the end.
    
    ![CleanShot 2024-08-17 at 16 04 24@2x](https://github.com/user-attachments/assets/714de280-ce1e-47b2-b7c0-2fc85264a5ad)

--- packages/create-next-app/templates/app/js/app/page.js
+++ packages/create-next-app/templates/app/js/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.js</code>
+            Get started by editing <code>app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
--- packages/create-next-app/templates/default-tw/ts/pages/index.tsx
+++ packages/create-next-app/templates/default-tw/ts/pages/index.tsx
@@ -32,6 +32,7 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               pages/index.tsx
             </code>
+            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
