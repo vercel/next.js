22e24cf73b62d8e2db3fa8e613f6aec11d8787a7
Sam-Phillemon9493 <yphilemon9493@gmail.com>
2024-08-22 10:39:30 +0530

    Addressed PR review feedback

--- examples/custom-routes-proxying/app/hello/[slug]/page.tsx
+++ examples/custom-routes-proxying/app/hello/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function About({ params }) {
+export default function About({ params }: { params: { slug: string } }) {
   return (
     <div>
       <h3>This is the `hello/[slug]` page. slug: {params.slug} </h3>
