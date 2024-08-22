60be1500ca33c8d51664c4f0ef17082aa92ea356
Strift <laurent@meilisearch.com>
2024-08-22 16:17:22 +0200

    format with prettier

--- examples/with-meilisearch/tailwind.config.ts
+++ examples/with-meilisearch/tailwind.config.ts
@@ -1,17 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)'],
-        title: ['var(--font-poppins)'],
+        sans: ["var(--font-inter)"],
+        title: ["var(--font-poppins)"],
       },
       colors: {
-        primary: '#FF5CAA',
+        primary: "#FF5CAA",
       },
     },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
