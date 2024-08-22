cc1c2a34cc93a815f9214986fe7f43dd9679c383
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-22 16:21:23 +0900

    nodejs modules

--- crates/next-custom-transforms/src/transforms/warn_for_edge_runtime.rs
+++ crates/next-custom-transforms/src/transforms/warn_for_edge_runtime.rs
@@ -44,11 +44,82 @@ const EDGE_UNSUPPORTED_NODE_APIS: &[&str] = &[
     "WritableStreamDefaultController",
 ];
 
-const NODEJS_MODULE_NAMES: &[&str] = &[];
+/// Get this value from `require('module').builtinModules`
+const NODEJS_MODULE_NAMES: &[&str] = &[
+    "_http_agent",
+    "_http_client",
+    "_http_common",
+    "_http_incoming",
+    "_http_outgoing",
+    "_http_server",
+    "_stream_duplex",
+    "_stream_passthrough",
+    "_stream_readable",
+    "_stream_transform",
+    "_stream_wrap",
+    "_stream_writable",
+    "_tls_common",
+    "_tls_wrap",
+    "assert",
+    "assert/strict",
+    "async_hooks",
+    "buffer",
+    "child_process",
+    "cluster",
+    "console",
+    "constants",
+    "crypto",
+    "dgram",
+    "diagnostics_channel",
+    "dns",
+    "dns/promises",
+    "domain",
+    "events",
+    "fs",
+    "fs/promises",
+    "http",
+    "http2",
+    "https",
+    "inspector",
+    "module",
+    "net",
+    "os",
+    "path",
+    "path/posix",
+    "path/win32",
+    "perf_hooks",
+    "process",
+    "punycode",
+    "querystring",
+    "readline",
+    "readline/promises",
+    "repl",
+    "stream",
+    "stream/consumers",
+    "stream/promises",
+    "stream/web",
+    "string_decoder",
+    "sys",
+    "timers",
+    "timers/promises",
+    "tls",
+    "trace_events",
+    "tty",
+    "url",
+    "util",
+    "util/types",
+    "v8",
+    "vm",
+    "wasi",
+    "worker_threads",
+    "zlib",
+];
 
 impl WarnForEdgeRuntime {
     fn warn_if_nodejs_module(&self, span: Span, module_specifier: &str) -> Option<()> {
-        if NODEJS_MODULE_NAMES.contains(&module_specifier) {
+        // Node.js modules can be loaded with `node:` prefix or directly
+        if module_specifier.starts_with("node:") || NODEJS_MODULE_NAMES.contains(&module_specifier)
+        {
             let loc = self.cm.lookup_line(span.lo).ok()?;
 
             let msg = format!(
@@ -105,15 +176,14 @@ Learn more: https://nextjs.org/docs/api-reference/edge-runtime",
 }
 
 impl Visit for WarnForEdgeRuntime {
-    fn visit_member_expr(&mut self, n: &MemberExpr) {
-        if n.obj.is_global_ref_to(&self.ctx, "process") {
-            if let MemberProp::Ident(prop) = &n.prop {
-                self.warn_for_unsupported_process_api(n.span, prop);
-                return;
+    fn visit_call_expr(&mut self, n: &CallExpr) {
+        n.visit_children_with(self);
+
+        if let Callee::Import(_) = &n.callee {
+            if let Some(Expr::Lit(Lit::Str(s))) = n.args.first().map(|e| &*e.expr) {
+                self.warn_if_nodejs_module(n.span, &s.value);
             }
         }
-
-        n.visit_children_with(self);
     }
 
     fn visit_expr(&mut self, n: &Expr) {
@@ -137,21 +207,22 @@ impl Visit for WarnForEdgeRuntime {
         self.warn_if_nodejs_module(n.span, &n.src.value);
     }
 
+    fn visit_member_expr(&mut self, n: &MemberExpr) {
+        if n.obj.is_global_ref_to(&self.ctx, "process") {
+            if let MemberProp::Ident(prop) = &n.prop {
+                self.warn_for_unsupported_process_api(n.span, prop);
+                return;
+            }
+        }
+
+        n.visit_children_with(self);
+    }
+
     fn visit_named_export(&mut self, n: &NamedExport) {
         n.visit_children_with(self);
 
         if let Some(module_specifier) = &n.src {
             self.warn_if_nodejs_module(n.span, &module_specifier.value);
-        }
-    }
-
-    fn visit_call_expr(&mut self, n: &CallExpr) {
-        n.visit_children_with(self);
-
-        if let Callee::Import(_) = &n.callee {
-            if let Some(Expr::Lit(Lit::Str(s))) = n.args.first().map(|e| &*e.expr) {
-                self.warn_if_nodejs_module(n.span, &s.value);
-            }
         }
     }
 }
