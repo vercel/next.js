a9549afac05c74bcb5d501568372609dba998bea
Shu Ding <g@shud.in>
2024-08-22 23:07:05 +0200

    Improve SWC transform ID generation (#69183)
    This adds an extra `hash_salt` option to the Server Actions transform's
    `generate_action_id` method which defaults to empty for now (so no
    actual behavior changes). When set, the salt will be used to prefix the
    payload.

--- packages/next-swc/crates/next-core/src/next_shared/transforms/server_actions.rs
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/server_actions.rs
@@ -50,6 +50,7 @@ impl CustomTransformer for NextServerActions {
             Config {
                 is_react_server_layer: matches!(self.transform, ActionsTransform::Server),
                 enabled: true,
+                hash_salt: "".into(),
             },
             ctx.comments.clone(),
         );
