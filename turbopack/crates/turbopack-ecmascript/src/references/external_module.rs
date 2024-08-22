1b6398a3295134cf4cd3647204f0ad3b31664ac8
LichuAcu <lichu.acuna@vercel.com>
2024-08-22 11:30:00 -0700

    Change ID generation strategy

--- turbopack/crates/turbopack-ecmascript/src/references/external_module.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/external_module.rs
@@ -245,7 +245,7 @@ impl EcmascriptChunkItem for CachedExternalModuleChunkItem {
     }
 }
 
-/// A module that only has an ident and no content. This is used
+/// A module that only has an ident and no content nor references. It is used
 /// to include a module's ident in the module graph before the module
 /// itself is resolved, as is the case with NextServerComponentModule's
 /// "client modules" and "client modules ssr".
