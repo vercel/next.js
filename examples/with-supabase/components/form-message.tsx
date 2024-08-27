f444cb1bf9cd291e25e89f0ceea50e3819b7f5bd
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-08-27 20:44:47 +0800

    Update form-message.tsx

--- examples/with-supabase/components/form-message.tsx
+++ examples/with-supabase/components/form-message.tsx
@@ -7,7 +7,7 @@ export function FormMessage({ message }: { message: Message }) {
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm">
       {"success" in message && (
-        <div className="text-green-500 border-l-2 border-green-500 px-4">
+        <div className="text-foreground border-l-2 border-foreground px-4">
           {message.success}
         </div>
       )}
