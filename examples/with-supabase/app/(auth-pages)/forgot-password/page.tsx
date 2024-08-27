194b3c6d8442cdb3a73ffb90b1d915cee8483e01
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-08-28 05:12:29 +0800

    add SMTP rate limit notice

--- examples/with-supabase/app/(auth-pages)/forgot-password/page.tsx
+++ examples/with-supabase/app/(auth-pages)/forgot-password/page.tsx
@@ -4,6 +4,7 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { SmtpMessage } from "../smtp-message";
 
 export default function ForgotPassword({
   searchParams,
@@ -11,24 +12,27 @@ export default function ForgotPassword({
   searchParams: Message;
 }) {
   return (
-    <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64">
-      <div>
-        <h1 className="text-2xl font-medium">Reset Password</h1>
-        <p className="text-sm text-foreground/60">
-          Already have an account?{" "}
-          <Link className="text-primary underline" href="/login">
-            Log in
-          </Link>
-        </p>
-      </div>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <SubmitButton formAction={forgotPasswordAction}>
-          Reset Password
-        </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
-    </form>
+    <>
+      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
+        <div>
+          <h1 className="text-2xl font-medium">Reset Password</h1>
+          <p className="text-sm text-secondary-foreground">
+            Already have an account?{" "}
+            <Link className="text-primary underline" href="/login">
+              Log in
+            </Link>
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" placeholder="you@example.com" required />
+          <SubmitButton formAction={forgotPasswordAction}>
+            Reset Password
+          </SubmitButton>
+          <FormMessage message={searchParams} />
+        </div>
+      </form>
+      <SmtpMessage />
+    </>
   );
 }
--- examples/with-supabase/app/(auth-pages)/signup/page.tsx
+++ examples/with-supabase/app/(auth-pages)/signup/page.tsx
@@ -4,6 +4,7 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { SmtpMessage } from "../smtp-message";
 
 export default function Signup({ searchParams }: { searchParams: Message }) {
   if ("message" in searchParams) {
@@ -15,30 +16,33 @@ export default function Signup({ searchParams }: { searchParams: Message }) {
   }
 
   return (
-    <form className="flex flex-col min-w-64 max-w-64 max-w-64">
-      <h1 className="text-2xl font-medium">Sign up</h1>
-      <p className="text-sm text text-foreground">
-        Already have an account?{" "}
-        <Link className="text-primary font-medium underline" href="/login">
-          Log in
-        </Link>
-      </p>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <Label htmlFor="password">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          minLength={6}
-          required
-        />
-        <SubmitButton formAction={signUpAction} pendingText="Signing up...">
-          Sign up
-        </SubmitButton>
-        <FormMessage message={searchParams} />
-      </div>
-    </form>
+    <>
+      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
+        <h1 className="text-2xl font-medium">Sign up</h1>
+        <p className="text-sm text text-foreground">
+          Already have an account?{" "}
+          <Link className="text-primary font-medium underline" href="/login">
+            Log in
+          </Link>
+        </p>
+        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+          <Label htmlFor="email">Email</Label>
+          <Input name="email" placeholder="you@example.com" required />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            minLength={6}
+            required
+          />
+          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
+            Sign up
+          </SubmitButton>
+          <FormMessage message={searchParams} />
+        </div>
+      </form>
+      <SmtpMessage />
+    </>
   );
 }
