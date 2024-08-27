76660f453299dc2bca91f52c58e3b526a73171d3
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-08-27 19:27:39 +0800

    moat

--- examples/with-supabase/app/page.tsx
+++ examples/with-supabase/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { verifySupabase } from "@/utils/supabase/verify-supabase";
@@ -6,7 +6,7 @@ import { verifySupabase } from "@/utils/supabase/verify-supabase";
 export default async function Index() {
   return (
     <>
-      <Header />
+      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
         {verifySupabase ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
