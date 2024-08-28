44eea1a9a1c5169bb4a302a5fcfc52d9a0c57571
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2024-08-28 17:35:38 +0800

    Update sign-up-user-steps.tsx

--- examples/with-supabase/components/tutorial/sign-up-user-steps.tsx
+++ examples/with-supabase/components/tutorial/sign-up-user-steps.tsx
@@ -8,15 +8,15 @@ export default function SignUpUserSteps() {
       {process.env.VERCEL_ENV === "preview" ||
       process.env.VERCEL_ENV === "production" ? (
         <TutorialStep title="Set up redirect urls">
-          <p>
-            It looks like this App is hosted on Vercel. This particular
-            deployment is{" "}
+          <p>It looks like this App is hosted on Vercel.</p>
+          <p className="mt-4">
+            This particular deployment is
             <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-              {process.env.VERCEL_ENV}
+              "{process.env.VERCEL_ENV}"
             </span>{" "}
             on
             <span className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-medium text-secondary-foreground border">
-              {process.env.VERCEL_URL}
+              https://{process.env.VERCEL_URL}
             </span>
           </p>
           <p className="mt-4">
