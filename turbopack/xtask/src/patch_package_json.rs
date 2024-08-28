d3118646ead44d068146b8d6c90c34d0e25e1eb6
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-08-28 14:04:47 -0700

    npm compat, detect and handle workspaces

--- turbopack/xtask/src/patch_package_json.rs
+++ turbopack/xtask/src/patch_package_json.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{BufReader, BufWriter, Write},
+    io::{self, BufReader, BufWriter, Write},
     path::{Path, PathBuf},
 };
 
@@ -15,7 +15,7 @@ type JsonMap = serde_json::Map<String, Value>;
 /// this next.js repository with `pnpm pack-next`.
 #[derive(Parser)]
 #[command(name = "patch-package-json")]
-pub struct PatchPackageJson {
+pub struct PatchPackageJsonArgs {
     target_project_path: PathBuf,
 
     #[arg(long, value_name = "FILE")]
@@ -46,57 +46,107 @@ struct NextPeerDeps {
     react_dom: String,
 }
 
-pub fn run(args: &PatchPackageJson) -> Result<()> {
+pub fn run(args: &PatchPackageJsonArgs) -> Result<()> {
+    let package_json_path = find_workspace_root(&args.target_project_path)?
+        .map(|root| root.join("package.json"))
+        .inspect(|json_path| println!("Found workspace! Patching {json_path:?} with overrides."))
+        // if we're not in a workspace, apply the changes we would've made to the workspace root to
+        // the project's package.json file
+        .unwrap_or_else(|| args.target_project_path.join("package.json"));
+
+    let mut package_json_value = read_json_value(&package_json_path)?;
+    let package_json_map = to_package_json_map(&mut package_json_value)?;
+    patch_workspace_package_json_map(args, package_json_map)?;
+    write_json_value(&package_json_path, &package_json_value)?;
+
+    println!("Successfully patched {package_json_path:?} to use local tarball files!");
+    // TODO: Bun is currently broken for unknown reasons, it generates ENOTDIR when trying to read
+    // the tarballs
+    // NOTE: Yarn is currently broken due to an upstream issue:
+    // https://github.com/yarnpkg/yarn/issues/6339
+    println!("Run `pnpm i` or `npm i` to install the overrides");
+
+    Ok(())
+}
+
+fn read_json_value(path: &Path) -> Result<Value> {
+    serde_json::from_reader(BufReader::new(
+        File::open(path).with_context(|| format!("could not read {path:?}"))?,
+    ))
+    .with_context(|| format!("failed to parse {path:?}"))
+}
+
+fn write_json_value(path: &Path, value: &Value) -> Result<()> {
+    (|| {
+        let file = File::options().write(true).truncate(true).open(path)?;
+        let mut writer = BufWriter::new(file);
+
+        // NOTE: serde_json defaults to two-space indents
+        serde_json::to_writer_pretty(&mut writer, value)?;
+        writer.write_all(b"\n")?;
+        writer.flush()?;
+        anyhow::Ok(())
+    })()
+    .with_context(|| format!("failed to write {path:?}"))
+}
+
+fn to_package_json_map(value: &mut Value) -> Result<&mut JsonMap> {
+    value
+        .as_object_mut()
+        .context("package.json must represent an object")
+}
+
+fn patch_workspace_package_json_map(
+    args: &PatchPackageJsonArgs,
+    package_json_map: &mut JsonMap,
+) -> Result<()> {
     let next_peer_deps = get_next_peer_deps()?;
 
-    let package_json_path = args.target_project_path.join("package.json");
-    let package_json_file = File::open(&package_json_path)
-        .with_context(|| format!("could not read {package_json_path:?}"))?;
-    let mut package_value: Value = serde_json::from_reader(BufReader::new(&package_json_file))
-        .with_context(|| format!("failed to parse {package_json_path:?}"))?;
+    // insert overrides
+    let overrides = [
+        ("next", &*format!("file:{}", args.next_tarball)),
+        ("@next/mdx", &*format!("file:{}", args.next_mdx_tarball)),
+        ("@next/env", &*format!("file:{}", args.next_env_tarball)),
+        (
+            "@next/bundle-analyzer",
+            &*format!("file:{}", args.next_bundle_analyzer_tarball),
+        ),
+        // next-swc is added to the project's package.json, but also set a global override just in
+        // case something else pulls it in
+        ("@next/swc", &*format!("file:{}", args.next_swc_tarball)),
+        // next's peerDependencies
+        ("react", &*next_peer_deps.react),
+        ("react-dom", &*next_peer_deps.react_dom),
+    ];
 
-    let package_map = package_value
-        .as_object_mut()
-        .context("package.json must represent an object")?;
+    // npm uses `overrides`: https://docs.npmjs.com/cli/v10/configuring-npm/package-json#overrides
+    // bun also supports this: https://bun.sh/docs/install/overrides
+    let overrides_map = get_mut_or_insert_default_object(package_json_map, "overrides")?;
+    insert_map_entries(overrides_map, &overrides[..]);
 
-    let package_deps_map = package_map
-        .get_mut("dependencies")
-        .and_then(Value::as_object_mut)
-        .context("failed to read package.json dependencies object")?;
-    insert_dependency_tarballs(package_deps_map, &[("@next/swc", &*args.next_swc_tarball)]);
-
-    package_deps_map.insert("react".to_string(), Value::String(next_peer_deps.react));
-    package_deps_map.insert(
-        "react-dom".to_string(),
-        Value::String(next_peer_deps.react_dom),
-    );
-
-    let pnpm_map = get_mut_or_insert_default_object(package_map, "pnpm")?;
+    // pnpm uses `pnpm.overrides`: https://pnpm.io/package_json#pnpmoverrides
+    let pnpm_map = get_mut_or_insert_default_object(package_json_map, "pnpm")?;
     let pnpm_overrides_map = get_mut_or_insert_default_object(pnpm_map, "overrides")?;
-    insert_dependency_tarballs(
-        pnpm_overrides_map,
-        &[
-            ("next", &args.next_tarball),
-            ("@next/mdx", &args.next_mdx_tarball),
-            ("@next/env", &args.next_env_tarball),
-            ("@next/bundle-analyzer", &args.next_bundle_analyzer_tarball),
-        ],
+    insert_map_entries(pnpm_overrides_map, &overrides[..]);
+
+    // yarn uses `resolutions`: https://yarnpkg.com/configuration/manifest#resolutions
+    let resolutions_map = get_mut_or_insert_default_object(package_json_map, "resolutions")?;
+    insert_map_entries(resolutions_map, &overrides[..]);
+
+    // add `@next/swc` to `dependencies`, without this next might fall back to downloading the
+    // binary blob from the release version, which isn't what we want.
+    let deps_map = get_mut_or_insert_default_object(package_json_map, "dependencies")?;
+    insert_map_entries(
+        deps_map,
+        &[("@next/swc", &*format!("file:{}", args.next_swc_tarball))],
     );
 
-    let mut package_json_file = File::options()
-        .write(true)
-        .truncate(true)
-        .open(&package_json_path)?;
-    let mut writer = BufWriter::new(&mut package_json_file);
-    // NOTE: serde_json defaults to two-space indents
-    serde_json::to_writer_pretty(&mut writer, &package_value)?;
-    writer.write_all(b"\n")?;
-    writer
-        .flush()
-        .with_context(|| format!("failed to write {package_json_path:?}"))?;
-
-    println!("Successfully patched `package_json_path` to use local tarball files!");
-    println!("Run `pnpm i` to install the overrides");
+    // npm requires that any direct dependencies in the workspace file match the version specified
+    // in the overrides:
+    //
+    // > You may not set an override for a package that you directly depend on unless both the
+    // > dependency and the override itself share the exact same spec.
+    update_map_entries_if_exists(deps_map, &overrides[..]);
 
     Ok(())
 }
@@ -119,9 +169,17 @@ fn get_next_peer_deps() -> Result<NextPeerDeps> {
     )
 }
 
-fn insert_dependency_tarballs(map: &mut JsonMap, entries: &[(&str, &str)]) {
-    for &(key, tarball) in entries {
-        map.insert(key.to_owned(), Value::String(format!("file:{tarball}")));
+fn insert_map_entries(map: &mut JsonMap, entries: &[(&str, &str)]) {
+    for &(key, value) in entries {
+        map.insert(key.to_owned(), Value::String(value.to_owned()));
+    }
+}
+
+fn update_map_entries_if_exists(map: &mut JsonMap, entries: &[(&str, &str)]) {
+    for &(key, value) in entries {
+        if let Some(old_value) = map.get_mut(key) {
+            *old_value = Value::String(value.to_owned());
+        }
     }
 }
 
@@ -133,4 +191,45 @@ fn get_mut_or_insert_default_object<'a>(
         .or_insert_with(|| Value::Object(Default::default()))
         .as_object_mut()
         .with_context(|| format!("failed to read package.json object with key {key}"))
+}
+
+/// pnpm and npm (and probably bun+yarn too) require that overrides are set in the root package.json
+/// of the workspace.
+///
+/// Different package managers have slightly different logic. We'll return the first workspace root
+/// we find that's valid for any package manager.
+///
+/// Returns `None` if we're not in a workspace.
+///
+/// Here's how pnpm does it:
+/// <https://github.com/pnpm/pnpm/blob/757e6be29d1916fd0c/workspace/find-workspace-dir/src/index.ts>
+fn find_workspace_root(project_path: &Path) -> Result<Option<PathBuf>> {
+    const ENV_VAR: &str = "NPM_CONFIG_WORKSPACE_DIR";
+    for ev in [ENV_VAR, &ENV_VAR.to_ascii_lowercase()] {
+        if let Some(root) = std::env::var_os(ev) {
+            return Ok(Some(root.into()));
+        }
+    }
+
+    for ancestor in project_path.canonicalize()?.ancestors() {
+        if ancestor.join("pnpm-workspace.yaml").exists() {
+            return Ok(Some(ancestor.to_owned()));
+        }
+        let package_json_path = ancestor.join("package.json");
+        let package_json_file = match File::open(&package_json_path) {
+            Ok(file) => file,
+            Err(err) if err.kind() == io::ErrorKind::NotFound => continue,
+            Err(err) => return Err(err.into()),
+        };
+        let package_json_value: Value = serde_json::from_reader(BufReader::new(package_json_file))?;
+        if package_json_value
+            .as_object()
+            .with_context(|| format!("failed to parse {package_json_path:?}"))?
+            .contains_key("workspaces")
+        {
+            return Ok(Some(ancestor.to_owned()));
+        }
+    }
+    // we didn't find a workspace root, we're not in a workspace
+    Ok(None)
 }
