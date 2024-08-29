0c519b4635a06299af9300b92ce25e68e83ec341
Piotr Tomczewski <piotr.tomczewski@callstack.com>
2024-08-29 23:18:00 +0200

    feat(eslint-plugin-next): enforce `unstable_rethrow` usage in try-catch blocks

--- packages/eslint-plugin-next/src/rules/no-redirect-in-try-catch.ts
+++ packages/eslint-plugin-next/src/rules/no-redirect-in-try-catch.ts
@@ -1,10 +1,13 @@
 import { defineRule } from '../utils/define-rule'
+import type { Node, BlockStatement } from 'estree'
+
 const url = 'https://nextjs.org/docs/messages/no-redirect-in-try-catch'
 
 export = defineRule({
   meta: {
     docs: {
-      description: 'Prevent usage of `redirect` in try-catch block.',
+      description:
+        'Ensure that when using `redirect` within a try-catch block, the catch block must start with a call to `unstable_rethrow` to ensures that errors are correctly propagated.',
       recommended: true,
       url,
     },
@@ -12,37 +15,50 @@ export = defineRule({
     schema: [],
   },
   create(context) {
-    function checkForRedirectCall(statement) {
-      if (
-        statement.type === 'ExpressionStatement' &&
-        statement.expression.type === 'CallExpression' &&
-        statement.expression.callee.name === 'redirect'
-      ) {
-        context.report({
-          node: statement,
-          message: `Do not use \`redirect\` within a try-catch block. Move the \`redirect\` call outside of the try-catch block. See: ${url}`,
-        })
-      } else if (statement.type === 'BlockStatement') {
-        statement.body.forEach((innerStatement) => {
-          checkForRedirectCall(innerStatement)
-        })
-      } else if (statement.type === 'IfStatement') {
-        checkForRedirectCall(statement.consequent)
-        if (statement.alternate) {
-          checkForRedirectCall(statement.alternate)
-        }
+    function isRethrowFirstStatement(blockStatement: BlockStatement): boolean {
+      const firstStatement = blockStatement.body[0]
+      return (
+        firstStatement?.type === 'ExpressionStatement' &&
+        firstStatement.expression.type === 'CallExpression' &&
+        firstStatement.expression.callee.type === 'Identifier' &&
+        firstStatement.expression.callee.name === 'unstable_rethrow'
+      )
+    }
+
+    function containsRedirectCall(node: Node): boolean {
+      switch (node.type) {
+        case 'ExpressionStatement':
+          return (
+            node.expression.type === 'CallExpression' &&
+            node.expression.callee.type === 'Identifier' &&
+            node.expression.callee.name === 'redirect'
+          )
+        case 'BlockStatement':
+          return node.body.some(containsRedirectCall)
+        case 'IfStatement':
+          return (
+            containsRedirectCall(node.consequent) ||
+            (node.alternate ? containsRedirectCall(node.alternate) : false)
+          )
+        default:
+          return false
       }
     }
 
     return {
-      ImportDeclaration(node) {
-        if (node.source.value !== 'next/navigation') {
-          return
-        }
-      },
       TryStatement(node) {
-        const tryBlockStatements = node.block.body
-        tryBlockStatements.forEach(checkForRedirectCall)
+        const tryBlock = node.block
+        const catchBlock = node.handler.body
+
+        if (
+          containsRedirectCall(tryBlock) &&
+          !isRethrowFirstStatement(catchBlock)
+        ) {
+          context.report({
+            node: catchBlock,
+            message: `When using \`redirect\` in a try-catch block, ensure you include \`unstable_rethrow\` at the start of the catch block to properly handle Next.js errors. See: ${url}`,
+          })
+        }
       },
     }
   },
