c4d754565d9961c4b5e24d6c8ae642c2bb048edd
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-08-30 11:11:16 -0700

    fix missing stylesheets when parallel routes are present

--- packages/next/src/client/components/layout-router.tsx
+++ packages/next/src/client/components/layout-router.tsx
@@ -517,7 +517,6 @@ export default function OuterLayoutRouter({
   template,
   notFound,
   notFoundStyles,
-  styles,
 }: {
   parallelRouterKey: string
   segmentPath: FlightSegmentPath
@@ -529,7 +528,6 @@ export default function OuterLayoutRouter({
   template: React.ReactNode
   notFound: React.ReactNode | undefined
   notFoundStyles: React.ReactNode | undefined
-  styles?: React.ReactNode
 }) {
   const context = useContext(LayoutRouterContext)
   if (!context) {
@@ -562,7 +560,6 @@ export default function OuterLayoutRouter({
 
   return (
     <>
-      {styles}
       {preservedSegments.map((preservedSegment) => {
         const preservedSegmentValue = getSegmentValue(preservedSegment)
         const cacheKey = createRouterCacheKey(preservedSegment)
