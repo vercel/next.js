fbd94e13c1b4372e0bb2feecc979c753893170cd
Janka Uryga <lolzatu2@gmail.com>
2024-09-02 23:20:18 +0200

    test: fix static GETs

--- test/e2e/app-dir/draft-mode-middleware/app/api/disable-draft/route.ts
+++ test/e2e/app-dir/draft-mode-middleware/app/api/disable-draft/route.ts
@@ -1,5 +1,8 @@
 import { draftMode } from 'next/headers'
 
+// BACKPORT: GETs are static by default in 14.x
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   draftMode().disable()
   return new Response('Draft mode is disabled')
