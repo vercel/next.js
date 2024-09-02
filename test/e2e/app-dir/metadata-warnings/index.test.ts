cb8ccd5c07ff4c0bca70ab9d1c3c1e4e25f9150b
Janka Uryga <lolzatu2@gmail.com>
2024-09-02 18:09:30 +0200

    test: convert metadata-warnings/index.test.ts to nextTestSetup

--- test/e2e/app-dir/metadata-warnings/index.test.ts
+++ test/e2e/app-dir/metadata-warnings/index.test.ts
@@ -1,60 +1,62 @@
-import { createNextDescribe } from 'e2e-utils'
+import { nextTestSetup } from 'e2e-utils'
 
 const METADATA_BASE_WARN_STRING =
   'metadataBase property in metadata export is not set for resolving social open graph or twitter images,'
 
-createNextDescribe(
-  'app dir - metadata missing metadataBase',
-  {
+describe('app dir - metadata missing metadataBase', () => {
+  const { next, isNextDev, skipped } = nextTestSetup({
     files: __dirname,
     skipDeployment: true,
-  },
-  ({ next, isNextDev }) => {
-    // If it's start mode, we get the whole logs since they're from build process.
-    // If it's development mode, we get the logs after request
-    function getCliOutput(logStartPosition: number) {
-      return isNextDev ? next.cliOutput.slice(logStartPosition) : next.cliOutput
-    }
+  })
 
-    if (isNextDev) {
-      it('should not warn metadataBase is missing if there is only absolute url', async () => {
-        const logStartPosition = next.cliOutput.length
-        await next.fetch('/absolute-url-og')
-        const output = getCliOutput(logStartPosition)
-        expect(output).not.toInclude(METADATA_BASE_WARN_STRING)
-      })
-    }
+  if (skipped) {
+    return
+  }
 
-    it('should fallback to localhost if metadataBase is missing for absolute urls resolving', async () => {
+  // If it's start mode, we get the whole logs since they're from build process.
+  // If it's development mode, we get the logs after request
+  function getCliOutput(logStartPosition: number) {
+    return isNextDev ? next.cliOutput.slice(logStartPosition) : next.cliOutput
+  }
+
+  if (isNextDev) {
+    it('should not warn metadataBase is missing if there is only absolute url', async () => {
       const logStartPosition = next.cliOutput.length
-      await next.fetch('/og-image-convention')
+      await next.fetch('/absolute-url-og')
       const output = getCliOutput(logStartPosition)
-      expect(output).toInclude(METADATA_BASE_WARN_STRING)
-      expect(output).toMatch(/using "http:\/\/localhost:\d+/)
-      expect(output).toInclude(
-        '. See https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase'
-      )
-    })
-
-    it('should warn for unsupported metadata properties', async () => {
-      const logStartPosition = next.cliOutput.length
-      await next.fetch('/unsupported-metadata')
-      const output = getCliOutput(logStartPosition)
-      expect(output).toInclude(
-        'Unsupported metadata themeColor is configured in metadata export in /unsupported-metadata. Please move it to viewport'
-      )
-      expect(output).toInclude(
-        'Read more: https://nextjs.org/docs/app/api-reference/functions/generate-viewport'
-      )
-    })
-
-    it('should not warn for viewport properties during manually merging metadata', async () => {
-      const outputLength = next.cliOutput.length
-      await next.fetch('/merge')
-      // Should not log the unsupported metadata viewport warning in the output
-      // during merging the metadata, if the value is still nullable.
-      const output = next.cliOutput.slice(outputLength)
-      expect(output).not.toContain('Unsupported metadata viewport')
+      expect(output).not.toInclude(METADATA_BASE_WARN_STRING)
     })
   }
-)
+
+  it('should fallback to localhost if metadataBase is missing for absolute urls resolving', async () => {
+    const logStartPosition = next.cliOutput.length
+    await next.fetch('/og-image-convention')
+    const output = getCliOutput(logStartPosition)
+    expect(output).toInclude(METADATA_BASE_WARN_STRING)
+    expect(output).toMatch(/using "http:\/\/localhost:\d+/)
+    expect(output).toInclude(
+      '. See https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase'
+    )
+  })
+
+  it('should warn for unsupported metadata properties', async () => {
+    const logStartPosition = next.cliOutput.length
+    await next.fetch('/unsupported-metadata')
+    const output = getCliOutput(logStartPosition)
+    expect(output).toInclude(
+      'Unsupported metadata themeColor is configured in metadata export in /unsupported-metadata. Please move it to viewport'
+    )
+    expect(output).toInclude(
+      'Read more: https://nextjs.org/docs/app/api-reference/functions/generate-viewport'
+    )
+  })
+
+  it('should not warn for viewport properties during manually merging metadata', async () => {
+    const outputLength = next.cliOutput.length
+    await next.fetch('/merge')
+    // Should not log the unsupported metadata viewport warning in the output
+    // during merging the metadata, if the value is still nullable.
+    const output = next.cliOutput.slice(outputLength)
+    expect(output).not.toContain('Unsupported metadata viewport')
+  })
+})
