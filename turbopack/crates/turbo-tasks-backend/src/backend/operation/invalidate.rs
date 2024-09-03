446caa6259690ccb45ee6235ed4e41534a2dfe7c
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-03 16:04:06 +0200

    remove scheduling on invalidation

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/invalidate.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/invalidate.rs
@@ -6,10 +6,7 @@ use super::{
     aggregation_update::{AggregatedDataUpdate, AggregationUpdateJob, AggregationUpdateQueue},
     ExecuteContext, Operation,
 };
-use crate::{
-    data::{CachedDataItem, InProgressState},
-    get, update,
-};
+use crate::data::CachedDataItem;
 
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub enum InvalidateOperation {
@@ -65,39 +62,6 @@ pub fn make_task_dirty(task_id: TaskId, queue: &mut AggregationUpdateQueue, ctx:
     let mut task = ctx.task(task_id);
 
     if task.add(CachedDataItem::Dirty { value: () }) {
-        let in_progress = match get!(task, InProgress) {
-            Some(InProgressState::InProgress {
-                stale, once_task, ..
-            }) if !once_task => {
-                if !*stale {
-                    update!(task, InProgress, |in_progress| {
-                        let Some(InProgressState::InProgress {
-                            stale: _,
-                            once_task,
-                            done_event,
-                        }) = in_progress
-                        else {
-                            unreachable!();
-                        };
-                        Some(InProgressState::InProgress {
-                            stale: true,
-                            once_task,
-                            done_event,
-                        })
-                    });
-                }
-                true
-            }
-            Some(_) => true,
-            None => false,
-        };
-        if !in_progress
-            && task.add(CachedDataItem::new_scheduled(
-                task.backend.get_task_desc_fn(task_id),
-            ))
-        {
-            ctx.turbo_tasks.schedule(task_id)
-        }
         queue.push(AggregationUpdateJob::DataUpdate {
             task_id,
             update: AggregatedDataUpdate::dirty_task(task_id),
