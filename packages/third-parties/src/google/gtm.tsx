9ea478c93ae8b4eae3302235301ca49f366bae1f
Zach Hawtof <zhawtof@gmail.com>
2024-09-04 00:36:03 -0400

    reinclude data layer

--- packages/third-parties/src/google/gtm.tsx
+++ packages/third-parties/src/google/gtm.tsx
@@ -47,6 +47,7 @@ export function GoogleTagManager(props: GTMParams) {
             (function(w,d,s,l,i){
               w[l]=w[l]||[];
               w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              ${dataLayer ? `w[l].push(${JSON.stringify(dataLayer)})` : ''}
               var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
               j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
