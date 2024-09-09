44814b91eaf000afc59d24aba4a83a3f09cab9df
Jiachi Liu <inbox@huozhi.im>
2024-09-09 19:18:34 +0200

    test: lock ts type check

--- test/production/middleware-typescript/test/index.test.ts
+++ test/production/middleware-typescript/test/index.test.ts
@@ -5,6 +5,11 @@ import { nextTestSetup } from 'e2e-utils'
 describe('middleware-typescript', () => {
   const { next } = nextTestSetup({
     files: join(__dirname, '../app'),
+    dependencies: {
+      // TODO: fix the TS error with the TS 5.6
+      // x-ref: https://github.com/vercel/next.js/actions/runs/10777104696/job/29887663970?pr=69784
+      typescript: '5.5.4',
+    },
   })
 
   it('should have built and started', async () => {
