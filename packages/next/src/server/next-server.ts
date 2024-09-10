c7000e17c505864619f3a880f482a5c66b6263b0
Abhishek <abhi.9811206369@gmail.com>
2024-09-10 14:36:53 +0530

    remove x-middleware-set-cookie in web/sandbox file instead of next-server file

--- packages/next/src/server/next-server.ts
+++ packages/next/src/server/next-server.ts
@@ -1486,7 +1486,6 @@ export default class NextNodeServer extends BaseServer<
       // Add cookies to request meta.
       addRequestMeta(params.request, 'middlewareCookie', cookies)
     }
-    result.response.headers.delete('x-middleware-set-cookie')
     return result
   }
 
