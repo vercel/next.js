47072bf4c52c7cda63b37ef051550da9c76f7449
Luca Forstner <luca.forstner@sentry.io>
2024-09-10 17:32:01 +0200

    Pass client trace metadata to client in pages router (#69620)

--- test/e2e/opentelemetry/client-trace-metadata/pages/pages-router/static-page.tsx
+++ test/e2e/opentelemetry/client-trace-metadata/pages/pages-router/static-page.tsx
@@ -4,10 +4,10 @@ export default function StaticPage() {
   return (
     <>
       <h1 id="static-page-header">Static Page</h1>
-      <Link href="/dynamic-page" id="go-to-dynamic-page">
+      <Link href="/pages-router/dynamic-page" id="go-to-dynamic-page">
         Go to dynamic page
       </Link>
-      <Link href="/static-page-2" id="go-to-static-page">
+      <Link href="/pages-router/static-page-2" id="go-to-static-page">
         Go to static page
       </Link>
     </>
