4ac10bf3effeb8bb6714ac695951182028b9210c
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-09-11 11:55:51 +0900

    Adjust test

--- test/integration/edge-runtime-dynamic-code/test/index.test.js
+++ test/integration/edge-runtime-dynamic-code/test/index.test.js
@@ -178,13 +178,14 @@ describe.each([
             expect(buildResult.stderr).toContain(`Ecmascript file had an error`)
           } else {
             expect(buildResult.stderr).toContain(`Failed to compile`)
+            expect(buildResult.stderr).toContain(
+              `Used by usingEval, usingEvalSync`
+            )
+            expect(buildResult.stderr).toContain(
+              `Used by usingWebAssemblyCompile`
+            )
           }
-          expect(buildResult.stderr).toContain(
-            `Used by usingEval, usingEvalSync`
-          )
-          expect(buildResult.stderr).toContain(
-            `Used by usingWebAssemblyCompile`
-          )
+
           expect(buildResult.stderr).toContain(DYNAMIC_CODE_ERROR)
         })
       }
