99b6c579dcae00ca3e1e665ada71e0d7d42c564a
Shu Ding <g@shud.in>
2024-09-11 19:02:01 +0200

    wip

--- crates/next-custom-transforms/tests/fixture/server-actions/server/34/input.js
+++ crates/next-custom-transforms/tests/fixture/server-actions/server/34/input.js
@@ -5,3 +5,15 @@ export { foo }
 const foo = async function () {
   return 'bar'
 }
+
+// output
+
+import { cache as $cache } from '...'
+
+export { foo }
+
+const foo = $cache(async function () {
+  return 'bar'
+})
+
+registerServerReference('', foo)
