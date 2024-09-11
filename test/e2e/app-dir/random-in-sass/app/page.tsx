0a5e4d2de3de76a026cc9836ce61c9040134b71a
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-11 21:23:40 +0200

    make tsc happy

--- test/e2e/app-dir/random-in-sass/app/page.tsx
+++ test/e2e/app-dir/random-in-sass/app/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 
-const DynamicComponent = dynamic(() => import('../components/hello.tsx'))
+const DynamicComponent = dynamic(() => import('../components/hello'))
 
 export default function Page() {
   return <DynamicComponent />
