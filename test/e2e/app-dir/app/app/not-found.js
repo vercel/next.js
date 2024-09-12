45cc801bd9150ad8d7ecc240070417e07e81bb5f
JJ Kasper <jj@jjsweb.site>
2024-09-12 11:15:10 -0700

    Fix not-found case with incremental tracing

---
+++ test/e2e/app-dir/app/app/not-found.js
@@ -0,0 +1,9 @@
+import * as styles from './not-found.module.css'
+
+export default function Page() {
+  return (
+    <>
+      <p className={styles.text}>top-level not found page</p>
+    </>
+  )
+}
