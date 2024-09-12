806ea1b8bba3d9d21450adb2bb5b617a5ad98933
JJ Kasper <jj@jjsweb.site>
2024-09-12 13:07:37 -0700

    revert extra script change

--- packages/third-parties/src/google/gtm.tsx
+++ packages/third-parties/src/google/gtm.tsx
@@ -44,17 +44,11 @@ export function GoogleTagManager(props: GTMParams) {
         id="_next-gtm-init"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              ${dataLayer ? `w[l].push(${JSON.stringify(dataLayer)})` : ''}
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              var n=d.querySelector('[nonce]');
-              n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','${dataLayerName}','${gtmId}');`,
+      (function(w,l){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        ${dataLayer ? `w[l].push(${JSON.stringify(dataLayer)})` : ''}
+      })(window,'${dataLayerName}');`,
         }}
         nonce={nonce}
       />
