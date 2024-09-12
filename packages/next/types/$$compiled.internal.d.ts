aa512ee2420a57ae607b28f1d85c47c9b55d918d
devjiwonchoi <devjiwonchoi@gmail.com>
2024-09-13 02:47:32 +0900

    chore: upgrade typescript to 5.6.2

--- packages/next/types/$$compiled.internal.d.ts
+++ packages/next/types/$$compiled.internal.d.ts
@@ -111,7 +111,7 @@ declare module 'next/dist/compiled/amphtml-validator' {
 }
 
 declare module 'next/dist/compiled/superstruct' {
-  import m from 'superstruct'
+  import * as m from 'superstruct'
   export = m
 }
 declare module 'next/dist/compiled/async-retry'
@@ -247,7 +247,7 @@ declare module 'next/dist/compiled/picomatch' {
   export = m
 }
 declare module 'next/dist/compiled/nanoid/index.cjs' {
-  import m from 'nanoid'
+  import * as m from 'nanoid'
   export = m
 }
 declare module 'next/dist/compiled/ora' {
@@ -293,7 +293,7 @@ declare module 'next/dist/compiled/tar' {
 }
 
 declare module 'next/dist/compiled/terser' {
-  import m from 'terser'
+  import * as m from 'terser'
   export = m
 }
 declare module 'next/dist/compiled/semver' {
@@ -322,7 +322,7 @@ declare module 'next/dist/compiled/unistore' {
   export = m
 }
 declare module 'next/dist/compiled/web-vitals' {
-  import m from 'web-vitals'
+  import * as m from 'web-vitals'
   export = m
 }
 declare module 'next/dist/compiled/web-vitals-attribution' {}
