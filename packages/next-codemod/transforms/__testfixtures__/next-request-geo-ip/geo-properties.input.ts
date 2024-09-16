e4bf310ca2abaf88fe68195ccd6a3d46c4a73e60
devjiwonchoi <devjiwonchoi@gmail.com>
2024-09-16 22:08:53 +0900

    update

--- packages/next-codemod/transforms/__testfixtures__/next-request-geo-ip/geo-properties.input.ts
+++ packages/next-codemod/transforms/__testfixtures__/next-request-geo-ip/geo-properties.input.ts
@@ -3,6 +3,8 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
-  const { city, country, region, latitude, longitude } = request.geo;
+  const { city, country, region } = request.geo;
+  const latitude = request.geo.latitude;
+  const longitude = request.geo.longitude;
   return NextResponse.json({ city, country, region, latitude, longitude });
 }
