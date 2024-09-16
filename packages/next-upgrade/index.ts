e8e9bfbfd9d7a33b2280bf717e4f6a5d71d3c4cd
LichuAcu <lichu.acuna@vercel.com>
2024-09-16 16:03:12 -0700

    Fixes small issues, adds support for custom Next.js versions, adds styling

--- packages/next-upgrade/index.ts
+++ packages/next-upgrade/index.ts
@@ -6,102 +6,122 @@ import { execSync } from 'child_process'
 import path from 'path'
 import { compareVersions } from 'compare-versions'
 import yaml from 'yaml'
+import chalk from 'chalk'
 
-type VersionSpecifier = 'canary' | 'rc' | 'latest'
+type StandardVersionSpecifier = 'canary' | 'rc' | 'latest'
+type CustomVersionSpecifier = string
+type VersionSpecifier = StandardVersionSpecifier | CustomVersionSpecifier
 type PackageManager = 'pnpm' | 'npm' | 'yarn'
 
 interface Response {
-  version: VersionSpecifier
+  version: StandardVersionSpecifier
 }
 
 async function run(): Promise<void> {
-  let targetVersionSpecifier: VersionSpecifier
-
-  let nextPackageJson: { [key: string]: any } = {}
-  try {
-    const resCanary = await fetch(`https://registry.npmjs.org/next/canary`)
-    nextPackageJson['canary'] = await resCanary.json()
-
-    const resRc = await fetch(`https://registry.npmjs.org/next/rc`)
-    nextPackageJson['rc'] = await resRc.json()
-
-    const resLatest = await fetch(`https://registry.npmjs.org/next/latest`)
-    nextPackageJson['latest'] = await resLatest.json()
-  } catch (error) {
-    console.error('Failed to fetch versions from npm registry.')
-    return
-  }
-
-  let showRc = true
-  if (nextPackageJson['latest'].version && nextPackageJson['rc'].version) {
-    showRc =
-      compareVersions(
-        nextPackageJson['rc'].version,
-        nextPackageJson['latest'].version
-      ) === 1
-  }
-
-  const choices = [
-    {
-      title: 'Canary',
-      value: 'canary',
-      description: `Experimental version with latest features (${nextPackageJson['canary'].version})`,
-    },
-  ]
-  if (showRc) {
-    choices.push({
-      title: 'Latest Release Candidate',
-      value: 'rc',
-      description: `Pre-release version for final testing (${nextPackageJson['rc'].version})`,
-    })
-  }
-  choices.push({
-    title: 'Latest Stable',
-    value: 'latest',
-    description: `Production-ready release (${nextPackageJson['latest'].version})`,
-  })
+  let targetNextPackageJson
+  let targetVersionSpecifier: VersionSpecifier = ''
 
   const shortcutVersion = process.argv[2]?.replace('@', '')
-  if (['canary', 'rc', 'latest'].includes(shortcutVersion)) {
-    // Shortcut
-    targetVersionSpecifier = shortcutVersion as VersionSpecifier
-  } else {
-    const initialVersionSpecifierIdx = await processCurrentVersion()
+  if (shortcutVersion) {
+    const res = await fetch(
+      `https://registry.npmjs.org/next/${shortcutVersion}`
+    )
+    if (res.status === 200) {
+      targetNextPackageJson = await res.json()
+      targetVersionSpecifier = targetNextPackageJson.version
+    } else {
+      console.error(
+        `${chalk.yellow('Next.js ' + shortcutVersion)} does not exist. Check available versions at ${chalk.underline('https://www.npmjs.com/package/next?activeTab=versions')}, or choose one from below\n`
+      )
+    }
+  }
 
-    const response: Response = await prompts({
-      type: 'select',
-      name: 'version',
-      message: 'What Next.js version do you want to upgrade to?',
-      choices: choices,
-      initial: initialVersionSpecifierIdx,
-    })
+  if (!targetNextPackageJson) {
+    let nextPackageJson: { [key: string]: any } = {}
+    try {
+      const resCanary = await fetch(`https://registry.npmjs.org/next/canary`)
+      nextPackageJson['canary'] = await resCanary.json()
 
-    if (!response.version) {
+      const resRc = await fetch(`https://registry.npmjs.org/next/rc`)
+      nextPackageJson['rc'] = await resRc.json()
+
+      const resLatest = await fetch(`https://registry.npmjs.org/next/latest`)
+      nextPackageJson['latest'] = await resLatest.json()
+    } catch (error) {
+      console.error('Failed to fetch versions from npm registry.')
       return
     }
 
+    let showRc = true
+    if (nextPackageJson['latest'].version && nextPackageJson['rc'].version) {
+      showRc =
+        compareVersions(
+          nextPackageJson['rc'].version,
+          nextPackageJson['latest'].version
+        ) === 1
+    }
+
+    const choices = [
+      {
+        title: 'Canary',
+        value: 'canary',
+        description: `Experimental version with latest features (${nextPackageJson['canary'].version})`,
+      },
+    ]
+    if (showRc) {
+      choices.push({
+        title: 'Release Candidate',
+        value: 'rc',
+        description: `Pre-release version for final testing (${nextPackageJson['rc'].version})`,
+      })
+    }
+    choices.push({
+      title: 'Stable',
+      value: 'latest',
+      description: `Production-ready release (${nextPackageJson['latest'].version})`,
+    })
+
+    const initialVersionSpecifierIdx = await processCurrentVersion(showRc)
+
+    const response: Response = await prompts(
+      {
+        type: 'select',
+        name: 'version',
+        message: 'What Next.js version do you want to upgrade to?',
+        choices: choices,
+        initial: initialVersionSpecifierIdx,
+      },
+      { onCancel: () => process.exit(0) }
+    )
+
+    targetNextPackageJson = nextPackageJson[response.version]
     targetVersionSpecifier = response.version
   }
 
-  // TODO(LichuAcu): support Turborepo
   const appPackageJsonPath = path.resolve(process.cwd(), 'package.json')
   let appPackageJson = JSON.parse(fs.readFileSync(appPackageJsonPath, 'utf8'))
 
-  const targetNextPackageJson = nextPackageJson[targetVersionSpecifier]
-
-  if (targetNextPackageJson.version) {
-    if (compareVersions(targetNextPackageJson.version, '15.0.0-canary') >= 0) {
-      await suggestTurbopack(appPackageJson)
-    }
+  if (
+    targetNextPackageJson.version &&
+    compareVersions(targetNextPackageJson.version, '15.0.0-canary') >= 0
+  ) {
+    await suggestTurbopack(appPackageJson)
   }
 
   fs.writeFileSync(appPackageJsonPath, JSON.stringify(appPackageJson, null, 2))
 
-  // TODO(LichuAcu): keep the original styling of the file
-  console.log('\nUpdating depencies...\n')
+  console.log(
+    `Upgrading your project to ${chalk.blueBright('Next.js ' + targetVersionSpecifier)}...\n`
+  )
 
   let packageManager: PackageManager = getPackageManager(appPackageJson)
-  const dependencies = `react@${targetNextPackageJson.peerDependencies['react']} react-dom@${targetNextPackageJson.peerDependencies['react-dom']} next@${targetVersionSpecifier}`
+  const dependencies = [
+    `react@${targetNextPackageJson.peerDependencies['react']}`,
+    `@types/react@${targetNextPackageJson.devDependencies['@types/react']}`,
+    `react-dom@${targetNextPackageJson.peerDependencies['react-dom']}`,
+    `@types/react-dom@${targetNextPackageJson.devDependencies['@types/react-dom']}`,
+    `next@${targetNextPackageJson.version}`,
+  ].join(' ')
 
   let updateCommand
   switch (packageManager) {
@@ -128,7 +148,7 @@ async function run(): Promise<void> {
   fs.writeFileSync(appPackageJsonPath, JSON.stringify(appPackageJson, null, 2))
 
   console.log(
-    '\nYour Next.js project has been upgraded successfully. Time to ship! 🚢'
+    `\n${chalk.green('✔')} Your Next.js project has been upgraded successfully. ${chalk.bold('Time to ship! 🚢')}`
   )
 }
 
@@ -136,8 +156,8 @@ async function run(): Promise<void> {
  * Logs the current version and returns the index of the current version specifier
  * in the array ['canary', 'rc', 'latest']
  */
-async function processCurrentVersion(): Promise<number> {
-  // TODO(LichuAcu): support Turborepo
+async function processCurrentVersion(showRc: boolean): Promise<number> {
+  // TODO(LichuAcu): support other package managers
   const appLockFilePath = path.resolve(process.cwd(), 'pnpm-lock.yaml')
   if (fs.existsSync(appLockFilePath)) {
     const appLockFileContent = fs.readFileSync(appLockFilePath, 'utf8')
@@ -147,7 +167,7 @@ async function processCurrentVersion(): Promise<number> {
     if (appNextDependency) {
       if (appNextDependency.version) {
         console.log(
-          `You are currently using Next.js ${appNextDependency.version.split('(')[0]}`
+          `You are currently using ${chalk.blueBright('Next.js ' + appNextDependency.version.split('(')[0])}`
         )
       }
       if (appNextDependency.specifier) {
@@ -155,9 +175,9 @@ async function processCurrentVersion(): Promise<number> {
           case 'canary':
             return 0
           case 'rc':
-            return 1
+            return 1 // If rc is not available, will default to the latest version
           case 'latest':
-            return 2
+            return showRc ? 2 : 1
           default:
             return 0
         }
@@ -187,16 +207,22 @@ async function suggestTurbopack(packageJson: any): Promise<void> {
   const devScript = packageJson.scripts['dev']
   if (devScript.includes('--turbo')) return
 
-  const responseTurbopack = await prompts({
-    type: 'confirm',
-    name: 'enable',
-    message:
-      'Turbopack is stable for development in this version. Do you want to enable it?',
-    initial: true,
-  })
+  const responseTurbopack = await prompts(
+    {
+      type: 'confirm',
+      name: 'enable',
+      message:
+        'Turbopack is stable for development in this version. Do you want to enable it?',
+      initial: true,
+    },
+    {
+      onCancel: () => {
+        process.exit(0)
+      },
+    }
+  )
 
   // TODO(LichuAcu): handle ctrl-c
-
   if (!responseTurbopack.enable) {
     return
   }
