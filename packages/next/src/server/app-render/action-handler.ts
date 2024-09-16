2c14a66bcfccb6b3c2453215058b91bfdd5674f7
JJ Kasper <jj@jjsweb.site>
2024-09-16 12:26:39 -0700

    lint-fix

--- packages/next/src/server/app-render/action-handler.ts
+++ packages/next/src/server/app-render/action-handler.ts
@@ -218,7 +218,9 @@ async function createForwardedActionResponse(
       },
     })
 
-    if (response.headers.get('content-type')?.startsWith(RSC_CONTENT_TYPE_HEADER)) {
+    if (
+      response.headers.get('content-type')?.startsWith(RSC_CONTENT_TYPE_HEADER)
+    ) {
       // copy the headers from the redirect response to the response we're sending
       for (const [key, value] of response.headers) {
         if (!actionsForbiddenHeaders.includes(key)) {
@@ -340,7 +342,11 @@ async function createRedirectRenderResult(
         },
       })
 
-      if (response.headers.get('content-type')?.startsWith(RSC_CONTENT_TYPE_HEADER)) {
+      if (
+        response.headers
+          .get('content-type')
+          ?.startsWith(RSC_CONTENT_TYPE_HEADER)
+      ) {
         // copy the headers from the redirect response to the response we're sending
         for (const [key, value] of response.headers) {
           if (!actionsForbiddenHeaders.includes(key)) {
