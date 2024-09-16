vercel/next.js

commit 35706e30e6e35fe0acc9ed5232c77ce45dc8edd5

fix: eslint-plugin-next glob (#68773)
Closes https://github.com/vercel/next.js/issues/68752

---------

Co-authored-by: Sam Ko <sam@vercel.com>

packages/eslint-plugin-next/src/utils/get-root-dirs.ts
@@ -5,9 +5,9 @@ import type { Rule } from 'eslint'
  * Process a Next.js root directory glob.
  */
 const processRootDir = (rootDir: string): string[] => {
-  // Ensures we only match folders.
-  if (!rootDir.endsWith('/')) rootDir += '/'
-  return globSync(rootDir)
+  return globSync(rootDir, {
+    onlyDirectories: true,
+  })
 }
 
 /**

test/unit/eslint-plugin-next/no-html-link-for-pages.test.ts
@@ -5,12 +5,16 @@ import assert from 'assert'
 import path from 'path'
 
 const withCustomPagesDirectory = path.join(__dirname, 'with-custom-pages-dir')
+const withNestedPagesDirectory = path.join(__dirname, 'with-nested-pages-dir')
 
 const withoutPagesLinter = new Linter({
   cwd: path.join(__dirname, 'without-pages-dir'),
 })
 const withAppLinter = new Linter({
   cwd: path.join(__dirname, 'with-app-dir'),
+})
+const withNestedPagesLinter = new Linter({
+  cwd: withNestedPagesDirectory,
 })
 const withCustomPagesLinter = new Linter({
   cwd: withCustomPagesDirectory,
@@ -50,11 +54,22 @@ const linterConfigWithMultipleDirectories = {
     ],
   },
 }
+const linterConfigWithNestedContentRootDirDirectory = {
+  ...linterConfig,
+  settings: {
+    next: {
+      rootDir: path.join(withNestedPagesDirectory, 'demos/with-nextjs'),
+    },
+  },
+}
 
 withoutPagesLinter.defineRules({
   'no-html-link-for-pages': rule,
 })
 withAppLinter.defineRules({
+  'no-html-link-for-pages': rule,
+})
+withNestedPagesLinter.defineRules({
   'no-html-link-for-pages': rule,
 })
 withCustomPagesLinter.defineRules({
@@ -212,6 +227,19 @@ export class Blah extends Head {
 `
 
 describe('no-html-link-for-pages', function () {
+  it('does not print warning when there are "pages" or "app" directories with rootDir in context settings', function () {
+    const consoleSpy = jest.spyOn(console, 'warn').mockImplementation()
+    withNestedPagesLinter.verify(
+      validCode,
+      linterConfigWithNestedContentRootDirDirectory,
+      {
+        filename: 'foo.js',
+      }
+    )
+    expect(consoleSpy).not.toHaveBeenCalled()
+
+    consoleSpy.mockRestore()
+  })
   it('prints warning when there are no "pages" or "app" directories', function () {
     const consoleSpy = jest.spyOn(console, 'warn').mockImplementation()
     withoutPagesLinter.verify(validCode, linterConfig, {

test/unit/eslint-plugin-next/with-nested-pages-dir/demos/with-nextjs/pages/index.jsx
@@ -1,0 +1,1 @@
+export default () => {}

commit 6a69c358ecfc6f293676b2110a88fe3004166ea8

Fix eslint root dirs test on Windows (#70160)
The root dirs tests have been failing on Windows since
https://github.com/vercel/next.js/pull/68773 so this fixes to ensure we
normalize the slashes passed to `fast-glob` so it looks up as expected.

x-ref:
https://dev.azure.com/nextjs/next.js/_build/results?buildId=98629&view=logs&j=8af7cf9c-43a1-584d-6f5c-57bad8880974&t=7ae70e63-3625-50f4-6764-5b3e72b4bd7a

packages/eslint-plugin-next/src/utils/get-root-dirs.ts
@@ -5,7 +5,7 @@ import type { Rule } from 'eslint'
  * Process a Next.js root directory glob.
  */
 const processRootDir = (rootDir: string): string[] => {
-  return globSync(rootDir, {
+  return globSync(rootDir.replace(/\\/g, '/'), {
     onlyDirectories: true,
   })
 }