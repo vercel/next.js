7631340dfa279ef0fe7c7014b2ece9b371f7b20d
devjiwonchoi <devjiwonchoi@gmail.com>
2024-09-19 05:06:52 +0900

    fix: patch browser only

--- packages/next/src/lib/url.ts
+++ packages/next/src/lib/url.ts
@@ -24,7 +24,9 @@ export function stripNextRscUnionQuery(relativeUrl: string): string {
 // patch URL.canParse to be available in older browsers e.g. Safari 16
 // x-ref: https://github.com/vercel/next.js/pull/70215
 // x-ref: https://caniuse.com/?search=URL.canParse
-if (!('canParse' in URL)) {
+// Since URL.canParse is available in Node.js v18.17.0 and later,
+// we only need to patch it for the browser.
+if (typeof window !== 'undefined' && !('canParse' in URL)) {
   ;(URL as any).canParse = (url: string, base?: string): boolean => {
     try {
       return !!new URL(url, base)
