15fe5db1c98f1eefa34a2933bf5da3f5822582b3
Jiachi Liu <inbox@huozhi.im>
2024-09-23 00:35:56 +0200

    test: update SA tree-shaking test (#70327)

--- test/production/app-dir/actions-tree-shaking/shared-module-actions/shared-module-actions.test.ts
+++ test/production/app-dir/actions-tree-shaking/shared-module-actions/shared-module-actions.test.ts
@@ -5,31 +5,34 @@ import {
 } from '../_testing/utils'
 
 // TODO: revisit when we have a better side-effect free transform approach for server action
-describe.skip('actions-tree-shaking - shared-module-actions', () => {
-  const { next } = nextTestSetup({
-    files: __dirname,
-  })
-
-  if (process.env.TEST_EDGE) {
-    markLayoutAsEdge(next)
-  }
-
-  it('should not have the unused action in the manifest', async () => {
-    const actionsRoutesState = await getActionsRoutesStateByRuntime(next)
-
-    expect(actionsRoutesState).toMatchObject({
-      'app/server/one/page': {
-        rsc: 1,
-      },
-      'app/server/two/page': {
-        rsc: 1,
-      },
-      'app/client/one/page': {
-        'action-browser': 1,
-      },
-      'app/client/two/page': {
-        'action-browser': 1,
-      },
+;(process.env.TURBOPACK ? describe : describe.skip)(
+  'actions-tree-shaking - shared-module-actions',
+  () => {
+    const { next } = nextTestSetup({
+      files: __dirname,
     })
-  })
-})
+
+    if (process.env.TEST_EDGE) {
+      markLayoutAsEdge(next)
+    }
+
+    it('should not have the unused action in the manifest', async () => {
+      const actionsRoutesState = await getActionsRoutesStateByRuntime(next)
+
+      expect(actionsRoutesState).toMatchObject({
+        'app/server/one/page': {
+          rsc: 1,
+        },
+        'app/server/two/page': {
+          rsc: 1,
+        },
+        'app/client/one/page': {
+          'action-browser': 1,
+        },
+        'app/client/two/page': {
+          'action-browser': 1,
+        },
+      })
+    })
+  }
+)
