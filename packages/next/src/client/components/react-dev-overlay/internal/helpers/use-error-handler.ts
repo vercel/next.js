543924d1b31655a79f207b4a0ca823cb06832936
Jiachi Liu <inbox@huozhi.im>
2024-09-24 01:00:05 +0200

    refactor: reorganize client error listeners

--- packages/next/src/client/components/react-dev-overlay/internal/helpers/use-error-handler.ts
+++ packages/next/src/client/components/react-dev-overlay/internal/helpers/use-error-handler.ts
@@ -1,23 +1,14 @@
 import { useEffect } from 'react'
-
-import { isNextRouterError } from '../../../is-next-router-error'
 import { isHydrationError } from '../../../is-hydration-error'
 import { attachHydrationErrorState } from './attach-hydration-error-state'
 
 export type ErrorHandler = (error: Error) => void
 
-if (typeof window !== 'undefined') {
-  try {
-    // Increase the number of stack frames on the client
-    Error.stackTraceLimit = 50
-  } catch {}
-}
-
 let hasHydrationError = false
 const errorQueue: Array<Error> = []
-const rejectionQueue: Array<Error> = []
 const errorHandlers: Array<ErrorHandler> = []
-const rejectionHandlers: Array<ErrorHandler> = []
+export const rejectionQueue: Array<Error> = []
+export const rejectionHandlers: Array<ErrorHandler> = []
 
 export function handleClientError(error: unknown) {
   if (!error || !(error instanceof Error) || typeof error.stack !== 'string') {
@@ -37,48 +28,6 @@ export function handleClientError(error: unknown) {
   for (const handler of errorHandlers) {
     handler(error)
   }
-}
-if (typeof window !== 'undefined') {
-  // These event handlers must be added outside of the hook because there is no
-  // guarantee that the hook will be alive in a mounted component in time to
-  // when the errors occur.
-  // uncaught errors go through reportError
-  window.addEventListener(
-    'error',
-    (event: WindowEventMap['error']): void | boolean => {
-      if (isNextRouterError(event.error)) {
-        event.preventDefault()
-        return false
-      }
-      handleClientError(event.error)
-    }
-  )
-
-  window.addEventListener(
-    'unhandledrejection',
-    (ev: WindowEventMap['unhandledrejection']): void => {
-      const reason = ev?.reason
-      if (isNextRouterError(reason)) {
-        ev.preventDefault()
-        return
-      }
-
-      if (
-        !reason ||
-        !(reason instanceof Error) ||
-        typeof reason.stack !== 'string'
-      ) {
-        // A non-error was thrown, we don't have anything to show. :-(
-        return
-      }
-
-      const e = reason
-      rejectionQueue.push(e)
-      for (const handler of rejectionHandlers) {
-        handler(e)
-      }
-    }
-  )
 }
 
 export function useErrorHandler(
