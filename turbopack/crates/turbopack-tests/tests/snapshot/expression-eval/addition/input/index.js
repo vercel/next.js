beee0ef2da226a0018fee07891df20ce203cec12
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-09-23 10:58:02 +0900

    Use execution test

--- turbopack/crates/turbopack-tests/tests/snapshot/expression-eval/addition/input/index.js
+++
@@ -1,7 +0,0 @@
-console.log("side effect: before")
-
-if(1 +2 !==3){
-  throw new Error("1 + 2 !== 3");
-}
-
-console.log("side effect: after");
--- turbopack/crates/turbopack-tests/tests/snapshot/expression-eval/addition/output/b1abf_turbopack-tests_tests_snapshot_expression-eval_addition_input_index_f5f483.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/b1abf_turbopack-tests_tests_snapshot_expression-eval_addition_input_index_f5f483.js",
-    {},
-    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_expression-eval_addition_input_index_1c85a5.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/expression-eval/addition/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
