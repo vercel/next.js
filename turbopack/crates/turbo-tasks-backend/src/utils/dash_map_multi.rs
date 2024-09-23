2978c8dade53d7a44d3497c5c0ed1d6d4681f5d7
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-23 13:04:46 +0200

    fix deadlock

--- turbopack/crates/turbo-tasks-backend/src/utils/dash_map_multi.rs
+++ turbopack/crates/turbo-tasks-backend/src/utils/dash_map_multi.rs
@@ -137,13 +137,17 @@ where
         )
     } else {
         let (mut guard1, mut guard2) = loop {
-            let g1 = shards[s1].write();
-            if let Some(g2) = shards[s2].try_write() {
-                break (g1, g2);
+            {
+                let g1 = shards[s1].write();
+                if let Some(g2) = shards[s2].try_write() {
+                    break (g1, g2);
+                }
             }
-            let g2 = shards[s2].write();
-            if let Some(g1) = shards[s1].try_write() {
-                break (g1, g2);
+            {
+                let g2 = shards[s2].write();
+                if let Some(g1) = shards[s1].try_write() {
+                    break (g1, g2);
+                }
             }
         };
         let e1 = guard1
