4622f6bcc043d928d27f4e49c70023fd8fb9cb92
Janka Uryga <lolzatu2@gmail.com>
2024-09-24 17:01:27 +0200

    test: check "@next/request-context" in route handlers and middleware

--- test/e2e/app-dir/next-after-app/app/nodejs/provided-request-context/page/page.js
+++ test/e2e/app-dir/next-after-app/app/nodejs/provided-request-context/page/page.js
@@ -1,9 +1,9 @@
 import { unstable_after as after } from 'next/server'
-import { cliLog } from '../../../utils/log'
+import { cliLog } from '../../../../utils/log'
 
 export default function Page() {
   after(() => {
-    cliLog({ source: '[page] /provided-request-context' })
+    cliLog({ source: '[page] /provided-request-context/page' })
   })
   return <div>provided-request-context</div>
 }
