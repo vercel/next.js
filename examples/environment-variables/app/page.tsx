1fc933b4eb1f84e44cbe233522f7985ff01c6bb0
neoFinch <vaibhav27.neo@gmail.com>
2024-09-25 10:55:33 +0530

    added necessary comments describing scope of environment variables

--- examples/environment-variables/app/page.tsx
+++ examples/environment-variables/app/page.tsx
@@ -11,6 +11,10 @@ const Code = ({ children }: CodeProps) => (
 
 const IndexPage = () => {
 
+  // The following console.log statements will only be executed on Node.js. 
+  // Check the terminal to see the environment variables
+  // Using the variables below in the browser will return `undefined`. 
+  // Next.js doesn't expose environment variables unless they start with `NEXT_PUBLIC_`
   console.log("[Node.js only] ENV_VARIABLE:", process.env.ENV_VARIABLE);
   console.log(
     "[Node.js only] ENV_LOCAL_VARIABLE:",
