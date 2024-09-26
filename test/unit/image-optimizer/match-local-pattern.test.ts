6fb98b61292dd70aef200550690ca0419e57f1c9
Steven <steven@ceriously.com>
2024-09-26 13:08:45 -0400

    fix tests

--- test/unit/image-optimizer/match-local-pattern.test.ts
+++ test/unit/image-optimizer/match-local-pattern.test.ts
@@ -5,159 +5,94 @@ import {
   hasLocalMatch as hasMatch,
 } from 'next/dist/shared/lib/match-local-pattern'
 
-const m = (p: LocalPattern, urlPathAndQuery: string) => matchLocalPattern(p, new URL(urlPathAndQuery, 'http://n'))
+const m = (p: LocalPattern, urlPathAndQuery: string) =>
+  matchLocalPattern(p, new URL(urlPathAndQuery, 'http://n'))
 
 describe('matchLocalPattern', () => {
-
   it('should match anything when no pattern is defined', () => {
     const p = {} as const
-    expect(m(p, ('/'))).toBe(true)
-    expect(m(p, ('/path'))).toBe(true)
-    expect(m(p, ('/path/to'))).toBe(true)
-    expect(m(p, ('/path/to/file'))).toBe(true)
-    expect(m(p, ('/path/to/file.txt'))).toBe(true)
-    expect(m(p, ('/path/to/file?q=1'))).toBe(false)
-    expect(m(p, ('/path/to/file?q=1&a=two'))).toBe(false)
-  })
-
-  it('should match literal pathname and any search query string', () => {
-    const p = {
-      pathname: '/path/to/file',
-    } as const
-    expect(m(p, ('/'))).toBe(false)
-    expect(m(p, ('/path'))).toBe(false)
-    expect(m(p, ('/path/to'))).toBe(false)
-    expect(m(p, ('/path/to/file'))).toBe(true)
-    expect(m(p, ('/path/to/file.txt'))).toBe(false)
-    expect(m(p, ('/path/to/file?q=1'))).toBe(true)
-    expect(m(p, ('/path/to/file?q=1&a=two'))).toBe(true)
+    expect(m(p, '/')).toBe(true)
+    expect(m(p, '/path')).toBe(true)
+    expect(m(p, '/path/to')).toBe(true)
+    expect(m(p, '/path/to/file')).toBe(true)
+    expect(m(p, '/path/to/file.txt')).toBe(true)
+    expect(m(p, '/path/to/file?q=1')).toBe(true)
+    expect(m(p, '/path/to/file?q=1&a=two')).toBe(true)
   })
 
   it('should match any path without a search query string', () => {
     const p = {
       search: '',
     } as const
-    expect(m(p, ('/'))).toBe(true)
-    expect(m(p, ('/path'))).toBe(true)
-    expect(m(p, ('/path/to'))).toBe(true)
-    expect(m(p, ('/path/to/file'))).toBe(true)
-    expect(m(p, ('/path/to/file.txt'))).toBe(true)
-    expect(m(p, ('/path/to/file?q=1'))).toBe(false)
-    expect(m(p, ('/path/to/file?q=1&a=two'))).toBe(false)
+    expect(m(p, '/')).toBe(true)
+    expect(m(p, '/path')).toBe(true)
+    expect(m(p, '/path/to')).toBe(true)
+    expect(m(p, '/path/to/file')).toBe(true)
+    expect(m(p, '/path/to/file.txt')).toBe(true)
+    expect(m(p, '/path/to/file?q=1')).toBe(false)
+    expect(m(p, '/path/to/file?q=1&a=two')).toBe(false)
+    expect(m(p, '/path/to/file.txt?q=1&a=two')).toBe(false)
   })
 
-  /*
-
-  it('should match literal protocol, hostname, port, pathname, search', () => {
+  it('should match literal pathname and any search query string', () => {
     const p = {
-      protocol: 'https',
-      hostname: 'example.com',
-      port: '42',
       pathname: '/path/to/file',
-      search: '?q=1&a=two&s=!@$^&-_+/()[]{};:~',
     } as const
-    expect(m(p, (':42'))).toBe(false)
-    expect(m(p, ('.uk:42'))).toBe(false)
-    expect(m(p, ('https://sub.example.com:42'))).toBe(false)
-    expect(m(p, (':42/path'))).toBe(false)
-    expect(m(p, (':42/path/to'))).toBe(false)
-    expect(m(p, (':42/file'))).toBe(false)
-    expect(m(p, (':42/path/to/file'))).toBe(false)
-    expect(m(p, ('http://example.com:42/path/to/file'))).toBe(false)
-    expect(m(p, ('ftp://example.com:42/path/to/file'))).toBe(false)
-    expect(m(p, (''))).toBe(false)
-    expect(m(p, ('.uk'))).toBe(false)
-    expect(m(p, ('https://sub.example.com'))).toBe(false)
-    expect(m(p, ('/path'))).toBe(false)
-    expect(m(p, ('/path/to'))).toBe(false)
-    expect(m(p, ('/path/to/file'))).toBe(false)
-    expect(m(p, ('/path/to/file?q=1'))).toBe(false)
-    expect(m(p, ('http://example.com/path/to/file'))).toBe(false)
-    expect(m(p, ('ftp://example.com/path/to/file'))).toBe(false)
-    expect(m(p, (':81/path/to/file'))).toBe(false)
-    expect(m(p, (':81/path/to/file?q=1'))).toBe(false)
-    expect(m(p, (':42/path/to/file?q=1'))).toBe(false)
-    expect(m(p, (':42/path/to/file?q=1&a=two'))).toBe(
-      false
-    )
-    expect(
-      m(p, (':42/path/to/file?q=1&a=two&s'))
-    ).toBe(false)
-    expect(
-      m(p, (':42/path/to/file?q=1&a=two&s='))
-    ).toBe(false)
-    expect(
-      m(p, (':42/path/to/file?q=1&a=two&s=!@'))
-    ).toBe(false)
-    expect(
-      m(
-        p,
-        (
-          ':42/path/to/file?q=1&a=two&s=!@$^&-_+/()[]{};:~'
-        )
-      )
-    ).toBe(true)
-    expect(
-      m(
-        p,
-        (
-          ':42/path/to/file?q=1&s=!@$^&-_+/()[]{};:~&a=two'
-        )
-      )
-    ).toBe(false)
-    expect(
-      m(
-        p,
-        (
-          ':42/path/to/file?a=two&q=1&s=!@$^&-_+/()[]{};:~'
-        )
-      )
-    ).toBe(false)
+    expect(m(p, '/')).toBe(false)
+    expect(m(p, '/path')).toBe(false)
+    expect(m(p, '/path/to')).toBe(false)
+    expect(m(p, '/path/to/file')).toBe(true)
+    expect(m(p, '/path/to/file.txt')).toBe(false)
+    expect(m(p, '/path/to/file?q=1')).toBe(true)
+    expect(m(p, '/path/to/file?q=1&a=two')).toBe(true)
+    expect(m(p, '/path/to/file.txt?q=1&a=two')).toBe(false)
+  })
+
+  it('should match pathname with double asterisk', () => {
+    const p = {
+      pathname: '/path/to/**',
+    } as const
+    expect(m(p, '/')).toBe(false)
+    expect(m(p, '/path')).toBe(false)
+    expect(m(p, '/path/to')).toBe(true)
+    expect(m(p, '/path/to/file')).toBe(true)
+    expect(m(p, '/path/to/file.txt')).toBe(true)
+    expect(m(p, '/path/to/file?q=1')).toBe(true)
+    expect(m(p, '/path/to/file?q=1&a=two')).toBe(true)
+    expect(m(p, '/path/to/file.txt?q=1&a=two')).toBe(true)
   })
 
   it('should properly work with hasMatch', () => {
-    const url = ('')
-    expect(hasMatch([], [], url)).toBe(false)
-    expect(hasMatch(['foo.com'], [], url)).toBe(false)
-    expect(hasMatch(['example.com'], [], url)).toBe(true)
-    expect(hasMatch(['**.example.com'], [], url)).toBe(false)
-    expect(hasMatch(['*.example.com'], [], url)).toBe(false)
-    expect(hasMatch(['*.example.com'], [], url)).toBe(false)
-    expect(hasMatch([], [{ hostname: 'foo.com' }], url)).toBe(false)
+    const url = '/path/to/file?q=1&a=two'
+    expect(hasMatch(undefined, url)).toBe(true)
+    expect(hasMatch([], url)).toBe(false)
+    expect(hasMatch([{ pathname: '/path' }], url)).toBe(false)
+    expect(hasMatch([{ pathname: '/path/to' }], url)).toBe(false)
+    expect(hasMatch([{ pathname: '/path/to/file' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/path/to/file' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/path/to/file', search: '' }], url)).toBe(
+      false
+    )
+    expect(hasMatch([{ pathname: '/path/to/file', search: '?q=1' }], url)).toBe(
+      false
+    )
     expect(
-      hasMatch([], [{ hostname: 'foo.com' }, { hostname: 'example.com' }], url)
+      hasMatch([{ pathname: '/path/to/file', search: '?q=1&a=two' }], url)
     ).toBe(true)
-    expect(
-      hasMatch([], [{ hostname: 'example.com', pathname: '/act123/**' }], url)
-    ).toBe(false)
+    expect(hasMatch([{ pathname: '/path/**' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/path/to/**' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/path/to/f*' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/path/to/*le' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/path/*/file' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/*/to/file' }], url)).toBe(true)
+    expect(hasMatch([{ pathname: '/foo' }, { pathname: '/bar' }], url)).toBe(
+      false
+    )
     expect(
       hasMatch(
-        ['example.com'],
-        [{ hostname: 'example.com', pathname: '/act123/**' }],
+        [{ pathname: '/foo' }, { pathname: '/bar' }, { pathname: '/path/**' }],
         url
       )
     ).toBe(true)
-    expect(
-      hasMatch([], [{ protocol: 'https', hostname: 'example.com' }], url)
-    ).toBe(true)
-    expect(
-      hasMatch([], [{ protocol: 'http', hostname: 'example.com' }], url)
-    ).toBe(false)
-    expect(
-      hasMatch(
-        ['example.com'],
-        [{ protocol: 'http', hostname: 'example.com' }],
-        url
-      )
-    ).toBe(true)
-    expect(
-      hasMatch(
-        ['foo.com'],
-        [{ protocol: 'http', hostname: 'example.com' }],
-        url
-      )
-    ).toBe(false)
   })
-
-  */
 })
