bb474558dc65c21258dbf0e62ebcca74376d28e2
archanaagivale30 <archana.agivale@qed42.com>
2024-09-26 13:52:48 +0530

    video sitemap all props check test case added

--- test/e2e/app-dir/metadata-dynamic-routes/index.test.ts
+++ test/e2e/app-dir/metadata-dynamic-routes/index.test.ts
@@ -117,13 +117,40 @@ describe('app dir - metadata dynamic routes', () => {
 
     it('should support videos in sitemap', async () => {
       const xml = await (await next.fetch('/sitemap-video/sitemap.xml')).text()
+      expect(xml).toMatchInlineSnapshot(`
+        "<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+        <url>
+        <loc>https://example.com/about</loc>
+        <video:video>
+        <video:title>example</video:title>
+        <video:thumbnail_loc>https://example.com/image.jpg</video:thumbnail_loc>
+        <video:description>this is the description</video:description>
+        <video:content_loc>http://streamserver.example.com/video123.mp4</video:content_loc>
+        <video:player_loc>https://www.example.com/videoplayer.php?video=123</video:player_loc>
+        <video:duration>2</video:duration>
+        <video:view_count>50</video:view_count>
+        <video:tag>summer</video:tag>
+        <video:rating>4</video:rating>
+        <video:expiration_date>2025-09-16</video:expiration_date>
+        <video:publication_date>2024-09-16</video:publication_date>
+        <video:family_friendly>yes</video:family_friendly>
+        <video:requires_subscription>no</video:requires_subscription>
+        <video:live>no</video:live>
+        <video:restriction relationship="allow">IE GB US CA</video:restriction>
+        <video:platform relationship="allow">web</video:platform>
+        <video:uploader info="https://www.example.com/users/grillymcgrillerson">GrillyMcGrillerson</video:uploader>
+        </video:video></url>
+        </urlset>
+        "
+      `)
 
-      expect(xml).toContain(
-        `<video:video>\n<video:title>example</video:title>\n<video:thumbnail_loc>https://example.com/image.jpg</video:thumbnail_loc>\n<video:description>this is the description</video:description>\n<video:content_loc>http://streamserver.example.com/video123.mp4</video:content_loc>\n<video:player_loc>https://www.example.com/videoplayer.php?video=123</video:player_loc>\n<video:duration>2</video:duration>\n<video:view_count>50</video:view_count>\n<video:tag>summer</video:tag>\n<video:rating>4</video:rating>\n<video:expiration_date>2025-09-16</video:expiration_date>\n<video:publication_date>2024-09-16</video:publication_date>\n<video:family_friendly>yes</video:family_friendly>\n<video:requires_subscription>no</video:requires_subscription>\n<video:live>no</video:live>\n</video:video>`
-      )
-      expect(xml).toContain(
-        `<video:video>\n<video:title>example</video:title>\n<video:thumbnail_loc>https://example.com/image.jpg</video:thumbnail_loc>\n<video:description>this is the description</video:description>\n<video:content_loc>http://streamserver.example.com/video123.mp4</video:content_loc>\n<video:player_loc>https://www.example.com/videoplayer.php?video=123</video:player_loc>\n<video:duration>2</video:duration>\n<video:view_count>50</video:view_count>\n<video:tag>summer</video:tag>\n<video:rating>4</video:rating>\n<video:expiration_date>2025-09-16</video:expiration_date>\n<video:publication_date>2024-09-16</video:publication_date>\n<video:family_friendly>yes</video:family_friendly>\n<video:requires_subscription>no</video:requires_subscription>\n<video:live>no</video:live>\n</video:video>`
-      )
+      // expect(xml).toContain(
+      //   `<video:video>\n<video:title>example</video:title>\n<video:thumbnail_loc>https://example.com/image.jpg</video:thumbnail_loc>\n<video:description>this is the description</video:description>\n<video:content_loc>http://streamserver.example.com/video123.mp4</video:content_loc>\n<video:player_loc>https://www.example.com/videoplayer.php?video=123</video:player_loc>\n<video:duration>2</video:duration>\n<video:view_count>50</video:view_count>\n<video:tag>summer</video:tag>\n<video:rating>4</video:rating>\n<video:expiration_date>2025-09-16</video:expiration_date>\n<video:publication_date>2024-09-16</video:publication_date>\n<video:family_friendly>yes</video:family_friendly>\n<video:requires_subscription>no</video:requires_subscription>\n<video:live>no</video:live>\n<video:restriction relationship=\"allow\">IE GB US CA</video:restriction>\n<video:platform relationship=\"allow\">web</video:platform>\n<video:uploader info=\"https://www.example.com/users/grillymcgrillerson\">GrillyMcGrillerson</video:uploader>\n</video:video>`
+      // )
+      // expect(xml).toContain(
+      //   `<video:video>\n<video:title>example</video:title>\n<video:thumbnail_loc>https://example.com/image.jpg</video:thumbnail_loc>\n<video:description>this is the description</video:description>\n<video:content_loc>http://streamserver.example.com/video123.mp4</video:content_loc>\n<video:player_loc>https://www.example.com/videoplayer.php?video=123</video:player_loc>\n<video:duration>2</video:duration>\n<video:view_count>50</video:view_count>\n<video:tag>summer</video:tag>\n<video:rating>4</video:rating>\n<video:expiration_date>2025-09-16</video:expiration_date>\n<video:publication_date>2024-09-16</video:publication_date>\n<video:family_friendly>yes</video:family_friendly>\n<video:requires_subscription>no</video:requires_subscription>\n<video:live>no</video:live>\n<video:restriction relationship=\"allow\">IE GB US CA</video:restriction>\n<video:platform relationship=\"allow\">web</video:platform>\n<video:uploader info=\"https://www.example.com/users/grillymcgrillerson\">GrillyMcGrillerson</video:uploader>\n</video:video>`
+      // )
     })
 
     if (isNextStart) {
