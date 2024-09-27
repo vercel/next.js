39d3551375231fcd6aa198ce4caefd8ea7da4995
devjiwonchoi <devjiwonchoi@gmail.com>
2024-09-27 20:14:27 +0900

    uncomment test

--- test/integration/next-codemod/upgrade/prompt.test.ts
+++ test/integration/next-codemod/upgrade/prompt.test.ts
@@ -1,5 +1,6 @@
 import { join } from 'node:path'
-import { createApp, useTempDir } from '../utils'
+import { check } from 'next-test-utils'
+import { createApp, useTempDir, runNextCodemodPrompt } from '../utils'
 
 describe('next-codemod upgrade prompt', () => {
   let canaryVersion: string | undefined
@@ -31,26 +32,26 @@ describe('next-codemod upgrade prompt', () => {
       })
       expect(app.exitCode).toBe(0)
 
-      // const cp = runNextCodemodPrompt(['upgrade'], { cwd: appDir })
-      // await new Promise<void>(async (resolve) => {
-      //   cp.on('exit', async (exitCode) => {
-      //     expect(exitCode).toBe(0)
-      //     resolve()
-      //   })
+      const cp = runNextCodemodPrompt(['upgrade'], { cwd: appDir })
+      await new Promise<void>(async (resolve) => {
+        cp.on('exit', async (exitCode) => {
+          expect(exitCode).toBe(0)
+          resolve()
+        })
 
-      //   let output = ''
-      //   cp.stdout.on('data', (data) => {
-      //     output += data
-      //     process.stdout.write(data)
-      //   })
+        let output = ''
+        cp.stdout.on('data', (data) => {
+          output += data
+          process.stdout.write(data)
+        })
 
-      //   await check(
-      //     () => output,
-      //     /Which Next.js release do you want to upgrade to/
-      //   )
-      //   // select canary
-      //   cp.stdin.write(`\n`)
-      // })
+        await check(
+          () => output,
+          /Which Next.js release do you want to upgrade to/
+        )
+        // select canary
+        cp.stdin.write(`\n`)
+      })
 
       const pkg = require(join(appDir, 'package.json'))
       if (canaryVersion) {
@@ -69,26 +70,26 @@ describe('next-codemod upgrade prompt', () => {
       })
       expect(app.exitCode).toBe(0)
 
-      // const cp = runNextCodemodPrompt(['upgrade'], { cwd: appDir })
-      // await new Promise<void>(async (resolve) => {
-      //   cp.on('exit', async (exitCode) => {
-      //     expect(exitCode).toBe(0)
-      //     resolve()
-      //   })
+      const cp = runNextCodemodPrompt(['upgrade'], { cwd: appDir })
+      await new Promise<void>(async (resolve) => {
+        cp.on('exit', async (exitCode) => {
+          expect(exitCode).toBe(0)
+          resolve()
+        })
 
-      //   let output = ''
-      //   cp.stdout.on('data', (data) => {
-      //     output += data
-      //     process.stdout.write(data)
-      //   })
+        let output = ''
+        cp.stdout.on('data', (data) => {
+          output += data
+          process.stdout.write(data)
+        })
 
-      //   await check(
-      //     () => output,
-      //     /Which Next.js release do you want to upgrade to/
-      //   )
-      //   // cursor below, select rc
-      //   cp.stdin.write('\u001b[B\n')
-      // })
+        await check(
+          () => output,
+          /Which Next.js release do you want to upgrade to/
+        )
+        // cursor below, select rc
+        cp.stdin.write('\u001b[B\n')
+      })
 
       const pkg = require(join(appDir, 'package.json'))
       if (rcVersion) {
@@ -107,26 +108,26 @@ describe('next-codemod upgrade prompt', () => {
       })
       expect(app.exitCode).toBe(0)
 
-      // const cp = runNextCodemodPrompt(['upgrade'], { cwd: appDir })
-      // await new Promise<void>(async (resolve) => {
-      //   cp.on('exit', async (exitCode) => {
-      //     expect(exitCode).toBe(0)
-      //     resolve()
-      //   })
+      const cp = runNextCodemodPrompt(['upgrade'], { cwd: appDir })
+      await new Promise<void>(async (resolve) => {
+        cp.on('exit', async (exitCode) => {
+          expect(exitCode).toBe(0)
+          resolve()
+        })
 
-      //   let output = ''
-      //   cp.stdout.on('data', (data) => {
-      //     output += data
-      //     process.stdout.write(data)
-      //   })
+        let output = ''
+        cp.stdout.on('data', (data) => {
+          output += data
+          process.stdout.write(data)
+        })
 
-      //   await check(
-      //     () => output,
-      //     /Which Next.js release do you want to upgrade to/
-      //   )
-      //   // cursor below twice, select latest
-      //   cp.stdin.write('\u001b[B\u001b[B\n')
-      // })
+        await check(
+          () => output,
+          /Which Next.js release do you want to upgrade to/
+        )
+        // cursor below twice, select latest
+        cp.stdin.write('\u001b[B\u001b[B\n')
+      })
 
       const pkg = require(join(appDir, 'package.json'))
       const installedNextVersion = require(
