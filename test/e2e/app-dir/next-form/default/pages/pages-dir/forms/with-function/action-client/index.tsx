2f1dc0f2745a6f3278af3edbcdc3b487400cffdc
Janka Uryga <lolzatu2@gmail.com>
2024-09-27 14:51:57 +0200

    test: fix pages router navigations

--- test/e2e/app-dir/next-form/default/pages/pages-dir/forms/with-function/action-client/index.tsx
+++ test/e2e/app-dir/next-form/default/pages/pages-dir/forms/with-function/action-client/index.tsx
@@ -10,13 +10,12 @@ export default function Page() {
   const [, dispatch] = useActionState(() => {
     const to = destination + '?' + new URLSearchParams({ query })
 
-    // TODO(lubieowoce): this doesn't work unless wrapped in a startTransition...
-    //   return startTransition(() => {
-    //     router.push(to)
-    //   })
+    // TODO(form): this doesn't work unless wrapped in a startTransition...
     // why is this necessary here?
     // without it, the URL updates, but we stay on the old page...
-    router.push(to)
+    return React.startTransition(() => {
+      router.push(to)
+    })
   }, undefined)
 
   const [query, setQuery] = useState('')
--- test/e2e/app-dir/next-form/default/pages/pages-dir/forms/with-function/button-formaction-client/index.tsx
+++ test/e2e/app-dir/next-form/default/pages/pages-dir/forms/with-function/button-formaction-client/index.tsx
@@ -27,6 +27,13 @@ function NavigateButton({
   ...props
 }: { to: string } & ComponentProps<'button'>) {
   const router = useRouter()
-  const [, dispatch] = useActionState(() => router.push(to), undefined)
+  const [, dispatch] = useActionState(() => {
+    // TODO(form): this doesn't work unless wrapped in a startTransition...
+    // why is this necessary here?
+    // without it, the URL updates, but we stay on the old page...
+    React.startTransition(() => {
+      router.push(to)
+    })
+  }, undefined)
   return <button type="submit" formAction={dispatch} {...props} />
 }
