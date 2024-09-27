bb04472254d5ec4b3e2ffc5637ba6f5d2b204799
devjiwonchoi <devjiwonchoi@gmail.com>
2024-09-27 16:50:59 +0900

    try 3

--- test/integration/next-codemod/utils.ts
+++ test/integration/next-codemod/utils.ts
@@ -3,23 +3,8 @@ import { join } from 'node:path'
 import { fetchViaHTTP, findPort, killApp, launchApp } from 'next-test-utils'
 import { spawn, SpawnOptions } from 'node:child_process'
 
-export const NEXT_CODEMOD_PATH = (() => {
-  try {
-    return require.resolve('@next/codemod/bin/next-codemod.js')
-  } catch (error) {
-    console.warn('Failed to resolve @next/codemod, using fallback path')
-    return join(
-      __dirname,
-      '..',
-      '..',
-      'packages',
-      'next-codemod',
-      'bin',
-      'next-codemod.js'
-    )
-  }
-})()
-export const CNA_PATH = require.resolve('create-next-app/dist/index.js')
+const NEXT_CODEMOD_PATH = require.resolve('@next/codemod/bin/next-codemod.js')
+const CNA_PATH = require.resolve('create-next-app/dist/index.js')
 
 export const runNextCodemod = (args: string[], options: execa.Options) => {
   console.log(`[TEST] $ ${NEXT_CODEMOD_PATH} ${args.join(' ')}`)
