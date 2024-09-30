vercel/next.js

commit 025f5b6bf6f7728cb6bd04ac210346bb0c35f863

Update revalidateTag to batch tags in one request (#65296)
As discussed this collects all `revalidateTag` calls and invokes in a
single request to avoid race conditions and overhead from multiple
pending requests.

x-ref: [slack
thread](https://vercel.slack.com/archives/C0676QZBWKS/p1714688045037509?thread_ts=1710902198.529179&cid=C0676QZBWKS)

Closes NEXT-3306

packages/next/src/server/app-render/app-render.tsx
@@ -1374,9 +1374,12 @@ async function renderToHTMLOrFlightImpl(
 
   // If we have pending revalidates, wait until they are all resolved.
   if (staticGenerationStore.pendingRevalidates) {
-    options.waitUntil = Promise.all(
-      Object.values(staticGenerationStore.pendingRevalidates)
-    )
+    options.waitUntil = Promise.all([
+      staticGenerationStore.incrementalCache?.revalidateTag(
+        staticGenerationStore.revalidatedTags || []
+      ),
+      ...Object.values(staticGenerationStore.pendingRevalidates || {}),
+    ])
   }
 
   addImplicitTags(staticGenerationStore)

packages/next/src/server/web/spec-extension/revalidate.ts
@@ -68,15 +68,6 @@ function revalidate(tag: string, expression: string) {
     store.revalidatedTags.push(tag)
   }
 
-  if (!store.pendingRevalidates) {
-    store.pendingRevalidates = {}
-  }
-  store.pendingRevalidates[tag] = store.incrementalCache
-    .revalidateTag?.(tag)
-    .catch((err) => {
-      console.error(`revalidate failed for ${tag}`, err)
-    })
-
   // TODO: only revalidate if the path matches
   store.pathWasRevalidated = true
 }

packages/next/src/server/app-render/action-handler.ts
@@ -113,9 +113,12 @@ async function addRevalidationHeader(
     requestStore: RequestStore
   }
 ) {
-  await Promise.all(
-    Object.values(staticGenerationStore.pendingRevalidates || [])
-  )
+  await Promise.all([
+    staticGenerationStore.incrementalCache?.revalidateTag(
+      staticGenerationStore.revalidatedTags || []
+    ),
+    ...Object.values(staticGenerationStore.pendingRevalidates || {}),
+  ])
 
   // If a tag was revalidated, the client router needs to invalidate all the
   // client router cache as they may be stale. And if a path was revalidated, the
@@ -480,9 +483,12 @@ export async function handleAction({
 
       if (isFetchAction) {
         res.statusCode = 500
-        await Promise.all(
-          Object.values(staticGenerationStore.pendingRevalidates || [])
-        )
+        await Promise.all([
+          staticGenerationStore.incrementalCache?.revalidateTag(
+            staticGenerationStore.revalidatedTags || []
+          ),
+          ...Object.values(staticGenerationStore.pendingRevalidates || {}),
+        ])
 
         const promise = Promise.reject(error)
         try {
@@ -867,9 +873,12 @@ export async function handleAction({
 
     if (isFetchAction) {
       res.statusCode = 500
-      await Promise.all(
-        Object.values(staticGenerationStore.pendingRevalidates || [])
-      )
+      await Promise.all([
+        staticGenerationStore.incrementalCache?.revalidateTag(
+          staticGenerationStore.revalidatedTags || []
+        ),
+        ...Object.values(staticGenerationStore.pendingRevalidates || {}),
+      ])
       const promise = Promise.reject(err)
       try {
         // we need to await the promise to trigger the rejection early

packages/next/src/server/future/route-modules/app-route/module.ts
@@ -395,11 +395,14 @@ export class AppRouteRouteModule extends RouteModule<
                     context.renderOpts.fetchMetrics =
                       staticGenerationStore.fetchMetrics
 
-                    context.renderOpts.waitUntil = Promise.all(
-                      Object.values(
-                        staticGenerationStore.pendingRevalidates || []
-                      )
-                    )
+                    context.renderOpts.waitUntil = Promise.all([
+                      staticGenerationStore.incrementalCache?.revalidateTag(
+                        staticGenerationStore.revalidatedTags || []
+                      ),
+                      ...Object.values(
+                        staticGenerationStore.pendingRevalidates || {}
+                      ),
+                    ])
 
                     addImplicitTags(staticGenerationStore)
                     ;(context.renderOpts as any).fetchTags =

packages/next/src/server/lib/incremental-cache/fetch-cache.ts
@@ -126,10 +126,16 @@ export default class FetchCache implements CacheHandler {
     memoryCache?.reset()
   }
 
-  public async revalidateTag(tag: string) {
+  public async revalidateTag(
+    ...args: Parameters<CacheHandler['revalidateTag']>
+  ) {
+    let [tags] = args
+    tags = typeof tags === 'string' ? [tags] : tags
     if (this.debug) {
-      console.log('revalidateTag', tag)
+      console.log('revalidateTag', tags)
     }
+
+    if (!tags.length) return
 
     if (Date.now() < rateLimitedUntil) {
       if (this.debug) {
@@ -140,9 +146,9 @@ export default class FetchCache implements CacheHandler {
 
     try {
       const res = await fetch(
-        `${
-          this.cacheEndpoint
-        }/v1/suspense-cache/revalidate?tags=${encodeURIComponent(tag)}`,
+        `${this.cacheEndpoint}/v1/suspense-cache/revalidate?tags=${tags
+          .map((tag) => encodeURIComponent(tag))
+          .join(',')}`,
         {
           method: 'POST',
           headers: this.headers,
@@ -160,7 +166,7 @@ export default class FetchCache implements CacheHandler {
         throw new Error(`Request failed with status ${res.status}.`)
       }
     } catch (err) {
-      console.warn(`Failed to revalidate tag ${tag}`, err)
+      console.warn(`Failed to revalidate tag ${tags}`, err)
     }
   }
 

packages/next/src/server/lib/incremental-cache/file-system-cache.ts
@@ -105,9 +105,18 @@ export default class FileSystemCache implements CacheHandler {
     if (this.debug) console.log('loadTagsManifest', tagsManifest)
   }
 
-  public async revalidateTag(tag: string) {
+  public async revalidateTag(
+    ...args: Parameters<CacheHandler['revalidateTag']>
+  ) {
+    let [tags] = args
+    tags = typeof tags === 'string' ? [tags] : tags
+
     if (this.debug) {
-      console.log('revalidateTag', tag)
+      console.log('revalidateTag', tags)
+    }
+
+    if (tags.length === 0) {
+      return
     }
 
     // we need to ensure the tagsManifest is refreshed
@@ -118,9 +127,11 @@ export default class FileSystemCache implements CacheHandler {
       return
     }
 
-    const data = tagsManifest.items[tag] || {}
-    data.revalidatedAt = Date.now()
-    tagsManifest.items[tag] = data
+    for (const tag of tags) {
+      const data = tagsManifest.items[tag] || {}
+      data.revalidatedAt = Date.now()
+      tagsManifest.items[tag] = data
+    }
 
     try {
       await this.fs.mkdir(path.dirname(this.tagsManifestPath))

packages/next/src/server/lib/incremental-cache/index.ts
@@ -58,7 +58,9 @@ export class CacheHandler {
     ..._args: Parameters<IncrementalCache['set']>
   ): Promise<void> {}
 
-  public async revalidateTag(_tag: string): Promise<void> {}
+  public async revalidateTag(
+    ..._args: Parameters<IncrementalCache['revalidateTag']>
+  ): Promise<void> {}
 
   public resetRequestCache(): void {}
 }
@@ -280,7 +282,7 @@ export class IncrementalCache implements IncrementalCacheType {
     return unlockNext
   }
 
-  async revalidateTag(tag: string) {
+  async revalidateTag(tags: string | string[]): Promise<void> {
     if (
       process.env.__NEXT_INCREMENTAL_CACHE_IPC_PORT &&
       process.env.__NEXT_INCREMENTAL_CACHE_IPC_KEY &&
@@ -296,7 +298,7 @@ export class IncrementalCache implements IncrementalCacheType {
       })
     }
 
-    return this.cacheHandler?.revalidateTag?.(tag)
+    return this.cacheHandler?.revalidateTag?.(tags)
   }
 
   // x-ref: https://github.com/facebook/react/blob/2655c9354d8e1c54ba888444220f63e836925caa/packages/react/src/ReactFetch.js#L23

commit cdb78b4e61a252ee9ed4105212809db42569b5f2

Fix `revalidateTag()` behaviour when invoked in server components (#70446)
Fixes #70403

### What?
When `revalidateTag()` is called directly in server components, the
cache is not purged for the corresponding tags. Reproduction steps
available in #70403

### How?
Check
[app-render.tsx](https://github.com/vercel/next.js/compare/canary...abhi12299:fix-revalidatetag-rsc?expand=1#diff-a3e2e024db1faa1b501e0dd6040eaaf0d931cb9878ae0fb0f4c3658daa982768)
This issue was introduced in #65296 in this file:
[revalidate.ts](https://github.com/vercel/next.js/pull/65296/files#diff-7f0cb5bb30d44b9153d724e31c25859b9aab6cc258b35563a1d9464cd0688283).
The lines removed from the file resulted in the revalidation checks to
be skipped when there is an RSC request.

Also fixed checks on `pendingRevalidates` to also check for
`revalidatedTags`.

---------

Co-authored-by: JJ Kasper <jj@jjsweb.site>

packages/next/src/server/app-render/app-render.tsx
@@ -514,6 +514,22 @@ async function generateDynamicFlightRenderResult(
       onError,
     }
   )
+  await waitAtLeastOneReactRenderTask()
+
+  if (
+    ctx.staticGenerationStore.pendingRevalidates ||
+    ctx.staticGenerationStore.revalidatedTags ||
+    ctx.staticGenerationStore.pendingRevalidateWrites
+  ) {
+    const promises = Promise.all([
+      ctx.staticGenerationStore.incrementalCache?.revalidateTag(
+        ctx.staticGenerationStore.revalidatedTags || []
+      ),
+      ...Object.values(ctx.staticGenerationStore.pendingRevalidates || {}),
+      ...(ctx.staticGenerationStore.pendingRevalidateWrites || []),
+    ])
+    ctx.renderOpts.waitUntil = (p) => promises.then(() => p)
+  }
 
   return new FlightRenderResult(flightReadableStream, {
     fetchMetrics: ctx.staticGenerationStore.fetchMetrics,
@@ -1086,7 +1102,8 @@ async function renderToHTMLOrFlightImpl(
     // If we have pending revalidates, wait until they are all resolved.
     if (
       staticGenerationStore.pendingRevalidates ||
-      staticGenerationStore.pendingRevalidateWrites
+      staticGenerationStore.pendingRevalidateWrites ||
+      staticGenerationStore.revalidatedTags
     ) {
       options.waitUntil = Promise.all([
         staticGenerationStore.incrementalCache?.revalidateTag(
@@ -1195,7 +1212,8 @@ async function renderToHTMLOrFlightImpl(
     // If we have pending revalidates, wait until they are all resolved.
     if (
       staticGenerationStore.pendingRevalidates ||
-      staticGenerationStore.pendingRevalidateWrites
+      staticGenerationStore.pendingRevalidateWrites ||
+      staticGenerationStore.revalidatedTags
     ) {
       options.waitUntil = Promise.all([
         staticGenerationStore.incrementalCache?.revalidateTag(

test/e2e/app-dir/revalidatetag-rsc/app/RevalidateViaForm.tsx
@@ -1,0 +1,17 @@
+'use client'
+
+import { revalidate } from './actions/revalidate'
+
+export default function RevalidateViaForm({ tag }: { tag: string }) {
+  const handleRevalidate = async () => {
+    await revalidate(tag)
+  }
+
+  return (
+    <form action={handleRevalidate}>
+      <button type="submit" id="submit-form" className="underline">
+        Revalidate via form
+      </button>
+    </form>
+  )
+}

test/e2e/app-dir/revalidatetag-rsc/app/actions/revalidate.ts
@@ -1,0 +1,11 @@
+'use server'
+
+import { revalidateTag } from 'next/cache'
+
+export const revalidate = async (
+  tag: string
+): Promise<{ revalidated: boolean }> => {
+  revalidateTag(tag)
+
+  return { revalidated: true }
+}

test/e2e/app-dir/revalidatetag-rsc/app/layout.tsx
@@ -1,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/e2e/app-dir/revalidatetag-rsc/app/page.tsx
@@ -1,0 +1,24 @@
+import RevalidateViaForm from './RevalidateViaForm'
+import Link from 'next/link'
+
+export default async function Page() {
+  const data = await fetch(
+    'https://next-data-api-endpoint.vercel.app/api/random',
+    {
+      next: {
+        tags: ['data'],
+        revalidate: false,
+      },
+    }
+  ).then((res) => res.text())
+
+  return (
+    <div>
+      <span id="data">{data}</span>
+      <RevalidateViaForm tag="data" />
+      <Link href="/revalidate_via_page?tag=data" id="revalidate-via-page">
+        Revalidate via page
+      </Link>
+    </div>
+  )
+}

test/e2e/app-dir/revalidatetag-rsc/app/revalidate_via_page/page.tsx
@@ -1,0 +1,24 @@
+'use server'
+
+import Link from 'next/link'
+import { revalidateTag } from 'next/cache'
+
+const RevalidateViaPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ tag: string }>
+}) => {
+  const { tag } = await searchParams
+  revalidateTag(tag)
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <pre>Tag [{tag}] has been revalidated</pre>
+      <Link href="/" id="home">
+        To Home
+      </Link>
+    </div>
+  )
+}
+
+export default RevalidateViaPage

test/e2e/app-dir/revalidatetag-rsc/next.config.js
@@ -1,0 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {}
+
+module.exports = nextConfig

test/e2e/app-dir/revalidatetag-rsc/revalidatetag-rsc.test.ts
@@ -1,0 +1,38 @@
+import { nextTestSetup } from 'e2e-utils'
+import { retry } from 'next-test-utils'
+
+describe('revalidateTag-rsc', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should revalidate fetch cache if revalidateTag invoked via server action', async () => {
+    const browser = await next.browser('/')
+    const randomNumber = await browser.elementById('data').text()
+    await browser.refresh()
+    const randomNumber2 = await browser.elementById('data').text()
+    expect(randomNumber).toEqual(randomNumber2)
+
+    await browser.elementByCss('#submit-form').click()
+
+    await retry(async () => {
+      const randomNumber3 = await browser.elementById('data').text()
+      expect(randomNumber3).not.toEqual(randomNumber)
+    })
+  })
+
+  it('should revalidate fetch cache if revalidateTag invoked via server component', async () => {
+    const browser = await next.browser('/')
+    const randomNumber = await browser.elementById('data').text()
+    await browser.refresh()
+    const randomNumber2 = await browser.elementById('data').text()
+    expect(randomNumber).toEqual(randomNumber2)
+
+    await browser.elementByCss('#revalidate-via-page').click()
+    await browser.waitForElementByCss('#home')
+    await browser.elementByCss('#home').click()
+    await browser.waitForElementByCss('#data')
+    const randomNumber3 = await browser.elementById('data').text()
+    expect(randomNumber3).not.toEqual(randomNumber)
+  })
+})