0301de306feba24906a4eac50b6148ebf1647b02
Abhishek <abhi.9811206369@gmail.com>
2024-10-02 20:19:47 +0530

    add tests

---
+++ test/development/app-dir/scroll-behaviour-on-not-found-client-navigation/app/not-found.tsx
@@ -0,0 +1,18 @@
+const Page404 = async () => {
+  return (
+    <div
+      className="not-found"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1500px',
+        minHeight: '100%',
+      }}
+    >
+      <p>Not found page</p>
+      <p>Not found page</p>
+    </div>
+  )
+}
+
+export default Page404
---
+++ test/development/app-dir/scroll-behaviour-on-not-found-client-navigation/scroll-behaviour-on-not-found-client-navigation.test.ts
@@ -0,0 +1,44 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('scroll-behaviour-on-not-found-client-navigation', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should scroll to top when visiting non-existent route', async () => {
+    const browser = await next.browser('/')
+
+    // scroll to bottom of the / page
+    await browser.eval('window.scrollTo({top: document.body.scrollHeight})')
+    const isAtBottom = await browser.eval(
+      'window.innerHeight + window.scrollY >= document.documentElement.scrollHeight'
+    )
+    expect(isAtBottom).toBe(true)
+
+    await browser.elementByCss('.non-existent-route').click()
+    await browser.waitForElementByCss('.not-found')
+    const isAtTop = await browser.eval('window.scrollY === 0')
+    expect(isAtTop).toBe(true)
+  })
+
+  it('should scroll to top when visiting route that triggers notFound()', async () => {
+    const browser = await next.browser('/', {
+      headless: false,
+    })
+
+    // scroll to bottom of the / page
+    await browser.eval('window.scrollTo({top: document.body.scrollHeight})')
+    // await new Promise((r) => setTimeout(r, 20000))
+    const isAtBottom = await browser.eval(
+      'window.innerHeight + window.scrollY >= document.documentElement.scrollHeight'
+    )
+    // await new Promise((r) => setTimeout(r, 10000))
+    expect(isAtBottom).toBe(true)
+
+    await browser.elementByCss('.trigger-not-found').click()
+    await browser.waitForElementByCss('.not-found')
+    // await new Promise((r) => setTimeout(r, 20000))
+    const isAtTop = await browser.eval('window.scrollY === 0')
+    expect(isAtTop).toBe(true)
+  })
+})
