eb64b5b6ae6d2c1b23685a72e9c2a6aa188e774b
Mark Knichel <7355009+mknichel@users.noreply.github.com>
2024-10-02 14:59:24 -0700

    [Testing] Export utilities to help test middleware

---
+++ packages/next/src/server/testing/index.ts
@@ -0,0 +1,1 @@
+export * from './middleware-testing-utils'
---
+++ packages/next/src/server/testing/middleware-testing-utils.ts
@@ -0,0 +1,53 @@
+import type { IncomingHttpHeaders } from 'http'
+import { getMiddlewareMatchers } from '../../build/analysis/get-page-static-info'
+import type { Middleware } from '../../lib/load-custom-routes'
+import { getMiddlewareRouteMatcher } from '../../shared/lib/router/utils/middleware-route-matcher'
+import type { NextConfig } from '../config-shared'
+import { MockedRequest } from '../lib/mock-request'
+import { NodeNextRequest } from '../base-http/node'
+
+export interface MiddlewareSourceConfig {
+  matcher?: string | string[] | Middleware[]
+}
+
+/**
+ * Checks whether the middleware config will match the provide URL and request
+ * information such as headers and cookies. This function is useful for
+ * unit tests to assert that middleware is matching (and therefore executing)
+ * only when it should be.
+ */
+export function doesMiddlewareMatch({
+  config,
+  url,
+  headers,
+  cookies,
+  nextConfig,
+}: {
+  config: MiddlewareSourceConfig
+  url: string
+  headers?: IncomingHttpHeaders
+  cookies?: Record<string, string>
+  nextConfig?: NextConfig
+}): boolean {
+  if (!config.matcher) {
+    return true
+  }
+  const matchers = getMiddlewareMatchers(config.matcher, nextConfig ?? {})
+  const routeMatchFn = getMiddlewareRouteMatcher(matchers)
+  const { pathname, searchParams } = new URL(url, 'http://localhost')
+  if (!headers) {
+    headers = {}
+  }
+  if (cookies) {
+    headers = {
+      ...headers,
+      cookie: Object.entries(cookies)
+        .map(([name, value]) => `${name}=${value}`)
+        .join(';'),
+    }
+  }
+  const request = new NodeNextRequest(
+    new MockedRequest({ url, headers, method: 'GET' })
+  )
+  return routeMatchFn(pathname, request, Object.fromEntries(searchParams))
+}
