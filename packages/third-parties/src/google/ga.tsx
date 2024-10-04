7711dde937afefee2dea9cd51c66fc9d3706ca81
Lorenzo Galassi <104760516+LorenzoGalassi@users.noreply.github.com>
2024-10-04 20:27:42 +0200

    Update ga.tsxUpdate ga.tsx to support standalone usage of sendGAEvent
    The current implementation prevents the sendGAEvent function from being used if Google Tag Manager is imported with a custom script and not with the GoogleAnalytics function, this is because the currDataLayerName variable is initialized to undefined and initialized to “dataLayer” (or a string passed in as prop) only in the GoogleAnalytics function and, in the sendGAEvent function, it is checked to see if that value is undefined and, if so, a warning is triggered. By using the sendGAEvent function standalone, it is impossible for the currDataLayerName variable to be initialized and so the warning always starts, preventing its use.

--- packages/third-parties/src/google/ga.tsx
+++ packages/third-parties/src/google/ga.tsx
@@ -11,10 +11,10 @@ declare global {
   }
 }
 
-let currDataLayerName: string | undefined = undefined
+let currDataLayerName: string = 'dataLayer'
 
 export function GoogleAnalytics(props: GAParams) {
-  const { gaId, debugMode, dataLayerName = 'dataLayer', nonce } = props
+  const { gaId, debugMode, dataLayerName, nonce } = props
 
   if (currDataLayerName === undefined) {
     currDataLayerName = dataLayerName
@@ -57,11 +57,6 @@ export function GoogleAnalytics(props: GAParams) {
 }
 
 export function sendGAEvent(..._args: Object[]) {
-  if (currDataLayerName === undefined) {
-    console.warn(`@next/third-parties: GA has not been initialized`)
-    return
-  }
-
   if (window[currDataLayerName]) {
     window[currDataLayerName].push(arguments)
   } else {
