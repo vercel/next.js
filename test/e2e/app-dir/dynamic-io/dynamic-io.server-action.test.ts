7201f4901d6bbe2996235896a3d0713797ce47d5
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-04 12:10:30 +0200

    Move new test into existing `dynamic-io` test app

--- test/e2e/app-dir/dynamic-io/dynamic-io.server-action.test.ts
+++ test/e2e/app-dir/dynamic-io/dynamic-io.server-action.test.ts
@@ -1,13 +1,13 @@
 import { nextTestSetup } from 'e2e-utils'
 import { retry } from 'next-test-utils'
 
-describe('dynamic-io-server-action', () => {
+describe('dynamic-io', () => {
   const { next } = nextTestSetup({
     files: __dirname,
   })
 
   it('should not fail decoding server action arguments', async () => {
-    const browser = await next.browser('/')
+    const browser = await next.browser('/server-action')
     expect(await browser.elementByCss('p').text()).toBe('initial')
     await browser.elementByCss('button').click()
 
