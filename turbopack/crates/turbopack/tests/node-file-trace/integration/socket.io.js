ff90deb14c6229ce7f2377ba633c71475dfe6aec
Tobias Koppers <tobias.koppers@googlemail.com>
2024-10-04 09:52:59 +0200

    randomize port

--- turbopack/crates/turbopack/tests/node-file-trace/integration/socket.io.js
+++ turbopack/crates/turbopack/tests/node-file-trace/integration/socket.io.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const io = require("socket.io");
-const opts = { port: 3000 };
+const opts = { port: 4000 + Math.floor(Math.random() * 1000) };
 const server = http.createServer((req, res) => {});
 server.listen(opts.port);
 server.close(() => {
