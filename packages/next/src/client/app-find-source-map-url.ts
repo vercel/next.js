9698f9c9e623861659d76cc124dc2025bc65de93
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-06 22:17:07 +0200

    Implement source map dev middleware for Webpack

--- packages/next/src/client/app-find-source-map-url.ts
+++ packages/next/src/client/app-find-source-map-url.ts
@@ -1,4 +1,11 @@
-// TODO: Will be implemented later.
-export function findSourceMapURL(_filename: string): string | null {
-  return null
+// TODO: DEV-only
+const basePath = process.env.__NEXT_ROUTER_BASEPATH || ''
+const pathname = `${basePath}/__nextjs_source-map`
+
+export function findSourceMapURL(filename: string): string | null {
+  const url = new URL(pathname, document.location.origin)
+
+  url.searchParams.set('filename', filename)
+
+  return url.href
 }
--- packages/next/src/server/dev/hot-reloader-webpack.ts
+++ packages/next/src/server/dev/hot-reloader-webpack.ts
@@ -7,7 +7,10 @@ import type { UrlObject } from 'url'
 import type { RouteDefinition } from '../route-definitions/route-definition'
 
 import { webpack, StringXor } from 'next/dist/compiled/webpack/webpack'
-import { getOverlayMiddleware } from '../../client/components/react-dev-overlay/server/middleware'
+import {
+  getOverlayMiddleware,
+  getSourceMapMiddleware,
+} from '../../client/components/react-dev-overlay/server/middleware'
 import { WebpackHotMiddleware } from './hot-middleware'
 import { join, relative, isAbsolute, posix } from 'path'
 import {
@@ -1501,6 +1504,11 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
     this.middlewares = [
       getOverlayMiddleware({
         rootDirectory: this.dir,
+        stats: () => this.clientStats,
+        serverStats: () => this.serverStats,
+        edgeServerStats: () => this.edgeServerStats,
+      }),
+      getSourceMapMiddleware({
         stats: () => this.clientStats,
         serverStats: () => this.serverStats,
         edgeServerStats: () => this.edgeServerStats,
