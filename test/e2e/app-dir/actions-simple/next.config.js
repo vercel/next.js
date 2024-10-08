fea812a6e7d21f0c713d3584766961bd5cc700d7
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-08 10:21:12 +0200

    Remove `findSourceMapURL` mock

--- test/e2e/app-dir/actions-simple/next.config.js
+++ test/e2e/app-dir/actions-simple/next.config.js
@@ -3,20 +3,8 @@
  */
 
 const nextConfig = {
-  webpack(config) {
-    config.resolve.alias['next/dist/client/app-find-source-map-url'] =
-      require.resolve('./find-source-map-url-webpack-mock.ts')
-
-    return config
-  },
   experimental: {
     ppr: true,
-    turbo: {
-      resolveAlias: {
-        'next/dist/client/app-find-source-map-url':
-          './find-source-map-url-turbopack-mock.ts',
-      },
-    },
   },
 }
 
