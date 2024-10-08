30e6794b69a46aef30b275fe7a0222909c95b75b
Josh Story <story@hey.com>
2024-10-08 16:06:50 -0700

    Refactor prerender workUnitStores (#70986)
    This continues the refactor by adding an explicit type for PPR without
    dynamicIO. It gets rid of `isDynamicIOPrerender` helper in favor or just
    using a new discriminated type. It narrows the function arguments where
    possible. dynamicIO prerenders encompass both PPR and non-PPR since
    prerendering semnatics aren't different even when PPR is enabled. It
    also sets up passing an AbortSignal into the dynamicIO prerender stores.
    We don't use it yet but we will end up relying on it to clean up hanging
    promises after the render is complete

--- packages/next/src/server/node-environment-extensions/utils.tsx
+++ packages/next/src/server/node-environment-extensions/utils.tsx
@@ -1,17 +1,10 @@
 import { workAsyncStorage } from '../../client/components/work-async-storage.external'
-import {
-  isDynamicIOPrerender,
-  workUnitAsyncStorage,
-} from '../app-render/work-unit-async-storage.external'
+import { workUnitAsyncStorage } from '../app-render/work-unit-async-storage.external'
 import { abortOnSynchronousDynamicDataAccess } from '../app-render/dynamic-rendering'
 
 export function io(expression: string) {
   const workUnitStore = workUnitAsyncStorage.getStore()
-  if (
-    workUnitStore &&
-    workUnitStore.type === 'prerender' &&
-    isDynamicIOPrerender(workUnitStore)
-  ) {
+  if (workUnitStore && workUnitStore.type === 'prerender') {
     const workStore = workAsyncStorage.getStore()
     const route = workStore ? workStore.route : ''
     if (workStore) {
