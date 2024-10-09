8029fb84acf8f82cd76c4fb73cc363cfa8a0749e
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-09 17:36:34 +0200

    In production, omit `findSourceMapURL` from main chunk

--- packages/next/src/client/app-find-source-map-url.ts
+++ packages/next/src/client/app-find-source-map-url.ts
@@ -1,14 +1,16 @@
-// TODO: DEV-only
 const basePath = process.env.__NEXT_ROUTER_BASEPATH || ''
 const pathname = `${basePath}/__nextjs_source-map`
 
-export function findSourceMapURL(filename: string): string | null {
-  const url = new URL(pathname, document.location.origin)
+export const findSourceMapURL =
+  process.env.NODE_ENV === 'development'
+    ? function findSourceMapURL(filename: string): string | null {
+        const url = new URL(pathname, document.location.origin)
 
-  url.searchParams.set(
-    'filename',
-    filename.replace(new RegExp(`^${document.location.origin}`), '')
-  )
+        url.searchParams.set(
+          'filename',
+          filename.replace(new RegExp(`^${document.location.origin}`), '')
+        )
 
-  return url.href
-}
+        return url.href
+      }
+    : undefined
