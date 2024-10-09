54097c1c74da0ac5391b4ae08c6c1f7de6e01e24
eps1lon <sebastian.silbermann@vercel.com>
2024-10-09 14:44:57 +0200

    Lazily require `react-server-dom-webpack` in `next-flight-loader`
    Testing if this unblocks sync.

--- packages/next/src/build/webpack/loaders/next-flight-loader/action-client-wrapper.ts
+++ packages/next/src/build/webpack/loaders/next-flight-loader/action-client-wrapper.ts
@@ -10,10 +10,25 @@ export { findSourceMapURL } from 'next/dist/client/app-find-source-map-url'
 // also use the same Edge build to create the reference. For the client bundle,
 // we use the default and let Webpack to resolve it to the correct version.
 // 1: https://github.com/vercel/next.js/blob/16eb80b0b0be13f04a6407943664b5efd8f3d7d0/packages/next/src/server/app-render/use-flight-response.tsx#L24-L26
-export const createServerReference = (
-  (!!process.env.NEXT_RUNTIME
-    ? // eslint-disable-next-line import/no-extraneous-dependencies
-      require('react-server-dom-webpack/client.edge')
-    : // eslint-disable-next-line import/no-extraneous-dependencies
-      require('react-server-dom-webpack/client')) as typeof import('react-server-dom-webpack/client')
-).createServerReference
+// TODO: Figure out why we can't just eagerly require `react-serer-dom-webpack/client` here. 
+export function createServerReference(
+  id: string,
+  callServer: unknown,
+  encodeFormAction: unknown,
+  findSourceMapURL: unknown,
+  functionName: unknown
+) {
+  // Since we're using the Edge build of Flight client for SSR [1], here we need to
+  // also use the same Edge build to create the reference. For the client bundle,
+  // we use the default and let Webpack to resolve it to the correct version.
+  // 1: https://github.com/vercel/next.js/blob/16eb80b0b0be13f04a6407943664b5efd8f3d7d0/packages/next/src/server/app-render/use-flight-response.tsx#L24-L26
+  const { createServerReference: createServerReferenceImpl } = (
+    !!process.env.NEXT_RUNTIME
+      ? // eslint-disable-next-line import/no-extraneous-dependencies
+        require('react-server-dom-webpack/client.edge')
+      : // eslint-disable-next-line import/no-extraneous-dependencies
+        require('react-server-dom-webpack/client')
+  ) as typeof import('react-server-dom-webpack/client')
+
+  return createServerReferenceImpl(id, callServer, encodeFormAction, findSourceMapURL, functionName)
+}
