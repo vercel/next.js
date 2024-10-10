78fc478a7e0240d8c1ad45bff959ce6502c92a04
Jiachi Liu <inbox@huozhi.im>
2024-10-10 23:35:56 +0200

    add type file handling

--- packages/next-codemod/lib/parser.ts
+++ packages/next-codemod/lib/parser.ts
@@ -1,6 +1,20 @@
 import j from 'jscodeshift'
+import babylonParse from 'jscodeshift/parser/babylon'
+import tsOptions from 'jscodeshift/parser/tsOptions'
+
+const dtsOptions = {
+  ...tsOptions,
+  plugins: [
+    ...tsOptions.plugins.filter((plugin) => plugin !== 'typescript'),
+    ['typescript', { dts: true }],
+  ],
+}
 
 function createParserFromPath(filePath: string): j.JSCodeshift {
+  const isTypeFile = /\.d\.(m|c)?ts$/.test(filePath)
+  if (isTypeFile) {
+    return j.withParser(babylonParse(dtsOptions))
+  }
   // In Next.js js files could also contain jsx syntax, applying with tsx parser is most compatible.
   // This is similar to how Next.js itself handles tsx and ts files with SWC.
   const isTs = filePath.endsWith('.ts')
