cfcfababdd1b578c08ca4115f5142e3e6dacee2d
Tobias Koppers <tobias.koppers@googlemail.com>
2024-10-11 08:13:14 +0200

    [Turbopack] add unstable prefix to persistent caching option and force to provide an expected level of stability (#71090)
    ### What?
    
    Rename option to `unstablePersistentCaching` and require to pass a
    persistent caching version (currently 1).
    
    We increase the number when making improvements to persistent caching.
    Smaller numbers are accepted too, but providing a larger number will
    fail the build.
    
    e. g. if we document `experimental.turbo.unstablePersistentCaching: 5`,
    this will only work for next version that ship at least persistent
    caching with version 5. Older version would be rejected.

--- packages/next/src/server/dev/turbopack-utils.ts
+++ packages/next/src/server/dev/turbopack-utils.ts
@@ -1159,3 +1159,15 @@ export function normalizedPageToTurbopackStructureRoute(
   }
   return entrypointKey
 }
+
+export function isPersistentCachingEnabled(
+  config: NextConfigComplete
+): boolean {
+  const unstableValue = config.experimental.turbo?.unstablePersistentCaching
+  if (typeof unstableValue === 'number' && unstableValue > 1) {
+    throw new Error(
+      'Persistent caching in this version of Turbopack is not as stable as expected. Upgrade to a newer version of Turbopack to use this feature with the expected stability.'
+    )
+  }
+  return !!unstableValue
+}
