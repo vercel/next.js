efdee0150bb013aec247dc0cd697e94e544f1c98
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-11 13:58:13 +0200

    Include filename in error of source map can't be parsed

--- packages/next/src/client/components/react-dev-overlay/internal/helpers/get-source-map-from-file.ts
+++ packages/next/src/client/components/react-dev-overlay/internal/helpers/get-source-map-from-file.ts
@@ -17,7 +17,9 @@ export async function getSourceMapFromFile(
   try {
     fileContents = await fs.readFile(filename, 'utf-8')
   } catch (error) {
-    throw new Error('Failed to read file contents.', { cause: error })
+    throw new Error(`Failed to read file contents of ${filename}.`, {
+      cause: error,
+    })
   }
 
   const sourceUrl = getSourceMapUrl(fileContents)
@@ -32,17 +34,23 @@ export async function getSourceMapFromFile(
     try {
       buffer = dataUriToBuffer(sourceUrl)
     } catch (error) {
-      throw new Error('Failed to parse source map URL.', { cause: error })
+      throw new Error(`Failed to parse source map URL for ${filename}.`, {
+        cause: error,
+      })
     }
 
     if (buffer.type !== 'application/json') {
-      throw new Error(`Unknown source map type: ${buffer.typeFull}.`)
+      throw new Error(
+        `Unknown source map type for ${filename}: ${buffer.typeFull}.`
+      )
     }
 
     try {
       return JSON.parse(buffer.toString())
     } catch (error) {
-      throw new Error('Failed to parse source map.', { cause: error })
+      throw new Error(`Failed to parse source map for ${filename}.`, {
+        cause: error,
+      })
     }
   }
 
@@ -53,6 +61,8 @@ export async function getSourceMapFromFile(
 
     return JSON.parse(sourceMapContents.toString())
   } catch (error) {
-    throw new Error('Failed to parse source map.', { cause: error })
+    throw new Error(`Failed to parse source map ${sourceMapFilename}.`, {
+      cause: error,
+    })
   }
 }
