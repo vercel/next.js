8ea77d92d2a2b463c92225d013d78879c4285567
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-14 14:55:30 +0900

    Update test refs

--- turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-named-all/output/4c35f_tests_snapshot_basic-tree-shake_import-named-all_input_index_8d9d5e.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/4c35f_tests_snapshot_basic-tree-shake_import-named-all_input_index_8d9d5e.js",
-    {},
-    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_basic-tree-shake_import-named-all_input_d3f17c._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-named-all/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
