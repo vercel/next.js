921c5d7555c8930d6cf03a4f30d09a8d1974e20a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-14 20:02:55 +0200

    Add unit test for complex resolve errors

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/imports/resolve_error_complex/output/b1abf_turbopack-tests_tests_snapshot_imports_resolve_error_complex_input_index_a0451a.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/b1abf_turbopack-tests_tests_snapshot_imports_resolve_error_complex_input_index_a0451a.js",
+    {},
+    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_imports_resolve_error_complex_input_index_bc8bed.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/imports/resolve_error_complex/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
