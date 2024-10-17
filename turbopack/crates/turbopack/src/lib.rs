d9ec381a63700da16db23b157e286164b395cf82
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-17 15:03:43 +0200

    Don't replace externals in tracing

--- turbopack/crates/turbopack/src/lib.rs
+++ turbopack/crates/turbopack/src/lib.rs
@@ -335,6 +335,9 @@ pub struct ModuleAssetContext {
     pub resolve_options_context: Vc<ResolveOptionsContext>,
     pub layer: Vc<RcStr>,
     transition: Option<ResolvedVc<Box<dyn Transition>>>,
+    /// Whether to replace external resolutions with CachedExternalModules. Used by
+    /// enable_externals_tracing to handle transitive external dependencies.
+    replace_externals: bool,
 }
 
 #[turbo_tasks::value_impl]
@@ -354,6 +357,7 @@ impl ModuleAssetContext {
             resolve_options_context,
             transition: None,
             layer,
+            replace_externals: true,
         })
     }
 
@@ -373,6 +377,26 @@ impl ModuleAssetContext {
             resolve_options_context,
             layer,
             transition: Some(transition),
+            replace_externals: true,
+        })
+    }
+
+    #[turbo_tasks::function]
+    fn new_without_replace_externals(
+        transitions: Vc<TransitionOptions>,
+        compile_time_info: Vc<CompileTimeInfo>,
+        module_options_context: Vc<ModuleOptionsContext>,
+        resolve_options_context: Vc<ResolveOptionsContext>,
+        layer: Vc<RcStr>,
+    ) -> Vc<Self> {
+        Self::cell(ModuleAssetContext {
+            transitions,
+            compile_time_info,
+            module_options_context,
+            resolve_options_context,
+            transition: None,
+            layer,
+            replace_externals: false,
         })
     }
 
@@ -638,6 +662,27 @@ Read more: https://nextjs.org/docs/app/api-reference/next-config-js/turbo#webpac
     ))
 }
 
+#[turbo_tasks::function]
+fn externals_tracing_module_context() -> Vc<ModuleAssetContext> {
+    let env = Environment::new(Value::new(ExecutionEnvironment::NodeJsLambda(
+        NodeJsEnvironment::default().cell(),
+    )));
+
+    let resolve_options = ResolveOptionsContext {
+        emulate_environment: Some(env),
+        loose_errors: true,
+        ..Default::default()
+    };
+
+    ModuleAssetContext::new_without_replace_externals(
+        Default::default(),
+        CompileTimeInfo::builder(env).cell(),
+        ModuleOptionsContext::default().cell(),
+        resolve_options.cell(),
+        Vc::cell("externals-tracing".into()),
+    )
+}
+
 #[turbo_tasks::value_impl]
 impl AssetContext for ModuleAssetContext {
     #[turbo_tasks::function]
@@ -732,32 +777,8 @@ impl AssetContext for ModuleAssetContext {
                                             .await?
                                             .enable_externals_tracing =>
                                     {
-                                        let external_context = {
-                                            let env = Environment::new(Value::new(
-                                                ExecutionEnvironment::NodeJsLambda(
-                                                    NodeJsEnvironment::default().cell(),
-                                                ),
-                                            ));
-                                            let compile_time_info =
-                                                CompileTimeInfo::builder(env).cell();
-
-                                            let resolve_options = ResolveOptionsContext {
-                                                emulate_environment: Some(env),
-                                                loose_errors: true,
-                                                ..Default::default()
-                                            };
-
-                                            ModuleAssetContext::new(
-                                                Default::default(),
-                                                compile_time_info,
-                                                ModuleOptionsContext::default().cell(),
-                                                resolve_options.cell(),
-                                                Vc::cell("external_tracing".into()),
-                                            )
-                                        };
-
                                         match &*process_default(
-                                            external_context,
+                                            externals_tracing_module_context(),
                                             source,
                                             reference_type,
                                             vec![],
@@ -778,14 +799,18 @@ impl AssetContext for ModuleAssetContext {
             })
             .await?;
 
-        let result = replace_externals(
-            result,
-            this.module_options_context
-                .await?
-                .ecmascript
-                .import_externals,
-        )
-        .await?;
+        let result = if this.replace_externals {
+            replace_externals(
+                result,
+                this.module_options_context
+                    .await?
+                    .ecmascript
+                    .import_externals,
+            )
+            .await?
+        } else {
+            result
+        };
 
         Ok(result.cell())
     }
