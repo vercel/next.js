b9f31aba435e93ff339f41db89f68fa1d7fb1763
Tobias Koppers <tobias.koppers@googlemail.com>
2024-10-17 16:31:32 +0200

    add dump db

--- turbopack/crates/turbo-tasks-backend/src/database/mod.rs
+++ turbopack/crates/turbo-tasks-backend/src/database/mod.rs
@@ -1,13 +1,16 @@
 mod by_key_space;
 pub mod db_versioning;
+mod dump_db;
 pub mod fresh_db_optimization;
 pub mod key_value_database;
 pub mod lmdb;
 pub mod noop_kv;
 pub mod read_transaction_cache;
+mod rw_pair;
 mod startup_cache;
 
 pub use db_versioning::handle_db_versioning;
+pub use dump_db::DumpKeyValueDatabase;
 pub use fresh_db_optimization::{is_fresh, FreshDbOptimization};
 #[allow(unused_imports)]
 pub use noop_kv::NoopKvDb;
