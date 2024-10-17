e868b70ecb632a38f549850a9ee6a7b6299df2c4
Jiachi Liu <inbox@huozhi.im>
2024-10-17 14:26:05 +0200

    add ssr test

--- test/development/app-dir/capture-console-error/capture-console-error.test.ts
+++ test/development/app-dir/capture-console-error/capture-console-error.test.ts
@@ -3,32 +3,41 @@ import {
   assertHasRedbox,
   getRedboxCallStack,
   getRedboxDescription,
+  getRedboxTotalErrorCount,
   waitForAndOpenRuntimeError,
 } from 'next-test-utils'
+
+async function getRedboxResult(browser: any) {
+  const description = await getRedboxDescription(browser)
+  const callStacks = await getRedboxCallStack(browser)
+  const count = await getRedboxTotalErrorCount(browser)
+  const result = {
+    count,
+    description,
+    callStacks,
+  }
+  return result
+}
 
 describe('app-dir - capture-console-error', () => {
   const { next } = nextTestSetup({
     files: __dirname,
   })
 
-  it('should error on capture console error', async () => {
-    const browser = await next.browser('/')
+  it('should error on capture browser console error and format the error message', async () => {
+    const browser = await next.browser('/browser')
     await browser.elementByCss('button').click()
 
     await waitForAndOpenRuntimeError(browser)
     await assertHasRedbox(browser)
 
-    const description = await getRedboxDescription(browser)
-    const callStacks = await getRedboxCallStack(browser)
-    const result = {
-      description,
-      callStacks,
-    }
+    const result = await getRedboxResult(browser)
 
     if (process.env.TURBOPACK) {
       expect(result).toMatchInlineSnapshot(`
         {
           "callStacks": "",
+          "count": 1,
           "description": "Error: trigger an console <error>",
         }
       `)
@@ -36,7 +45,35 @@ describe('app-dir - capture-console-error', () => {
       expect(result).toMatchInlineSnapshot(`
         {
           "callStacks": "",
+          "count": 1,
           "description": "Error: trigger an console <error>",
+        }
+      `)
+    }
+  })
+
+  it('should error on capture server replay console error', async () => {
+    const browser = await next.browser('/ssr')
+
+    await waitForAndOpenRuntimeError(browser)
+    await assertHasRedbox(browser)
+
+    const result = await getRedboxResult(browser)
+
+    if (process.env.TURBOPACK) {
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "callStacks": "",
+          "count": 2,
+          "description": "Error: ssr console error:client",
+        }
+      `)
+    } else {
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "callStacks": "",
+          "count": 2,
+          "description": "Error: ssr console error:client",
         }
       `)
     }
