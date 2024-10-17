ce9564666d5c7c788e7c6b526d71f339a9f7cec0
Jiachi Liu <inbox@huozhi.im>
2024-10-17 23:16:45 +0200

    remove work around

--- test/development/acceptance-app/fixtures/default-template/next.config.js
+++ test/development/acceptance-app/fixtures/default-template/next.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-  experimental: {
-    // TODO: Remove this when the dynamic API warning on hydration error is fixed on react side
-    internal_disableSyncDynamicAPIWarnings: true,
-  },
-}
+module.exports = {}
