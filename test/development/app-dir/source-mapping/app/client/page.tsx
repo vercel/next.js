12c193ba40c493c811ff1d7e76039b3be06fb4fe
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-18 14:13:57 +0200

    Add more use cases to source mapping test app

--- test/development/app-dir/source-mapping/app/client/page.tsx
+++ test/development/app-dir/source-mapping/app/client/page.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import { Form } from '../form'
-import defaultAction, { bar } from '../actions'
+import defaulAction1, { foo, bar, baz, qux } from '../actions1'
+import defaulAction2 from '../actions2'
+import defaulAction3 from '../actions3'
 import Link from 'next/link'
 
 export default function Page() {
   return (
     <main>
       <h1>client component page</h1>
-      <Form action={defaultAction} />
+      <Form action={defaulAction1} />
+      <Form action={defaulAction2} />
+      <Form action={defaulAction3} />
+      <Form action={foo} />
       <Form action={bar} />
+      <Form action={baz} />
+      <Form action={qux} />
       <Link href="/">server component page</Link>
     </main>
   )
--- test/development/app-dir/source-mapping/app/page.tsx
+++ test/development/app-dir/source-mapping/app/page.tsx
@@ -1,16 +1,18 @@
 import { Form } from './form'
-import { foo } from './actions'
+import defaulAction1, { foo, bar, baz, qux } from './actions1'
+import defaulAction2 from './actions2'
+import defaulAction3 from './actions3'
 import Link from 'next/link'
 import { ServerComponent } from './server-component'
 
 export default function Page() {
-  const bar = async () => {
+  const action1 = async () => {
     'use server'
 
     return 'declarator arrow function expression'
   }
 
-  async function baz() {
+  async function action2() {
     'use server'
 
     return 'function declaration'
@@ -19,9 +21,15 @@ export default function Page() {
   return (
     <main>
       <ServerComponent />
+      <Form action={defaulAction1} />
+      <Form action={defaulAction2} />
+      <Form action={defaulAction3} />
       <Form action={foo} />
       <Form action={bar} />
       <Form action={baz} />
+      <Form action={qux} />
+      <Form action={action1} />
+      <Form action={action2} />
       <Form
         action={async () => {
           'use server'
