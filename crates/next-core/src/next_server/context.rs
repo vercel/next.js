0857c8e376eacdf37f398e6c5c96b37024294491
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-19 09:35:58 +0200

    Set cwd in get_server_compile_time_info
    for `path.join(process.cwd(), ...)`

--- crates/next-core/src/next_server/context.rs
+++ crates/next-core/src/next_server/context.rs
@@ -19,8 +19,11 @@ use turbopack_core::{
         FreeVarReferences,
     },
     condition::ContextCondition,
-    environment::{Environment, ExecutionEnvironment, NodeJsEnvironment, RuntimeVersions},
+    environment::{
+        Environment, ExecutionEnvironment, NodeJsEnvironment, NodeJsVersion, RuntimeVersions,
+    },
     free_var_references,
+    target::CompileTarget,
 };
 use turbopack_ecmascript::references::esm::UrlRewriteBehavior;
 use turbopack_ecmascript_plugins::transform::directives::{
@@ -348,9 +351,17 @@ async fn next_server_free_vars(define_env: Vc<EnvMap>) -> Result<Vc<FreeVarRefer
 pub fn get_server_compile_time_info(
     process_env: Vc<Box<dyn ProcessEnv>>,
     define_env: Vc<EnvMap>,
+    cwd: RcStr,
 ) -> Vc<CompileTimeInfo> {
     CompileTimeInfo::builder(Environment::new(Value::new(
-        ExecutionEnvironment::NodeJsLambda(NodeJsEnvironment::current(process_env)),
+        ExecutionEnvironment::NodeJsLambda(
+            NodeJsEnvironment {
+                compile_target: CompileTarget::current(),
+                node_version: NodeJsVersion::cell(NodeJsVersion::Current(process_env)),
+                cwd: Vc::cell(Some(cwd)),
+            }
+            .cell(),
+        ),
     )))
     .defines(next_server_defines(define_env))
     .free_var_references(next_server_free_vars(define_env))
