7708a3cd39f1e31fee6a4a28c35f0120d7b592f5
Jiachi Liu <inbox@huozhi.im>
2024-10-20 12:52:53 +0200

    codemod: fix props types modify for type alias

--- packages/next-codemod/transforms/__testfixtures__/next-async-request-api-dynamic-props/access-props-38.input.tsx
+++ packages/next-codemod/transforms/__testfixtures__/next-async-request-api-dynamic-props/access-props-38.input.tsx
@@ -6,3 +6,13 @@ export default function Page(props: PageProps) {
   const params = props.params
   return <p>child {params.slug}</p>
 }
+
+type MetadataProps = {
+  params: { slug: string }
+}
+
+export function generateMetadata(props: MetadataProps) {
+  return {
+    title: props.params.slug,
+  }
+}
--- packages/next-codemod/transforms/__testfixtures__/next-async-request-api-dynamic-props/access-props-38.output.tsx
+++ packages/next-codemod/transforms/__testfixtures__/next-async-request-api-dynamic-props/access-props-38.output.tsx
@@ -1,8 +1,18 @@
 type PageProps = {
-  params: { slug: string }
+  params: Promise<{ slug: string }>
 }
 
 export default async function Page(props: PageProps) {
   const params = (await props.params)
   return <p>child {params.slug}</p>
+}
+
+type MetadataProps = {
+  params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata(props: MetadataProps) {
+  return {
+    title: (await props.params).slug,
+  };
 }
