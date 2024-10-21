d21103e74f2b809699c874d0fddb00edb913db9d
Janka Uryga <lolzatu2@gmail.com>
2024-10-21 21:00:09 +0200

    prevent errors thrown by redirect/notFound from being logged on the client

--- packages/next/src/client/components/globals/intercept-console-error.ts
+++ packages/next/src/client/components/globals/intercept-console-error.ts
@@ -11,15 +11,62 @@ export function patchConsoleError() {
   }
 
   window.console.error = (...args: any[]) => {
-    // See https://github.com/facebook/react/blob/d50323eb845c5fde0d720cae888bf35dedd05506/packages/react-reconciler/src/ReactFiberErrorLogger.js#L78
-    const error = process.env.NODE_ENV !== 'production' ? args[1] : args[0]
+    const maybeError = getPotentialErrorFromLogArgs(...args)
 
-    if (!isNextRouterError(error)) {
+    if (!isNextRouterError(maybeError)) {
       if (process.env.NODE_ENV !== 'production') {
-        handleClientError(error, args)
+        handleClientError(maybeError, args)
       }
 
       originConsoleError.apply(window.console, args)
     }
   }
+}
+
+function getPotentialErrorFromLogArgs(...args: unknown[]): unknown {
+  if (process.env.NODE_ENV !== 'production') {
+    const replayedError = matchReplayedError(...args)
+    if (replayedError) {
+      return replayedError
+    }
+    // See https://github.com/facebook/react/blob/d50323eb845c5fde0d720cae888bf35dedd05506/packages/react-reconciler/src/ReactFiberErrorLogger.js#L78
+    return args[1]
+  } else {
+    return args[0]
+  }
+}
+
+function matchReplayedError(...args: unknown[]): Error | null {
+  // See
+  // https://github.com/facebook/react/blob/65a56d0e99261481c721334a3ec4561d173594cd/packages/react-devtools-shared/src/backend/flight/renderer.js#L88-L93
+  //
+  // Logs replayed from the server look like this:
+  // [
+  //   "%c%s%c %o\n\n%s\n\n%s\n",
+  //   "background: #e6e6e6; ...",
+  //   " Server ", // can also be e.g. " Prerender "
+  //   "",
+  //   Error
+  //   "The above error occurred in the <Page> component."
+  //   ...
+  // ]
+  if (
+    args.length > 3 &&
+    typeof args[0] === 'string' &&
+    args[0].startsWith('%c%s%c ') &&
+    typeof args[1] === 'string' &&
+    typeof args[2] === 'string' &&
+    typeof args[3] === 'string'
+  ) {
+    const maybeError = args[4]
+    if (
+      maybeError &&
+      typeof maybeError === 'object' &&
+      maybeError instanceof Error
+    ) {
+      return maybeError
+    }
+  }
+
+  return null
 }
