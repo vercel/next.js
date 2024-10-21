b27c65a165e03abd820a503c0bd900d2f3f8bc3f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-21 14:25:43 +0200

    Extend example

--- bench/basic-app/next.config.js
+++ bench/basic-app/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // output: 'standalone',
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
