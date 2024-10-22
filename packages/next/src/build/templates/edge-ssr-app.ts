ba50f4c31d8c97cd8982ebac7a77cd62c49a6383
Hendrik Liebau <mail@hendrik-liebau.de>
2024-10-23 01:30:19 +0200

    Read page name from work store in server module map proxy

--- packages/next/src/build/templates/edge-ssr-app.ts
+++ packages/next/src/build/templates/edge-ssr-app.ts
@@ -60,7 +60,6 @@ if (rscManifest && rscServerManifest) {
     serverActionsManifest: rscServerManifest,
     serverModuleMap: createServerModuleMap({
       serverActionsManifest: rscServerManifest,
-      pageName: 'VAR_PAGE',
     }),
   })
 }
