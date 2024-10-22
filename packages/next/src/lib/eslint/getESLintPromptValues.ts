045efa4f2f8d2e8ad9c0d0bb25f00be5e4162adc
devjiwonchoi <devjiwonchoi@gmail.com>
2024-10-22 20:57:27 +0900

    improve syntax

--- packages/next/src/lib/eslint/getESLintPromptValues.ts
+++ packages/next/src/lib/eslint/getESLintPromptValues.ts
@@ -7,10 +7,15 @@ import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-const eslintConfig = [...compat.extends(${compatExtendsStr})];
+
+const eslintConfig = [
+  ...compat.extends(${compatExtendsStr}),
+];
+
 export default eslintConfig;`
 }
 
