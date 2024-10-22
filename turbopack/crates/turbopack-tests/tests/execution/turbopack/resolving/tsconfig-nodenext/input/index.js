vercel/turborepo

commit 64e5946522b319cee5efceb2ce105ce49ab3d3bf

Support `module_resolution: "nodenext"` (#8748)
### Description

Allow resolving for example `./foo.js` to `./foo.ts`

`.js` can resolve to `.ts` and `.tsx`
`.mjs` can resolve to `.mts`
`.cjs` can resolve to `.cts`

Closes PACK-3031

This is what `tsc --traceResolution` says about priority of the various
possible resolution:
```
======== Resolving module '../libs/f.js' from '/Users/niklas/Desktop/nodenext-app/app/page.tsx'. ========
Explicitly specified module resolution kind: 'NodeNext'.
Resolving in CJS mode with conditions 'require', 'types', 'node'.
Loading module as file / folder, candidate module location '/Users/niklas/Desktop/nodenext-app/libs/f.js', target file types: TypeScript, JavaScript, Declaration.
File name '/Users/niklas/Desktop/nodenext-app/libs/f.js' has a '.js' extension - stripping it.
File '/Users/niklas/Desktop/nodenext-app/libs/f.ts' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.tsx' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.d.ts' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.js' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.jsx' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.js.ts' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.js.tsx' does not exist.
File '/Users/niklas/Desktop/nodenext-app/libs/f.js.d.ts' does not exist.
```

### Testing Instructions

I also added a test case

crates/turbopack-core/src/resolve/mod.rs
@@ -1922,7 +1922,6 @@ async fn resolve_relative_request(
     let mut new_path = path_pattern.clone();
 
     let fragment_val = fragment.await?;
-
     if !fragment_val.is_empty() {
         new_path.push(Pattern::Alternatives(
             once(Pattern::Constant("".into()))
@@ -1944,9 +1943,49 @@ async fn resolve_relative_request(
                 )
                 .collect(),
         ));
-
         new_path.normalize();
     };
+
+    if options_value.enable_typescript_with_output_extension {
+        new_path.replace_final_constants(&|c: &RcStr| -> Option<Pattern> {
+            let result = match c.rsplit_once(".") {
+                Some((base, "js")) => Some((
+                    base,
+                    vec![
+                        Pattern::Constant(".ts".into()),
+                        Pattern::Constant(".tsx".into()),
+                        Pattern::Constant(".js".into()),
+                    ],
+                )),
+                Some((base, "mjs")) => Some((
+                    base,
+                    vec![
+                        Pattern::Constant(".mts".into()),
+                        Pattern::Constant(".js".into()),
+                    ],
+                )),
+                Some((base, "cjs")) => Some((
+                    base,
+                    vec![
+                        Pattern::Constant(".cts".into()),
+                        Pattern::Constant(".js".into()),
+                    ],
+                )),
+                _ => None,
+            };
+            result.map(|(base, replacement)| {
+                if base.is_empty() {
+                    Pattern::Alternatives(replacement)
+                } else {
+                    Pattern::Concatenation(vec![
+                        Pattern::Constant(base.into()),
+                        Pattern::Alternatives(replacement),
+                    ])
+                }
+            })
+        });
+        new_path.normalize();
+    }
 
     let mut results = Vec::new();
     let matches = read_matches(

crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/index.js
@@ -1,0 +1,11 @@
+import foo from "./src/foo.js";
+import bar from "./src/bar.js";
+import fooEsm from "./src/foo-esm.mjs";
+import fooCjs from "./src/foo-cjs.cjs";
+
+it("should correctly resolve explicit extensions with nodenext", () => {
+  expect(foo).toBe("foo");
+  expect(bar).toBe("bar");
+  expect(fooEsm).toBe("fooEsm");
+  expect(fooCjs).toBe("fooCjs");
+});

crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/bar.tsx
@@ -1,0 +1,1 @@
+export default "bar";

crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/foo-cjs.cts
@@ -1,0 +1,1 @@
+module.exports = "fooCjs";

crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/foo-esm.mts
@@ -1,0 +1,1 @@
+export default "fooEsm";

crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/foo.ts
@@ -1,0 +1,1 @@
+export default "foo";

crates/turbopack-core/src/resolve/options.rs
@@ -442,6 +442,9 @@ pub struct ResolveOptions {
     pub resolved_map: Option<Vc<ResolvedMap>>,
     pub before_resolve_plugins: Vec<Vc<Box<dyn BeforeResolvePlugin>>>,
     pub plugins: Vec<Vc<Box<dyn AfterResolvePlugin>>>,
+    /// Support resolving *.js requests to *.ts files
+    pub enable_typescript_with_output_extension: bool,
+
     pub placeholder_for_future_extensions: (),
 }
 

crates/turbopack-core/src/resolve/pattern.rs
@@ -627,6 +627,33 @@ impl Pattern {
         new.normalize();
         Pattern::alternatives([self.clone(), new])
     }
+
+    /// Calls `cb` on all constants that are at the end of the pattern and
+    /// replaces the given final constant with the returned pattern. Returns
+    /// true if replacements were performed.
+    pub fn replace_final_constants(&mut self, cb: &impl Fn(&RcStr) -> Option<Pattern>) -> bool {
+        let mut replaced = false;
+        match self {
+            Pattern::Constant(c) => {
+                if let Some(replacement) = cb(c) {
+                    *self = replacement;
+                    replaced = true;
+                }
+            }
+            Pattern::Dynamic => {}
+            Pattern::Alternatives(list) => {
+                for i in list {
+                    replaced = i.replace_final_constants(cb) || replaced;
+                }
+            }
+            Pattern::Concatenation(list) => {
+                if let Some(i) = list.last_mut() {
+                    replaced = i.replace_final_constants(cb) || replaced;
+                }
+            }
+        }
+        replaced
+    }
 }
 
 impl Pattern {
@@ -1111,6 +1138,7 @@ pub async fn read_matches(
 #[cfg(test)]
 mod tests {
     use rstest::*;
+    use turbo_tasks::RcStr;
 
     use super::Pattern;
 
@@ -1357,5 +1385,78 @@ mod tests {
         #[case] expected: Option<Vec<(&str, bool)>>,
     ) {
         assert_eq!(pat.next_constants(value), expected);
+    }
+
+    #[test]
+    fn replace_final_constants() {
+        fn f(mut p: Pattern, cb: &impl Fn(&RcStr) -> Option<Pattern>) -> Pattern {
+            p.replace_final_constants(cb);
+            p
+        }
+
+        let js_to_ts_tsx = |c: &RcStr| -> Option<Pattern> {
+            c.strip_suffix(".js").map(|rest| {
+                let new_ending = Pattern::Alternatives(vec![
+                    Pattern::Constant(".ts".into()),
+                    Pattern::Constant(".tsx".into()),
+                    Pattern::Constant(".js".into()),
+                ]);
+                if !rest.is_empty() {
+                    Pattern::Concatenation(vec![Pattern::Constant(rest.into()), new_ending])
+                } else {
+                    new_ending
+                }
+            })
+        };
+
+        assert_eq!(
+            f(
+                Pattern::Concatenation(vec![
+                    Pattern::Constant(".".into()),
+                    Pattern::Constant("/".into()),
+                    Pattern::Dynamic,
+                    Pattern::Alternatives(vec![
+                        Pattern::Constant(".js".into()),
+                        Pattern::Constant(".node".into()),
+                    ])
+                ]),
+                &js_to_ts_tsx
+            ),
+            Pattern::Concatenation(vec![
+                Pattern::Constant(".".into()),
+                Pattern::Constant("/".into()),
+                Pattern::Dynamic,
+                Pattern::Alternatives(vec![
+                    Pattern::Alternatives(vec![
+                        Pattern::Constant(".ts".into()),
+                        Pattern::Constant(".tsx".into()),
+                        Pattern::Constant(".js".into()),
+                    ]),
+                    Pattern::Constant(".node".into()),
+                ])
+            ]),
+        );
+        assert_eq!(
+            f(
+                Pattern::Concatenation(vec![
+                    Pattern::Constant(".".into()),
+                    Pattern::Constant("/".into()),
+                    Pattern::Constant("abc.js".into()),
+                ]),
+                &js_to_ts_tsx
+            ),
+            Pattern::Concatenation(vec![
+                Pattern::Constant(".".into()),
+                Pattern::Constant("/".into()),
+                Pattern::Concatenation(vec![
+                    Pattern::Constant("abc".into()),
+                    Pattern::Alternatives(vec![
+                        Pattern::Constant(".ts".into()),
+                        Pattern::Constant(".tsx".into()),
+                        Pattern::Constant(".js".into()),
+                    ])
+                ]),
+            ])
+        );
     }
 }

vercel/next.js

commit 0a3508adcce34fe891bb3be3f55e02916da67147

Fix `module_resolution: "nodenext"` with `mjs` or `cjs` (#71635)
A regression from https://github.com/vercel/turborepo/pull/8748

Closes PACK-3312

Previously, it treated imports to `something.mjs` as importing
`something.mts` with a fallback to `something.js`.

But it should obviously instead fallback to the original extension
`mjs`, which is also what tsc does:

```
Found 'package.json' at 'input/package.json'.
======== Resolving module './src/foo.mjs' from 'input/index.js'. ========
Explicitly specified module resolution kind: 'NodeNext'.
Resolving in CJS mode with conditions 'require', 'types', 'node'.
Loading module as file / folder, candidate module location 'input/src/foo.mjs', target file types: TypeScript, JavaScript, Declaration.
File name 'input/src/foo.mjs' has a '.mjs' extension - stripping it.
File 'input/src/foo.mts' does not exist.
File 'input/src/foo.d.mts' does not exist.
File 'input/src/foo.mjs' does not exist.
File 'input/src/foo.mjs.ts' does not exist.
File 'input/src/foo.mjs.tsx' does not exist.
File 'input/src/foo.mjs.d.ts' does not exist.
File 'input/src/foo.mjs.js' does not exist.
File 'input/src/foo.mjs.jsx' does not exist.
Directory 'input/src/foo.mjs' does not exist, skipping all lookups in it.
======== Module name './src/foo.mjs' was not resolved. ========
```

turbopack/crates/turbopack-core/src/resolve/mod.rs
@@ -2006,41 +2006,41 @@ async fn resolve_relative_request(
 
     if options_value.enable_typescript_with_output_extension {
         new_path.replace_final_constants(&|c: &RcStr| -> Option<Pattern> {
-            let result = match c.rsplit_once(".") {
-                Some((base, "js")) => Some((
+            let (base, replacement) = match c.rsplit_once(".") {
+                Some((base, "js")) => (
                     base,
                     vec![
                         Pattern::Constant(".ts".into()),
                         Pattern::Constant(".tsx".into()),
                         Pattern::Constant(".js".into()),
                     ],
-                )),
-                Some((base, "mjs")) => Some((
+                ),
+                Some((base, "mjs")) => (
                     base,
                     vec![
                         Pattern::Constant(".mts".into()),
-                        Pattern::Constant(".js".into()),
+                        Pattern::Constant(".mjs".into()),
                     ],
-                )),
-                Some((base, "cjs")) => Some((
+                ),
+                Some((base, "cjs")) => (
                     base,
                     vec![
                         Pattern::Constant(".cts".into()),
-                        Pattern::Constant(".js".into()),
+                        Pattern::Constant(".cjs".into()),
                     ],
-                )),
-                _ => None,
-            };
-            result.map(|(base, replacement)| {
-                if base.is_empty() {
-                    Pattern::Alternatives(replacement)
-                } else {
-                    Pattern::Concatenation(vec![
-                        Pattern::Constant(base.into()),
-                        Pattern::Alternatives(replacement),
-                    ])
+                ),
+                _ => {
+                    return None;
                 }
-            })
+            };
+            if base.is_empty() {
+                Some(Pattern::Alternatives(replacement))
+            } else {
+                Some(Pattern::Concatenation(vec![
+                    Pattern::Constant(base.into()),
+                    Pattern::Alternatives(replacement),
+                ]))
+            }
         });
         new_path.normalize();
     }

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/index.js
@@ -1,11 +1,21 @@
 import foo from "./src/foo.js";
-import bar from "./src/bar.js";
-import fooEsm from "./src/foo-esm.mjs";
-import fooCjs from "./src/foo-cjs.cjs";
+import fileTsx from "./src/file-tsx.js";
+import fileMts from "./src/file-mts.mjs";
+import fileCts from "./src/file-cts.cjs";
+import fileMjs from "./src/file-mjs.mjs";
+import fileCjs from "./src/file-cjs.cjs";
 
 it("should correctly resolve explicit extensions with nodenext", () => {
-  expect(foo).toBe("foo");
-  expect(bar).toBe("bar");
-  expect(fooEsm).toBe("fooEsm");
-  expect(fooCjs).toBe("fooCjs");
+  expect(foo).toBe("foo.ts");
+  expect(fileTsx).toBe("file-tsx");
+  expect(fileMts).toBe("file-mts");
+  expect(fileCts).toBe("file-cts");
+  expect(fileMjs).toBe("file-mjs");
+  expect(fileCjs).toBe("file-cjs");
 });
+
+// import fooButton from "foo/button";
+
+// it("should correctly resolve explicit extensions with nodenext", () => {
+//   expect(fooButton).toBe("button");
+// });

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/bar.tsx
@@ -1,1 +1,0 @@
-export default "bar";

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/file-cjs.cjs
@@ -1,0 +1,1 @@
+module.exports = 'file-cjs'

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/file-cts.cts
@@ -1,0 +1,1 @@
+module.exports = "file-cts";

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/file-mjs.mjs
@@ -1,0 +1,1 @@
+export default 'file-mjs'

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/file-mts.mts
@@ -1,0 +1,1 @@
+export default "file-mts";

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/file-tsx.tsx
@@ -1,0 +1,1 @@
+export default "file-tsx";

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/foo-cjs.cts
@@ -1,1 +1,0 @@
-module.exports = "fooCjs";

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/foo-esm.mts
@@ -1,1 +1,0 @@
-export default "fooEsm";

turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-nodenext/input/src/foo.ts
@@ -1,1 +1,1 @@
-export default "foo";
+export default "foo.ts";