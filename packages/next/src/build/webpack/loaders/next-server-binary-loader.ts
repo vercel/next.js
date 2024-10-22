ca7ab43404e067826d930a82079f801bdf2f0cca
Janka Uryga <lolzatu2@gmail.com>
2024-10-22 22:23:33 +0200

    fix: temporarily fix incorrect relative path (bad)

--- packages/next/src/build/webpack/loaders/next-server-binary-loader.ts
+++ packages/next/src/build/webpack/loaders/next-server-binary-loader.ts
@@ -1,12 +1,10 @@
 import type { webpack } from 'next/dist/compiled/webpack/webpack'
-import path from 'path'
 
-export default function nextErrorBrowserBinaryLoader(
+export default function nextServerBinaryLoader(
   this: webpack.LoaderContext<any>
 ) {
-  let relativePath = path.relative(this.rootContext, this.resourcePath)
-  if (!relativePath.startsWith('.')) {
-    relativePath = './' + relativePath
-  }
-  return `module.exports = __non_webpack_require__(${JSON.stringify(relativePath)})`
+  // TODO: figure out how to relativize this (but how can we know where the module will end up on disk?)
+  // also, how can we know if the package's "imports" even lets us directly require the file like that?
+  const importPath = this.resourcePath
+  return `module.exports = __non_webpack_require__(${JSON.stringify(importPath)})`
 }
