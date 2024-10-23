32cc72120b9423ee1b9d56c451d818fc4baebcb9
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-10-23 15:26:10 +0200

    Update test for Turbopack

--- test/e2e/prerender.test.ts
+++ test/e2e/prerender.test.ts
@@ -2122,7 +2122,7 @@ describe('Prerender', () => {
           {
             page: '/_app',
             tests: [
-              /webpack-runtime\.js/,
+              /(webpack-runtime\.js|\[turbopack\]_runtime\.js)/,
               /node_modules\/react\/index\.js/,
               /node_modules\/react\/package\.json/,
               isReact18
@@ -2134,7 +2134,7 @@ describe('Prerender', () => {
           {
             page: '/another',
             tests: [
-              /webpack-runtime\.js/,
+              /(webpack-runtime\.js|\[turbopack\]_runtime\.js)/,
               /chunks\/.*?\.js/,
               /node_modules\/react\/index\.js/,
               /node_modules\/react\/package\.json/,
@@ -2151,7 +2151,7 @@ describe('Prerender', () => {
           {
             page: '/blog/[post]',
             tests: [
-              /webpack-runtime\.js/,
+              /(webpack-runtime\.js|\[turbopack\]_runtime\.js)/,
               /chunks\/.*?\.js/,
               /node_modules\/react\/index\.js/,
               /node_modules\/react\/package\.json/,
