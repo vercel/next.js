0e047a283514e303be2d73ff0266b14df43c99c8
Janka Uryga <lolzatu2@gmail.com>
2024-10-23 14:15:01 +0200

    attempt at correct relative paths

--- packages/next/src/build/webpack/loaders/next-server-binary-loader.ts
+++ packages/next/src/build/webpack/loaders/next-server-binary-loader.ts
@@ -1,10 +1,14 @@
 import type { webpack } from 'next/dist/compiled/webpack/webpack'
+import path from 'path'
 
 export default function nextServerBinaryLoader(
   this: webpack.LoaderContext<any>
 ) {
-  // TODO: figure out how to relativize this (but how can we know where the module will end up on disk?)
-  // also, how can we know if the package's "imports" even lets us directly require the file like that?
-  const importPath = this.resourcePath
-  return `module.exports = __non_webpack_require__(${JSON.stringify(importPath)})`
+  // TODO: how can we know if the package's "imports" even lets us directly require the file like that?
+  const pathFromProjectRoot = path.relative(this.rootContext, this.resourcePath)
+  return `
+const path = require('path');
+const moduleAbsPath = path.resolve(process.cwd(), ${JSON.stringify(pathFromProjectRoot)});
+module.exports = __non_webpack_require__(moduleAbsPath);
+`
 }
