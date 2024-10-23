2ba03565d2a741f43623572aa6a69cb68aa0bc07
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-23 15:04:23 +0900

    Update test refs

--- turbopack/crates/turbopack-tests/tests/snapshot/tree-shaking/dce/output/4e721_crates_turbopack-tests_tests_snapshot_tree-shaking_dce_input_index_004fcb.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/4e721_crates_turbopack-tests_tests_snapshot_tree-shaking_dce_input_index_004fcb.js",
-    {},
-    {"otherChunks":["output/turbopack_crates_turbopack-tests_tests_snapshot_tree-shaking_dce_input_11c6f4._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/tree-shaking/dce/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
