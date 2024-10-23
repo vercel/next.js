09a0b73d9f12f3017fa39ab20f6c2b92d2fe553c
Aurélien MANCA <aurelien.manca.ext@jobteaser.com>
2024-10-23 11:01:09 +0200

    fix(next): use the asset prefix when loading a CSS in App Router
    The current behavior loads the CSS with only the pathname and so, it results with a 404.
    
    The new behavior uses the full url of the asset.

---
+++ test/integration/next-dynamic-css/src/app/test-app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React, { useState } from 'react'
+import style from '../../Content4.module.css'
+import { Comp } from '../../inner/k'
+
+export default function Index() {
+  const [s] = useState(true)
+
+  if (s) {
+    return (
+      <>
+        <div className={style.header}></div>
+        <Comp />
+      </>
+    )
+  }
+
+  return null
+}
+
+export const revalidate = 0
