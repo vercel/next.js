b3bf72b04613818b6e2709d8d332bbc97bf9d94d
eps1lon <sebastian.silbermann@vercel.com>
2024-10-23 12:42:18 +0200

    Updated peer dependency references in apps

--- run-tests.js
+++ run-tests.js
@@ -20,7 +20,7 @@ const { getTestFilter } = require('./test/get-test-filter')
 
 // Do not rename or format. sync-react script relies on this line.
 // prettier-ignore
-const nextjsReactPeerVersion = "19.0.0-rc-69d4b800-20241021";
+const nextjsReactPeerVersion = "19.0.0-rc-45804af1-20241021";
 
 let argv = require('yargs/yargs')(process.argv.slice(2))
   .string('type')
