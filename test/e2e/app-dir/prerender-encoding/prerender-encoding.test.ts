a6d53dd7c8efd43e1286b85c94e8bc38468d6cfd
samcx <sam@vercel.com>
2024-10-23 23:02:44 -0700

    test(app-dir): add e2e test for prerender encoding

---
+++ test/e2e/app-dir/prerender-encoding/prerender-encoding.test.ts
@@ -0,0 +1,15 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('prerender-encoding', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should respond with the prerendered page correctly', async () => {
+    const browser = await next.browser('/sticks%20%26%20stones')
+    console.log('browser = ', browser)
+    expect(await browser.elementByCss('div').text()).toBe(
+      'params.id is sticks%20%26%20stones'
+    )
+  })
+})
