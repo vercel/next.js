0e56f203e6fe7211ca8a74ce93f8ec20f41d87dd
Kaikai <98205900+Kaikaikaifang@users.noreply.github.com>
2024-11-03 22:16:01 +0800

    fix(with zones): params causes a failure to compile
    Type '{ params: { id: any; }; }' does not satisfy the constraint 'PageProps'.

--- examples/with-zones/blog/app/blog/post/[id]/page.tsx
+++ examples/with-zones/blog/app/blog/post/[id]/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 
-export default function Post({ params: { id } }) {
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
   return (
     <div>
       <h3>Post #{id}</h3>
