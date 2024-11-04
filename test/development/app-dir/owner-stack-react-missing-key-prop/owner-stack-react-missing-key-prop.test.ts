ef96eb2773fa6cf36597883b63bf6418b46ccf75
Jiachi Liu <inbox@huozhi.im>
2024-11-04 12:41:14 +0100

    add test with owner stack disabled

--- test/development/app-dir/owner-stack-react-missing-key-prop/owner-stack-react-missing-key-prop.test.ts
+++ test/development/app-dir/owner-stack-react-missing-key-prop/owner-stack-react-missing-key-prop.test.ts
@@ -1,6 +1,8 @@
 import { nextTestSetup } from 'e2e-utils'
 import { getRedboxSource, waitForAndOpenRuntimeError } from 'next-test-utils'
 
+const isOwnerStackEnabled = process.env.TEST_OWNER_STACK !== 'false'
+
 async function getStackFramesContent(browser) {
   const stackFrameElements = await browser.elementsByCss(
     '[data-nextjs-call-stack-frame]'
@@ -33,6 +35,28 @@ describe('owner-stack-react-missing-key-prop', () => {
     files: __dirname,
   })
 
+  let nextConfig: string = ''
+  beforeAll(async () => {
+    if (!isOwnerStackEnabled) {
+      await next.stop()
+      await next.patchFile('next.config.js', (content: string) => {
+        nextConfig = content
+        return content.replace(
+          `reactOwnerStack: true`,
+          `reactOwnerStack: false`
+        )
+      })
+      await next.start()
+    }
+  })
+  afterAll(async () => {
+    if (!isOwnerStackEnabled) {
+      await next.stop()
+      // Restore original next.config.js
+      await next.patchFile('next.config.js', nextConfig)
+    }
+  })
+
   it('should catch invalid element from on rsc component', async () => {
     const browser = await next.browser('/rsc')
     await waitForAndOpenRuntimeError(browser)
@@ -41,35 +65,65 @@ describe('owner-stack-react-missing-key-prop', () => {
     const source = await getRedboxSource(browser)
 
     if (process.env.TURBOPACK) {
-      expect(stackFramesContent).toMatchInlineSnapshot(
-        `"at Page (app/rsc/page.tsx (6:13))"`
-      )
-      expect(source).toMatchInlineSnapshot(`
-        "app/rsc/page.tsx (7:9) @ <anonymous>
+      if (isOwnerStackEnabled) {
+        expect(stackFramesContent).toMatchInlineSnapshot(
+          `"at Page (app/rsc/page.tsx (6:13))"`
+        )
+        expect(source).toMatchInlineSnapshot(`
+          "app/rsc/page.tsx (7:9) @ <anonymous>
 
-           5 |     <div>
-           6 |       {list.map((item, index) => (
-        >  7 |         <span>{item}</span>
-             |         ^
-           8 |       ))}
-           9 |     </div>
-          10 |   )"
-      `)
+             5 |     <div>
+             6 |       {list.map((item, index) => (
+          >  7 |         <span>{item}</span>
+               |         ^
+             8 |       ))}
+             9 |     </div>
+            10 |   )"
+        `)
+      } else {
+        expect(stackFramesContent).toMatchInlineSnapshot(`""`)
+        expect(source).toMatchInlineSnapshot(`
+          "app/rsc/page.tsx (5:5) @ Page
+
+            3 | export default function Page() {
+            4 |   return (
+          > 5 |     <div>
+              |     ^
+            6 |       {list.map((item, index) => (
+            7 |         <span>{item}</span>
+            8 |       ))}"
+        `)
+      }
     } else {
-      expect(stackFramesContent).toMatchInlineSnapshot(
-        `"at map (app/rsc/page.tsx (6:13))"`
-      )
-      expect(source).toMatchInlineSnapshot(`
-        "app/rsc/page.tsx (7:10) @ span
+      if (isOwnerStackEnabled) {
+        expect(stackFramesContent).toMatchInlineSnapshot(
+          `"at map (app/rsc/page.tsx (6:13))"`
+        )
+        expect(source).toMatchInlineSnapshot(`
+          "app/rsc/page.tsx (7:10) @ span
 
-           5 |     <div>
-           6 |       {list.map((item, index) => (
-        >  7 |         <span>{item}</span>
-             |          ^
-           8 |       ))}
-           9 |     </div>
-          10 |   )"
-      `)
+             5 |     <div>
+             6 |       {list.map((item, index) => (
+          >  7 |         <span>{item}</span>
+               |          ^
+             8 |       ))}
+             9 |     </div>
+            10 |   )"
+        `)
+      } else {
+        expect(stackFramesContent).toMatchInlineSnapshot(`""`)
+        expect(source).toMatchInlineSnapshot(`
+          "app/rsc/page.tsx (5:6) @ div
+
+            3 | export default function Page() {
+            4 |   return (
+          > 5 |     <div>
+              |      ^
+            6 |       {list.map((item, index) => (
+            7 |         <span>{item}</span>
+            8 |       ))}"
+        `)
+      }
     }
   })
 
@@ -80,35 +134,65 @@ describe('owner-stack-react-missing-key-prop', () => {
     const stackFramesContent = await getStackFramesContent(browser)
     const source = await getRedboxSource(browser)
     if (process.env.TURBOPACK) {
-      expect(stackFramesContent).toMatchInlineSnapshot(
-        `"at Page (app/ssr/page.tsx (8:13))"`
-      )
-      expect(source).toMatchInlineSnapshot(`
-        "app/ssr/page.tsx (9:9) @ <unknown>
+      if (isOwnerStackEnabled) {
+        expect(stackFramesContent).toMatchInlineSnapshot(
+          `"at Page (app/ssr/page.tsx (8:13))"`
+        )
+        expect(source).toMatchInlineSnapshot(`
+          "app/ssr/page.tsx (9:9) @ <unknown>
 
-           7 |     <div>
-           8 |       {list.map((item, index) => (
-        >  9 |         <p>{item}</p>
-             |         ^
-          10 |       ))}
-          11 |     </div>
-          12 |   )"
-      `)
+             7 |     <div>
+             8 |       {list.map((item, index) => (
+          >  9 |         <p>{item}</p>
+               |         ^
+            10 |       ))}
+            11 |     </div>
+            12 |   )"
+        `)
+      } else {
+        expect(stackFramesContent).toMatchInlineSnapshot(`""`)
+        expect(source).toMatchInlineSnapshot(`
+          "app/ssr/page.tsx (7:5) @ Page
+
+             5 | export default function Page() {
+             6 |   return (
+          >  7 |     <div>
+               |     ^
+             8 |       {list.map((item, index) => (
+             9 |         <p>{item}</p>
+            10 |       ))}"
+        `)
+      }
     } else {
-      expect(stackFramesContent).toMatchInlineSnapshot(
-        `"at map (app/ssr/page.tsx (8:13))"`
-      )
-      expect(source).toMatchInlineSnapshot(`
-        "app/ssr/page.tsx (9:10) @ p
+      if (isOwnerStackEnabled) {
+        expect(stackFramesContent).toMatchInlineSnapshot(
+          `"at map (app/ssr/page.tsx (8:13))"`
+        )
+        expect(source).toMatchInlineSnapshot(`
+          "app/ssr/page.tsx (9:10) @ p
 
-           7 |     <div>
-           8 |       {list.map((item, index) => (
-        >  9 |         <p>{item}</p>
-             |          ^
-          10 |       ))}
-          11 |     </div>
-          12 |   )"
-      `)
+             7 |     <div>
+             8 |       {list.map((item, index) => (
+          >  9 |         <p>{item}</p>
+               |          ^
+            10 |       ))}
+            11 |     </div>
+            12 |   )"
+        `)
+      } else {
+        expect(stackFramesContent).toMatchInlineSnapshot(`""`)
+        expect(source).toMatchInlineSnapshot(`
+          "app/ssr/page.tsx (7:6) @ div
+
+             5 | export default function Page() {
+             6 |   return (
+          >  7 |     <div>
+               |      ^
+             8 |       {list.map((item, index) => (
+             9 |         <p>{item}</p>
+            10 |       ))}"
+        `)
+      }
     }
   })
 })
---
+++ test/development/app-dir/owner-stack-react-missing-key-prop/react-missing-key-prop.test.ts
@@ -0,0 +1,2 @@
+process.env.TEST_OWNER_STACK = 'false'
+require('./owner-stack-react-missing-key-prop.test')
