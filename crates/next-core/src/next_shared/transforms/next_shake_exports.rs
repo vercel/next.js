f0d50b4feba9516a8846b8530cf4a72f8fbb4709
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-04 11:54:46 +0900

    more

--- crates/next-core/src/next_shared/transforms/next_shake_exports.rs
+++ crates/next-core/src/next_shared/transforms/next_shake_exports.rs
@@ -33,9 +33,7 @@ struct NextShakeExports {
 impl CustomTransformer for NextShakeExports {
     #[tracing::instrument(level = tracing::Level::TRACE, name = "next_shake_exports", skip_all)]
     async fn transform(&self, program: &mut Program, _ctx: &TransformContext<'_>) -> Result<()> {
-        let p = std::mem::replace(program, Program::Module(Module::dummy()));
-
-        *program = p.fold_with(&mut shake_exports(Config {
+        program.mutate(shake_exports(Config {
             ignore: self.ignore.iter().map(|s| s.clone().into()).collect(),
         }));
         Ok(())
