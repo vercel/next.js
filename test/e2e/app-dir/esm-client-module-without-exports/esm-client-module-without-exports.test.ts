vercel/next.js

commit c376f39172ab819abf7769f516315b91a6aa31fa

Use `registerClientReference` for ESM client component modules (#71968)

packages/next/src/build/webpack/loaders/next-flight-loader/index.ts
@@ -2,8 +2,6 @@ import type { webpack } from 'next/dist/compiled/webpack/webpack'
 import { RSC_MOD_REF_PROXY_ALIAS } from '../../../../lib/constants'
 import {
   BARREL_OPTIMIZATION_PREFIX,
-  DEFAULT_RUNTIME_WEBPACK,
-  EDGE_RUNTIME_WEBPACK,
   RSC_MODULE_TYPES,
 } from '../../../../shared/lib/constants'
 import { warnOnce } from '../../../../shared/lib/utils/warn-once'
@@ -14,11 +12,6 @@ import type {
   javascript,
   LoaderContext,
 } from 'next/dist/compiled/webpack/webpack'
-import picomatch from 'next/dist/compiled/picomatch'
-
-export interface NextFlightLoaderOptions {
-  isEdgeServer: boolean
-}
 
 type SourceType = javascript.JavascriptParser['sourceType'] | 'commonjs'
 
@@ -26,10 +19,6 @@ const noopHeadPath = require.resolve('next/dist/client/components/noop-head')
 // For edge runtime it will be aliased to esm version by webpack
 const MODULE_PROXY_PATH =
   'next/dist/build/webpack/loaders/next-flight-loader/module-proxy'
-
-const isSharedRuntime = picomatch('**/next/dist/**/*.shared-runtime.js', {
-  dot: true, // required for .pnpm paths
-})
 
 export function getAssumedSourceType(
   mod: webpack.Module,
@@ -42,26 +31,31 @@ export function getAssumedSourceType(
   // It's tricky to detect the type of a client boundary, but we should always
   // use the `module` type when we can, to support `export *` and `export from`
   // syntax in other modules that import this client boundary.
-  let assumedSourceType = sourceType
-  if (assumedSourceType === 'auto' && detectedClientEntryType === 'auto') {
-    if (
-      clientRefs.length === 0 ||
-      (clientRefs.length === 1 && clientRefs[0] === '')
-    ) {
-      // If there's zero export detected in the client boundary, and it's the
-      // `auto` type, we can safely assume it's a CJS module because it doesn't
-      // have ESM exports.
-      assumedSourceType = 'commonjs'
-    } else if (!clientRefs.includes('*')) {
-      // Otherwise, we assume it's an ESM module.
-      assumedSourceType = 'module'
+
+  if (sourceType === 'auto') {
+    if (detectedClientEntryType === 'auto') {
+      if (
+        clientRefs.length === 0 ||
+        (clientRefs.length === 1 && clientRefs[0] === '')
+      ) {
+        // If there's zero export detected in the client boundary, and it's the
+        // `auto` type, we can safely assume it's a CJS module because it doesn't
+        // have ESM exports.
+        return 'commonjs'
+      } else if (!clientRefs.includes('*')) {
+        // Otherwise, we assume it's an ESM module.
+        return 'module'
+      }
+    } else if (detectedClientEntryType === 'cjs') {
+      return 'commonjs'
     }
   }
-  return assumedSourceType
+
+  return sourceType
 }
 
 export default function transformSource(
-  this: LoaderContext<NextFlightLoaderOptions>,
+  this: LoaderContext<undefined>,
   source: string,
   sourceMap: any
 ) {
@@ -70,8 +64,6 @@ export default function transformSource(
     throw new Error('Expected source to have been transformed to a string.')
   }
 
-  const options = this.getOptions()
-  const { isEdgeServer } = options
   const module = this._module!
 
   // Assign the RSC meta information to buildInfo.
@@ -107,6 +99,7 @@ export default function transformSource(
     )
 
     const clientRefs = buildInfo.rsc.clientRefs!
+    const stringifiedResourceKey = JSON.stringify(resourceKey)
 
     if (assumedSourceType === 'module') {
       if (clientRefs.includes('*')) {
@@ -118,46 +111,40 @@ export default function transformSource(
         return
       }
 
-      if (!isSharedRuntime(resourceKey)) {
-        // Prevent module concatenation, and prevent export names from being
-        // mangled, in production builds, so that exports of client reference
-        // modules can be resolved by React using the metadata from the client
-        // manifest.
-        this._compilation!.moduleGraph.getExportsInfo(
-          module
-        ).setUsedInUnknownWay(
-          isEdgeServer ? EDGE_RUNTIME_WEBPACK : DEFAULT_RUNTIME_WEBPACK
-        )
-      }
-
-      // `proxy` is the module proxy that we treat the module as a client boundary.
-      // For ESM, we access the property of the module proxy directly for each export.
-      // This is bit hacky that treating using a CJS like module proxy for ESM's exports,
-      // but this will avoid creating nested proxies for each export. It will be improved in the future.
-
-      // Explanation for: await createProxy(...)
-      // We need to await the module proxy creation because it can be async module for SSR layer
-      // due to having async dependencies.
-      // We only apply `the await` for Node.js as only Edge doesn't have external dependencies.
       let esmSource = `\
-import { createProxy } from "${MODULE_PROXY_PATH}"
-
-const proxy = ${isEdgeServer ? '' : 'await'} createProxy(String.raw\`${resourceKey}\`)
+import { registerClientReference } from "react-server-dom-webpack/server.edge";
 `
-      let cnt = 0
       for (const ref of clientRefs) {
-        if (ref === '') {
-          esmSource += `exports[''] = proxy['']\n`
-        } else if (ref === 'default') {
-          esmSource += `export default proxy.default;\n`
+        if (ref === 'default') {
+          esmSource += `export default registerClientReference(
+function() { throw new Error(${JSON.stringify(`Attempted to call the default \
+export of ${stringifiedResourceKey} from the server, but it's on the client. \
+It's not possible to invoke a client function from the server, it can only be \
+rendered as a Component or passed to props of a Client Component.`)}); },
+${stringifiedResourceKey},
+"default",
+);\n`
         } else {
-          esmSource += `const e${cnt} = proxy["${ref}"];\n`
-          esmSource += `export { e${cnt++} as ${ref} };\n`
+          esmSource += `export const ${ref} = registerClientReference(
+function() { throw new Error(${JSON.stringify(`Attempted to call ${ref}() from \
+the server but ${ref} is on the client. It's not possible to invoke a client \
+function from the server, it can only be rendered as a Component or passed to \
+props of a Client Component.`)}); },
+${stringifiedResourceKey},
+${JSON.stringify(ref)},
+);`
         }
       }
 
-      this.callback(null, esmSource, sourceMap)
-      return
+      return this.callback(null, esmSource, sourceMap)
+    } else if (assumedSourceType === 'commonjs') {
+      let cjsSource = `\
+const { createProxy } = require("${MODULE_PROXY_PATH}")
+
+module.exports = createProxy(${stringifiedResourceKey})
+`
+
+      return this.callback(null, cjsSource, sourceMap)
     }
   }
 

test/e2e/app-dir/dynamic-import/app/layout.tsx
@@ -1,0 +1,8 @@
+import { ReactNode } from 'react'
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/e2e/app-dir/dynamic-import/app/page.tsx
@@ -1,0 +1,16 @@
+// x-ref: https://github.com/vercel/next.js/issues/71840
+
+import { ElementType } from 'react'
+
+async function getImport(
+  slug: string,
+  exportName: string
+): Promise<ElementType> {
+  const moduleExports = await import(`./${slug}`)
+  return moduleExports[exportName]
+}
+
+export default async function Home() {
+  const Button = await getImport('button', 'Button')
+  return <Button />
+}

test/e2e/app-dir/dynamic-import/next.config.js
@@ -1,0 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {}
+
+module.exports = nextConfig

packages/next/src/build/webpack-config.ts
@@ -88,7 +88,6 @@ import {
   getBabelLoader,
   getReactCompilerLoader,
 } from './get-babel-loader-config'
-import type { NextFlightLoaderOptions } from './webpack/loaders/next-flight-loader'
 import {
   NEXT_PROJECT_ROOT,
   NEXT_PROJECT_ROOT_DIST_CLIENT,
@@ -519,13 +518,6 @@ export default async function getBaseWebpackConfig(
     babel: useSWCLoader ? swcDefaultLoader : babelLoader!,
   }
 
-  const nextFlightLoader = {
-    loader: 'next-flight-loader',
-    options: {
-      isEdgeServer,
-    } satisfies NextFlightLoaderOptions,
-  }
-
   const appServerLayerLoaders = hasAppDir
     ? [
         // When using Babel, we will have to add the SWC loader
@@ -539,7 +531,7 @@ export default async function getBaseWebpackConfig(
     : []
 
   const instrumentLayerLoaders = [
-    nextFlightLoader,
+    'next-flight-loader',
     // When using Babel, we will have to add the SWC loader
     // as an additional pass to handle RSC correctly.
     // This will cause some performance overhead but
@@ -549,7 +541,7 @@ export default async function getBaseWebpackConfig(
   ].filter(Boolean)
 
   const middlewareLayerLoaders = [
-    nextFlightLoader,
+    'next-flight-loader',
     // When using Babel, we will have to use SWC to do the optimization
     // for middleware to tree shake the unused default optimized imports like "next/server".
     // This will cause some performance overhead but
@@ -1366,7 +1358,7 @@ export default async function getBaseWebpackConfig(
                     isEdgeServer,
                   }),
                 },
-                use: nextFlightLoader,
+                use: 'next-flight-loader',
               },
             ]
           : []),

packages/next/src/build/webpack/plugins/flight-client-entry-plugin.ts
@@ -72,32 +72,29 @@ export type ActionManifest = {
   edge: Actions
 }
 
+export interface ModuleInfo {
+  moduleId: string | number
+  async: boolean
+}
+
 const pluginState = getProxiedPluginState({
   // A map to track "action" -> "list of bundles".
   serverActions: {} as ActionManifest['node'],
   edgeServerActions: {} as ActionManifest['edge'],
 
-  actionModServerId: {} as Record<
-    string,
-    {
-      server?: { moduleId: string | number; async: boolean }
-      client?: { moduleId: string | number; async: boolean }
-    }
-  >,
-  actionModEdgeServerId: {} as Record<
-    string,
-    {
-      server?: { moduleId: string | number; async: boolean }
-      client?: { moduleId: string | number; async: boolean }
-    }
-  >,
+  serverActionModules: {} as {
+    [workerName: string]: { server?: ModuleInfo; client?: ModuleInfo }
+  },
 
-  // Mapping of resource path to module id for server/edge server.
-  serverModuleIds: {} as Record<string, string | number>,
-  edgeServerModuleIds: {} as Record<string, string | number>,
+  edgeServerActionModules: {} as {
+    [workerName: string]: { server?: ModuleInfo; client?: ModuleInfo }
+  },
 
-  rscModuleIds: {} as Record<string, string | number>,
-  edgeRscModuleIds: {} as Record<string, string | number>,
+  ssrModules: {} as { [ssrModuleId: string]: ModuleInfo },
+  edgeSsrModules: {} as { [ssrModuleId: string]: ModuleInfo },
+
+  rscModules: {} as { [rscModuleId: string]: ModuleInfo },
+  edgeRscModules: {} as { [rscModuleId: string]: ModuleInfo },
 
   injectedClientEntries: {} as Record<string, string>,
 })
@@ -220,10 +217,15 @@ export class FlightClientEntryPlugin {
               .relative(compiler.context, modResource)
               .replace(/\/next\/dist\/esm\//, '/next/dist/')
 
+            const moduleInfo: ModuleInfo = {
+              moduleId: modId,
+              async: compilation.moduleGraph.isAsync(mod),
+            }
+
             if (this.isEdgeServer) {
-              pluginState.edgeRscModuleIds[key] = modId
+              pluginState.edgeRscModules[key] = moduleInfo
             } else {
-              pluginState.rscModuleIds[key] = modId
+              pluginState.rscModules[key] = moduleInfo
             }
           }
         }
@@ -244,12 +246,17 @@ export class FlightClientEntryPlugin {
             ssrNamedModuleId = `./${normalizePathSep(ssrNamedModuleId)}`
           }
 
+          const moduleInfo: ModuleInfo = {
+            moduleId: modId,
+            async: compilation.moduleGraph.isAsync(mod),
+          }
+
           if (this.isEdgeServer) {
-            pluginState.edgeServerModuleIds[
+            pluginState.edgeSsrModules[
               ssrNamedModuleId.replace(/\/next\/dist\/esm\//, '/next/dist/')
-            ] = modId
+            ] = moduleInfo
           } else {
-            pluginState.serverModuleIds[ssrNamedModuleId] = modId
+            pluginState.ssrModules[ssrNamedModuleId] = moduleInfo
           }
         }
       }
@@ -279,7 +286,7 @@ export class FlightClientEntryPlugin {
     > = []
     const createdSSRDependenciesForEntry: Record<
       string,
-      ReturnType<typeof this.injectClientEntryAndSSRModules>[2][]
+      ReturnType<typeof this.injectClientEntryAndSSRModules>[3][]
     > = {}
 
     const addActionEntryList: Array<ReturnType<typeof this.injectActionEntry>> =
@@ -393,7 +400,7 @@ export class FlightClientEntryPlugin {
           createdSSRDependenciesForEntry[clientEntryToInject.entryName] = []
         }
         createdSSRDependenciesForEntry[clientEntryToInject.entryName].push(
-          injected[2]
+          injected[3]
         )
 
         addClientEntryAndSSRModulesList.push(injected)
@@ -450,12 +457,14 @@ export class FlightClientEntryPlugin {
       invalidator.invalidate([COMPILER_NAMES.client])
     }
 
-    // Client compiler is invalidated before awaiting the compilation of the SSR client component entries
-    // so that the client compiler is running in parallel to the server compiler.
+    // Client compiler is invalidated before awaiting the compilation of the SSR
+    // and RSC client component entries so that the client compiler is running
+    // in parallel to the server compiler.
     await Promise.all(
-      addClientEntryAndSSRModulesList.map(
-        (addClientEntryAndSSRModules) => addClientEntryAndSSRModules[1]
-      )
+      addClientEntryAndSSRModulesList.flatMap((addClientEntryAndSSRModules) => [
+        addClientEntryAndSSRModules[1],
+        addClientEntryAndSSRModules[2],
+      ])
     )
 
     // Wait for action entries to be added.
@@ -737,7 +746,8 @@ export class FlightClientEntryPlugin {
     absolutePagePath?: string
   }): [
     shouldInvalidate: boolean,
-    addEntryPromise: Promise<void>,
+    addSSREntryPromise: Promise<void>,
+    addRSCEntryPromise: Promise<void>,
     ssrDep: ReturnType<typeof webpack.EntryPlugin.createDependency>,
   ] {
     let shouldInvalidate = false
@@ -766,7 +776,7 @@ export class FlightClientEntryPlugin {
       server: false,
     })}!`
 
-    const clientSSRLoader = `next-flight-client-entry-loader?${stringify({
+    const clientServerLoader = `next-flight-client-entry-loader?${stringify({
       modules: modules.map((x) => JSON.stringify(x)),
       server: true,
     })}!`
@@ -809,33 +819,30 @@ export class FlightClientEntryPlugin {
       pluginState.injectedClientEntries[bundlePath] = clientBrowserLoader
     }
 
-    // Inject the entry to the server compiler (__ssr__).
-    const clientComponentEntryDep = webpack.EntryPlugin.createDependency(
-      clientSSRLoader,
-      {
-        name: bundlePath,
-      }
+    const clientComponentSSREntryDep = webpack.EntryPlugin.createDependency(
+      clientServerLoader,
+      { name: bundlePath }
+    )
+
+    const clientComponentRSCEntryDep = webpack.EntryPlugin.createDependency(
+      clientServerLoader,
+      { name: bundlePath }
     )
 
     return [
       shouldInvalidate,
-      // Add the dependency to the server compiler.
-      // This promise is awaited later using `Promise.all` in order to parallelize adding the entries.
-      // It ensures we can parallelize the SSR and Client compiler entries.
-      this.addEntry(
-        compilation,
-        // Reuse compilation context.
-        compiler.context,
-        clientComponentEntryDep,
-        {
-          // By using the same entry name
-          name: entryName,
-          // Layer should be client for the SSR modules
-          // This ensures the client components are bundled on client layer
-          layer: WEBPACK_LAYERS.serverSideRendering,
-        }
-      ),
-      clientComponentEntryDep,
+      // Add the entries to the server compiler for the SSR and RSC layers. The
+      // promises are awaited later using `Promise.all` in order to parallelize
+      // adding the entries.
+      this.addEntry(compilation, compiler.context, clientComponentSSREntryDep, {
+        name: entryName,
+        layer: WEBPACK_LAYERS.serverSideRendering,
+      }),
+      this.addEntry(compilation, compiler.context, clientComponentRSCEntryDep, {
+        name: entryName,
+        layer: WEBPACK_LAYERS.reactServerComponents,
+      }),
+      clientComponentSSREntryDep,
     ]
   }
 
@@ -968,8 +975,8 @@ export class FlightClientEntryPlugin {
         const fromClient = /&__client_imported__=true/.test(mod.request)
 
         const mapping = this.isEdgeServer
-          ? pluginState.actionModEdgeServerId
-          : pluginState.actionModServerId
+          ? pluginState.edgeServerActionModules
+          : pluginState.serverActionModules
 
         if (!mapping[chunkGroup.name]) {
           mapping[chunkGroup.name] = {}
@@ -985,7 +992,7 @@ export class FlightClientEntryPlugin {
       const action = pluginState.serverActions[id]
       for (let name in action.workers) {
         const modId =
-          pluginState.actionModServerId[name][
+          pluginState.serverActionModules[name][
             action.layer[name] === WEBPACK_LAYERS.actionBrowser
               ? 'client'
               : 'server'
@@ -999,7 +1006,7 @@ export class FlightClientEntryPlugin {
       const action = pluginState.edgeServerActions[id]
       for (let name in action.workers) {
         const modId =
-          pluginState.actionModEdgeServerId[name][
+          pluginState.edgeServerActionModules[name][
             action.layer[name] === WEBPACK_LAYERS.actionBrowser
               ? 'client'
               : 'server'

packages/next/src/build/webpack/plugins/flight-manifest-plugin.ts
@@ -27,6 +27,7 @@ import {
 import type { ChunkGroup } from 'webpack'
 import { encodeURIPath } from '../../../shared/lib/encode-uri-path'
 import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
+import type { ModuleInfo } from './flight-client-entry-plugin'
 
 interface Options {
   dev: boolean
@@ -43,11 +44,11 @@ type ModuleId = string | number /*| null*/
 export type ManifestChunks = Array<string>
 
 const pluginState = getProxiedPluginState({
-  serverModuleIds: {} as Record<string, string | number>,
-  edgeServerModuleIds: {} as Record<string, string | number>,
+  ssrModules: {} as { [ssrModuleId: string]: ModuleInfo },
+  edgeSsrModules: {} as { [ssrModuleId: string]: ModuleInfo },
 
-  rscModuleIds: {} as Record<string, string | number>,
-  edgeRscModuleIds: {} as Record<string, string | number>,
+  rscModules: {} as { [rscModuleId: string]: ModuleInfo },
+  edgeRscModules: {} as { [rscModuleId: string]: ModuleInfo },
 })
 
 export interface ManifestNode {
@@ -356,12 +357,18 @@ export class ClientReferenceManifestPlugin {
         }
 
         function addClientReference() {
+          const isAsync = Boolean(
+            compilation.moduleGraph.isAsync(mod) ||
+              pluginState.ssrModules[ssrNamedModuleId]?.async ||
+              pluginState.edgeSsrModules[ssrNamedModuleId]?.async
+          )
+
           const exportName = resource
           manifest.clientModules[exportName] = {
             id: modId,
             name: '*',
             chunks: requiredChunks,
-            async: false,
+            async: isAsync,
           }
           if (esmResource) {
             const edgeExportName = esmResource
@@ -372,9 +379,9 @@ export class ClientReferenceManifestPlugin {
 
         function addSSRIdMapping() {
           const exportName = resource
-          if (
-            typeof pluginState.serverModuleIds[ssrNamedModuleId] !== 'undefined'
-          ) {
+          const moduleInfo = pluginState.ssrModules[ssrNamedModuleId]
+
+          if (moduleInfo) {
             moduleIdMapping[modId] = moduleIdMapping[modId] || {}
             moduleIdMapping[modId]['*'] = {
               ...manifest.clientModules[exportName],
@@ -382,14 +389,14 @@ export class ClientReferenceManifestPlugin {
               // side with our architecture of Webpack / Turbopack. We can keep
               // this field empty to save some bytes.
               chunks: [],
-              id: pluginState.serverModuleIds[ssrNamedModuleId],
+              id: moduleInfo.moduleId,
+              async: moduleInfo.async,
             }
           }
 
-          if (
-            typeof pluginState.edgeServerModuleIds[ssrNamedModuleId] !==
-            'undefined'
-          ) {
+          const edgeModuleInfo = pluginState.edgeSsrModules[ssrNamedModuleId]
+
+          if (edgeModuleInfo) {
             edgeModuleIdMapping[modId] = edgeModuleIdMapping[modId] || {}
             edgeModuleIdMapping[modId]['*'] = {
               ...manifest.clientModules[exportName],
@@ -397,16 +404,17 @@ export class ClientReferenceManifestPlugin {
               // side with our architecture of Webpack / Turbopack. We can keep
               // this field empty to save some bytes.
               chunks: [],
-              id: pluginState.edgeServerModuleIds[ssrNamedModuleId],
+              id: edgeModuleInfo.moduleId,
+              async: edgeModuleInfo.async,
             }
           }
         }
 
         function addRSCIdMapping() {
           const exportName = resource
-          if (
-            typeof pluginState.rscModuleIds[rscNamedModuleId] !== 'undefined'
-          ) {
+          const moduleInfo = pluginState.rscModules[rscNamedModuleId]
+
+          if (moduleInfo) {
             rscIdMapping[modId] = rscIdMapping[modId] || {}
             rscIdMapping[modId]['*'] = {
               ...manifest.clientModules[exportName],
@@ -414,14 +422,14 @@ export class ClientReferenceManifestPlugin {
               // side with our architecture of Webpack / Turbopack. We can keep
               // this field empty to save some bytes.
               chunks: [],
-              id: pluginState.rscModuleIds[rscNamedModuleId],
+              id: moduleInfo.moduleId,
+              async: moduleInfo.async,
             }
           }
 
-          if (
-            typeof pluginState.edgeRscModuleIds[rscNamedModuleId] !==
-            'undefined'
-          ) {
+          const edgeModuleInfo = pluginState.ssrModules[rscNamedModuleId]
+
+          if (edgeModuleInfo) {
             edgeRscIdMapping[modId] = edgeRscIdMapping[modId] || {}
             edgeRscIdMapping[modId]['*'] = {
               ...manifest.clientModules[exportName],
@@ -429,7 +437,8 @@ export class ClientReferenceManifestPlugin {
               // side with our architecture of Webpack / Turbopack. We can keep
               // this field empty to save some bytes.
               chunks: [],
-              id: pluginState.edgeRscModuleIds[rscNamedModuleId],
+              id: edgeModuleInfo.moduleId,
+              async: edgeModuleInfo.async,
             }
           }
         }

packages/next/src/build/webpack/plugins/next-trace-entrypoints-plugin.ts
@@ -24,6 +24,7 @@ import { getPageFilePath } from '../../entries'
 import { resolveExternal } from '../../handle-externals'
 import swcLoader from '../loaders/next-swc-loader'
 import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
+import { isClientComponentEntryModule } from '../loaders/utils'
 
 const PLUGIN_NAME = 'TraceEntryPointsPlugin'
 export const TRACE_IGNORES = [
@@ -533,10 +534,7 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
                 // fallback to reading raw source file, this may fail
                 // due to unsupported syntax but best effort attempt
                 let usingOriginalSource = false
-                if (
-                  !source ||
-                  source.toString().includes('next-flight-loader/module-proxy')
-                ) {
+                if (!source || isClientComponentEntryModule(mod)) {
                   source = await readOriginalSource(path)
                   usingOriginalSource = true
                 }

commit 762761dbded372ab8471926d45de8d5cfdce6aef

Handle empty ESM client modules
If an ESM module only includes a `'use client'` directive and no
exports, which is the case for one of the modules that are included in
`@chakra-ui/system` for example, we must not try to add
`registerClientReference` statements for this module. This is a
regression from #71968.

fixes #72358
closes #72359

packages/next/src/build/analysis/get-page-static-info.ts
@@ -135,10 +135,13 @@ export function getRSCModuleInformation(
     }
   }
 
-  const clientRefs = clientInfoMatch?.[1]?.split(',')
-  const clientEntryType = clientInfoMatch?.[2] as 'cjs' | 'auto'
+  const clientRefsString = clientInfoMatch?.[1]
+  const clientRefs = clientRefsString ? clientRefsString.split(',') : []
+  const clientEntryType = clientInfoMatch?.[2] as 'cjs' | 'auto' | undefined
 
-  const type = clientRefs ? RSC_MODULE_TYPES.client : RSC_MODULE_TYPES.server
+  const type = clientInfoMatch
+    ? RSC_MODULE_TYPES.client
+    : RSC_MODULE_TYPES.server
 
   return {
     type,

packages/next/src/build/webpack/loaders/next-flight-loader/index.ts
@@ -34,10 +34,7 @@ export function getAssumedSourceType(
 
   if (sourceType === 'auto') {
     if (detectedClientEntryType === 'auto') {
-      if (
-        clientRefs.length === 0 ||
-        (clientRefs.length === 1 && clientRefs[0] === '')
-      ) {
+      if (clientRefs.length === 0) {
         // If there's zero export detected in the client boundary, and it's the
         // `auto` type, we can safely assume it's a CJS module because it doesn't
         // have ESM exports.
@@ -102,6 +99,10 @@ export default function transformSource(
     const stringifiedResourceKey = JSON.stringify(resourceKey)
 
     if (assumedSourceType === 'module') {
+      if (clientRefs.length === 0) {
+        return this.callback(null, 'export {}')
+      }
+
       if (clientRefs.includes('*')) {
         this.callback(
           new Error(

test/e2e/app-dir/esm-client-module-without-exports/app/layout.tsx
@@ -1,0 +1,8 @@
+import { ReactNode } from 'react'
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/e2e/app-dir/esm-client-module-without-exports/app/page.tsx
@@ -1,0 +1,5 @@
+import 'lib'
+
+export default function Page() {
+  return <p>hello world</p>
+}

test/e2e/app-dir/esm-client-module-without-exports/esm-client-module-without-exports.test.ts
@@ -1,0 +1,12 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('esm-client-module-without-exports', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should render without errors', async () => {
+    const html = await next.render('/')
+    expect(html).toContain('hello world')
+  })
+})

test/e2e/app-dir/esm-client-module-without-exports/next.config.js
@@ -1,0 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {}
+
+module.exports = nextConfig