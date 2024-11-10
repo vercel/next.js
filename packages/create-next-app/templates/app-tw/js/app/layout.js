44060c90baf4b345be800d6f4482c706b71eaf1d
Jade van Dörsten <vcs@vandor.sx>
2024-11-10 14:28:34 -0600

    Revert font antialiasing

--- packages/create-next-app/templates/app-tw/js/app/layout.js
+++ packages/create-next-app/templates/app-tw/js/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}`}
       >
         {children}
       </body>
--- packages/create-next-app/templates/app-tw/ts/app/layout.tsx
+++ packages/create-next-app/templates/app-tw/ts/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}`}
       >
         {children}
       </body>
