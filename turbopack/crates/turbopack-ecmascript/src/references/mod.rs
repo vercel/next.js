829a3286fd314388d733ab4ffd5e1aea656d60d8
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-11 12:00:31 +0900

    Fix

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -1998,7 +1998,7 @@ async fn handle_call<G: Fn(Vec<Effect>) + Send + Sync>(
                                     js_value_to_pattern(&linked_func_call)
                                 };
                                 analysis.add_reference(
-                                    DirAssetReference::new(source, Pattern::new(abs_pattern))
+                                    DirAssetReference::new(*source, Pattern::new(abs_pattern))
                                         .to_resolved()
                                         .await?,
                                 );
@@ -2060,7 +2060,7 @@ async fn handle_call<G: Fn(Vec<Effect>) + Send + Sync>(
                     js_value_to_pattern(&linked_func_call)
                 };
                 analysis.add_reference(
-                    DirAssetReference::new(source, Pattern::new(abs_pattern))
+                    DirAssetReference::new(*source, Pattern::new(abs_pattern))
                         .to_resolved()
                         .await?,
                 );
