4843b49bda23b28aab236a7add33a7991a90243d
Jiachi Liu <inbox@huozhi.im>
2024-11-11 19:11:40 +0100

    update snapshot

--- test/development/acceptance-app/hydration-error.test.ts
+++ test/development/acceptance-app/hydration-error.test.ts
@@ -457,8 +457,8 @@ describe('Error overlay for hydration errors in App router', () => {
               <InnerScrollAndFocusHandler segmentPath={[...]} focusAndScrollRef={{apply:false, ...}}>
                 <ErrorBoundary errorComponent={undefined} errorStyles={undefined} errorScripts={undefined}>
                   <LoadingBoundary hasLoading={false} loading={undefined} loadingStyles={undefined} loadingScripts={undefined}>
-                    <NotFoundBoundary notFound={[...]} notFoundStyles={[...]}>
-                      <NotFoundErrorBoundary pathname="/" notFound={[...]} notFoundStyles={[...]} missingSlots={Set}>
+                    <HTTPErrorFallbackBoundary notFound={[...]}>
+                      <HTTPErrorFallbackErrorBoundary pathname="/" notFound={[...]} errorStatus={undefined} missingSlots={Set}>
                         <RedirectBoundary>
                           <RedirectErrorBoundary router={{...}}>
                             <InnerLayoutRouter parallelRouterKey="children" url="/" tree={[...]} childNodes={Map} ...>
@@ -927,7 +927,7 @@ describe('Error overlay for hydration errors in App router', () => {
     if (isTurbopack) {
       expect(fullPseudoHtml).toMatchInlineSnapshot(`
         "...
-          <NotFoundErrorBoundary pathname="/" notFound={[...]} notFoundStyles={[...]} missingSlots={Set}>
+          <HTTPErrorFallbackErrorBoundary pathname="/" notFound={[...]} errorStatus={undefined} missingSlots={Set}>
             <RedirectBoundary>
               <RedirectErrorBoundary router={{...}}>
                 <InnerLayoutRouter parallelRouterKey="children" url="/" tree={[...]} childNodes={Map} segmentPath={[...]} ...>
@@ -947,7 +947,7 @@ describe('Error overlay for hydration errors in App router', () => {
     } else {
       expect(fullPseudoHtml).toMatchInlineSnapshot(`
         "...
-          <NotFoundErrorBoundary pathname="/" notFound={[...]} notFoundStyles={[...]} missingSlots={Set}>
+          <HTTPErrorFallbackErrorBoundary pathname="/" notFound={[...]} errorStatus={undefined} missingSlots={Set}>
             <RedirectBoundary>
               <RedirectErrorBoundary router={{...}}>
                 <InnerLayoutRouter parallelRouterKey="children" url="/" tree={[...]} childNodes={Map} segmentPath={[...]} ...>
