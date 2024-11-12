754c54115279d5242ff99ca52f928292a5325368
samcx <sam@vercel.com>
2024-11-12 13:26:10 -0800

    test: update server-component-compiler-errors-in-pages.test.ts

--- test/development/acceptance/server-component-compiler-errors-in-pages.test.ts
+++ test/development/acceptance/server-component-compiler-errors-in-pages.test.ts
@@ -67,8 +67,8 @@ describe('Error Overlay for server components compiler errors in pages', () => {
       expect(next.normalizeTestDirContent(await session.getRedboxSource()))
         .toMatchInlineSnapshot(`
         "./components/Comp.js
-        Error:   x You're importing a component that needs "next/headers". That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/getting-started/
-          | react-essentials#server-components
+        Error:   x You're importing a component that needs "next/headers". That only works in a Server Component which is not supported in the pages/ directory. Read more: hhttps://nextjs.org/docs/app/building-
+          | your-application/rendering/server-components
           |
           |
            ,-[1:1]
@@ -129,8 +129,8 @@ describe('Error Overlay for server components compiler errors in pages', () => {
         )
       ).toMatchInlineSnapshot(`
         "./components/Comp.js
-        Error:   x You're importing a component that needs "server-only". That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/getting-started/
-          | react-essentials#server-components
+        Error:   x You're importing a component that needs "server-only". That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/app/building-
+          | your-application/rendering/server-components
           |
           |
            ,-[1:1]
@@ -188,8 +188,8 @@ describe('Error Overlay for server components compiler errors in pages', () => {
         )
       ).toMatchInlineSnapshot(`
         "./components/Comp.js
-        Error:   x You're importing a component that needs "unstable_after". That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/getting-
-          | started/react-essentials#server-components
+        Error:   x You're importing a component that needs "unstable_after". That only works in a Server Component which is not supported in the pages/ directory. Read more: https://nextjs.org/docs/app/building-
+          | your-application/rendering/server-components
           |
           |
            ,-[1:1]
