33f7fde5eb34c28a0ec73d76706f0aeec443a259
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-14 14:16:44 +0900

    fix more

--- crates/next-core/src/next_shared/transforms/next_optimize_server_react.rs
+++ crates/next-core/src/next_shared/transforms/next_optimize_server_react.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use next_custom_transforms::transforms::optimize_server_react::{optimize_server_react, Config};
 use swc_core::ecma::ast::*;
-use turbo_tasks::Vc;
+use turbo_tasks::{ResolvedVc, Vc};
 use turbopack::module_options::{ModuleRule, ModuleRuleEffect};
 use turbopack_ecmascript::{CustomTransformer, EcmascriptInputTransform, TransformContext};
 
@@ -20,8 +20,8 @@ pub fn get_next_optimize_server_react_rule(
     ModuleRule::new(
         module_rule_match_js_no_url(enable_mdx_rs),
         vec![ModuleRuleEffect::ExtendEcmascriptTransforms {
-            prepend: Vc::cell(vec![]),
-            append: Vc::cell(vec![transformer]),
+            prepend: ResolvedVc::cell(vec![]),
+            append: ResolvedVc::cell(vec![transformer]),
         }],
     )
 }
