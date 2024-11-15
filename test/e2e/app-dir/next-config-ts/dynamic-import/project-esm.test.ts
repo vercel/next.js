883243d9aeaf645c8fbfeb827c54e8f8a638fb94
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-16 04:45:13 +0900

    test: dynamic import

--- test/e2e/app-dir/next-config-ts/dynamic-import/project-esm.test.ts
+++ test/e2e/app-dir/next-config-ts/dynamic-import/project-esm.test.ts
@@ -1,11 +1,14 @@
 import { nextTestSetup } from 'e2e-utils'
 
-describe('next-config-ts-dynamic-import', () => {
+describe('next-config-ts - dynamic import (project ESM)', () => {
   const { next } = nextTestSetup({
     files: __dirname,
+    packageJson: {
+      type: 'module',
+    },
   })
 
-  it('should support dynamic import', async () => {
+  it('should support dynamic import (project ESM)', async () => {
     const $ = await next.render$('/')
     expect($('p').text()).toBe('foo')
   })
