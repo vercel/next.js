a6f5588fedbce5e5c6d95648c0fcc4d2d837dce6
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-15 17:45:09 +0900

    cover namespaces

--- packages/next-codemod/transforms/revalidate-to-expire.ts
+++ packages/next-codemod/transforms/revalidate-to-expire.ts
@@ -9,44 +9,69 @@ export default function transformer(file: FileInfo, _api: API) {
     return
   }
 
-  const imports = root
-    .find(j.ImportDeclaration, {
-      source: {
-        value: 'next/cache',
-      },
-    })
-    .map((path) => {
-      path.node.specifiers.forEach((specifier) => {
-        if (specifier.type !== 'ImportSpecifier') {
-          return
-        }
+  const nextCacheImports = root.find(j.ImportDeclaration, {
+    source: {
+      value: 'next/cache',
+    },
+  })
+
+  if (!nextCacheImports.length) {
+    return
+  }
+
+  let nextCacheNamespace = ''
+
+  nextCacheImports.forEach((path) => {
+    path.node.specifiers.forEach((specifier) => {
+      if (specifier.type === 'ImportSpecifier') {
         if (specifier.imported.name === 'revalidateTag') {
           specifier.imported.name = 'expireTag'
         }
         if (specifier.imported.name === 'revalidatePath') {
           specifier.imported.name = 'expirePath'
         }
-      })
-      return path
-    })
+      }
 
-  if (!imports.length) {
-    return
-  }
-
-  root
-    .find(j.CallExpression, {
-      callee: {
-        type: 'Identifier',
-        name: (n) => n === 'revalidateTag' || n === 'revalidatePath',
-      },
-    })
-    .forEach((path) => {
-      if (path.node.callee.type === 'Identifier') {
-        path.node.callee.name =
-          path.node.callee.name === 'revalidateTag' ? 'expireTag' : 'expirePath'
+      // import * as <namespace> from 'next/cache'
+      if (specifier.type === 'ImportNamespaceSpecifier') {
+        nextCacheNamespace = specifier.local.name
       }
     })
+  })
+
+  root.find(j.CallExpression).forEach((path) => {
+    const callee = path.node.callee
+
+    // Handle direct function calls:
+    // ```ts
+    // revalidateTag('next')
+    // revalidatePath('/next')
+    // ```
+    if (
+      callee.type === 'Identifier' &&
+      (callee.name === 'revalidateTag' || callee.name === 'revalidatePath')
+    ) {
+      callee.name = callee.name === 'revalidateTag' ? 'expireTag' : 'expirePath'
+    }
+
+    // Handle namespace calls:
+    // ```ts
+    // import * as cache from 'next/cache'
+    // cache.revalidateTag('next')
+    // cache.revalidatePath('/next')
+    // ```
+    if (
+      callee.type === 'MemberExpression' &&
+      callee.object.type === 'Identifier' &&
+      callee.object.name === nextCacheNamespace &&
+      callee.property.type === 'Identifier' &&
+      (callee.property.name === 'revalidateTag' ||
+        callee.property.name === 'revalidatePath')
+    ) {
+      callee.property.name =
+        callee.property.name === 'revalidateTag' ? 'expireTag' : 'expirePath'
+    }
+  })
 
   return root.toSource()
 }
