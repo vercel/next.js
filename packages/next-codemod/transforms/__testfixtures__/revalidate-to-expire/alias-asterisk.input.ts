9c6ea01813de0ab6e8c52ab24708f05793454ca7
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-15 22:15:54 +0900

    test: add bracket string literal

--- packages/next-codemod/transforms/__testfixtures__/revalidate-to-expire/alias-asterisk.input.ts
+++ packages/next-codemod/transforms/__testfixtures__/revalidate-to-expire/alias-asterisk.input.ts
@@ -1,6 +1,8 @@
-import * as cache from 'next/cache';
+import * as cache from "next/cache";
 
 export async function GET() {
-  cache.revalidatePath('next');
-  cache.revalidateTag('next');
+  cache.revalidatePath("next");
+  cache.revalidateTag("next");
+  cache["revalidatePath"]("next");
+  cache["revalidateTag"]("next");
 }
