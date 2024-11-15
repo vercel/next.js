113bc05e702e8f482d8d7fbf892f0e173f4333e8
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-16 04:52:22 +0900

    test: export default

---
+++ test/e2e/app-dir/next-config-ts/export-default/next-config-mts.test.ts
@@ -0,0 +1,18 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('next-config-ts - export default (next.config.mts)', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+    skipStart: true,
+  })
+
+  beforeAll(async () => {
+    await next.renameFile('next.config.ts', 'next.config.mts')
+    await next.start()
+  })
+
+  it('should support export default (next.config.mts)', async () => {
+    const $ = await next.render$('/')
+    expect($('p').text()).toBe('foo')
+  })
+})
--- test/e2e/app-dir/next-config-ts/export-default/project-cjs.test.ts
+++ test/e2e/app-dir/next-config-ts/export-default/project-cjs.test.ts
@@ -1,11 +1,11 @@
 import { nextTestSetup } from 'e2e-utils'
 
-describe('next-config-ts-export-default', () => {
+describe('next-config-ts - export default (project CJS)', () => {
   const { next } = nextTestSetup({
     files: __dirname,
   })
 
-  it('should support export default { ... }', async () => {
+  it('should support export default (project CJS)', async () => {
     const $ = await next.render$('/')
     expect($('p').text()).toBe('foo')
   })
