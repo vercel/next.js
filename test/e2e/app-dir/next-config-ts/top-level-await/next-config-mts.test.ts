7b3da318d8e74eecd97fb309d549052c3a100deb
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-19 01:26:27 +0900

    test: top-level await

---
+++ test/e2e/app-dir/next-config-ts/top-level-await/next-config-mts.test.ts
@@ -0,0 +1,18 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('next-config-ts - top-level await (next.config.mts)', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+    skipStart: true,
+  })
+
+  beforeAll(async () => {
+    await next.renameFile('next.config.ts', 'next.config.mts')
+    await next.start()
+  })
+
+  it('should support top-level await (next.config.mts)', async () => {
+    const $ = await next.render$('/')
+    expect($('p').text()).toBe('foo')
+  })
+})
--- test/e2e/app-dir/next-config-ts/top-level-await/project-esm.test.ts
+++ test/e2e/app-dir/next-config-ts/top-level-await/project-esm.test.ts
@@ -1,11 +1,14 @@
 import { nextTestSetup } from 'e2e-utils'
 
-describe('next-config-ts-top-level-await', () => {
+describe('next-config-ts - top-level await (project ESM)', () => {
   const { next } = nextTestSetup({
     files: __dirname,
+    packageJson: {
+      type: 'module',
+    },
   })
 
-  it('should support top-level await', async () => {
+  it('should support top-level await (project ESM)', async () => {
     const $ = await next.render$('/')
     expect($('p').text()).toBe('foo')
   })
