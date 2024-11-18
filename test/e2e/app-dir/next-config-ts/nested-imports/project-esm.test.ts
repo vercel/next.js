ecc4a8fd67a02d567b3d2c12abc7cc4387447c2e
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-18 22:22:16 +0900

    test: nested imports

--- test/e2e/app-dir/next-config-ts/nested-imports/project-esm.test.ts
+++ test/e2e/app-dir/next-config-ts/nested-imports/project-esm.test.ts
@@ -1,11 +1,14 @@
 import { nextTestSetup } from 'e2e-utils'
 
-describe('next-config-ts-nested-imports', () => {
+describe('next-config-ts - nested imports (project ESM)', () => {
   const { next } = nextTestSetup({
     files: __dirname,
+    packageJson: {
+      type: 'module',
+    },
   })
 
-  it('should handle nested imports', async () => {
+  it('should handle nested imports (project ESM)', async () => {
     const $ = await next.render$('/')
     expect($('p').text()).toBe('foobarbaz')
   })
