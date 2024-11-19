eea7a65246405fe7947b7179b3fa863936d8f92d
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-11-19 15:32:35 +0100

    Review comments: cleanup tests

--- test/e2e/edge-pages-support/index.test.ts
+++ test/e2e/edge-pages-support/index.test.ts
@@ -11,18 +11,15 @@ describe('edge-render-getserversideprops', () => {
   })
 
   if ((global as any).isNextStart) {
-    it('should not output trace files for edge routes', async () => {
-      expect(await fs.pathExists(join(next.testDir, '.next/pages'))).toBe(false)
-      if (process.env.TURBOPACK) {
-        expect(
-          await fs.pathExists(
-            join(
-              next.testDir,
-              '.next/server/pages/[id]/middleware-manifest.json'
-            )
-          )
-        ).toBe(true)
-      } else {
+    // Turbopack doesn't have entry chunks for edge routes like Webpack does, so there is no fixed
+    // known path where the nft file would be written to.
+    ;(process.env.TURBOPACK ? it.skip : it)(
+      'should not output trace files for edge routes',
+      async () => {
+        /* eslint-disable jest/no-standalone-expect */
+        expect(await fs.pathExists(join(next.testDir, '.next/pages'))).toBe(
+          false
+        )
         expect(
           await fs.pathExists(join(next.testDir, '.next/server/pages/[id].js'))
         ).toBe(true)
@@ -31,17 +28,6 @@ describe('edge-render-getserversideprops', () => {
             join(next.testDir, '.next/server/pages/[id].js.nft.json')
           )
         ).toBe(false)
-      }
-      if (process.env.TURBOPACK) {
-        expect(
-          await fs.pathExists(
-            join(
-              next.testDir,
-              '.next/server/pages/index/middleware-manifest.json'
-            )
-          )
-        ).toBe(true)
-      } else {
         expect(
           await fs.pathExists(join(next.testDir, '.next/server/pages/index.js'))
         ).toBe(true)
@@ -50,8 +36,9 @@ describe('edge-render-getserversideprops', () => {
             join(next.testDir, '.next/server/pages/index.js.nft.json')
           )
         ).toBe(false)
+        /* eslint-enable jest/no-standalone-expect */
       }
-    })
+    )
   }
 
   it('should have correct query for pages/api', async () => {
