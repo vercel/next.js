1c3bb21ea0393262c58fc84fb438d031a95d46eb
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-11-20 15:38:22 +0100

    WIP example

---
+++ bench/heavy-npm-deps/components/big.js
@@ -0,0 +1,12 @@
+import { MantineComponent } from './mantine'
+import { LodashComponent } from './lodash'
+import { MermaidComponent } from './mermaid'
+export function Component() {
+  return (
+    <>
+      <MantineComponent />
+      <LodashComponent />
+      <MermaidComponent />
+    </>
+  )
+}
