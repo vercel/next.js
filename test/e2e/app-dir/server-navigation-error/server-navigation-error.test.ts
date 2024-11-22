31a27c954bcf81d796c318b26154570e0f15cc2b
Jiachi Liu <inbox@huozhi.im>
2024-11-22 17:38:42 +0100

    add snapshot for source

--- test/e2e/app-dir/server-navigation-error/server-navigation-error.test.ts
+++ test/e2e/app-dir/server-navigation-error/server-navigation-error.test.ts
@@ -1,21 +1,83 @@
 import { nextTestSetup } from 'e2e-utils'
-import { assertHasRedbox, getRedboxDescription } from 'next-test-utils'
+import {
+  assertHasRedbox,
+  getRedboxDescription,
+  getRedboxSource,
+} from 'next-test-utils'
 
 describe('server-navigation-error', () => {
   const { next } = nextTestSetup({
     files: __dirname,
   })
 
-  describe('redirect', () => {
-    it('should error on navigation usage in pages router', async () => {
+  describe('pages router', () => {
+    it('should error on navigation API redirect', async () => {
       const browser = await next.browser('/pages/redirect')
       await assertHasRedbox(browser)
       expect(await getRedboxDescription(browser)).toMatch(
         `Next.js navigation API is not allowed to be used in Pages Router.`
       )
+      const source = await getRedboxSource(browser)
+      if (process.env.TURBOPACK) {
+        expect(source).toMatchInlineSnapshot(`
+          "pages/pages/redirect.tsx (4:10) @ Page
+
+            2 |
+            3 | export default function Page() {
+          > 4 |   redirect('/')
+              |          ^
+            5 | }
+            6 |"
+        `)
+      } else {
+        expect(source).toMatchInlineSnapshot(`
+          "pages/pages/redirect.tsx (4:12) @ Page
+
+            2 |
+            3 | export default function Page() {
+          > 4 |   redirect('/')
+              |            ^
+            5 | }
+            6 |"
+        `)
+      }
     })
 
-    it('should error on navigation usage in middleware ', async () => {
+    it('should error on navigation API notFound', async () => {
+      const browser = await next.browser('/pages/not-found')
+      await assertHasRedbox(browser)
+      expect(await getRedboxDescription(browser)).toMatch(
+        `Next.js navigation API is not allowed to be used in Pages Router.`
+      )
+      const source = await getRedboxSource(browser)
+      if (process.env.TURBOPACK) {
+        expect(source).toMatchInlineSnapshot(`
+          "pages/pages/not-found.tsx (4:10) @ Page
+
+            2 |
+            3 | export default function Page() {
+          > 4 |   notFound()
+              |          ^
+            5 | }
+            6 |"
+        `)
+      } else {
+        expect(source).toMatchInlineSnapshot(`
+          "pages/pages/not-found.tsx (4:11) @ notFound
+
+            2 |
+            3 | export default function Page() {
+          > 4 |   notFound()
+              |           ^
+            5 | }
+            6 |"
+        `)
+      }
+    })
+  })
+
+  describe('middleware', () => {
+    it('should error on navigation API redirect ', async () => {
       const browser = await next.browser('/middleware/redirect')
       // FIXME: the first request to middleware error load didn't show the redbox, need one more reload
       await browser.refresh()
@@ -23,24 +85,29 @@ describe('server-navigation-error', () => {
       expect(await getRedboxDescription(browser)).toMatch(
         `Next.js navigation API is not allowed to be used in Middleware.`
       )
-    })
-  })
-
-  describe('not-found', () => {
-    it('should error on navigation usage in pages router', async () => {
-      const browser = await next.browser('/pages/not-found')
-      await assertHasRedbox(browser)
-      expect(await getRedboxDescription(browser)).toMatch(
-        `Next.js navigation API is not allowed to be used in Pages Router.`
-      )
+      // No trace for middleware as it's thrown in the early phase of the adapter
+      const source = await getRedboxSource(browser)
+      if (process.env.TURBOPACK) {
+        expect(source).toMatchInlineSnapshot(`null`)
+      } else {
+        expect(source).toMatchInlineSnapshot(`null`)
+      }
     })
 
-    it('should error on navigation usage in middleware ', async () => {
+    it('should error on navigation API not-found', async () => {
       const browser = await next.browser('/middleware/not-found')
       await assertHasRedbox(browser)
       expect(await getRedboxDescription(browser)).toMatch(
         `Next.js navigation API is not allowed to be used in Middleware.`
       )
+      const source = await getRedboxSource(browser)
+
+      // No trace for middleware as it's thrown in the early phase of the adapter
+      if (process.env.TURBOPACK) {
+        expect(source).toMatchInlineSnapshot(`null`)
+      } else {
+        expect(source).toMatchInlineSnapshot(`null`)
+      }
     })
   })
 })
