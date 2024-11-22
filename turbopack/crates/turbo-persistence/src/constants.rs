81224169cc4c6133ff43f8da00135d4f8d6fb32b
Tobias Koppers <tobias.koppers@googlemail.com>
2024-11-22 16:03:06 +0100

    increase SST file size, parellel finishing

--- turbopack/crates/turbo-persistence/src/constants.rs
+++ turbopack/crates/turbo-persistence/src/constants.rs
@@ -7,10 +7,10 @@ pub const MAX_SMALL_VALUE_SIZE: usize = 64 * 1024 - 1;
 
 // This value is chosen so the AQMF fits into 16bit length
 /// Maximum number of entries per SST file
-pub const MAX_ENTRIES_PER_FILE: usize = 30 * 1024;
+pub const MAX_ENTRIES_PER_FILE: usize = 1024 * 1024;
 
 /// Finish file when total amount of data exceeds this
-pub const DATA_THRESHOLD_PER_FILE: usize = 64 * 1024 * 1024;
+pub const DATA_THRESHOLD_PER_FILE: usize = 1024 * 1024 * 1024;
 
 /// Maximum RAM bytes for AQMF cache
 pub const AQMF_CACHE_SIZE: u64 = 10 * 1024 * 1024;
@@ -21,9 +21,9 @@ pub const INDEX_BLOCK_CACHE_SIZE: u64 = 100 * 1024 * 1024;
 pub const INDEX_BLOCK_AVG_SIZE: usize = 152000;
 
 /// Maximum RAM bytes for key block cache
-pub const KEY_BLOCK_CACHE_SIZE: u64 = 100 * 1024 * 1024;
+pub const KEY_BLOCK_CACHE_SIZE: u64 = 300 * 1024 * 1024;
 pub const KEY_BLOCK_AVG_SIZE: usize = 16 * 1024;
 
 /// Maximum RAM bytes for value block cache
-pub const VALUE_BLOCK_CACHE_SIZE: u64 = 100 * 1024 * 1024;
+pub const VALUE_BLOCK_CACHE_SIZE: u64 = 300 * 1024 * 1024;
 pub const VALUE_BLOCK_AVG_SIZE: usize = 132000;
