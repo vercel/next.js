b8647035f85f33ac65544e297909dadf66ca7f44
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-26 04:07:20 +0900

    Fix more

--- crates/next-core/src/next_font/google/mod.rs
+++ crates/next-core/src/next_font/google/mod.rs
@@ -357,7 +357,7 @@ impl ImportMappingReplacement for NextFontGoogleFontFileReplacer {
             url,
             preload,
             has_size_adjust: size_adjust,
-        } = font_file_options_from_query_map(*q * uery_vc).await?;
+        } = font_file_options_from_query_map(**query_vc).await?;
 
         let (filename, ext) = split_extension(&url);
         let ext = ext.with_context(|| format!("font url {} is missing an extension", &url))?;
@@ -685,7 +685,9 @@ async fn get_mock_stylesheet(
                     .into(),
                 ),
             )),
-            Value::new(ReferenceType::Internal(InnerAssets::empty())),
+            Value::new(ReferenceType::Internal(
+                InnerAssets::empty().to_resolved().await?,
+            )),
         )
         .module();
 
