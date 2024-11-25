3ddd390ca53b8f3fb7bff2c9032d1dfef5bce477
Donny/강동윤 <kdy1997.dev@gmail.com>
2024-11-26 04:46:57 +0900

    perf(turbopack): Use `ResolvedVc` for `turbopack-css` (#73172)
    ### What?
    
    Use `ResolvedVc<T>` instead of `Vc<T>` for struct fields in
    `turbopack-css`
    
    ### Why?
    
    ### How?

--- turbopack/crates/turbopack-css/src/references/url.rs
+++ turbopack/crates/turbopack-css/src/references/url.rs
@@ -32,18 +32,18 @@ pub enum ReferencedAsset {
 #[turbo_tasks::value]
 #[derive(Hash, Debug)]
 pub struct UrlAssetReference {
-    pub origin: Vc<Box<dyn ResolveOrigin>>,
-    pub request: Vc<Request>,
-    pub issue_source: Vc<IssueSource>,
+    pub origin: ResolvedVc<Box<dyn ResolveOrigin>>,
+    pub request: ResolvedVc<Request>,
+    pub issue_source: ResolvedVc<IssueSource>,
 }
 
 #[turbo_tasks::value_impl]
 impl UrlAssetReference {
     #[turbo_tasks::function]
     pub fn new(
-        origin: Vc<Box<dyn ResolveOrigin>>,
-        request: Vc<Request>,
-        issue_source: Vc<IssueSource>,
+        origin: ResolvedVc<Box<dyn ResolveOrigin>>,
+        request: ResolvedVc<Request>,
+        issue_source: ResolvedVc<IssueSource>,
     ) -> Vc<Self> {
         Self::cell(UrlAssetReference {
             origin,
@@ -86,10 +86,10 @@ impl ModuleReference for UrlAssetReference {
     #[turbo_tasks::function]
     fn resolve_reference(&self) -> Vc<ModuleResolveResult> {
         url_resolve(
-            self.origin,
-            self.request,
+            *self.origin,
+            *self.request,
             Value::new(ReferenceType::Url(UrlReferenceSubType::CssUrl)),
-            Some(self.issue_source),
+            Some(*self.issue_source),
             false,
         )
     }
