0eaa9c901f3ac90ca5babaaba66f25fdaeba63b7
Antoine Kingue <contact@antoinek.fr>
2024-11-25 21:47:34 +0100

    Update next-config documentation with defineConfig

--- packages/next/src/shared/lib/runtime-config.external.ts
+++ packages/next/src/shared/lib/runtime-config.external.ts
@@ -10,6 +10,9 @@ export function setConfig(configValue: any): void {
   runtimeConfig = configValue
 }
 
+/**
+ * Type helper to make it easier to use `next.config.ts`
+ */
 export function defineConfig(config: NextConfig): NextConfig {
   return config
 }
