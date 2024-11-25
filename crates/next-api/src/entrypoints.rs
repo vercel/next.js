5a7ab9240acfdd8dd47ebefbff910ca19736ea81
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-26 05:20:20 +0900

    ResolvedVc<T> for `next-api`

--- crates/next-api/src/entrypoints.rs
+++ crates/next-api/src/entrypoints.rs
@@ -11,7 +11,7 @@ pub struct Entrypoints {
     pub routes: FxIndexMap<RcStr, Route>,
     pub middleware: Option<Middleware>,
     pub instrumentation: Option<Instrumentation>,
-    pub pages_document_endpoint: Vc<Box<dyn Endpoint>>,
-    pub pages_app_endpoint: Vc<Box<dyn Endpoint>>,
-    pub pages_error_endpoint: Vc<Box<dyn Endpoint>>,
+    pub pages_document_endpoint: ResolvedVc<Box<dyn Endpoint>>,
+    pub pages_app_endpoint: ResolvedVc<Box<dyn Endpoint>>,
+    pub pages_error_endpoint: ResolvedVc<Box<dyn Endpoint>>,
 }
