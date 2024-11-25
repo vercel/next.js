3eb2105d7d166c28e064c9863186ed71d56ee9f7
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-26 05:06:32 +0900

    ResolvedVc<T>

--- crates/next-core/src/next_client_reference/ecmascript_client_reference/ecmascript_client_reference_transition.rs
+++ crates/next-core/src/next_client_reference/ecmascript_client_reference/ecmascript_client_reference_transition.rs
@@ -17,8 +17,8 @@ use super::ecmascript_client_reference_proxy_module::EcmascriptClientReferencePr
 
 #[turbo_tasks::value(shared)]
 pub struct NextEcmascriptClientReferenceTransition {
-    client_transition: Vc<Box<dyn Transition>>,
-    ssr_transition: Vc<ContextTransition>,
+    client_transition: ResolvedVc<Box<dyn Transition>>,
+    ssr_transition: ResolvedVc<ContextTransition>,
 }
 
 #[turbo_tasks::value_impl]
