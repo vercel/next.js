ab099d03d9884fbdaf060dd6ab6b6c4592ba35bc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-26 15:32:50 +0900

    `ResolvedVc<T>`

--- turbopack/crates/turbopack-env/src/issue.rs
+++ turbopack/crates/turbopack-env/src/issue.rs
@@ -1,12 +1,12 @@
-use turbo_tasks::Vc;
+use turbo_tasks::{ResolvedVc, Vc};
 use turbo_tasks_fs::FileSystemPath;
 use turbopack_core::issue::{Issue, IssueStage, OptionStyledString, StyledString};
 
 /// An issue that occurred while resolving the parsing or evaluating the .env.
 #[turbo_tasks::value(shared)]
 pub struct ProcessEnvIssue {
-    pub path: Vc<FileSystemPath>,
-    pub description: Vc<StyledString>,
+    pub path: ResolvedVc<FileSystemPath>,
+    pub description: ResolvedVc<StyledString>,
 }
 
 #[turbo_tasks::value_impl]
