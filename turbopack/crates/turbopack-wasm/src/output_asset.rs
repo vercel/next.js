392f62fd8431d54b06c62bc75fd33f6e179d8247
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-11-26 12:08:32 +0900

    ResolvedVc<T>

--- turbopack/crates/turbopack-wasm/src/output_asset.rs
+++ turbopack/crates/turbopack-wasm/src/output_asset.rs
@@ -1,5 +1,5 @@
 use turbo_rcstr::RcStr;
-use turbo_tasks::Vc;
+use turbo_tasks::{ResolvedVc, Vc};
 use turbopack_core::{
     asset::{Asset, AssetContent},
     chunk::ChunkingContext,
@@ -19,8 +19,8 @@ fn modifier() -> Vc<RcStr> {
 /// [ChunkingContext].
 #[turbo_tasks::value]
 pub(crate) struct WebAssemblyAsset {
-    source: Vc<WebAssemblySource>,
-    chunking_context: Vc<Box<dyn ChunkingContext>>,
+    source: ResolvedVc<WebAssemblySource>,
+    chunking_context: ResolvedVc<Box<dyn ChunkingContext>>,
 }
 
 #[turbo_tasks::value_impl]
