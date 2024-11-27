2c2a2098ea199b772fa83225970e4e1729a32443
PapatMayuri <papat.mayuri25@gmail.com>
2024-11-27 15:46:12 +0530

    updated with-styletron example to utilize the app router.

--- examples/with-styletron/app/layout.tsx
+++ examples/with-styletron/app/layout.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { Provider as StyletronProvider } from "styletron-react";
 import { styletron } from "../styletron";
 
-export default function App({ Component, pageProps }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <StyletronProvider value={styletron}>
-      <Component {...pageProps} />
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
     </StyletronProvider>
   );
 }
--- examples/with-styletron/app/page.tsx
+++ examples/with-styletron/app/page.tsx
@@ -1,22 +1,25 @@
 // DOCUMENTATION: http://styletron.org
 
+"use client";
+
 import { styled, useStyletron } from "styletron-react";
 
-// statically styled component
+// Statically styled component
 const Title = styled("h1", {
   color: "red",
   fontSize: "82px",
 });
 
-// dynamically styled component
-const SubTitle = styled("h2", ({ $size }) => ({
+// Dynamically styled component
+const SubTitle = styled("h2", ({ $size }: { $size: number }) => ({
   color: "blue",
   fontSize: `${$size}px`,
 }));
 
 export default function Home() {
-  // an alternative hook based API
+  // An alternative hook-based API
   const [css] = useStyletron();
+
   return (
     <div>
       <Title>Title</Title>
