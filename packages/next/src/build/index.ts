fcccc1282a57e5f0b3cea06804dd2a201e3f2a9e
Tobias Koppers <tobias.koppers@googlemail.com>
2024-11-27 22:02:58 +0100

    log throw errors on build

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1547,6 +1547,7 @@ export default async function build(
           currentEntrypoints.page.size + currentEntrypoints.app.size + 1,
           'Building'
         )
+        const unhandledErrors: { page: string; error: unknown }[] = []
         const promises: Promise<any>[] = []
 
         // Concurrency will start at INITIAL_CONCURRENCY and
@@ -1557,12 +1558,17 @@ export default async function build(
 
         const sema = new Sema(INITIAL_CONCURRENCY)
         let remainingRampup = CONCURRENCY - INITIAL_CONCURRENCY
-        const enqueue = (fn: () => Promise<void>) => {
+        const enqueue = (page: string, fn: () => Promise<void>) => {
           promises.push(
             (async () => {
               await sema.acquire()
               try {
                 await fn()
+              } catch (error) {
+                unhandledErrors.push({
+                  page,
+                  error,
+                })
               } finally {
                 sema.release()
                 if (remainingRampup > 0) {
@@ -1577,7 +1583,7 @@ export default async function build(
 
         if (!appDirOnly) {
           for (const [page, route] of currentEntrypoints.page) {
-            enqueue(() =>
+            enqueue(page, () =>
               handleRouteType({
                 dev,
                 page,
@@ -1596,7 +1602,7 @@ export default async function build(
         }
 
         for (const [page, route] of currentEntrypoints.app) {
-          enqueue(() =>
+          enqueue(page, () =>
             handleRouteType({
               page,
               dev: false,
@@ -1612,7 +1618,7 @@ export default async function build(
           )
         }
 
-        enqueue(() =>
+        enqueue('_error', () =>
           handlePagesErrorRoute({
             dev: false,
             currentEntryIssues,
@@ -1655,6 +1661,15 @@ export default async function build(
               }
             }
           }
+        }
+        for (const { page, error } of unhandledErrors) {
+          errors.push({
+            page,
+            message:
+              error instanceof Error
+                ? error.stack || String(error)
+                : String(error),
+          })
         }
 
         const shutdownPromise = project.shutdown()
