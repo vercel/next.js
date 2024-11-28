8ed01da45d8c277bf4d5ddbc3e40e714641b55bd
PapatMayuri <papat.mayuri25@gmail.com>
2024-11-28 18:17:51 +0530

    updated with-context-api example to utilize the App Router.

--- examples/with-context-api/app/_components/Counter.tsx
+++ examples/with-context-api/app/_components/Counter.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   useReducer,
   useContext,
@@ -9,12 +10,12 @@ import {
 type CounterState = number;
 type CounterAction =
   | {
-      type: "INCREASE" | "DECREASE";
-    }
+    type: "INCREASE" | "DECREASE";
+  }
   | {
-      type: "INCREASE_BY";
-      payload: number;
-    };
+    type: "INCREASE_BY";
+    payload: number;
+  };
 
 const CounterStateContext = createContext<CounterState>(0);
 const CounterDispatchContext = createContext<Dispatch<CounterAction>>(
