23c51e373145f5f379fbfb6de625e88df7948083
samcx <sam@vercel.com>
2024-12-01 17:35:36 -0800

    chore(examples): update Layout, Home and About pages

--- examples/with-context-api/app/page.tsx
+++ examples/with-context-api/app/page.tsx
@@ -1,32 +1,31 @@
-"use client"
+"use client";
+
 import type { MouseEvent } from "react";
 import Link from "next/link";
-import { useCount, useDispatchCount } from "../app/_components/Counter";
+import { useCount, useDispatchCount } from "@/_components/Counter";
 
-const IndexPage = () => {
-    const count = useCount();
-    const dispatch = useDispatchCount();
+export default function Home() {
+  const count = useCount();
+  const dispatch = useDispatchCount();
 
-    const handleIncrease = (event: MouseEvent<HTMLButtonElement>) =>
-        dispatch({
-            type: "INCREASE",
-        });
-    const handleDecrease = (event: MouseEvent<HTMLButtonElement>) =>
-        dispatch({
-            type: "DECREASE",
-        });
+  const handleIncrease = (event: MouseEvent<HTMLButtonElement>) =>
+    dispatch({
+      type: "INCREASE",
+    });
+  const handleDecrease = (event: MouseEvent<HTMLButtonElement>) =>
+    dispatch({
+      type: "DECREASE",
+    });
 
-    return (
-        <>
-            <h1>HOME</h1>
-            <p>Counter: {count}</p>
-            <button onClick={handleIncrease}>Increase</button>
-            <button onClick={handleDecrease}>Decrease</button>
-            <p>
-                <Link href="/about">About</Link>
-            </p>
-        </>
-    );
-};
-
-export default IndexPage;
+  return (
+    <>
+      <h1>HOME</h1>
+      <p>Counter: {count}</p>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
+    </>
+  );
+}
