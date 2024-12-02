80686173c22a7fa4d63c7176f5462abfd97feff0
samcx <sam@vercel.com>
2024-12-02 14:55:54 -0800

    chore(examples): fix next-env.d.ts

--- examples/with-linaria/next-env.d.ts
+++ examples/with-linaria/next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
