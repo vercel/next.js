3a10faf47aa7b27e2ed45fd7dc69c737426b66e9
PapatMayuri <papat.mayuri25@gmail.com>
2024-12-02 15:29:00 +0530

    updating with-linaria example to utilize the App Router.

---
+++ examples/with-linaria/app/page.tsx
@@ -0,0 +1,9 @@
+import './styles.css'; // Import the CSS file
+
+export default function Home() {
+  return (
+    <div className="box anotherClass">
+      Zero runtime CSS in JS
+    </div>
+  );
+}
