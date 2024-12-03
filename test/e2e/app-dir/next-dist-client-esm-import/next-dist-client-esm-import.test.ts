f57541b2d9ff9d52ca8bc7ee908f6c02fe02a76c
Hendrik Liebau <mail@hendrik-liebau.de>
2024-12-03 23:25:10 +0100

    Fix deploy test

--- test/e2e/app-dir/next-dist-client-esm-import/next-dist-client-esm-import.test.ts
+++ test/e2e/app-dir/next-dist-client-esm-import/next-dist-client-esm-import.test.ts
@@ -1,9 +1,17 @@
 import { nextTestSetup } from 'e2e-utils'
 
 describe('next-dist-client-esm-import', () => {
+  const dependencies = require('./package.json').dependencies
+
+  if ((global as any).isNextDeploy) {
+    // The `link:` protocol is incompatible with the npm version that's used
+    // when this test is deployed, so we use `file:` instead.
+    dependencies['@monorepo/adapter-next'] = 'file:./adapter-next'
+  }
+
   const { next } = nextTestSetup({
     files: __dirname,
-    dependencies: require('./package.json').dependencies,
+    dependencies,
   })
 
   it('should resolve ESM modules that have "next/dist/client" in their filename', async () => {
