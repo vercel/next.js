2c369aac8b08cc5e370c44e8415ab44a7ba123e8
Tobias Koppers <tobias.koppers@googlemail.com>
2024-12-04 15:10:26 +0100

    add test case

--- test/integration/server-side-dev-errors/test/index.test.js
+++ test/integration/server-side-dev-errors/test/index.test.js
@@ -66,10 +66,9 @@ describe('server-side dev errors', () => {
 
       const stderrOutput = stripAnsi(stderr.slice(stderrIdx)).trim()
       if (isTurbopack) {
-        // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
         expect(stderrOutput).toContain(
           ' ⨯ ReferenceError: missingVar is not defined' +
-            '\n    at getStaticProps (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/gsp.js:6:2)' +
+            '\n    at getStaticProps (../../test/integration/server-side-dev-errors/pages/gsp.js:6:2)' +
             // Next.js internal frame. Feel free to adjust.
             // Not ignore-listed because we're not in an isolated app and Next.js is symlinked so it's not in node_modules
             '\n    at fn'
@@ -118,11 +117,9 @@ describe('server-side dev errors', () => {
 
       const stderrOutput = stripAnsi(stderr.slice(stderrIdx)).trim()
       if (isTurbopack) {
-        // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
         expect(stderrOutput).toContain(
           ' ⨯ ReferenceError: missingVar is not defined' +
-            // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
-            '\n    at getServerSideProps (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/gssp.js:6:2)' +
+            '\n    at getServerSideProps (../../test/integration/server-side-dev-errors/pages/gssp.js:6:2)' +
             // Next.js internal frame. Feel free to adjust.
             // Not ignore-listed because we're not in an isolated app and Next.js is symlinked so it's not in node_modules
             '\n    at fn'
@@ -171,11 +168,9 @@ describe('server-side dev errors', () => {
 
       const stderrOutput = stripAnsi(stderr.slice(stderrIdx)).trim()
       if (isTurbopack) {
-        // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
         expect(stderrOutput).toContain(
           ' ⨯ ReferenceError: missingVar is not defined' +
-            // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
-            '\n    at getServerSideProps (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/blog/[slug].js:6:2)' +
+            '\n    at getServerSideProps (../../test/integration/server-side-dev-errors/pages/blog/[slug].js:6:2)' +
             // Next.js internal frame. Feel free to adjust.
             // Not ignore-listed because we're not in an isolated app and Next.js is symlinked so it's not in node_modules
             '\n    at fn'
@@ -224,11 +219,9 @@ describe('server-side dev errors', () => {
 
       const stderrOutput = stripAnsi(stderr.slice(stderrIdx)).trim()
       if (isTurbopack) {
-        // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
         expect(stderrOutput).toContain(
           ' ⨯ ReferenceError: missingVar is not defined' +
-            // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
-            '\n    at handler (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/api/hello.js:2:2)' +
+            '\n    at handler (../../test/integration/server-side-dev-errors/pages/api/hello.js:2:2)' +
             // Next.js internal frame. Feel free to adjust.
             // Not ignore-listed because we're not in an isolated app and Next.js is symlinked so it's not in node_modules
             '\n    at async'
@@ -278,11 +271,9 @@ describe('server-side dev errors', () => {
       const stderrOutput = stripAnsi(stderr.slice(stderrIdx)).trim()
       // FIXME(veil): error repeated
       if (isTurbopack) {
-        // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
         expect(stderrOutput).toContain(
           ' ⨯ ReferenceError: missingVar is not defined' +
-            // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
-            '\n    at handler (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/api/blog/[slug].js:2:2)' +
+            '\n    at handler (../../test/integration/server-side-dev-errors/pages/api/blog/[slug].js:2:2)' +
             // Next.js internal frame. Feel free to adjust.
             // Not ignore-listed because we're not in an isolated app and Next.js is symlinked so it's not in node_modules
             '\n    at'
@@ -329,10 +320,9 @@ describe('server-side dev errors', () => {
       .trim()
     // FIXME(veil): error repeated
     if (isTurbopack) {
-      // FIXME(veil): Paths include root twice. Bug in generated Turbopack sourcemaps.
       expect(stderrOutput).toMatchInlineSnapshot(`
         "Error: catch this rejection
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-rejection.js:7:19)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-rejection.js:7:19)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     Promise.reject(new Error('catch this rejection'))
@@ -341,7 +331,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ unhandledRejection: Error: catch this rejection
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-rejection.js:7:19)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-rejection.js:7:19)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     Promise.reject(new Error('catch this rejection'))
@@ -350,7 +340,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ unhandledRejection:  Error: catch this rejection
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-rejection.js:7:19)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-rejection.js:7:19)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     Promise.reject(new Error('catch this rejection'))
@@ -411,7 +401,7 @@ describe('server-side dev errors', () => {
     if (isTurbopack) {
       expect(stderrOutput).toMatchInlineSnapshot(`
         "Error: 
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-empty-rejection.js:7:19)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-empty-rejection.js:7:19)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     Promise.reject(new Error())
@@ -420,7 +410,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ unhandledRejection: Error: 
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-empty-rejection.js:7:19)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-empty-rejection.js:7:19)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     Promise.reject(new Error())
@@ -429,7 +419,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ unhandledRejection:  Error: 
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-empty-rejection.js:7:19)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-empty-rejection.js:7:19)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     Promise.reject(new Error())
@@ -489,7 +479,7 @@ describe('server-side dev errors', () => {
     if (isTurbopack) {
       expect(stderrOutput).toMatchInlineSnapshot(`
         "Error: catch this exception
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-exception.js:7:10)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-exception.js:7:10)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     throw new Error('catch this exception')
@@ -498,7 +488,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ uncaughtException: Error: catch this exception
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-exception.js:7:10)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-exception.js:7:10)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     throw new Error('catch this exception')
@@ -507,7 +497,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ uncaughtException:  Error: catch this exception
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-exception.js:7:10)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-exception.js:7:10)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     throw new Error('catch this exception')
@@ -567,7 +557,7 @@ describe('server-side dev errors', () => {
     if (isTurbopack) {
       expect(stderrOutput).toMatchInlineSnapshot(`
         "Error: 
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-empty-exception.js:7:10)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-empty-exception.js:7:10)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     throw new Error()
@@ -576,7 +566,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ uncaughtException: Error: 
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-empty-exception.js:7:10)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-empty-exception.js:7:10)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     throw new Error()
@@ -585,7 +575,7 @@ describe('server-side dev errors', () => {
            9 |   return {
           10 |     props: {},
          ⨯ uncaughtException:  Error: 
-            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/test/integration/server-side-dev-errors/pages/uncaught-empty-exception.js:7:10)
+            at Timeout._onTimeout (../../test/integration/server-side-dev-errors/pages/uncaught-empty-exception.js:7:10)
            5 | export async function getServerSideProps() {
            6 |   setTimeout(() => {
         >  7 |     throw new Error()
