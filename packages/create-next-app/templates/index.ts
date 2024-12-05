26d00a510b3ce2b17271536a2ea2e00329543335
Sebastian "Sebbie" Silbermann <sebastian.silbermann@vercel.com>
2024-12-05 23:21:52 +0100

    create-next-app: Use stable React 19 types (#73569)

--- packages/create-next-app/templates/index.ts
+++ packages/create-next-app/templates/index.ts
@@ -211,8 +211,8 @@ export const installTemplate = async ({
       ...packageJson.devDependencies,
       typescript: "^5",
       "@types/node": "^20",
-      "@types/react": "^18",
-      "@types/react-dom": "^18",
+      "@types/react": "^19",
+      "@types/react-dom": "^19",
     };
   }
 
