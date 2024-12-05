a17f2d2cb0365c9206c2fb0a6a0a5c521681e16f
Wyatt Johnson <accounts+github@wyattjoh.ca>
2024-12-05 12:42:34 -0700

    feat: added compression to resume data cache using deflate (#73227)
    Previously the resume data cache was simply a JSON encoded string. This
    adds deflate compression to it to help reduce the bandwidth between the
    edge and the file system.
    
    ---------
    
    Co-authored-by: Janka Uryga <lolzatu2@gmail.com>

--- packages/next/src/server/resume-data-cache/resume-data-cache.test.ts
+++ packages/next/src/server/resume-data-cache/resume-data-cache.test.ts
@@ -4,6 +4,24 @@ import {
 } from './resume-data-cache'
 import { createPrerenderResumeDataCache } from './resume-data-cache'
 import { streamFromString } from '../stream-utils/node-web-streams-helper'
+import { inflateSync } from 'node:zlib'
+
+function createCacheWithSingleEntry() {
+  const cache = createPrerenderResumeDataCache()
+  cache.cache.set(
+    'key',
+    Promise.resolve({
+      value: streamFromString('value'),
+      tags: [],
+      stale: 0,
+      timestamp: 0,
+      expire: 0,
+      revalidate: 0,
+    })
+  )
+
+  return cache
+}
 
 describe('stringifyResumeDataCache', () => {
   it('serializes an empty cache', async () => {
@@ -12,20 +30,17 @@ describe('stringifyResumeDataCache', () => {
   })
 
   it('serializes a cache with a single entry', async () => {
-    const cache = createPrerenderResumeDataCache()
-    cache.cache.set(
-      'key',
-      Promise.resolve({
-        value: streamFromString('value'),
-        tags: [],
-        stale: 0,
-        timestamp: 0,
-        expire: 0,
-        revalidate: 0,
-      })
-    )
+    const cache = createCacheWithSingleEntry()
+    const compressed = await stringifyResumeDataCache(cache)
 
-    expect(await stringifyResumeDataCache(cache)).toMatchInlineSnapshot(
+    // We have to decompress the output because the compressed string is not
+    // deterministic. If it fails here it's because the compressed string is
+    // different.
+    const decompressed = inflateSync(
+      Buffer.from(compressed, 'base64')
+    ).toString('utf-8')
+
+    expect(decompressed).toMatchInlineSnapshot(
       `"{"store":{"fetch":{},"cache":{"key":{"value":"dmFsdWU=","tags":[],"stale":0,"timestamp":0,"expire":0,"revalidate":0}}}}"`
     )
   })
@@ -36,5 +51,15 @@ describe('parseResumeDataCache', () => {
     expect(createRenderResumeDataCache('null')).toEqual(
       createPrerenderResumeDataCache()
     )
+  })
+
+  it('parses a cache with a single entry', async () => {
+    const cache = createCacheWithSingleEntry()
+    const serialized = await stringifyResumeDataCache(cache)
+
+    const parsed = createRenderResumeDataCache(serialized)
+
+    expect(parsed.cache.size).toBe(1)
+    expect(parsed.fetch.size).toBe(0)
   })
 })
