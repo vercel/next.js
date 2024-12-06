2b7ca400381ceac49a16a59a913620bc07766441
Tobias Koppers <tobias.koppers@googlemail.com>
2024-12-06 09:46:08 +0100

    fix bug and test cases

--- test/e2e/app-dir/non-root-project-monorepo/apps/web/app/monorepo-package-ssr/page.tsx
+++ test/e2e/app-dir/non-root-project-monorepo/apps/web/app/monorepo-package-ssr/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { text } from 'my-package/typescript.ts'
+import { text } from 'my-package/typescript'
 
 export default function Page() {
   return <p>{text}</p>
--- test/e2e/app-dir/non-root-project-monorepo/apps/web/app/source-maps-rsc/page.tsx
+++ test/e2e/app-dir/non-root-project-monorepo/apps/web/app/source-maps-rsc/page.tsx
@@ -1,5 +1,9 @@
 export default async function Page({ searchParams }) {
-  innerFunction()
+  // We don't want the build to fail in production
+  if (process.env.NODE_ENV === 'development') {
+    innerFunction()
+  }
+  return <p>Hello Source Maps</p>
 }
 
 function innerFunction() {
