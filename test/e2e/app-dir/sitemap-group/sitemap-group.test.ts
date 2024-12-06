vercel/next.js

commit f893c1852837122842557d5033f22b0ffa49f97f

Append sitemap extension and optimize imafe metadata static generation (#66477)
### What

Optimizing the static generation for dynamic metadata routes

If you're not using `generateSitemaps()` or `generateSitemaps()`, you
don't need to change any file conventions.
If you're using multi sitemap routes, make sure the returned `id`
properties from `generateSitemaps()` don't need to contain `.xml`, since
we'll always append one for you.

Analyzing the exports of metadata routes and determine if we need to
make them as dynamic routes.

### Why

Previously, users are struggling with the multi routes of sitemap or
images.
For sitemap, the `.xml` extension in url doesn't get appended
consistently to the multi sitemap route between dev and prod.
For image routes, the generated image routes are always dynamic routes
which cannot get static optimized.

The reason is that we need to always generate a catch-all route (such as
`/icon/[[...id]]` to handle both single route case (e.g. without
`generateImageMetadata`, representing url `/icon`) or multi route (e.g.
with `generateImageMetadata`, representing url `/icon/[id]`), only
catch-all routes can do it. This approach fail the static optimization
and make mapping url pretty difficult as parsing the file to check the
module exports has to be done before it.

#### Benifits

For image routes urls, this approach could help on static generation
such as single `/opengraph-image` route can be treated as static, and
then it can get static optimized if possible.

**Before**: `/opengraph-image/[[...id]]` cannot be optimized
**After**: single route `/opengraph-image` and multi-route
`/opengraph-image/[id]` are both possible to be statically optimized

For sitemap, since we removed appending `.xml` for dynamic routes, it’s
hard for users to have `/sitemap.xml` url with dynamic route convention
`sitemap.js` . But users desire smooth migration and flexibility.

packages/next/src/lib/metadata/get-metadata-route.ts
@@ -1,4 +1,4 @@
-import { isMetadataRoute, isStaticMetadataRoute } from './is-metadata-route'
+import { isMetadataRoute } from './is-metadata-route'
 import path from '../../shared/lib/isomorphic/path'
 import { interpolateDynamicPath } from '../../server/server-utils'
 import { getNamedRouteRegex } from '../../shared/lib/router/utils/route-regex'
@@ -11,8 +11,8 @@ import { normalizePathSep } from '../../shared/lib/page-path/normalize-path-sep'
  * Give it a unique hash suffix to avoid conflicts
  *
  * e.g.
- * /app/open-graph.tsx -> /open-graph/route
- * /app/(post)/open-graph.tsx -> /open-graph/route-[0-9a-z]{6}
+ * /app/opengraph-image.tsx -> /opengraph-image
+ * /app/(post)/opengraph-image.tsx -> /opengraph-image-[0-9a-z]{6}
  */
 function getMetadataRouteSuffix(page: string) {
   let suffix = ''
@@ -65,10 +65,10 @@ export function normalizeMetadataRoute(page: string) {
     route += '.txt'
   } else if (page === '/manifest') {
     route += '.webmanifest'
-  }
-  // For sitemap, we don't automatically add the route suffix since it can have sub-routes
-  else if (!page.endsWith('/sitemap')) {
-    // Remove the file extension, e.g. /route-path/robots.txt -> /route-path
+  } else {
+    // Remove the file extension,
+    // e.g. /path/robots.txt -> /route-path
+    // e.g. /path/opengraph-image.tsx -> /path/opengraph-image
     const pathnamePrefix = page.slice(0, -(path.basename(page).length + 1))
     suffix = getMetadataRouteSuffix(pathnamePrefix)
   }
@@ -76,15 +76,30 @@ export function normalizeMetadataRoute(page: string) {
   // If it's a metadata file route, we need to append /[id]/route to the page.
   if (!route.endsWith('/route')) {
     const { dir, name: baseName, ext } = path.parse(route)
-    const isStaticRoute = isStaticMetadataRoute(page)
-
     route = path.posix.join(
       dir,
       `${baseName}${suffix ? `-${suffix}` : ''}${ext}`,
-      isStaticRoute ? '' : '[[...__metadata_id__]]',
       'route'
     )
   }
 
   return route
+}
+
+// Normalize metadata route page to either a single route or a dynamic route.
+// e.g. Input: /sitemap/route
+// when isDynamic is false, single route -> /sitemap.xml/route
+// when isDynamic is false, dynamic route -> /sitemap/[__metadata_id__]/route
+// also works for pathname such as /sitemap -> /sitemap.xml, but will not append /route suffix
+export function normalizeMetadataPageToRoute(page: string, isDynamic: boolean) {
+  const isRoute = page.endsWith('/route')
+  const routePagePath = isRoute ? page.slice(0, -'/route'.length) : page
+  const metadataRouteExtension = routePagePath.endsWith('/sitemap')
+    ? '.xml'
+    : ''
+  const mapped = isDynamic
+    ? `${routePagePath}/[__metadata_id__]`
+    : `${routePagePath}${metadataRouteExtension}`
+
+  return mapped + (isRoute ? '/route' : '')
 }

packages/next-swc/crates/next-core/src/next_app/metadata/mod.rs
@@ -306,8 +306,7 @@ pub fn normalize_metadata_route(mut page: AppPage) -> Result<AppPage> {
         route += ".txt"
     } else if route == "/manifest" {
         route += ".webmanifest"
-    // Do not append the suffix for the sitemap route
-    } else if !route.ends_with("/sitemap") {
+    } else {
         // Remove the file extension, e.g. /route-path/robots.txt -> /route-path
         let pathname_prefix = split_directory(&route).0.unwrap_or_default();
         suffix = get_metadata_route_suffix(pathname_prefix);
@@ -317,12 +316,7 @@ pub fn normalize_metadata_route(mut page: AppPage) -> Result<AppPage> {
     // /<metadata-route>/route.ts. If it's a metadata file route, we need to
     // append /[id]/route to the page.
     if !route.ends_with("/route") {
-        let is_static_metadata_file = is_static_metadata_route_file(&page.to_string());
         let (base_name, ext) = split_extension(&route);
-
-        let is_static_route = route.starts_with("/robots")
-            || route.starts_with("/manifest")
-            || is_static_metadata_file;
 
         page.0.pop();
 
@@ -337,10 +331,6 @@ pub fn normalize_metadata_route(mut page: AppPage) -> Result<AppPage> {
             )
             .into(),
         ))?;
-
-        if !is_static_route {
-            page.push(PageSegment::OptionalCatchAll("__metadata_id__".into()))?;
-        }
 
         page.push(PageSegment::PageType(PageType::Route))?;
     }
@@ -358,11 +348,11 @@ mod test {
         let cases = vec![
             [
                 "/client/(meme)/more-route/twitter-image",
-                "/client/(meme)/more-route/twitter-image-769mad/[[...__metadata_id__]]/route",
+                "/client/(meme)/more-route/twitter-image-769mad/route",
             ],
             [
                 "/client/(meme)/more-route/twitter-image2",
-                "/client/(meme)/more-route/twitter-image2-769mad/[[...__metadata_id__]]/route",
+                "/client/(meme)/more-route/twitter-image2-769mad/route",
             ],
             ["/robots.txt", "/robots.txt/route"],
             ["/manifest.webmanifest", "/manifest.webmanifest/route"],

docs/02-app/02-api-reference/02-file-conventions/01-metadata/sitemap.mdx
@@ -97,7 +97,7 @@ export default function sitemap() {
 
 Output:
 
-```xml filename="acme.com/sitemap"
+```xml filename="acme.com/sitemap.xml"
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://acme.com</loc>
@@ -163,7 +163,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
 Output:
 
-```xml filename="acme.com/sitemap"
+```xml filename="acme.com/sitemap.xml"
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
   <url>
     <loc>https://acme.com</loc>
@@ -264,7 +264,7 @@ export default async function sitemap({ id }) {
 }
 ```
 
-Your generated sitemaps will be available at `/.../sitemap/[id]`. For example, `/product/sitemap/1`.
+Your generated sitemaps will be available at `/.../sitemap/[id]`. For example, `/product/sitemap/1.xml`.
 
 See the [`generateSitemaps` API reference](/docs/app/api-reference/functions/generate-sitemaps) for more information.
 

packages/next-swc/crates/next-core/src/app_segment_config.rs
@@ -8,7 +8,7 @@ use turbo_tasks_fs::FileSystemPath;
 use turbopack_binding::{
     swc::core::{
         common::{source_map::Pos, Span, Spanned, GLOBALS},
-        ecma::ast::{Expr, Ident, Program},
+        ecma::ast::{Decl, Expr, FnExpr, Ident, Program},
     },
     turbopack::{
         core::{
@@ -73,6 +73,9 @@ pub struct NextSegmentConfig {
     pub runtime: Option<NextRuntime>,
     pub preferred_region: Option<Vec<RcStr>>,
     pub experimental_ppr: Option<bool>,
+    /// Wether these metadata exports are defined in the source file.
+    pub generate_image_metadata: bool,
+    pub generate_sitemaps: bool,
 }
 
 #[turbo_tasks::value_impl]
@@ -95,6 +98,7 @@ impl NextSegmentConfig {
             runtime,
             preferred_region,
             experimental_ppr,
+            ..
         } = self;
         *dynamic = dynamic.or(parent.dynamic);
         *dynamic_params = dynamic_params.or(parent.dynamic_params);
@@ -137,6 +141,7 @@ impl NextSegmentConfig {
             runtime,
             preferred_region,
             experimental_ppr,
+            ..
         } = self;
         merge_parallel(dynamic, &parallel_config.dynamic, "dynamic")?;
         merge_parallel(
@@ -272,22 +277,35 @@ pub async fn parse_segment_config_from_source(
         let mut config = NextSegmentConfig::default();
 
         for item in &module_ast.body {
-            let Some(decl) = item
+            let Some(export_decl) = item
                 .as_module_decl()
                 .and_then(|mod_decl| mod_decl.as_export_decl())
-                .and_then(|export_decl| export_decl.decl.as_var())
             else {
                 continue;
             };
 
-            for decl in &decl.decls {
-                let Some(ident) = decl.name.as_ident().map(|ident| ident.deref()) else {
-                    continue;
-                };
+            match &export_decl.decl {
+                Decl::Var(var_decl) => {
+                    for decl in &var_decl.decls {
+                        let Some(ident) = decl.name.as_ident().map(|ident| ident.deref()) else {
+                            continue;
+                        };
 
-                if let Some(init) = decl.init.as_ref() {
-                    parse_config_value(source, &mut config, ident, init, eval_context);
+                        if let Some(init) = decl.init.as_ref() {
+                            parse_config_value(source, &mut config, ident, init, eval_context);
+                        }
+                    }
                 }
+                Decl::Fn(fn_decl) => {
+                    let ident = &fn_decl.ident;
+                    // create an empty expression of {}, we don't need init for function
+                    let init = Expr::Fn(FnExpr {
+                        ident: None,
+                        function: fn_decl.function.clone(),
+                    });
+                    parse_config_value(source, &mut config, ident, &init, eval_context);
+                }
+                _ => {}
             }
         }
         config
@@ -430,6 +448,14 @@ fn parse_config_value(
             };
 
             config.preferred_region = Some(preferred_region);
+        }
+        // Match exported generateImageMetadata function and generateSitemaps function, and pass
+        // them to config.
+        "generateImageMetadata" => {
+            config.generate_image_metadata = true;
+        }
+        "generateSitemaps" => {
+            config.generate_sitemaps = true;
         }
         "experimental_ppr" => {
             let value = eval_context.eval(init);

packages/next-swc/crates/next-core/src/next_app/metadata/route.rs
@@ -2,7 +2,7 @@
 //!
 //! See `next/src/build/webpack/loaders/next-metadata-route-loader`
 
-use anyhow::{bail, Result};
+use anyhow::{bail, Ok, Result};
 use base64::{display::Base64Display, engine::general_purpose::STANDARD};
 use indoc::{formatdoc, indoc};
 use turbo_tasks::{ValueToString, Vc};
@@ -22,7 +22,9 @@ use super::get_content_type;
 use crate::{
     app_structure::MetadataItem,
     mode::NextMode,
-    next_app::{app_entry::AppEntry, app_route_entry::get_app_route_entry, AppPage, PageSegment},
+    next_app::{
+        app_entry::AppEntry, app_route_entry::get_app_route_entry, AppPage, PageSegment, PageType,
+    },
     next_config::NextConfig,
     parse_segment_config_from_source,
 };
@@ -30,9 +32,9 @@ use crate::{
 /// Computes the route source for a Next.js metadata file.
 #[turbo_tasks::function]
 pub async fn get_app_metadata_route_source(
-    page: AppPage,
     mode: NextMode,
     metadata: MetadataItem,
+    is_multi_dynamic: bool,
 ) -> Result<Vc<Box<dyn Source>>> {
     Ok(match metadata {
         MetadataItem::Static { path } => static_route_source(mode, path),
@@ -43,7 +45,7 @@ pub async fn get_app_metadata_route_source(
             if stem == "robots" || stem == "manifest" {
                 dynamic_text_route_source(path)
             } else if stem == "sitemap" {
-                dynamic_site_map_route_source(mode, path, page)
+                dynamic_site_map_route_source(mode, path, is_multi_dynamic)
             } else {
                 dynamic_image_route_source(path)
             }
@@ -52,11 +54,11 @@ pub async fn get_app_metadata_route_source(
 }
 
 #[turbo_tasks::function]
-pub fn get_app_metadata_route_entry(
+pub async fn get_app_metadata_route_entry(
     nodejs_context: Vc<ModuleAssetContext>,
     edge_context: Vc<ModuleAssetContext>,
     project_root: Vc<FileSystemPath>,
-    page: AppPage,
+    mut page: AppPage,
     mode: NextMode,
     metadata: MetadataItem,
     next_config: Vc<NextConfig>,
@@ -69,11 +71,43 @@ pub fn get_app_metadata_route_entry(
 
     let source = Vc::upcast(FileSource::new(original_path));
     let segment_config = parse_segment_config_from_source(source);
+    let is_dynamic_metadata = matches!(metadata, MetadataItem::Dynamic { .. });
+    let is_multi_dynamic: bool = if Some(segment_config).is_some() {
+        // is_multi_dynamic is true when config.generateSitemaps or
+        // config.generateImageMetadata is defined in dynamic routes
+        let config = segment_config.await.unwrap();
+        config.generate_sitemaps || config.generate_image_metadata
+    } else {
+        false
+    };
+
+    // Map dynamic sitemap and image routes based on the exports.
+    // if there's generator export: add /[__metadata_id__] to the route;
+    // otherwise keep the original route.
+    // For sitemap, if the last segment is sitemap, appending .xml suffix.
+    if is_dynamic_metadata {
+        // remove the last /route segment of page
+        page.0.pop();
+
+        let _ = if is_multi_dynamic {
+            page.push(PageSegment::Dynamic("__metadata_id__".into()))
+        } else {
+            // if page last segment is sitemap, change to sitemap.xml
+            if page.last() == Some(&PageSegment::Static("sitemap".into())) {
+                page.0.pop();
+                page.push(PageSegment::Static("sitemap.xml".into()))
+            } else {
+                Ok(())
+            }
+        };
+        // Push /route back
+        let _ = page.push(PageSegment::PageType(PageType::Route));
+    };
 
     get_app_route_entry(
         nodejs_context,
         edge_context,
-        get_app_metadata_route_source(page.clone(), mode, metadata),
+        get_app_metadata_route_source(mode, metadata, is_multi_dynamic),
         page,
         project_root,
         Some(segment_config),
@@ -208,26 +242,24 @@ async fn dynamic_text_route_source(path: Vc<FileSystemPath>) -> Result<Vc<Box<dy
 async fn dynamic_site_map_route_source(
     mode: NextMode,
     path: Vc<FileSystemPath>,
-    page: AppPage,
+    is_multi_dynamic: bool,
 ) -> Result<Vc<Box<dyn Source>>> {
     let stem = path.file_stem().await?;
     let stem = stem.as_deref().unwrap_or_default();
     let ext = &*path.extension().await?;
-
     let content_type = get_content_type(path).await?;
-
     let mut static_generation_code = "";
 
-    if mode.is_production() && page.contains(&PageSegment::Dynamic("[__metadata_id__]".into())) {
+    if mode.is_production() && is_multi_dynamic {
         static_generation_code = indoc! {
             r#"
                 export async function generateStaticParams() {
                     const sitemaps = await generateSitemaps()
                     const params = []
 
-                    for (const item of sitemaps) {
-                        params.push({ __metadata_id__: item.id.toString() })
-                    }
+                    for (const item of sitemaps) {{
+                        params.push({ __metadata_id__: item.id.toString() + '.xml' })
+                    }}
                     return params
                 }
             "#,
@@ -252,29 +284,25 @@ async fn dynamic_site_map_route_source(
             }}
 
             export async function GET(_, ctx) {{
-                const {{ __metadata_id__ = [], ...params }} = ctx.params || {{}}
-                const targetId = __metadata_id__[0]
-                let id = undefined
-                const sitemaps = generateSitemaps ? await generateSitemaps() : null
-
-                if (sitemaps) {{
-                    id = sitemaps.find((item) => {{
-                        if (process.env.NODE_ENV !== 'production') {{
-                            if (item?.id == null) {{
-                                throw new Error('id property is required for every item returned from generateSitemaps')
-                            }}
-                        }}
-                        return item.id.toString() === targetId
-                    }})?.id
-
-                    if (id == null) {{
-                        return new NextResponse('Not Found', {{
-                            status: 404,
-                        }})
-                    }}
+                const {{ __metadata_id__: id, ...params }} = ctx.params || {{}}
+                const hasXmlExtension = id ? id.endsWith('.xml') : false
+                if (id && !hasXmlExtension) {{
+                    return new NextResponse('Not Found', {{
+                        status: 404,
+                    }})
                 }}
 
-                const data = await handler({{ id }})
+                if (process.env.NODE_ENV !== 'production' && sitemapModule.generateSitemaps) {{
+                    const sitemaps = await sitemapModule.generateSitemaps()
+                    for (const item of sitemaps) {{
+                        if (item?.id == null) {{
+                            throw new Error('id property is required for every item returned from generateSitemaps')
+                        }}
+                    }}
+                }}
+                
+                const targetId = id && hasXmlExtension ? id.slice(0, -4) : undefined
+                const data = await handler({{ id: targetId }})
                 const content = resolveRouteData(data, fileType)
 
                 return new NextResponse(content, {{
@@ -324,12 +352,12 @@ async fn dynamic_image_route_source(path: Vc<FileSystemPath>) -> Result<Vc<Box<d
             }}
 
             export async function GET(_, ctx) {{
-                const {{ __metadata_id__ = [], ...params }} = ctx.params || {{}}
-                const targetId = __metadata_id__[0]
+                const {{ __metadata_id__, ...params }} = ctx.params || {{}}
+                const targetId = __metadata_id__
                 let id = undefined
-                const imageMetadata = generateImageMetadata ? await generateImageMetadata({{ params }}) : null
 
-                if (imageMetadata) {{
+                if (generateImageMetadata) {{
+                    const imageMetadata = await generateImageMetadata({{ params }})
                     id = imageMetadata.find((item) => {{
                         if (process.env.NODE_ENV !== 'production') {{
                             if (item?.id == null) {{

commit 36b4e3004587eb6aa259ffd2a3d8871af0bf49ea

fix: do not add suffix for sitemap under group routes (#73570)
### What

Do not add hash suffix for the metadata `/sitemap.xml` routes under the
routes group.

### Why

We previously added the suffix to all the metadata routes where they're
under the routes group. This was intentional for cases like having both
`/(group)/og image` and `/og image` so they can be different og images
inlined into different group routes. But for sitemap it's different
since sitemap usually wasn't intended to have duplicates. Each sitemap
contains all the urls of sub routes, we don't have the case
`/(group)/sitemap.[ext]` and `/sitemap.[ext]` exist at the same time. So
we always normalize the urls for sitemap and do not append hash suffix.

This was already the case in v14, it's regressed in #66477 

Fixes #72787
Closes NEXT-3909

crates/next-core/src/next_app/metadata/mod.rs
@@ -282,11 +282,31 @@ fn format_radix(mut x: u32, radix: u32) -> String {
 /// Give it a unique hash suffix to avoid conflicts
 ///
 /// e.g.
-/// /app/open-graph.tsx -> /open-graph/route
-/// /app/(post)/open-graph.tsx -> /open-graph/route-[0-9a-z]{6}
+/// /opengraph-image -> /opengraph-image
+/// /(post)/opengraph-image.tsx -> /opengraph-image-[0-9a-z]{6}
+///
+/// Sitemap is an exception, it should not have a suffix.
+/// As the generated urls are for indexer and usually one sitemap contains all the urls of the sub
+/// routes. The sitemap should be unique in each level and not have a suffix.
+///
+/// /sitemap -> /sitemap
+/// /(post)/sitemap -> /sitemap
 fn get_metadata_route_suffix(page: &str) -> Option<String> {
-    if (page.contains('(') && page.contains(')')) || page.contains('@') {
-        Some(format_radix(djb2_hash(page), 36))
+    // skip sitemap
+    if page.ends_with("/sitemap") {
+        return None;
+    }
+
+    // Get the parent pathname of the page
+    let parent_pathname = split_directory(page).0.unwrap_or_default();
+    let segments = parent_pathname.split('/').collect::<Vec<&str>>();
+
+    // if any segment is group or parallel route segment, we should add a suffix.
+    if segments.iter().any(|segment| {
+        segment.starts_with('(') && segment.ends_with(')')
+            || segment.starts_with('@') && *segment != "@children"
+    }) {
+        Some(format_radix(djb2_hash(parent_pathname), 36))
     } else {
         None
     }
@@ -308,9 +328,7 @@ pub fn normalize_metadata_route(mut page: AppPage) -> Result<AppPage> {
     } else if route == "/manifest" {
         route += ".webmanifest"
     } else {
-        // Remove the file extension, e.g. /route-path/robots.txt -> /route-path
-        let pathname_prefix = split_directory(&route).0.unwrap_or_default();
-        suffix = get_metadata_route_suffix(pathname_prefix);
+        suffix = get_metadata_route_suffix(&route);
     }
 
     // Support both /<metadata-route.ext> and custom routes

packages/next/src/build/normalize-catchall-routes.ts
@@ -77,7 +77,7 @@ function hasMatchedSlots(path1: string, path2: string): boolean {
 }
 
 /**
- * Returns true for slots that should be considered when checking for match compatability.
+ * Returns true for slots that should be considered when checking for match compatibility.
  * Excludes children slots because these are similar to having a segment-level `page`
  * which would cause a slot length mismatch when comparing it to a catch-all route.
  */

packages/next/src/lib/metadata/get-metadata-route.ts
@@ -5,20 +5,45 @@ import { getNamedRouteRegex } from '../../shared/lib/router/utils/route-regex'
 import { djb2Hash } from '../../shared/lib/hash'
 import { normalizeAppPath } from '../../shared/lib/router/utils/app-paths'
 import { normalizePathSep } from '../../shared/lib/page-path/normalize-path-sep'
+import {
+  isGroupSegment,
+  isParallelRouteSegment,
+} from '../../shared/lib/segment'
 
 /*
  * If there's special convention like (...) or @ in the page path,
  * Give it a unique hash suffix to avoid conflicts
  *
  * e.g.
- * /app/opengraph-image.tsx -> /opengraph-image
- * /app/(post)/opengraph-image.tsx -> /opengraph-image-[0-9a-z]{6}
+ * /opengraph-image -> /opengraph-image
+ * /(post)/opengraph-image.tsx -> /opengraph-image-[0-9a-z]{6}
+ *
+ * Sitemap is an exception, it should not have a suffix.
+ * Each sitemap contains all the urls of sub routes, we don't have the case of duplicates `/(group)/sitemap.[ext]` and `/sitemap.[ext]` since they should be the same.
+ * Hence we always normalize the urls for sitemap and do not append hash suffix, and ensure user-land only contains one sitemap per pathname.
+ *
+ * /sitemap -> /sitemap
+ * /(post)/sitemap -> /sitemap
  */
 function getMetadataRouteSuffix(page: string) {
-  let suffix = ''
+  // Remove the last segment and get the parent pathname
+  // e.g. /parent/a/b/c -> /parent/a/b
+  // e.g. /parent/opengraph-image -> /parent
+  const parentPathname = path.dirname(page)
+  // Only apply suffix to metadata routes except for sitemaps
+  if (page.endsWith('/sitemap')) {
+    return ''
+  }
 
-  if ((page.includes('(') && page.includes(')')) || page.includes('@')) {
-    suffix = djb2Hash(page).toString(36).slice(0, 6)
+  // Calculate the hash suffix based on the parent path
+  let suffix = ''
+  // Check if there's any special characters in the parent pathname.
+  const segments = parentPathname.split('/')
+  if (
+    segments.some((seg) => isGroupSegment(seg) || isParallelRouteSegment(seg))
+  ) {
+    // Hash the parent path to get a unique suffix
+    suffix = djb2Hash(parentPathname).toString(36).slice(0, 6)
   }
   return suffix
 }
@@ -33,15 +58,15 @@ function getMetadataRouteSuffix(page: string) {
 export function fillMetadataSegment(
   segment: string,
   params: any,
-  imageSegment: string
+  lastSegment: string
 ) {
   const pathname = normalizeAppPath(segment)
   const routeRegex = getNamedRouteRegex(pathname, false)
   const route = interpolateDynamicPath(pathname, params, routeRegex)
-  const suffix = getMetadataRouteSuffix(segment)
+  const { name, ext } = path.parse(lastSegment)
+  const pagePath = path.posix.join(segment, name)
+  const suffix = getMetadataRouteSuffix(pagePath)
   const routeSuffix = suffix ? `-${suffix}` : ''
-
-  const { name, ext } = path.parse(imageSegment)
 
   return normalizePathSep(path.join(route, `${name}${routeSuffix}${ext}`))
 }
@@ -66,11 +91,7 @@ export function normalizeMetadataRoute(page: string) {
   } else if (page === '/manifest') {
     route += '.webmanifest'
   } else {
-    // Remove the file extension,
-    // e.g. /path/robots.txt -> /route-path
-    // e.g. /path/opengraph-image.tsx -> /path/opengraph-image
-    const pathnamePrefix = page.slice(0, -(path.basename(page).length + 1))
-    suffix = getMetadataRouteSuffix(pathnamePrefix)
+    suffix = getMetadataRouteSuffix(page)
   }
   // Support both /<metadata-route.ext> and custom routes /<metadata-route>/route.ts.
   // If it's a metadata file route, we need to append /[id]/route to the page.

packages/next/src/shared/lib/segment.ts
@@ -5,6 +5,10 @@ export function isGroupSegment(segment: string) {
   return segment[0] === '(' && segment.endsWith(')')
 }
 
+export function isParallelRouteSegment(segment: string) {
+  return segment.startsWith('@') && segment !== '@children'
+}
+
 export function addSearchParamsIfPageSegment(
   segment: Segment,
   searchParams: Record<string, string | string[] | undefined>

test/e2e/app-dir/sitemap-group/app/(group)/foo/sitemap.ts
@@ -1,0 +1,9 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://www.vercel.com',
+      lastModified: '2024-12-05T23:45:13.405Z',
+      changeFrequency: 'monthly',
+    },
+  ]
+}

test/e2e/app-dir/sitemap-group/app/layout.tsx
@@ -1,0 +1,8 @@
+import { ReactNode } from 'react'
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/e2e/app-dir/sitemap-group/app/page.tsx
@@ -1,0 +1,3 @@
+export default function Page() {
+  return <p>hello world</p>
+}

test/e2e/app-dir/sitemap-group/next.config.js
@@ -1,0 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {}
+
+module.exports = nextConfig

test/e2e/app-dir/sitemap-group/sitemap-group.test.ts
@@ -1,0 +1,25 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('sitemap-group', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should not add suffix to sitemap under group routes', async () => {
+    const res = await next.fetch('/foo/sitemap.xml')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('application/xml')
+    const text = await res.text()
+    expect(text).toMatchInlineSnapshot(`
+      "<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+      <loc>https://www.vercel.com</loc>
+      <lastmod>2024-12-05T23:45:13.405Z</lastmod>
+      <changefreq>monthly</changefreq>
+      </url>
+      </urlset>
+      "
+    `)
+  })
+})