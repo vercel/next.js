9283716864809d9952c59ae2965e50d3ae084777
devjiwonchoi <devjiwonchoi@gmail.com>
2024-12-07 02:20:05 +0900

    test: split basic HMR test into multiple files

--- test/development/basic/hmr/basic-no-base-path-yes-asset-prefix.test.ts
+++ test/development/basic/hmr/basic-no-base-path-yes-asset-prefix.test.ts
@@ -5,14 +5,10 @@ import {
   waitFor,
 } from 'next-test-utils'
 import { createNext, nextTestSetup } from 'e2e-utils'
-import type { NextConfig } from 'next'
 
-describe.each([
-  { basePath: '', assetPrefix: '' },
-  { basePath: '', assetPrefix: '/asset-prefix' },
-  { basePath: '/docs', assetPrefix: '' },
-  { basePath: '/docs', assetPrefix: '/asset-prefix' },
-])('basic HMR, nextConfig: %o', (nextConfig: Partial<NextConfig>) => {
+const nextConfig = { basePath: '', assetPrefix: '/asset-prefix' }
+
+describe(`HMR - basic, nextConfig: ${JSON.stringify(nextConfig)}`, () => {
   const { next } = nextTestSetup({
     files: __dirname,
     nextConfig,
