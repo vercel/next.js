3c414b9ecb1e00fee21e11b5dc56ede5d5527721
Tobias Koppers <tobias.koppers@googlemail.com>
2024-12-09 23:48:30 +0100

    [Turbopack] Introduce OperationVc that wraps operations (#70242)
    ### What?
    
    Introduce `OperationVc` that wraps operations
    
    *(Note: This was called `VcOperation`, but I changed it to `OperationVc` to better match the `ResolvedVc` naming convention -- @bgw)*
    
    We should only expose `OperationVc` into JS to connect to the whole computation (and be strongly consistent with the whole computation).
    
    ~also fix query string~ Fixed in #70461
    
    ### Why?
    
    We want operations to be strongly consistent to the whole operation. Also HMR subscriptions should include the whole entrypoints and endpoint operation. The `OperationVc` type makes it easy to track that and enforces connecting to the operation correctly.
    
    In regards of the `ResolvedVc` work, we also want operations to be explicit.

---
+++ turbopack/crates/turbo-tasks/src/vc/operation.rs
@@ -0,0 +1,158 @@
+use std::{fmt::Debug, hash::Hash};
+
+use auto_hash_map::AutoSet;
+use serde::{Deserialize, Serialize};
+
+use crate::{trace::TraceRawVcs, CollectiblesSource, RawVc, TaskInput, Upcast, Vc, VcValueTrait};
+
+#[must_use]
+pub struct OperationVc<T>
+where
+    T: ?Sized,
+{
+    pub(crate) node: Vc<T>,
+}
+
+impl<T: ?Sized> OperationVc<T> {
+    /// Creates a new `OperationVc` from a `Vc`.
+    ///
+    /// The caller must ensure that the `Vc` is not a local task and it points to a a single
+    /// operation.
+    pub fn new(node: Vc<T>) -> Self {
+        // TODO to avoid this runtime check, we should mark functions with `(operation)` and return
+        // a OperationVc directly
+        assert!(
+            matches!(node.node, RawVc::TaskOutput(..)),
+            "OperationVc::new must be called on the immediate return value of a task function"
+        );
+        Self { node }
+    }
+
+    /// Marks this operation's underlying function call as a child of the current task, and returns
+    /// a dereferenced [`Vc`] that can be [resolved][Vc::to_resolved] or read with `.await?`.
+    ///
+    /// By marking this function call as a child of the current task, turbo-tasks will re-run tasks
+    /// as-needed to achieve strong consistency at the root of the function call tree. This explicit
+    /// operation is needed as `OperationVc` types can be stored outside of the call graph as part
+    /// of [`State`][crate::State]s.
+    pub fn connect(self) -> Vc<T> {
+        self.node.node.connect();
+        self.node
+    }
+
+    /// Returns the `RawVc` corresponding to this `Vc`.
+    pub fn into_raw(vc: Self) -> RawVc {
+        vc.node.node
+    }
+
+    /// Upcasts the given `OperationVc<T>` to a `OperationVc<Box<dyn K>>`.
+    ///
+    /// This is also available as an `Into`/`From` conversion.
+    #[inline(always)]
+    pub fn upcast<K>(vc: Self) -> OperationVc<K>
+    where
+        T: Upcast<K>,
+        K: VcValueTrait + ?Sized,
+    {
+        OperationVc {
+            node: Vc::upcast(vc.node),
+        }
+    }
+}
+
+impl<T> Copy for OperationVc<T> where T: ?Sized {}
+
+impl<T> Clone for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Hash for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.node.hash(state);
+    }
+}
+
+impl<T> PartialEq<OperationVc<T>> for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.node == other.node
+    }
+}
+
+impl<T> Eq for OperationVc<T> where T: ?Sized {}
+
+impl<T> Debug for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("OperationVc")
+            .field("node", &self.node.node)
+            .finish()
+    }
+}
+
+impl<T> TaskInput for OperationVc<T> where T: ?Sized + Send + Sync {}
+
+impl<T> From<RawVc> for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn from(raw: RawVc) -> Self {
+        Self {
+            node: Vc::from(raw),
+        }
+    }
+}
+
+impl<T> Serialize for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.node.serialize(serializer)
+    }
+}
+
+impl<'de, T> Deserialize<'de> for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        Ok(OperationVc {
+            node: Vc::deserialize(deserializer)?,
+        })
+    }
+}
+
+impl<T> TraceRawVcs for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn trace_raw_vcs(&self, trace_context: &mut crate::trace::TraceRawVcsContext) {
+        self.node.trace_raw_vcs(trace_context);
+    }
+}
+
+impl<T> CollectiblesSource for OperationVc<T>
+where
+    T: ?Sized,
+{
+    fn take_collectibles<Vt: VcValueTrait>(self) -> AutoSet<Vc<Vt>> {
+        self.node.node.take_collectibles()
+    }
+
+    fn peek_collectibles<Vt: VcValueTrait>(self) -> AutoSet<Vc<Vt>> {
+        self.node.node.peek_collectibles()
+    }
+}
