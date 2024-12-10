vercel/next.js

commit 0ea736efc0711cadb1bf5a534a87875d6e690582

fix: memory leak from `warnOnce()` (#73483)
The `warnOnce()` function is used to warn only once time, however this
means we might be storing every possible message in memory forever (aka
memory leak) since it can grow unbounded.

This PR changes the behavior to only store only 10KB in a Least Recently
Used (LRU) cache.

The tradeoff here is that you might see the same warning twice if you
have a lot of unique warnings.

packages/next/src/build/output/log.ts
@@ -1,4 +1,5 @@
 import { bold, green, magenta, red, yellow, white } from '../../lib/picocolors'
+import { LRUCache } from '../../server/lib/lru-cache'
 
 export const prefixes = {
   wait: white(bold('○')),
@@ -76,11 +77,12 @@ export function trace(...message: any[]) {
   prefixedLog('trace', ...message)
 }
 
-const warnOnceMessages = new Set()
-export function warnOnce(...message: any[]) {
-  if (!warnOnceMessages.has(message[0])) {
-    warnOnceMessages.add(message.join(' '))
+const warnOnceCache = new LRUCache<string>(10_000, (message) => message.length)
 
+export function warnOnce(...message: any[]) {
+  const key = message.join(' ')
+  if (!warnOnceCache.has(key)) {
+    warnOnceCache.set(key)
     warn(...message)
   }
 }

commit c7d6ab7f4c351c45a41b6361efc3dea76a0ddffc

fix: `warnOnce()` lru (#73742)
We regressed in https://github.com/vercel/next.js/pull/73483 since the
LRU calculates the value based on the value, not the key (and we were
not setting the value).

packages/next/src/build/output/log.ts
@@ -77,12 +77,11 @@ export function trace(...message: any[]) {
   prefixedLog('trace', ...message)
 }
 
-const warnOnceCache = new LRUCache<string>(10_000, (message) => message.length)
-
+const warnOnceCache = new LRUCache<string>(10_000, (value) => value.length)
 export function warnOnce(...message: any[]) {
   const key = message.join(' ')
   if (!warnOnceCache.has(key)) {
-    warnOnceCache.set(key)
+    warnOnceCache.set(key, key)
     warn(...message)
   }
 }

test/unit/build-output-log.test.ts
@@ -1,0 +1,27 @@
+import { warnOnce } from 'next/dist/build/output/log'
+
+describe('build/output/log', () => {
+  it('warnOnce', () => {
+    const original = console.warn
+    try {
+      const messages = []
+      console.warn = (m: any) => messages.push(m)
+      warnOnce('test')
+      expect(messages.length).toEqual(1)
+      warnOnce('test again')
+      expect(messages.length).toEqual(2)
+      warnOnce('test', 'more')
+      expect(messages.length).toEqual(3)
+      warnOnce('test')
+      expect(messages.length).toEqual(3)
+      warnOnce('test again')
+      expect(messages.length).toEqual(3)
+      warnOnce('test', 'more')
+      expect(messages.length).toEqual(3)
+      warnOnce('test', 'should', 'add', 'another')
+      expect(messages.length).toEqual(4)
+    } finally {
+      console.warn = original
+    }
+  })
+})