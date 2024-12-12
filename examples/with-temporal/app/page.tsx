a22a7e83dc6688d77f4ce95a625b713f70469b6f
PapatMayuri <papat.mayuri25@gmail.com>
2024-12-12 13:35:25 +0530

    updated page and component.

--- examples/with-temporal/app/page.tsx
+++ examples/with-temporal/app/page.tsx
@@ -1,29 +1,12 @@
-"use client"; // Add this at the very top
-
+// app/page.jsx or pages/index.jsx
 import Link from "next/link";
 import Layout from "../components/Layout";
-import { Data } from "./api/orders";
+import CreateOrderButton from "../components/CreateOrderButton";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + Temporal Example">
     <h1>Hello Next.js 👋</h1>
-
-    <button
-      onClick={async () => {
-        const newOrder = { itemId: "B102", quantity: 2 };
-        const response = await fetch("/api/orders", {
-          method: "POST",
-          headers: { Authorization: "session-id-or-jwt" },
-          body: JSON.stringify(newOrder),
-        });
-        const data: Data = await response.json();
-        console.log(data);
-        alert(data.result);
-      }}
-    >
-      Create order
-    </button>
-
+    <CreateOrderButton />
     <p>
       <Link href="/about">About</Link>
     </p>
