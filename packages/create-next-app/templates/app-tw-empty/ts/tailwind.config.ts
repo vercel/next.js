f779f87b2ceb952145f941fd05468a6b932b9e5f
oil-bleach-re-el-idited <oil-bleach-reprint@duck.com>
2024-12-13 06:56:36 -0500

    Delete packages/create-next-app/templates directory

--- packages/create-next-app/templates/app-tw-empty/ts/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
-  plugins: [],
-} satisfies Config;
--- packages/create-next-app/templates/default/ts/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
