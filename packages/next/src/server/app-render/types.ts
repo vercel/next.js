2d106a8495c66c07a1c4b369d8ffb14f305fff48
Andrew Clark <git@andrewclark.io>
2024-12-13 00:37:04 -0500

    Allow prefetches to skip already cached segments
    During a non-PPR prefetch, where we render up to the nearest loading
    boundary, we should be able to skip segments that are already cached on
    the client. To do this, we need to be able to tell the server that
    a segment is already cached while also indicating where the "new" part
    of the navigation starts — that is, the part inside the shared layout.
    
    To do this, I added a new kind of marker to FlightRouterState called
    "inside-shared-layout". It's similar to the "refresh" marker but has
    different semantics. I tried my best to explain how it's used in the
    code comments.
    
    This implements the marker but does not yet change any behavior on the
    client. I'm submitting this as its own PR to confirm that none of the
    existing behavior regresses. I'll follow up with the client changes
    in a separate PR.
    
    As another follow-up, we're overdue for a redesign of the protocol for
    sending dynamic requests. I'm not sure it makes sense to use the
    FlightRouterState type, given that it's overloaded with so many
    other concerns.

--- packages/next/src/server/app-render/types.ts
+++ packages/next/src/server/app-render/types.ts
@@ -46,7 +46,15 @@ export const flightRouterStateSchema: s.Describe<any> = s.tuple([
     s.lazy(() => flightRouterStateSchema)
   ),
   s.optional(s.nullable(s.string())),
-  s.optional(s.nullable(s.union([s.literal('refetch'), s.literal('refresh')]))),
+  s.optional(
+    s.nullable(
+      s.union([
+        s.literal('refetch'),
+        s.literal('refresh'),
+        s.literal('inside-shared-layout'),
+      ])
+    )
+  ),
   s.optional(s.boolean()),
 ])
 
@@ -57,13 +65,32 @@ export type FlightRouterState = [
   segment: Segment,
   parallelRoutes: { [parallelRouterKey: string]: FlightRouterState },
   url?: string | null,
-  /*
-  /* "refresh" and "refetch", despite being similarly named, have different semantics.
-   * - "refetch" is a server indicator which informs where rendering should start from.
-   * - "refresh" is a client router indicator that it should re-fetch the data from the server for the current segment.
-   *   It uses the "url" property above to determine where to fetch from.
+  /**
+   * "refresh" and "refetch", despite being similarly named, have different
+   * semantics:
+   * - "refetch" is used during a requets to inform the server where rendering
+   *   should start from.
+   *
+   * - "refresh" is used by the client to mark that a segmentshould re-fetch the
+   *   data from the server for the current segment. It uses the "url" property
+   *   above to determine where to fetch from.
+   *
+   * - "inside-shared-layout" is used during a prefetch request to inform the
+   *   server that even if the segment matches, it should be treated as if it's
+   *   within the "new" part of a navigation — inside the shared layout. If
+   *   the segment doesn't match, then it has no effect, since it would be
+   *   treated as new regardless. If it does match, though, the server does not
+   *   need to render it, because the client already has it.
+   *
+   *   A bit confusing, but that's because it has only one extremely narrow use
+   *   case — during a non-PPR prefetch, the server uses it to find the first
+   *   loading boundary beneath a shared layout.
+   *
+   *   TODO: We should rethink the protocol for dynamic requests. It might not
+   *   make for the client to send a FlightRouterState, since this type is
+   *   overloaded with concerns.
    */
-  refresh?: 'refetch' | 'refresh' | null,
+  refresh?: 'refetch' | 'refresh' | 'inside-shared-layout' | null,
   isRootLayout?: boolean,
 ]
 
