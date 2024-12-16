d61fcd88af1625b3b28b8efc4cfea7df1ac024d3
Jiachi Liu <inbox@huozhi.im>
2024-12-16 22:03:42 +0100

    Separate bot utils

--- packages/next/src/shared/lib/router/utils/is-bot.ts
+++ packages/next/src/shared/lib/router/utils/is-bot.ts
@@ -1,6 +1,19 @@
-const BOT_UA_RE =
-  /Googlebot|Mediapartners-Google|AdsBot-Google|googleweblight|Storebot-Google|Google-PageRenderer|Bingbot|BingPreview|Slurp|DuckDuckBot|baiduspider|yandex|sogou|LinkedInBot|bitlybot|tumblr|vkShare|quora link preview|facebookexternalhit|facebookcatalog|Twitterbot|applebot|redditbot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|ia_archiver/i
+// Bot crawler that will spin up a headless browser
+const HEADLESS_BOT_UA_RE =
+  /Googlebot|Google-PageRenderer|AdsBot-Google|googleweblight|Storebot-Google|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview/i
+
+// Static fetcher bot crawler that doesn't spin up a headless browser or execute JS
+const STATIC_FETCHER_BOT_UA_RE =
+  /Mediapartners-Google|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver/i
+
+function isHeadlessBrowserBot(userAgent: string) {
+  return HEADLESS_BOT_UA_RE.test(userAgent)
+}
+
+function isNonBrowserBot(userAgent: string) {
+  return STATIC_FETCHER_BOT_UA_RE.test(userAgent)
+}
 
 export function isBot(userAgent: string): boolean {
-  return BOT_UA_RE.test(userAgent)
+  return isHeadlessBrowserBot(userAgent) || isNonBrowserBot(userAgent)
 }
