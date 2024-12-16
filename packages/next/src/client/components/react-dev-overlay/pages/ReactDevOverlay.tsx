20fa944974b281acd7c0e79c7a9d050a2a909681
devjiwonchoi <devjiwonchoi@gmail.com>
2024-12-17 05:24:16 +0900

    refactor pages react dev overlay

--- packages/next/src/client/components/react-dev-overlay/pages/ReactDevOverlay.tsx
+++ packages/next/src/client/components/react-dev-overlay/pages/ReactDevOverlay.tsx
@@ -1,64 +1,54 @@
-import * as React from 'react'
-
-import * as Bus from './bus'
+import type { ReactDevOverlayProps } from '../types'
+import { ErrorBoundary } from './ErrorBoundary'
+import { usePagesReactDevOverlay } from './hooks'
 import { ShadowPortal } from '../internal/components/ShadowPortal'
 import { BuildError } from '../internal/container/BuildError'
 import { Errors } from '../internal/container/Errors'
-import { ErrorBoundary } from './ErrorBoundary'
 import { Base } from '../internal/styles/Base'
 import { ComponentStyles } from '../internal/styles/ComponentStyles'
 import { CssReset } from '../internal/styles/CssReset'
-import { useErrorOverlayReducer } from '../shared'
-
-type ErrorType = 'runtime' | 'build'
-
-const shouldPreventDisplay = (
-  errorType?: ErrorType | null,
-  preventType?: ErrorType[] | null
-) => {
-  if (!preventType || !errorType) {
-    return false
-  }
-  return preventType.includes(errorType)
-}
-
-interface ReactDevOverlayProps {
-  children?: React.ReactNode
-  preventDisplay?: ErrorType[]
-  globalOverlay?: boolean
-}
 
 export default function ReactDevOverlay({
   children,
   preventDisplay,
   globalOverlay,
 }: ReactDevOverlayProps) {
-  const [state, dispatch] = useErrorOverlayReducer()
+  const {
+    isMounted,
+    displayPrevented,
+    hasBuildError,
+    hasRuntimeErrors,
+    state,
+    onComponentError,
+  } = usePagesReactDevOverlay(preventDisplay)
 
-  React.useEffect(() => {
-    Bus.on(dispatch)
-    return function () {
-      Bus.off(dispatch)
+  const renderErrorContent = () => {
+    if (displayPrevented) {
+      return null
     }
-  }, [dispatch])
 
-  const onComponentError = React.useCallback(
-    (_error: Error, _componentStack: string | null) => {
-      // TODO: special handling
-    },
-    []
-  )
+    if (hasBuildError) {
+      return (
+        <BuildError
+          message={state.buildError!}
+          versionInfo={state.versionInfo}
+        />
+      )
+    }
 
-  const hasBuildError = state.buildError != null
-  const hasRuntimeErrors = Boolean(state.errors.length)
-  const errorType = hasBuildError
-    ? 'build'
-    : hasRuntimeErrors
-      ? 'runtime'
-      : null
-  const isMounted = errorType !== null
+    if (hasRuntimeErrors) {
+      return (
+        <Errors
+          isAppDir={false}
+          errors={state.errors}
+          versionInfo={state.versionInfo}
+          initialDisplayState="fullscreen"
+        />
+      )
+    }
 
-  const displayPrevented = shouldPreventDisplay(errorType, preventDisplay)
+    return null
+  }
 
   return (
     <>
@@ -69,27 +59,14 @@ export default function ReactDevOverlay({
       >
         {children ?? null}
       </ErrorBoundary>
-      {isMounted ? (
+      {isMounted && (
         <ShadowPortal>
           <CssReset />
           <Base />
           <ComponentStyles />
-
-          {displayPrevented ? null : hasBuildError ? (
-            <BuildError
-              message={state.buildError!}
-              versionInfo={state.versionInfo}
-            />
-          ) : hasRuntimeErrors ? (
-            <Errors
-              isAppDir={false}
-              errors={state.errors}
-              versionInfo={state.versionInfo}
-              initialDisplayState={'fullscreen'}
-            />
-          ) : undefined}
+          {renderErrorContent()}
         </ShadowPortal>
-      ) : undefined}
+      )}
     </>
   )
 }
