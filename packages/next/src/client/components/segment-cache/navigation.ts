6ed03f4f7615c89ecca8e036146af8116269fef7
Andrew Clark <git@andrewclark.io>
2024-12-17 12:51:01 -0500

    [Segment Cache] Move cache key creation to client  (#73853)
    When I originally set up the Segment Cache transport types, I had the
    notion that the client should never have to compute the request key that
    is sent to the server. That way we could, say, encrypt the keys on the
    server to make them unguessable.
    
    But once we added the concept of access tokens for that purpose, this
    motivation was gone. The remaining motivation was to keep the contract
    as simple as possible.
    
    However, there's a lot of existing code that relies on manipulating the
    "decoded" form of the segment values. Since we're sending the full
    segment value anyway, it's wasteful to also send the encoded form, given
    that it can be trivially re-derived by the client.
    
    My immediate motivation is that, I need to be able to construct a cache
    key from a FlightRouterState. As a bonus, this will also get us closer
    to being able to remove the dynamic param values from the server
    response, a separate goal that we're currently working on.
    
    We can revisit this once if/when we're able to remove the existing cases
    where the full segment is needed by the client.

--- packages/next/src/client/components/segment-cache/navigation.ts
+++ packages/next/src/client/components/segment-cache/navigation.ts
@@ -21,8 +21,8 @@ import {
   readRouteCacheEntry,
   readSegmentCacheEntry,
   waitForSegmentCacheEntry,
+  type RouteTree,
 } from './cache'
-import type { TreePrefetch } from '../../../server/app-render/collect-segment-data'
 import { createCacheKey } from './cache-key'
 
 export const enum NavigationResultTag {
@@ -180,7 +180,7 @@ function navigationTaskToResult(
 
 function readRenderSnapshotFromCache(
   now: number,
-  tree: TreePrefetch
+  tree: RouteTree
 ): { flightRouterState: FlightRouterState; seedData: CacheNodeSeedData } {
   let childRouterStates: { [parallelRouteKey: string]: FlightRouterState } = {}
   let childSeedDatas: {
@@ -200,7 +200,7 @@ function readRenderSnapshotFromCache(
   let loading: LoadingModuleData | Promise<LoadingModuleData> = null
   let isPartial: boolean = true
 
-  const segmentEntry = readSegmentCacheEntry(now, tree.path)
+  const segmentEntry = readSegmentCacheEntry(now, tree.key)
   if (segmentEntry !== null) {
     switch (segmentEntry.status) {
       case EntryStatus.Fulfilled: {
@@ -237,25 +237,15 @@ function readRenderSnapshotFromCache(
     }
   }
 
-  const extra = tree.extra
-  const flightRouterStateSegment = extra[0]
-  const isRootLayout = extra[1]
-
   return {
     flightRouterState: [
-      flightRouterStateSegment,
+      tree.segment,
       childRouterStates,
       null,
       null,
-      isRootLayout,
+      tree.isRootLayout,
     ],
-    seedData: [
-      flightRouterStateSegment,
-      rsc,
-      childSeedDatas,
-      loading,
-      isPartial,
-    ],
+    seedData: [tree.segment, rsc, childSeedDatas, loading, isPartial],
   }
 }
 
