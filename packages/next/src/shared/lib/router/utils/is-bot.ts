5e725e6b84cb36011d5541affa282527fac798da
Jiachi Liu <inbox@huozhi.im>
2024-12-17 23:31:25 +0100

    rename and comment

--- packages/next/src/shared/lib/router/utils/is-bot.ts
+++ packages/next/src/shared/lib/router/utils/is-bot.ts
@@ -1,19 +1,21 @@
-// Bot crawler that will spin up a headless browser
+// Bot crawler that will spin up a headless browser and execute JS
 const HEADLESS_BOT_UA_RE =
-  /Googlebot|Google-PageRenderer|AdsBot-Google|googleweblight|Storebot-Google|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview/i
+  /Googlebot|Google-PageRenderer|AdsBot-Google|googleweblight|Storebot-Google/i
 
-// Static fetcher bot crawler that doesn't spin up a headless browser or execute JS
-const STATIC_FETCHER_BOT_UA_RE =
-  /Mediapartners-Google|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver/i
+// This regex contains the bots that we need to do static rendering for.
+// - Static fetcher bot crawler that doesn't spin up a headless browser or execute JS
+// - Bots that possible can do headless browser crawling but the crawler is limited to some restrictions.
+const HTML_LIMITED_BOT_UA_RE =
+  /Mediapartners-Google|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview/i
 
 function isHeadlessBrowserBot(userAgent: string) {
   return HEADLESS_BOT_UA_RE.test(userAgent)
 }
 
-function isNonBrowserBot(userAgent: string) {
-  return STATIC_FETCHER_BOT_UA_RE.test(userAgent)
+function isHtmlLimitedBot(userAgent: string) {
+  return HTML_LIMITED_BOT_UA_RE.test(userAgent)
 }
 
 export function isBot(userAgent: string): boolean {
-  return isHeadlessBrowserBot(userAgent) || isNonBrowserBot(userAgent)
+  return isHeadlessBrowserBot(userAgent) || isHtmlLimitedBot(userAgent)
 }
