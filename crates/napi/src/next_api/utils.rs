490623be335520c22eb71647c901108a6540b19b
Tobias Koppers <tobias.koppers@googlemail.com>
2024-12-17 20:28:02 +0100

    [Turbopack] move git version info to top-level crate to fix caching issues (#74015)
    ### What?
    
    crate level caching caused a Persisting Caching bug when upgrading the next.js version.
    
    This moves the version info to the top-level crate to avoid this issue.

--- crates/napi/src/next_api/utils.rs
+++ crates/napi/src/next_api/utils.rs
@@ -127,6 +127,25 @@ pub fn create_turbo_tasks(
     memory_limit: usize,
 ) -> Result<NextTurboTasks> {
     Ok(if persistent_caching {
+        let dirty_suffix = if crate::build::GIT_CLEAN || !env!("CI").is_empty() {
+            ""
+        } else {
+            "-dirty"
+        };
+        #[allow(
+            clippy::const_is_empty,
+            reason = "LAST_TAG might be empty if the tag can't be determined"
+        )]
+        let version_info = if crate::build::LAST_TAG.is_empty() {
+            format!("{}{}", crate::build::SHORT_COMMIT, dirty_suffix)
+        } else {
+            format!(
+                "{}-{}{}",
+                crate::build::LAST_TAG,
+                crate::build::SHORT_COMMIT,
+                dirty_suffix
+            )
+        };
         NextTurboTasks::PersistentCaching(TurboTasks::new(
             turbo_tasks_backend::TurboTasksBackend::new(
                 turbo_tasks_backend::BackendOptions {
@@ -137,7 +156,7 @@ pub fn create_turbo_tasks(
                     }),
                     ..Default::default()
                 },
-                default_backing_storage(&output_path.join("cache/turbopack"))?,
+                default_backing_storage(&output_path.join("cache/turbopack"), &version_info)?,
             ),
         ))
     } else {
