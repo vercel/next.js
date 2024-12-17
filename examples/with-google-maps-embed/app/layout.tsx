ec394f2cbee9d9647125f04ca456a67113991c05
samcx <sam@vercel.com>
2024-12-16 17:01:30 -0800

    chore(examples): update Layouts

--- examples/with-google-maps-embed/app/layout.tsx
+++ examples/with-google-maps-embed/app/layout.tsx
@@ -1,4 +1,15 @@
-export default function RootLayout({ children }) {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "With Google Maps Embed",
+  description: "Next.js example with Google Maps Embed.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
--- examples/with-google-tag-manager/app/layout.tsx
+++ examples/with-google-tag-manager/app/layout.tsx
@@ -1,10 +1,16 @@
+import type { Metadata } from "next";
 import { GoogleTagManager } from "@next/third-parties/google";
+
+export const metadata: Metadata = {
+  title: "With Google Tag Manager",
+  description: "Next.js example with Google Tag Manager.",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
