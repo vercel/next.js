13a71be8c708ce56b268286a1e4deb93f791df2c
devjiwonchoi <devjiwonchoi@gmail.com>
2024-12-19 04:41:03 +0900

    Move ErrorIndicator to separate file

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/container/Errors.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/container/Errors.tsx
@@ -14,11 +14,9 @@ import {
 } from '../components/Dialog'
 import { LeftRightDialogHeader } from '../components/LeftRightDialogHeader'
 import { Overlay } from '../components/Overlay'
-import { Toast } from '../components/Toast'
 import { getErrorByType } from '../helpers/get-error-by-type'
 import type { ReadyRuntimeError } from '../helpers/get-error-by-type'
 import { noop as css } from '../helpers/noop-template'
-import { CloseIcon } from '../icons/CloseIcon'
 import { RuntimeError } from './RuntimeError'
 import { VersionStalenessInfo } from '../components/VersionStalenessInfo'
 import type { VersionInfo } from '../../../../../../server/dev/parse-version-info'
@@ -36,6 +34,7 @@ import {
   isUnhandledConsoleOrRejection,
 } from '../helpers/console-error'
 import { extractNextErrorCode } from '../../../../../../lib/error-telemetry-utils'
+import { ErrorIndicator } from '../components/ErrorIndicator/ErrorIndicator'
 
 export type SupportedErrorEvent = {
   id: number
@@ -234,44 +233,12 @@ export function Errors({
 
   if (displayState === 'minimized') {
     return (
-      <Toast
-        data-nextjs-toast
-        className={`nextjs-toast-errors-parent${hasStaticIndicator ? ' nextjs-error-with-static' : ''}`}
-        onClick={fullscreen}
-      >
-        <div className="nextjs-toast-errors">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="12" cy="12" r="10"></circle>
-            <line x1="12" y1="8" x2="12" y2="12"></line>
-            <line x1="12" y1="16" x2="12.01" y2="16"></line>
-          </svg>
-          <span>
-            {readyErrors.length} issue{readyErrors.length > 1 ? 's' : ''}
-          </span>
-          <button
-            data-nextjs-toast-errors-hide-button
-            className="nextjs-toast-hide-button"
-            type="button"
-            onClick={(e) => {
-              e.stopPropagation()
-              hide()
-            }}
-            aria-label="Hide Issues"
-          >
-            <CloseIcon />
-          </button>
-        </div>
-      </Toast>
+      <ErrorIndicator
+        hasStaticIndicator={hasStaticIndicator}
+        readyErrors={readyErrors}
+        fullscreen={fullscreen}
+        hide={hide}
+      />
     )
   }
 
