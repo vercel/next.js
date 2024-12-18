f49eee847f3bfb9d961623c866379f0d51a8bfc6
devjiwonchoi <devjiwonchoi@gmail.com>
2024-12-19 05:57:02 +0900

    Port LeftIcon RightIcon from ErrorPagination

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/Errors/ErrorPagination/ErrorPagination.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/Errors/ErrorPagination/ErrorPagination.tsx
@@ -1,5 +1,7 @@
 import type { ReadyRuntimeError } from '../../../helpers/get-error-by-type'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { LeftArrow } from '../../../icons/LeftArrow'
+import { RightArrow } from '../../../icons/RightArrow'
 
 type ErrorPaginationProps = {
   activeIdx: number
@@ -115,20 +117,7 @@ export function ErrorPagination({
           aria-disabled={previousHandler == null ? true : undefined}
           onClick={previousHandler ?? undefined}
         >
-          <svg
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>previous</title>
-            <path
-              d="M6.99996 1.16666L1.16663 6.99999L6.99996 12.8333M12.8333 6.99999H1.99996H12.8333Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <LeftArrow title="previous" />
         </button>
         <button
           ref={buttonRight}
@@ -137,20 +126,7 @@ export function ErrorPagination({
           aria-disabled={nextHandler == null ? true : undefined}
           onClick={nextHandler ?? undefined}
         >
-          <svg
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>next</title>
-            <path
-              d="M6.99996 1.16666L12.8333 6.99999L6.99996 12.8333M1.16663 6.99999H12H1.16663Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <RightArrow title="next" />
         </button>
         <small>
           <span>{activeIdx + 1}</span> of{' '}
