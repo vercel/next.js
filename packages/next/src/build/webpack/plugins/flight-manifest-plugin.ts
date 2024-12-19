04297fa99a86b6297cc2d97b14a67f2db07bca8c
Hendrik Liebau <mail@hendrik-liebau.de>
2024-12-19 18:59:16 +0100

    Revert "Do not create client reference manifest for metadata route handlers (#71225)"
    This reverts commit 39dfa3affd5369ea138b46f7a677b917188f5967.

--- packages/next/src/build/webpack/plugins/flight-manifest-plugin.ts
+++ packages/next/src/build/webpack/plugins/flight-manifest-plugin.ts
@@ -26,7 +26,6 @@ import {
 } from '../utils'
 import type { ChunkGroup } from 'webpack'
 import { encodeURIPath } from '../../../shared/lib/encode-uri-path'
-import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
 import type { ModuleInfo } from './flight-client-entry-plugin'
 
 interface Options {
@@ -559,9 +558,9 @@ export class ClientReferenceManifestPlugin {
         manifestEntryFiles.push(entryName.replace(/\/page(\.[^/]+)?$/, '/page'))
       }
 
-      // We also need to create manifests for route handler entrypoints
-      // (excluding metadata route handlers) to enable `'use cache'`.
-      if (/\/route$/.test(entryName) && !isMetadataRoute(entryName)) {
+      // We also need to create manifests for route handler entrypoints to
+      // enable `'use cache'`.
+      if (/\/route$/.test(entryName)) {
         manifestEntryFiles.push(entryName)
       }
 
