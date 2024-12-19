7d6e3f2962fd1bbdfe063203e76e595c945070db
Janka Uryga <lolzatu2@gmail.com>
2024-12-19 22:19:50 +0100

    refactor: use swc quote

--- crates/next-custom-transforms/src/transforms/track_dynamic_imports.rs
+++ crates/next-custom-transforms/src/transforms/track_dynamic_imports.rs
@@ -1,13 +1,12 @@
-use core::option::Option::None;
-
 use serde::Deserialize;
 use swc_core::{
-    common::DUMMY_SP,
+    common::util::take::Take,
     ecma::{
         ast::*,
-        utils::{private_ident, quote_ident, ExprFactory},
+        utils::private_ident,
         visit::{noop_visit_mut_type, visit_mut_pass, VisitMut, VisitMutWith},
     },
+    quote,
 };
 
 #[derive(Clone, Debug, Deserialize)]
@@ -40,30 +39,13 @@ impl VisitMut for ImportReplacer {
 
         if self.has_dynamic_import {
             // if we wrapped a dynamic import above, we need to import the wrapper
-            //
-            // import {
-            //   trackDynamicImport as $$trackDynamicImport__
-            // } from 'private-next-rsc-track-dynamic-import'
-
             stmts.insert(
                 0,
-                ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
-                    span: DUMMY_SP,
-                    specifiers: vec![ImportSpecifier::Named(ImportNamedSpecifier {
-                        span: DUMMY_SP,
-                        imported: Some(quote_ident!("trackDynamicImport").into()),
-                        local: self.wrapper_function_local_ident.clone(),
-                        is_type_only: false,
-                    })],
-                    src: Box::new(Str {
-                        span: DUMMY_SP,
-                        value: "private-next-rsc-track-dynamic-import".into(),
-                        raw: None,
-                    }),
-                    type_only: false,
-                    with: None,
-                    phase: Default::default(),
-                })),
+                quote!(
+                    "import { trackDynamicImport as $wrapper_fn } from \
+                     'private-next-rsc-track-dynamic-import'" as ModuleItem,
+                    wrapper_fn = self.wrapper_function_local_ident.clone()
+                ),
             );
         }
     }
@@ -80,12 +62,11 @@ impl VisitMut for ImportReplacer {
         }) = expr
         {
             self.has_dynamic_import = true;
-            *expr = Expr::Call(CallExpr {
-                span: DUMMY_SP,
-                callee: Callee::Expr(self.wrapper_function_local_ident.clone().into()),
-                args: vec![expr.clone().as_arg()],
-                ..Default::default()
-            })
+            *expr = quote!(
+                "$wrapper_fn($expr)" as Expr,
+                wrapper_fn = self.wrapper_function_local_ident.clone().into(),
+                expr: Expr = expr.take()
+            )
         }
     }
 }
--- crates/next-custom-transforms/tests/fixture/track-dynamic-imports/2/output.js
+++ crates/next-custom-transforms/tests/fixture/track-dynamic-imports/2/output.js
@@ -1,4 +1,4 @@
-import { trackDynamicImport as $$trackDynamicImport__ } from "private-next-rsc-track-dynamic-import";
+import { trackDynamicImport as $$trackDynamicImport__ } from 'private-next-rsc-track-dynamic-import';
 export default async function Page() {
     await $$trackDynamicImport__(import((await $$trackDynamicImport__(import('./get-name'))).default));
     return null;
