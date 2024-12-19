5442a8307b330db611d61a4a7001b0536bd1a9f8
PapatMayuri <papat.mayuri25@gmail.com>
2024-12-19 12:07:01 +0530

    refactored the Layout component into the Root Layout file.

--- examples/with-temporal/app/page.tsx
+++ examples/with-temporal/app/page.tsx
@@ -1,16 +1,14 @@
-// app/page.jsx or pages/index.jsx
 import Link from "next/link";
-import Layout from "../components/Layout";
 import CreateOrderButton from "../components/CreateOrderButton";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + Temporal Example">
+  <>
     <h1>Hello Next.js 👋</h1>
     <CreateOrderButton />
     <p>
       <Link href="/about">About</Link>
     </p>
-  </Layout>
+  </>
 );
 
 export default IndexPage;
