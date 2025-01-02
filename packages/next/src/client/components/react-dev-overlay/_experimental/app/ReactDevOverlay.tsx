4d45e1d6baa4fc20360387b296492639688e49ab
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-02 21:58:23 +0900

    [DevOverlay] Add color palette script

--- packages/next/src/client/components/react-dev-overlay/_experimental/app/ReactDevOverlay.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/app/ReactDevOverlay.tsx
@@ -12,6 +12,7 @@ import { ComponentStyles } from '../internal/styles/ComponentStyles'
 import { CssReset } from '../internal/styles/CssReset'
 import { RootLayoutMissingTagsError } from '../internal/container/RootLayoutMissingTagsError'
 import { RuntimeErrorHandler } from '../internal/helpers/runtime-error-handler'
+import { Colors } from '../internal/styles/colors'
 
 interface ReactDevOverlayState {
   isReactError: boolean
@@ -57,6 +58,7 @@ export default class ReactDevOverlay extends React.PureComponent<
         <ShadowPortal>
           <CssReset />
           <Base />
+          <Colors />
           <ComponentStyles />
           {state.rootLayoutMissingTags?.length ? (
             <RootLayoutMissingTagsError
