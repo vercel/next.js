d2eb95259aaf499062edf562fb374be8f472a8a9
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-01-06 12:49:25 -0700

    fix: handle optional catchall parameters properly on Vercel

---
+++ packages/next/src/server/server-utils.test.ts
@@ -0,0 +1,73 @@
+import { getUtils } from './server-utils'
+
+describe('getParamsFromRouteMatches', () => {
+  it('should return nothing for a non-dynamic route', () => {
+    const { getParamsFromRouteMatches } = getUtils({
+      page: '/',
+      basePath: '',
+      rewrites: {},
+      i18n: undefined,
+      pageIsDynamic: false,
+      caseSensitive: false,
+    })
+
+    const params = getParamsFromRouteMatches('nxtPslug=hello-world')
+    expect(params).toEqual(null)
+  })
+
+  it('should return the params from the route matches', () => {
+    const { getParamsFromRouteMatches } = getUtils({
+      page: '/[slug]',
+      basePath: '',
+      rewrites: {},
+      i18n: undefined,
+      pageIsDynamic: true,
+      caseSensitive: false,
+    })
+
+    const params = getParamsFromRouteMatches('nxtPslug=hello-world')
+    expect(params).toEqual({ slug: 'hello-world' })
+  })
+
+  it('should handle optional params', () => {
+    const { getParamsFromRouteMatches } = getUtils({
+      page: '/[slug]/[[...optional]]',
+      basePath: '',
+      rewrites: {},
+      i18n: undefined,
+      pageIsDynamic: true,
+      caseSensitive: false,
+    })
+
+    // Missing optional param
+    let params = getParamsFromRouteMatches('nxtPslug=hello-world')
+    expect(params).toEqual({ slug: 'hello-world' })
+
+    // Providing optional param
+    params = getParamsFromRouteMatches(
+      'nxtPslug=hello-world&nxtPoptional=im-optional'
+    )
+    expect(params).toEqual({ slug: 'hello-world', optional: ['im-optional'] })
+  })
+
+  it('should handle rest params', () => {
+    const { getParamsFromRouteMatches } = getUtils({
+      page: '/[slug]/[...rest]',
+      basePath: '',
+      rewrites: {},
+      i18n: undefined,
+      pageIsDynamic: true,
+      caseSensitive: false,
+    })
+
+    // Missing rest param
+    let params = getParamsFromRouteMatches('nxtPslug=hello-world')
+    expect(params).toEqual(null)
+
+    // Providing rest param
+    params = getParamsFromRouteMatches(
+      'nxtPslug=hello-world&nxtPrest=im-the/rest'
+    )
+    expect(params).toEqual({ slug: 'hello-world', rest: ['im-the', 'rest'] })
+  })
+})
