e14447b4c6170d535158aafaf3267847cf15b97a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-08 15:36:29 +0900

    Update test refs

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/next-issues/74190/output/4e721_crates_turbopack-tests_tests_snapshot_next-issues_74190_input_index_40c590.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4e721_crates_turbopack-tests_tests_snapshot_next-issues_74190_input_index_40c590.js",
+    {},
+    {"otherChunks":["output/4e721_crates_turbopack-tests_tests_snapshot_next-issues_74190_input_index_4120f6.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/next-issues/74190/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
---
+++ turbopack/crates/turbopack-tests/tests/snapshot/next-issues/74190/output/4e721_crates_turbopack-tests_tests_snapshot_next-issues_74190_input_index_4120f6.js
@@ -0,0 +1,25 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/4e721_crates_turbopack-tests_tests_snapshot_next-issues_74190_input_index_4120f6.js", {
+
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/next-issues/74190/input/index.js [test] (ecmascript)": (function(__turbopack_context__) {
+
+var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, m: module, e: exports, t: __turbopack_require_real__ } = __turbopack_context__;
+{
+var root = {};
+(function _(namespace, path, parent) {
+    for(var i = 0, keys = Object.keys(namespace); i < keys.length; i++){
+        var key = keys[i];
+        console.debug("####", path, path ? true : false);
+        parent[key] = _(namespace[key], path ? `${path}.${key}` : key, parent[key] || {});
+    }
+    return parent;
+})({
+    wrapper: {
+        data: {
+            key: {}
+        }
+    }
+}, "", root);
+}}),
+}]);
+
+//# sourceMappingURL=4e721_crates_turbopack-tests_tests_snapshot_next-issues_74190_input_index_4120f6.js.map
