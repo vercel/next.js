f9d8d793fac12ea3f7ff9621bd92e39195b4e520
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-09 07:16:10 +0900

    styory

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/call-stack-frame/call-stack-frame.stories.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/call-stack-frame/call-stack-frame.stories.tsx
@@ -14,29 +14,39 @@ const meta: Meta<typeof CallStackFrame> = {
 export default meta
 type Story = StoryObj<typeof CallStackFrame>
 
-export const SimpleFrame: Story = {
+const frame = {
+  originalStackFrame: {
+    file: './app/page.tsx',
+    methodName: 'MyComponent',
+    arguments: [],
+    lineNumber: 10,
+    column: 5,
+    ignored: false,
+  },
+  sourceStackFrame: {
+    file: './app/page.tsx',
+    methodName: 'MyComponent',
+    arguments: [],
+    lineNumber: 10,
+    column: 5,
+  },
+  error: false,
+  reason: null,
+  external: false,
+  ignored: false,
+}
+
+export const HasSource: Story = {
   args: {
     frame: {
-      originalStackFrame: {
-        file: './app/page.tsx',
-        methodName: 'MyComponent',
-        arguments: [],
-        lineNumber: 10,
-        column: 5,
-        ignored: false,
-      },
-      sourceStackFrame: {
-        file: './app/page.tsx',
-        methodName: 'MyComponent',
-        arguments: [],
-        lineNumber: 10,
-        column: 5,
-      },
+      ...frame,
       originalCodeFrame: 'export default function MyComponent() {',
-      error: false,
-      reason: null,
-      external: false,
-      ignored: false,
     },
+  },
+}
+
+export const NoSource: Story = {
+  args: {
+    frame,
   },
 }
