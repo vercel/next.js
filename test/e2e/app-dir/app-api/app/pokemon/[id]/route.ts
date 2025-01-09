3c0dda0a8918a755999cac5faae718324a0b5bbe
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-01-09 13:04:07 -0800

    remove unnecessary test

--- test/e2e/app-dir/app-api/app/pokemon/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { requestHandler } from '../../request-handler'
-
-export const GET = requestHandler(async (_, context) => {
-  const res = await fetch(`https://api.vercel.app/pokemon/${context.params.id}`)
-  const pokemon = await res.json()
-
-  return NextResponse.json(pokemon)
-})
