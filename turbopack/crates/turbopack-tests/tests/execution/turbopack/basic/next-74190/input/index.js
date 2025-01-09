9364cc3f80f3d483842abb06a956918eb8d822c4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-01-09 13:52:26 +0900

    Use execution test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/next-74190/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/next-74190/input/index.js
@@ -1,8 +1,8 @@
 var root = {};
-(function _(namespace, path, parent) {
+const result = (function _(namespace, path, parent) {
+  console.debug("####", path, path ? true : false)
   for (var i = 0, keys = Object.keys(namespace); i < keys.length; i++) {
     var key = keys[i];
-    console.debug("####", path, path ? true : false)
     parent[key] = _(namespace[key], path ? `${path}.${key}` : key, parent[key] || {});
   }
   return parent;
@@ -13,3 +13,13 @@ var root = {};
     },
   }
 }, "", root);
+
+it('should evaluate IIFE correctly', () => {
+  expect(result).toEqual({
+    wrapper: {
+      data: {
+        key: {}
+      },
+    }
+  });
+});
