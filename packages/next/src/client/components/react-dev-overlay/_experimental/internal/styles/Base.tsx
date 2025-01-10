6c52257a354dcf3237d7f1f435b4242baa7dff09
Jiwon Choi <devjiwonchoi@gmail.com>
2025-01-11 01:33:32 +0800

    [DevOverlay] Add Runtime Error CodeFrame (#74682)
    This PR added style for Runtime Error Code Frame. The file icon will
    follow up and will be replaced with correct icons (e.g. js, ts, etc.)
    
    > [!NOTE]
    > Does not fully align with Figma due to restrictions on how
    `@babel/code-frame` handles colors and spacing.
    
    ### Light
    
    ![CleanShot 2025-01-10 at 23 15
    52](https://github.com/user-attachments/assets/fec89610-117a-416e-8554-9f83e2b68003)
    
    ### Dark
    
    ![CleanShot 2025-01-10 at 23 16
    22](https://github.com/user-attachments/assets/58b39260-a1b5-494d-b334-295e8bb0faa8)
    
    Closes NDX-650
    
    ---------
    
    Co-authored-by: Jiachi Liu <inbox@huozhi.im>

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/styles/Base.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/styles/Base.tsx
@@ -44,27 +44,6 @@ export function Base() {
           --font-stack-sans: 'Geist', -apple-system, 'Source Sans Pro',
             sans-serif;
 
-          --color-ansi-selection: rgba(95, 126, 151, 0.48);
-          --color-ansi-bg: #111111;
-          --color-ansi-fg: #cccccc;
-
-          --color-ansi-white: #777777;
-          --color-ansi-black: #141414;
-          --color-ansi-blue: #00aaff;
-          --color-ansi-cyan: #88ddff;
-          --color-ansi-green: #98ec65;
-          --color-ansi-magenta: #aa88ff;
-          --color-ansi-red: #ff5555;
-          --color-ansi-yellow: #ffcc33;
-          --color-ansi-bright-white: #ffffff;
-          --color-ansi-bright-black: #777777;
-          --color-ansi-bright-blue: #33bbff;
-          --color-ansi-bright-cyan: #bbecff;
-          --color-ansi-bright-green: #b6f292;
-          --color-ansi-bright-magenta: #cebbff;
-          --color-ansi-bright-red: #ff8888;
-          --color-ansi-bright-yellow: #ffd966;
-
           font-family: var(--font-stack-sans);
 
           /* TODO: Remove replaced ones. */
