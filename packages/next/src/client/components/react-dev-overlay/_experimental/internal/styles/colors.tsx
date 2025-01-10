f4c85a249023af3fed5df7cd2c7635dd7ee18a0a
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-10 23:20:37 +0900

    remove ansi temp dupl

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/styles/colors.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/styles/colors.tsx
@@ -220,28 +220,6 @@ export function Colors() {
             --color-turbopack-border-blue: #284f80;
             --color-turbopack-background-red: #250d12;
             --color-turbopack-background-blue: #0a1723;
-
-            /* Ansi Light - Temporary */
-            --color-ansi-selection: rgba(95, 126, 151, 0.48);
-            --color-ansi-bg: #111111;
-            --color-ansi-fg: #cccccc;
-
-            --color-ansi-white: #777777;
-            --color-ansi-black: #141414;
-            --color-ansi-blue: #00aaff;
-            --color-ansi-cyan: #88ddff;
-            --color-ansi-green: #98ec65;
-            --color-ansi-magenta: #aa88ff;
-            --color-ansi-red: #ff5555;
-            --color-ansi-yellow: #ffcc33;
-            --color-ansi-bright-white: #ffffff;
-            --color-ansi-bright-black: #777777;
-            --color-ansi-bright-blue: #33bbff;
-            --color-ansi-bright-cyan: #bbecff;
-            --color-ansi-bright-green: #b6f292;
-            --color-ansi-bright-magenta: #cebbff;
-            --color-ansi-bright-red: #ff8888;
-            --color-ansi-bright-yellow: #ffd966;
           }
         }
       `}
