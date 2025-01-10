d12fceaca30445a1c419559de3862fea697cde53
Jude Gao <jude.gao@vercel.com>
2025-01-10 16:17:22 -0500

    good progress

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/Errors/dev-tools-indicator/internal/next-logo.stories.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/Errors/dev-tools-indicator/internal/next-logo.stories.tsx
@@ -17,23 +17,55 @@ type Story = StoryObj<typeof NextLogo>
 export const NoIssues: Story = {
   args: {
     issueCount: 0,
+    isDevBuilding: false,
+    isDevRendering: false,
   },
 }
 
 export const SingleIssue: Story = {
   args: {
     issueCount: 1,
+    isDevBuilding: false,
+    isDevRendering: false,
   },
 }
 
 export const MultipleIssues: Story = {
   args: {
     issueCount: 5,
+    isDevBuilding: false,
+    isDevRendering: false,
   },
 }
 
 export const ManyIssues: Story = {
   args: {
     issueCount: 99,
+    isDevBuilding: false,
+    isDevRendering: false,
+  },
+}
+
+export const Building: Story = {
+  args: {
+    issueCount: 0,
+    isDevBuilding: true,
+    isDevRendering: false,
+  },
+}
+
+export const BuildingWithError: Story = {
+  args: {
+    issueCount: 1,
+    isDevBuilding: true,
+    isDevRendering: false,
+  },
+}
+
+export const Rendering: Story = {
+  args: {
+    issueCount: 0,
+    isDevBuilding: false,
+    isDevRendering: true,
   },
 }
