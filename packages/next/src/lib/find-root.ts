bb29e87fcd752f615f3b8795754d7eed610c9332
Tim Neutkens <tim@timneutkens.nl>
2025-01-13 14:32:09 +0100

    Ensure pnpm-workspace is checked first before looking for lockfiles.

--- packages/next/src/lib/find-root.ts
+++ packages/next/src/lib/find-root.ts
@@ -1,10 +1,23 @@
 import { dirname } from 'path'
 import findUp from 'next/dist/compiled/find-up'
 
-export function findRootLockFile(cwd: string) {
+function findRootLockFile(cwd: string) {
+  // Find-up evaluates the list of files at each level.
+  // For pnpm-workspace.yaml we first want to look up before searching for lockfiles as those can be included in the application directory by accident.
+  const pnpmWorkspaceFile = findUp.sync(
+    'pnpm-workspace.yaml',
+
+    {
+      cwd,
+    }
+  )
+
+  if (pnpmWorkspaceFile) {
+    return pnpmWorkspaceFile
+  }
+
   return findUp.sync(
     [
-      'pnpm-workspace.yaml',
       'pnpm-lock.yaml',
       'package-lock.json',
       'yarn.lock',
