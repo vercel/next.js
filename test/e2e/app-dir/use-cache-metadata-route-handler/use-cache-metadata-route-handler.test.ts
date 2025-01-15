vercel/next.js

commit 6cc85deaeac30ac916e0f310d05d99923d85a1eb

Add partial support for `"use cache"` in metadata route handlers (#74835)
Adds support for using `"use cache"` in the special metadata route
handlers like
[`sitemap.ts`](https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap#generating-a-sitemap-using-code-js-ts),
[`opengraph-image.tsx`](https://nextjs.org/docs/app/api-reference/file-conventions/metadata/opengraph-image#generate-images-using-code-js-ts-tsx),
[`icon.tsx`](https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons#generate-icons-using-code-js-ts-tsx),
and other [metadata
files](https://nextjs.org/docs/app/api-reference/file-conventions/metadata).

reverts #71225
fixes #74146
closes NAR-51

As a follow-up we need to ensure that opengraph image responses do not
bail out of static generation when `dynamicIO` is enabled.

packages/next/src/build/webpack/plugins/flight-client-entry-plugin.ts
@@ -4,7 +4,7 @@ import type {
 } from '../loaders/next-flight-client-entry-loader'
 
 import { webpack } from 'next/dist/compiled/webpack/webpack'
-import { stringify } from 'querystring'
+import { parse, stringify } from 'querystring'
 import path from 'path'
 import { sources } from 'next/dist/compiled/webpack/webpack'
 import {
@@ -13,7 +13,10 @@ import {
   EntryTypes,
   getEntryKey,
 } from '../../../server/dev/on-demand-entry-handler'
-import { WEBPACK_LAYERS } from '../../../lib/constants'
+import {
+  WEBPACK_LAYERS,
+  WEBPACK_RESOURCE_QUERIES,
+} from '../../../lib/constants'
 import {
   APP_CLIENT_INTERNALS,
   BARREL_OPTIMIZATION_PREFIX,
@@ -41,6 +44,7 @@ import { PAGE_TYPES } from '../../../lib/page-types'
 import { getModuleBuildInfo } from '../loaders/get-module-build-info'
 import { getAssumedSourceType } from '../loaders/next-flight-loader'
 import { isAppRouteRoute } from '../../../lib/is-app-route-route'
+import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
 
 interface Options {
   dev: boolean
@@ -296,9 +300,13 @@ export class FlightClientEntryPlugin {
         compilation.moduleGraph
       )) {
         // Entry can be any user defined entry files such as layout, page, error, loading, etc.
-        const entryRequest = (
+        let entryRequest = (
           connection.dependency as unknown as webpack.NormalModule
         ).request
+
+        if (entryRequest.endsWith(WEBPACK_RESOURCE_QUERIES.metadataRoute)) {
+          entryRequest = getMetadataRouteResource(entryRequest)
+        }
 
         const { clientComponentImports, actionImports, cssImports } =
           this.collectComponentInfoFromServerEntryDependency({
@@ -332,9 +340,15 @@ export class FlightClientEntryPlugin {
           : entryRequest
 
         // Replace file suffix as `.js` will be added.
-        const bundlePath = normalizePathSep(
+        let bundlePath = normalizePathSep(
           relativeRequest.replace(/\.[^.\\/]+$/, '').replace(/^src[\\/]/, '')
         )
+
+        // For metadata routes, the entry name can be used as the bundle path,
+        // as it has been normalized already.
+        if (isMetadataRoute(bundlePath)) {
+          bundlePath = name
+        }
 
         Object.assign(mergedCSSimports, cssImports)
         clientEntriesToInject.push({
@@ -1094,5 +1108,16 @@ function getModuleResource(mod: webpack.NormalModule): string {
   if (mod.matchResource?.startsWith(BARREL_OPTIMIZATION_PREFIX)) {
     modResource = mod.matchResource + ':' + modResource
   }
+
+  if (mod.resource === `?${WEBPACK_RESOURCE_QUERIES.metadataRoute}`) {
+    return getMetadataRouteResource(mod.rawRequest)
+  }
+
   return modResource
+}
+
+function getMetadataRouteResource(request: string): string {
+  const query = request.split('next-metadata-route-loader?')[1]
+
+  return parse(query).filePath as string
 }

packages/next/src/build/webpack/plugins/next-trace-entrypoints-plugin.ts
@@ -19,7 +19,6 @@ import picomatch from 'next/dist/compiled/picomatch'
 import { getModuleBuildInfo } from '../loaders/get-module-build-info'
 import { getPageFilePath } from '../../entries'
 import { resolveExternal } from '../../handle-externals'
-import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
 
 const PLUGIN_NAME = 'TraceEntryPointsPlugin'
 export const TRACE_IGNORES = [
@@ -243,18 +242,15 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
         )
 
         if (entrypoint.name.startsWith('app/')) {
-          // Include the client reference manifest for pages and route handlers,
-          // excluding metadata route handlers.
-          const clientManifestsForEntrypoint = isMetadataRoute(entrypoint.name)
-            ? null
-            : nodePath.join(
-                outputPath,
-                outputPrefix,
-                entrypoint.name.replace(/%5F/g, '_') +
-                  '_' +
-                  CLIENT_REFERENCE_MANIFEST +
-                  '.js'
-              )
+          // include the client reference manifest
+          const clientManifestsForEntrypoint = nodePath.join(
+            outputPath,
+            outputPrefix,
+            entrypoint.name.replace(/%5F/g, '_') +
+              '_' +
+              CLIENT_REFERENCE_MANIFEST +
+              '.js'
+          )
 
           if (clientManifestsForEntrypoint !== null) {
             entryFiles.add(clientManifestsForEntrypoint)

test/e2e/app-dir/use-cache-metadata-route-handler/use-cache-metadata-route-handler.test.ts
@@ -1,0 +1,134 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('use-cache-metadata-route-handler', () => {
+  const { next, isNextDev, isNextStart } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('should generate an opengraph image with a metadata route handler that uses "use cache"', async () => {
+    const res = await next.fetch('/opengraph-image')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('image/png')
+
+    if (isNextStart) {
+      const [buildStatus] = next.cliOutput.match(/. \/opengraph-image/)
+
+      // TODO: Should always be `○ /opengraph-image`.
+      expect(buildStatus).toBeOneOf([
+        '○ /opengraph-image',
+        'ƒ /opengraph-image',
+      ])
+    }
+  })
+
+  it('should generate an icon image with a metadata route handler that uses "use cache"', async () => {
+    const res = await next.fetch('/icon')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('image/png')
+
+    if (isNextStart) {
+      const [buildStatus] = next.cliOutput.match(/. \/icon/)
+
+      // TODO: Should always be `○ /icon`.
+      expect(buildStatus).toBeOneOf(['○ /icon', 'ƒ /icon'])
+    }
+  })
+
+  it('should generate sitemaps with a metadata route handler that uses "use cache"', async () => {
+    const res = await next.fetch('/sitemap.xml')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('application/xml')
+
+    const body = await res.text()
+
+    if (isNextDev) {
+      expect(body).toMatchInlineSnapshot(`
+       "<?xml version="1.0" encoding="UTF-8"?>
+       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+       <loc>https://acme.com?sentinel=runtime</loc>
+       </url>
+       </urlset>
+       "
+      `)
+    } else {
+      expect(body).toMatchInlineSnapshot(`
+       "<?xml version="1.0" encoding="UTF-8"?>
+       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+       <loc>https://acme.com?sentinel=buildtime</loc>
+       </url>
+       </urlset>
+       "
+      `)
+    }
+  })
+
+  it('should generate multiple sitemaps with a metadata route handler that uses "use cache"', async () => {
+    const res = await next.fetch('/products/sitemap/1.xml')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('application/xml')
+
+    const body = await res.text()
+
+    if (isNextDev) {
+      expect(body).toMatchInlineSnapshot(`
+       "<?xml version="1.0" encoding="UTF-8"?>
+       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+       <loc>https://acme.com/1?sentinel=runtime</loc>
+       </url>
+       </urlset>
+       "
+      `)
+    } else {
+      expect(body).toMatchInlineSnapshot(`
+       "<?xml version="1.0" encoding="UTF-8"?>
+       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+       <url>
+       <loc>https://acme.com/1?sentinel=buildtime</loc>
+       </url>
+       </urlset>
+       "
+      `)
+    }
+  })
+
+  it('should generate robots.txt with a metadata route handler that uses "use cache"', async () => {
+    const res = await next.fetch('/robots.txt')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('text/plain')
+
+    const body = await res.text()
+
+    if (isNextDev) {
+      expect(body).toMatchInlineSnapshot(`
+        "User-Agent: *
+        Allow: /runtime
+        
+        "
+        `)
+    } else {
+      expect(body).toMatchInlineSnapshot(`
+       "User-Agent: *
+       Allow: /buildtime
+
+       "
+      `)
+    }
+  })
+
+  it('should generate manifest.json with a metadata route handler that uses "use cache"', async () => {
+    const res = await next.fetch('/manifest.webmanifest')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('content-type')).toBe('application/manifest+json')
+
+    const body = await res.json()
+
+    if (isNextDev) {
+      expect(body).toEqual({ name: 'runtime' })
+    } else {
+      expect(body).toEqual({ name: 'buildtime' })
+    }
+  })
+})

packages/next/src/build/webpack-config.ts
@@ -1309,6 +1309,17 @@ export default async function getBaseWebpackConfig(
                     },
                   ],
                 },
+                resourceQuery: {
+                  // Do not apply next-flight-loader to imports generated by the
+                  // next-metadata-image-loader, to avoid generating unnecessary
+                  // and conflicting entries in the flight client entry plugin.
+                  // These are already covered by the next-metadata-route-loader
+                  // entries.
+                  not: [
+                    new RegExp(WEBPACK_RESOURCE_QUERIES.metadata),
+                    new RegExp(WEBPACK_RESOURCE_QUERIES.metadataImageMeta),
+                  ],
+                },
                 resolve: {
                   mainFields: getMainField(compilerType, true),
                   conditionNames: reactServerCondition,

packages/next/src/build/webpack/plugins/flight-manifest-plugin.ts
@@ -26,7 +26,6 @@ import {
 } from '../utils'
 import type { ChunkGroup } from 'webpack'
 import { encodeURIPath } from '../../../shared/lib/encode-uri-path'
-import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
 import type { ModuleInfo } from './flight-client-entry-plugin'
 
 interface Options {
@@ -559,9 +558,9 @@ export class ClientReferenceManifestPlugin {
         manifestEntryFiles.push(entryName.replace(/\/page(\.[^/]+)?$/, '/page'))
       }
 
-      // We also need to create manifests for route handler entrypoints
-      // (excluding metadata route handlers) to enable `'use cache'`.
-      if (/\/route$/.test(entryName) && !isMetadataRoute(entryName)) {
+      // We also need to create manifests for route handler entrypoints to
+      // enable `'use cache'`.
+      if (/\/route$/.test(entryName)) {
         manifestEntryFiles.push(entryName)
       }
 

packages/next/src/build/webpack/utils.ts
@@ -7,7 +7,6 @@ import type {
   ModuleGraph,
 } from 'webpack'
 import type { ModuleGraphConnection } from 'webpack'
-import { isMetadataRoute } from '../../lib/metadata/is-metadata-route'
 
 export function traverseModules(
   compilation: Compilation,
@@ -48,11 +47,7 @@ export function forEachEntryModule(
 ) {
   for (const [name, entry] of compilation.entries.entries()) {
     // Skip for entries under pages/
-    if (
-      name.startsWith('pages/') ||
-      // Skip for metadata route handlers
-      (name.startsWith('app/') && isMetadataRoute(name))
-    ) {
+    if (name.startsWith('pages/')) {
       continue
     }
 

packages/next/src/server/load-components.ts
@@ -32,7 +32,6 @@ import { wait } from '../lib/wait'
 import { setReferenceManifestsSingleton } from './app-render/encryption-utils'
 import { createServerModuleMap } from './app-render/action-utils'
 import type { DeepReadonly } from '../shared/lib/deep-readonly'
-import { isMetadataRoute } from '../lib/metadata/is-metadata-route'
 import { normalizePagePath } from '../shared/lib/page-path/normalize-page-path'
 
 export type ManifestItem = {
@@ -169,9 +168,6 @@ async function loadComponentsImpl<N = any>({
     ])
   }
 
-  // Make sure to avoid loading the manifest for metadata route handlers.
-  const hasClientManifest = isAppPath && !isMetadataRoute(page)
-
   // In dev mode we retry loading a manifest file to handle a race condition
   // that can occur while app and pages are compiling at the same time, and the
   // build-manifest is still being written to disk while an app path is
@@ -227,7 +223,7 @@ async function loadComponentsImpl<N = any>({
           join(distDir, `${DYNAMIC_CSS_MANIFEST}.json`),
           manifestLoadAttempts
         ).catch(() => undefined),
-    hasClientManifest
+    isAppPath
       ? tryLoadClientReferenceManifest(
           join(
             distDir,

commit 441a2352acf0b1e0e36855f71d652908caf23ea7

Skip client reference manifests for static metadata route handlers (#74876)
Fixes a regression caused by #74835.


[x-ref](https://github.com/vercel/next.js/actions/runs/12773698779/job/35607288377)

packages/next/src/build/webpack/loaders/next-metadata-route-loader.ts
@@ -41,7 +41,7 @@ const cacheHeader = {
   revalidate: 'public, max-age=0, must-revalidate',
 }
 
-type MetadataRouteLoaderOptions = {
+export type MetadataRouteLoaderOptions = {
   // Using separate argument to avoid json being parsed and hit error
   // x-ref: https://github.com/vercel/next.js/pull/62615
   filePath: string

packages/next/src/build/webpack/plugins/flight-client-entry-plugin.ts
@@ -45,6 +45,7 @@ import { getModuleBuildInfo } from '../loaders/get-module-build-info'
 import { getAssumedSourceType } from '../loaders/next-flight-loader'
 import { isAppRouteRoute } from '../../../lib/is-app-route-route'
 import { isMetadataRoute } from '../../../lib/metadata/is-metadata-route'
+import type { MetadataRouteLoaderOptions } from '../loaders/next-metadata-route-loader'
 
 interface Options {
   dev: boolean
@@ -305,7 +306,12 @@ export class FlightClientEntryPlugin {
         ).request
 
         if (entryRequest.endsWith(WEBPACK_RESOURCE_QUERIES.metadataRoute)) {
-          entryRequest = getMetadataRouteResource(entryRequest)
+          const { filePath, isDynamicRouteExtension } =
+            getMetadataRouteResource(entryRequest)
+
+          if (isDynamicRouteExtension === '1') {
+            entryRequest = filePath
+          }
         }
 
         const { clientComponentImports, actionImports, cssImports } =
@@ -1110,14 +1116,15 @@ function getModuleResource(mod: webpack.NormalModule): string {
   }
 
   if (mod.resource === `?${WEBPACK_RESOURCE_QUERIES.metadataRoute}`) {
-    return getMetadataRouteResource(mod.rawRequest)
+    return getMetadataRouteResource(mod.rawRequest).filePath
   }
 
   return modResource
 }
 
-function getMetadataRouteResource(request: string): string {
-  const query = request.split('next-metadata-route-loader?')[1]
+function getMetadataRouteResource(request: string): MetadataRouteLoaderOptions {
+  // e.g. next-metadata-route-loader?filePath=<some-url-encoded-path>&isDynamicRouteExtension=1!?__next_metadata_route__
+  const query = request.split('!')[0].split('next-metadata-route-loader?')[1]
 
-  return parse(query).filePath as string
+  return parse(query) as MetadataRouteLoaderOptions
 }

packages/next/src/build/webpack/plugins/next-trace-entrypoints-plugin.ts
@@ -19,6 +19,7 @@ import picomatch from 'next/dist/compiled/picomatch'
 import { getModuleBuildInfo } from '../loaders/get-module-build-info'
 import { getPageFilePath } from '../../entries'
 import { resolveExternal } from '../../handle-externals'
+import { isStaticMetadataRoute } from '../../../lib/metadata/is-metadata-route'
 
 const PLUGIN_NAME = 'TraceEntryPointsPlugin'
 export const TRACE_IGNORES = [
@@ -115,6 +116,7 @@ export interface BuildTraceContext {
     outputPath: string
     depModArray: string[]
     entryNameMap: Record<string, string>
+    absolutePathByEntryName: Record<string, string>
   }
   chunksTrace?: {
     action: TurbotraceAction
@@ -241,19 +243,28 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
           nodePath.join(outputPath, `${outputPrefix}${entrypoint.name}.js`)
         )
 
-        if (entrypoint.name.startsWith('app/')) {
-          // include the client reference manifest
-          const clientManifestsForEntrypoint = nodePath.join(
-            outputPath,
-            outputPrefix,
-            entrypoint.name.replace(/%5F/g, '_') +
-              '_' +
-              CLIENT_REFERENCE_MANIFEST +
-              '.js'
-          )
+        if (entrypoint.name.startsWith('app/') && this.appDir) {
+          const appDirRelativeEntryPath =
+            this.buildTraceContext.entriesTrace?.absolutePathByEntryName[
+              entrypoint.name
+            ]?.replace(this.appDir, '')
 
-          if (clientManifestsForEntrypoint !== null) {
-            entryFiles.add(clientManifestsForEntrypoint)
+          // Include the client reference manifest in the trace, but not for
+          // static metadata routes, for which we don't generate those.
+          if (
+            appDirRelativeEntryPath &&
+            !isStaticMetadataRoute(appDirRelativeEntryPath)
+          ) {
+            entryFiles.add(
+              nodePath.join(
+                outputPath,
+                outputPrefix,
+                entrypoint.name.replace(/%5F/g, '_') +
+                  '_' +
+                  CLIENT_REFERENCE_MANIFEST +
+                  '.js'
+              )
+            )
           }
         }
 
@@ -315,6 +326,7 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
             const entryNameMap = new Map<string, string>()
             const entryModMap = new Map<string, any>()
             const additionalEntries = new Map<string, Map<string, any>>()
+            const absolutePathByEntryName = new Map<string, string>()
 
             const depModMap = new Map<string, any>()
 
@@ -356,6 +368,7 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
                           ) {
                             entryModMap.set(absolutePath, entryMod)
                             entryNameMap.set(absolutePath, name)
+                            absolutePathByEntryName.set(name, absolutePath)
                           }
                         }
 
@@ -441,6 +454,9 @@ export class TraceEntryPointsPlugin implements webpack.WebpackPluginInstance {
               appDir: this.rootDir,
               depModArray: Array.from(depModMap.keys()),
               entryNameMap: Object.fromEntries(entryNameMap),
+              absolutePathByEntryName: Object.fromEntries(
+                absolutePathByEntryName
+              ),
               outputPath: compilation.outputOptions.path!,
             }
 

test/e2e/app-dir/use-cache-metadata-route-handler/use-cache-metadata-route-handler.test.ts
@@ -1,7 +1,7 @@
 import { nextTestSetup } from 'e2e-utils'
 
 describe('use-cache-metadata-route-handler', () => {
-  const { next, isNextDev, isNextStart } = nextTestSetup({
+  const { next, isNextDev, isNextStart, isTurbopack } = nextTestSetup({
     files: __dirname,
   })
 
@@ -131,4 +131,31 @@ describe('use-cache-metadata-route-handler', () => {
       expect(body).toEqual({ name: 'buildtime' })
     }
   })
+
+  if (isNextStart && !isTurbopack) {
+    it('should include the client reference manifest in the route.js.nft.json files of dynamic metadata routes', async () => {
+      for (const filename of [
+        'icon',
+        'manifest.webmanifest',
+        'opengraph-image',
+        'products/sitemap/[__metadata_id__]',
+        'robots.txt',
+        'sitemap.xml',
+      ]) {
+        const { files } = await next.readJSON(
+          `/.next/server/app/${filename}/route.js.nft.json`
+        )
+
+        expect(files).toInclude('route_client-reference-manifest.js')
+      }
+    })
+
+    it('should not include the client reference manifest in the route.js.nft.json files of static metadata routes', async () => {
+      const { files } = await next.readJSON(
+        '/.next/server/app/favicon.ico/route.js.nft.json'
+      )
+
+      expect(files).not.toInclude('route_client-reference-manifest.js')
+    })
+  }
 })