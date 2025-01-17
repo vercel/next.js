02b0d50266a5f9ceadbfbc3d8f17feb2bb4a1d48
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-01-17 08:37:41 -0800

    fix tests

--- test/e2e/app-dir/cssnano-colormin/index.test.ts
+++ test/e2e/app-dir/cssnano-colormin/index.test.ts
@@ -1,17 +1,15 @@
-import { createNextDescribe } from 'e2e-utils'
+import { nextTestSetup } from 'e2e-utils'
 
-createNextDescribe(
-  'cssnano-colormin',
-  {
+describe('cssnano-colormin', () => {
+  const { next } = nextTestSetup({
     files: __dirname,
-  },
-  ({ next }) => {
-    it('should not minify rgb colors to hsla', async () => {
-      const browser = await next.browser('/')
-      let color = await browser.eval(
-        `window.getComputedStyle(document.querySelector('.foo')).backgroundColor`
-      )
-      expect(color).toBe('rgba(143, 101, 98, 0.43)')
-    })
-  }
-)
+  })
+
+  it('should not minify rgb colors to hsla', async () => {
+    const browser = await next.browser('/')
+    let color = await browser.eval(
+      `window.getComputedStyle(document.querySelector('.foo')).backgroundColor`
+    )
+    expect(color).toBe('rgba(143, 101, 98, 0.43)')
+  })
+})
