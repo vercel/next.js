6e88360560dbddbc5be98dad1f6bceb7c5747b34
rauno <freiberggg@gmail.com>
2025-01-17 10:27:25 +0200

    wip

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/dev-tools-indicator/internal/next-logo.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/dev-tools-indicator/internal/next-logo.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { noop as css } from '../../../../../../internal/helpers/noop-template'
 
 interface Props extends React.ComponentProps<'button'> {
@@ -15,7 +15,24 @@ export const NextLogo = ({
   isDevRendering,
   ...props
 }: Props) => {
+  const hasError = issueCount > 0
+
+  const ref = useRef<HTMLDivElement>(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(true)
+  const [width, setWidth] = useState()
+
+  // bind to F key
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'f') {
+        setError((e) => !e)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [])
 
   // Only shows the loading state after a 200ms delay when building or rendering,
   // to avoid flashing the loading state for quick updates
@@ -30,135 +47,237 @@ export const NextLogo = ({
     }
   }, [isDevBuilding, isDevRendering])
 
+  // measure width of an element ref
+  useEffect(() => {
+    const el = ref.current
+
+    if (!el) {
+      return
+    }
+
+    const observer = new ResizeObserver(() => {
+      const { width: w } = el.getBoundingClientRect()
+      setWidth(w)
+    })
+
+    observer.observe(el)
+    return () => observer.disconnect()
+  }, [])
+
   return (
-    <button
-      onClick={onClick}
-      data-next-logo
-      data-error={issueCount > 0}
-      {...props}
-    >
-      <style>
-        {css`
-          [data-next-logo] {
-            --color-outer-border: var(--color-gray-1000);
-            --color-inner-border: hsla(0, 0%, 100%, 0.14);
-            width: 36px;
-            height: 36px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            position: relative;
-            background: rgba(0, 0, 0, 0.8);
-            box-shadow:
-              0 0 0 1px var(--color-outer-border),
-              inset 0 0 0 1px var(--color-inner-border),
-              0px 16px 32px -8px rgba(0, 0, 0, 0.24);
-            backdrop-filter: blur(48px);
-            border-radius: 50%;
-            user-select: none;
-            cursor: pointer;
-            scale: 1;
-            transition: scale 150ms ease;
+    <>
+      <div data-next-badge data-error={error} style={{ width }}>
+        {/* Styles */}
+        <style>
+          {css`
+            [data-next-badge] {
+              --bezier: cubic-bezier(0.175, 0.5, 0.32, 1.1);
+              --color-outer-border: #171717;
+              --color-inner-border: hsla(0, 0%, 100%, 0.14);
+              --color-hover-alpha: hsla(0, 0%, 100%, 0.14);
+              --color-hover-alpha-2: hsla(0, 0%, 100%, 0.23);
+              --padding: 2px;
+              --size: 36px;
+              --mark-size: calc(var(--size) - var(--padding) * 2);
 
-            &:active {
-              scale: 0.95;
-            }
+              -webkit-font-smoothing: antialiased;
+              width: var(--size);
+              height: var(--size);
+              display: flex;
+              align-items: center;
+              position: relative;
+              background: rgba(0, 0, 0, 0.8);
+              box-shadow:
+                0 0 0 1px var(--color-outer-border),
+                inset 0 0 0 1px var(--color-inner-border),
+                0px 16px 32px -8px rgba(0, 0, 0, 0.24);
+              backdrop-filter: blur(48px);
+              border-radius: 9999px;
+              user-select: none;
+              cursor: pointer;
+              scale: 1;
+              overflow: hidden;
+              transition:
+                scale 150ms var(--bezier),
+                width 250ms var(--bezier),
+                background 150ms var(--bezier);
 
-            &[data-error='true'] {
-              background: #ca2a30;
-              --color-inner-border: #e5484d;
-
-              &:after {
-                opacity: 0;
+              &:active:not([data-error='true']) {
+                scale: 0.95;
               }
 
-              [data-issue-badge] {
-                display: block !important;
+              &[data-error='true'] {
+                width: auto;
+                background: #ca2a30;
+                --color-inner-border: #e5484d;
+
+                [data-next-mark] {
+                  background: var(--color-hover-alpha);
+
+                  &:hover {
+                    background: var(--color-hover-alpha-2);
+                  }
+                }
               }
             }
-          }
 
-          [data-issue-badge] {
-            top: 0;
-            right: 0;
-            position: absolute;
-            width: 8px;
-            height: 8px;
-            background: white;
-            border-radius: 50%;
-            box-shadow: 0 0 0 1px #171717;
-            display: none;
-            z-index: 2;
-          }
+            [data-issues] {
+              display: flex;
+              gap: var(--padding);
+              align-items: center;
+              padding-left: 8px;
+              padding-right: calc(var(--padding) * 2);
+              height: 32px;
+              margin: 0 var(--padding);
+              border-radius: 9999px;
+              transition:
+                background 300ms var(--bezier),
+                translate 300ms var(--bezier);
 
-          .path0 {
-            animation: draw0 1.5s ease-in-out infinite;
-          }
+              &:has([data-issues-open]:hover) {
+                background: var(--color-hover-alpha);
+              }
 
-          .path1 {
-            animation: draw1 1.5s ease-out infinite;
-            animation-delay: 0.3s;
-          }
-
-          .paused {
-            stroke-dashoffset: 0;
-          }
-
-          @keyframes draw0 {
-            0%,
-            25% {
-              stroke-dashoffset: -29.6;
+              [data-cross] {
+                translate: 0 -1px;
+              }
             }
-            25%,
-            50% {
+
+            [data-issues-open] {
+              font-size: 13px;
+              color: white;
+              width: fit-content;
+              height: 100%;
+              display: flex;
+              gap: 8px;
+              align-items: center;
+              margin: 0;
+              line-height: 36px;
+              font-weight: 500;
+              z-index: 2;
+              white-space: nowrap;
+            }
+
+            [data-issues-close] {
+              width: 24px;
+              height: 24px;
+              border-radius: 9999px;
+
+              &:hover {
+                background: var(--color-hover-alpha);
+              }
+            }
+
+            [data-next-mark] {
+              width: var(--mark-size);
+              height: var(--mark-size);
+              margin-left: var(--padding);
+              display: flex;
+              align-items: center;
+              border-radius: 9999px;
+              transition: background 300ms var(--bezier);
+
+              svg {
+                flex-shrink: 0;
+              }
+            }
+
+            .path0 {
+              animation: draw0 1.5s ease-in-out infinite;
+            }
+
+            .path1 {
+              animation: draw1 1.5s ease-out infinite;
+              animation-delay: 0.3s;
+            }
+
+            .paused {
               stroke-dashoffset: 0;
             }
-            50%,
-            75% {
-              stroke-dashoffset: 0;
-            }
-            75%,
-            100% {
-              stroke-dashoffset: 29.6;
-            }
-          }
 
-          @keyframes draw1 {
-            0%,
-            20% {
-              stroke-dashoffset: -11.6;
+            @keyframes draw0 {
+              0%,
+              25% {
+                stroke-dashoffset: -29.6;
+              }
+              25%,
+              50% {
+                stroke-dashoffset: 0;
+              }
+              50%,
+              75% {
+                stroke-dashoffset: 0;
+              }
+              75%,
+              100% {
+                stroke-dashoffset: 29.6;
+              }
             }
-            20%,
-            50% {
-              stroke-dashoffset: 0;
+
+            @keyframes draw1 {
+              0%,
+              20% {
+                stroke-dashoffset: -11.6;
+              }
+              20%,
+              50% {
+                stroke-dashoffset: 0;
+              }
+              50%,
+              75% {
+                stroke-dashoffset: 0;
+              }
+              75%,
+              100% {
+                stroke-dashoffset: 11.6;
+              }
             }
-            50%,
-            75% {
-              stroke-dashoffset: 0;
-            }
-            75%,
-            100% {
-              stroke-dashoffset: 11.6;
-            }
-          }
-        `}
-      </style>
-      <NextMark isLoading={isLoading} />
-      <div data-issue-badge />
-    </button>
+          `}
+        </style>
+        {/* Children */}
+        <button data-next-mark onClick={onClick} {...props}>
+          <NextMark isLoading={isLoading} />
+        </button>
+        {hasError && (
+          <div data-issues>
+            <button data-issues-open aria-label="Open errors overlay">
+              {issueCount} {issueCount === 1 ? 'Issue' : 'Issues'}
+            </button>
+            <button data-issues-close aria-label="Clear errors">
+              <Cross />
+            </button>
+          </div>
+        )}
+      </div>
+      <div
+        ref={ref}
+        data-next-badge
+        data-error={error}
+        style={{ opacity: 0, position: 'absolute' }}
+      >
+        {/* Children */}
+        <button data-next-mark onClick={onClick} {...props}>
+          <NextMark isLoading={isLoading} />
+        </button>
+        {hasError && (
+          <div data-issues>
+            <button data-issues-open aria-label="Open errors overlay">
+              {issueCount} {issueCount === 1 ? 'Issue' : 'Issues'}
+            </button>
+            <button data-issues-close aria-label="Clear errors">
+              <Cross />
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
-function NextMark({ isLoading }: { isLoading?: boolean }) {
+function NextMark({ isLoading, style }: { isLoading?: boolean }) {
   return (
-    <svg
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
-      fill="none"
-      style={{ scale: 1.2 }}
-    >
-      <g transform="translate(12.5, 13)">
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none">
+      <g transform="translate(8.5, 13)">
         <path
           className={isLoading ? 'path0' : 'paused'}
           d="M13.3 15.2 L2.34 1 V12.6"
@@ -207,6 +326,26 @@ function NextMark({ isLoading }: { isLoading?: boolean }) {
           <rect width="5" height="1.5" fill="black" />
         </mask>
       </defs>
+    </svg>
+  )
+}
+
+function Cross() {
+  return (
+    <svg
+      data-cross
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M3.08889 11.8384L2.62486 12.3024L1.69678 11.3744L2.16082 10.9103L6.07178 6.99937L2.16082 3.08841L1.69678 2.62437L2.62486 1.69629L3.08889 2.16033L6.99986 6.07129L10.9108 2.16033L11.3749 1.69629L12.3029 2.62437L11.8389 3.08841L7.92793 6.99937L11.8389 10.9103L12.3029 11.3744L11.3749 12.3024L10.9108 11.8384L6.99986 7.92744L3.08889 11.8384Z"
+        fill="#EAEAEA"
+      />
     </svg>
   )
 }
