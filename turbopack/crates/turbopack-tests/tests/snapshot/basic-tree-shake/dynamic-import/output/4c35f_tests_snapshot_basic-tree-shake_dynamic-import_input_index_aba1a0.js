45ca35122a5eb769c882bc7d04aca8bd7ff62760
Tobias Koppers <tobias.koppers@googlemail.com>
2025-01-18 01:04:49 +0100

    update snapshots

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/dynamic-import/output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_index_aba1a0.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_index_aba1a0.js",
+    {},
+    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_basic-tree-shake_dynamic-import_input_lib_44a9b7.js","output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_index_92a5f4.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/dynamic-import/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
