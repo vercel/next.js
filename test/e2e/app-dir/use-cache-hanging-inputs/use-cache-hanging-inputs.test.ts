2151b791af0eb3f8906a5cadb118051979c5bfec
Hendrik Liebau <mail@hendrik-liebau.de>
2025-01-18 20:13:25 +0100

    Add nested cache test case

--- test/e2e/app-dir/use-cache-hanging-inputs/use-cache-hanging-inputs.test.ts
+++ test/e2e/app-dir/use-cache-hanging-inputs/use-cache-hanging-inputs.test.ts
@@ -102,6 +102,38 @@ describe('use-cache-hanging-inputs', () => {
       }, 180_000)
     })
 
+    describe('when an uncached promise is used inside of a nested "use cache"', () => {
+      it('should show an error toast after a timeout', async () => {
+        const outputIndex = next.cliOutput.length
+        const browser = await next.browser('/uncached-promise-nested')
+
+        // The request is pending while we stall on the hanging inputs, and
+        // playwright will wait for the load even before continuing. So we don't
+        // need to wait for the "use cache" timeout of 50 seconds here.
+
+        await openRedbox(browser)
+
+        const errorDescription = await getRedboxDescription(browser)
+        const errorSource = await getRedboxSource(browser)
+
+        expect(errorDescription).toBe(`[ Cache ] ${expectedErrorMessage}`)
+
+        // TODO(veil): This should have an error source if the source mapping works.
+        expect(errorSource).toBe(null)
+
+        const cliOutput = stripAnsi(next.cliOutput.slice(outputIndex))
+
+        // TODO(veil): Should include properly source mapped stack frames.
+        expect(cliOutput).toContain(
+          isTurbopack
+            ? `${expectedErrorMessage}
+    at [project]/app/uncached-promise-nested/page.tsx [app-rsc] (ecmascript)`
+            : `${expectedErrorMessage}
+    at eval (webpack-internal:///(rsc)/./app/uncached-promise-nested/page.tsx:35:97)`
+        )
+      }, 180_000)
+    })
+
     describe('when an error is thrown', () => {
       it('should show an error overlay with only one error', async () => {
         const browser = await next.browser('/error')
@@ -131,6 +163,10 @@ describe('use-cache-hanging-inputs', () => {
 
       expect(cliOutput).toInclude(
         'Error occurred prerendering page "/uncached-promise"'
+      )
+
+      expect(cliOutput).toInclude(
+        'Error occurred prerendering page "/uncached-promise-nested"'
       )
     }, 180_000)
   }
