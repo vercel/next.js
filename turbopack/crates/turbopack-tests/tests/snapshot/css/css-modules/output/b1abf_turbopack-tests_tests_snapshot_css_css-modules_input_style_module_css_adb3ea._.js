bfc6dcb0222e5acfd73955a865a43d1b5a7d5fae
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-01-21 15:28:01 +0100

    Update snaphots

--- turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules/output/b1abf_turbopack-tests_tests_snapshot_css_css-modules_input_style_module_css_adb3ea._.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules/output/b1abf_turbopack-tests_tests_snapshot_css_css-modules_input_style_module_css_adb3ea._.js
@@ -4,10 +4,12 @@
 
 var { r: __turbopack_require__, f: __turbopack_module_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, M: __turbopack_modules__, l: __turbopack_load__, j: __turbopack_dynamic__, P: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, b: __turbopack_worker_blob_url__, g: global, __dirname, t: __turbopack_require_real__ } = __turbopack_context__;
 {
+let cached = null;
 __turbopack_export_value__((__turbopack_import__) => {
-    return Promise.resolve().then(() => {
+    cached = cached || Promise.resolve().then(() => {
         return __turbopack_import__("[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules/input/style.module.css [test] (css module)");
     });
+    return cached;
 });
 }}),
 }]);
