043d5903b41185d6f573cc5d5e3fd91dc928e403
Tobias Koppers <tobias.koppers@googlemail.com>
2025-01-22 16:27:46 +0100

    fix bug where collectibles where not tracked

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
@@ -203,15 +203,15 @@ impl AggregatedDataUpdate {
             for collectible in collectibles {
                 collectibles_update.push((collectible, 1));
             }
-            collectibles_update.extend(iter_many!(
-                task,
-                Collectible {
-                    collectible
-                } count => {
-                    (collectible, *count)
-                }
-            ));
         }
+        collectibles_update.extend(iter_many!(
+            task,
+            Collectible {
+                collectible
+            } count => {
+                (collectible, *count)
+            }
+        ));
         if let Some(dirty) = get!(task, Dirty) {
             dirty_container_count.update_with_dirty_state(dirty);
         }
