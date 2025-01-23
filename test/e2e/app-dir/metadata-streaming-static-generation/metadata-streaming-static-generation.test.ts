58e62caf61b670e65d8228dde75c86f2c46b49fc
Jiachi Liu <inbox@huozhi.im>
2025-01-23 14:45:32 +0100

    fix test

--- test/e2e/app-dir/metadata-streaming-static-generation/metadata-streaming-static-generation.test.ts
+++ test/e2e/app-dir/metadata-streaming-static-generation/metadata-streaming-static-generation.test.ts
@@ -1,7 +1,7 @@
 import { nextTestSetup } from 'e2e-utils'
 
 describe('app-dir - metadata-streaming-static-generation', () => {
-  const { next, isNextStart } = nextTestSetup({
+  const { next, isNextDev, isNextStart } = nextTestSetup({
     files: __dirname,
   })
 
@@ -20,22 +20,41 @@ describe('app-dir - metadata-streaming-static-generation', () => {
     })
   }
 
-  describe('static pages', () => {
-    it('should contain async generated metadata in head for simple static page', async () => {
-      const $ = await next.render$('/')
-      expect($('head title').text()).toBe('index page')
-    })
+  if (isNextDev) {
+    describe('static pages (development)', () => {
+      it('should contain async generated metadata in body for simple static page', async () => {
+        const $ = await next.render$('/')
+        expect($('body title').text()).toBe('index page')
+      })
 
-    it('should contain async generated metadata in head for slow static page', async () => {
-      const $ = await next.render$('/slow/static')
-      expect($('head title').text()).toBe('slow page - static')
-    })
+      it('should contain async generated metadata in body for slow static page', async () => {
+        const $ = await next.render$('/slow/static')
+        expect($('body title').text()).toBe('slow page - static')
+      })
 
-    it('should contain async generated metadata in head static page with suspenseful content', async () => {
-      const $ = await next.render$('/suspenseful/static')
-      expect($('head title').text()).toBe('suspenseful page - static')
+      it('should contain async generated metadata in body static page with suspenseful content', async () => {
+        const $ = await next.render$('/suspenseful/static')
+        expect($('body title').text()).toBe('suspenseful page - static')
+      })
     })
-  })
+  } else {
+    describe('static pages (production)', () => {
+      it('should contain async generated metadata in head for simple static page', async () => {
+        const $ = await next.render$('/')
+        expect($('head title').text()).toBe('index page')
+      })
+
+      it('should contain async generated metadata in head for slow static page', async () => {
+        const $ = await next.render$('/slow/static')
+        expect($('head title').text()).toBe('slow page - static')
+      })
+
+      it('should contain async generated metadata in head static page with suspenseful content', async () => {
+        const $ = await next.render$('/suspenseful/static')
+        expect($('head title').text()).toBe('suspenseful page - static')
+      })
+    })
+  }
 
   describe('dynamic pages', () => {
     it('should contain async generated metadata in body for simple dynamics page', async () => {
@@ -45,6 +64,26 @@ describe('app-dir - metadata-streaming-static-generation', () => {
 
     it('should contain async generated metadata in body for suspenseful dynamic page', async () => {
       const $ = await next.render$('/slow/dynamic')
+      expect($('body title').text()).toBe('slow page - dynamic')
+    })
+  })
+
+  describe('dynamic pages with html limited bots', () => {
+    it('should contain async generated metadata in body for simple dynamics page', async () => {
+      const $ = await next.render$('/suspenseful/dynamic', {
+        headers: {
+          'User-Agent': 'Discordbot/2.0;',
+        },
+      })
+      expect($('body title').text()).toBe('suspenseful page - dynamic')
+    })
+
+    it('should contain async generated metadata in body for suspenseful dynamic page', async () => {
+      const $ = await next.render$('/slow/dynamic', {
+        headers: {
+          'User-Agent': 'Discordbot/2.0;',
+        },
+      })
       expect($('body title').text()).toBe('slow page - dynamic')
     })
   })
