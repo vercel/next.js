530c4a2a6cdcd3c8980d1235d05962294ab41c20
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-01-24 19:45:17 +0100

    Turbopack: don't hardcode `edge-instrumentation.js` (#75289)

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -2457,7 +2457,11 @@ export default async function build(
           path.join(SERVER_DIRECTORY, `${INSTRUMENTATION_HOOK_FILENAME}.js`)
         )
         // If there's edge routes, append the edge instrumentation hook
-        if (edgeRuntimeAppCount || edgeRuntimePagesCount) {
+        // Turbopack generates this chunk with a hashed name and references it in middleware-manifest.
+        if (
+          !process.env.TURBOPACK &&
+          (edgeRuntimeAppCount || edgeRuntimePagesCount)
+        ) {
           instrumentationHookEntryFiles.push(
             path.join(
               SERVER_DIRECTORY,
