e0a12a0eed03f77573917574e538dc5520a0c04d
Hendrik Liebau <mail@hendrik-liebau.de>
2025-01-24 17:24:34 +0100

    Fix typo in fixture

--- crates/next-custom-transforms/tests/errors/react-server-components/server-graph/use-cache/input.js
+++ crates/next-custom-transforms/tests/errors/react-server-components/server-graph/use-cache/input.js
@@ -1,5 +1,5 @@
 export const runtime = 'edge'
-// The following route segment config are currently allowed:
+// The following route segment configs are currently allowed:
 export const dynamic = 'force-dynamic'
 export const dynamicParams = false
 export const fetchCache = 'force-no-store'
