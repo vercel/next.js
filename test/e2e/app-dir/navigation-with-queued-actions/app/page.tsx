d59a5bd423e15fc8f4e86b56b1ea571849c432c2
Janka Uryga <lolzatu2@gmail.com>
2025-01-27 19:23:47 +0100

    test: wip

--- test/e2e/app-dir/navigation-with-queued-actions/app/page.tsx
+++ test/e2e/app-dir/navigation-with-queued-actions/app/page.tsx
@@ -1,30 +1,27 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { myAction } from './server'
 
 export default function Page() {
   const router = useRouter()
   const [text, setText] = useState('initial')
 
-  useEffect(() => {
-    let count = 1
-    const myActionWrapped = async () => {
-      const id = count++
-      console.log('myAction()', `[call number ${id}]`)
-
-      await myAction()
-      console.log('-> myAction() finished', `[call number ${id}]`)
-    }
-    Promise.all([myActionWrapped(), myActionWrapped()]).then(() =>
-      setText('actions finished')
-    )
-    setTimeout(() => {
-      console.log(`router.replace('?')`)
-      router.replace('?')
-    })
-  }, [router])
-
-  return <>{text}</>
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          Promise.all([myAction(0), myAction(1)]).then(() => setText('done'))
+          setTimeout(() => {
+            router.replace('?')
+          })
+        }}
+      >
+        run actions
+      </button>
+      <div id="action-state">{text}</div>
+    </>
+  )
 }
--- test/e2e/app-dir/navigation-with-queued-actions/app/server.ts
+++ test/e2e/app-dir/navigation-with-queued-actions/app/server.ts
@@ -1,3 +1,8 @@
 'use server'
 
-export const myAction = async () => {}
+import { setTimeout } from 'timers/promises'
+
+export async function myAction(id: number) {
+  console.log(`myAction(${id}) :: server`)
+  await setTimeout(100)
+}
