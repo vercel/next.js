f849b80c571e269819e68e4d22f24c2a91495ec5
devjiwonchoi <devjiwonchoi@gmail.com>
2025-01-29 06:36:24 +0900

    fix lint

--- packages/eslint-config-next/parser.js
+++ packages/eslint-config-next/parser.js
@@ -2,7 +2,7 @@ const {
   parse,
   parseForESLint,
 } = require('next/dist/compiled/babel/eslint-parser')
-const { version } = require('./package.json');
+const { version } = require('./package.json')
 
 module.exports = {
   parse,
