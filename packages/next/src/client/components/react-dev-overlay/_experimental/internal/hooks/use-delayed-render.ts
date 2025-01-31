0f5e2e26d479891be4efb0addef4277ce39f2871
rauno <freiberggg@gmail.com>
2025-01-31 12:20:14 +0200

    Comment useDelayedRender

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/hooks/use-delayed-render.ts
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/hooks/use-delayed-render.ts
@@ -6,7 +6,31 @@ interface Options {
   onUnmount?: () => void
 }
 
-/** Useful to perform CSS animations on React components */
+/**
+ * Useful to perform CSS transitions on React components without
+ * using libraries like Framer Motion. This hook will defer the
+ * unmount of a React component until after a delay.
+ *
+ * @param active - Whether the component should be rendered
+ * @param options - Options for the delayed render
+ * @param options.enterDelay - Delay before rendering the component
+ * @param options.exitDelay - Delay before unmounting the component
+ *
+ * const Modal = ({ active }) => {
+ * const { mounted, rendered } = useDelayedRender(active, {
+ *  exitDelay: 2000,
+ * })
+ *
+ * if (!mounted) return null
+ *
+ * return (
+ *   <Portal>
+ *     <div className={rendered ? 'modal visible' : 'modal'}>...</div>
+ *   </Portal>
+ * )
+ *}
+ *
+ * */
 export function useDelayedRender(
   active: boolean = false,
   options: Options = {}
