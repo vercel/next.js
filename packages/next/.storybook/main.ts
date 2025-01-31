dbce745f4d44c05f8ab4cbcc5c0b751c288db5e3
Jiwon Choi <devjiwonchoi@gmail.com>
2025-02-01 04:28:02 +0900

    [dev-overlay] add a11y test (#75523)
    Added a11y test, but haven't enabled for the CI. Will follow up to
    resolve the test result.
    
    ![CleanShot 2025-02-01 at 03 20
    54](https://github.com/user-attachments/assets/7315d740-dec1-4327-977b-8a8f3b06e95e)
    
    You can manually test a11y at the storybook portal:
    
    
    https://github.com/user-attachments/assets/72929e65-bd70-481e-ac2f-1b518c344373
    
    Closes https://linear.app/vercel/issue/NDX-584/add-a11y-tests

--- packages/next/.storybook/main.ts
+++ packages/next/.storybook/main.ts
@@ -19,6 +19,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-webpack5'),
