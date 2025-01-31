3b26aaef5f3084d4f4af5aaf72984eeab273bc6a
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-01 02:13:07 +0900

    [dev-overlay] add a11y test

--- packages/next/.storybook/preview.ts
+++ packages/next/.storybook/preview.ts
@@ -2,6 +2,9 @@ import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
   parameters: {
+    a11y: {
+      element: 'nextjs-portal',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
