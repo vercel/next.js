51ce3989bfbe012349939a9c1d92e637e2db1b42
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-03 14:44:44 +0100

    [Turbopack] allow to manually mark tasks as root (#75436)
    ### What?
    
    Add `mark_root()` to manually mark tasks as root as performance optimization.

--- turbopack/crates/turbo-tasks/src/backend.rs
+++ turbopack/crates/turbo-tasks/src/backend.rs
@@ -589,6 +589,15 @@ pub trait Backend: Sync + Send {
         // Do nothing by default
     }
 
+    fn set_own_task_aggregation_number(
+        &self,
+        _task: TaskId,
+        _aggregation_number: u32,
+        _turbo_tasks: &dyn TurboTasksBackendApi<Self>,
+    ) {
+        // Do nothing by default
+    }
+
     fn mark_own_task_as_session_dependent(
         &self,
         _task: TaskId,
