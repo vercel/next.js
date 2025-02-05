12b4eabc7414968af64febf23bae4c381c715627
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-05 17:28:13 +0100

    Fix expectations

--- test/e2e/app-dir/use-cache-standalone-search-params/use-cache-standalone-search-params.test.ts
+++ test/e2e/app-dir/use-cache-standalone-search-params/use-cache-standalone-search-params.test.ts
@@ -7,8 +7,8 @@ import {
 } from 'next-test-utils'
 import stripAnsi from 'strip-ansi'
 
-const expectedErrorMessage =
-  'Error: Route /search-params-used used "searchParams" inside "use cache". Accessing Dynamic data sources inside a cache scope is not supported. If you need this data inside a cached function use "searchParams" outside of the cached function and pass the required dynamic data in as an argument. See more info here: https://nextjs.org/docs/messages/next-request-in-use-cache'
+const getExpectedErrorMessage = (route: string) =>
+  `Error: Route ${route} used "searchParams" inside "use cache". Accessing Dynamic data sources inside a cache scope is not supported. If you need this data inside a cached function use "searchParams" outside of the cached function and pass the required dynamic data in as an argument. See more info here: https://nextjs.org/docs/messages/next-request-in-use-cache`
 
 describe('use-cache-standalone-search-params', () => {
   const { next, isNextDev, isTurbopack, skipped } = nextTestSetup({
@@ -22,25 +22,29 @@ describe('use-cache-standalone-search-params', () => {
   }
 
   if (isNextDev) {
+    let route: string
+
     describe('when searchParams are used inside of "use cache"', () => {
+      beforeAll(() => {
+        route = '/search-params-used'
+      })
+
       it('should show an error', async () => {
         const outputIndex = next.cliOutput.length
-        const browser = await next.browser('/search-params-used?foo=1')
+        const browser = await next.browser(`${route}?foo=1`)
 
         await assertHasRedbox(browser)
 
         const errorDescription = await getRedboxDescription(browser)
         const errorSource = await getRedboxSource(browser)
+        const expectedErrorMessage = getExpectedErrorMessage(route)
 
         expect(errorDescription).toBe(`[ Cache ] ${expectedErrorMessage}`)
 
         const cliOutput = stripAnsi(next.cliOutput.slice(outputIndex))
 
         if (isTurbopack) {
-          // TODO(veil): For Turbopack, a fix in the React Flight Client, where
-          // sourceURL is encoded, is needed for the error source and stack
-          // frames to be source mapped.
-
+          // TODO(veil): Should have a mapped error source.
           expect(errorSource).toBe(null)
 
           // TODO(veil): Should be a relative filename.
@@ -66,22 +70,32 @@ describe('use-cache-standalone-search-params', () => {
     })
 
     describe('when searchParams are unused inside of "use cache"', () => {
+      beforeAll(() => {
+        route = '/search-params-unused'
+      })
+
       it('should not show an error', async () => {
         const outputIndex = next.cliOutput.length
-        const browser = await next.browser('/search-params-unused?foo=1')
+        const browser = await next.browser(`${route}?foo=1`)
 
         await assertNoRedbox(browser)
 
         const cliOutput = stripAnsi(next.cliOutput.slice(outputIndex))
 
-        expect(cliOutput).not.toContain(expectedErrorMessage)
+        expect(cliOutput).not.toContain(getExpectedErrorMessage(route))
       })
     })
   } else {
     it('should fail the build with errors', async () => {
       const { cliOutput } = await next.build()
 
-      expect(cliOutput).toInclude(expectedErrorMessage)
+      expect(cliOutput).toInclude(
+        getExpectedErrorMessage('/search-params-used')
+      )
+
+      expect(cliOutput).not.toInclude(
+        getExpectedErrorMessage('/search-params-unused')
+      )
 
       expect(cliOutput).toInclude(
         'Error occurred prerendering page "/search-params-used"'
