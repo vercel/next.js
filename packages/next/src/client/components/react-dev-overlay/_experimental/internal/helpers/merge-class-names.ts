0382a71959452c307f4657b846176e3ebc3555db
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-07 03:17:37 +0900

    add comment

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/helpers/merge-class-names.ts
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/helpers/merge-class-names.ts
@@ -1,7 +1,16 @@
+/**
+ * Merge multiple args to a single string with spaces. Useful for merging class names.
+ *
+ * @example
+ * mergeClassNames('foo', 'bar') // 'foo bar'
+ * mergeClassNames('foo', null, 'bar', undefined, 'baz', false) // 'foo bar baz'
+ *
+ * @alias `cn()`
+ */
 export function mergeClassNames(
-  ...classNames: (string | undefined | null | false)[]
+  ...args: (string | undefined | null | false)[]
 ): string {
-  return classNames.filter(Boolean).join(' ')
+  return args.filter(Boolean).join(' ')
 }
 
 export { mergeClassNames as cn }
