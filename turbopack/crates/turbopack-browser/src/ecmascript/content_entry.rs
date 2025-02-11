f50164ed2d1502cb5e2848d2b065a53d2df6e3c9
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-02-11 11:06:38 +0100

    Turbopack: remove unused code (#75897)

--- turbopack/crates/turbopack-browser/src/ecmascript/content_entry.rs
+++ turbopack/crates/turbopack-browser/src/ecmascript/content_entry.rs
@@ -1,13 +1,9 @@
-use std::io::Write as _;
-
 use anyhow::Result;
 use tracing::{info_span, Instrument};
 use turbo_tasks::{FxIndexMap, ReadRef, ResolvedVc, TryJoinIterExt, ValueToString, Vc};
 use turbopack_core::{
     chunk::{AsyncModuleInfo, ChunkItem, ChunkItemExt, ChunkItemTy, ModuleId},
-    code_builder::{Code, CodeBuilder},
-    error::PrettyPrintError,
-    issue::{code_gen::CodeGenerationIssue, IssueExt, IssueSeverity, StyledString},
+    code_builder::Code,
 };
 use turbopack_ecmascript::chunk::{
     EcmascriptChunkContent, EcmascriptChunkItem, EcmascriptChunkItemExt,
@@ -99,45 +95,4 @@ impl EcmascriptDevChunkContentEntries {
 
         Ok(Vc::cell(entries))
     }
-}
-
-#[turbo_tasks::function]
-async fn item_code(
-    item: Vc<Box<dyn EcmascriptChunkItem>>,
-    async_module_info: Option<Vc<AsyncModuleInfo>>,
-) -> Result<Vc<Code>> {
-    Ok(
-        match item
-            .content_with_async_module_info(async_module_info)
-            .module_factory()
-            .resolve()
-            .await
-        {
-            Ok(factory) => factory,
-            Err(error) => {
-                let id = item.id().to_string().await;
-                let id = id.as_ref().map_or_else(|_| "unknown", |id| &**id);
-                let error = error.context(format!(
-                    "An error occurred while generating the chunk item {}",
-                    id
-                ));
-                let error_message = format!("{}", PrettyPrintError(&error));
-                let js_error_message = serde_json::to_string(&error_message)?;
-                CodeGenerationIssue {
-                    severity: IssueSeverity::Error.resolved_cell(),
-                    path: item.asset_ident().path().to_resolved().await?,
-                    title: StyledString::Text("Code generation for chunk item errored".into())
-                        .resolved_cell(),
-                    message: StyledString::Text(error_message.into()).resolved_cell(),
-                }
-                .resolved_cell()
-                .emit();
-                let mut code = CodeBuilder::default();
-                code += "(() => {{\n\n";
-                writeln!(code, "throw new Error({error});", error = &js_error_message)?;
-                code += "\n}})";
-                code.build().cell()
-            }
-        },
-    )
 }
