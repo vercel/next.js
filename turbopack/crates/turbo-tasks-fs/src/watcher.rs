fa2ccaf5f8e7678cd51628ee9c3b04ed3a5c0688
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-14 10:04:04 +0100

    [Turbopack] only invalidate fs writes with a different content (#75936)
    ### What?
    
    There are cases where two write tasks write to the same file. This can happen e. g. when two different image source files with the same content are used and end up with the same content hashed filename.
    
    But since dependency tracking is disabled for one off builds, this results in an invalidation of the first write task when the second write task effect is executed. And an invalidation will panic in dependency-tracking-disabled mode.
    
    This change only invalidates other write tasks when they write a different content and keeps writes with the same content intact.
    
    This should avoid the panic as no task is invalidated.

--- turbopack/crates/turbo-tasks-fs/src/watcher.rs
+++ turbopack/crates/turbo-tasks-fs/src/watcher.rs
@@ -22,6 +22,7 @@ use turbo_tasks::{spawn_thread, Invalidator};
 use crate::{
     format_absolute_fs_path,
     invalidation::{WatchChange, WatchStart},
+    invalidator_map::WriteContent,
     path_to_key, DiskFileSystemInner,
 };
 
@@ -192,7 +193,7 @@ impl DiskWatcher {
                         .into_par_iter()
                         .map(move |i| (reason.clone(), i))
                 })
-                .for_each(|(reason, invalidator)| {
+                .for_each(|(reason, (invalidator, _))| {
                     let _span = span.clone().entered();
                     let _guard = handle.enter();
                     invalidator.invalidate_with_reason(reason)
@@ -202,7 +203,7 @@ impl DiskWatcher {
                     let _span = span.clone().entered();
                     invalidators.into_par_iter().map(move |i| i)
                 })
-                .for_each(|invalidator| {
+                .for_each(|(invalidator, _)| {
                     let _span = span.clone().entered();
                     let _guard = handle.enter();
                     invalidator.invalidate()
@@ -459,7 +460,7 @@ fn invalidate(
 fn invalidate_path(
     inner: &DiskFileSystemInner,
     report_invalidation_reason: bool,
-    invalidator_map: &mut FxHashMap<String, FxHashSet<Invalidator>>,
+    invalidator_map: &mut FxHashMap<String, FxHashMap<Invalidator, Option<WriteContent>>>,
     paths: impl Iterator<Item = PathBuf>,
 ) {
     for path in paths {
@@ -467,7 +468,7 @@ fn invalidate_path(
         if let Some(invalidators) = invalidator_map.remove(&key) {
             invalidators
                 .into_iter()
-                .for_each(|i| invalidate(inner, report_invalidation_reason, &path, i));
+                .for_each(|(i, _)| invalidate(inner, report_invalidation_reason, &path, i));
         }
     }
 }
@@ -475,7 +476,7 @@ fn invalidate_path(
 fn invalidate_path_and_children_execute(
     inner: &DiskFileSystemInner,
     report_invalidation_reason: bool,
-    invalidator_map: &mut FxHashMap<String, FxHashSet<Invalidator>>,
+    invalidator_map: &mut FxHashMap<String, FxHashMap<Invalidator, Option<WriteContent>>>,
     paths: impl Iterator<Item = PathBuf>,
 ) {
     for path in paths {
@@ -483,7 +484,7 @@ fn invalidate_path_and_children_execute(
         for (_, invalidators) in invalidator_map.extract_if(|key, _| key.starts_with(&path_key)) {
             invalidators
                 .into_iter()
-                .for_each(|i| invalidate(inner, report_invalidation_reason, &path, i));
+                .for_each(|(i, _)| invalidate(inner, report_invalidation_reason, &path, i));
         }
     }
 }
