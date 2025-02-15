0ec9937f8c3c71659b3e4435a83c57564e43a05f
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-02-15 21:31:44 +0100

    [dev-overlay] Stop hiding `rsc:` protocol
    It's indicative that there's a sourcemapping issue.

--- packages/next/src/client/components/react-dev-overlay/internal/helpers/webpack-module-path.ts
+++ packages/next/src/client/components/react-dev-overlay/internal/helpers/webpack-module-path.ts
@@ -1,9 +1,6 @@
 const replacementRegExes = [
-  /^(rsc:\/\/React\/[^/]+\/)/,
   /^webpack-internal:\/\/\/(\([\w-]+\)\/)?/,
   /^(webpack:\/\/\/|webpack:\/\/(_N_E\/)?)(\([\w-]+\)\/)?/,
-  /\?\w+(\?\d+)?$/, // React replay error query param, .e.g. ?c69d?0, ?c69d
-  /\?\d+$/, // React's fakeFunctionIdx query param
 ]
 
 export function isWebpackInternalResource(file: string) {
@@ -20,8 +17,6 @@ export function isWebpackInternalResource(file: string) {
  * Format the webpack internal id to original file path
  *
  * webpack-internal:///./src/hello.tsx => ./src/hello.tsx
- * rsc://React/Server/webpack-internal:///(rsc)/./src/hello.tsx?42 => ./src/hello.tsx
- * rsc://React/Server/webpack:///app/indirection.tsx?14cb?0 => app/indirection.tsx
  * webpack://_N_E/./src/hello.tsx => ./src/hello.tsx
  * webpack://./src/hello.tsx => ./src/hello.tsx
  * webpack:///./src/hello.tsx => ./src/hello.tsx
