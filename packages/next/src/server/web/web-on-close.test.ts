ec431d7abfcd2c51573b53b2b8bc6ba8ea15f604
Janka Uryga <lolzatu2@gmail.com>
2025-02-17 16:14:46 +0100

    test: assert that errors/cancellation are forwarded correctly

--- packages/next/src/server/web/web-on-close.test.ts
+++ packages/next/src/server/web/web-on-close.test.ts
@@ -19,6 +19,11 @@ describe('trackStreamConsumed', () => {
 
     controller.close()
 
+    await expect(reader.read()).resolves.toEqual({
+      done: true,
+      value: undefined,
+    })
+
     await endPromise.promise
     expect(onEnd).toHaveBeenCalledTimes(1)
   })
@@ -38,7 +43,11 @@ describe('trackStreamConsumed', () => {
     await reader.read()
     expect(onEnd).not.toHaveBeenCalled()
 
-    controller.error(new Error('kaboom'))
+    const error = new Error('kaboom')
+    controller.error(error)
+
+    // if the underlying stream errors, we should error as well
+    await expect(reader.read()).rejects.toThrow(error)
 
     await endPromise.promise
     expect(onEnd).toHaveBeenCalledTimes(1)
@@ -48,8 +57,11 @@ describe('trackStreamConsumed', () => {
     const endPromise = new DetachedPromise<void>()
     const onEnd = jest.fn(endPromise.resolve)
 
+    const cancelledPromise = new DetachedPromise<unknown>()
+    const onCancel = jest.fn(cancelledPromise.resolve)
+
     const { stream: inputStream, controller } =
-      readableStreamWithController<string>()
+      readableStreamWithController<string>(onCancel)
     const trackedStream = trackStreamConsumed(inputStream, onEnd)
 
     const reader = trackedStream.getReader()
@@ -59,17 +71,35 @@ describe('trackStreamConsumed', () => {
     await reader.read()
     expect(onEnd).not.toHaveBeenCalled()
 
-    await reader.cancel()
+    const cancellationReason = new Error('cancelled')
+    await reader.cancel(cancellationReason)
+
+    // from a reader's perspective, a cancelled stream behaves like it's done
+    // (which is a bit weird honestly?)
+    await expect(reader.read()).resolves.toEqual({
+      done: true,
+      value: undefined,
+    })
+
     await endPromise.promise
     expect(onEnd).toHaveBeenCalledTimes(1)
+
+    //  the cancellation should propagate to back to the underlying stream
+    await cancelledPromise.promise
+    expect(onCancel).toHaveBeenCalledWith(cancellationReason)
   })
 })
 
-function readableStreamWithController<TChunk>() {
+function readableStreamWithController<TChunk>(
+  onCancel?: (reason: unknown) => void
+) {
   let controller: ReadableStreamDefaultController<TChunk> = undefined!
   const stream = new ReadableStream<TChunk>({
     start(_controller) {
       controller = _controller
+    },
+    cancel(reason) {
+      onCancel?.(reason)
     },
   })
   return { controller, stream }
