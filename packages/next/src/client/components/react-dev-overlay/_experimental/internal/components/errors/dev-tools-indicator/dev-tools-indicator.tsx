b30743aaa84f2ec684fc9d6a552a57a94c85a376
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-19 01:44:21 +0900

    remove external icon
    Co-authored-by: Zack Tanner <1939140+ztanner@users.noreply.github.com>

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/dev-tools-indicator/dev-tools-indicator.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/dev-tools-indicator/dev-tools-indicator.tsx
@@ -307,7 +307,7 @@ function DevToolsPopover({
                 <MenuItem
                   index={1}
                   label="Try Turbopack"
-                  value={<ExternalIcon />}
+                  value={isTurbopack ? 'Enabled' : 'Disabled'}
                   onClick={() => setIsTurbopackInfoOpen(true)}
                 />
               )}
@@ -478,26 +478,6 @@ function useClickOutside(
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
-
-function ExternalIcon() {
-  return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      role="img"
-      aria-label="External link"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M13.5 10.25V13.25C13.5 13.3881 13.3881 13.5 13.25 13.5H2.75C2.61193 13.5 2.5 13.3881 2.5 13.25L2.5 2.75C2.5 2.61193 2.61193 2.5 2.75 2.5H5.75H6.5V1H5.75H2.75C1.7835 1 1 1.7835 1 2.75V13.25C1 14.2165 1.7835 15 2.75 15H13.25C14.2165 15 15 14.2165 15 13.25V10.25V9.5H13.5V10.25ZM9 1H9.75H14.2495C14.6637 1 14.9995 1.33579 14.9995 1.75V6.25V7H13.4995V6.25V3.56066L8.53033 8.52978L8 9.06011L6.93934 7.99945L7.46967 7.46912L12.4388 2.5H9.75H9V1Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
 
 export const DEV_TOOLS_INDICATOR_STYLES = css`
   .dev-tools-indicator-menu {
