ad53e4e8ae1df15171bda21d9e147c9d003dfada
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-19 12:13:25 +0100

    update snapshots

--- turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/dynamic-import/output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_lib_44a9b7f1.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/dynamic-import/output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_lib_44a9b7f1.js
@@ -6,7 +6,7 @@ var { g: global, d: __dirname } = __turbopack_context__;
 {
 __turbopack_context__.v((parentImport) => {
     return Promise.all([
-  "output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_lib_8debd8f1.js"
+  "output/4c35f_tests_snapshot_basic-tree-shake_dynamic-import_input_lib_f3b03c2b.js"
 ].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
         return parentImport("[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/dynamic-import/input/lib.js [test] (ecmascript)");
     });
---
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules/output/4e721_crates_turbopack-tests_tests_snapshot_css_css-modules_input_index_87255495.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4e721_crates_turbopack-tests_tests_snapshot_css_css-modules_input_index_87255495.js",
+    {},
+    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_css_css-modules_input_style_module_css_4e2d6e50._.js","output/turbopack_crates_turbopack-tests_tests_snapshot_css_css-modules_input_8f2e7f32._.js",{"path":"output/b1abf_turbopack-tests_tests_snapshot_css_css-modules_input_style_module_76a2527b.css","included":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules/input/style.module.css [test] (css)"],"moduleChunks":["output/b1abf_turbopack-tests_tests_snapshot_css_css-modules_input_style_module_7d7e1c0b.css"]}],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
