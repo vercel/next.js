458a40977a198c2b2ea07cc75fb6efbdda79a492
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-19 18:42:22 +0100

    Never send `stale-while-revalidate` without a time value
    This would be malformed.

--- packages/next/src/server/lib/cache-control.ts
+++ packages/next/src/server/lib/cache-control.ts
@@ -19,17 +19,17 @@ export function getCacheControlHeader({
   expire,
 }: CacheControl): string {
   const swrHeader =
-    typeof revalidate === 'number' && expire !== undefined
-      ? revalidate >= expire
-        ? ''
-        : `stale-while-revalidate=${expire - revalidate}`
-      : 'stale-while-revalidate'
+    typeof revalidate === 'number' &&
+    expire !== undefined &&
+    revalidate < expire
+      ? `, stale-while-revalidate=${expire - revalidate}`
+      : ''
 
   if (revalidate === 0) {
     return 'private, no-cache, no-store, max-age=0, must-revalidate'
   } else if (typeof revalidate === 'number') {
-    return `s-maxage=${revalidate}, ${swrHeader}`
+    return `s-maxage=${revalidate}${swrHeader}`
   }
 
-  return `s-maxage=${CACHE_ONE_YEAR}, ${swrHeader}`
+  return `s-maxage=${CACHE_ONE_YEAR}${swrHeader}`
 }
