4fc56c47dc892a1faa8f0210e2a90b2ca3abc627
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-19 10:49:12 +0100

    refactor url references from css

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/imports/ignore-comments/output/b1abf_turbopack-tests_tests_snapshot_imports_ignore-comments_input_index_b530b1.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/b1abf_turbopack-tests_tests_snapshot_imports_ignore-comments_input_index_b530b1.js",
+    {},
+    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_imports_ignore-comments_input_vercel_mjs_f0a767._.js","output/4e721_crates_turbopack-tests_tests_snapshot_imports_ignore-comments_input_e18491._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/imports/ignore-comments/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
