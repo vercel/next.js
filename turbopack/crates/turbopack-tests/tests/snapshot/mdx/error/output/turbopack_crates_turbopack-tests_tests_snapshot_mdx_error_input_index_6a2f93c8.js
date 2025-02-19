c68dfe92878324039724ca0244685fd5a995cd1e
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-19 23:19:41 +0100

    update snapshots

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/mdx/error/output/turbopack_crates_turbopack-tests_tests_snapshot_mdx_error_input_index_6a2f93c8.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/turbopack_crates_turbopack-tests_tests_snapshot_mdx_error_input_index_6a2f93c8.js",
+    {},
+    {"otherChunks":["output/turbopack_crates_turbopack-tests_tests_snapshot_mdx_error_input_index_e6acdd0c.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/mdx/error/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
---
+++ turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/output/turbopack_crates_turbopack-tests_tests_snapshot_bff03a32._.js
@@ -0,0 +1,26 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/turbopack_crates_turbopack-tests_tests_snapshot_bff03a32._.js", {
+
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/input/index.js [test] (ecmascript)": ((__turbopack_context__) => {
+"use strict";
+
+var { g: global, d: __dirname } = __turbopack_context__;
+{
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$node_modules$2f40$swc$2f$helpers$2f$_$2f$_class_call_check$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/node_modules/@swc/helpers/_/_class_call_check.js [test] (ecmascript)");
+;
+;
+var Foo = function Foo() {
+    "use strict";
+    (0, __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$node_modules$2f40$swc$2f$helpers$2f$_$2f$_class_call_check$2e$js__$5b$test$5d$__$28$ecmascript$29$__["_"])(this, Foo);
+};
+console.log(Foo, [].includes("foo"));
+}}),
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/node_modules/@swc/helpers/_/_class_call_check.js [test] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, d: __dirname, m: module, e: exports } = __turbopack_context__;
+{
+"purposefully empty stub";
+"@swc/helpers/_/_class_call_check.js";
+}}),
+}]);
+
+//# sourceMappingURL=turbopack_crates_turbopack-tests_tests_snapshot_bff03a32._.js.map
