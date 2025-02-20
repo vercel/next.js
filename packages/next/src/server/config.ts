3ee7e38953166ec5ec6c5e2accbbda00a45b9626
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-20 15:43:37 +0900

    update deprecation warning

--- packages/next/src/server/config.ts
+++ packages/next/src/server/config.ts
@@ -469,14 +469,14 @@ function assignDefaults(
   warnOptionHasBeenDeprecated(
     result,
     'devIndicators.appIsrStatus',
-    `\`devIndicators.appIsrStatus\` is no longer needed and is enabled by default. You can remove it from ${configFileName}.`,
+    `\`devIndicators.appIsrStatus\` is deprecated and no longer configurable. Please remove it from ${configFileName}.`,
     silent
   )
 
   warnOptionHasBeenDeprecated(
     result,
     'devIndicators.buildActivity',
-    `\`devIndicators.buildActivity\` is no longer needed and is enabled by default. You can remove it from ${configFileName}.`,
+    `\`devIndicators.buildActivity\` is deprecated and no longer configurable. Please remove it from ${configFileName}.`,
     silent
   )
 
