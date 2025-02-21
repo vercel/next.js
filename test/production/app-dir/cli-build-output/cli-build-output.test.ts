8f76b9a14d6558c27971f413e3ef286a5a0240a4
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-21 23:55:31 +0100

    Remove obsolete differentiation for Turbopack

--- test/production/app-dir/cli-build-output/cli-build-output.test.ts
+++ test/production/app-dir/cli-build-output/cli-build-output.test.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 describe('cli-build-output', () => {
   describe('with mixed static and dynamic pages and app router routes', () => {
-    const { next, isTurbopack } = nextTestSetup({
+    const { next } = nextTestSetup({
       files: path.join(__dirname, 'fixtures/mixed'),
       skipStart: true,
       env: {
@@ -19,72 +19,40 @@ describe('cli-build-output', () => {
 
       // TODO: Fix double-listing of the /ppr/[slug] fallback.
 
-      if (isTurbopack) {
-        expect(getTreeView(next.cliOutput)).toMatchInlineSnapshot(`
-         "Route (app)                             Size   First Load JS
-         ┌ ○ /_not-found                         42 kB          42 kB
-         ├ ƒ /api                                42 kB          42 kB
-         ├ ○ /api/force-static                   42 kB          42 kB
-         ├ ○ /app-static                         42 kB          42 kB
-         ├ ○ /cache-life                         42 kB          42 kB
-         ├ ƒ /dynamic                            42 kB          42 kB
-         ├ ◐ /ppr/[slug]                         42 kB          42 kB
-         ├   ├ /ppr/[slug]
-         ├   ├ /ppr/[slug]
-         ├   ├ /ppr/days
-         ├   └ /ppr/weeks
-         └ ○ /revalidate                         42 kB          42 kB
-         + First Load JS shared by all           42 kB
+      expect(getTreeView(next.cliOutput)).toMatchInlineSnapshot(`
+       "Route (app)                             Size   First Load JS
+       ┌ ○ /_not-found                         42 kB          42 kB
+       ├ ƒ /api                                42 kB          42 kB
+       ├ ○ /api/force-static                   42 kB          42 kB
+       ├ ○ /app-static                         42 kB          42 kB
+       ├ ○ /cache-life                         42 kB          42 kB
+       ├ ƒ /dynamic                            42 kB          42 kB
+       ├ ◐ /ppr/[slug]                         42 kB          42 kB
+       ├   ├ /ppr/[slug]
+       ├   ├ /ppr/[slug]
+       ├   ├ /ppr/days
+       ├   └ /ppr/weeks
+       └ ○ /revalidate                         42 kB          42 kB
+       + First Load JS shared by all           42 kB
 
-         Route (pages)                           Size   First Load JS
-         ┌ ƒ /api/hello                          42 kB          42 kB
-         ├ ● /gsp-revalidate (ISR: 300 Seconds)  42 kB          42 kB
-         ├ ƒ /gssp                               42 kB          42 kB
-         └ ○ /static                             42 kB          42 kB
-         + First Load JS shared by all           42 kB
+       Route (pages)                           Size   First Load JS
+       ┌ ƒ /api/hello                          42 kB          42 kB
+       ├ ● /gsp-revalidate (ISR: 300 Seconds)  42 kB          42 kB
+       ├ ƒ /gssp                               42 kB          42 kB
+       └ ○ /static                             42 kB          42 kB
+       + First Load JS shared by all           42 kB
 
-         ○  (Static)             prerendered as static content
-         ●  (SSG)                prerendered as static HTML (uses generateStaticParams)
-            (ISR)                incremental static regeneration (uses revalidate in generateStaticParams)
-         ◐  (Partial Prerender)  prerendered as static HTML with dynamic server-streamed content
-         ƒ  (Dynamic)            server-rendered on demand"
-        `)
-      } else {
-        expect(getTreeView(next.cliOutput)).toMatchInlineSnapshot(`
-         "Route (app)                             Size   First Load JS
-         ┌ ○ /_not-found                         42 kB          42 kB
-         ├ ƒ /api                                42 kB          42 kB
-         ├ ○ /api/force-static                   42 kB          42 kB
-         ├ ○ /app-static                         42 kB          42 kB
-         ├ ○ /cache-life                         42 kB          42 kB
-         ├ ƒ /dynamic                            42 kB          42 kB
-         ├ ◐ /ppr/[slug]                         42 kB          42 kB
-         ├   ├ /ppr/[slug]
-         ├   ├ /ppr/[slug]
-         ├   ├ /ppr/days
-         ├   └ /ppr/weeks
-         └ ○ /revalidate                         42 kB          42 kB
-         + First Load JS shared by all           42 kB
-
-         Route (pages)                           Size   First Load JS
-         ┌ ƒ /api/hello                          42 kB          42 kB
-         ├ ● /gsp-revalidate (ISR: 300 Seconds)  42 kB          42 kB
-         ├ ƒ /gssp                               42 kB          42 kB
-         └ ○ /static                             42 kB          42 kB
-         + First Load JS shared by all           42 kB
-
-         ○  (Static)             prerendered as static content
-         ●  (SSG)                prerendered as static HTML (uses generateStaticParams)
-            (ISR)                incremental static regeneration (uses revalidate in generateStaticParams)
-         ◐  (Partial Prerender)  prerendered as static HTML with dynamic server-streamed content
-         ƒ  (Dynamic)            server-rendered on demand"
-        `)
-      }
+       ○  (Static)             prerendered as static content
+       ●  (SSG)                prerendered as static HTML (uses generateStaticParams)
+          (ISR)                incremental static regeneration (uses revalidate in generateStaticParams)
+       ◐  (Partial Prerender)  prerendered as static HTML with dynamic server-streamed content
+       ƒ  (Dynamic)            server-rendered on demand"
+      `)
     })
   })
 
   describe('with only a few static routes', () => {
-    const { next, isTurbopack } = nextTestSetup({
+    const { next } = nextTestSetup({
       files: path.join(__dirname, 'fixtures/minimal-static'),
       skipStart: true,
       env: {
@@ -95,33 +63,18 @@ describe('cli-build-output', () => {
     beforeAll(() => next.build())
 
     it('should show info about prerendered routes in a compact tree view', async () => {
-      if (isTurbopack) {
-        expect(getTreeView(next.cliOutput)).toMatchInlineSnapshot(`
-         "Route (app)                    Size   First Load JS
-         ┌ ○ /                          42 kB          42 kB
-         └ ○ /_not-found                42 kB          42 kB
-         + First Load JS shared by all  42 kB
+      expect(getTreeView(next.cliOutput)).toMatchInlineSnapshot(`
+       "Route (app)                    Size   First Load JS
+       ┌ ○ /                          42 kB          42 kB
+       └ ○ /_not-found                42 kB          42 kB
+       + First Load JS shared by all  42 kB
 
-         Route (pages)                  Size   First Load JS
-         ─ ○ /static                    42 kB          42 kB
-         + First Load JS shared by all  42 kB
+       Route (pages)                  Size   First Load JS
+       ─ ○ /static                    42 kB          42 kB
+       + First Load JS shared by all  42 kB
 
-         ○  (Static)  prerendered as static content"
-        `)
-      } else {
-        expect(getTreeView(next.cliOutput)).toMatchInlineSnapshot(`
-         "Route (app)                    Size   First Load JS
-         ┌ ○ /                          42 kB          42 kB
-         └ ○ /_not-found                42 kB          42 kB
-         + First Load JS shared by all  42 kB
-
-         Route (pages)                  Size   First Load JS
-         ─ ○ /static                    42 kB          42 kB
-         + First Load JS shared by all  42 kB
-
-         ○  (Static)  prerendered as static content"
-        `)
-      }
+       ○  (Static)  prerendered as static content"
+      `)
     })
   })
 })
