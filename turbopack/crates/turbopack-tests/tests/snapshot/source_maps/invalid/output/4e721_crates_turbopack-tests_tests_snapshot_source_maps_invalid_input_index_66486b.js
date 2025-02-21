1f98765c2755496c777f206a548b0f790b8f3de9
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-21 12:54:01 +0100

    Update outdated snapshots (#76318)

--- turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_66486b.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_66486b.js",
-    {},
-    {"otherChunks":["output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
