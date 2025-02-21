3e8a2bfd6da7a90b3bf01bf787d2cd3e2ac859f3
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-21 11:17:26 +0100

    Update outdated snapshots
    The snapshot files were added in #76215, and became outdated when #76245
    was merged in the meantime.

--- turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e27.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e27.js
@@ -1,4 +1,4 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e.js", {
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e27.js", {
 
 "[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/invalid/input/index.js [test] (ecmascript)": (function(__turbopack_context__) {
 
@@ -9,4 +9,4 @@ console.log("test") //# sourceMappingURL=index.js.map
 }}),
 }]);
 
-//# sourceMappingURL=4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e.js.map
+//# sourceMappingURL=4e721_crates_turbopack-tests_tests_snapshot_source_maps_invalid_input_index_fc734e27.js.map
