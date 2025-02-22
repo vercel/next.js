75efbbacacb9deffccec48c65a1af7b2b44751f6
Jiachi Liu <inbox@huozhi.im>
2025-02-22 20:03:47 +0100

    disable based on next config

--- packages/next/src/export/worker.ts
+++ packages/next/src/export/worker.ts
@@ -257,7 +257,7 @@ async function exportPageImpl(
   // when it's available. When we're building the PPR rendering result, we don't need to rely
   // on the user agent. The result can be determined to serve streaming on infrastructure level.
   const serveStreamingMetadata = Boolean(
-    isRoutePPREnabled && input.renderOpts.serveStreamingMetadata
+    isRoutePPREnabled && input.streamingMetadata
   )
 
   const renderOpts: WorkerRenderOpts = {
@@ -425,6 +425,12 @@ export async function exportPages(
             enableExperimentalReact: needsExperimentalReact(nextConfig),
             sriEnabled: Boolean(nextConfig.experimental.sri?.algorithm),
             buildId: input.buildId,
+            streamingMetadata:
+              // Disable streaming metadata when dynamic IO is enabled.
+              // FIXME: remove dynamic IO guard once we fixed the dynamic indicator case.
+              // test/e2e/app-dir/dynamic-io/dynamic-io.test.ts - should not have static indicator on not-found route
+              !nextConfig.experimental.dynamicIO &&
+              !!nextConfig.experimental.streamingMetadata,
           }),
           // If exporting the page takes longer than the timeout, reject the promise.
           new Promise((_, reject) => {
