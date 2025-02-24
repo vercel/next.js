4cfa1d312a69bb30f94948e4bde13a9d5726119a
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-24 15:37:46 +0100

    update snapshots

--- turbopack/crates/turbopack-tests/tests/snapshot/css/css/output/08d19_foo_style_css_7d60f23f._.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/css/output/08d19_foo_style_css_7d60f23f._.js
@@ -1,4 +1,4 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/08d19_foo_style_css_1f8924d2._.js", {
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/08d19_foo_style_css_7d60f23f._.js", {
 
 "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/css/input/node_modules/foo/style.css [test] (css, async loader)": ((__turbopack_context__) => {
 
