27d45337b8078b6d349aabac5efcb4336d749ea5
MayuriPapat25 <mayuri.papat@qed42.com>
2025-02-25 16:46:18 +0530

    Update with-urql example to utilize the app router.

--- examples/with-urql/app/page.tsx
+++ examples/with-urql/app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { getPokemons } from "../graphql/getPokemons";
 
-export default function Home({ pokemons }) {
+export default async function Home() {
+  const pokemons = await getPokemons();
+
   return (
     <ul>
       {pokemons.map((pokemon) => (
         <li key={pokemon.name}>
-          <Link as={`/pokemon/${pokemon.name}`} href="/pokemon/[name]">
+          <Link href={`/pokemon/${pokemon.name}`}>
             <h2 style={{ textTransform: "capitalize" }}>{pokemon.name}</h2>
             <img src={pokemon.image} alt={`${pokemon.name} picture`} />
           </Link>
@@ -15,12 +17,3 @@ export default function Home({ pokemons }) {
     </ul>
   );
 }
-
-export const getStaticProps = async () => {
-  const pokemons = await getPokemons();
-  return {
-    props: {
-      pokemons,
-    },
-  };
-};
