26fd014797a905e80c90e9980b28d51ad3d4d835
Jiachi Liu <inbox@huozhi.im>
2025-02-25 11:01:25 +0100

    add ts hint and explanation

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -1071,6 +1071,9 @@ export interface NextConfig extends Record<string, any> {
   /**
    * User Agent of bots that can handle streaming metadata.
    * Besides the default behavior, Next.js act differently on serving metadata to bots based on their capability.
+   *
+   * @default
+   * /Mediapartners-Google|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview/
    */
   htmlLimitedBots?: RegExp
 }
