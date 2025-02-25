61617156de5de10916977f8a37a3029043f961b5
MayuriPapat25 <mayuri.papat@qed42.com>
2025-02-25 17:04:47 +0530

    Update package.json in with-urql example.

--- examples/with-urql/app/pokemon/[name]/page.tsx
+++ examples/with-urql/app/pokemon/[name]/page.tsx
@@ -1,16 +1,7 @@
 import { getPokemon as getPokemonByName } from "../../../graphql/getPokemon";
 
 export default async function PokemonPage({ params }: { params: { name?: string } }) {
-  if (!params?.name) {
-    return <p>Loading...</p>; 
-  }
-
   const pokemon = await getPokemonByName(params.name);
-
-  if (!pokemon) {
-    return <p>Pokémon not found!</p>; 
-  }
-
   return (
     <div>
       <h1 style={{ textTransform: "capitalize" }}>{pokemon.name}</h1>
