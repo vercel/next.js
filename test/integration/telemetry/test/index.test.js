ddcffba05d3cbaa88337315986f6bfdf2be65b59
Tim Neutkens <tim@timneutkens.nl>
2025-02-25 20:12:34 +0100

    Add test

--- test/integration/telemetry/test/index.test.js
+++ test/integration/telemetry/test/index.test.js
@@ -9,6 +9,7 @@ import {
   killApp,
   waitFor,
   nextBuild,
+  findAllTelemetryEvents,
 } from 'next-test-utils'
 
 const appDir = path.join(__dirname, '..')
@@ -355,6 +356,21 @@ describe('Telemetry CLI', () => {
         expect(event).toMatch(/"buildTarget": "default"/)
         expect(event).toMatch(/"hasWebpackConfig": true/)
         expect(event).toMatch(/"hasBabelConfig": false/)
+
+        // Check if features are detected correctly when custom webpack config exists
+        const featureUsageEvents = findAllTelemetryEvents(
+          stderr,
+          'NEXT_BUILD_FEATURE_USAGE'
+        )
+        expect(featureUsageEvents).toContainEqual({
+          featureName: 'build-lint',
+          invocationCount: 1,
+        })
+
+        expect(featureUsageEvents).toContainEqual({
+          featureName: 'webpackPlugins',
+          invocationCount: 1,
+        })
       })
       it('detect static 404 correctly for `next build`', async () => {
         const { stderr } = await nextBuild(appDir, [], {
