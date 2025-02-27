220a7cf888fbd6aeaa25ff0e61f7f168d529b077
Hendrik Liebau <mail@hendrik-liebau.de>
2025-02-27 14:16:37 +0100

    Revert "Unflake `parallel-routes-revalidation` test"
    This reverts commit 7c3ef83fb6b19fc1e4bbfe5ad21804cbdb452f6c.

--- test/lib/browsers/playwright.ts
+++ test/lib/browsers/playwright.ts
@@ -302,19 +302,9 @@ export class Playwright extends BrowserInterface {
       await page.goForward(options)
     })
   }
-  refresh({
-    waitHydration,
-    retryWaitHydration,
-  }: {
-    waitHydration?: boolean
-    retryWaitHydration?: boolean
-  } = {}) {
+  refresh() {
     return this.chain(async () => {
       await page.reload()
-
-      if (waitHydration) {
-        this.waitForHydration({ retryWaitHydration })
-      }
     })
   }
   setDimensions({ width, height }: { height: number; width: number }) {
@@ -526,59 +516,6 @@ export class Playwright extends BrowserInterface {
     return this.chain(() => {
       return page.waitForLoadState('networkidle')
     })
-  }
-
-  async waitForHydration({
-    retryWaitHydration = false,
-  }: { retryWaitHydration?: boolean } = {}): Promise<void> {
-    const fullUrl = page.url()
-    console.log(`\n> Waiting hydration for ${fullUrl}\n`)
-
-    const checkHydrated = async () => {
-      await this.evalAsync(function () {
-        var callback = arguments[arguments.length - 1]
-
-        // if it's not a Next.js app return
-        if (
-          !document.documentElement.innerHTML.includes('__NEXT_DATA__') &&
-          // @ts-ignore next exists on window if it's a Next.js page.
-          typeof ((window as any).next && (window as any).next.version) ===
-            'undefined'
-        ) {
-          console.log('Not a next.js page, resolving hydrate check')
-          callback()
-        }
-
-        // TODO: should we also ensure router.isReady is true
-        // by default before resolving?
-        if ((window as any).__NEXT_HYDRATED) {
-          console.log('Next.js page already hydrated')
-          callback()
-        } else {
-          var timeout = setTimeout(callback, 10 * 1000)
-          ;(window as any).__NEXT_HYDRATED_CB = function () {
-            clearTimeout(timeout)
-            console.log('Next.js hydrate callback fired')
-            callback()
-          }
-        }
-      })
-    }
-
-    try {
-      await checkHydrated()
-    } catch (err) {
-      if (retryWaitHydration) {
-        // re-try in case the page reloaded during check
-        await new Promise((resolve) => setTimeout(resolve, 2000))
-        await checkHydrated()
-      } else {
-        console.error('failed to check hydration')
-        throw err
-      }
-    }
-
-    console.log(`\n> Hydration complete for ${fullUrl}\n`)
   }
 
   locateRedbox(): Locator {
--- test/lib/next-webdriver.ts
+++ test/lib/next-webdriver.ts
@@ -145,7 +145,53 @@ export default async function webdriver(
 
   // Wait for application to hydrate
   if (waitHydration) {
-    await browser.waitForHydration({ retryWaitHydration })
+    console.log(`\n> Waiting hydration for ${fullUrl}\n`)
+
+    const checkHydrated = async () => {
+      await browser.evalAsync(function () {
+        var callback = arguments[arguments.length - 1]
+
+        // if it's not a Next.js app return
+        if (
+          !document.documentElement.innerHTML.includes('__NEXT_DATA__') &&
+          // @ts-ignore next exists on window if it's a Next.js page.
+          typeof ((window as any).next && (window as any).next.version) ===
+            'undefined'
+        ) {
+          console.log('Not a next.js page, resolving hydrate check')
+          callback()
+        }
+
+        // TODO: should we also ensure router.isReady is true
+        // by default before resolving?
+        if ((window as any).__NEXT_HYDRATED) {
+          console.log('Next.js page already hydrated')
+          callback()
+        } else {
+          var timeout = setTimeout(callback, 10 * 1000)
+          ;(window as any).__NEXT_HYDRATED_CB = function () {
+            clearTimeout(timeout)
+            console.log('Next.js hydrate callback fired')
+            callback()
+          }
+        }
+      })
+    }
+
+    try {
+      await checkHydrated()
+    } catch (err) {
+      if (retryWaitHydration) {
+        // re-try in case the page reloaded during check
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+        await checkHydrated()
+      } else {
+        console.error('failed to check hydration')
+        throw err
+      }
+    }
+
+    console.log(`\n> Hydration complete for ${fullUrl}\n`)
   }
 
   // This is a temporary workaround for turbopack starting watching too late.
