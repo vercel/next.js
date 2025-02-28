f828224a316d2f1813e44f78d1208b78a3f506d5
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-28 18:04:34 +0100

    improve cycle test case

--- turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_2167552f._.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_2167552f._.js
@@ -1,4 +1,4 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_af36f555._.js", {
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_2167552f._.js", {
 
 "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/1.css [test] (css, async loader)": ((__turbopack_context__) => {
 
@@ -75,6 +75,28 @@ var { g: global, d: __dirname } = __turbopack_context__;
 __turbopack_context__.v((parentImport) => {
     return Promise.all([
   {
+    "path": "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_99c586b3._.css",
+    "included": [
+      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/y.css [test] (css)",
+      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/x.css [test] (css)"
+    ],
+    "moduleChunks": [
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_y_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_x_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_x_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_y_7d7e1c0b.css"
+    ]
+  }
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {});
+});
+}}),
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/y.css [test] (css, async loader)": ((__turbopack_context__) => {
+
+var { g: global, d: __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  {
     "path": "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_5b2f9846._.css",
     "included": [
       "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/x.css [test] (css)",
@@ -90,23 +112,26 @@ __turbopack_context__.v((parentImport) => {
 ].map((chunk) => __turbopack_context__.l(chunk))).then(() => {});
 });
 }}),
-"[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/y.css [test] (css, async loader)": ((__turbopack_context__) => {
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/i.css [test] (css, async loader)": ((__turbopack_context__) => {
 
 var { g: global, d: __dirname } = __turbopack_context__;
 {
 __turbopack_context__.v((parentImport) => {
     return Promise.all([
   {
-    "path": "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_99c586b3._.css",
+    "path": "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_fba71ce2._.css",
     "included": [
-      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/y.css [test] (css)",
-      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/x.css [test] (css)"
+      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/k.css [test] (css)",
+      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/j.css [test] (css)",
+      "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/i.css [test] (css)"
     ],
     "moduleChunks": [
-      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_y_7d7e1c0b.css",
-      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_x_7d7e1c0b.css",
-      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_x_7d7e1c0b.css",
-      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_y_7d7e1c0b.css"
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_k_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_j_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_i_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_i_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_k_7d7e1c0b.css",
+      "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_j_7d7e1c0b.css"
     ]
   }
 ].map((chunk) => __turbopack_context__.l(chunk))).then(() => {});
--- turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_index_2ae5463a.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_index_2ae5463a.js",
-    {},
-    {"otherChunks":["output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_af36f555._.js","output/turbopack_crates_turbopack-tests_tests_snapshot_css_cycle_input_index_2a740bfb.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/cycle/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
