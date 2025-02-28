9fd1766f330444b91be2af900441c7e173906013
Andrew Clark <git@andrewclark.io>
2025-02-28 14:15:43 -0500

    [Segment Cache] Search param fallback handling (#75990)
    This implements search param handling in the Segment Cache.
    
    When a cache entry is fetched via PPR, search params are treated as
    dynamic data. We can share the same cache entry for every set of search
    params for a given page segment.
    
    By contrast, when a cache entry is fetched using a dynamic request (e.g.
    using `<Link prefetch={true}`), the result may vary by the search param
    values. So we must include the search params in the cache key.
    
    During a navigation, we will first check for the more specific cache
    entry (i.e. one that includes search params), and then fallback to the
    PPR version if no such entry exists.

--- packages/next/src/client/components/segment-cache-impl/cache-key.ts
+++ packages/next/src/client/components/segment-cache-impl/cache-key.ts
@@ -3,13 +3,17 @@ type Opaque<K, T> = T & { __brand: K }
 
 // Only functions in this module should be allowed to create CacheKeys.
 export type NormalizedHref = Opaque<'NormalizedHref', string>
+export type NormalizedSearch = Opaque<'NormalizedSearch', string>
 export type NormalizedNextUrl = Opaque<'NormalizedNextUrl', string>
 
 export type RouteCacheKey = Opaque<
   'RouteCacheKey',
   {
     href: NormalizedHref
+    search: NormalizedSearch
     nextUrl: NormalizedNextUrl | null
+
+    // TODO: Eventually the dynamic params will be added here, too.
   }
 >
 
@@ -18,20 +22,10 @@ export function createCacheKey(
   nextUrl: string | null
 ): RouteCacheKey {
   const originalUrl = new URL(originalHref)
-
-  // TODO: As of now, we never include search params in the cache key because
-  // per-segment prefetch requests are always static, and cannot contain search
-  // params. But to support <Link prefetch={true}>, we will sometimes populate
-  // the cache with dynamic data, so this will have to change.
-  originalUrl.search = ''
-
-  const normalizedHref = originalUrl.href as NormalizedHref
-  const normalizedNextUrl = nextUrl as NormalizedNextUrl | null
-
   const cacheKey = {
-    href: normalizedHref,
-    nextUrl: normalizedNextUrl,
+    href: originalHref as NormalizedHref,
+    search: originalUrl.search as NormalizedSearch,
+    nextUrl: nextUrl as NormalizedNextUrl | null,
   } as RouteCacheKey
-
   return cacheKey
 }
