6805a929e10e1fa53b99ee848ae5a63dc76c97b6
Tobias Koppers <tobias.koppers@googlemail.com>
2025-02-28 11:02:09 +0100

    more percision

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -3630,7 +3630,7 @@ function durationToString(compilerDuration: number) {
   let durationString
   if (compilerDuration > 120) {
     durationString = `${(compilerDuration / 60).toFixed(1)}min`
-  } else if (compilerDuration > 20) {
+  } else if (compilerDuration > 40) {
     durationString = `${compilerDuration.toFixed(0)}s`
   } else if (compilerDuration > 2) {
     durationString = `${compilerDuration.toFixed(1)}s`
