vercel/next.js

commit ce69888af1082aee5353c82f644dbc3adb9d0025

Reland "Middleware to use react-server condition" (#66534)

packages/next/src/build/entries.ts
@@ -838,8 +838,9 @@ export function finalizeEntrypoint({
     }
     case COMPILER_NAMES.edgeServer: {
       return {
-        layer:
-          isMiddlewareFilename(name) || isApi || isInstrumentation
+        layer: isApi
+          ? WEBPACK_LAYERS.api
+          : isMiddlewareFilename(name) || isInstrumentation
             ? WEBPACK_LAYERS.middleware
             : undefined,
         library: { name: ['_ENTRIES', `middleware_[name]`], type: 'assign' },

packages/next/src/build/webpack-config.ts
@@ -9,7 +9,7 @@ import { escapeStringRegexp } from '../shared/lib/escape-regexp'
 import { WEBPACK_LAYERS, WEBPACK_RESOURCE_QUERIES } from '../lib/constants'
 import type { WebpackLayerName } from '../lib/constants'
 import {
-  isWebpackAppLayer,
+  isWebpackBundledLayer,
   isWebpackClientOnlyLayer,
   isWebpackDefaultLayer,
   isWebpackServerOnlyLayer,
@@ -81,6 +81,7 @@ import {
   createRSCAliases,
   createNextApiEsmAliases,
   createAppRouterApiAliases,
+  createRSCRendererAliases,
 } from './create-compiler-aliases'
 import { hasCustomExportOutput } from '../export/utils'
 import { CssChunkingPlugin } from './webpack/plugins/css-chunking-plugin'
@@ -529,6 +530,7 @@ export default async function getBaseWebpackConfig(
     : []
 
   const instrumentLayerLoaders = [
+    'next-flight-loader',
     // When using Babel, we will have to add the SWC loader
     // as an additional pass to handle RSC correctly.
     // This will cause some performance overhead but
@@ -538,12 +540,13 @@ export default async function getBaseWebpackConfig(
   ].filter(Boolean)
 
   const middlewareLayerLoaders = [
+    'next-flight-loader',
     // When using Babel, we will have to use SWC to do the optimization
     // for middleware to tree shake the unused default optimized imports like "next/server".
     // This will cause some performance overhead but
     // acceptable as Babel will not be recommended.
     getSwcLoader({
-      serverComponents: false,
+      serverComponents: true,
       bundleLayer: WEBPACK_LAYERS.middleware,
     }),
     babelLoader,
@@ -592,13 +595,12 @@ export default async function getBaseWebpackConfig(
   // Loader for API routes needs to be differently configured as it shouldn't
   // have RSC transpiler enabled, so syntax checks such as invalid imports won't
   // be performed.
-  const apiRoutesLayerLoaders =
-    hasAppDir && useSWCLoader
-      ? getSwcLoader({
-          serverComponents: false,
-          bundleLayer: WEBPACK_LAYERS.api,
-        })
-      : defaultLoaders.babel
+  const apiRoutesLayerLoaders = useSWCLoader
+    ? getSwcLoader({
+        serverComponents: false,
+        bundleLayer: WEBPACK_LAYERS.api,
+      })
+    : defaultLoaders.babel
 
   const pageExtensions = config.pageExtensions
 
@@ -1304,7 +1306,7 @@ export default async function getBaseWebpackConfig(
                 test: /next[\\/]dist[\\/](esm[\\/])?server[\\/]route-modules[\\/]app-page[\\/]module/,
               },
               {
-                issuerLayer: isWebpackAppLayer,
+                issuerLayer: isWebpackBundledLayer,
                 resolve: {
                   alias: createNextApiEsmAliases(),
                 },
@@ -1460,8 +1462,11 @@ export default async function getBaseWebpackConfig(
               ...codeCondition,
               issuerLayer: WEBPACK_LAYERS.api,
               parser: {
-                // Switch back to normal URL handling
-                url: true,
+                // In Node.js, switch back to normal URL handling.
+                // In Edge runtime, we should disable parser.url handling in webpack so URLDependency is not added.
+                // Then there's browser code won't be injected into the edge runtime chunk.
+                // x-ref: https://github.com/webpack/webpack/blob/d9ce3b1f87e63c809d8a19bbd92257d65922e81f/lib/web/JsonpChunkLoadingRuntimeModule.js#L69
+                url: !isEdgeServer,
               },
               use: apiRoutesLayerLoaders,
             },
@@ -1469,11 +1474,23 @@ export default async function getBaseWebpackConfig(
               test: codeCondition.test,
               issuerLayer: WEBPACK_LAYERS.middleware,
               use: middlewareLayerLoaders,
+              resolve: {
+                mainFields: getMainField(compilerType, true),
+                conditionNames: reactServerCondition,
+                // Always use default channels when use installed react
+                alias: createRSCRendererAliases(''),
+              },
             },
             {
               test: codeCondition.test,
               issuerLayer: WEBPACK_LAYERS.instrument,
               use: instrumentLayerLoaders,
+              resolve: {
+                mainFields: getMainField(compilerType, true),
+                conditionNames: reactServerCondition,
+                // Always use default channels when use installed react
+                alias: createRSCRendererAliases(''),
+              },
             },
             ...(hasAppDir
               ? [

packages/next/src/build/webpack/plugins/middleware-plugin.ts
@@ -28,7 +28,10 @@ import type { Telemetry } from '../../../telemetry/storage'
 import { traceGlobals } from '../../../trace/shared'
 import { EVENT_BUILD_FEATURE_USAGE } from '../../../telemetry/events'
 import { normalizeAppPath } from '../../../shared/lib/router/utils/app-paths'
-import { INSTRUMENTATION_HOOK_FILENAME } from '../../../lib/constants'
+import {
+  INSTRUMENTATION_HOOK_FILENAME,
+  WEBPACK_LAYERS,
+} from '../../../lib/constants'
 import type { CustomRoutes } from '../../../lib/load-custom-routes'
 import { isInterceptionRouteRewrite } from '../../../lib/generate-interception-routes-rewrites'
 import { getDynamicCodeEvaluationError } from './wellknown-errors-plugin/parse-dynamic-code-evaluation-error'
@@ -272,7 +275,8 @@ function buildWebpackError({
 }
 
 function isInMiddlewareLayer(parser: webpack.javascript.JavascriptParser) {
-  return parser.state.module?.layer === 'middleware'
+  const layer = parser.state.module?.layer
+  return layer === WEBPACK_LAYERS.middleware || layer === WEBPACK_LAYERS.api
 }
 
 function isNodeJsModule(moduleName: string) {
@@ -849,7 +853,8 @@ export async function handleWebpackExternalForEdgeRuntime({
   getResolve: () => any
 }) {
   if (
-    contextInfo.issuerLayer === 'middleware' &&
+    (contextInfo.issuerLayer === WEBPACK_LAYERS.middleware ||
+      contextInfo.issuerLayer === WEBPACK_LAYERS.api) &&
     isNodeJsModule(request) &&
     !supportedEdgePolyfills.has(request)
   ) {

packages/next/src/lib/constants.ts
@@ -159,6 +159,11 @@ export type WebpackLayerName =
 const WEBPACK_LAYERS = {
   ...WEBPACK_LAYERS_NAMES,
   GROUP: {
+    builtinReact: [
+      WEBPACK_LAYERS_NAMES.reactServerComponents,
+      WEBPACK_LAYERS_NAMES.actionBrowser,
+      WEBPACK_LAYERS_NAMES.appMetadataRoute,
+    ],
     serverOnly: [
       WEBPACK_LAYERS_NAMES.reactServerComponents,
       WEBPACK_LAYERS_NAMES.actionBrowser,
@@ -174,7 +179,7 @@ const WEBPACK_LAYERS = {
       WEBPACK_LAYERS_NAMES.serverSideRendering,
       WEBPACK_LAYERS_NAMES.appPagesBrowser,
     ],
-    app: [
+    bundled: [
       WEBPACK_LAYERS_NAMES.reactServerComponents,
       WEBPACK_LAYERS_NAMES.actionBrowser,
       WEBPACK_LAYERS_NAMES.appMetadataRoute,

packages/next-swc/crates/next-api/src/app.rs
@@ -59,7 +59,7 @@ use turbopack_binding::{
         turbopack::{
             module_options::ModuleOptionsContext,
             resolve_options_context::ResolveOptionsContext,
-            transition::{ContextTransition, FullContextTransition},
+            transition::{ContextTransition, FullContextTransition, Transition},
             ModuleAssetContext,
         },
     },
@@ -118,7 +118,7 @@ impl AppProject {
     }
 }
 
-const ECMASCRIPT_CLIENT_TRANSITION_NAME: &str = "next-ecmascript-client-reference";
+pub(crate) const ECMASCRIPT_CLIENT_TRANSITION_NAME: &str = "next-ecmascript-client-reference";
 
 #[turbo_tasks::value_impl]
 impl AppProject {
@@ -166,7 +166,7 @@ impl AppProject {
     }
 
     #[turbo_tasks::function]
-    fn client_transition_name(self: Vc<Self>) -> Vc<RcStr> {
+    pub(crate) fn client_transition_name(self: Vc<Self>) -> Vc<RcStr> {
         Vc::cell(ECMASCRIPT_CLIENT_TRANSITION_NAME.into())
     }
 
@@ -271,14 +271,27 @@ impl AppProject {
     }
 
     #[turbo_tasks::function]
+    pub fn client_reference_transition(self: Vc<Self>) -> Vc<Box<dyn Transition>> {
+        Vc::upcast(NextEcmascriptClientReferenceTransition::new(
+            Vc::upcast(self.client_transition()),
+            self.ssr_transition(),
+        ))
+    }
+
+    #[turbo_tasks::function]
+    pub fn edge_client_reference_transition(self: Vc<Self>) -> Vc<Box<dyn Transition>> {
+        Vc::upcast(NextEcmascriptClientReferenceTransition::new(
+            Vc::upcast(self.client_transition()),
+            self.edge_ssr_transition(),
+        ))
+    }
+
+    #[turbo_tasks::function]
     fn rsc_module_context(self: Vc<Self>) -> Vc<ModuleAssetContext> {
         let transitions = [
             (
                 ECMASCRIPT_CLIENT_TRANSITION_NAME.into(),
-                Vc::upcast(NextEcmascriptClientReferenceTransition::new(
-                    Vc::upcast(self.client_transition()),
-                    self.ssr_transition(),
-                )),
+                self.client_reference_transition(),
             ),
             (
                 "next-dynamic".into(),
@@ -305,10 +318,7 @@ impl AppProject {
         let transitions = [
             (
                 ECMASCRIPT_CLIENT_TRANSITION_NAME.into(),
-                Vc::upcast(NextEcmascriptClientReferenceTransition::new(
-                    Vc::upcast(self.client_transition()),
-                    self.edge_ssr_transition(),
-                )),
+                self.edge_client_reference_transition(),
             ),
             (
                 "next-dynamic".into(),
@@ -338,10 +348,7 @@ impl AppProject {
         let transitions = [
             (
                 ECMASCRIPT_CLIENT_TRANSITION_NAME.into(),
-                Vc::upcast(NextEcmascriptClientReferenceTransition::new(
-                    Vc::upcast(self.client_transition()),
-                    self.ssr_transition(),
-                )),
+                self.client_reference_transition(),
             ),
             (
                 "next-dynamic".into(),
@@ -369,10 +376,7 @@ impl AppProject {
         let transitions = [
             (
                 ECMASCRIPT_CLIENT_TRANSITION_NAME.into(),
-                Vc::upcast(NextEcmascriptClientReferenceTransition::new(
-                    Vc::upcast(self.client_transition()),
-                    self.edge_ssr_transition(),
-                )),
+                self.edge_client_reference_transition(),
             ),
             (
                 "next-dynamic".into(),

packages/next-swc/crates/next-api/src/instrumentation.rs
@@ -6,9 +6,9 @@ use next_core::{
     next_server::{get_server_runtime_entries, ServerContextType},
 };
 use tracing::Instrument;
-use turbo_tasks::{Completion, Value, Vc};
+use turbo_tasks::{Completion, RcStr, Value, Vc};
 use turbopack_binding::{
-    turbo::tasks_fs::{File, FileContent},
+    turbo::tasks_fs::{File, FileContent, FileSystemPath},
     turbopack::{
         core::{
             asset::AssetContent,
@@ -41,6 +41,9 @@ pub struct InstrumentationEndpoint {
     context: Vc<Box<dyn AssetContext>>,
     source: Vc<Box<dyn Source>>,
     is_edge: bool,
+
+    app_dir: Option<Vc<FileSystemPath>>,
+    ecmascript_client_reference_transition_name: Option<Vc<RcStr>>,
 }
 
 #[turbo_tasks::value_impl]
@@ -51,12 +54,16 @@ impl InstrumentationEndpoint {
         context: Vc<Box<dyn AssetContext>>,
         source: Vc<Box<dyn Source>>,
         is_edge: bool,
+        app_dir: Option<Vc<FileSystemPath>>,
+        ecmascript_client_reference_transition_name: Option<Vc<RcStr>>,
     ) -> Vc<Self> {
         Self {
             project,
             context,
             source,
             is_edge,
+            app_dir,
+            ecmascript_client_reference_transition_name,
         }
         .cell()
     }
@@ -79,7 +86,11 @@ impl InstrumentationEndpoint {
         );
 
         let mut evaluatable_assets = get_server_runtime_entries(
-            Value::new(ServerContextType::Middleware),
+            Value::new(ServerContextType::Instrumentation {
+                app_dir: self.app_dir,
+                ecmascript_client_reference_transition_name: self
+                    .ecmascript_client_reference_transition_name,
+            }),
             self.project.next_mode(),
         )
         .resolve_entries(self.context)
@@ -131,7 +142,11 @@ impl InstrumentationEndpoint {
                     .join("server/instrumentation.js".into()),
                 module,
                 get_server_runtime_entries(
-                    Value::new(ServerContextType::Instrumentation),
+                    Value::new(ServerContextType::Instrumentation {
+                        app_dir: self.app_dir,
+                        ecmascript_client_reference_transition_name: self
+                            .ecmascript_client_reference_transition_name,
+                    }),
                     self.project.next_mode(),
                 )
                 .resolve_entries(self.context),

packages/next-swc/crates/next-api/src/middleware.rs
@@ -8,9 +8,9 @@ use next_core::{
     util::{parse_config_from_source, MiddlewareMatcherKind},
 };
 use tracing::Instrument;
-use turbo_tasks::{Completion, Value, Vc};
+use turbo_tasks::{Completion, RcStr, Value, Vc};
 use turbopack_binding::{
-    turbo::tasks_fs::{File, FileContent},
+    turbo::tasks_fs::{File, FileContent, FileSystemPath},
     turbopack::{
         core::{
             asset::AssetContent,
@@ -40,6 +40,8 @@ pub struct MiddlewareEndpoint {
     project: Vc<Project>,
     context: Vc<Box<dyn AssetContext>>,
     source: Vc<Box<dyn Source>>,
+    app_dir: Option<Vc<FileSystemPath>>,
+    ecmascript_client_reference_transition_name: Option<Vc<RcStr>>,
 }
 
 #[turbo_tasks::value_impl]
@@ -49,11 +51,15 @@ impl MiddlewareEndpoint {
         project: Vc<Project>,
         context: Vc<Box<dyn AssetContext>>,
         source: Vc<Box<dyn Source>>,
+        app_dir: Option<Vc<FileSystemPath>>,
+        ecmascript_client_reference_transition_name: Option<Vc<RcStr>>,
     ) -> Vc<Self> {
         Self {
             project,
             context,
             source,
+            app_dir,
+            ecmascript_client_reference_transition_name,
         }
         .cell()
     }
@@ -79,7 +85,11 @@ impl MiddlewareEndpoint {
         );
 
         let mut evaluatable_assets = get_server_runtime_entries(
-            Value::new(ServerContextType::Middleware),
+            Value::new(ServerContextType::Middleware {
+                app_dir: self.app_dir,
+                ecmascript_client_reference_transition_name: self
+                    .ecmascript_client_reference_transition_name,
+            }),
             self.project.next_mode(),
         )
         .resolve_entries(self.context)

commit 6dc85796c257b3632d37d09901cea5b23fe6835b

fix edge runtime asset fetch in pages api (#76750)
### What

We discoved an issue that fetch new URL with static assets in Pages API on edge runtime were failing. Located the issue was an introduced in #66534 where we changed the layer of Pages API with edge runtime entries into API layer, where it actually should be another separate layer like middleware.

The quick fix is using middleware bundle layer for Pages API edge functions, but middleware is server-only layer which Pages API is not yet, since it doesn't bundle everything and picking up the `react-server` condition. So we have to create a new layer `apiEdge` to minimize the behavior changes and also fix the bug.

The test suite was disabled due to node-fetch blocker, but we now we can enable it.

Closes NDX-941
Fixes #74385 
[slack-ref](https://vercel.slack.com/archives/C07CGAA2RS6/p1740868643335099)

packages/next/src/build/entries.ts
@@ -424,7 +424,10 @@ export function getEdgeServerEntry(opts: {
       ).toString('base64'),
     }
 
-    return `next-edge-function-loader?${stringify(loaderParams)}!`
+    return {
+      import: `next-edge-function-loader?${stringify(loaderParams)}!`,
+      layer: WEBPACK_LAYERS.apiEdge,
+    }
   }
 
   const loaderParams: EdgeSSRLoaderQuery = {
@@ -876,7 +879,7 @@ export function finalizeEntrypoint({
   switch (compilerType) {
     case COMPILER_NAMES.server: {
       const layer = isApi
-        ? WEBPACK_LAYERS.api
+        ? WEBPACK_LAYERS.apiNode
         : isInstrumentation
           ? WEBPACK_LAYERS.instrument
           : isServerComponent
@@ -895,7 +898,7 @@ export function finalizeEntrypoint({
     case COMPILER_NAMES.edgeServer: {
       return {
         layer: isApi
-          ? WEBPACK_LAYERS.api
+          ? WEBPACK_LAYERS.apiEdge
           : isMiddlewareFilename(name) || isInstrumentation
             ? WEBPACK_LAYERS.middleware
             : name.startsWith('pages/')

packages/next/src/build/webpack-config.ts
@@ -645,7 +645,7 @@ export default async function getBaseWebpackConfig(
   const apiRoutesLayerLoaders = useSWCLoader
     ? getSwcLoader({
         serverComponents: false,
-        bundleLayer: WEBPACK_LAYERS.api,
+        bundleLayer: WEBPACK_LAYERS.apiNode,
       })
     : defaultLoaders.babel
 
@@ -1575,15 +1575,20 @@ export default async function getBaseWebpackConfig(
           oneOf: [
             {
               ...codeCondition,
-              issuerLayer: WEBPACK_LAYERS.api,
-              parser: {
-                // In Node.js, switch back to normal URL handling.
-                // In Edge runtime, we should disable parser.url handling in webpack so URLDependency is not added.
-                // Then there's browser code won't be injected into the edge runtime chunk.
-                // x-ref: https://github.com/webpack/webpack/blob/d9ce3b1f87e63c809d8a19bbd92257d65922e81f/lib/web/JsonpChunkLoadingRuntimeModule.js#L69
-                url: !isEdgeServer,
-              },
+              issuerLayer: WEBPACK_LAYERS.apiNode,
               use: apiRoutesLayerLoaders,
+              // In Node.js, switch back to normal URL handling.
+              // We won't bundle `new URL()` cases in Node.js bundler layer.
+              parser: {
+                url: true,
+              },
+            },
+            {
+              ...codeCondition,
+              issuerLayer: WEBPACK_LAYERS.apiEdge,
+              use: apiRoutesLayerLoaders,
+              // In Edge runtime, we leave the url handling by default.
+              // The new URL assets will be converted into edge assets through assets loader.
             },
             {
               test: codeCondition.test,

packages/next/src/build/webpack/plugins/middleware-plugin.ts
@@ -279,7 +279,7 @@ function buildWebpackError({
 
 function isInMiddlewareLayer(parser: webpack.javascript.JavascriptParser) {
   const layer = parser.state.module?.layer
-  return layer === WEBPACK_LAYERS.middleware || layer === WEBPACK_LAYERS.api
+  return layer === WEBPACK_LAYERS.middleware || layer === WEBPACK_LAYERS.apiEdge
 }
 
 function isNodeJsModule(moduleName: string) {
@@ -868,7 +868,7 @@ export async function handleWebpackExternalForEdgeRuntime({
 }) {
   if (
     (contextInfo.issuerLayer === WEBPACK_LAYERS.middleware ||
-      contextInfo.issuerLayer === WEBPACK_LAYERS.api) &&
+      contextInfo.issuerLayer === WEBPACK_LAYERS.apiEdge) &&
     isNodeJsModule(request) &&
     !supportedEdgePolyfills.has(request)
   ) {

packages/next/src/lib/constants.ts
@@ -118,9 +118,13 @@ const WEBPACK_LAYERS_NAMES = {
    */
   actionBrowser: 'action-browser',
   /**
-   * The layer for the API routes.
+   * The Node.js bundle layer for the API routes.
    */
-  api: 'api',
+  apiNode: 'api-node',
+  /**
+   * The Edge Lite bundle layer for the API routes.
+   */
+  apiEdge: 'api-edge',
   /**
    * The layer for the middleware code.
    */
@@ -169,7 +173,8 @@ const WEBPACK_LAYERS = {
     ],
     neutralTarget: [
       // pages api
-      WEBPACK_LAYERS_NAMES.api,
+      WEBPACK_LAYERS_NAMES.apiNode,
+      WEBPACK_LAYERS_NAMES.apiEdge,
     ],
     clientOnly: [
       WEBPACK_LAYERS_NAMES.serverSideRendering,

test/e2e/edge-compiler-can-import-blob-assets/index.test.ts
@@ -1,28 +1,13 @@
-import { createNext, FileRef } from 'e2e-utils'
-import { NextInstance } from 'e2e-utils'
+import { nextTestSetup } from 'e2e-utils'
 import { fetchViaHTTP, renderViaHTTP } from 'next-test-utils'
 import path from 'path'
 import { promises as fs } from 'fs'
 import { readJson } from 'fs-extra'
 
-// TODO: `node-fetch` hangs on some of these tests in Node.js.
-// Re-enable when `node-fetch` is dropped.
-// See: https://github.com/vercel/next.js/pull/55112
-describe.skip('Edge Compiler can import asset assets', () => {
-  let next: NextInstance
-
-  // TODO: remove after this is supported for deploy
-  if ((global as any).isNextDeploy) {
-    it('should skip for deploy for now', () => {})
-    return
-  }
-
-  beforeAll(async () => {
-    next = await createNext({
-      files: new FileRef(path.join(__dirname, './app')),
-    })
+describe('Edge Compiler can import asset assets', () => {
+  const { next, isTurbopack, isNextDeploy } = nextTestSetup({
+    files: path.join(__dirname, './app'),
   })
-  afterAll(() => next.destroy())
 
   it('allows to fetch a remote URL', async () => {
     const response = await fetchViaHTTP(next.url, '/api/edge', {
@@ -66,33 +51,63 @@ describe.skip('Edge Compiler can import asset assets', () => {
     })
   })
 
-  it('extracts all the assets from the bundle', async () => {
-    const manifestPath = path.join(
-      next.testDir,
-      '.next/server/middleware-manifest.json'
-    )
-    const manifest = await readJson(manifestPath)
-    const orderedAssets = manifest.functions['/api/edge'].assets.sort(
-      (a, z) => {
-        return String(a.name).localeCompare(z.name)
-      }
-    )
+  // Cannot read the file on deployment
+  if (!isNextDeploy) {
+    it('extracts all the assets from the bundle', async () => {
+      const manifestPath = path.join(
+        next.testDir,
+        '.next/server/middleware-manifest.json'
+      )
+      const manifest = await readJson(manifestPath)
+      const orderedAssets = manifest.functions['/api/edge'].assets.sort(
+        (a, z) => {
+          return String(a.name).localeCompare(z.name)
+        }
+      )
 
-    expect(orderedAssets).toMatchObject([
-      {
-        name: expect.stringMatching(/^text-file\.[0-9a-f]{16}\.txt$/),
-        filePath: expect.stringMatching(
-          /^server\/edge-chunks\/asset_text-file/
-        ),
-      },
-      {
-        name: expect.stringMatching(/^vercel\.[0-9a-f]{16}\.png$/),
-        filePath: expect.stringMatching(/^server\/edge-chunks\/asset_vercel/),
-      },
-      {
-        name: expect.stringMatching(/^world\.[0-9a-f]{16}\.json/),
-        filePath: expect.stringMatching(/^server\/edge-chunks\/asset_world/),
-      },
-    ])
-  })
+      if (isTurbopack) {
+        expect(orderedAssets).toMatchObject([
+          {
+            name: expect.stringMatching(
+              /server\/edge\/assets\/text-file\.[0-9a-f]{8}\.txt$/
+            ),
+            filePath: expect.stringMatching(/^server\/edge\/assets\/text-file/),
+          },
+          {
+            name: expect.stringMatching(
+              /^server\/edge\/assets\/vercel\.[0-9a-f]{8}\.png$/
+            ),
+            filePath: expect.stringMatching(/^server\/edge\/assets\/vercel/),
+          },
+          {
+            name: expect.stringMatching(
+              /^server\/edge\/assets\/world\.[0-9a-f]{8}\.json/
+            ),
+            filePath: expect.stringMatching(/^server\/edge\/assets\/world/),
+          },
+        ])
+      } else {
+        expect(orderedAssets).toMatchObject([
+          {
+            name: expect.stringMatching(/^text-file\.[0-9a-f]{16}\.txt$/),
+            filePath: expect.stringMatching(
+              /^server\/edge-chunks\/asset_text-file/
+            ),
+          },
+          {
+            name: expect.stringMatching(/^vercel\.[0-9a-f]{16}\.png$/),
+            filePath: expect.stringMatching(
+              /^server\/edge-chunks\/asset_vercel/
+            ),
+          },
+          {
+            name: expect.stringMatching(/^world\.[0-9a-f]{16}\.json/),
+            filePath: expect.stringMatching(
+              /^server\/edge-chunks\/asset_world/
+            ),
+          },
+        ])
+      }
+    })
+  }
 })