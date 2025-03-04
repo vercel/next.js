4b5000bcc9da4aa65eea730e2765d38aadfd9d5e
Jiachi Liu <inbox@huozhi.im>
2025-03-04 17:35:48 +0100

    add failure tests

--- test/e2e/app-dir/parallel-routes-not-found/parallel-routes-not-found.test.ts
+++ test/e2e/app-dir/parallel-routes-not-found/parallel-routes-not-found.test.ts
@@ -1,5 +1,7 @@
 import { nextTestSetup } from 'e2e-utils'
 
+const isPPR = process.env.__NEXT_EXPERIMENTAL_PPR === 'true'
+
 describe('parallel-routes-and-interception', () => {
   const { next, skipped } = nextTestSetup({
     files: __dirname,
@@ -22,7 +24,20 @@ describe('parallel-routes-and-interception', () => {
     // we also check that the #children-slot id is not present
     expect(await browser.hasElementByCssSelector('#children-slot')).toBe(false)
 
-    const $ = await next.render$('/')
-    expect($('title').text()).toBe('layout title')
+    if (isPPR) {
+      let $ = await next.render$('/')
+      expect($('title').text()).toBe('layout title')
+    } else {
+      const $ = await next.render$('/')
+      expect($('title').text()).toBe('layout title')
+    }
+  })
+
+  it('should render the title once for the non-existed route', async () => {
+    const browser = await next.browser('/non-existed')
+    const titles = await browser.elementsByCss('title')
+
+    // FIXME: (metadata), the title should only be rendered once and using the not-found title
+    expect(titles).toHaveLength(3)
   })
 })
