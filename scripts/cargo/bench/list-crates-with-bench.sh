499dfa463d073b3107ab8e346ed390ddb0c36486
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-03-07 12:05:12 +0900

    Update scripts/cargo/bench/list-crates-with-bench.sh
    Co-authored-by: Benjamin Woodruff <benjamin.woodruff@vercel.com>

--- scripts/cargo/bench/list-crates-with-bench.sh
+++ scripts/cargo/bench/list-crates-with-bench.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 WS_CRATES="$("$SCRIPT_DIR"/get-workspace-crates.sh)"
 echo "$WS_CRATES" | jq -r -c '[.[] | select(.targets[] | .kind | contains(["bench"])) | .name] | sort | unique' | jq -r -c '[.[] | select(. != "napi" and . != "wasm")]'
