9c20b70444acba4a7f4d3913fe2f4f6ea914af26
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-03-10 11:47:31 +0100

    Update snapshots

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-namespace/output/4c35f_tests_snapshot_basic-tree-shake_import-namespace_input_index_fc74b3c5.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4c35f_tests_snapshot_basic-tree-shake_import-namespace_input_index_fc74b3c5.js",
+    {},
+    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_basic-tree-shake_import-namespace_input_78f1a109._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/import-namespace/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
