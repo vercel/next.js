91d94b1c1464c16e15cb646b800354264f09f002
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-03-10 17:10:00 +0900

    fix(turbopack): Fix the span of a magic comment (#76939)
    ### What?
    
    Fix the span of the turbopack tree shaking magic comment.
    
    ### Why?
    
    Previously it was printed on strange position.
    
    ### How?
    
    Closes NAR-91
    
    Closes PACK-4087

--- crates/next-custom-transforms/tests/fixture/server-actions/server-graph/27/output.js
+++ crates/next-custom-transforms/tests/fixture/server-actions/server-graph/27/output.js
@@ -1,9 +1,9 @@
 // Rules here:
 // 1. Each exported function should still be exported, but as a reference `registerServerReference(...)`.
 // 2. Actual action functions should be renamed to `$$ACTION_...` and got exported.
-/*#__TURBOPACK_DISABLE_EXPORT_MERGING__*/ /* __next_internal_action_entry_do_not_use__ {"001c36b06e398c97abe5d5d7ae8c672bfddf4e1b91":"$$RSC_SERVER_ACTION_2","006a88810ecce4a4e8b59d53b8327d7e98bbf251d7":"$$RSC_SERVER_ACTION_0","0090b5db271335765a4b0eab01f044b381b5ebd5cd":"$$RSC_SERVER_ACTION_1","009ed0cc47abc4e1c64320cf42b74ae60b58c40f00":"$$RSC_SERVER_ACTION_3","00a9b2939c1f39073a6bed227fd20233064c8b7869":"$$RSC_SERVER_ACTION_4"} */ import { registerServerReference } from "private-next-rsc-server-reference";
+/* __next_internal_action_entry_do_not_use__ {"001c36b06e398c97abe5d5d7ae8c672bfddf4e1b91":"$$RSC_SERVER_ACTION_2","006a88810ecce4a4e8b59d53b8327d7e98bbf251d7":"$$RSC_SERVER_ACTION_0","0090b5db271335765a4b0eab01f044b381b5ebd5cd":"$$RSC_SERVER_ACTION_1","009ed0cc47abc4e1c64320cf42b74ae60b58c40f00":"$$RSC_SERVER_ACTION_3","00a9b2939c1f39073a6bed227fd20233064c8b7869":"$$RSC_SERVER_ACTION_4"} */ import { registerServerReference } from "private-next-rsc-server-reference";
 import { encryptActionBoundArgs, decryptActionBoundArgs } from "private-next-rsc-action-encryption";
-export const $$RSC_SERVER_ACTION_0 = async function foo() {
+export const /*#__TURBOPACK_DISABLE_EXPORT_MERGING__*/ $$RSC_SERVER_ACTION_0 = async function foo() {
     console.log(1);
 };
 var foo = registerServerReference($$RSC_SERVER_ACTION_0, "006a88810ecce4a4e8b59d53b8327d7e98bbf251d7", null);
