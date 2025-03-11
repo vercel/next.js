f1ab584ca2c4f80a55fac3f2a5ff5db65865d1bc
Janka Uryga <lolzatu2@gmail.com>
2025-03-11 16:25:17 +0100

    refactor: extract node body helpers

--- packages/next/src/server/app-render/action-handler.ts
+++ packages/next/src/server/app-render/action-handler.ts
@@ -51,6 +51,8 @@ import type { TemporaryReferenceSet } from 'react-server-dom-webpack/server.edge
 import { workUnitAsyncStorage } from '../app-render/work-unit-async-storage.external'
 import { InvariantError } from '../../shared/lib/invariant-error'
 import { executeRevalidates } from '../revalidation-utils'
+import type { NodeNextRequest } from '../base-http/node'
+import type { Readable } from 'node:stream'
 
 function formDataFromSearchQueryString(query: string) {
   const searchParams = new URLSearchParams(query)
@@ -486,7 +488,6 @@ export async function handleAction({
   serverActions?: ServerActionsConfig
   ctx: AppRenderContext
 }): Promise<HandleActionResult> {
-  const contentType = req.headers['content-type']
   const { serverActionsManifest, page } = ctx.renderOpts
 
   const {
@@ -802,46 +803,10 @@ export async function handleAction({
 
           temporaryReferences = createTemporaryReferenceSet()
 
-          const { Transform, pipeline } =
-            require('node:stream') as typeof import('node:stream')
-
-          const defaultBodySizeLimit = '1 MB'
-          const bodySizeLimit =
-            serverActions?.bodySizeLimit ?? defaultBodySizeLimit
-          const bodySizeLimitBytes =
-            bodySizeLimit !== defaultBodySizeLimit
-              ? (
-                  require('next/dist/compiled/bytes') as typeof import('bytes')
-                ).parse(bodySizeLimit)
-              : 1024 * 1024 // 1 MB
-
-          let size = 0
-          const sizeLimitTransform = new Transform({
-            transform(chunk, encoding, callback) {
-              size += Buffer.byteLength(chunk, encoding)
-              if (size > bodySizeLimitBytes) {
-                const { ApiError } = require('../api-utils')
-
-                callback(
-                  new ApiError(
-                    413,
-                    `Body exceeded ${bodySizeLimit} limit.\n` +
-                      `To configure the body size limit for Server Actions, see: https://nextjs.org/docs/app/api-reference/next-config-js/serverActions#bodysizelimit`
-                  )
-                )
-                return
-              }
-
-              callback(null, chunk)
-            },
-          })
-
-          const sizeLimitedBody = pipeline(
-            req.body,
-            sizeLimitTransform,
-            // Avoid unhandled errors from `pipeline()` by passing an empty completion callback.
-            // We'll propagate the errors properly when consuming the stream.
-            () => {}
+          const bodySizeLimit = resolveBodySizeLimitNode(serverActions)
+          const sizeLimitedBody = getSizeLimitedRequestBodyNode(
+            req,
+            bodySizeLimit
           )
 
           if (isMultipartAction) {
@@ -854,12 +819,15 @@ export async function handleAction({
                 return handleUnrecognizedFetchAction(err)
               }
 
+              const { pipeline } =
+                require('node:stream') as typeof import('node:stream')
+
               const busboy = (
                 require('next/dist/compiled/busboy') as typeof import('next/dist/compiled/busboy')
               )({
                 defParamCharset: 'utf8',
                 headers: req.headers,
-                limits: { fieldSize: bodySizeLimitBytes },
+                limits: { fieldSize: bodySizeLimit.byteLength },
               })
 
               // We need to use `pipeline(one, two)` instead of `one.pipe(two)` to propagate size limit errors correctly.
@@ -882,26 +850,10 @@ export async function handleAction({
 
               // React doesn't yet publish a busboy version of decodeAction
               // so we polyfill the parsing of FormData.
-              const fakeRequest = new Request('http://localhost', {
-                method: 'POST',
-                // @ts-expect-error
-                headers: { 'Content-Type': contentType },
-                body: new ReadableStream({
-                  start: (controller) => {
-                    sizeLimitedBody.on('data', (chunk) => {
-                      controller.enqueue(new Uint8Array(chunk))
-                    })
-                    sizeLimitedBody.on('end', () => {
-                      controller.close()
-                    })
-                    sizeLimitedBody.on('error', (err) => {
-                      controller.error(err)
-                    })
-                  },
-                }),
-                duplex: 'half',
-              })
-              const formData = await fakeRequest.formData()
+              const formData = await parseBodyAsFormDataNode(
+                sizeLimitedBody,
+                req.headers['content-type']
+              )
 
               let action: Awaited<ReturnType<typeof decodeAction>>
               try {
@@ -1220,4 +1172,101 @@ function createActionNotFoundError(actionId: string, cause?: unknown) {
       cause instanceof Error ? ` Original error: ${cause.message}` : ''
     }\nRead more: https://nextjs.org/docs/messages/failed-to-find-server-action`
   )
+}
+
+async function parseBodyAsFormDataNode(
+  body: Readable,
+  contentType: string | undefined
+) {
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    throw new InvariantError('This function cannot be used in the edge runtime')
+  } else {
+    const fakeRequest = new Request('http://localhost', {
+      method: 'POST',
+      // @ts-expect-error
+      headers: { 'Content-Type': contentType },
+      body: new ReadableStream({
+        start: (controller) => {
+          body.on('data', (chunk) => {
+            controller.enqueue(new Uint8Array(chunk))
+          })
+          body.on('end', () => {
+            controller.close()
+          })
+          body.on('error', (err) => {
+            controller.error(err)
+          })
+        },
+      }),
+      duplex: 'half',
+    })
+    return await fakeRequest.formData()
+  }
+}
+
+type ResolvedBodySizeLimit = {
+  byteLength: number
+  humanReadable: SizeLimit
+}
+
+function resolveBodySizeLimitNode(
+  serverActions: ServerActionsConfig | undefined
+): ResolvedBodySizeLimit {
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    throw new InvariantError('This function cannot be used in the edge runtime')
+  } else {
+    const defaultBodySizeLimit: SizeLimit = '1MB'
+    const bodySizeLimit = serverActions?.bodySizeLimit ?? defaultBodySizeLimit
+    const byteLength =
+      bodySizeLimit !== defaultBodySizeLimit
+        ? (require('next/dist/compiled/bytes') as typeof import('bytes')).parse(
+            bodySizeLimit
+          )
+        : 1024 * 1024 // 1 MB
+    return {
+      byteLength,
+      humanReadable: bodySizeLimit,
+    }
+  }
+}
+
+function getSizeLimitedRequestBodyNode(
+  req: NodeNextRequest,
+  sizeLimit: ResolvedBodySizeLimit
+): Readable {
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    throw new InvariantError('This function cannot be used in the edge runtime')
+  } else {
+    const { Transform, pipeline } =
+      require('node:stream') as typeof import('node:stream')
+
+    let size = 0
+    const sizeLimitTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        size += Buffer.byteLength(chunk, encoding)
+        if (size > sizeLimit.byteLength) {
+          const { ApiError } = require('../api-utils')
+
+          callback(
+            new ApiError(
+              413,
+              `Body exceeded ${sizeLimit.humanReadable} limit.
+                To configure the body size limit for Server Actions, see: https://nextjs.org/docs/app/api-reference/next-config-js/serverActions#bodysizelimit`
+            )
+          )
+          return
+        }
+
+        callback(null, chunk)
+      },
+    })
+
+    return pipeline(
+      req.body,
+      sizeLimitTransform,
+      // Avoid unhandled errors from `pipeline()` by passing an empty completion callback.
+      // We'll propagate the errors properly when consuming the stream.
+      () => {}
+    )
+  }
 }
