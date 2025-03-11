1369cd4e53bd59b09efb3a1cd5715ad4130f343b
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2025-03-10 17:15:38 -0700

    Use shared constant for failed HMR message

--- packages/next/src/client/components/react-dev-overlay/app/hot-reloader-client.tsx
+++ packages/next/src/client/components/react-dev-overlay/app/hot-reloader-client.tsx
@@ -21,6 +21,7 @@ import {
   ACTION_UNHANDLED_ERROR,
   ACTION_UNHANDLED_REJECTION,
   ACTION_VERSION_INFO,
+  REACT_REFRESH_FULL_RELOAD,
   useErrorOverlayReducer,
 } from '../shared'
 import { parseStack } from '../utils/parse-stack'
@@ -210,14 +211,7 @@ function tryApplyUpdates(
   function handleApplyUpdates(err: any, updatedModules: string[] | null) {
     if (err || RuntimeErrorHandler.hadRuntimeError || !updatedModules) {
       if (err) {
-        console.warn(
-          '[Fast Refresh] performing full reload\n\n' +
-            "Fast Refresh will perform a full reload when you edit a file that's imported by modules outside of the React rendering tree.\n" +
-            'You might have a file which exports a React component but also exports a value that is imported by a non-React component file.\n' +
-            'Consider migrating the non-React component export to a separate file and importing it into both files.\n\n' +
-            'It is also possible the parent component of the component you edited is a class component, which disables Fast Refresh.\n' +
-            'Fast Refresh requires at least one parent function component in your React tree.'
-        )
+        console.warn(REACT_REFRESH_FULL_RELOAD)
       } else if (RuntimeErrorHandler.hadRuntimeError) {
         console.warn(REACT_REFRESH_FULL_RELOAD_FROM_ERROR)
       }
