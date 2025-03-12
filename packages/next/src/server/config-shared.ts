5e59da1f5c8b9e8b3a759048bd371efcd77813ae
JJ Kasper <jj@jjsweb.site>
2025-03-12 14:06:43 -0700

    Add dev warning for cross-origin and stabilize allowedDevOrigins (#77044)
    Adding a warning when we block cross-origin requests in dev so users are
    aware this is why easier and know how to allow the origin through as
    custom setups can use different origins and it be valid.
    
    x-ref:
    https://github.com/vercel/next.js/pull/76880#issuecomment-2718487112
    
    ---------
    
    Co-authored-by: Jiachi Liu <inbox@huozhi.im>
    Co-authored-by: Zack Tanner <1939140+ztanner@users.noreply.github.com>

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -673,6 +673,8 @@ export type ExportPathMap = {
  * Read more: [Next.js Docs: `next.config.js`](https://nextjs.org/docs/app/api-reference/config/next-config-js)
  */
 export interface NextConfig extends Record<string, any> {
+  allowedDevOrigins?: string[]
+
   exportPathMap?: (
     defaultMap: ExportPathMap,
     ctx: {
@@ -1134,6 +1136,7 @@ export const defaultConfig: NextConfig = {
   output: !!process.env.NEXT_PRIVATE_STANDALONE ? 'standalone' : undefined,
   modularizeImports: undefined,
   outputFileTracingRoot: process.env.NEXT_PRIVATE_OUTPUT_TRACE_ROOT || '',
+  allowedDevOrigins: [],
   experimental: {
     allowedDevOrigins: [],
     nodeMiddleware: false,
