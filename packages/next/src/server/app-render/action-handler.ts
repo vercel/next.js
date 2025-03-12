d9b014b2144c4e20e5d9289c4f70654454449ecf
Janka Uryga <lolzatu2@gmail.com>
2025-03-12 06:50:34 +0100

    fix: return 404 instead of 500 if an MPA action has an unrecognized id
    If an action id is not found in the module map, `decodeAction` throws:
    ```
    Error: Could not find the module "{id}" in the React Server Manifest. This is probably a bug in the React Server Components bundler.
    ```
    Previously, this error would bubble up from the action handler and result in a 500.
    This is inconsistent with fetch actions, where we print a special warning and return a 404.
    
    To distinguish "action not found" from other error causes (e.g. a malformed request),
    I wrap the server action manifest in a proxy that throws a special error if the action is missing.
    We can then detect this error and handle it separately.

--- packages/next/src/server/app-render/action-handler.ts
+++ packages/next/src/server/app-render/action-handler.ts
@@ -676,7 +676,22 @@ export async function handleAction({
             } else {
               // Multipart POST, but not a fetch action.
               // Potentially an MPA action, we have to try decoding it to check.
-              const action = await decodeAction(formData, serverModuleMap)
+
+              let action: Awaited<ReturnType<typeof decodeAction>>
+              try {
+                action = await decodeAction(
+                  formData,
+                  throwIfActionNotInModuleMap(serverModuleMap)
+                )
+              } catch (err) {
+                if (err instanceof ActionNotInModuleMapError) {
+                  console.error(err)
+                  return { type: 'not-found' }
+                } else {
+                  throw err
+                }
+              }
+
               if (typeof action === 'function') {
                 // an MPA action.
 
@@ -869,7 +884,22 @@ export async function handleAction({
                 duplex: 'half',
               })
               const formData = await fakeRequest.formData()
-              const action = await decodeAction(formData, serverModuleMap)
+
+              let action: Awaited<ReturnType<typeof decodeAction>>
+              try {
+                action = await decodeAction(
+                  formData,
+                  throwIfActionNotInModuleMap(serverModuleMap)
+                )
+              } catch (err) {
+                if (err instanceof ActionNotInModuleMapError) {
+                  console.error(err)
+                  return { type: 'not-found' }
+                } else {
+                  throw err
+                }
+              }
+
               if (typeof action === 'function') {
                 // an MPA action.
 
@@ -1148,10 +1178,42 @@ function getActionModIdOrError(
   const actionModId = serverModuleMap[actionId]?.id
 
   if (!actionModId) {
-    throw new Error(
-      `Failed to find Server Action "${actionId}". This request might be from an older or newer deployment.\nRead more: https://nextjs.org/docs/messages/failed-to-find-server-action`
-    )
+    throw createActionNotFoundError(actionId)
   }
 
   return actionModId
+}
+
+class ActionNotInModuleMapError extends Error {}
+
+/** If an action id is not found in the module map, `decodeAction` throws:
+ *   `"Error: Could not find the module "{id}" in the React Server Manifest."`
+ * We want to be able to distinguish this case from other potential reasons it could throw,
+ * (e.g. a malformed request) so we can use this wrapper to throw our own special error.
+ */
+function throwIfActionNotInModuleMap(
+  serverModuleMap: ServerModuleMap
+): ServerModuleMap {
+  return new Proxy(serverModuleMap, {
+    get(target, actionId: string) {
+      let result
+      try {
+        result = Reflect.get(target, actionId)
+      } catch (err) {
+        throw createActionNotFoundError(actionId, err)
+      }
+      if (result === undefined) {
+        throw createActionNotFoundError(actionId)
+      }
+      return result
+    },
+  })
+}
+
+function createActionNotFoundError(actionId: string, cause?: unknown) {
+  return new ActionNotInModuleMapError(
+    `Failed to find Server Action "${actionId}". This request might be from an older or newer deployment.${
+      cause instanceof Error ? ` Original error: ${cause.message}` : ''
+    }\nRead more: https://nextjs.org/docs/messages/failed-to-find-server-action`
+  )
 }
