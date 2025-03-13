70af4bee8587808d55fa48b766b5c6a2d6ead8ff
Tobias Koppers <tobias.koppers@googlemail.com>
2025-03-13 12:37:19 +0100

    update snapshots

--- test/development/pages-dir/client-navigation/index.test.ts
+++ test/development/pages-dir/client-navigation/index.test.ts
@@ -1335,7 +1335,7 @@ describe('Client Navigation', () => {
              "description": "Error: An Expected error occurred",
              "environmentLabel": null,
              "label": "Runtime Error",
-             "source": "pages/error-in-the-browser-global-scope.js (2:9) @ [Module.
+             "source": "pages/error-in-the-browser-global-scope.js (2:9) @ Module.
            {module evaluation}
            > 2 |   throw new Error('An Expected error occurred')
                |         ^",
--- test/e2e/app-dir/use-cache-close-over-function/use-cache-close-over-function.test.ts
+++ test/e2e/app-dir/use-cache-close-over-function/use-cache-close-over-function.test.ts
@@ -113,7 +113,7 @@ describe('use-cache-close-over-function', () => {
               '\n   ^^^^^^^^^^^' +
               '\n    at createCachedFn (app/server/page.tsx:6:2)' +
               // TODO(veil): Should be source-mapped.
-              '\n    at [project]'
+              '\n    at Module.__TURBOPACK__module__evaluation__ (../app/server/page.tsx:12:23)'
           : '' +
               'Error: Functions cannot be passed directly to Client Components unless you explicitly expose it by marking it with "use server". Or maybe you meant to call this function rather than return it.' +
               '\n  [function fn]' +
