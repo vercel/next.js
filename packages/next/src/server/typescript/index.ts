vercel/next.js

commit 1708497bffa28954fc96f27779cd25506bedfa8a

feat: option to disable next types plugin (#76300)

packages/next/src/server/typescript/index.ts
@@ -28,6 +28,10 @@ import metadata from './rules/metadata'
 import errorEntry from './rules/error'
 import type tsModule from 'typescript/lib/tsserverlibrary'
 
+type NextTypePluginOptions = {
+  enabled?: boolean
+}
+
 export const createTSPlugin: tsModule.server.PluginModuleFactory = ({
   typescript: ts,
 }) => {
@@ -42,6 +46,13 @@ export const createTSPlugin: tsModule.server.PluginModuleFactory = ({
     for (let k of Object.keys(info.languageService)) {
       const x = (info.languageService as any)[k]
       proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args)
+    }
+
+    const pluginOptions: NextTypePluginOptions = info.config ?? {
+      enabled: true,
+    }
+    if (!pluginOptions.enabled) {
+      return proxy
     }
 
     // Auto completion

packages/next/src/server/next.ts
@@ -466,7 +466,10 @@ function createServer(
     'typescript' in options &&
     'version' in (options as any).typescript
   ) {
-    return require('./next-typescript').createTSPlugin(options)
+    const pluginMod: typeof import('./next-typescript') = require('./next-typescript')
+    return pluginMod.createTSPlugin(
+      options as any
+    ) as unknown as NextWrapperServer
   }
 
   if (options == null) {

commit 75505438dcf8409db8ead9d86ce6f0561171d61e

[ts-hint] fix vscode type hint plugin enabling (#77099)
### What

Fix the enabling of ts hint plugin in vscode. This is a regression
introduced in #76300, where we use the plugin config `{ "name": "next"
}` to directly check if it has a `enabled` property, and determine if we
should enable the plugin. This PR fixed the way of reading plugin config
and keep it enabled by default.

Fixes 76980

packages/next/src/server/typescript/index.ts
@@ -28,10 +28,6 @@ import metadata from './rules/metadata'
 import errorEntry from './rules/error'
 import type tsModule from 'typescript/lib/tsserverlibrary'
 
-type NextTypePluginOptions = {
-  enabled?: boolean
-}
-
 export const createTSPlugin: tsModule.server.PluginModuleFactory = ({
   typescript: ts,
 }) => {
@@ -48,10 +44,14 @@ export const createTSPlugin: tsModule.server.PluginModuleFactory = ({
       proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args)
     }
 
-    const pluginOptions: NextTypePluginOptions = info.config ?? {
-      enabled: true,
-    }
-    if (!pluginOptions.enabled) {
+    // Get plugin options
+    // config is the plugin options from the user's tsconfig.json
+    // e.g. { "plugins": [{ "name": "next", "enabled": true }] }
+    // config will be { "name": "next", "enabled": true }
+    // The default user config is { "name": "next" }
+    const isPluginEnabled = info.config.enabled ?? true
+
+    if (!isPluginEnabled) {
       return proxy
     }
 