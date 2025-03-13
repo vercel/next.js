2a1153b4d5fc427a63548176cc4f9cdb690effdb
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-03-13 19:48:43 +0100

    saturating_sub

--- crates/next-api/src/app.rs
+++ crates/next-api/src/app.rs
@@ -860,7 +860,7 @@ impl AppProject {
                     // as part of rsc_entry
                     for module in server_component_entries
                         .iter()
-                        .take(server_component_entries.len() - 1)
+                        .take(server_component_entries.len().saturating_sub(1))
                     {
                         let graph = SingleModuleGraph::new_with_entries_visited_intern(
                             // This should really be ChunkGroupEntry::Shared(module.await?.module),
