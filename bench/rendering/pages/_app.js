f95361dce098cd10f6ce87be83fb932f106b6443
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-03-14 17:49:43 +0100

    example

---
+++ bench/rendering/pages/_app.js
@@ -0,0 +1,6 @@
+import * as lodash from 'lodash-es'
+console.log(lodash)
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ bench/rendering/pages/page2.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => {
+  return <div>span2</div>
+}
