5a831130e0f93d07faa6de77ca28be3443a05832
Kir93 <kir931028@gmail.com>
2025-03-15 21:59:44 +0900

    feat: Refactored components for chakra ui v3

--- examples/with-chakra-ui/src/components/Container.tsx
+++ examples/with-chakra-ui/src/components/Container.tsx
@@ -1,17 +1,24 @@
+import { FC } from "react";
 import { Flex, FlexProps } from "@chakra-ui/react";
 
-export const Container = (props: FlexProps) => (
-  <Flex
-    direction="column"
-    alignItems="center"
-    justifyContent="flex-start"
-    bg="gray.50"
-    color="black"
-    _dark={{
-      bg: "gray.900",
-      color: "white",
-    }}
-    transition="all 0.15s ease-out"
-    {...props}
-  />
-);
+const Container: FC<FlexProps> = ({ children, ...props }) => {
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="flex-start"
+      bg="gray.50"
+      color="black"
+      _dark={{
+        bg: "gray.900",
+        color: "white",
+      }}
+      transition="all 0.15s ease-out"
+      {...props}
+    >
+      {children}
+    </Flex>
+  );
+};
+
+export default Container;
