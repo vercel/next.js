f8dc72dd34cea72062540a30bdc808e3529a2691
Jiachi Liu <inbox@huozhi.im>
2025-03-18 16:45:15 +0100

    add failing test

---
+++ test/e2e/barrel-optimization-recurrsive-bundle/barrel-optimization-recurrsive-bundle.test.ts
@@ -0,0 +1,19 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('barrel-optimization-recurrsive-bundle', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+    installCommand: 'yarn install',
+    dependencies: {
+      antd: '5.23.4',
+    },
+  })
+
+  it('should work using cheerio', async () => {
+    const $ = await next.render$('/')
+    expect($('button').text()).toBe('Click')
+
+    const browser = await next.browser('/')
+    expect(await browser.elementByCss('button').text()).toBe('Click')
+  })
+})
