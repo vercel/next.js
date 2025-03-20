317de3b8fc8fed32933c40fa40e62c0faee75604
Agus Setya R <30733517+agussetyar@users.noreply.github.com>
2025-03-21 04:24:03 +0700

    fix(examples): incorrect headers() usage in the Content Security Policy

--- examples/with-strict-csp/app/page.js
+++ examples/with-strict-csp/app/page.js
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 import Script from "next/script";
 
 export default function Page() {
-  const nonce = headers().get("x-nonce");
+  const nonce = (await headers()).get("x-nonce");
 
   return <Script src="https://..." strategy="afterInteractive" nonce={nonce} />;
 }
