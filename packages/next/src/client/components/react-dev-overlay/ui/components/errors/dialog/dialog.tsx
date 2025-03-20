e7aa88a2f5c2ec1947bff464f46685251806d503
rauno <freiberggg@gmail.com>
2025-03-20 15:50:15 +0200

    [dev-overlay] Make footer sticky without side effects (#77327)
    Closes NDX-988
    
    ---
    
    
    
    
    https://github.com/user-attachments/assets/1e9dc54e-d247-4b6b-93e6-b926c69b2571

--- packages/next/src/client/components/react-dev-overlay/ui/components/errors/dialog/dialog.tsx
+++ packages/next/src/client/components/react-dev-overlay/ui/components/errors/dialog/dialog.tsx
@@ -4,49 +4,48 @@ type ErrorOverlayDialogProps = {
   children?: React.ReactNode
   onClose?: () => void
   dialogResizerRef?: React.RefObject<HTMLDivElement | null>
+  footer?: React.ReactNode
 } & React.HTMLAttributes<HTMLDivElement>
 
 export function ErrorOverlayDialog({
   children,
   onClose,
+  footer,
   ...props
 }: ErrorOverlayDialogProps) {
   return (
-    <Dialog
-      type="error"
-      aria-labelledby="nextjs__container_errors_label"
-      aria-describedby="nextjs__container_errors_desc"
-      onClose={onClose}
-      className="error-overlay-dialog"
-      {...props}
-    >
-      {children}
-    </Dialog>
+    <div className="error-overlay-dialog-container">
+      <Dialog
+        type="error"
+        aria-labelledby="nextjs__container_errors_label"
+        aria-describedby="nextjs__container_errors_desc"
+        className="error-overlay-dialog-scroll"
+        onClose={onClose}
+        {...props}
+      >
+        {children}
+      </Dialog>
+      {footer}
+    </div>
   )
 }
 
 export const DIALOG_STYLES = `
-  .error-overlay-dialog {
-    overflow-y: auto;
+  .error-overlay-dialog-container {
     -webkit-font-smoothing: antialiased;
+    display: flex;
+    flex-direction: column;
     background: var(--color-background-100);
     background-clip: padding-box;
     border: var(--next-dialog-border-width) solid var(--color-gray-400);
-    border-radius: var(--rounded-xl);
+    border-radius: 0 0 var(--next-dialog-radius) var(--next-dialog-radius);
     box-shadow: var(--shadow-menu);
     position: relative;
+    overflow: hidden;
+  }
 
-    &:has(
-        ~ [data-nextjs-error-overlay-nav] .error-overlay-notch[data-side='left']
-      ) {
-      border-top-left-radius: 0;
-    }
-
-    &:has(
-        ~ [data-nextjs-error-overlay-nav]
-          .error-overlay-notch[data-side='right']
-      ) {
-      border-top-right-radius: 0;
-    }
+  .error-overlay-dialog-scroll {
+    overflow-y: auto;
+    height: 100%;
   }
 `
