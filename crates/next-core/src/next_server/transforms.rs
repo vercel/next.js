94f7319f84d5ff375929cbc1c35e16acc0c1e9f9
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-03-21 17:09:03 +0100

    Only in routes, obviously

--- crates/next-core/src/next_server/transforms.rs
+++ crates/next-core/src/next_server/transforms.rs
@@ -195,28 +195,30 @@ pub async fn get_next_server_transforms_rules(
             ));
         }
 
-        // Ignore static asset imports in Edge, these are really intended for the client (i.e. for
-        // pages and not routes), while still allowing `new URL(..., import.meta.url)`
-        rules.push(ModuleRule::new(
-            RuleCondition::all(vec![
-                RuleCondition::not(RuleCondition::ReferenceType(ReferenceType::Url(
-                    UrlReferenceSubType::Undefined,
-                ))),
-                RuleCondition::any(vec![
-                    RuleCondition::ResourcePathEndsWith(".apng".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".avif".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".gif".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".ico".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".jpg".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".jpeg".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".png".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".svg".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".webp".to_string()),
-                    RuleCondition::ResourcePathEndsWith(".woff2".to_string()),
+        if matches!(context_ty, ServerContextType::AppRoute { .. }) {
+            // Ignore static asset imports in Edge routes, these are really intended for the client
+            // (i.e. for pages), while still allowing `new URL(..., import.meta.url)`
+            rules.push(ModuleRule::new(
+                RuleCondition::all(vec![
+                    RuleCondition::not(RuleCondition::ReferenceType(ReferenceType::Url(
+                        UrlReferenceSubType::Undefined,
+                    ))),
+                    RuleCondition::any(vec![
+                        RuleCondition::ResourcePathEndsWith(".apng".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".avif".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".gif".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".ico".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".jpg".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".jpeg".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".png".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".svg".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".webp".to_string()),
+                        RuleCondition::ResourcePathEndsWith(".woff2".to_string()),
+                    ]),
                 ]),
-            ]),
-            vec![ModuleRuleEffect::Ignore],
-        ));
+                vec![ModuleRuleEffect::Ignore],
+            ));
+        }
     }
 
     Ok(rules)
