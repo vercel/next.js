9231e34f4727e8193de4daf1798f9c080336071e
🤖Esteban Dalel R <estebandalelr@gmail.com>
2025-03-21 22:22:59 -0400

    Fix message not showing
    The message uses an undefined color

--- examples/with-supabase/components/form-message.tsx
+++ examples/with-supabase/components/form-message.tsx
@@ -12,7 +12,7 @@ export function FormMessage({ message }: { message: Message }) {
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="text-destructive border-l-2 border-destructive-foreground px-4">
           {message.error}
         </div>
       )}
