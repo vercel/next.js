1316c11f0439b815c725f3fde135f2d51c817d33
Tim Neutkens <tim@timneutkens.nl>
2025-03-23 13:05:13 -0700

    Update index.ts

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -2335,7 +2335,7 @@ export default async function build(
                   ]
                 : []),
               BUILD_ID_FILE,
-              IS_TURBOPACK_BUILD,
+              isTurbopackBuild ? IS_TURBOPACK_BUILD : null,
               path.join(SERVER_DIRECTORY, NEXT_FONT_MANIFEST + '.js'),
               path.join(SERVER_DIRECTORY, NEXT_FONT_MANIFEST + '.json'),
               ...instrumentationHookEntryFiles,
