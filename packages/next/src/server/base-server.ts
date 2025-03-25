60c2939f570fad53c3145b698f619dbc248f888f
Jiachi Liu <inbox@huozhi.im>
2025-03-24 17:42:44 -0700

    fix typo

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -564,7 +564,6 @@ export default abstract class Server<
     }
 
     this.renderOpts = {
-      appDir: '',
       supportsDynamicResponse: true,
       trailingSlash: this.nextConfig.trailingSlash,
       deploymentId: this.nextConfig.deploymentId,
