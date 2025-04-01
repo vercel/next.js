90b136e75055a3a14768fe30a4a72e50d39b75b8
Tim Neutkens <tim@timneutkens.nl>
2025-04-01 13:23:16 +0200

    Update turbopack-build-marker.test.ts

--- test/production/app-dir/turbopack-build-marker/turbopack-build-marker.test.ts
+++ test/production/app-dir/turbopack-build-marker/turbopack-build-marker.test.ts
@@ -1,13 +1,12 @@
 import { nextTestSetup } from 'e2e-utils'
-;(process.env.TURBOPACK ? describe : describe.skip)(
-  'turbopack-build-marker',
-  () => {
-    const { next } = nextTestSetup({
-      files: __dirname,
-    })
+describe('turbopack-build-marker', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
 
-    it('should have Turbopack build marker', async () => {
-      expect(await next.hasFile('.next/IS_TURBOPACK_BUILD')).toBe(true)
-    })
-  }
-)
+  it('should have Turbopack build marker', async () => {
+    expect(await next.hasFile('.next/IS_TURBOPACK_BUILD')).toBe(
+      !!process.env.TURBOPACK
+    )
+  })
+})
