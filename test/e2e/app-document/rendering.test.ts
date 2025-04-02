8236d00791314d25cf679dfd1a554d40fe796a02
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-04-02 11:40:21 -0700

    Cleanup

--- test/e2e/app-document/rendering.test.ts
+++ test/e2e/app-document/rendering.test.ts
@@ -6,20 +6,9 @@ describe('app-document - Rendering via HTTP', () => {
     files: __dirname,
   })
 
-  function render(path: string) {
-    return next.render(path, undefined, {
-      headers: { 'user-agent': 'Safari' },
-    })
-  }
-  function render$(path: string) {
-    return next.render$(path, undefined, {
-      headers: { 'user-agent': 'Safari' },
-    })
-  }
-
   describe('_document', () => {
     it('should include required elements in rendered html', async () => {
-      const $ = await render$('/')
+      const $ = await next.render$('/')
       // It has a custom html class
       expect($('html').hasClass('test-html-props')).toBe(true)
       // It has a custom body class
@@ -34,14 +23,14 @@ describe('app-document - Rendering via HTTP', () => {
 
     it('Document.getInitialProps returns html prop representing app shell', async () => {
       // Extract css-in-js-class from the rendered HTML, which is returned by Document.getInitialProps
-      const $index = await render$('/')
-      const $about = await render$('/about')
+      const $index = await next.render$('/')
+      const $about = await next.render$('/about')
       expect($index('#css-in-cjs-count').text()).toBe('2')
       expect($about('#css-in-cjs-count').text()).toBe('0')
     })
 
     it('adds nonces to all scripts and preload links', async () => {
-      const $ = await render$('/')
+      const $ = await next.render$('/')
       const nonce = 'test-nonce'
       let noncesAdded = true
       $('script, link[rel=preload]').each((index, element) => {
@@ -51,7 +40,7 @@ describe('app-document - Rendering via HTTP', () => {
     })
 
     it('adds crossOrigin to all scripts and preload links', async () => {
-      const $ = await render$('/')
+      const $ = await next.render$('/')
       const crossOrigin = 'anonymous'
       $('script, link[rel=preload]').each((index, element) => {
         expect($(element).attr('crossorigin') === crossOrigin).toBeTruthy()
@@ -59,7 +48,7 @@ describe('app-document - Rendering via HTTP', () => {
     })
 
     it('renders ctx.renderPage with enhancer correctly', async () => {
-      const $ = await render$('/?withEnhancer=true')
+      const $ = await next.render$('/?withEnhancer=true')
       const nonce = 'RENDERED'
       expect($('#render-page-enhance-component').text().includes(nonce)).toBe(
         true
@@ -67,7 +56,7 @@ describe('app-document - Rendering via HTTP', () => {
     })
 
     it('renders ctx.renderPage with enhanceComponent correctly', async () => {
-      const $ = await render$('/?withEnhanceComponent=true')
+      const $ = await next.render$('/?withEnhanceComponent=true')
       const nonce = 'RENDERED'
       expect($('#render-page-enhance-component').text().includes(nonce)).toBe(
         true
@@ -75,13 +64,15 @@ describe('app-document - Rendering via HTTP', () => {
     })
 
     it('renders ctx.renderPage with enhanceApp correctly', async () => {
-      const $ = await render$('/?withEnhanceApp=true')
+      const $ = await next.render$('/?withEnhanceApp=true')
       const nonce = 'RENDERED'
       expect($('#render-page-enhance-app').text().includes(nonce)).toBe(true)
     })
 
     it('renders ctx.renderPage with enhanceApp and enhanceComponent correctly', async () => {
-      const $ = await render$('/?withEnhanceComponent=true&withEnhanceApp=true')
+      const $ = await next.render$(
+        '/?withEnhanceComponent=true&withEnhanceApp=true'
+      )
       const nonce = 'RENDERED'
       expect($('#render-page-enhance-app').text().includes(nonce)).toBe(true)
       expect($('#render-page-enhance-component').text().includes(nonce)).toBe(
@@ -89,33 +80,37 @@ describe('app-document - Rendering via HTTP', () => {
       )
     })
 
-    // This is a workaround to fix https://github.com/vercel/next.js/issues/5860
-    // TODO: remove this workaround when https://bugs.webkit.org/show_bug.cgi?id=187726 is fixed.
-    it('adds a timestamp to link tags with preload attribute to invalidate the cache (DEV only)', async () => {
-      const $ = await render$('/')
-      $('link[rel=preload]').each((index, element) => {
-        const href = $(element).attr('href')
-        expect(href.match(/\?/g)).toHaveLength(1)
-        expect(href).toMatch(/\?ts=/)
+    if (isNextDev) {
+      // This is a workaround to fix https://github.com/vercel/next.js/issues/5860
+      // TODO: remove this workaround when https://bugs.webkit.org/show_bug.cgi?id=187726 is fixed.
+      it('adds a timestamp to link tags with preload attribute to invalidate the cache in dev', async () => {
+        const $ = await next.render$('/', undefined, {
+          headers: { 'user-agent': 'Safari' },
+        })
+        $('link[rel=preload]').each((index, element) => {
+          const href = $(element).attr('href')
+          expect(href.match(/\?/g)).toHaveLength(1)
+          expect(href).toMatch(/\?ts=/)
+        })
+        $('script[src]').each((index, element) => {
+          const src = $(element).attr('src')
+          expect(src.match(/\?/g)).toHaveLength(1)
+          expect(src).toMatch(/\?ts=/)
+        })
       })
-      $('script[src]').each((index, element) => {
-        const src = $(element).attr('src')
-        expect(src.match(/\?/g)).toHaveLength(1)
-        expect(src).toMatch(/\?ts=/)
-      })
-    })
+    }
   })
 
   describe('_app', () => {
     it('shows a custom tag', async () => {
-      const $ = await render$('/')
+      const $ = await next.render$('/')
       expect($('#hello-app').text()).toBe('Hello App')
     })
 
     // For example react context uses shared module state
     // Also known as singleton modules
     it('should share module state with pages', async () => {
-      const $ = await render$('/shared')
+      const $ = await next.render$('/shared')
       expect($('#currentstate').text()).toBe('UPDATED')
     })
 
@@ -124,7 +119,7 @@ describe('app-document - Rendering via HTTP', () => {
         const errMsg = 'have an error from _app getInitialProps'
         const origContent = await next.readFile('pages/_app.js')
 
-        expect(await render('/')).toContain('page-index')
+        expect(await next.render('/')).toContain('page-index')
 
         await next.patchFile(
           'pages/_app.js',
@@ -136,14 +131,16 @@ describe('app-document - Rendering via HTTP', () => {
 
         let foundErr = false
         try {
-          await retry(async () => expect(await render('/')).toContain(errMsg))
+          await retry(async () =>
+            expect(await next.render('/')).toContain(errMsg)
+          )
           foundErr = true
         } finally {
           await next.patchFile('pages/_app.js', origContent)
 
           // Make sure _app is restored
           await retry(async () =>
-            expect(await render('/')).toContain('page-index')
+            expect(await next.render('/')).toContain('page-index')
           )
           expect(foundErr).toBeTruthy()
         }
