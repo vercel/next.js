3763f31290e13e576b9dd58ad1a30433f514861a
Tim Neutkens <tim@timneutkens.nl>
2025-04-03 16:15:44 +0200

    Update loading for Turbopack development

--- packages/next/src/server/route-modules/app-page/module.compiled.js
+++ packages/next/src/server/route-modules/app-page/module.compiled.js
@@ -2,7 +2,9 @@ if (process.env.NEXT_RUNTIME === 'edge') {
   module.exports = require('next/dist/server/route-modules/app-page/module.js')
 } else {
   if (process.env.__NEXT_EXPERIMENTAL_REACT) {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' && process.env.TURBOPACK) {
+      module.exports = require('next/dist/compiled/next-server/app-page-turbo-experimental.runtime.dev.js')
+    } else if (process.env.NODE_ENV === 'development') {
       module.exports = require('next/dist/compiled/next-server/app-page-experimental.runtime.dev.js')
     } else if (process.env.TURBOPACK) {
       module.exports = require('next/dist/compiled/next-server/app-page-turbo-experimental.runtime.prod.js')
@@ -10,7 +12,9 @@ if (process.env.NEXT_RUNTIME === 'edge') {
       module.exports = require('next/dist/compiled/next-server/app-page-experimental.runtime.prod.js')
     }
   } else {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' && process.env.TURBOPACK) {
+      module.exports = require('next/dist/compiled/next-server/app-page-turbo.runtime.dev.js')
+    } else if (process.env.NODE_ENV === 'development') {
       module.exports = require('next/dist/compiled/next-server/app-page.runtime.dev.js')
     } else if (process.env.TURBOPACK) {
       module.exports = require('next/dist/compiled/next-server/app-page-turbo.runtime.prod.js')
--- packages/next/src/server/route-modules/app-route/module.compiled.js
+++ packages/next/src/server/route-modules/app-route/module.compiled.js
@@ -1,7 +1,9 @@
 if (process.env.NEXT_RUNTIME === 'edge') {
   module.exports = require('next/dist/server/route-modules/app-route/module.js')
 } else {
-  if (process.env.__NEXT_EXPERIMENTAL_REACT) {
+  if (process.env.NODE_ENV === 'development' && process.env.TURBOPACK) {
+    module.exports = require('next/dist/compiled/next-server/app-route-turbo-experimental.runtime.dev.js')
+  } else if (process.env.__NEXT_EXPERIMENTAL_REACT) {
     if (process.env.NODE_ENV === 'development') {
       module.exports = require('next/dist/compiled/next-server/app-route-experimental.runtime.dev.js')
     } else if (process.env.TURBOPACK) {
@@ -10,7 +12,9 @@ if (process.env.NEXT_RUNTIME === 'edge') {
       module.exports = require('next/dist/compiled/next-server/app-route-experimental.runtime.prod.js')
     }
   } else {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' && process.env.TURBOPACK) {
+      module.exports = require('next/dist/compiled/next-server/app-route-turbo.runtime.dev.js')
+    } else if (process.env.NODE_ENV === 'development') {
       module.exports = require('next/dist/compiled/next-server/app-route.runtime.dev.js')
     } else if (process.env.TURBOPACK) {
       module.exports = require('next/dist/compiled/next-server/app-route-turbo.runtime.prod.js')
