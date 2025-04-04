057dfc1b4fcb51e9c9200f3db83d59601f01ec86
Tobias Koppers <tobias.koppers@googlemail.com>
2025-04-04 17:33:53 +0200

    update snapshot

--- test/integration/css/test/basic-global-support.test.js
+++ test/integration/css/test/basic-global-support.test.js
@@ -62,28 +62,28 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_single-global_styles_global_04ffe231.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_single-global_styles_global_04ffe231.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/e6f86e82ed770205.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/e6f86e82ed770205.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}",
              ]
             `)
@@ -146,28 +146,28 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/aa64a_css-fixtures_single-global-special-characters_a%2Bb_styles_global_238bffda.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/aa64a_css-fixtures_single-global-special-characters_a%2Bb_styles_global_238bffda.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/e6f86e82ed770205.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/e6f86e82ed770205.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}",
              ]
             `)
@@ -225,28 +225,28 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_single-global-src_styles_global_3de272ac.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_single-global-src_styles_global_3de272ac.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/e6f86e82ed770205.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/e6f86e82ed770205.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}",
              ]
             `)
@@ -305,7 +305,7 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_multi-global_styles_e0f6ec64._.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}
 
 
@@ -315,7 +315,7 @@ module.exports = {
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_multi-global_styles_e0f6ec64._.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red}
 
 
@@ -325,14 +325,14 @@ module.exports = {
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/5e3a80a101f2afdd.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}.blue-text{color:#00f}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/52172921917299d1.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red}.blue-text{color:blue}",
              ]
             `)
@@ -391,7 +391,7 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_nested-global_styles_613f73a3._.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:purple;font-weight:bolder}
 
 
@@ -407,7 +407,7 @@ module.exports = {
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_nested-global_styles_613f73a3._.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:purple;font-weight:bolder}
 
 
@@ -423,14 +423,14 @@ module.exports = {
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/197173a06644483f.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:purple;font-weight:bolder;color:red}.blue-text{color:orange;font-weight:bolder;color:#00f}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/5fd4808215677a0f.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:purple;font-weight:bolder;color:red}.blue-text{color:orange;font-weight:bolder;color:blue}",
              ]
             `)
@@ -490,7 +490,7 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_multi-global-reversed_styles_9924990c._.css:
+               "/_next/static/chunks/HASH.css:
              .blue-text{color:#00f}
 
 
@@ -500,7 +500,7 @@ module.exports = {
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_multi-global-reversed_styles_9924990c._.css:
+               "/_next/static/chunks/HASH.css:
              .blue-text{color:#00f}
 
 
@@ -510,14 +510,14 @@ module.exports = {
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/67c5003fc41fdadb.css:
+               "/_next/static/css/HASH.css:
              .blue-text{color:#00f}.red-text{color:red}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/f06b052da7bb0328.css:
+               "/_next/static/css/HASH.css:
              .blue-text{color:blue}.red-text{color:red}",
              ]
             `)
@@ -576,7 +576,7 @@ module.exports = {
           if (process.env.TURBOPACK && useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_url-global_styles_46281715._.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red;background-image:url(../media/dark.993bedd3.svg),url(../media/dark2.993bedd3.svg)}
 
 
@@ -589,7 +589,7 @@ module.exports = {
           } else if (process.env.TURBOPACK && !useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/chunks/test_integration_css-fixtures_url-global_styles_46281715._.css:
+               "/_next/static/chunks/HASH.css:
              .red-text{color:red;background-image:url(../media/dark.993bedd3.svg),url(../media/dark2.993bedd3.svg)}
 
 
@@ -602,14 +602,14 @@ module.exports = {
           } else if (useLightningcss) {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/02830b0dd136adeb.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red;background-image:url(/_next/static/media/dark.6b01655b.svg),url(/_next/static/media/dark2.6b01655b.svg)}.blue-text{color:orange;background-image:url(/_next/static/media/light.2da1d3d6.svg);font-weight:bolder;color:#00f}",
              ]
             `)
           } else {
             expect(cssContent).toMatchInlineSnapshot(`
              [
-               "/_next/static/css/bf86b6e30872ca37.css:
+               "/_next/static/css/HASH.css:
              .red-text{color:red;background-image:url(/_next/static/media/dark.6b01655b.svg),url(/_next/static/media/dark2.6b01655b.svg)}.blue-text{color:orange;font-weight:bolder;background-image:url(/_next/static/media/light.2da1d3d6.svg);color:blue}",
              ]
             `)
@@ -653,7 +653,7 @@ describe('CSS URL via `file-loader` and asset prefix (1)', () => {
         if (process.env.TURBOPACK) {
           expect(cssContent).toMatchInlineSnapshot(`
            [
-             "/_next/static/chunks/test_integration_css-fixtures_url-global-asset-prefix-1_styles_e99cb7f8._.css:
+             "/_next/static/chunks/HASH.css:
            .red-text{color:red;background-image:url(../media/dark.993bedd3.svg) url(../media/dark2.993bedd3.svg)}
 
 
@@ -666,7 +666,7 @@ describe('CSS URL via `file-loader` and asset prefix (1)', () => {
         } else {
           expect(cssContent).toMatchInlineSnapshot(`
            [
-             "/_next/static/css/18198e5ba4d54f6f.css:
+             "/_next/static/css/HASH.css:
            .red-text{color:red;background-image:url(/foo/_next/static/media/dark.6b01655b.svg) url(/foo/_next/static/media/dark2.6b01655b.svg)}.blue-text{color:orange;font-weight:bolder;background-image:url(/foo/_next/static/media/light.2da1d3d6.svg);color:blue}",
            ]
           `)
@@ -709,7 +709,7 @@ describe('CSS URL via `file-loader` and asset prefix (2)', () => {
         if (process.env.TURBOPACK) {
           expect(cssContent).toMatchInlineSnapshot(`
            [
-             "/_next/static/chunks/test_integration_css-fixtures_url-global-asset-prefix-2_styles_7b29c72b._.css:
+             "/_next/static/chunks/HASH.css:
            .red-text{color:red;background-image:url(../media/dark.993bedd3.svg) url(../media/dark2.993bedd3.svg)}
 
 
@@ -722,7 +722,7 @@ describe('CSS URL via `file-loader` and asset prefix (2)', () => {
         } else {
           expect(cssContent).toMatchInlineSnapshot(`
            [
-             "/_next/static/css/18198e5ba4d54f6f.css:
+             "/_next/static/css/HASH.css:
            .red-text{color:red;background-image:url(/foo/_next/static/media/dark.6b01655b.svg) url(/foo/_next/static/media/dark2.6b01655b.svg)}.blue-text{color:orange;font-weight:bolder;background-image:url(/foo/_next/static/media/light.2da1d3d6.svg);color:blue}",
            ]
           `)
@@ -741,7 +741,12 @@ async function getStylesheetContents($, appPort, items) {
     if (res.status !== 200)
       throw new Error(`Failed to load stylesheet: ${href}`)
     const text = await res.text()
-    results.push(`${href}:\n${text.replace(/\/\*.*?\*\//g, '').trim()}`)
+    results.push(
+      `${href.replace(
+        /[0-9a-f]{8,}/g,
+        'HASH'
+      )}:\n${text.replace(/\/\*.*?\*\//g, '').trim()}`
+    )
   }
   return results
 }
