4aa0240dacbcaab3ac2d5885b5ddac725a9c94ee
JJ Kasper <jj@jjsweb.site>
2025-04-07 09:32:14 -0700

    lint fix

--- packages/create-next-app/templates/default-tw/ts/pages/index.tsx
+++ packages/create-next-app/templates/default-tw/ts/pages/index.tsx
@@ -13,7 +13,9 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
+    <div
+      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
