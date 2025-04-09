1e35a10ffb017483df91a43b2e2146477528cb37
Dario Lehmhus <dario.lehmhus@ffw.com>
2025-04-09 21:22:06 +0200

    chore(examples): update with-linaria example

--- examples/with-linaria/app/page.tsx
+++ examples/with-linaria/app/page.tsx
@@ -1,5 +1,19 @@
-import "./styles.css";
+import { styled } from "@linaria/react";
+import { ClientComponent } from "./ClientComponent";
+import { ServerComponent } from "./ServerComponent";
+
+const Container = styled.div`
+  padding: 20px;
+  background-color: #f0f0f0;
+  border-radius: 8px;
+`;
 
 export default function Home() {
-  return <div className="box anotherClass">Zero runtime CSS in JS</div>;
+  return (
+    <Container>
+      Zero runtime CSS in JS
+      <ServerComponent />
+      <ClientComponent />
+    </Container>
+  );
 }
