670bd7eebc8d6b695aca76cd8600d5bab1429dc1
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-04-10 11:06:26 +0200

    skip prod builds

--- test/e2e/swc-plugins/index.test.ts
+++ test/e2e/swc-plugins/index.test.ts
@@ -1,6 +1,7 @@
-import { nextTestSetup } from 'e2e-utils'
+import { nextTestSetup, isNextDev } from 'e2e-utils'
+
 describe('swcPlugins', () => {
-  describe('support swcPlugins', () => {
+  describe('supports swcPlugins', () => {
     const { next, skipped } = nextTestSetup({
       files: __dirname,
       skipDeployment: true,
@@ -16,14 +17,10 @@ describe('swcPlugins', () => {
       expect(html).not.toContain('data-custom-attribute')
     })
   })
-
-  describe('invalid plugin names', () => {
+  ;(isNextDev ? describe : describe.skip)('invalid plugin names', () => {
     const { next, skipped, isTurbopack } = nextTestSetup({
       files: __dirname,
       skipDeployment: true,
-      dependencies: {
-        '@swc/plugin-react-remove-properties': '7.0.2',
-      },
       overrideFiles: {
         'next.config.js': `
 module.exports = {
@@ -35,22 +32,22 @@ module.exports = {
     })
     if (skipped) return
 
-    it('shows a proper error', async () => {
+    it('shows a redbox in dev', async () => {
       const browser = await next.browser('/')
 
       if (isTurbopack) {
         await expect(browser).toDisplayRedbox(`
-                {
-                  "count": 1,
-                  "description": "Module not found: Can't resolve '@swc/plugin-nonexistent'",
-                  "environmentLabel": null,
-                  "label": "Build Error",
-                  "source": "./
-                Module not found: Can't resolve '@swc/plugin-nonexistent'
-                https://nextjs.org/docs/messages/module-not-found",
-                  "stack": [],
-                }
-              `)
+         {
+           "count": 1,
+           "description": "Module not found: Can't resolve '@swc/plugin-nonexistent'",
+           "environmentLabel": null,
+           "label": "Build Error",
+           "source": "./
+         Module not found: Can't resolve '@swc/plugin-nonexistent'
+         https://nextjs.org/docs/messages/module-not-found",
+           "stack": [],
+         }
+        `)
       } else {
         // TODO missing proper error with Webpack
         await expect(browser).toDisplayRedbox(
