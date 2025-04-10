3bb6854057ac67be71d5ee5d3a334e5a67a67c7d
Andrew Hepworth <andrewohepworth@gmail.com>
2025-04-10 09:49:28 -0700

    update test's app files and update comment for clarity

--- test/e2e/dynamic-route-interpolation/app/page.tsx
+++ test/e2e/dynamic-route-interpolation/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Page() {
   return (
     <div>
-      <Link href="/comments/some-text">Open Modal</Link>
+      <Link href="/comments/comment1">Open Modal</Link>
     </div>
   )
 }
