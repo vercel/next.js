b7d2aaec4bf42e863e37b7fe623ccfa523c52afd
Hendrik Liebau <mail@hendrik-liebau.de>
2025-04-10 12:31:53 +0200

    Use display name in custom cache handler test

--- test/e2e/app-dir/use-cache-custom-handler/handler.js
+++ test/e2e/app-dir/use-cache-custom-handler/handler.js
@@ -7,13 +7,13 @@ const defaultCacheHandler =
  * @type {import('next/dist/server/lib/cache-handlers/types').CacheHandlerV2}
  */
 const cacheHandler = {
-  async get(cacheKey) {
-    console.log('ModernCustomCacheHandler::get', cacheKey)
+  async get(cacheKey, metaData) {
+    console.log('ModernCustomCacheHandler::get', metaData.displayName, cacheKey)
     return defaultCacheHandler.get(cacheKey)
   },
 
-  async set(cacheKey, pendingEntry) {
-    console.log('ModernCustomCacheHandler::set', cacheKey)
+  async set(cacheKey, pendingEntry, metaData) {
+    console.log('ModernCustomCacheHandler::set', metaData.displayName, cacheKey)
     return defaultCacheHandler.set(cacheKey, pendingEntry)
   },
 
