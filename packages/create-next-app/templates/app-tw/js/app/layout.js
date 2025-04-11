bc469a66ce10de54355548af7c5abe922b6388c5
Kyle Holmberg <inbox@kylemh.com>
2025-04-11 15:02:04 +0700

    fix formatting

--- packages/create-next-app/templates/app-tw/js/app/layout.js
+++ packages/create-next-app/templates/app-tw/js/app/layout.js
@@ -18,7 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
       <body>
         {children}
       </body>
