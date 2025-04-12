b3224777147275b0a54d4c536fe5c3b4a34a7b3d
Janka Uryga <lolzatu2@gmail.com>
2025-04-12 18:30:37 +0200

    fix(types): Activity seems to exist now

--- test/e2e/app-dir/back-forward-cache/app/page.tsx
+++ test/e2e/app-dir/back-forward-cache/app/page.tsx
@@ -3,7 +3,6 @@
 // TODO: "use client" is required in this module because Activity is not
 // exported from the server entrypoint of React
 
-// @ts-expect-error: Not yet part of the TypeScript types
 import { unstable_Activity as Activity } from 'react'
 
 export default function Page() {
