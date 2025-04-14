e2d00bd43e8df133eb18551019f8e6216eb2f5a6
Cy-Tek <josh.hannaford@vercel.com>
2025-04-14 15:16:22 -0400

    Removed extra snapshot tests in favor of e2e tests

--- turbopack/crates/turbopack-tests/tests/snapshot/imports/require-context/output/b1abf_turbopack-tests_tests_snapshot_imports_require-context_input_index_dc745c90.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/b1abf_turbopack-tests_tests_snapshot_imports_require-context_input_index_dc745c90.js",
-    {},
-    {"otherChunks":["output/4e721_crates_turbopack-tests_tests_snapshot_imports_require-context_input_901fbaa3._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/imports/require-context/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
