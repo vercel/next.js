07204d7090fa2068292b982810374d3e3c6e778b
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2025-04-16 18:26:49 -0700

    HACK: Transform NotFound error to PathNotFound error

--- turbopack/crates/turbo-tasks-fs/src/watcher.rs
+++ turbopack/crates/turbo-tasks-fs/src/watcher.rs
@@ -8,7 +8,7 @@ use std::{
     time::Duration,
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use notify::{
     event::{MetadataKind, ModifyKind, RenameMode},
     Config, EventKind, PollWatcher, RecommendedWatcher, RecursiveMode, Watcher,
@@ -107,8 +107,18 @@ impl DiskWatcher {
         dir_path: &Path,
         root_path: &Path,
     ) -> Result<()> {
-        use anyhow::Context;
-        use notify::ErrorKind;
+        // HACK: Rewrite NotFound io errors to PathNotFound
+        // This shouldn't ever happen (notify should transform this for us), but even after
+        // https://github.com/notify-rs/notify/pull/611, it seems like there's still some case where
+        // it can occur with inotify on Linux.
+        fn map_notify_err(mut err: notify::Error) -> notify::Error {
+            if let notify::ErrorKind::Io(io_err) = &err.kind {
+                if io_err.kind() == std::io::ErrorKind::NotFound {
+                    err.kind = notify::ErrorKind::PathNotFound;
+                }
+            }
+            err
+        }
 
         if let Some(watcher) = watcher.as_mut() {
             let mut path = dir_path;
@@ -120,11 +130,14 @@ impl DiskWatcher {
                 ));
             };
             loop {
-                match watcher.watch(path, RecursiveMode::NonRecursive) {
+                match watcher
+                    .watch(path, RecursiveMode::NonRecursive)
+                    .map_err(map_notify_err)
+                {
                     Ok(()) => break,
                     Err(
                         err @ notify::Error {
-                            kind: ErrorKind::PathNotFound,
+                            kind: notify::ErrorKind::PathNotFound,
                             ..
                         },
                     ) => {
