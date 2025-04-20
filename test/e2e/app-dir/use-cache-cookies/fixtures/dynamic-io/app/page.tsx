be3f42edf35c880012e859fe852bbb5a52394392
Hendrik Liebau <mail@hendrik-liebau.de>
2025-04-20 16:20:25 +0200

    Unflake test

--- test/e2e/app-dir/use-cache-cookies/fixtures/dynamic-io/app/page.tsx
+++ test/e2e/app-dir/use-cache-cookies/fixtures/dynamic-io/app/page.tsx
@@ -4,7 +4,7 @@ import { setTimeout } from 'timers/promises'
 export default async function Page() {
   'use cache'
 
-  await setTimeout(1000)
+  await setTimeout(2000)
   const loggedIn = (await cookies()).get('isLoggedIn')
 
   return <p id="login-status">{loggedIn ? 'logged in' : 'logged out'}</p>
