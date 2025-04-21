69ebfd093ac807e7e3d5e99ced9318ccb7450112
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-04-21 13:27:12 -0600

    fix: handle incremental PPR with client segment cache

---
+++ test/e2e/app-dir/segment-cache/conflicting-routes/conflicting-routes.test.ts
@@ -0,0 +1,33 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('conflicting routes', () => {
+  const { next, isNextDev, isNextDeploy } = nextTestSetup({
+    files: __dirname,
+  })
+  if (isNextDev) {
+    test('prefetching is disabled', () => {})
+    return
+  }
+
+  it.each([
+    '/en/vercel/~/monitoring',
+    '/fr/vercel/~/monitoring',
+    '/es/vercel/~/monitoring',
+  ])('%s matches the right route', async (path) => {
+    const res = await next.fetch(path, {
+      headers: {
+        RSC: '1',
+        'Next-Router-Prefetch': '1',
+        'Next-Router-Segment-Prefetch': '/_tree',
+      },
+    })
+
+    expect(res.status).toBe(200)
+
+    if (isNextDeploy) {
+      expect(res.headers.get('x-matched-path')).toBe(
+        '/[lang]/[teamSlug]/~/monitoring.prefetch.rsc'
+      )
+    }
+  })
+})
