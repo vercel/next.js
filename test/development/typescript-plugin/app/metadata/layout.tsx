62ddf6858810ee792f6143141d9e78019f9a572e
devjiwonchoi <devjiwonchoi@gmail.com>
2025-04-22 21:23:56 +0900

    [ts-next-plugin] Add metadata and generateMetadata examples

---
+++ test/development/typescript-plugin/app/metadata/layout.tsx
@@ -0,0 +1,9 @@
+export default function MetadataLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
+
+// type `export const` to see the completions for `metadata` and `generateMetadata`.
---
+++ test/development/typescript-plugin/app/metadata/page.tsx
@@ -0,0 +1,5 @@
+export default function MetadataPage() {
+  return <p>metadata</p>
+}
+
+// type `export const` to see the completions for `metadata` and `generateMetadata`.
