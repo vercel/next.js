e201233da675eff7e36527e6f24d30fcfe48ef80
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-04-22 20:13:54 +0200

    decode base64

--- test/e2e/app-dir/css-modules-data-urls/app/page.tsx
+++ test/e2e/app-dir/css-modules-data-urls/app/page.tsx
@@ -1,6 +1,5 @@
 // @ts-expect-error
-// .home{font-weight:700}
-import styles from 'data:text/css+module;base64,LmhvbWV7Zm9udC13ZWlnaHQ6NzAwfQo='
+import styles from 'data:text/css+module;.home{font-weight:700}'
 import { ClientComponent } from './client'
 
 export default function Home() {
