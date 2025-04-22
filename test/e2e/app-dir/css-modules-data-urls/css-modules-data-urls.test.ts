6723db393b972cc7f6f4bd636903112096e8145c
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-04-22 20:41:19 +0200

    Skip for webpack

--- test/e2e/app-dir/css-modules-data-urls/css-modules-data-urls.test.ts
+++ test/e2e/app-dir/css-modules-data-urls/css-modules-data-urls.test.ts
@@ -1,45 +1,49 @@
 import { nextTestSetup } from 'e2e-utils'
 
-describe('css-modules-data-urls', () => {
-  const { next } = nextTestSetup({
-    files: __dirname,
-  })
+// CSS data urls are only support in Turbopack
+;(process.env.IS_TURBOPACK_TEST ? describe : describe.skip)(
+  'css-modules-data-urls',
+  () => {
+    const { next } = nextTestSetup({
+      files: __dirname,
+    })
 
-  it('should apply rsc class name from data url correctly', async () => {
-    const browser = await next.browser('/')
+    it('should apply rsc class name from data url correctly', async () => {
+      const browser = await next.browser('/')
 
-    const clientElementClass =
-      (await browser.elementByCss('#client').getAttribute('class')) || ''
+      const clientElementClass =
+        (await browser.elementByCss('#client').getAttribute('class')) || ''
 
-    expect(clientElementClass).not.toBe('')
-  })
+      expect(clientElementClass).not.toBe('')
+    })
 
-  it('should apply rsc styles from data url correctly', async () => {
-    const browser = await next.browser('/')
+    it('should apply rsc styles from data url correctly', async () => {
+      const browser = await next.browser('/')
 
-    const rscElement = await browser
-      .elementByCss('#rsc')
-      .getComputedCss('font-weight')
+      const rscElement = await browser
+        .elementByCss('#rsc')
+        .getComputedCss('font-weight')
 
-    expect(rscElement).toBe('700')
-  })
+      expect(rscElement).toBe('700')
+    })
 
-  it('should apply client class name from data url correctly', async () => {
-    const browser = await next.browser('/')
+    it('should apply client class name from data url correctly', async () => {
+      const browser = await next.browser('/')
 
-    const clientElementClass =
-      (await browser.elementByCss('#client').getAttribute('class')) || ''
+      const clientElementClass =
+        (await browser.elementByCss('#client').getAttribute('class')) || ''
 
-    expect(clientElementClass).not.toBe('')
-  })
+      expect(clientElementClass).not.toBe('')
+    })
 
-  it('should apply client styles from data url correctly', async () => {
-    const browser = await next.browser('/')
+    it('should apply client styles from data url correctly', async () => {
+      const browser = await next.browser('/')
 
-    const clientElement = await browser
-      .elementByCss('#client')
-      .getComputedCss('font-weight')
+      const clientElement = await browser
+        .elementByCss('#client')
+        .getComputedCss('font-weight')
 
-    expect(clientElement).toBe('700')
-  })
-})
+      expect(clientElement).toBe('700')
+    })
+  }
+)
