747a86243a07b976f0b1670c9a5762cb54c7ebde
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-23 05:55:10 -0700

    Squashed commit of the following:
    commit 703d7361ac87a7c065602cd0479c419e9092de76
    Author: Donny/강동윤 <kdy1997.dev@gmail.com>
    Date:   Tue Apr 22 20:17:26 2025 -0700
    
        Update lib.rs
    
    commit 0c13abb2dd175ed3feb280f3951072ad4ad11ec4
    Author: James P. Verde <james@justsuperhuman.com>
    Date:   Tue Apr 22 20:49:25 2025 -0600
    
        fix(windows): normalize backslashes to forward slashes in try_join method
    
    commit 92c3bdfa81b235a016b2a2f929a2a7467624ce0a
    Merge: 11b603e24e a556825dd7
    Author: James P. Verde <james@justsuperhuman.com>
    Date:   Tue Apr 22 18:38:35 2025 -0600
    
        Merge branch 'canary' into fix/windows-uri-from-file
    
    commit 11b603e24e0d0f42a0fa31e1ef7786eef62e5f72
    Merge: 3bdb49d706 b252726e91
    Author: James Greene <james@justsuperhuman.com>
    Date:   Tue Apr 22 18:37:45 2025 -0600
    
        fix(windows): normalize path separators in uri_from_file function
    
    commit 3bdb49d7066aeb555bfb3791e37c328631ea53df
    Author: James Greene <james@justsuperhuman.com>
    Date:   Tue Apr 22 17:52:21 2025 -0600
    
        fix(windows):
    
    commit b252726e912b1cb4777ade33ff01c7b8c13fcf15
    Author: James Greene <james@justsuperhuman.com>
    Date:   Tue Apr 22 17:52:21 2025 -0600
    
        fix(windows): preserve drive letter formatting (e.g. C:) in file URIs
    
    commit abecb91a32adfca9c43e84004c8be60bef147dbe
    Author: James Greene <james@justsuperhuman.com>
    Date:   Tue Apr 22 17:39:15 2025 -0600
    
        fix(windows): correct file URI formatting for Turbopack source maps

--- turbopack/crates/turbo-tasks-fs/src/lib.rs
+++ turbopack/crates/turbo-tasks-fs/src/lib.rs
@@ -1288,6 +1288,8 @@ impl FileSystemPath {
     /// None when the joined path would leave the filesystem root.
     #[turbo_tasks::function]
     pub async fn try_join(&self, path: RcStr) -> Result<Vc<FileSystemPathOption>> {
+        let path = path.replace('\\', "/");
+
         if let Some(path) = join_path(&self.path, &path) {
             Ok(Vc::cell(Some(
                 Self::new_normalized(*self.fs, path.into())
