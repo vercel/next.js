fac39fe8369e9e6426c4aea5fb7159b0edf6aceb
devjiwonchoi <devjiwonchoi@gmail.com>
2025-04-24 22:42:16 +0900

    update test

--- test/development/typescript-plugin/index.test.ts
+++ test/development/typescript-plugin/index.test.ts
@@ -1,60 +1,18 @@
-import tsNextPluginFactory from 'next'
-import ts from 'typescript'
-import { resolve } from 'node:path'
+import type { PluginLanguageService } from './test-utils'
+import { getPluginLanguageService } from './test-utils'
 
-const rootDir = resolve(__dirname, '..', '..', '..')
-const { name } = require(resolve(rootDir, 'package.json'))
-if (name !== 'nextjs-project') {
-  throw new Error(
-    'This script must be run from the root of the Next.js project'
-  )
-}
+describe('typescript-plugin', () => {
+  let languageService: PluginLanguageService
 
-const compilerOptions = ts.getDefaultCompilerOptions()
+  beforeAll(() => {
+    languageService = getPluginLanguageService()
+  })
 
-const files = ts.sys.readDirectory(resolve(__dirname, 'app'))
-
-const compilerHost = ts.createCompilerHost(compilerOptions)
-const languageServiceHost: ts.LanguageServiceHost = {
-  ...compilerHost,
-  getCompilationSettings: () => compilerOptions,
-  getScriptFileNames: () => files,
-  getScriptSnapshot: (fileName) => {
-    const contents = ts.sys.readFile(fileName)
-    if (contents && typeof contents === 'string') {
-      return ts.ScriptSnapshot.fromString(contents)
-    }
-    return
-  },
-  getScriptVersion: () => '0',
-  writeFile: ts.sys.writeFile,
-}
-const languageService = ts.createLanguageService(languageServiceHost)
-const plugin: ts.server.PluginModule = (
-  tsNextPluginFactory as unknown as ts.server.PluginModuleFactory
-)({ typescript: ts })
-
-beforeEach(() => {
-  jest.spyOn(console, 'log').mockImplementation()
+  it('should be able to get the language service', () => {
+    expect(languageService).toBeDefined()
+    const capturedLogs = languageService.getCapturedLogs()
+    expect(capturedLogs).toContain(
+      '[next] Initialized Next.js TypeScript plugin'
+    )
+  })
 })
-
-const pluginCreateInfo: ts.server.PluginCreateInfo = {
-  project: {
-    projectService: {
-      logger: {
-        info: console.log,
-      },
-    },
-    getCurrentDirectory: () => __dirname,
-  } as unknown as ts.server.Project,
-  languageService,
-  languageServiceHost,
-  serverHost: null,
-  config: {},
-}
-const pluginLanguageService = plugin.create(pluginCreateInfo)
-const pagePath = resolve(__dirname, 'app/metadata/client/page.tsx')
-console.log({ pagePath })
-const semanticDiagnostics =
-  pluginLanguageService.getSemanticDiagnostics(pagePath)
-console.log(semanticDiagnostics)
--- test/development/typescript-plugin/test-utils.ts
+++ test/development/typescript-plugin/test-utils.ts
@@ -1,1 +1,68 @@
-export function getPluginLanguageService() {}
+import tsNextPluginFactory from 'next'
+import ts from 'typescript'
+import { resolve } from 'node:path'
+
+const fixtureDir = resolve(__dirname, 'app')
+
+export type PluginLanguageService = ts.LanguageService & {
+  getCapturedLogs: () => string
+}
+
+export function getPluginLanguageService(): PluginLanguageService {
+  const files = ts.sys.readDirectory(fixtureDir)
+
+  const compilerOptions = ts.getDefaultCompilerOptions()
+  const compilerHost = ts.createCompilerHost(compilerOptions)
+
+  let logs = ''
+  const logger = {
+    info: (...args: any[]) => {
+      const message = args
+        .map((arg) => (typeof arg === 'string' ? arg : JSON.stringify(arg)))
+        .join(' ')
+      logs += message + '\n'
+      console.log(...args)
+    },
+  }
+
+  const languageServiceHost: ts.LanguageServiceHost = {
+    ...compilerHost,
+    getCompilationSettings: () => compilerOptions,
+    getScriptFileNames: () => files,
+    getScriptSnapshot: (fileName) => {
+      const contents = ts.sys.readFile(fileName)
+      if (contents && typeof contents === 'string') {
+        return ts.ScriptSnapshot.fromString(contents)
+      }
+      return
+    },
+    getScriptVersion: () => '0',
+    writeFile: ts.sys.writeFile,
+  }
+
+  const languageService = ts.createLanguageService(languageServiceHost)
+
+  const pluginCreateInfo: ts.server.PluginCreateInfo = {
+    project: {
+      projectService: {
+        logger,
+      },
+      getCurrentDirectory: () => __dirname,
+    } as unknown as ts.server.Project,
+    languageService,
+    languageServiceHost,
+    serverHost: null,
+    config: {},
+  }
+
+  const plugin: ts.server.PluginModule = (
+    tsNextPluginFactory as unknown as ts.server.PluginModuleFactory
+  )({ typescript: ts })
+
+  const service = plugin.create(pluginCreateInfo) as PluginLanguageService
+
+  // Add a custom method to get captured logs
+  service.getCapturedLogs = () => logs
+
+  return service
+}
