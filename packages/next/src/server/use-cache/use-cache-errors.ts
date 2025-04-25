deb49f45780be2700c329ddfc9dd742a05fc6ac9
Hendrik Liebau <mail@hendrik-liebau.de>
2025-04-25 14:19:17 +0200

    Cleanup debug logs

--- packages/next/src/server/use-cache/use-cache-errors.ts
+++ packages/next/src/server/use-cache/use-cache-errors.ts
@@ -1,15 +1,11 @@
-import { workAsyncStorage } from '../app-render/work-async-storage.external'
-
 const USE_CACHE_TIMEOUT_ERROR_CODE = 'USE_CACHE_TIMEOUT'
 
 export class UseCacheTimeoutError extends Error {
   digest: typeof USE_CACHE_TIMEOUT_ERROR_CODE = USE_CACHE_TIMEOUT_ERROR_CODE
 
   constructor() {
-    const page = workAsyncStorage.getStore()?.page
     super(
-      page +
-        ' Filling a cache during prerender timed out, likely because request-specific arguments such as params, searchParams, cookies() or dynamic data were used inside "use cache".'
+      'Filling a cache during prerender timed out, likely because request-specific arguments such as params, searchParams, cookies() or dynamic data were used inside "use cache".'
     )
   }
 }
