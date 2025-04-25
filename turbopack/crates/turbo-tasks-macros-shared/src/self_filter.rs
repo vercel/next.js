af214b0e8becdf78dddddfe8e0bc3bdcaa6854fb
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-25 15:18:38 -0700

    Detect `self` tokens in macros

--- turbopack/crates/turbo-tasks-macros-shared/src/self_filter.rs
+++ turbopack/crates/turbo-tasks-macros-shared/src/self_filter.rs
@@ -1,6 +1,8 @@
+use proc_macro2::TokenTree;
+use quote::ToTokens;
 use syn::{
-    visit::{visit_expr, Visit},
-    Block, Expr,
+    visit::{visit_expr, visit_macro, Visit},
+    Block, Expr, Macro,
 };
 
 pub fn is_self_used(block: &Block) -> bool {
@@ -15,6 +17,10 @@ struct SelfFinder {
 
 impl Visit<'_> for SelfFinder {
     fn visit_expr(&mut self, expr: &Expr) {
+        if self.found {
+            return;
+        }
+
         if let Expr::Path(path) = expr {
             if path.path.is_ident("self") {
                 self.found = true;
@@ -23,5 +29,22 @@ impl Visit<'_> for SelfFinder {
         }
 
         visit_expr(self, expr);
+    }
+
+    fn visit_macro(&mut self, mac: &Macro) {
+        if self.found {
+            return;
+        }
+
+        for token in mac.tokens.to_token_stream() {
+            if let TokenTree::Ident(ident) = token {
+                if ident == "self" {
+                    self.found = true;
+                    return;
+                }
+            }
+        }
+
+        visit_macro(self, mac);
     }
 }
