21a66cb30391c920454503f1c4a37a48ac2ba301
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-25 11:04:37 -0700

    Implement `is_self_used`

--- turbopack/crates/turbo-tasks-macros-shared/src/self_filter.rs
+++ turbopack/crates/turbo-tasks-macros-shared/src/self_filter.rs
@@ -1,5 +1,21 @@
-use syn::Block;
+use syn::{visit::Visit, Block, Expr};
 
 pub fn is_self_used(block: &Block) -> bool {
-    todo!()
+    let mut finder = SelfFinder { found: false };
+    finder.visit_block(block);
+    finder.found
+}
+
+struct SelfFinder {
+    found: bool,
+}
+
+impl Visit<'_> for SelfFinder {
+    fn visit_expr(&mut self, expr: &Expr) {
+        if let Expr::Path(path) = expr {
+            if path.path.is_ident("self") {
+                self.found = true;
+            }
+        }
+    }
 }
