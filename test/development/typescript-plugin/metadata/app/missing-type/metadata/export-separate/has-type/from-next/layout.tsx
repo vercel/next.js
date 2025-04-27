f3b7f18354158cf1d3598aa3595ca17c77b4b5cd
devjiwonchoi <devjiwonchoi@gmail.com>
2025-04-28 06:20:17 +0900

    metadata

---
+++ test/development/typescript-plugin/metadata/app/missing-type/metadata/export-separate/has-type/from-next/layout.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
+
+const metadata: Metadata = {
+  title: 'Metadata',
+}
+
+export { metadata }
---
+++ test/development/typescript-plugin/metadata/app/missing-type/metadata/export-separate/has-type/from-next/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next'
+
+export default function Page() {
+  return <p>hello world</p>
+}
+
+const metadata: Metadata = {
+  title: 'Metadata',
+}
+
+export { metadata }
