2992f905a3c634f029cdac2f318199ccca3806c0
Andrew Clark <git@andrewclark.io>
2025-04-28 16:37:09 -0400

    Repeat fix in #78387 for routes without params (#78568)
    Depends on:
    - https://github.com/vercel/vercel/pull/13275
    
    ---
    
    Follow up to #78387. When incremental PPR mode is enabled, we need to
    create a special "inverse" route to handle tree requests for every route
    in the manifest that doesn't have PPR enabled. #78387 fixed this for
    "dynamic" routes — which in the context of the relevant module I believe
    means routes with params, and has nothing to do with dynamic data in the
    PPR sense — but it didn't handle for "static" routes — which here means
    routes without any params.
    
    The fix is to apply the same logic in #78387 for all routes, both
    "static" and "dynamic", in the routes manifest.

--- test/e2e/app-dir/segment-cache/conflicting-routes/conflicting-routes.test.ts
+++ test/e2e/app-dir/segment-cache/conflicting-routes/conflicting-routes.test.ts
@@ -30,4 +30,28 @@ describe('conflicting routes', () => {
       )
     }
   })
+
+  it('matches the right route when the original route has no dynamic params, is dynamic, and PPR is disabled', async () => {
+    if (isNextDeploy) {
+      // TODO: Temporarily disabled until corresponding fix in Vercel builder
+      // (https://github.com/vercel/vercel/pull/13275) is released.
+      return
+    }
+
+    const res = await next.fetch('/prefetch-tests/dynamic', {
+      headers: {
+        RSC: '1',
+        'Next-Router-Prefetch': '1',
+        'Next-Router-Segment-Prefetch': '/_tree',
+      },
+    })
+
+    expect(res.status).toBe(200)
+
+    if (isNextDeploy) {
+      expect(res.headers.get('x-matched-path')).toBe(
+        '/prefetch-tests/dynamic.prefetch.rsc'
+      )
+    }
+  })
 })
