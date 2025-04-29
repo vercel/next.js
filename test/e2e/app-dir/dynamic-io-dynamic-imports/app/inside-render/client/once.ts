9f65c0b1f380a6e39113c0f9a1ae43462355cc9d
Janka Uryga <lolzatu2@gmail.com>
2025-04-29 07:37:05 +0200

    test: remove spawnDynamicValidationInDev workaround

--- test/e2e/app-dir/dynamic-io-dynamic-imports/app/inside-render/client/once.ts
+++ test/e2e/app-dir/dynamic-io-dynamic-imports/app/inside-render/client/once.ts
@@ -1,5 +1,3 @@
-import type { WorkUnitStore } from 'next/dist/server/app-render/work-unit-async-storage.external'
-
 /** Memoize a callback to only invoke it once. */
 export function once<T>(cb: () => T) {
   let cache: null | { value: T } = null
@@ -8,35 +6,5 @@ export function once<T>(cb: () => T) {
       cache = { value: cb() }
     }
     return cache.value
-  }
-}
-
-/** A ponyfill of `React.cache` for client modules. This is a workaround, see callsites for motivation. */
-export function oncePerRender<T>(cb: () => T) {
-  if (typeof window !== 'undefined') {
-    return once(cb)
-  } else {
-    let callbackWithoutStore: (() => T) | null = null
-    const callbacksByStore = new WeakMap<WorkUnitStore, () => T>()
-    const { workUnitAsyncStorage } =
-      require('next/dist/server/app-render/work-unit-async-storage.external') as typeof import('next/dist/server/app-render/work-unit-async-storage.external')
-    return () => {
-      let cachedCb: () => T
-      const workUnitStore = workUnitAsyncStorage.getStore()
-      if (!workUnitStore) {
-        if (!callbackWithoutStore) {
-          callbackWithoutStore = once(cb)
-        }
-        cachedCb = callbackWithoutStore
-      } else {
-        let callbackForStore = callbacksByStore.get(workUnitStore)
-        if (!callbackForStore) {
-          callbacksByStore.set(workUnitStore, (callbackForStore = once(cb)))
-        }
-        cachedCb = callbackForStore
-      }
-
-      return cachedCb()
-    }
   }
 }
--- test/e2e/app-dir/dynamic-io-dynamic-imports/app/inside-render/client/sync-module/client.tsx
+++ test/e2e/app-dir/dynamic-io-dynamic-imports/app/inside-render/client/sync-module/client.tsx
@@ -1,25 +1,8 @@
 'use client'
 import * as React from 'react'
-import { once, oncePerRender } from '../once'
+import { once } from '../once'
 
-// TODO: this should always be `once`, but it's not.
-//
-// We need this workaround in dev, because `spawnDynamicValidationInDev` is raced with the actual render.
-// The problem is that `trackDynamicImport` relies on `workUnitStore.cacheSignal`,
-// but the validation render might get here first, and then we'd
-//   1. invoke the import without a `cacheSignal` (because the store type is 'render')
-//   2. *cache that promise forever*,
-// meaning that when we get here again in the validation render,
-// we won't track the promise on the cache signal (because `once` already cached the result).
-//
-// The workaround works by simulating `React.cache()`, i.e. creating the value once per render
-// (or rather, once per workUnitStore),
-// which means that we'll invoke the import again in the validation render and track it properly.
-//
-// The proper fix is to handle tracking `import()` that happen without a `cacheSignal` available,
-// which will be done in a follow-up PR.
-const onceImpl = process.env.NODE_ENV === 'development' ? oncePerRender : once
-const doImport = onceImpl(() => import('./messages'))
+const doImport = once(() => import('./messages'))
 
 export function Client() {
   const messages = React.use(doImport()).default
