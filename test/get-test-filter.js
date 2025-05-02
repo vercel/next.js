2f2838603c8d3aee6a73e629dfeacfe318b4fa7b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-05-02 12:17:16 +0200

    try again

--- test/get-test-filter.js
+++ test/get-test-filter.js
@@ -2,43 +2,46 @@ const path = require('path')
 const minimatch = require('minimatch')
 
 function getTestFilter() {
-  const manifest = process.env.NEXT_EXTERNAL_TESTS_FILTERS
-    ? require(path.resolve(process.env.NEXT_EXTERNAL_TESTS_FILTERS))
-    : null
-  if (!manifest) return null
+  const manifests = process.env.NEXT_EXTERNAL_TESTS_FILTERS
+    ? process.env.NEXT_EXTERNAL_TESTS_FILTERS.trim()
+        .split(' ')
+        .map((f) => require(path.resolve(f)))
+    : []
+  if (manifests.length === 0) return null
 
   console.log(
     'Filtering tests using manifest:',
     process.env.NEXT_EXTERNAL_TESTS_FILTERS
   )
 
-  // For the legacy manifest without a version, we assume it's a complete list
-  // of all the tests.
-  if (!manifest.version || typeof manifest.version !== 'number') {
-    return (tests) =>
-      tests
-        .filter((test) => {
-          const info = manifest[test.file]
-          // Include tests that are not in the manifest
-          return !info || !info.runtimeError
-        })
-        .map((test) => {
-          const info = manifest[test.file]
-          // Exclude failing and flakey tests, newly added tests are automatically included
-          if (info && (info.failed.length > 0 || info.flakey.length > 0)) {
-            test.excludedCases = info.failed.concat(info.flakey)
-          }
-          return test
-        })
-  }
+  // // For the legacy manifest without a version, we assume it's a complete list
+  // // of all the tests.
+  // if (!manifest.version || typeof manifest.version !== 'number') {
+  //   return (tests) =>
+  //     tests
+  //       .filter((test) => {
+  //         const info = manifest[test.file]
+  //         // Include tests that are not in the manifest
+  //         return !info || !info.runtimeError
+  //       })
+  //       .map((test) => {
+  //         const info = manifest[test.file]
+  //         // Exclude failing and flakey tests, newly added tests are automatically included
+  //         if (info && (info.failed.length > 0 || info.flakey.length > 0)) {
+  //           test.excludedCases = info.failed.concat(info.flakey)
+  //         }
+  //         return test
+  //       })
+  // }
 
   // The new manifest version 2 only contains the list of tests that should
   // be run, with exclusions added based on rules. Any new tests that are added
   // will be automatically included if they match the include rules.
-  if (manifest.version === 2) {
-    return (tests) =>
-      tests
-        .filter((test) => {
+  // if (manifest.version === 2) {
+  return (tests) =>
+    tests
+      .filter((test) =>
+        manifests.every((manifest) => {
           // Check to see if this was included as-is in the manifest.
           if (test.file in manifest.suites) return true
 
@@ -65,13 +68,15 @@ function getTestFilter() {
           // Otherwise, it should be included.
           return true
         })
-        .map((test) => {
+      )
+      .map((test) => {
+        for (let manifest of manifests) {
           const info = manifest.suites[test.file]
 
           // If there's no info for this test, then it's a test that has no
           // failures or flakey tests, so we can just include it as-is.
           if (!info) {
-            return test
+            continue
           }
 
           // Exclude failing and flakey tests, newly added tests are
@@ -80,12 +85,13 @@ function getTestFilter() {
           if (failed.length > 0 || flakey.length > 0) {
             test.excludedCases = failed.concat(flakey)
           }
+        }
 
-          return test
-        })
-  }
+        return test
+      })
+  // }
 
-  throw new Error(`Unknown manifest version: ${manifest.version}`)
+  // throw new Error(`Unknown manifest version: ${manifest.version}`)
 }
 
 module.exports = { getTestFilter }
