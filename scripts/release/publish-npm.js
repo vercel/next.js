1494dadc1f2a2d789e8fc71bc814fb066144f5ed
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-04 03:05:38 +0900

    npm publish feature flag based on commit message

--- scripts/release/publish-npm.js
+++ scripts/release/publish-npm.js
@@ -1,45 +1,30 @@
 // @ts-check
-const path = require('path')
-const { exec } = require('./utils')
+const { exec, getTag } = require('./utils')
 
-async function getTag() {
-  let isCanary = true
-  let isReleaseCandidate = false
+/** @param {boolean} isDryRun */
+async function main(isDryRun) {
+  const tag = getTag(process.cwd())
+  console.log(`Publishing ${tag}`)
+
+  // When running `pnpm recursive publish`, pnpm will
+  // publish all the packages that have versions not
+  // yet published to the registry.
+  // x-ref: https://pnpm.io/9.x/cli/recursive
+  //
+  // This differs from running `pnpm publish --recursive`
+  // where pnpm will try to publish ALL packages.
+  let command = `pnpm recursive publish --tag ${tag}`
+
+  if (isDryRun) {
+    command += ' --dry-run'
+  }
 
   try {
-    const tagOutput = await exec(
-      `node ${path.join(__dirname, 'check-is-release.js')}`
-    )
-    console.log({ tagOutput })
-
-    if (tagOutput.trim().startsWith('v')) {
-      isCanary = tagOutput.includes('-canary')
-    }
-    isReleaseCandidate = tagOutput.includes('-rc')
+    await exec(command)
   } catch (err) {
-    console.log(err)
-
-    if (err.message && err.message.includes('no tag exactly matches')) {
-      console.log('Nothing to publish, exiting...')
-      process.exit(0)
-    }
-    throw err
+    // TODO: Notify via Slack.
+    console.error(err)
   }
-  console.log(
-    `Publishing ${isCanary ? 'canary' : isReleaseCandidate ? 'rc' : 'stable'}`
-  )
-
-  if (isCanary) {
-    return 'canary'
-  }
-  if (isReleaseCandidate) {
-    return 'rc'
-  }
-}
-
-async function main() {
-  const tag = await getTag()
-  await exec(`pnpm publish --recursive --tag ${tag}`)
 }
 
 // TODO: Uncomment when replacing legacy release.
--- scripts/release/utils.js
+++ scripts/release/utils.js
@@ -1,8 +1,38 @@
+// @ts-check
+const fs = require('fs')
+const path = require('path')
 const execa = require('execa')
+const cp = require('child_process')
 
 export function exec(command) {
   return execa(command, {
     stdio: 'inherit',
     shell: true,
   })
+}
+
+export function getTag(cwd) {
+  const preJsonPath = path.join(cwd, '.changeset', 'pre.json')
+  if (fs.existsSync(preJsonPath)) {
+    const preJson = require(preJsonPath)
+    if (preJson.tag) {
+      return preJson.tag
+    }
+  }
+
+  return 'latest'
+}
+
+/** @returns { { isNewRelease: boolean, isDryRun: boolean } } */
+export function checkIsNewRelease() {
+  const commitMsg = cp
+    .execSync(`git log -n 1 --pretty='format:%B'`)
+    .toString()
+    .trim()
+
+  // "Version Packages" or "Version Packages (dry)"
+  const publishMsgRegex = /^Version Packages( \(dry\))?$/
+  const isNewRelease = publishMsgRegex.test(commitMsg)
+  const isDryRun = isNewRelease && commitMsg.includes('(dry)')
+  return { isNewRelease, isDryRun }
 }
