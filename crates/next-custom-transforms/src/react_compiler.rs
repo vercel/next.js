04176decbefde629e2490751b984bdc48c19b96f
JJ Kasper <jj@jjsweb.site>
2025-05-06 11:52:20 -0700

    fix(react-compiler): Fix detection of interest (#78879)
    Backport of #78874 to 15.3.x
    
    Co-authored-by: Donny/강동윤 <kdy1997.dev@gmail.com>

--- crates/next-custom-transforms/src/react_compiler.rs
+++ crates/next-custom-transforms/src/react_compiler.rs
@@ -1,5 +1,5 @@
 use swc_core::ecma::{
-    ast::{Callee, Expr, FnDecl, FnExpr, Program, ReturnStmt},
+    ast::{Callee, Expr, FnDecl, FnExpr, Pat, Program, ReturnStmt, VarDeclarator},
     visit::{Visit, VisitWith},
 };
 
@@ -67,5 +67,20 @@ impl Visit for Finder {
         }
 
         node.visit_children_with(self);
+    }
+
+    fn visit_var_declarator(&mut self, node: &VarDeclarator) {
+        let old = self.is_interested;
+
+        if let Pat::Ident(ident) = &node.name {
+            self.is_interested = ident.sym.starts_with("use")
+                || ident.sym.starts_with(|c: char| c.is_ascii_uppercase());
+        } else {
+            self.is_interested = false;
+        }
+
+        node.visit_children_with(self);
+
+        self.is_interested = old;
     }
 }
