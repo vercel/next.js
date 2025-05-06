7dc0843abe4a2c0dd3dd8614ede09964bc6adfb9
Jiachi Liu <inbox@huozhi.im>
2025-05-06 13:56:10 +0200

    drop test

--- test/development/app-dir/segment-viewer/segment-viewer.test.ts
+++
@@ -1,8 +0,0 @@
-// import { nextTestSetup } from 'e2e-utils'
-
-// TODO(segment-viewer): enable test once stabilized
-describe.skip('segment-viewer', () => {
-  // const { next } = nextTestSetup({
-  //   files: __dirname,
-  // })
-})
