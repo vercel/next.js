210ce11dc53f7788da7e60d08fd27c6df6f5c386
Andrew Clark <git@andrewclark.io>
2025-05-07 14:03:35 -0400

    [Fix] Inverse prefetch segment for Pages routes
    Another follow-up to #78387 and #78568. Refer to those PRs for
    full details.
    
    Fixes a routing conflict related to per-segment prefetches in sites
    that use both App Router and Pages Router.
    
    The fix is to expand the behavior added in the previous PRs — we output
    an "inverse" tree prefetch route for every route in the route manifest
    that is not PPR-enabled, including ones defined by Pages Router.

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -3557,31 +3557,20 @@ export default async function build(
               ...routesManifest.staticRoutes,
               ...routesManifest.dynamicRoutes,
             ]) {
-              // We only want to handle pages that are using the app router. We
-              // need this path in order to determine if it's an app route or an
-              // app page.
-              const originalAppPath = pageInfos.get(route.page)?.originalAppPath
-              if (!originalAppPath) {
-                continue
-              }
-
-              // We only want to handle app pages, not app routes.
-              if (isAppRouteRoute(originalAppPath)) {
-                continue
-              }
+              // If the segment paths aren't defined, we need to insert a
+              // reverse routing rule so that there isn't any conflicts
+              // with other dynamic routes for the prefetch segment
+              // routes. This is true for any route that is not PPR-enabled,
+              // including all routes defined by Pages Router.
 
               // We don't need to add the prefetch segment data routes if it was
               // added due to a page that was already generated. This would have
               // happened if the page was static or partially static.
-              if (route.prefetchSegmentDataRoutes) {
+              const originalAppPath = pageInfos.get(route.page)?.originalAppPath
+              if (originalAppPath && route.prefetchSegmentDataRoutes) {
                 continue
               }
 
-              // If the segment paths aren't defined, we need to insert a
-              // reverse routing rule so that there isn't any conflicts
-              // with other dynamic routes for the prefetch segment
-              // routes. This is only an issue for pages that do not have
-              // partial prerendering enabled.
               route.prefetchSegmentDataRoutes = [
                 buildInversePrefetchSegmentDataRoute(
                   route.page,
