af0473b7f43bbabe0b327ec65348bf85610a1153
JJ Kasper <jj@jjsweb.site>
2025-05-08 15:54:46 -0700

    Fix tracing of server actions imported by client components (#78968)
    This fixes us failing to trace actions imported by client components due
    to the transform removing the actions import so our tracing isn't able
    to discover the action import correctly. This was previously handled in
    https://github.com/vercel/next.js/pull/73710/files#diff-f39646a8a89ce97754025c8258dc0709f67e4340c07735a3a82a87595ba01c10L526
    but removed as part of the experiment.
    
    A regression test for the `use client` importing an actions file with
    file needing to be traced has been added.

--- packages/next/src/build/webpack-config.ts
+++ packages/next/src/build/webpack-config.ts
@@ -1962,6 +1962,7 @@ export default async function getBaseWebpackConfig(
             appDirEnabled: hasAppDir,
             traceIgnores: [],
             compilerType,
+            swcLoaderConfig: swcDefaultLoader,
           }
         ),
       // Moment.js is an extremely popular library that bundles large locale files
