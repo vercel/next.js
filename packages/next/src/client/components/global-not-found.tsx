c38a471af27e82d8b80479cb17433d990eae4605
Jiachi Liu <inbox@huozhi.im>
2025-05-08 11:37:12 +0200

    reuse component

--- packages/next/src/client/components/global-not-found.tsx
+++ packages/next/src/client/components/global-not-found.tsx
@@ -1,43 +1,13 @@
-import { styles } from './styles/access-error-styles'
-
-const status = 404
-const message = 'This page could not be found.'
+import { HTTPAccessErrorFallback } from './http-access-fallback/error-fallback'
 
 function GlobalNotFound() {
   return (
     <html>
       <body>
-        <head>
-          <title>{`${status}: ${message}`}</title>
-        </head>
-        <div style={styles.error}>
-          <div>
-            <style
-              dangerouslySetInnerHTML={{
-                /* Minified CSS from
-                body { margin: 0; color: #000; background: #fff; }
-                .next-error-h1 {
-                  border-right: 1px solid rgba(0, 0, 0, .3);
-                }
-
-                @media (prefers-color-scheme: dark) {
-                  body { color: #fff; background: #000; }
-                  .next-error-h1 {
-                    border-right: 1px solid rgba(255, 255, 255, .3);
-                  }
-                }
-              */
-                __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
-              }}
-            />
-            <h1 className="next-error-h1" style={styles.h1}>
-              {status}
-            </h1>
-            <div style={styles.desc}>
-              <h2 style={styles.h2}>{message}</h2>
-            </div>
-          </div>
-        </div>
+        <HTTPAccessErrorFallback
+          status={404}
+          message={'This page could not be found.'}
+        />
       </body>
     </html>
   )
