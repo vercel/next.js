6ef003f9c87b10410d6d700f831a858baeb0b4d4
Hendrik Liebau <mail@hendrik-liebau.de>
2025-05-11 19:25:19 +0200

    Introduce `DynamicAccessStorage`

--- packages/next/src/build/webpack-config.ts
+++ packages/next/src/build/webpack-config.ts
@@ -133,7 +133,7 @@ const browserNonTranspileModules = [
 const precompileRegex = /[\\/]next[\\/]dist[\\/]compiled[\\/]/
 
 const asyncStoragesRegex =
-  /next[\\/]dist[\\/](esm[\\/])?server[\\/]app-render[\\/](work-async-storage|action-async-storage|work-unit-async-storage)/
+  /next[\\/]dist[\\/](esm[\\/])?server[\\/]app-render[\\/](work-async-storage|action-async-storage|dynamic-access-async-storage|work-unit-async-storage)/
 
 // Support for NODE_PATH
 const nodePathList = (process.env.NODE_PATH || '')
