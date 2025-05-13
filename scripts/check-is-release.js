fce3e9b6725e0bd053656af883b67ae6feaa0a7b
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-13 19:20:54 +0200

    new publish msg regex to add release type

--- scripts/check-is-release.js
+++ scripts/check-is-release.js
@@ -13,7 +13,8 @@ const checkIsRelease = async () => {
 
   const versionString = commitMsg.split(' ').pop().trim()
   const publishMsgRegex = /^v\d{1,}\.\d{1,}\.\d{1,}(-\w{1,}\.\d{1,})?$/
-  const newPublishMsgRegex = /^Version Packages$/
+  const newPublishMsgRegex =
+    /^Version Packages \((canary|release-candidate|stable)\)$/
 
   if (publishMsgRegex.test(versionString)) {
     console.log(versionString)
