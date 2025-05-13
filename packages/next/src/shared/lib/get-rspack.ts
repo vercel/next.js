c82af22ce7e10f28cdc878ef3e40e92a84b7a4c1
Cong-Cong <dacongsama@live.com>
2025-05-13 08:58:06 +0800

    prettier

--- packages/next/src/shared/lib/get-rspack.ts
+++ packages/next/src/shared/lib/get-rspack.ts
@@ -44,7 +44,7 @@ export function getRspackReactRefresh() {
 
 function warnRspack() {
   if (process.env.__NEXT_TEST_MODE) {
-    return;
+    return
   }
   warnOnce(
     `\`next-rspack\` is currently experimental. It's not an official Next.js plugin, and is supported by the Rspack team in partnership with Next.js. Help improve Next.js and Rspack by providing feedback at https://github.com/vercel/next.js/discussions/77800`
