8d5d3013e0e6b89ebac2a76b882261a373e231a9
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-14 12:07:57 +0200

    add type alias to action test

--- test/development/typescript-plugin/client-boundary/app/non-serializable-action-props.tsx
+++ test/development/typescript-plugin/client-boundary/app/non-serializable-action-props.tsx
@@ -2,13 +2,17 @@
 
 class Class {}
 
+type ArrowFunctionTypeAlias = () => void
+
 export default function ClientComponent({
   _arrowFunctionAction,
+  _arrowFunctionTypeAliasAction,
   // Doesn't make sense, but check for loophole
   _classAction,
   _constructorAction,
 }: {
   _arrowFunctionAction: () => void
+  _arrowFunctionTypeAliasAction: ArrowFunctionTypeAlias
   // Doesn't make sense, but check for loophole
   _classAction: Class
   _constructorAction: new () => void
