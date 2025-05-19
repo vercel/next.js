26661844f612441c11e00dd76f60e12399fc9e2e
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-05-19 11:00:08 +0200

    Update snapshots

--- turbopack/crates/turbopack-tests/tests/snapshot/css/css-legacy-nesting/output/b1abf_turbopack-tests_tests_snapshot_css_css-legacy-nesting_input_index_49d4edb8.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/css-legacy-nesting/output/b1abf_turbopack-tests_tests_snapshot_css_css-legacy-nesting_input_index_49d4edb8.js
@@ -3,11 +3,10 @@
 "[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/css-legacy-nesting/input/index.js [test] (ecmascript)": ((__turbopack_context__) => {
 "use strict";
 
-{
 __turbopack_context__.s({});
 ;
 console.log('css-legacy-nesting');
-}}),
+}),
 }]);
 
 //# sourceMappingURL=b1abf_turbopack-tests_tests_snapshot_css_css-legacy-nesting_input_index_49d4edb8.js.map
