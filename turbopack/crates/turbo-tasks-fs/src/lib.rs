22ab1ca09922a64dff21e21f82467f4e4af6276d
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-19 07:28:31 -0700

    let else

--- turbopack/crates/turbo-tasks-fs/src/lib.rs
+++ turbopack/crates/turbo-tasks-fs/src/lib.rs
@@ -777,32 +777,31 @@ impl FileSystem for DiskFileSystem {
                         use std::io::Write;
 
                         let mut f = std::fs::File::create(full_path)?;
-                        if let FileContent::Content(file) = &*content {
+                        let FileContent::Content(file) = &*content else {
+                            unreachable!()
+                        };
+                        std::io::copy(&mut file.read(), &mut f)?;
+                        #[cfg(target_family = "unix")]
+                        f.set_permissions(file.meta.permissions.into())?;
+                        f.flush()?;
+                        #[cfg(feature = "write_version")]
+                        {
+                            let mut full_path = full_path.into_owned();
+                            let hash = hash_xxh3_hash64(file);
+                            let ext = full_path.extension();
+                            let ext = if let Some(ext) = ext {
+                                format!("{:016x}.{}", hash, ext.to_string_lossy())
+                            } else {
+                                format!("{:016x}", hash)
+                            };
+                            full_path.set_extension(ext);
+                            let mut f = std::fs::File::create(&full_path)?;
                             std::io::copy(&mut file.read(), &mut f)?;
                             #[cfg(target_family = "unix")]
                             f.set_permissions(file.meta.permissions.into())?;
                             f.flush()?;
-                            #[cfg(feature = "write_version")]
-                            {
-                                let mut full_path = full_path.into_owned();
-                                let hash = hash_xxh3_hash64(file);
-                                let ext = full_path.extension();
-                                let ext = if let Some(ext) = ext {
-                                    format!("{:016x}.{}", hash, ext.to_string_lossy())
-                                } else {
-                                    format!("{:016x}", hash)
-                                };
-                                full_path.set_extension(ext);
-                                let mut f = std::fs::File::create(&full_path)?;
-                                std::io::copy(&mut file.read(), &mut f)?;
-                                #[cfg(target_family = "unix")]
-                                f.set_permissions(file.meta.permissions.into())?;
-                                f.flush()?;
-                            }
-                            Ok::<(), io::Error>(())
-                        } else {
-                            unreachable!()
                         }
+                        Ok::<(), io::Error>(())
                     })
                     .concurrency_limited(&inner.semaphore)
                     .instrument(tracing::info_span!(
