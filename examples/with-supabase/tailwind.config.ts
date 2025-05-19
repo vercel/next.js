a9cf59ab38993b20243b96ad0c270e76fede474c
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-05-19 17:45:06 +0200

    Minor fixes to the tailwind setup.

--- examples/with-supabase/tailwind.config.ts
+++ examples/with-supabase/tailwind.config.ts
@@ -6,6 +6,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
