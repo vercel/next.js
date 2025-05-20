fbe6516b268bbace0cb4ac1c7ff4aa0ba47843b5
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-20 13:33:56 -0700

    pnpm install

--- turbopack/crates/turbopack-node/build.rs
+++ turbopack/crates/turbopack-node/build.rs
@@ -5,6 +5,17 @@ use turbo_tasks_build::generate_register;
 fn main() {
     generate_register();
 
+    // Run pnpm install
+    let status = Command::new("pnpm")
+        .args(["install"])
+        .current_dir("js")
+        .status()
+        .expect("failed to run pnpm install");
+
+    if !status.success() {
+        panic!("pnpm install failed to run");
+    }
+
     // Run pnpm webpack
     let status = Command::new("pnpm")
         .args(["webpack"])
