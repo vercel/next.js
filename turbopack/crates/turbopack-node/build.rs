0f2751629d8688894d00fa0fd5834a2533091418
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-20 11:43:07 -0700

    build script

--- turbopack/crates/turbopack-node/build.rs
+++ turbopack/crates/turbopack-node/build.rs
@@ -1,5 +1,18 @@
+use std::process::Command;
+
 use turbo_tasks_build::generate_register;
 
 fn main() {
     generate_register();
+
+    // Run pnpm webpack
+    let status = Command::new("pnpm")
+        .args(["webpack"])
+        .current_dir("js")
+        .status()
+        .expect("failed to run webpack");
+
+    if !status.success() {
+        panic!("webpack failed to run");
+    }
 }
