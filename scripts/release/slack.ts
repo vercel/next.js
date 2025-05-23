77e501a468808f203e13cc857d52c8fd6c8ae9ff
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-23 17:04:08 +0200

    update to take args
    Co-authored-by: Sebastian "Sebbie" Silbermann <sebastian.silbermann@vercel.com>

--- scripts/release/slack.ts
+++ scripts/release/slack.ts
@@ -2,22 +2,27 @@ import { WebClient } from '@slack/web-api'
 
 async function slack() {
   if (!process.env.SLACK_TOKEN) {
-    throw new TypeError('SLACK_TOKEN not set')
+    throw new Error('SLACK_TOKEN not set')
   }
 
   const workflowLink = process.env.WORKFLOW_LINK
   if (!workflowLink) {
-    throw new TypeError('WORKFLOW_LINK not set')
+    throw new Error('WORKFLOW_LINK not set')
   }
 
+  const channel = process.env.SLACK_CHANNEL_ID ?? 'C0668R2391V' // #next-releases
+  // TODO: Configure SLACK_ envs for dry run testing to not ping the oncall.
+  // TODO: It'll become `@oncall-nextjs`
+  const ping = process.env.SLACK_PING ?? '@nextjs-oncall'
+
   const message =
-    process.env.RELEASE_STATUS === 'true'
+    process.env.SLACK_MESSAGE ?? process.env.RELEASE_STATUS === 'true'
       ? `Successfully published a new release!\n<https://github.com/vercel/next.js/releases|Releases Link>`
-      : `Failed to publish a new release triggered by "${process.env.WORKFLOW_ACTOR}". @nextjs-oncall\n<${workflowLink}|Workflow Link>`
+      : `Failed to publish a new release triggered by "${process.env.WORKFLOW_ACTOR}". ${ping}\n<${workflowLink}|Workflow Link>`
 
   const slackClient = new WebClient(process.env.SLACK_TOKEN)
   const res = await slackClient.chat.postMessage({
-    channel: '#next-releases',
+    channel,
     text: message,
     username: 'Next.js Bot',
     icon_emoji: ':nextjs:',
