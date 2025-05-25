1d2d19c28e19bf8ec28f14ac220838160bde89c6
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-05-25 15:22:02 +0200

    Upgrade React from `c4676e72-20250520` to `23884812-20250520` (#79467)
    Co-authored-by: Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>

--- packages/next/src/compiled/react-experimental/cjs/react.development.js
+++ packages/next/src/compiled/react-experimental/cjs/react.development.js
@@ -1325,7 +1325,7 @@
     exports.useTransition = function () {
       return resolveDispatcher().useTransition();
     };
-    exports.version = "19.2.0-experimental-c4676e72-20250520";
+    exports.version = "19.2.0-experimental-23884812-20250520";
     "undefined" !== typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ &&
       "function" ===
         typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.registerInternalModuleStop &&
