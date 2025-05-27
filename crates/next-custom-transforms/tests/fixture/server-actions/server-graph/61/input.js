b283cf3d17a180c722fe0ccfeec195287f79a670
Hendrik Liebau <mail@hendrik-liebau.de>
2025-05-27 12:53:03 +0200

    Use function expression and arrow function examples

--- crates/next-custom-transforms/tests/fixture/server-actions/server-graph/61/input.js
+++ crates/next-custom-transforms/tests/fixture/server-actions/server-graph/61/input.js
@@ -1,9 +1,26 @@
-export function Component({ list, y }) {
+export function ComponentA({ list, y }) {
   return (
     <form
       action={async () => {
         'use server'
         console.log(list.find((x) => x === y))
+      }}
+    >
+      <button>submit</button>
+    </form>
+  )
+}
+
+export function ComponentB({ list, y }) {
+  return (
+    <form
+      action={async () => {
+        'use server'
+        console.log(
+          list.find(function (x) {
+            return x === y
+          })
+        )
       }}
     >
       <button>submit</button>
