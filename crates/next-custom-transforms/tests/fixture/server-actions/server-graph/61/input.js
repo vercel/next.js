a417f0931ffa290dea340ab741a77a5315f61d79
Hendrik Liebau <mail@hendrik-liebau.de>
2025-05-27 12:48:05 +0200

    Update test case

--- crates/next-custom-transforms/tests/fixture/server-actions/server-graph/61/input.js
+++ crates/next-custom-transforms/tests/fixture/server-actions/server-graph/61/input.js
@@ -1,9 +1,9 @@
-export function Component({ list }) {
+export function Component({ list, y }) {
   return (
     <form
       action={async () => {
         'use server'
-        console.log(list.find((x) => !!x))
+        console.log(list.find((x) => x === y))
       }}
     >
       <button>submit</button>
