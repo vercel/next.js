2b7a259169b85d82643b95eaf8d0601f1a9f3e7e
Ivan Vasilov <vasilov.ivan@gmail.com>
2025-05-28 19:19:55 +0200

    chore: Update the `with-supabase` example to use the Supabase Auth block (#78913)
    This PR updates the `with-supabase` template to better auth pages,
    Tailwind v4 and newest `components.json` for full compatibility with
    shadcn/ui.
    
    The `(auth-pages)` folder has been removed. For auth, the template is
    now using the [password based auth
    block](https://supabase.com/ui/docs/nextjs/password-based-auth) from the
    Supabase UI Library which makes it easier to update it and keep it
    up-to-date.
    
    ---------
    
    Co-authored-by: Lee Robinson <lee@leerob.com>

--- examples/with-supabase/app/protected/layout.tsx
+++ examples/with-supabase/app/protected/layout.tsx
@@ -1,14 +1,15 @@
 import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
-export default function Home() {
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -24,11 +25,7 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
+          {children}
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
