2233d58f19bb0c915b8138b8de856c3fc1d00b6e
Jiachi Liu <inbox@huozhi.im>
2025-06-03 18:21:04 +0200

    simplify the regex to google

--- packages/next/src/shared/lib/router/utils/is-bot.ts
+++ packages/next/src/shared/lib/router/utils/is-bot.ts
@@ -1,8 +1,9 @@
 import { HTML_LIMITED_BOT_UA_RE } from './html-bots'
 
-// Bot crawler that will spin up a headless browser and execute JS
-const HEADLESS_BROWSER_BOT_UA_RE =
-  /Googlebot|Google-PageRenderer|AdsBot-Google|googleweblight|Storebot-Google|Google-InspectionTool|Google-CloudVertexBot|GoogleOther/i
+// Bot crawler that will spin up a headless browser and execute JS.
+// By default, only googlebots are considered as DOM bots. Blow is where the regex is computed from:
+// x-ref: https://developers.google.com/search/docs/crawling-indexing/google-common-crawlers
+const HEADLESS_BROWSER_BOT_UA_RE = /google/i
 
 export const HTML_LIMITED_BOT_UA_RE_STRING = HTML_LIMITED_BOT_UA_RE.source
 
