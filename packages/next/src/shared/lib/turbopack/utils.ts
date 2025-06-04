1a18884755973acb4d68ac3989971ef81e2876bb
Tim Neutkens <tim@timneutkens.nl>
2025-06-04 14:45:21 +0200

    Turbopack Build: Implement helpful error for missing sass package (#80155)
    ## What?
    
    Implements the same error message that we add to the webpack handling. I'm not super happy about this solution (though it matches what we do for webpack) ideally we improve the sass-loader error itself in the future but that requires upgrading sass-loader as well and introducing a breaking change. For now this unblocks the test passing.

--- packages/next/src/shared/lib/turbopack/utils.ts
+++ packages/next/src/shared/lib/turbopack/utils.ts
@@ -190,7 +190,17 @@ export function formatIssue(issue: Issue) {
   }
 
   if (description) {
-    message += renderStyledStringToErrorAnsi(description) + '\n\n'
+    if (
+      description.type === 'text' &&
+      description.value.includes(`Cannot find module 'sass'`)
+    ) {
+      message +=
+        "To use Next.js' built-in Sass support, you first need to install `sass`.\n"
+      message += 'Run `npm i sass` or `yarn add sass` inside your workspace.\n'
+      message += '\nLearn more: https://nextjs.org/docs/messages/install-sass'
+    } else {
+      message += renderStyledStringToErrorAnsi(description) + '\n\n'
+    }
   }
 
   // TODO: make it possible to enable this for debugging, but not in tests.
