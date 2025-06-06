73a90a1d47b7676018dcf59ba8315e3a4a4298a1
Janka Uryga <lolzatu2@gmail.com>
2025-06-06 19:50:16 +0200

    [webpack] generate next/root-params

--- packages/next/src/build/webpack/loaders/next-invalid-import-error-loader.ts
+++ packages/next/src/build/webpack/loaders/next-invalid-import-error-loader.ts
@@ -1,4 +1,11 @@
-export default function nextInvalidImportErrorLoader(this: any) {
-  const { message } = this.getOptions()
-  throw new Error(message)
-}
+import type webpack from 'webpack'
+
+export type InvalidImportLoaderOpts = { message: string }
+
+const nextInvalidImportErrorLoader: webpack.LoaderDefinitionFunction<InvalidImportLoaderOpts> =
+  function () {
+    const { message } = this.getOptions()
+    throw new Error(message)
+  }
+
+export default nextInvalidImportErrorLoader
