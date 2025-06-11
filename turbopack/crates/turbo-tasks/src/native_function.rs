d93e0580741f9ee0d8c959b3c996d7b7028929f4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-11 07:44:26 -0700

    invalidator flag

--- turbopack/crates/turbo-tasks/src/native_function.rs
+++ turbopack/crates/turbo-tasks/src/native_function.rs
@@ -144,6 +144,10 @@ pub struct FunctionMeta {
     /// task-local state. The function call itself will not be cached, but cells will be created on
     /// the parent task.
     pub local: bool,
+
+    /// If true, the function will be allowed to call `get_invalidator` . If this is false, the
+    /// `get_invalidator` function will panic on calls.
+    pub invalidator: bool,
 }
 
 /// A native (rust) turbo-tasks function. It's used internally by
