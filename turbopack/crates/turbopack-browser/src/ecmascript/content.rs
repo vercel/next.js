8c68a215917216fe07d90a471e5c88d3355706b4
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-06-12 13:07:13 -0700

    rayon for minify()

--- turbopack/crates/turbopack-browser/src/ecmascript/content.rs
+++ turbopack/crates/turbopack-browser/src/ecmascript/content.rs
@@ -128,7 +128,7 @@ impl EcmascriptBrowserChunkContent {
         let mut code = code.build();
 
         if let MinifyType::Minify { mangle } = this.chunking_context.await?.minify_type() {
-            code = minify(code, source_maps, mangle)?;
+            code = minify(code, source_maps, mangle).await?;
         }
 
         Ok(code.cell())
