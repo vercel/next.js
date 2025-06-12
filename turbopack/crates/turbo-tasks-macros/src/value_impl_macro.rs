a0cd350efee499456c43d4da8a3aa6fc957dc1b7
Donny/강동윤 <kdy.1997.dev@gmail.com>
2025-06-12 15:06:17 -0700

    Revert "refactor(turbopack): Make invalidator flag explicit (#80414)"
    This reverts commit a720ef8e056d6dcea8bc5ed8f34f43968d3a0c0d.

--- turbopack/crates/turbo-tasks-macros/src/value_impl_macro.rs
+++ turbopack/crates/turbo-tasks-macros/src/value_impl_macro.rs
@@ -84,7 +84,6 @@ pub fn value_impl(args: TokenStream, input: TokenStream) -> TokenStream {
                     continue;
                 };
                 let local = func_args.local.is_some();
-                let invalidator = func_args.invalidator.is_some();
                 let is_self_used = func_args.operation.is_some() || is_self_used(block);
 
                 let Some(turbo_fn) =
@@ -107,7 +106,6 @@ pub fn value_impl(args: TokenStream, input: TokenStream) -> TokenStream {
                     is_self_used,
                     filter_trait_call_args: None, // not a trait method
                     local,
-                    invalidator,
                 };
 
                 let native_function_ident = get_inherent_impl_function_ident(ty_ident, ident);
@@ -193,7 +191,6 @@ pub fn value_impl(args: TokenStream, input: TokenStream) -> TokenStream {
                 };
 
                 let local = func_args.local.is_some();
-                let invalidator = func_args.invalidator.is_some();
                 let is_self_used = func_args.operation.is_some() || is_self_used(block);
 
                 let Some(turbo_fn) =
@@ -226,7 +223,6 @@ pub fn value_impl(args: TokenStream, input: TokenStream) -> TokenStream {
                     is_self_used,
                     filter_trait_call_args: turbo_fn.filter_trait_call_args(),
                     local,
-                    invalidator,
                 };
 
                 let native_function_ident =
