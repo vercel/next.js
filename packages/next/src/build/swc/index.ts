5e65b3ea5dcffe9aee90ed48f97b79791a2ff299
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2025-06-13 13:36:27 -0700

    remove explicit nullability check

--- packages/next/src/build/swc/index.ts
+++ packages/next/src/build/swc/index.ts
@@ -1077,6 +1077,7 @@ function bindingToApi(
   }
 }
 
+// helper for tryLoadWasmWithFallback / loadBindings.
 async function loadWasm(importPath = '') {
   let attempts = []
   let rawBindings: RawWasmBindings | null = null
@@ -1085,7 +1086,7 @@ async function loadWasm(importPath = '') {
   // unstable and subject to change.
   const testWasmDir = process.env.NEXT_TEST_WASM_DIR
 
-  if (testWasmDir != null) {
+  if (testWasmDir) {
     // assume these are node.js bindings and don't need a call to `.default()`
     rawBindings = await import(
       pathToFileURL(path.join(testWasmDir, 'wasm.js')).toString()
