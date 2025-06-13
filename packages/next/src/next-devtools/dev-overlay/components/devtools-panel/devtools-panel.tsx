d1a4886a1d5a2f360dc9721639343c077a7aa8b8
devjiwonchoi <devjiwonchoi@gmail.com>
2025-06-13 23:34:56 +0200

    [devtools] panel ui header

--- packages/next/src/next-devtools/dev-overlay/components/devtools-panel/devtools-panel.tsx
+++ packages/next/src/next-devtools/dev-overlay/components/devtools-panel/devtools-panel.tsx
@@ -1,6 +1,6 @@
 import type { OverlayDispatch, OverlayState } from '../../shared'
 
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import Draggable from 'next/dist/compiled/react-draggable'
 
 import { Dialog, DialogContent, DialogHeader, DialogBody } from '../dialog'
@@ -9,6 +9,8 @@ import { useFocusTrap } from '../errors/dev-tools-indicator/utils'
 import { useDelayedRender } from '../../hooks/use-delayed-render'
 import { ACTION_DEVTOOLS_PANEL_TOGGLE } from '../../shared'
 import { css } from '../../utils/css'
+import { FullscreenIcon } from '../../icons/fullscreen'
+import { Cross } from '../../icons/cross'
 
 export function DevToolsPanel({
   state,
@@ -17,6 +19,10 @@ export function DevToolsPanel({
   state: OverlayState
   dispatch: OverlayDispatch
 }) {
+  const [activeTab, setActiveTab] = useState<'issues' | 'route' | 'settings'>(
+    'settings'
+  )
+
   const dialogRef = useRef<HTMLDivElement>(null as unknown as HTMLDivElement)
 
   // This hook lets us do an exit animation before unmounting the component
@@ -45,7 +51,42 @@ export function DevToolsPanel({
             onClose={onClose}
           >
             <DialogContent>
-              <DialogHeader></DialogHeader>
+              <DialogHeader>
+                <div data-nextjs-devtools-panel-header>
+                  <div data-nextjs-devtools-panel-tabs>
+                    <button
+                      data-nextjs-devtools-panel-tab={activeTab === 'issues'}
+                      onClick={() => setActiveTab('issues')}
+                    >
+                      Issues <span className="badge">1</span>
+                    </button>
+                    <button
+                      data-nextjs-devtools-panel-tab={activeTab === 'route'}
+                      onClick={() => setActiveTab('route')}
+                    >
+                      Route Info
+                    </button>
+                    <button
+                      data-nextjs-devtools-panel-tab={activeTab === 'settings'}
+                      onClick={() => setActiveTab('settings')}
+                    >
+                      Settings
+                    </button>
+                  </div>
+                  <div data-nextjs-devtools-panel-action-buttons>
+                    {/* TODO: Currently no-op, will add fullscreen toggle. */}
+                    <button data-nextjs-devtools-panel-fullscreen-button>
+                      <FullscreenIcon width={16} height={16} />
+                    </button>
+                    <button
+                      data-nextjs-devtools-panel-close-button
+                      onClick={onClose}
+                    >
+                      <Cross width={16} height={16} />
+                    </button>
+                  </div>
+                </div>
+              </DialogHeader>
               <DialogBody></DialogBody>
             </DialogContent>
           </Dialog>
@@ -81,8 +122,16 @@ export const DEVTOOLS_PANEL_STYLES = css`
     max-width: var(--next-dialog-max-width);
   }
 
-  [data-nextjs-devtools-panel-dialog] {
-    overflow-y: auto;
-    height: 100%;
+  [data-nextjs-devtools-panel-header] {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid var(--color-gray-300);
+    background: var(--color-gray-100);
+  }
+
+  [data-nextjs-devtools-panel-tabs] {
+    display: flex;
+    align-items: center;
   }
 `
