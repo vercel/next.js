4eedffa528cd94885d791829c3a29f56e4cc9a75
devjiwonchoi <devjiwonchoi@gmail.com>
2025-06-14 01:19:37 +0200

    [devtools] panel ui footer

--- packages/next/src/next-devtools/dev-overlay/components/devtools-panel/devtools-panel.tsx
+++ packages/next/src/next-devtools/dev-overlay/components/devtools-panel/devtools-panel.tsx
@@ -11,6 +11,8 @@ import { ACTION_DEVTOOLS_PANEL_TOGGLE } from '../../shared'
 import { css } from '../../utils/css'
 import { FullScreenIcon } from '../../icons/fullscreen'
 import { Cross } from '../../icons/cross'
+import { QuestionIcon } from '../../icons/question'
+import { BugIcon } from '../../icons/bug'
 
 export function DevToolsPanel({
   state,
@@ -101,6 +103,28 @@ export function DevToolsPanel({
               <DialogBody></DialogBody>
             </DialogContent>
           </Dialog>
+          <div data-nextjs-devtools-panel-footer>
+            <div data-nextjs-devtools-panel-footer-tab-group>
+              <div data-nextjs-devtools-panel-footer-tab>
+                <span>Next.js</span>
+                <span>v15.0.0</span>
+              </div>
+              <div data-nextjs-devtools-panel-footer-tab>
+                <span>Next.js</span>
+                <span>v15.0.0</span>
+              </div>
+            </div>
+            <div data-nextjs-devtools-panel-footer-action-button-group>
+              {/* TODO: Add help feature, details TBD */}
+              <button data-nextjs-devtools-panel-footer-action-button>
+                <QuestionIcon width={16} height={16} />
+              </button>
+              {/* TODO: Add debugging/report GitHub issue feature, details TBD */}
+              <button data-nextjs-devtools-panel-footer-action-button>
+                <BugIcon width={16} height={16} />
+              </button>
+            </div>
+          </div>
         </div>
       </Draggable>
     </Overlay>
@@ -211,6 +235,71 @@ export const DEVTOOLS_PANEL_STYLES = css`
 
     &:active {
       background-color: var(--color-gray-300);
+    }
+  }
+
+  [data-nextjs-devtools-panel-footer] {
+    background-color: var(--color-background-200);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: auto;
+    border-top: 1px solid var(--color-gray-400);
+  }
+
+  [data-nextjs-devtools-panel-footer-tab-group] {
+    display: flex;
+    align-items: center;
+  }
+
+  [data-nextjs-devtools-panel-footer-tab] {
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    gap: 8px;
+    align-self: stretch;
+    border-right: 1px solid var(--color-gray-400);
+
+    color: var(--color-gray-900);
+    font-size: 12px;
+    font-family: var(--font-stack-monospace);
+  }
+
+  [data-nextjs-devtools-panel-footer-action-button-group] {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding-right: 8px;
+  }
+
+  [data-nextjs-devtools-panel-footer-action-button] {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 4px;
+    background: var(--color-background-100);
+    background-clip: padding-box;
+    border: 1px solid var(--color-gray-alpha-400);
+    box-shadow: var(--shadow-small);
+    border-radius: var(--rounded-full);
+    color: var(--color-gray-800);
+
+    &:focus {
+      outline: var(--focus-ring);
+    }
+
+    &:not(:disabled):hover {
+      background: var(--color-gray-alpha-100);
+    }
+
+    &:not(:disabled):active {
+      background: var(--color-gray-alpha-200);
+    }
+
+    &:disabled {
+      background-color: var(--color-gray-100);
+      cursor: not-allowed;
     }
   }
 `
