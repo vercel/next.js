76bf9acaac4bbd6ece1a0ce35fd01b7c7feb8a6e
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-06-13 19:39:39 +0200

    commit

--- test/e2e/app-dir/hello-world/app/[teamSlug]/[project]/deployments/page.tsx
+++ test/e2e/app-dir/hello-world/app/[teamSlug]/[project]/deployments/page.tsx
@@ -1,5 +1,29 @@
+
+import type { Metadata } from 'next'
+import { Suspense } from 'react'
+
 export const experimental_ppr = true
 
-export default function DeploymentsPage() {
-  return null
+export const generateMetadata = async (props: any): Promise<Metadata> => {
+  const params = await props.params
+  return {
+    title: {
+      template: `${params.project} – %s – Vercel`,
+      default: `${params.project} - Overview`,
+    },
+  }
+}
+
+async function Suspend({ params }: any) {
+  ;(await params).teamSlug
+
+  return 'Hello, Dave!'
+}
+
+export default function ProjectOverviewLayout({ params }) {
+  return (
+    <Suspense>
+      <Suspend params={params} />
+    </Suspense>
+  )
 }
