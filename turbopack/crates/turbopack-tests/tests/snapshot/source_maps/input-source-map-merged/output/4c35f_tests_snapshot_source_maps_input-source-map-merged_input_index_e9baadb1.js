b1dd4a23eea5ec54af08868523f9d4334f98648f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-14 12:44:26 +0200

    Really set tree_shaking_mode for snapshots

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/output/4c35f_tests_snapshot_source_maps_input-source-map-merged_input_index_e9baadb1.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/4c35f_tests_snapshot_source_maps_input-source-map-merged_input_index_e9baadb1.js", {
+
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/input/index.js [test] (ecmascript)": (function(__turbopack_context__) {
+
+var { m: module, e: exports } = __turbopack_context__;
+{
+const e = new Error("Could not parse module '[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/input/index.js'");
+e.code = 'MODULE_UNPARSEABLE';
+throw e;
+}}),
+}]);
