6a812907d0cb405f9315c031cc464ab7ddc92900
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-14 12:45:03 +0200

    Add snapshot

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/output/2b719_intermediate-tree-shake_rename-side-effect-free-facade_input_index_31a1e558.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/2b719_intermediate-tree-shake_rename-side-effect-free-facade_input_index_31a1e558.js",
+    {},
+    {"otherChunks":["output/457d9_snapshot_intermediate-tree-shake_rename-side-effect-free-facade_input_4936de22._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
---
+++ turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/output/457d9_snapshot_intermediate-tree-shake_rename-side-effect-free-facade_input_4936de22._.js
@@ -0,0 +1,30 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/457d9_snapshot_intermediate-tree-shake_rename-side-effect-free-facade_input_4936de22._.js", {
+
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/input/index.js [test] (ecmascript)": ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s({});
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$intermediate$2d$tree$2d$shake$2f$rename$2d$side$2d$effect$2d$free$2d$facade$2f$input$2f$node_modules$2f$lib$2f$a$2e$js__$5b$test$5d$__$28$ecmascript$2c$__facade$29$__$3c$export__a__as__a0$3e$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/input/node_modules/lib/a.js [test] (ecmascript, facade) <export a as a0>");
+;
+console.log(__TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$intermediate$2d$tree$2d$shake$2f$rename$2d$side$2d$effect$2d$free$2d$facade$2f$input$2f$node_modules$2f$lib$2f$a$2e$js__$5b$test$5d$__$28$ecmascript$2c$__facade$29$__$3c$export__a__as__a0$3e$__["a0"]);
+}),
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/input/node_modules/lib/a.js [test] (ecmascript)": ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s({
+    "a": (()=>a)
+});
+const a = 'a';
+const a_unused = 'a_unused';
+}),
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/input/node_modules/lib/a.js [test] (ecmascript, facade) <export a as a0>": ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s({
+    "a0": (()=>__TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$intermediate$2d$tree$2d$shake$2f$rename$2d$side$2d$effect$2d$free$2d$facade$2f$input$2f$node_modules$2f$lib$2f$a$2e$js__$5b$test$5d$__$28$ecmascript$29$__["a"])
+});
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$intermediate$2d$tree$2d$shake$2f$rename$2d$side$2d$effect$2d$free$2d$facade$2f$input$2f$node_modules$2f$lib$2f$a$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/intermediate-tree-shake/rename-side-effect-free-facade/input/node_modules/lib/a.js [test] (ecmascript)");
+}),
+}]);
+
+//# sourceMappingURL=457d9_snapshot_intermediate-tree-shake_rename-side-effect-free-facade_input_4936de22._.js.map
