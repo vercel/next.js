f4add60998ab079dfb502c0135816d5e8c40a1bf
Janka Uryga <lolzatu2@gmail.com>
2025-06-20 18:16:10 +0200

    [turbopack] fix styled-jsx import message

--- crates/next-core/src/next_shared/resolve.rs
+++ crates/next-core/src/next_shared/resolve.rs
@@ -192,7 +192,7 @@ pub(crate) fn get_invalid_styled_jsx_resolve_plugin(
         root,
         "styled-jsx".into(),
         vec![
-            "'client-only' cannot be imported from a Server Component module. It should only be \
+            "'styled-jsx' cannot be imported from a Server Component module. It should only be \
              used from a Client Component."
                 .into(),
             "The error was caused by using 'styled-jsx'. It only works in a Client Component but \
