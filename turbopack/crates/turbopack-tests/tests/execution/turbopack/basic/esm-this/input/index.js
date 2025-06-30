b52aade3b70bde8bf1315a793943c8dbf0666bc5
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-30 10:02:31 -0700

    remove the intententionally broken test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/esm-this/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/esm-this/input/index.js
@@ -8,7 +8,7 @@ it('`this` in esm should be undefined', () => {
     },
   }
   // Regression test for a bug where we didn't identify the `this` ref in `foo` as being bound.
-  expect(o.foo).toBe(undefined)
+  expect(o.foo).toBe(o)
 })
 
 // Use a dummy export to ensure this is parsed as an esm module
