5ffa7b643e9a0b5acefb69b3a5406aac2f5c9d14
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-30 13:00:36 +0200

    add test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/kysely/dist2/esm/expression/expression-builder.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/kysely/dist2/esm/expression/expression-builder.js
@@ -1,4 +1,7 @@
-import { createFunctionModule } from '../query-builder/function-module.js'
+// import { createFunctionModule } from '../query-builder/function-module.js'
+import { parseSelectAll } from '../parser/select-parser.js'
+
+console.log(parseSelectAll)
 import { freeze } from '../util/object-utils.js'
 
 console.log(createFunctionModule, freeze)
--- turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/kysely/dist2/esm/query-builder/function-module.js
+++
@@ -1,5 +0,0 @@
-import { parseSelectAll } from '../parser/select-parser.js'
-
-console.log(parseSelectAll)
-
-export function createFunctionModule() {}
