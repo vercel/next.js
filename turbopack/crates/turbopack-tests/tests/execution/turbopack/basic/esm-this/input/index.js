bef03f239a9fc1cb9d3ed178fca1b26b7247d52f
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-30 09:56:58 -0700

    Defend against cycles in our execution tests

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/esm-this/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/esm-this/input/index.js
@@ -8,7 +8,7 @@ it('`this` in esm should be undefined', () => {
     },
   }
   // Regression test for a bug where we didn't identify the `this` ref in `foo` as being bound.
-  expect(o.foo).toBe(o)
+  expect(o.foo).toBe(undefined)
 })
 
 // Use a dummy export to ensure this is parsed as an esm module
