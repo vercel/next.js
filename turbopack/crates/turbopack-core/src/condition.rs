a25a45662e4eb2cf32e5f622425c62f9ffc9af15
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-30 16:15:53 -0700

    tweaks

--- turbopack/crates/turbopack-core/src/condition.rs
+++ turbopack/crates/turbopack-core/src/condition.rs
@@ -1,4 +1,3 @@
-use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 use turbo_tasks::{NonLocalValue, trace::TraceRawVcs};
 use turbo_tasks_fs::FileSystemPath;
@@ -32,25 +31,12 @@ impl ContextCondition {
     /// Returns true if the condition matches the context.
     pub fn matches(&self, path: &FileSystemPath) -> bool {
         match self {
-            ContextCondition::All(conditions) => {
-                for condition in conditions {
-                    if !condition.matches(path) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            ContextCondition::Any(conditions) => {
-                for condition in conditions {
-                    if condition.matches(path) {
-                        return true;
-                    }
-                }
-                return false;
-            }
+            ContextCondition::All(conditions) => conditions.iter().all(|c| c.matches(path)),
+            ContextCondition::Any(conditions) => conditions.iter().any(|c| c.matches(path)),
             ContextCondition::Not(condition) => !condition.matches(path),
             ContextCondition::InPath(other_path) => path.is_inside_or_equal_ref(other_path),
             ContextCondition::InDirectory(dir) => {
+                // `dir` must be a substring and bracketd by either `'/'` or the end of the path.
                 if let Some(pos) = path.path.find(dir) {
                     let end = pos + dir.len();
                     (pos == 0 || path.path.as_bytes()[pos - 1] == b'/')
