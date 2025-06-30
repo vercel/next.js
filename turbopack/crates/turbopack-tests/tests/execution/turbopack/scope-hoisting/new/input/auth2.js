d95b9f6e1531472cb400c8b9ab3e08cdb7befdb4
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-30 14:06:55 +0200

    add test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/auth2.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/auth2.js
@@ -1,5 +1,6 @@
 import { freeze } from './src/util/object-utils.js'
+console.log(freeze)
 import { parseExpression } from './src/parser/expression-parser.js'
-console.log(freeze, parseExpression)
+console.log(parseExpression)
 
 export const sql = 123
