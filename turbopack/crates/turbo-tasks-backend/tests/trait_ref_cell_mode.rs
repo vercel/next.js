86d31c5c641ec908ce0803d5938d3fc8de4b7d60
Tobias Koppers <tobias.koppers@googlemail.com>
2025-06-30 08:21:28 +0200

    fix test case

--- turbopack/crates/turbo-tasks-backend/tests/trait_ref_cell_mode.rs
+++ turbopack/crates/turbo-tasks-backend/tests/trait_ref_cell_mode.rs
@@ -135,12 +135,12 @@ where
     Ok(TraitRef::cell(picked_vc.into_trait_ref().await?))
 }
 
-#[turbo_tasks::function]
+#[turbo_tasks::function(invalidator)]
 async fn shared_value_from_input(input: Vc<CellIdSelector>) -> Result<Vc<Box<dyn ValueTrait>>> {
     value_from_input::<SharedValue>(input, Vc::<SharedValue>::cell).await
 }
 
-#[turbo_tasks::function]
+#[turbo_tasks::function(invalidator)]
 async fn new_value_from_input(input: Vc<CellIdSelector>) -> Result<Vc<Box<dyn ValueTrait>>> {
     value_from_input::<NewValue>(input, Vc::<NewValue>::cell).await
 }
