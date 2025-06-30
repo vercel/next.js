bc35ecbbc7d7cd7d7d4cbca787e3cd91c229f91c
Luke Sandberg <lukeisandberg@gmail.com>
2025-06-30 09:25:12 -0700

    Fix a bug in top level `this` analysis
    getter and setter object properties are not `method props`

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/cjs-this/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/cjs-this/input/index.js
@@ -2,6 +2,12 @@ const t = this
 
 it('`this` in cjs should be exports', () => {
   expect(t).toBe(exports)
+  let o = {
+    get foo() {
+      return this
+    },
+  }
+  expect(o.foo).toBe(exports)
 })
 
 // Use a dummy assignment to ensure we are parsed as a cjs module
--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/esm-this/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/esm-this/input/index.js
@@ -2,6 +2,12 @@ const t = this
 
 it('`this` in esm should be undefined', () => {
   expect(t).toBeUndefined()
+  let o = {
+    get foo() {
+      return this
+    },
+  }
+  expect(o.foo).toBeUndefined()
 })
 
 // Use a dummy export to ensure this is parsed as an esm module
