273297ad44d5adb57dd690a0a5e08c819f917d60
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-30 14:19:25 +0200

    add test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/src/parser/expression-parser.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/src/parser/expression-parser.js
@@ -2,5 +2,3 @@ import { expressionBuilder } from '../expression/expression-builder.js'
 console.log(expressionBuilder)
 
 export function parseExpression() {}
-export function parseAliasedExpression() {}
-export function isExpressionOrFactory() {}
--- turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/src/parser/select-parser.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/new/input/src/parser/select-parser.js
@@ -1,5 +1,4 @@
 import { expressionBuilder } from '../expression/expression-builder.js'
 console.log(expressionBuilder)
 
-export function parseSelectArg() {}
 export function parseSelectAll() {}
