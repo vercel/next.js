329b319dd7c1c0b8f4d3d0dd3f6d53fa2736b85a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-07-02 20:19:48 +0900

    Snapshot

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/output/b1abf_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_f69e76d6.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["output/b1abf_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_f69e76d6.js", {
+
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env/input/index.js [test] (ecmascript)": ((__turbopack_context__) => {
+
+var { m: module, e: exports } = __turbopack_context__;
+{
+class Foo {
+}
+console.log(Foo, [].includes('foo'));
+}}),
+}]);
+
+//# sourceMappingURL=b1abf_turbopack-tests_tests_snapshot_swc_transforms_preset_env_input_index_f69e76d6.js.map
