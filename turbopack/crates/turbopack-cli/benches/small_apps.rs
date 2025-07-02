c9c813614dc4c1f71368da11233ffd38d30686e6
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-07-02 11:38:52 +0900

    print thread count

--- turbopack/crates/turbopack-cli/benches/small_apps.rs
+++ turbopack/crates/turbopack-cli/benches/small_apps.rs
@@ -55,6 +55,8 @@ fn bench_small_apps(c: &mut Criterion) {
         * 3
         / 2;
 
+    eprintln!("thread_count: {}", thread_count);
+
     for app in apps {
         g.bench_function(
             BenchmarkId::new("build", app.file_name().unwrap().to_string_lossy()),
