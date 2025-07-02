84708a66457adf55c6c99512646dc49016a53d1d
JJ Kasper <jj@jjsweb.site>
2025-07-01 18:24:10 -0700

    update test

--- test/e2e/app-dir/middleware-rewrite-dynamic/app/layout.tsx
+++ test/e2e/app-dir/middleware-rewrite-dynamic/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
--- test/e2e/app-dir/middleware-rewrite-dynamic/middleware.ts
+++ test/e2e/app-dir/middleware-rewrite-dynamic/middleware.ts
@@ -1,9 +1,5 @@
 import { NextResponse } from 'next/server'
 
-export const config = {
-  matcher: '/robots.txt',
-}
-
 export function middleware(request: Request) {
   return NextResponse.rewrite(new URL('/render/next', request.url))
 }
