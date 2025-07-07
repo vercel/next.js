1f8ad8786e2c935d0696c0ed7759f02e6c1c50f8
Stojan Dimitrovski <sdimitrovski@gmail.com>
2025-07-07 18:51:16 +0200

    chore: Update `with-supabase` example to use `getClaims()`

--- examples/with-supabase/components/auth-button.tsx
+++ examples/with-supabase/components/auth-button.tsx
@@ -6,9 +6,12 @@ import { LogoutButton } from "./logout-button";
 export async function AuthButton() {
   const supabase = await createClient();
 
+  // You can also use getUser() which will be slower.
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data,
+  } = await supabase.auth.getClaims();
+
+  const user = data?.claims
 
   return user ? (
     <div className="flex items-center gap-4">
