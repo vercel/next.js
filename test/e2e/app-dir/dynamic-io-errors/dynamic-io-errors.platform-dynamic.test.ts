d33c9006d9466f48a8bf09a00dc68506432d89ab
Hendrik Liebau <mail@hendrik-liebau.de>
2025-07-09 09:56:51 +0200

    Migrate `platform-dynamic` test suite

--- test/e2e/app-dir/dynamic-io-errors/dynamic-io-errors.platform-dynamic.test.ts
+++ test/e2e/app-dir/dynamic-io-errors/dynamic-io-errors.platform-dynamic.test.ts
@@ -2,74 +2,89 @@ import { isNextDev, nextTestSetup } from 'e2e-utils'
 import { assertNoErrorToast } from 'next-test-utils'
 import { getPrerenderOutput } from './utils'
 
-describe.each(
-  isNextDev
-    ? [
-        {
-          inPrerenderDebugMode: false,
-          name: 'Dev',
-        },
-      ]
-    : [
-        {
-          inPrerenderDebugMode: false,
-          name: 'Build Without --prerender-debug',
-        },
-        {
-          inPrerenderDebugMode: true,
-          name: 'Build With --prerender-debug',
-        },
-      ]
-)('Dynamic IO Errors - $name', ({ inPrerenderDebugMode }) => {
-  describe('Sync Dynamic - With Fallback - Math.random()', () => {
-    const { next, skipped } = nextTestSetup({
-      files: __dirname + '/fixtures/sync-random-with-fallback',
-      skipStart: !isNextDev,
-      skipDeployment: true,
-      buildArgs: inPrerenderDebugMode ? ['--debug-prerender'] : undefined,
-    })
-
-    if (skipped) {
-      return
-    }
-
-    if (isNextDev) {
-      it('should not show a collapsed redbox error', async () => {
-        const browser = await next.browser('/')
-        await assertNoErrorToast(browser)
-      })
-    } else {
-      it('should not error the build when calling Math.random() if all dynamic access is inside a Suspense boundary', async () => {
-        try {
-          await next.start()
-        } catch {
-          throw new Error('expected build not to fail for fully static project')
-        }
-
-        expect(next.cliOutput).toContain('◐ / ')
-        const $ = await next.render$('/')
-        expect($('[data-fallback]').length).toBe(2)
-      })
-    }
+describe('Dynamic IO Errors', () => {
+  const { next, isTurbopack, skipped } = nextTestSetup({
+    files: __dirname,
+    skipStart: !isNextDev,
+    skipDeployment: true,
   })
 
-  describe('Sync Dynamic - Without Fallback - Math.random()', () => {
-    const { next, isTurbopack, skipped } = nextTestSetup({
-      files: __dirname + '/fixtures/sync-random-without-fallback',
-      skipStart: !isNextDev,
-      skipDeployment: true,
-      buildArgs: inPrerenderDebugMode ? ['--debug-prerender'] : undefined,
+  let cliOutputLength: number
+
+  beforeEach(async () => {
+    cliOutputLength = next.cliOutput.length
+  })
+
+  afterEach(async () => {
+    await next.stop()
+  })
+
+  describe.each(
+    isNextDev
+      ? [
+          {
+            inPrerenderDebugMode: false,
+            name: 'Dev',
+          },
+        ]
+      : [
+          {
+            inPrerenderDebugMode: false,
+            name: 'Build Without --prerender-debug',
+          },
+          {
+            inPrerenderDebugMode: true,
+            name: 'Build With --prerender-debug',
+          },
+        ]
+  )('$name', ({ inPrerenderDebugMode }) => {
+    const build = (pathname: string) =>
+      next.build({
+        env: {
+          NEXT_PRIVATE_APP_PATHS: JSON.stringify([`${pathname}/page.tsx`]),
+        },
+        args: inPrerenderDebugMode ? ['--debug-prerender'] : [],
+      })
+
+    describe('Sync Dynamic - With Fallback - Math.random()', () => {
+      const pathname = '/sync-random-with-fallback'
+
+      if (skipped) {
+        return
+      }
+
+      if (isNextDev) {
+        it('should not show a collapsed redbox error', async () => {
+          const browser = await next.browser(pathname)
+          await assertNoErrorToast(browser)
+        })
+      } else {
+        it('should not error the build when calling Math.random() if all dynamic access is inside a Suspense boundary', async () => {
+          try {
+            await next.start()
+          } catch (error) {
+            throw new Error('expected build not to fail', { cause: error })
+          }
+
+          expect(next.cliOutput).toContain(`◐ ${pathname}`)
+          const $ = await next.render$(pathname)
+          expect($('[data-fallback]').length).toBe(2)
+        })
+      }
     })
 
-    if (skipped) {
-      return
-    }
+    describe('Sync Dynamic - Without Fallback - Math.random()', () => {
+      const pathname = '/sync-random-without-fallback'
 
-    if (isNextDev) {
-      it('should show a collapsed redbox error', async () => {
-        const browser = await next.browser('/')
+      if (skipped) {
+        return
+      }
 
-        await expect(browser).toDisplayCollapsedRedbox(`
+      if (isNextDev) {
+        it('should show a collapsed redbox error', async () => {
+          const browser = await next.browser(pathname)
+
+          await expect(browser).toDisplayCollapsedRedbox(`
          {
            "description": "Route "/" used \`Math.random()\` outside of \`"use cache"\` and without explicitly calling \`await connection()\` beforehand. See more info here: https://nextjs.org/docs/messages/next-prerender-random",
            "environmentLabel": "Server",
@@ -85,22 +100,23 @@ describe.each(
            ],
          }
         `)
-      })
-    } else {
-      it('should error the build if Math.random() happens before some component outside a Suspense boundary is complete', async () => {
-        try {
-          await next.build()
-        } catch {
-          // we expect the build to fail
-        }
-
-        const output = getPrerenderOutput(next.cliOutput, {
-          isMinified: !inPrerenderDebugMode,
         })
+      } else {
+        it('should error the build if Math.random() happens before some component outside a Suspense boundary is complete', async () => {
+          try {
+            await build(pathname)
+          } catch {
+            // we expect the build to fail
+          }
 
-        if (isTurbopack) {
-          if (inPrerenderDebugMode) {
-            expect(output).toMatchInlineSnapshot(`
+          const output = getPrerenderOutput(
+            next.cliOutput.slice(cliOutputLength),
+            { isMinified: !inPrerenderDebugMode }
+          )
+
+          if (isTurbopack) {
+            if (inPrerenderDebugMode) {
+              expect(output).toMatchInlineSnapshot(`
              "Error: Route "/" used \`Math.random()\` outside of \`"use cache"\` and without explicitly calling \`await connection()\` beforehand. See more info here: https://nextjs.org/docs/messages/next-prerender-random
                  at getRandomNumber (turbopack:///[project]/app/page.tsx:32:14)
                  at RandomReadingComponent (turbopack:///[project]/app/page.tsx:40:17)
@@ -117,8 +133,8 @@ describe.each(
              > Export encountered errors on following paths:
              	/page: /"
             `)
-          } else {
-            expect(output).toMatchInlineSnapshot(`
+            } else {
+              expect(output).toMatchInlineSnapshot(`
              "Error: Route "/" used \`Math.random()\` outside of \`"use cache"\` and without explicitly calling \`await connection()\` beforehand. See more info here: https://nextjs.org/docs/messages/next-prerender-random
                  at a (<next-dist-dir>)
              To get a more detailed stack trace and pinpoint the issue, try one of the following:
@@ -127,10 +143,10 @@ describe.each(
              Error occurred prerendering page "/". Read more: https://nextjs.org/docs/messages/prerender-error
              Export encountered an error on /page: /, exiting the build."
             `)
-          }
-        } else {
-          if (inPrerenderDebugMode) {
-            expect(output).toMatchInlineSnapshot(`
+            }
+          } else {
+            if (inPrerenderDebugMode) {
+              expect(output).toMatchInlineSnapshot(`
              "Error: Route "/" used \`Math.random()\` outside of \`"use cache"\` and without explicitly calling \`await connection()\` beforehand. See more info here: https://nextjs.org/docs/messages/next-prerender-random
                  at getRandomNumber (webpack:///app/page.tsx:32:14)
                  at RandomReadingComponent (webpack:///app/page.tsx:40:17)
@@ -147,8 +163,8 @@ describe.each(
              > Export encountered errors on following paths:
              	/page: /"
             `)
-          } else {
-            expect(output).toMatchInlineSnapshot(`
+            } else {
+              expect(output).toMatchInlineSnapshot(`
              "Error: Route "/" used \`Math.random()\` outside of \`"use cache"\` and without explicitly calling \`await connection()\` beforehand. See more info here: https://nextjs.org/docs/messages/next-prerender-random
                  at a (<next-dist-dir>)
              To get a more detailed stack trace and pinpoint the issue, try one of the following:
@@ -157,9 +173,10 @@ describe.each(
              Error occurred prerendering page "/". Read more: https://nextjs.org/docs/messages/prerender-error
              Export encountered an error on /page: /, exiting the build."
             `)
+            }
           }
-        }
-      })
-    }
+        })
+      }
+    })
   })
 })
