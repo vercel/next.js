8427486f45d22d341c3d1b3280686016efc2946d
Joseph Chamochumbi <joseph.chamochumbi@vercel.com>
2025-07-10 16:51:24 +0200

    chore: bootstrap fuma docs

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
+import { createMDX } from 'fumadocs-mdx/next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withMDX = createMDX()
+
+const config: NextConfig = {
+  reactStrictMode: true,
 }
 
-export default nextConfig
+export default withMDX(config)
--- apps/docs/postcss.config.mjs
+++ apps/docs/postcss.config.mjs
@@ -1,5 +1,5 @@
-const config = {
-  plugins: ['@tailwindcss/postcss'],
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 }
-
-export default config
