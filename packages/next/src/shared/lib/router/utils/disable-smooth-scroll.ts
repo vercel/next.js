649e6e1d9c2f8fd102e00f004264a9047699553e
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-07-11 13:19:01 -0700

    rename handleSmoothScroll

--- packages/next/src/shared/lib/router/utils/disable-smooth-scroll.ts
+++ packages/next/src/shared/lib/router/utils/disable-smooth-scroll.ts
@@ -2,7 +2,7 @@
  * Run function with `scroll-behavior: auto` applied to `<html/>`.
  * This css change will be reverted after the function finishes.
  */
-export function handleSmoothScroll(
+export function disableSmoothScrollDuringRouteTransition(
   fn: () => void,
   options: { dontForceLayout?: boolean; onlyHashChange?: boolean } = {}
 ) {
