192afeee35080d89d9fbc2bfed27bfe715d47243
RobPruzan <rapruzan@buffalo.edu>
2025-07-15 12:21:56 -0700

    don't double display none

--- packages/next/src/next-devtools/dev-overlay/components/devtools-indicator/devtools-indicator.tsx
+++ packages/next/src/next-devtools/dev-overlay/components/devtools-indicator/devtools-indicator.tsx
@@ -38,7 +38,6 @@ export function DevToolsIndicatorNew() {
           boxShadow: 'none',
           [vertical]: `${INDICATOR_PADDING}px`,
           [horizontal]: `${INDICATOR_PADDING}px`,
-          display: state.disableDevIndicator && 'none',
         } as CSSProperties
       }
     >
