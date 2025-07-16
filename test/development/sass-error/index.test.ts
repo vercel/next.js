6f5fcd84de735fcc9da578998c89b6db0a311d56
Luke Sandberg <lukeisandberg@gmail.com>
2025-07-16 10:36:48 -0700

    fix a snapshot

--- test/development/sass-error/index.test.ts
+++ test/development/sass-error/index.test.ts
@@ -38,7 +38,7 @@ describe('app dir - css', () => {
 
            Pseudo-elements like '::before' or '::after' can't be followed by selectors like 'Ident("path")' at [project]/app/global.scss.css:0:884
 
-           Import trace:
+           Import trace [Client Component Browser]:
              ./app/global.scss.css [Client Component Browser]
              ./app/layout.js [Client Component Browser]
              ./app/layout.js [Server Component]"
