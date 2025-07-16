4ff214c4487c7825a580bb1b31521d25c5919bcd
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-07-16 12:40:09 -0700

    fix: custom head properties are duplicated in pages router

--- test/development/pages-dir/client-navigation/fixture/next.config.js
+++ test/development/pages-dir/client-navigation/fixture/next.config.js
@@ -4,9 +4,12 @@ module.exports = {
     // Make sure entries are not getting disposed.
     maxInactiveAge: 1000 * 60 * 60,
   },
-  experimental: {
-    strictNextHead: process.env.TEST_STRICT_NEXT_HEAD !== 'false',
-  },
+  experimental:
+    process.env.TEST_STRICT_NEXT_HEAD !== undefined
+      ? {
+          strictNextHead: process.env.TEST_STRICT_NEXT_HEAD === 'true',
+        }
+      : {},
   // scroll position can be finicky with the
   // indicators showing so hide by default
   devIndicators: false,
