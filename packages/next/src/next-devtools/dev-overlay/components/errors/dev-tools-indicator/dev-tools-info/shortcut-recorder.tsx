20c5b924265935892204a94cf63c0a5b0cd5a157
rauno <freiberggg@gmail.com>
2025-07-17 13:43:19 +0300

    Update shortcut-recorder.tsx

--- packages/next/src/next-devtools/dev-overlay/components/errors/dev-tools-indicator/dev-tools-info/shortcut-recorder.tsx
+++ packages/next/src/next-devtools/dev-overlay/components/errors/dev-tools-indicator/dev-tools-info/shortcut-recorder.tsx
@@ -64,7 +64,7 @@ export function ShortcutRecorder({
         )
         if (existingNonModifierIndex !== -1) {
           const next = [...prev]
-          next[existingNonModifierIndex] = e.key
+          next[existingNonModifierIndex] = e.code
           handleValidation(next)
           return next
         }
