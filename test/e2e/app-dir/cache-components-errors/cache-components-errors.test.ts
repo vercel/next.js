8c5af0dc137ef6402eca4bde92d0076ec2d54ee7
Hendrik Liebau <mail@hendrik-liebau.de>
2025-07-19 19:03:39 +0200

    Remove rogue `Promise.then <anonymous>` from snapshots

--- test/e2e/app-dir/cache-components-errors/cache-components-errors.test.ts
+++ test/e2e/app-dir/cache-components-errors/cache-components-errors.test.ts
@@ -558,7 +558,6 @@ describe('Cache Components Errors', () => {
              > 59 |   const response = await fetch(
                   |                          ^",
                  "stack": [
-                   "Promise.then <anonymous>",
                    "fetchRandom app/dynamic-root/page.tsx (59:26)",
                    "FetchingComponent app/dynamic-root/page.tsx (45:56)",
                    "Page app/dynamic-root/page.tsx (22:9)",
@@ -573,7 +572,6 @@ describe('Cache Components Errors', () => {
              > 59 |   const response = await fetch(
                   |                          ^",
                  "stack": [
-                   "Promise.then <anonymous>",
                    "fetchRandom app/dynamic-root/page.tsx (59:26)",
                    "FetchingComponent app/dynamic-root/page.tsx (45:56)",
                    "Page app/dynamic-root/page.tsx (27:7)",
