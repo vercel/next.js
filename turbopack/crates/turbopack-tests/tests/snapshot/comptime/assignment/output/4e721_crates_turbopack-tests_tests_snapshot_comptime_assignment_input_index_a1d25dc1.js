a0758191abbb03ead90c4ceff05a135354b59569
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2025-07-21 14:53:24 -0700

    Switch from a snapshot test to an execution test

--- turbopack/crates/turbopack-tests/tests/snapshot/comptime/assignment/output/4e721_crates_turbopack-tests_tests_snapshot_comptime_assignment_input_index_a1d25dc1.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "output/4e721_crates_turbopack-tests_tests_snapshot_comptime_assignment_input_index_a1d25dc1.js",
-    {},
-    {"otherChunks":["output/4e721_crates_turbopack-tests_tests_snapshot_comptime_assignment_input_index_ef20e8c9.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/comptime/assignment/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
