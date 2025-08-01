8a344829be387a5fedd50d9bad0f512942d79cbc
Gurpal <gillgurpal699@gmail.com>
2025-08-01 11:39:20 +0530

    Add support for bun package manager in installation process

--- packages/next/src/lib/helpers/get-npx-command.ts
+++ packages/next/src/lib/helpers/get-npx-command.ts
@@ -11,6 +11,8 @@ export function getNpxCommand(baseDir: string) {
       execSync('yarn dlx --help', { stdio: 'ignore' })
       command = 'yarn dlx'
     } catch {}
+  } else if (pkgManager === 'npm') {
+    command = 'bunx'
   }
 
   return command
