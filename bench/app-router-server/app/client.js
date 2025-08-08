1d2e341205941252da07301bcfafa0683d81b98b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-08-08 14:36:04 +0200

    Revert "WIP example"
    This reverts commit 44bd766b26b0c6c09f3ea57d7804e8a1094c1463.

--- bench/app-router-server/app/client.js
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import dynamic from 'next/dynamic'
-
-const Client = dynamic(() => import('./client2'), {
-  ssr: false,
-})
-
-export default Client
--- bench/app-router-server/app/client2.js
+++
@@ -1,5 +0,0 @@
-import * as styles from './styles.module.scss'
-
-export default function Client() {
-  return <div className={styles.foo}>Hello from Client</div>
-}
