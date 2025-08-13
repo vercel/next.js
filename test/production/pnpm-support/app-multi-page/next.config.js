e3341c286d6ae15b5a25c1096c9879339375df25
Jiachi Liu <inbox@huozhi.im>
2025-08-13 12:30:04 +0200

    fix test fixture

--- test/production/pnpm-support/app-multi-page/next.config.js
+++ test/production/pnpm-support/app-multi-page/next.config.js
@@ -2,8 +2,6 @@ const path = require('path')
 
 module.exports = {
   output: 'standalone',
-  experimental: {
-    // pnpm virtual-store-dir is outside the app directory
-    outputFileTracingRoot: path.resolve(__dirname, '../'),
-  },
+  // pnpm virtual-store-dir is outside the app directory
+  outputFileTracingRoot: path.resolve(__dirname, '../'),
 }
