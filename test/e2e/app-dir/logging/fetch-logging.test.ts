vercel/next.js

commit 36e3f4e7abcad8077a687e307ed26805c2eab61c

Clean-up fetch metrics tracking (#64746)
This ensures we only track fetch metrics in development mode as that's
the only time we report them currently, this also adds an upper limit on
how many metrics we store per-request as previously this was unbounded,
on top of that this ensures we don't keep tracking fetch metrics after
the request has ended as we only report on request end, and finally this
adds a clean-up to the reference we attach to the request object
containing the fetch metrics once we have used them.

Closes: https://github.com/vercel/next.js/issues/64212

Closes NEXT-3159

packages/next/src/server/app-render/app-render.tsx
@@ -590,7 +590,8 @@ async function renderToHTMLOrFlightImpl(
   pagePath: string,
   query: NextParsedUrlQuery,
   renderOpts: RenderOpts,
-  baseCtx: AppRenderBaseContext
+  baseCtx: AppRenderBaseContext,
+  requestEndedState: { ended?: boolean }
 ) {
   const isNotFoundPath = pagePath === '/404'
 
@@ -631,6 +632,8 @@ async function renderToHTMLOrFlightImpl(
     isNodeNextRequest(req)
   ) {
     req.originalRequest.on('end', () => {
+      requestEndedState.ended = true
+
       if ('performance' in globalThis) {
         const metrics = getClientComponentLoaderMetrics({ reset: true })
         if (metrics) {
@@ -1448,14 +1451,23 @@ export const renderToHTMLOrFlight: AppPageRender = (
         {
           urlPathname: pathname,
           renderOpts,
+          requestEndedState: { ended: false },
         },
         (staticGenerationStore) =>
-          renderToHTMLOrFlightImpl(req, res, pagePath, query, renderOpts, {
-            requestStore,
-            staticGenerationStore,
-            componentMod: renderOpts.ComponentMod,
+          renderToHTMLOrFlightImpl(
+            req,
+            res,
+            pagePath,
+            query,
             renderOpts,
-          })
+            {
+              requestStore,
+              staticGenerationStore,
+              componentMod: renderOpts.ComponentMod,
+              renderOpts,
+            },
+            staticGenerationStore.requestEndedState || {}
+          )
       )
   )
 }

packages/next/src/server/lib/patch-fetch.ts
@@ -163,7 +163,13 @@ function trackFetchMetric(
   staticGenerationStore: StaticGenerationStore,
   ctx: Omit<FetchMetric, 'end' | 'idx'>
 ) {
-  if (!staticGenerationStore) return
+  if (
+    !staticGenerationStore ||
+    staticGenerationStore.requestEndedState?.ended ||
+    process.env.NODE_ENV !== 'development'
+  ) {
+    return
+  }
   staticGenerationStore.fetchMetrics ??= []
 
   const dedupeFields = ['url', 'status', 'method'] as const
@@ -181,6 +187,25 @@ function trackFetchMetric(
     end: Date.now(),
     idx: staticGenerationStore.nextFetchId || 0,
   })
+
+  // only store top 10 metrics to avoid storing too many
+  if (staticGenerationStore.fetchMetrics.length > 10) {
+    // sort slowest first as these should be highlighted
+    staticGenerationStore.fetchMetrics.sort((a, b) => {
+      const aDur = a.end - a.start
+      const bDur = b.end - b.start
+
+      if (aDur < bDur) {
+        return 1
+      } else if (aDur > bDur) {
+        return -1
+      }
+      return 0
+    })
+    // now grab top 10
+    staticGenerationStore.fetchMetrics =
+      staticGenerationStore.fetchMetrics.slice(0, 10)
+  }
 }
 
 interface PatchableModule {

packages/next/src/client/components/static-generation-async-storage.external.ts
@@ -51,6 +51,8 @@ export interface StaticGenerationStore {
 
   isDraftMode?: boolean
   isUnstableNoStore?: boolean
+
+  requestEndedState?: { ended?: boolean }
 }
 
 export type StaticGenerationAsyncStorage =

packages/next/src/server/async-storage/static-generation-async-storage-wrapper.ts
@@ -9,6 +9,7 @@ import type { FetchMetric } from '../base-http'
 
 export type StaticGenerationContext = {
   urlPathname: string
+  requestEndedState?: { ended?: boolean }
   renderOpts: {
     incrementalCache?: IncrementalCache
     isOnDemandRevalidate?: boolean
@@ -50,7 +51,7 @@ export const StaticGenerationAsyncStorageWrapper: AsyncStorageWrapper<
 > = {
   wrap<Result>(
     storage: AsyncLocalStorage<StaticGenerationStore>,
-    { urlPathname, renderOpts }: StaticGenerationContext,
+    { urlPathname, renderOpts, requestEndedState }: StaticGenerationContext,
     callback: (store: StaticGenerationStore) => Result
   ): Result {
     /**
@@ -96,6 +97,7 @@ export const StaticGenerationAsyncStorageWrapper: AsyncStorageWrapper<
       isDraftMode: renderOpts.isDraftMode,
 
       prerenderState,
+      requestEndedState,
     }
 
     // TODO: remove this when we resolve accessing the store outside the execution context

packages/next/src/server/next-server.ts
@@ -1232,6 +1232,7 @@ export default class NextNodeServer extends BaseServer<
               }
             }
           }
+          delete normalizedReq.fetchMetrics
           originalResponse.off('close', reqCallback)
         }
         originalResponse.on('close', reqCallback)

commit 3f5fc562d716d3fc4cc1459d55553da6a354ad51

Fix fetch logging after revalidation via server action
When revalidating a tag or path with a server action, we do want to log
any fetches that are made during the subsequent rerendering, when
[fetch logging](https://nextjs.org/docs/app/api-reference/config/next-config-js/logging#fetching)
is enabled.

This got accidentally broken in #64746, where we wrote:

> [...] this ensures we don't keep tracking fetch metrics after the
> request has ended as we only report on request end

That's incorrect though, because we report fetch metrics when the
_response_ is closed, and not the request. This is crucial for server
actions, when the request is likely already closed while we're
rerendering and streaming the response to the client.

fixes #80444
closes NAR-307

packages/next/src/server/app-render/app-render.tsx
@@ -1454,7 +1454,6 @@ async function renderToHTMLOrFlightImpl(
   renderOpts: RenderOpts,
   workStore: WorkStore,
   parsedRequestHeaders: ParsedRequestHeaders,
-  requestEndedState: { ended?: boolean },
   postponedState: PostponedState | null,
   serverComponentsHmrCache: ServerComponentsHmrCache | undefined,
   sharedContext: AppSharedContext,
@@ -1555,9 +1554,13 @@ async function renderToHTMLOrFlightImpl(
     process.env.NEXT_RUNTIME !== 'edge' &&
     isNodeNextRequest(req)
   ) {
-    req.originalRequest.on('end', () => {
-      requestEndedState.ended = true
+    res.onClose(() => {
+      // We stop tracking fetch metrics when the response closes, since we
+      // report them at that time.
+      workStore.shouldTrackFetchMetrics = false
+    })
 
+    req.originalRequest.on('end', () => {
       if ('performance' in globalThis) {
         const metrics = getClientComponentLoaderMetrics({ reset: true })
         if (metrics) {
@@ -1966,7 +1969,6 @@ export const renderToHTMLOrFlight: AppPageRender = (
 
   const { isPrefetchRequest, previouslyRevalidatedTags } = parsedRequestHeaders
 
-  const requestEndedState = { ended: false }
   let postponedState: PostponedState | null = null
 
   // If provided, the postpone state should be parsed so it can be provided to
@@ -1996,7 +1998,6 @@ export const renderToHTMLOrFlight: AppPageRender = (
   const workStore = createWorkStore({
     page: renderOpts.routeModule.definition.page,
     renderOpts,
-    requestEndedState,
     // @TODO move to workUnitStore of type Request
     isPrefetchRequest,
     buildId: sharedContext.buildId,
@@ -2016,7 +2017,6 @@ export const renderToHTMLOrFlight: AppPageRender = (
     renderOpts,
     workStore,
     parsedRequestHeaders,
-    requestEndedState,
     postponedState,
     serverComponentsHmrCache,
     sharedContext,

packages/next/src/server/app-render/work-async-storage.external.ts
@@ -85,12 +85,11 @@ export interface WorkStore {
   readonly refreshTagsByCacheKind: Map<string, LazyResult<void>>
 
   fetchMetrics?: FetchMetrics
+  shouldTrackFetchMetrics: boolean
 
   isDraftMode?: boolean
   isUnstableNoStore?: boolean
   isPrefetchRequest?: boolean
-
-  requestEndedState?: { ended?: boolean }
 
   buildId: string
 

packages/next/src/server/async-storage/work-store.ts
@@ -19,7 +19,6 @@ export type WorkStoreContext = {
    */
   page: string
 
-  requestEndedState?: { ended?: boolean }
   isPrefetchRequest?: boolean
   renderOpts: {
     cacheLifeProfiles?: { [profile: string]: CacheLife }
@@ -77,7 +76,6 @@ export type WorkStoreContext = {
 export function createWorkStore({
   page,
   renderOpts,
-  requestEndedState,
   isPrefetchRequest,
   buildId,
   previouslyRevalidatedTags,
@@ -105,6 +103,17 @@ export function createWorkStore({
     !renderOpts.isDraftMode &&
     !renderOpts.isPossibleServerAction
 
+  const isDevelopment = renderOpts.dev ?? false
+
+  const shouldTrackFetchMetrics =
+    isDevelopment ||
+    // The only times we want to track fetch metrics outside of development is
+    // when we are performing a static generation and we either are in debug
+    // mode, or tracking fetch metrics was specifically opted into.
+    (isStaticGeneration &&
+      (!!process.env.NEXT_DEBUG_BUILD ||
+        process.env.NEXT_SSG_FETCH_METRICS === '1'))
+
   const store: WorkStore = {
     isStaticGeneration,
     page,
@@ -122,7 +131,6 @@ export function createWorkStore({
 
     isDraftMode: renderOpts.isDraftMode,
 
-    requestEndedState,
     isPrefetchRequest,
     buildId,
     reactLoadableManifest: renderOpts?.reactLoadableManifest || {},
@@ -130,10 +138,11 @@ export function createWorkStore({
 
     afterContext: createAfterContext(renderOpts),
     cacheComponentsEnabled: renderOpts.experimental.cacheComponents,
-    dev: renderOpts.dev ?? false,
+    dev: isDevelopment,
     previouslyRevalidatedTags,
     refreshTagsByCacheKind: createRefreshTagsByCacheKind(),
     runInCleanSnapshot: createSnapshot(),
+    shouldTrackFetchMetrics,
   }
 
   // TODO: remove this when we resolve accessing the store outside the execution context

packages/next/src/server/lib/patch-fetch.ts
@@ -121,22 +121,7 @@ function trackFetchMetric(
   workStore: WorkStore,
   ctx: Omit<FetchMetric, 'end' | 'idx'>
 ) {
-  // If the static generation store is not available, we can't track the fetch
-  if (!workStore) return
-  if (workStore.requestEndedState?.ended) return
-
-  const isDebugBuild =
-    (!!process.env.NEXT_DEBUG_BUILD ||
-      process.env.NEXT_SSG_FETCH_METRICS === '1') &&
-    workStore.isStaticGeneration
-  const isDevelopment = process.env.NODE_ENV === 'development'
-
-  if (
-    // The only time we want to track fetch metrics outside of development is when
-    // we are performing a static generation & we are in debug mode.
-    !isDebugBuild &&
-    !isDevelopment
-  ) {
+  if (!workStore.shouldTrackFetchMetrics) {
     return
   }
 

packages/next/src/server/web/adapter.ts
@@ -297,7 +297,6 @@ export async function adapter(
                 onClose: closeController.onClose.bind(closeController),
                 onAfterTaskError: undefined,
               },
-              requestEndedState: { ended: false },
               isPrefetchRequest:
                 request.headers.get(NEXT_ROUTER_PREFETCH_HEADER) === '1',
               buildId: buildId ?? '',

test/e2e/app-dir/logging/app/default-cache/page.js
@@ -1,3 +1,5 @@
+import { revalidateTag } from 'next/cache'
+
 export const fetchCache = 'default-cache'
 
 async function AnotherRsc() {
@@ -55,7 +57,8 @@ async function FirstRsc() {
   ).then((res) => res.text())
 
   const dataAutoCache = await fetch(
-    'https://next-data-api-endpoint.vercel.app/api/random?auto-cache'
+    'https://next-data-api-endpoint.vercel.app/api/random?auto-cache',
+    { next: { tags: ['test-tag'] } }
   ).then((res) => res.text())
 
   return (
@@ -72,12 +75,26 @@ async function FirstRsc() {
   )
 }
 
+function RevalidateForm() {
+  return (
+    <form
+      action={async () => {
+        'use server'
+        revalidateTag('test-tag')
+      }}
+    >
+      <button id="revalidate-button">Revalidate</button>
+    </form>
+  )
+}
+
 export default async function Page() {
   return (
     <>
       <h1>Default Cache</h1>
       <FirstRsc />
       <AnotherRsc />
+      <RevalidateForm />
     </>
   )
 }

test/e2e/app-dir/logging/fetch-logging.test.ts
@@ -267,6 +267,29 @@ describe('app-dir - logging', () => {
           })
         })
 
+        it('should log requests for after revalidation via server action', async () => {
+          let outputIndex = next.cliOutput.length
+          const browser = await next.browser('/default-cache')
+
+          const expectedUrl = withFullUrlFetches
+            ? 'https://next-data-api-endpoint.vercel.app/api/random'
+            : 'https://next-data-api-en../api/random'
+
+          await retry(() => {
+            const logs = stripAnsi(next.cliOutput.slice(outputIndex))
+            expect(logs).toIncludeRepeated(` │ GET ${expectedUrl}`, 7)
+          })
+
+          outputIndex = next.cliOutput.length
+
+          await browser.elementById('revalidate-button').click()
+
+          await retry(() => {
+            const logs = stripAnsi(next.cliOutput.slice(outputIndex))
+            expect(logs).toIncludeRepeated(` │ GET ${expectedUrl}`, 7)
+          })
+        })
+
         describe('when logging.fetches.hmrRefreshes is true', () => {
           beforeAll(async () => {
             await next.patchFile('next.config.js', (content) =>