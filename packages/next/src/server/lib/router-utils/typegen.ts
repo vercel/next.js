7987a02cef1ebb52352c69d9454154e45ac7fe86
Chung Wei Leong <15154097+chungweileong94@users.noreply.github.com>
2025-08-20 23:05:57 +0800

    Update packages/next/src/server/lib/router-utils/typegen.ts
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- packages/next/src/server/lib/router-utils/typegen.ts
+++ packages/next/src/server/lib/router-utils/typegen.ts
@@ -292,7 +292,7 @@ declare module 'next' {
 }
 
 declare module 'next/link' {
-  export * from 'next/dist/client/link.js'
+  export { useLinkStatus } from 'next/dist/client/link.js'
 
   import type { LinkProps as OriginalLinkProps } from 'next/dist/client/link.js'
   import type { AnchorHTMLAttributes, DetailedHTMLProps } from 'react'
