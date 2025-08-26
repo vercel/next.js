4905291da3362c6337428f534404b0b036904d35
Cong-Cong <dacongsama@live.com>
2025-08-26 18:57:39 +0800

    fix lint

--- packages/next/src/build/webpack-config.ts
+++ packages/next/src/build/webpack-config.ts
@@ -2250,7 +2250,7 @@ export default async function getBaseWebpackConfig(
     // TODO: Remove this workaround after Rspack 1.5.1 release
     webpack5Config.experiments.incremental = {
       buildChunkGraph: false,
-    };
+    }
   }
 
   webpack5Config.module!.parser = {
