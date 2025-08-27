c605d5870c77017198a28bab938ad8b3551b39ca
Cy-Tek <josh.hannaford@vercel.com>
2025-08-27 17:24:05 -0400

    Fixes issue with intercept routes not being applied in middleware
    Our previous implementation did not preserve the dots in the
    intercept route like (...) or (..)(..)

--- crates/next-core/src/next_edge/route_regex.rs
+++ crates/next-core/src/next_edge/route_regex.rs
@@ -164,6 +164,7 @@ fn get_safe_key_from_segment(
     segment: &str,
     route_keys: &mut FxHashMap<String, String>,
     key_prefix: Option<&'static str>,
+    intercept_prefix: Option<&str>,
 ) -> String {
     let ParsedParameter {
         key,
@@ -195,11 +196,13 @@ fn get_safe_key_from_segment(
     } else {
         route_keys.insert(cleaned_key.clone(), key);
     }
+
+    let intercept_prefix = escape_string_regexp(intercept_prefix.unwrap_or(""));
     match (repeat, optional) {
-        (true, true) => format!(r"(?:/(?P<{cleaned_key}>.+?))?"),
-        (true, false) => format!(r"/(?P<{cleaned_key}>.+?)"),
-        (false, true) => format!(r"(?:/(?P<{cleaned_key}>[^/]+?))?"),
-        (false, false) => format!(r"/(?P<{cleaned_key}>[^/]+?)"),
+        (true, true) => format!(r"(?:/{intercept_prefix}(?P<{cleaned_key}>.+?))?"),
+        (true, false) => format!(r"/{intercept_prefix}(?P<{cleaned_key}>.+?)"),
+        (false, true) => format!(r"(?:/{intercept_prefix}(?P<{cleaned_key}>[^/]+?))?"),
+        (false, false) => format!(r"/{intercept_prefix}(?P<{cleaned_key}>[^/]+?)"),
     }
 }
 
@@ -213,11 +216,12 @@ fn get_named_parametrized_route(
     let parameterized_route = segments
         .iter()
         .map(|segment| {
+            let interception_marker = INTERCEPTION_ROUTE_MARKERS
+                .iter()
+                .find(|&m| segment.starts_with(m))
+                .copied();
             let key_prefix = if prefix_route_keys {
-                let has_interception_marker = INTERCEPTION_ROUTE_MARKERS
-                    .iter()
-                    .any(|&m| segment.starts_with(m));
-                if has_interception_marker {
+                if interception_marker.is_some() {
                     Some(NEXT_INTERCEPTION_MARKER_PREFIX)
                 } else {
                     Some(NEXT_QUERY_PARAM_PREFIX)
@@ -228,14 +232,17 @@ fn get_named_parametrized_route(
             static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\[((?:\[.*\])|.+)\]").unwrap());
             let param_matches = RE.captures(segment);
             if let Some(matches) = param_matches {
-                return get_safe_key_from_segment(
+                let safe_key = get_safe_key_from_segment(
                     get_safe_route_key,
                     &matches[1],
                     &mut route_keys,
                     key_prefix,
+                    interception_marker,
                 );
+
+                return safe_key;
             }
-            format!("/{}", escape_string_regexp(segment))
+            format!("/{}{}", interception_marker.unwrap_or(&""), (segment))
         })
         .collect::<Vec<String>>()
         .join("");
@@ -264,4 +271,40 @@ pub fn get_named_route_regex(normalized_route: &str) -> NamedRouteRegex {
 pub fn get_named_middleware_regex(normalized_route: &str) -> String {
     let (parameterized_route, _route_keys) = get_named_parametrized_route(normalized_route, true);
     format!("^{parameterized_route}(?:/)?$")
+}
+
+#[cfg(test)]
+mod test {
+    use super::get_named_middleware_regex;
+
+    #[test]
+    fn should_properly_handle_intercept_routes() {
+        let tests = [
+            (
+                "/[locale]/example/(...)[locale]/intercepted",
+                "^/(?P<nxtPlocale>[^/]+?)/example/\\(\\.\\.\\.\\)(?P<nxtIlocale>[^/]+?)/\
+                 intercepted(?:/)?$",
+            ),
+            (
+                "/[locale]/example/(..)[locale]/intercepted",
+                "^/(?P<nxtPlocale>[^/]+?)/example/\\(\\.\\.\\)(?P<nxtIlocale>[^/]+?)/intercepted(?\
+                 :/)?$",
+            ),
+            (
+                "/[locale]/example/(.)[locale]/intercepted",
+                "^/(?P<nxtPlocale>[^/]+?)/example/\\(\\.\\)(?P<nxtIlocale>[^/]+?)/intercepted(?:/\
+                 )?$",
+            ),
+            (
+                "/[locale]/example/(..)(..)[locale]/intercepted",
+                "^/(?P<nxtPlocale>[^/]+?)/example/\\(\\.\\.\\)\\(\\.\\.\\)(?P<nxtIlocale>[^/]+?)/\
+                 intercepted(?:/)?$",
+            ),
+        ];
+
+        for test in tests {
+            let intercept_route = get_named_middleware_regex(test.0);
+            assert_eq!(intercept_route, test.1);
+        }
+    }
 }
