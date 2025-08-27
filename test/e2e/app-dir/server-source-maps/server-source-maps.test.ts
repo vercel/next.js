e4309e2a3e4a1fee14141145a5118144e7bbd34e
Tim Neutkens <tim@timneutkens.nl>
2025-08-27 14:59:21 +0200

    Update server-source-maps.test.ts

--- test/e2e/app-dir/server-source-maps/server-source-maps.test.ts
+++ test/e2e/app-dir/server-source-maps/server-source-maps.test.ts
@@ -571,6 +571,8 @@ describe('app-dir - server source maps', () => {
          > 6 |   runHiddenSetOfSetsInternal('rsc-anonymous-stack-frame-sandwich: internal')
              |                             ^",
              "stack": [
+               "Set.forEach <anonymous>",
+               "Set.forEach <anonymous>",
                "Page app/rsc-anonymous-stack-frame-sandwich/page.js (6:29)",
                "Page <anonymous>",
              ],
