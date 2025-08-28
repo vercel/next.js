8bc9818033b658518a59d224f16e5f3b13654fa8
Steven <steven@ceriously.com>
2025-08-28 11:54:16 -0400

    update warning

--- test/integration/next-image-new/app-dir/test/index.test.ts
+++ test/integration/next-image-new/app-dir/test/index.test.ts
@@ -1060,7 +1060,7 @@ function runTests(mode: 'dev' | 'server') {
         .join('\n')
       await assertNoRedbox(browser)
       expect(warnings).toMatch(
-        /Image with src (.*)jpg(.*) is using quality "50" which is not configured in images.qualities. This config will be required starting in Next.js 16./gm
+        /Image with src (.*)jpg(.*) is using quality "50" which is not configured in images.qualities \[75\]. Please update your config to \[50, 75\]./gm
       )
     })
 
@@ -1636,7 +1636,7 @@ function runTests(mode: 'dev' | 'server') {
           localPatterns: undefined,
           minimumCacheTTL: 60,
           path: '/_next/image',
-          qualities: undefined,
+          qualities: [75],
           sizes: [
             640, 750, 828, 1080, 1200, 1920, 2048, 3840, 16, 32, 48, 64, 96,
             128, 256, 384,
