291e1eac3b7a3936eb57634d817476c24f9260e9
Cong-Cong <dacongsama@live.com>
2025-09-01 10:59:12 +0800

    prettier

--- rspack/lib/index.d.ts
+++ rspack/lib/index.d.ts
@@ -1,15 +1,16 @@
-import * as RspackCore from '@rspack/core';
-import { NapiNextExternalsPluginOptions } from '@next-rspack/binding';
+import * as RspackCore from '@rspack/core'
+import { NapiNextExternalsPluginOptions } from '@next-rspack/binding'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare class NextExternalsPlugin {
   /**
    * The banner text to be added to the output file.
    */
-  constructor(options: NapiNextExternalsPluginOptions);
+  constructor(options: NapiNextExternalsPluginOptions)
 }
 
 declare const core: typeof RspackCore & {
-  NextExternalsPlugin: typeof NextExternalsPlugin;
-};
+  NextExternalsPlugin: typeof NextExternalsPlugin
+}
 
-export = core;
+export = core
--- rspack/lib/index.js
+++ rspack/lib/index.js
@@ -1,23 +1,23 @@
 process.env.RSPACK_BINDING = require('node:path').dirname(
   require.resolve('@next-rspack/binding')
-);
+)
 
-const binding = require('@next-rspack/binding');
+const binding = require('@next-rspack/binding')
 
 // Register the plugin `NextExternalsPlugin` exported by `crates/binding/src/lib.rs`.
-binding.registerNextExternalsPlugin();
+binding.registerNextExternalsPlugin()
 
-const core = require('@rspack/core');
+const core = require('@rspack/core')
 
 const NextExternalsPlugin = core.experiments.createNativePlugin(
   'NextExternalsPlugin',
   function (options) {
-    return options;
+    return options
   }
-);
+)
 
 Object.defineProperty(core, 'NextExternalsPlugin', {
   value: NextExternalsPlugin,
-});
+})
 
-module.exports = core;
+module.exports = core
