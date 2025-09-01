3bfabb9ca472195ab01d11c301375df7beace549
Remco Haszing <remcohaszing@gmail.com>
2025-09-01 10:03:35 +0200

    Update Jest snapshot

--- packages/next/src/lib/typescript/writeConfigurationDefaults.test.ts
+++ packages/next/src/lib/typescript/writeConfigurationDefaults.test.ts
@@ -115,7 +115,7 @@ describe('writeConfigurationDefaults()', () => {
            	- moduleResolution was set to node (to match webpack resolution)
            	- resolveJsonModule was set to true (to match webpack resolution)
            	- isolatedModules was set to true (requirement for SWC / Babel)
-           	- jsx was set to preserve (next.js implements its own optimized jsx transform)
+           	- jsx was set to react-jsx (next.js uses the React automatic runtime)
         "
       `)
     })
