6353744ab50b8cc27b097a9012e884843025bb9f
Jiachi Liu <inbox@huozhi.im>
2025-09-03 15:54:27 +0200

    fix client pages

--- test/e2e/on-request-error/basic/app/client-page/edge/page.js
+++ test/e2e/on-request-error/basic/app/client-page/edge/page.js
@@ -1,9 +1,6 @@
 'use client'
 
-import { connection } from 'next/server'
-
 export default async function Page() {
-  await connection()
   if (typeof window === 'undefined') {
     throw new Error('client-page-edge-error')
   }
