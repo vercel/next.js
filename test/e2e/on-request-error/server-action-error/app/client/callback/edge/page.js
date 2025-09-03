a037ce2b18547fedeaf67ae79f04a2775982e8b2
Jiachi Liu <inbox@huozhi.im>
2025-09-03 14:52:36 +0200

    fix fixture

--- test/e2e/on-request-error/server-action-error/app/client/callback/edge/page.js
+++ test/e2e/on-request-error/server-action-error/app/client/callback/edge/page.js
@@ -1,10 +1,8 @@
 'use client'
 
-import { connection } from 'next/server'
 import { serverLog } from '../../actions'
 
-export default async function Page() {
-  await connection()
+export default function Page() {
   return (
     <div>
       <button onClick={() => serverLog('callback:edge')}>Log to server</button>
