143a1d95b2442990327fc6000f345e74194433b1
Tim Neutkens <tim@timneutkens.nl>
2025-09-05 14:36:12 +0200

    Turbopack: Don't resolve tsconfig relative to file

--- crates/next-core/src/next_edge/context.rs
+++ crates/next-core/src/next_edge/context.rs
@@ -192,6 +192,9 @@ pub async fn get_edge_resolve_options_context(
             .typescript_tsconfig_path()
             .await?
             .as_ref()
+            // Fall back to tsconfig only for resolving. This is because we don't want Turbopack to
+            // resolve tsconfig.json relative to the file being compiled.
+            .or(Some(&RcStr::from("tsconfig.json")))
             .map(|p| project_path.join(p))
             .transpose()?,
         rules: vec![(
