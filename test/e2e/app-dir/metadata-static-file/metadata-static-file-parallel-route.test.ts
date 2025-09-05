2d7412fec3562ec9047c52e3e6eebeb9c070a536
devjiwonchoi <devjiwonchoi@gmail.com>
2025-09-05 11:09:14 +0200

    test: skip CC testing for dynamic route

--- test/e2e/app-dir/metadata-static-file/metadata-static-file-parallel-route.test.ts
+++ test/e2e/app-dir/metadata-static-file/metadata-static-file-parallel-route.test.ts
@@ -3,6 +3,20 @@ import { getCommonMetadataHeadTags } from './utils'
 import { getMetadataRouteSuffix } from 'next/dist/lib/metadata/get-metadata-route'
 
 describe('metadata-files-static-output-parallel-route', () => {
+  if (process.env.__NEXT_EXPERIMENTAL_CACHE_COMPONENTS) {
+    // Cache Components build fails when metadata files are inside a dynamic route.
+    //
+    // Route "/dynamic/[id]" has a `generateMetadata` that depends on Request data (`cookies()`, etc...)
+    // or uncached external data (`fetch(...)`, etc...) when the rest of the route does not.
+    // See more info here: https://nextjs.org/docs/messages/next-prerender-dynamic-metadata
+    // Error occurred prerendering page "/dynamic/[id]". Read more: https://nextjs.org/docs/messages/prerender-error
+    // Export encountered an error on /dynamic/[id]/page: /dynamic/[id], exiting the build.
+    //
+    // TODO: Remove this skip when metadata files are supported in dynamic routes for Cache Components.
+    it.skip('should skip test for Cache Components', () => {})
+    return
+  }
+
   const { next, skipped } = nextTestSetup({
     files: __dirname,
     skipDeployment: true,
