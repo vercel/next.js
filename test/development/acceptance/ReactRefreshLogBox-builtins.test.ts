f4c7af74793f71c020f69df69775d0ba9fe32c67
Cong-Cong <dacongsama@live.com>
2025-09-05 18:55:40 +0800

    test/development/acceptance/ReactRefreshLogBox-builtins.test.ts

--- test/development/acceptance/ReactRefreshLogBox-builtins.test.ts
+++ test/development/acceptance/ReactRefreshLogBox-builtins.test.ts
@@ -8,6 +8,7 @@ describe('ReactRefreshLogBox', () => {
     files: new FileRef(path.join(__dirname, 'fixtures', 'default-template')),
     skipStart: true,
   })
+  const isRspack = !!process.env.NEXT_RSPACK
 
   // Module trace is only available with webpack 5
   test('Node.js builtins', async () => {
@@ -54,6 +55,25 @@ describe('ReactRefreshLogBox', () => {
        Module not found: Can't resolve 'dns'
        > 1 | const dns = require('dns')
            |             ^^^^^^^^^^^^^^",
+         "stack": [],
+       }
+      `)
+    } else if (isRspack) {
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "  × Module not found: Can't resolve 'dns' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773/node_modules/my-package'",
+         "environmentLabel": null,
+         "label": "Build Error",
+         "source": "./node_modules/my-package/index.js
+         × Module not found: Can't resolve 'dns' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773/node_modules/my-package'
+          ╭─[1:12]
+        1 │ const dns = require('dns')
+          ·             ──────────────
+        2 │ module.exports = dns
+          ╰────
+       Import trace for requested module:
+       ./node_modules/my-package/index.js
+       ./index.js",
          "stack": [],
        }
       `)
@@ -105,6 +125,27 @@ describe('ReactRefreshLogBox', () => {
          "stack": [],
        }
       `)
+    } else if (isRspack) {
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "  × Module not found: Can't resolve 'b' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773'",
+         "environmentLabel": null,
+         "label": "Build Error",
+         "source": "./index.js
+         × Module not found: Can't resolve 'b' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773'
+          ╭─[2:17]
+        1 │ import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+        2 │ import Comp from 'b';
+          ·                  ───
+        3 │ export default function Oops() {
+        4 │     return /*#__PURE__*/ _jsxDEV("div", {
+          ╰────
+       Import trace for requested module:
+       ./index.js
+       ./pages/index.js",
+         "stack": [],
+       }
+      `)
     } else {
       await expect(browser).toDisplayRedbox(`
        {
@@ -150,6 +191,26 @@ describe('ReactRefreshLogBox', () => {
        Module not found: Can't resolve 'b'
        > 1 | import Comp from 'b'
            | ^^^^^^^^^^^^^^^^^^^^",
+         "stack": [],
+       }
+      `)
+    } else if (isRspack) {
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "  × Module not found: Can't resolve 'b' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773/pages'",
+         "environmentLabel": null,
+         "label": "Build Error",
+         "source": "./pages/index.js
+         × Module not found: Can't resolve 'b' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773/pages'
+          ╭─[2:17]
+        1 │ import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+        2 │ import Comp from 'b';
+          ·                  ───
+        3 │ export default function Oops() {
+        4 │     return /*#__PURE__*/ _jsxDEV("div", {
+          ╰────
+       Import trace for requested module:
+       ./pages/index.js",
          "stack": [],
        }
       `)
@@ -205,6 +266,24 @@ describe('ReactRefreshLogBox', () => {
        Module not found: Can't resolve './non-existent.css'
        > 1 | import './non-existent.css'
            | ^^^^^^^^^^^^^^^^^^^^^^^^^^^",
+         "stack": [],
+       }
+      `)
+    } else if (isRspack) {
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "  × Module not found: Can't resolve './non-existent.css' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773/pages'",
+         "environmentLabel": null,
+         "label": "Build Error",
+         "source": "./pages/_app.js
+         × Module not found: Can't resolve './non-existent.css' in '/private/var/folders/b1/0fd1b6hs7lz0fm_mh346lybm0000gn/T/next-install-11c6a8113f8641ebf16d8f95148ca2dd2235a41908f43340e0934bf5e8235773/pages'
+          ╭─[2:7]
+        1 │ import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+        2 │ import './non-existent.css';
+          ·        ────────────────────
+        3 │ export default function App({ Component, pageProps }) {
+        4 │     return /*#__PURE__*/ _jsxDEV(Component, {
+          ╰────",
          "stack": [],
        }
       `)
