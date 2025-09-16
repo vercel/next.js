de5ba980016a2375f4ce75808c8b2d751fcf81f1
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-16 12:00:26 +0200

    Env replacements

--- crates/next-core/src/raw_ecmascript_module.rs
+++ crates/next-core/src/raw_ecmascript_module.rs
@@ -1,20 +1,31 @@
+use std::io::Write;
+
 use anyhow::{Result, bail};
+use once_cell::sync::Lazy;
+use regex::Regex;
 use turbo_rcstr::rcstr;
-use turbo_tasks::{ResolvedVc, Vc};
+use turbo_tasks::{FxIndexMap, FxIndexSet, ResolvedVc, TryJoinIterExt, Vc};
 use turbo_tasks_fs::{FileContent, glob::Glob, rope::RopeBuilder};
 use turbopack::{ModuleAssetContext, module_options::CustomModuleType};
 use turbopack_core::{
     asset::{Asset, AssetContent},
     chunk::{ChunkItem, ChunkType, ChunkableModule, ChunkingContext},
+    compile_time_info::{
+        CompileTimeDefineValue, CompileTimeInfo, DefinableNameSegment, FreeVarReference,
+    },
+    context::AssetContext,
     ident::AssetIdent,
     module::Module,
     module_graph::ModuleGraph,
     resolve::ModulePart,
     source::Source,
 };
-use turbopack_ecmascript::chunk::{
-    EcmascriptChunkItem, EcmascriptChunkItemContent, EcmascriptChunkPlaceable, EcmascriptChunkType,
-    EcmascriptExports,
+use turbopack_ecmascript::{
+    chunk::{
+        EcmascriptChunkItem, EcmascriptChunkItemContent, EcmascriptChunkItemOptions,
+        EcmascriptChunkPlaceable, EcmascriptChunkType, EcmascriptExports,
+    },
+    utils::StringifyJs,
 };
 
 #[turbo_tasks::value(shared)]
@@ -26,23 +37,34 @@ impl CustomModuleType for RawEcmascriptModuleType {
     fn create_module(
         &self,
         source: Vc<Box<dyn Source>>,
-        _module_asset_context: Vc<ModuleAssetContext>,
+        module_asset_context: Vc<ModuleAssetContext>,
         _part: Option<ModulePart>,
     ) -> Vc<Box<dyn Module>> {
-        Vc::upcast(RawEcmascriptModule::new(source))
+        Vc::upcast(RawEcmascriptModule::new(
+            source,
+            module_asset_context.compile_time_info(),
+        ))
     }
 }
 
 #[turbo_tasks::value]
 pub struct RawEcmascriptModule {
     source: ResolvedVc<Box<dyn Source>>,
+    compile_time_info: ResolvedVc<CompileTimeInfo>,
 }
 
 #[turbo_tasks::value_impl]
 impl RawEcmascriptModule {
     #[turbo_tasks::function]
-    pub fn new(source: ResolvedVc<Box<dyn Source>>) -> Vc<Self> {
-        Self::cell(RawEcmascriptModule { source })
+    pub fn new(
+        source: ResolvedVc<Box<dyn Source>>,
+        compile_time_info: ResolvedVc<CompileTimeInfo>,
+    ) -> Vc<Self> {
+        RawEcmascriptModule {
+            source,
+            compile_time_info,
+        }
+        .cell()
     }
 }
 
@@ -130,12 +152,83 @@ impl EcmascriptChunkItem for RawEcmascriptChunkItem {
             FileContent::Content(file) => file.content(),
             FileContent::NotFound => bail!("RawEcmascriptModule content not found"),
         };
+
+        static ENV_REGEX: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"process\.env\.([a-zA-Z0-9_]+)").unwrap());
+
+        let content_str = content.to_str()?;
+
+        let mut env_vars = FxIndexSet::default();
+        for (_, [name]) in ENV_REGEX.captures_iter(&content_str).map(|c| c.extract()) {
+            env_vars.insert(name);
+        }
+
         let mut inner_code = RopeBuilder::default();
+        if !env_vars.is_empty() {
+            let replacements = self
+                .module
+                .await?
+                .compile_time_info
+                .await?
+                .free_var_references
+                .individual()
+                .await?;
+            inner_code += "var process = {env:\n";
+            writeln!(
+                inner_code,
+                "{}",
+                StringifyJs(
+                    &env_vars
+                        .into_iter()
+                        .map(async |name| {
+                            Ok((
+                                name,
+                                if let Some(value) =
+                                    replacements.get(&DefinableNameSegment::Name(name.into()))
+                                    && let Some(value) = value.get(&vec![
+                                        DefinableNameSegment::Name("process".into()),
+                                        DefinableNameSegment::Name("env".into()),
+                                    ])
+                                {
+                                    let value = value.await?;
+                                    let value = match &*value {
+                                        FreeVarReference::Value(
+                                            CompileTimeDefineValue::String(value),
+                                        ) => serde_json::Value::String(value.to_string()),
+                                        FreeVarReference::Value(CompileTimeDefineValue::Bool(
+                                            value,
+                                        )) => serde_json::Value::Bool(*value),
+                                        _ => {
+                                            bail!(
+                                                "Unexpected replacement for process.env.{name} in \
+                                                 RawEcmascriptModule: {value:?}"
+                                            );
+                                        }
+                                    };
+                                    Some(value)
+                                } else {
+                                    None
+                                },
+                            ))
+                        })
+                        .try_join()
+                        .await?
+                        .into_iter()
+                        .collect::<FxIndexMap<_, _>>()
+                )
+            )?;
+            inner_code += "};\n";
+        }
+        inner_code += "{\n";
         inner_code.concat(content);
         // Add newline in case the raw code had a comment as the last line and no final newline.
-        inner_code += "\n";
+        inner_code += "\n}\n";
         Ok(EcmascriptChunkItemContent {
             inner_code: inner_code.build(),
+            options: EcmascriptChunkItemOptions {
+                module_and_exports: true,
+                ..Default::default()
+            },
             ..Default::default()
         }
         .into())
