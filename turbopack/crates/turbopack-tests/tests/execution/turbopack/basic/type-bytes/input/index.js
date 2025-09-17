d14632dcf11daafd0767ebee8bfb4a6e0558140f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-17 23:09:00 +0200

    remove unit test

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/basic/type-bytes/input/index.js
+++
@@ -1,6 +0,0 @@
-import bytes from './data.bin' with { type: 'bytes' }
-
-it('import type:bytes should work', () => {
-  expect(bytes).toBeInstanceOf(Uint8Array)
-  expect(new TextDecoder().decode(bytes)).toBe('this is some data\n')
-})
