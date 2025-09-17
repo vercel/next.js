8eabb2fdb8035dec68a95e82663d08057057f41b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-17 17:44:02 +0200

    Turbopack: make graph traversal sync

--- turbopack/crates/turbopack/src/global_module_ids.rs
+++ turbopack/crates/turbopack/src/global_module_ids.rs
@@ -30,24 +30,21 @@ pub async fn get_global_module_id_strategy(
 
         // And additionally, all the modules that are inserted by chunking (i.e. async loaders)
         let mut async_idents = vec![];
-        module_graph
-            .traverse_all_edges_unordered(|parent, current| {
-                if let (
-                    _,
-                    &RefData {
-                        chunking_type: ChunkingType::Async,
-                        ..
-                    },
-                ) = parent
-                {
-                    let module =
-                        ResolvedVc::try_sidecast::<Box<dyn ChunkableModule>>(current.module)
-                            .context("expected chunkable module for async reference")?;
-                    async_idents.push(AsyncLoaderModule::asset_ident_for(*module));
-                }
-                Ok(())
-            })
-            .await?;
+        module_graph.traverse_all_edges_unordered(|parent, current| {
+            if let (
+                _,
+                &RefData {
+                    chunking_type: ChunkingType::Async,
+                    ..
+                },
+            ) = parent
+            {
+                let module = ResolvedVc::try_sidecast::<Box<dyn ChunkableModule>>(current.module)
+                    .context("expected chunkable module for async reference")?;
+                async_idents.push(AsyncLoaderModule::asset_ident_for(*module));
+            }
+            Ok(())
+        })?;
 
         let mut module_id_map = module_idents
             .chain(async_idents.into_iter())
