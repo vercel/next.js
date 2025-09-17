29ded7d332c4fa53765406743d33d8efbe7e9ff9
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-09-17 05:20:29 -0600

    test: added test case to validate the resolution

--- test/e2e/app-dir/sub-shell-generation-middleware/next.config.js
+++ test/e2e/app-dir/sub-shell-generation-middleware/next.config.js
@@ -1,6 +1,40 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    useCache: true,
+  },
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: '/:first/~/overview/:path*',
+          destination: '/404',
+          missing: [
+            // Ensure that during static page revalidations, we don't consider
+            // this rule. The presence of this header indicates that the request
+            // is being revalidated by Vercel.
+            {
+              type: 'header',
+              key: 'x-now-route-matches',
+            },
+          ],
+        },
+        {
+          source: '/:first',
+          has: [
+            {
+              type: 'cookie',
+              key: 'overview-param',
+              value: 'grid',
+            },
+          ],
+          destination: '/:first/~/overview/grid',
+        },
+      ],
+    }
+  },
+}
 
 module.exports = nextConfig
