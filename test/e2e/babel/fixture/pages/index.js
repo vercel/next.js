418deff376c50b58bf67e1da662d1852a2201be8
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2025-09-17 15:23:49 -0700

    move flow syntax into a non-entrypoint file

--- test/e2e/babel/fixture/pages/index.js
+++ test/e2e/babel/fixture/pages/index.js
@@ -1,11 +1,12 @@
-// @flow
-// This page is written in flowtype to test Babel's functionality
+import MyComponent from '../lib/mycomponent'
 import { React } from '../namespace-exported-react'
 
-type Props = {}
+// Note: Flow syntax doesn't work in routes, except in a very narrow case of pages router on webpack
+// without a route segment config.
+//
+// See https://github.com/vercel/next.js/pull/83919 for a full explanation why. This could be fixed
+// if/when edge runtime is deprecated and removed from Next.js.
 
-export default class MyComponent extends React.Component<Props> {
-  render() {
-    return <div id="text">Test Babel</div>
-  }
+export default function App() {
+  return <MyComponent />
 }
