35798dd8afd2dcc636961c47aba0c60532a0a911
devjiwonchoi <devjiwonchoi@gmail.com>
2025-09-23 12:37:18 +0200

    test: use redirect and matcher

--- test/e2e/app-dir/proxy/proxy.ts
+++ test/e2e/app-dir/proxy/proxy.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server'
 
 export function proxy(req: Request) {
-  return NextResponse.rewrite(new URL('/', req.url))
+  return NextResponse.redirect(new URL('/', req.url))
+}
+
+export const config = {
+  matcher: ['/foo'],
 }
