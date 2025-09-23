531a902f82aea8a25dc881991e24435d69fda2ad
devjiwonchoi <devjiwonchoi@gmail.com>
2025-09-23 12:50:10 +0200

    test: remove proxy test

--- test/e2e/app-dir/proxy/proxy.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function proxy(req: Request) {
-  return NextResponse.redirect(new URL('/', req.url))
-}
-
-export const config = {
-  matcher: ['/foo'],
-}
