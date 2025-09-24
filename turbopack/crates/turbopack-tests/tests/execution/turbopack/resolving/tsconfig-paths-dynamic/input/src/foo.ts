e5210b9e0552bca9160844d0b771b73ada4ce422
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-24 14:29:10 +0200

    Turbopack: support more dynamic request with import map

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-paths-dynamic/input/src/foo.ts
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/resolving/tsconfig-paths-dynamic/input/src/foo.ts
@@ -2,10 +2,9 @@ export function loadSub(v: string) {
   return require(`@/sub/${v}`)
 }
 
-// TODO not supported
-// export function loadSubNested(v: string) {
-//   return require(`@/sub-nested/${v}/${v}.js`)
-// }
+export function loadSubNested(v: string) {
+  return require(`@/sub-nested/${v}/${v}.js`)
+}
 
 export function loadSubFallback(v: string) {
   return require(`@sub/${v}`)
