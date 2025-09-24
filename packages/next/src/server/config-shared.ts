83bac4b41fc4225d87559aed05192e46ef36c308
Jiwon Choi <devjiwonchoi@gmail.com>
2025-09-24 11:01:26 +0200

    add closing parenthesis to docs link
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -1118,7 +1118,7 @@ export interface NextConfig {
   /**
    * Add public (in browser) runtime configuration to your app
    *
-   * @see [Runtime configuration](https://nextjs.org/docs/pages/api-reference/config/next-config-js/runtime-configuration
+   * @see [Runtime configuration](https://nextjs.org/docs/pages/api-reference/config/next-config-js/runtime-configuration)
    * @deprecated Runtime config is deprecated and will be removed in Next.js 16.
    */
   publicRuntimeConfig?: { [key: string]: any }
@@ -1126,7 +1126,7 @@ export interface NextConfig {
   /**
    * Add server runtime configuration to your app
    *
-   * @see [Runtime configuration](https://nextjs.org/docs/pages/api-reference/config/next-config-js/runtime-configuration
+   * @see [Runtime configuration](https://nextjs.org/docs/pages/api-reference/config/next-config-js/runtime-configuration)
    * @deprecated Runtime config is deprecated and will be removed in Next.js 16.
    */
   serverRuntimeConfig?: { [key: string]: any }
