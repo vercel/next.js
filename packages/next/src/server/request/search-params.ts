27e76c03e43af8162958351f1d469bc71952218a
devjiwonchoi <devjiwonchoi@gmail.com>
2025-09-25 03:21:53 +0200

    fix: should use promiseInitialized

--- packages/next/src/server/request/search-params.ts
+++ packages/next/src/server/request/search-params.ts
@@ -513,9 +513,16 @@ function makeDynamicallyTrackedSearchParamsWithDevWarnings(
     return cachedSearchParams
   }
 
+  // We have an unfortunate sequence of events that requires this initialization logic. We want to instrument the underlying
+  // searchParams object to detect if you are accessing values in dev. This is used for warnings and for things like the static prerender
+  // indicator. However when we pass this proxy to our Promise.resolve() below the VM checks if the resolved value is a promise by looking
+  // at the `.then` property. To our dynamic tracking logic this is indistinguishable from a `then` searchParam and so we would normally trigger
+  // dynamic tracking. However we know that this .then is not real dynamic access, it's just how thenables resolve in sequence. So we introduce
+  // this initialization concept so we omit the dynamic check until after we've constructed our resolved promise.
+  let promiseInitialized = false
   const proxiedUnderlying = new Proxy(underlyingSearchParams, {
     get(target, prop, receiver) {
-      if (typeof prop === 'string') {
+      if (typeof prop === 'string' && promiseInitialized) {
         if (store.dynamicShouldError) {
           const expression = describeStringPropertyAccess('searchParams', prop)
           throwWithStaticGenerationBailoutErrorWithDynamicError(
@@ -562,6 +569,9 @@ function makeDynamicallyTrackedSearchParamsWithDevWarnings(
   // supports copying with spread and we don't want to unnecessarily
   // instrument the promise with spreadable properties of ReactPromise.
   const promise = makeDevtoolsIOAwarePromise(proxiedUnderlying)
+  promise.then(() => {
+    promiseInitialized = true
+  })
 
   const proxiedPromise = new Proxy(promise, {
     get(target, prop, receiver) {
