b5753c88a9b8af604750f79782e5c66d1482cba9
Hendrik Liebau <mail@hendrik-liebau.de>
2025-09-25 15:56:25 +0200

    Indent code

--- test/development/acceptance-app/undefined-default-export.test.ts
+++ test/development/acceptance-app/undefined-default-export.test.ts
@@ -2,51 +2,56 @@ import path from 'path'
 import { FileRef, nextTestSetup } from 'e2e-utils'
 
 describe('Undefined default export', () => {
-  const { next } = nextTestSetup({
+  const { next, isNextDev } = nextTestSetup({
     files: new FileRef(
       path.join(__dirname, 'fixtures', 'undefined-default-export')
     ),
   })
 
-  it('should error if page component does not have default export', async () => {
-    const browser = await next.browser('/specific-path/1')
+  // TODO: This is currently always true. It's just here to already indent the
+  // code, so that git blame is preserved in the subsequent commit, when we're
+  // moving the file.
+  if (isNextDev) {
+    it('should error if page component does not have default export', async () => {
+      const browser = await next.browser('/specific-path/1')
 
-    await expect(browser).toDisplayRedbox(`
-     {
-       "description": "The default export is not a React Component in "/specific-path/1/page"",
-       "environmentLabel": null,
-       "label": "Runtime Error",
-       "source": null,
-       "stack": [],
-     }
-    `)
-  })
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "The default export is not a React Component in "/specific-path/1/page"",
+         "environmentLabel": null,
+         "label": "Runtime Error",
+         "source": null,
+         "stack": [],
+       }
+      `)
+    })
 
-  it('should error if layout component does not have default export', async () => {
-    const browser = await next.browser('/specific-path/2')
+    it('should error if layout component does not have default export', async () => {
+      const browser = await next.browser('/specific-path/2')
 
-    await expect(browser).toDisplayRedbox(`
-     {
-       "description": "The default export is not a React Component in "/specific-path/2/layout"",
-       "environmentLabel": null,
-       "label": "Runtime Error",
-       "source": null,
-       "stack": [],
-     }
-    `)
-  })
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "The default export is not a React Component in "/specific-path/2/layout"",
+         "environmentLabel": null,
+         "label": "Runtime Error",
+         "source": null,
+         "stack": [],
+       }
+      `)
+    })
 
-  it('should error if not-found component does not have default export when trigger not-found boundary', async () => {
-    const browser = await next.browser('/will-not-found')
+    it('should error if not-found component does not have default export when trigger not-found boundary', async () => {
+      const browser = await next.browser('/will-not-found')
 
-    await expect(browser).toDisplayRedbox(`
-     {
-       "description": "The default export is not a React Component in "/will-not-found/not-found"",
-       "environmentLabel": null,
-       "label": "Runtime Error",
-       "source": null,
-       "stack": [],
-     }
-    `)
-  })
+      await expect(browser).toDisplayRedbox(`
+       {
+         "description": "The default export is not a React Component in "/will-not-found/not-found"",
+         "environmentLabel": null,
+         "label": "Runtime Error",
+         "source": null,
+         "stack": [],
+       }
+      `)
+    })
+  }
 })
