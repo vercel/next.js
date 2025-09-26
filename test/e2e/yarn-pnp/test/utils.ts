8e6069cc44b4aae8e7c99873d8a7916ea9f3d3cb
Cong-Cong <dacongsama@live.com>
2025-09-26 16:00:44 +0800

    fix: yarn pnp test

--- test/e2e/yarn-pnp/test/utils.ts
+++ test/e2e/yarn-pnp/test/utils.ts
@@ -35,10 +35,6 @@ export function runTests(
       delete packageJson.dependencies['react']
       delete packageJson.dependencies['react-dom']
 
-      if (process.env.NEXT_RSPACK) {
-        packageJson.dependencies['@next/rspack-core'] = 'latest'
-      }
-
       next = await createNext({
         files: srcFiles.reduce(
           (prev, file) => {
