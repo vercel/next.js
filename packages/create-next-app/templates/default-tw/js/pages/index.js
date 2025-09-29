4e12b1650e5be1d1f8550e3b8e35919ec0b35805
skullface <skullface@users.noreply.github.com>
2025-09-29 18:47:20 -0400

    fix pages directory utm param

--- packages/create-next-app/templates/default-tw/js/pages/index.js
+++ packages/create-next-app/templates/default-tw/js/pages/index.js
@@ -32,14 +32,14 @@ export default function Home() {
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=pagesdir-template-tw&utm_campaign=create-next-app"
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Template
             </a>{" "}
             or the{" "}
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=pagesdir-template-tw&utm_campaign=create-next-app"
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Learning
