a6fee278cd1c288fd37f068e5e4fd9ac2cf3b75b
skullface <skullface@users.noreply.github.com>
2025-09-29 18:23:43 -0400

    update links to templates and learning

--- packages/create-next-app/templates/default-tw/js/pages/index.js
+++ packages/create-next-app/templates/default-tw/js/pages/index.js
@@ -31,13 +31,19 @@ export default function Home() {
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
-            <span className="font-medium text-zinc-950 dark:text-zinc-50">
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=pagesdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
               Template
-            </span>{" "}
+            </a>{" "}
             or the{" "}
-            <span className="font-medium text-zinc-950 dark:text-zinc-50">
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=pagesdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
               Learning
-            </span>{" "}
+            </a>{" "}
             center.
           </p>
         </div>
