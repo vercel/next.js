5ee54477ef7c151be3009abe03e5b31bb6789395
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-30 12:12:10 +0200

    Cleanup tests

--- test/e2e/twoslash/index.test.ts
+++ test/e2e/twoslash/index.test.ts
@@ -8,9 +8,11 @@ describe('twoslash', () => {
     },
   })
 
-  it('should annotate twoslash types default', async () => {
-    const { code, nodes, error } = JSON.parse(await next.render('/'))
-    expect({ code, nodes, error }).toMatchInlineSnapshot(`
+  it.each(['default', 'esnext'])(
+    'should annotate twoslash types %s',
+    async (mode) => {
+      const { code, nodes, error } = JSON.parse(await next.render(`/?${mode}`))
+      expect({ code, nodes, error }).toMatchInlineSnapshot(`
      {
        "code": "type X = Promise<number>;
      'hello'.toUpperCase()",
@@ -48,47 +50,6 @@ describe('twoslash', () => {
        ],
      }
     `)
-  })
-
-  it('should annotate twoslash types esnext', async () => {
-    const { code, nodes, error } = JSON.parse(await next.render('/?esnext'))
-    expect({ code, nodes, error }).toMatchInlineSnapshot(`
-     {
-       "code": "type X = Promise<number>;
-     'hello'.toUpperCase()",
-       "error": undefined,
-       "nodes": [
-         {
-           "character": 5,
-           "length": 1,
-           "line": 0,
-           "start": 5,
-           "target": "X",
-           "text": "type X = Promise<number>",
-           "type": "hover",
-         },
-         {
-           "character": 9,
-           "docs": "Represents the completion of an asynchronous operation",
-           "length": 7,
-           "line": 0,
-           "start": 9,
-           "target": "Promise",
-           "text": "interface Promise<T>",
-           "type": "hover",
-         },
-         {
-           "character": 8,
-           "docs": "Converts all the alphabetic characters in a string to uppercase.",
-           "length": 11,
-           "line": 1,
-           "start": 34,
-           "target": "toUpperCase",
-           "text": "(method) String.toUpperCase(): string",
-           "type": "hover",
-         },
-       ],
-     }
-    `)
-  })
+    }
+  )
 })
--- test/e2e/twoslash/standalone.test.ts
+++ test/e2e/twoslash/standalone.test.ts
@@ -25,129 +25,89 @@ if (!(globalThis as any).isNextStart) {
       return
     }
 
+    let server: any
+    let appPort: number
+    let tmpFolder: string
+
     beforeAll(async () => {
       await next.patchFile(
         'next.config.js',
         (await next.readFile('next.config.js')).replace('// output', 'output')
       )
-      await next.start()
-    })
+      await next.build()
 
-    it('should annotate twoslash types', async () => {
-      const tmpFolder = path.join(os.tmpdir(), 'next-standalone-' + Date.now())
+      tmpFolder = path.join(os.tmpdir(), 'next-standalone-' + Date.now())
       await fs.mkdirp(tmpFolder)
       const distFolder = path.join(tmpFolder, 'test')
       await fs.move(path.join(next.testDir, '.next/standalone'), distFolder)
-      let server: any
-      try {
-        const testServer = path.join(distFolder, 'server.js')
-        const appPort = await findPort()
-        server = await initNextServerScript(
-          testServer,
-          /- Local:/,
-          {
-            ...process.env,
-            PORT: appPort.toString(),
-          },
-          undefined,
-          {
-            cwd: distFolder,
-          }
-        )
-
+      const testServer = path.join(distFolder, 'server.js')
+      appPort = await findPort()
+      server = await initNextServerScript(
+        testServer,
+        /- Local:/,
         {
-          const { code, nodes, error } = await (
-            await fetchViaHTTP(appPort, '/')
-          ).json()
-          expect({ code, nodes, error }).toMatchInlineSnapshot(`
-           {
-             "code": "type X = Promise<number>;
-           'hello'.toUpperCase()",
-             "error": undefined,
-             "nodes": [
-               {
-                 "character": 5,
-                 "length": 1,
-                 "line": 0,
-                 "start": 5,
-                 "target": "X",
-                 "text": "type X = Promise<number>",
-                 "type": "hover",
-               },
-               {
-                 "character": 9,
-                 "docs": "Represents the completion of an asynchronous operation",
-                 "length": 7,
-                 "line": 0,
-                 "start": 9,
-                 "target": "Promise",
-                 "text": "interface Promise<T>",
-                 "type": "hover",
-               },
-               {
-                 "character": 8,
-                 "docs": "Converts all the alphabetic characters in a string to uppercase.",
-                 "length": 11,
-                 "line": 1,
-                 "start": 34,
-                 "target": "toUpperCase",
-                 "text": "(method) String.toUpperCase(): string",
-                 "type": "hover",
-               },
-             ],
-           }
-          `)
-        }
-
+          ...process.env,
+          PORT: appPort.toString(),
+        },
+        undefined,
         {
-          const { code, nodes, error } = await (
-            await fetchViaHTTP(appPort, '/?esnext')
-          ).json()
-          expect({ code, nodes, error }).toMatchInlineSnapshot(`
-           {
-             "code": "type X = Promise<number>;
-           'hello'.toUpperCase()",
-             "error": undefined,
-             "nodes": [
-               {
-                 "character": 5,
-                 "length": 1,
-                 "line": 0,
-                 "start": 5,
-                 "target": "X",
-                 "text": "type X = Promise<number>",
-                 "type": "hover",
-               },
-               {
-                 "character": 9,
-                 "docs": "Represents the completion of an asynchronous operation",
-                 "length": 7,
-                 "line": 0,
-                 "start": 9,
-                 "target": "Promise",
-                 "text": "interface Promise<T>",
-                 "type": "hover",
-               },
-               {
-                 "character": 8,
-                 "docs": "Converts all the alphabetic characters in a string to uppercase.",
-                 "length": 11,
-                 "line": 1,
-                 "start": 34,
-                 "target": "toUpperCase",
-                 "text": "(method) String.toUpperCase(): string",
-                 "type": "hover",
-               },
-             ],
-           }
-          `)
+          cwd: distFolder,
         }
-      } finally {
-        if (server) await killApp(server)
-        if (!process.env.NEXT_TEST_SKIP_CLEANUP) {
-          await fs.remove(tmpFolder)
-        }
+      )
+    })
+
+    afterAll(async () => {
+      if (server) await killApp(server)
+      if (!process.env.NEXT_TEST_SKIP_CLEANUP) {
+        await fs.remove(tmpFolder)
       }
     })
+
+    it.each(['default', 'esnext'])(
+      'should annotate twoslash types %s',
+      async (mode) => {
+        const { code, nodes, error } = await (
+          await fetchViaHTTP(appPort, `/?${mode}`)
+        ).json()
+        expect({ code, nodes, error }).toMatchInlineSnapshot(`
+           {
+             "code": "type X = Promise<number>;
+           'hello'.toUpperCase()",
+             "error": undefined,
+             "nodes": [
+               {
+                 "character": 5,
+                 "length": 1,
+                 "line": 0,
+                 "start": 5,
+                 "target": "X",
+                 "text": "type X = Promise<number>",
+                 "type": "hover",
+               },
+               {
+                 "character": 9,
+                 "docs": "Represents the completion of an asynchronous operation",
+                 "length": 7,
+                 "line": 0,
+                 "start": 9,
+                 "target": "Promise",
+                 "text": "interface Promise<T>",
+                 "type": "hover",
+               },
+               {
+                 "character": 8,
+                 "docs": "Converts all the alphabetic characters in a string to uppercase.",
+                 "length": 11,
+                 "line": 1,
+                 "start": 34,
+                 "target": "toUpperCase",
+                 "text": "(method) String.toUpperCase(): string",
+                 "type": "hover",
+               },
+             ],
+           }
+          `)
+      }
+    )
   })
 }
