7781684ef759ed8f22f47eb3510dd6449981a8a9
skullface <skullface@users.noreply.github.com>
2025-09-30 14:47:17 -0400

    update docs link on pages router

--- packages/create-next-app/templates/default-tw/js/pages/index.js
+++ packages/create-next-app/templates/default-tw/js/pages/index.js
@@ -65,7 +65,7 @@ export default function Home() {
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://nextjs.org/docs/pages/getting-started?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
