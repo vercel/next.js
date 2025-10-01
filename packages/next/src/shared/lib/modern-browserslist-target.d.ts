9206d8139b7f858288ca0bff47911bc96cf61cd2
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-10-01 18:40:13 +0200

    Cleanup types

--- packages/next/src/shared/lib/modern-browserslist-target.d.ts
+++ packages/next/src/shared/lib/modern-browserslist-target.d.ts
@@ -1,9 +1,2 @@
-// Keep in sync with the `.js` file.
-declare const MODERN_BROWSERSLIST_TARGET: [
-  'chrome 111',
-  'edge 111',
-  'firefox 111',
-  'safari 16.4',
-]
-
+declare const MODERN_BROWSERSLIST_TARGET: Array<string>
 export default MODERN_BROWSERSLIST_TARGET
