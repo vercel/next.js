ad75d50d7875a38814c528d8a5f4c36ed81f5f0e
Steven <steven@ceriously.com>
2025-10-09 12:32:42 -0400

    separate into two files

--- test/integration/image-optimizer/test/maximum-redirects-1.test.ts
+++ test/integration/image-optimizer/test/maximum-redirects-1.test.ts
@@ -3,25 +3,6 @@ import { setupTests } from './util'
 
 const appDir = join(__dirname, '../app')
 
-describe('with maximumRedirects 0', () => {
-  setupTests({
-    nextConfigImages: {
-      dangerouslyAllowPrivateIP: true,
-      // Configure external domains so we can try out external redirects
-      domains: [
-        'localhost',
-        '127.0.0.1',
-        'example.com',
-        'assets.vercel.com',
-        'image-optimization-test.vercel.app',
-      ],
-      // Prevent redirects
-      maximumRedirects: 0,
-    },
-    appDir,
-  })
-})
-
 describe('with maximumRedirects 1', () => {
   setupTests({
     nextConfigImages: {
