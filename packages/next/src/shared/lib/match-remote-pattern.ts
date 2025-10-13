5e95e7dbc86e1df0f6063edaf1a093c86e05d41f
Rachna chavan <rachna.chavan@emeritus.org>
2025-10-13 19:14:58 +0530

    fix: Addressed review comments

--- packages/next/src/shared/lib/match-remote-pattern.ts
+++ packages/next/src/shared/lib/match-remote-pattern.ts
@@ -17,7 +17,7 @@ export function matchRemotePattern(
     }
   }
 
-  if (pattern.hostname === undefined || pattern.hostname === '') {
+  if (pattern.hostname === undefined) {
     throw new Error(
       `Pattern should define hostname but found\n${JSON.stringify(pattern)}`
     )
