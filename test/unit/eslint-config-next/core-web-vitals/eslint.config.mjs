f70ce8115fb3a67edb671a94d219a37263bcb00b
devjiwonchoi <devjiwonchoi@gmail.com>
2025-10-13 17:48:11 +0200

    Add test for eslint-config-next

---
+++ test/unit/eslint-config-next/core-web-vitals/eslint.config.mjs
@@ -0,0 +1,12 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default compat.extends('next/core-web-vitals')
