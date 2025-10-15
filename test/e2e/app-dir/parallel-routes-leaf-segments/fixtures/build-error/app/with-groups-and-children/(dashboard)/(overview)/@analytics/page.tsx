3cfadbbf1602ed73b6d2374c678299a584af48cd
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-10-14 23:07:14 -0600

    test: remove other slot to make test more deterministic

--- test/e2e/app-dir/parallel-routes-leaf-segments/fixtures/build-error/app/with-groups-and-children/(dashboard)/(overview)/@analytics/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AnalyticsSlot() {
-  return (
-    <div>
-      <h4>Analytics Slot (Parent)</h4>
-      <p>This is the @analytics parallel route with route groups</p>
-    </div>
-  )
-}
