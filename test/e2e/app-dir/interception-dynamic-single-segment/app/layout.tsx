a199d3f849beb3dc0b758ddfe886cdfdc4e781c7
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-10-15 12:32:45 -0600

    test: added support for cc

--- test/e2e/app-dir/interception-dynamic-single-segment/app/layout.tsx
+++ test/e2e/app-dir/interception-dynamic-single-segment/app/layout.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from 'react'
+
 export default function Layout(props: {
   children: React.ReactNode
   modal: React.ReactNode
 }) {
   return (
-    <html>
-      <body>
-        <div id="children">{props.children}</div>
-        <div id="modal">{props.modal}</div>
-      </body>
-    </html>
+    <Suspense>
+      <html>
+        <body>
+          <div id="children">{props.children}</div>
+          <div id="modal">{props.modal}</div>
+        </body>
+      </html>
+    </Suspense>
   )
 }
