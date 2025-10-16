2459079cbeffbdab54faa933a4cff26ed4a1beb6
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-10-16 10:35:21 -0700

    fix

--- test/integration/url-imports/next.config.js
+++ test/integration/url-imports/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   experimental: {
     urlImports: [
       'http://localhost:12345/',
-      'https://github.com/vercel/next.js/raw/canary/',
+      'https://github.com/vercel/next.js/raw/next-15-5/',
     ],
   },
 }
