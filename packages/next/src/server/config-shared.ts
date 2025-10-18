069c75b90cb21abc8e57d61a4d4f57f03fe9f774
Andrea Cappuccio <hood@null.net>
2025-10-18 17:00:15 +0200

    Clarify user agent comment for streaming metadata
    Updated comment to clarify user agent behavior for bots. We’re actually specifying user agents for bots that can NOT handle streaming metadata.

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -1309,7 +1309,7 @@ export interface NextConfig {
   }
 
   /**
-   * User Agent of bots that can handle streaming metadata.
+   * User Agent of bots that can not handle streaming metadata.
    * Besides the default behavior, Next.js act differently on serving metadata to bots based on their capability.
    *
    * @default
