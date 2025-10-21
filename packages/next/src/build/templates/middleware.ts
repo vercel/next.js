f64edae7a829afd52c253bb6c6cc7818e7f7c370
devjiwonchoi <devjiwonchoi@gmail.com>
2025-10-21 12:03:13 -0700

    add handler function check

--- packages/next/src/build/templates/middleware.ts
+++ packages/next/src/build/templates/middleware.ts
@@ -15,6 +15,22 @@ const page: string = 'VAR_DEFINITION_PAGE'
 const isProxy = page === '/proxy' || page === '/src/proxy'
 const handler = (isProxy ? mod.proxy : mod.middleware) || mod.default
 
+class ProxyMissingExportError extends Error {
+  constructor(message: string) {
+    super(message)
+    // Stack isn't useful here, remove it considering it spams logs during development.
+    this.stack = ''
+  }
+}
+
+// TODO: This spams logs during development. Find a better way to handle this.
+// Removing this will spam "fn is not a function" logs which is worse.
+if (typeof handler !== 'function') {
+  throw new ProxyMissingExportError(
+    `The ${isProxy ? 'Proxy' : 'Middleware'} file "${page}" must export a function named \`${isProxy ? 'proxy' : 'middleware'}\` or a default function.`
+  )
+}
+
 // Proxy will only sent out the FetchEvent to next server,
 // so load instrumentation module here and track the error inside proxy module.
 function errorHandledHandler(fn: AdapterOptions['handler']) {
