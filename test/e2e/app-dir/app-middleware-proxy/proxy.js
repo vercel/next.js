60ed606b4396d1f53410f6771123cb50ae43ba27
devjiwonchoi <devjiwonchoi@gmail.com>
2025-10-21 07:46:06 -0700

    draftMode cannot be set on nodejs runtime Middleware/Proxy

--- test/e2e/app-dir/app-middleware-proxy/proxy.js
+++ test/e2e/app-dir/app-middleware-proxy/proxy.js
@@ -97,3 +97,5 @@ export async function proxy(request) {
     },
   })
 }
+
+export const runtime = 'nodejs'
--- test/e2e/app-dir/app-middleware/middleware.js
+++ test/e2e/app-dir/app-middleware/middleware.js
@@ -95,3 +95,5 @@ export async function middleware(request) {
     },
   })
 }
+
+export const runtime = 'nodejs'
