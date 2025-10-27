717175bf5b513459da0915fb796261e41bfcc5b5
Luke Sandberg <lukeisandberg@gmail.com>
2025-10-27 16:31:36 -0700

    write tests, flip default the other way

--- crates/next-core/src/next_config.rs
+++ crates/next-core/src/next_config.rs
@@ -2209,7 +2209,7 @@ impl NextConfig {
             NextMode::Development => self
                 .experimental
                 .turbopack_use_whole_app_module_graph_in_dev
-                .unwrap_or(true),
+                .unwrap_or(false),
             NextMode::Build => true,
         }))
     }
