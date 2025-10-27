ed8d6e22e950bc55705ef62bdcab7df0437129b9
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2025-10-27 11:59:35 +0100

    fix

--- test/e2e/api-resolver-query-writeable/pages/api/index.js
+++ test/e2e/api-resolver-query-writeable/pages/api/index.js
@@ -1,12 +1,4 @@
 export default (req, res) => {
-  if (
-    process.env.CUSTOM_SERVER &&
-    typeof req.fromCustomServer === 'undefined'
-  ) {
-    throw new Error('missing custom req field')
-  }
-
-  if (req.method === 'POST') {
-    res.status(200).json(req.body)
-  }
+    req.query = { ...req.query, changed: 'yes' }
+    res.status(200).json({ query: req.query })
 }
