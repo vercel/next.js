566f86a679fad8cb7947d6fb7d66834a09debcad
Tim Neutkens <tim@timneutkens.nl>
2025-10-27 13:00:05 +0100

    Fix lint

--- test/e2e/api-resolver-query-writeable/pages/api/index.js
+++ test/e2e/api-resolver-query-writeable/pages/api/index.js
@@ -1,4 +1,4 @@
 export default (req, res) => {
-    req.query = { ...req.query, changed: 'yes' }
-    res.status(200).json({ query: req.query })
+  req.query = { ...req.query, changed: 'yes' }
+  res.status(200).json({ query: req.query })
 }
