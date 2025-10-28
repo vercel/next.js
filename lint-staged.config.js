7a044385ac104685757e41ee5013d22b548b08f9
Tim Neutkens <tim@timneutkens.nl>
2025-10-28 16:16:19 +0100

    FIx cwd with lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,12 @@
+const path = require('path')
 module.exports = {
   '*.{js,jsx,mjs,ts,tsx,mts,mdx}': [
-    'prettier --with-node-modules --ignore-path .prettierignore --write',
-    'oxfmt --ignore-path .oxfmtignore --config=.prettierrc.json',
-    'eslint --config eslint.config.mjs --fix',
+    `cd ${__dirname} && prettier --with-node-modules --ignore-path ${path.join(__dirname, '.prettierignore')} --config=${path.join(__dirname, '.prettierrc.json')} --write`,
+    `cd ${__dirname} && oxfmt --ignore-path ${path.join(__dirname, '.oxfmtignore')} --config=${path.join(__dirname, '.prettierrc.json')}`,
+    `cd ${__dirname} && eslint --config ${path.join(__dirname, 'eslint.config.mjs')} --fix`,
   ],
   '*.{json,md,css,html,yml,yaml,scss}': [
-    'prettier --with-node-modules --ignore-path .prettierignore --write',
+    `cd ${__dirname} && prettier --with-node-modules --ignore-path ${path.join(__dirname, '.prettierignore')} --write`,
   ],
-  '*.rs': ['rustfmt --edition 2024 --'],
+  '*.rs': [`cd ${__dirname} && rustfmt --edition 2024 --`],
 }
