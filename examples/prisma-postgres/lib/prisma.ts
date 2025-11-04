fe4b5ba6b608756bbca308e62f0183246b728cb5
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-04 18:33:12 +0530

    remove accelerate and use pg adapter only

--- examples/prisma-postgres/lib/prisma.ts
+++ examples/prisma-postgres/lib/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma-client/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-
-const prisma = new PrismaClient({ adapter }).$extends(withAccelerate());
+const prisma = new PrismaClient({ adapter })
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
