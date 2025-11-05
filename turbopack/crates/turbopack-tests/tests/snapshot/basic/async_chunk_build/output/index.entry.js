eeaddd5e8cf39e4c845c4c4efdcb456e6d31173e
Tobias Koppers <tobias.koppers@googlemail.com>
2025-11-05 12:06:57 +0100

    add test cases

--- turbopack/crates/turbopack-tests/tests/snapshot/basic/async_chunk_build/output/index.entry.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic/async_chunk_build/output/index.entry.js
@@ -1,4 +1,4 @@
 var R=require("./[turbopack]_runtime.js")("output/index.entry.js")
-R.c("output/aaf3a_crates_turbopack-tests_tests_snapshot_basic_async_chunk_build_input_c808baa6._.js")
+R.c("output/aaf3a_crates_turbopack-tests_tests_snapshot_basic_async_chunk_build_input_4eca5628._.js")
 R.m("[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic/async_chunk_build/input/index.js [test] (ecmascript)")
 module.exports=R.m("[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic/async_chunk_build/input/index.js [test] (ecmascript)").exports
--- turbopack/crates/turbopack-tests/tests/snapshot/css/split-shared/output/ba425_crates_turbopack-tests_tests_snapshot_css_split-shared_input_index_a38a53ec.js
+++
@@ -1,5 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([
-    "output/ba425_crates_turbopack-tests_tests_snapshot_css_split-shared_input_index_a38a53ec.js",
-    {"otherChunks":["output/turbopack_crates_turbopack-tests_tests_snapshot_css_split-shared_input_943ac5d4._.js","output/aaf3a_crates_turbopack-tests_tests_snapshot_css_split-shared_input_index_c4f10641.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/split-shared/input/index.js [test] (ecmascript)"]}
-]);
-// Dummy runtime
