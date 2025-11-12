87a016953b156aa4931063c6523678804930247e
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-12 11:05:15 +0100

    NODE_ENV = "production" in execution tests

--- turbopack/crates/turbopack-tests/tests/execution.rs
+++ turbopack/crates/turbopack-tests/tests/execution.rs
@@ -353,7 +353,7 @@ async fn run_test_operation(prepared_test: ResolvedVc<PreparedTest>) -> Result<V
             compile_time_defines!(
                 process.turbopack = true,
                 process.env.TURBOPACK = true,
-                process.env.NODE_ENV = "development",
+                process.env.NODE_ENV = "production",
             )
             .resolved_cell(),
         )
