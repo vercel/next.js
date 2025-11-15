46ba9252d157886f4f2df0201d27d7936563dd4d
Rastislav Elias <contact@rastislavelias.com>
2025-11-15 18:16:24 +0000

    fix(template): improve mobile spacing in Tailwind starter layout

--- packages/create-next-app/templates/app-tw/ts/app/page.tsx
+++ packages/create-next-app/templates/app-tw/ts/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-6 justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
