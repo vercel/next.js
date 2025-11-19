77422089f62521b31f220ab85e042b49b3df5ee2
devjiwonchoi <devjiwonchoi@gmail.com>
2025-11-19 15:06:56 +0100

    Exclude nft check in turbopack

--- test/e2e/app-dir/proxy-nfc-traced/proxy-nfc-traced.test.ts
+++ test/e2e/app-dir/proxy-nfc-traced/proxy-nfc-traced.test.ts
@@ -7,21 +7,25 @@ import path from 'path'
 // from the actual outputs, which will fail for the providers like Vercel that checks for the files in NFT.
 
 describe('proxy-nfc-traced', () => {
-  const { next } = nextTestSetup({
+  const { next, isTurbopack, isNextStart } = nextTestSetup({
     files: __dirname,
   })
 
-  it('should have renamed trace file as middleware instead of proxy', async () => {
-    const nfc = JSON.parse(
-      fs.readFileSync(
-        path.join(next.testDir, '.next/server/middleware.js.nft.json'),
-        'utf-8'
+  // 'middleware.js' won't be traced because Turbopack doesn't bundle all code to .next/server/middleware.js
+  if (isNextStart && !isTurbopack) {
+    it('should have renamed trace file as middleware instead of proxy', async () => {
+      const nfc = JSON.parse(
+        fs.readFileSync(
+          path.join(next.testDir, '.next/server/middleware.js.nft.json'),
+          'utf-8'
+        )
       )
-    )
-    expect(nfc.files).toContain('middleware.js')
-    expect(nfc.files).not.toContain('proxy.js')
-  })
+      expect(nfc.files).toContain('middleware.js')
+      expect(nfc.files).not.toContain('proxy.js')
+    })
+  }
 
+  // Previously, the deployment tests failed because of the traced file name mismatch.
   it('should successfully build and be redirected from proxy', async () => {
     const $ = await next.render$('/home')
     expect($('p').text()).toBe('hello world')
