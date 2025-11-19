69e5f94c7624552707a4b9ce5ff9b23ed0c6e615
Sebastian "Sebbie" Silbermann <sebastian.silbermann@vercel.com>
2025-11-19 19:47:46 +0100

    [devtools] Ensure Chrome DevTools workspace can connect with proxy rewrites (#86289)

--- packages/next/src/server/lib/router-server.ts
+++ packages/next/src/server/lib/router-server.ts
@@ -575,7 +575,8 @@ export async function initialize(opts: {
         )
       }
 
-      if (opts.dev && isChromeDevtoolsWorkspaceUrl(parsedUrl)) {
+      // We want the original pathname without any basePath or proxy rewrites.
+      if (opts.dev && isChromeDevtoolsWorkspaceUrl(req.url)) {
         await handleChromeDevtoolsWorkspaceRequest(res, opts, config)
         return
       }
