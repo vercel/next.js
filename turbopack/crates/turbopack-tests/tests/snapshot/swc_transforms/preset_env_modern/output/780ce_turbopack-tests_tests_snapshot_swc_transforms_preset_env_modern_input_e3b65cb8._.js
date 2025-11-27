1920da5d2ec4927352ad8b323471fc4ab7fd289f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-27 14:40:45 +0100

    update snapshot after fix

--- turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env_modern/output/780ce_turbopack-tests_tests_snapshot_swc_transforms_preset_env_modern_input_e3b65cb8._.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env_modern/output/780ce_turbopack-tests_tests_snapshot_swc_transforms_preset_env_modern_input_e3b65cb8._.js
@@ -4,12 +4,11 @@
 
 __turbopack_context__.s([
     "char",
-    ()=>_char
+    ()=>char
 ]);
-function _char() {
+function char() {
     return 0;
 }
-;
 }),
 "[project]/turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env_modern/input/Parser.js [test] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
@@ -21,7 +20,7 @@ __turbopack_context__.s([
 var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$swc_transforms$2f$preset_env_modern$2f$input$2f$Tokenizer$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env_modern/input/Tokenizer.js [test] (ecmascript)");
 ;
 function comment() {
-    console.log(char());
+    console.log((0, __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$swc_transforms$2f$preset_env_modern$2f$input$2f$Tokenizer$2e$js__$5b$test$5d$__$28$ecmascript$29$__["char"])());
 }
 }),
 "[project]/turbopack/crates/turbopack-tests/tests/snapshot/swc_transforms/preset_env_modern/input/index.js [test] (ecmascript)", ((__turbopack_context__) => {
