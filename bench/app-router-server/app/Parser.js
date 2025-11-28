89e49d1db67d3cc7ed2fa4acdad8755592eee736
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-28 10:19:56 +0100

    cleanup

--- bench/app-router-server/app/Parser.js
+++
@@ -1,5 +0,0 @@
-import { char } from './Tokenizer.js'
-
-export function comment() {
-  console.log(char())
-}
--- bench/app-router-server/app/page.js
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import * as x from './Parser.js'
-
-console.log(x.comment())
-
-export default function Stylis() {
-  return 123
-}
