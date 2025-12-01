3e3048c7119860f0edb2d7a7a25713c55d4503a4
JJ Kasper <jj@jjsweb.site>
2025-12-01 12:08:23 -0800

    config

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ const customJestConfig = {
     '<rootDir>/../packages/next-codemod/',
     '<rootDir>/../packages/eslint-plugin-internal/',
     '<rootDir>/../packages/font/src/',
-    '<rootDir>/../packages/nex-trouting/',
+    '<rootDir>/../packages/next-routing/',
   ],
   modulePathIgnorePatterns: ['/\\.next/'],
   modulePaths: ['<rootDir>/lib'],
