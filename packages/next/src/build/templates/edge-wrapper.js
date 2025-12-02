4b4c2ee1be1829101df7f126be2a21e1837ab65d
Jiachi Liu <inbox@huozhi.im>
2025-12-02 23:55:17 +0100

    Add comments to edge-wrapper.js for clarity

--- packages/next/src/build/templates/edge-wrapper.js
+++ packages/next/src/build/templates/edge-wrapper.js
@@ -1,3 +1,11 @@
+// The wrapped module could be an async module, we handle that with the proxy
+// here. The comma expression makes sure we don't call the function with the
+// module as the "this" arg.
+// Turn exports into functions that are also a thenable. This way you can await the whole object
+// or  exports (e.g. for Components) or call them directly as though they are async functions
+// (e.g. edge functions/middleware, this is what the Edge Runtime does).
+// Catch promise to prevent UnhandledPromiseRejectionWarning, this will be propagated through
+// the awaited export(s) anyway.
 self._ENTRIES ||= {}
 const modProm = import('MODULE')
 modProm.catch(() => {})
