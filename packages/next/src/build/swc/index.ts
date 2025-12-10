b27fc678d90d107867e4efa830213551b8c02a05
JJ Kasper <jj@jjsweb.site>
2025-12-09 19:26:54 -0800

    lock binaries

--- packages/next/src/build/swc/index.ts
+++ packages/next/src/build/swc/index.ts
@@ -47,7 +47,8 @@ type RawWasmBindings = typeof import('./generated-wasm') & {
   default?(): Promise<typeof import('./generated-wasm')>
 }
 
-const nextVersion = process.env.__NEXT_VERSION as string
+// locked version as binaries are locked
+const nextVersion = '15.1.9'
 
 const ArchName = arch()
 const PlatformName = platform()
