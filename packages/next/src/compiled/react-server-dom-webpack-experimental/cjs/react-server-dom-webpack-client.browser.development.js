b23e532da2c13ab750b21797a74741dd766e3a53
Hendrik Liebau <mail@hendrik-liebau.de>
2025-12-11 20:36:07 +0100

    Update React Version (#44)
    Co-authored-by: Josh Story <story@hey.com>

--- packages/next/src/compiled/react-server-dom-webpack-experimental/cjs/react-server-dom-webpack-client.browser.development.js
+++ packages/next/src/compiled/react-server-dom-webpack-experimental/cjs/react-server-dom-webpack-client.browser.development.js
@@ -2929,10 +2929,10 @@
       return hook.checkDCE ? !0 : !1;
     })({
       bundleType: 1,
-      version: "19.1.0-experimental-86a61a43-20251209",
+      version: "19.1.0-experimental-8ad20fc0-20251211",
       rendererPackageName: "react-server-dom-webpack",
       currentDispatcherRef: ReactSharedInternals,
-      reconcilerVersion: "19.1.0-experimental-86a61a43-20251209",
+      reconcilerVersion: "19.1.0-experimental-8ad20fc0-20251211",
       getCurrentComponentInfo: function () {
         return currentOwnerInDEV;
       }
