dd233994aeb24e906cdb9aedca5447cdef401792
Hendrik Liebau <mail@hendrik-liebau.de>
2025-12-11 23:46:21 +0100

    Backport facebook/react#35351 for 15.5.8 (#87086)
    facebook/react#35351
    
    Co-authored-by: Josh Story <story@hey.com>

--- packages/next/src/compiled/react-server-dom-webpack-experimental/cjs/react-server-dom-webpack-client.browser.development.js
+++ packages/next/src/compiled/react-server-dom-webpack-experimental/cjs/react-server-dom-webpack-client.browser.development.js
@@ -4561,10 +4561,10 @@
       return hook.checkDCE ? !0 : !1;
     })({
       bundleType: 1,
-      version: "19.2.0-experimental-3b8da6c4-20251211",
+      version: "19.2.0-experimental-7288a7d9-20251211",
       rendererPackageName: "react-server-dom-webpack",
       currentDispatcherRef: ReactSharedInternals,
-      reconcilerVersion: "19.2.0-experimental-3b8da6c4-20251211",
+      reconcilerVersion: "19.2.0-experimental-7288a7d9-20251211",
       getCurrentComponentInfo: function () {
         return currentOwnerInDEV;
       }
