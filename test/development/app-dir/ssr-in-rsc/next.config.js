5549bc0892fa6f7258ab71ace3bde3bd00eab607
Cong-Cong <dacongsama@live.com>
2025-12-17 16:12:02 +0800

    update snapshopt

--- test/development/app-dir/ssr-in-rsc/next.config.js
+++ test/development/app-dir/ssr-in-rsc/next.config.js
@@ -5,8 +5,11 @@ const nextConfig = {
   webpack(config) {
     if (process.env.NEXT_RSPACK) {
       // Disable persistent cache when using Rspack.
-      // Rspack's persistent cache may reuse the previously compiled pages.
-      // This differs from webpack in dev mode, which typically builds only the page being requested.
+      // Rspack may reuse previously compiled pages from its persistent cache.
+      // In development, webpack typically compiles only the page being requested.
+      // Keeping Rspack's persistent cache enabled can cause tests to surface errors
+      // from previously compiled pages, making it hard to assert errors for the
+      // currently requested page only.
       config.cache = false
     }
     return config
