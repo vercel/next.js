7a41ccafa24beb175951821cd6754da07b2f659b
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-12-26 00:20:06 +0530

    Add example: with-better-auth

---
+++ examples/with-better-auth-prisma/lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import prisma from "./prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET, // optional
+  emailAndPassword: { enabled: true },
+  plugins: [nextCookies()],
+});
---
+++ examples/with-better-auth-prisma/types.d.ts
@@ -0,0 +1,9 @@
+declare module "*.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
