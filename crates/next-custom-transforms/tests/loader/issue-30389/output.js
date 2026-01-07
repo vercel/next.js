0c06f06838365b697f186f6e889f89d86b4c326a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-07 23:30:29 +0100

    Upgrade to swc 54 (#88207)
    Somewhat surprisingly, no breaking API changes
    
    https://github.com/swc-project/swc/compare/v1.15.5...v1.15.8 tracks the changes

--- crates/next-custom-transforms/tests/loader/issue-30389/output.js
+++ crates/next-custom-transforms/tests/loader/issue-30389/output.js
@@ -1,3 +1,3 @@
-var _my_array;
 var _ref;
+var _my_array;
 var test = (_ref = (_my_array = my_array) === null || _my_array === void 0 ? void 0 : _my_array[0]) !== null && _ref !== void 0 ? _ref : [];
