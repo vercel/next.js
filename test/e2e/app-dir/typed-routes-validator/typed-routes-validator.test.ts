8821555d246298f36816ce53fe61e1886020b100
Tim Neutkens <tim@timneutkens.nl>
2026-01-08 11:51:07 +0100

    Update typed-routes-validator.test.ts

--- test/e2e/app-dir/typed-routes-validator/typed-routes-validator.test.ts
+++ test/e2e/app-dir/typed-routes-validator/typed-routes-validator.test.ts
@@ -18,10 +18,10 @@ describe('typed-routes-validator', () => {
   it('should generate route validation correctly', async () => {
     if (isNextDev) {
       await next.start()
+      await next.fetch('/')
     } else {
       await next.build()
     }
-    await next.fetch('/')
     const dts = await next.readFile(`${getDistDir()}/types/validator.ts`)
     // sanity check that dev generation is working
     expect(dts).toContain('const handler = {} as typeof import(')
