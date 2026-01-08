d26e9c48e6fabec26c617fa8a6e59eae6fa36eee
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-08 10:32:21 +0100

    proper error

--- test/e2e/swc-plugins/index.test.ts
+++ test/e2e/swc-plugins/index.test.ts
@@ -30,36 +30,20 @@ describe('swcPlugins', () => {
     it('shows a redbox in dev', async () => {
       const browser = await next.browser('/')
 
-      // TODO update messages
       if (isTurbopack) {
         await expect(browser).toDisplayRedbox(`
          {
-           "description": "Module not found: Can't resolve 'next/dist/esm/build/templates/helpers'",
+           "description": "Failed to execute SWC plugin",
            "environmentLabel": null,
            "label": "Build Error",
-           "source": "<FIXME-nextjs-internal-source>
-         Module not found: Can't resolve 'next/dist/esm/build/templates/helpers'
-         failed to analyze ecmascript module '[project]/node_modules/.pnpm/next@file+..+next-repo-2bf56e024dbccc85257a8c308492f2375d690b206153b02d3bea2029707d4c38+packa_tacnzof7rtmb5bwqa7bdnfcdbq/node_modules/next/dist/esm/build/templates/helpers.js [ssr] (ecmascript)'
+           "source": "./app/layout.js
+         Failed to execute SWC plugin
+         An unexpected error occurred when executing an SWC EcmaScript transform plugin.
+         This might be due to a version mismatch between the plugin and Next.js.
+         Failed to execute @swc/plugin-react-remove-properties
          Caused by:
-         - failed to parse [project]/node_modules/.pnpm/next@file+..+next-repo-2bf56e024dbccc85257a8c308492f2375d690b206153b02d3bea2029707d4c38+packa_tacnzof7rtmb5bwqa7bdnfcdbq/node_modules/next/dist/esm/build/templates/helpers.js
-         - Transforming and/or parsing of [project]/node_modules/.pnpm/next@file+..+next-repo-2bf56e024dbccc85257a8c308492f2375d690b206153b02d3bea2029707d4c38+packa_tacnzof7rtmb5bwqa7bdnfcdbq/node_modules/next/dist/esm/build/templates/helpers.js failed
-         - failed to deserialize \`swc_common::plugin::diagnostics::PluginCorePkgDiagnostics\`
-         - Mismatch { name: "array", found: 48 }
-         Debug info:
-         - Execution of <ModuleAssetContext as AssetContext>::process_resolve_result failed
-         - Execution of <ModuleAssetContext as AssetContext>::process failed
-         - Execution of *EcmascriptExports::split_locals_and_reexports failed
-         - Execution of <EcmascriptModuleAsset as EcmascriptChunkPlaceable>::get_exports failed
-         - Execution of analyze_ecmascript_module failed
-         - failed to analyze ecmascript module '[project]/node_modules/.pnpm/next@file+..+next-repo-2bf56e024dbccc85257a8c308492f2375d690b206153b02d3bea2029707d4c38+packa_tacnzof7rtmb5bwqa7bdnfcdbq/node_modules/next/dist/esm/build/templates/helpers.js [ssr] (ecmascript)'
-         - Execution of <EcmascriptModuleAsset as EcmascriptParsable>::failsafe_parse failed
-         - Execution of parse failed
-         - failed to parse [project]/node_modules/.pnpm/next@file+..+next-repo-2bf56e024dbccc85257a8c308492f2375d690b206153b02d3bea2029707d4c38+packa_tacnzof7rtmb5bwqa7bdnfcdbq/node_modules/next/dist/esm/build/templates/helpers.js
-         - Transforming and/or parsing of [project]/node_modules/.pnpm/next@file+..+next-repo-2bf56e024dbccc85257a8c308492f2375d690b206153b02d3bea2029707d4c38+packa_tacnzof7rtmb5bwqa7bdnfcdbq/node_modules/next/dist/esm/build/templates/helpers.js failed
-         - failed to deserialize \`swc_common::plugin::diagnostics::PluginCorePkgDiagnostics\`
-         - Mismatch { name: "array", found: 48 }
-         Import map: aliased to module 'next' with subpath '/dist/esm/build/templates/helpers' inside of [project]/
-         https://nextjs.org/docs/messages/module-not-found",
+             0: failed to deserialize \`swc_common::plugin::diagnostics::PluginCorePkgDiagnostics\`
+             1: Mismatch { name: "array", found: 48 }",
            "stack": [],
          }
         `)
