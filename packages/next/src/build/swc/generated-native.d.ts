863cdb8033b7a3485325f20c7b16316c74d67a41
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-14 09:55:48 +0100

    cleanup

--- packages/next/src/build/swc/generated-native.d.ts
+++ packages/next/src/build/swc/generated-native.d.ts
@@ -34,12 +34,6 @@ export declare class ExternalObject<T> {
     [K: symbol]: T
   }
 }
-export interface TransformOutput {
-  code: string
-  map?: string
-  output?: string
-  diagnostics: Array<string>
-}
 export declare function lockfileTryAcquireSync(
   path: string
 ): { __napiType: 'Lockfile' } | null
@@ -58,6 +52,12 @@ export declare function mdxCompile(
   signal?: AbortSignal | undefined | null
 ): Promise<unknown>
 export declare function mdxCompileSync(value: string, option: Buffer): string
+export interface TransformOutput {
+  code: string
+  map?: string
+  output?: string
+  diagnostics: Array<string>
+}
 export declare function minify(
   input: Buffer,
   opts: Buffer,
