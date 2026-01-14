8d536148735e4ecc4054e2b8b1da4d5e7370c1c0
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-14 17:23:23 +0100

    cleanup tests

--- test/production/escheck-output/index.test.ts
+++ test/production/escheck-output/index.test.ts
@@ -1,10 +1,9 @@
 import { createNext } from 'e2e-utils'
 import { NextInstance } from 'e2e-utils'
 
-// Currently broken: https://github.com/yowainwright/es-check/issues/321
-describe.skip('ES Check .next output', () => {
-  let next: NextInstance
-  afterEach(() => next.destroy())
+describe('escheck-output', () => {
+  let next: NextInstance | undefined
+  afterEach(() => next?.destroy())
 
   it('should downlevel JS according to manual browserslist with es2020', async () => {
     let browserslist = [
@@ -16,9 +15,9 @@ describe.skip('ES Check .next output', () => {
     ]
     next = await createNext({
       files: __dirname,
-      dependencies: { 'es-check': '9.4.3' },
+      dependencies: { 'es-check': '9.5.3' },
       packageJson: {
-        browserslist: browserslist,
+        browserslist,
         scripts: {
           build: 'next build && es-check es2020 ".next/static/**/*.js"',
         },
@@ -26,16 +25,14 @@ describe.skip('ES Check .next output', () => {
       installCommand: 'pnpm i',
       buildCommand: 'pnpm build',
     })
-    expect(next.cliOutput).toContain(
-      'info: ES-Check: there were no ES version matching errors!  🎉'
-    )
+    expect(next.cliOutput).toContain('info: ✓ ES-Check passed!')
   })
 
   it('should downlevel JS according to default browserslist', async () => {
     let browserslist = ['chrome 111', 'edge 111', 'firefox 111', 'safari 16.4']
     next = await createNext({
       files: __dirname,
-      dependencies: { 'es-check': '9.4.3' },
+      dependencies: { 'es-check': '9.5.3' },
       packageJson: {
         scripts: {
           build: `next build && es-check checkBrowser ".next/static/**/*.js" --browserslistQuery="${browserslist.join(', ')}"`,
@@ -44,8 +41,6 @@ describe.skip('ES Check .next output', () => {
       installCommand: 'pnpm i',
       buildCommand: 'pnpm build',
     })
-    expect(next.cliOutput).toContain(
-      'info: ES-Check: there were no ES version matching errors!  🎉'
-    )
+    expect(next.cliOutput).toContain('info: ✓ ES-Check passed!')
   })
 })
