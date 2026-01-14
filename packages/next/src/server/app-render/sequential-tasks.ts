53fac7a4d055612e42ca78f742db19331284463c
Janka Uryga <lolzatu2@gmail.com>
2026-01-14 16:03:53 +0100

    remove listener cleanup in favor of unref

--- packages/next/src/server/app-render/sequential-tasks.ts
+++ packages/next/src/server/app-render/sequential-tasks.ts
@@ -18,16 +18,14 @@ export function cancelTask(handle: TaskHandle): void {
   if (!lazyQueue) {
     return
   }
-  const queue = getOrCreateQueue()
-  queue.cancelTask(handle)
+  lazyQueue.cancelTask(handle)
 }
 
 export function expectNoPendingTasks() {
   if (!lazyQueue) {
     return
   }
-  const queue = getOrCreateQueue()
-  if (queue.tasks.length > 0) {
+  if (lazyQueue.tasks.length > 0) {
     throw new InvariantError(`Expected all tasks to have been executed`)
   }
 }
@@ -45,89 +43,34 @@ function createTaskQueue() {
   const { port1: receivePort, port2: sendPort } = new MessageChannel()
 
   let nextTaskId = 0
-  let state: 'idle' | 'running' = 'idle'
 
   type Task = { id: TaskId; callback: () => void }
   const tasks: Task[] = []
 
-  type Action = 'RUN_NEXT_TASK' | 'TRY_STOP'
+  receivePort.addEventListener('message', function runNextTaskOnMessage() {
+    const task = tasks.shift()
 
-  function onMessage(event: MessageEvent<Action>) {
-    const action = event.data as Action
-    switch (action) {
-      case 'RUN_NEXT_TASK': {
-        const task = tasks.shift()
-
-        // We may receive more 'RUN_NEXT_TASK' messages than we have tasks if a task is cancelled.
-        // In that case, we should already be closed, but do it again just in case.
-        if (!task) {
-          stop()
-          return
-        }
-
-        state = 'running'
-        const { callback } = task
-        try {
-          callback()
-        } catch (err) {
-          // Thrown errors will interrupt the message processing loop.
-          // Rethrow then in a microtask instead
-          queueMicrotask(() => {
-            throw err
-          })
-        } finally {
-          if (tasks.length === 0) {
-            // We have emptied the queue, but we still
-            // need to wait for nextTicks and microtasks to run
-            // in case they schedule more tasks.
-            dispatchMessage('TRY_STOP')
-          }
-        }
-        return
-      }
-      case 'TRY_STOP': {
-        if (tasks.length === 0) {
-          // We got a 'TRY_STOP' message, and no other tasks are scheduled.
-          // We have no more work to do for now.
-          stop()
-        } else {
-          // we scheduled a stop, but new tasks were queued before we got here.
-          // let the loop execute the queued tasks.
-        }
-        return
-      }
-      default: {
-        action satisfies never
-      }
+    // We may receive more messages than we have tasks if a task is cancelled.
+    // In that case, we should already be closed, but do it again just in case.
+    if (!task) {
+      return
     }
-  }
 
-  function dispatchMessage(action: Action) {
-    startListeningForMessages()
-    sendPort.postMessage(action)
-  }
-
-  let isListening = false
-  function startListeningForMessages() {
-    if (!isListening) {
-      receivePort.addEventListener('message', onMessage)
-      isListening = true
+    const { callback } = task
+    try {
+      callback()
+    } catch (err) {
+      // Thrown errors will interrupt the message processing loop.
+      // Rethrow then in a microtask instead
+      queueMicrotask(() => {
+        throw err
+      })
     }
-  }
+  })
 
-  function stopListeningForMessages() {
-    if (isListening) {
-      receivePort.removeEventListener('message', onMessage)
-      isListening = false
-    }
-  }
-
-  function stop() {
-    state = 'idle'
-    // We need to stop listening for messages after running queued tasks.
-    // otherwise, the message channel will prevent the process from exiting.
-    stopListeningForMessages()
-  }
+  // In node, we need to `unref()` the MessagePort. Otherwise,
+  // a port with listeners can prevent the process from shutting down.
+  tryUnref(receivePort)
 
   return {
     tasks,
@@ -140,8 +83,8 @@ function createTaskQueue() {
       })
 
       // Each message is processed in a separate task.
-      // We'll pick the callback up from the queue and run it.
-      dispatchMessage('RUN_NEXT_TASK')
+      // When the message is received, we'll pick the callback up from the queue and run it.
+      sendPort.postMessage(null)
 
       return id as TaskHandle
     },
@@ -156,13 +99,14 @@ function createTaskQueue() {
       if (index !== -1) {
         tasks.splice(index, 1)
       }
-      // Note that we have no way of cancelling the 'RUN_NEXT_TASK' message we queued,
-      // so the loop needs to handle receiving more messages than it has tasks.
-
-      if (tasks.length === 0 && state === 'idle') {
-        // if this was the last task, and we aren't executing, nothing else will clean us up.
-        stop()
-      }
+      // Note that we have no way of cancelling the message we queued,
+      // so the loop needs to handle receiving more messages than it has queued tasks.
     },
+  }
+}
+
+function tryUnref(port: MessagePort | import('worker_threads').MessagePort) {
+  if ('unref' in port && typeof port.unref === 'function') {
+    port.unref()
   }
 }
