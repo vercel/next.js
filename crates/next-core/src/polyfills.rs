275b64d21647ab1906a496498c7d46bd606b52a0
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-14 18:09:12 +0100

    polyfill-nomodule

--- crates/next-core/src/polyfills.rs
+++ crates/next-core/src/polyfills.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
-use swc_core::ecma::preset_env::Version;
-use turbo_rcstr::{RcStr, rcstr};
+use swc_core::ecma::preset_env::{Version, Versions};
+use turbo_rcstr::rcstr;
 use turbo_tasks::{ResolvedVc, Vc};
 use turbo_tasks_fs::{File, FileContent, FileSystem, FileSystemPath, VirtualFileSystem};
 use turbopack_core::{
@@ -17,20 +17,36 @@ use crate::next_import_map::get_next_package;
 
 #[derive(Debug, Clone, Copy)]
 pub struct PolyfillDefinition {
-    /// Identifier for the polyfill
-    pub name: &'static str,
-    /// Path to the polyfill file relative to next/dist/build/polyfills/module/
     pub filename: &'static str,
-    /// Minimum Chrome version that natively supports this feature (None = never)
-    pub chrome_min: Option<Version>,
-    /// Minimum Firefox version
-    pub firefox_min: Option<Version>,
-    /// Minimum Safari version
-    pub safari_min: Option<Version>,
-    /// Minimum Edge version (Chromium-based, None = not in legacy Edge)
-    pub edge_min: Option<Version>,
-    /// Minimum Opera version
-    pub opera_min: Option<Version>,
+    pub chrome: Option<Version>,
+    pub firefox: Option<Version>,
+    pub safari: Option<Version>,
+    pub edge: Option<Version>,
+    pub opera: Option<Version>,
+}
+
+impl PolyfillDefinition {
+    fn is_needed(&self, target: &Versions) -> bool {
+        fn polyfill_needed_for_browser(
+            polyfill_min: Option<Version>,
+            target_version: Option<Version>,
+        ) -> bool {
+            match (polyfill_min, target_version) {
+                // No target for this browser - don't need polyfill for it
+                (_, None) => false,
+                // Browser never supports this feature
+                (None, Some(_)) => true,
+                // Check if target version is below the minimum required
+                (Some(min), Some(target)) => target < min,
+            }
+        }
+
+        polyfill_needed_for_browser(self.chrome, target.chrome)
+            || polyfill_needed_for_browser(self.firefox, target.firefox)
+            || polyfill_needed_for_browser(self.safari, target.safari)
+            || polyfill_needed_for_browser(self.edge, target.edge)
+            || polyfill_needed_for_browser(self.opera, target.opera)
+    }
 }
 
 const fn v(major: u32, minor: u32) -> Version {
@@ -41,143 +57,110 @@ const fn v(major: u32, minor: u32) -> Version {
     }
 }
 
-/// All available module polyfills with their browser support requirements.
-///
-/// These polyfills target ES Modules supporting browsers:
-/// Edge 16+, Firefox 60+, Chrome 61+, Safari 10.1+
-pub static MODULE_POLYFILLS: &[PolyfillDefinition] = &[
+// https://caniuse.com/es6-module
+static NOMODULE_POLYFILL: PolyfillDefinition = PolyfillDefinition {
+    filename: "polyfill-nomodule.js",
+    edge: Some(v(16, 0)),
+    firefox: Some(v(60, 0)),
+    chrome: Some(v(61, 0)),
+    safari: Some(v(10, 1)),
+    opera: Some(v(48, 0)),
+};
+
+static MODULE_POLYFILLS: &[PolyfillDefinition] = &[
     // String.prototype.trimStart/trimEnd
     // https://caniuse.com/mdn-javascript_builtins_string_trimstart
     PolyfillDefinition {
-        name: "string-trim",
-        filename: "string-trim.js",
-        chrome_min: Some(v(66, 0)),
-        firefox_min: Some(v(61, 0)),
-        safari_min: Some(v(12, 0)),
-        edge_min: Some(v(79, 0)),
-        opera_min: Some(v(53, 0)),
+        filename: "module/string-trim.js",
+        chrome: Some(v(66, 0)),
+        firefox: Some(v(61, 0)),
+        safari: Some(v(12, 0)),
+        edge: Some(v(79, 0)),
+        opera: Some(v(53, 0)),
     },
     // Symbol.prototype.description
     // https://caniuse.com/mdn-javascript_builtins_symbol_description
     PolyfillDefinition {
-        name: "symbol-description",
-        filename: "symbol-description.js",
-        chrome_min: Some(v(70, 0)),
-        firefox_min: Some(v(63, 0)),
-        safari_min: Some(v(12, 1)),
-        edge_min: Some(v(79, 0)),
-        opera_min: Some(v(57, 0)),
+        filename: "module/symbol-description.js",
+        chrome: Some(v(70, 0)),
+        firefox: Some(v(63, 0)),
+        safari: Some(v(12, 1)),
+        edge: Some(v(79, 0)),
+        opera: Some(v(57, 0)),
     },
     // Array.prototype.flat/flatMap
     // https://caniuse.com/array-flat
     PolyfillDefinition {
-        name: "array-flat",
-        filename: "array-flat.js",
-        chrome_min: Some(v(69, 0)),
-        firefox_min: Some(v(62, 0)),
-        safari_min: Some(v(12, 0)),
-        edge_min: Some(v(79, 0)),
-        opera_min: Some(v(56, 0)),
+        filename: "module/array-flat.js",
+        chrome: Some(v(69, 0)),
+        firefox: Some(v(62, 0)),
+        safari: Some(v(12, 0)),
+        edge: Some(v(79, 0)),
+        opera: Some(v(56, 0)),
     },
     // Promise.prototype.finally
     // https://caniuse.com/promise-finally
     PolyfillDefinition {
-        name: "promise-finally",
-        filename: "promise-finally.js",
-        chrome_min: Some(v(63, 0)),
-        firefox_min: Some(v(58, 0)),
-        safari_min: Some(v(11, 1)),
-        edge_min: Some(v(18, 0)),
-        opera_min: Some(v(50, 0)),
+        filename: "module/promise-finally.js",
+        chrome: Some(v(63, 0)),
+        firefox: Some(v(58, 0)),
+        safari: Some(v(11, 1)),
+        edge: Some(v(18, 0)),
+        opera: Some(v(50, 0)),
     },
     // Object.fromEntries
     // https://caniuse.com/mdn-javascript_builtins_object_fromentries
     PolyfillDefinition {
-        name: "object-fromentries",
-        filename: "object-fromentries.js",
-        chrome_min: Some(v(73, 0)),
-        firefox_min: Some(v(63, 0)),
-        safari_min: Some(v(12, 1)),
-        edge_min: Some(v(79, 0)),
-        opera_min: Some(v(60, 0)),
+        filename: "module/object-fromentries.js",
+        chrome: Some(v(73, 0)),
+        firefox: Some(v(63, 0)),
+        safari: Some(v(12, 1)),
+        edge: Some(v(79, 0)),
+        opera: Some(v(60, 0)),
     },
     // Array.prototype.at
     // https://caniuse.com/mdn-javascript_builtins_array_at
     PolyfillDefinition {
-        name: "array-at",
-        filename: "array-at.js",
-        chrome_min: Some(v(92, 0)),
-        firefox_min: Some(v(90, 0)),
-        safari_min: Some(v(15, 4)),
-        edge_min: Some(v(92, 0)),
-        opera_min: Some(v(78, 0)),
+        filename: "module/array-at.js",
+        chrome: Some(v(92, 0)),
+        firefox: Some(v(90, 0)),
+        safari: Some(v(15, 4)),
+        edge: Some(v(92, 0)),
+        opera: Some(v(78, 0)),
     },
     // Object.hasOwn
     // https://caniuse.com/mdn-javascript_builtins_object_hasown
     PolyfillDefinition {
-        name: "object-hasown",
-        filename: "object-hasown.js",
-        chrome_min: Some(v(93, 0)),
-        firefox_min: Some(v(92, 0)),
-        safari_min: Some(v(15, 4)),
-        edge_min: Some(v(93, 0)),
-        opera_min: Some(v(79, 0)),
+        filename: "module/object-hasown.js",
+        chrome: Some(v(93, 0)),
+        firefox: Some(v(92, 0)),
+        safari: Some(v(15, 4)),
+        edge: Some(v(93, 0)),
+        opera: Some(v(79, 0)),
     },
     // URL.canParse
     // https://caniuse.com/mdn-api_url_canparse_static
     PolyfillDefinition {
-        name: "url-canparse",
-        filename: "url-canparse.js",
-        chrome_min: Some(v(120, 0)),
-        firefox_min: Some(v(115, 0)),
-        safari_min: Some(v(17, 0)),
-        edge_min: Some(v(120, 0)),
-        opera_min: Some(v(106, 0)),
+        filename: "module/url-canparse.js",
+        chrome: Some(v(120, 0)),
+        firefox: Some(v(115, 0)),
+        safari: Some(v(17, 0)),
+        edge: Some(v(120, 0)),
+        opera: Some(v(106, 0)),
     },
 ];
 
-/// Check if a polyfill is needed for a specific browser and target version.
-fn polyfill_needed_for_browser(
-    polyfill_min: Option<Version>,
-    target_version: Option<Version>,
-) -> bool {
-    match (polyfill_min, target_version) {
-        // No target for this browser - don't need polyfill for it
-        (_, None) => false,
-        // Browser never supports this feature
-        (None, Some(_)) => true,
-        // Check if target version is below the minimum required
-        (Some(min), Some(target)) => target < min,
+pub fn get_required_polyfills(versions: &Versions) -> Vec<&'static str> {
+    if NOMODULE_POLYFILL.is_needed(versions) {
+        // NOMODULE_POLYFILL includes everything in MODULE_POLYFILLS
+        vec![NOMODULE_POLYFILL.filename]
+    } else {
+        MODULE_POLYFILLS
+            .iter()
+            .filter(|polyfill| polyfill.is_needed(versions))
+            .map(|d| d.filename)
+            .collect()
     }
-}
-
-/// Determines which polyfills are needed for the given runtime versions.
-pub fn get_required_polyfills(
-    versions: &swc_core::ecma::preset_env::Versions,
-) -> Vec<&'static PolyfillDefinition> {
-    MODULE_POLYFILLS
-        .iter()
-        .filter(|polyfill| {
-            // Need polyfill if ANY target browser needs it
-            polyfill_needed_for_browser(polyfill.chrome_min, versions.chrome)
-                || polyfill_needed_for_browser(polyfill.firefox_min, versions.firefox)
-                || polyfill_needed_for_browser(polyfill.safari_min, versions.safari)
-                || polyfill_needed_for_browser(polyfill.edge_min, versions.edge)
-                || polyfill_needed_for_browser(polyfill.opera_min, versions.opera)
-        })
-        .collect()
-}
-
-/// Returns the list of required polyfill filenames for the given runtime versions.
-#[turbo_tasks::function]
-pub async fn get_required_polyfill_paths(
-    runtime_versions: Vc<RuntimeVersions>,
-) -> Result<Vc<Vec<RcStr>>> {
-    let versions = runtime_versions.await?;
-    let required = get_required_polyfills(&versions);
-    let paths: Vec<RcStr> = required.iter().map(|p| RcStr::from(p.filename)).collect();
-
-    Ok(Vc::cell(paths))
 }
 
 #[turbo_tasks::value]
@@ -210,10 +193,7 @@ impl PolyfillsAsset {
 
         let mut builder = CodeBuilder::default();
         for polyfill in required {
-            let path = next_package.join(&format!(
-                "dist/build/polyfills/module/{}",
-                polyfill.filename
-            ))?;
+            let path = next_package.join(&format!("dist/build/polyfills/{}", polyfill))?;
             if let FileContent::Content(file) = &*path.read().await? {
                 let content = file.content();
                 let source_map = serde_json::json!({
@@ -319,29 +299,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_polyfill_needed_for_browser() {
-        // Target below minimum - needs polyfill
-        assert!(polyfill_needed_for_browser(
-            Some(v(120, 0)),
-            Some(v(119, 0))
-        ));
-        // Target at minimum - doesn't need polyfill
-        assert!(!polyfill_needed_for_browser(
-            Some(v(120, 0)),
-            Some(v(120, 0))
-        ));
-        // Target above minimum - doesn't need polyfill
-        assert!(!polyfill_needed_for_browser(
-            Some(v(120, 0)),
-            Some(v(121, 0))
-        ));
-        // No target - doesn't need polyfill
-        assert!(!polyfill_needed_for_browser(Some(v(120, 0)), None));
-        // Browser never supports - needs polyfill
-        assert!(polyfill_needed_for_browser(None, Some(v(100, 0))));
-    }
-
-    #[test]
     fn test_modern_browsers_need_few_polyfills() {
         // Chrome 120+ should not need any of these polyfills
         let versions = Versions {
@@ -349,25 +306,55 @@ mod tests {
             ..Default::default()
         };
         let required = get_required_polyfills(&versions);
-        assert!(
-            required.is_empty(),
-            "Expected no polyfills for Chrome 120+, got: {:?}",
-            required.iter().map(|p| p.name).collect::<Vec<_>>()
+        assert_eq!(required, Vec::<&str>::new());
+    }
+
+    #[test]
+    fn test_old_browsers_need_more_polyfills() {
+        let versions = Versions {
+            chrome: Some(v(80, 0)),
+            ..Default::default()
+        };
+        let required = get_required_polyfills(&versions);
+        assert_eq!(
+            required,
+            vec![
+                "module/array-at.js",
+                "module/object-hasown.js",
+                "module/url-canparse.js"
+            ]
         );
     }
 
     #[test]
-    fn test_older_browsers_need_more_polyfills() {
-        // Chrome 61 should need most polyfills
+    fn test_oldest_browsers_need_all_polyfills() {
         let versions = Versions {
             chrome: Some(v(61, 0)),
             ..Default::default()
         };
         let required = get_required_polyfills(&versions);
-        assert!(
-            required.len() >= 6,
-            "Expected at least 6 polyfills for Chrome 61, got: {:?}",
-            required.iter().map(|p| p.name).collect::<Vec<_>>()
+        assert_eq!(
+            required,
+            vec![
+                "module/string-trim.js",
+                "module/symbol-description.js",
+                "module/array-flat.js",
+                "module/promise-finally.js",
+                "module/object-fromentries.js",
+                "module/array-at.js",
+                "module/object-hasown.js",
+                "module/url-canparse.js"
+            ]
         );
+    }
+
+    #[test]
+    fn test_ancient_browsers_need_nomodule_polyfills() {
+        let versions = Versions {
+            chrome: Some(v(50, 0)),
+            ..Default::default()
+        };
+        let required = get_required_polyfills(&versions);
+        assert_eq!(required, vec!["polyfill-nomodule.js"]);
     }
 }
