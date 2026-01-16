1c98b1db51b14dbec1a57a3587980eb92c5c2987
Tobias Koppers <tobias.koppers@googlemail.com>
2026-01-16 15:27:05 +0100

    add test case for resource queries

--- test/e2e/app-dir/webpack-loader-resource-query/app/page.tsx
+++ test/e2e/app-dir/webpack-loader-resource-query/app/page.tsx
@@ -1,7 +1,14 @@
 // @ts-expect-error -- ignore
 import { v } from './test.mdx?test=hi'
+// @ts-expect-error -- ignore
+import reversed from './data.txt?reverse'
 
 export default function Page() {
   console.log(v)
-  return <p>hello world</p>
+  return (
+    <div>
+      <p>hello world</p>
+      <p id="reversed">{reversed}</p>
+    </div>
+  )
 }
--- test/e2e/app-dir/webpack-loader-resource-query/next.config.js
+++ test/e2e/app-dir/webpack-loader-resource-query/next.config.js
@@ -6,12 +6,23 @@ const nextConfig = {
         loaders: [require.resolve('./test-file-loader.js')],
         as: '*.js',
       },
+      '*.txt': [
+        {
+          condition: { query: '?reverse' },
+          loaders: [require.resolve('./reverse-loader.js')],
+          as: '*.js',
+        },
+      ],
     },
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.mdx/,
       use: require.resolve('./test-file-loader.js'),
+    })
+    config.module.rules.push({
+      resourceQuery: '?reverse',
+      use: require.resolve('./reverse-loader.js'),
     })
     return config
   },
