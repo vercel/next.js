2174ae5d1705fd42495f7c4d6264b728e40e0eec
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-19 19:42:59 +0100

    use the value

--- packages/next/src/shared/lib/deployment-id.ts
+++ packages/next/src/shared/lib/deployment-id.ts
@@ -1,10 +1,11 @@
 // This could also be a variable instead of a function, but some unit tests want to change the ID at
 // runtime. Even though that would never happen in a real deployment.
 export function getDeploymentId(): string | undefined {
-  return (
-    process.env.NEXT_DEPLOYMENT_ID ??
-    process.env.NEXT_DEPLOYMENT_ID_COMPILE_TIME
-  )
+  if (typeof document !== 'undefined') {
+    return document.documentElement.dataset.dplId
+  }
+  // build/define-env.ts might replace this with "false"
+  return process.env.NEXT_DEPLOYMENT_ID || undefined
 }
 
 export function getDeploymentIdQueryOrEmptyString(): string {
