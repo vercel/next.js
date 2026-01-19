5f75d22b804d444d6c27240f5f7af32aee4a8f92
JJ Kasper <jj@jjsweb.site>
2026-01-20 08:23:43 +0900

    backport: Only filter next config if experimental flag is enabled (#88733) (#88772)
    Backports https://github.com/vercel/next.js/pull/88733
    
    Co-authored-by: Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1852,11 +1852,15 @@ export default async function build(
             }
           }
 
+          // getNextConfigRuntime only filters if experimental.runtimeServerDeploymentId is true,
+          // but we unconditionally want to remove configFile for this manifest
+          let runtimeConfigWithoutFilePath = { ...runtimeConfig }
+          delete (runtimeConfigWithoutFilePath as NextConfigComplete).configFile
+
           const serverFilesManifest: RequiredServerFilesManifest = {
             version: 1,
             config: {
-              ...runtimeConfig,
-
+              ...runtimeConfigWithoutFilePath,
               ...(ciEnvironment.hasNextSupport
                 ? {
                     compress: false,
