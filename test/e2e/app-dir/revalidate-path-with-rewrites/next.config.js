487f5b74ecbcff2d1b8eaef6adf890c664807bd5
Hendrik Liebau <mail@hendrik-liebau.de>
2026-01-19 16:09:41 +0100

    Don't read `__NEXT_CACHE_COMPONENTS` in module scope of `next.config.js`

--- test/e2e/app-dir/revalidate-path-with-rewrites/next.config.js
+++ test/e2e/app-dir/revalidate-path-with-rewrites/next.config.js
@@ -1,21 +1,24 @@
-const isCacheComponentsEnabled = process.env.__NEXT_CACHE_COMPONENTS === 'true'
-
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  rewrites: async () => [
-    {
-      source: '/static',
-      destination: isCacheComponentsEnabled
-        ? '/cache-components/static'
-        : '/legacy/static',
-    },
-    {
-      source: '/dynamic',
-      destination: isCacheComponentsEnabled
-        ? '/cache-components/dynamic'
-        : '/legacy/dynamic',
-    },
-  ],
+  rewrites: async () => {
+    const isCacheComponentsEnabled =
+      process.env.__NEXT_CACHE_COMPONENTS === 'true'
+
+    return [
+      {
+        source: '/static',
+        destination: isCacheComponentsEnabled
+          ? '/cache-components/static'
+          : '/legacy/static',
+      },
+      {
+        source: '/dynamic',
+        destination: isCacheComponentsEnabled
+          ? '/cache-components/dynamic'
+          : '/legacy/dynamic',
+      },
+    ]
+  },
 }
