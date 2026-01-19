50757fd1e7b30b13fa570e44e1bd06d5b24b21e6
Hendrik Liebau <mail@hendrik-liebau.de>
2026-01-19 16:10:57 +0100

    Don't read `__NEXT_CACHE_COMPONENTS` in module scope of `next.config.js`

--- test/e2e/app-dir/trailingslash/next.config.js
+++ test/e2e/app-dir/trailingslash/next.config.js
@@ -1,16 +1,19 @@
-const isCacheComponentsEnabled = process.env.__NEXT_CACHE_COMPONENTS === 'true'
-
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = {
   trailingSlash: true,
-  rewrites: async () => [
-    {
-      source: '/:lang(en|es)/',
-      destination: isCacheComponentsEnabled
-        ? '/:lang/cache-components/'
-        : '/:lang/legacy/',
-    },
-  ],
+  rewrites: async () => {
+    const isCacheComponentsEnabled =
+      process.env.__NEXT_CACHE_COMPONENTS === 'true'
+
+    return [
+      {
+        source: '/:lang(en|es)/',
+        destination: isCacheComponentsEnabled
+          ? '/:lang/cache-components/'
+          : '/:lang/legacy/',
+      },
+    ]
+  },
 }
