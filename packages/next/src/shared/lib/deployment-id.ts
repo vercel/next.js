3dae3068ef99a74301ab09ae2d1b9cb3b4d42c41
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-20 12:51:57 +0100

    prevent hydration error

--- packages/next/src/shared/lib/deployment-id.ts
+++ packages/next/src/shared/lib/deployment-id.ts
@@ -1,15 +1,20 @@
-// This could also be a variable instead of a function, but some unit tests want to change the ID at
-// runtime. Even though that would never happen in a real deployment.
-export function getDeploymentId(): string | undefined {
-  if (typeof document !== 'undefined') {
-    return document.documentElement.dataset.dplId
-  }
+let deploymentId: string | undefined
+
+if (typeof window !== 'undefined') {
+  deploymentId = document.documentElement.dataset.dplId
+  // Immediately remove the attribute to prevent hydration errors (the dplId was inserted into the
+  // HTML only), React isn't aware of it at all.
+  delete document.documentElement.dataset.dplId
+} else {
   // build/define-env.ts might replace this with "false"
-  return process.env.NEXT_DEPLOYMENT_ID || undefined
+  deploymentId = process.env.NEXT_DEPLOYMENT_ID || undefined
+}
+
+export function getDeploymentId(): string | undefined {
+  return deploymentId
 }
 
 export function getDeploymentIdQueryOrEmptyString(): string {
-  let deploymentId = getDeploymentId()
   if (deploymentId) {
     return `?dpl=${deploymentId}`
   }
