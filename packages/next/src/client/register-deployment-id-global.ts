8ad15b926b1d4f3d3d31df158d14a1569b4c13b1
Tobias Koppers <tobias.koppers@googlemail.com>
2026-01-23 08:42:24 +0100

    set suffix for prerendering

--- packages/next/src/client/register-deployment-id-global.ts
+++ packages/next/src/client/register-deployment-id-global.ts
@@ -2,6 +2,3 @@ import { getDeploymentId } from '../shared/lib/deployment-id'
 
 const deploymentId = getDeploymentId()
 ;(globalThis as any).NEXT_DEPLOYMENT_ID = deploymentId
-;(globalThis as any).NEXT_CLIENT_ASSET_SUFFIX = deploymentId
-  ? `?dpl=${deploymentId}`
-  : ''
