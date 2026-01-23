ba961b517e416c2b09116240896a9a7f73d7adf4
Brooke Mosby <25040341+brookemosby@users.noreply.github.com>
2026-01-23 12:05:12 -0700

    updating

--- test/e2e/app-dir/deployment-id-edge-cases/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react'
-export default function Root({ children }: { children: ReactNode }) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  )
-}
--- test/e2e/app-dir/deployment-id-edge-cases/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <p>Deployment ID: {process.env.NEXT_DEPLOYMENT_ID}</p>
-}
