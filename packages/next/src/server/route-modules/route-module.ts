841b8d1259570e34ff89036828716fbe4b53c33b
JJ Kasper <jj@jjsweb.site>
2026-01-24 18:56:52 -0800

    fix query prioritizing case

--- packages/next/src/server/route-modules/route-module.ts
+++ packages/next/src/server/route-modules/route-module.ts
@@ -838,18 +838,15 @@ export abstract class RouteModule<
 
       if (
         // if both query and params are valid but one
-        // provided more information rely on that one
+        // provided more information and the query params
+        // were nxtP prefixed rely on that one
         query &&
         params &&
         paramsResult.hasValidParams &&
         queryResult.hasValidParams &&
-        (Object.keys(paramsResult.params).length <
-          Object.keys(queryResult.params).length ||
-          // in minimal mode favor query values if equal length
-          // since it can have more information from routing
-          (getRequestMeta(req, 'minimalMode') &&
-            Object.keys(paramsResult.params).length <=
-              Object.keys(queryResult.params).length))
+        routeParamKeys.size > 0 &&
+        Object.keys(paramsResult.params).length <=
+          Object.keys(queryResult.params).length
       ) {
         paramsToInterpolate = queryResult.params
         params = Object.assign(queryResult.params)
