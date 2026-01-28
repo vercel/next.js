41ff829788f96d82759e05ea90616672ab70d8dc
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-28 09:34:21 +0100

    fixup merge

--- turbopack/crates/turbopack-core/src/node_addon_module.rs
+++ turbopack/crates/turbopack-core/src/node_addon_module.rs
@@ -121,7 +121,12 @@ async fn dir_references(package_dir: FileSystemPath) -> Result<Vc<ModuleReferenc
             PatternMatch::File(_, file) => {
                 let realpath = file.realpath_with_links().await?;
                 results.extend(realpath.symlinks.iter().cloned());
-                results.insert(realpath.path.clone());
+                match &realpath.path_or_error {
+                    Ok(path) => {
+                        results.insert(path.clone());
+                    }
+                    Err(e) => anyhow::bail!(e.as_error_message(file, &realpath)),
+                }
             }
             PatternMatch::Directory(..) => {}
         }
