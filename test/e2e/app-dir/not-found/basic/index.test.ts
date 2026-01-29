7639972d25e6446ff741aee959c5dd45e8624236
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-29 22:34:00 +0100

    Keep pages/404.js to be able to runtime-render it anyway

--- test/e2e/app-dir/not-found/basic/index.test.ts
+++ test/e2e/app-dir/not-found/basic/index.test.ts
@@ -145,6 +145,7 @@ describe('app dir - not-found - basic', () => {
         expect(
           await next.readFile('.next/server/pages-manifest.json')
         ).toContain('"pages/404.html"')
+        expect(await next.readFile('.next/server/pages/404.js')).toBeTruthy()
       })
     }
   }
