a75d6f5fa095050a93c94f2356f28245bd446371
Jiachi Liu <inbox@huozhi.im>
2026-01-29 17:30:09 +0100

    ts type

--- packages/next/src/build/swc/generated-native.d.ts
+++ packages/next/src/build/swc/generated-native.d.ts
@@ -35,11 +35,15 @@ export declare class ExternalObject<T> {
   }
 }
 export declare function lockfileTryAcquireSync(
-  path: string
+  path: string,
+  content?: string | undefined | null
 ): { __napiType: 'Lockfile' } | null
 export declare function lockfileTryAcquire(
-  path: string
+  path: string,
+  content?: string | undefined | null
 ): Promise<{ __napiType: 'Lockfile' } | null>
+export declare function lockfileReadSync(path: string): string | null
+export declare function lockfileRead(path: string): Promise<string | null>
 export declare function lockfileUnlockSync(lockfile: {
   __napiType: 'Lockfile'
 }): void
