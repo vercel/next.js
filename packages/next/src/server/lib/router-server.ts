4c129feb4c7aeface2b5625340bc565fdad499a0
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2026-01-30 16:50:24 -0800

    Revert "Fix/zlib mem node" (#89322)
    Reverts vercel/next.js#89099
    
    Apparently this did not solve the user's memory leak issue:
    https://github.com/vercel/next.js/issues/89091#issuecomment-3826050800

--- packages/next/src/server/lib/router-server.ts
+++ packages/next/src/server/lib/router-server.ts
@@ -262,12 +262,6 @@ export async function initialize(opts: {
     if (compress) {
       // @ts-expect-error not express req/res
       compress(req, res, () => {})
-
-      // Only on client abort: destroy response so compression streams are cleaned up
-      req.once('aborted', () => {
-        if (res.destroyed || res.writableFinished) return
-        res.destroy()
-      })
     }
     req.on('error', (_err) => {
       // TODO: log socket errors?
