6a59cee118806020ebd99237ed1ec88207568154
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-05 18:01:14 +0100

    improve

--- test/e2e/app-dir/not-found-with-pages-i18n/not-found-with-pages.test.ts
+++ test/e2e/app-dir/not-found-with-pages-i18n/not-found-with-pages.test.ts
@@ -17,35 +17,27 @@ describe('not-found-with-pages-i18n', () => {
     })
   }
 
-  it('should prefer the app router 404 over the pages router 404 when both are present - app', async () => {
-    const browser = await next.browser('/app-dir/foo')
-    expect(await browser.elementByCss('h1').text()).toBe(
-      'APP ROUTER - 404 PAGE'
-    )
+  it.each(['/app-dir/foo', '/foo'])(
+    'should prefer the app router 404 over the pages router 404 when both are present - browser %s',
+    async (s) => {
+      const browser = await next.browser(s)
+      expect(await browser.elementByCss('h1').text()).toBe(
+        'APP ROUTER - 404 PAGE'
+      )
 
-    await browser.loadPage(next.url)
-    expect(await browser.elementByCss('h1').text()).toBe(
-      'APP ROUTER - 404 PAGE'
-    )
+      await browser.loadPage(next.url)
+      expect(await browser.elementByCss('h1').text()).toBe(
+        'APP ROUTER - 404 PAGE'
+      )
+    }
+  )
 
-    const ssr = await next.fetch('/app-dir/foo')
-    expect(ssr.status).toEqual(404)
-    expect(await ssr.text()).toContain('APP ROUTER - 404 PAGE')
-  })
-
-  it('should prefer the app router 404 over the pages router 404 when both are present - pages', async () => {
-    const browser = await next.browser('/foo')
-    expect(await browser.elementByCss('h1').text()).toBe(
-      'APP ROUTER - 404 PAGE'
-    )
-
-    await browser.loadPage(next.url)
-    expect(await browser.elementByCss('h1').text()).toBe(
-      'APP ROUTER - 404 PAGE'
-    )
-
-    const ssr = await next.fetch('/foo')
-    expect(ssr.status).toEqual(404)
-    expect(await ssr.text()).toContain('APP ROUTER - 404 PAGE')
-  })
+  it.each(['/app-dir/foo', '/foo'])(
+    'should prefer the app router 404 over the pages router 404 when both are present - SSR %s',
+    async (s) => {
+      const ssr = await next.fetch(s)
+      expect(ssr.status).toEqual(404)
+      expect(await ssr.text()).toContain('APP ROUTER - 404 PAGE')
+    }
+  )
 })
