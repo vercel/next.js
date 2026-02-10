7b51a8de3885c9bd4208aba081f97b984a61117d
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-10 15:00:17 +0100

    Turbopack: fix CSS dependency locations

--- turbopack/crates/turbopack-css/src/references/mod.rs
+++ turbopack/crates/turbopack-css/src/references/mod.rs
@@ -86,6 +86,22 @@ impl<'a> ModuleReferencesVisitor<'a> {
     }
 }
 
+fn convert_loc(source: ResolvedVc<Box<dyn Source>>, line: u32, column: u32) -> IssueSource {
+    // Convert from 1-based to 0-based indexing.
+    IssueSource::from_line_col(
+        source,
+        SourcePos {
+            line: line - 1,
+            column: column - 1,
+        },
+        SourcePos {
+            line: line - 1,
+            // The IssueSource end column is exclusive, so -1+1=0
+            column,
+        },
+    )
+}
+
 impl Visitor<'_> for ModuleReferencesVisitor<'_> {
     type Error = Infallible;
 
@@ -105,17 +121,7 @@ impl Visitor<'_> for ModuleReferencesVisitor<'_> {
                     Request::parse(RcStr::from(src).into()),
                     ImportAttributes::new_from_lightningcss(&i.clone().into_owned()).cell(),
                     self.import_context.map(|ctx| *ctx),
-                    IssueSource::from_line_col(
-                        self.source,
-                        SourcePos {
-                            line: issue_span.line as _,
-                            column: issue_span.column as _,
-                        },
-                        SourcePos {
-                            line: issue_span.line as _,
-                            column: issue_span.column as _,
-                        },
-                    ),
+                    convert_loc(self.source, issue_span.line, issue_span.column),
                 )));
 
                 *rule = CssRule::Ignored;
@@ -140,17 +146,7 @@ impl Visitor<'_> for ModuleReferencesVisitor<'_> {
             let vc = UrlAssetReference::new(
                 *self.origin,
                 Request::parse(RcStr::from(src).into()),
-                IssueSource::from_line_col(
-                    self.source,
-                    SourcePos {
-                        line: issue_span.line as _,
-                        column: issue_span.column as _,
-                    },
-                    SourcePos {
-                        line: issue_span.line as _,
-                        column: issue_span.column as _,
-                    },
-                ),
+                convert_loc(self.source, issue_span.line, issue_span.column),
             );
 
             self.references.push(Vc::upcast(vc));
