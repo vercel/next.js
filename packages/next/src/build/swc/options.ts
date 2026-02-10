d65781ac0cd21785f0638c3d4fbc30176e594613
Rishi <rishi.shanbhag@somaiya.edu>
2026-02-10 22:59:24 +0530

    Update packages/next/src/build/swc/options.ts
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/next/src/build/swc/options.ts
+++ packages/next/src/build/swc/options.ts
@@ -356,6 +356,7 @@ export function getJestSWCOptions({
   if (imageConfig) {
     baseOptions.jsc.transform.optimizer.globals = {
       envs: {
+        ...baseOptions.jsc.transform.optimizer.globals?.env,
         __NEXT_IMAGE_OPTS: JSON.stringify(imageConfig),
       },
     } as any
