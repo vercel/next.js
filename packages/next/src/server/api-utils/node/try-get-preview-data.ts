637ec3eafe338106ce5be59c7ace5a806e51fe6e
Dustaboy3 <44070950+Dustaboy3@users.noreply.github.com>
2026-02-19 15:29:28 +0100

    Apply suggestion from @vercel[bot]
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- packages/next/src/server/api-utils/node/try-get-preview-data.ts
+++ packages/next/src/server/api-utils/node/try-get-preview-data.ts
@@ -88,7 +88,7 @@ export function tryGetPreviewData(
       tokenPreviewData,
       options.previewModeSigningKey
     ) as typeof encryptedPreviewData
-  } catch () {
+  } catch {
     // Behind DEBUG to avoid noisy logs on prod
     // Do not include cookie/token content
     debug('Failed to verify preview mode cookies; clearing preview data')
