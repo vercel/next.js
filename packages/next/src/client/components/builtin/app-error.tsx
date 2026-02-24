b5f39723bdae5b2cf2b5310dde64f0c4e00c575d
mitul-s <mitulxshah@gmail.com>
2026-02-24 17:19:35 -0500

    new design

--- packages/next/src/client/components/builtin/app-error.tsx
+++ packages/next/src/client/components/builtin/app-error.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { errorStyles, errorThemeCss, ErrorIcon } from './error-styles'
+import { errorStyles, errorThemeCss, WarningIcon } from './error-styles'
 
 // This is the static 500.html page for App Router apps.
 // Always a server error, rendered at build time.
@@ -7,27 +7,39 @@ function AppError() {
   return (
     <html id="__next_error__">
       <head>
-        <title>500: This page failed to load</title>
+        <title>500: This page couldn&apos;t load</title>
         <style dangerouslySetInnerHTML={{ __html: errorThemeCss }} />
       </head>
       <body>
         <div style={errorStyles.container}>
           <div style={errorStyles.card}>
-            <ErrorIcon />
-            <h1 style={errorStyles.title}>This page failed to load</h1>
-            <p style={errorStyles.message}>
-              Something went wrong while loading this page.
+            <WarningIcon />
+            <h1 style={errorStyles.title}>This page couldn&apos;t load</h1>
+            <p style={{ ...errorStyles.message, margin: '0 0 20px 0' }}>
+              Try reloading or go back.
             </p>
-            <p style={errorStyles.messageHint}>
-              If this keeps happening, it may be a server issue.
-            </p>
-            <form>
-              <button type="submit" style={errorStyles.button}>
-                Reload page
+            <div style={errorStyles.buttonGroup}>
+              <form style={{ margin: 0 }}>
+                <button type="submit" style={errorStyles.button}>
+                  Reload
+                </button>
+              </form>
+              <button
+                type="button"
+                id="back-btn"
+                style={errorStyles.buttonSecondary}
+              >
+                Back
               </button>
-            </form>
+            </div>
           </div>
         </div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              'document.getElementById("back-btn").onclick=function(){history.length>1?history.back():location.href="/"}',
+          }}
+        />
       </body>
     </html>
   )
