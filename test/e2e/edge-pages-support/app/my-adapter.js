fb4de2e3b4d0cc9d9d6d8392288190f4542af4e5
JJ Kasper <jj@jjsweb.site>
2026-02-26 13:22:02 -0800

    update test

--- test/e2e/edge-pages-support/app/my-adapter.js
+++ test/e2e/edge-pages-support/app/my-adapter.js
@@ -1,15 +1,5 @@
 module.exports = {
   onBuildComplete({ outputs }) {
-    const duplicateIds = new Set()
-    const seenIds = new Set()
-    for (const output of outputs.pages) {
-      if (seenIds.has(output.id)) {
-        duplicateIds.add(output.id)
-      } else {
-        seenIds.add(output.id)
-      }
-    }
-
     const pagesDataPathnames = outputs.pages
       .map((item) => item.pathname)
       .filter((item) => item.startsWith('/_next/data/'))
@@ -27,10 +17,6 @@ module.exports = {
       ...pagesDataPathnames,
       ...appPagesDataPathnames,
     ].some((item) => /^\/_next\/data\/[^/]+\/index\/index\.json$/.test(item))
-    const hasDataPathWithDifferentId = outputs.pages.some(
-      (item) =>
-        item.pathname.startsWith('/_next/data/') && item.id !== item.pathname
-    )
 
     if (!hasIndexData || !hasDynamicData || hasDoubleIndexData) {
       throw new Error(
@@ -41,8 +27,6 @@ module.exports = {
           `hasIndexData=${hasIndexData}`,
           `hasDynamicData=${hasDynamicData}`,
           `hasDoubleIndexData=${hasDoubleIndexData}`,
-          `hasDataPathWithDifferentId=${hasDataPathWithDifferentId}`,
-          `duplicateIds=${JSON.stringify(Array.from(duplicateIds))}`,
         ].join('\n')
       )
     }
@@ -50,16 +34,6 @@ module.exports = {
     if (appPagesDataPathnames.length > 0) {
       throw new Error(
         `Expected no pages-router data outputs in appPages: ${JSON.stringify(appPagesDataPathnames)}`
-      )
-    }
-    if (hasDataPathWithDifferentId) {
-      throw new Error(
-        `Expected pages-router _next/data outputs to use pathname as id: ${JSON.stringify(outputs.pages.filter((item) => item.pathname.startsWith('/_next/data/')))}`
-      )
-    }
-    if (duplicateIds.size > 0) {
-      throw new Error(
-        `Expected unique output ids, found duplicates: ${JSON.stringify(Array.from(duplicateIds))}`
       )
     }
   },
