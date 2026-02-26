5673fc38081bf2420511080158403efc6d5d8051
Jiwon Choi <devjiwonchoi@gmail.com>
2026-02-26 21:22:27 +0100

    Add `unstable_retry()` to error.js (#89685)
    Extends the error components API to give better control over recovery.
    Previously, the `reset()` prop only cleared the error state and
    re-rendered the children. However, this only handles a temporary
    rendering error.
    
    The error can be due to data fetching or an RSC phase. In these cases,
    `reset()` alone is not sufficient. Users would even need to implement
    retry logic using `router.refresh()`.
    
    Therefore, this PR adds a new `unstable_retry()` prop that calls
    `router.refresh()` and `reset()` within a `startTransition()` to provide
    built-in retry logic. This feature is expected to be preferred over the
    `reset()` prop. Only the cases where you'd choose `reset()` are when you
    have a reason to do a sync reset without loading any new data.
    
    Closes NAR-767

--- test/e2e/app-dir/errors/app/client-component/error.js
+++ test/e2e/app-dir/errors/app/client-component/error.js
@@ -2,12 +2,19 @@
 
 import styles from './style.module.css'
 
-export default function ErrorBoundary({ error, reset }) {
+export default function ErrorBoundary({ error, reset, unstable_retry }) {
   return (
     <>
       <p id="error-boundary-message">An error occurred: {error.message}</p>
       <button id="reset" onClick={() => reset()} className={styles.button}>
         Try again
+      </button>
+      <button
+        id="retry"
+        onClick={() => unstable_retry()}
+        className={styles.button}
+      >
+        Retry
       </button>
     </>
   )
