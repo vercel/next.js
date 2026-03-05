8386014bc74828cd2c3dd38a89a1f5c5f7cfd48f
Tim Neutkens <tim@timneutkens.nl>
2026-03-05 12:01:58 +0100

    Update flight-render-result.ts

--- packages/next/src/server/app-render/flight-render-result.ts
+++ packages/next/src/server/app-render/flight-render-result.ts
@@ -1,13 +1,13 @@
-import type { Readable } from 'node:stream'
 import { RSC_CONTENT_TYPE_HEADER } from '../../client/components/app-router-headers'
 import RenderResult, { type RenderResultMetadata } from '../render-result'
+import type { AnyStream } from './stream-ops'
 
 /**
  * Flight Response is always set to RSC_CONTENT_TYPE_HEADER to ensure it does not get interpreted as HTML.
  */
 export class FlightRenderResult extends RenderResult {
   constructor(
-    response: string | ReadableStream<Uint8Array> | Readable,
+    response: string | AnyStream,
     metadata: RenderResultMetadata = {},
     waitUntil?: Promise<unknown>
   ) {
