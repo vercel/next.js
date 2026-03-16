348958555a5e867288bf389897e4353a50297408
devjiwonchoi <devjiwonchoi@gmail.com>
2026-03-16 21:57:14 +0100

    Revert unnecessary changes

--- packages/next/src/api/error.ts
+++ packages/next/src/api/error.ts
@@ -1,8 +1,6 @@
-'use turbopack no side effects'
+// Pages Router only
+export { default } from '../pages/_error'
+export * from '../pages/_error'
 
 export { unstable_catchError } from '../client/components/catch-error'
 export type { ErrorInfo } from '../client/components/error-boundary'
-
-// Pages Router
-export { default } from '../pages/_error'
-export * from '../pages/_error'
