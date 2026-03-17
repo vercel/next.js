4e94f89e541128c79d41453c2113b75b9d3922d3
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-03-17 15:28:13 +0100

    Turbopack: single module graph for Pages Router pages

--- crates/next-api/src/app.rs
+++ crates/next-api/src/app.rs
@@ -1252,6 +1252,7 @@ impl AppEndpoint {
 
         let is_app_page = matches!(this.ty, AppEndpointType::Page { .. });
 
+        // TODO replace with self.module_graphs()
         let module_graphs = this
             .app_project
             .app_module_graphs(
