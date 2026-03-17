4cbdb9b192dd8c29980197f021c59bb2e7482093
Jiwon Choi <devjiwonchoi@gmail.com>
2026-03-17 19:30:50 +0100

    Explicit `iseReact18` assertion
    https://github.com/vercel/next.js/pull/91528#discussion_r2948744748

--- test/e2e/app-dir/catch-error/catch-error-react-compiler.test.ts
+++ test/e2e/app-dir/catch-error/catch-error-react-compiler.test.ts
@@ -3,7 +3,8 @@ import { nextTestSetup } from 'e2e-utils'
 // FIXME: If NEXT_TEST_REACT_VERSION is set, skip the test for now. Need to address react/compiler-runtime
 // compatibility with React below 19.
 // _describe for cleaner git history.
-const _describe = process.env.NEXT_TEST_REACT_VERSION ? describe.skip : describe
+const isReact18 = parseInt(process.env.NEXT_TEST_REACT_VERSION) === 18
+const _describe = isReact18 ? describe.skip : describe
 
 _describe('app-dir - unstable_catchError with react compiler', () => {
   const { next, isNextDev } = nextTestSetup({
