98b83fa5ddd66505a76a5318b2df8e5aab2b71e8
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-03-31 16:36:35 +0200

    [test] Add suite for `.env` in App router based on existing Pages Router tests

---
+++ test/e2e/app-dir/app-env-config/app-env-config.test.ts
@@ -0,0 +1,186 @@
+import { nextTestSetup } from 'e2e-utils'
+import { retry } from 'next-test-utils'
+import fs from 'fs/promises'
+import { join } from 'path'
+
+describe('app-dir env-config', () => {
+  const { next, isNextDev } = nextTestSetup({
+    files: __dirname,
+    env: {
+      PROCESS_ENV_KEY: 'processenvironment',
+      ENV_FILE_PROCESS_ENV: 'env-cli',
+    },
+  })
+
+  const getEnvFromHtml = async (path: string) => {
+    const $ = await next.render$(path)
+    const env = JSON.parse($('p').text())
+    env.nextConfigEnv = $('#nextConfigEnv').text()
+    env.nextConfigPublicEnv = $('#nextConfigPublicEnv').text()
+    env.nextConfigNewPublicEnv = $('#nextConfigNewPublicEnv').text()
+    return env
+  }
+
+  const checkEnvData = (data: Record<string, string | undefined>) => {
+    expect(data.ENV_FILE_KEY).toBe('env')
+    expect(data.LOCAL_ENV_FILE_KEY).toBe('localenv')
+    expect(data.DEVELOPMENT_ENV_FILE_KEY).toBe(
+      isNextDev ? 'development' : undefined
+    )
+    expect(data.LOCAL_DEVELOPMENT_ENV_FILE_KEY).toBe(
+      isNextDev ? 'localdevelopment' : undefined
+    )
+    expect(data.PRODUCTION_ENV_FILE_KEY).toBe(
+      isNextDev ? undefined : 'production'
+    )
+    expect(data.LOCAL_PRODUCTION_ENV_FILE_KEY).toBe(
+      isNextDev ? undefined : 'localproduction'
+    )
+    expect(data.ENV_FILE_EXPANDED).toBe('env')
+    expect(data.ENV_FILE_EXPANDED_CONCAT).toBe('hello-env')
+    expect(data.ENV_FILE_EXPANDED_ESCAPED).toBe('$ENV_FILE_KEY')
+    expect(data.ENV_FILE_KEY_EXCLAMATION).toBe('hello!')
+    expect(data.ENV_FILE_EMPTY_FIRST).toBe('$escaped')
+    expect(data.ENV_FILE_PROCESS_ENV).toBe('env-cli')
+
+    expect(data.nextConfigEnv).toBe('hello from next.config.js')
+    expect(data.nextConfigPublicEnv).toBe('hello again from next.config.js')
+    expect(data.nextConfigNewPublicEnv).toBe('hello set in next.config.js')
+  }
+
+  it('should have process environment override .env', async () => {
+    const data = await getEnvFromHtml('/')
+
+    // content streams in
+    await retry(() => {
+      expect(data.PROCESS_ENV_KEY).toEqual('processenvironment')
+    })
+  })
+
+  it('should provide global env to next.config.js', async () => {
+    const res = await next.fetch('/hello', { redirect: 'manual' })
+    const { pathname } = new URL(res.headers.get('location')!, 'http://n')
+
+    expect(res.status).toBe(307)
+    expect(pathname).toBe('/another')
+  })
+
+  it('should inline global values during build', async () => {
+    const browser = await next.browser('/global')
+
+    expect(await browser.waitForElementByCss('#global-value').text()).toBe(
+      'another'
+    )
+  })
+
+  it('should provide env for static server component', async () => {
+    const data = await getEnvFromHtml('/static')
+    checkEnvData(data)
+  })
+
+  it('should provide env correctly for dynamic server component', async () => {
+    const data = await getEnvFromHtml('/dynamic')
+
+    // content streams in
+    await retry(() => {
+      checkEnvData(data)
+    })
+  })
+
+  it('should provide env correctly for route handlers', async () => {
+    const data = await next.fetch('/api/all').then((res) => res.json())
+    checkEnvData(data)
+  })
+
+  it('should load env from .env', async () => {
+    const data = await getEnvFromHtml('/')
+
+    // content streams in
+    await retry(() => {
+      expect(data.ENV_FILE_KEY).toEqual('env')
+    })
+    expect(data.ENV_FILE_DEVELOPMENT_OVERRIDE_TEST).toEqual(
+      isNextDev ? 'development' : 'env'
+    )
+    expect(data.ENV_FILE_DEVELOPMENT_LOCAL_OVERRIDEOVERRIDE_TEST).toEqual(
+      isNextDev ? 'localdevelopment' : 'env'
+    )
+    expect(data.ENV_FILE_PRODUCTION_OVERRIDEOVERRIDE_TEST).toEqual(
+      isNextDev ? 'env' : 'production'
+    )
+    expect(data.ENV_FILE_PRODUCTION_LOCAL_OVERRIDEOVERRIDE_TEST).toEqual(
+      isNextDev ? 'env' : 'localproduction'
+    )
+    expect(data.LOCAL_ENV_FILE_KEY).toBe('localenv')
+    expect(data.NEXT_PUBLIC_EMPTY_ENV_VAR).toEqual('')
+
+    const browser = await next.browser('/')
+
+    // content streams in
+    await retry(async () => {
+      expect(
+        await browser.elementByCssInstant('#nextPublicEmptyEnvVar').text()
+      ).toBe('content:')
+    })
+  })
+
+  if (isNextDev) {
+    describe('with hot reload', () => {
+      let originalContents: Array<{ file: string; content: string }> = []
+
+      beforeAll(async () => {
+        const testDir = next.testDir
+        const envFiles = (await fs.readdir(testDir)).filter((file) =>
+          file.startsWith('.env')
+        )
+        originalContents = []
+        const envToUpdate = [
+          {
+            toAdd: 'NEW_ENV_KEY=true',
+            file: '.env',
+          },
+          {
+            toAdd: 'NEW_ENV_LOCAL_KEY=hello',
+            file: '.env.local',
+          },
+          {
+            toAdd: 'NEW_ENV_DEV_KEY=from-dev\nNEXT_PUBLIC_HELLO_WORLD=again',
+            file: '.env.development',
+          },
+        ]
+
+        for (const file of envFiles) {
+          const filePath = join(testDir, file)
+          const content = await fs.readFile(filePath, 'utf8')
+          originalContents.push({ file, content })
+
+          const toUpdate = envToUpdate.find((item) => item.file === file)
+          if (toUpdate) {
+            await fs.writeFile(filePath, content + `\n${toUpdate.toAdd}`)
+          }
+        }
+        await retry(() => {
+          expect(next.cliOutput).toContain('Reload env:')
+        })
+      })
+      afterAll(async () => {
+        const testDir = next.testDir
+        for (const { file, content } of originalContents) {
+          await fs.writeFile(join(testDir, file), content)
+        }
+      })
+
+      it('should have new env values after .env file change', async () => {
+        const data = await getEnvFromHtml('/')
+
+        // content streams in
+        await retry(() => {
+          checkEnvData(data)
+        })
+        expect(data.NEW_ENV_KEY).toBe('true')
+        expect(data.NEW_ENV_LOCAL_KEY).toBe('hello')
+        expect(data.NEW_ENV_DEV_KEY).toBe('from-dev')
+      })
+    })
+  }
+})
---
+++ test/e2e/app-dir/app-env-config/app/page.js
@@ -0,0 +1,64 @@
+import { Suspense } from 'react'
+import { connection } from 'next/server'
+import { EnvDisplay } from './env-display'
+
+const variables = [
+  'PROCESS_ENV_KEY',
+  'ENV_FILE_KEY',
+  'ENV_FILE_EMPTY_FIRST',
+  'ENV_FILE_PROCESS_ENV',
+  'LOCAL_ENV_FILE_KEY',
+  'ENV_FILE_LOCAL_OVERRIDE_TEST',
+  'PRODUCTION_ENV_FILE_KEY',
+  'LOCAL_PRODUCTION_ENV_FILE_KEY',
+  'DEVELOPMENT_ENV_FILE_KEY',
+  'LOCAL_DEVELOPMENT_ENV_FILE_KEY',
+  'ENV_FILE_DEVELOPMENT_OVERRIDE_TEST',
+  'ENV_FILE_DEVELOPMENT_LOCAL_OVERRIDEOVERRIDE_TEST',
+  'ENV_FILE_PRODUCTION_OVERRIDEOVERRIDE_TEST',
+  'ENV_FILE_PRODUCTION_LOCAL_OVERRIDEOVERRIDE_TEST',
+  'TEST_ENV_FILE_KEY',
+  'LOCAL_TEST_ENV_FILE_KEY',
+  'ENV_FILE_TEST_OVERRIDE_TEST',
+  'ENV_FILE_TEST_LOCAL_OVERRIDEOVERRIDE_TEST',
+  'ENV_FILE_EXPANDED',
+  'ENV_FILE_EXPANDED_CONCAT',
+  'ENV_FILE_EXPANDED_ESCAPED',
+  'ENV_FILE_KEY_EXCLAMATION',
+  'NEW_ENV_KEY',
+  'NEW_ENV_LOCAL_KEY',
+  'NEW_ENV_DEV_KEY',
+  'NEXT_PUBLIC_HELLO_WORLD',
+  'NEXT_PUBLIC_EMPTY_ENV_VAR',
+]
+
+async function EnvInDynamic() {
+  await connection()
+  const items = {}
+
+  variables.forEach((variable) => {
+    if (typeof process.env[variable] !== 'undefined') {
+      items[variable] = process.env[variable]
+    }
+  })
+
+  return (
+    <>
+      <p>{JSON.stringify(items)}</p>
+      <div id="nextConfigEnv">{process.env.nextConfigEnv}</div>
+      <div id="nextConfigPublicEnv">{process.env.nextConfigPublicEnv}</div>
+      <div id="nextConfigNewPublicEnv">
+        {process.env.NEXT_PUBLIC_NEW_NEXT_CONFIG_VALUE}
+      </div>
+      <EnvDisplay />
+    </>
+  )
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback="Loading...">
+      <EnvInDynamic />
+    </Suspense>
+  )
+}
