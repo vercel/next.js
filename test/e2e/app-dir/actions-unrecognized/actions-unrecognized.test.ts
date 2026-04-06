ed2a778fa62d9049c3b09a9470156569a89a6f94
Tim Neutkens <tim@timneutkens.nl>
2026-04-06 11:43:39 +0200

    Update test/e2e/app-dir/actions-unrecognized/actions-unrecognized.test.ts
    Co-authored-by: Zack Tanner <1939140+ztanner@users.noreply.github.com>

--- test/e2e/app-dir/actions-unrecognized/actions-unrecognized.test.ts
+++ test/e2e/app-dir/actions-unrecognized/actions-unrecognized.test.ts
@@ -10,8 +10,6 @@ describe('unrecognized server actions', () => {
 
   let cliOutputPosition: number = 0
   beforeEach(() => {
-    console.log('update file 2')
-    console.log('update file 3')
     cliOutputPosition = next.cliOutput.length
   })
   const getLogs = () => {
