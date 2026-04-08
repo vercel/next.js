cd27516ce599ab3b48e612a7916038f219bf19fe
Tim Neutkens <tim@timneutkens.nl>
2026-04-08 18:28:48 +0200

    Deprecate preferredRegion as it's linked to edge runtime

--- packages/next/src/build/warn-about-edge-runtime.ts
+++ packages/next/src/build/warn-about-edge-runtime.ts
@@ -5,3 +5,9 @@ export function warnAboutEdgeRuntime() {
     `The Edge Runtime is deprecated. You can use the "nodejs" runtime instead. Learn more: https://nextjs.org/docs/messages/edge-runtime-deprecated`
   )
 }
+
+export function warnAboutPreferredRegion() {
+  Log.warnOnce(
+    `The "preferredRegion" route segment config is deprecated. Learn more: https://nextjs.org/docs/messages/preferred-region-deprecated`
+  )
+}
