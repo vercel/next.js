b45725d4aabc194e51b9064544401d1185d9292e
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-04-08 16:15:23 +0200

    WIP

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/pure-comments/input/index.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/scope-hoisting/pure-comments/input/index.js
@@ -1,13 +1,15 @@
-import './a.ts'
+import { Used } from './a.ts'
 
 it('should retain PURE comments with scope hoisting', () => {
+  expect(Used.THIS_IS_USED).toBe(0)
+
   let factory = __turbopack_modules__.get(
     [...__turbopack_modules__.keys()].find((m) =>
-      m.endsWith(
-        'scope-hoisting/pure-comments/input/index.js [test] (ecmascript)'
-      )
+      m.endsWith('scope-hoisting/pure-comments/input/a.ts [test] (ecmascript)')
     )
   )
 
-  expect(factory.toString()).not.toContain('THIS_SHOULD_BE_REMOVED')
+  const source = factory.toString()
+  expect(source).not.toContain('THIS_SHOULD_BE_REMOVED')
+  expect(source).toContain('THIS_IS_USED')
 })
