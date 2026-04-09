d6d97bae9030fc829166e028785979090e0a20c0
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-04-09 23:37:06 +0200

    Stop ignore-listing original stack frames if source maps are disabled

--- packages/next/src/server/node-environment-extensions/error-inspect.tsx
+++ packages/next/src/server/node-environment-extensions/error-inspect.tsx
@@ -1,3 +1,19 @@
+import * as module from 'module'
+import * as process from 'process'
 import { patchErrorInspectNodeJS } from '../patch-error-inspect'
 
-patchErrorInspectNodeJS(globalThis.Error)
+const sourceMappingEnabled =
+  // @ts-expect-error Node.js 22+ only
+  typeof module.getSourceMapsSupport === 'function'
+    ? // @ts-expect-error Node.js 22+ only
+      module.getSourceMapsSupport().enabled
+    : process.env.NODE_OPTIONS?.includes('--enable-source-maps') ||
+      process.execArgv.includes('--enable-source-maps')
+
+if (sourceMappingEnabled) {
+  patchErrorInspectNodeJS(globalThis.Error)
+} else {
+  // leave Errors untouched if source maps are disabled.
+  // We're likely in a prod server where sourcemapping is done lazily when logs
+  // are inspected so ignore-listing would permanently hide these frames.
+}
