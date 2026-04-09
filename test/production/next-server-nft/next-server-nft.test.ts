983f31b7b7f8081d87edbaebd3daddbe4c480c47
Jude Gao <jude.gao@vercel.com>
2026-04-08 23:42:47 -0400

    Update next-server-nft snapshot to include detect-agent.js

--- test/production/next-server-nft/next-server-nft.test.ts
+++ test/production/next-server-nft/next-server-nft.test.ts
@@ -355,6 +355,7 @@ async function readNormalizedNFT(next, name) {
            "/node_modules/next/dist/shared/*",
            "/node_modules/next/dist/telemetry/anonymous-meta.js",
            "/node_modules/next/dist/telemetry/detached-flush.js",
+           "/node_modules/next/dist/telemetry/detect-agent.js",
            "/node_modules/next/dist/telemetry/events/build.js",
            "/node_modules/next/dist/telemetry/events/index.js",
            "/node_modules/next/dist/telemetry/events/plugins.js",
