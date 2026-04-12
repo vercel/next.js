3067e1af3c0c2164c9f74f94360a0c31af2785a4
Jimmy Lai <laijimmy0@gmail.com>
2026-04-11 17:22:25 -0700

    Propagate fallback artifact base through hydration

---
+++ test/e2e/app-dir-export/fixtures/dynamic-fallback-cache-components/app/hydrated/[thread]/page.js
@@ -0,0 +1,20 @@
+import { Suspense } from 'react'
+import { LinkAccordion } from '../../components/link-accordion'
+import HydratedThreadClient from './thread-client'
+
+export default function HydratedThreadPage() {
+  return (
+    <>
+      <Suspense fallback={<h1>Loading hydrated thread...</h1>}>
+        <HydratedThreadClient />
+      </Suspense>
+      <ul>
+        <li>
+          <LinkAccordion href="/hydrated/second">
+            Visit hydrated thread second
+          </LinkAccordion>
+        </li>
+      </ul>
+    </>
+  )
+}
