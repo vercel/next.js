c143c5db8da642880510b999b3eccc57b43be2e1
Hanul Lee <lnuvy.code@gmail.com>
2026-04-12 22:40:12 +0900

    fix: set Cross-Origin-Resource-Policy on /_next/static assets

---
+++ test/e2e/app-dir/worker-coep/next.config.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
