eb5eae03b0c4814521c1c2350a6ec7551da87ade
Josh Story <story@hey.com>
2026-04-13 15:30:14 -0700

    Update tests to use unstable_prefetch export
    adjust all the fixtures to use the new API

--- test/e2e/app-dir/instant-validation/app/suspense-in-root/disable-validation/in-page-with-outer/page.tsx
+++ test/e2e/app-dir/instant-validation/app/suspense-in-root/disable-validation/in-page-with-outer/page.tsx
@@ -5,6 +5,7 @@ export const unstable_instant = {
   samples: [{ cookies: [] }],
   unstable_disableValidation: true,
 }
+export const unstable_prefetch = 'runtime'
 
 export default async function Page() {
   await connection()
