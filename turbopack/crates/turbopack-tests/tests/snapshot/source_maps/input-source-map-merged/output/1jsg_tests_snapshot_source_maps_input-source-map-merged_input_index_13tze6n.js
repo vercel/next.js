46a3570fdf0cb5857d9f68ca98efdf9d5356dedc
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-04-16 16:05:03 +0200

    revert snapshot changes

--- turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/output/1jsg_tests_snapshot_source_maps_input-source-map-merged_input_index_13tze6n.js
+++ turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/output/1jsg_tests_snapshot_source_maps_input-source-map-merged_input_index_13tze6n.js
@@ -5,7 +5,7 @@
 // MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/input/index.js [test] (ecmascript)
 ;
 __turbopack_context__.s([], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/input/index.js [test] (ecmascript)");
-// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/input/sourcemapped.ts [test] (ecmascript)
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map-merged/input/sourcemapped.js [test] (ecmascript)
 ;
 function runExternalSourceMapped(fn) {
     return fn();
