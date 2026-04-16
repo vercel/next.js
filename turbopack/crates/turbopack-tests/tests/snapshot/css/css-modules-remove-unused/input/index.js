bb6c6867559c91f36f595f3adafe5e10090d0272
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-04-16 22:08:48 +0200

    add snapshot test

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules-remove-unused/input/index.js
@@ -0,0 +1,3 @@
+import * as styles from './style.module.css'
+
+console.log(styles.green)
---
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules-remove-unused/output/1ece_tests_snapshot_css_css-modules-remove-unused_input_index_03orf36.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    "output/1ece_tests_snapshot_css_css-modules-remove-unused_input_index_03orf36.js",
+    {"otherChunks":["output/1jsg_tests_snapshot_css_css-modules-remove-unused_input_style_module_1ee6vwx.css","output/0_9x_turbopack-tests_tests_snapshot_css_css-modules-remove-unused_input_0hzk-no._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/css-modules-remove-unused/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
