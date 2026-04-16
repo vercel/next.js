f7a89376ec73245bdb0635dfe78e42150151a991
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-04-16 10:58:50 +0200

    Update sourcemap snapshots due to TS transpilation changes

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map/output/0rv8_turbopack-tests_tests_snapshot_source_maps_input-source-map_input_index_1_ghqz3.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([
+    "output/0rv8_turbopack-tests_tests_snapshot_source_maps_input-source-map_input_index_1_ghqz3.js",
+    {"otherChunks":["output/0_9x_turbopack-tests_tests_snapshot_source_maps_input-source-map_input_0shkpmy._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/source_maps/input-source-map/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
