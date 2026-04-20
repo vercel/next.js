6cb8eab9125874027fd71470447bc81ad501dbaf
Janka Uryga <lolzatu2@gmail.com>
2026-04-20 15:12:09 +0200

    wip

---
+++ packages/next/src/server/node-environment-extensions/track-work-unit.test.ts
@@ -0,0 +1,332 @@
+import { AsyncLocalStorage } from 'node:async_hooks'
+import {
+  createWorkUnitTracker,
+  getPromiseAsyncId,
+  rootTask,
+  workUnitAsyncStorage,
+  log,
+  type WorkUnitTracker,
+  getOriginWorkUnit,
+} from './track-work-unit'
+
+async function setup<T>(
+  onMismatch: Parameters<typeof createWorkUnitTracker>[0],
+  cb: (tracker: WorkUnitTracker) => Promise<T>
+): Promise<T> {
+  const tracker = createWorkUnitTracker(onMismatch)
+  tracker.hook.enable()
+  try {
+    return await rootTask(() => cb(tracker))
+  } finally {
+    tracker.hook.disable()
+  }
+}
+
+it('one', async () => {
+  const onMismatch = jest.fn()
+  const tracker = createWorkUnitTracker(onMismatch)
+  tracker.hook.enable()
+  try {
+    await rootTask(async () => {
+      log(`creating promise`)
+      const p = Promise.resolve(2)
+      log(`created promise: ${getPromiseAsyncId(p)}`)
+
+      log(`=============================`)
+      log(`creating derived promise`)
+      const derived = p.then((x) => 2 * x)
+      log(`created derived promise: ${getPromiseAsyncId(derived)}`)
+      await derived
+
+      log(`=============================`)
+      log(`creating derived promise 2`)
+      const derived2 = p.then((x) => 4 * x)
+      log(`created derived promise 2: ${getPromiseAsyncId(derived2)}`)
+      await derived2
+    })
+  } finally {
+    tracker.hook.disable()
+  }
+})
+
+it('successive 1', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async (tracker) => {
+    let cachedPromise: Promise<any>
+
+    const initialStore = { id: 'initial' }
+    await workUnitAsyncStorage.run(initialStore, async () => {
+      log('[initial] creating cached promise')
+      cachedPromise = Promise.resolve()
+      log(
+        `[initial] created cached promise ${getPromiseAsyncId(cachedPromise)}`
+      )
+      expect(getOriginWorkUnit(tracker, cachedPromise)).toBe(initialStore)
+      const x = cachedPromise.then(() =>
+        log('[initial] cached promise resolved')
+      )
+      expect(getOriginWorkUnit(tracker, x)).toBe(initialStore)
+    })
+
+    const finalStore = { id: 'final' }
+    await workUnitAsyncStorage.run(finalStore, async () => {
+      // wait for promise to be set
+      await Promise.resolve()
+      await Promise.resolve()
+      log('-----------------------')
+
+      log('[final] then-ing cached promise')
+      expect(onMismatch).not.toHaveBeenCalled()
+      const p = cachedPromise.then(() => {
+        log("then'ed cached promise resolved")
+      })
+      log('[final] then-ed cached promise')
+      expect(getOriginWorkUnit(tracker, p)).toBe(finalStore)
+      expect(onMismatch).toHaveBeenNthCalledWith(1, 'initial', 'final')
+      await p
+
+      log('[final] awaiting cached promise')
+      await cachedPromise
+      expect(onMismatch).toHaveBeenNthCalledWith(2, 'initial', 'final')
+    })
+  })
+})
+
+it('successive 2', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async (tracker) => {
+    let cachedPromise: Promise<any>
+
+    const initialStore = { id: 'initial' }
+    await workUnitAsyncStorage.run(initialStore, async () => {
+      log('[initial] creating cached promise')
+      const trigger = Promise.resolve()
+      // attempt to launder the promise
+      cachedPromise = new Promise((resolve, reject) => {
+        trigger.then(resolve, reject)
+      })
+      log(
+        `[initial] created cached promise ${getPromiseAsyncId(cachedPromise)}`
+      )
+      expect(getOriginWorkUnit(tracker, cachedPromise)).toBe(initialStore)
+      const x = cachedPromise.then(() =>
+        log('[initial] cached promise resolved')
+      )
+      expect(getOriginWorkUnit(tracker, x)).toBe(initialStore)
+    })
+
+    const finalStore = { id: 'final' }
+    await workUnitAsyncStorage.run(finalStore, async () => {
+      // wait for promise to be set
+      await Promise.resolve()
+      await Promise.resolve()
+      log('-----------------------')
+
+      log('[final] then-ing cached promise')
+      expect(onMismatch).not.toHaveBeenCalled()
+      const p = cachedPromise.then(() => {
+        log("then'ed cached promise resolved")
+      })
+      log('[final] then-ed cached promise')
+      expect(getOriginWorkUnit(tracker, p)).toBe(finalStore)
+      expect(onMismatch).toHaveBeenNthCalledWith(1, 'initial', 'final')
+      await p
+
+      log('[final] awaiting cached promise')
+      await cachedPromise
+      expect(onMismatch).toHaveBeenNthCalledWith(2, 'initial', 'final')
+    })
+  })
+})
+
+it('nested 1', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async (tracker) => {
+    let cachedPromise: Promise<any>
+
+    const outerStore = { id: 'outer' }
+    log(`[outer] running outer store`)
+    const outerWorkUnitPromise = workUnitAsyncStorage.run(
+      outerStore,
+      async () => {
+        log('[outer] creating cached promise')
+        cachedPromise = Promise.resolve()
+        log(
+          `[outer] created cached promise ${getPromiseAsyncId(cachedPromise)}`
+        )
+        expect(getOriginWorkUnit(tracker, cachedPromise)).toBe(outerStore)
+
+        log(`[outer] running inner store`)
+        const innerStore = { id: 'inner' }
+        const innerWorkUnitPromise = workUnitAsyncStorage.run(
+          innerStore,
+          async () => {
+            log('-----------------------')
+
+            log('[inner] then-ing cached promise')
+            expect(onMismatch).not.toHaveBeenCalled()
+            const p = cachedPromise.then(() => {
+              log("then'ed cached promise resolved")
+            })
+            log('[inner] then-ed cached promise')
+            expect(getOriginWorkUnit(tracker, p)).toBe(innerStore)
+            expect(onMismatch).toHaveBeenNthCalledWith(1, 'outer', 'inner')
+            await p
+
+            log('[inner] awaiting cached promise')
+            await cachedPromise
+            expect(onMismatch).toHaveBeenNthCalledWith(2, 'outer', 'inner')
+          }
+        )
+        log('inner store promise', getPromiseAsyncId(innerWorkUnitPromise))
+        // TODO: fix this (should null)
+        // expect(getOriginWorkUnit(tracker, innerWorkUnitPromise)).toBe(
+        //   outerStore
+        // )
+        await innerWorkUnitPromise
+      }
+    )
+    log('outer store promise', getPromiseAsyncId(outerWorkUnitPromise))
+    // TODO: fix this (should null)
+    // expect(getOriginWorkUnit(tracker, outerWorkUnitPromise)).toBe(null)
+    await outerWorkUnitPromise
+  })
+})
+
+it('one store', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async (tracker) => {
+    log(`running store`)
+    const store = { id: 'one' }
+    const workUnitPromise = workUnitAsyncStorage.run(store, async () => {
+      log('[one] creating promise')
+      const promise = Promise.resolve()
+      log(`[one] created promise ${getPromiseAsyncId(promise)}`)
+      expect(getOriginWorkUnit(tracker, promise)).toBe(store)
+
+      await promise
+
+      await (async () => {
+        const anotherPromise = (async () => {})()
+        expect(getOriginWorkUnit(tracker, anotherPromise)).toBe(store)
+      })()
+    })
+    log(`store promise ${getPromiseAsyncId(workUnitPromise)}`)
+    expect(getOriginWorkUnit(tracker, workUnitPromise)).toBe(null)
+    await workUnitPromise
+
+    expect(getOriginWorkUnit(tracker, Promise.resolve())).toBe(null)
+  })
+})
+
+it('one store, sync run', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async (tracker) => {
+    log(`running store`)
+    const store = { id: 'one' }
+    const promises = workUnitAsyncStorage.run(store, () => {
+      const promise1 = Promise.resolve()
+      expect(getOriginWorkUnit(tracker, promise1)).toBe(store)
+      const promise2 = Promise.resolve()
+      expect(getOriginWorkUnit(tracker, promise2)).toBe(store)
+      return [promise1, promise2]
+    })
+    expect(getOriginWorkUnit(tracker, Promise.resolve())).toBe(null)
+    await Promise.all(promises)
+    expect(onMismatch).not.toHaveBeenCalled()
+  })
+})
+
+it('async snapshot', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async (tracker) => {
+    log(`running store`)
+    const store = { id: 'one' }
+    const runInStore = workUnitAsyncStorage.run(store, () =>
+      AsyncLocalStorage.snapshot()
+    )
+
+    const workUnitPromise = runInStore(async () => {
+      const promise = Promise.resolve()
+      log(`[one] created promise ${getPromiseAsyncId(promise)}`)
+      expect(getOriginWorkUnit(tracker, promise)).toBe(store)
+    })
+    log(`store promise ${getPromiseAsyncId(workUnitPromise)}`)
+
+    // TODO: `AsyncLocalStorage#run()` returns a promise where the store is set,
+    // so this will give us the store, not null.
+    // expect(getOriginWorkUnit(tracker, storePromise)).toBe(null)
+    await workUnitPromise
+
+    // Going from no store to a store is not a mismatch.
+    // We need to be able to await a promise returned from `AsyncLocalStorage#run()`.
+    expect(onMismatch).not.toHaveBeenCalled()
+
+    // We're outside the run() call, so there shouldn't be a store here.
+    expect(getOriginWorkUnit(tracker, Promise.resolve())).toBe(null)
+
+    // still no calls here.
+    expect(onMismatch).not.toHaveBeenCalled()
+  })
+})
+
+it.only('new promise', async () => {
+  const onMismatch = jest.fn<void, [string, string]>()
+  await setup(onMismatch, async () => {
+    const original = Promise.reject(4)
+    log(`original promise: ${getPromiseAsyncId(original)}`)
+
+    const another = Promise.resolve(10)
+    log(`another promise: ${getPromiseAsyncId(another)}`)
+
+    const derived = new Promise((resolve, reject) => {
+      // const x = original.then(resolve, reject)
+      const x = original.then(
+        () => {
+          another.then(resolve, reject)
+        },
+        () => {
+          another.then(resolve, reject)
+        }
+      )
+      log(`intermediate promise: ${getPromiseAsyncId(x)}`)
+    })
+    log(`derived promise: ${getPromiseAsyncId(derived)}`)
+    await derived
+  })
+})
+
+// await rootTask(async () => {
+//   let cachedPromise: Promise<any>;
+//   await Promise.all([
+//     workUnitAsyncStorage.run({ id: "initial" }, async () => {
+//       log("[initial] creating cached promise");
+//       cachedPromise = new Promise((resolve) => setTimeout(resolve));
+//     }),
+//     workUnitAsyncStorage.run({ id: "final" }, async () => {
+//       log("-----------------------");
+//       log("[final] awaiting cached promise");
+//       const result = await cachedPromise;
+//     }),
+//   ]);
+// });
+
+// const p = Promise.resolve(2);
+// log(`created promise: ${getPromiseAsyncId(p)}`);
+// const derived = p.then((x) => 2 * x);
+// log(`created derived promise: ${getPromiseAsyncId(derived)}`);
+// await derived;
+// log("------------------------");
+// log("creating timeout promise");
+// const tp = new Promise<void>((resolve) => {
+//   log("creating timeout");
+//   setTimeout(() => {
+//     log(
+//       `timeout fired, resolving timeout promise ${getPromiseAsyncId(tp)}`,
+//     );
+//     resolve();
+//   });
+// });
+// log(`created timeout promise ${getPromiseAsyncId(tp)}`);
+// log(`awaiting timeout promise ${getPromiseAsyncId(tp)}`);
+// await tp;
---
+++ packages/next/src/server/node-environment-extensions/track-work-unit.ts
@@ -0,0 +1,269 @@
+import {
+  AsyncLocalStorage,
+  AsyncResource,
+  createHook,
+  executionAsyncId,
+  triggerAsyncId,
+} from 'node:async_hooks'
+import * as util from 'node:util'
+import * as fs from 'node:fs'
+
+type StoreId = string
+type WorkUnitStore = { id: StoreId }
+export const workUnitAsyncStorage = new AsyncLocalStorage<WorkUnitStore>()
+
+let currentAlsRunCall: WorkUnitStore | null = null
+
+const AsyncLocalStorage$run = AsyncLocalStorage.prototype.run
+AsyncLocalStorage.prototype.run = function <T, R>(this: AsyncLocalStorage<T>) {
+  const storage = this
+  if (storage === workUnitAsyncStorage) {
+    const value = arguments[0] as WorkUnitStore
+    const callback = arguments[1]
+    const previous = currentAlsRunCall
+
+    currentAlsRunCall = value
+    try {
+      return AsyncLocalStorage$run.call(
+        this,
+        value,
+        wrapWithNamedAsyncNode(WORK_UNIT_SCOPE_MARKER, callback),
+        ...Array.prototype.slice.call(arguments, 2)
+      ) as R
+      // return AsyncLocalStorage$run.apply(this, arguments as any) as R
+    } finally {
+      currentAlsRunCall = previous
+    }
+  } else {
+    return AsyncLocalStorage$run.apply(this, arguments as any) as R
+  }
+}
+
+const WORK_UNIT_SCOPE_MARKER = 'NEXT:WORK_UNIT_SCOPE_MARKER'
+
+function wrapWithNamedAsyncNode<T>(name: string, callback: () => T): () => T {
+  return () => {
+    // when a named function wrapped with `AsyncLocalStorage.bind` is invoked,
+    // it triggers async hooks (`init`/`before`/`after`).
+    // inside `init`, the `type` will be the name of the function. We can use this to
+    // add a marker to detect when we're in a `AsyncLocalStorage#run` call
+    // and transitioning to a new workUnitStore.
+    const namedCallback = { [name]: () => callback() }[name.slice(0)]
+    return AsyncLocalStorage.bind(namedCallback)()
+  }
+}
+
+const INFO_MISSING_TYPE = 'MISSING_INIT'
+const INFO_PROMISE_TYPE = 'PROMISE'
+
+type AsyncResourceNode = {
+  type: string
+  status: 'pending' | 'executing' | 'finished'
+  // isStoreTransition: boolean
+  originStore: WorkUnitStore | null
+}
+
+function createMissingNode(): AsyncResourceNode {
+  return {
+    type: INFO_MISSING_TYPE,
+    status: 'pending',
+    originStore: null,
+  }
+}
+
+export function createWorkUnitTracker(
+  onStoreMismatch: (expected: StoreId, actual: StoreId) => void
+) {
+  const nodesById = new Map<number, AsyncResourceNode>()
+
+  const getInfo = (id: number) => {
+    if (id === 0) return 'main'
+    const node = nodesById.get(id)
+    if (!node) return '<missing>'
+    return `${node.type}@${node.status}, ${node.originStore?.id ?? '<no store>'}`
+  }
+
+  const hook = createHook({
+    // called when the async resource is created
+    init(id, type, triggerId, _resource) {
+      debug('---------')
+      debug(
+        `# init ${id} (${type}), trigger: ${triggerId} (${getInfo(triggerId)}), eid: ${executionAsyncId()}, tid: ${triggerAsyncId()} store: ${workUnitAsyncStorage.getStore()?.id ?? '<no store>'}`
+      )
+
+      const triggerStore: WorkUnitStore | null =
+        nodesById.get(triggerId)?.originStore ?? null
+
+      // let originStore: WorkUnitStore | null
+      // if (type === WORK_UNIT_SCOPE_MARKER) {
+      //   originStore =
+      // } else {
+
+      // }
+
+      const currentStore = workUnitAsyncStorage.getStore() ?? null
+      // Note: we don't always get one promise that acts like a root for the store transition,
+      // there can be multiple
+      const node: AsyncResourceNode = {
+        type,
+        status: 'pending',
+        originStore: currentStore,
+      }
+      nodesById.set(id, node)
+
+      if (
+        type === WORK_UNIT_SCOPE_MARKER &&
+        currentAlsRunCall !== null &&
+        currentAlsRunCall === currentStore
+      ) {
+        // we're currently executing workUnitAsyncStorage.run for this store.
+        // it will be different from the outer context, and that's not an error.
+        debug(
+          `AsyncLocalStorage.run: from ${triggerStore?.id ?? '<no store>'} to ${currentStore.id}`
+        )
+      } else {
+        if (
+          currentStore &&
+          triggerStore &&
+          currentStore.id !== triggerStore.id
+        ) {
+          debug(
+            '#'.repeat(40) +
+              '\n' +
+              `store mismatch for ${id}, ${currentStore.id} != ${triggerStore.id}`,
+            '\n' + '#'.repeat(40)
+          )
+          debug(new Error().stack)
+          onStoreMismatch(triggerStore.id, currentStore.id)
+        }
+      }
+    },
+
+    // before() is called after init(), just before the resource's callback is called. It can be
+    // called 0-N times for handles (such as TCPWrap), and will be called exactly 1
+    // time for requests (such as FSReqCallback).
+    before(id) {
+      debug(`before ${id} (${getInfo(id)})`)
+      logIndent += 1
+      let info = nodesById.get(id)
+      if (!info) {
+        nodesById.set(id, (info = createMissingNode()))
+      }
+      info.status = 'executing'
+    },
+
+    // We don't get before/after for a Promise.resolve call,
+    // we need to catch those here.
+    // It also gets called for promise resolutions in general, so we handle those here
+    promiseResolve(id) {
+      debug(`promiseResolve ${id} (${getInfo(id)}), tid: ${triggerAsyncId()}`)
+      let info = nodesById.get(id)
+      if (!info) {
+        nodesById.set(id, (info = createMissingNode()))
+      }
+      if (info.status !== 'finished') {
+        info.status = 'finished'
+        // if we're backfilling a node that didn't init, update the type --
+        // we know it's a promise.
+        if (info.type === INFO_MISSING_TYPE) {
+          info.type = INFO_PROMISE_TYPE
+        }
+      }
+    },
+
+    after(id) {
+      logIndent -= 1
+      debug(`after ${id} (${getInfo(id)})`)
+      let info = nodesById.get(id)
+      if (!info) {
+        nodesById.set(id, (info = createMissingNode()))
+      }
+      if (info.status !== 'finished') {
+        info.status = 'finished'
+      }
+      // TODO: does this need to be a stack or something?
+      // currentId = 0;
+    },
+
+    destroy(id) {
+      debug(`destroy ${id} (${getInfo(id)})`)
+    },
+  })
+
+  return {
+    hook,
+    nodesById,
+  }
+}
+
+export type WorkUnitTracker = ReturnType<typeof createWorkUnitTracker>
+
+export function getOriginWorkUnit(
+  tracker: WorkUnitTracker,
+  resource: object
+): WorkUnitStore | null {
+  const id = AsyncResource$asyncId.call(resource)
+  return tracker.nodesById.get(id)?.originStore ?? null
+}
+
+export const rootTask = <T>(cb: () => Promise<T>) => {
+  debug('rootTask')
+  logIndent += 1
+  const rootPromise = Promise.resolve()
+  const taskPromise = rootPromise.then(cb)
+  logIndent -= 1
+  debug(
+    'created root task promise',
+    getPromiseAsyncId(rootPromise),
+    '->',
+    getPromiseAsyncId(taskPromise)
+  )
+  return taskPromise
+}
+
+// behavior of awaiting an already resolved promise (after its initial then() was called) -
+// does not get the promise as a trigger it gets the outer scope instead
+// (the one where .then() was called)
+// actually maybe this isn't tue?
+
+//======================
+
+let logIndent = 0
+
+function formatLog(args: any[]): string {
+  const msg = args
+    .map((arg) => (typeof arg === 'string' ? arg : `${util.inspect(arg)}`))
+    .join(' ')
+  const indentStr = ' '.repeat(logIndent * 2)
+  if (msg.includes('\n')) {
+    return msg
+      .split('\n')
+      .map((line) => indentStr + line)
+      .join('\n')
+  } else {
+    return indentStr + msg
+  }
+}
+
+export function log(...args: any[]) {
+  logImpl(formatLog(args))
+}
+
+function debug(...args: any[]) {
+  logImpl(dim(formatLog(args)))
+}
+
+function logImpl(msg: string) {
+  fs.writeFileSync(process.stdout.fd, msg + '\n')
+}
+
+function dim(text: string) {
+  // cannot use `util.styleText` -- somehow, it triggers async_hooks by creating a TTY object
+  return '\x1B[2m' + text + '\x1B[22m'
+}
+
+const AsyncResource$asyncId = AsyncResource.prototype.asyncId
+
+export function getPromiseAsyncId(promise: Promise<any>): number {
+  return AsyncResource$asyncId.call(promise)
+}
