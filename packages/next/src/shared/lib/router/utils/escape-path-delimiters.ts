70a3e702659732e56f5f63619cda9b3011edb9a8
Aman Gupta <amanmacair98@gmail.com>
2026-04-20 15:34:30 +0530

    fix: add type check for non-string path params in getStaticPaths (#41281)

--- packages/next/src/shared/lib/router/utils/escape-path-delimiters.ts
+++ packages/next/src/shared/lib/router/utils/escape-path-delimiters.ts
@@ -3,6 +3,12 @@ export default function escapePathDelimiters(
   segment: string,
   escapeEncoded?: boolean
 ): string {
+  if (typeof segment !== 'string') {
+    throw new Error(
+      `A required parameter was not provided as a string received ${typeof segment === 'object' ? JSON.stringify(segment) : segment} (${typeof segment}). All path parameters in getStaticPaths must be strings.`
+    )
+  }
+
   return segment.replace(
     new RegExp(`([/#?]${escapeEncoded ? '|%(2f|23|3f|5c)' : ''})`, 'gi'),
     (char: string) => encodeURIComponent(char)
