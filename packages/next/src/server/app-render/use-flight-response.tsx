c09cd76c28305730a91578f3796f8ab373e804d2
JJ Kasper <jj@jjsweb.site>
2026-04-21 16:30:03 -0700

    Ignore malformed CSP nonce headers

--- packages/next/src/server/app-render/use-flight-response.tsx
+++ packages/next/src/server/app-render/use-flight-response.tsx
@@ -1,7 +1,7 @@
 import type { BinaryStreamOf } from './app-render'
 import type { Readable } from 'node:stream'
 
-import { htmlEscapeJsonString } from '../htmlescape'
+import { htmlEscapeAttributeString, htmlEscapeJsonString } from '../htmlescape'
 import { workUnitAsyncStorage } from './work-unit-async-storage.external'
 import { InvariantError } from '../../shared/lib/invariant-error'
 import { getClientReferenceManifest } from './manifests-singleton'
@@ -165,7 +165,7 @@ export function createInlinedDataReadableStream(
   formState: unknown | null
 ): ReadableStream<Uint8Array> {
   const startScriptTag = nonce
-    ? `<script nonce=${JSON.stringify(nonce)}>`
+    ? `<script nonce="${htmlEscapeAttributeString(nonce)}">`
     : '<script>'
 
   const flightReader = flightStream.getReader()
