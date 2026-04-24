7d474d76bc7615227f8ecde6e3c0dd669c868895
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2026-04-23 17:24:10 -0700

    fix napi build on mac

--- crates/next-napi-bindings/build.rs
+++ crates/next-napi-bindings/build.rs
@@ -5,11 +5,8 @@ use serde_json::Value;
 fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-env-changed=CI");
     println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_OS");
-    println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
     let is_ci = env::var("CI").is_ok_and(|value| !value.is_empty());
-    let is_macos_aarch64_target = env::var("CARGO_CFG_TARGET_OS")
-        .is_ok_and(|value| value == "macos")
-        && env::var("CARGO_CFG_TARGET_ARCH").is_ok_and(|value| value == "aarch64");
+    let is_macos_target = env::var("CARGO_CFG_TARGET_OS").is_ok_and(|value| value == "macos");
 
     let nextjs_version = {
         let package_json_path = Path::new(env!("CARGO_MANIFEST_DIR"))
@@ -73,12 +70,12 @@ fn main() -> anyhow::Result<()> {
         _ => println!("cargo:warning=`git rev-parse HEAD` failed"),
     }
 
-    if !is_macos_aarch64_target {
+    if !is_macos_target {
         napi_build::setup();
     }
 
-    // This is a workaround for napi always including a GCC specific flag.
-    if is_macos_aarch64_target {
+    // This is a workaround for napi always including a GCC-specific flag on macOS.
+    if is_macos_target {
         println!("cargo:rerun-if-env-changed=DEBUG_GENERATED_CODE");
         println!("cargo:rerun-if-env-changed=TYPE_DEF_TMP_PATH");
         println!("cargo:rerun-if-env-changed=CARGO_CFG_NAPI_RS_CLI_VERSION");
