b0aa57b474c661f5bfec514c017b465b7987f4a2
Luke Sandberg <lukesandberg@users.noreply.github.com>
2026-04-28 15:29:39 -0700

    reset how the selftimetree is reset

--- turbopack/crates/turbopack-trace-server/src/store.rs
+++ turbopack/crates/turbopack-trace-server/src/store.rs
@@ -81,7 +81,9 @@ impl Store {
     pub fn reset(&mut self) {
         self.spans = ChunkedVec::new();
         self.spans.push(new_root_span());
-        self.self_time_tree.take();
+        if let Some(tree) = self.self_time_tree.as_mut() {
+            *tree = SelfTimeTree::new();
+        }
         *self.max_self_time_lookup_time.get_mut() = 0;
         self.memory_samples.clear();
     }
