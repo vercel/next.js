e5b2d25a4c2348c5393ca4d7153b7911ffe93f9f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-04-28 23:22:46 +0200

    Turbopack: pull in updated vercel/nft tests (#93321)
    From
    https://github.com/vercel/nft/commit/baf9df627e30f3e3c4199eeda128a527c715fffc
    
    Still need to eventually look into actually enabling all of these

---
+++ turbopack/crates/turbopack-tracing/tests/node-file-trace/test/unit/pino-transport-constructor/output.js
@@ -0,0 +1,8 @@
+;[
+  'test/unit/pino-transport-constructor/input.js',
+  'test/unit/pino-transport-constructor/node_modules/my-pino-transport/index.js',
+  'test/unit/pino-transport-constructor/node_modules/my-pino-transport/package.json',
+  'test/unit/pino-transport-constructor/node_modules/pino/index.js',
+  'test/unit/pino-transport-constructor/node_modules/pino/package.json',
+  'test/unit/pino-transport-constructor/package.json',
+]
