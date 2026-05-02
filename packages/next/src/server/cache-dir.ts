6d25aba207d11153fc24cbcf76fb3f55b662bf8a
Tim Neutkens <tim@timneutkens.nl>
2026-05-02 21:50:25 +0200

    Update

--- packages/next/src/server/cache-dir.ts
+++ packages/next/src/server/cache-dir.ts
@@ -2,7 +2,10 @@ import path from 'path'
 import isDockerFunction from 'next/dist/compiled/is-docker'
 
 export function getStorageDirectory(distDir: string): string | undefined {
-  const isLikelyEphemeral = isDockerFunction()
+  const isLikelyEphemeral =
+    process.env.NEXT_PRIVATE_TEST_NOT_IN_DOCKER === '1'
+      ? false
+      : isDockerFunction()
 
   if (isLikelyEphemeral) {
     return undefined
