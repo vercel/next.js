2829c6f99db12550afb3a9e360a7ac6e457ef288
Luke Sandberg <lukesandberg@users.noreply.github.com>
2026-05-05 11:23:53 -0700

    snap

--- turbopack/crates/turbopack-ecmascript/src/analyzer/imports.rs
+++ turbopack/crates/turbopack-ecmascript/src/analyzer/imports.rs
@@ -18,7 +18,6 @@ use swc_core::{
         visit::{Visit, VisitWith},
     },
 };
-use triomphe::Arc;
 use turbo_frozenmap::FrozenMap;
 use turbo_rcstr::{RcStr, rcstr};
 use turbo_tasks::{FxIndexMap, FxIndexSet, ResolvedVc};
