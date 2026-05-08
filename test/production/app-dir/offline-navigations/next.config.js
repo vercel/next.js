cc2d61a07303b0484024cf14907221d40de9da2f
Jimmy Lai <laijimmy0@gmail.com>
2026-05-08 10:11:49 -0700

    offline navigations: emit fallback manifest (3/25)

--- test/production/app-dir/offline-navigations/next.config.js
+++ test/production/app-dir/offline-navigations/next.config.js
@@ -3,9 +3,12 @@
  */
 const nextConfig = {
   cacheComponents: true,
+  assetPrefix: 'https://cdn.example.com/app-assets',
+  basePath: '/docs',
   experimental: {
     offlineNavigations: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
