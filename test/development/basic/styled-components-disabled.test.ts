49b2580c38c5009cbbe62a51b4fcc8c371f7217c
Tim Neutkens <tim@timneutkens.nl>
2026-05-11 17:03:45 +0200

    Update styled-components-disabled.test.ts

--- test/development/basic/styled-components-disabled.test.ts
+++ test/development/basic/styled-components-disabled.test.ts
@@ -1,28 +1,22 @@
 import { join } from 'path'
 import webdriver from 'next-webdriver'
-import { createNext, FileRef } from 'e2e-utils'
-import { NextInstance } from 'e2e-utils'
+import { FileRef, nextTestSetup } from 'e2e-utils'
 import { retry } from 'next-test-utils'
 
 // TODO: Somehow the warning doesn't show up with Turbopack, even though the transform is not enabled.
 // TODO: It no longer shows up with Webpack either in tests.
 describe.skip('styled-components SWC transform', () => {
-  let next: NextInstance
-
-  beforeAll(async () => {
-    next = await createNext({
-      files: {
-        'next.config.js': new FileRef(
-          join(__dirname, 'styled-components-disabled/next.config.js')
-        ),
-        pages: new FileRef(join(__dirname, 'styled-components-disabled/pages')),
-      },
-      dependencies: {
-        'styled-components': '6.1.16',
-      },
-    })
+  const { next } = nextTestSetup({
+    files: {
+      'next.config.js': new FileRef(
+        join(__dirname, 'styled-components-disabled/next.config.js')
+      ),
+      pages: new FileRef(join(__dirname, 'styled-components-disabled/pages')),
+    },
+    dependencies: {
+      'styled-components': '6.1.16',
+    },
   })
-  afterAll(() => next.destroy())
 
   it('should have hydration mismatch with styled-components transform disabled', async () => {
     let browser
