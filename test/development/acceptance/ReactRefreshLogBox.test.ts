a94b6396536cc44fde866aead89050bc2e8b8f99
Cong-Cong Pan <dacongsama@live.com>
2026-05-14 16:38:08 +0800

    update snapshot

--- test/development/acceptance/ReactRefreshLogBox.test.ts
+++ test/development/acceptance/ReactRefreshLogBox.test.ts
@@ -731,7 +731,7 @@ describe('ReactRefreshLogBox', () => {
          "source": "./index.module.css
          ╰─▶   × SyntaxError
                │
-               │ (1:1) <FIXME-project-root>/index.module.css Unknown word
+               │ (1:1) <FIXME-project-root>/index.module.css Unknown word .button
                │
                │ > 1 | .button
                │     | ^
--- test/e2e/turbopack-worker-asset-prefix/turbopack-worker-asset-prefix.test.ts
+++ test/e2e/turbopack-worker-asset-prefix/turbopack-worker-asset-prefix.test.ts
@@ -3,7 +3,7 @@ import type { NextInstance } from 'e2e-utils'
 import { findPort, retry } from 'next-test-utils'
 
 // `experimental.turbopackWorkerAssetPrefix` is turbopack-only.
-const isTurbopack = !process.env.IS_WEBPACK_TEST
+const isTurbopack = !process.env.IS_WEBPACK_TEST && !process.env.NEXT_RSPACK
 const describeTurbopack =
   isTurbopack && !isNextDeploy ? describe : describe.skip
 
