90070d177b5e8be6c72ecd1f597701fbd79a9cc3
Tim Neutkens <tim@timneutkens.nl>
2026-05-15 16:04:42 +0200

    Remove cache components skip

--- test/e2e/app-dir/metadata-static-file/metadata-static-file-group-route.test.ts
+++ test/e2e/app-dir/metadata-static-file/metadata-static-file-group-route.test.ts
@@ -2,28 +2,6 @@ import { nextTestSetup } from 'e2e-utils'
 import { getCommonMetadataHeadTags } from './utils'
 
 describe('metadata-files-static-output-group-route', () => {
-  if (process.env.__NEXT_CACHE_COMPONENTS) {
-    // Cache Components build fails when metadata files are inside a dynamic route.
-    //
-    // Route "/dynamic/[id]": Next.js encountered uncached or runtime data in `generateMetadata()`.
-    //
-    // This prevents the page from being prerendered, leading to a slower user experience.
-    //
-    // Ways to fix this:
-    //   - Use a static metadata export instead of `generateMetadata()`
-    //   - Cache the metadata with `"use cache"` in `generateMetadata()`
-    //   - Add a dynamic data access (e.g. `await connection()`) to the page to render it at request time
-    //   - Set `export const instant = false` to allow a blocking route
-    //
-    // Learn more: https://nextjs.org/docs/messages/next-prerender-dynamic-metadata
-    // Error occurred prerendering page "/dynamic/[id]". Read more: https://nextjs.org/docs/messages/prerender-error
-    // Export encountered an error on /dynamic/[id]/page: /dynamic/[id], exiting the build.
-    //
-    // TODO: Remove this skip when metadata files are supported in dynamic routes for Cache Components.
-    it.skip('should skip test for Cache Components', () => {})
-    return
-  }
-
   const { next, skipped } = nextTestSetup({
     files: __dirname,
     skipDeployment: true,
