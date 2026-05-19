80ee6f7378a7fbf8ebbd9e54648b0aee7ffe8296
Tim Neutkens <tim@timneutkens.nl>
2026-05-19 10:14:06 +0200

    Use Node Fizz stream for server inserted HTML

--- packages/next/src/server/app-render/make-get-server-inserted-html.tsx
+++ packages/next/src/server/app-render/make-get-server-inserted-html.tsx
@@ -6,11 +6,14 @@ import {
   getRedirectStatusCodeFromError,
 } from '../../client/components/redirect'
 import { isRedirectError } from '../../client/components/redirect-error'
-import { renderToReadableStream } from 'react-dom/server'
-import { streamToString } from '../stream-utils/node-web-streams-helper'
 import { RedirectStatusCode } from '../../client/components/redirect-status-code'
 import { addPathPrefix } from '../../shared/lib/router/utils/add-path-prefix'
 import type { ClientTraceDataEntry } from '../lib/trace/tracer'
+import {
+  renderToNodeFizzStream,
+  renderToWebFizzStream,
+  streamToString,
+} from './stream-ops'
 
 export function makeGetServerInsertedHTML({
   polyfills,
@@ -84,19 +87,23 @@ export function makeGetServerInsertedHTML({
       return ''
     }
 
-    // TODO: This should use Node streams when __NEXT_USE_NODE_STREAMS is true
-    const stream = await renderToReadableStream(
+    const streamOptions = {
+      // Larger chunk because this isn't sent over the network.
+      // Let's set it to 1MB.
+      progressiveChunkSize: 1024 * 1024,
+    }
+    const { stream } = await (
+      process.env.__NEXT_USE_NODE_STREAMS
+        ? renderToNodeFizzStream
+        : renderToWebFizzStream
+    )(
       <>
         {polyfillTags}
         {serverInsertedHTML}
         {traceMetaTags}
         {errorMetaTags}
       </>,
-      {
-        // Larger chunk because this isn't sent over the network.
-        // Let's set it to 1MB.
-        progressiveChunkSize: 1024 * 1024,
-      }
+      streamOptions
     )
 
     // The polyfills and trace metadata have been flushed, so they don't need to be rendered again
