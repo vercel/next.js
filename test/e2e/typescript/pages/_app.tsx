84a9c91cf303f60e4f6016d1c5ec99fda38c043e
Bob Put <bobu.work@gmail.com>
2026-05-24 15:34:53 -0400

    fix(next): type AppType generic as app props

--- test/e2e/typescript/pages/_app.tsx
+++ test/e2e/typescript/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppType } from 'next/app'
 
-const MyApp: AppType<{ foo: string }> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const MyApp: AppType<{ foo: string }> = ({ Component, pageProps, foo }) => {
+  // @ts-expect-error AppType's parameter applies to app props, not page props.
+  type _PageFoo = typeof pageProps.foo
+
+  return (
+    <>
+      <p id="app-prop">{foo}</p>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 MyApp.getInitialProps = () => ({ foo: 'bar' })
