7f6a70a54103597dafae6bd6b98ab79d23dfb3c2
Tim Neutkens <tim@timneutkens.nl>
2026-05-29 13:43:46 +0200

    test(instrumentation): use cookies for dynamic app route

--- test/e2e/instrumentation-hook/with-async-node-app-route/app/api/check/route.ts
+++ test/e2e/instrumentation-hook/with-async-node-app-route/app/api/check/route.ts
@@ -1,6 +1,8 @@
-export const dynamic = 'force-dynamic'
+import { cookies } from 'next/headers'
 
 export async function GET() {
+  await cookies()
+
   return Response.json({
     finished: Boolean((globalThis as any).instrumentationFinished),
   })
