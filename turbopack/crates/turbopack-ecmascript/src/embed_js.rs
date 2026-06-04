b215f3bd3b0311ce716c8cd54bef257a53de8be4
Sam Poder <sampoder@berkeley.edu>
2026-06-04 16:01:41 -0700

    Update turbopack/crates/turbopack-ecmascript/src/embed_js.rs
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- turbopack/crates/turbopack-ecmascript/src/embed_js.rs
+++ turbopack/crates/turbopack-ecmascript/src/embed_js.rs
@@ -3,5 +3,5 @@ use turbo_tasks_fs::{FileSystem, embed_directory};
 
 #[turbo_tasks::function]
 pub fn embed_fs() -> Vc<Box<dyn FileSystem>> {
-    embed_directory!("turbopack-cli", "$CARGO_MANIFEST_DIR/js/src")
+    embed_directory!("turbopack-ecmascript", "$CARGO_MANIFEST_DIR/js/src")
 }
