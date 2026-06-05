a8292515d45cb5234cc9ca441bdf19316093e1f1
Luke Sandberg <lukesandberg@users.noreply.github.com>
2026-06-05 10:22:20 -0700

    fix

--- test/e2e/filesystem-cache/next.config.js
+++ test/e2e/filesystem-cache/next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
   experimental: {
     turbopackFileSystemCacheForBuild: enableCaching,
     turbopackFileSystemCacheForDev: enableCaching,
-    turbopackMemoryEviction: enableEviction ? 'full' : 'off',
+    turbopackMemoryEviction: enableEviction ? false : 'off',
   },
   env: {
     NEXT_PUBLIC_CONFIG_ENV: 'hello world',
