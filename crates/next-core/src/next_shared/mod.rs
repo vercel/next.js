fbe4bbfa0eac1e280e207d61f5fe19b0f7a9f9b7
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-06-05 19:06:18 +0200

    WIP

--- crates/next-core/src/next_shared/mod.rs
+++ crates/next-core/src/next_shared/mod.rs
@@ -1,3 +1,12 @@
+use crate::{next_client::ClientContextType, next_server::ServerContextType};
+
 pub(crate) mod resolve;
 pub(crate) mod transforms;
 pub(crate) mod webpack_rules;
+
+#[turbo_tasks::value(shared, task_input)]
+#[derive(Debug, Clone, Hash)]
+enum ContextType {
+    Client(ClientContextType),
+    Server(ServerContextType),
+}
