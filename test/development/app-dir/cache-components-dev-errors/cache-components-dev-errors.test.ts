97a5d4b9bd5f90e3eeadc43eab18191d01bb7c68
Aurora Scharff <aurora.sofie@gmail.com>
2026-06-06 17:01:20 +0200

    fix: update E1290 -> E1318 in cache-components-dev-errors test

--- test/development/app-dir/cache-components-dev-errors/cache-components-dev-errors.test.ts
+++ test/development/app-dir/cache-components-dev-errors/cache-components-dev-errors.test.ts
@@ -98,7 +98,7 @@ describe('Cache Components Dev Errors', () => {
 
     await expect(browser).toDisplayCollapsedRedbox(`
      {
-       "code": "E1290",
+       "code": "E1318",
        "description": "Next.js encountered uncached data during prerendering.",
        "environmentLabel": "Server",
        "label": "Blocking Route",
