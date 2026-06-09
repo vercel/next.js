99a96f96333d8771b5c3569d8dbb4072b0f3c37b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-06-09 11:53:03 +0200

    add test

---
+++ test/production/adapter-root/adapter-root.test.ts
@@ -0,0 +1,58 @@
+// import fs from 'fs'
+import path from 'path'
+import { nextTestSetup } from 'e2e-utils'
+import type { NextAdapter } from 'next'
+
+describe('adapter-root', () => {
+  describe.each([
+    { name: 'via lockfile', env: {} },
+    // The Vercel CLI sets this to the repo root
+    { name: 'via NEXT_PRIVATE_OUTPUT_TRACE_ROOT', setEnvVar: true },
+  ])('$name', ({ setEnvVar }) => {
+    const { next } = nextTestSetup({
+      files: path.join(__dirname, 'fixture'),
+      subDir: 'sub',
+      skipStart: true,
+      overrideFiles: {
+        '../package-lock.json': JSON.stringify({
+          name: 'parent-workspace',
+          version: '1.0.0',
+          lockfileVersion: 3,
+        }),
+      },
+    })
+
+    it('should correctly determine repoRoot', async () => {
+      const expectedRepoRoot = path.dirname(next.testDir)
+
+      if (setEnvVar) {
+        next.env.NEXT_PRIVATE_OUTPUT_TRACE_ROOT = expectedRepoRoot
+      }
+      await next.build()
+
+      const {
+        outputs,
+        repoRoot,
+        projectDir,
+      }: Parameters<NextAdapter['onBuildComplete']>[0] = await next.readJSON(
+        'build-complete.json'
+      )
+
+      expect(projectDir).toBe(next.testDir)
+      expect(repoRoot).toBe(expectedRepoRoot)
+
+      const combinedRouteOutputs = [
+        ...outputs.appPages,
+        ...outputs.appRoutes,
+        ...outputs.pages,
+        ...outputs.pagesApi,
+      ]
+      console.log(combinedRouteOutputs)
+      // for (const output of combinedRouteOutputs) {
+      //   for (const asset of Object.keys(output.assets)) {
+      //     expect(fs.existsSync(path.join(repoRoot, output.assets))).toBeTrue()
+      //   }
+      // }
+    })
+  })
+})
