3fb5abc1cf6d7492a28360d5f46968ef07358646
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-06-16 17:04:05 +0200

    make issue title colored

--- packages/next/src/shared/lib/turbopack/utils.ts
+++ packages/next/src/shared/lib/turbopack/utils.ts
@@ -5,7 +5,7 @@ import type {
   TurbopackResult,
 } from '../../../build/swc/types'
 
-import { bold, green, magenta, red } from '../../../lib/picocolors'
+import { bold, green, magenta, red, yellow } from '../../../lib/picocolors'
 import { deobfuscateText } from '../magic-identifier'
 import type { EntryKey } from './entry-key'
 import * as Log from '../../../build/output/log'
@@ -99,12 +99,28 @@ function formatFilePath(filePath: string): string {
 }
 
 export function formatIssue(issue: Issue) {
-  const { filePath, title, description, detail, source, importTraces } = issue
+  const {
+    filePath,
+    title,
+    description,
+    detail,
+    source,
+    importTraces,
+    severity,
+  } = issue
   let { documentationLink } = issue
-  const formattedTitle = renderStyledStringToErrorAnsi(title).replace(
+  let formattedTitle = renderStyledStringToErrorAnsi(title).replace(
     /\n/g,
     '\n    '
   )
+
+  if (severity === 'bug' || severity === 'error' || severity === 'fatal') {
+    formattedTitle = `Error: ${red(bold(formattedTitle))}`
+  } else if (severity === 'warning') {
+    formattedTitle = `Warning: ${yellow(bold(formattedTitle))}`
+  } else {
+    formattedTitle = `Info: ${bold(formattedTitle)}`
+  }
 
   // TODO: Use error codes to identify these
   // TODO: Generalize adapting Turbopack errors to Next.js errors
