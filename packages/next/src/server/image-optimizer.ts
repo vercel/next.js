8c8c2304a52a7f93a2e6840411b8b510d4f11e5b
Steven <steven@ceriously.com>
2026-06-18 12:17:49 -0400

    fix(next/image): improve performance of detectContentType() (#106)
    In a [previous PR](https://github.com/vercel/next.js/pull/82118), we added a fallback to `sharp().metadata()` when the content type could not be detected.
    
    This was a mistake because `sharp().metadata()` is much slower and we don't want to block the event loop.
    
    Even though the user could could disable the behavior with `imgOptSkipMetadata: true`, its best to remove the slow code path. I have confirmed this passes all the tests when true so we can now remove the slow code path.

--- packages/next/src/server/image-optimizer.ts
+++ packages/next/src/server/image-optimizer.ts
@@ -227,10 +227,7 @@ async function deleteFromCacheDir(cacheDir: string, cacheKey: string) {
  * https://en.wikipedia.org/wiki/List_of_file_signatures
  */
 export async function detectContentType(
-  buffer: Buffer,
-  skipMetadata: boolean | null | undefined,
-  concurrency?: number | null | undefined,
-  operationCache?: boolean | null | undefined
+  buffer: Buffer
 ): Promise<string | null> {
   if (buffer.byteLength === 0) {
     return null
@@ -308,28 +305,13 @@ export async function detectContentType(
     return JP2
   }
 
-  let format:
-    | import('sharp').Metadata['format']
-    | ReturnType<typeof detector>
-    | undefined
-  format = detector(buffer)
-
-  if (!format && !skipMetadata) {
-    const sharp = getSharp(concurrency, operationCache)
-    const meta = await sharp(buffer)
-      .metadata()
-      .catch((_) => null)
-    format = meta?.format
-  }
+  const format = detector(buffer.subarray(0, 1024))
 
   switch (format) {
-    case 'avif':
-      return AVIF
     case 'webp':
       return WEBP
     case 'png':
       return PNG
-    case 'jpeg':
     case 'jpg':
       return JPEG
     case 'gif':
@@ -342,28 +324,14 @@ export async function detectContentType(
     case 'jp2':
       return JP2
     case 'tiff':
-    case 'tif':
       return TIFF
-    case 'pdf':
-      return PDF
     case 'bmp':
       return BMP
     case 'ico':
       return ICO
     case 'icns':
       return ICNS
-    case 'dcraw':
-    case 'dz':
-    case 'exr':
-    case 'fits':
     case 'heif':
-    case 'input':
-    case 'magick':
-    case 'openslide':
-    case 'ppm':
-    case 'rad':
-    case 'raw':
-    case 'v':
     case 'cur':
     case 'dds':
     case 'j2c':
@@ -372,8 +340,10 @@ export async function detectContentType(
     case 'psd':
     case 'tga':
     case undefined:
+      return null // unsupported formats
     default:
-      return null
+      format satisfies never // exhaustive check
+      return null // impossible to reach
   }
 }
 
@@ -1066,7 +1036,6 @@ export async function imageOptimizer(
       | 'imgOptOperationCache'
       | 'imgOptMaxInputPixels'
       | 'imgOptSequentialRead'
-      | 'imgOptSkipMetadata'
       | 'imgOptTimeoutInSeconds'
     >
     images: Pick<
@@ -1094,12 +1063,7 @@ export async function imageOptimizer(
     getMaxAge(imageUpstream.cacheControl)
   )
 
-  const upstreamType = await detectContentType(
-    upstreamBuffer,
-    nextConfig.experimental.imgOptSkipMetadata,
-    nextConfig.experimental.imgOptConcurrency,
-    nextConfig.experimental.imgOptOperationCache
-  )
+  const upstreamType = await detectContentType(upstreamBuffer)
 
   if (
     !upstreamType ||
