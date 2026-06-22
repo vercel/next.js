687476557baeeaa01ba5364dd943331bd7b0b511
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-06-22 11:30:47 +0200

    f

--- test/e2e/opentelemetry/instrumentation/app/api/app/[param]/cached/route.ts
+++ test/e2e/opentelemetry/instrumentation/app/api/app/[param]/cached/route.ts
@@ -2,8 +2,8 @@ export function generateStaticParams() {
   return []
 }
 
+export const revalidate = 120
+
 export async function GET() {
   return new Response(JSON.stringify({ test: 'data' }), { status: 201 })
 }
-
-export const revalidate = 120
--- test/e2e/opentelemetry/instrumentation/app/app/[param]/cached/page.tsx
+++ test/e2e/opentelemetry/instrumentation/app/app/[param]/cached/page.tsx
@@ -1,3 +1,7 @@
+export function generateStaticParams() {
+  return []
+}
+
 export const revalidate = 120
 
 export default function Page() {
