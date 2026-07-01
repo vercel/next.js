85b2d100bc4f2302ba6df8fefd13d98f2e37d274
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-07-01 20:18:25 +0200

    fix?

--- packages/next/src/build/templates/middleware.ts
+++ packages/next/src/build/templates/middleware.ts
@@ -92,6 +92,26 @@ export async function handler(
     requestMeta?: RequestMeta
   }
 ): Promise<Response> {
+  if (process.env.NEXT_RUNTIME !== 'edge') {
+    // This mirror what `RouteModule.prepare` does for routes
+    const { join, relative } =
+      require('node:path') as typeof import('node:path')
+    const { ensureInstrumentationRegistered } =
+      require('../../server/lib/router-utils/instrumentation-globals.external') as typeof import('../../server/lib/router-utils/instrumentation-globals.external')
+
+    const absoluteProjectDir = join(
+      /* turbopackIgnore: true */
+      process.cwd(),
+      ctx.requestMeta?.relativeProjectDir || ''
+    )
+    const absoluteDistDir = ctx.requestMeta?.distDir
+    const distDir = absoluteDistDir
+      ? relative(absoluteProjectDir, absoluteDistDir)
+      : '.next'
+
+    await ensureInstrumentationRegistered(absoluteProjectDir, distDir)
+  }
+
   const result = await internalHandler({
     request: {
       url: request.url,
