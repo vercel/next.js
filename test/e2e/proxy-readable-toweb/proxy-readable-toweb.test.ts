8075615d72a11352b9f5ade32172d1ff78326478
Tim Neutkens <tim@timneutkens.nl>
2026-07-07 16:03:03 +0200

    Skip deploy mode for proxy body stream test

--- test/e2e/proxy-readable-toweb/proxy-readable-toweb.test.ts
+++ test/e2e/proxy-readable-toweb/proxy-readable-toweb.test.ts
@@ -3,6 +3,7 @@ import { nextTestSetup } from 'e2e-utils'
 describe('proxy request body with Readable.toWeb', () => {
   const { next } = nextTestSetup({
     files: __dirname,
+    skipDeployment: true,
   })
 
   it('allows a pages api route to read the proxied body with Readable.toWeb()', async () => {
