af2f9a94606191a07d78d17014d2f124f728c038
Sam Poder <sampoder@berkeley.edu>
2026-07-16 00:39:40 -0700

    [turbopack] [wip/testing] Switch `shared_runtime` to on by-default

--- crates/next-core/src/next_config.rs
+++ crates/next-core/src/next_config.rs
@@ -2534,7 +2534,7 @@ impl NextConfig {
             // The shared runtime / inlined bootstrap is a production-only optimization; in
             // development the per-route runtime is required for HMR.
             NextMode::Development => false,
-            NextMode::Build => self.experimental.turbopack_shared_runtime.unwrap_or(false),
+            NextMode::Build => self.experimental.turbopack_shared_runtime.unwrap_or(true),
         }))
     }
 
