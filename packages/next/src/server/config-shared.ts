530d4fa31e010a05f28ea6e26a5f51f80f61e0c6
Sebastian "Sebbie" Silbermann <sebastian.silbermann@vercel.com>
2026-07-20 17:18:14 +0200

    [15.x] fix(next/image): improve performance of detectContentType()
    https://github.com/vercel/next-js-mirror/pull/123
    
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -494,7 +494,6 @@ export interface ExperimentalConfig {
   imgOptTimeoutInSeconds?: number
   imgOptMaxInputPixels?: number
   imgOptSequentialRead?: boolean | null
-  imgOptSkipMetadata?: boolean | null
   optimisticClientCache?: boolean
   /**
    * @deprecated use config.expireTime instead
@@ -1566,7 +1565,6 @@ export const defaultConfig = Object.freeze({
     imgOptTimeoutInSeconds: 7,
     imgOptMaxInputPixels: 268_402_689, // https://sharp.pixelplumbing.com/api-constructor#:~:text=%5Boptions.limitInputPixels%5D
     imgOptSequentialRead: null,
-    imgOptSkipMetadata: null,
     isrFlushToDisk: true,
     workerThreads: false,
     proxyTimeout: undefined,
