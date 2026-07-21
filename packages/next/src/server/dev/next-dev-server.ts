3ed60553da7c27352bc3d27d078b45521dc7f420
Sam Poder <sampoder@berkeley.edu>
2026-07-21 14:03:04 -0700

    Revert "[turbopack] Don't SSR on pages only navigated to through a soft nav (#95539)" (#96028)
    Causes issues with `use client` it seems.

--- packages/next/src/server/dev/next-dev-server.ts
+++ packages/next/src/server/dev/next-dev-server.ts
@@ -235,13 +235,12 @@ export default class DevServer extends Server {
     const { pagesDir, appDir } = findPagesDir(this.dir)
 
     const ensurer: RouteEnsurer = {
-      ensure: async (match, pathname, options) => {
+      ensure: async (match, pathname) => {
         await this.ensurePage({
           definition: match.definition,
           page: match.definition.page,
           clientOnly: false,
           url: pathname,
-          rscOnly: options?.rscOnly,
         })
       },
     }
@@ -983,7 +982,6 @@ export default class DevServer extends Server {
     appPaths?: ReadonlyArray<string> | null
     definition: RouteDefinition | undefined
     url?: string
-    rscOnly?: boolean
   }): Promise<void> {
     await this.bundlerService.ensurePage(opts)
   }
