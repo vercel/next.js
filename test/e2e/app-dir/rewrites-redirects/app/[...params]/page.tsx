a753ea9f3f3484fa1bfa96e4f025ac83b5d4d243
dan <dan.abramov@gmail.com>
2026-07-27 20:54:09 +0100

    [test] Run more test suites under cacheComponents flag, again (#96196)
    Reverts the revert of #95878.
    
    This failed on `main` due to React bugs
    (https://github.com/react/react/pull/37061?) which are fixed now.

--- test/e2e/app-dir/rewrites-redirects/app/[...params]/page.tsx
+++ test/e2e/app-dir/rewrites-redirects/app/[...params]/page.tsx
@@ -1,11 +1,20 @@
-export default async function Page(props: {
-  params: Promise<{ params: Array<string> }>
-}) {
-  const params = await props.params
-  const { params: catchAllParams } = await params
+import { Suspense } from 'react'
+
+async function Content(props: { params: Promise<{ params: Array<string> }> }) {
+  const { params: catchAllParams } = await props.params
   return (
     <div id="page" className={`page_${catchAllParams.join('_')}`}>
       {catchAllParams.join('/')}
     </div>
+  )
+}
+
+export default function Page(props: {
+  params: Promise<{ params: Array<string> }>
+}) {
+  return (
+    <Suspense>
+      <Content params={props.params} />
+    </Suspense>
   )
 }
