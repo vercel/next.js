b54789333c6745510ace8aef109e1ddf513a6ab4
Marcos Hernanz <96699542+marcoshernanz@users.noreply.github.com>
2026-07-30 02:06:00 +0000

    Stabilize dev middleware redirect test

--- test/e2e/app-dir/navigation/navigation.test.ts
+++ test/e2e/app-dir/navigation/navigation.test.ts
@@ -999,11 +999,14 @@ describe('app dir - navigation', () => {
   describe('middleware redirect', () => {
     it('should change browser location when router.refresh() gets a redirect response', async () => {
       const browser = await next.browser('/redirect-on-refresh/auth')
-      await retry(async () =>
-        expect(await browser.url()).toBe(
-          next.url + '/redirect-on-refresh/dashboard'
-        )
+      await retry(
+        async () =>
+          expect(await browser.url()).toBe(
+            next.url + '/redirect-on-refresh/dashboard'
+          ),
+        10_000
       )
+      expect(await browser.elementByCss('p').text()).toBe('Dashboard')
     })
   })
 
