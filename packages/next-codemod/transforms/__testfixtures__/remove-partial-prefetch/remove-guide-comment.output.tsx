50dd6f7a4176d90761ac3646fdefd2fb50b95edd
Aurora Scharff <aurora.sofie@gmail.com>
2026-08-02 12:56:31 +0200

    skill: link partial prefetching migration comments

---
+++ packages/next-codemod/transforms/__testfixtures__/remove-partial-prefetch/remove-guide-comment.output.tsx
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { Suspense } from 'react';
+
+// TODO(runtime-prefetch): assess whether this link should prefetch URL data.
+export default function Page() {
+  return <p>hello world</p>;
+}
