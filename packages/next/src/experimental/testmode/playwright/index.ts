9a88040105982f356561000a0815f4e37033b7b3
lazerg <lazerg2@gmail.com>
2026-08-04 04:51:34 +0500

    Fix playwright config merging for projects property

--- packages/next/src/experimental/testmode/playwright/index.ts
+++ packages/next/src/experimental/testmode/playwright/index.ts
@@ -17,17 +17,17 @@ export function defineConfig<T extends NextOptionsConfig, W>(
 export function defineConfig<T extends NextOptionsConfig = NextOptionsConfig>(
   config: base.PlaywrightTestConfig<T>
 ): base.PlaywrightTestConfig<T> {
-  if (config.webServer !== undefined) {
-    // Playwright doesn't merge the `webServer` field as we'd expect, so remove our default if the user specifies one.
-    const { webServer, ...partialDefaultPlaywrightConfig } =
-      defaultPlaywrightConfig as base.PlaywrightTestConfig<T>
-    return base.defineConfig<T>(partialDefaultPlaywrightConfig, config)
-  } else {
-    return base.defineConfig<T>(
-      defaultPlaywrightConfig as base.PlaywrightTestConfig<T>,
-      config
-    )
-  }
+  // Playwright doesn't replace the `webServer` and `projects` fields as we'd expect, so remove our defaults if the user specifies their own.
+  const { webServer, projects, ...partialDefaultPlaywrightConfig } =
+    defaultPlaywrightConfig as base.PlaywrightTestConfig<T>
+  return base.defineConfig<T>(
+    {
+      ...partialDefaultPlaywrightConfig,
+      ...(config.webServer === undefined ? { webServer } : undefined),
+      ...(config.projects === undefined ? { projects } : undefined),
+    },
+    config
+  )
 }
 
 export type { NextFixture, NextOptions }
