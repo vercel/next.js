49b6a05923e32ca440d9c9e2ed97eafeb4b4d720
Joseph Chamochumbi <joseph.chamochumbi@vercel.com>
2026-08-04 16:23:15 +0200

    Normalize hash out of route cache canonical URLs in discoverKnownRoute

--- test/e2e/app-dir/navigation/app/hash-cross-path-push/client-component.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export function RouterPushToTarget() {
-  return (
-    <Link id="link-to-target-bar" href="/hash-cross-path-push/destination#bar">
-      To destination#bar
-    </Link>
-  )
-}
