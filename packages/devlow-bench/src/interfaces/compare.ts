658f56f18c69af2f87bc5ea9840608a280b452a7
Will Binns-Smith <wbinnssmith@gmail.com>
2026-08-06 16:08:44 -0700

    Apply suggestions from code review
    Co-authored-by: Luke Sandberg <lukesandberg@users.noreply.github.com>

--- packages/devlow-bench/src/interfaces/compare.ts
+++ packages/devlow-bench/src/interfaces/compare.ts
@@ -1,6 +1,5 @@
 import type { Interface } from '../index.js'
-import type { SampleGroup } from '../compare.js'
-import { groupRows, makeKey, printComparison } from '../compare.js'
+import { type SampleGroup, groupRows, makeKey, printComparison } from '../compare.js'
 import { readSnapshot } from '../snapshot.js'
 import { formatVariantProps } from '../utils.js'
 
