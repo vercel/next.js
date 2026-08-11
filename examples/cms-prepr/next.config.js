1a67ae8bdd89787c1e5efb35da172f4d34e1b641
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2026-08-11 14:40:49 +0200

    examples(cms-prepr): update config for Next 16 and Tailwind v4

--- examples/cms-prepr/next.config.js
+++ examples/cms-prepr/next.config.js
@@ -2,18 +2,8 @@
 module.exports = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "b-cdn.net",
-        port: "",
-        pathname: "/my-account/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.stream.prepr.io",
-        port: "",
-        pathname: "/my-account/**",
-      },
+      { protocol: 'https', hostname: '*.stream.prepr.io' },
+      { protocol: 'https', hostname: '*.b-cdn.net' },
     ],
   },
-};
+}
