lineaje-labs-gos/vercel-next.js

[turbopack] Track members across namespace re-exports.

turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/output/1jsg_tests_snapshot_ns-value-reexport_named-import_input_index_1a3a-lo.js
@@ -1,0 +1,75 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push(["output/1jsg_tests_snapshot_ns-value-reexport_named-import_input_index_1a3a-lo.js",
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/index.js [test] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/index.js [test] (ecmascript)
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z members object>
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript)
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <locals>
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/locales/en.js [test] (ecmascript)
+;
+const __TURBOPACK__default__export__ = 'EN_USED_MARKER';
+;
+;
+const object = ()=>'object';
+const string = ()=>'string';
+const defaultLocale = __TURBOPACK__default__export__;
+__turbopack_context__.s([
+    "defaultLocale",
+    0,
+    defaultLocale,
+    "object",
+    0,
+    object,
+    "string",
+    0,
+    string
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <locals>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <locals>");
+__turbopack_context__.s([
+    "defaultLocale",
+    0,
+    defaultLocale,
+    "object",
+    0,
+    object,
+    "string",
+    0,
+    string
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript)");
+__turbopack_context__.s([
+    "z",
+    0,
+    __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z members object>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z members string>
+;
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+__turbopack_context__.s([
+    "z",
+    0,
+    __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z members string>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z members defaultLocale>
+;
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+__turbopack_context__.s([
+    "z",
+    0,
+    __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z members defaultLocale>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+;
+console.log(__TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__.object(), __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1.string(), __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2.defaultLocale);
+__turbopack_context__.s([], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/index.js [test] (ecmascript)");
+}),
+]);
+
+//# sourceMappingURL=1jsg_tests_snapshot_ns-value-reexport_named-import_input_index_1a3a-lo.js.map

vercel/next.js

turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/output/1jsg_tests_snapshot_ns-value-reexport_named-import_input_index_1a3a-lo.js
@@ -1,0 +1,75 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push(["output/1jsg_tests_snapshot_ns-value-reexport_named-import_input_index_1a3a-lo.js",
+"[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/index.js [test] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/index.js [test] (ecmascript)
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z member object>
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript)
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <locals>
+;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/locales/en.js [test] (ecmascript)
+;
+const __TURBOPACK__default__export__ = 'EN_USED_MARKER';
+;
+;
+const object = ()=>'object';
+const string = ()=>'string';
+const defaultLocale = __TURBOPACK__default__export__;
+__turbopack_context__.s([
+    "defaultLocale",
+    0,
+    defaultLocale,
+    "object",
+    0,
+    object,
+    "string",
+    0,
+    string
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <locals>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__$3c$locals$3e$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <locals>");
+__turbopack_context__.s([
+    "defaultLocale",
+    0,
+    defaultLocale,
+    "object",
+    0,
+    object,
+    "string",
+    0,
+    string
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript)");
+__turbopack_context__.s([
+    "z",
+    0,
+    __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z member object>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__ = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z member string>
+;
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+__turbopack_context__.s([
+    "z",
+    0,
+    __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z member string>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+// MERGED MODULE: [project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z member defaultLocale>
+;
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+__turbopack_context__.s([
+    "z",
+    0,
+    __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2
+], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/external.js [test] (ecmascript) <export * as z member defaultLocale>");
+var __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2 = __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__;
+;
+console.log(__TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__.object(), __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__1.string(), __TURBOPACK__imported__module__$5b$project$5d2f$turbopack$2f$crates$2f$turbopack$2d$tests$2f$tests$2f$snapshot$2f$ns$2d$value$2d$reexport$2f$named$2d$import$2f$input$2f$external$2e$js__$5b$test$5d$__$28$ecmascript$29$__2.defaultLocale);
+__turbopack_context__.s([], "[project]/turbopack/crates/turbopack-tests/tests/snapshot/ns-value-reexport/named-import/input/index.js [test] (ecmascript)");
+}),
+]);
+
+//# sourceMappingURL=1jsg_tests_snapshot_ns-value-reexport_named-import_input_index_1a3a-lo.js.map