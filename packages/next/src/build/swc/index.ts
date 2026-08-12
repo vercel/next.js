lineaje-labs-gos/vercel-next.js

[turbopack] Add `clusters` to chunking heuristics

packages/next/src/build/swc/index.ts
@@ -1040,6 +1040,9 @@ function bindingToApi(
         ...nextConfigSerializable.experimental,
         turbopackChunkingHeuristics: {
           ...chunkingHeuristics,
+          clusters: chunkingHeuristics.clusters?.map((cluster: RegExp[]) =>
+            cluster.map(regexComponents)
+          ),
           priorityRoutes:
             chunkingHeuristics.priorityRoutes?.map(regexComponents),
         },

vercel/next.js

packages/next/src/build/swc/index.ts
@@ -1050,6 +1050,9 @@ function bindingToApi(
         ...nextConfigSerializable.experimental,
         turbopackChunking: {
           ...chunkingConfig,
+          clusters: chunkingConfig.clusters?.map((cluster: RegExp[]) =>
+            cluster.map(regexComponents)
+          ),
           priorityRoutes: chunkingConfig.priorityRoutes?.map(regexComponents),
         },
       }