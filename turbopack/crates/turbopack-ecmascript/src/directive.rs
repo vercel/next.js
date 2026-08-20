605425e139855c2664732bb814d0447512c05d00
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-08-20 15:35:48 +0200

    Turbopack: rename to use turbopack: no side effects (#94427)
    Consistent naming:
    - `"use turbopack no side effects"`
    - `"use turbopack: constants"`

--- turbopack/crates/turbopack-ecmascript/src/directive.rs
+++ turbopack/crates/turbopack-ecmascript/src/directive.rs
@@ -37,6 +37,8 @@ pub fn parse_module_turbopack_directives(program: &Program) -> TurbopackDirectiv
             && let Expr::Lit(Lit::Str(Str { value, .. })) = &*expr.expr
         {
             match &*value.to_string_lossy() {
+                "use turbopack: no side effects" => result.no_side_effects = true,
+                // Keep for backwards compatibility
                 "use turbopack no side effects" => result.no_side_effects = true,
                 "use turbopack: constants" => result.constants_module = true,
                 v if v.starts_with("use turbopack ") => {
