90f89c72f19486949343b471b54b6137481a8b32
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2026-08-26 22:19:09 +0200

    Use published version

--- test/production/deterministic-build/deployment-id.test.ts
+++ test/production/deterministic-build/deployment-id.test.ts
@@ -243,7 +243,7 @@ async function runTest(
         // We use NEXT_TEST_PREFER_OFFLINE, so just declaring `vercel: latest` as a dependency still
         // doesn't force the latest version.
         // TODO: Go back to integration testing with latest once breakage in 59.6.2 is resolved.
-        buildCommand: 'pnpm dlx vercel@59.6.1 build',
+        buildCommand: 'pnpm dlx vercel@59.5.0 build',
         env:
           mode === 'adapter'
             ? {
