5dfb844f91309a753472ad1d118929d6ad8dc76b
Janka Uryga <lolzatu2@gmail.com>
2026-08-28 14:05:46 +0200

    add failing tests for RDC with PPF

---
+++ test/e2e/app-dir/resume-data-cache/resume-data-cache.partial-prefetching.test.ts
@@ -0,0 +1,2 @@
+process.env.__NEXT_PARTIAL_PREFETCHING = 'true'
+require('./resume-data-cache.test')
---
+++ test/e2e/app-dir/use-cache/use-cache.partial-prefetching.test.ts
@@ -0,0 +1,3 @@
+process.env.__NEXT_CACHE_COMPONENTS = 'true'
+process.env.__NEXT_PARTIAL_PREFETCHING = 'true'
+require('./use-cache.test')
