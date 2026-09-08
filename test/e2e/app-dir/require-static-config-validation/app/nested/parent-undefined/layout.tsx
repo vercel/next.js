a95e3b00bfa315b6abcb2800e87325cd5ed17fd5
Janka Uryga <lolzatu2@gmail.com>
2026-09-08 13:57:19 +0200

    [PPF] Validate unstable_requireStatic nesting

---
+++ test/e2e/app-dir/require-static-config-validation/app/nested/parent-undefined/layout.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react'
+
+// Deliberately no export
+// export const unstable_requireStatic = ...
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return children
+}
