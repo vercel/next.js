40026f4978373772f0e86458b4cec9014a2f3813
Steven <steven@ceriously.com>
2026-09-11 16:43:03 -0400

    fix tests

--- test/e2e/image-optimizer-internal-redirect/app/redirect-absolute/route.ts
+++ test/e2e/image-optimizer-internal-redirect/app/redirect-absolute/route.ts
@@ -1,8 +1,10 @@
+import { connection } from 'next/server'
+
 // Responds with a redirect (302) to an absolute URL on a different origin,
 // which is allowed by `images.remotePatterns` in `next.config.js`.
-export const dynamic = 'force-dynamic'
-
 export async function GET(req: Request) {
+  await connection()
+
   const { port } = new URL(req.url)
   return new Response(null, {
     status: 302,
--- test/e2e/image-optimizer-internal-redirect/app/redirect-relative/route.ts
+++ test/e2e/image-optimizer-internal-redirect/app/redirect-relative/route.ts
@@ -1,8 +1,10 @@
+import { connection } from 'next/server'
+
 // Responds with a redirect (302) to a static image on the same origin using a
 // relative `Location` header.
-export const dynamic = 'force-dynamic'
-
 export async function GET() {
+  await connection()
+
   return new Response(null, {
     status: 302,
     headers: { Location: '/test.png' },
