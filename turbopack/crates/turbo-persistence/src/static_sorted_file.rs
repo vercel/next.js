lineaje-labs-gos/vercel-next.js

Hoist key hashes into the variable key block offset table
A lookup in a variable-size key block cost two dependent loads per probe: read
the 4-byte table word, extract a position from its low 24 bits, then chase that
position into the payload to reach the hash. The payload address is not known
until the table word lands, so it cannot be prefetched, and the line it lands
on is mostly key and value bytes the comparison never reads.

Store the hash in the table slot instead, ahead of the type/position word:

  before: [type|pos]*4B   then [hash|key|val]...
  after:  [hash|type|pos]*12B then [key|val]...

compare_hash_key compares the hash first and only reaches for the key when two
hashes are equal, so with the hash in the table a binary search reads nothing
but that dense array. The payload is touched once on a match and never on a
miss.

Size is unchanged: the table grows by 8 bytes per entry and the payload shrinks
by the same, so this is a permutation like the fixed-block split in the parent
commit. KeyOnly blocks are untouched -- they have no hash to hoist -- so the
table stride is now derived from the layout via key_block_table_stride().

Both reader entry points gained the bounds check that only lookup_key_block
had; the iterator previously sliced the table without one.

sst_inspect decodes the table by hand to sample entry types and follows the new
stride, otherwise its type histograms would be garbage.

Verified with the existing suite and with --features verify_sst_content, plus a
scratch check that decoded a written block and confirmed the table really holds
sorted hashes -- with an assertion that a variable-with-hash block was actually
produced, since uniform keys take the fixed path and would have made it vacuous.

turbopack/crates/turbo-persistence/src/static_sorted_file.rs
@@ -109,6 +109,25 @@ pub const KEY_BLOCK_ENTRY_TYPE_INLINE_MIN: u8 = 8;
 pub const KEY_BLOCK_ENTRY_TYPE_KEY_VALUE_DELETED_MIN: u8 =
     KEY_BLOCK_ENTRY_TYPE_INLINE_MIN + MAX_INLINE_VALUE_SIZE as u8 + 1;
 
+/// Size of one variable-size key block offset table entry when the block stores no hash:
+/// 1 byte entry type packed into the top of a 3-byte in-block position.
+pub const KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH: usize = 4;
+/// Size of one variable-size key block offset table entry when the block stores a hash: the key's
+/// 8-byte hash followed by the type/position word.
+///
+/// The hash lives in the table rather than beside the key so that a binary search reads only this
+/// dense array — [`compare_hash_key`] compares the hash first and reaches for the key only when two
+/// hashes are equal, so the payload is touched once on a match and never on a miss. Total bytes are
+/// unchanged: the table grows by 8 per entry and the payload shrinks by the same.
+pub const KEY_BLOCK_TABLE_ENTRY_SIZE_WITH_HASH: usize =
+    KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH + size_of::<u64>();
+
+/// Bytes per offset table entry for a variable-size key block with the given hash length.
+#[inline(always)]
+pub fn key_block_table_stride(hash_len: u8) -> usize {
+    KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH + hash_len as usize
+}
+
 /// Encoded size of a small value reference: 2B block index + 2B size + 4B offset.
 pub(crate) const SMALL_VALUE_REF_SIZE: usize = 8;
 /// Encoded size of a medium value reference: 2B block index.
@@ -395,12 +414,13 @@ impl StaticSortedFile {
         ensure!(block.len() >= 4, "key block too short");
         let entry_count = be::read_u24(&block[1..]) as usize;
         let data = &block[4..];
+        let table_len = entry_count * key_block_table_stride(hash_len);
         ensure!(
-            data.len() >= entry_count * 4,
+            data.len() >= table_len,
             "key block too short for {entry_count} entries"
         );
-        let offsets = &data[..entry_count * 4];
-        let entries = &data[entry_count * 4..];
+        let offsets = &data[..table_len];
+        let entries = &data[table_len..];
 
         self.lookup_block_inner::<K, FIND_ALL>(
             &block,
@@ -830,8 +850,11 @@ enum CurrentKeyBlockKind {
 
 impl CurrentKeyBlockKind {
     /// Decodes entry `index`, dispatching on the block's entry layout.
+    ///
+    /// The result borrows from `entries` and, for a variable block storing hashes, from the offset
+    /// table held by `self` — hence the shared lifetime.
     fn entry<'l>(
-        &self,
+        &'l self,
         entries: &'l [u8],
         entry_count: u32,
         index: usize,
@@ -966,8 +989,13 @@ impl StaticSortedFileIter {
             )
         } else {
             let n = entry_count as usize;
-            let offsets = block.clone().slice(4..4 + n * 4);
-            let entries = block.slice(4 + n * 4..block_len);
+            let table_end = 4 + n * key_block_table_stride(hash_len);
+            ensure!(
+                block_len >= table_end,
+                "key block too short for {entry_count} entries"
+            );
+            let offsets = block.clone().slice(4..table_end);
+            let entries = block.slice(table_end..block_len);
             (CurrentKeyBlockKind::Variable { offsets }, entries)
         };
 
@@ -1139,10 +1167,14 @@ fn entry_val_size(ty: u8) -> Result<usize> {
 }
 
 /// Reads the type and start offset from an offset table entry.
-/// Each entry is 4 bytes: 1 byte type + 3 bytes BE offset.
+///
+/// The trailing 4 bytes of every entry pack 1 byte of type into the top of a 3-byte BE offset.
+/// `HashThenKey` entries carry the key's 8-byte hash ahead of that word — see
+/// [`KEY_BLOCK_TABLE_ENTRY_SIZE_WITH_HASH`].
 #[inline(always)]
-fn read_offset_entry(offsets: &[u8], index: usize) -> (u8, usize) {
-    let base = index * 4;
+fn read_offset_entry(offsets: &[u8], index: usize, table_stride: usize) -> (u8, usize) {
+    // The offset word is last, so skip any hash that precedes it.
+    let base = index * table_stride + (table_stride - KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH);
     let word = be::read_u32(&offsets[base..]);
     let ty = (word >> 24) as u8;
     let offset = (word & 0x00FF_FFFF) as usize;
@@ -1151,26 +1183,26 @@ fn read_offset_entry(offsets: &[u8], index: usize) -> (u8, usize) {
 
 /// Reads a key entry from a key block.
 fn get_key_entry<'l>(
-    offsets: &[u8],
+    offsets: &'l [u8],
     entries: &'l [u8],
     entry_count: usize,
     index: usize,
     hash_len: u8,
 ) -> Result<GetKeyEntryResult<'l>> {
-    let hash_len_usize = hash_len as usize;
-    let (ty, start) = read_offset_entry(offsets, index);
+    let table_stride = key_block_table_stride(hash_len);
+    let (ty, start) = read_offset_entry(offsets, index, table_stride);
     let end = if index == entry_count - 1 {
         entries.len()
     } else {
-        let (_, next_start) = read_offset_entry(offsets, index + 1);
+        let (_, next_start) = read_offset_entry(offsets, index + 1, table_stride);
         next_start
     };
-    // Return the raw hash bytes slice (0-8 bytes depending on hash_len)
-    let hash = &entries[start..start + hash_len_usize];
+    // Hoisted into the table, so the search never reaches into the payload; empty for `KeyOnly`.
+    let hash = &offsets[index * table_stride..index * table_stride + hash_len as usize];
     let val_size = entry_val_size(ty)?;
     Ok(GetKeyEntryResult {
         hash,
-        key: &entries[start + hash_len_usize..end - val_size],
+        key: &entries[start..end - val_size],
         ty,
         val: &entries[end - val_size..end],
     })

vercel/next.js

turbopack/crates/turbo-persistence/src/static_sorted_file.rs
@@ -111,6 +111,25 @@ pub const KEY_BLOCK_ENTRY_TYPE_INLINE_MIN: u8 = 8;
 pub const KEY_BLOCK_ENTRY_TYPE_KEY_VALUE_DELETED_MIN: u8 =
     KEY_BLOCK_ENTRY_TYPE_INLINE_MIN + MAX_INLINE_VALUE_SIZE as u8 + 1;
 
+/// Size of one variable-size key block offset table entry when the block stores no hash:
+/// 1 byte entry type packed into the top of a 3-byte in-block position.
+pub const KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH: usize = 4;
+/// Size of one variable-size key block offset table entry when the block stores a hash: the key's
+/// 8-byte hash followed by the type/position word.
+///
+/// The hash lives in the table rather than beside the key so that a binary search reads only this
+/// dense array — [`compare_hash_key`] compares the hash first and reaches for the key only when two
+/// hashes are equal, so the payload is touched once on a match and never on a miss. Total bytes are
+/// unchanged: the table grows by 8 per entry and the payload shrinks by the same.
+pub const KEY_BLOCK_TABLE_ENTRY_SIZE_WITH_HASH: usize =
+    KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH + size_of::<u64>();
+
+/// Bytes per offset table entry for a variable-size key block with the given hash length.
+#[inline(always)]
+pub fn key_block_table_stride(hash_len: u8) -> usize {
+    KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH + hash_len as usize
+}
+
 /// Encoded size of a small value reference: 2B block index + 2B size + 4B offset.
 pub(crate) const SMALL_VALUE_REF_SIZE: usize = 8;
 /// Encoded size of a medium value reference: 2B block index.
@@ -465,12 +484,13 @@ impl StaticSortedFile {
         ensure!(block.len() >= 4, "key block too short");
         let entry_count = be::read_u24(&block[1..]) as usize;
         let data = &block[4..];
+        let table_len = entry_count * key_block_table_stride(hash_len);
         ensure!(
-            data.len() >= entry_count * 4,
+            data.len() >= table_len,
             "key block too short for {entry_count} entries"
         );
-        let offsets = &data[..entry_count * 4];
-        let entries = &data[entry_count * 4..];
+        let offsets = &data[..table_len];
+        let entries = &data[table_len..];
 
         self.lookup_block_inner::<K, FIND_ALL>(
             &block,
@@ -1064,8 +1084,11 @@ enum CurrentKeyBlockKind {
 
 impl CurrentKeyBlockKind {
     /// Decodes entry `index`, dispatching on the block's entry layout.
+    ///
+    /// The result borrows from `entries` and, for a variable block storing hashes, from the offset
+    /// table held by `self` — hence the shared lifetime.
     fn entry<'l>(
-        &self,
+        &'l self,
         entries: &'l [u8],
         entry_count: u32,
         index: usize,
@@ -1242,8 +1265,11 @@ impl StaticSortedFileIter {
             )
         } else {
             let offset_table_begin = 4usize;
-            let offset_table_end = offset_table_begin + (entry_count as usize) * 4;
-            // In variable blocks the offsets table starts immediately after the entry count
+            let offset_table_end = 4 + (entry_count as usize) * key_block_table_stride(hash_len);
+            ensure!(
+                block_len >= offset_table_end,
+                "key block too short for {entry_count} entries"
+            );
             let offsets = block.clone().slice(offset_table_begin..offset_table_end);
             let entries = block.slice(offset_table_end..block_len);
             (CurrentKeyBlockKind::Variable { offsets }, entries)
@@ -1420,10 +1446,14 @@ fn entry_val_size(ty: u8) -> Result<usize> {
 }
 
 /// Reads the type and start offset from an offset table entry.
-/// Each entry is 4 bytes: 1 byte type + 3 bytes BE offset.
+///
+/// The trailing 4 bytes of every entry pack 1 byte of type into the top of a 3-byte BE offset.
+/// `HashThenKey` entries carry the key's 8-byte hash ahead of that word — see
+/// [`KEY_BLOCK_TABLE_ENTRY_SIZE_WITH_HASH`].
 #[inline(always)]
-fn read_offset_entry(offsets: &[u8], index: usize) -> (u8, usize) {
-    let base = index * 4;
+fn read_offset_entry(offsets: &[u8], index: usize, table_stride: usize) -> (u8, usize) {
+    // The offset word is last, so skip any hash that precedes it.
+    let base = index * table_stride + (table_stride - KEY_BLOCK_TABLE_ENTRY_SIZE_NO_HASH);
     let word = be::read_u32(&offsets[base..]);
     let ty = (word >> 24) as u8;
     let offset = (word & 0x00FF_FFFF) as usize;
@@ -1432,26 +1462,26 @@ fn read_offset_entry(offsets: &[u8], index: usize) -> (u8, usize) {
 
 /// Reads a key entry from a key block.
 fn get_key_entry<'l>(
-    offsets: &[u8],
+    offsets: &'l [u8],
     entries: &'l [u8],
     entry_count: usize,
     index: usize,
     hash_len: u8,
 ) -> Result<GetKeyEntryResult<'l>> {
-    let hash_len_usize = hash_len as usize;
-    let (ty, start) = read_offset_entry(offsets, index);
+    let table_stride = key_block_table_stride(hash_len);
+    let (ty, start) = read_offset_entry(offsets, index, table_stride);
     let end = if index == entry_count - 1 {
         entries.len()
     } else {
-        let (_, next_start) = read_offset_entry(offsets, index + 1);
+        let (_, next_start) = read_offset_entry(offsets, index + 1, table_stride);
         next_start
     };
-    // Return the raw hash bytes slice (0-8 bytes depending on hash_len)
-    let hash = &entries[start..start + hash_len_usize];
+    // Hoisted into the table, so the search never reaches into the payload; empty for `KeyOnly`.
+    let hash = &offsets[index * table_stride..index * table_stride + hash_len as usize];
     let val_size = entry_val_size(ty)?;
     Ok(GetKeyEntryResult {
         hash,
-        key: &entries[start + hash_len_usize..end - val_size],
+        key: &entries[start..end - val_size],
         ty,
         val: &entries[end - val_size..end],
     })