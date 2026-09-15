eda5e636e76bd1f95736483254253efb86caf0e9
Joseph Chamochumbi <joseph.chamochumbi@vercel.com>
2026-09-15 17:59:11 +0200

    fix(next/image): copy image config arrays before sorting

--- packages/next/src/client/legacy/image.tsx
+++ packages/next/src/client/legacy/image.tsx
@@ -705,8 +705,10 @@ export default function Image({
   const config: ImageConfig = useMemo(() => {
     const c = configEnv || configContext || imageConfigDefault
     const allSizes = [...c.deviceSizes, ...c.imageSizes].sort((a, b) => a - b)
-    const deviceSizes = c.deviceSizes.sort((a, b) => a - b)
-    const qualities = c.qualities?.sort((a, b) => a - b)
+    const deviceSizes = [...c.deviceSizes].sort((a, b) => a - b)
+    const qualities = c.qualities
+      ? [...c.qualities].sort((a, b) => a - b)
+      : undefined
     return {
       ...c,
       allSizes,
