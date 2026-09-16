ceef22ba5b2b3f583dc04f2dfeb3f8c67a64a00d
Hendrik Liebau <mail@hendrik-liebau.de>
2026-09-16 11:39:05 +0200

    Revert "Fix App Router locale path matching with Pages i18n (#98095)"
    This reverts commit 1d5dd6720010520d7d8bc1350183300234d3bd18.

--- packages/next/src/server/lib/router-server.ts
+++ packages/next/src/server/lib/router-server.ts
@@ -745,9 +745,6 @@ export async function initialize(opts: {
           handleIndex,
           {
             invokeOutput: matchedOutput.itemPath,
-            ...(matchedOutput.didMatchLocalePrefixedPath
-              ? { didMatchLocalePrefixedPath: true }
-              : undefined),
             ...(matchedOutput.error
               ? {
                   invokeStatus: 500,
