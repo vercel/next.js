d2356d4fb0b220e12321c2f26f0060127599d6d9
Will Binns-Smith <wbinnssmith@gmail.com>
2026-09-16 10:21:38 -0700

    Use internal prefix for Turbopack shutdown env

--- packages/next/src/server/dev/hot-reloader-turbopack.ts
+++ packages/next/src/server/dev/hot-reloader-turbopack.ts
@@ -569,7 +569,7 @@ export async function createHotReloaderTurbopack(
   opts.onDevServerCleanup?.(async () => {
     setBundlerFindSourceMapImplementation(() => undefined)
     setBundlerFindSourceMapURLImplementation(() => null)
-    if (process.env.NEXT_DEV_WAIT_FOR_TURBOPACK_SHUTDOWN === '1') {
+    if (process.env.__NEXT_DEV_WAIT_FOR_TURBOPACK_SHUTDOWN === '1') {
       await project.shutdown()
     } else {
       await project.onExit()
