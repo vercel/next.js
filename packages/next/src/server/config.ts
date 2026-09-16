5347559767db169bf0a77b3c8d7773ec2762ce47
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2026-09-16 14:44:18 -0700

    Remove path resolution from assignDefaultsAndValidate in typescript, let canonicalization happen in rust

--- packages/next/src/server/config.ts
+++ packages/next/src/server/config.ts
@@ -1191,20 +1191,6 @@ function assignDefaultsAndValidate(
     }
   }
 
-  if (result?.experimental?.turbopackAdditionalRoots) {
-    result.experimental.turbopackAdditionalRoots = Object.fromEntries(
-      Object.entries(result.experimental.turbopackAdditionalRoots).map(
-        ([key, root]) => [
-          key,
-          {
-            ...root,
-            path: isAbsolute(root.path) ? root.path : resolve(root.path),
-          },
-        ]
-      )
-    )
-  }
-
   if (result?.turbopack?.chunkLoadingGlobal) {
     const g = result.turbopack.chunkLoadingGlobal
     if (!g.startsWith('TURBOPACK_')) {
