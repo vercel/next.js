fdff41a5006c04146630e10cdc784938e6be93e6
Jamiboy Mohammad <jamiboym@gmail.com>
2026-09-17 15:51:08 -0700

    test: enable deploy coverage for asset and compiler errors

--- test/e2e/script-loader/partytown-missing.test.ts
+++ test/e2e/script-loader/partytown-missing.test.ts
@@ -1,21 +1,19 @@
 import { nextTestSetup } from 'e2e-utils'
 import { join } from 'path'
 
+// @force-gate !dev
 describe('script-loader - partytown-missing', () => {
-  const { next, isNextDev } = nextTestSetup({
+  const { next } = nextTestSetup({
     files: join(__dirname, 'partytown-missing'),
     skipStart: true,
-    // Vercel deployment fails to build/deploy this fixture in CI; skip in deploy mode.
-    skipDeployment: true,
   })
 
   it('Error message is shown if Partytown is not installed locally', async () => {
-    if (isNextDev) return
-
-    const { cliOutput } = await next.build()
+    await expect(next.start()).rejects.toThrow()
+    const cliOutput = next.cliOutput
 
     expect(cliOutput.replace(/[\n\r]/g, '')).toMatch(
       /It looks like you're trying to use Partytown with next\/script but do not have the required package\(s\) installed.Please install Partytown by running:.*?(npm|pnpm|yarn) (install|add) (--save-dev|--dev) @builder\.io\/partytownIf you are not trying to use Partytown, please disable the experimental "nextScriptWorkers" flag in next\.config\.js\./
     )
-  })
+  }, 240_000)
 })
