8a92f33d76e1296a48e9158b166be8e9a9cdd93e
Jamiboy Mohammad <jamiboym@gmail.com>
2026-09-17 15:45:46 -0700

    test: enable deploy coverage for TypeScript config errors

--- test/e2e/app-dir/next-config-ts-native-mts/type-error/next-config-ts-type-error-cjs.test.ts
+++ test/e2e/app-dir/next-config-ts-native-mts/type-error/next-config-ts-type-error-cjs.test.ts
@@ -7,15 +7,10 @@ describe('next-config-ts-type-error-cjs', () => {
     return
   }
 
-  const { next, isNextDev, skipped } = nextTestSetup({
+  const { next, isNextDev } = nextTestSetup({
     files: __dirname,
     skipStart: true,
-    skipDeployment: true,
   })
-
-  if (skipped) {
-    return
-  }
 
   it('should throw with type error on build (CJS)', async () => {
     if (isNextDev) {
@@ -23,10 +18,11 @@ describe('next-config-ts-type-error-cjs', () => {
       const $ = await next.render$('/')
       expect($('p').text()).toBe('foo')
     } else {
-      const { cliOutput } = await next.build()
-      await expect(cliOutput).toContain(
+      await expect(next.start()).rejects.toThrow()
+      const cliOutput = next.cliOutput
+      expect(cliOutput).toContain(
         `Type 'string' is not assignable to type 'number'.`
       )
     }
-  })
+  }, 240_000)
 })
