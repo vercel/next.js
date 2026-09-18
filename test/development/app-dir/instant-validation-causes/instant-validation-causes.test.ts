d362fc3f6224e7f166abe0f0cd4e91331e4bcb3c
Jamiboy Mohammad <jamiboym@gmail.com>
2026-09-18 12:50:15 -0700

    Move whole development-only suites out of e2e

--- test/development/app-dir/instant-validation-causes/instant-validation-causes.test.ts
+++ test/development/app-dir/instant-validation-causes/instant-validation-causes.test.ts
@@ -2,18 +2,13 @@ import { nextTestSetup } from 'e2e-utils'
 import { createGetInstantInsight } from 'e2e-utils/instant-validation'
 
 describe('instant validation causes', () => {
-  const { next, skipped, isNextDev } = nextTestSetup({
+  const { next } = nextTestSetup({
     files: __dirname,
-    skipDeployment: true,
+
     env: {
       NEXT_TEST_LOG_VALIDATION: '1',
     },
   })
-  if (skipped) return
-  if (!isNextDev) {
-    it.skip('Only implemented in dev', () => {})
-    return
-  }
 
   let currentCliOutputIndex = 0
   beforeEach(() => {
--- test/development/app-dir/webpack-loader-errors/webpack-loader-errors.test.ts
+++ test/development/app-dir/webpack-loader-errors/webpack-loader-errors.test.ts
@@ -3,17 +3,11 @@ import { retry, waitForRedbox, getRedboxSource } from 'next-test-utils'
 import stripAnsi from 'strip-ansi'
 
 describe('webpack-loader-errors', () => {
-  const { next, isNextDev, isTurbopack, skipped } = nextTestSetup({
+  const { next, isTurbopack } = nextTestSetup({
     files: __dirname,
-    skipDeployment: true,
+
     skipStart: true,
   })
-  if (skipped) return
-
-  if (!isNextDev) {
-    it('should skip in non-dev mode', () => {})
-    return
-  }
 
   beforeAll(async () => {
     await next.start()
